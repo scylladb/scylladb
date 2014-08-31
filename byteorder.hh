93804c2b25868b27d0e3e84220ad65624d639cc7
Avi Kivity <avi@cloudius-systems.com>
2014-08-31 13:34:08 +0300

    Split byte-order support to new file byteorder.hh

---
+++ byteorder.hh
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ */
+
+#ifndef BYTEORDER_HH_
+#define BYTEORDER_HH_
+
+#include <arpa/inet.h>  // for ntohs() and friends
+
+inline uint64_t ntohq(uint64_t v) {
+    return __builtin_bswap64(v);
+}
+
+namespace net {
+
+inline void ntoh() {}
+inline void hton() {}
+
+inline void ntoh(uint16_t& x) { x = ntohs(x); }
+inline void hton(uint16_t& x) { x = htons(x); }
+inline void ntoh(uint32_t& x) { x = ntohl(x); }
+inline void hton(uint32_t& x) { x = htonl(x); }
+
+// Wrapper around a primitive type to provide an unaligned version.
+// This is because gcc (correctly) doesn't allow binding an unaligned
+// scalar variable to a reference, and (unfortunately) doesn't allow
+// specifying unaligned references.
+//
+// So, packed<uint32_t>& is our way of passing a reference (or pointer)
+// to a uint32_t around, without losing the knowledge about its alignment
+// or lack thereof.
+template <typename T>
+struct packed {
+    T raw;
+    packed() = default;
+    packed(T x) : raw(x) {}
+    packed& operator=(const T& x) { raw = x; return *this; }
+    operator T() const { return raw; }
+} __attribute__((packed));
+
+template <typename T>
+inline void ntoh(packed<T>& x) {
+    T v = x;
+    ntoh(v);
+    x = v;
+}
+
+template <typename T>
+inline void hton(packed<T>& x) {
+    T v = x;
+    hton(v);
+    x = v;
+}
+
+template <typename First, typename... Rest>
+inline
+void ntoh(First& first, Rest&... rest) {
+    ntoh(first);
+    ntoh(std::forward<Rest&>(rest)...);
+}
+
+template <typename First, typename... Rest>
+inline
+void hton(First& first, Rest&... rest) {
+    hton(first);
+    hton(std::forward<Rest&>(rest)...);
+}
+
+template <class T>
+inline
+void ntoh(T& x) {
+    x.adjust_endianness([] (auto&... what) { ntoh(std::forward<decltype(what)&>(what)...); });
+}
+
+template <class T>
+inline
+void hton(T& x) {
+    x.adjust_endianness([] (auto&... what) { hton(std::forward<decltype(what)&>(what)...); });
+}
+
+}
+
+#endif /* BYTEORDER_HH_ */
--- ip.hh
+++ ip.hh
@@ -9,81 +9,13 @@
 #include <arpa/inet.h>
 #include <cstdint>
 #include <array>
+#include "byteorder.hh"
 
 namespace net {
 
 using ethernet_address = std::array<uint8_t, 6>;
 
-inline uint64_t ntohq(uint64_t v) {
-    return __builtin_bswap64(v);
-}
-
 uint16_t ip_checksum(void* data, size_t len);
-
-inline void ntoh() {}
-inline void hton() {}
-
-inline void ntoh(uint16_t& x) { x = ntohs(x); }
-inline void hton(uint16_t& x) { x = htons(x); }
-inline void ntoh(uint32_t& x) { x = ntohl(x); }
-inline void hton(uint32_t& x) { x = htonl(x); }
-
-// Wrapper around a primitive type to provide an unaligned version.
-// This is because gcc (correctly) doesn't allow binding an unaligned
-// scalar variable to a reference, and (unfortunately) doesn't allow
-// specifying unaligned references.
-//
-// So, packed<uint32_t>& is our way of passing a reference (or pointer)
-// to a uint32_t around, without losing the knowledge about its alignment
-// or lack thereof.
-template <typename T>
-struct packed {
-    T raw;
-    packed() = default;
-    packed(T x) : raw(x) {}
-    packed& operator=(const T& x) { raw = x; return *this; }
-    operator T() const { return raw; }
-} __attribute__((packed));
-
-template <typename T>
-inline void ntoh(packed<T>& x) {
-    T v = x;
-    ntoh(v);
-    x = v;
-}
-
-template <typename T>
-inline void hton(packed<T>& x) {
-    T v = x;
-    hton(v);
-    x = v;
-}
-
-template <typename First, typename... Rest>
-inline
-void ntoh(First& first, Rest&... rest) {
-    ntoh(first);
-    ntoh(std::forward<Rest&>(rest)...);
-}
-
-template <typename First, typename... Rest>
-inline
-void hton(First& first, Rest&... rest) {
-    hton(first);
-    hton(std::forward<Rest&>(rest)...);
-}
-
-template <class T>
-inline
-void ntoh(T& x) {
-    x.adjust_endianness([] (auto&... what) { ntoh(std::forward<decltype(what)&>(what)...); });
-}
-
-template <class T>
-inline
-void hton(T& x) {
-    x.adjust_endianness([] (auto&... what) { hton(std::forward<decltype(what)&>(what)...); });
-}
 
 struct eth_hdr {
     ethernet_address dst_mac;
