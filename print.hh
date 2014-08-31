9e16c5ab62d77872bcfd726e7e8e5b0958e3d48f
Avi Kivity <avi@cloudius-systems.com>
2014-08-31 20:18:09 +0300

    Add print() helper
    Forwards everything to boost::format, but with a nicer syntax.

---
+++ print.hh
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ */
+
+#ifndef PRINT_HH_
+#define PRINT_HH_
+
+#include <boost/format.hpp>
+#include <iostream>
+
+inline void
+apply_format(boost::format& fmt) {
+}
+
+template <typename A0, typename... Arest>
+inline void
+apply_format(boost::format& fmt, A0&& a0, Arest&&... arest) {
+    apply_format(fmt % std::forward<A0>(a0), std::forward<Arest>(arest)...);
+}
+
+template <typename... A>
+std::ostream&
+fprint(std::ostream& os, boost::format& fmt, A&&... a) {
+    apply_format(fmt, std::forward<A>(a)...);
+    return os << fmt;
+}
+
+template <typename... A>
+void
+print(boost::format& fmt, A&&... a) {
+    fprint(std::cout, fmt, std::forward<A>(a)...);
+}
+
+template <typename... A>
+std::ostream&
+fprint(std::ostream& os, const char* fmt, A&&... a) {
+    boost::format bfmt(fmt);
+    return fprint(os, bfmt, std::forward<A>(a)...);
+}
+
+template <typename... A>
+void
+print(const char* fmt, A&&... a) {
+    boost::format bfmt(fmt);
+    return print(bfmt, std::forward<A>(a)...);
+}
+
+#endif /* PRINT_HH_ */
