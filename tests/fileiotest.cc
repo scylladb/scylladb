89147b253278266f3fec6645941e741a18c52e95
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-10-01 01:04:14 -0300

    fileiotest: verify read size
    add assert to dma_read return
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>

--- tests/fileiotest.cc
+++ tests/fileiotest.cc
@@ -27,7 +27,8 @@ int main(int ac, char** av) {
                     auto rbuf = allocate_aligned_buffer<unsigned char>(4096, 4096);
                     auto rb = rbuf.get();
                     ft->f.dma_read(i * 4096, rb, 4096).then(
-                            [ft, i, rbuf = std::move(rbuf), wbuf = std::move(wbuf)] (size_t) mutable {
+                            [ft, i, rbuf = std::move(rbuf), wbuf = std::move(wbuf)] (size_t ret) mutable {
+                        assert(ret == 4096);
                         bool eq = std::equal(rbuf.get(), rbuf.get() + 4096, wbuf.get());
                         assert(eq);
                         ft->sem.signal(1);
