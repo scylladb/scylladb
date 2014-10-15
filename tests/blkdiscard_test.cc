9ef466bae2fa1af327e35d4b90656abca180d8d8
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-10-15 15:18:54 -0300

    blkdiscard_test: switch to app-template
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>

--- tests/blkdiscard_test.cc
+++ tests/blkdiscard_test.cc
@@ -3,8 +3,7 @@
  */
 
 #include <algorithm>
-#include <boost/program_options.hpp>
-#include "core/reactor.hh"
+#include "core/app-template.hh"
 
 struct file_test {
     file_test(file&& f) : f(std::move(f)) {}
@@ -12,46 +11,36 @@ struct file_test {
     semaphore sem = { 0 };
 };
 
-namespace po = boost::program_options;
-
 int main(int ac, char** av) {
-    po::options_description desc("Allowed options");
-    desc.add_options()
-        ("dev", po::value<std::string>(),
-            "e.g. --dev /dev/sdb");
-    po::variables_map vm;
-    po::store(po::parse_command_line(ac, av, desc), vm);
-    po::notify(vm);
+    app_template app;
+    app.add_options()
+        ("dev", bpo::value<std::string>(), "e.g. --dev /dev/sdb")
+        ;
 
-    const char *filepath = nullptr;
-    if (vm.count("dev")) {
-        filepath = vm["dev"].as<std::string>().c_str();
-    } else {
-        std::cerr << "usage: program --dev /dev/sdx\n";
-        ::exit(1);
-    }
+    return app.run(ac, av, [&app] {
+        static constexpr auto max = 10000;
+        auto&& config = app.configuration();
+        auto filepath = config["dev"].as<std::string>();
 
-    static constexpr auto max = 10000;
-    engine.open_file_dma(filepath).then([] (file f) {
-        auto ft = new file_test{std::move(f)};
+        engine.open_file_dma(filepath).then([] (file f) {
+            auto ft = new file_test{std::move(f)};
 
-        ft->f.stat().then([ft] (struct stat st) mutable {
-            assert(S_ISBLK(st.st_mode));
-            auto offset = 0;
-            auto length = max * 4096;
-            ft->f.discard(offset, length).then([ft] () mutable {
-                ft->sem.signal();
+            ft->f.stat().then([ft] (struct stat st) mutable {
+                assert(S_ISBLK(st.st_mode));
+                auto offset = 0;
+                auto length = max * 4096;
+                ft->f.discard(offset, length).then([ft] () mutable {
+                    ft->sem.signal();
+                });
+            });
+
+            ft->sem.wait().then([ft] () mutable {
+                return ft->f.flush();
+            }).then([ft] () mutable {
+                std::cout << "done\n";
+                delete ft;
+                engine.exit(0);
             });
         });
-
-        ft->sem.wait().then([ft] () mutable {
-            return ft->f.flush();
-        }).then([ft] () mutable {
-            std::cout << "done\n";
-            delete ft;
-            ::exit(0);
-        });
     });
-    engine.run();
-    return 0;
 }
