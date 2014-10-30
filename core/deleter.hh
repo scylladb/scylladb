8e38cb4159f9ac21cb22e84a93eb852695db97b5
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-10-30 16:58:07 +0100

    deleter: introduce append()
    It's to help chaining up deleters when appending a fragment with anq
    already created deleter.

--- core/deleter.hh
+++ core/deleter.hh
@@ -7,6 +7,7 @@
 
 #include <memory>
 #include <cstdlib>
+#include <assert.h>
 #include <type_traits>
 
 class deleter {
@@ -36,14 +37,21 @@ public:
         this->~deleter();
         new (this) deleter(i);
     }
+    void append(deleter d);
 private:
-    bool is_raw_object() const {
-        auto x = reinterpret_cast<uintptr_t>(_impl);
+    static bool is_raw_object(impl* i) {
+        auto x = reinterpret_cast<uintptr_t>(i);
         return x & 1;
     }
-    void* to_raw_object() const {
-        auto x = reinterpret_cast<uintptr_t>(_impl);
+    bool is_raw_object() const {
+        return is_raw_object(_impl);
+    }
+    static void* to_raw_object(impl* i) {
+        auto x = reinterpret_cast<uintptr_t>(i);
         return reinterpret_cast<void*>(x & ~uintptr_t(1));
+    }
+    void* to_raw_object() const {
+        return to_raw_object(_impl);
     }
     impl* from_raw_object(void* object) {
         auto x = reinterpret_cast<uintptr_t>(object);
@@ -86,10 +94,28 @@ struct lambda_deleter_impl final : deleter::impl {
     virtual ~lambda_deleter_impl() override { del(); }
 };
 
+template <typename Object>
+struct object_deleter_impl final : deleter::impl {
+    Object obj;
+    object_deleter_impl(deleter next, Object&& obj)
+        : impl(std::move(next)), obj(std::move(obj)) {}
+};
+
+template <typename Object>
+object_deleter_impl<Object>* make_object_deleter_impl(deleter next, Object obj) {
+    return new object_deleter_impl<Object>(std::move(next), std::move(obj));
+}
+
 template <typename Deleter>
 deleter
 make_deleter(deleter next, Deleter d) {
     return deleter(new lambda_deleter_impl<Deleter>(std::move(next), std::move(d)));
+}
+
+template <typename Deleter>
+deleter
+make_deleter(Deleter d) {
+    return make_deleter(deleter(), std::move(d));
 }
 
 struct free_deleter_impl final : deleter::impl {
@@ -109,6 +135,31 @@ deleter::share() {
     }
     ++_impl->refs;
     return deleter(_impl);
+}
+
+// Appends 'd' to the chain of deleters. Avoids allocation if possible. For
+// performance reasons the current chain should be shorter and 'd' should be
+// longer.
+inline
+void deleter::append(deleter d) {
+    if (!d._impl) {
+        return;
+    }
+    impl* next_impl = _impl;
+    deleter* next_d = this;
+    while (next_impl) {
+        assert(next_impl != d._impl);
+        if (is_raw_object(next_impl)) {
+            next_d->_impl = next_impl = new free_deleter_impl(to_raw_object(next_impl));
+        }
+        if (next_impl->refs != 1) {
+            next_d->_impl = next_impl = make_object_deleter_impl(std::move(next_impl->next), deleter(next_impl));
+        }
+        next_d = &next_impl->next;
+        next_impl = next_d->_impl;
+    }
+    next_d->_impl = d._impl;
+    d._impl = nullptr;
 }
 
 inline
