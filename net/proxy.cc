af91a11b2bba788f7740eac39be56c5bbf3915fb
Gleb Natapov <gleb@cloudius-systems.com>
2014-10-30 16:32:08 +0200

    net: implement virtual packet queue in net::proxy::send()
    Currently net::proxy::send() waits for previous packet to be sent to
    another cpu before accepting next packet.  This slows down sender to much.
    This patch implement virtual queue that allows many packets to be send
    simultaneously but it starts to drop packets when number of outstanding
    packets goes over the limit, if we will try to queue them we will run
    out of memory if a sender generates packets faster that they can be
    sent. It also tries to generate back pressure by returning a feature
    that will become ready when queue goes under the limit, but it returns
    it only for a first sender that goes over it. Doing this for all callers
    may be an option too, but it is not clear which one and how many should
    we wake when queue goes under the limit again.

--- net/proxy.cc
+++ net/proxy.cc
@@ -6,10 +6,13 @@ namespace net {
 
 class proxy_net_device : public net::device {
 private:
+    static constexpr size_t _send_queue_length = 1000;
     boost::program_options::variables_map _opts;
     stream<packet> _rx_stream;
     future<> _rx_ready;
     net::hw_features _hw_features;
+    size_t _send_depth = 0;
+    promise<> _send_promise;
 public:
     explicit proxy_net_device(boost::program_options::variables_map opts);
     virtual subscription<packet> receive(std::function<future<> (packet)> next) override;
@@ -48,10 +51,25 @@ future<> proxy_net_device::l2inject(packet p)
 
 future<> proxy_net_device::send(packet p)
 {
-    // Assumes that there is only one virtio device and it is on cpu 0
-    return smp::submit_to(0, [p = std::move(p), cpu = engine._id]() mutable {
-        return dev->send(p.free_on_cpu(cpu));
-    });
+    if (_send_depth < _send_queue_length) {
+        _send_depth++;
+
+        // Assumes that there is only one virtio device and it is on cpu 0
+        smp::submit_to(0, [p = std::move(p), cpu = engine._id]() mutable {
+            return dev->send(p.free_on_cpu(cpu));
+        }).then([this] () {
+            if (_send_depth == _send_queue_length) {
+                _send_promise.set_value();
+            }
+            _send_depth--;
+        });
+
+        if (_send_depth == _send_queue_length) {
+            _send_promise = promise<>();
+            return _send_promise.get_future();
+        }
+    }
+    return make_ready_future();
 }
 
 std::unique_ptr<device> create_proxy_net_device(boost::program_options::variables_map opts) {
