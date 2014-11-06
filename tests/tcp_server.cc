2e9366ba2430ff2a35ba207a76c7da3b6ce74147
Asias He <asias@cloudius-systems.com>
2014-11-06 20:47:19 +0800

    tests: Add send test to tcp_server
    $ printf "txtx" | nc 192.168.66.123 10000
    
    Server will send a large mount of data to client for TCP tx testing.
    Currently, it sends 10MB of char 'X'.

--- tests/tcp_server.cc
+++ tests/tcp_server.cc
@@ -9,6 +9,15 @@
 #include <vector>
 #include <iostream>
 
+static std::string str_ping{"ping"};
+static std::string str_txtx{"txtx"};
+static std::string str_pong{"pong"};
+static std::string str_unknow{"unknow cmd"};
+static int tx_msg_total_size = 10 * 1024 * 1024;
+static int tx_msg_size = 4 * 1024;
+static int tx_msg_nr = tx_msg_total_size / tx_msg_size;
+static std::string str_txbuf(tx_msg_size, 'X');
+
 class http_server {
     std::vector<server_socket> _listeners;
 public:
@@ -55,17 +64,48 @@ public:
             if (_read_buf.eof()) {
                 return make_ready_future();
             }
-            // Expect a ping from client.
+            // Expect 4 bytes cmd from client
             size_t n = 4;
             return _read_buf.read_exactly(n).then([this] (temporary_buffer<char> buf) {
                 if (buf.size() == 0) {
                     return make_ready_future();
                 }
-                return _write_buf.write("pong", 4).then([this] {
-                    return _write_buf.flush();
-                }).then([this] {
-                    return this->read();
-                });
+                auto cmd = std::string(buf.get(), buf.size());
+                // pingpong test
+                if (cmd == str_ping) {
+                    return _write_buf.write(str_pong).then([this] {
+                        return _write_buf.flush();
+                    }).then([this] {
+                        return this->read();
+                    });
+                // server tx test
+                } else if (cmd == str_txtx) {
+                    return tx_test();
+                // unknow test
+                } else {
+                    return _write_buf.write(str_unknow).then([this] {
+                        return _write_buf.flush();
+                    }).then([this] {
+                        return make_ready_future();
+                    });
+                }
+            });
+        }
+        future<> do_write(int end) {
+            if (end == 0) {
+                return make_ready_future<>();
+            }
+            return _write_buf.write(str_txbuf).then([this, end] {
+                return _write_buf.flush();
+            }).then([this, end] {
+                return do_write(end - 1);
+            });
+        }
+        future<> tx_test() {
+            return do_write(tx_msg_nr).then([this] {
+                return _write_buf.close();
+            }).then([this] {
+                return make_ready_future<>();
             });
         }
     };
