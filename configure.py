300b310a2764284f62fdfc0d5fc6cfc63f2a67a8
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-11-24 20:10:39 -0200

    memcache: move ./memcached.cc to ./memcache.cc
    Actual purpose is explained by the subsequent commit.

--- configure.py
+++ configure.py
@@ -169,7 +169,7 @@ deps = {
     'apps/seastar/seastar': ['apps/seastar/main.cc'] + core,
     'tests/test-reactor': ['tests/test-reactor.cc'] + core,
     'apps/httpd/httpd': ['apps/httpd/httpd.cc', 'apps/httpd/request_parser.rl'] + libnet + core,
-    'apps/memcached/memcached': ['apps/memcached/memcached.cc'] + memcached,
+    'apps/memcached/memcached': ['apps/memcached/memcache.cc'] + memcached,
     'tests/memcached/test_ascii_parser': ['tests/memcached/test_ascii_parser.cc'] + memcached,
     'tests/fileiotest': ['tests/fileiotest.cc'] + core,
     'tests/virtiotest': ['tests/virtiotest.cc'] + core + libnet,
