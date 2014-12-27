48019c2df26e4e99628963e5caacbf0f60bd3d73
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-12-27 15:26:31 -0200

    memcache: fix cache::item_footprint()
    size of the sstring _ascii_prefix should also be added when computing
    item footprint. Without this change, reclaimer would end up evicting
    more than needed.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>

--- apps/memcached/memcache.cc
+++ apps/memcached/memcache.cc
@@ -733,7 +733,7 @@ private:
     memory::reclaimer _reclaimer;
 private:
     size_t item_footprint(item_type& item_ref) {
-        return sizeof(item_type) + item_ref._data.size() + item_ref.key().size();
+        return sizeof(item_type) + item_ref._data.size() + item_ref._ascii_prefix.size() + item_ref.key().size();
     }
 
     template <bool IsInCache = true, bool IsInTimerList = true>
