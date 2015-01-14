c3558f188be323af675d57c5206140ae77d101b8
Gleb Natapov <gleb@cloudius-systems.com>
2015-01-14 14:50:14 +0200

    replace ad-hoc index list generator with standard one

--- core/apply.hh
+++ core/apply.hh
@@ -6,32 +6,13 @@
 #define APPLY_HH_
 
 #include <tuple>
-
-template <size_t... I>
-struct index_list {
-};
-
-template <size_t Cur, size_t N, typename Result>
-struct index_list_helper;
-
-template <size_t N, size_t... I>
-struct index_list_helper<N, N, index_list<I...>> {
-    using type = index_list<I...>;
-};
-
-template <size_t Cur, size_t N, size_t... I>
-struct index_list_helper<Cur, N, index_list<I...>> {
-    using type = typename index_list_helper<Cur + 1, N, index_list<I..., Cur>>::type;
-};
-
-template <size_t N>
-using make_index_list = typename index_list_helper<0, N, index_list<>>::type;
+#include <utility>
 
 template <typename Func, typename Args, typename IndexList>
 struct apply_helper;
 
 template <typename Func, typename... T, size_t... I>
-struct apply_helper<Func, std::tuple<T...>, index_list<I...>> {
+struct apply_helper<Func, std::tuple<T...>, std::index_sequence<I...>> {
     static auto apply(Func func, std::tuple<T...> args) {
         return func(std::get<I>(std::move(args))...);
     }
@@ -39,7 +20,7 @@ struct apply_helper<Func, std::tuple<T...>, index_list<I...>> {
 
 template <typename Func, typename... T>
 auto apply(Func func, std::tuple<T...>&& args) {
-    using helper = apply_helper<Func, std::tuple<T...>, make_index_list<sizeof...(T)>>;
+    using helper = apply_helper<Func, std::tuple<T...>, std::index_sequence_for<T...>>;
     return helper::apply(std::move(func), std::move(args));
 }
 
