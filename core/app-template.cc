30aa81819a22459641ac70581aa8c713f723faf4
Avi Kivity <avi@cloudius-systems.com>
2015-01-15 10:46:44 +0200

    app: handle command line parsing errors

--- core/app-template.cc
+++ core/app-template.cc
@@ -38,13 +38,18 @@ app_template::run(int ac, char ** av, std::function<void ()>&& func) {
     print("WARNING: debug mode. Not for benchmarking or production\n");
 #endif
     bpo::variables_map configuration;
-    bpo::store(bpo::command_line_parser(ac, av).options(_opts).run(), configuration);
-    auto home = std::getenv("HOME");
-    if (home) {
-        std::ifstream ifs(std::string(home) + "/.config/seastar/seastar.conf");
-        if (ifs) {
-            bpo::store(bpo::parse_config_file(ifs, _opts), configuration);
+    try {
+        bpo::store(bpo::command_line_parser(ac, av).options(_opts).run(), configuration);
+        auto home = std::getenv("HOME");
+        if (home) {
+            std::ifstream ifs(std::string(home) + "/.config/seastar/seastar.conf");
+            if (ifs) {
+                bpo::store(bpo::parse_config_file(ifs, _opts), configuration);
+            }
         }
+    } catch (bpo::error& e) {
+        print("error: %s\n\nTry --help.\n", e.what());
+        return 2;
     }
     bpo::notify(configuration);
     if (configuration.count("help")) {
