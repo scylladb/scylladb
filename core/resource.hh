555977f5e6985312dcd8d7a0db4576975ccdd0fe
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-01-28 15:10:43 +0100

    core: drop BSD license text from resource.hh

--- core/resource.hh
+++ core/resource.hh
@@ -1,8 +1,5 @@
 /*
  * Copyright (C) 2014 Cloudius Systems, Ltd.
- *
- * This work is open source software, licensed under the terms of the
- * BSD license as described in the LICENSE file in the top-level directory.
  */
 
 #ifndef RESOURCE_HH_
