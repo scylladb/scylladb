88e276d2725f67823311708320ff3b79c2121ec0
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-02-03 11:21:37 +0100

    Introduce to_string() working on vectors of printable

---
+++ to_string.hh
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+
+#pragma once
+
+/**
+ * Converts a vector of pointers to Printable elements.
+ * Printable is an object which has to_string() method.
+ */
+template<typename PtrToPrintable>
+static inline
+sstring
+to_string(std::vector<PtrToPrintable> items) {
+    // TODO: optimize
+    std::ostringstream oss;
+    int left = items.size();
+    oss << "[";
+    for (auto&& item : items) {
+        oss << item->to_string();
+        if (left != 1) {
+            oss << ", ";
+        }
+        --left;
+    }
+    oss << "]";
+    return oss.str();
+}
