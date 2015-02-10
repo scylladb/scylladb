d7eb4e96fb3e251c33fe53523a44730255753d98
Avi Kivity <avi@cloudius-systems.com>
2015-02-10 19:02:19 +0200

    app-template: add support for positional options
    Example:
    
        app_template app;
        namespace bpo = boost::program_options;
        app.add_positional_options({
            { "file", bpo::value<std::string>(), "File to process", 1 },
        });

--- core/app-template.hh
+++ core/app-template.hh
@@ -11,10 +11,19 @@
 class app_template {
 private:
     boost::program_options::options_description _opts;
+    boost::program_options::positional_options_description _pos_opts;
     boost::optional<boost::program_options::variables_map> _configuration;
+public:
+    struct positional_option {
+        const char* name;
+        const boost::program_options::value_semantic* value_semantic;
+        const char* help;
+        int max_count;
+    };
 public:
     app_template();
     boost::program_options::options_description_easy_init add_options();
+    void add_positional_options(std::initializer_list<positional_option> options);
     boost::program_options::variables_map& configuration();
     int run(int ac, char ** av, std::function<void ()>&& func);
 };
