b3544238d1ea784734eec7ed20dd6fd42a4cdf61
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-02-10 16:26:21 +0100

    service: Implement query_state:: get_client_state() and get_timestamp()
    Needed by query_options and query_processor.

--- service/query_state.hh
+++ service/query_state.hh
@@ -1,10 +1,21 @@
 #ifndef SERVICE_QUERY_STATE_HH
 #define SERVICE_QUERY_STATE_HH
 
+#include "service/client_state.hh"
+
 namespace service {
 
-class query_state {
-    // FIXME: stub
+class query_state final {
+private:
+    client_state& _client_state;
+public:
+    query_state(client_state& client_state_) : _client_state(client_state_) {}
+    client_state& get_client_state() const {
+        return _client_state;
+    }
+    api::timestamp_type get_timestamp() {
+        return _client_state.get_timestamp();
+    }
 };
 
 }
