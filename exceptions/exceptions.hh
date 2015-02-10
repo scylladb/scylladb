09c54d256d9b39307863cba7dbcbf5f6d80017d7
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-02-10 09:14:08 +0100

    excpetions: Convert ExceptionCode

--- exceptions/exceptions.hh
+++ exceptions/exceptions.hh
@@ -26,6 +26,29 @@ public:
     { }
 };
 
+enum exception_code {
+    SERVER_ERROR    = 0x0000,
+    PROTOCOL_ERROR  = 0x000A,
+
+    BAD_CREDENTIALS = 0x0100,
+
+    // 1xx: problem during request execution
+    UNAVAILABLE     = 0x1000,
+    OVERLOADED      = 0x1001,
+    IS_BOOTSTRAPPING= 0x1002,
+    TRUNCATE_ERROR  = 0x1003,
+    WRITE_TIMEOUT   = 0x1100,
+    READ_TIMEOUT    = 0x1200,
+
+    // 2xx: problem validating the request
+    SYNTAX_ERROR    = 0x2000,
+    UNAUTHORIZED    = 0x2100,
+    INVALID         = 0x2200,
+    CONFIG_ERROR    = 0x2300,
+    ALREADY_EXISTS  = 0x2400,
+    UNPREPARED      = 0x2500
+};
+
 }
 
 #endif
