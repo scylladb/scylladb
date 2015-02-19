7f8d88371aaf72125b6dd5b6e7c1cfb33d86f470
Avi Kivity <avi@cloudius-systems.com>
2015-02-19 10:08:12 +0200

    Add LICENSE, NOTICE, and copyright headers to all source files.
    The two files imported from the OSv project retain their original licenses.

--- core/xen/osv_xen.hh
+++ core/xen/osv_xen.hh
@@ -1,3 +1,20 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 #ifndef _XEN_HH
 #define _XEN_HH
 // Those should come directly from the OSv three. However, the xen exported
