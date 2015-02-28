81ca06fa36368799bf7fd627e7d5003b9c0008f3
Avi Kivity <avi@cloudius-systems.com>
2015-02-28 23:50:36 +0200

    sstable: fix TOC size check
    Make sure we detect a too-large TOC correctly.

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -325,7 +325,7 @@ future<> sstable::read_toc() {
             // This file is supposed to be very small. Theoretically we should check its size,
             // but if we so much as read a whole page from it, there is definitely something fishy
             // going on - and this simplifies the code.
-            if (size > 4096) {
+            if (size >= 4096) {
                 throw malformed_sstable_exception("SSTable too big: " + to_sstring(size) + " bytes.");
             }
 
