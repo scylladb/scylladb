8e15ed452fd8ffe6c0369ad59b2e283d97487e09
Asias He <asias@cloudius-systems.com>
2015-03-04 16:08:10 +0800

    gossip: Add gms/gms.cc

---
+++ gms/gms.cc
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2015 Cloudius Systems, Ltd.
+ */
+
+// Used to ensure that all .hh files build, as well as a place to put
+// out-of-line implementations.
+
+#include "gms/application_state.hh"
+#include "gms/version_generator.hh"
+#include "gms/versioned_value.hh"
