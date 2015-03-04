41846256d86a3d35154b85b74d574fae6113cf00
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2015-03-03 23:18:30 -0300

    tests: fix fileiotest
    engine() cannot be called before the local_engine was constructed because it
    dereferences the pointer local_engine to create a reference.
    Consequently, the following error can be seen while running fileiotest:
    ./core/reactor.hh:854:13: runtime error: reference binding to null pointer of
    type 'struct reactor' ASAN:SIGSEGV
    
    Let's switch the test to use app_template.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>

--- tests/fileiotest.cc
+++ tests/fileiotest.cc
@@ -21,6 +21,7 @@
 
 #include <algorithm>
 #include "core/reactor.hh"
+#include "core/app-template.hh"
 
 struct file_test {
     file_test(file&& f) : f(std::move(f)) {}
@@ -30,40 +31,42 @@ struct file_test {
 };
 
 int main(int ac, char** av) {
-    static constexpr auto max = 10000;
-    engine().open_file_dma("testfile.tmp", open_flags::rw | open_flags::create).then([] (file f) {
-        auto ft = new file_test{std::move(f)};
-        for (size_t i = 0; i < max; ++i) {
-            ft->par.wait().then([ft, i] {
-                auto wbuf = allocate_aligned_buffer<unsigned char>(4096, 4096);
-                std::fill(wbuf.get(), wbuf.get() + 4096, i);
-                auto wb = wbuf.get();
-                ft->f.dma_write(i * 4096, wb, 4096).then(
-                        [ft, i, wbuf = std::move(wbuf)] (size_t ret) mutable {
-                    assert(ret == 4096);
-                    auto rbuf = allocate_aligned_buffer<unsigned char>(4096, 4096);
-                    auto rb = rbuf.get();
-                    ft->f.dma_read(i * 4096, rb, 4096).then(
-                            [ft, i, rbuf = std::move(rbuf), wbuf = std::move(wbuf)] (size_t ret) mutable {
+    app_template app;
+
+    return app.run(ac, av, [&app] {
+        static constexpr auto max = 10000;
+        engine().open_file_dma("testfile.tmp", open_flags::rw | open_flags::create).then([] (file f) {
+            auto ft = new file_test{std::move(f)};
+            for (size_t i = 0; i < max; ++i) {
+                ft->par.wait().then([ft, i] {
+                    auto wbuf = allocate_aligned_buffer<unsigned char>(4096, 4096);
+                    std::fill(wbuf.get(), wbuf.get() + 4096, i);
+                    auto wb = wbuf.get();
+                    ft->f.dma_write(i * 4096, wb, 4096).then(
+                            [ft, i, wbuf = std::move(wbuf)] (size_t ret) mutable {
                         assert(ret == 4096);
-                        bool eq = std::equal(rbuf.get(), rbuf.get() + 4096, wbuf.get());
-                        assert(eq);
-                        ft->sem.signal(1);
-                        ft->par.signal();
+                        auto rbuf = allocate_aligned_buffer<unsigned char>(4096, 4096);
+                        auto rb = rbuf.get();
+                        ft->f.dma_read(i * 4096, rb, 4096).then(
+                                [ft, i, rbuf = std::move(rbuf), wbuf = std::move(wbuf)] (size_t ret) mutable {
+                            assert(ret == 4096);
+                            bool eq = std::equal(rbuf.get(), rbuf.get() + 4096, wbuf.get());
+                            assert(eq);
+                            ft->sem.signal(1);
+                            ft->par.signal();
+                        });
                     });
                 });
+            }
+            ft->sem.wait(max).then([ft] () mutable {
+                return ft->f.flush();
+            }).then([ft] () mutable {
+                std::cout << "done\n";
+                delete ft;
+                engine().exit(0);
             });
-        }
-        ft->sem.wait(max).then([ft] () mutable {
-            return ft->f.flush();
-        }).then([ft] () mutable {
-            std::cout << "done\n";
-            delete ft;
-            ::exit(0);
         });
     });
-    engine().run();
-    return 0;
 }
 
 
