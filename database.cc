0f1b6b079a4698d991a9e4fd7d7e286808b84ccf
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-03-04 17:44:59 +0100

    schema: Store partition_key_prefix_type
    single_column_primary_key_restrictions may generate partition key
    prefixes.

--- database.cc
+++ database.cc
@@ -55,7 +55,8 @@ schema::schema(sstring ks_name, sstring cf_name, std::vector<column> partition_k
         , cf_name(std::move(cf_name))
         , partition_key_type(::make_shared<tuple_type<>>(get_column_types(partition_key)))
         , clustering_key_type(::make_shared<tuple_type<>>(get_column_types(clustering_key)))
-        , clustering_key_prefix_type(::make_shared<tuple_prefix>(get_column_types(clustering_key)))
+        , clustering_key_prefix_type(::make_shared(clustering_key_type->as_prefix()))
+        , partition_key_prefix_type(::make_shared(partition_key_type->as_prefix()))
         , regular_column_name_type(regular_column_name_type)
 {
     if (partition_key.size() == 1) {
