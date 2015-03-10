8fd570d854429f9ab02062f6b66bf6e411767ab3
Gleb Natapov <gleb@cloudius-systems.com>
2015-03-10 12:51:12 +0200

    core: use perfect forwarding for function object in apply()

--- core/apply.hh
+++ core/apply.hh
@@ -30,30 +30,30 @@ struct apply_helper;
 
 template <typename Func, typename Tuple, size_t... I>
 struct apply_helper<Func, Tuple, std::index_sequence<I...>> {
-    static auto apply(Func func, Tuple args) {
+    static auto apply(Func&& func, Tuple args) {
         return func(std::get<I>(std::forward<Tuple>(args))...);
     }
 };
 
 template <typename Func, typename... T>
 inline
-auto apply(Func func, std::tuple<T...>&& args) {
+auto apply(Func&& func, std::tuple<T...>&& args) {
     using helper = apply_helper<Func, std::tuple<T...>&&, std::index_sequence_for<T...>>;
-    return helper::apply(std::move(func), std::move(args));
+    return helper::apply(std::forward<Func>(func), std::move(args));
 }
 
 template <typename Func, typename... T>
 inline
-auto apply(Func func, std::tuple<T...>& args) {
+auto apply(Func&& func, std::tuple<T...>& args) {
     using helper = apply_helper<Func, std::tuple<T...>&, std::index_sequence_for<T...>>;
-    return helper::apply(std::move(func), args);
+    return helper::apply(std::forward<Func>(func), args);
 }
 
 template <typename Func, typename... T>
 inline
-auto apply(Func func, const std::tuple<T...>& args) {
+auto apply(Func&& func, const std::tuple<T...>& args) {
     using helper = apply_helper<Func, const std::tuple<T...>&, std::index_sequence_for<T...>>;
-    return helper::apply(std::move(func), args);
+    return helper::apply(std::forward<Func>(func), args);
 }
 
 #endif /* APPLY_HH_ */
