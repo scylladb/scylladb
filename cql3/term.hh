33f6703be6b4995b1d973d17457232047c3c6e6a
Avi Kivity <avi@cloudius-systems.com>
2015-03-11 13:08:06 +0200

    cql3: move enable_shared_from_this up the cql3::term hierarchy
    Casting upwards past an enable_shared_from_this confuses shared_ptr,
    so move it to the base of the hierarchy.

--- cql3/term.hh
+++ cql3/term.hh
@@ -44,7 +44,7 @@ class term;
  * from a raw term (Term.Raw) by poviding the actual receiver to which the term is supposed to be a
  * value of.
  */
-class term {
+class term : public ::enable_shared_from_this<term> {
 public:
     virtual ~term() {}
 
@@ -137,13 +137,13 @@ public:
      * Note that a terminal term will always have been type checked, and thus
      * consumer can (and should) assume so.
      */
-    class terminal : public term, public ::enable_shared_from_this<terminal> {
+    class terminal : public term {
     public:
         virtual void collect_marker_specification(::shared_ptr<variable_specifications> bound_names) {
         }
 
         virtual ::shared_ptr<terminal> bind(const query_options& options) override {
-            return this->shared_from_this();
+            return static_pointer_cast<terminal>(this->shared_from_this());
         }
 
         virtual bool uses_function(const sstring& ks_name, const sstring& function_name) const override {
