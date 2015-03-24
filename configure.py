606368f6f64570c2d115e7151725256897e4dcea
Avi Kivity <avi@cloudius-systems.com>
2015-03-24 10:43:48 +0200

    shared_ptr: add debug option for detecting copies on wrong cpus
    Track the cpu on which the reference count referred to by shared_ptr was
    created, and prevent modifying it on the wrong cpu.

--- configure.py
+++ configure.py
@@ -105,7 +105,7 @@ modes = {
     'debug': {
         'sanitize': '-fsanitize=address -fsanitize=leak -fsanitize=undefined',
         'sanitize_libs': '-lubsan -lasan',
-        'opt': '-O0 -DDEBUG -DDEFAULT_ALLOCATOR',
+        'opt': '-O0 -DDEBUG -DDEBUG_SHARED_PTR -DDEFAULT_ALLOCATOR',
         'libs': '',
     },
     'release': {
