2bcc36813812b9b528a6d4e24617420a7dc87852
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-03-26 20:31:26 +0100

    db: Move implementations to source file

--- database.hh
+++ database.hh
@@ -211,69 +211,16 @@ public:
         , key(std::move(key_))
         , p(schema)
     { }
-
     mutation(mutation&&) = default;
     mutation(const mutation&) = default;
-
-    void set_static_cell(const column_definition& def, atomic_cell_or_collection value) {
-        update_column(p.static_row(), def, std::move(value));
-    }
-
-    void set_clustered_cell(const exploded_clustering_prefix& prefix, const column_definition& def, atomic_cell_or_collection value) {
-        auto& row = p.clustered_row(clustering_key::from_clustering_prefix(*schema, prefix));
-        update_column(row, def, std::move(value));
-    }
-
-    void set_clustered_cell(const clustering_key& key, const column_definition& def, atomic_cell_or_collection value) {
-        auto& row = p.clustered_row(key);
-        update_column(row, def, std::move(value));
-    }
-    void set_cell(const exploded_clustering_prefix& prefix, const bytes& name, const boost::any& value, api::timestamp_type timestamp, ttl_opt ttl = {}) {
-        auto column_def = schema->get_column_definition(name);
-        if (!column_def) {
-            throw std::runtime_error(sprint("no column definition found for '%s'", name));
-        }
-        return set_cell(prefix, *column_def, atomic_cell::make_live(timestamp, ttl, column_def->type->decompose(value)));
-    }
-    void set_cell(const exploded_clustering_prefix& prefix, const column_definition& def, atomic_cell_or_collection value) {
-        if (def.is_static()) {
-            set_static_cell(def, std::move(value));
-        } else if (def.is_regular()) {
-            set_clustered_cell(prefix, def, std::move(value));
-        } else {
-            throw std::runtime_error("attemting to store into a key cell");
-        }
-    }
-    auto get_cell(const clustering_key& rkey, const column_definition& def)
-            -> std::experimental::optional<atomic_cell_or_collection> {
-        auto find_cell = [&def] (row& r) {
-            auto i = r.find(def.id);
-            if (i == r.end()) {
-                return std::experimental::optional<atomic_cell_or_collection>{};
-            }
-            return std::experimental::optional<atomic_cell_or_collection>{i->second};
-        };
-        if (def.is_static()) {
-            return find_cell(p.static_row());
-        } else {
-            auto r = p.find_row(rkey);
-            if (!r) {
-                return {};
-            }
-            return find_cell(*r);
-        }
-    }
+    void set_static_cell(const column_definition& def, atomic_cell_or_collection value);
+    void set_clustered_cell(const exploded_clustering_prefix& prefix, const column_definition& def, atomic_cell_or_collection value);
+    void set_clustered_cell(const clustering_key& key, const column_definition& def, atomic_cell_or_collection value);
+    void set_cell(const exploded_clustering_prefix& prefix, const bytes& name, const boost::any& value, api::timestamp_type timestamp, ttl_opt ttl = {});
+    void set_cell(const exploded_clustering_prefix& prefix, const column_definition& def, atomic_cell_or_collection value);
+    std::experimental::optional<atomic_cell_or_collection> get_cell(const clustering_key& rkey, const column_definition& def);
 private:
-    static void update_column(row& row, const column_definition& def, atomic_cell_or_collection&& value) {
-        // our mutations are not yet immutable
-        auto id = def.id;
-        auto i = row.lower_bound(id);
-        if (i == row.end() || i->first != id) {
-            row.emplace_hint(i, id, std::move(value));
-        } else {
-            merge_column(def, i->second, value);
-        }
-    }
+    static void update_column(row& row, const column_definition& def, atomic_cell_or_collection&& value);
     friend std::ostream& operator<<(std::ostream& os, const mutation& m);
 };
 
