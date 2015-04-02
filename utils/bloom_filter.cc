b86218b2ef485a0d900c4f92e1e284c660e47e51
Glauber Costa <glommer@cloudius-systems.com>
2015-04-02 16:10:47 -0400

    bloom filter
    This comes from Origin, but the changes I had to do are quite large.
    These files also represents many files, but I found it to be inconvenient
    to keep all the originals, simply because we would end up with way too many
    files: one .cc and one .hh per filter + an enveloping .hh so users could include
    without knowing which filter to use.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

---
+++ utils/bloom_filter.cc
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright 2015 Cloudius Systems
+ *
+ * Modified by Cloudius Systems
+ */
+
+#include "i_filter.hh"
+#include "fb_utilities.hh"
+#include "bytes.hh"
+#include "utils/murmur_hash.hh"
+#include "core/shared_ptr.hh"
+#include <boost/dynamic_bitset.hpp>
+#include <array>
+#include <cstdlib>
+#include "bloom_filter.hh"
+
+namespace utils {
+namespace filter {
+static thread_local auto reusable_indexes = std::vector<long>();
+
+void bloom_filter::set_indexes(uint64_t base, uint64_t inc, int count, long max, std::vector<long>& results) {
+    for (int i = 0; i < count; i++) {
+        results[i] = abs(base % max);
+        base += inc;
+    }
+}
+
+std::vector<long> bloom_filter::get_hash_buckets(const bytes_view& key, int hash_count, long max) {
+    std::array<uint64_t, 2> h;
+    hash(key, 0, h);
+
+    auto indexes = std::make_unique<std::vector<long>>();
+
+    indexes->resize(hash_count);
+    set_indexes(h[0], h[1], hash_count, max, *indexes);
+    return *(indexes.release());
+}
+
+std::vector<long> bloom_filter::indexes(const bytes_view& key) {
+    // we use the same array both for storing the hash result, and for storing the indexes we return,
+    // so that we do not need to allocate two arrays.
+    auto& idx = reusable_indexes;
+    std::array<uint64_t, 2> h;
+    hash(key, 0, h);
+
+    idx.resize(_hash_count);
+    set_indexes(h[0], h[1], _hash_count, _bitset.size(), idx);
+    return idx;
+}
+
+filter_ptr create_filter(int hash, boost::dynamic_bitset<uint64_t>&& bitset) {
+    return std::make_unique<murmur3_bloom_filter>(hash, std::move(bitset));
+}
+
+filter_ptr create_filter(int hash, long num_elements, int buckets_per) {
+    long num_bits = (num_elements * buckets_per) + bloom_calculations::EXCESS;
+    boost::dynamic_bitset<uint64_t> bitset(num_bits);
+    return std::make_unique<murmur3_bloom_filter>(hash, std::move(bitset));
+}
+}
+}
---
+++ utils/bloom_filter.hh
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright 2015 Cloudius Systems
+ *
+ * Modified by Cloudius Systems
+ *
+ * This file contains code from a variety of .java files from the
+ * Cassandra project, adapted to our needs.
+ */
+#pragma once
+#include "i_filter.hh"
+#include "utils/murmur_hash.hh"
+#include <boost/dynamic_bitset.hpp>
+
+#include <vector>
+
+namespace utils {
+namespace filter {
+
+class bloom_filter: public i_filter {
+public:
+    using bitmap_block = uint64_t;
+    using bitmap = boost::dynamic_bitset<bitmap_block>;
+
+private:
+    bitmap _bitset;
+    int _hash_count;
+
+    void set_indexes(uint64_t base, uint64_t inc, int count, long max, std::vector<long>& results);
+    std::vector<long> get_hash_buckets(const bytes_view& key, int hash_count, long max);
+    std::vector<long> indexes(const bytes_view& key);
+
+public:
+    int num_hashes() { return _hash_count; }
+    bitmap& bits() { return _bitset; }
+
+    bloom_filter(int hashes, bitmap&& bs) : _bitset(bs), _hash_count(hashes) {}
+    virtual void hash(const bytes_view& b, long seed, std::array<uint64_t, 2>& result) = 0;
+
+    virtual void add(const bytes_view& key) override {
+
+        auto idx = indexes(key);
+        for (int i = 0; i < _hash_count; i++) {
+            _bitset.set(idx[i]);
+        }
+    }
+
+    virtual bool is_present(const bytes_view& key) override {
+
+        auto idx = indexes(key);
+        for (int i = 0; i < _hash_count; i++) {
+            if (!_bitset.test(idx[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    virtual void clear() override {
+        _bitset.clear();
+    }
+
+    virtual void close() override { }
+};
+
+struct murmur3_bloom_filter: public bloom_filter {
+
+    murmur3_bloom_filter(int hashes, bitmap&& bs) : bloom_filter(hashes, std::move(bs)) {}
+
+    virtual void hash(const bytes_view& b, long seed, std::array<uint64_t, 2>& result) {
+        utils::murmur_hash::hash3_x64_128(b, seed, result);
+    }
+};
+
+struct always_present_filter: public i_filter {
+
+    virtual bool is_present(const bytes_view& key) override {
+        return true;
+    }
+
+    virtual void add(const bytes_view& key) override { }
+
+    virtual void clear() override { }
+
+    virtual void close() override { }
+};
+
+filter_ptr create_filter(int hash, boost::dynamic_bitset<uint64_t>&& bitset);
+filter_ptr create_filter(int hash, long num_elements, int buckets_per);
+}
+}
