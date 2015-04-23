fe4b33053eca8663bc9a6c4d66e773f881eb8e63
Glauber Costa <glommer@cloudius-systems.com>
2015-04-23 18:04:24 -0400

    sstable_test: move some interesting functions to a header file
    This test is too big already. The mutation tests would be better off in their own files
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- tests/urchin/sstable_test.cc
+++ tests/urchin/sstable_test.cc
@@ -15,70 +15,12 @@
 #include "tests/test-utils.hh"
 #include "schema.hh"
 #include <memory>
+#include "sstable_test.hh"
 
 using namespace sstables;
 
-static auto la = sstable::version_types::la;
-static auto big = sstable::format_types::big;
-
-using sstable_ptr = lw_shared_ptr<sstable>;
-
 bytes as_bytes(const sstring& s) {
     return { reinterpret_cast<const int8_t*>(s.begin()), s.size() };
-}
-
-namespace sstables {
-
-class test {
-    sstable_ptr _sst;
-public:
-
-    test(sstable_ptr s) : _sst(s) {}
-
-    summary& _summary() {
-        return _sst->_summary;
-    }
-
-    future<temporary_buffer<char>> data_read(uint64_t pos, size_t len) {
-        return _sst->data_read(pos, len);
-    }
-    future<index_list> read_indexes(uint64_t position, uint64_t quantity) {
-        return _sst->read_indexes(position, quantity);
-    }
-
-    future<> read_statistics() {
-        return _sst->read_statistics();
-    }
-
-    statistics& get_statistics() {
-        return _sst->_statistics;
-    }
-
-    future<> read_summary() {
-        return _sst->read_summary();
-    }
-
-    future<summary_entry&> read_summary_entry(size_t i) {
-        return _sst->read_summary_entry(i);
-    }
-
-    summary& get_summary() {
-        return _sst->_summary;
-    }
-
-    future<> read_toc() {
-        return _sst->read_toc();
-    }
-
-    auto& get_components() {
-        return _sst->_components;
-    }
-
-    template <typename T>
-    int binary_search(const T& entries, const key& sk) {
-        return _sst->binary_search(entries, sk);
-    }
-};
 }
 
 static future<> broken_sst(sstring dir, unsigned long generation) {
@@ -94,18 +36,6 @@ static future<> broken_sst(sstring dir, unsigned long generation) {
         }
         return make_ready_future<>();
     });
-}
-
-static future<sstable_ptr> reusable_sst(sstring dir, unsigned long generation) {
-    auto sst = make_lw_shared<sstable>(dir, generation, la, big);
-    auto fut = sst->load();
-    return std::move(fut).then([sst = std::move(sst)] {
-        return make_ready_future<sstable_ptr>(std::move(sst));
-    });
-}
-
-static future<> working_sst(sstring dir, unsigned long generation) {
-    return reusable_sst(dir, generation).then([] (auto ptr) { return make_ready_future<>(); });
 }
 
 SEASTAR_TEST_CASE(empty_toc) {
@@ -682,88 +612,6 @@ SEASTAR_TEST_CASE(deleted_cell_read) {
             });
         });
     });
-}
-
-
-schema_ptr composite_schema() {
-    static thread_local auto s = make_lw_shared(schema({}, "tests", "composite",
-        // partition key
-        {{"name", bytes_type}, {"col1", bytes_type}},
-        // clustering key
-        {},
-        // regular columns
-        {},
-        // static columns
-        {},
-        // regular column name type
-        utf8_type,
-        // comment
-        "Table with a composite key as pkey"
-       ));
-    return s;
-}
-
-schema_ptr set_schema() {
-    auto my_set_type = set_type_impl::get_instance(bytes_type, false);
-    static thread_local auto s = make_lw_shared(schema({}, "tests", "set_pk",
-        // partition key
-        {{"ss", my_set_type}},
-        // clustering key
-        {},
-        // regular columns
-        {
-            {"ns", utf8_type},
-        },
-        // static columns
-        {},
-        // regular column name type
-        utf8_type,
-        // comment
-        "Table with a set as pkeys"
-       ));
-    return s;
-}
-
-schema_ptr map_schema() {
-    auto my_map_type = map_type_impl::get_instance(bytes_type, bytes_type, false);
-    static thread_local auto s = make_lw_shared(schema({}, "tests", "map_pk",
-        // partition key
-        {{"ss", my_map_type}},
-        // clustering key
-        {},
-        // regular columns
-        {
-            {"ns", utf8_type},
-        },
-        // static columns
-        {},
-        // regular column name type
-        utf8_type,
-        // comment
-        "Table with a map as pkeys"
-       ));
-    return s;
-}
-
-schema_ptr list_schema() {
-    auto my_list_type = list_type_impl::get_instance(bytes_type, false);
-    static thread_local auto s = make_lw_shared(schema({}, "tests", "list_pk",
-        // partition key
-        {{"ss", my_list_type}},
-        // clustering key
-        {},
-        // regular columns
-        {
-            {"ns", utf8_type},
-        },
-        // static columns
-        {},
-        // regular column name type
-        utf8_type,
-        // comment
-        "Table with a list as pkeys"
-       ));
-    return s;
 }
 
 
