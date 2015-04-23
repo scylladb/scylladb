6c07f4ae5d132c136029328846ee9ecf11ca4304
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-04-23 09:51:14 +0200

    Introduce database_fwd.hh with forward declarations

---
+++ database_fwd.hh
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+
+#pragma once
+
+// database.hh
+class database;
+
+// mutation.hh
+class mutation;
+
+// schema.hh
+class schema;
+class column_definition;
+
+// keys.hh
+class exploded_clustering_prefix;
+class partition_key;
+class clustering_key;
+class clustering_key_prefix;
