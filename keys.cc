0bc00713739661a25c9cc9cbf9c7c1fe197b51ac
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-05-06 09:36:02 +0200

    keys: Introduce view wrappers
    We now have partition_key_view, clustering_key_view, etc.
    Database APIs will be extended to also accept views.
    
    This will alows us to avoid allocations in certain scenarios.

--- keys.cc
+++ keys.cc
@@ -17,3 +17,14 @@ std::ostream& operator<<(std::ostream& out, const clustering_key& ck) {
 std::ostream& operator<<(std::ostream& out, const clustering_key_prefix& ckp) {
     return out << "ckp{" << to_hex(ckp) << "}";
 }
+
+const legacy_compound_view<partition_key_view::c_type>
+partition_key_view::legacy_form(const schema& s) const {
+    return { *get_compound_type(s), _bytes };
+}
+
+int
+partition_key_view::legacy_tri_compare(const schema& s, const partition_key& o) const {
+    auto cmp = legacy_compound_view<c_type>::tri_comparator(*get_compound_type(s));
+    return cmp(this->representation(), o.representation());
+}
