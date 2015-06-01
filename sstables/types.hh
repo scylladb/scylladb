53a26a5966ba3cc05a6917c5ff527cb565648aea
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2015-06-01 17:13:07 -0300

    sstables: move disk_* types to a header
    That's needed to avoid circular dependencies of header files.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>

--- sstables/types.hh
+++ sstables/types.hh
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "disk_types.hh"
 #include "core/enum.hh"
 #include "bytes.hh"
 #include "gc_clock.hh"
@@ -13,39 +14,6 @@
 #include <type_traits>
 
 namespace sstables {
-
-// Some in-disk structures have an associated integer (of varying sizes) that
-// represents how large they are. They can be a byte-length, in the case of a
-// string, number of elements, in the case of an array, etc.
-//
-// For those elements, we encapsulate the underlying type in an outter
-// structure that embeds how large is the in-disk size. It is a lot more
-// convenient to embed it in the size than explicitly writing it in the parser.
-// This way, we don't need to encode this information in multiple places at
-// once - it is already part of the type.
-template <typename Size>
-struct disk_string {
-    bytes value;
-    explicit operator bytes_view() const {
-        return value;
-    }
-};
-
-template <typename Size>
-struct disk_string_view {
-    bytes_view value;
-};
-
-template <typename Size, typename Members>
-struct disk_array {
-    static_assert(std::is_integral<Size>::value, "Length type must be convertible to integer");
-    std::vector<Members> elements;
-};
-
-template <typename Size, typename Key, typename Value>
-struct disk_hash {
-    std::unordered_map<Key, Value, std::hash<Key>> map;
-};
 
 struct option {
     disk_string<uint16_t> key;
