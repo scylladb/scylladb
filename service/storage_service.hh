77e8f361bb7e4dc1457fdc8cbd931bfb63226047
Asias He <asias@cloudius-systems.com>
2015-06-03 10:24:24 +0800

    storage_service: Reduce time for non-seed node to join the ring
    Waiting for 30 seconds is way too long for testing. Reduce it to 5
    seconds.
    
    When we have a proper config system, we can specify in cmdline.

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -83,7 +83,7 @@ private:
         }
         else
 #endif
-            return 30 * 1000;
+            return 5 * 1000;
     }
     /* This abstraction maintains the token/endpoint metadata information */
     token_metadata _token_metadata;
