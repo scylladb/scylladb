3e31c56ed9374fa890f60c105de715013d9fce23
Shlomi Livne <shlomi@cloudius-systems.com>
2015-06-15 15:33:40 +0300

    Support snitch registration on long and short format
    Snitches can be inialized based on short and long name format. In case a
    short name is provided the "org.apache.cassandra.locator." is
    contacenated to it. Out solution is simpler with double regitration.
    Default cassandra.yaml includes "endpoint_snitch: SimpleSnitch"
    
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>

--- locator/rack_inferring_snitch.cc
+++ locator/rack_inferring_snitch.cc
@@ -2,5 +2,6 @@
 
 namespace locator {
 using registry = class_registrator<i_endpoint_snitch, rack_inferring_snitch>;
-static registry registrator("org.apache.cassandra.locator.RackInferringSnitch");
+static registry registrator1("org.apache.cassandra.locator.RackInferringSnitch");
+static registry registrator2("RackInferringSnitch");
 }
