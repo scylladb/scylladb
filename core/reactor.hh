31d38012153173e082fac2c287906974599aa3ec
Avi Kivity <avi@cloudius-systems.com>
2015-06-15 16:38:52 +0300

    reactor: wire up shutdown() on pollable_fd

--- core/reactor.hh
+++ core/reactor.hh
@@ -174,6 +174,7 @@ public:
     future<size_t> recvmsg(struct msghdr *msg);
     future<size_t> sendto(socket_address addr, const void* buf, size_t len);
     file_desc& get_file_desc() const { return _s->fd; }
+    void shutdown(int how) { _s->fd.shutdown(how); }
     void close() { _s.reset(); }
 protected:
     int get_fd() const { return _s->fd.get(); }
