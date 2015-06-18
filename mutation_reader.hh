b35bc8e75ae5065d07b6113aa6c7e54cb70c7362
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-06-18 10:55:49 +0200

    mutation_reader: State monotonicy requirement in the interafce contract
    Currently column_family::for_all_partitions() relies on monotonicity
    of keys. Adding strict monotonicity requirement doesn't hurt
    implementaitons, but makes some consumers simpler.

--- mutation_reader.hh
+++ mutation_reader.hh
@@ -13,6 +13,10 @@
 // the function to get a future for the next mutation, with an unset optional
 // marking the end of iteration. After calling mutation_reader's operator(),
 // caller must keep the object alive until the returned future is fulfilled.
+//
+// The mutations returned have strictly monotonically increasing keys. Two
+// consecutive mutations never have equal keys.
+//
 // TODO: When iterating over mutations, we don't need a schema_ptr for every
 // single one as it is normally the same for all of them. So "mutation" might
 // not be the optimal object to use here.
