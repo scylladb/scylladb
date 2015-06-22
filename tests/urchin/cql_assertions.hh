26ff34060dadc016734f50b90a04a70cab34ef3e
Paweł Dziepak <pdziepak@cloudius-systems.com>
2015-06-22 09:58:18 +0200

    tests/cql: add assert_that_failed()
    Signed-off-by: Paweł Dziepak <pdziepak@cloudius-systems.com>

--- tests/urchin/cql_assertions.hh
+++ tests/urchin/cql_assertions.hh
@@ -3,6 +3,7 @@
 #include "transport/messages/result_message_base.hh"
 #include "bytes.hh"
 #include "core/shared_ptr.hh"
+#include "core/future.hh"
 
 class rows_assertions {
     shared_ptr<transport::messages::result_message::rows> _rows;
@@ -24,3 +25,14 @@ public:
 };
 
 result_msg_assertions assert_that(shared_ptr<transport::messages::result_message> msg);
+
+template<typename T>
+void assert_that_failed(future<T>& f)
+{
+    assert(f.failed());
+    try {
+        f.get();
+    }
+    catch (...) {
+    }
+}
