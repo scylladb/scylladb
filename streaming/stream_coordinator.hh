66a9f92df9a47e56bdce1e444d3a919d4cd1eb35
Asias He <asias@cloudius-systems.com>
2015-06-26 10:07:57 +0800

    streaming: Move stream_coordinator code to source file

--- streaming/stream_coordinator.hh
+++ streaming/stream_coordinator.hh
@@ -37,9 +37,11 @@ namespace streaming {
  * inbound StreamResultFuture context.
  */
 class stream_coordinator {
-    class host_streaming_data;
-private:
+public:
     using inet_address = gms::inet_address;
+
+private:
+    class host_streaming_data;
     std::map<inet_address, host_streaming_data> _peer_sessions;
     int _connections_per_host;
     bool _keep_ss_table_level;
@@ -61,19 +63,9 @@ public:
      */
     bool has_active_sessions();
 
-    std::vector<stream_session> get_all_stream_sessions() {
-        std::vector<stream_session> results;
-        for (auto& x : _peer_sessions) {
-            auto s = x.second.get_all_stream_sessions();
-            std::move(s.begin(), s.end(), std::back_inserter(results));
-        }
-        return results;
-    }
+    std::vector<stream_session> get_all_stream_sessions();
 
-    bool is_receiving() {
-        return _connections_per_host == 0;
-    }
-
+    bool is_receiving();
 #if 0
     public void connectAllStreamSessions()
     {
@@ -82,13 +74,7 @@ public:
     }
 #endif
 
-    std::set<inet_address> get_peers() {
-        std::set<inet_address> r;
-        for (auto& x : _peer_sessions) {
-            r.insert(x.first);
-        }
-        return r;
-    }
+    std::set<inet_address> get_peers();
 
 public:
     stream_session& get_or_create_next_session(inet_address peer, inet_address connecting) {
@@ -103,39 +89,12 @@ public:
         get_host_data(info.peer).update_progress(info);
     }
 
-    void add_session_info(session_info session) {
-        auto& data = get_or_create_host_data(session.peer);
-        data.add_session_info(std::move(session));
-    }
+    void add_session_info(session_info session);
 
-    std::vector<session_info> get_all_session_info() {
-        std::vector<session_info> result;
-        for (auto& x : _peer_sessions) {
-            auto s = x.second.get_all_session_info();
-            std::move(s.begin(), s.end(), std::back_inserter(result));
-        }
-        return result;
-    }
+    std::vector<session_info> get_all_session_info();
 
 public:
-    void transfer_files(inet_address to, std::vector<stream_session::ss_table_streaming_sections> sstable_details) {
-        host_streaming_data& session_list = get_or_create_host_data(to);
-        if (_connections_per_host > 1) {
-            abort();
-#if 0
-            List<List<StreamSession.SSTableStreamingSections>> buckets = sliceSSTableDetails(sstableDetails);
-
-            for (List<StreamSession.SSTableStreamingSections> subList : buckets)
-            {
-                StreamSession session = sessionList.get_or_create_next_session(to, to);
-                session.addTransferFiles(subList);
-            }
-#endif
-        } else {
-            auto& session = session_list.get_or_create_next_session(to, to);
-            session.add_transfer_files(sstable_details);
-        }
-    }
+    void transfer_files(inet_address to, std::vector<stream_session::ss_table_streaming_sections> sstable_details);
 
 private:
 #if 0
@@ -167,19 +126,9 @@ private:
     }
 
 #endif
-    host_streaming_data& get_host_data(inet_address peer) {
-        auto it = _peer_sessions.find(peer);
-        if (it == _peer_sessions.end()) {
-            throw std::runtime_error(sprint("Unknown peer requested: %s", peer));
-        }
-        return it->second;
-    }
+    host_streaming_data& get_host_data(inet_address peer);
 
-    host_streaming_data& get_or_create_host_data(inet_address peer) {
-        _peer_sessions[peer] = host_streaming_data(_connections_per_host, _keep_ss_table_level);
-        return _peer_sessions[peer];
-    }
-
+    host_streaming_data& get_or_create_host_data(inet_address peer);
 #if 0
     private static class StreamSessionConnector implements Runnable
     {
@@ -197,7 +146,6 @@ private:
         }
     }
 #endif
-
 private:
     class host_streaming_data {
         using inet_address = gms::inet_address;
@@ -218,21 +166,7 @@ private:
 
         bool has_active_sessions();
 
-        stream_session& get_or_create_next_session(inet_address peer, inet_address connecting) {
-            // create
-            int size = _stream_sessions.size();
-            if (size < _connections_per_host) {
-                auto session = stream_session(peer, connecting, size, _keep_ss_table_level);
-                _stream_sessions.emplace(++_last_returned, std::move(session));
-                return _stream_sessions[_last_returned];
-            // get
-            } else {
-                if (_last_returned >= (size - 1)) {
-                    _last_returned = 0;
-                }
-                return _stream_sessions[_last_returned++];
-            }
-        }
+        stream_session& get_or_create_next_session(inet_address peer, inet_address connecting);
 #if 0
         public void connectAllStreamSessions()
         {
@@ -242,41 +176,15 @@ private:
             }
         }
 #endif
+        std::vector<stream_session> get_all_stream_sessions();
 
-        std::vector<stream_session> get_all_stream_sessions() {
-            std::vector<stream_session> sessions;
-            for (auto& x : _stream_sessions) {
-                sessions.push_back(x.second);
-            }
-            return sessions;
-        }
+        stream_session& get_or_create_session_by_id(inet_address peer, int id, inet_address connecting);
 
-        stream_session& get_or_create_session_by_id(inet_address peer, int id, inet_address connecting) {
-            auto it = _stream_sessions.find(id);
-            if (it == _stream_sessions.end()) {
-                it = _stream_sessions.emplace(id, stream_session(peer, connecting, id, _keep_ss_table_level)).first;
-            }
-            return it->second;
-        }
+        void update_progress(progress_info info);
 
-        void update_progress(progress_info info) {
-            auto it = _session_infos.find(info.session_index);
-            if (it != _session_infos.end()) {
-                it->second.update_progress(std::move(info));
-            }
-        }
+        void add_session_info(session_info info);
 
-        void add_session_info(session_info info) {
-            _session_infos[info.session_index] = std::move(info);
-        }
-
-        std::vector<session_info> get_all_session_info() {
-            std::vector<session_info> sessions;
-            for (auto& x : _session_infos) {
-                sessions.push_back(x.second);
-            }
-            return sessions;
-        }
+        std::vector<session_info> get_all_session_info();
     };
 };
 
