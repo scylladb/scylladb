b211bfa514348458597df3fb20d65557efc746e0
Paweł Dziepak <pdziepak@cloudius-systems.com>
2015-07-02 16:46:20 +0200

    gc_clock: support adding an offset to the current time
    This, intended for tests, feature allows testing time related event without
    need for real time waits.
    
    Signed-off-by: Paweł Dziepak <pdziepak@cloudius-systems.com>

---
+++ gc_clock.cc
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+
+#include "gc_clock.hh"
+
+std::atomic<int64_t> clocks_offset;
--- gc_clock.hh
+++ gc_clock.hh
@@ -4,8 +4,17 @@
 
 #pragma once
 
+#include <atomic>
 #include <chrono>
 #include <experimental/optional>
+
+extern std::atomic<int64_t> clocks_offset;
+
+static inline std::chrono::seconds get_clocks_offset()
+{
+    auto off = clocks_offset.load(std::memory_order_relaxed);
+    return std::chrono::seconds(off);
+}
 
 // FIXME: wraps around in 2038
 class gc_clock {
@@ -17,8 +26,11 @@ public:
     using time_point = std::chrono::time_point<gc_clock, duration>;
 
     static time_point now() {
-        return time_point(std::chrono::duration_cast<duration>(base::now().time_since_epoch()));
+        return time_point(std::chrono::duration_cast<duration>(base::now().time_since_epoch())) + get_clocks_offset();
     }
+
+    // Intended for tests. Allows testing time related event without need
+    // for real time waits.
 };
 
 
@@ -27,3 +39,10 @@ using ttl_opt = std::experimental::optional<gc_clock::duration>;
 
 // 20 years in seconds
 static constexpr gc_clock::duration max_ttl = gc_clock::duration{20 * 365 * 24 * 60 * 60};
+
+template<typename Duration>
+static inline void forward_jump_clocks(Duration delta)
+{
+    auto d = std::chrono::duration_cast<std::chrono::seconds>(delta).count();
+    clocks_offset.fetch_add(d, std::memory_order_relaxed);
+}
