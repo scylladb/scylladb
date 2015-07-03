5545e08bf71af27e543782c323dcca3d36c7e770
Glauber Costa <glommer@cloudius-systems.com>
2015-07-03 18:11:58 -0400

    database: introduce flush method
    We will have to flush it from other places as well, so wrap the flushing code
    into a method - specially because the current code has issues and it will be
    easier to deal with it if it is in a single place.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- database.hh
+++ database.hh
@@ -150,6 +150,12 @@ public:
         seal_active_memtable(db);
         return _in_flight_seals.close();
     }
+
+    future<> flush(database* db) {
+        // FIXME: this will synchronously wait for this write to finish, but doesn't guarantee
+        // anything about previous writes.
+        return seal_active_memtable(db);
+    }
     // FIXME: this is just an example, should be changed to something more
     // general. compact_all_sstables() starts a compaction of all sstables.
     // It doesn't flush the current memtable first. It's just a ad-hoc method,
