f537c0f0ac51d72e37910ec92df7dd84ced084f2
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-07-03 17:45:47 +0200

    mutation_partition_serializer: Refactor the interface
    write() will now write framing information. It will be needed when
    serializing many mutations. The old version was left as
    write_without_framing().
    
    This change also makes the serializer interface closer to that of
    db::serializer<>. To write an object, the serializer instance is
    instantiated with the object and remembers the size, so that size
    calculation doesn't have to be repeated again for write().

--- mutation_partition_serializer.hh
+++ mutation_partition_serializer.hh
@@ -4,15 +4,27 @@
 
 #pragma once
 
-#include "database_fwd.hh"
+#include "utils/data_input.hh"
 #include "utils/data_output.hh"
+#include "database_fwd.hh"
 #include "mutation_partition_view.hh"
+#include "bytes_ostream.hh"
 
 class mutation_partition_serializer {
+    static size_t size(const schema&, const mutation_partition&);
+public:
+    using size_type = uint32_t;
+private:
+    const schema& _schema;
+    const mutation_partition& _p;
+    size_type _size;
 public:
     using count_type = uint32_t;
-
-    static size_t size(const schema& schema, const mutation_partition& p);
-    static void write(data_output& out, const schema& schema, const mutation_partition& p);
-    static mutation_partition_view view(bytes_view v) { return mutation_partition_view::from_bytes(v); }
+    mutation_partition_serializer(const schema&, const mutation_partition&);
+public:
+    size_t size() const { return _size + sizeof(size_type); }
+    size_t size_without_framing() const { return _size ; }
+    void write(data_output&) const;
+    void write_without_framing(data_output&) const;
+    void write(bytes_ostream&) const;
 };
