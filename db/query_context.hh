cb103449a54df137a33542153f94fec610470b5d
Glauber Costa <glommer@cloudius-systems.com>
2015-07-07 18:04:58 -0400

    db: move query_context to a header
    Do this so we can use it from legacy_schema_tables as well
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

---
+++ db/query_context.hh
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+#pragma once
+
+#include <memory>
+#include "core/sharded.hh"
+#include "core/future.hh"
+#include "cql3/query_processor.hh"
+
+class database;
+
+namespace db {
+struct query_context {
+    distributed<database>& _db;
+    distributed<cql3::query_processor>& _qp;
+    query_context(distributed<database>& db, distributed<cql3::query_processor>& qp) : _db(db), _qp(qp) {}
+
+    template <typename... Args>
+    future<::shared_ptr<cql3::untyped_result_set>> execute_cql(sstring text, sstring cf, Args&&... args) {
+        // FIXME: Would be better not to use sprint here.
+        sstring req = sprint(text, cf);
+        return this->_qp.local().execute_internal(req, { boost::any(std::forward<Args>(args))... });
+    }
+    database& db() {
+        return _db.local();
+    }
+};
+
+// This does not have to be thread local, because all cores will share the same context.
+extern std::unique_ptr<query_context> qctx;
+
+// Sometimes we are not concerned about system tables at all - for instance, when we are testing. In those cases, just pretend
+// we executed the query, and return an empty result
+template <typename... Args>
+static future<::shared_ptr<cql3::untyped_result_set>> execute_cql(sstring text, Args&&... args) {
+    if (qctx) {
+        return qctx->execute_cql(text, std::forward<Args>(args)...);
+    }
+    return make_ready_future<shared_ptr<cql3::untyped_result_set>>(::make_shared<cql3::untyped_result_set>(cql3::untyped_result_set::make_empty()));
+}
+}
--- db/system_keyspace.cc
+++ db/system_keyspace.cc
@@ -39,8 +39,12 @@
 #include "exceptions/exceptions.hh"
 #include "cql3/query_processor.hh"
 #include "db/serializer.hh"
+#include "query_context.hh"
 
 namespace db {
+
+std::unique_ptr<query_context> qctx = {};
+
 namespace system_keyspace {
 
 // Currently, the type variables (uuid_type, etc.) are thread-local reference-
@@ -311,35 +315,6 @@ schema_ptr built_indexes() {
         "historic sstable read rates"
         ));
     return sstable_activity;
-}
-
-struct query_context {
-    distributed<database>& _db;
-    distributed<cql3::query_processor>& _qp;
-    query_context(distributed<database>& db, distributed<cql3::query_processor>& qp) : _db(db), _qp(qp) {}
-
-    template <typename... Args>
-    future<::shared_ptr<cql3::untyped_result_set>> execute_cql(sstring text, sstring cf, Args&&... args) {
-        // FIXME: Would be better not to use sprint here.
-        sstring req = sprint(text, cf);
-        return this->_qp.local().execute_internal(req, { boost::any(std::forward<Args>(args))... });
-    }
-    database& db() {
-        return _db.local();
-    }
-};
-
-// This does not have to be thread local, because all cores will share the same context.
-static std::unique_ptr<query_context> qctx = {};
-
-// Sometimes we are not concerned about system tables at all - for instance, when we are testing. In those cases, just pretend
-// we executed the query, and return an empty result
-template <typename... Args>
-static future<::shared_ptr<cql3::untyped_result_set>> execute_cql(sstring text, Args&&... args) {
-    if (qctx) {
-        return qctx->execute_cql(text, std::forward<Args>(args)...);
-    }
-    return make_ready_future<shared_ptr<cql3::untyped_result_set>>(::make_shared<cql3::untyped_result_set>(cql3::untyped_result_set::make_empty()));
 }
 
 #if 0
