b8c3d8de4be76086dcb01574adafd88a1dd8559c
Paweł Dziepak <pdziepak@cloudius-systems.com>
2015-07-08 10:47:50 +0200

    tests/cql: add test for empty sstable_compression property
    Signed-off-by: Paweł Dziepak <pdziepak@cloudius-systems.com>

--- tests/urchin/cql_query_test.cc
+++ tests/urchin/cql_query_test.cc
@@ -907,6 +907,11 @@ SEASTAR_TEST_CASE(test_table_compression) {
             assert(!f.failed());
             e.require_table_exists("ks", "tb1");
             BOOST_REQUIRE(e.local_db().find_schema("ks", "tb1")->get_compressor_params().get_compressor() == compressor::none);
+            return e.execute_cql("create table tb5 (foo text PRIMARY KEY, bar text) with compression = { 'sstable_compression' : '' };");
+        }).then_wrapped([&e] (auto f) {
+            assert(!f.failed());
+            e.require_table_exists("ks", "tb5");
+            BOOST_REQUIRE(e.local_db().find_schema("ks", "tb5")->get_compressor_params().get_compressor() == compressor::none);
             return e.execute_cql("create table tb2 (foo text PRIMARY KEY, bar text) with compression = { 'sstable_compression' : 'LossyCompressor' };");
         }).then_wrapped([&e] (auto f) {
             assert_that_failed(f);
