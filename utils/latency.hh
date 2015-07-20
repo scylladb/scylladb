b2c5e2a7ccdbf6ea07a0df1150e7918c5a0499a2
Amnon Heiman <amnon@cloudius-systems.com>
2015-07-20 11:40:57 +0300

    utils: adding the latency object
    The latency object is used to simplify calculating latencies. It uses a
    start and stop time_point so the latency can be queried multiple time.
    
    The start need to be done explicitely and not in the constructor to
    allow reuse of the object.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>

---
+++ utils/latency.hh
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+
+#pragma once
+
+#include <chrono>
+/**
+ * A helper class to keep track of latencies
+ */
+namespace utils {
+
+class latency_counter {
+public:
+    using time_point = std::chrono::system_clock::time_point;
+    using duration = std::chrono::system_clock::duration;
+private:
+    time_point _start;
+    time_point _stop;
+public:
+    void start() {
+        _start = now();
+    }
+
+    latency_counter& stop() {
+        _stop = now();
+        return *this;
+    }
+
+    bool is_stopped() const {
+        // if stop was not set, it is still zero
+        return _stop.time_since_epoch().count();
+    }
+
+    duration latency() const {
+        return _stop - _start;
+    }
+
+    latency_counter& check_and_stop() {
+        if (!is_stopped()) {
+            return stop();
+        }
+        return *this;
+    }
+
+    int64_t latency_in_nano() const {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(latency()).count();
+    }
+
+    static time_point now() {
+        return std::chrono::system_clock::now();
+    }
+};
+
+}
