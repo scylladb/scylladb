45b4471a0ec5661ea04e51befda6f4189ea43da9
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2015-07-23 18:37:35 +0200

    tests: Introduce test for query::partition_range

--- configure.py
+++ configure.py
@@ -113,6 +113,7 @@ modes = {
 
 urchin_tests = [
     'tests/urchin/mutation_test',
+    'tests/urchin/range_test',
     'tests/urchin/types_test',
     'tests/urchin/keys_test',
     'tests/urchin/partitioner_test',
@@ -376,7 +377,7 @@ deps = {
 
 for t in urchin_tests:
     deps[t] = urchin_tests_dependencies + [t + '.cc']
-    if 'types_test' not in t and 'keys_test' not in t and 'partitioner_test' not in t and 'map_difference_test' not in t and 'frozen_mutation_test' not in t and 'perf_mutation' not in t and 'cartesian_product_test' not in t and 'perf_hash' not in t and 'perf_cql_parser' not in t and 'message' not in t and 'perf_simple_query' not in t and 'serialization' not in t and t != 'tests/urchin/gossip' and 'compound_test' not in t:
+    if 'types_test' not in t and 'keys_test' not in t and 'partitioner_test' not in t and 'map_difference_test' not in t and 'frozen_mutation_test' not in t and 'perf_mutation' not in t and 'cartesian_product_test' not in t and 'perf_hash' not in t and 'perf_cql_parser' not in t and 'message' not in t and 'perf_simple_query' not in t and 'serialization' not in t and t != 'tests/urchin/gossip' and 'compound_test' not in t and 'range_test' not in t:
         deps[t] += urchin_tests_seastar_deps
 
 deps['tests/urchin/sstable_test'] += ['tests/urchin/sstable_datafile_test.cc']
---
+++ tests/urchin/range_test.cc
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE core
+
+#include <boost/test/unit_test.hpp>
+#include "query-request.hh"
+#include "schema_builder.hh"
+
+static
+bool includes_token(const schema& s, const query::partition_range& r, const dht::token& tok) {
+    dht::ring_position_comparator cmp(s);
+
+    if (r.is_wrap_around(cmp)) {
+        auto sub = r.unwrap();
+        return includes_token(s, sub.first, tok)
+            || includes_token(s, sub.second, tok);
+    }
+
+    return !r.before(dht::ring_position(tok, dht::ring_position::token_bound::end), cmp)
+        && !r.after(dht::ring_position(tok, dht::ring_position::token_bound::start), cmp);
+}
+
+BOOST_AUTO_TEST_CASE(test_range_with_positions_within_the_same_token) {
+    auto s = schema_builder("ks", "cf")
+        .with_column("key", bytes_type, column_kind::partition_key)
+        .with_column("v", bytes_type)
+        .build();
+
+    dht::token tok = dht::global_partitioner().get_random_token();
+
+    auto key1 = dht::decorated_key{tok,
+                                   partition_key::from_single_value(*s, bytes_type->decompose(bytes("key1")))};
+
+    auto key2 = dht::decorated_key{tok,
+                                   partition_key::from_single_value(*s, bytes_type->decompose(bytes("key2")))};
+
+    {
+        auto r = query::partition_range::make(
+            dht::ring_position(key2),
+            dht::ring_position(key1));
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            dht::ring_position(key1),
+            dht::ring_position(key2));
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.is_wrap_around(dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key2), false},
+            {dht::ring_position(key1), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(!r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key2, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key1), false},
+            {dht::ring_position(key2), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(!r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key2, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.is_wrap_around(dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::starting_at(tok), false},
+            {dht::ring_position(key2), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.is_wrap_around(dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::ending_at(tok), false},
+            {dht::ring_position(key2), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::ending_at(tok), false},
+            {dht::ring_position(key1), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key1), false},
+            {dht::ring_position::starting_at(tok), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::starting_at(tok), true},
+            {dht::ring_position::ending_at(tok), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(!r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::ending_at(tok), true},
+            {dht::ring_position::starting_at(tok), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_range_with_equal_value_but_opposite_inclusiveness_is_a_full_wrap_around) {
+    auto s = schema_builder("ks", "cf")
+        .with_column("key", bytes_type, column_kind::partition_key)
+        .with_column("v", bytes_type)
+        .build();
+
+    dht::token tok = dht::global_partitioner().get_random_token();
+
+    auto key1 = dht::decorated_key{
+        tok, partition_key::from_single_value(*s, bytes_type->decompose(bytes("key1")))};
+
+    auto key2 = dht::decorated_key{
+        tok, partition_key::from_single_value(*s, bytes_type->decompose(bytes("key2")))};
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::starting_at(tok), true},
+            {dht::ring_position::starting_at(tok), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::starting_at(tok), false},
+            {dht::ring_position::starting_at(tok), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::ending_at(tok), false},
+            {dht::ring_position::ending_at(tok), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position::ending_at(tok), true},
+            {dht::ring_position::ending_at(tok), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key1), true},
+            {dht::ring_position(key1), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key1), false},
+            {dht::ring_position(key1), true});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key1), false},
+            {dht::ring_position(key1), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+
+    {
+        auto r = query::partition_range::make(
+            {dht::ring_position(key2), false},
+            {dht::ring_position(key2), false});
+
+        BOOST_REQUIRE(includes_token(*s, r, tok));
+        BOOST_REQUIRE(r.is_wrap_around(dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(r.contains(key1, dht::ring_position_comparator(*s)));
+        BOOST_REQUIRE(!r.contains(key2, dht::ring_position_comparator(*s)));
+    }
+}
