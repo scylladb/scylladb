e8dbfdb56b2eee4d80204b450f3934022abe0a40
Avi Kivity <avi@cloudius-systems.com>
2015-07-26 18:50:23 +0300

    gdb: pretty-print uuids

--- scylla-gdb.py
+++ scylla-gdb.py
@@ -1,4 +1,10 @@
-import gdb, gdb.printing
+import gdb, gdb.printing, uuid
+
+def uint64_t(val):
+    val = int(val)
+    if val < 0:
+        val += 1 << 64
+    return val
 
 class sstring_printer(gdb.printing.PrettyPrinter):
     'print an sstring'
@@ -14,9 +20,22 @@ class sstring_printer(gdb.printing.PrettyPrinter):
     def display_hint(self):
         return 'string'
 
+class uuid_printer(gdb.printing.PrettyPrinter):
+    'print a uuid'
+    def __init__(self, val):
+        self.val = val
+    def to_string(self):
+        msb = uint64_t(self.val['most_sig_bits'])
+        lsb = uint64_t(self.val['least_sig_bits'])
+        return str(uuid.UUID(int=(msb << 64) | lsb))
+    def display_hint(self):
+        return 'string'
+    
+
 def build_pretty_printer():
     pp = gdb.printing.RegexpCollectionPrettyPrinter('scylla')
     pp.add_printer('sstring', r'^basic_sstring<char,.*>$', sstring_printer)
+    pp.add_printer('uuid', r'^utils::UUID$', uuid_printer)
     return pp
 
 gdb.printing.register_pretty_printer(gdb.current_objfile(), build_pretty_printer())
