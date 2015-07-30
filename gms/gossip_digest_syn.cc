eb79a119bfdfb7d29a7d398702ef353257eb3c25
Asias He <asias@cloudius-systems.com>
2015-07-30 16:35:01 +0800

    gossip: Move code from gms/gossip_digest_syn.hh to source file

--- gms/gossip_digest_syn.cc
+++ gms/gossip_digest_syn.cc
@@ -33,4 +33,22 @@ std::ostream& operator<<(std::ostream& os, const gossip_digest_syn& syn) {
     return os << "}";
 }
 
+void gossip_digest_syn::serialize(bytes::iterator& out) const {
+    serialize_string(out, _cluster_id);
+    serialize_string(out, _partioner);
+    gossip_digest_serialization_helper::serialize(out, _digests);
+}
+
+gossip_digest_syn gossip_digest_syn::deserialize(bytes_view& v) {
+    sstring cluster_id = read_simple_short_string(v);
+    sstring partioner = read_simple_short_string(v);
+    std::vector<gossip_digest> digests = gossip_digest_serialization_helper::deserialize(v);
+    return gossip_digest_syn(cluster_id, partioner, std::move(digests));
+}
+
+size_t gossip_digest_syn::serialized_size() const {
+    return serialize_string_size(_cluster_id) + serialize_string_size(_partioner) +
+           gossip_digest_serialization_helper::serialized_size(_digests);
+}
+
 } // namespace gms
