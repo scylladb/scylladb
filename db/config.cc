1d31a929b92b6364af6f3349162330337855f86b
Nadav Har'El <nyh@cloudius-systems.com>
2015-08-02 17:15:38 +0300

    config: fix options with string_map type
    Several of Scylla's options (in db/config.hh) have the type "string_map"
    (unordered_map<sstring, sstring>). The intent was such options could get
    multiple "key=value" settings. However, this never actually worked correctly,
    and had two bugs:
    
     1. Any option name with a space in it would fail, for example:
           $ scylla --logger-log-level  'BatchLog Manager=info'
           error: the argument ('BatchLog Manager=info') for option '--level'
                  is invalid
    
     2. Trying to set multiple entries in the map did *not* work. For example,
           $ scylla --logger-log-level  a=info --logger-log-level b=info
           error: option '--level' cannot be specified more than once
    
    The problem is that boost::program_options does not actually understand
    unordered_map<sstring, sstring>: It doesn't know it is a container (it
    only recognizes std::vector) so it doesn't allow multiple options, and
    it doesn't know how to convert a string to it, so it uses boost::lexical_cast
    which for strings, cuts the string at a space...
    
    The solution is to write a custom "validate()" function overload, which
    boost::program_options uses to validate (and consume) options into object
    types it doesn't understand by default. Getting this function in the right
    place in the code was a difficult exercise, but here it is, a working
    implementation :-) And it fixes the above two bugs.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

--- db/config.cc
+++ db/config.cc
@@ -28,6 +28,28 @@ db::config::config()
 
 namespace bpo = boost::program_options;
 
+// Special "validator" for boost::program_options to allow reading options
+// into an unordered_map<string, string> (we have in config.hh a bunch of
+// those). This validator allows the parameter of each option to look like
+// 'key=value'. It also allows multiple occurrences of this option to add
+// multiple entries into the map. "String" can be any time which can be
+// converted from std::string, e.g., sstring.
+template<typename String>
+static void validate(boost::any& out, const std::vector<std::string>& in,
+        std::unordered_map<String, String>*, int) {
+    if (out.empty()) {
+        out = boost::any(std::unordered_map<String, String>());
+    }
+    auto* p = boost::any_cast<std::unordered_map<String, String>>(&out);
+    for (const auto& s : in) {
+        auto i = s.find_first_of('=');
+        if (i == std::string::npos) {
+            throw boost::program_options::invalid_option_value(s);
+        }
+        (*p)[String(s.substr(0, i))] = String(s.substr(i+1));
+    }
+}
+
 namespace YAML {
 /*
  * Add converters as needed here...
