4b5a89be69482e3cd4b2d1768b7317616170cf9f
Pekka Enberg <penberg@cloudius-systems.com>
2015-08-06 15:55:51 +0300

    transport/event: Eliminate ifdef'd code
    We do serialization in transport/server.cc and there's no need for
    the equals() and hashCode() function so just drop ifdef'd code.
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- transport/event.hh
+++ transport/event.hh
@@ -45,35 +45,6 @@ private:
     { }
 public:
 
-#if 0
-    public static Event deserialize(ByteBuf cb, int version)
-    {
-        switch (CBUtil.readEnumValue(Type.class, cb))
-        {
-            case TOPOLOGY_CHANGE:
-                return TopologyChange.deserializeEvent(cb, version);
-            case STATUS_CHANGE:
-                return StatusChange.deserializeEvent(cb, version);
-            case SCHEMA_CHANGE:
-                return SchemaChange.deserializeEvent(cb, version);
-        }
-        throw new AssertionError();
-    }
-
-    public void serialize(ByteBuf dest, int version)
-    {
-        CBUtil.writeEnumValue(type, dest);
-        serializeEvent(dest, version);
-    }
-
-    public int serializedSize(int version)
-    {
-        return CBUtil.sizeOfEnumValue(type) + eventSerializedSize(version);
-    }
-
-    protected abstract void serializeEvent(ByteBuf dest, int version);
-    protected abstract int eventSerializedSize(int version);
-#endif
     class topology_change;
     class status_change;
     class schema_change;
@@ -106,50 +77,6 @@ public:
         {
             return topology_change{change_type::MOVED_NODE, ipv4_addr{host.raw_addr(), port}};
         }
-
-#if 0
-        // Assumes the type has already been deserialized
-        private static TopologyChange deserializeEvent(ByteBuf cb, int version)
-        {
-            Change change = CBUtil.readEnumValue(Change.class, cb);
-            InetSocketAddress node = CBUtil.readInet(cb);
-            return new TopologyChange(change, node);
-        }
-
-        protected void serializeEvent(ByteBuf dest, int version)
-        {
-            CBUtil.writeEnumValue(change, dest);
-            CBUtil.writeInet(node, dest);
-        }
-
-        protected int eventSerializedSize(int version)
-        {
-            return CBUtil.sizeOfEnumValue(change) + CBUtil.sizeOfInet(node);
-        }
-
-        @Override
-        public String toString()
-        {
-            return change + " " + node;
-        }
-
-        @Override
-        public int hashCode()
-        {
-            return Objects.hashCode(change, node);
-        }
-
-        @Override
-        public boolean equals(Object other)
-        {
-            if (!(other instanceof TopologyChange))
-                return false;
-
-            TopologyChange tpc = (TopologyChange)other;
-            return Objects.equal(change, tpc.change)
-                && Objects.equal(node, tpc.node);
-        }
-#endif
     };
 
     class event::status_change : public event {
@@ -174,50 +101,6 @@ public:
         {
             return status_change{status_type::DOWN, ipv4_addr{host.raw_addr(), port}};
         }
-
-#if 0
-        // Assumes the type has already been deserialized
-        private static StatusChange deserializeEvent(ByteBuf cb, int version)
-        {
-            Status status = CBUtil.readEnumValue(Status.class, cb);
-            InetSocketAddress node = CBUtil.readInet(cb);
-            return new StatusChange(status, node);
-        }
-
-        protected void serializeEvent(ByteBuf dest, int version)
-        {
-            CBUtil.writeEnumValue(status, dest);
-            CBUtil.writeInet(node, dest);
-        }
-
-        protected int eventSerializedSize(int version)
-        {
-            return CBUtil.sizeOfEnumValue(status) + CBUtil.sizeOfInet(node);
-        }
-
-        @Override
-        public String toString()
-        {
-            return status + " " + node;
-        }
-
-        @Override
-        public int hashCode()
-        {
-            return Objects.hashCode(status, node);
-        }
-
-        @Override
-        public boolean equals(Object other)
-        {
-            if (!(other instanceof StatusChange))
-                return false;
-
-            StatusChange stc = (StatusChange)other;
-            return Objects.equal(status, stc.status)
-                && Objects.equal(node, stc.node);
-        }
-#endif
     };
 
     class event::schema_change : public event {
@@ -246,107 +129,6 @@ public:
         schema_change(const change_type change_, const sstring keyspace_)
             : schema_change{change_, target_type::KEYSPACE, keyspace_, std::experimental::optional<sstring>{}}
         { }
-#if 0
-        // Assumes the type has already been deserialized
-        public static SchemaChange deserializeEvent(ByteBuf cb, int version)
-        {
-            Change change = CBUtil.readEnumValue(Change.class, cb);
-            if (version >= 3)
-            {
-                Target target = CBUtil.readEnumValue(Target.class, cb);
-                String keyspace = CBUtil.readString(cb);
-                String tableOrType = target == Target.KEYSPACE ? null : CBUtil.readString(cb);
-                return new SchemaChange(change, target, keyspace, tableOrType);
-            }
-            else
-            {
-                String keyspace = CBUtil.readString(cb);
-                String table = CBUtil.readString(cb);
-                return new SchemaChange(change, table.isEmpty() ? Target.KEYSPACE : Target.TABLE, keyspace, table.isEmpty() ? null : table);
-            }
-        }
-
-        public void serializeEvent(ByteBuf dest, int version)
-        {
-            if (version >= 3)
-            {
-                CBUtil.writeEnumValue(change, dest);
-                CBUtil.writeEnumValue(target, dest);
-                CBUtil.writeString(keyspace, dest);
-                if (target != Target.KEYSPACE)
-                    CBUtil.writeString(tableOrTypeOrFunction, dest);
-            }
-            else
-            {
-                if (target == Target.TYPE)
-                {
-                    // For the v1/v2 protocol, we have no way to represent type changes, so we simply say the keyspace
-                    // was updated.  See CASSANDRA-7617.
-                    CBUtil.writeEnumValue(Change.UPDATED, dest);
-                    CBUtil.writeString(keyspace, dest);
-                    CBUtil.writeString("", dest);
-                }
-                else
-                {
-                    CBUtil.writeEnumValue(change, dest);
-                    CBUtil.writeString(keyspace, dest);
-                    CBUtil.writeString(target == Target.KEYSPACE ? "" : tableOrTypeOrFunction, dest);
-                }
-            }
-        }
-
-        public int eventSerializedSize(int version)
-        {
-            if (version >= 3)
-            {
-                int size = CBUtil.sizeOfEnumValue(change)
-                         + CBUtil.sizeOfEnumValue(target)
-                         + CBUtil.sizeOfString(keyspace);
-
-                if (target != Target.KEYSPACE)
-                    size += CBUtil.sizeOfString(tableOrTypeOrFunction);
-
-                return size;
-            }
-            else
-            {
-                if (target == Target.TYPE)
-                {
-                    return CBUtil.sizeOfEnumValue(Change.UPDATED)
-                         + CBUtil.sizeOfString(keyspace)
-                         + CBUtil.sizeOfString("");
-                }
-                return CBUtil.sizeOfEnumValue(change)
-                     + CBUtil.sizeOfString(keyspace)
-                     + CBUtil.sizeOfString(target == Target.KEYSPACE ? "" : tableOrTypeOrFunction);
-            }
-        }
-
-        @Override
-        public String toString()
-        {
-            return change + " " + target + " " + keyspace + (tableOrTypeOrFunction == null ? "" : "." + tableOrTypeOrFunction);
-        }
-
-        @Override
-        public int hashCode()
-        {
-            return Objects.hashCode(change, target, keyspace, tableOrTypeOrFunction);
-        }
-
-        @Override
-        public boolean equals(Object other)
-        {
-            if (!(other instanceof SchemaChange))
-                return false;
-
-            SchemaChange scc = (SchemaChange)other;
-            return Objects.equal(change, scc.change)
-                && Objects.equal(target, scc.target)
-                && Objects.equal(keyspace, scc.keyspace)
-                && Objects.equal(tableOrTypeOrFunction, scc.tableOrTypeOrFunction);
-        }
-#endif
     };
 
 }
