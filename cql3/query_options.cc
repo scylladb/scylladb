b165d224435b5417e0f6ad11e4780a09f2294311
Pekka Enberg <penberg@cloudius-systems.com>
2015-08-14 09:08:44 +0300

    cql3/query_options: Move implementation to source file
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- cql3/query_options.cc
+++ cql3/query_options.cc
@@ -32,11 +32,91 @@ thread_local const query_options::specific_options query_options::specific_optio
 thread_local query_options query_options::DEFAULT{db::consistency_level::ONE, std::experimental::nullopt,
     {}, false, query_options::specific_options::DEFAULT, version::native_protocol(), serialization_format::use_32_bit()};
 
+query_options::query_options(db::consistency_level consistency,
+        std::experimental::optional<std::vector<sstring>> names, std::vector<bytes_opt> values,
+        bool skip_metadata, specific_options options, int32_t protocol_version, serialization_format sf)
+        : _consistency(consistency)
+        , _names(std::move(names))
+        , _values(std::move(values))
+        , _skip_metadata(skip_metadata)
+        , _options(std::move(options))
+        , _protocol_version(protocol_version)
+        , _serialization_format(sf)
+{ }
 
 query_options::query_options(std::vector<bytes_opt> values)
         : query_options(db::consistency_level::ONE, { }, std::move(values),
         false, query_options::specific_options::DEFAULT, version::native_protocol(),
                 serialization_format::use_32_bit()) {
+}
+
+db::consistency_level query_options::get_consistency() const {
+    return _consistency;
+}
+
+const bytes_opt& query_options::get_value_at(size_t idx) const {
+    return _values.at(idx);
+}
+
+size_t query_options::get_values_count() const {
+    return _values.size();
+}
+
+bool query_options::skip_metadata() const {
+    return _skip_metadata;
+}
+
+int32_t query_options::get_page_size() const { return get_specific_options().page_size; }
+
+::shared_ptr<service::pager::paging_state> query_options::get_paging_state() const {
+    return get_specific_options().state;
+}
+
+std::experimental::optional<db::consistency_level> query_options::get_serial_consistency() const {
+    return get_specific_options().serial_consistency;
+}
+
+api::timestamp_type query_options::get_timestamp(service::query_state& state) const {
+    auto tstamp = get_specific_options().timestamp;
+    return tstamp != api::missing_timestamp ? tstamp : state.get_timestamp();
+}
+
+int query_options::get_protocol_version() const {
+    return _protocol_version;
+}
+
+serialization_format query_options::get_serialization_format() const { return _serialization_format; }
+
+const query_options::specific_options& query_options::get_specific_options() const {
+    return _options;
+}
+
+const query_options& query_options::for_statement(size_t i) const {
+    if (!_batch_options) {
+        // No per-statement options supplied, so use the "global" options
+        return *this;
+    }
+    return _batch_options->at(i);
+}
+
+void query_options::prepare(const std::vector<::shared_ptr<column_specification>>& specs) {
+    if (!_names) {
+        return;
+    }
+
+    auto& names = *_names;
+    std::vector<bytes_opt> ordered_values;
+    ordered_values.reserve(specs.size());
+    for (auto&& spec : specs) {
+        auto& spec_name = spec->name->text();
+        for (size_t j = 0; j < names.size(); j++) {
+            if (names[j] == spec_name) {
+                ordered_values.emplace_back(_values[j]);
+                break;
+            }
+        }
+    }
+    _values = std::move(ordered_values);
 }
 
 }
--- cql3/query_options.hh
+++ cql3/query_options.hh
@@ -61,15 +61,7 @@ private:
 public:
     explicit query_options(db::consistency_level consistency,
             std::experimental::optional<std::vector<sstring>> names, std::vector<bytes_opt> values,
-            bool skip_metadata, specific_options options, int32_t protocol_version, serialization_format sf)
-            : _consistency(consistency)
-            , _names(std::move(names))
-            , _values(std::move(values))
-            , _skip_metadata(skip_metadata)
-            , _options(std::move(options))
-            , _protocol_version(protocol_version)
-            , _serialization_format(sf)
-        { }
+            bool skip_metadata, specific_options options, int32_t protocol_version, serialization_format sf);
 
     // It can't be const because of prepare()
     static thread_local query_options DEFAULT;
@@ -77,78 +69,27 @@ public:
     // forInternalUse
     explicit query_options(std::vector<bytes_opt> values);
 
-    db::consistency_level get_consistency() const {
-        return _consistency;
-    }
-    const bytes_opt& get_value_at(size_t idx) const {
-        return _values.at(idx);
-    }
-    size_t get_values_count() const {
-        return _values.size();
-    }
-    bool skip_metadata() const {
-        return _skip_metadata;
-    }
-
+    db::consistency_level get_consistency() const;
+    const bytes_opt& get_value_at(size_t idx) const;
+    size_t get_values_count() const;
+    bool skip_metadata() const;
     /**  The pageSize for this query. Will be <= 0 if not relevant for the query.  */
-    int32_t get_page_size() const { return get_specific_options().page_size; }
-
+    int32_t get_page_size() const;
     /** The paging state for this query, or null if not relevant. */
-    ::shared_ptr<service::pager::paging_state> get_paging_state() const {
-        return get_specific_options().state;
-    }
-
+    ::shared_ptr<service::pager::paging_state> get_paging_state() const;
     /**  Serial consistency for conditional updates. */
-    std::experimental::optional<db::consistency_level> get_serial_consistency() const {
-        return get_specific_options().serial_consistency;
-    }
-
-    api::timestamp_type get_timestamp(service::query_state& state) const {
-        auto tstamp = get_specific_options().timestamp;
-        return tstamp != api::missing_timestamp ? tstamp : state.get_timestamp();
-    }
-
+    std::experimental::optional<db::consistency_level> get_serial_consistency() const;
+    api::timestamp_type get_timestamp(service::query_state& state) const;
     /**
      * The protocol version for the query. Will be 3 if the object don't come from
      * a native protocol request (i.e. it's been allocated locally or by CQL-over-thrift).
      */
-    int get_protocol_version() const {
-        return _protocol_version;
-    }
-    serialization_format get_serialization_format() const { return _serialization_format; }
-
+    int get_protocol_version() const;
+    serialization_format get_serialization_format() const;
     // Mainly for the sake of BatchQueryOptions
-    const specific_options& get_specific_options() const {
-        return _options;
-    }
-
-    const query_options& for_statement(size_t i) const {
-        if (!_batch_options) {
-            // No per-statement options supplied, so use the "global" options
-            return *this;
-        }
-        return _batch_options->at(i);
-    }
-
-    void prepare(const std::vector<::shared_ptr<column_specification>>& specs) {
-        if (!_names) {
-            return;
-        }
-
-        auto& names = *_names;
-        std::vector<bytes_opt> ordered_values;
-        ordered_values.reserve(specs.size());
-        for (auto&& spec : specs) {
-            auto& spec_name = spec->name->text();
-            for (size_t j = 0; j < names.size(); j++) {
-                if (names[j] == spec_name) {
-                    ordered_values.emplace_back(_values[j]);
-                    break;
-                }
-            }
-        }
-        _values = std::move(ordered_values);
-    }
+    const specific_options& get_specific_options() const;
+    const query_options& for_statement(size_t i) const;
+    void prepare(const std::vector<::shared_ptr<column_specification>>& specs);
 };
 
 }
