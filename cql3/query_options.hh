401c3668a46c72e0634744f487faae0aefe53ed1
Pekka Enberg <penberg@cloudius-systems.com>
2015-08-14 09:00:27 +0300

    cql3/query_options: Remove ifdef'd code
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- cql3/query_options.hh
+++ cql3/query_options.hh
@@ -76,39 +76,6 @@ public:
 
     // forInternalUse
     explicit query_options(std::vector<bytes_opt> values);
-#if 0
-    public static final CBCodec<QueryOptions> codec = new Codec();
-
-    public static QueryOptions fromProtocolV1(ConsistencyLevel consistency, List<ByteBuffer> values)
-    {
-        return new DefaultQueryOptions(consistency, values, false, SpecificOptions.DEFAULT, 1);
-    }
-
-    public static QueryOptions fromProtocolV2(ConsistencyLevel consistency, List<ByteBuffer> values)
-    {
-        return new DefaultQueryOptions(consistency, values, false, SpecificOptions.DEFAULT, 2);
-    }
-
-    public static QueryOptions forInternalCalls(ConsistencyLevel consistency, List<ByteBuffer> values)
-    {
-        return new DefaultQueryOptions(consistency, values, false, SpecificOptions.DEFAULT, 3);
-    }
-
-    public static QueryOptions forInternalCalls(List<ByteBuffer> values)
-    {
-        return new DefaultQueryOptions(ConsistencyLevel.ONE, values, false, SpecificOptions.DEFAULT, 3);
-    }
-
-    public static QueryOptions fromPreV3Batch(ConsistencyLevel consistency)
-    {
-        return new DefaultQueryOptions(consistency, Collections.<ByteBuffer>emptyList(), false, SpecificOptions.DEFAULT, 2);
-    }
-
-    public static QueryOptions create(ConsistencyLevel consistency, List<ByteBuffer> values, boolean skipMetadata, int pageSize, PagingState pagingState, ConsistencyLevel serialConsistency)
-    {
-        return new DefaultQueryOptions(consistency, values, skipMetadata, new SpecificOptions(pageSize, pagingState, serialConsistency, -1L), 0);
-    }
-#endif
 
     db::consistency_level get_consistency() const {
         return _consistency;
@@ -182,78 +149,6 @@ public:
         }
         _values = std::move(ordered_values);
     }
-
-#if 0
-    private static class Codec implements CBCodec<QueryOptions>
-    {
-
-        public void encode(QueryOptions options, ByteBuf dest, int version)
-        {
-            assert version >= 2;
-
-            CBUtil.writeConsistencyLevel(options.getConsistency(), dest);
-
-            EnumSet<Flag> flags = gatherFlags(options);
-            dest.writeByte((byte)Flag.serialize(flags));
-
-            if (flags.contains(Flag.VALUES))
-                CBUtil.writeValueList(options.getValues(), dest);
-            if (flags.contains(Flag.PAGE_SIZE))
-                dest.writeInt(options.getPageSize());
-            if (flags.contains(Flag.PAGING_STATE))
-                CBUtil.writeValue(options.getPagingState().serialize(), dest);
-            if (flags.contains(Flag.SERIAL_CONSISTENCY))
-                CBUtil.writeConsistencyLevel(options.getSerialConsistency(), dest);
-            if (flags.contains(Flag.TIMESTAMP))
-                dest.writeLong(options.getSpecificOptions().timestamp);
-
-            // Note that we don't really have to bother with NAMES_FOR_VALUES server side,
-            // and in fact we never really encode QueryOptions, only decode them, so we
-            // don't bother.
-        }
-
-        public int encodedSize(QueryOptions options, int version)
-        {
-            int size = 0;
-
-            size += CBUtil.sizeOfConsistencyLevel(options.getConsistency());
-
-            EnumSet<Flag> flags = gatherFlags(options);
-            size += 1;
-
-            if (flags.contains(Flag.VALUES))
-                size += CBUtil.sizeOfValueList(options.getValues());
-            if (flags.contains(Flag.PAGE_SIZE))
-                size += 4;
-            if (flags.contains(Flag.PAGING_STATE))
-                size += CBUtil.sizeOfValue(options.getPagingState().serialize());
-            if (flags.contains(Flag.SERIAL_CONSISTENCY))
-                size += CBUtil.sizeOfConsistencyLevel(options.getSerialConsistency());
-            if (flags.contains(Flag.TIMESTAMP))
-                size += 8;
-
-            return size;
-        }
-
-        private EnumSet<Flag> gatherFlags(QueryOptions options)
-        {
-            EnumSet<Flag> flags = EnumSet.noneOf(Flag.class);
-            if (options.getValues().size() > 0)
-                flags.add(Flag.VALUES);
-            if (options.skipMetadata())
-                flags.add(Flag.SKIP_METADATA);
-            if (options.getPageSize() >= 0)
-                flags.add(Flag.PAGE_SIZE);
-            if (options.getPagingState() != null)
-                flags.add(Flag.PAGING_STATE);
-            if (options.getSerialConsistency() != ConsistencyLevel.SERIAL)
-                flags.add(Flag.SERIAL_CONSISTENCY);
-            if (options.getSpecificOptions().timestamp != Long.MIN_VALUE)
-                flags.add(Flag.TIMESTAMP);
-            return flags;
-        }
-    }
-#endif
 };
 
 }
