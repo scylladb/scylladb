e7272d27cc384df2c2e572a4a54bf637ec274c68
Avi Kivity <avi@cloudius-systems.com>
2015-08-18 19:42:06 +0300

    tests: perf_mutation: convert to app_template
    Won't work with lsa without it, due to too small default memory size.

--- tests/perf/perf_mutation.cc
+++ tests/perf/perf_mutation.cc
@@ -1,26 +1,30 @@
 #include "database.hh"
 #include "perf.hh"
+#include <seastar/core/app-template.hh>
 
 static atomic_cell make_atomic_cell(bytes value) {
     return atomic_cell::make_live(0, value);
 };
 
 int main(int argc, char* argv[]) {
-    auto s = make_lw_shared(schema({}, "ks", "cf",
-        {{"p1", utf8_type}}, {{"c1", int32_type}}, {{"r1", int32_type}}, {}, utf8_type));
+    return app_template().run(argc, argv, [] {
+        auto s = make_lw_shared(schema({}, "ks", "cf",
+            {{"p1", utf8_type}}, {{"c1", int32_type}}, {{"r1", int32_type}}, {}, utf8_type));
 
-    memtable mt(s);
+        memtable mt(s);
 
-    std::cout << "Timing mutation of single column within one row...\n";
+        std::cout << "Timing mutation of single column within one row...\n";
 
-    auto key = partition_key::from_exploded(*s, {to_bytes("key1")});
-    auto c_key = clustering_key::from_exploded(*s, {int32_type->decompose(2)});
-    bytes value = int32_type->decompose(3);
+        auto key = partition_key::from_exploded(*s, {to_bytes("key1")});
+        auto c_key = clustering_key::from_exploded(*s, {int32_type->decompose(2)});
+        bytes value = int32_type->decompose(3);
 
-    time_it([&] {
-        mutation m(key, s);
-        const column_definition& col = *s->get_column_definition("r1");
-        m.set_clustered_cell(c_key, col, make_atomic_cell(value));
-        mt.apply(std::move(m));
+        time_it([&] {
+            mutation m(key, s);
+            const column_definition& col = *s->get_column_definition("r1");
+            m.set_clustered_cell(c_key, col, make_atomic_cell(value));
+            mt.apply(std::move(m));
+        });
+        engine().exit(0);
     });
 }
