a3a02968abc96ad75139485c5689453575f85d07
Calle Wilund <calle@cloudius-systems.com>
2015-08-25 09:39:44 +0200

    Commitlog: Expose list_existing_descriptors

--- db/commitlog/commitlog.hh
+++ db/commitlog/commitlog.hh
@@ -184,6 +184,9 @@ public:
 
     const config& active_config() const;
 
+    future<std::vector<descriptor>> list_existing_descriptors() const;
+    future<std::vector<descriptor>> list_existing_descriptors(const sstring& dir) const;
+
     typedef std::function<future<>(temporary_buffer<char>, replay_position)> commit_load_reader_func;
 
     static subscription<temporary_buffer<char>, replay_position> read_log_file(file, commit_load_reader_func);
