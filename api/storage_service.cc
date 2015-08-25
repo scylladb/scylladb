6b4f27dc84b573530980311fa2debd1169185149
Asias He <asias@cloudius-systems.com>
2015-08-25 09:46:19 +0800

    api/storage_service: Add is_gossip_running
    $ curl -X GET --header "Accept: application/json"
    "http://127.0.0.1:10000/storage_service/gossiping"
    
    true

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -353,9 +353,8 @@ void set_storage_service(http_context& ctx, routes& r) {
         return make_ready_future<json::json_return_type>(json_void());
     });
 
-    ss::is_gossip_running.set(r, [](std::unique_ptr<request> req) {
-        //TBD
-        return make_ready_future<json::json_return_type>(true);
+    ss::is_gossip_running.set(r, [](const_req req) {
+        return service::get_local_storage_service().is_gossip_running();
     });
 
     ss::stop_daemon.set(r, [](std::unique_ptr<request> req) {
