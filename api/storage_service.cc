3889e0568d900df651c361c2cb141a524cf60491
Asias He <asias@cloudius-systems.com>
2015-08-28 10:47:52 +0800

    api/storage_service: Add start_native_transport

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -396,8 +396,9 @@ void set_storage_service(http_context& ctx, routes& r) {
     });
 
     ss::start_native_transport.set(r, [](std::unique_ptr<request> req) {
-        //TBD
-        return make_ready_future<json::json_return_type>(json_void());
+        return service::get_local_storage_service().start_native_transport().then([] {
+            return make_ready_future<json::json_return_type>(json_void());
+        });
     });
 
     ss::stop_native_transport.set(r, [](std::unique_ptr<request> req) {
