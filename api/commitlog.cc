09da053bee9bfbdb663164b49e9fa722d49f1b08
Calle Wilund <calle@cloudius-systems.com>
2015-08-31 15:49:36 +0200

    Commitlog API: Implement some more metrics accessors
    Fixes #99
    v2/3: Use map_reduce0 + some abstraction.

--- api/commitlog.cc
+++ api/commitlog.cc
@@ -9,6 +9,20 @@
 
 namespace api {
 
+template<typename Func>
+static auto acquire_cl_metric(http_context& ctx, Func&& func) {
+    typedef std::result_of_t<Func(db::commitlog *)> ret_type;
+
+    return ctx.db.map_reduce0([func = std::forward<Func>(func)](database& db) {
+        if (db.commitlog() == nullptr) {
+            return make_ready_future<ret_type>();
+        }
+        return make_ready_future<ret_type>(func(db.commitlog()));
+    }, ret_type(), std::plus<ret_type>()).then([](ret_type res) {
+        return make_ready_future<json::json_return_type>(res);
+    });
+}
+
 void set_commitlog(http_context& ctx, routes& r) {
     httpd::commitlog_json::get_active_segment_names.set(r,
             [&ctx](std::unique_ptr<request> req) {
@@ -31,19 +45,16 @@ void set_commitlog(http_context& ctx, routes& r) {
         return res;
     });
 
-    httpd::commitlog_json::get_completed_tasks.set(r, [](std::unique_ptr<request> req) {
-        //TBD
-        return make_ready_future<json::json_return_type>(0);
+    httpd::commitlog_json::get_completed_tasks.set(r, [&ctx](std::unique_ptr<request> req) {
+        return acquire_cl_metric(ctx, std::bind(&db::commitlog::get_completed_tasks, std::placeholders::_1));
     });
 
-    httpd::commitlog_json::get_pending_tasks.set(r, [](std::unique_ptr<request> req) {
-        //TBD
-        return make_ready_future<json::json_return_type>(0);
+    httpd::commitlog_json::get_pending_tasks.set(r, [&ctx](std::unique_ptr<request> req) {
+        return acquire_cl_metric(ctx, std::bind(&db::commitlog::get_pending_tasks, std::placeholders::_1));
     });
 
-    httpd::commitlog_json::get_total_commit_log_size.set(r, [](std::unique_ptr<request> req) {
-        //TBD
-        return make_ready_future<json::json_return_type>(0);
+    httpd::commitlog_json::get_total_commit_log_size.set(r, [&ctx](std::unique_ptr<request> req) {
+        return acquire_cl_metric(ctx, std::bind(&db::commitlog::get_total_size, std::placeholders::_1));
     });
 }
 
