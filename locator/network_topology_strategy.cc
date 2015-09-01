scylladb/scylladb

commit b8211c436b7223085c1e1b9097b3a4b03cdb7805

locator/abstract_replication_strategy: Add validate_options() helper
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

locator/network_topology_strategy.cc
@@ -181,6 +181,17 @@ network_topology_strategy::calculate_natural_endpoints(
     return std::move(replicas.get_vector());
 }
 
+void network_topology_strategy::validate_options() const {
+    for (auto& c : _config_options) {
+        if (c.first == sstring("replication_factor"))
+            throw exceptions::configuration_exception(
+                "replication_factor is an option for simple_strategy, not "
+                "network_topology_strategy");
+
+        validate_replication_factor(c.second);
+    }
+}
+
 std::experimental::optional<std::set<sstring>> network_topology_strategy::recognized_options() const {
     // We explicitely allow all options
     return std::experimental::nullopt;

locator/simple_strategy.cc
@@ -52,6 +52,13 @@ size_t simple_strategy::get_replication_factor() const {
     return _replication_factor;
 }
 
+void simple_strategy::validate_options() const {
+    auto it = _config_options.find("replication_factor");
+    if (it == _config_options.end())
+        throw exceptions::configuration_exception("SimpleStrategy requires a replication_factor strategy option.");
+    validate_replication_factor(it->second);
+}
+
 std::experimental::optional<std::set<sstring>>simple_strategy::recognized_options() const {
     return {{ "replication_factor" }};
 }

locator/abstract_replication_strategy.hh
@@ -74,6 +74,7 @@ public:
     virtual ~abstract_replication_strategy() {}
     static std::unique_ptr<abstract_replication_strategy> create_replication_strategy(const sstring& ks_name, const sstring& strategy_name, token_metadata& token_metadata, const std::map<sstring, sstring>& config_options);
     virtual std::vector<inet_address> get_natural_endpoints(const token& search_token);
+    virtual void validate_options() const = 0;
     virtual std::experimental::optional<std::set<sstring>> recognized_options() const = 0;
     virtual size_t get_replication_factor() const = 0;
     uint64_t get_cache_hits_count() const { return _cache_hits_count; }

locator/local_strategy.cc
@@ -21,6 +21,9 @@ std::vector<inet_address> local_strategy::calculate_natural_endpoints(const toke
     return std::vector<inet_address>({utils::fb_utilities::get_broadcast_address()});
 }
 
+void local_strategy::validate_options() const {
+}
+
 std::experimental::optional<std::set<sstring>> local_strategy::recognized_options() const {
     // LocalStrategy doesn't expect any options.
     return {};

locator/local_strategy.hh
@@ -31,6 +31,8 @@ public:
      */
     std::vector<inet_address> get_natural_endpoints(const token& search_token) override;
 
+    virtual void validate_options() const override;
+
     virtual std::experimental::optional<std::set<sstring>> recognized_options() const override;
 };
 

locator/network_topology_strategy.hh
@@ -57,6 +57,8 @@ protected:
     virtual std::vector<inet_address> calculate_natural_endpoints(
         const token& search_token) const override;
 
+    virtual void validate_options() const override;
+
     virtual std::experimental::optional<std::set<sstring>> recognized_options() const override;
 
 private:
@@ -72,18 +74,6 @@ private:
                            std::unordered_set<inet_address>>& dc_replicas,
         std::unordered_map<sstring,
                            std::unordered_set<inet_address>>& all_endpoints) const;
-
-    void validate_options() const {
-        for (auto& c : _config_options)
-        {
-            if (c.first == sstring("replication_factor"))
-                throw exceptions::configuration_exception(
-                    "replication_factor is an option for simple_strategy, not "
-                    "network_topology_strategy");
-
-            validate_replication_factor(c.second);
-        }
-    }
 
 private:
     // map: data centers -> replication factor

locator/simple_strategy.hh
@@ -18,6 +18,7 @@ public:
     simple_strategy(const sstring& keyspace_name, token_metadata& token_metadata, snitch_ptr& snitch, const std::map<sstring, sstring>& config_options);
     virtual ~simple_strategy() {};
     virtual size_t get_replication_factor() const override;
+    virtual void validate_options() const override;
     virtual std::experimental::optional<std::set<sstring>> recognized_options() const override;
 private:
     size_t _replication_factor = 1;

commit 47b84d9660d5f023ab7572a34be7bf571d61ed56

locator: Fix minor formatting issues
Fix minor formatting issues introduced in commit
b8211c436b7223085c1e1b9097b3a4b03cdb7805
("locator/abstract_replication_strategy: Add validate_options() helper").

Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

locator/network_topology_strategy.cc
@@ -183,11 +183,11 @@ network_topology_strategy::calculate_natural_endpoints(
 
 void network_topology_strategy::validate_options() const {
     for (auto& c : _config_options) {
-        if (c.first == sstring("replication_factor"))
+        if (c.first == sstring("replication_factor")) {
             throw exceptions::configuration_exception(
                 "replication_factor is an option for simple_strategy, not "
                 "network_topology_strategy");
-
+        }
         validate_replication_factor(c.second);
     }
 }

locator/simple_strategy.cc
@@ -54,8 +54,9 @@ size_t simple_strategy::get_replication_factor() const {
 
 void simple_strategy::validate_options() const {
     auto it = _config_options.find("replication_factor");
-    if (it == _config_options.end())
+    if (it == _config_options.end()) {
         throw exceptions::configuration_exception("SimpleStrategy requires a replication_factor strategy option.");
+    }
     validate_replication_factor(it->second);
 }
 