d590e327c056c3971e7fd35664399aa9c1526b53
Avi Kivity <avi@cloudius-systems.com>
2015-09-07 19:26:50 +0300

    large_bitmap: support for loading and saving the bitmap to raw ints
    Single-bit accessors are very slow, especially because we don't support
    setting a bit to a value (just set to 1 and clear to 0).  This causes
    loading and retrieving the contents of a bitmap to be painfully slow.
    
    Fix by providing iterator-based load() and save() methods.  The methods
    support partial load/save so that access to very large bitmaps can be
    split over multiple tasks.

--- utils/large_bitset.hh
+++ utils/large_bitset.hh
@@ -10,6 +10,8 @@
 #include <memory>
 #include <vector>
 #include <limits>
+#include <iterator>
+#include <algorithm>
 
 class large_bitset {
     static constexpr size_t block_size() { return 128 * 1024; }
@@ -58,4 +60,86 @@ public:
         _storage[idx1][idx2] &= ~(int_type(1) << idx3);
     }
     void clear();
+    // load data from host bitmap (in host byte order); returns end bit position
+    template <typename IntegerIterator>
+    size_t load(IntegerIterator start, IntegerIterator finish, size_t position = 0);
+    template <typename IntegerIterator>
+    IntegerIterator save(IntegerIterator out, size_t position = 0, size_t n = std::numeric_limits<size_t>::max());
 };
+
+template <typename IntegerIterator>
+size_t
+large_bitset::load(IntegerIterator start, IntegerIterator finish, size_t position) {
+    using input_int_type = typename std::iterator_traits<IntegerIterator>::value_type;
+    if (position % bits_per_int() == 0 && sizeof(input_int_type) == sizeof(int_type)) {
+        auto idx = position;
+        auto idx1 = idx / bits_per_block();
+        idx %= bits_per_block();
+        auto idx2 = idx / bits_per_int();
+        while (start != finish) {
+            auto now = std::min<size_t>(ints_per_block() - idx1, std::distance(start, finish));
+            std::copy_n(start, now, _storage[idx1].get() + idx2);
+            start += now;
+            ++idx1;
+            idx2 = 0;
+        }
+    } else {
+        while (start != finish) {
+            auto bitmask = *start++;
+            for (size_t i = 0; i < std::numeric_limits<input_int_type>::digits; ++i) {
+                if (bitmask & 1) {
+                    set(position);
+                } else {
+                    clear(position);
+                }
+                bitmask >>= 1;
+                ++position;
+            }
+        }
+    }
+    return position;
+}
+
+template <typename IntegerIterator>
+IntegerIterator
+large_bitset::save(IntegerIterator out, size_t position, size_t n) {
+    n = std::min(n, size() - position);
+    using output_int_type = typename std::iterator_traits<IntegerIterator>::value_type;
+    if (position % bits_per_int() == 0
+            && n % bits_per_int() == 0
+            && sizeof(output_int_type) == sizeof(int_type)) {
+        auto idx = position;
+        auto idx1 = idx / bits_per_block();
+        idx %= bits_per_block();
+        auto idx2 = idx / bits_per_int();
+        auto n_ints = n / bits_per_int();
+        while (n_ints) {
+            auto now = std::min(ints_per_block() - idx1, n_ints);
+            out = std::copy_n(_storage[idx1].get() + idx2, now, out);
+            ++idx1;
+            idx2 = 0;
+            n_ints -= now;
+        }
+    } else {
+        output_int_type result = 0;
+        unsigned bitpos = 0;
+        while (n) {
+            result |= output_int_type(test(position)) << bitpos;
+            ++position;
+            ++bitpos;
+            --n;
+            if (bitpos == std::numeric_limits<output_int_type>::digits) {
+                *out = result;
+                ++out;
+                result = 0;
+                bitpos = 0;
+            }
+        }
+        if (bitpos) {
+            *out = result;
+            ++out;
+        }
+    }
+    return out;
+}
+
