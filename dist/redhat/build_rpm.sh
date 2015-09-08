9790cafe496a0b80fee84fa77c32f30d26038cae
Pekka Enberg <penberg@cloudius-systems.com>
2015-09-08 14:59:57 +0300

    dist/redhat: Use generated version number in spec file
    Fix the hard-coded version number from RPM spec file by using the
    SCYLLA-VERSION-GEN script.
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- dist/redhat/build_rpm.sh
+++ dist/redhat/build_rpm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 
-SCYLLA_VER=0.00
 RPMBUILD=build/rpmbuild
 
 if [ ! -e dist/redhat/build_rpm.sh ]; then
@@ -10,7 +9,15 @@ fi
 if [ ! -f /usr/bin/mock ]; then
     sudo yum -y install mock
 fi
+VERSION=$(./SCYLLA-VERSION-GEN)
+SCYLLA_VERSION=$(cat build/SCYLLA-VERSION-FILE)
+SCYLLA_RELEASE=$(cat build/SCYLLA-RELEASE-FILE)
 mkdir -p $RPMBUILD/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-./scripts/git-archive-all --force-submodules --prefix scylla-server-$SCYLLA_VER $RPMBUILD/SOURCES/scylla-server-$SCYLLA_VER.tar
-rpmbuild -bs --define "_topdir $RPMBUILD" -ba dist/redhat/scylla-server.spec
-mock rebuild --resultdir=`pwd`/build/rpms $RPMBUILD/SRPMS/scylla-server-$SCYLLA_VER*.src.rpm
+echo $VERSION >version
+./scripts/git-archive-all --extra version --force-submodules --prefix scylla-server-$VERSION $RPMBUILD/SOURCES/scylla-server-$VERSION.tar
+rm -f version
+cp dist/redhat/scylla-server.spec.in $RPMBUILD/SPECS/scylla-server.spec
+sed -i -e "s/@@VERSION@@/$SCYLLA_VERSION/g" $RPMBUILD/SPECS/scylla-server.spec
+sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" $RPMBUILD/SPECS/scylla-server.spec
+rpmbuild -bs --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/scylla-server.spec
+mock rebuild --resultdir=`pwd`/build/rpms $RPMBUILD/SRPMS/scylla-server-$VERSION*.src.rpm
