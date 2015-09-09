04d2bef55b8b46a68bb75362a45d3e976d56fe11
Gleb Natapov <gleb@cloudius-systems.com>
2015-09-09 13:33:06 +0300

    give preference to local data during query
    Until dynamic snitch is implemented this is better than nothing.
    
    Fixes #322

--- db/consistency_level.cc
+++ db/consistency_level.cc
@@ -25,6 +25,7 @@
 #include "db/consistency_level.hh"
 
 #include <boost/range/algorithm/partition.hpp>
+#include <boost/range/algorithm/find.hpp>
 #include "exceptions/exceptions.hh"
 #include "core/sstring.hh"
 #include "schema.hh"
@@ -171,6 +172,11 @@ filter_for_query(consistency_level cl,
      * replica gets the data read) or not (since we'll take the block_for first
      * ones).
      */
+    // FIXME: before dynamic snitch is implement put local address (if present) at the beginning
+    auto it = boost::range::find(live_endpoints, utils::fb_utilities::get_broadcast_address());
+    if (it != live_endpoints.end() && it != live_endpoints.begin()) {
+        std::iter_swap(it, live_endpoints.begin());
+    }
     if (is_datacenter_local(cl)) {
         boost::range::partition(live_endpoints, is_local);
     }
