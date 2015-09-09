e42382a9d57f7efb8f152d271df1960f432cf3c0
Takuya ASADA <syuu@cloudius-systems.com>
2015-09-09 08:51:52 +0000

    dist: add mock to RPM build prerequisites
    fixes #313.
    
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>

--- dist/redhat/build_rpm.sh
+++ dist/redhat/build_rpm.sh
@@ -7,6 +7,9 @@ if [ ! -e dist/redhat/build_rpm.sh ]; then
     echo "run build_rpm.sh in top of scylla dir"
     exit 1
 fi
+if [ ! -f /usr/bin/mock ]; then
+    sudo yum -y install mock
+fi
 mkdir -p $RPMBUILD/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 ./scripts/git-archive-all --force-submodules --prefix scylla-server-$SCYLLA_VER $RPMBUILD/SOURCES/scylla-server-$SCYLLA_VER.tar
 rpmbuild -bs --define "_topdir $RPMBUILD" -ba dist/redhat/scylla-server.spec
