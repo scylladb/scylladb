4ba3580fa7b6bde4979b27939064c9fccf8a9a31
Shlomi Livne <shlomi@cloudius-systems.com>
2015-09-19 02:58:00 +0300

    dist: aws ami install scylla-tools
    Signed-off-by: Shlomi Livne <shlomi@cloudius-systems.com>

--- dist/ami/build_ami.sh
+++ dist/ami/build_ami.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
 
-if [ ! -e dist/ami/build_ami.sh ] || [ ! -e ../scylla-jmx/dist/redhat/build_rpm.sh ]; then
+if [ ! -e dist/ami/build_ami.sh ] || [ ! -e ../scylla-jmx/dist/redhat/build_rpm.sh ] || [ ! -e ../cassandra/dist/redhat/build_rpm.sh ]; then
     echo "run build_ami.sh in top of scylla dir"
     echo "please make sure scylla-jmx is checked out under the same directory as scylla"
+    echo "please make sure cassandra with scylla tools branch checked out under the same directory as scylla"
     exit 1
 fi
 
@@ -28,6 +29,15 @@ if [ ! -f files/scylla-jmx.rpm ]; then
     dist/redhat/build_rpm.sh
     RPM=`ls build/rpms/scylla-jmx-*.noarch.rpm`
     cp $RPM $CWD/files/scylla-jmx.rpm
+    cd -
+fi
+
+if [ ! -f files/scylla-tools.rpm ]; then
+    CWD=`pwd`
+    cd ../../../cassandra
+    dist/redhat/build_rpm.sh
+    RPM=`ls build/rpms/scylla-tools-*.noarch.rpm`
+    cp $RPM $CWD/files/scylla-tools.rpm
     cd -
 fi
 
