d5cf0fb2b16148c1f8fe60339f20f6802371bacc
Avi Kivity <avi@cloudius-systems.com>
2015-09-20 10:43:39 +0300

    Add license notices

--- cql3/single_column_relation.cc
+++ cql3/single_column_relation.cc
@@ -22,6 +22,23 @@
  * Modified by Cloudius Systems
  */
 
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "cql3/single_column_relation.hh"
 #include "cql3/restrictions/single_column_restriction.hh"
 #include "cql3/statements/request_validations.hh"
