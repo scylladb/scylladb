1c040898e8592b8ba7c7914ebe54bf0a8bff48d2
Takuya ASADA <syuu@cloudius-systems.com>
2015-09-24 03:13:15 +0000

    dist: update packages on ec2 instance first bootup
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>
    (cherry picked from commit aaeccdee60e0519a4382712999354df48a5b3ec6)

--- dist/ami/files/scylla-setup.sh
+++ dist/ami/files/scylla-setup.sh
@@ -4,6 +4,8 @@ if [ -f /dev/md0 ]; then
     echo "RAID already constructed."
     exit 1
 fi
+
+dnf update -y
 mdadm --create --verbose --force --run /dev/md0 --level=0 -c256 --raid-devices=2 /dev/xvdb /dev/xvdc
 blockdev --setra 65536 /dev/md0
 mkfs.xfs /dev/md0 -f
