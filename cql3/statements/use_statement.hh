6dbee22d2e9605aeb3d7e6318d2d6768dbd8716f
Pekka Enberg <penberg@scylladb.com>
2015-10-23 15:37:30 +0300

    cql3: Move use_statement class implementation to source file
    Signed-off-by: Pekka Enberg <penberg@scylladb.com>

--- cql3/statements/use_statement.hh
+++ cql3/statements/use_statement.hh
@@ -54,32 +54,21 @@ private:
     const sstring _keyspace;
 
 public:
-    use_statement(sstring keyspace)
-        : _keyspace(keyspace)
-    { }
+    use_statement(sstring keyspace);
 
-    virtual uint32_t get_bound_terms() override {
-        return 0;
-    }
+    virtual uint32_t get_bound_terms() override;
 
-    virtual ::shared_ptr<prepared> prepare(database& db) override {
-        return ::make_shared<parsed_statement::prepared>(this->shared_from_this());
-    }
+    virtual ::shared_ptr<prepared> prepare(database& db) override;
 
-    virtual bool uses_function(const sstring& ks_name, const sstring& function_name) const override {
-        return parsed_statement::uses_function(ks_name, function_name);
-    }
+    virtual bool uses_function(const sstring& ks_name, const sstring& function_name) const override;
 
     virtual bool depends_on_keyspace(const sstring& ks_name) const override;
 
     virtual bool depends_on_column_family(const sstring& cf_name) const override;
 
-    virtual void check_access(const service::client_state& state) override {
-        state.validate_login();
-    }
+    virtual void check_access(const service::client_state& state) override;
 
-    virtual void validate(distributed<service::storage_proxy>&, const service::client_state& state) override {
-    }
+    virtual void validate(distributed<service::storage_proxy>&, const service::client_state& state) override;
 
     virtual future<::shared_ptr<transport::messages::result_message>>
     execute(distributed<service::storage_proxy>& proxy, service::query_state& state, const query_options& options) override;
