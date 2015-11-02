bef3604f5ab4cc3913f698704805ec010331b8d3
Calle Wilund <calle@scylladb.com>
2015-11-02 10:35:26 +0100

    query_pager: Pure virtual interface
    Basic interface for paging control objects.
    
    We probably do not need virtual behaviour for paging, but on the other
    hand it does not really cost much, and it keeps a nice symmetry with
    origin.

---
+++ service/pager/query_pager.hh
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright 2015 Cloudius Systems
+ *
+ * Modified by Cloudius Systems
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "paging_state.hh"
+#include "cql3/result_set.hh"
+
+namespace service {
+
+namespace pager {
+
+/**
+ * Perform a query, paging it by page of a given size.
+ *
+ * This is essentially an iterator of pages. Each call to fetchPage() will
+ * return the next page (i.e. the next list of rows) and isExhausted()
+ * indicates whether there is more page to fetch. The pageSize will
+ * either be in term of cells or in term of CQL3 row, depending on the
+ * parameters of the command we page.
+ *
+ * Please note that the pager might page within rows, so there is no guarantee
+ * that successive pages won't return the same row (though with different
+ * columns every time).
+ *
+ * Also, there is no guarantee that fetchPage() won't return an empty list,
+ * even if isExhausted() return false (but it is guaranteed to return an empty
+ * list *if* isExhausted() return true). Indeed, isExhausted() does *not*
+ * trigger a query so in some (fairly rare) case we might not know the paging
+ * is done even though it is.
+ */
+class query_pager {
+public:
+    virtual ~query_pager() {}
+
+    /**
+     * Fetches the next page.
+     *
+     * @param pageSize the maximum number of elements to return in the next page.
+     * @return the page of result.
+     */
+    virtual future<std::unique_ptr<cql3::result_set>> fetch_page(uint32_t page_size, db_clock::time_point) = 0;
+
+    /**
+     * Whether or not this pager is exhausted, i.e. whether or not a call to
+     * fetchPage may return more result.
+     *
+     * @return whether the pager is exhausted.
+     */
+    virtual bool is_exhausted() const = 0;
+
+    /**
+     * The maximum number of cells/CQL3 row that we may still have to return.
+     * In other words, that's the initial user limit minus what we've already
+     * returned (note that it's not how many we *will* return, just the upper
+     * limit on it).
+     */
+    virtual int max_remaining() const = 0;
+
+    /**
+     * Get the current state (snapshot) of the pager. The state can allow to restart the
+     * paging on another host from where we are at this point.
+     *
+     * @return the current paging state. Will return null if paging is at the
+     * beginning. If the pager is exhausted, the result is undefined.
+     */
+    virtual ::shared_ptr<const paging_state> state() const = 0;
+};
+
+}
+}
+
