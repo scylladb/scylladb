919d94a52b9c295d7ac882e34adaa0219a210d2d
Takuya ASADA <syuu@scylladb.com>
2015-11-02 20:07:40 +0900

    dist: update Ubuntu's g++ to 5.x
    Now ubuntu-toolchain-r repo has g++-5 package for 14.04, so switch to it
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>

--- dist/ubuntu/build_deb.sh
+++ dist/ubuntu/build_deb.sh
@@ -10,6 +10,6 @@ fi
 sudo apt-get -y install libyaml-cpp-dev liblz4-dev libsnappy-dev libcrypto++-dev libboost1.55-dev libjsoncpp-dev libaio-dev ragel ninja-build git libyaml-cpp0.5 liblz4-1 libsnappy1 libcrypto++9 libboost-program-options1.55.0 libboost-program-options1.55-dev libboost-system1.55.0 libboost-system1.55-dev libboost-thread1.55.0 libboost-thread1.55-dev libboost-test1.55.0 libboost-test1.55-dev libjsoncpp0 libaio1 hugepages software-properties-common libboost-filesystem1.55-dev libboost-filesystem1.55.0
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -y update
-sudo apt-get -y install g++-4.9
+sudo apt-get -y install g++-5
 
 debuild -r fakeroot --no-tgz-check -us -uc
