9ee82049937e31226cf5be2e5e6affe93cf17463
Calle Wilund <calle@scylladb.com>
2015-11-02 10:13:14 +0100

    data_input: Fix missing bounds check

--- utils/data_input.hh
+++ utils/data_input.hh
@@ -72,6 +72,7 @@ public:
     template<typename T> T read();
 
     bytes_view read_view(size_t len) {
+        ensure(len);
         bytes_view v(_view.begin(), len);
         _view.remove_prefix(len);
         return v;
