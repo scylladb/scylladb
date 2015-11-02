0fa543800acc6f57a5efccf335950ccb73de0b53
Calle Wilund <calle@scylladb.com>
2015-11-02 10:15:15 +0100

    data_output: Template "blob" writers (bytes*) to allow for varying "size" type

--- utils/data_output.hh
+++ utils/data_output.hh
@@ -64,20 +64,23 @@ public:
     static inline std::enable_if_t<std::is_fundamental<T>::value, size_t> serialized_size() {
         return sizeof(T);
     }
+    template<typename SizeType = uint16_t>
     static inline size_t serialized_size(const sstring& s) {
-        if (s.size() > std::numeric_limits<uint16_t>::max()) {
+        if (s.size() > std::numeric_limits<SizeType>::max()) {
             throw std::out_of_range("String too large");
         }
-        return sizeof(uint16_t) + s.size();
+        return sizeof(SizeType) + s.size();
     }
+    template<typename SizeType = uint32_t>
     static inline size_t serialized_size(const bytes_view& s) {
-        if (s.size() > std::numeric_limits<uint32_t>::max()) {
+        if (s.size() > std::numeric_limits<SizeType>::max()) {
             throw std::out_of_range("Buffer too large");
         }
-        return sizeof(uint32_t) + s.size();
+        return sizeof(SizeType) + s.size();
     }
+    template<typename SizeType = uint32_t>
     static inline size_t serialized_size(const bytes& s) {
-        return serialized_size(bytes_view(s));
+        return serialized_size<SizeType>(bytes_view(s));
     }
 
     size_t avail() const {
@@ -97,20 +100,23 @@ public:
     template<typename T>
     inline std::enable_if_t<std::is_fundamental<T>::value, data_output&> write(T t);
 
+    template<typename SizeType = uint16_t>
     data_output& write(const sstring& s) {
-        ensure(serialized_size(s));
-        write(uint16_t(s.size()));
+        ensure(serialized_size<SizeType>(s));
+        write(SizeType(s.size()));
         _ptr = std::copy(s.begin(), s.end(), _ptr);
         return *this;
     }
+    template<typename SizeType = uint32_t>
     data_output& write(const bytes_view& s) {
-        ensure(serialized_size(s));
-        write(uint32_t(s.size()));
+        ensure(serialized_size<SizeType>(s));
+        write(SizeType(s.size()));
         _ptr = std::copy(s.begin(), s.end(), _ptr);
         return *this;
     }
+    template<typename SizeType = uint32_t>
     data_output& write(const bytes & s) {
-        return write(bytes_view(s));
+        return write<SizeType>(bytes_view(s));
     }
     template<typename Iter>
     data_output& write(Iter s, Iter e) {
