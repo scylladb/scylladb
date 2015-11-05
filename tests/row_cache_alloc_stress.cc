c4e71bac7f037b53f11b7eb41e633a809de84ba4
Paweł Dziepak <pdziepak@scylladb.com>
2015-11-05 13:14:56 +0100

    tests/row_cache_alloc_stress: make sure that allocation fails
    Currently test case "Testing reading when memory can't be reclaimed."
    assumes that the allocation section used by row cache upon entering
    will require more free memory than there is available (inc. evictable).
    However, the reserves used by allocation section are adjusted
    dynamically and depend solely on previous events. In other words there
    is no guarantee that the reserve would be increased so much that the
    allocation will fail.
    
    The problem is solved by adding another allocation that is guaranteed
    to be bigger than all evictable and free memory.
    
    Signed-off-by: Paweł Dziepak <pdziepak@scylladb.com>

--- tests/row_cache_alloc_stress.cc
+++ tests/row_cache_alloc_stress.cc
@@ -234,7 +234,9 @@ int main(int argc, char** argv) {
 
                 try {
                     auto reader = cache.make_reader(range);
-                    reader().get0();
+                    assert(!reader().get0());
+                    auto evicted_from_cache = logalloc::segment_size + cell_size * row_count;
+                    new char[evicted_from_cache + logalloc::segment_size];
                     assert(false); // The test is not invoking the case which it's supposed to test
                 } catch (const std::bad_alloc&) {
                     // expected
