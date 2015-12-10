220a3b23c00e18df6674de53940431fd28954a23
Paweł Dziepak <pdziepak@scylladb.com>
2015-12-10 05:15:13 +0100

    keys: allow creating partial views of prefixes
    Signed-off-by: Paweł Dziepak <pdziepak@scylladb.com>

--- keys.hh
+++ keys.hh
@@ -301,6 +301,53 @@ public:
     };
 };
 
+template <typename TopLevel>
+class prefix_view_on_prefix_compound {
+public:
+    using iterator = typename compound_type<allow_prefixes::yes>::iterator;
+private:
+    bytes_view _b;
+    unsigned _prefix_len;
+    iterator _begin;
+    iterator _end;
+public:
+    prefix_view_on_prefix_compound(const schema& s, bytes_view b, unsigned prefix_len)
+        : _b(b)
+        , _prefix_len(prefix_len)
+        , _begin(TopLevel::get_compound_type(s)->begin(_b))
+        , _end(_begin)
+    {
+        std::advance(_end, prefix_len);
+    }
+
+    iterator begin() const { return _begin; }
+    iterator end() const { return _end; }
+
+    struct less_compare_with_prefix {
+        typename TopLevel::compound prefix_type;
+
+        less_compare_with_prefix(const schema& s)
+            : prefix_type(TopLevel::get_compound_type(s))
+        { }
+
+        bool operator()(const prefix_view_on_prefix_compound& k1, const TopLevel& k2) const {
+            return lexicographical_tri_compare(
+                prefix_type->types().begin(), prefix_type->types().end(),
+                k1.begin(), k1.end(),
+                prefix_type->begin(k2), prefix_type->end(k2),
+                tri_compare) < 0;
+        }
+
+        bool operator()(const TopLevel& k1, const prefix_view_on_prefix_compound& k2) const {
+            return lexicographical_tri_compare(
+                prefix_type->types().begin(), prefix_type->types().end(),
+                prefix_type->begin(k1), prefix_type->end(k1),
+                k2.begin(), k2.end(),
+                tri_compare) < 0;
+        }
+    };
+};
+
 template <typename TopLevel, typename TopLevelView, typename PrefixTopLevel>
 class prefixable_full_compound : public compound_wrapper<TopLevel, TopLevelView> {
     using base = compound_wrapper<TopLevel, TopLevelView>;
@@ -391,6 +438,12 @@ class prefix_compound_wrapper : public compound_wrapper<TopLevel, TopLevelView> 
 protected:
     prefix_compound_wrapper(bytes&& b) : base(std::move(b)) {}
 public:
+    using prefix_view_type = prefix_view_on_prefix_compound<TopLevel>;
+
+    prefix_view_type prefix_view(const schema& s, unsigned prefix_len) const {
+        return { s, this->representation(), prefix_len };
+    }
+
     bool is_full(const schema& s) const {
         return TopLevel::get_compound_type(s)->is_full(base::_bytes);
     }
