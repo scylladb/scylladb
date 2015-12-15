0d5166dcd89d4ab6aeea6868b3b5305e9e0a4f34
Tomasz Grabiec <tgrabiec@scylladb.com>
2015-12-15 17:49:31 +0100

    tests: Add test for get_restricted_ranges()

--- tests/storage_proxy_test.cc
+++ tests/storage_proxy_test.cc
@@ -29,6 +29,7 @@
 #include "tests/result_set_assertions.hh"
 #include "service/storage_proxy.hh"
 #include "partition_slice_builder.hh"
+#include "schema_builder.hh"
 
 static query::result to_data_query_result(mutation_reader& reader, const query::partition_slice& slice) {
     query::result::builder builder(slice);
@@ -104,6 +105,100 @@ SEASTAR_TEST_CASE(test_make_local_reader) {
                         {dht::ring_position(dht::maximum_token(), dht::ring_position::token_bound::start)},
                         {dht::ring_position(dht::maximum_token(), dht::ring_position::token_bound::start)}));
                 assert_that(to_result_set(s, reader)).is_empty();
+            }
+        });
+    });
+}
+
+// Returns random keys sorted in ring order.
+// The schema must have a single bytes_type partition key column.
+static std::vector<dht::ring_position> make_ring(schema_ptr s, int n_keys) {
+    std::vector<dht::ring_position> ring;
+    for (int i = 0; i < 10; ++i) {
+        auto pk = partition_key::from_single_value(*s, to_bytes(sprint("key%d", i)));
+        ring.emplace_back(dht::global_partitioner().decorate_key(*s, pk));
+    }
+    std::sort(ring.begin(), ring.end(), dht::ring_position_less_comparator(*s));
+    return ring;
+}
+
+SEASTAR_TEST_CASE(test_get_restricted_ranges) {
+    return do_with_cql_env([](cql_test_env& e) {
+        return seastar::async([] {
+            auto s = schema_builder("ks", "cf")
+                    .with_column("pk", bytes_type, column_kind::partition_key)
+                    .with_column("v", bytes_type, column_kind::regular_column)
+                    .build();
+
+            std::vector<dht::ring_position> ring = make_ring(s, 10);
+
+            auto check = [&s](locator::token_metadata& tm, query::partition_range input,
+                              std::vector<query::partition_range> expected) {
+                auto actual = service::get_restricted_ranges(tm, *s, input);
+                if (!std::equal(actual.begin(), actual.end(), expected.begin(), [&s](auto&& r1, auto&& r2) {
+                    return r1.equal(r2, dht::ring_position_comparator(*s));
+                })) {
+                    BOOST_FAIL(sprint("Ranges differ, expected %s but got %s", expected, actual));
+                }
+            };
+
+            {
+                // Ring with minimum token
+                locator::token_metadata tm;
+                tm.update_normal_token(dht::minimum_token(), {"10.0.0.1"});
+
+                check(tm, query::partition_range::make_singular(ring[0]), {
+                        query::partition_range::make_singular(ring[0])
+                });
+
+                check(tm, query::partition_range({ring[2]}, {ring[3]}), {
+                        query::partition_range({ring[2]}, {ring[3]})
+                });
+
+                check(tm, query::partition_range({ring[4]}, {ring[2]}), {
+                    query::partition_range({ring[4]}, {}),
+                    query::partition_range({}, {dht::ring_position::ending_at(dht::minimum_token())}),
+                    query::partition_range({{dht::ring_position::ending_at(dht::minimum_token()), false}}, {ring[2]})
+                });
+            }
+
+            {
+                locator::token_metadata tm;
+                tm.update_normal_token(ring[2].token(), {"10.0.0.1"});
+                tm.update_normal_token(ring[5].token(), {"10.0.0.2"});
+
+                check(tm, query::partition_range::make_singular(ring[0]), {
+                        query::partition_range::make_singular(ring[0])
+                });
+
+                check(tm, query::partition_range::make_singular(ring[2]), {
+                        query::partition_range::make_singular(ring[2])
+                });
+
+                check(tm, query::partition_range({{dht::ring_position::ending_at(ring[2].token()), false}}, {ring[3]}), {
+                        query::partition_range({{dht::ring_position::ending_at(ring[2].token()), false}}, {ring[3]})
+                });
+
+                check(tm, query::partition_range({ring[3]}, {ring[4]}), {
+                    query::partition_range({ring[3]}, {ring[4]})
+                });
+
+                check(tm, query::partition_range({ring[2]}, {ring[3]}), {
+                    query::partition_range({ring[2]}, {dht::ring_position::ending_at(ring[2].token())}),
+                    query::partition_range({{dht::ring_position::ending_at(ring[2].token()), false}}, {ring[3]})
+                });
+
+                check(tm, query::partition_range({{ring[2], false}}, {ring[3]}), {
+                    query::partition_range({{ring[2], false}}, {dht::ring_position::ending_at(ring[2].token())}),
+                    query::partition_range({{dht::ring_position::ending_at(ring[2].token()), false}}, {ring[3]})
+                });
+
+                check(tm, query::partition_range({ring[4]}, {ring[3]}), {
+                    query::partition_range({ring[4]}, {dht::ring_position::ending_at(ring[5].token())}),
+                    query::partition_range({{dht::ring_position::ending_at(ring[5].token()), false}}, {}),
+                    query::partition_range({}, {dht::ring_position::ending_at(ring[2].token())}),
+                    query::partition_range({{dht::ring_position::ending_at(ring[2].token()), false}}, {ring[3]}),
+                });
             }
         });
     });
