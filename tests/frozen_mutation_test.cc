88a6a17f7200911d4c4fbae254b03ec402e25f1b
Tomasz Grabiec <tgrabiec@scylladb.com>
2015-12-31 14:14:27 +0100

    tests: Use mutation generators in frozen_mutation_test

--- tests/frozen_mutation_test.cc
+++ tests/frozen_mutation_test.cc
@@ -27,6 +27,7 @@
 #include "frozen_mutation.hh"
 #include "schema_builder.hh"
 #include "tests/mutation_assertions.hh"
+#include "tests/mutation_source_test.hh"
 
 static schema_builder new_table() {
     return { "some_keyspace", "some_table" };
@@ -42,67 +43,12 @@ static tombstone new_tombstone() {
 };
 
 BOOST_AUTO_TEST_CASE(test_writing_and_reading) {
-    schema_ptr s = new_table()
-        .with_column("pk_col", bytes_type, column_kind::partition_key)
-        .with_column("ck_col_1", bytes_type, column_kind::clustering_key)
-        .with_column("ck_col_2", bytes_type, column_kind::clustering_key)
-        .with_column("regular_col_1", bytes_type)
-        .with_column("regular_col_2", bytes_type)
-        .with_column("static_col_1", bytes_type, column_kind::static_column)
-        .with_column("static_col_2", bytes_type, column_kind::static_column)
-        .build();
-
-    partition_key key = partition_key::from_single_value(*s, bytes("key"));
-    clustering_key ck1 = clustering_key::from_deeply_exploded(*s, {data_value(bytes("ck1_0")), data_value(bytes("ck1_1"))});
-    clustering_key ck2 = clustering_key::from_deeply_exploded(*s, {data_value(bytes("ck2_0")), data_value(bytes("ck2_1"))});
-    auto ttl = gc_clock::duration(1);
-
-    auto test_freezing = [] (const mutation& m) {
-        assert_that(freeze(m).unfreeze(m.schema())).is_equal_to(m);
-    };
-
-    mutation m(key, s);
-    m.partition().apply(new_tombstone());
-
-    test_freezing(m);
-
-    m.partition().apply_delete(*s, ck2, new_tombstone());
-
-    test_freezing(m);
-
-    m.partition().apply_row_tombstone(*s, clustering_key_prefix::from_deeply_exploded(*s, {data_value(bytes("ck2_0"))}), new_tombstone());
-
-    test_freezing(m);
-
-    m.set_clustered_cell(ck1, "regular_col_1", data_value(bytes("regular_col_value")), new_timestamp(), ttl);
-
-    test_freezing(m);
-
-    m.set_clustered_cell(ck1, "regular_col_2", data_value(bytes("regular_col_value")), new_timestamp(), ttl);
-
-    test_freezing(m);
-
-    m.partition().apply_insert(*s, ck2, new_timestamp());
-
-    test_freezing(m);
-
-    m.set_clustered_cell(ck2, "regular_col_1", data_value(bytes("ck2_regular_col_1_value")), new_timestamp());
-
-    test_freezing(m);
-
-    m.set_static_cell("static_col_1", data_value(bytes("static_col_value")), new_timestamp(), ttl);
-
-    test_freezing(m);
-
-    m.set_static_cell("static_col_2", data_value(bytes("static_col_value")), new_timestamp());
-
-    test_freezing(m);
-
-    {
-        auto fm = freeze(m);
-        auto dk = fm.decorated_key(*s);
-        BOOST_REQUIRE(dk.equal(*s, m.decorated_key()));
-    }
+    for_each_mutation([] (const mutation& m) {
+        auto frozen = freeze(m);
+        BOOST_REQUIRE_EQUAL(frozen.schema_version(), m.schema()->version());
+        assert_that(frozen.unfreeze(m.schema())).is_equal_to(m);
+        BOOST_REQUIRE(frozen.decorated_key(*m.schema()).equal(*m.schema(), m.decorated_key()));
+    });
 }
 
 BOOST_AUTO_TEST_CASE(test_application_of_partition_view_has_the_same_effect_as_applying_regular_mutation) {
