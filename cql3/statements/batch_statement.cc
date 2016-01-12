7d7d592665353b8a89a10d60c39225688b7861b3
Calle Wilund <calle@scylladb.com>
2016-01-12 16:08:15 +0000

    batch_statement: Modify verify_batch_size to match current origin
    Fixes #614
    
    * Use warning threshold from config
    * Don't throw exceptions. We're only supposed to warn.
    * Try to actually estimate mutation data payload size, not
      number of mutations.

--- cql3/statements/batch_statement.cc
+++ cql3/statements/batch_statement.cc
@@ -38,6 +38,7 @@
  */
 
 #include "batch_statement.hh"
+#include "db/config.hh"
 
 namespace cql3 {
 
@@ -53,6 +54,50 @@ bool batch_statement::depends_on_keyspace(const sstring& ks_name) const
 bool batch_statement::depends_on_column_family(const sstring& cf_name) const
 {
     return false;
+}
+
+void batch_statement::verify_batch_size(const std::vector<mutation>& mutations) {
+    size_t warn_threshold = service::get_local_storage_proxy().get_db().local().get_config().batch_size_warn_threshold_in_kb();
+
+    class my_partition_visitor : public mutation_partition_visitor {
+    public:
+        void accept_partition_tombstone(tombstone) override {}
+        void accept_static_cell(column_id, atomic_cell_view v)  override {
+            size += v.value().size();
+        }
+        void accept_static_cell(column_id, collection_mutation_view v) override {
+            size += v.data.size();
+        }
+        void accept_row_tombstone(clustering_key_prefix_view, tombstone) override {}
+        void accept_row(clustering_key_view, tombstone, const row_marker&) override {}
+        void accept_row_cell(column_id, atomic_cell_view v) override {
+            size += v.value().size();
+        }
+        void accept_row_cell(column_id id, collection_mutation_view v) override {
+            size += v.data.size();
+        }
+
+        size_t size = 0;
+    };
+
+    my_partition_visitor v;
+
+    for (auto&m : mutations) {
+        m.partition().accept(*m.schema(), v);
+    }
+
+    auto size = v.size / 1024;
+
+    if (v.size > warn_threshold) {
+        std::unordered_set<sstring> ks_cf_pairs;
+        for (auto&& m : mutations) {
+            ks_cf_pairs.insert(m.schema()->ks_name() + "." + m.schema()->cf_name());
+        }
+        _logger.warn(
+                        "Batch of prepared statements for {} is of size {}, exceeding specified threshold of {} by {}.{}",
+                        join(", ", ks_cf_pairs), size, warn_threshold,
+                        size - warn_threshold, "");
+    }
 }
 
 }
