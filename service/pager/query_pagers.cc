81e9dc0c2acba3bac4b2e96a3d00be9a5cd9e545
Calle Wilund <calle@scylladb.com>
2016-01-12 13:52:30 +0000

    paging bugfix: Ensure limit for single page is min(page size, limit left)
    Fixes #752
    
    We set row limit for query to be min of page size/remaining in limit,
    but if we have a multinode query we might end up with more rows than asked
    for, so must do this again in post-processing.

--- service/pager/query_pagers.cc
+++ service/pager/query_pagers.cc
@@ -279,7 +279,7 @@ private:
             }
         };
 
-        myvisitor v(*this, page_size, builder, *_schema, *_selection);
+        myvisitor v(*this, std::min(page_size, _max), builder, *_schema, *_selection);
         query::result_view::consume(results->buf(), _cmd->slice, v);
 
         if (_last_pkey) {
