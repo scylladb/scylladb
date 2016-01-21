4b4363b62d3a458132d91c2f1bfe0d60ee92fffa
Asias He <asias@scylladb.com>
2016-01-21 16:45:06 +0800

    streaming: Drop streaming/messages/received_message
    It is not used.

--- streaming/stream_session.cc
+++ streaming/stream_session.cc
@@ -42,7 +42,6 @@
 #include "streaming/messages/stream_init_message.hh"
 #include "streaming/messages/prepare_message.hh"
 #include "streaming/messages/outgoing_file_message.hh"
-#include "streaming/messages/received_message.hh"
 #include "streaming/messages/retry_message.hh"
 #include "streaming/messages/session_failed_message.hh"
 #include "streaming/stream_result_future.hh"
