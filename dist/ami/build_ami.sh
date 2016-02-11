64f8d5a50e468e201fb11589105a8274f37ae9ee
Shlomi Livne <shlomi@scylladb.com>
2016-02-11 10:53:31 +0200

    dist: update packer location
    Signed-off-by: Shlomi Livne <shlomi@scylladb.com>
    Message-Id: <3c33ea073f702e00b789930fce9befef03ad9e88.1455178900.git.shlomi@scylladb.com>

--- dist/ami/build_ami.sh
+++ dist/ami/build_ami.sh
@@ -30,7 +30,7 @@ if [ ! -f variables.json ]; then
 fi
 
 if [ ! -d packer ]; then
-    wget https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip
+    wget https://releases.hashicorp.com/packer/0.8.6/packer_0.8.6_linux_amd64.zip
     mkdir packer
     cd packer
     unzip -x ../packer_0.8.6_linux_amd64.zip
