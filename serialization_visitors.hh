33d5c95b90bfe36d7709dc6a3f6fa230a5f7c7ee
Amnon Heiman <amnon@scylladb.com>
2016-02-17 15:02:03 +0200

    serialization_visitors: Add skip template
    The skip template function is used when skipping data types.
    By default is uses a deserializer to calculate the size.
    
    A specific implementation save unneeded deserialization. For fix sized
    object the skip function would become an expression allowing the
    compiler to drop the function altogether.
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>

--- serialization_visitors.hh
+++ serialization_visitors.hh
@@ -66,6 +66,24 @@ inline frame start_frame(bytes_ostream& out) {
     return frame { size_ph, offset };
 }
 
+template<typename T>
+inline void skip(seastar::simple_input_stream& v, boost::type<T>) {
+    deserialize(v, boost::type<T>());
+}
+
+template<>
+inline void skip(seastar::simple_input_stream& v, boost::type<sstring>) {
+    v.skip(deserialize(v, boost::type<size_type>()));
+}
+
+template<typename T>
+inline void skip(seastar::simple_input_stream& v, boost::type<std::vector<T>>) {
+    auto ln = deserialize(v, boost::type<size_type>());
+    for (size_type i = 0; i < ln; i++) {
+        skip(v, boost::type<T>());
+    }
+}
+
 
 }
 
