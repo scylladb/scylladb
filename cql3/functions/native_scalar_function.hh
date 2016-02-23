78c6fdf42981f110f39ceaced4200649c8476ac0
Pekka Enberg <penberg@scylladb.com>
2016-02-23 13:32:51 +0200

    cql3/functions: Fix is_pure() for native scalar functions
    Every native scalar function is already tagged whether they're pure or
    not but because we don't implement the is_pure() function, all functions
    end up being advertised as pure. This means that functions like now()
    that are *not* pure, end up being evaluated only once.
    
    Fixes #571.
    Message-Id: <1456227171-461-1-git-send-email-penberg@scylladb.com>

--- cql3/functions/native_scalar_function.hh
+++ cql3/functions/native_scalar_function.hh
@@ -74,6 +74,9 @@ public:
             : native_scalar_function(std::move(name), std::move(return_type), std::move(arg_types))
             , _func(std::forward<Func>(func)) {
     }
+    virtual bool is_pure() override {
+        return Pure;
+    }
     virtual bytes_opt execute(cql_serialization_format sf, const std::vector<bytes_opt>& parameters) override {
         return _func(sf, parameters);
     }
