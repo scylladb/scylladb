28ccd866e22add723ab7daeb548a9101ee85179b
Asias He <asias@scylladb.com>
2016-03-21 14:09:23 +0800

    streaming: Move ranges in stream_plan
    The ranges are not used afterwards. We can move instead of copy.
    Message-Id: <1458540564-34277-1-git-send-email-asias@scylladb.com>

--- streaming/stream_plan.cc
+++ streaming/stream_plan.cc
@@ -45,18 +45,18 @@ namespace streaming {
 extern logging::logger sslog;
 
 stream_plan& stream_plan::request_ranges(inet_address from, sstring keyspace, std::vector<query::range<token>> ranges) {
-    return request_ranges(from, keyspace, ranges, {});
+    return request_ranges(from, keyspace, std::move(ranges), {});
 }
 
 stream_plan& stream_plan::request_ranges(inet_address from, sstring keyspace, std::vector<query::range<token>> ranges, std::vector<sstring> column_families) {
     _range_added = true;
     auto session = _coordinator->get_or_create_session(from);
-    session->add_stream_request(keyspace, ranges, std::move(column_families));
+    session->add_stream_request(keyspace, std::move(ranges), std::move(column_families));
     return *this;
 }
 
 stream_plan& stream_plan::transfer_ranges(inet_address to, sstring keyspace, std::vector<query::range<token>> ranges) {
-    return transfer_ranges(to, keyspace, ranges, {});
+    return transfer_ranges(to, keyspace, std::move(ranges), {});
 }
 
 stream_plan& stream_plan::transfer_ranges(inet_address to, sstring keyspace, std::vector<query::range<token>> ranges, std::vector<sstring> column_families) {
