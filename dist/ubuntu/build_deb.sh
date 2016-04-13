fa0b90b727d255114063a443f1cc175ed5619b24
Takuya ASADA <syuu@scylladb.com>
2016-04-13 23:21:32 +0000

    dist/ubuntu: add dependency for libsystemd-dev to handle startup correctly on recent versions of Ubuntu
    To handle scylla startup correctly on systemd versions of Ubuntu, scylla requires to build with libsystemd-dev.
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>

--- dist/ubuntu/build_deb.sh
+++ dist/ubuntu/build_deb.sh
@@ -48,13 +48,6 @@ sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" debian/changelog
 sed -i -e "s/@@CODENAME@@/$CODENAME/g" debian/changelog
 cp dist/ubuntu/rules.in debian/rules
 cp dist/ubuntu/control.in debian/control
-if [ "$RELEASE" = "15.10" ]; then
-    sed -i -e "s/@@COMPILER@@/g++/g" debian/rules
-    sed -i -e "s/@@COMPILER@@/g++/g" debian/control
-else
-    sed -i -e "s/@@COMPILER@@/g++-5/g" debian/rules
-    sed -i -e "s/@@COMPILER@@/g++-5/g" debian/control
-fi
 if [ "$RELEASE" = "14.04" ]; then
     sed -i -e "s/@@DH_INSTALLINIT@@/--upstart-only/g" debian/rules
     sed -i -e "s/@@COMPILER@@/g++-5/g" debian/rules
