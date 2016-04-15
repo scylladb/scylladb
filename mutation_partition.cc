scylladb/scylladb

commit f15c380a4f85de5e9cf6d8ecb882f275995d0318

database: Compact mutations when executing data queries
Currently data query digest includes cells and tombstones which may have
expired or be covered by higher-level tombstones. This causes digest
mismatch between replicas if some elements are compacted on one of the
nodes and not on others. This mismatch triggers read-repair which doesn't
resolve because mutations received by mutation queries are not differing,
they are compacted already.

The fix adds compacting step before writing and digesting query results by
reusing the algorithm used by mutation query. This is not the most optimal
way to fix this. The compaction step could be folded with the query writing,
there is redundancy in both steps. However such change carries more risk,
and thus was postponed.

perf_simple_query test (cassandra-stress-like partitions) shows regression
from 83k to 77k (7%) ops/s.

Fixes #1165.

mutation_partition.cc
@@ -538,13 +538,11 @@ static void hash_row_slice(md5_hasher& hasher,
 }
 
 template<typename RowWriter>
-static void get_row_slice(const schema& s,
+static void get_compacted_row_slice(const schema& s,
     const query::partition_slice& slice,
     column_kind kind,
     const row& cells,
     const std::vector<column_id>& columns,
-    tombstone tomb,
-    gc_clock::time_point now,
     RowWriter& writer)
 {
     for (auto id : columns) {
@@ -555,7 +553,7 @@ static void get_row_slice(const schema& s,
             auto&& def = s.column_at(kind, id);
             if (def.is_atomic()) {
                 auto c = cell->as_atomic_cell();
-                if (!c.is_live(tomb, now)) {
+                if (!c.is_live()) {
                     writer.add().skip();
                 } else {
                     write_cell(writer, slice, cell->as_atomic_cell());
@@ -563,21 +561,18 @@ static void get_row_slice(const schema& s,
             } else {
                 auto&& mut = cell->as_collection_mutation();
                 auto&& ctype = static_pointer_cast<const collection_type_impl>(def.type);
-                auto m_view = ctype->deserialize_mutation_form(mut);
-                m_view.tomb.apply(tomb);
-                // FIXME: Instead of this, write optimistically and retract if empty
-                auto m_ser = ctype->serialize_mutation_form_only_live(m_view, now);
-                if (ctype->is_empty(m_ser)) {
+                if (ctype->is_empty(mut)) {
                     writer.add().skip();
                 } else {
-                    write_cell(writer, slice, def.type, m_ser);
+                    write_cell(writer, slice, def.type, mut);
                 }
             }
         }
     }
 }
 
-bool has_any_live_data(const schema& s, column_kind kind, const row& cells, tombstone tomb, gc_clock::time_point now) {
+bool has_any_live_data(const schema& s, column_kind kind, const row& cells, tombstone tomb = tombstone(),
+                       gc_clock::time_point now = gc_clock::time_point::min()) {
     bool any_live = false;
     cells.for_each_cell_until([&] (column_id id, const atomic_cell_or_collection& cell_or_collection) {
         const column_definition& def = s.column_at(kind, id);
@@ -600,25 +595,20 @@ bool has_any_live_data(const schema& s, column_kind kind, const row& cells, tomb
     return any_live;
 }
 
-uint32_t
-mutation_partition::query(query::result::partition_writer& pw,
-    const schema& s,
-    gc_clock::time_point now,
-    uint32_t limit) const
-{
+void
+mutation_partition::query_compacted(query::result::partition_writer& pw, const schema& s, uint32_t limit) const {
     const query::partition_slice& slice = pw.slice();
 
     if (limit == 0) {
         pw.retract();
-        return 0;
+        return;
     }
 
     auto static_cells_wr = pw.start().start_static_row().start_cells();
 
     if (!slice.static_columns.empty()) {
         if (pw.requested_result()) {
-            get_row_slice(s, slice, column_kind::static_column, static_row(), slice.static_columns, partition_tombstone(),
-                          now, static_cells_wr);
+            get_compacted_row_slice(s, slice, column_kind::static_column, static_row(), slice.static_columns, static_cells_wr);
         }
         if (pw.requested_digest()) {
             ::feed_hash(pw.digest(), partition_tombstone());
@@ -637,47 +627,35 @@ mutation_partition::query(query::result::partition_writer& pw,
 
     auto is_reversed = slice.options.contains(query::partition_slice::option::reversed);
     auto send_ck = slice.options.contains(query::partition_slice::option::send_clustering_key);
-    for (auto&& row_range : pw.ranges()) {
-        if (limit == 0) {
-            break;
+    for_each_row(s, query::clustering_range::make_open_ended_both_sides(), is_reversed, [&] (const rows_entry& e) {
+        auto& row = e.row();
+        auto row_tombstone = tombstone_for_row(s, e);
+
+        if (pw.requested_digest()) {
+            e.key().feed_hash(pw.digest(), s);
+            ::feed_hash(pw.digest(), row_tombstone);
+            hash_row_slice(pw.digest(), s, column_kind::regular_column, row.cells(), slice.regular_columns);
         }
 
-        has_ck_selector |= !row_range.is_full();
-
-        // FIXME: Optimize for a full-tuple singular range. mutation_partition::range()
-        // does two lookups to form a range, even for singular range. We need
-        // only one lookup for a full-tuple singular range though.
-        for_each_row(s, row_range, is_reversed, [&] (const rows_entry& e) {
-            auto& row = e.row();
-            auto row_tombstone = tombstone_for_row(s, e);
-
-            if (pw.requested_digest()) {
-                e.key().feed_hash(pw.digest(), s);
-                ::feed_hash(pw.digest(), row_tombstone);
-                hash_row_slice(pw.digest(), s, column_kind::regular_column, row.cells(), slice.regular_columns);
+        if (row.is_live(s)) {
+            if (pw.requested_result()) {
+                auto cells_wr = [&] {
+                    if (send_ck) {
+                        return rows_wr.add().write_key(e.key()).start_cells().start_cells();
+                    } else {
+                        return rows_wr.add().skip_key().start_cells().start_cells();
+                    }
+                }();
+                get_compacted_row_slice(s, slice, column_kind::regular_column, row.cells(), slice.regular_columns, cells_wr);
+                std::move(cells_wr).end_cells().end_cells().end_qr_clustered_row();
             }
-
-            if (row.is_live(s, row_tombstone, now)) {
-                if (pw.requested_result()) {
-                    auto cells_wr = [&] {
-                        if (send_ck) {
-                            return rows_wr.add().write_key(e.key()).start_cells().start_cells();
-                        } else {
-                            return rows_wr.add().skip_key().start_cells().start_cells();
-                        }
-                    }();
-                    get_row_slice(s, slice, column_kind::regular_column, row.cells(), slice.regular_columns, row_tombstone,
-                                  now, cells_wr);
-                    std::move(cells_wr).end_cells().end_cells().end_qr_clustered_row();
-                }
-                ++row_count;
-                if (--limit == 0) {
-                    return stop_iteration::yes;
-                }
+            ++row_count;
+            if (--limit == 0) {
+                return stop_iteration::yes;
             }
-            return stop_iteration::no;
-        });
-    }
+        }
+        return stop_iteration::no;
+    });
 
     // If we got no rows, but have live static columns, we should only
     // give them back IFF we did not have any CK restrictions.
@@ -686,16 +664,10 @@ mutation_partition::query(query::result::partition_writer& pw,
     // rows, or return nothing, since cql does not allow "is null".
     if (row_count == 0
 			&& (has_ck_selector
-					|| !has_any_live_data(s, column_kind::static_column,
-							static_row(), _tombstone, now))) {
+					|| !has_any_live_data(s, column_kind::static_column, static_row()))) {
 		pw.retract();
-        return 0;
 	} else {
         std::move(rows_wr).end_rows().end_qr_partition();
-
-        // The partition is live. If there are no clustered rows, there
-        // must be something live in the static row, which counts as one row.
-        return std::max<uint32_t>(row_count, 1);
 	}
 }
 
@@ -1185,7 +1157,7 @@ bool mutation_partition::empty() const
 }
 
 bool
-deletable_row::is_live(const schema& s, tombstone base_tombstone, gc_clock::time_point query_time = gc_clock::time_point::min()) const {
+deletable_row::is_live(const schema& s, tombstone base_tombstone, gc_clock::time_point query_time) const {
     // _created_at corresponds to the row marker cell, present for rows
     // created with the 'insert' statement. If row marker is live, we know the
     // row is live. Otherwise, a row is considered live if it has any cell

database.cc
@@ -1734,21 +1734,12 @@ column_family::query(schema_ptr s, const query::read_command& cmd, query::result
     {
         return do_until(std::bind(&query_state::done, &qs), [this, &qs] {
             auto&& range = *qs.current_partition_range++;
-            qs.reader = make_reader(qs.schema, range, service::get_local_sstable_query_read_priority());
-            qs.range_empty = false;
-            return do_until([&qs] { return !qs.limit || qs.range_empty; }, [&qs] {
-                return qs.reader().then([&qs](mutation_opt mo) {
-                    if (mo) {
-                        auto p_builder = qs.builder.add_partition(*mo->schema(), mo->key());
-                        auto is_distinct = qs.cmd.slice.options.contains(query::partition_slice::option::distinct);
-                        auto limit = !is_distinct ? qs.limit : 1;
-                        auto rows_added = mo->partition().query(p_builder, *qs.schema, qs.cmd.timestamp, limit);
-                        qs.limit -= rows_added;
-                    } else {
-                        qs.range_empty = true;
-                    }
-                });
-            });
+            auto add_partition = [&qs] (uint32_t live_rows, mutation&& m) {
+                auto pb = qs.builder.add_partition(*qs.schema, m.key());
+                m.partition().query_compacted(pb, *qs.schema, live_rows);
+            };
+            return do_with(querying_reader(qs.schema, as_mutation_source(), range, qs.cmd.slice, qs.limit, qs.cmd.timestamp, add_partition),
+                           [] (auto&& rd) { return rd.read(); });
         }).then([qs_ptr = std::move(qs_ptr), &qs] {
             return make_ready_future<lw_shared_ptr<query::result>>(
                     make_lw_shared<query::result>(qs.builder.build()));

mutation.cc
@@ -126,14 +126,35 @@ bool mutation::operator!=(const mutation& m) const {
     return !(*this == m);
 }
 
+void
+mutation::query(query::result::builder& builder,
+    const query::partition_slice& slice,
+    gc_clock::time_point now,
+    uint32_t row_limit) &&
+{
+    auto pb = builder.add_partition(*schema(), key());
+    auto is_reversed = slice.options.contains<query::partition_slice::option::reversed>();
+    mutation_partition& p = partition();
+    p.compact_for_query(*schema(), now, slice.row_ranges(*schema(), key()), is_reversed, row_limit);
+    p.query_compacted(pb, *schema(), row_limit);
+}
+
 query::result
-mutation::query(const query::partition_slice& slice, query::result_request request,
-    gc_clock::time_point now, uint32_t row_limit) const
+mutation::query(const query::partition_slice& slice,
+    query::result_request request,
+    gc_clock::time_point now, uint32_t row_limit) &&
 {
     query::result::builder builder(slice, request);
-    auto pb = builder.add_partition(*schema(), key());
-    partition().query(pb, *schema(), now, row_limit);
+    std::move(*this).query(builder, slice, now, row_limit);
     return builder.build();
+}
+
+query::result
+mutation::query(const query::partition_slice& slice,
+    query::result_request request,
+    gc_clock::time_point now, uint32_t row_limit) const&
+{
+    return mutation(*this).query(slice, request, now, row_limit);
 }
 
 size_t

mutation.hh
@@ -104,8 +104,23 @@ public:
     bool operator!=(const mutation&) const;
 public:
     // The supplied partition_slice must be governed by this mutation's schema
-    query::result query(const query::partition_slice&, query::result_request request = query::result_request::only_result,
-        gc_clock::time_point now = gc_clock::now(), uint32_t row_limit = query::max_rows) const;
+    query::result query(const query::partition_slice&,
+        query::result_request request = query::result_request::only_result,
+        gc_clock::time_point now = gc_clock::now(),
+        uint32_t row_limit = query::max_rows) &&;
+
+    // The supplied partition_slice must be governed by this mutation's schema
+    // FIXME: Slower than the r-value version
+    query::result query(const query::partition_slice&,
+        query::result_request request = query::result_request::only_result,
+        gc_clock::time_point now = gc_clock::now(),
+        uint32_t row_limit = query::max_rows) const&;
+
+    // The supplied partition_slice must be governed by this mutation's schema
+    void query(query::result::builder& builder,
+        const query::partition_slice& slice,
+        gc_clock::time_point now = gc_clock::now(),
+        uint32_t row_limit = query::max_rows) &&;
 
     // See mutation_partition::live_row_count()
     size_t live_row_count(gc_clock::time_point query_time = gc_clock::time_point::min()) const;

mutation_partition.hh
@@ -425,7 +425,7 @@ public:
     row& cells() { return _cells; }
     friend std::ostream& operator<<(std::ostream& os, const deletable_row& dr);
     bool equal(column_kind, const schema& s, const deletable_row& other, const schema& other_schema) const;
-    bool is_live(const schema& s, tombstone base_tombstone, gc_clock::time_point query_time) const;
+    bool is_live(const schema& s, tombstone base_tombstone = tombstone(), gc_clock::time_point query_time = gc_clock::time_point::min()) const;
     bool empty() const { return !_deleted_at && _marker.is_missing() && !_cells.size(); }
     deletable_row difference(const schema&, column_kind, const deletable_row& other) const;
 };
@@ -763,8 +763,11 @@ public:
     tombstone tombstone_for_row(const schema& schema, const rows_entry& e) const;
     boost::iterator_range<rows_type::const_iterator> range(const schema& schema, const query::range<clustering_key_prefix>& r) const;
     boost::iterator_range<rows_type::iterator> range(const schema& schema, const query::range<clustering_key_prefix>& r);
-    // Returns the number of live CQL rows written. No more than limit.
-    uint32_t query(query::result::partition_writer& pw, const schema& s, gc_clock::time_point now, uint32_t limit = query::max_rows) const;
+    // Writes this partition using supplied query result writer.
+    // The partition should be first compacted with compact_for_query(), otherwise
+    // results may include data which is deleted/expired.
+    // At most row_limit CQL rows will be written and digested.
+    void query_compacted(query::result::partition_writer& pw, const schema& s, uint32_t row_limit) const;
     void accept(const schema&, mutation_partition_visitor&) const;
 
     // Returns the number of live CQL rows in this partition.

mutation_query.cc
@@ -57,8 +57,7 @@ query::result
 to_data_query_result(const reconcilable_result& r, schema_ptr s, const query::partition_slice& slice) {
     query::result::builder builder(slice, query::result_request::only_result);
     for (const partition& p : r.partitions()) {
-        auto pb = builder.add_partition(*s, p._m.key(*s));
-        p.mut().unfreeze(s).partition().query(pb, *s, gc_clock::time_point::min(), query::max_rows);
+        p.mut().unfreeze(s).query(builder, slice, gc_clock::time_point::min(), query::max_rows);
     }
     return builder.build();
 }

query-result-set.cc
@@ -201,7 +201,7 @@ result_set::from_raw_result(schema_ptr s, const partition_slice& slice, const re
 
 result_set::result_set(const mutation& m) : result_set([&m] {
     auto slice = partition_slice_builder(*m.schema()).build();
-    auto qr = m.query(slice, result_request::only_result);
+    auto qr = mutation(m).query(slice, result_request::only_result);
     return result_set::from_raw_result(m.schema(), slice, qr);
 }())
 { }

tests/storage_proxy_test.cc
@@ -45,8 +45,7 @@ static query::result to_data_query_result(mutation_reader& reader, const query::
         if (!mo) {
             break;
         }
-        auto pb = builder.add_partition(*mo->schema(), mo->key());
-        mo->partition().query(pb, *mo->schema(), now);
+        std::move(*mo).query(builder, slice, now);
     }
     return builder.build();
 }

commit c69d0a8e873b4447bf524064dd533cc414d2c8df

mutation_partition: Fix collection emptiness check
Broken by f15c380a4f85de5e9cf6d8ecb882f275995d0318.

This resulted in empty collection being returned in the results
instead of no collection.

Fixes org.apache.cassandra.cql3.validation.entities.CollectionsTest
from cassandra-unit-tests.

mutation_partition.cc
@@ -596,7 +596,7 @@ static void get_compacted_row_slice(const schema& s,
             } else {
                 auto&& mut = cell->as_collection_mutation();
                 auto&& ctype = static_pointer_cast<const collection_type_impl>(def.type);
-                if (ctype->is_empty(mut)) {
+                if (!ctype->is_any_live(mut)) {
                     writer.add().skip();
                 } else {
                     write_cell(writer, slice, def.type, mut);