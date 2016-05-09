glommer/scylla

throttle: always release at least one request if we are below the limit
Our current throttling code releases one requests per 1MB of memory available
that we have. If we are below the memory limit, but not by 1MB or more, then
we will keep getting to unthrottle, but never really do anything.

If another memtable is close to the flushing point, those requests may be
exactly the ones that would make it flush. Without them, we'll freeze the
database.

In general, we need to always release at least one request to make sure that
progress is always achieved.

This fixes #1144

Signed-off-by: Glauber Costa <glauber@scylladb.com>

database.cc
@@ -1934,7 +1934,7 @@ void throttle_state::unthrottle() {
     if (should_throttle()) {
         return;
     }
-    size_t avail = (_max_space - _region_group.memory_used()) >> 20;
+    size_t avail = std::min((_max_space - _region_group.memory_used()) >> 20, size_t(1));
     avail = std::min(_throttled_requests.size(), avail);
     for (size_t i = 0; i < avail; ++i) {
         _throttled_requests.front().set_value();

scylladb/scylladb

(cherry picked from commit 9c87ae34967bd3902e98661a7949d2d1e6aa066d)

database.cc
@@ -1969,7 +1969,7 @@ void throttle_state::unthrottle() {
     if (should_throttle()) {
         return;
     }
-    size_t avail = (_max_space - _region_group.memory_used()) >> 20;
+    size_t avail = std::max((_max_space - _region_group.memory_used()) >> 20, size_t(1));
     avail = std::min(_throttled_requests.size(), avail);
     for (size_t i = 0; i < avail; ++i) {
         _throttled_requests.front().set_value();