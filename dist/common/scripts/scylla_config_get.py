53820393da1cd066cc6954cd85a6e444db7b21ff
Takuya ASADA <syuu@scylladb.com>
2016-05-11 17:37:03 +0900

    dist/common/scripts: add scylla.yaml parser for scripts
    To parse scylla.yaml, scylla_config_get.py is added.
    It can be use like 'scylla_config_get.py [key name]' from shell script, or command line.
    This is needed for scylla_io_setup, to get 'data_file_directories' from shellscript.
    Currently it does not supported to specify key name of nested data structure, but enough for scyll_io_setup.
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <1462955824-21983-1-git-send-email-syuu@scylladb.com>

---
+++ dist/common/scripts/scylla_config_get.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#
+# Copyright 2016 ScyllaDB
+#
+
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import yaml
+import argparse
+
+def get(config, key):
+    s = open(config).read()
+    cfg = yaml.load(s)
+    try:
+        val = cfg[key]
+    except KeyError:
+        print "key '%s' not found" % key
+        sys.exit(1)
+    if isinstance(val, list):
+        for v in val:
+            print "%s" % v
+    elif isinstance(val, dict):
+        for k, v in val.items():
+            print "%s:%s" % (k, v)
+    else:
+        print val
+
+def main():
+    parser = argparse.ArgumentParser(description='scylla.yaml config reader/writer from shellscript.')
+    parser.add_argument('-c', '--config', dest='config', action='store',
+                        default='/etc/scylla/scylla.yaml',
+                        help='path to scylla.yaml')
+    parser.add_argument('-g', '--get', dest='get', action='store',
+                        required=True, help='get parameter')
+    args = parser.parse_args()
+    get(args.config, args.get)
+
+if __name__ == "__main__":
+    main()
