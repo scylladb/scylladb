20a48b0f20f318f84fca4f71b9e036ccc4a025c6
Amnon Heiman <amnon@scylladb.com>
2016-05-15 15:29:40 +0300

    API: column family stats break the map_reduce functionality
    This patch replaces the helper function for column family with two
    function, one that collect the relevant column family from all shareds
    and another one that do the translation to json object.
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>

--- api/column_family.hh
+++ api/column_family.hh
@@ -34,31 +34,44 @@ future<> foreach_column_family(http_context& ctx, const sstring& name, std::func
 
 
 template<class Mapper, class I, class Reducer>
-future<json::json_return_type> map_reduce_cf(http_context& ctx, const sstring& name, I init,
+future<I> map_reduce_cf_raw(http_context& ctx, const sstring& name, I init,
         Mapper mapper, Reducer reducer) {
     auto uuid = get_uuid(name, ctx.db.local());
     return ctx.db.map_reduce0([mapper, uuid](database& db) {
         return mapper(db.find_column_family(uuid));
-    }, init, reducer).then([](const I& res) {
+    }, init, reducer);
+}
+
+
+template<class Mapper, class I, class Reducer>
+future<json::json_return_type> map_reduce_cf(http_context& ctx, const sstring& name, I init,
+        Mapper mapper, Reducer reducer) {
+    return map_reduce_cf_raw(ctx, name, init, mapper, reducer).then([](const I& res) {
         return make_ready_future<json::json_return_type>(res);
     });
 }
 
 template<class Mapper, class I, class Reducer, class Result>
-future<json::json_return_type> map_reduce_cf(http_context& ctx, const sstring& name, I init,
+future<I> map_reduce_cf_raw(http_context& ctx, const sstring& name, I init,
         Mapper mapper, Reducer reducer, Result result) {
     auto uuid = get_uuid(name, ctx.db.local());
     return ctx.db.map_reduce0([mapper, uuid](database& db) {
         return mapper(db.find_column_family(uuid));
-    }, init, reducer).then([result](const I& res) mutable {
+    }, init, reducer);
+}
+
+
+template<class Mapper, class I, class Reducer, class Result>
+future<json::json_return_type> map_reduce_cf(http_context& ctx, const sstring& name, I init,
+        Mapper mapper, Reducer reducer, Result result) {
+    return map_reduce_cf_raw(ctx, name, init, mapper, reducer, result).then([result](const I& res) mutable {
         result = res;
         return make_ready_future<json::json_return_type>(result);
     });
 }
 
-
 template<class Mapper, class I, class Reducer>
-future<json::json_return_type> map_reduce_cf(http_context& ctx, I init,
+future<I> map_reduce_cf_raw(http_context& ctx, I init,
         Mapper mapper, Reducer reducer) {
     return ctx.db.map_reduce0([mapper, init, reducer](database& db) {
         auto res = init;
@@ -66,10 +79,18 @@ future<json::json_return_type> map_reduce_cf(http_context& ctx, I init,
             res = reducer(res, mapper(*i.second.get()));
         }
         return res;
-    }, init, reducer).then([](const I& res) {
+    }, init, reducer);
+}
+
+
+template<class Mapper, class I, class Reducer>
+future<json::json_return_type> map_reduce_cf(http_context& ctx, I init,
+        Mapper mapper, Reducer reducer) {
+    return map_reduce_cf_raw(ctx, init, mapper, reducer).then([](const I& res) {
         return make_ready_future<json::json_return_type>(res);
     });
 }
+
 future<json::json_return_type>  get_cf_stats(http_context& ctx, const sstring& name,
         int64_t column_family::stats::*f);
 
