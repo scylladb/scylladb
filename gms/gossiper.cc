eb9ac9ab91ce77573485586cdedcc7ca925efba8
Asias He <asias@scylladb.com>
2016-05-18 02:58:42 +0000

    gms: Optimize gossiper::is_alive
    In perf-flame, I saw in
    
    service::storage_proxy::create_write_response_handler (2.66% cpu)
    
      gossiper::is_alive takes 0.72% cpu
      locator::token_metadata::pending_endpoints_for takes 1.2% cpu
    
    After this patch:
    
    service::storage_proxy::create_write_response_handler (2.17% cpu)
    
      gossiper::is_alive does not show up at all
      locator::token_metadata::pending_endpoints_for takes 1.3% cpu
    
    There is no need to copy the endpoint_state from the endpoint_state_map
    to check if a node is alive. Optimize it since gossiper::is_alive is
    called in the fast path.
    
    Message-Id: <2144310aef8d170cab34a2c96cb67cabca761ca8.1463540290.git.asias@scylladb.com>

--- gms/gossiper.cc
+++ gms/gossiper.cc
@@ -1589,12 +1589,12 @@ bool gossiper::is_alive(inet_address ep) {
     if (ep == get_broadcast_address()) {
         return true;
     }
-    auto eps = get_endpoint_state_for_endpoint(ep);
+    auto it = endpoint_state_map.find(ep);
     // we could assert not-null, but having isAlive fail screws a node over so badly that
     // it's worth being defensive here so minor bugs don't cause disproportionate
     // badness.  (See CASSANDRA-1463 for an example).
-    if (eps) {
-        return eps->is_alive();
+    if (it != endpoint_state_map.end()) {
+        return it->second.is_alive();
     } else {
         logger.warn("unknown endpoint {}", ep);
         return false;
