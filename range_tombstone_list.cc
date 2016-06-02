duarten/scylla

mutations: Encapsulate row tombstones difference
This patch moves the difference between two mutation_partition's
row_tombstones inside the range_tombstone_list.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

range_tombstone_list.cc
@@ -207,6 +207,24 @@ tombstone range_tombstone_list::search_tombstone_covering(const schema& s, const
     return it->tomb;
 }
 
+range_tombstone_list range_tombstone_list::difference(const schema& s, const range_tombstone_list& other) const {
+    range_tombstone_list diff(s);
+    clustering_key_prefix::less_compare cmp_rt(s);
+    auto it_rt = other.begin();
+    for (auto&& rt : _tombstones) {
+        while (it_rt != other.end() && cmp_rt(it_rt->start, rt.start)) {
+            ++it_rt;
+        }
+        if (it_rt == other.end()
+                || !it_rt->start.equal(s, rt.start)
+                || !it_rt->stop.equal(s, rt.stop)
+                || rt.tomb > it_rt->tomb) {
+            diff.add(s, rt);
+        }
+    }
+    return diff;
+}
+
 void range_tombstone_list::apply(const schema& s, range_tombstone_list& rt_list) {
     for (auto&& rt : rt_list) {
         add(s, rt);

scylladb/scylladb

range_tombstone_list.cc
@@ -206,6 +206,24 @@ tombstone range_tombstone_list::search_tombstone_covering(const schema& s, const
     return it->tomb;
 }
 
+range_tombstone_list range_tombstone_list::difference(const schema& s, const range_tombstone_list& other) const {
+    range_tombstone_list diff(s);
+    bound_view::compare cmp_rt(s);
+    auto it_rt = other.begin();
+    for (auto&& rt : _tombstones) {
+        while (it_rt != other.end() && cmp_rt(it_rt->start_bound(), rt.start_bound())) {
+            ++it_rt;
+        }
+        if (it_rt == other.end()
+                || !it_rt->start_bound().equal(s, rt.start_bound())
+                || !it_rt->end_bound().equal(s, rt.end_bound())
+                || rt.tomb > it_rt->tomb) {
+            diff.apply(s, rt);
+        }
+    }
+    return diff;
+}
+
 void range_tombstone_list::apply(const schema& s, range_tombstone_list& rt_list) {
     for (auto&& rt : rt_list) {
         apply(s, rt);