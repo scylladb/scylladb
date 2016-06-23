9b14c93677b6b58426f5228444070204deb1f12e
Paweł Dziepak <pdziepak@scylladb.com>
2016-06-23 17:28:48 +0100

    streamed_mutation: return reference to decorated key
    Signed-off-by: Paweł Dziepak <pdziepak@scylladb.com>

--- streamed_mutation.hh
+++ streamed_mutation.hh
@@ -476,7 +476,7 @@ public:
         : _impl(std::move(i)) { }
 
     const partition_key& key() const { return _impl->_key.key(); }
-    const dht::decorated_key decorated_key() const { return _impl->_key; }
+    const dht::decorated_key& decorated_key() const { return _impl->_key; }
 
     schema_ptr schema() const { return _impl->_schema; }
 
