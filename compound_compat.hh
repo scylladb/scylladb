b179d8d378e588dbf348113cb7b51df2038b420a
Duarte Nunes <duarte@scylladb.com>
2016-07-04 11:48:55 +0200

    compound_compat: Parse legacy compound values
    This patch adds support for parsing legacy compound values by
    introducing the composite class, a wrapper around a sequence of bytes
    serialized in the legacy format for compounds. Compound values can be
    sent though the thrift API.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- compound_compat.hh
+++ compound_compat.hh
@@ -21,7 +21,10 @@
 
 #pragma once
 
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/range/adaptor/transformed.hpp>
 #include "compound.hh"
+#include "schema.hh"
 
 //
 // This header provides adaptors between the representation used by our compound_type<>
@@ -180,3 +183,340 @@ bytes to_legacy(CompoundType& type, bytes_view packed) {
     std::copy(lv.begin(), lv.end(), legacy_form.begin());
     return legacy_form;
 }
+
+// Represents a value serialized according to Origin's CompositeType.
+// If is_compound is true, then the value is one or more components encoded as:
+//
+//   <representation> ::= ( <component> )+
+//   <component>      ::= <length> <value> <EOC>
+//   <length>         ::= <uint16_t>
+//   <EOC>            ::= <uint8_t>
+//
+// If false, then it encodes a single value, without a prefix length or a suffix EOC.
+class composite final {
+    bytes _bytes;
+    bool _is_compound;
+public:
+    composite(bytes&& b, bool is_compound)
+            : _bytes(std::move(b))
+            , _is_compound(is_compound)
+    { }
+
+    composite(bytes&& b)
+            : _bytes(std::move(b))
+            , _is_compound(true)
+    { }
+
+    composite()
+            : _bytes()
+            , _is_compound(true)
+    { }
+
+    using size_type = uint16_t;
+    using eoc_type = int8_t;
+
+    /*
+     * The 'end-of-component' byte should always be 0 for actual column name.
+     * However, it can set to 1 for query bounds. This allows to query for the
+     * equivalent of 'give me the full range'. That is, if a slice query is:
+     *   start = <3><"foo".getBytes()><0>
+     *   end   = <3><"foo".getBytes()><1>
+     * then we'll return *all* the columns whose first component is "foo".
+     * If for a component, the 'end-of-component' is != 0, there should not be any
+     * following component. The end-of-component can also be -1 to allow
+     * non-inclusive query. For instance:
+     *   end = <3><"foo".getBytes()><-1>
+     * allows to query everything that is smaller than <3><"foo".getBytes()>, but
+     * not <3><"foo".getBytes()> itself.
+     */
+    enum class eoc : eoc_type {
+        start = -1,
+        none = 0,
+        end = 1
+    };
+
+    using component = std::pair<bytes, eoc>;
+    using component_view = std::pair<bytes_view, eoc>;
+private:
+    template<typename Value, typename = std::enable_if_t<!std::is_same<const data_value, std::decay_t<Value>>::value>>
+    static size_t size(Value& val) {
+        return val.size();
+    }
+    static size_t size(const data_value& val) {
+        return val.serialized_size();
+    }
+    template<typename Value, typename = std::enable_if_t<!std::is_same<data_value, std::decay_t<Value>>::value>>
+    static void write_value(Value&& val, bytes::iterator& out) {
+        out = std::copy(val.begin(), val.end(), out);
+    }
+    static void write_value(const data_value& val, bytes::iterator& out) {
+        val.serialize(out);
+    }
+    template<typename RangeOfSerializedComponents>
+    static void serialize_value(RangeOfSerializedComponents&& values, bytes::iterator& out, bool is_compound) {
+        if (!is_compound) {
+            auto it = values.begin();
+            write_value(std::forward<decltype(*it)>(*it), out);
+            return;
+        }
+
+        for (auto&& val : values) {
+            write<size_type>(out, static_cast<size_type>(size(val)));
+            write_value(std::forward<decltype(val)>(val), out);
+            // Range tombstones are not keys. For collections, only frozen
+            // values can be keys. Therefore, for as long as it is safe to
+            // assume that this code will be used to create keys, it is safe
+            // to assume the trailing byte is always zero.
+            write<eoc_type>(out, eoc_type(eoc::none));
+        }
+    }
+    template <typename RangeOfSerializedComponents>
+    static size_t serialized_size(RangeOfSerializedComponents&& values, bool is_compound) {
+        size_t len = 0;
+        auto it = values.begin();
+        if (it != values.end()) {
+            // CQL3 uses a specific prefix (0xFFFF) to encode "static columns"
+            // (CASSANDRA-6561). This does mean the maximum size of the first component of a
+            // composite is 65534, not 65535 (or we wouldn't be able to detect if the first 2
+            // bytes is the static prefix or not).
+            auto value_size = size(*it);
+            if (value_size > static_cast<size_type>(std::numeric_limits<size_type>::max() - uint8_t(is_compound))) {
+                throw std::runtime_error(sprint("First component size too large: %d > %d", value_size, std::numeric_limits<size_type>::max() - is_compound));
+            }
+            if (!is_compound) {
+                return value_size;
+            }
+            len += sizeof(size_type) + value_size + sizeof(eoc_type);
+            ++it;
+        }
+        for ( ; it != values.end(); ++it) {
+            auto value_size = size(*it);
+            if (value_size > std::numeric_limits<size_type>::max()) {
+                throw std::runtime_error(sprint("Component size too large: %d > %d", value_size, std::numeric_limits<size_type>::max()));
+            }
+            len += sizeof(size_type) + value_size + sizeof(eoc_type);
+        }
+        return len;
+    }
+public:
+    template <typename Describer>
+    auto describe_type(Describer f) const {
+        return f(const_cast<bytes&>(_bytes));
+    }
+
+    template<typename RangeOfSerializedComponents>
+    static bytes serialize_value(RangeOfSerializedComponents&& values, bool is_compound = true) {
+        auto size = serialized_size(values, is_compound);
+        bytes b(bytes::initialized_later(), size);
+        auto i = b.begin();
+        serialize_value(std::forward<decltype(values)>(values), i, is_compound);
+        return b;
+    }
+
+    class iterator : public std::iterator<std::input_iterator_tag, const component_view> {
+        bytes_view _v;
+        component_view _current;
+    private:
+        eoc to_eoc(int8_t eoc_byte) {
+            return eoc_byte == 0 ? eoc::none : (eoc_byte < 0 ? eoc::start : eoc::end);
+        }
+
+        void read_current() {
+            size_type len;
+            {
+                if (_v.empty()) {
+                    _v = bytes_view(nullptr, 0);
+                    return;
+                }
+                len = read_simple<size_type>(_v);
+                if (_v.size() < len) {
+                    throw marshal_exception();
+                }
+            }
+            auto value = bytes_view(_v.begin(), len);
+            _v.remove_prefix(len);
+            _current = component_view(std::move(value), to_eoc(read_simple<eoc_type>(_v)));
+        }
+    public:
+        struct end_iterator_tag {};
+
+        iterator(const bytes_view& v, bool is_compound, bool is_static)
+                : _v(v) {
+            if (is_static) {
+                _v.remove_prefix(2);
+            }
+            if (is_compound) {
+                read_current();
+            } else {
+                _current = component_view(_v, eoc::none);
+                _v.remove_prefix(_v.size());
+            }
+        }
+
+        iterator(end_iterator_tag) : _v(nullptr, 0) {}
+
+        iterator& operator++() {
+            read_current();
+            return *this;
+        }
+
+        iterator operator++(int) {
+            iterator i(*this);
+            ++(*this);
+            return i;
+        }
+
+        const value_type& operator*() const { return _current; }
+        const value_type* operator->() const { return &_current; }
+        bool operator!=(const iterator& i) const { return _v.begin() != i._v.begin(); }
+        bool operator==(const iterator& i) const { return _v.begin() == i._v.begin(); }
+    };
+
+    iterator begin() const {
+        return iterator(_bytes, _is_compound, is_static());
+    }
+
+    iterator end() const {
+        return iterator(iterator::end_iterator_tag());
+    }
+
+    boost::iterator_range<iterator> components() const & {
+        return { begin(), end() };
+    }
+
+    auto values() const & {
+        return components() | boost::adaptors::transformed([](auto&& c) { return c.first; });
+    }
+
+    std::vector<component> components() const && {
+        std::vector<component> result;
+        std::transform(begin(), end(), std::back_inserter(result), [](auto&& p) {
+            return component(bytes(p.first.begin(), p.first.end()), p.second);
+        });
+        return result;
+    }
+
+    std::vector<bytes> values() const && {
+        std::vector<bytes> result;
+        boost::copy(components() | boost::adaptors::transformed([](auto&& c) { return to_bytes(c.first); }), std::back_inserter(result));
+        return result;
+    }
+
+    const bytes& get_bytes() const {
+        return _bytes;
+    }
+
+    size_t size() const {
+        return _bytes.size();
+    }
+
+    bool empty() const {
+        return _bytes.empty();
+    }
+
+    bool is_static() const {
+        return size() > 2 && (_bytes.at(0) & _bytes.at(1) & 0xff) == 0xff;
+    }
+
+    bool is_compound() const {
+        return _is_compound;
+    }
+
+    // The following factory functions assume this composite is a compound value.
+    template <typename ClusteringElement>
+    static composite from_clustering_element(const schema& s, const ClusteringElement& ce) {
+        return serialize_value(ce.components(s));
+    }
+
+    static composite from_exploded(const std::vector<bytes_view>& v, eoc marker = eoc::none) {
+        if (v.size() == 0) {
+            return bytes(size_t(1), bytes::value_type(marker));
+        }
+        auto b = serialize_value(v);
+        b.back() = eoc_type(marker);
+        return composite(std::move(b));
+    }
+
+    static composite static_prefix(const schema& s) {
+        static bytes static_marker(size_t(2), bytes::value_type(0xff));
+
+        std::vector<bytes_view> sv(s.clustering_key_size());
+        return static_marker + serialize_value(sv);
+    }
+
+    explicit operator bytes_view() const {
+        return _bytes;
+    }
+
+    template <typename Component>
+    friend inline std::ostream& operator<<(std::ostream& os, const std::pair<Component, eoc>& c) {
+        return os << "{value=" << c.first << "; eoc=" << sprint("0x%02x", eoc_type(c.second) & 0xff) << "}";
+    }
+};
+
+class composite_view final {
+    bytes_view _bytes;
+    bool _is_compound;
+public:
+    composite_view(bytes_view b, bool is_compound = true)
+            : _bytes(b)
+            , _is_compound(is_compound)
+    { }
+
+    composite_view(const composite& c)
+            : composite_view(static_cast<bytes_view>(c), c.is_compound())
+    { }
+
+    composite_view()
+            : _bytes(nullptr, 0)
+            , _is_compound(true)
+    { }
+
+    std::vector<bytes> explode() const {
+        std::vector<bytes> ret;
+        for (auto it = begin(), e = end(); it != e; ) {
+            ret.push_back(to_bytes(it->first));
+            auto marker = it->second;
+            ++it;
+            if (it != e && marker != composite::eoc::none) {
+                throw runtime_exception(sprint("non-zero component divider found (%d) mid", sprint("0x%02x", composite::eoc_type(marker) & 0xff)));
+            }
+        }
+        return ret;
+    }
+
+    composite::iterator begin() const {
+        return composite::iterator(_bytes, _is_compound, is_static());
+    }
+
+    composite::iterator end() const {
+        return composite::iterator(composite::iterator::end_iterator_tag());
+    }
+
+    boost::iterator_range<composite::iterator> components() const {
+        return { begin(), end() };
+    }
+
+    auto values() const {
+        return components() | boost::adaptors::transformed([](auto&& c) { return c.first; });
+    }
+
+    size_t size() const {
+        return _bytes.size();
+    }
+
+    bool empty() const {
+        return _bytes.empty();
+    }
+
+    bool is_static() const {
+        return size() > 2 && (_bytes.at(0) & _bytes.at(1)) == 0xff;
+    }
+
+    explicit operator bytes_view() const {
+        return _bytes;
+    }
+
+    bool operator==(const composite_view& k) const { return k._bytes == _bytes && k._is_compound == _is_compound; }
+    bool operator!=(const composite_view& k) const { return !(k == *this); }
+};
