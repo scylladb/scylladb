8d38fa49d4a7fd119ed6752b3925cebcdac15768
Raphael S. Carvalho <raphaelsc@scylladb.com>
2016-07-06 01:29:37 -0300

    sstables: move code to get uncompacting sstables to a function
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- sstables/compaction.cc
+++ sstables/compaction.cc
@@ -120,6 +120,19 @@ static void delete_sstables_for_interrupted_compaction(std::vector<shared_sstabl
     }
 }
 
+static std::vector<shared_sstable> get_uncompacting_sstables(column_family& cf, std::vector<shared_sstable>& sstables) {
+    auto all_sstables = cf.get_sstables_including_compacted_undeleted();
+    std::sort(sstables.begin(), sstables.end(), [] (const shared_sstable& x, const shared_sstable& y) {
+        return x->generation() < y->generation();
+    });
+    std::vector<shared_sstable> not_compacted_sstables;
+    boost::set_difference(*all_sstables, sstables,
+        std::back_inserter(not_compacted_sstables), [] (const shared_sstable& x, const shared_sstable& y) {
+            return x->generation() < y->generation();
+        });
+    return not_compacted_sstables;
+}
+
 class compacting_sstable_writer {
     const schema& _schema;
     std::function<shared_sstable()> _creator;
@@ -217,15 +230,7 @@ compact_sstables(std::vector<shared_sstable> sstables, column_family& cf, std::f
 
         db::replay_position rp;
 
-        auto all_sstables = cf.get_sstables_including_compacted_undeleted();
-        std::sort(sstables.begin(), sstables.end(), [] (const shared_sstable& x, const shared_sstable& y) {
-            return x->generation() < y->generation();
-        });
-        std::vector<shared_sstable> not_compacted_sstables;
-        boost::set_difference(*all_sstables, sstables,
-            std::back_inserter(not_compacted_sstables), [] (const shared_sstable& x, const shared_sstable& y) {
-                return x->generation() < y->generation();
-            });
+        std::vector<shared_sstable> not_compacted_sstables = get_uncompacting_sstables(cf, sstables);
 
         auto schema = cf.schema();
         for (auto sst : sstables) {
