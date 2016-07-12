scylladb/scylladb

commit f81329be60810d383eb0b6c10cb261d3b10cf17f

sstables: sstables::key delegates to composite
The sstables::key class now delegates much of its functionality
to the composite class. All existing behavior is preserved.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

sstables/key.hh
@@ -24,37 +24,45 @@
 #include "schema.hh"
 #include "core/future.hh"
 #include "database_fwd.hh"
+#include "keys.hh"
+#include "compound_compat.hh"
 
 namespace sstables {
 
 class key_view {
-    bytes_view _bytes;
+    composite_view _c;
 public:
-    key_view(bytes_view b) : _bytes(b) {}
-    key_view() : _bytes() {}
+    key_view(bytes_view b) : _c(composite_view(b)) {}
+    key_view(composite_view c) : _c(c) {}
+    key_view() : _c() {}
 
-    std::vector<bytes> explode(const schema& s) const;
+    std::vector<bytes> explode(const schema& s) const {
+        if (s.partition_key_size() == 1) {
+            return { to_bytes(static_cast<bytes_view>(_c)) };
+        }
+        return _c.explode();
+    }
 
-    bool operator==(const key_view& k) const { return k._bytes == _bytes; }
+    bool operator==(const key_view& k) const { return k._c == _c; }
     bool operator!=(const key_view& k) const { return !(k == *this); }
 
-    bool empty() { return _bytes.empty(); }
+    bool empty() { return _c.empty(); }
 
     explicit operator bytes_view() const {
-        return _bytes;
+        return static_cast<bytes_view>(_c);
     }
 
     int tri_compare(key_view other) const {
-        return compare_unsigned(_bytes, other._bytes);
+        return compare_unsigned(static_cast<bytes_view>(_c), static_cast<bytes_view>(other));
     }
 
-    int tri_compare(const schema& s, partition_key_view other) const;
-};
-
-enum class composite_marker : bytes::value_type {
-    start_range = -1,
-    none = 0,
-    end_range = 1,
+    int tri_compare(const schema& s, partition_key_view other) const {
+        auto lf = other.legacy_form(s);
+        auto bytes = static_cast<bytes_view>(_c);
+        return lexicographical_tri_compare(
+            bytes.begin(), bytes.end(), lf.begin(), lf.end(),
+            [] (uint8_t b1, uint8_t b2) { return (int)b1 - b2; });
+    }
 };
 
 // Our internal representation differs slightly (in the way it serializes) from Origin.
@@ -67,19 +75,39 @@ class key {
         after_all_keys,
     };
     kind _kind;
-    bytes _bytes;
+    composite _c;
 public:
-    key(bytes&& b) : _kind(kind::regular), _bytes(std::move(b)) {}
+    key(bytes&& c) : _kind(kind::regular), _c(composite(std::move(c))) {}
+    key(composite&& c) : _kind(kind::regular), _c(std::move(c)) {}
     key(kind k) : _kind(k) {}
-    static key from_bytes(bytes b) { return key(std::move(b)); }
-    static key from_deeply_exploded(const schema& s, const std::vector<data_value>& v);
-    static key from_exploded(const schema& s, const std::vector<bytes>& v);
-    static key from_exploded(const schema& s, std::vector<bytes>&& v);
+    static key from_bytes(bytes b) {
+        return key(composite(std::move(b)));
+    }
+    template <typename RangeOfSerializedComponents>
+    static key make_key(const schema& s, RangeOfSerializedComponents&& values) {
+        bool is_compound = s.partition_key_type()->types().size() > 1;
+        return key(composite(composite::serialize_value(std::forward<decltype(values)>(values), is_compound), is_compound));
+    }
+    static key from_deeply_exploded(const schema& s, const std::vector<data_value>& v) {
+        return make_key(s, v);
+    }
+    static key from_exploded(const schema& s, std::vector<bytes>& v) {
+        return make_key(s, v);
+    }
+    static key from_exploded(const schema& s, std::vector<bytes>&& v) {
+        return make_key(s, std::move(v));
+    }
     // Unfortunately, the _bytes field for the partition_key are not public. We can't move.
-    static key from_partition_key(const schema& s, partition_key_view pk);
-    partition_key to_partition_key(const schema& s);
+    static key from_partition_key(const schema& s, partition_key_view pk) {
+        return make_key(s, pk);
+    }
+    partition_key to_partition_key(const schema& s) const {
+        return partition_key::from_exploded(s, explode(s));
+    }
 
-    std::vector<bytes> explode(const schema& s) const;
+    std::vector<bytes> explode(const schema& s) const {
+        return key_view(_c).explode(s);
+    }
 
     int32_t tri_compare(key_view k) const {
         if (_kind == kind::before_all_keys) {
@@ -88,16 +116,16 @@ public:
         if (_kind == kind::after_all_keys) {
             return 1;
         }
-        return key_view(_bytes).tri_compare(k);
+        return key_view(_c).tri_compare(k);
     }
     operator key_view() const {
-        return key_view(_bytes);
+        return key_view(_c);
     }
     explicit operator bytes_view() const {
-        return _bytes;
+        return static_cast<bytes_view>(_c);
     }
-    bytes& get_bytes() {
-        return _bytes;
+    const bytes& get_bytes() const {
+        return _c.get_bytes();
     }
     friend key minimum_key();
     friend key maximum_key();

configure.py
@@ -303,7 +303,6 @@ scylla_core = (['database.cc',
                  'sstables/sstables.cc',
                  'sstables/compress.cc',
                  'sstables/row.cc',
-                 'sstables/key.cc',
                  'sstables/partition.cc',
                  'sstables/filter.cc',
                  'sstables/compaction.cc',

sstables/key.cc
@@ -1,195 +1,0 @@
-
-/*
- * Copyright (C) 2015 ScyllaDB
- */
-
-/*
- * This file is part of Scylla.
- *
- * Scylla is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Scylla is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include <utils/serialization.hh>
-#include <iterator>
-#include "cql_serialization_format.hh"
-
-#include "bytes.hh"
-#include "schema.hh"
-#include "keys.hh"
-#include "sstables/key.hh"
-#include "../types.hh"
-
-#include "utils/data_input.hh"
-
-namespace sstables {
-
-class internal_serializer {
-    using t_type = std::vector<data_type>;
-    using it_type = t_type::iterator;
-
-    t_type _t;
-    it_type _it;
-
-public:
-    internal_serializer(t_type types) : _t(types),  _it(_t.begin()) {}
-
-    inline void reset() {
-        _it = _t.begin();
-    }
-
-    inline void advance() {
-        assert(_it != _t.end());
-        _it++;
-    }
-
-    inline size_t serialized_size(const data_value& value) {
-        return value.serialized_size();
-    }
-
-    inline void serialize(const data_value& value, bytes::iterator& out) {
-        value.serialize(out);
-    }
-};
-
-class sstable_serializer {
-public:
-    inline void reset() {}
-    inline void advance() {}
-    inline size_t serialized_size(bytes_view value) {
-        return value.size();
-    }
-
-    inline void serialize(bytes_view value, bytes::iterator& out) {
-        out = std::copy_n(value.begin(), value.size(), out);
-    }
-};
-
-// The iterator has to provide successive elements that are from one of the
-// type above ( so we know how to serialize them)
-template <typename Iterator, typename Serializer>
-inline
-bytes from_components(Iterator begin, Iterator end, Serializer&& serializer, bool composite = true) {
-    size_t len = 0;
-
-    for (auto c = begin; c != end; ++c) {
-        auto& component = *c;
-
-        len += uint8_t(composite) * sizeof(uint16_t);
-        len += serializer.serialized_size(component);
-        len += uint8_t(composite);
-        serializer.advance();
-    }
-
-    bytes b(bytes::initialized_later(), len);
-    auto bi = b.begin();
-
-    serializer.reset();
-    for (auto c = begin; c != end; ++c) {
-        auto& component = *c;
-
-        auto sz = serializer.serialized_size(component);
-        if (sz > std::numeric_limits<uint16_t>::max()) {
-            throw runtime_exception(sprint("Cannot serialize component: value too big (%ld bytes)", sz));
-        }
-
-        if (composite) {
-            write<uint16_t>(bi, sz);
-        }
-
-        serializer.serialize(component, bi);
-
-        if (composite) {
-            // Range tombstones are not keys. For collections, only frozen
-            // values can be keys. Therefore, for as long as it is safe to
-            // assume that this code will be used to create representation of
-            // keys, it is safe to assume the trailing byte is always zero.
-            write<uint8_t>(bi, uint8_t(0));
-        }
-        serializer.advance();
-    }
-    return b;
-}
-
-key key::from_deeply_exploded(const schema& s, const std::vector<data_value>& v) {
-    auto &pt = s.partition_key_type()->types();
-    bool composite = pt.size() > 1;
-    return from_components(v.begin(), v.end(), internal_serializer(pt), composite);
-}
-
-key key::from_exploded(const schema& s, const std::vector<bytes>& v) {
-    auto &pt = s.partition_key_type()->types();
-    bool composite = pt.size() > 1;
-    return from_components(v.begin(), v.end(), sstable_serializer(), composite);
-}
-
-key key::from_exploded(const schema& s, std::vector<bytes>&& v) {
-    if (s.partition_key_type()->types().size() == 1) {
-        return key(std::move(v[0]));
-    }
-    return from_components(v.begin(), v.end(), sstable_serializer());
-}
-
-key key::from_partition_key(const schema& s, partition_key_view pk) {
-    auto &pt = s.partition_key_type()->types();
-    bool composite = pt.size() > 1;
-    return from_components(pk.begin(s), pk.end(s), sstable_serializer(), composite);
-}
-
-partition_key
-key::to_partition_key(const schema& s) {
-    return partition_key::from_exploded(s, explode(s));
-}
-
-inline
-std::vector<bytes> explode_composite(bytes_view _bytes) {
-    std::vector<bytes> ret;
-
-    data_input in(_bytes);
-    while (in.has_next()) {
-        auto b = in.read_view_to_blob<uint16_t>();
-        ret.push_back(to_bytes(b));
-        auto marker = in.read<uint8_t>();
-        // The components will be separated by a null byte, but the last one has special significance.
-        if (in.has_next() && (marker != 0)) {
-            throw runtime_exception(sprint("non-zero component divider found (%d) mid", marker));
-        }
-    }
-    return ret;
-}
-
-std::vector<bytes> key::explode(const schema& s) const {
-
-    if (s.partition_key_size() == 1) {
-        return { _bytes };
-    }
-
-    return explode_composite(bytes_view(_bytes));
-}
-
-std::vector<bytes> key_view::explode(const schema& s) const {
-    if (s.partition_key_size() == 1) {
-        return { to_bytes(_bytes) };
-    }
-
-    return explode_composite(_bytes);
-}
-
-int key_view::tri_compare(const schema& s, partition_key_view other) const {
-    auto lf = other.legacy_form(s);
-    return lexicographical_tri_compare(
-        _bytes.begin(), _bytes.end(), lf.begin(), lf.end(),
-        [] (uint8_t b1, uint8_t b2) { return (int)b1 - b2; });
-}
-
-}

sstables/partition.cc
@@ -477,15 +477,15 @@ public:
     }
 
     static bound_kind start_marker_to_bound_kind(bytes_view component) {
-        auto found = composite_marker(component.back());
+        auto found = composite::eoc(component.back());
         switch (found) {
         // start_col may have composite_marker::none in sstables
         // from older versions of Cassandra (see CASSANDRA-7593).
-        case composite_marker::none:
+        case composite::eoc::none:
             return bound_kind::incl_start;
-        case composite_marker::start_range:
+        case composite::eoc::start:
             return bound_kind::incl_start;
-        case composite_marker::end_range:
+        case composite::eoc::end:
             return bound_kind::excl_start;
         default:
             throw malformed_sstable_exception(sprint("Unexpected start composite marker %d\n", uint16_t(uint8_t(found))));
@@ -493,15 +493,15 @@ public:
     }
 
     static bound_kind end_marker_to_bound_kind(bytes_view component) {
-        auto found = composite_marker(component.back());
+        auto found = composite::eoc(component.back());
         switch (found) {
         // start_col may have composite_marker::none in sstables
         // from older versions of Cassandra (see CASSANDRA-7593).
-        case composite_marker::none:
+        case composite::eoc::none:
             return bound_kind::incl_end;
-        case composite_marker::start_range:
+        case composite::eoc::start:
             return bound_kind::excl_end;
-        case composite_marker::end_range:
+        case composite::eoc::end:
             return bound_kind::incl_end;
         default:
             throw malformed_sstable_exception(sprint("Unexpected start composite marker %d\n", uint16_t(uint8_t(found))));

commit 4eca7632ec479aa7d0977c6d75e01e56f187239d

sstables: Replace composite fields with raw bytes
This patch fixes a regression introduced in
f81329be60810d383eb0b6c10cb261d3b10cf17f, which made keys compound by
default when using a particular ctor, in turn leading to mismatches
when comparing the same key built with functions that properly
consider compoundness.

As a temporary fix, the sstable::key and sstable::key_view classes
store raw bytes instead of a composite.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>
Message-Id: <1468339295-3924-1-git-send-email-duarte@scylladb.com>

compound_compat.hh
@@ -473,6 +473,10 @@ public:
     { }
 
     std::vector<bytes> explode() const {
+        if (!_is_compound) {
+            return { to_bytes(_bytes) };
+        }
+
         std::vector<bytes> ret;
         for (auto it = begin(), e = end(); it != e; ) {
             ret.push_back(to_bytes(it->first));

sstables/key.hh
@@ -30,37 +30,32 @@
 namespace sstables {
 
 class key_view {
-    composite_view _c;
+    bytes_view _bytes;
 public:
-    key_view(bytes_view b) : _c(composite_view(b)) {}
-    key_view(composite_view c) : _c(c) {}
-    key_view() : _c() {}
+    key_view(bytes_view b) : _bytes(b) {}
+    key_view() : _bytes() {}
 
     std::vector<bytes> explode(const schema& s) const {
-        if (s.partition_key_size() == 1) {
-            return { to_bytes(static_cast<bytes_view>(_c)) };
-        }
-        return _c.explode();
+        return composite_view(_bytes, s.partition_key_size() > 1).explode();
     }
 
-    bool operator==(const key_view& k) const { return k._c == _c; }
+    bool operator==(const key_view& k) const { return k._bytes == _bytes; }
     bool operator!=(const key_view& k) const { return !(k == *this); }
 
-    bool empty() { return _c.empty(); }
+    bool empty() { return _bytes.empty(); }
 
     explicit operator bytes_view() const {
-        return static_cast<bytes_view>(_c);
+        return _bytes;
     }
 
     int tri_compare(key_view other) const {
-        return compare_unsigned(static_cast<bytes_view>(_c), static_cast<bytes_view>(other));
+        return compare_unsigned(_bytes, other._bytes);
     }
 
     int tri_compare(const schema& s, partition_key_view other) const {
         auto lf = other.legacy_form(s);
-        auto bytes = static_cast<bytes_view>(_c);
         return lexicographical_tri_compare(
-            bytes.begin(), bytes.end(), lf.begin(), lf.end(),
+            _bytes.begin(), _bytes.end(), lf.begin(), lf.end(),
             [] (uint8_t b1, uint8_t b2) { return (int)b1 - b2; });
     }
 };
@@ -75,18 +70,20 @@ class key {
         after_all_keys,
     };
     kind _kind;
-    composite _c;
+    bytes _bytes;
+private:
+    static bool is_compound(const schema& s) {
+        return s.partition_key_size() > 1;
+    }
 public:
-    key(bytes&& c) : _kind(kind::regular), _c(composite(std::move(c))) {}
-    key(composite&& c) : _kind(kind::regular), _c(std::move(c)) {}
+    key(bytes&& b) : _kind(kind::regular), _bytes(std::move(b)) {}
     key(kind k) : _kind(k) {}
     static key from_bytes(bytes b) {
-        return key(composite(std::move(b)));
+        return key(std::move(b));
     }
     template <typename RangeOfSerializedComponents>
     static key make_key(const schema& s, RangeOfSerializedComponents&& values) {
-        bool is_compound = s.partition_key_type()->types().size() > 1;
-        return key(composite(composite::serialize_value(std::forward<decltype(values)>(values), is_compound), is_compound));
+        return key(composite::serialize_value(std::forward<decltype(values)>(values), is_compound(s)));
     }
     static key from_deeply_exploded(const schema& s, const std::vector<data_value>& v) {
         return make_key(s, v);
@@ -106,7 +103,7 @@ public:
     }
 
     std::vector<bytes> explode(const schema& s) const {
-        return key_view(_c).explode(s);
+        return composite_view(_bytes, is_compound(s)).explode();
     }
 
     int32_t tri_compare(key_view k) const {
@@ -116,16 +113,16 @@ public:
         if (_kind == kind::after_all_keys) {
             return 1;
         }
-        return key_view(_c).tri_compare(k);
+        return key_view(_bytes).tri_compare(k);
     }
     operator key_view() const {
-        return key_view(_c);
+        return key_view(_bytes);
     }
     explicit operator bytes_view() const {
-        return static_cast<bytes_view>(_c);
+        return _bytes;
     }
     const bytes& get_bytes() const {
-        return _c.get_bytes();
+        return _bytes;
     }
     friend key minimum_key();
     friend key maximum_key();