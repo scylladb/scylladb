duarten/scylla

thrift: Add partition key validation
This patch validates whether the specified partition key is not empty
and under the size limit.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

thrift/thrift_validation.hh
@@ -83,7 +83,7 @@ namespace thrift_validation {
     private static final Logger logger = LoggerFactory.getLogger(ThriftValidation.class);
 #endif
 
-void validate_key(schema_ptr schema_, const bytes& key);
+void validate_key(const schema& s, const bytes& key);
 
 #if 0
     public static void validateKeyspace(String keyspaceName) throws KeyspaceNotDefinedException

scylladb/scylladb

thrift/thrift_validation.hh
@@ -88,7 +88,7 @@ namespace thrift_validation {
     private static final Logger logger = LoggerFactory.getLogger(ThriftValidation.class);
 #endif
 
-void validate_key(schema_ptr schema_, const bytes& key);
+void validate_key(const schema& s, const bytes_view& key);
 void validate_keyspace_not_system(const std::string& keyspace);
 void validate_ks_def(const KsDef& ks_def);
 void validate_cf_def(const CfDef& cf_def);