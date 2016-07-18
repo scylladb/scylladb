f8f61cf2461dd4c55ba15e73a173002f937946eb
Duarte Nunes <duarte@scylladb.com>
2016-07-18 18:57:59 +0000

    system_keyspace: Record and clear size estimates
    This patch implements functions that allow the size_estimates system
    table to be updated and cleared. The size_estimates table is updated
    per schema with a set of token ranges and the associated estimations
    of how many partitions there are and their mean size.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- db/system_keyspace.cc
+++ db/system_keyspace.cc
@@ -1195,5 +1195,40 @@ future<int> increment_and_get_generation() {
     });
 }
 
+future<> update_size_estimates(const sstring& ks_name, const sstring& cf_name, std::vector<range_estimates> estimates) {
+    auto&& schema = size_estimates();
+    auto timestamp = api::new_timestamp();
+    mutation m_to_apply{partition_key::from_singular(*schema, ks_name), schema};
+
+    // delete all previous values with a single range tombstone.
+    auto ck = clustering_key_prefix::from_single_value(*schema, utf8_type->decompose(cf_name));
+    m_to_apply.partition().apply_row_tombstone(*schema, std::move(ck), {timestamp - 1, gc_clock::now()});
+
+    // add a CQL row for each primary token range.
+    for (auto&& e : estimates) {
+        // This range has both start and end bounds. We're only interested in the tokens.
+        const range<dht::ring_position>* range = e.first;
+        auto ck = clustering_key_prefix(std::vector<bytes>{
+                     utf8_type->decompose(cf_name),
+                     utf8_type->decompose(dht::global_partitioner().to_sstring(range->start()->value().token())),
+                     utf8_type->decompose(dht::global_partitioner().to_sstring(range->end()->value().token()))});
+
+        auto mean_partition_size_col = schema->get_column_definition("mean_partition_size");
+        auto cell = atomic_cell::make_live(timestamp, long_type->decompose(e.second.mean_partition_size), { });
+        m_to_apply.set_clustered_cell(ck, *mean_partition_size_col, std::move(cell));
+
+        auto partitions_count_col = schema->get_column_definition("partitions_count");
+        cell = atomic_cell::make_live(timestamp, long_type->decompose(e.second.partitions_count), { });
+        m_to_apply.set_clustered_cell(std::move(ck), *partitions_count_col, std::move(cell));
+    }
+
+    return service::get_local_storage_proxy().mutate_locally(std::move(m_to_apply));
+}
+
+future<> clear_size_estimates(const sstring& ks_name, const sstring& cf_name) {
+    sstring req = "DELETE FROM system.%s WHERE keyspace_name = ? AND table_name = ?";
+    return execute_cql(req, SIZE_ESTIMATES, ks_name, cf_name).discard_result();
+}
+
 } // namespace system_keyspace
 } // namespace db
