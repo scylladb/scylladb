d1a052237dc6fc5a8a198a130c203e823e86ccec
Pekka Enberg <penberg@scylladb.com>
2016-08-11 11:33:15 +0300

    dist/docker: Fix typo in "--overprovisioned" help text
    Reported by Mathias Bogaert (@analytically).
    Message-Id: <1470904395-4614-1-git-send-email-penberg@scylladb.com>

--- dist/docker/redhat/commandlineparser.py
+++ dist/docker/redhat/commandlineparser.py
@@ -8,7 +8,7 @@ def parse():
     parser.add_argument('--cpuset', default=None, help="e.g. --cpuset 0-3 for the first four CPUs")
     parser.add_argument('--smp', default=None, help="e.g --smp 2 to use two CPUs")
     parser.add_argument('--memory', default=None, help="e.g. --memory 1G to use 1 GB of RAM")
-    parser.add_argument('--overprovisioned', default='0', choices=['0', '1'], help="run in overprovisined environment")
+    parser.add_argument('--overprovisioned', default='0', choices=['0', '1'], help="run in overprovisioned environment")
     parser.add_argument('--broadcast-address', default=None, dest='broadcastAddress')
     parser.add_argument('--broadcast-rpc-address', default=None, dest='broadcastRpcAddress')
     return parser.parse_args()
