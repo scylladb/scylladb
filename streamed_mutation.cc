148e9c56089a4191952791ac5c075cd87b112696
Paweł Dziepak <pdziepak@scylladb.com>
2016-08-17 11:03:59 +0100

    streamed_mutation_from_mutation: fix destroying bi::sets
    Once unlink_leftmost_without_rebalance() has been called on a bi::set no
    other method can be used. This includes clear_and_disposed() used by the
    mutation_partition destructor.
    
    We like unlink_leftmost_without_rebalance() because it is efficient, so
    the solution is to manually finish destroying clustering row and range
    tombstone sets in the reader destructor using that function.
    
    Signed-off-by: Paweł Dziepak <pdziepak@scylladb.com>

--- streamed_mutation.cc
+++ streamed_mutation.cc
@@ -193,6 +193,27 @@ streamed_mutation streamed_mutation_from_mutation(mutation m)
             do_fill_buffer();
         }
 
+        ~reader() {
+            // After unlink_leftmost_without_rebalance() was called on a bi::set
+            // we need to complete destroying the tree using that function.
+            // clear_and_dispose() used by mutation_partition destructor won't
+            // work properly.
+
+            auto& crs = _mutation.partition().clustered_rows();
+            auto re = crs.unlink_leftmost_without_rebalance();
+            while (re) {
+                current_deleter<rows_entry>()(re);
+                re = crs.unlink_leftmost_without_rebalance();
+            }
+
+            auto& rts = _mutation.partition().row_tombstones().tombstones();
+            auto rt = rts.unlink_leftmost_without_rebalance();
+            while (rt) {
+                current_deleter<range_tombstone>()(rt);
+                rt = rts.unlink_leftmost_without_rebalance();
+            }
+        }
+
         virtual future<> fill_buffer() override {
             do_fill_buffer();
             return make_ready_future<>();
