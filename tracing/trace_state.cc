92921fe1107c89836acd0d6a2dfce059be9289cf
Vlad Zolotarov <vladz@cloudius-systems.com>
2016-08-21 14:59:43 +0300

    tracing::trace_state: push the UUID to the end of an error message in a destructor
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>
    Message-Id: <1471780783-25406-1-git-send-email-vladz@cloudius-systems.com>

--- tracing/trace_state.cc
+++ tracing/trace_state.cc
@@ -84,7 +84,7 @@ void trace_state::build_parameters_map() {
 
 trace_state::~trace_state() {
     if (!_primary && _state == state::background) {
-        trace_state_logger.error("{}: secondary session is in a background state!", get_session_id());
+        trace_state_logger.error("Secondary session is in a background state! session_id: {}", get_session_id());
     }
 
     stop_foreground_and_write();
