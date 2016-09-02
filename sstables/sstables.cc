raphaelsc/scylla

sstables: store components ranges in sstable object
Store range for each clustering component in sstable itself to
optimize sstable filtering based on clustering key.
If schema defines no clustering key, this new field will be
empty. Each range stores min and max value of that specific
component. With this information, it's possible to know if a
sstable possibly stores a given clustering component.

Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

sstables/sstables.cc
@@ -1028,6 +1028,25 @@ void sstable::validate_min_max_metadata() {
     }
 }
 
+void sstable::set_clustering_components_ranges() {
+    if (!_schema->clustering_key_size()) {
+        return;
+    }
+    auto& min_column_names = get_stats_metadata().min_column_names.elements;
+    auto& max_column_names = get_stats_metadata().max_column_names.elements;
+
+    auto s = std::min(min_column_names.size(), max_column_names.size());
+    _clustering_components_ranges.reserve(s);
+    for (auto i = 0U; i < s; i++) {
+        auto r = range<bytes_view>({{ min_column_names[i].value, true }}, {{ max_column_names[i].value, true }});
+        _clustering_components_ranges.push_back(std::move(r));
+    }
+}
+
+const std::vector<range<bytes_view>>& sstable::clustering_components_ranges() const {
+    return _clustering_components_ranges;
+}
+
 future<> sstable::read_statistics(const io_priority_class& pc) {
     return read_simple<component_type::Statistics>(_statistics, pc);
 }
@@ -1088,6 +1107,8 @@ future<> sstable::open_data() {
               _index_file_size = size;
             });
         }).then([this] {
+            this->set_clustering_components_ranges();
+
             // Get disk usage for this sstable (includes all components).
             _bytes_on_disk = 0;
             return do_for_each(_components, [this] (component_type c) {
@@ -1124,6 +1145,7 @@ future<> sstable::load() {
         return read_statistics(default_priority_class());
     }).then([this] {
         validate_min_max_metadata();
+        set_clustering_components_ranges();
         return read_compression(default_priority_class());
     }).then([this] {
         return read_filter(default_priority_class());

sstables/sstables.hh
@@ -404,6 +404,7 @@ private:
     uint64_t _index_file_size;
     uint64_t _filter_file_size = 0;
     uint64_t _bytes_on_disk = 0;
+    std::vector<range<bytes_view>> _clustering_components_ranges;
 
     // _pi_write is used temporarily for building the promoted
     // index (column sample) of one partition when writing a new sstable.
@@ -479,6 +480,12 @@ private:
     // it should be cleared. Otherwise, it could lead to bad decisions.
     // Metadata is probably incorrect if generated by previous Scylla versions.
     void validate_min_max_metadata();
+
+    // Create one range for each clustering component of this sstable.
+    // Each range stores min and max value for that specific component.
+    // It does nothing if schema defines no clustering key, and it's supposed
+    // to be called when loading an existing sstable or after writing a new one.
+    void set_clustering_components_ranges();
 
     future<> create_data();
 
@@ -627,6 +634,8 @@ public:
     // Return sstable key range as range<partition_key> reading only the summary component.
     future<range<partition_key>>
     get_sstable_key_range(const schema& s);
+
+    const std::vector<range<bytes_view>>& clustering_components_ranges() const;
 
     // Used to mark a sstable for deletion that is not relevant to the current shard.
     // It doesn't mean that the sstable will be deleted, but that the sstable is not

scylladb/scylladb

sstables/sstables.cc
@@ -1028,6 +1028,25 @@ void sstable::validate_min_max_metadata() {
     }
 }
 
+void sstable::set_clustering_components_ranges() {
+    if (!_schema->clustering_key_size()) {
+        return;
+    }
+    auto& min_column_names = get_stats_metadata().min_column_names.elements;
+    auto& max_column_names = get_stats_metadata().max_column_names.elements;
+
+    auto s = std::min(min_column_names.size(), max_column_names.size());
+    _clustering_components_ranges.reserve(s);
+    for (auto i = 0U; i < s; i++) {
+        auto r = nonwrapping_range<bytes_view>({{ min_column_names[i].value, true }}, {{ max_column_names[i].value, true }});
+        _clustering_components_ranges.push_back(std::move(r));
+    }
+}
+
+const std::vector<nonwrapping_range<bytes_view>>& sstable::clustering_components_ranges() const {
+    return _clustering_components_ranges;
+}
+
 future<> sstable::read_statistics(const io_priority_class& pc) {
     return read_simple<component_type::Statistics>(_statistics, pc);
 }
@@ -1088,6 +1107,8 @@ future<> sstable::open_data() {
               _index_file_size = size;
             });
         }).then([this] {
+            this->set_clustering_components_ranges();
+
             // Get disk usage for this sstable (includes all components).
             _bytes_on_disk = 0;
             return do_for_each(_components, [this] (component_type c) {
@@ -1125,6 +1146,7 @@ future<> sstable::load() {
         return read_statistics(default_priority_class());
     }).then([this] {
         validate_min_max_metadata();
+        set_clustering_components_ranges();
         return read_compression(default_priority_class());
     }).then([this] {
         return read_filter(default_priority_class());

sstables/sstables.hh
@@ -410,6 +410,7 @@ private:
     uint64_t _index_file_size;
     uint64_t _filter_file_size = 0;
     uint64_t _bytes_on_disk = 0;
+    std::vector<nonwrapping_range<bytes_view>> _clustering_components_ranges;
 
     // _pi_write is used temporarily for building the promoted
     // index (column sample) of one partition when writing a new sstable.
@@ -485,6 +486,12 @@ private:
     // it should be cleared. Otherwise, it could lead to bad decisions.
     // Metadata is probably incorrect if generated by previous Scylla versions.
     void validate_min_max_metadata();
+
+    // Create one range for each clustering component of this sstable.
+    // Each range stores min and max value for that specific component.
+    // It does nothing if schema defines no clustering key, and it's supposed
+    // to be called when loading an existing sstable or after writing a new one.
+    void set_clustering_components_ranges();
 
     future<> create_data();
 
@@ -633,6 +640,8 @@ public:
     // Return sstable key range as range<partition_key> reading only the summary component.
     future<range<partition_key>>
     get_sstable_key_range(const schema& s);
+
+    const std::vector<nonwrapping_range<bytes_view>>& clustering_components_ranges() const;
 
     // Used to mark a sstable for deletion that is not relevant to the current shard.
     // It doesn't mean that the sstable will be deleted, but that the sstable is not