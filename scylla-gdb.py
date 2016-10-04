41e66ebce29e5b91463ee142b02154fe07f402a8
Tomasz Grabiec <tgrabiec@scylladb.com>
2016-10-04 14:15:34 +0200

    gdb: Introduce 'scylla heapprof'
    Presents current heap profile recording.
    
    Works in text mode or dumps to collapsed stacks format from which
    flame graph can be generated.
    
    To generate a flamegraph:
    
      (gdb) scylla heapprof --flame
      Wrote heapprof.stacks
    
      $ flamegraph.pl --colors mem < heapprof.stacks > heapprof.svg
    
    flamegraph.pl comes from:
    
      https://github.com/brendangregg/FlameGraph.git
    
    Text mode example:
    
    (gdb) scylla heapprof --min 100000000
    All (274699676, #10213)
     \-- void* memory::cpu_pages::allocate_large_and_trim<memory::cpu_pages::allocate_large_aligned(unsigned int, unsigned int)::{lambda(unsigned int, unsigned int)#1}>(unsigned int, memory::cpu_pages::allocate_large_aligned(unsigned int, unsigned int)::{lambda(unsigned int, unsigned int)#1}) + 169  (268435456, #1)
         memory::allocate_large_aligned(unsigned long, unsigned long) + 87
         memory::allocate_aligned(unsigned long, unsigned long) + 48
         aligned_alloc + 9
         logalloc::segment_zone::segment_zone() + 304
         logalloc::segment_pool::allocate_segment() + 477
         logalloc::segment_pool::segment_pool() + 304
         __tls_init.part.801 + 72
         logalloc::region_group::release_requests() + 1333
         logalloc::region_group::add(logalloc::region_group*) + 514
    
    The branches are formatted like this:
    
       -- <symbol> (<size>, #<count>)
    
    Where <size> is total size of live objects and <count> is total
    number of live objects, for all objects allocated from paths going
    through this node.
    
    Nodes which share the same <size> and <count> are stacked like this:
    
       -- <symbol_1> (<size>, #<count>)
          <symbol_2>
          <symbol_3>
    
    Message-Id: <1475583334-19524-1-git-send-email-tgrabiec@scylladb.com>

--- scylla-gdb.py
+++ scylla-gdb.py
@@ -1,4 +1,5 @@
-import gdb, gdb.printing, uuid
+import gdb, gdb.printing, uuid, argparse
+from operator import attrgetter
 
 def template_arguments(gdb_type):
     n = 0
@@ -234,6 +235,194 @@ class scylla_memory(gdb.Command):
                 total += int(span['span_size'])
                 front = int(span['link']['_next'])
             gdb.write('{index:5} {size:13} {total}\n'.format(index=index, size=(1<<index)*page_size, total=total*page_size))
+
+
+
+class TreeNode(object):
+    def __init__(self, key):
+        self.key = key
+        self.children_by_key = {}
+
+    def get_or_add(self, key):
+        node = self.children_by_key.get(key, None)
+        if not node:
+            node = self.__class__(key)
+            self.add(node)
+        return node
+
+    def add(self, node):
+        self.children_by_key[node.key] = node
+
+    def squash_child(self):
+        assert self.has_only_one_child()
+        self.children_by_key = next(iter(self.children)).children_by_key
+
+    @property
+    def children(self):
+        return self.children_by_key.values()
+
+    def has_only_one_child(self):
+        return len(self.children_by_key) == 1
+
+    def has_children(self):
+        return bool(self.children_by_key)
+
+    def remove_all(self):
+        self.children_by_key.clear()
+
+class ProfNode(TreeNode):
+    def __init__(self, key):
+        super(ProfNode, self).__init__(key)
+        self.size = 0
+        self.count = 0
+        self.tail = []
+
+    @property
+    def attributes(self):
+        return {
+            'size': self.size,
+            'count': self.count
+        }
+
+def collapse_similar(node):
+    while node.has_only_one_child():
+        child = next(iter(node.children))
+        if node.attributes == child.attributes:
+            node.squash_child()
+            node.tail.append(child.key)
+        else:
+            break
+
+    for child in node.children:
+        collapse_similar(child)
+
+def strip_level(node, level):
+    if level <= 0:
+        node.remove_all()
+    else:
+        for child in node.children:
+            strip_level(child, level - 1)
+
+def print_tree(root_node,
+        formatter=attrgetter('key'),
+        order_by=attrgetter('key'),
+        printer=sys.stdout.write,
+        node_filter=None):
+
+    def print_node(node, is_last_history):
+        stems = (" |   ", "     ")
+        branches = (" |-- ", " \-- ")
+
+        label_lines = formatter(node).rstrip('\n').split('\n')
+        prefix_without_branch = ''.join(map(stems.__getitem__, is_last_history[:-1]))
+
+        if is_last_history:
+            printer(prefix_without_branch)
+            printer(branches[is_last_history[-1]])
+        printer("%s\n" % label_lines[0])
+
+        for line in label_lines[1:]:
+            printer(''.join(map(stems.__getitem__, is_last_history)))
+            printer("%s\n" % line)
+
+        children = sorted(filter(node_filter, node.children), key=order_by)
+        if children:
+            for child in children[:-1]:
+                print_node(child, is_last_history + [False])
+            print_node(children[-1], is_last_history + [True])
+
+        is_last = not is_last_history or is_last_history[-1]
+        if not is_last:
+           printer("%s%s\n" % (prefix_without_branch, stems[False]))
+
+    if not node_filter or node_filter(root_node):
+        print_node(root_node, [])
+
+
+class scylla_heapprof(gdb.Command):
+    def __init__(self):
+        gdb.Command.__init__(self, 'scylla heapprof', gdb.COMMAND_USER, gdb.COMPLETE_COMMAND)
+
+    def invoke(self, arg, from_tty):
+        parser = argparse.ArgumentParser(description="scylla heapprof")
+        parser.add_argument("-G", "--inverted", action="store_true",
+            help="Compute caller-first profile instead of callee-first")
+        parser.add_argument("-a", "--addresses", action="store_true",
+            help="Show raw addresses before resolved symbol names")
+        parser.add_argument("--no-symbols", action="store_true",
+            help="Show only raw addresses")
+        parser.add_argument("--flame", action="store_true",
+            help="Write flamegraph data to heapprof.stacks instead of showing the profile")
+        parser.add_argument("--min", action="store", type=int, default=0,
+            help="Drop branches allocating less than given amount")
+        try:
+            args = parser.parse_args(arg.split())
+        except SystemExit:
+            return
+
+        root = ProfNode(None)
+        cpu_mem = gdb.parse_and_eval('\'memory::cpu_mem\'')
+        site = cpu_mem['alloc_site_list_head']
+
+        while site:
+            size = int(site['size'])
+            count = int(site['count'])
+            if size:
+                n = root
+                n.size += size
+                n.count += count
+                addresses = list(map(int, std_vector(site['backtrace'])))
+                addresses.pop(0) # drop memory::get_backtrace()
+                if args.inverted:
+                    seq = reversed(addresses)
+                else:
+                    seq = addresses
+                for addr in seq:
+                    n = n.get_or_add(addr)
+                    n.size += size
+                    n.count += count
+            site = site['next']
+
+        def resolver(addr):
+            if args.no_symbols:
+                return '0x%x' % addr
+            if args.addresses:
+                return '0x%x %s' % (addr, resolve(addr) or '')
+            return resolve(addr) or ('0x%x' % addr)
+
+        if args.flame:
+            file_name = 'heapprof.stacks'
+            with open(file_name, 'w') as out:
+                trace = list()
+                def print_node(n):
+                    if n.key:
+                        trace.append(n.key)
+                        trace.extend(n.tail)
+                    for c in n.children:
+                        print_node(c)
+                    if not n.has_children():
+                        out.write("%s %d\n" % (';'.join(map(lambda x: '%s (#%d)' % (x, n.count), map(resolver, trace))), n.size))
+                    if n.key:
+                        del trace[-1 - len(n.tail):]
+                print_node(root)
+            gdb.write('Wrote %s\n' % (file_name))
+        else:
+            def node_formatter(n):
+                if n.key is None:
+                    name = "All"
+                else:
+                    name = resolver(n.key)
+                return "%s (%d, #%d)\n%s" % (name, n.size, n.count, '\n'.join(map(resolver, n.tail)))
+
+            def node_filter(n):
+                return n.size >= args.min
+
+            collapse_similar(root)
+            print_tree(root,
+                formatter=node_formatter,
+                order_by=lambda n: -n.size,
+                node_filter=node_filter,
+                printer=gdb.write)
 
 def get_seastar_memory_start_and_size():
     cpu_mem = gdb.parse_and_eval('\'memory::cpu_mem\'')
@@ -686,6 +875,7 @@ scylla_memory()
 scylla_ptr()
 scylla_mem_ranges()
 scylla_mem_range()
+scylla_heapprof()
 scylla_lsa()
 scylla_lsa_zones()
 scylla_lsa_segment()
