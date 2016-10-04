glommer/scylla

sstables: use special reader for writing a memtable
Right now the special reader doesn't do much, but the idea is that we will
soon replace it will a reader that specializes in flush, and is in turn able
to provide read-side on-flush functionality like virtual dirty.

Signed-off-by: Glauber Costa <glauber@scylladb.com>

memtable.cc
@@ -217,6 +217,11 @@ memtable::make_reader(schema_ptr s,
     }
 }
 
+mutation_reader
+memtable::make_flush_reader(schema_ptr s) {
+    return make_mutation_reader<scanning_reader>(std::move(s), shared_from_this(), query::full_partition_range, query::no_clustering_key_filtering, default_priority_class());
+}
+
 void
 memtable::update(const db::replay_position& rp) {
     if (_replay_position < rp) {

scylladb/scylladb

memtable.cc
@@ -217,6 +217,11 @@ memtable::make_reader(schema_ptr s,
     }
 }
 
+mutation_reader
+memtable::make_flush_reader(schema_ptr s) {
+    return make_mutation_reader<scanning_reader>(std::move(s), shared_from_this(), query::full_partition_range, query::full_slice, default_priority_class());
+}
+
 void
 memtable::update(const db::replay_position& rp) {
     if (_replay_position < rp) {