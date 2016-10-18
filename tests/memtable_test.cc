308434f89136e8c790688e55e409798a07c68706
Tomasz Grabiec <tgrabiec@scylladb.com>
2016-10-18 11:13:49 +0200

    tests: memtable: Add test for partition version list consistency after compaction

--- tests/memtable_test.cc
+++ tests/memtable_test.cc
@@ -32,6 +32,7 @@
 #include "memtable.hh"
 #include "mutation_source_test.hh"
 #include "mutation_reader_assertions.hh"
+#include "mutation_assertions.hh"
 
 #include "disk-error-handler.hh"
 
@@ -190,6 +191,70 @@ SEASTAR_TEST_CASE(test_virtual_dirty_accounting_on_flush) {
 
         std::reverse(virtual_dirty_values.begin(), virtual_dirty_values.end());
         BOOST_REQUIRE(std::is_sorted(virtual_dirty_values.begin(), virtual_dirty_values.end()));
+    });
+}
+
+// Reproducer for #1753
+SEASTAR_TEST_CASE(test_partition_version_consistency_after_lsa_compaction_happens) {
+    return seastar::async([] {
+        schema_ptr s = schema_builder("ks", "cf")
+                .with_column("pk", bytes_type, column_kind::partition_key)
+                .with_column("ck", bytes_type, column_kind::clustering_key)
+                .with_column("col", bytes_type, column_kind::regular_column)
+                .build();
+
+        auto mt = make_lw_shared<memtable>(s);
+
+        auto empty_m = make_unique_mutation(s);
+        auto ck1 = clustering_key::from_single_value(*s, data_value(make_unique_bytes()).serialize());
+        auto ck2 = clustering_key::from_single_value(*s, data_value(make_unique_bytes()).serialize());
+        auto ck3 = clustering_key::from_single_value(*s, data_value(make_unique_bytes()).serialize());
+
+        auto m1 = empty_m;
+        m1.set_clustered_cell(ck1, to_bytes("col"), data_value(bytes(bytes::initialized_later(), 8)), next_timestamp());
+
+        auto m2 = empty_m;
+        m2.set_clustered_cell(ck2, to_bytes("col"), data_value(bytes(bytes::initialized_later(), 8)), next_timestamp());
+
+        auto m3 = empty_m;
+        m3.set_clustered_cell(ck3, to_bytes("col"), data_value(bytes(bytes::initialized_later(), 8)), next_timestamp());
+
+        mt->apply(m1);
+        auto rd1 = mt->make_reader(s);
+        streamed_mutation_opt stream1 = rd1().get0();
+
+        mt->apply(m2);
+        auto rd2 = mt->make_reader(s);
+        streamed_mutation_opt stream2 = rd2().get0();
+
+        mt->apply(m3);
+        auto rd3 = mt->make_reader(s);
+        streamed_mutation_opt stream3 = rd3().get0();
+
+        logalloc::shard_tracker().full_compaction();
+
+        auto rd4 = mt->make_reader(s);
+        streamed_mutation_opt stream4 = rd4().get0();
+        auto rd5 = mt->make_reader(s);
+        streamed_mutation_opt stream5 = rd5().get0();
+        auto rd6 = mt->make_reader(s);
+        streamed_mutation_opt stream6 = rd6().get0();
+
+        assert_that(mutation_from_streamed_mutation(std::move(stream1)).get0()).has_mutation().is_equal_to(m1);
+        assert_that(mutation_from_streamed_mutation(std::move(stream2)).get0()).has_mutation().is_equal_to(m1 + m2);
+        assert_that(mutation_from_streamed_mutation(std::move(stream3)).get0()).has_mutation().is_equal_to(m1 + m2 + m3);
+
+        rd3 = {};
+
+        assert_that(mutation_from_streamed_mutation(std::move(stream4)).get0()).has_mutation().is_equal_to(m1 + m2 + m3);
+
+        rd1 = {};
+
+        assert_that(mutation_from_streamed_mutation(std::move(stream5)).get0()).has_mutation().is_equal_to(m1 + m2 + m3);
+
+        rd2 = {};
+
+        assert_that(mutation_from_streamed_mutation(std::move(stream6)).get0()).has_mutation().is_equal_to(m1 + m2 + m3);
     });
 }
 
