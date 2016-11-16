c052a1bc4f9ad3c7b4602d8e6d1fccf05a2232a0
Gleb Natapov <gleb@scylladb.com>
2016-11-16 20:19:37 +0200

    sstable: use schema's min_index_interval config when generating missing summary
    Message-Id: <20161116181937.GA25303@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -1883,9 +1883,7 @@ future<> sstable::generate_summary(const io_priority_class& pc) {
             return index_file.size().then([this, &pc, index_file] (auto size) {
                 // an upper bound. Surely to be less than this.
                 auto estimated_partitions = size / sizeof(uint64_t);
-                // Since we don't have a summary, use a default min_index_interval, and if needed we'll resample
-                // later.
-                prepare_summary(_summary, estimated_partitions, DEFAULT_MIN_INDEX_INTERVAL);
+                prepare_summary(_summary, estimated_partitions, _schema->min_index_interval());
 
                 file_input_stream_options options;
                 options.buffer_size = sstable_buffer_size;
