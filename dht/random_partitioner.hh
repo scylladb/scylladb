8b1d689de84f7e8e4fc981cccfd1882b0c7ca4c5
Avi Kivity <avi@scylladb.com>
2016-11-20 17:20:15 +0200

    partitioner: add ignore_msb parameters to byte ordered and random partitioners
    Ignored; doesn't make sense on byte ordered, and random is deprecated.

--- dht/random_partitioner.hh
+++ dht/random_partitioner.hh
@@ -31,7 +31,7 @@ namespace dht {
 class random_partitioner final : public i_partitioner {
     unsigned _shard_count;
 public:
-    random_partitioner(unsigned shard_count = smp::count) : _shard_count(shard_count) {}
+    random_partitioner(unsigned shard_count = smp::count, unsigned ignore_msb = 0) : _shard_count(shard_count) {}
     virtual const sstring name() { return "org.apache.cassandra.dht.RandomPartitioner"; }
     virtual token get_token(const schema& s, partition_key_view key) override;
     virtual token get_token(const sstables::key_view& key) override;
