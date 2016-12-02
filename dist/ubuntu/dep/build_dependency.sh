5a5ab512542a95b8b3b3f727c3a58201a208efe3
Takuya ASADA <syuu@scylladb.com>
2016-12-02 17:34:32 +0900

    dist/ubuntu/dep: fix incorrect file path to detect previously built .deb existance check
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <1480667672-9453-4-git-send-email-syuu@scylladb.com>

--- dist/ubuntu/dep/build_dependency.sh
+++ dist/ubuntu/dep/build_dependency.sh
@@ -5,7 +5,7 @@ DISTRIBUTION=`lsb_release -i|awk '{print $3}'`
 
 sudo apt-get install -y gdebi-core
 if [ "$VERSION_ID" = "14.04" ] || [ "$DISTRIBUTION" = "Debian" ]; then
-    if [ ! -f build/antlr3_3.5.2-1_all.deb ]; then
+    if [ ! -f build/antlr3_*.deb ]; then
         rm -rf build/antlr3-3.5.2
         mkdir -p build/antlr3-3.5.2
         cp -a dist/ubuntu/dep/antlr3-3.5.2/* build/antlr3-3.5.2
@@ -15,14 +15,14 @@ if [ "$VERSION_ID" = "14.04" ] || [ "$DISTRIBUTION" = "Debian" ]; then
         debuild -r fakeroot --no-tgz-check -us -uc
         cd -
     fi
-    if [ ! -f build/scylla-env_1.0-0ubuntu1_all.deb ]; then
+    if [ ! -f build/scylla-env_*.deb ]; then
         rm -rf build/scylla-env-1.0
         cp -a dist/common/dep/scylla-env-1.0 build/
         cd build/scylla-env-1.0
         debuild -r fakeroot --no-tgz-check -us -uc
         cd -
     fi
-    if [ ! -f build/scylla-gdb_7.11-0ubuntu1_amd64.deb ]; then
+    if [ ! -f build/scylla-gdb_*.deb ]; then
         rm -rf build/gdb-7.11
         if [ ! -f build/gdb_7.11-0ubuntu1.dsc ]; then
             wget -nv -O build/gdb_7.11-0ubuntu1.dsc http://archive.ubuntu.com/ubuntu/pool/main/g/gdb/gdb_7.11-0ubuntu1.dsc
@@ -45,7 +45,7 @@ if [ "$VERSION_ID" = "14.04" ] || [ "$DISTRIBUTION" = "Debian" ]; then
     fi
 fi
 
-if [ ! -f build/antlr3-c++-dev_3.5.2-1_all.deb ]; then
+if [ ! -f build/antlr3-c++-dev_*.deb ]; then
     rm -rf build/antlr3-c++-dev-3.5.2
     if [ ! -f build/3.5.2.tar.gz ]; then
         wget -nv -O build/3.5.2.tar.gz https://github.com/antlr/antlr3/archive/3.5.2.tar.gz
@@ -61,7 +61,7 @@ if [ ! -f build/antlr3-c++-dev_3.5.2-1_all.deb ]; then
     cd -
 fi
 
-if [ ! -f build/libthrift0_1.0.0-dev_amd64.deb ]; then
+if [ ! -f build/libthrift0_*.deb ]; then
     rm -rf build/thrift-0.9.3
     if [ ! -f build/thrift-0.9.3.tar.gz ]; then
         wget -nv -O build/thrift-0.9.3.tar.gz http://archive.apache.org/dist/thrift/0.9.3/thrift-0.9.3.tar.gz
@@ -76,17 +76,19 @@ if [ ! -f build/libthrift0_1.0.0-dev_amd64.deb ]; then
 fi
 
 if [ "$DISTRIBUTION" = "Debian" ] && [ "$VERSION_ID" = "8" ]; then
-    sudo cp dist/ubuntu/dep/debian-stretch-source.list /etc/apt/sources.list.d/
-    sudo apt-get update
-    cd build
-    apt-get source gcc-5/stretch=5.4.1-2
-    cd gcc-5-5.4.1
-    # resolve build time dependencies manually, since mk-build-deps doesn't works for gcc package
-    sudo apt-get install -y g++-multilib libc6-dev-i386 lib32gcc1 libc6-dev-x32 libx32gcc1 libc6-dbg m4 libtool autoconf2.64 autogen gawk zlib1g-dev systemtap-sdt-dev gperf bison flex gdb texinfo locales sharutils libantlr-java libffi-dev gnat-4.9 libisl-dev libmpc-dev libmpfr-dev libgmp-dev dejagnu realpath chrpath quilt doxygen graphviz ghostscript texlive-latex-base xsltproc libxml2-utils docbook-xsl-ns
-    patch -p0 < ../../dist/ubuntu/dep/debian-gcc-5-jessie.diff
-    ./debian/rules control
-    debuild -r fakeroot -us -uc
-    cd ../..
+    if [ ! -f build/gcc-5_*.deb ]; then
+        sudo cp dist/ubuntu/dep/debian-stretch-source.list /etc/apt/sources.list.d/
+        sudo apt-get update
+        cd build
+        apt-get source gcc-5/stretch=5.4.1-2
+        cd gcc-5-5.4.1
+        # resolve build time dependencies manually, since mk-build-deps doesn't works for gcc package
+        sudo apt-get install -y g++-multilib libc6-dev-i386 lib32gcc1 libc6-dev-x32 libx32gcc1 libc6-dbg m4 libtool autoconf2.64 autogen gawk zlib1g-dev systemtap-sdt-dev gperf bison flex gdb texinfo locales sharutils libantlr-java libffi-dev gnat-4.9 libisl-dev libmpc-dev libmpfr-dev libgmp-dev dejagnu realpath chrpath quilt doxygen graphviz ghostscript texlive-latex-base xsltproc libxml2-utils docbook-xsl-ns
+        patch -p0 < ../../dist/ubuntu/dep/debian-gcc-5-jessie.diff
+        ./debian/rules control
+        debuild -r fakeroot -us -uc
+        cd ../..
+    fi
 fi
 
 if [ "$VERSION_ID" = "14.04" ]; then
