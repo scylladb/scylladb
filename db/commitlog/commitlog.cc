53c3219e560540524633261a83a152e4e3d58944
Pekka Enberg <penberg@scylladb.com>
2016-12-13 09:47:27 +0200

    Revert "commitlog: use commitlog priority for replay"
    This reverts commit 58504bda5b05f5633da03723c14456c8d785f321.

--- db/commitlog/commitlog.cc
+++ db/commitlog/commitlog.cc
@@ -1557,15 +1557,6 @@ db::commitlog::read_log_file(const sstring& filename, commit_load_reader_func ne
 subscription<temporary_buffer<char>, db::replay_position>
 db::commitlog::read_log_file(file f, commit_load_reader_func next, position_type off) {
     struct work {
-    private:
-        file_input_stream_options make_file_input_stream_options() {
-            file_input_stream_options fo;
-            fo.buffer_size = db::commitlog::segment::default_size;
-            fo.read_ahead = 0;
-            fo.io_priority_class = service::get_local_commitlog_priority();
-            return fo;
-        }
-    public:
         file f;
         stream<temporary_buffer<char>, replay_position> s;
         input_stream<char> fin;
@@ -1581,7 +1572,7 @@ db::commitlog::read_log_file(file f, commit_load_reader_func next, position_type
         bool header = true;
 
         work(file f, position_type o = 0)
-                : f(f), fin(make_file_input_stream(f, o, make_file_input_stream_options())), start_off(o) {
+                : f(f), fin(make_file_input_stream(f)), start_off(o) {
         }
         work(work&&) = default;
 
