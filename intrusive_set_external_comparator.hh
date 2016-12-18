041b0a65ac62be01ba733332ab7d3ddaa9df9fb7
Piotr Jastrzebski <piotr@scylladb.com>
2016-12-18 18:56:55 +0100

    Implement intrusive set using rbtree_algorithms
    This new implementation takes less memory because it
    does not store comparator.
    
    It also uses tree nodes optimized for size. This means
    that instead of storing an enum field |color| they embed
    this information inside pointer to parent.
    
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

---
+++ intrusive_set_external_comparator.hh
@@ -0,0 +1,220 @@
+/*
+ * Copyright (C) 2016 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * (C) Copyright Ion Gaztanaga 2013-2014
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#pragma once
+
+#include <boost/intrusive/set.hpp>
+
+namespace bi = boost::intrusive;
+
+typedef bi::rbtree_algorithms<bi::rbtree_node_traits<void*, true>> algo;
+
+class intrusive_set_external_comparator_member_hook : public bi::set_member_hook<bi::optimize_size<true>> {
+public:
+    intrusive_set_external_comparator_member_hook() = default;
+    intrusive_set_external_comparator_member_hook(intrusive_set_external_comparator_member_hook&& o) noexcept {
+        algo::replace_node(o.this_ptr(), this_ptr());
+        algo::init(o.this_ptr());
+    }
+};
+
+template<typename Elem,
+         intrusive_set_external_comparator_member_hook Elem::* PtrToMember>
+class intrusive_set_external_comparator final {
+    typedef boost::intrusive::mhtraits<Elem, intrusive_set_external_comparator_member_hook, PtrToMember> value_traits;
+    typedef typename value_traits::node_traits node_traits;
+    typedef typename node_traits::node_ptr node_ptr;
+public:
+    typedef Elem value_type;
+    typedef typename bi::tree_iterator<value_traits, false> iterator;
+    typedef typename bi::tree_iterator<value_traits, true> const_iterator;
+    typedef typename bi::reverse_iterator<iterator> reverse_iterator;
+    typedef typename bi::reverse_iterator<const_iterator> const_reverse_iterator;
+
+private:
+    intrusive_set_external_comparator_member_hook _header;
+    value_traits _value_traits;
+
+    struct key_of_value {
+        typedef Elem type;
+        Elem& operator()(Elem& t) { return t; }
+    };
+
+    template <typename Comparator>
+    struct key_node_comparator {
+        Comparator _cmp;
+        const value_traits& _value_traits;
+        key_node_comparator(Comparator cmp, const value_traits& value_traits) : _cmp(cmp), _value_traits(value_traits) { }
+        bool operator()(const node_ptr& a, const node_ptr& b) {
+            return _cmp(*_value_traits.to_value_ptr(a), *_value_traits.to_value_ptr(b));
+        }
+        template <typename T1>
+        bool operator()(const node_ptr& a, const T1& b) {
+            return _cmp(*_value_traits.to_value_ptr(a), b);
+        }
+        template <typename T1>
+        bool operator()(const T1& a, const node_ptr& b) {
+            return _cmp(a, *_value_traits.to_value_ptr(b));
+        }
+        template <typename T1, typename T2>
+        bool operator()(const T1& a, const T2& b) {
+            return _cmp(a, b);
+        }
+    };
+
+    typedef typename bi::value_traits_pointers<value_traits>::const_value_traits_ptr const_value_traits_ptr;
+
+    const_value_traits_ptr priv_value_traits_ptr() const {
+        return bi::pointer_traits<const_value_traits_ptr>::pointer_to(_value_traits);
+    }
+    template <typename Comparator>
+    key_node_comparator<Comparator> key_node_comp(Comparator comp) const {
+        return key_node_comparator<Comparator>(comp, _value_traits);
+    }
+    iterator insert_unique_commit(Elem& value, const algo::insert_commit_data &commit_data) {
+        node_ptr to_insert(_value_traits.to_node_ptr(value));
+        algo::insert_unique_commit(_header.this_ptr(), to_insert, commit_data);
+        return iterator(to_insert, priv_value_traits_ptr());
+    }
+public:
+    intrusive_set_external_comparator() { algo::init_header(_header.this_ptr()); }
+    intrusive_set_external_comparator(intrusive_set_external_comparator&& o) : _value_traits(std::move(o._value_traits)) {
+        algo::swap_tree(_header.this_ptr(), node_ptr(o._header.this_ptr()));
+    }
+    iterator begin() { return iterator(algo::begin_node(_header.this_ptr()), priv_value_traits_ptr()); }
+    const_iterator begin() const { return const_iterator(algo::begin_node(_header.this_ptr()), priv_value_traits_ptr()); }
+    iterator end() { return iterator(algo::end_node(_header.this_ptr()), priv_value_traits_ptr()); }
+    const_iterator end() const { return const_iterator(algo::end_node(_header.this_ptr()), priv_value_traits_ptr()); }
+    reverse_iterator rbegin() { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+    reverse_iterator rend() { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    template<class Disposer>
+    void clear_and_dispose(Disposer disposer) {
+        algo::clear_and_dispose(_header.this_ptr(),
+                                [this, &disposer] (const node_ptr& p) {
+                                    disposer(this->_value_traits.to_value_ptr(p));
+                                });
+        algo::init_header(_header.this_ptr());
+    }
+    bool empty() const { return algo::unique(_header.this_ptr()); }
+
+    // WARNING: this method has O(N) time complexity, use with care
+    auto size() const { return algo::size(_header.this_ptr()); }
+    iterator erase(const_iterator i) {
+        const_iterator ret(i);
+        ++ret;
+        node_ptr to_erase(i.pointed_node());
+        algo::erase(_header.this_ptr(), to_erase);
+        algo::init(to_erase);
+        return ret.unconst();
+    }
+    iterator erase(const_iterator b, const_iterator e) {
+        while (b != e) {
+            erase(b++);
+        }
+        return b.unconst();
+    }
+    template<class Disposer>
+    iterator erase_and_dispose(const_iterator i, Disposer disposer) {
+        node_ptr to_erase(i.pointed_node());
+        iterator ret(erase(i));
+        disposer(_value_traits.to_value_ptr(to_erase));
+        return ret;
+    }
+    template<class Disposer>
+    iterator erase_and_dispose(const_iterator b, const_iterator e, Disposer disposer) {
+        while (b != e) {
+            erase_and_dispose(b++, disposer);
+        }
+        return b.unconst();
+    }
+    template <class Cloner, class Disposer>
+    void clone_from(const intrusive_set_external_comparator &src, Cloner cloner, Disposer disposer) {
+        clear_and_dispose(disposer);
+        if (!src.empty()) {
+            auto rollback = defer([this, &disposer] { this->clear_and_dispose(disposer); });
+            algo::clone(src._header.this_ptr(),
+                        _header.this_ptr(),
+                        [this, &cloner] (const node_ptr& p) {
+                            return this->_value_traits.to_node_ptr(*cloner(*this->_value_traits.to_value_ptr(p)));
+                        },
+                        [this, &disposer] (const node_ptr& p) {
+                            disposer(this->_value_traits.to_value_ptr(p));
+                        });
+            rollback.cancel();
+        }
+    }
+    Elem* unlink_leftmost_without_rebalance() {
+        node_ptr to_be_disposed(algo::unlink_leftmost_without_rebalance(_header.this_ptr()));
+        if(!to_be_disposed)
+            return 0;
+        algo::init(to_be_disposed);
+        return _value_traits.to_value_ptr(to_be_disposed);
+    }
+    iterator insert_before(const_iterator pos, Elem& value) {
+        node_ptr to_insert(_value_traits.to_node_ptr(value));
+        return iterator(algo::insert_before(_header.this_ptr(), pos.pointed_node(), to_insert), priv_value_traits_ptr());
+    }
+    template<class KeyType, class KeyTypeKeyCompare>
+    iterator upper_bound(const KeyType& key, KeyTypeKeyCompare comp) {
+        return iterator(algo::upper_bound(_header.this_ptr(), key, key_node_comp(comp)), priv_value_traits_ptr());
+    }
+    template<class KeyType, class KeyTypeKeyCompare>
+    const_iterator upper_bound(const KeyType& key, KeyTypeKeyCompare comp) const {
+        return const_iterator(algo::upper_bound(_header.this_ptr(), key, key_node_comp(comp)), priv_value_traits_ptr());
+    }
+    template<class KeyType, class KeyTypeKeyCompare>
+    iterator lower_bound(const KeyType &key, KeyTypeKeyCompare comp) {
+        return iterator(algo::lower_bound(_header.this_ptr(), key, key_node_comp(comp)), priv_value_traits_ptr());
+    }
+    template<class KeyType, class KeyTypeKeyCompare>
+    const_iterator lower_bound(const KeyType &key, KeyTypeKeyCompare comp) const {
+        return const_iterator(algo::lower_bound(_header.this_ptr(), key, key_node_comp(comp)), priv_value_traits_ptr());
+    }
+    template<class KeyType, class KeyTypeKeyCompare>
+    iterator find(const KeyType &key, KeyTypeKeyCompare comp) {
+        return iterator(algo::find(_header.this_ptr(), key, key_node_comp(comp)), priv_value_traits_ptr());
+    }
+    template<class KeyType, class KeyTypeKeyCompare>
+    const_iterator find(const KeyType &key, KeyTypeKeyCompare comp) const {
+        return const_iterator(algo::find(_header.this_ptr(), key, key_node_comp(comp)), priv_value_traits_ptr());
+    }
+    template<class ElemCompare>
+    iterator insert(const_iterator hint, Elem& value, ElemCompare cmp) {
+        algo::insert_commit_data commit_data;
+        std::pair<node_ptr, bool> ret =
+            algo::insert_unique_check(_header.this_ptr(),
+                                      hint.pointed_node(),
+                                      key_of_value()(value),
+                                      key_node_comp(cmp),
+                                      commit_data);
+        return ret.second ? insert_unique_commit(value, commit_data)
+                          : iterator(ret.first, priv_value_traits_ptr());
+    }
+};
--- mutation_partition.hh
+++ mutation_partition.hh
@@ -41,7 +41,7 @@
 #include "hashing_partition_visitor.hh"
 #include "range_tombstone_list.hh"
 #include "clustering_key_filter.hh"
-#include "intrusive_set.hh"
+#include "intrusive_set_external_comparator.hh"
 
 //
 // Container for cells of a row. Cells are identified by column_id.
@@ -439,7 +439,7 @@ public:
 };
 
 class rows_entry {
-    intrusive_set_member_hook _link;
+    intrusive_set_external_comparator_member_hook _link;
     clustering_key _key;
     deletable_row _row;
     friend class mutation_partition;
@@ -535,7 +535,7 @@ class serializer;
 
 class mutation_partition final {
 public:
-    using rows_type = intrusive_set<rows_entry, &rows_entry::_link, rows_entry::compare>;
+    using rows_type = intrusive_set_external_comparator<rows_entry, &rows_entry::_link>;
     friend class rows_entry;
     friend class size_calculator;
 private:
@@ -553,11 +553,11 @@ private:
 public:
     struct copy_comparators_only {};
     mutation_partition(schema_ptr s)
-        : _rows(rows_entry::compare(*s))
+        : _rows()
         , _row_tombstones(*s)
     { }
     mutation_partition(mutation_partition& other, copy_comparators_only)
-        : _rows(other._rows.key_comp())
+        : _rows()
         , _row_tombstones(other._row_tombstones, range_tombstone_list::copy_comparator_only())
     { }
     mutation_partition(mutation_partition&&) = default;
