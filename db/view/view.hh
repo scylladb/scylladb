duarten/scylla

materialized views: Add view class
This patch adds the view class, which will contains functions related
to populating a view, either from the base table's write path or from
the view building mechanism which copies over already existing data in
the base table.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

db/view/view.hh
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2016 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "schema.hh"
+
+namespace db {
+
+namespace view {
+
+class view final {
+    schema_ptr _schema;
+public:
+    view(schema_ptr schema)
+            : _schema(schema)
+    { }
+
+    view(const view&) = delete;
+    view& operator=(const view&) = delete;
+
+    schema_ptr schema() const {
+        return _schema;
+    }
+
+    void update(schema_ptr new_schema) {
+        _schema = new_schema;
+    }
+};
+
+}
+
+}

scylladb/scylladb

db/view/view.hh
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2016 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "schema.hh"
+
+namespace db {
+
+namespace view {
+
+class view final {
+    view_ptr _schema;
+public:
+    view(view_ptr schema)
+            : _schema(schema)
+    { }
+
+    view_ptr schema() const {
+        return _schema;
+    }
+
+    void update(view_ptr new_schema) {
+        _schema = new_schema;
+    }
+};
+
+}
+
+}