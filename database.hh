duarten/scylla

keyspace_metadata: Add tables() and views() functions
This patch adds utility functions to keyspace_metadata to select only
the tables or only the views out of all the schemas.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

database.hh
@@ -912,6 +912,8 @@ public:
     void remove_user_type(const user_type ut) {
         _user_types->remove_type(ut);
     }
+    std::vector<schema_ptr> tables() const;
+    std::vector<schema_ptr> views() const;
     friend std::ostream& operator<<(std::ostream& os, const keyspace_metadata& m);
 };
 

scylladb/scylladb

database.hh
@@ -956,6 +956,8 @@ public:
     void remove_user_type(const user_type ut) {
         _user_types->remove_type(ut);
     }
+    std::vector<schema_ptr> tables() const;
+    std::vector<view_ptr> views() const;
     friend std::ostream& operator<<(std::ostream& os, const keyspace_metadata& m);
 };
 