duarten/scylla

schema_tables: Make view mutations
This patch adds functions that translate a view schema to the
corresponding mutations.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

db/schema_tables.hh
@@ -149,6 +149,10 @@ schema_ptr create_view_from_mutations(schema_mutations sm, std::experimental::op
 
 future<std::vector<schema_ptr>> create_views_from_schema_partition(distributed<service::storage_proxy>& proxy, const schema_result::mapped_type& result);
 
+schema_mutations make_view_mutations(schema_ptr view, api::timestamp_type timestamp, bool with_columns);
+
+void add_view_to_schema_mutation(schema_ptr view, api::timestamp_type timestamp, bool with_columns, std::vector<mutation>& mutations);
+
 sstring serialize_kind(column_kind kind);
 column_kind deserialize_kind(sstring kind);
 data_type parse_type(sstring str);

scylladb/scylladb

db/schema_tables.hh
@@ -149,6 +149,10 @@ view_ptr create_view_from_mutations(schema_mutations sm, std::experimental::opti
 
 future<std::vector<view_ptr>> create_views_from_schema_partition(distributed<service::storage_proxy>& proxy, const schema_result::mapped_type& result);
 
+schema_mutations make_schema_mutations(schema_ptr s, api::timestamp_type timestamp, bool with_columns);
+
+void add_table_or_view_to_schema_mutation(schema_ptr view, api::timestamp_type timestamp, bool with_columns, std::vector<mutation>& mutations);
+
 sstring serialize_kind(column_kind kind);
 column_kind deserialize_kind(sstring kind);
 data_type parse_type(sstring str);