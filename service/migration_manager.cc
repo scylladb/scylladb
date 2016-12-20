duarten/scylla

migration_manager: Announce view update
Signed-off-by: Duarte Nunes <duarte@scylladb.com>

service/migration_manager.cc
@@ -635,6 +635,27 @@ future<> migration_manager::announce_new_view(schema_ptr view, bool announce_loc
     }
 }
 
+future<> migration_manager::announce_view_update(schema_ptr view, bool announce_locally)
+{
+#if 0
+    view.metadata.validate();
+#endif
+    auto& db = get_local_storage_proxy().get_db().local();
+    try {
+        auto&& keyspace = db.find_keyspace(view->ks_name()).metadata();
+        auto& old_view = keyspace->cf_meta_data().at(view->cf_name());
+#if 0
+        oldCfm.validateCompatility(cfm);
+#endif
+        logger.info("Update view '{}.{}' From {} To {}", view->ks_name(), view->cf_name(), *old_view, *view);
+        auto mutations = db::schema_tables::make_update_view_mutations(std::move(keyspace), old_view, std::move(view), api::new_timestamp());
+        return announce(std::move(mutations), announce_locally);
+    } catch (const std::out_of_range& e) {
+        throw exceptions::configuration_exception(sprint("Cannot update non existing materialized view '%s' in keyspace '%s'.",
+                                                         view->cf_name(), view->ks_name()));
+    }
+}
+
 #if 0
 public static void announceFunctionDrop(UDFunction udf, boolean announceLocally)
 {

service/migration_manager.hh
@@ -119,6 +119,8 @@ public:
 
     future<> announce_new_view(schema_ptr view, bool announce_locally = false);
 
+    future<> announce_view_update(schema_ptr view, bool announce_locally = false);
+
     /**
      * actively announce a new version to active hosts via rpc
      * @param schema The schema mutation to be applied

scylladb/scylladb

service/migration_manager.cc
@@ -635,6 +635,30 @@ future<> migration_manager::announce_new_view(view_ptr view, bool announce_local
     }
 }
 
+future<> migration_manager::announce_view_update(view_ptr view, bool announce_locally)
+{
+#if 0
+    view.metadata.validate();
+#endif
+    auto& db = get_local_storage_proxy().get_db().local();
+    try {
+        auto&& keyspace = db.find_keyspace(view->ks_name()).metadata();
+        auto& old_view = keyspace->cf_meta_data().at(view->cf_name());
+        if (!old_view->is_view()) {
+            throw exceptions::invalid_request_exception("Cannot use ALTER MATERIALIZED VIEW on Table");
+        }
+#if 0
+        oldCfm.validateCompatility(cfm);
+#endif
+        logger.info("Update view '{}.{}' From {} To {}", view->ks_name(), view->cf_name(), *old_view, *view);
+        auto mutations = db::schema_tables::make_update_view_mutations(std::move(keyspace), view_ptr(old_view), std::move(view), api::new_timestamp());
+        return announce(std::move(mutations), announce_locally);
+    } catch (const std::out_of_range& e) {
+        throw exceptions::configuration_exception(sprint("Cannot update non existing materialized view '%s' in keyspace '%s'.",
+                                                         view->cf_name(), view->ks_name()));
+    }
+}
+
 #if 0
 public static void announceFunctionDrop(UDFunction udf, boolean announceLocally)
 {

service/migration_manager.hh
@@ -119,6 +119,8 @@ public:
 
     future<> announce_new_view(view_ptr view, bool announce_locally = false);
 
+    future<> announce_view_update(view_ptr view, bool announce_locally = false);
+
     /**
      * actively announce a new version to active hosts via rpc
      * @param schema The schema mutation to be applied