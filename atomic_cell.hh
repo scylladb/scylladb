7604b926e1881069e8a1ec29e602172e0e227812
Paweł Dziepak <pdziepak@scylladb.com>
2017-02-22 11:27:11 +0000

    atomic_cell: add COUNTER_IN_PLACE_REVERT flag
    The general algorithm for merging counter cells involves allocating a
    new buffer for the shards. However, it is expected that most of the
    applies are just updating the values of existing shards and not adding
    new ones, therefore can be done in place.
    However, reverting the general and in-place applies requires different
    logic, hence the need for an additional flag to differentiate between
    them.

--- atomic_cell.hh
+++ atomic_cell.hh
@@ -59,6 +59,7 @@ private:
     static constexpr int8_t EXPIRY_FLAG = 0x02; // When present, expiry field is present. Set only for live cells
     static constexpr int8_t REVERT_FLAG = 0x04; // transient flag used to efficiently implement ReversiblyMergeable for atomic cells.
     static constexpr int8_t COUNTER_UPDATE_FLAG = 0x08; // Cell is a counter update.
+    static constexpr int8_t COUNTER_IN_PLACE_REVERT = 0x10;
     static constexpr unsigned flags_size = 1;
     static constexpr unsigned timestamp_offset = flags_size;
     static constexpr unsigned timestamp_size = 8;
@@ -76,9 +77,16 @@ private:
     static bool is_revert_set(bytes_view cell) {
         return cell[0] & REVERT_FLAG;
     }
+    static bool is_counter_in_place_revert_set(bytes_view cell) {
+        return cell[0] & COUNTER_IN_PLACE_REVERT;
+    }
     template<typename BytesContainer>
     static void set_revert(BytesContainer& cell, bool revert) {
         cell[0] = (cell[0] & ~REVERT_FLAG) | (revert * REVERT_FLAG);
+    }
+    template<typename BytesContainer>
+    static void set_counter_in_place_revert(BytesContainer& cell, bool flag) {
+        cell[0] = (cell[0] & ~COUNTER_IN_PLACE_REVERT) | (flag * COUNTER_IN_PLACE_REVERT);
     }
     static bool is_live(const bytes_view& cell) {
         return cell[0] & LIVE_FLAG;
@@ -211,6 +219,9 @@ public:
     bool is_revert_set() const {
         return atomic_cell_type::is_revert_set(_data);
     }
+    bool is_counter_in_place_revert_set() const {
+        return atomic_cell_type::is_counter_in_place_revert_set(_data);
+    }
     bool is_live() const {
         return atomic_cell_type::is_live(_data);
     }
@@ -265,6 +276,9 @@ public:
     }
     void set_revert(bool revert) {
         atomic_cell_type::set_revert(_data, revert);
+    }
+    void set_counter_in_place_revert(bool flag) {
+        atomic_cell_type::set_counter_in_place_revert(_data, flag);
     }
 };
 
