6dc62c62154363bd94277ab0dcc9943766ec7add
Asias He <asias@scylladb.com>
2017-03-13 09:23:08 +0800

    api: Add force_terminate_repair API
    The api /storage_service/force_terminate is supposed to be
    /storage_service/force_terminate_repair.
    
    scylla-jmx uses /storage_service/force_terminate api.
    So instead of renaming it, it is better to add a new name for it.

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -373,6 +373,12 @@ void set_storage_service(http_context& ctx, routes& r) {
         return make_ready_future<json::json_return_type>(json_void());
     });
 
+    ss::force_terminate_all_repair_sessions_new.set(r, [](std::unique_ptr<request> req) {
+        return repair_abort_all(service::get_local_storage_service().db()).then([] {
+            return make_ready_future<json::json_return_type>(json_void());
+        });
+    });
+
     ss::decommission.set(r, [](std::unique_ptr<request> req) {
         return service::get_local_storage_service().decommission().then([] {
             return make_ready_future<json::json_return_type>(json_void());
