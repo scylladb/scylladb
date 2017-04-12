81c1b07bacd363e7626fb5621de2dccc12c4fbe3
Takuya ASADA <syuu@scylladb.com>
2017-04-12 20:38:11 +0900

    dist: add offline installer
    This introduce offline installer generator.
    It will generate self-extractable archive witch contains Scylla packages
    and dependency packages.
    Package installation automatically starts when the archive executed.
    
    Limitation: Only supported CentOS at this point.
    
    Fixes #2268
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <1491997091-15323-1-git-send-email-syuu@scylladb.com>

---
+++ dist/offline_installer/build_offline_installer.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -e
+#
+#  Copyright (C) 2017 ScyllaDB
+
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ ! -e dist/offline_installer/build_offline_installer.sh ]; then
+    echo "run build_offline_installer.sh in top of scylla dir"
+    exit 1
+fi
+
+print_usage() {
+    echo "build_offline_installer.sh --repo [URL]"
+    echo "  --repo  repository for fetching scylla rpm, specify .repo/.list file URL"
+    exit 1
+}
+
+is_rhel7_variant() {
+    [ "$ID" = "rhel" -o "$ID" = "ol" -o "$ID" = "centos" ] && [[ "$VERSION_ID" =~ ^7 ]]
+}
+
+REPO=
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "--repo")
+            REPO=$2
+            shift 2
+            ;;
+        *)
+            print_usage
+            ;;
+    esac
+done
+
+. /etc/os-release
+
+if [ -z $REPO ]; then
+    print_usage
+    exit 1
+fi
+
+if ! is_rhel7_variant; then
+    echo "Unsupported distribution"
+    exit 1
+fi
+
+if [ ! -f /usr/bin/yumdownloader ]; then
+    sudo yum -y install yum-utils
+fi
+
+if [ ! -f /usr/bin/wget ]; then
+    sudo yum -y install wget
+fi
+
+if [ ! -f /usr/bin/makeself ]; then
+    sudo yum -y install makeself
+fi
+
+sudo rm -rf build/installroot build/offline_installer build/scylla_offline_installer.sh
+mkdir -p build/installroot
+sudo rpm --root build/installroot --initdb
+sudo yum --nogpgcheck --releasever=7 --installroot=`pwd`/build/installroot install -y @base 
+sudo yum --nogpgcheck --releasever=7 --installroot=`pwd`/build/installroot install -y epel-release
+sudo wget -P build/installroot/etc/yum.repos.d $REPO
+
+mkdir -p build/offline_installer
+cp dist/offline_installer/header build/offline_installer
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve scylla
+# XXX: resolve option doesn't fetch some dependencies, need to manually fetch them
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve yajl.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve libjpeg-turbo.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve libaio.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve snappy.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve pciutils.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve hwdata.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve libpciaccess.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve pciutils-libs.x86_64
+sudo yumdownloader --installroot=`pwd`/build/installroot --archlist=x86_64 --destdir=build/offline_installer --resolve python-six.noarch
+(cd build; makeself offline_installer scylla_offline_installer.sh "Scylla offline package" ./header)
