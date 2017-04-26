vladzcloudius/scylla

tracing: introduce a span ID and parent span ID
This patch makes the tracing framework follow the general idea of Google's
Dapper paper: traces generated in a context of the same query are forming
a single-rooted acyclic tree where in a ScyllaDB case vertexes are spans running
on each involved replica Node and edges are RPCs sent from one Node to another.

   - Each vertex in the tree above has an ID - "span ID".
   - In order to be able to build the tree from the sessions traces we need
     to know the parent "span ID" - the ID of a span that sent an RPC that created
     the current span.
   - Each span of a tracing session is given a 64-bit random span ID.
   - The root span has the parent span ID equal to its own span ID.

This patch adds:
   - The described above parent span ID and a span ID to the one_session_records
     object.
   - The current span ID is passed in the trace_info struct to the remote replica.
   - Add parent_id and span_id columns to system_traces.events table for the parent
     ID and span ID.

Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

fix_system_distributed_tables.py
@@ -30,7 +30,9 @@ events_cols = {
     'activity'      : 'text',
     'source'        : 'inet',
     'source_elapsed': 'int',
-    'thread'        : 'text'
+    'thread'        : 'text',
+    'span_id'       : 'bigint',
+    'parent_id'     : 'bigint'
 }
 
 sessions_cols = {

tracing/tracing.cc
@@ -207,5 +207,9 @@ void tracing::set_trace_probability(double p) {
 one_session_records::one_session_records()
     : backend_state_ptr(tracing::get_local_tracing_instance().allocate_backend_session_state())
     , budget_ptr(tracing::get_local_tracing_instance().get_cached_records_ptr()) {}
+
+std::ostream& operator<<(std::ostream& os, const span_id& id) {
+    return os << id.value();
+}
 }
 

scylladb/scylladb

   - The root span has a span_id::illegal_id value.

fix_system_distributed_tables.py
@@ -30,7 +30,9 @@ events_cols = {
     'activity'      : 'text',
     'source'        : 'inet',
     'source_elapsed': 'int',
-    'thread'        : 'text'
+    'thread'        : 'text',
+    'scylla_span_id'   : 'bigint',
+    'scylla_parent_id' : 'bigint'
 }
 
 sessions_cols = {

tracing/tracing.cc
@@ -207,5 +207,9 @@ void tracing::set_trace_probability(double p) {
 one_session_records::one_session_records()
     : backend_state_ptr(tracing::get_local_tracing_instance().allocate_backend_session_state())
     , budget_ptr(tracing::get_local_tracing_instance().get_cached_records_ptr()) {}
+
+std::ostream& operator<<(std::ostream& os, const span_id& id) {
+    return os << id.get_id();
+}
 }
 