7e29dd7066216296a0bb2316f5c6be0f76c35c03
Avi Kivity <avi@scylladb.com>
2017-05-02 10:14:04 +0300

    managed_bytes: improve alignment hygene
    While blob_storage is marked as an unaligned type, the back references also
    point to an unaligned type (a pointer to blob_storage), since a back
    reference can live in a blob_storage.  This triggers errors from zapcc/clang 4.
    
    Fix by creating a type for the reference, which is marked as unaligned.
    Message-Id: <20170502071404.507-1-avi@scylladb.com>

--- utils/managed_bytes.hh
+++ utils/managed_bytes.hh
@@ -31,22 +31,31 @@
 #include <type_traits>
 
 struct blob_storage {
+    struct [[gnu::packed]] ref_type {
+        blob_storage* ptr;
+
+        ref_type() {}
+        ref_type(blob_storage* ptr) : ptr(ptr) {}
+        operator blob_storage*() const { return ptr; }
+        blob_storage* operator->() const { return ptr; }
+        blob_storage& operator*() const { return *ptr; }
+    };
     using size_type = uint32_t;
     using char_type = bytes_view::value_type;
 
-    blob_storage** backref;
+    ref_type* backref;
     size_type size;
     size_type frag_size;
-    blob_storage* next;
+    ref_type next;
     char_type data[];
 
-    blob_storage(blob_storage** backref, size_type size, size_type frag_size) noexcept
+    blob_storage(ref_type* backref, size_type size, size_type frag_size) noexcept
         : backref(backref)
         , size(size)
         , frag_size(frag_size)
         , next(nullptr)
     {
-        *unaligned_cast<blob_storage**>(backref) = this;
+        *backref = this;
     }
 
     blob_storage(blob_storage&& o) noexcept
@@ -55,7 +64,7 @@ struct blob_storage {
         , frag_size(o.frag_size)
         , next(o.next)
     {
-        *unaligned_cast<blob_storage**>(backref) = this;
+        *backref = this;
         o.next = nullptr;
         if (next) {
             next->backref = &next;
@@ -98,8 +107,10 @@ private:
         bytes_view::value_type data[max_inline_size];
         int8_t size; // -1 -> use blob_storage
     };
-    union {
-        blob_storage* ptr;
+    union u {
+        u() {}
+        ~u() {}
+        blob_storage::ref_type ptr;
         small_blob small;
     } _u;
     static_assert(sizeof(small_blob) > sizeof(blob_storage*), "inline size too small");
@@ -214,23 +225,23 @@ public:
             return;
         }
         auto s = size();
-        blob_storage* const* next_src = &o._u.ptr;
+        const blob_storage::ref_type* next_src = &o._u.ptr;
         blob_storage* blob_src = nullptr;
         size_type size_src = 0;
         size_type offs_src = 0;
-        blob_storage** next_dst = &_u.ptr;
+        blob_storage::ref_type* next_dst = &_u.ptr;
         blob_storage* blob_dst = nullptr;
         size_type size_dst = 0;
         size_type offs_dst = 0;
         while (s) {
             if (!size_src) {
-                blob_src = *unaligned_cast<blob_storage**>(next_src);
+                blob_src = *next_src;
                 next_src = &blob_src->next;
                 size_src = blob_src->frag_size;
                 offs_src = 0;
             }
             if (!size_dst) {
-                blob_dst = *unaligned_cast<blob_storage**>(next_dst);
+                blob_dst = *next_dst;
                 next_dst = &blob_dst->next;
                 size_dst = blob_dst->frag_size;
                 offs_dst = 0;
