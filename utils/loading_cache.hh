6a63c87a9fc869ea8f91661fbdb115f68ffb7528
Vlad Zolotarov <vladz@scylladb.com>
2017-05-04 18:41:31 -0400

    utils::loading_cache: avoid the reads storm when the key is not in the cache
    Use a mutex to serialize producers when the key is not present in the cache.
    
    Fixes #2262
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- utils/loading_cache.hh
+++ utils/loading_cache.hh
@@ -34,24 +34,72 @@ namespace utils {
 typedef lowres_clock loading_cache_clock_type;
 
 template<typename _Tp>
-struct timestamped_val {
-    _Tp value;
-    loading_cache_clock_type::time_point loaded;
-    loading_cache_clock_type::time_point last_read;
+class timestamped_val {
+private:
+    std::experimental::optional<_Tp> _opt_value;
+    loading_cache_clock_type::time_point _loaded;
+    loading_cache_clock_type::time_point _last_read;
 
-    timestamped_val(_Tp v, loading_cache_clock_type::time_point tp)
-        : value(std::move(v))
-        , loaded(tp)
-        , last_read(tp) {}
+public:
+    timestamped_val()
+        : _loaded(loading_cache_clock_type::now())
+        , _last_read(_loaded) {}
+
+    timestamped_val(const timestamped_val&) = default;
+    timestamped_val(timestamped_val&&) = default;
+
+    // Make sure copy/move-assignments don't go through the template below
+    timestamped_val& operator=(const timestamped_val&) = default;
+    timestamped_val& operator=(timestamped_val&) = default;
+    timestamped_val& operator=(timestamped_val&&) = default;
+
+    template <typename U>
+    timestamped_val& operator=(U&& new_val) {
+        _opt_value = std::forward<U>(new_val);
+        _loaded = loading_cache_clock_type::now();
+        return *this;
+    }
+
+    const _Tp& value() {
+        _last_read = loading_cache_clock_type::now();
+        return _opt_value.value();
+    }
+
+    explicit operator bool() const noexcept {
+        return bool(_opt_value);
+    }
+
+    loading_cache_clock_type::time_point last_read() const noexcept {
+        return _last_read;
+    }
+
+    loading_cache_clock_type::time_point loaded() const noexcept {
+        return _loaded;
+    }
 };
 
-template<typename _Key, typename _Tp, typename _Hash = std::hash<_Key>,
-                typename _Pred = std::equal_to<_Key>,
-                typename _Alloc = std::allocator<std::pair<const _Key, timestamped_val<_Tp>> > >
+class shared_mutex {
+private:
+    lw_shared_ptr<semaphore> _mutex_ptr;
+
+public:
+    shared_mutex() : _mutex_ptr(make_lw_shared<semaphore>(1)) {}
+    semaphore& get() const noexcept {
+        return *_mutex_ptr;
+    }
+};
+
+template<typename _Key,
+         typename _Tp,
+         typename _Hash = std::hash<_Key>,
+         typename _Pred = std::equal_to<_Key>,
+         typename _Alloc = std::allocator<std::pair<const _Key, timestamped_val<_Tp>>>,
+         typename SharedMutexMapAlloc = std::allocator<std::pair<const _Key, shared_mutex>>>
 class loading_cache {
 private:
     typedef timestamped_val<_Tp> ts_value_type;
     typedef std::unordered_map<_Key, ts_value_type, _Hash, _Pred, _Alloc> map_type;
+    typedef std::unordered_map<_Key, shared_mutex, _Hash, _Pred, SharedMutexMapAlloc> write_mutex_map_type;
     typedef loading_cache<_Key, _Tp, _Hash, _Pred, _Alloc> _MyType;
 
 public:
@@ -91,13 +139,16 @@ public:
             return _load(k);
         }
 
-        auto i = _map.find(k);
-        if (i == _map.end()) {
-            _logger.trace("{}: key not found - loading in the forground", k);
-            return load(k);
+        // If the key is not in the cache yet, then _map[k] is going to create a
+        // new uninitialized value in the map. If the value is already in the
+        // cache (the fast path) simply return the value. Otherwise, take the
+        // mutex and try to load the value (the slow path).
+        ts_value_type& ts_val = _map[k];
+        if (ts_val) {
+            return make_ready_future<_Tp>(ts_val.value());
+        } else {
+            return slow_load(k);
         }
-        i->second.last_read = loading_cache_clock_type::now();
-        return make_ready_future<_Tp>(i->second.value);
     }
 
 private:
@@ -105,30 +156,39 @@ private:
         return _expiry != std::chrono::milliseconds(0);
     }
 
-    future<_Tp> load(const _Key& k) {
-        return _load(k).then([this, k] (_Tp t) {
-            auto i = _map.emplace(k, ts_value_type(std::move(t), loading_cache_clock_type::now()));
-            // It may happen that here we tried to emplace the already existing
-            // key because a few queries are trying to insert it.
-            // We may safely ignore this.
-            return make_ready_future<_Tp>(i.first->second.value);
-        });
+    future<_Tp> slow_load(const _Key& k) {
+        // If the key is not in the cache yet, then _write_mutex_map[k] is going
+        // to create a new value with the initialized mutex. In this case a
+        // mutex is going to serialize the producers and only the first one is
+        // going to actually issue a load operation and initialize
+        // the value with the received result. The rest are going to see (and
+        // read) the initialized value when they enter the critical section.
+        shared_mutex sm = _write_mutex_map[k];
+        return with_semaphore(sm.get(), 1, [this, k] {
+            ts_value_type& ts_val = _map[k];
+            if (ts_val) {
+                return make_ready_future<_Tp>(ts_val.value());
+            }
+            _logger.trace("{}: storing the value for the first time", k);
+            return _load(k).then([this, k] (_Tp t) {
+                // we have to "re-read" the _map here because the value may have been evicted by now
+                ts_value_type& ts_val = _map[k];
+                ts_val = std::move(t);
+                return make_ready_future<_Tp>(ts_val.value());
+            });
+        }).finally([sm] {});
     }
 
-    future<> reload(const _Key& k) {
-        return _load(k).then_wrapped([this, k] (auto&& f) {
-            auto i = _map.find(k);
-            if (i == _map.end()) {
-                throw std::logic_error("Calling reload() on a non-existing key");
-            }
-            // The exceptions in _load() may be related to the READ mutation
-            // itself. We should ignore them for the background reads - if they
-            // persist the value will age and will be reloaded in the forground.
-            // If the foreground READ fails the error will be propagated up to
-            // the user and will fail the corresponding query.
+    future<> reload(const _Key& k, ts_value_type& ts_val) {
+        return _load(k).then_wrapped([this, &ts_val, &k] (auto&& f) {
+            // The exceptions are related to the load operation itself.
+            // We should ignore them for the background reads - if
+            // they persist the value will age and will be reloaded in
+            // the forground. If the foreground READ fails the error
+            // will be propagated up to the user and will fail the
+            // corresponding query.
             try {
-                i->second.value = f.get0();
-                i->second.loaded = loading_cache_clock_type::now();
+                ts_val = f.get0();
             } catch (std::exception& e) {
                 _logger.debug("{}: reload failed: {}", k, e.what());
             } catch (...) {
@@ -145,8 +205,8 @@ private:
 
         while (i != e) {
             // An entry should be discarded if it hasn't been reloaded for too long or nobody cares about it anymore
-            auto since_last_read = now - i->second.last_read;
-            auto since_loaded = now - i->second.loaded;
+            auto since_last_read = now - i->second.last_read();
+            auto since_loaded = now - i->second.loaded();
             if (_expiry < since_last_read || _expiry < since_loaded) {
                 using namespace std::chrono;
                 _logger.trace("drop_expired(): {}: dropping the entry: _expiry {},  ms passed since: loaded {} last_read {}", i->first, _expiry.count(), duration_cast<milliseconds>(since_loaded).count(), duration_cast<milliseconds>(since_last_read).count());
@@ -169,13 +229,13 @@ private:
             }
 
             std::sort(tmp.begin(), tmp.end(), [] (iterator i1, iterator i2) {
-               return i1->second.last_read < i2->second.last_read;
+               return i1->second.last_read() < i2->second.last_read();
             });
 
             tmp.resize(_map.size() - _max_size);
             std::for_each(tmp.begin(), tmp.end(), [this] (auto& k) {
                 using namespace std::chrono;
-                _logger.trace("shrink(): {}: dropping the entry: ms since last_read {}", k->first, duration_cast<milliseconds>(loading_cache_clock_type::now() - k->second.last_read).count());
+                _logger.trace("shrink(): {}: dropping the entry: ms since last_read {}", k->first, duration_cast<milliseconds>(loading_cache_clock_type::now() - k->second.last_read()).count());
                 _map.erase(k);
             });
         }
@@ -186,6 +246,9 @@ private:
 
         auto timer_start_tp = loading_cache_clock_type::now();
 
+        // Clear all cached mutexes
+        _write_mutex_map.clear();
+
         // Clean up items that were not touched for the whole _expiry period.
         drop_expired();
 
@@ -193,16 +256,11 @@ private:
         shrink();
 
         // Reload all those which vlaue needs to be reloaded.
-        //
-        // The code below uses the fact that _map iterators are not passed to
-        // the asynch part of the function. Otherwise there could be a race
-        // with the get() which may insert a new element into the map, which may
-        // invalidate the previously created iterators.
-        parallel_for_each(_map.begin(), _map.end(), [this] (auto& i) {
-            _logger.trace("on_timer(): {}: checking the key", i.first);
-            if (i.second.loaded + _refresh < loading_cache_clock_type::now()) {
-                _logger.trace("on_timer(): {}: reloading the key", i.first);
-                return this->reload(i.first);
+        parallel_for_each(_map.begin(), _map.end(), [this, curr_time = timer_start_tp] (auto& i) {
+            _logger.trace("on_timer(): {}: checking the value age", i.first);
+            if (i.second && i.second.loaded() + _refresh < curr_time) {
+                _logger.trace("on_timer(): {}: reloading the value", i.first);
+                return this->reload(i.first, i.second);
             }
             return now();
         }).finally([this, timer_start_tp] {
@@ -212,6 +270,7 @@ private:
     }
 
     map_type _map;
+    write_mutex_map_type _write_mutex_map;
     size_t _max_size;
     std::chrono::milliseconds _expiry;
     std::chrono::milliseconds _refresh;
