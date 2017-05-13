7777b558c424a3109a390cf42eb40fd4b279dabc
Takuya ASADA <syuu@scylladb.com>
2017-05-14 02:12:00 +0900

    dist/redhat: Use mock for CentOS/RHEL rpms
    Enable mock for CentOS/RHEL, also support cross building by mock.
    
    Fixes #630
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <20170513171200.14926-1-syuu@scylladb.com>

--- dist/redhat/build_rpm.sh
+++ dist/redhat/build_rpm.sh
@@ -2,15 +2,19 @@
 
 . /etc/os-release
 print_usage() {
-    echo "build_rpm.sh --rebuild-dep --jobs 2"
+    echo "build_rpm.sh --rebuild-dep --jobs 2 --target epel-7-x86_64 --configure-user"
     echo "  --rebuild-dep  rebuild dependency packages (CentOS)"
     echo "  --jobs  specify number of jobs"
     echo "  --dist  create a public distribution rpm"
+    echo "  --target target distribution in mock cfg name"
+    echo "  --configure-user  configure current user as mock group member"
     exit 1
 }
 REBUILD=0
 JOBS=0
 DIST=0
+TARGET=
+USERMOD=0
 while [ $# -gt 0 ]; do
     case "$1" in
         "--rebuild-dep")
@@ -25,91 +29,91 @@ while [ $# -gt 0 ]; do
             DIST=1
             shift 1
             ;;
+        "--target")
+            TARGET=$2
+            shift 2
+            ;;
+        "--configure-user")
+            USERMOD=1
+            shift 1
+            ;;
         *)
             print_usage
             ;;
     esac
 done
 
-RPMBUILD=`pwd`/build/rpmbuild
+is_redhat_variant() {
+    [ -f /etc/redhat-release ]
+}
+pkg_install() {
+    if is_redhat_variant; then
+        sudo yum install -y $1
+    else
+        echo "Requires to install following command: $1"
+        exit 1
+    fi
+}
+
 
 if [ ! -e dist/redhat/build_rpm.sh ]; then
     echo "run build_rpm.sh in top of scylla dir"
     exit 1
 fi
 
-if [ "$ID" != "fedora" ] && [ "$ID" != "centos" ] && [ "$ID" != "rhel" ]; then
-    echo "Unsupported distribution"
+if [ "$(arch)" != "x86_64" ]; then
+    echo "Unsupported architecture: $(arch)"
     exit 1
 fi
-if [ "$ID" = "fedora" ] && [ ! -f /usr/bin/mock ]; then
-    sudo yum -y install mock
-elif [ ! -f /usr/bin/yum-builddep ]; then
-    sudo yum -y install yum-utils
+if [ -z "$TARGET" ]; then
+    if [ "$ID" = "centos" -o "$ID" = "rhel" ] && [ "$VERSION_ID" = "7" ]; then
+        TARGET=epel-7-x86_64
+    elif [ "$ID" = "fedora" ]; then
+        TARGET=$ID-$VERSION_ID-x86_64
+    else
+        echo "Please specify target"
+        exit 1
+    fi
+fi
+
+if [ ! -f /usr/bin/mock ]; then
+    pkg_install mock
 fi
 if [ ! -f /usr/bin/git ]; then
-    sudo yum -y install git
+    pkg_install git
 fi
-if [ ! -f /usr/bin/rpmbuild ]; then
-    sudo yum -y install rpm-build
+if [ ! -f /usr/bin/wget ]; then
+    pkg_install wget
 fi
-mkdir -p $RPMBUILD/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-if [ "$ID" = "centos" ] || [ "$ID" = "rhel" ]; then
-    if [ "$ID" = "centos" ]; then
-        if [ ! -f /etc/yum.repos.d/epel.repo ]; then
-            sudo yum install -y epel-release
-        fi
-    else
-        if [ ! -f /etc/yum.repos.d/epel.repo ]; then
-            sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-        fi
-        for repo in rhui-REGION-rhel-server-optional rhel-7-server-optional-rpms; do
-            repo_avail=$(sudo yum repolist all|grep $repo)
-            if [ "$repo_avail" != "" ]; then
-                sudo yum-config-manager --enable $repo
-            fi
-        done
-    fi
-    if [ $REBUILD = 1 ]; then
-        ./dist/redhat/centos_dep/build_dependency.sh
-    else
-        if [ "$ID" = "centos" ]; then
-            sudo curl https://s3.amazonaws.com/downloads.scylladb.com/rpm/unstable/centos/master/latest/scylla.repo -o /etc/yum.repos.d/scylla.repo
-        else
-            echo "RHEL requires --rebuild-deps option."
-            exit 1
-        fi
-    fi
-fi
+
 VERSION=$(./SCYLLA-VERSION-GEN)
 SCYLLA_VERSION=$(cat build/SCYLLA-VERSION-FILE)
 SCYLLA_RELEASE=$(cat build/SCYLLA-RELEASE-FILE)
 echo $VERSION >version
-./scripts/git-archive-all --extra version --force-submodules --prefix scylla-$SCYLLA_VERSION $RPMBUILD/SOURCES/scylla-$VERSION.tar
+./scripts/git-archive-all --extra version --force-submodules --prefix scylla-$SCYLLA_VERSION build/scylla-$VERSION.tar
 rm -f version
-cp dist/redhat/scylla.spec.in $RPMBUILD/SPECS/scylla.spec
-sed -i -e "s/@@VERSION@@/$SCYLLA_VERSION/g" $RPMBUILD/SPECS/scylla.spec
-sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" $RPMBUILD/SPECS/scylla.spec
+cp dist/redhat/scylla.spec.in build/scylla.spec
+sed -i -e "s/@@VERSION@@/$SCYLLA_VERSION/g" build/scylla.spec
+sed -i -e "s/@@RELEASE@@/$SCYLLA_RELEASE/g" build/scylla.spec
 
 if [ $DIST -gt 0 ]; then
-  sed -i -e "s/@@HOUSEKEEPING_CONF@@/true/g" $RPMBUILD/SPECS/scylla.spec
+  sed -i -e "s/@@HOUSEKEEPING_CONF@@/true/g" build/scylla.spec
 else
-  sed -i -e "s/@@HOUSEKEEPING_CONF@@/false/g" $RPMBUILD/SPECS/scylla.spec
+  sed -i -e "s/@@HOUSEKEEPING_CONF@@/false/g" build/scylla.spec
 fi
 
-if [ "$ID" = "fedora" ]; then
-    if [ $JOBS -gt 0 ]; then
-        rpmbuild -bs --define "_topdir $RPMBUILD" --define "_smp_mflags -j$JOBS" $RPMBUILD/SPECS/scylla.spec
-    else
-        rpmbuild -bs --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla.spec
-    fi
-    mock rebuild --resultdir=`pwd`/build/rpms $RPMBUILD/SRPMS/scylla-$VERSION*.src.rpm
-else
-    sudo yum-builddep -y  $RPMBUILD/SPECS/scylla.spec
-    . /etc/profile.d/scylla.sh
-    if [ $JOBS -gt 0 ]; then
-        rpmbuild -ba --define "_topdir $RPMBUILD" --define "_smp_mflags -j$JOBS" $RPMBUILD/SPECS/scylla.spec
-    else
-        rpmbuild -ba --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla.spec
-    fi
+
+if [ $JOBS -gt 0 ]; then
+    SRPM_OPTS="$SRPM_OPTS --define='_smp_mflags -j$JOBS'"
 fi
+sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=build/scylla.spec --sources=build/scylla-$VERSION.tar $SRPM_OPTS
+if [ "$TARGET" = "epel-7-x86_64" ] && [ $REBUILD = 1 ]; then
+    ./dist/redhat/centos_dep/build_dependency.sh
+    sudo mock --init --root=$TARGET
+    sudo mock --install --root=$TARGET build/rpms/*.{x86_64,noarch}.rpm
+    RPM_OPTS="$RPM_OPTS --no-clean"
+elif [ "$TARGET" = "epel-7-x86_64" ] && [ $REBUILD = 0 ]; then
+    TARGET=scylla-$TARGET
+    RPM_OPTS="$RPM_OPTS --configdir=dist/redhat/mock"
+fi
+sudo mock --rebuild --root=$TARGET --resultdir=`pwd`/build/rpms $RPM_OPTS build/srpms/scylla-$VERSION*.src.rpm
--- dist/redhat/centos_dep/build_dependency.sh
+++ dist/redhat/centos_dep/build_dependency.sh
@@ -1,16 +1,36 @@
 #!/bin/bash -e
-export RPMBUILD=`pwd`/build/rpmbuild
 
-do_install()
-{
-    pkg=$1
-    sudo yum install -y $RPMBUILD/RPMS/*/$pkg 2> build/err || if [ "`cat build/err`" != "Error: Nothing to do" ]; then cat build/err; exit 1;fi
-    echo Install $name done
+RPMBUILD=`pwd`/build/rpmbuild
+TARGET=epel-7-x86_64
+
+is_redhat_variant() {
+    [ -f /etc/redhat-release ]
+}
+pkg_install() {
+    if is_redhat_variant; then
+        sudo yum install -y $1
+    else
+        echo "Requires to install following command: $1"
+        exit 1
+    fi
 }
 
-sudo yum install -y wget yum-utils rpm-build rpmdevtools gcc gcc-c++ make patch
-mkdir -p build/srpms
-cd build/srpms
+if [ ! -f /usr/bin/mock ]; then
+    pkg_install mock
+fi
+if [ ! -f /usr/bin/rpm ]; then
+    pkg_install rpm
+fi
+if [ ! -f /usr/bin/wget ]; then
+    pkg_install wget
+fi
+if [ ! -f /usr/bin/patch ]; then
+    pkg_install patch
+fi
+
+mkdir -p $RPMBUILD
+mkdir -p build/downloads
+cd build/downloads
 
 if [ ! -f binutils-2.25-15.fc23.src.rpm ]; then
     wget -nv https://kojipkgs.fedoraproject.org//packages/binutils/2.25/15.fc23/src/binutils-2.25-15.fc23.src.rpm
@@ -22,8 +42,7 @@ fi
 
 if [ ! -f gcc-5.3.1-2.fc23.src.rpm ]; then
     wget -nv https://kojipkgs.fedoraproject.org//packages/gcc/5.3.1/2.fc23/src/gcc-5.3.1-2.fc23.src.rpm
-fi
-
+fi 
 if [ ! -f boost-1.58.0-11.fc23.src.rpm ]; then
     wget -nv https://kojipkgs.fedoraproject.org//packages/boost/1.58.0/11.fc23/src/boost-1.58.0-11.fc23.src.rpm
 fi
@@ -42,91 +61,103 @@ fi
 
 cd -
 
-sudo yum install -y cryptopp cryptopp-devel jsoncpp jsoncpp-devel lz4 lz4-devel yaml-cpp yaml-cpp-devel thrift thrift-devel scons gtest gtest-devel python34
-sudo ln -sf /usr/bin/python3.4 /usr/bin/python3
-
-sudo yum install -y python-devel libicu-devel openmpi-devel mpich-devel libstdc++-devel bzip2-devel zlib-devel
-sudo yum install -y flex bison dejagnu zlib-static glibc-static sharutils libstdc++-static gmp-devel texinfo texinfo-tex systemtap-sdt-devel mpfr-devel libmpc-devel elfutils-devel elfutils-libelf-devel glibc-devel.x86_64 glibc-devel.i686 gcc-gnat libgnat doxygen graphviz dblatex texlive-collection-latex docbook5-style-xsl python-sphinx cmake
-sudo yum install -y gcc-objc
-sudo yum install -y asciidoc
-sudo yum install -y gettext
-sudo yum install -y rpm-devel python34-devel guile-devel readline-devel ncurses-devel expat-devel texlive-collection-latexrecommended xz-devel libselinux-devel
-sudo yum install -y dos2unix
-
-if [ ! -f $RPMBUILD/RPMS/noarch/scylla-env-1.0-1.el7*.noarch.rpm ]; then
+if [ ! -f build/srpms/scylla-env-1.0-1.el7*.src.rpm ]; then
     cd dist/common/dep
-    tar cpf $RPMBUILD/SOURCES/scylla-env-1.0.tar scylla-env-1.0
+    tar cpf ../../../build/scylla-env-1.0.tar scylla-env-1.0
     cd -
-    rpmbuild --define "_topdir $RPMBUILD" --ba dist/common/dep/scylla-env.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=dist/common/dep/scylla-env.spec --sources=build/
 fi
-do_install scylla-env-1.0-1.el7*.noarch.rpm
+if [ ! -f build/rpms/scylla-env-1.0-1.el7*.noarch.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-env-1.0-1.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-binutils-2.25-15.el7*.x86_64.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/binutils-2.25-15.fc23.src.rpm
+if [ ! -f build/srpms/scylla-binutils-2.25-15.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/binutils-2.25-15.fc23.src.rpm
     patch $RPMBUILD/SPECS/binutils.spec < dist/redhat/centos_dep/binutils.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/binutils.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/binutils.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install scylla-binutils-2.25-15.el7*.x86_64.rpm
+if [ ! -f build/rpms/scylla-binutils-2.25-15.el7*.x86_64.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-binutils-2.25-15.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-isl-0.14-4.el7*.x86_64.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/isl-0.14-4.fc23.src.rpm
+if [ ! -f build/srpms/scylla-isl-0.14-4.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/isl-0.14-4.fc23.src.rpm
     patch $RPMBUILD/SPECS/isl.spec < dist/redhat/centos_dep/isl.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/isl.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/isl.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install scylla-isl-0.14-4.el7*.x86_64.rpm
-do_install scylla-isl-devel-0.14-4.el7*.x86_64.rpm
+if [ ! -f build/rpms/scylla-isl-0.14-4.el7*.x86_64.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-isl-0.14-4.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-gcc-5.3.1-2.el7*.x86_64.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/gcc-5.3.1-2.fc23.src.rpm
+if [ ! -f build/srpms/scylla-gcc-5.3.1-2.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/gcc-5.3.1-2.fc23.src.rpm
     patch $RPMBUILD/SPECS/gcc.spec < dist/redhat/centos_dep/gcc.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/gcc.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/gcc.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install scylla-*5.3.1-2*
+if [ ! -f build/rpms/scylla-gcc-5.3.1-2.el7*.x86_64.rpm ]; then
+    sudo mock --root=$TARGET --init
+    sudo mock --root=$TARGET --install build/rpms/scylla-env-1.0-1.el7*.noarch.rpm
+    sudo mock --root=$TARGET --install build/rpms/scylla-binutils-2.25-15.el7*.x86_64.rpm
+    sudo mock --root=$TARGET --install build/rpms/scylla-isl-0.14-4.el7*.x86_64.rpm
+    sudo mock --root=$TARGET --install build/rpms/scylla-isl-devel-0.14-4.el7*.x86_64.rpm
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms --no-clean build/srpms/scylla-gcc-5.3.1-2.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-boost-1.58.0-11.el7*.x86_64.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/boost-1.58.0-11.fc23.src.rpm
+if [ ! -f build/srpms/scylla-boost-1.58.0-11.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/boost-1.58.0-11.fc23.src.rpm
     patch $RPMBUILD/SPECS/boost.spec < dist/redhat/centos_dep/boost.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/boost.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/boost.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install scylla-boost*
+if [ ! -f build/rpms/scylla-boost-1.58.0-11.el7*.x86_64.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-boost-1.58.0-11.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-ragel-6.8-5.el7*.x86_64.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/ragel-6.8-5.fc23.src.rpm
+if [ ! -f build/srpms/scylla-ragel-6.8-5.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/ragel-6.8-5.fc23.src.rpm
     patch $RPMBUILD/SPECS/ragel.spec < dist/redhat/centos_dep/ragel.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/ragel.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/ragel.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install scylla-ragel-6.8-5.el7*.x86_64.rpm
+if [ ! -f build/rpms/scylla-ragel-6.8-5.el7*.x86_64.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-ragel-6.8-5.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-gdb-7.10.1-30.el7*.x86_64.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/gdb-7.10.1-30.fc23.src.rpm
+if [ ! -f build/srpms/scylla-gdb-7.10.1-30.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/gdb-7.10.1-30.fc23.src.rpm
     patch $RPMBUILD/SPECS/gdb.spec < dist/redhat/centos_dep/gdb.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/gdb.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/gdb.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install scylla-gdb-7.10.1-30.el7*.x86_64.rpm
+if [ ! -f build/rpms/scylla-gdb-7.10.1-30.el7*.x86_64.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-gdb-7.10.1-30.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/noarch/python34-pyparsing-2.0.3-2.el7*.noarch.rpm ]; then
-    rpm --define "_topdir $RPMBUILD" -ivh build/srpms/pyparsing-2.0.3-2.fc23.src.rpm
+if [ ! -f build/srpms/pyparsing-2.0.3-2.el7*.src.rpm ]; then
+    rpm --define "_topdir $RPMBUILD" -ivh build/downloads/pyparsing-2.0.3-2.fc23.src.rpm
     patch $RPMBUILD/SPECS/pyparsing.spec < dist/redhat/centos_dep/pyparsing.diff
-    rpmbuild --define "_topdir $RPMBUILD" -ba $RPMBUILD/SPECS/pyparsing.spec
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=$RPMBUILD/SPECS/pyparsing.spec --sources=$RPMBUILD/SOURCES
 fi
-do_install python34-pyparsing-2.0.3-2.el7*.noarch.rpm
+if [ ! -f build/rpms/python34-pyparsing-2.0.3-2.el7*.noarch.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/pyparsing-2.0.3-2.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/noarch/scylla-antlr3-tool-3.5.2-1.el7*.noarch.rpm ]; then
-   mkdir build/scylla-antlr3-tool-3.5.2
-   cp dist/redhat/centos_dep/antlr3 build/scylla-antlr3-tool-3.5.2
-   cd build/scylla-antlr3-tool-3.5.2
-   wget -nv http://www.antlr3.org/download/antlr-3.5.2-complete-no-st3.jar
-   cd -
-   cd build
-   tar cJpf $RPMBUILD/SOURCES/scylla-antlr3-tool-3.5.2.tar.xz scylla-antlr3-tool-3.5.2
-   cd -
-   rpmbuild --define "_topdir $RPMBUILD" -ba dist/redhat/centos_dep/scylla-antlr3-tool.spec
+if [ ! -f build/srpms/scylla-antlr3-tool-3.5.2-1.el7*.src.rpm ]; then
+    mkdir build/scylla-antlr3-tool-3.5.2
+    cp dist/redhat/centos_dep/antlr3 build/scylla-antlr3-tool-3.5.2
+    cd build/scylla-antlr3-tool-3.5.2
+    wget -nv http://www.antlr3.org/download/antlr-3.5.2-complete-no-st3.jar
+    cd -
+    cd build
+    tar cJpf scylla-antlr3-tool-3.5.2.tar.xz scylla-antlr3-tool-3.5.2
+    cd -
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=dist/redhat/centos_dep/scylla-antlr3-tool.spec --sources=build/
 fi
-do_install scylla-antlr3-tool-3.5.2-1.el7*.noarch.rpm
+if [ ! -f build/rpms/scylla-antlr3-tool-3.5.2-1.el7*.noarch.rpm ]; then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-antlr3-tool-3.5.2-1.el7*.src.rpm
+fi
 
-if [ ! -f $RPMBUILD/RPMS/x86_64/scylla-antlr3-C++-devel-3.5.2-1.el7*.x86_64.rpm ];then
-   wget -nv -O build/3.5.2.tar.gz https://github.com/antlr/antlr3/archive/3.5.2.tar.gz
-   mv build/3.5.2.tar.gz $RPMBUILD/SOURCES
-   rpmbuild --define "_topdir $RPMBUILD" -ba dist/redhat/centos_dep/scylla-antlr3-C++-devel.spec
+if [ ! -f build/srpms/scylla-antlr3-C++-devel-3.5.2-1.el7*.src.rpm ];then
+    wget -nv -O build/3.5.2.tar.gz https://github.com/antlr/antlr3/archive/3.5.2.tar.gz
+    sudo mock --buildsrpm --root=$TARGET --resultdir=`pwd`/build/srpms --spec=dist/redhat/centos_dep/scylla-antlr3-C++-devel.spec --sources=build/
 fi
-do_install scylla-antlr3-C++-devel-3.5.2-1.el7*.x86_64.rpm
+if [ ! -f build/rpms/scylla-antlr3-C++-devel-3.5.2-1.el7*.x86_64.rpm ];then
+    sudo mock --root=$TARGET --rebuild --resultdir=`pwd`/build/rpms build/srpms/scylla-antlr3-C++-devel-3.5.2-1.el7*.src.rpm
+fi
