2be3596a4f764eb82cc9d10cbf4d42317fd53d32
Vlad Zolotarov <vladz@scylladb.com>
2017-05-22 18:07:31 -0400

    utils::loading_cache: make the underlying map to be an intrusive unordered_set
    Make the underlying map to be a boost::intrusive::unordered_set<timestamped_val>
    instead of std::unordered_set<Key, timestamped_val>.
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- utils/loading_cache.hh
+++ utils/loading_cache.hh
@@ -23,27 +23,51 @@
 
 #include <chrono>
 #include <unordered_map>
+#include <boost/intrusive/unordered_set.hpp>
 
 #include <seastar/core/timer.hh>
 
 #include "utils/exceptions.hh"
+
+namespace bi = boost::intrusive;
 
 namespace utils {
 // Simple variant of the "LoadingCache" used for permissions in origin.
 
 typedef lowres_clock loading_cache_clock_type;
 
-template<typename _Tp>
-class timestamped_val {
+template<typename _Tp, typename _Key, typename _Hash, typename _EqualPred>
+class timestamped_val : public bi::unordered_set_base_hook<> {
+public:
+    typedef _Key key_type;
+    typedef _Tp value_type;
+
 private:
     std::experimental::optional<_Tp> _opt_value;
     loading_cache_clock_type::time_point _loaded;
     loading_cache_clock_type::time_point _last_read;
+    _Key _key;
 
 public:
-    timestamped_val()
+    struct key_eq {
+       bool operator()(const _Key& k, const timestamped_val& c) const {
+           return _EqualPred()(k, c.key());
+       }
+
+       bool operator()(const timestamped_val& c, const _Key& k) const {
+           return _EqualPred()(c.key(), k);
+       }
+    };
+
+    timestamped_val(const _Key& key)
         : _loaded(loading_cache_clock_type::now())
-        , _last_read(_loaded) {}
+        , _last_read(_loaded)
+        , _key(key) {}
+
+    timestamped_val(_Key&& key)
+        : _loaded(loading_cache_clock_type::now())
+        , _last_read(_loaded)
+        , _key(std::move(key)) {}
 
     timestamped_val(const timestamped_val&) = default;
     timestamped_val(timestamped_val&&) = default;
@@ -76,6 +100,18 @@ public:
     loading_cache_clock_type::time_point loaded() const noexcept {
         return _loaded;
     }
+
+    const _Key& key() const {
+        return _key;
+    }
+
+    friend bool operator==(const timestamped_val& a, const timestamped_val& b){
+        return _EqualPred()(a.key(), b.key());
+    }
+
+    friend std::size_t hash_value(const timestamped_val& v) {
+        return _Hash()(v.key());
+    }
 };
 
 class shared_mutex {
@@ -93,26 +129,27 @@ template<typename _Key,
          typename _Tp,
          typename _Hash = std::hash<_Key>,
          typename _Pred = std::equal_to<_Key>,
-         typename _Alloc = std::allocator<std::pair<const _Key, timestamped_val<_Tp>>>,
+         typename _Alloc = std::allocator<timestamped_val<_Tp, _Key, _Hash, _Pred>>,
          typename SharedMutexMapAlloc = std::allocator<std::pair<const _Key, shared_mutex>>>
 class loading_cache {
 private:
-    typedef timestamped_val<_Tp> ts_value_type;
-    typedef std::unordered_map<_Key, ts_value_type, _Hash, _Pred, _Alloc> map_type;
+    typedef timestamped_val<_Tp, _Key, _Hash, _Pred> ts_value_type;
+    typedef bi::unordered_set<ts_value_type> set_type;
     typedef std::unordered_map<_Key, shared_mutex, _Hash, _Pred, SharedMutexMapAlloc> write_mutex_map_type;
     typedef loading_cache<_Key, _Tp, _Hash, _Pred, _Alloc> _MyType;
+    typedef typename set_type::bucket_traits bi_set_bucket_traits;
+
+    static constexpr int num_buckets = 1021; // A prime number close to 1024
 
 public:
     typedef _Tp value_type;
-    typedef typename map_type::key_type key_type;
-    typedef typename map_type::allocator_type allocator_type;
-    typedef typename map_type::hasher hasher;
-    typedef typename map_type::key_equal key_equal;
-    typedef typename map_type::iterator iterator;
+    typedef _Key key_type;
+    typedef typename set_type::iterator iterator;
 
     template<typename Func>
-    loading_cache(size_t max_size, std::chrono::milliseconds expiry, std::chrono::milliseconds refresh, logging::logger& logger, Func&& load, const hasher& hf = hasher(), const key_equal& eql = key_equal(), const allocator_type& a = allocator_type())
-                : _map(10, hf, eql, a)
+    loading_cache(size_t max_size, std::chrono::milliseconds expiry, std::chrono::milliseconds refresh, logging::logger& logger, Func&& load)
+                : _buckets(num_buckets)
+                , _set(bi_set_bucket_traits(_buckets.data(), _buckets.size()))
                 , _max_size(max_size)
                 , _expiry(expiry)
                 , _refresh(refresh)
@@ -133,19 +170,23 @@ public:
         _timer.arm(_refresh);
     }
 
+    ~loading_cache() {
+        _set.clear_and_dispose([] (ts_value_type* ptr) { loading_cache::destroy_ts_value(ptr); });
+    }
+
     future<_Tp> get(const _Key& k) {
         // If caching is disabled - always load in the foreground
         if (!caching_enabled()) {
             return _load(k);
         }
 
-        // If the key is not in the cache yet, then _map[k] is going to create a
-        // new uninitialized value in the map. If the value is already in the
-        // cache (the fast path) simply return the value. Otherwise, take the
-        // mutex and try to load the value (the slow path).
-        ts_value_type& ts_val = _map[k];
-        if (ts_val) {
-            return make_ready_future<_Tp>(ts_val.value());
+        // If the key is not in the cache yet, then find_or_create() is going to
+        // create a new uninitialized value in the map. If the value is already
+        // in the cache (the fast path) simply return the value. Otherwise, take
+        // the mutex and try to load the value (the slow path).
+        iterator ts_value_it = find_or_create(k);
+        if (*ts_value_it) {
+            return make_ready_future<_Tp>(ts_value_it->value());
         } else {
             return slow_load(k);
         }
@@ -156,31 +197,54 @@ private:
         return _expiry != std::chrono::milliseconds(0);
     }
 
+    /// Look for the entry with the given key. It it doesn't exist - create a new one and add it to the _set.
+    ///
+    /// \param k The key to look for
+    ///
+    /// \return An iterator to the value with the given key (always dirrerent from _set.end())
+    template <typename KeyType>
+    iterator find_or_create(KeyType&& k) {
+        iterator i = _set.find(k, _Hash(), typename ts_value_type::key_eq());
+        if (i == _set.end()) {
+            ts_value_type* new_ts_val = _Alloc().allocate(1);
+            new(new_ts_val) ts_value_type(std::forward<KeyType>(k));
+            auto p = _set.insert(*new_ts_val);
+            i = p.first;
+        }
+
+        return i;
+    }
+
+    static void destroy_ts_value(ts_value_type* val) {
+        val->~ts_value_type();
+        _Alloc().deallocate(val, 1);
+    }
+
     future<_Tp> slow_load(const _Key& k) {
         // If the key is not in the cache yet, then _write_mutex_map[k] is going
-        // to create a new value with the initialized mutex. In this case a
-        // mutex is going to serialize the producers and only the first one is
-        // going to actually issue a load operation and initialize
-        // the value with the received result. The rest are going to see (and
-        // read) the initialized value when they enter the critical section.
+        // to create a new value with the initialized mutex. The mutex is going
+        // to serialize the producers and only the first one is going to
+        // actually issue a load operation and initialize the value with the
+        // received result. The rest are going to see (and read) the initialized
+        // value when they enter the critical section.
         shared_mutex sm = _write_mutex_map[k];
         return with_semaphore(sm.get(), 1, [this, k] {
-            ts_value_type& ts_val = _map[k];
-            if (ts_val) {
-                return make_ready_future<_Tp>(ts_val.value());
+            iterator ts_value_it = find_or_create(k);
+            if (*ts_value_it) {
+                return make_ready_future<_Tp>(ts_value_it->value());
             }
             _logger.trace("{}: storing the value for the first time", k);
             return _load(k).then([this, k] (_Tp t) {
-                // we have to "re-read" the _map here because the value may have been evicted by now
-                ts_value_type& ts_val = _map[k];
-                ts_val = std::move(t);
-                return make_ready_future<_Tp>(ts_val.value());
+                // we have to "re-read" the _set here because the value may have been evicted by now
+                iterator ts_value_it = find_or_create(std::move(k));
+                *ts_value_it = std::move(t);
+                return make_ready_future<_Tp>(ts_value_it->value());
             });
         }).finally([sm] {});
     }
 
-    future<> reload(const _Key& k, ts_value_type& ts_val) {
-        return _load(k).then_wrapped([this, &ts_val, &k] (auto&& f) {
+    future<> reload(ts_value_type& ts_val) {
+        return _load(ts_val.key()).then_wrapped([this, &ts_val] (auto&& f) {
             // The exceptions are related to the load operation itself.
             // We should ignore them for the background reads - if
             // they persist the value will age and will be reloaded in
@@ -190,27 +254,31 @@ private:
             try {
                 ts_val = f.get0();
             } catch (std::exception& e) {
-                _logger.debug("{}: reload failed: {}", k, e.what());
+                _logger.debug("{}: reload failed: {}", ts_val.key(), e.what());
             } catch (...) {
-                _logger.debug("{}: reload failed: unknown error", k);
+                _logger.debug("{}: reload failed: unknown error", ts_val.key());
             }
         });
+    }
+
+    void erase(iterator it) {
+        _set.erase_and_dispose(it, [] (ts_value_type* ptr) { loading_cache::destroy_ts_value(ptr); });
     }
 
     // We really miss the std::erase_if()... :(
     void drop_expired() {
         auto now = loading_cache_clock_type::now();
-        auto i = _map.begin();
-        auto e = _map.end();
+        auto i = _set.begin();
+        auto e = _set.end();
 
         while (i != e) {
             // An entry should be discarded if it hasn't been reloaded for too long or nobody cares about it anymore
-            auto since_last_read = now - i->second.last_read();
-            auto since_loaded = now - i->second.loaded();
+            auto since_last_read = now - i->last_read();
+            auto since_loaded = now - i->loaded();
             if (_expiry < since_last_read || _expiry < since_loaded) {
                 using namespace std::chrono;
-                _logger.trace("drop_expired(): {}: dropping the entry: _expiry {},  ms passed since: loaded {} last_read {}", i->first, _expiry.count(), duration_cast<milliseconds>(since_loaded).count(), duration_cast<milliseconds>(since_last_read).count());
-                i = _map.erase(i);
+                _logger.trace("drop_expired(): {}: dropping the entry: _expiry {},  ms passed since: loaded {} last_read {}", i->key(), _expiry.count(), duration_cast<milliseconds>(since_loaded).count(), duration_cast<milliseconds>(since_last_read).count());
+                erase(i++);
                 continue;
             }
             ++i;
@@ -219,24 +287,24 @@ private:
 
     // Shrink the cache to the _max_size discarding the least recently used items
     void shrink() {
-        if (_max_size != 0 && _map.size() > _max_size) {
+        if (_max_size != 0 && _set.size() > _max_size) {
             std::vector<iterator> tmp;
-            tmp.reserve(_map.size());
+            tmp.reserve(_set.size());
 
-            iterator i = _map.begin();
-            while (i != _map.end()) {
+            iterator i = _set.begin();
+            while (i != _set.end()) {
                 tmp.emplace_back(i++);
             }
 
             std::sort(tmp.begin(), tmp.end(), [] (iterator i1, iterator i2) {
-               return i1->second.last_read() < i2->second.last_read();
+               return i1->last_read() < i2->last_read();
             });
 
-            tmp.resize(_map.size() - _max_size);
+            tmp.resize(_set.size() - _max_size);
             std::for_each(tmp.begin(), tmp.end(), [this] (auto& k) {
                 using namespace std::chrono;
-                _logger.trace("shrink(): {}: dropping the entry: ms since last_read {}", k->first, duration_cast<milliseconds>(loading_cache_clock_type::now() - k->second.last_read()).count());
-                _map.erase(k);
+                _logger.trace("shrink(): {}: dropping the entry: ms since last_read {}", k->key(), duration_cast<milliseconds>(loading_cache_clock_type::now() - k->last_read()).count());
+                this->erase(k);
             });
         }
     }
@@ -256,11 +324,11 @@ private:
         shrink();
 
         // Reload all those which vlaue needs to be reloaded.
-        parallel_for_each(_map.begin(), _map.end(), [this, curr_time = timer_start_tp] (auto& i) {
-            _logger.trace("on_timer(): {}: checking the value age", i.first);
-            if (i.second && i.second.loaded() + _refresh < curr_time) {
-                _logger.trace("on_timer(): {}: reloading the value", i.first);
-                return this->reload(i.first, i.second);
+        parallel_for_each(_set.begin(), _set.end(), [this, curr_time = timer_start_tp] (auto& ts_val) {
+            _logger.trace("on_timer(): {}: checking the value age", ts_val.key());
+            if (ts_val && ts_val.loaded() + _refresh < curr_time) {
+                _logger.trace("on_timer(): {}: reloading the value", ts_val.key());
+                return this->reload(ts_val);
             }
             return now();
         }).finally([this, timer_start_tp] {
@@ -269,7 +337,8 @@ private:
         });
     }
 
-    map_type _map;
+    std::vector<typename set_type::bucket_type> _buckets;
+    set_type _set;
     write_mutex_map_type _write_mutex_map;
     size_t _max_size;
     std::chrono::milliseconds _expiry;
