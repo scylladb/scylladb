a4c392c113e15c500c1cb215a7e0a6882401681b
Takuya ASADA <syuu@scylladb.com>
2017-06-06 17:25:02 +0900

    dist/debian: use gdebi instead of mk-build-deps -i
    At least on Debian8, mk-build-deps -i silently finishes with return code 0
    even it fails to install dependencies.
    To prevent this, we should manually install the metapackage generated by
    mk-build-deps using gdebi.
    
    Fixes #2445
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <1496737502-10737-2-git-send-email-syuu@scylladb.com>

--- dist/debian/dep/build_dependency.sh
+++ dist/debian/dep/build_dependency.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 
 . /etc/os-release
+install_deps() {
+    echo Y | sudo mk-build-deps
+    DEB_FILE=`ls *-build-deps*.deb`
+    sudo gdebi -n $DEB_FILE
+    sudo rm -f $DEB_FILE
+    sudo dpkg -P ${DEB_FILE%%_*.deb}
+}
+
 DISTRIBUTION=`lsb_release -i|awk '{print $3}'`
 CODENAME=`lsb_release -c|awk '{print $2}'`
 
@@ -21,7 +29,7 @@ if [ "$VERSION_ID" = "14.04" ] || [ "$DISTRIBUTION" = "Debian" ]; then
         cp -a dist/debian/dep/antlr3-3.5.2/* build/antlr3-3.5.2
         cd build/antlr3-3.5.2
         wget -nv http://www.antlr3.org/download/antlr-3.5.2-complete-no-st3.jar
-        echo Y | sudo mk-build-deps -i -r
+        install_deps
         debuild -r fakeroot --no-tgz-check -us -uc
         cd -
     fi
@@ -49,7 +57,7 @@ if [ "$VERSION_ID" = "14.04" ] || [ "$DISTRIBUTION" = "Debian" ]; then
         cd -
         cd build/gdb-7.11
         patch -p0 < ../../dist/debian/dep/gdb.diff
-        echo Y | sudo mk-build-deps -i -r
+        install_deps
         debuild -r fakeroot --no-tgz-check -us -uc
         cd -
     fi
@@ -66,7 +74,7 @@ if [ ! -f build/antlr3-c++-dev_*.deb ]; then
     cd -
     cp -a dist/debian/dep/antlr3-c++-dev-3.5.2/debian build/antlr3-c++-dev-3.5.2
     cd build/antlr3-c++-dev-3.5.2
-    echo Y | sudo mk-build-deps -i -r
+    install_deps
     debuild -r fakeroot --no-tgz-check -us -uc
     cd -
 fi
@@ -80,7 +88,7 @@ if [ ! -f build/libthrift0_*.deb ]; then
     tar xpf thrift-0.9.3.tar.gz
     cd thrift-0.9.3
     patch -p0 < ../../dist/debian/dep/thrift.diff
-    echo Y | sudo mk-build-deps -i -r
+    install_deps
     debuild -r fakeroot --no-tgz-check -us -uc
     cd ../..
 fi
