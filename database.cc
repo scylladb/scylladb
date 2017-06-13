gleb-cloudius/scylla

database: reset node's hit rate information on connection drop
Node may go down, so after it restarts cache hit rate info will be
incorrect and it can be overwhelmed with traffic until new and
up-to-date cache hit rate arrives. Solve this by dropping node's
information on connection reset, it is more accurate than relying on
gossip which may be slow and miss reboot of a node.

database.cc
@@ -58,6 +58,7 @@
 #include "core/do_with.hh"
 #include "service/migration_manager.hh"
 #include "service/storage_service.hh"
+#include "message/messaging_service.hh"
 #include "mutation_query.hh"
 #include "sstable_mutation_readers.hh"
 #include <core/fstream.hh>
@@ -2567,6 +2568,15 @@ bool database::is_replacing() {
     return bool(get_replace_address());
 }
 
+void database::register_connection_drop_notifier(net::messaging_service& ms) {
+    ms.register_connection_drop_notifier([this] (gms::inet_address ep) {
+        dblog.info("Drop hit rate info for {} because of disconnect", ep);
+        for (auto&& cf : get_non_system_column_families()) {
+            cf->drop_hit_rate(ep);
+        }
+    });
+}
+
 std::ostream& operator<<(std::ostream& out, const atomic_cell_or_collection& c) {
     return out << to_hex(c._data);
 }
@@ -3657,4 +3667,8 @@ column_family::cache_hit_rate column_family::get_hit_rate(gms::inet_address addr
     } else {
         return it->second;
     }
+}
+
+void column_family::drop_hit_rate(gms::inet_address addr) {
+    _cluster_cache_hit_rates.erase(addr);
 }

database.hh
@@ -88,6 +88,10 @@ namespace service {
 class storage_proxy;
 }
 
+namespace net {
+class messaging_service;
+}
+
 namespace sstables {
 
 class sstable;
@@ -843,6 +847,7 @@ public:
 
     void set_hit_rate(gms::inet_address addr, cache_temperature rate);
     cache_hit_rate get_hit_rate(gms::inet_address addr);
+    void drop_hit_rate(gms::inet_address addr);
 
     template<typename Func, typename Result = futurize_t<std::result_of_t<Func()>>>
     Result run_with_compaction_disabled(Func && func) {
@@ -1303,6 +1308,7 @@ public:
     semaphore& system_keyspace_read_concurrency_sem() {
         return _system_read_concurrency_sem;
     }
+    void register_connection_drop_notifier(net::messaging_service& ms);
 
     friend class distributed_loader;
 };

main.cc
@@ -529,6 +529,10 @@ int main(int ac, char** av) {
             }).get();
             supervisor::notify("loading sstables");
             distributed_loader::init_non_system_keyspaces(db, proxy).get();
+            // register connection drop notification to update cf's cache hit rate data
+            db.invoke_on_all([] (database& db) {
+                db.register_connection_drop_notifier(net::get_local_messaging_service());
+            }).get();
             supervisor::notify("setting up system keyspace");
             db::system_keyspace::setup(db, qp).get();
             supervisor::notify("starting commit log");

scylladb/scylladb

database.cc
@@ -59,6 +59,7 @@
 #include "core/do_with.hh"
 #include "service/migration_manager.hh"
 #include "service/storage_service.hh"
+#include "message/messaging_service.hh"
 #include "mutation_query.hh"
 #include "sstable_mutation_readers.hh"
 #include <core/fstream.hh>
@@ -2862,6 +2863,15 @@ bool database::is_replacing() {
     return bool(get_replace_address());
 }
 
+void database::register_connection_drop_notifier(netw::messaging_service& ms) {
+    ms.register_connection_drop_notifier([this] (gms::inet_address ep) {
+        dblog.debug("Drop hit rate info for {} because of disconnect", ep);
+        for (auto&& cf : get_non_system_column_families()) {
+            cf->drop_hit_rate(ep);
+        }
+    });
+}
+
 std::ostream& operator<<(std::ostream& out, const atomic_cell_or_collection& c) {
     return out << to_hex(c._data);
 }
@@ -4073,4 +4083,8 @@ column_family::cache_hit_rate column_family::get_hit_rate(gms::inet_address addr
     } else {
         return it->second;
     }
+}
+
+void column_family::drop_hit_rate(gms::inet_address addr) {
+    _cluster_cache_hit_rates.erase(addr);
 }

database.hh
@@ -89,6 +89,10 @@ namespace service {
 class storage_proxy;
 }
 
+namespace netw {
+class messaging_service;
+}
+
 namespace sstables {
 
 class sstable;
@@ -869,6 +873,7 @@ public:
 
     void set_hit_rate(gms::inet_address addr, cache_temperature rate);
     cache_hit_rate get_hit_rate(gms::inet_address addr);
+    void drop_hit_rate(gms::inet_address addr);
 
     template<typename Func, typename Result = futurize_t<std::result_of_t<Func()>>>
     Result run_with_compaction_disabled(Func && func) {
@@ -1341,6 +1346,7 @@ public:
     semaphore& sstable_load_concurrency_sem() {
         return _sstable_load_concurrency_sem;
     }
+    void register_connection_drop_notifier(netw::messaging_service& ms);
 
     friend class distributed_loader;
 };

main.cc
@@ -540,6 +540,10 @@ int main(int ac, char** av) {
 
             supervisor::notify("loading sstables");
             distributed_loader::init_non_system_keyspaces(db, proxy).get();
+            // register connection drop notification to update cf's cache hit rate data
+            db.invoke_on_all([] (database& db) {
+                db.register_connection_drop_notifier(netw::get_local_messaging_service());
+            }).get();
             supervisor::notify("setting up system keyspace");
             db::system_keyspace::setup(db, qp).get();
             supervisor::notify("starting commit log");