scylladb/scylladb

commit 08b2ceb28eea0f916d32d890c8117719e6e46c4c

cql3/cql3_type: Quote UDT name if needed
This patch ensures we properly quote a UDT name, which may contain
characters like ".", which can lead the name to be interpreted as a
keyspace qualified name when parsed by the CQL parser.

Fixes #2491

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

cql3/cql3_type.cc
@@ -31,7 +31,7 @@ namespace cql3 {
 
 sstring cql3_type::to_string() const {
     if (_type->is_user_type() || _type->is_tuple()) {
-        return "frozen<" + _name + ">";
+        return "frozen<" + util::maybe_quote(_name) + ">";
     }
     return _name;
 }

commit 7c17eba8e805ea4b1f76fce0f3047083ff6b6181

cql3/cql3_type: Don't quote tuple types
A regression introduced in 08b2ceb28eea0f916d32d890c8117719e6e46c4c
quoted tuple type names, which, being of the form tuple<t1, ..., tn>,
would always be quoted. The quoted name would then not be found in any
internal data structures.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>
Message-Id: <1497813816-39956-1-git-send-email-duarte@scylladb.com>

cql3/cql3_type.cc
@@ -30,8 +30,11 @@
 namespace cql3 {
 
 sstring cql3_type::to_string() const {
-    if (_type->is_user_type() || _type->is_tuple()) {
+    if (_type->is_user_type()) {
         return "frozen<" + util::maybe_quote(_name) + ">";
+    }
+    if (_type->is_tuple()) {
+        return "frozen<" + _name + ">";
     }
     return _name;
 }