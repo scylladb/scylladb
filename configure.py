8df2afc2558b5fdfaf5bb96858252b60cb748321
Michael Munday <mike.munday@ibm.com>
2017-06-20 20:51:19 -0400

    build: add -lcryptopp to libs
    Not sure why this is necessary on s390x but not x86.

--- configure.py
+++ configure.py
@@ -857,7 +857,7 @@ seastar_deps = 'practically_anything_can_change_so_lets_run_it_every_time_and_re
 
 args.user_cflags += " " + pkg_config("--cflags", "jsoncpp")
 libs = ' '.join(['-lyaml-cpp', '-llz4', '-lz', '-lsnappy', pkg_config("--libs", "jsoncpp"),
-                 maybe_static(args.staticboost, '-lboost_filesystem'), ' -lcrypt',
+                 maybe_static(args.staticboost, '-lboost_filesystem'), ' -lcrypt', ' -lcryptopp',
                  maybe_static(args.staticboost, '-lboost_date_time'),
                 ])
 
