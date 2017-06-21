399d219cab36ef011531a99d5ffcda19fca47339
Avi Kivity <avi@scylladb.com>
2017-06-21 11:28:29 +0300

    Revert "db: prevent latency spikes during streaming/repair"
    This reverts commit bdfa2ed923245e236837f58925c797e26df32361; prevents nodes
    from joining.

--- database.cc
+++ database.cc
@@ -877,8 +877,7 @@ column_family::seal_active_streaming_memtable_immediate() {
             }).then([this, old, newtab] () {
                 add_sstable(newtab, {engine().cpu_id()});
                 trigger_compaction();
-                return old->clear_gently();
-            }).handle_exception([old] (auto ep) {
+            }).handle_exception([] (auto ep) {
                 dblog.error("failed to write streamed sstable: {}", ep);
                 return make_exception_future<>(ep);
             });
