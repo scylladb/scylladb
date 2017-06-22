c423330917e7a4fd214e96fd7137333909f3b20d
Avi Kivity <avi@scylladb.com>
2017-06-22 17:52:33 +0300

    seastarx: add missing make_shared forward declaration
    Without this, clang (correctly) complains that it can't deduce the type when
    it is not explicitly mentioned.

--- seastarx.hh
+++ seastarx.hh
@@ -28,6 +28,9 @@ namespace seastar {
 template <typename T>
 class shared_ptr;
 
+template <typename T>
+shared_ptr<T> make_shared(T&&);
+
 template <typename T, typename... A>
 shared_ptr<T> make_shared(A&&... a);
 
