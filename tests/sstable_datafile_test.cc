dd7052023bdc7c0d264f9734898060fbb31c2785
Raphael S. Carvalho <raphaelsc@scylladb.com>
2017-07-14 16:49:46 -0300

    tests/sstable_test: fix compaction_manager_test
    after 'compaction: make major compaction go through compaction manager',
    the test fails because task is preempted in debug mode before it reaches
    intruction to increase stat.
    
    Backport from 8dfb5f9c33e576916f90acfa426b2045f85e199d
    Included single-line change in compaction_manager_test from
    8b0e358d731d5c87ed0d81ad30f2bdd01db70b02 that fixes release mode too.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>
    Message-Id: <20170714194946.12467-1-raphaelsc@scylladb.com>

--- tests/sstable_datafile_test.cc
+++ tests/sstable_datafile_test.cc
@@ -1052,10 +1052,10 @@ SEASTAR_TEST_CASE(compaction_manager_test) {
 
         BOOST_REQUIRE(cf->sstables_count() == generations->size());
         cf->trigger_compaction();
-        BOOST_REQUIRE(cm->get_stats().active_tasks == 1);
+        BOOST_REQUIRE(cm->get_stats().pending_tasks == 1 || cm->get_stats().active_tasks == 1);
 
         // wait for submitted job to finish.
-        auto end = [cm] { return cm->get_stats().active_tasks == 0; };
+        auto end = [cm] { return cm->get_stats().pending_tasks == 0 && cm->get_stats().active_tasks == 0; };
         return do_until(end, [] {
             // sleep until compaction manager selects cf for compaction.
             return sleep(std::chrono::milliseconds(100));
