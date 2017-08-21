93eaa95378da53d925c2f57e24b958dc90c68a07
Paweł Dziepak <pdziepak@scylladb.com>
2017-08-21 17:29:32 +0100

    sstables: make shared_index_lists::get_or_load exception safe
    Message-Id: <20170821162934.25386-3-pdziepak@scylladb.com>

--- sstables/shared_index_lists.hh
+++ sstables/shared_index_lists.hh
@@ -96,14 +96,17 @@ public:
     future<list_ptr> get_or_load(key_type key, Loader&& loader) {
         auto i = _lists.find(key);
         lw_shared_ptr<entry> e;
+      auto f = [&] {
         if (i != _lists.end()) {
             e = i->second->shared_from_this();
+            return e->loaded.get_shared_future();
         } else {
             ++_shard_stats.misses;
             e = make_lw_shared<entry>(*this, key);
+            auto f = e->loaded.get_shared_future();
             auto res = _lists.emplace(key, e.get());
             assert(res.second);
-            loader(key).then_wrapped([e](future<index_list>&& f) mutable {
+            futurize_apply(loader, key).then_wrapped([e](future<index_list>&& f) mutable {
                 if (f.failed()) {
                     e->loaded.set_exception(f.get_exception());
                 } else {
@@ -111,13 +114,16 @@ public:
                     e->loaded.set_value();
                 }
             });
+            return f;
         }
-        future<> f = e->loaded.get_shared_future();
+      }();
         if (!f.available()) {
             ++_shard_stats.blocks;
             return f.then([e]() mutable {
                 return list_ptr(std::move(e));
             });
+        } else if (f.failed()) {
+            return make_exception_future<list_ptr>(std::move(f).get_exception());
         } else {
             ++_shard_stats.hits;
             return make_ready_future<list_ptr>(list_ptr(std::move(e)));
