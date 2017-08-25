scylladb/scylladb

commit e989d65539e656ea48c15ef2a98786e6f06a0a71

dht: Make ring_position_view copyable
dht::token needs to be stored as a pointer now and not a reference so
that validity of old pointers is not impacted by in-place object
construction which would occur in the copy-assignment operator.

[1] says that old pointers can be used to access the new object only
if the type "does not contain any non-static data member whose type is
const-qualified or a reference type".

[1] http://en.cppreference.com/w/cpp/language/lifetime#Storage_reuse

dht/i_partitioner.cc
@@ -442,7 +442,7 @@ bool ring_position::less_compare(const schema& s, const ring_position& other) co
 }
 
 int ring_position_comparator::operator()(ring_position_view lh, ring_position_view rh) const {
-    auto token_cmp = tri_compare(lh._token, rh._token);
+    auto token_cmp = tri_compare(*lh._token, *rh._token);
     if (token_cmp) {
         return token_cmp;
     }
@@ -464,7 +464,7 @@ int ring_position_comparator::operator()(ring_position_view lh, ring_position_vi
 
 int ring_position_comparator::operator()(ring_position_view lh, sstables::key_view rh) const {
     auto rh_token = global_partitioner().get_token(rh);
-    auto token_cmp = tri_compare(lh._token, rh_token);
+    auto token_cmp = tri_compare(*lh._token, rh_token);
     if (token_cmp) {
         return token_cmp;
     }

dht/i_partitioner.hh
@@ -471,7 +471,7 @@ class ring_position_view {
     // For example {_token=t1, _key=nullptr, _weight=1} is ordered after {_token=t1, _key=k1, _weight=0},
     // but {_token=t1, _key=nullptr, _weight=-1} is ordered before it.
     //
-    const dht::token& _token;
+    const dht::token* _token; // always not nullptr
     const partition_key* _key; // Can be nullptr
     int8_t _weight;
 public:
@@ -487,11 +487,11 @@ public:
     }
 
     bool is_min() const {
-        return _token.is_minimum();
+        return _token->is_minimum();
     }
 
     bool is_max() const {
-        return _token.is_maximum();
+        return _token->is_maximum();
     }
 
     static ring_position_view for_range_start(const partition_range& r) {
@@ -511,10 +511,13 @@ public:
     }
 
     ring_position_view(const dht::ring_position& pos, after_key after = after_key::no)
-        : _token(pos.token())
+        : _token(&pos.token())
         , _key(pos.has_key() ? &*pos.key() : nullptr)
         , _weight(pos.has_key() ? bool(after) : pos.relation_to_keys())
     { }
+
+    ring_position_view(const ring_position_view& pos) = default;
+    ring_position_view& operator=(const ring_position_view& other) = default;
 
     ring_position_view(after_key_tag, const ring_position_view& v)
         : _token(v._token)
@@ -523,13 +526,13 @@ public:
     { }
 
     ring_position_view(const dht::decorated_key& key, after_key after_key = after_key::no)
-        : _token(key.token())
+        : _token(&key.token())
         , _key(&key.key())
         , _weight(bool(after_key))
     { }
 
     ring_position_view(const dht::token& token, partition_key* key, int8_t weight)
-        : _token(token)
+        : _token(&token)
         , _key(key)
         , _weight(weight)
     { }

commit 2ca99be27d62b53a399297078e9fe622221f28c1

ring_position_view: Print token instead of token pointer
Broken in e989d65539e656ea48c15ef2a98786e6f06a0a71.
Message-Id: <1503667158-7544-1-git-send-email-tgrabiec@scylladb.com>

dht/i_partitioner.cc
@@ -249,7 +249,7 @@ std::ostream& operator<<(std::ostream& out, const ring_position& pos) {
 }
 
 std::ostream& operator<<(std::ostream& out, ring_position_view pos) {
-    out << "{" << pos._token;
+    out << "{" << *pos._token;
     if (pos._key) {
         out << ", " << *pos._key;
     }