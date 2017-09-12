ee1e7732a61b5c060413d9e7945f92cfab830371
Tomasz Grabiec <tgrabiec@scylladb.com>
2017-09-12 09:11:09 +0200

    database: Create tables with continuous cache
    When table is created, it doesn't contain any data, so we can mark the whole
    data range as continuous in cache. This way reads will immediately hit, and
    flushes will populate. If sstables are later attached, the attaching process
    is supposed to invalidate affected ranges (and it does).
    
    Fixes #2536.
    
    Message-Id: <1505200269-4031-1-git-send-email-tgrabiec@scylladb.com>

--- database.cc
+++ database.cc
@@ -135,7 +135,7 @@ column_family::column_family(schema_ptr schema, config config, db::commitlog* cl
     , _streaming_memtables(_config.enable_disk_writes ? make_streaming_memtable_list() : make_memory_only_memtable_list())
     , _compaction_strategy(make_compaction_strategy(_schema->compaction_strategy(), _schema->compaction_strategy_options()))
     , _sstables(make_lw_shared(_compaction_strategy.make_sstable_set(_schema)))
-    , _cache(_schema, sstables_as_snapshot_source(), global_cache_tracker())
+    , _cache(_schema, sstables_as_snapshot_source(), global_cache_tracker(), is_continuous::yes)
     , _commitlog(cl)
     , _compaction_manager(compaction_manager)
     , _counter_cell_locks(std::make_unique<cell_locker>(_schema, cl_stats))
