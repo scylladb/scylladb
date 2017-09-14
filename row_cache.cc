scylladb/scylladb

commit c78047fa5b66e500cdb7295efc5159e87d756e3d

row_cache: Evict partition snapshots
If snapshots are not evicted, they may pin unbouned amount of memory
for a long time in cache, which may lead to OOM. Evict snapshots
together with the entry.

Fixes #2775.
Fixes #2730.

row_cache.cc
@@ -854,7 +854,7 @@ future<> row_cache::update_invalidating(external_updater eu, memtable& m) {
             // This invalidates all row ranges and the static row, leaving only the partition tombstone continuous,
             // which has to always be continuous.
             cache_entry& e = *cache_i;
-            e.partition() = partition_entry(mutation_partition::make_incomplete(*e.schema(), mem_e.partition().partition_tombstone()));
+            e.partition().evict(); // FIXME: evict gradually
         } else {
             _tracker.clear_continuity(*cache_i);
         }
@@ -964,6 +964,10 @@ cache_entry::cache_entry(cache_entry&& o) noexcept
         container_type::node_algorithms::replace_node(o._cache_link.this_ptr(), _cache_link.this_ptr());
         container_type::node_algorithms::init(o._cache_link.this_ptr());
     }
+}
+
+cache_entry::~cache_entry() {
+    _pe.evict();
 }
 
 void row_cache::set_schema(schema_ptr new_schema) noexcept {

row_cache.hh
@@ -124,6 +124,7 @@ public:
     }
 
     cache_entry(cache_entry&&) noexcept;
+    ~cache_entry();
 
     bool is_evictable() { return _lru_link.is_linked(); }
     const dht::decorated_key& key() const { return _key; }

commit 0911fbbdef27260a6d606e2bcc5f3ee1e3eaa4d0

row_cache: Fix row_cache::update_invalidating()
evict() doesn't guarantee that the whole partition is discontinuous.
In particular, partition tombstone cannot be marked as discontinuous.
The parts which are still continuous must be updated.

Broken after c78047fa5b.

Message-Id: <1505375684-28574-1-git-send-email-tgrabiec@scylladb.com>

row_cache.cc
@@ -855,6 +855,8 @@ future<> row_cache::update_invalidating(external_updater eu, memtable& m) {
             // which has to always be continuous.
             cache_entry& e = *cache_i;
             e.partition().evict(); // FIXME: evict gradually
+            upgrade_entry(e);
+            e.partition().apply_to_incomplete(*_schema, std::move(mem_e.partition()), *mem_e.schema());
         } else {
             _tracker.clear_continuity(*cache_i);
         }

tests/row_cache_test.cc
@@ -1579,6 +1579,7 @@ SEASTAR_TEST_CASE(test_update_invalidating) {
         auto mt = make_lw_shared<memtable>(s.schema());
 
         auto m3 = mutation_for_key(m1.decorated_key());
+        m3.partition().apply(s.new_tombstone());
         auto m4 = mutation_for_key(keys[2]);
         auto m5 = mutation_for_key(keys[0]);
         mt->apply(m3);