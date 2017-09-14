713d75fd518157fb8f021278e0b4154f9be2e491
Tomasz Grabiec <tgrabiec@scylladb.com>
2017-09-14 19:15:41 +0200

    storage_service: Introduce SCHEMA_TABLES_V3 feature

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -265,6 +265,7 @@ private:
     gms::feature _indexes_feature;
     gms::feature _digest_multipartition_read_feature;
     gms::feature _correct_counter_order_feature;
+    gms::feature _schema_tables_v3;
 public:
     void enable_all_features() {
         _range_tombstones_feature.enable();
@@ -274,6 +275,7 @@ public:
         _indexes_feature.enable();
         _digest_multipartition_read_feature.enable();
         _correct_counter_order_feature.enable();
+        _schema_tables_v3.enable();
     }
 
     void finish_bootstrapping() {
@@ -2246,6 +2248,10 @@ public:
 
     bool cluster_supports_correct_counter_order() const {
         return bool(_correct_counter_order_feature);
+    }
+
+    bool cluster_supports_schema_tables_v3() const {
+        return bool(_schema_tables_v3);
     }
 };
 
