scylladb/scylladb

commit 27a3b4bca9643c60ce7c444f8e52c3ec78c3edc5

row_cache: Don't invalidate references on insertion
modification_count is currently only used to detect invalidation of
references, intended to be incremented on erasure.

Insertion into intrusive set doesn't invalidate references, so no
need to increment the counter.

row_cache.cc
@@ -153,7 +153,6 @@ void cache_tracker::touch(cache_entry& e) {
 void cache_tracker::insert(cache_entry& entry) {
     ++_stats.partition_insertions;
     ++_stats.partitions;
-    ++_stats.modification_count;
     _lru.push_front(entry);
 }
 

row_cache.hh
@@ -355,10 +355,7 @@ private:
         previous_entry_pointer() = default; // Represents dht::ring_position_view::min()
         previous_entry_pointer(dht::decorated_key key) : _key(std::move(key)) {};
 
-        // TODO: Currently inserting an entry to the cache increases
-        // modification counter. That doesn't seem to be necessary and if we
-        // didn't do that we could store iterator here to avoid key comparison
-        // (not to mention avoiding lookups in just_cache_scanning_reader.
+        // TODO: store iterator here to avoid key comparison
     };
 
     template<typename CreateEntry, typename VisitEntry>

commit 083b9cddefba58a719d64b0e0bdb16859c340176

row_cache: Fix handling of concurrent partition population
This fixes a regression introduced in 27a3b4bca9643 (master only).

partition_range_cursor assumes that as long as references are valid,
_end is valid as well. But if new entries were inserted before _end,
it may not, if the new entries fall after the query range. This may
result in reads returning partitions from outside the query range.
Message-Id: <1507815478-20269-1-git-send-email-tgrabiec@scylladb.com>

row_cache.cc
@@ -152,6 +152,8 @@ void cache_tracker::touch(cache_entry& e) {
 void cache_tracker::insert(cache_entry& entry) {
     ++_stats.partition_insertions;
     ++_stats.partitions;
+    // partition_range_cursor depends on this to detect invalidation of _end
+    _region.allocator().invalidate_references();
     _lru.push_front(entry);
 }
 