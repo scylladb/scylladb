3e4c6cc4dab288571db0bfea33fac9b61acae597
Pekka Enberg <penberg@scylladb.com>
2017-11-02 08:44:14 +0200

    tests/cql_query_test: Add indexed clustering key query test

--- tests/cql_query_test.cc
+++ tests/cql_query_test.cc
@@ -2515,11 +2515,34 @@ SEASTAR_TEST_CASE(test_pg_style_string_literal) {
     });
 }
 
-SEASTAR_TEST_CASE(test_secondary_index_query) {
+SEASTAR_TEST_CASE(test_secondary_index_regular_column_query) {
     return do_with_cql_env([] (auto& e) {
         return e.execute_cql("CREATE TABLE users (userid int, name text, email text, country text, PRIMARY KEY (userid));").discard_result().then([&e] {
             return e.execute_cql("CREATE INDEX ON users (email);").discard_result();
         }).then([&e] {
+            return e.execute_cql("CREATE INDEX ON users (country);").discard_result();
+        }).then([&e] {
+            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (0, 'Bondie Easseby', 'beassebyv@house.gov', 'France');").discard_result();
+        }).then([&e] {
+            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (1, 'Demetri Curror', 'dcurrorw@techcrunch.com', 'France');").discard_result();
+        }).then([&e] {
+            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (2, 'Langston Paulisch', 'lpaulischm@reverbnation.com', 'United States');").discard_result();
+        }).then([&e] {
+            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (3, 'Channa Devote', 'cdevote14@marriott.com', 'Denmark');").discard_result();
+        }).then([&e] {
+            return e.execute_cql("SELECT email FROM users WHERE country = 'France';");
+        }).then([&e] (auto msg) {
+            assert_that(msg).is_rows().with_rows({
+                { utf8_type->decompose(sstring("beassebyv@house.gov")) },
+                { utf8_type->decompose(sstring("dcurrorw@techcrunch.com")) },
+            });
+        });
+    });
+}
+
+SEASTAR_TEST_CASE(test_secondary_index_clustering_key_query) {
+    return do_with_cql_env([] (auto& e) {
+        return e.execute_cql("CREATE TABLE users (userid int, name text, email text, country text, PRIMARY KEY (userid, country));").discard_result().then([&e] {
             return e.execute_cql("CREATE INDEX ON users (country);").discard_result();
         }).then([&e] {
             return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (0, 'Bondie Easseby', 'beassebyv@house.gov', 'France');").discard_result();
