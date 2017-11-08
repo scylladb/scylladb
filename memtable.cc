glommer/scylla

memtable: factor out calculation of memtable_entry memory size
The total size is the sum of two components. Add a method that
does that sum so this code gets easier to reuse.

Signed-off-by: Glauber Costa <glauber@scylladb.com>

memtable.cc
@@ -348,9 +348,7 @@ public:
         assert(_mt._flushed_memory <= _mt.occupancy().used_space());
     }
     void account_component(memtable_entry& e) {
-        auto delta = _mt.allocator().object_memory_size_in_allocator(&e)
-                     + e.external_memory_usage_without_rows();
-        update_bytes_read(delta);
+        update_bytes_read(e.total_size_in_allocator(_mt.allocator()));
     }
     void account_component(partition_snapshot& snp) {
         update_bytes_read(_mt.allocator().object_memory_size_in_allocator(&*snp.version()));

memtable.hh
@@ -65,6 +65,10 @@ public:
         return _key.key().external_memory_usage();
     }
 
+    size_t total_size_in_allocator(allocation_strategy& allocator) {
+        return allocator.object_memory_size_in_allocator(this) + external_memory_usage_without_rows();
+    }
+
     struct compare {
         dht::decorated_key::less_comparator _c;
 

scylladb/scylladb

memtable.cc
@@ -387,9 +387,7 @@ public:
         assert(_mt._flushed_memory <= _mt.occupancy().used_space());
     }
     void account_component(memtable_entry& e) {
-        auto delta = _mt.allocator().object_memory_size_in_allocator(&e)
-                     + e.external_memory_usage_without_rows();
-        update_bytes_read(delta);
+        update_bytes_read(e.size_in_allocator_without_rows(_mt.allocator()));
     }
     void account_component(partition_snapshot& snp) {
         update_bytes_read(_mt.allocator().object_memory_size_in_allocator(&*snp.version()));

memtable.hh
@@ -66,6 +66,10 @@ public:
         return _key.key().external_memory_usage();
     }
 
+    size_t size_in_allocator_without_rows(allocation_strategy& allocator) {
+        return allocator.object_memory_size_in_allocator(this) + external_memory_usage_without_rows();
+    }
+
     struct compare {
         dht::decorated_key::less_comparator _c;
 