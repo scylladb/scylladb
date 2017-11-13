5ba162171664a9b4b6ac63c67cd7036151a8116b
Avi Kivity <avi@scylladb.com>
2017-11-13 12:48:05 +0200

    Merge "thrift/server: Ensure stop() waits for accepts" from Duarte
    "Ensure stop() waits for the accept loop to complete to avoid crashes
    during shutdown."
    
    * 'thrift-server-stop/v4' of https://github.com/duarten/scylla:
      thrift/server: Restore code format
      thrift/server: Stopping the server waits for connection shutdown
      thrift/server: Abort listeners on stop()
      thrift/server: Avoid manual memory management
      thrift/server: Add move ctor for connection
      thrift/server: Extract retry logic
      thrift/server: Retry with backoff for some error types
      thrift/server: Retry accept in case of error
    
    (cherry picked from commit 061f6830faf893a673e49d05f028aef46a5a8c0f)

--- thrift/server.hh
+++ thrift/server.hh
@@ -25,6 +25,7 @@
 #include "core/reactor.hh"
 #include "core/distributed.hh"
 #include "cql3/query_processor.hh"
+#include <seastar/core/gate.hh>
 #include <memory>
 #include <cstdint>
 #include <boost/intrusive/list.hpp>
@@ -79,6 +80,7 @@ class thrift_server {
     public:
         connection(thrift_server& server, connected_socket&& fd, socket_address addr);
         ~connection();
+        connection(connection&&);
         future<> process();
         future<> read();
         future<> write();
@@ -96,6 +98,7 @@ private:
     uint64_t _current_connections = 0;
     uint64_t _requests_served = 0;
     boost::intrusive::list<connection> _connections_list;
+    seastar::gate _stop_gate;
 public:
     thrift_server(distributed<database>& db, distributed<cql3::query_processor>& qp);
     ~thrift_server();
@@ -105,6 +108,9 @@ public:
     uint64_t total_connections() const;
     uint64_t current_connections() const;
     uint64_t requests_served() const;
+
+private:
+    void maybe_retry_accept(int which, bool keepalive, std::exception_ptr ex);
 };
 
 #endif /* APPS_SEASTAR_THRIFT_SERVER_HH_ */
