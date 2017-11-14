scylladb/scylladb

commit f570e41d182cd98e5165b5573ed802fc75eced26

gms/gossiper: Remove periodic replication of endpoint state map
For large clusters the map can be big and cause latency problems.
Since we now actively replicate changes, this is no longer needed.

gms/gossiper.cc
@@ -598,12 +598,6 @@ void gossiper::run() {
             heart_beat_state& hbs = endpoint_state_map[br_addr].get_heart_beat_state();
             hbs.update_heart_beat();
 
-            //
-            // We don't care about heart_beat change on other CPUs - so ingnore this
-            // specific change.
-            //
-            shadow_endpoint_state_map[br_addr].get_heart_beat_state() = hbs;
-
             logger.trace("My heartbeat is now {}", endpoint_state_map[br_addr].get_heart_beat_state().get_heart_beat_version());
             std::vector<gossip_digest> g_digests;
             this->make_random_gossip_digest(g_digests);
@@ -683,15 +677,10 @@ void gossiper::run() {
             //      them across all other shards.
             //    - Reschedule the gossiper only after execution on all nodes is done.
             //
-            bool endpoint_map_changed = (shadow_endpoint_state_map != endpoint_state_map);
             bool live_endpoint_changed = (_live_endpoints != _shadow_live_endpoints);
             bool unreachable_endpoint_changed = (_unreachable_endpoints != _shadow_unreachable_endpoints);
 
-            if (endpoint_map_changed || live_endpoint_changed || unreachable_endpoint_changed) {
-                if (endpoint_map_changed) {
-                    shadow_endpoint_state_map = endpoint_state_map;
-                }
-
+            if (live_endpoint_changed || unreachable_endpoint_changed) {
                 if (live_endpoint_changed) {
                     _shadow_live_endpoints = _live_endpoints;
                 }
@@ -700,14 +689,9 @@ void gossiper::run() {
                     _shadow_unreachable_endpoints = _unreachable_endpoints;
                 }
 
-                _the_gossiper.invoke_on_all([this, endpoint_map_changed,
-                    live_endpoint_changed, unreachable_endpoint_changed] (gossiper& local_gossiper) {
+                _the_gossiper.invoke_on_all([this, live_endpoint_changed, unreachable_endpoint_changed] (gossiper& local_gossiper) {
                     // Don't copy gossiper(CPU0) maps into themselves!
                     if (engine().cpu_id() != 0) {
-                        if (endpoint_map_changed) {
-                            local_gossiper.endpoint_state_map = shadow_endpoint_state_map;
-                        }
-
                         if (live_endpoint_changed) {
                             local_gossiper._live_endpoints = _shadow_live_endpoints;
                         }

gms/gossiper.hh
@@ -130,7 +130,6 @@ public:
 public:
     /* map where key is the endpoint and value is the state associated with the endpoint */
     std::unordered_map<inet_address, endpoint_state> endpoint_state_map;
-    std::unordered_map<inet_address, endpoint_state> shadow_endpoint_state_map;
     // Used for serializing changes to endpoint_state_map and running of associated change listeners.
     endpoint_locks_map endpoint_locks;
 

commit 7323fe76dbdf137d924c13159d0b12fdaa5bb5f0

gossiper: Replicate endpoint_state::is_alive()
Broken in f570e41d182cd98e5165b5573ed802fc75eced26.

Not replicating this may cause coordinator to treat a node which is
down as alive, or vice verse.

Fixes regression in dtest:

  consistency_test.py:TestAvailability.test_simple_strategy

which was expected to get "unavailable" exception but it was getting a
timeout.

Message-Id: <1510666967-1288-1-git-send-email-tgrabiec@scylladb.com>

gms/endpoint_state.hh
@@ -161,12 +161,16 @@ public:
         return _is_alive;
     }
 
+    void set_alive(bool alive) {
+        _is_alive = alive;
+    }
+
     void mark_alive() {
-        _is_alive = true;
+        set_alive(true);
     }
 
     void mark_dead() {
-        _is_alive = false;
+        set_alive(false);
     }
 
     bool is_shutdown() const {

gms/gossiper.cc
@@ -689,7 +689,7 @@ void gossiper::run() {
                     _shadow_unreachable_endpoints = _unreachable_endpoints;
                 }
 
-                _the_gossiper.invoke_on_all([this, live_endpoint_changed, unreachable_endpoint_changed] (gossiper& local_gossiper) {
+                _the_gossiper.invoke_on_all([this, live_endpoint_changed, unreachable_endpoint_changed, es = endpoint_state_map] (gossiper& local_gossiper) {
                     // Don't copy gossiper(CPU0) maps into themselves!
                     if (engine().cpu_id() != 0) {
                         if (live_endpoint_changed) {
@@ -698,6 +698,10 @@ void gossiper::run() {
 
                         if (unreachable_endpoint_changed) {
                             local_gossiper._unreachable_endpoints = _shadow_unreachable_endpoints;
+                        }
+
+                        for (auto&& e : es) {
+                            local_gossiper.endpoint_state_map[e.first].set_alive(e.second.is_alive());
                         }
                     }
                 }).get();