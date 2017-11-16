5f0883119203c1650039b4cdbb57a639aee0215a
Paweł Dziepak <pdziepak@scylladb.com>
2017-11-16 13:25:23 +0000

    streamed_mutation: fix reversing range tombstones
    Right now reversed streamed mutation emits range tombstones after the
    mutation fragments affected by them. This breakes the queries.
    
    This patch reworks the way range tombstones are handled in reversed
    streams:
     - range tombstones are no longer flipped -- invariant that start bound
       is smaller than the end bound always holds
     - in reversed streams they are ordered by their end_position()
    
    Fixes #2982.

--- range_tombstone.hh
+++ range_tombstone.hh
@@ -202,11 +202,12 @@ private:
     }
 };
 
-// This is a helper intended for accumulating tombstones from a streamed
-// mutation and determining what is the tombstone for a given clustering row.
+// The accumulator expects the incoming range tombstones and clustered rows to
+// follow the ordering used by the mutation readers.
 //
-// After apply(rt) or tombstone_for_row(ck) are called there are followng
-// restrictions for subsequent calls:
+// Unless the accumulator is in the reverse mode, after apply(rt) or
+// tombstone_for_row(ck) are called there are followng restrictions for
+// subsequent calls:
 //  - apply(rt1) can be invoked only if rt.start_bound() < rt1.start_bound()
 //    and ck < rt1.start_bound()
 //  - tombstone_for_row(ck1) can be invoked only if rt.start_bound() < ck1
@@ -214,6 +215,15 @@ private:
 //
 // In other words position in partition of the mutation fragments passed to the
 // accumulator must be increasing.
+//
+// If the accumulator was created with the reversed flag set it expects the
+// stream of the range tombstone to come from a reverse partitions and follow
+// the ordering that they use. In particular, the restrictions from non-reversed
+// mode change to:
+//  - apply(rt1) can be invoked only if rt.end_bound() > rt1.end_bound() and
+//    ck > rt1.end_bound()
+//  - tombstone_for_row(ck1) can be invoked only if rt.end_bound() > ck1 and
+//    ck > ck1.
 class range_tombstone_accumulator {
     bound_view::compare _cmp;
     tombstone _partition_tombstone;
