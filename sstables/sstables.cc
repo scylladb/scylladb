10dea07ab7e32416302eaf4e0ede6160d4131f53
Duarte Nunes <duarte@scylladb.com>
2017-11-20 01:05:49 +0000

    sstables/sstables: No promoted index without clustering keys
    We don't need to generate promoted index if the schema lacks
    clustering keys.
    
    Fixes #2995
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -1576,6 +1576,9 @@ void sstable::maybe_flush_pi_block(file_writer& out,
         const composite& clustering_key,
         const std::vector<bytes_view>& column_names,
         composite::eoc marker) {
+    if (!_schema->clustering_key_size()) {
+        return;
+    }
     bytes_writer_for_column_name w;
     write_column_name(w, *_schema, clustering_key, column_names, marker);
     bytes colname = std::move(w).release();
@@ -1603,17 +1606,15 @@ void sstable::maybe_flush_pi_block(file_writer& out,
         // block includes them), but we set block_next_start_offset after - so
         // even if we wrote a lot of open tombstones, we still get a full
         // block size of new data.
-        if (!clustering_key.empty()) {
-            auto& rts = _pi_write.tombstone_accumulator->range_tombstones_for_row(
-                    clustering_key_prefix::from_range(clustering_key.values()));
-            for (const auto& rt : rts) {
-                auto start = composite::from_clustering_element(*_pi_write.schemap, rt.start);
-                auto end = composite::from_clustering_element(*_pi_write.schemap, rt.end);
-                write_range_tombstone(out,
-                        start, bound_kind_to_start_marker(rt.start_kind),
-                        end, bound_kind_to_end_marker(rt.end_kind),
-                        {}, rt.tomb);
-            }
+        auto& rts = _pi_write.tombstone_accumulator->range_tombstones_for_row(
+                clustering_key_prefix::from_range(clustering_key.values()));
+        for (const auto& rt : rts) {
+            auto start = composite::from_clustering_element(*_pi_write.schemap, rt.start);
+            auto end = composite::from_clustering_element(*_pi_write.schemap, rt.end);
+            write_range_tombstone(out,
+                    start, bound_kind_to_start_marker(rt.start_kind),
+                    end, bound_kind_to_end_marker(rt.end_kind),
+                    {}, rt.tomb);
         }
         _pi_write.block_next_start_offset = out.offset() + _pi_write.desired_block_size;
         _pi_write.block_first_colname = colname;
