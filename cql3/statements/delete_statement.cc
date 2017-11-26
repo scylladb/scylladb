scylladb/scylladb

commit 1c872e2ddc4c8aa03456c053c1ae9737189a25af

cql3/delete_statement: Constrain range deletions for non-compound schemas
We cannot represent ranged deletions with non-inclusive bounds on our
current storage format for schemas that are non-compound, since the
clustering key won't include the EOC byte.

Refs #2986

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

cql3/statements/delete_statement.cc
@@ -106,6 +106,9 @@ delete_statement::prepare_internal(database& db, schema_ptr schema, shared_ptr<v
             || !stmt->restrictions()->get_clustering_columns_restrictions()->has_bound(bound::END)) {
         throw exceptions::invalid_request_exception("A range deletion operation needs to specify both bounds");
     }
+    if (!schema->is_compound()) {
+        throw exceptions::invalid_request_exception("Range deletions on \"compact storage\" schemas are not supported");
+    }
     return stmt;
 }
 

sstables/sstables.cc
@@ -1770,6 +1770,9 @@ void sstable::write_range_tombstone(file_writer& out,
         std::vector<bytes_view> suffix,
         const tombstone t,
         column_mask mask) {
+    if (!_schema->is_compound() && (start_marker == composite::eoc::end || end_marker == composite::eoc::start)) {
+        throw std::logic_error(sprint("Cannot represent marker type in range tombstone for non-compound schemas"));
+    }
     write_column_name(out, start, suffix, start_marker);
     write(out, mask);
     write_column_name(out, end, suffix, end_marker);

commit 15fbb8e1ca5b517970f83e74779e9a4f65d9881b

cql3/delete_statement: Allow non-range deletions on non-compound schemas
This patch fixes a regression introduced in
1c872e2ddc4c8aa03456c053c1ae9737189a25af.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>
Message-Id: <20171126102333.3736-1-duarte@scylladb.com>

cql3/statements/delete_statement.cc
@@ -106,7 +106,7 @@ delete_statement::prepare_internal(database& db, schema_ptr schema, shared_ptr<v
             || !stmt->restrictions()->get_clustering_columns_restrictions()->has_bound(bound::END)) {
         throw exceptions::invalid_request_exception("A range deletion operation needs to specify both bounds");
     }
-    if (!schema->is_compound()) {
+    if (!schema->is_compound() && stmt->restrictions()->get_clustering_columns_restrictions()->is_slice()) {
         throw exceptions::invalid_request_exception("Range deletions on \"compact storage\" schemas are not supported");
     }
     return stmt;