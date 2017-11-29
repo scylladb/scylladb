586b61d57d53f17c18459ea259a26c5120e14e04
Paweł Dziepak <pdziepak@scylladb.com>
2017-11-29 10:59:09 +0000

    size_estimates: convert reader to flat mutation readers
    Message-Id: <20171129105909.27084-1-pdziepak@scylladb.com>

--- db/size_estimates_virtual_reader.hh
+++ db/size_estimates_virtual_reader.hh
@@ -42,42 +42,86 @@ namespace db {
 
 namespace size_estimates {
 
-class size_estimates_mutation_reader final : public mutation_reader::impl {
+class size_estimates_mutation_reader final : public flat_mutation_reader::impl {
     struct token_range {
         bytes start;
         bytes end;
     };
     schema_ptr _schema;
-    const dht::partition_range& _prange;
+    const dht::partition_range* _prange;
     const query::partition_slice& _slice;
     using ks_range = std::vector<sstring>;
     stdx::optional<ks_range> _keyspaces;
     ks_range::const_iterator _current_partition;
     streamed_mutation::forwarding _fwd;
+    flat_mutation_reader_opt _partition_reader;
 public:
     size_estimates_mutation_reader(schema_ptr schema, const dht::partition_range& prange, const query::partition_slice& slice, streamed_mutation::forwarding fwd)
-            : _schema(schema)
-            , _prange(prange)
+            : impl(schema)
+            , _schema(std::move(schema))
+            , _prange(&prange)
             , _slice(slice)
             , _fwd(fwd)
     { }
 
-    virtual future<streamed_mutation_opt> operator()() override {
+private:
+    future<> get_next_partition() {
         // For each specified range, estimate (crudely) mean partition size and partitions count.
         auto& db = service::get_local_storage_proxy().get_db().local();
         if (!_keyspaces) {
-            _keyspaces = get_keyspaces(*_schema, db, _prange);
+            _keyspaces = get_keyspaces(*_schema, db, *_prange);
             _current_partition = _keyspaces->begin();
         }
         if (_current_partition == _keyspaces->end()) {
-            return make_ready_future<streamed_mutation_opt>();
+            _end_of_stream = true;
+            return make_ready_future<>();
         }
         return get_local_ranges().then([&db, this] (auto&& ranges) {
             auto estimates = this->estimates_for_current_keyspace(db, std::move(ranges));
             auto mutations = db::system_keyspace::make_size_estimates_mutation(*_current_partition, std::move(estimates));
             ++_current_partition;
-            return streamed_mutation_opt(streamed_mutation_from_mutation(std::move(mutations), _fwd));
+            std::vector<mutation> ms;
+            ms.emplace_back(std::move(mutations));
+            _partition_reader = flat_mutation_reader_from_mutations(std::move(ms), _fwd);
         });
+    }
+public:
+    virtual future<> fill_buffer() override {
+        return do_until([this] { return is_end_of_stream() || is_buffer_full(); }, [this] {
+            if (!_partition_reader) {
+                return get_next_partition();
+            }
+            return _partition_reader->consume_pausable([this] (mutation_fragment mf) {
+                push_mutation_fragment(std::move(mf));
+                return stop_iteration(is_buffer_full());
+            }).then([this] {
+                if (_partition_reader->is_end_of_stream() && _partition_reader->is_buffer_empty()) {
+                    _partition_reader = stdx::nullopt;
+                }
+            });
+        });
+    }
+    virtual void next_partition() override {
+        clear_buffer_to_next_partition();
+        if (is_buffer_empty()) {
+            _partition_reader = stdx::nullopt;
+        }
+    }
+    virtual future<> fast_forward_to(const dht::partition_range& pr) override {
+        clear_buffer();
+        _prange = &pr;
+        _keyspaces = stdx::nullopt;
+        _partition_reader = stdx::nullopt;
+        _end_of_stream = false;
+        return make_ready_future<>();
+    }
+    virtual future<> fast_forward_to(position_range pr) override {
+        forward_buffer_to(pr.start());
+        _end_of_stream = false;
+        if (_partition_reader) {
+            return _partition_reader->fast_forward_to(std::move(pr));
+        }
+        return make_ready_future<>();
     }
     /**
      * Returns the primary ranges for the local node.
@@ -270,14 +314,14 @@ private:
 };
 
 struct virtual_reader {
-    mutation_reader operator()(schema_ptr schema,
+    flat_mutation_reader operator()(schema_ptr schema,
             const dht::partition_range& range,
             const query::partition_slice& slice,
             const io_priority_class& pc,
             tracing::trace_state_ptr trace_state,
             streamed_mutation::forwarding fwd,
             mutation_reader::forwarding fwd_mr) {
-        return make_mutation_reader<size_estimates_mutation_reader>(schema, range, slice, fwd);
+        return make_flat_mutation_reader<size_estimates_mutation_reader>(schema, range, slice, fwd);
     }
 };
 
