8f02967a3bd722263df1c240d4b2dc3f1390727c
Takuya ASADA <syuu@scylladb.com>
2017-12-06 08:36:41 +0900

    dist/debian: install CA certificates before install repo GPG key
    Since pbuilder chroot environment does not install CA certificates by default,
    accessing https://download.opensuse.org will cause certificate verification
    error.
    So we need to install it before installing 3rdparty repo GPG key.
    
    Also, checking existance of gpgkeys_curl is not needed, since it's always
    not installed since we are running the script in clean chroot environment.
    
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Message-Id: <1512517001-27524-1-git-send-email-syuu@scylladb.com>

--- dist/debian/debian_install_gpgkey.sh
+++ dist/debian/debian_install_gpgkey.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 . /etc/os-release
 
-if [ ! -f /usr/lib/gnupg/gpgkeys_curl ]; then
-    apt-get -y install gnupg-curl
-fi
+apt-get -y install gnupg-curl ca-certificates
 if [ "$VERSION_ID" = "8" ]; then
     apt-key adv --fetch-keys https://download.opensuse.org/repositories/home:/scylladb:/scylla-3rdparty-jessie/Debian_8.0/Release.key
 elif [ "$VERSION_ID" = "9" ]; then
