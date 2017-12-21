b583ef7c8bd2dbfcf8c6cc648dfc1393bef22a2b
Piotr Jastrzebski <piotr@scylladb.com>
2017-12-21 20:34:20 +0100

    Remove unused mutation_source constructors
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- mutation_reader.hh
+++ mutation_reader.hh
@@ -580,16 +580,6 @@ public:
         : mutation_source([fn = std::move(fn)] (schema_ptr s, partition_range range, const query::partition_slice& slice, io_priority pc, tracing::trace_state_ptr tr, streamed_mutation::forwarding fwd, mutation_reader::forwarding) {
             return fn(s, range, slice, pc, std::move(tr), fwd);
         }) {}
-    mutation_source(std::function<mutation_reader(schema_ptr, partition_range, const query::partition_slice&, io_priority)> fn)
-        : mutation_source([fn = std::move(fn)] (schema_ptr s, partition_range range, const query::partition_slice& slice, io_priority pc, tracing::trace_state_ptr, streamed_mutation::forwarding fwd, mutation_reader::forwarding) {
-            assert(!fwd);
-            return fn(s, range, slice, pc);
-        }) {}
-    mutation_source(std::function<mutation_reader(schema_ptr, partition_range, const query::partition_slice&)> fn)
-        : mutation_source([fn = std::move(fn)] (schema_ptr s, partition_range range, const query::partition_slice& slice, io_priority, tracing::trace_state_ptr, streamed_mutation::forwarding fwd, mutation_reader::forwarding) {
-            assert(!fwd);
-            return fn(s, range, slice);
-        }) {}
     mutation_source(std::function<mutation_reader(schema_ptr, partition_range range)> fn)
         : mutation_source([fn = std::move(fn)] (schema_ptr s, partition_range range, const query::partition_slice&, io_priority, tracing::trace_state_ptr, streamed_mutation::forwarding fwd, mutation_reader::forwarding) {
             assert(!fwd);
