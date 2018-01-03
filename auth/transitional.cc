scylladb/scylladb

commit ba6a41d397923c45045c4dddcbfa2c667a3a59ef

auth: Switch to sharded service
This change appears quite large, but is logically fairly simple.

Previously, the `auth` module was structured around global state in a
number of ways:

- There existed global instances for the authenticator and the
  authorizer, which were accessed pervasively throughout the system
  through `auth::authenticator::get()` and `auth::authorizer::get()`,
  respectively. These instances needed to be initialized before they
  could be used with `auth::authenticator::setup(sstring type_name)`
  and `auth::authorizer::setup(sstring type_name)`.

- The implementation of the `auth::auth` functions and the authenticator
  and authorizer depended on resources accessed globally through
  `cql3::get_local_query_processor()` and
  `service::get_local_migration_manager()`.

- CQL statements would check for access and manage users through static
  functions in `auth::auth`. These functions would access the global
  authenticator and authorizer instances and depended on the necessary
  systems being started before they were used.

This change eliminates global state from all of these.

The specific changes are:

- Move out `allow_all_authenticator` and `allow_all_authorizer` into
  their own files so that they're constructed like any other
  authenticator or authorizer.

- Delete `auth.hh` and `auth.cc`. Constants and helper functions useful
  for implementing functionality in the `auth` module have moved to
  `common.hh`.

- Remove silent global dependency in
  `auth::authenticated_user::is_super()` on the auth* service in favour
  of a new function `auth::is_super_user()` with an explicit auth*
  service argument.

- Remove global authenticator and authorizer instances, as well as the
  `setup()` functions.

- Expose dependency on the auth* service in
  `auth::authorizer::authorize()` and `auth::authorizer::list()`, which
  is necessary to check for superuser status.

- Add an explicit `service::migration_manager` argument to the

auth/transitional.cc
@@ -46,11 +46,12 @@
 #include "password_authenticator.hh"
 #include "default_authorizer.hh"
 #include "permission.hh"
-#include "auth.hh"
 #include "db/config.hh"
 #include "utils/class_registrator.hh"
 
 namespace auth {
+
+class service;
 
 static const sstring PACKAGE_NAME("com.scylladb.auth.");
 
@@ -69,8 +70,8 @@ class transitional_authenticator : public authenticator {
 public:
     static const sstring PASSWORD_AUTHENTICATOR_NAME;
 
-    transitional_authenticator(cql3::query_processor& qp)
-        : transitional_authenticator(std::make_unique<password_authenticator>(qp))
+    transitional_authenticator(cql3::query_processor& qp, ::service::migration_manager& mm)
+            : transitional_authenticator(std::make_unique<password_authenticator>(qp, mm))
     {}
     transitional_authenticator(std::unique_ptr<authenticator> a)
         : _authenticator(std::move(a))
@@ -153,8 +154,8 @@ public:
 class transitional_authorizer : public authorizer {
     std::unique_ptr<authorizer> _authorizer;
 public:
-    transitional_authorizer(cql3::query_processor& qp)
-        : transitional_authorizer(std::make_unique<default_authorizer>(qp))
+    transitional_authorizer(cql3::query_processor& qp, ::service::migration_manager& mm)
+        : transitional_authorizer(std::make_unique<default_authorizer>(qp, mm))
     {}
     transitional_authorizer(std::unique_ptr<authorizer> a)
         : _authorizer(std::move(a))
@@ -170,8 +171,8 @@ public:
     const sstring& qualified_java_name() const override {
         return transitional_authorizer_name();
     }
-    future<permission_set> authorize(::shared_ptr<authenticated_user> user, data_resource resource) const override {
-        return user->is_super().then([](bool s) {
+    future<permission_set> authorize(service& ser, ::shared_ptr<authenticated_user> user, data_resource resource) const override {
+        return is_super_user(ser, *user).then([](bool s) {
             static const permission_set transitional_permissions =
                             permission_set::of<permission::CREATE,
                                             permission::ALTER, permission::DROP,
@@ -186,8 +187,8 @@ public:
     future<> revoke(::shared_ptr<authenticated_user> user, permission_set ps, data_resource r, sstring s) override {
         return _authorizer->revoke(std::move(user), std::move(ps), std::move(r), std::move(s));
     }
-    future<std::vector<permission_details>> list(::shared_ptr<authenticated_user> user, permission_set ps, optional<data_resource> r, optional<sstring> s) const override {
-        return _authorizer->list(std::move(user), std::move(ps), std::move(r), std::move(s));
+    future<std::vector<permission_details>> list(service& ser, ::shared_ptr<authenticated_user> user, permission_set ps, optional<data_resource> r, optional<sstring> s) const override {
+        return _authorizer->list(ser, std::move(user), std::move(ps), std::move(r), std::move(s));
     }
     future<> revoke_all(sstring s) override {
         return _authorizer->revoke_all(std::move(s));
@@ -209,9 +210,14 @@ public:
 // To ensure correct initialization order, we unfortunately need to use string literals.
 //
 
-static const class_registrator<auth::authenticator,
-                auth::transitional_authenticator, cql3::query_processor&> transitional_authenticator_reg(
-                "com.scylladb.auth.TransitionalAuthenticator");
+static const class_registrator<
+        auth::authenticator,
+        auth::transitional_authenticator,
+        cql3::query_processor&,
+        ::service::migration_manager&> transitional_authenticator_reg("com.scylladb.auth.TransitionalAuthenticator");
 
-static const class_registrator<auth::authorizer, auth::transitional_authorizer, cql3::query_processor&> transitional_authorizer_reg(
-                "com.scylladb.auth.TransitionalAuthorizer");
+static const class_registrator<
+        auth::authorizer,
+        auth::transitional_authorizer,
+        cql3::query_processor&,
+        ::service::migration_manager&> transitional_authorizer_reg("com.scylladb.auth.TransitionalAuthorizer");

auth/allow_all_authenticator.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 ScyllaDB
+ * Copyright (C) 2017 ScyllaDB
  */
 
 /*
@@ -19,16 +19,23 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef APPS_SEASTAR_THRIFT_HANDLER_HH_
-#define APPS_SEASTAR_THRIFT_HANDLER_HH_
+#include "auth/allow_all_authenticator.hh"
 
-#include "Cassandra.h"
-#include "auth/service.hh"
-#include "database.hh"
-#include "core/distributed.hh"
-#include "cql3/query_processor.hh"
-#include <memory>
+#include "service/migration_manager.hh"
+#include "utils/class_registrator.hh"
 
-std::unique_ptr<::cassandra::CassandraCobSvIfFactory> create_handler_factory(distributed<database>& db, distributed<cql3::query_processor>& qp, auth::service&);
+namespace auth {
 
-#endif /* APPS_SEASTAR_THRIFT_HANDLER_HH_ */
+const sstring& allow_all_authenticator_name() {
+    static const sstring name = meta::AUTH_PACKAGE_NAME + "AllowAllAuthenticator";
+    return name;
+}
+
+// To ensure correct initialization order, we unfortunately need to use a string literal.
+static const class_registrator<
+        authenticator,
+        allow_all_authenticator,
+        cql3::query_processor&,
+        ::service::migration_manager&> registration("org.apache.cassandra.auth.AllowAllAuthenticator");
+
+}

auth/allow_all_authenticator.hh
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2017 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdexcept>
+
+#include "auth/authenticator.hh"
+#include "auth/authenticated_user.hh"
+#include "auth/common.hh"
+
+namespace cql3 {
+class query_processor;
+}
+
+namespace service {
+class migration_manager;
+}
+
+namespace auth {
+
+const sstring& allow_all_authenticator_name();
+
+class allow_all_authenticator final : public authenticator {
+public:
+    allow_all_authenticator(cql3::query_processor&, ::service::migration_manager&) {
+    }
+
+    future<> start() override {
+        return make_ready_future<>();
+    }
+
+    future<> stop() override {
+        return make_ready_future<>();
+    }
+
+    const sstring& qualified_java_name() const override {
+        return allow_all_authenticator_name();
+    }
+
+    bool require_authentication() const override {
+        return false;
+    }
+
+    option_set supported_options() const override {
+        return option_set();
+    }
+
+    option_set alterable_options() const override {
+        return option_set();
+    }
+
+    future<::shared_ptr<authenticated_user>> authenticate(const credentials_map& credentials) const override {
+        return make_ready_future<::shared_ptr<authenticated_user>>(::make_shared<authenticated_user>());
+    }
+
+    future<> create(sstring username, const option_map& options) override {
+        return make_ready_future();
+    }
+
+    future<> alter(sstring username, const option_map& options) override {
+        return make_ready_future();
+    }
+
+    future<> drop(sstring username) override {
+        return make_ready_future();
+    }
+
+    const resource_ids& protected_resources() const override {
+        static const resource_ids ids;
+        return ids;
+    }
+
+    ::shared_ptr<sasl_challenge> new_sasl_challenge() const override {
+        throw std::runtime_error("Should not reach");
+    }
+};
+
+}

auth/allow_all_authorizer.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 ScyllaDB
+ * Copyright (C) 2017 ScyllaDB
  */
 
 /*
@@ -19,16 +19,23 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef APPS_SEASTAR_THRIFT_HANDLER_HH_
-#define APPS_SEASTAR_THRIFT_HANDLER_HH_
+#include "auth/allow_all_authorizer.hh"
 
-#include "Cassandra.h"
-#include "auth/service.hh"
-#include "database.hh"
-#include "core/distributed.hh"
-#include "cql3/query_processor.hh"
-#include <memory>
+#include "auth/common.hh"
+#include "utils/class_registrator.hh"
 
-std::unique_ptr<::cassandra::CassandraCobSvIfFactory> create_handler_factory(distributed<database>& db, distributed<cql3::query_processor>& qp, auth::service&);
+namespace auth {
 
-#endif /* APPS_SEASTAR_THRIFT_HANDLER_HH_ */
+const sstring& allow_all_authorizer_name() {
+    static const sstring name = meta::AUTH_PACKAGE_NAME + "AllowAllAuthorizer";
+    return name;
+}
+
+// To ensure correct initialization order, we unfortunately need to use a string literal.
+static const class_registrator<
+    authorizer,
+    allow_all_authorizer,
+    cql3::query_processor&,
+    ::service::migration_manager&> registration("org.apache.cassandra.auth.AllowAllAuthorizer");
+
+}

auth/allow_all_authorizer.hh
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2017 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "authorizer.hh"
+#include "exceptions/exceptions.hh"
+#include "stdx.hh"
+
+namespace cql3 {
+class query_processor;
+}
+
+namespace service {
+class migration_manager;
+}
+
+namespace auth {
+
+class service;
+
+const sstring& allow_all_authorizer_name();
+
+class allow_all_authorizer final  : public authorizer {
+public:
+    allow_all_authorizer(cql3::query_processor&, ::service::migration_manager&) {
+    }
+
+    future<> start() override {
+        return make_ready_future<>();
+    }
+
+    future<> stop() override {
+        return make_ready_future<>();
+    }
+
+    const sstring& qualified_java_name() const override {
+        return allow_all_authorizer_name();
+    }
+
+    future<permission_set> authorize(service&, ::shared_ptr<authenticated_user>, data_resource) const override {
+        return make_ready_future<permission_set>(permissions::ALL);
+    }
+
+    future<> grant(::shared_ptr<authenticated_user>, permission_set, data_resource, sstring) override {
+        throw exceptions::invalid_request_exception("GRANT operation is not supported by AllowAllAuthorizer");
+    }
+
+    future<> revoke(::shared_ptr<authenticated_user>, permission_set, data_resource, sstring) override {
+        throw exceptions::invalid_request_exception("REVOKE operation is not supported by AllowAllAuthorizer");
+    }
+
+    future<std::vector<permission_details>> list(
+            service&,
+            ::shared_ptr<authenticated_user> performer,
+            permission_set,
+            stdx::optional<data_resource>,
+            stdx::optional<sstring>) const override {
+        throw exceptions::invalid_request_exception("LIST PERMISSIONS operation is not supported by AllowAllAuthorizer");
+    }
+
+    future<> revoke_all(sstring dropped_user) override {
+        return make_ready_future();
+    }
+
+    future<> revoke_all(data_resource) override {
+        return make_ready_future();
+    }
+
+    const resource_ids& protected_resources() override {
+        static const resource_ids ids;
+        return ids;
+    }
+
+    future<> validate_configuration() const override {
+        return make_ready_future();
+    }
+};
+
+}

auth/auth.cc
@@ -1,286 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Copyright (C) 2016 ScyllaDB
- *
- * Modified by ScyllaDB
- */
-
-/*
- * This file is part of Scylla.
- *
- * Scylla is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Scylla is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include <chrono>
-
-#include <seastar/core/sleep.hh>
-
-#include <seastar/core/distributed.hh>
-
-#include "auth.hh"
-#include "authenticator.hh"
-#include "authorizer.hh"
-#include "common.hh"
-#include "database.hh"
-#include "cql3/query_processor.hh"
-#include "cql3/untyped_result_set.hh"
-#include "cql3/statements/raw/cf_statement.hh"
-#include "cql3/statements/create_table_statement.hh"
-#include "db/config.hh"
-#include "delayed_tasks.hh"
-#include "permissions_cache.hh"
-#include "service/migration_manager.hh"
-#include "utils/loading_cache.hh"
-#include "utils/hash.hh"
-
-static const sstring USER_NAME("name");
-static const sstring SUPER("super");
-
-static logging::logger alogger("auth");
-
-// TODO: configurable
-using namespace std::chrono_literals;
-static const std::chrono::milliseconds SUPERUSER_SETUP_DELAY = 10000ms;
-
-class auth_migration_listener : public service::migration_listener {
-    void on_create_keyspace(const sstring& ks_name) override {}
-    void on_create_column_family(const sstring& ks_name, const sstring& cf_name) override {}
-    void on_create_user_type(const sstring& ks_name, const sstring& type_name) override {}
-    void on_create_function(const sstring& ks_name, const sstring& function_name) override {}
-    void on_create_aggregate(const sstring& ks_name, const sstring& aggregate_name) override {}
-    void on_create_view(const sstring& ks_name, const sstring& view_name) override {}
-
-    void on_update_keyspace(const sstring& ks_name) override {}
-    void on_update_column_family(const sstring& ks_name, const sstring& cf_name, bool) override {}
-    void on_update_user_type(const sstring& ks_name, const sstring& type_name) override {}
-    void on_update_function(const sstring& ks_name, const sstring& function_name) override {}
-    void on_update_aggregate(const sstring& ks_name, const sstring& aggregate_name) override {}
-    void on_update_view(const sstring& ks_name, const sstring& view_name, bool columns_changed) override {}
-
-    void on_drop_keyspace(const sstring& ks_name) override {
-        auth::authorizer::get().revoke_all(auth::data_resource(ks_name));
-    }
-    void on_drop_column_family(const sstring& ks_name, const sstring& cf_name) override {
-        auth::authorizer::get().revoke_all(auth::data_resource(ks_name, cf_name));
-    }
-    void on_drop_user_type(const sstring& ks_name, const sstring& type_name) override {}
-    void on_drop_function(const sstring& ks_name, const sstring& function_name) override {}
-    void on_drop_aggregate(const sstring& ks_name, const sstring& aggregate_name) override {}
-    void on_drop_view(const sstring& ks_name, const sstring& view_name) override {}
-};
-
-static auth_migration_listener auth_migration;
-
-static sharded<auth::permissions_cache> perm_cache;
-
-static future<> start_permission_cache() {
-    auto& db_config = cql3::get_local_query_processor().db().local().get_config();
-
-    auth::permissions_cache_config c;
-    c.max_entries = db_config.permissions_cache_max_entries();
-    c.validity_period = std::chrono::milliseconds(db_config.permissions_validity_in_ms());
-    c.update_period = std::chrono::milliseconds(db_config.permissions_update_interval_in_ms());
-
-    return perm_cache.start(c, std::ref(auth::authorizer::get()), std::ref(alogger));
-}
-
-static delayed_tasks<>& get_local_delayed_tasks() {
-    static thread_local delayed_tasks<> instance;
-    return instance;
-}
-
-void auth::auth::schedule_when_up(scheduled_func f) {
-    get_local_delayed_tasks().schedule_after(SUPERUSER_SETUP_DELAY, std::move(f));
-}
-
-future<> auth::auth::setup() {
-    auto& db = cql3::get_local_query_processor().db().local();
-    auto& cfg = db.get_config();
-
-    qualified_name authenticator_name(meta::AUTH_PACKAGE_NAME, cfg.authenticator()),
-                    authorizer_name(meta::AUTH_PACKAGE_NAME, cfg.authorizer());
-
-    if (allow_all_authenticator_name() == authenticator_name && allow_all_authorizer_name() == authorizer_name) {
-        return authenticator::setup(authenticator_name).then([authorizer_name = std::move(authorizer_name)] {
-            return authorizer::setup(authorizer_name);
-        }).then([] {
-            return start_permission_cache();
-        });
-    }
-
-    future<> f = make_ready_future<>();
-
-    if (!db.has_keyspace(meta::AUTH_KS)) {
-        std::map<sstring, sstring> opts;
-        opts["replication_factor"] = "1";
-        auto ksm = keyspace_metadata::new_keyspace(meta::AUTH_KS, "org.apache.cassandra.locator.SimpleStrategy", opts, true);
-        // We use min_timestamp so that default keyspace metadata will loose with any manual adjustments. See issue #2129.
-        f = service::get_local_migration_manager().announce_new_keyspace(ksm, api::min_timestamp, false);
-    }
-
-    return f.then([] {
-        return setup_table(meta::USERS_CF, sprint("CREATE TABLE %s.%s (%s text, %s boolean, PRIMARY KEY(%s)) WITH gc_grace_seconds=%d",
-                                              meta::AUTH_KS, meta::USERS_CF, USER_NAME, SUPER, USER_NAME,
-                                        90 * 24 * 60 * 60)); // 3 months.
-    }).then([authenticator_name = std::move(authenticator_name)] {
-        return authenticator::setup(authenticator_name);
-    }).then([authorizer_name = std::move(authorizer_name)] {
-        return authorizer::setup(authorizer_name);
-    }).then([] {
-        return start_permission_cache();
-    }).then([] {
-        service::get_local_migration_manager().register_listener(&auth_migration); // again, only one shard...
-        // instead of once-timer, just schedule this later
-        schedule_when_up([] {
-            // setup default super user
-            return has_existing_users(meta::USERS_CF, meta::DEFAULT_SUPERUSER_NAME, USER_NAME).then([](bool exists) {
-                if (!exists) {
-                    auto query = sprint("INSERT INTO %s.%s (%s, %s) VALUES (?, ?) USING TIMESTAMP 0",
-                                    meta::AUTH_KS, meta::USERS_CF, USER_NAME, SUPER);
-                    cql3::get_local_query_processor().process(query, db::consistency_level::ONE, {meta::DEFAULT_SUPERUSER_NAME, true}).then([](auto) {
-                        alogger.info("Created default superuser '{}'", meta::DEFAULT_SUPERUSER_NAME);
-                    }).handle_exception([](auto ep) {
-                        try {
-                            std::rethrow_exception(ep);
-                        } catch (exceptions::request_execution_exception&) {
-                            alogger.warn("Skipped default superuser setup: some nodes were not ready");
-                        }
-                    });
-                }
-            });
-        });
-    });
-}
-
-future<> auth::auth::shutdown() {
-    // just make sure we don't have pending tasks.
-    // this is mostly relevant for test cases where
-    // db-env-shutdown != process shutdown
-    return smp::invoke_on_all([] {
-        get_local_delayed_tasks().cancel_all();
-    }).then([] {
-        return perm_cache.stop();
-    }).then([] {
-        return when_all_succeed(authorizer::get().stop(), authenticator::get().stop());
-    });
-}
-
-future<auth::permission_set> auth::auth::get_permissions(::shared_ptr<authenticated_user> user, data_resource resource) {
-    return perm_cache.local().get(std::move(user), std::move(resource));
-}
-
-static db::consistency_level consistency_for_user(const sstring& username) {
-    if (username == auth::meta::DEFAULT_SUPERUSER_NAME) {
-        return db::consistency_level::QUORUM;
-    }
-    return db::consistency_level::LOCAL_ONE;
-}
-
-static future<::shared_ptr<cql3::untyped_result_set>> select_user(const sstring& username) {
-    // Here was a thread local, explicit cache of prepared statement. In normal execution this is
-    // fine, but since we in testing set up and tear down system over and over, we'd start using
-    // obsolete prepared statements pretty quickly.
-    // Rely on query processing caching statements instead, and lets assume
-    // that a map lookup string->statement is not gonna kill us much.
-    return cql3::get_local_query_processor().process(
-                    sprint("SELECT * FROM %s.%s WHERE %s = ?",
-                                    auth::meta::AUTH_KS, auth::meta::USERS_CF,
-                                    USER_NAME), consistency_for_user(username),
-                    { username }, true);
-}
-
-future<bool> auth::auth::is_existing_user(const sstring& username) {
-    return select_user(username).then(
-                    [](::shared_ptr<cql3::untyped_result_set> res) {
-                        return make_ready_future<bool>(!res->empty());
-                    });
-}
-
-future<bool> auth::auth::is_super_user(const sstring& username) {
-    return select_user(username).then(
-                    [](::shared_ptr<cql3::untyped_result_set> res) {
-                        return make_ready_future<bool>(!res->empty() && res->one().get_as<bool>(SUPER));
-                    });
-}
-
-future<> auth::auth::insert_user(const sstring& username, bool is_super) {
-    return cql3::get_local_query_processor().process(sprint("INSERT INTO %s.%s (%s, %s) VALUES (?, ?)",
-                    meta::AUTH_KS, meta::USERS_CF, USER_NAME, SUPER),
-                    consistency_for_user(username), { username, is_super }).discard_result();
-}
-
-future<> auth::auth::delete_user(const sstring& username) {
-    return cql3::get_local_query_processor().process(sprint("DELETE FROM %s.%s WHERE %s = ?",
-                    meta::AUTH_KS, meta::USERS_CF, USER_NAME),
-                    consistency_for_user(username), { username }).discard_result();
-}
-
-future<> auth::auth::setup_table(const sstring& name, const sstring& cql) {
-    auto& qp = cql3::get_local_query_processor();
-    auto& db = qp.db().local();
-
-    if (db.has_schema(meta::AUTH_KS, name)) {
-        return make_ready_future();
-    }
-
-    ::shared_ptr<cql3::statements::raw::cf_statement> parsed = static_pointer_cast<
-                    cql3::statements::raw::cf_statement>(cql3::query_processor::parse_statement(cql));
-    parsed->prepare_keyspace(meta::AUTH_KS);
-    ::shared_ptr<cql3::statements::create_table_statement> statement =
-                    static_pointer_cast<cql3::statements::create_table_statement>(
-                                    parsed->prepare(db, qp.get_cql_stats())->statement);
-    auto schema = statement->get_cf_meta_data();
-    auto uuid = generate_legacy_id(schema->ks_name(), schema->cf_name());
-
-    schema_builder b(schema);
-    b.set_uuid(uuid);
-    return service::get_local_migration_manager().announce_new_column_family(b.build(), false);
-}
-
-future<bool> auth::auth::has_existing_users(const sstring& cfname, const sstring& def_user_name, const sstring& name_column) {
-    auto default_user_query = sprint("SELECT * FROM %s.%s WHERE %s = ?", meta::AUTH_KS, cfname, name_column);
-    auto all_users_query = sprint("SELECT * FROM %s.%s LIMIT 1", meta::AUTH_KS, cfname);
-
-    return cql3::get_local_query_processor().process(default_user_query, db::consistency_level::ONE, { def_user_name }).then([=](::shared_ptr<cql3::untyped_result_set> res) {
-        if (!res->empty()) {
-            return make_ready_future<bool>(true);
-        }
-        return cql3::get_local_query_processor().process(default_user_query, db::consistency_level::QUORUM, { def_user_name }).then([all_users_query](::shared_ptr<cql3::untyped_result_set> res) {
-            if (!res->empty()) {
-                return make_ready_future<bool>(true);
-            }
-            return cql3::get_local_query_processor().process(all_users_query, db::consistency_level::QUORUM).then([](::shared_ptr<cql3::untyped_result_set> res) {
-                return make_ready_future<bool>(!res->empty());
-            });
-        });
-    });
-}
-

commit 35b9ec868acbe43075cd05f19e00201b0918d99a

auth: Fix transitional auth for non-valid credentials
Fixes #3096

The credentials processing for transitional auth was broken
in ba6a41d, "auth: Switch to sharded service which effectively removed
the "virtualization" of underlying auth in the SASL challenge.

As a quick workaround, add the permissive exception handling to
sasl object as well.

Message-Id: <20180103102724.1083-1-calle@scylladb.com>

auth/transitional.cc
@@ -141,7 +141,16 @@ public:
                 return _complete || _sasl->is_complete();
             }
             future<::shared_ptr<authenticated_user>> get_authenticated_user() const {
-                return _sasl->get_authenticated_user();
+                return futurize_apply([this] {
+                    return _sasl->get_authenticated_user().handle_exception([](auto ep) {
+                        try {
+                            std::rethrow_exception(ep);
+                        } catch (exceptions::authentication_exception&) {
+                            // return anon user
+                            return make_ready_future<::shared_ptr<authenticated_user>>(::make_shared<authenticated_user>());
+                        }
+                    });
+                });
             }
         private:
             ::shared_ptr<sasl_challenge> _sasl;