scylladb/scylladb

commit 259f6759b4c3335e971d181465a1912e22843e9a

partition_snapshot_reader: Use static_row() to read static_row
Signed-off-by: Duarte Nunes <duarte@scylladb.com>
Message-Id: <20180109162815.5811-2-duarte@scylladb.com>

partition_snapshot_reader.hh
@@ -163,17 +163,7 @@ private:
 
     mutation_fragment_opt read_static_row() {
         _last_entry = position_in_partition(position_in_partition::static_row_tag_t());
-        mutation_fragment_opt sr;
-        for (auto&& v : _snapshot->versions()) {
-            if (!v.partition().static_row().empty()) {
-                if (!sr) {
-                    sr = mutation_fragment(static_row(v.partition().static_row()));
-                } else {
-                    sr->as_mutable_static_row().apply(*_schema, v.partition().static_row());
-                }
-            }
-        }
-        return sr;
+        return mutation_fragment(_snapshot->static_row());
     }
 
     mutation_fragment_opt read_next() {

commit 891c22904b1b80b26aa0c27d71ed1e468de44707

partition_snapshot_reader: Don't push empty static rows
This patch fixes a regression introduced in 259f6759b4, which pushed
static row fragments regardless of them being empty.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>
Message-Id: <20180110222936.23085-1-duarte@scylladb.com>

partition_snapshot_reader.hh
@@ -163,7 +163,11 @@ private:
 
     mutation_fragment_opt read_static_row() {
         _last_entry = position_in_partition(position_in_partition::static_row_tag_t());
-        return mutation_fragment(_snapshot->static_row());
+        auto sr = _snapshot->static_row();
+        if (!sr.empty()) {
+            return mutation_fragment(std::move(sr));
+        }
+        return stdx::nullopt;
     }
 
     mutation_fragment_opt read_next() {