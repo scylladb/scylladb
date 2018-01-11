f78d89968e6e102e055f1136ff1bd67549946474
Jesse Haber-Kucharsky <jhaberku@scylladb.com>
2018-01-11 17:18:40 -0500

    auth/authorizer.hh: Replace documentation

--- auth/authorizer.hh
+++ auth/authorizer.hh
@@ -69,6 +69,9 @@ struct permission_details {
 
 using std::experimental::optional;
 
+///
+/// Abstract interface for authorizing users to access resources.
+///
 class authorizer {
 public:
     virtual ~authorizer() {
@@ -78,89 +81,66 @@ public:
 
     virtual future<> stop() = 0;
 
+    ///
+    /// A fully-qualified (class with package) Java-like name for this implementation.
+    ///
     virtual const sstring& qualified_java_name() const = 0;
 
-    /**
-     * The primary Authorizer method. Returns a set of permissions of a role on a resource.
-     *
-     * @param user Authenticated user requesting authorization.
-     * @param resource Resource for which the authorization is being requested. @see DataResource.
-     * @return Set of permissions of the user on the resource. Should never return empty. Use permission.NONE instead.
-     */
+    ///
+    /// Query for the permissions granted to a role for a particular \ref resource.
+    ///
+    /// The resulting permission set includes permissions obtained transitively through roles granted to this role.
+    ///
     virtual future<permission_set> authorize(service&, sstring role_name, resource) const = 0;
 
-    /**
-     * Grants a set of permissions on a resource to a user.
-     * The opposite of revoke().
-     *
-     * @param performer User who grants the permissions.
-     * @param permissions Set of permissions to grant.
-     * @param to Grantee of the permissions.
-     * @param resource Resource on which to grant the permissions.
-     *
-     * @throws RequestValidationException
-     * @throws RequestExecutionException
-     */
+    ///
+    /// Grant a set of permissions to a user for a particular \ref resource.
+    ///
     virtual future<> grant(const authenticated_user& performer, permission_set, resource, sstring to) = 0;
 
-    /**
-     * Revokes a set of permissions on a resource from a user.
-     * The opposite of grant().
-     *
-     * @param performer User who revokes the permissions.
-     * @param permissions Set of permissions to revoke.
-     * @param from Revokee of the permissions.
-     * @param resource Resource on which to revoke the permissions.
-     *
-     * @throws RequestValidationException
-     * @throws RequestExecutionException
-     */
+    ///
+    /// Revoke a set of permissions from a user for a particular \ref resource.
+    ///
     virtual future<> revoke(const authenticated_user& performer, permission_set, resource, sstring from) = 0;
 
-    /**
-     * Returns a list of permissions on a resource of a user.
-     *
-     * @param performer User who wants to see the permissions.
-     * @param permissions Set of Permission values the user is interested in. The result should only include the matching ones.
-     * @param resource The resource on which permissions are requested. Can be null, in which case permissions on all resources
-     *                 should be returned.
-     * @param of The user whose permissions are requested. Can be null, in which case permissions of every user should be returned.
-     *
-     * @return All of the matching permission that the requesting user is authorized to know about.
-     *
-     * @throws RequestValidationException
-     * @throws RequestExecutionException
-     */
+    ///
+    /// Query for granted permissions.
+    ///
+    /// Only information for permissions in `matching` is included.
+    ///
+    /// If `resource` is empty, then query for permissions on all resources.
+    ///
+    /// If `user` is empty, query for permissions of all users. Otherwise, query for permissions specific to that user.
+    ///
+    /// \returns an exceptional future with \ref exceptions::unauthorized_exception if the performer does not have
+    /// permission to view the user's permissions.
+    ///
     virtual future<std::vector<permission_details>>
-    list(service&, const authenticated_user& performer, permission_set, optional<resource>, optional<sstring>) const = 0;
+    list(
+            service&,
+            const authenticated_user& performer,
+            permission_set matching,
+            optional<resource> resource,
+            optional<sstring> user) const = 0;
 
-    /**
-     * This method is called before deleting a user with DROP USER query so that a new user with the same
-     * name wouldn't inherit permissions of the deleted user in the future.
-     *
-     * @param droppedUser The user to revoke all permissions from.
-     */
+    ///
+    /// Revoke all permissions granted to a particular user.
+    ///
     virtual future<> revoke_all(sstring dropped_user) = 0;
 
-    /**
-     * This method is called after a resource is removed (i.e. keyspace or a table is dropped).
-     *
-     * @param droppedResource The resource to revoke all permissions on.
-     */
+    ///
+    /// Revoke all permissions granted to any user for a particular resource.
+    ///
     virtual future<> revoke_all(resource) = 0;
 
-    /**
-     * Set of resources that should be made inaccessible to users and only accessible internally.
-     *
-     * @return Keyspaces, column families that will be unmodifiable by users; other resources.
-     */
+    ///
+    /// System resources used internally as part of the implementation. These are made inaccessible to users.
+    ///
     virtual const resource_set& protected_resources() = 0;
 
-    /**
-     * Validates configuration of IAuthorizer implementation (if configurable).
-     *
-     * @throws ConfigurationException when there is a configuration error.
-     */
+    ///
+    /// Validate configuration, if applicable.
+    ///
     virtual future<> validate_configuration() const = 0;
 };
 
