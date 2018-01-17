ab6ec571cbd78155f8ca9d9deddd669e14d8fb35
Tomasz Grabiec <tgrabiec@scylladb.com>
2018-01-17 17:11:09 +0100

    test.py: set BOOST_TEST_CATCH_SYSTEM_ERRORS=no
    This will make boost UTF abort execution on SIGABRT rather than trying
    to continue running other test cases. This doesn't work well with
    seastar integration, the suite will hang.
    Message-Id: <1516205469-16378-1-git-send-email-tgrabiec@scylladb.com>

--- test.py
+++ test.py
@@ -182,6 +182,7 @@ if __name__ == "__main__":
     # disable false positive due to new (with_alignment(...)) ...
     env['ASAN_OPTIONS'] = 'alloc_dealloc_mismatch=0'
     env['UBSAN_OPTIONS'] = 'print_stacktrace=1'
+    env['BOOST_TEST_CATCH_SYSTEM_ERRORS'] = 'no'
     for n, test in enumerate(test_to_run):
         path = test[0]
         exec_args = test[2] if len(test) >= 3 else []
