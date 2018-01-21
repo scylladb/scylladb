7d082e6ea7ba2684ba1839bb27fb84fb1800e3de
Piotr Jastrzebski <piotr@scylladb.com>
2018-01-21 18:40:42 +0100

    perf_fast_forward: use flat reader in slice_rows_single_key
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- tests/perf/perf_fast_forward.cc
+++ tests/perf/perf_fast_forward.cc
@@ -319,18 +319,14 @@ static test_result test_slicing_using_restrictions(column_family& cf, int_range 
 
 static test_result slice_rows_single_key(column_family& cf, int offset = 0, int n_read = 1) {
     auto pr = dht::partition_range::make_singular(make_pkey(*cf.schema(), 0));
-    auto rd = mutation_reader_from_flat_mutation_reader(cf.make_reader(cf.schema(), pr, cf.schema()->full_slice(), default_priority_class(), nullptr, streamed_mutation::forwarding::yes));
+    auto rd = cf.make_reader(cf.schema(), pr, cf.schema()->full_slice(), default_priority_class(), nullptr, streamed_mutation::forwarding::yes);
 
     metrics_snapshot before;
-    streamed_mutation_opt smo = rd().get0();
-    assert(smo);
-    streamed_mutation& sm = *smo;
-    sm.fast_forward_to(position_range(
+    assert_partition_start(rd);
+    rd.fast_forward_to(position_range(
         position_in_partition::for_key(clustering_key::from_singular(*cf.schema(), offset)),
         position_in_partition::for_key(clustering_key::from_singular(*cf.schema(), offset + n_read)))).get();
-    uint64_t fragments = consume_all(sm);
-
-    fragments += consume_all(rd);
+    uint64_t fragments = consume_all_with_next_partition(rd);
 
     return {before, fragments};
 }
