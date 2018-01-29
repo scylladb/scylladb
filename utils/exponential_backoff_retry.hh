e0c082616ab9b68957f7b61a55b70acbfc69af0f
Botond Dénes <bdenes@scylladb.com>
2018-01-29 12:37:01 +0200

    exponential_backoff_retry::do_until_value(): fix use-after-move
    The exponential_backoff_retry instance is captured by move and is then
    indirectly moved again as repeat_until_value() moves the lambda its
    passed into its internal state. This caused problems as internal
    lambdas store references to the instance and these references go stale
    after the move.
    To fix this keep hold of the existential_backoff_retry instance in an
    enclosing do_with() to make it safe for internal lambdas to reference
    it.
    
    Indentation will be fixed by the next patch.
    
    Signed-off-by: Botond Dénes <bdenes@scylladb.com>
    Message-Id: <adc49d25a6176756d60e092f3713c0c897732382.1517222195.git.bdenes@scylladb.com>

--- utils/exponential_backoff_retry.hh
+++ utils/exponential_backoff_retry.hh
@@ -77,7 +77,8 @@ public:
         using type_helper = retry_type_helper<std::result_of_t<Func()>>;
 
         auto r = exponential_backoff_retry(base_sleep_time, max_sleep_time);
-        return seastar::repeat_until_value([r = std::move(r), &as, f = std::move(f)] () mutable {
+      return seastar::do_with(std::move(r), [&as, f = std::move(f)] (exponential_backoff_retry& r) mutable {
+        return seastar::repeat_until_value([&r, &as, f = std::move(f)] () mutable {
             return f().then([&] (auto&& opt) -> typename type_helper::future_type {
                 if (opt) {
                     return make_ready_future<typename type_helper::optional_type>(std::move(opt));
@@ -87,5 +88,6 @@ public:
                 });
             });
         });
+      });
     }
 };
