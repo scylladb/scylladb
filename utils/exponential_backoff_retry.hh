12b152041580283556c9e84046ece89b7c3d2fd8
Botond Dénes <bdenes@scylladb.com>
2018-01-29 12:37:02 +0200

    exponential_backoff_retry::do_until_value(): restore indentation
    Deferred from previous patch.
    
    Signed-off-by: Botond Dénes <bdenes@scylladb.com>
    Message-Id: <a10053f6c0ed8a24a74e51f1df4e9a5acf59922d.1517222195.git.bdenes@scylladb.com>

--- utils/exponential_backoff_retry.hh
+++ utils/exponential_backoff_retry.hh
@@ -77,17 +77,17 @@ public:
         using type_helper = retry_type_helper<std::result_of_t<Func()>>;
 
         auto r = exponential_backoff_retry(base_sleep_time, max_sleep_time);
-      return seastar::do_with(std::move(r), [&as, f = std::move(f)] (exponential_backoff_retry& r) mutable {
-        return seastar::repeat_until_value([&r, &as, f = std::move(f)] () mutable {
-            return f().then([&] (auto&& opt) -> typename type_helper::future_type {
-                if (opt) {
-                    return make_ready_future<typename type_helper::optional_type>(std::move(opt));
-                }
-                return r.retry(as).then([] () -> typename type_helper::optional_type {
-                    return { };
+        return seastar::do_with(std::move(r), [&as, f = std::move(f)] (exponential_backoff_retry& r) mutable {
+            return seastar::repeat_until_value([&r, &as, f = std::move(f)] () mutable {
+                return f().then([&] (auto&& opt) -> typename type_helper::future_type {
+                    if (opt) {
+                        return make_ready_future<typename type_helper::optional_type>(std::move(opt));
+                    }
+                    return r.retry(as).then([] () -> typename type_helper::optional_type {
+                        return { };
+                    });
                 });
             });
         });
-      });
     }
 };
