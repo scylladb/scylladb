449f9af0db59789a5ad4dc4370588ea17893c664
Amnon Heiman <amnon@scylladb.com>
2018-02-07 17:33:06 +0200

    API: Use stream_range_as_array to return token endpoints
    The token_to_endpoint map can get big that trying to convert it to a
    vector will cause large allocation warning.
    
    This patch replace the implementation, so the return json array will be
    created directly from the map by using stream_range_as_array helper
    function.
    
    Fixes #3185
    
    Message-Id: <20180207153306.30921-1-amnon@scylladb.com>

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -94,9 +94,12 @@ void set_storage_service(http_context& ctx, routes& r) {
     });
 
     ss::get_token_endpoint.set(r, [] (std::unique_ptr<request> req) {
-        auto token_to_ep = service::get_local_storage_service().get_token_to_endpoint_map();
-        std::vector<storage_service_json::mapper> res;
-        return make_ready_future<json::json_return_type>(stream_object(std::move(map_to_key_value(token_to_ep, res))));
+        return make_ready_future<json::json_return_type>(stream_range_as_array(service::get_local_storage_service().get_token_to_endpoint_map(), [](const auto& i) {
+            storage_service_json::mapper val;
+            val.key = boost::lexical_cast<std::string>(i.first);
+            val.value = boost::lexical_cast<std::string>(i.second);
+            return val;
+        }));
     });
 
     ss::get_leaving_nodes.set(r, [](const_req req) {
