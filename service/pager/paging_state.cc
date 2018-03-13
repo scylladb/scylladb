nyh/scylla

Add query UUID to paging state
This patch adds to the "paging_state", the opaque cookie that clients are
supposed to provide when asking for the next page on a paged query, a
unique id field. This new field will be used to tell that a new request
for a page really continues the previous page, and doesn't just by chance
start at the same position the previous page stopped.

Beyond ensuring that unrelated queries can be told apart, we will (in the
next patches) also use this UUID to recognize a page query which got
repeated (e.g., because a coordinator failed so the client sent the query
again). We will change the UUID after every page, so that if we already
processed a page we will have changed the current UUID so that if the old
UUID is used again, it will be recognize as not fitting the current
position of the stream.

We need to support setups with mixed versions - a client may get a paging
state from a coordinator running a new version of Scylla and send it to
a different coordinator running an old version - or vice versa. So the new
uuid field is set up to have a default uuid of UUID() (a recognizable
invalid uuid 0), so new versions receiving no uuid from an old version will
set this invalid uuid, and old versions receiving a uuid from a new version
will simply ignore it.

Signed-off-by: Nadav Har'El <nyh@scylladb.com>

service/pager/paging_state.cc
@@ -44,15 +44,17 @@
 #include "paging_state.hh"
 #include "core/simple-stream.hh"
 #include "idl/keys.dist.hh"
+#include "idl/uuid.dist.hh"
 #include "idl/paging_state.dist.hh"
 #include "serializer_impl.hh"
 #include "idl/keys.dist.impl.hh"
+#include "idl/uuid.dist.impl.hh"
 #include "idl/paging_state.dist.impl.hh"
 #include "message/messaging_service.hh"
 
 service::pager::paging_state::paging_state(partition_key pk, std::experimental::optional<clustering_key> ck,
-        uint32_t rem)
-        : _partition_key(std::move(pk)), _clustering_key(std::move(ck)), _remaining(rem) {
+        uint32_t rem, utils::UUID reader_recall_uuid)
+        : _partition_key(std::move(pk)), _clustering_key(std::move(ck)), _remaining(rem), _reader_recall_uuid(reader_recall_uuid) {
 }
 
 ::shared_ptr<service::pager::paging_state> service::pager::paging_state::deserialize(

scylladb/scylladb

service/pager/paging_state.cc
@@ -44,15 +44,17 @@
 #include "paging_state.hh"
 #include "core/simple-stream.hh"
 #include "idl/keys.dist.hh"
+#include "idl/uuid.dist.hh"
 #include "idl/paging_state.dist.hh"
 #include "serializer_impl.hh"
 #include "idl/keys.dist.impl.hh"
+#include "idl/uuid.dist.impl.hh"
 #include "idl/paging_state.dist.impl.hh"
 #include "message/messaging_service.hh"
 
 service::pager::paging_state::paging_state(partition_key pk, std::experimental::optional<clustering_key> ck,
-        uint32_t rem)
-        : _partition_key(std::move(pk)), _clustering_key(std::move(ck)), _remaining(rem) {
+        uint32_t rem, utils::UUID query_uuid)
+        : _partition_key(std::move(pk)), _clustering_key(std::move(ck)), _remaining(rem), _query_uuid(query_uuid) {
 }
 
 ::shared_ptr<service::pager::paging_state> service::pager::paging_state::deserialize(