ad7b1321883fbc6c7decd189d3cb0f8811c3e20f
Asias He <asias@scylladb.com>
2018-03-14 09:51:03 +0800

    Revert "streaming: Do not abort session too early in idle detection"
    This reverts commit f792c78c96e4373a898fccc7457e78b077557c46.
    
    With the "Use range_streamer everywhere" (7217b7ab369e81) series,
    all the user of streaming now do streaming with relative small ranges
    and can retry streaming at higher level.
    
    Reduce the time-to-recover from 5 hours to 10 minutes per stream session.
    
    Even if the 10 minutes idle detection might cause higher false positive,
    it is fine, since we can retry the "small" stream session anyway. In the
    long term, we should replace the whole idle detection logic with
    whenever the stream initiator goes away, the stream slave goes away.
    
    Message-Id: <75f308baf25a520d42d884c7ef36f1aecb8a64b0.1520992219.git.asias@scylladb.com>

--- streaming/stream_session.hh
+++ streaming/stream_session.hh
@@ -175,10 +175,10 @@ private:
     bool _complete_sent = false;
     bool _received_failed_complete_message = false;
 
-    // If the session is idle for 300 minutes, close the session
-    std::chrono::seconds _keep_alive_timeout{60 * 300};
-    // Check every 10 minutes
-    std::chrono::seconds _keep_alive_interval{60 * 10};
+    // If the session is idle for 10 minutes, close the session
+    std::chrono::seconds _keep_alive_timeout{60 * 10};
+    // Check every 1 minutes
+    std::chrono::seconds _keep_alive_interval{60};
     timer<lowres_clock> _keep_alive;
     stream_bytes _last_stream_bytes;
     lowres_clock::time_point _last_stream_progress;
