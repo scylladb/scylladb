duarten/scylla

tests: Add unit test for build_progress_virtual_reader
Signed-off-by: Duarte Nunes <duarte@scylladb.com>

tests/virtual_reader_test.cc
@@ -33,12 +33,14 @@
 #include "tests/test_services.hh"
 
 #include "db/size_estimates_virtual_reader.hh"
+#include "db/system_keyspace.hh"
 #include "core/future-util.hh"
 #include "cql3/query_processor.hh"
 #include "cql3/untyped_result_set.hh"
+#include "dht/i_partitioner.hh"
 #include "transport/messages/result_message.hh"
 
-SEASTAR_TEST_CASE(test_query_virtual_table) {
+SEASTAR_TEST_CASE(test_query_size_estimates_virtual_table) {
     return do_with_cql_env([] (auto& e) {
         auto ranges = db::size_estimates::size_estimates_mutation_reader::get_local_ranges().get0();
         auto start_token1 = utf8_type->to_string(ranges[3].start);
@@ -213,5 +215,46 @@ SEASTAR_TEST_CASE(test_query_virtual_table) {
                 BOOST_REQUIRE_EQUAL(rs->size(), 259);
             });
         }).discard_result();
+    });
+}
+
+SEASTAR_TEST_CASE(test_query_view_built_progress_virtual_table) {
+    return do_with_cql_env_thread([] (auto& e) {
+        auto rand = [] { return dht::global_partitioner().get_random_token(); };
+        db::system_keyspace::register_view_for_building("ks", "v1", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v2", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v3", rand()).get();
+        db::system_keyspace::update_view_build_progress("ks", "v3", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v4", rand()).get();
+        db::system_keyspace::update_view_build_progress("ks", "v4", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v5", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v6", rand()).get();
+        db::system_keyspace::remove_view_build_progress_across_all_shards("ks", "v5").get();
+        db::system_keyspace::remove_view_build_progress_across_all_shards("ks", "v6").get();
+        auto &qp = e.local_qp();
+        auto rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 4);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 4);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress limit 1").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 1);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name = 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 1);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name > 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 2);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name >= 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 3);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name  < 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 1);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name  <= 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 2);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name in ('v1', 'v2', 'v3')").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 3);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name >= 'v2' and view_name  <= 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 1);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name >= 'v2' and view_name  <= 'v3'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 2);
+        rs = qp.execute_internal("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name > 'v1' and view_name  < 'v2'").get0();
+        BOOST_REQUIRE_EQUAL(rs->size(), 0);
     });
 }

scylladb/scylladb

tests/virtual_reader_test.cc
@@ -33,12 +33,14 @@
 #include "tests/test_services.hh"
 
 #include "db/size_estimates_virtual_reader.hh"
+#include "db/system_keyspace.hh"
 #include "core/future-util.hh"
 #include "cql3/query_processor.hh"
 #include "cql3/untyped_result_set.hh"
+#include "dht/i_partitioner.hh"
 #include "transport/messages/result_message.hh"
 
-SEASTAR_TEST_CASE(test_query_virtual_table) {
+SEASTAR_TEST_CASE(test_query_size_estimates_virtual_table) {
     return do_with_cql_env([] (auto& e) {
         auto ranges = db::size_estimates::size_estimates_mutation_reader::get_local_ranges().get0();
         auto start_token1 = utf8_type->to_string(ranges[3].start);
@@ -213,5 +215,52 @@ SEASTAR_TEST_CASE(test_query_virtual_table) {
                 BOOST_REQUIRE_EQUAL(rs->size(), 259);
             });
         }).discard_result();
+    });
+}
+
+SEASTAR_TEST_CASE(test_query_view_built_progress_virtual_table) {
+    return do_with_cql_env_thread([] (cql_test_env& e) {
+        auto rand = [] { return dht::global_partitioner().get_random_token(); };
+        auto next_token = rand();
+        auto next_token_str = dht::global_partitioner().to_sstring(next_token);
+        db::system_keyspace::register_view_for_building("ks", "v1", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v2", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v3", rand()).get();
+        db::system_keyspace::update_view_build_progress("ks", "v3", next_token).get();
+        db::system_keyspace::register_view_for_building("ks", "v4", rand()).get();
+        db::system_keyspace::update_view_build_progress("ks", "v4", next_token).get();
+        db::system_keyspace::register_view_for_building("ks", "v5", rand()).get();
+        db::system_keyspace::register_view_for_building("ks", "v6", rand()).get();
+        db::system_keyspace::remove_view_build_progress_across_all_shards("ks", "v5").get();
+        db::system_keyspace::remove_view_build_progress_across_all_shards("ks", "v6").get();
+        auto rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks'").get0();
+        assert_that(rs).is_rows().with_rows_ignore_order({
+                { {utf8_type->decompose(sstring("ks"))}, {utf8_type->decompose(sstring("v1"))}, {int32_type->decompose(0)}, { } },
+                { {utf8_type->decompose(sstring("ks"))}, {utf8_type->decompose(sstring("v2"))}, {int32_type->decompose(0)}, { } },
+                { {utf8_type->decompose(sstring("ks"))}, {utf8_type->decompose(sstring("v3"))}, {int32_type->decompose(0)}, {utf8_type->decompose(next_token_str)} },
+                { {utf8_type->decompose(sstring("ks"))}, {utf8_type->decompose(sstring("v4"))}, {int32_type->decompose(0)}, {utf8_type->decompose(next_token_str)} }
+        });
+        rs = e.execute_cql("select * from system.views_builds_in_progress").get0();
+        assert_that(rs).is_rows().with_size(4);
+        rs = e.execute_cql("select * from system.views_builds_in_progress limit 1").get0();
+        assert_that(rs).is_rows().with_size(1);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name = 'v2'").get0();
+        assert_that(rs).is_rows().with_size(1);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name > 'v2'").get0();
+        assert_that(rs).is_rows().with_size(2);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name >= 'v2'").get0();
+        assert_that(rs).is_rows().with_size(3);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name  < 'v2'").get0();
+        assert_that(rs).is_rows().with_size(1);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name  <= 'v2'").get0();
+        assert_that(rs).is_rows().with_size(2);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name in ('v1', 'v2', 'v3')").get0();
+        assert_that(rs).is_rows().with_size(3);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name >= 'v2' and view_name  <= 'v2'").get0();
+        assert_that(rs).is_rows().with_size(1);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name >= 'v2' and view_name  <= 'v3'").get0();
+        assert_that(rs).is_rows().with_size(2);
+        rs = e.execute_cql("select * from system.views_builds_in_progress where keyspace_name = 'ks' and view_name > 'v1' and view_name  < 'v2'").get0();
+        assert_that(rs).is_rows().with_size(0);
     });
 }