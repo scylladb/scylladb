fdad8eba97981aaacfc6e11e54c895cd8e3fba2b
Piotr Jastrzebski <piotr@scylladb.com>
2018-04-16 11:53:28 +0200

    buffer_input_stream: make it possible to specify chunk size
    This will allow to force input stream to return its data
    in chunks of a specified size.
    
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- utils/buffer_input_stream.hh
+++ utils/buffer_input_stream.hh
@@ -23,6 +23,7 @@
 
 #include <seastar/core/iostream.hh>
 #include <seastar/core/temporary_buffer.hh>
+#include <seastar/util/noncopyable_function.hh>
 #include "seastarx.hh"
 
 /// \brief Creates an input_stream to read from a supplied buffer
@@ -30,3 +31,11 @@
 /// \param buf Buffer to return from the stream while reading
 /// \return resulting input stream
 input_stream<char> make_buffer_input_stream(temporary_buffer<char>&& buf);
+
+/// \brief Creates an input_stream to read from a supplied buffer
+///
+/// \param buf Buffer to return from the stream while reading
+/// \param limit_generator Generates limits of chunk sizes
+/// \return resulting input stream
+input_stream<char> make_buffer_input_stream(temporary_buffer<char>&& buf,
+                                            seastar::noncopyable_function<size_t()>&& limit_generator);
