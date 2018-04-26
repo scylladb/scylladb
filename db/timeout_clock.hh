2be75bdfc91d74e4f18ef015c2814471fa702194
Duarte Nunes <duarte@scylladb.com>
2018-04-26 14:44:55 +0100

    db/timeout_clock: Properly scope type names
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>
    Message-Id: <20180426134457.21290-1-duarte@scylladb.com>

--- db/timeout_clock.hh
+++ db/timeout_clock.hh
@@ -27,6 +27,6 @@
 
 namespace db {
 using timeout_clock = seastar::lowres_clock;
-using timeout_semaphore = basic_semaphore<default_timeout_exception_factory, timeout_clock>;
+using timeout_semaphore = seastar::basic_semaphore<seastar::default_timeout_exception_factory, timeout_clock>;
 static constexpr timeout_clock::time_point no_timeout = timeout_clock::time_point::max();
 }
