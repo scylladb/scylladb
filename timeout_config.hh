b6d74b1c19ebb53a0dddaae6b2df2fa9dcde8813
Avi Kivity <avi@scylladb.com>
2018-04-29 19:52:40 +0300

    timeout_config: introduce timeout configuration
    Different request types have different timeouts (for example,
    read requests have shorter timeouts than truncate requests), and
    also different request sources have different timeouts (for example,
    an internal local query wants infinite timeout while a user query
    has a user-defined timeout).
    
    To allow for this, define two types: timeout_config represents the
    timeout configuration for a source (e.g. user), while
    timeout_config_selector represents the request type, and is used
    to select a timeout within a timeout configuration. The latter is
    implemented as a pointer-to-member.
    
    Also introduce an infinite timeout configuration for internal
    queries.

---
+++ timeout_config.hh
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018 ScyllaDB
+ *
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "db/timeout_clock.hh"
+
+struct timeout_config {
+    db::timeout_clock::duration read_timeout;
+    db::timeout_clock::duration write_timeout;
+    db::timeout_clock::duration range_read_timeout;
+    db::timeout_clock::duration counter_write_timeout;
+    db::timeout_clock::duration truncate_timeout;
+    db::timeout_clock::duration cas_timeout;
+    db::timeout_clock::duration other_timeout;
+};
+
+using timeout_config_selector = db::timeout_clock::duration (timeout_config::*);
+
+extern const timeout_config infinite_timeout_config;
