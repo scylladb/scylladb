pdziepak/scylla

tests/perf: add microbenchmarks for basic row operations

configure.py
@@ -300,7 +300,8 @@ scylla_tests = [
 ]
 
 perf_tests = [
-    'tests/perf/perf_mutation_readers'
+    'tests/perf/perf_mutation_readers',
+    'tests/perf/perf_partition_data',
 ]
 
 apps = [

scylladb/scylladb

configure.py
@@ -302,7 +302,8 @@ scylla_tests = [
 ]
 
 perf_tests = [
-    'tests/perf/perf_mutation_readers'
+    'tests/perf/perf_mutation_readers',
+    'tests/perf/perf_mutation_fragment',
 ]
 
 apps = [