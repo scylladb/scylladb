aa126639c0c2b2c3a33fffbf62c8b41847cfb015
Piotr Jastrzebski <piotr@scylladb.com>
2018-05-09 10:12:26 +0200

    Move random_access_reader to separate header
    It will be used not only in sstables.cc but also
    in helpers for reading sstables in M format.
    
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -67,6 +67,7 @@
 #include "unimplemented.hh"
 #include "vint-serialization.hh"
 #include "db/large_partition_handler.hh"
+#include "sstables/random_access_reader.hh"
 
 thread_local disk_error_signal_type sstable_read_error;
 thread_local disk_error_signal_type sstable_write_error;
@@ -129,62 +130,6 @@ future<> await_background_jobs_on_all_shards() {
         return await_background_jobs();
     });
 }
-
-class random_access_reader {
-    std::unique_ptr<input_stream<char>> _in;
-    seastar::gate _close_gate;
-protected:
-    virtual input_stream<char> open_at(uint64_t pos) = 0;
-public:
-    future<temporary_buffer<char>> read_exactly(size_t n) {
-        return _in->read_exactly(n);
-    }
-    void seek(uint64_t pos) {
-        if (_in) {
-            seastar::with_gate(_close_gate, [in = std::move(_in)] () mutable {
-                auto fut = in->close();
-                return fut.then([in = std::move(in)] {});
-            });
-        }
-        _in = std::make_unique<input_stream<char>>(open_at(pos));
-    }
-    bool eof() { return _in->eof(); }
-    virtual future<> close() {
-        return _close_gate.close().then([this] {
-            return _in->close();
-        });
-    }
-    virtual ~random_access_reader() { }
-};
-
-class file_random_access_reader : public random_access_reader {
-    file _file;
-    uint64_t _file_size;
-    size_t _buffer_size;
-    unsigned _read_ahead;
-public:
-    virtual input_stream<char> open_at(uint64_t pos) override {
-        auto len = _file_size - pos;
-        file_input_stream_options options;
-        options.buffer_size = _buffer_size;
-        options.read_ahead = _read_ahead;
-
-        return make_file_input_stream(_file, pos, len, std::move(options));
-    }
-    explicit file_random_access_reader(file f, uint64_t file_size, size_t buffer_size = 8192, unsigned read_ahead = 4)
-        : _file(std::move(f)), _file_size(file_size), _buffer_size(buffer_size), _read_ahead(read_ahead)
-    {
-        seek(0);
-    }
-    virtual future<> close() override {
-        return random_access_reader::close().finally([this] {
-            return _file.close().handle_exception([save = _file] (auto ep) {
-                sstlog.warn("sstable close failed: {}", ep);
-                general_disk_error();
-            });
-        });
-    }
-};
 
 shared_sstable
 make_sstable(schema_ptr schema, sstring dir, int64_t generation, sstable_version_types v, sstable_format_types f, gc_clock::time_point now,
