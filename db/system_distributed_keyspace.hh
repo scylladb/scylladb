duarten/scylla

db: Introduce system_distributed_keyspace
This patch introduces a distributed system keyspace, used to hold
system tables that need to be replicated across a set of replicas
(that is, can't use the LocalStrategy).

In following patches, we will use this keyspace to hold a table
containing view building status updates for each node, used to support
range movements and a new nodetool command.

Signed-off-by: Duarte Nunes <duarte@scylladb.com>

db/system_distributed_keyspace.hh
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2018 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "bytes.hh"
+#include "cql3/query_processor.hh"
+#include "schema.hh"
+#include "service/migration_manager.hh"
+#include "utils/UUID.hh"
+
+#include <seastar/core/future.hh>
+#include <seastar/core/sstring.hh>
+
+#include <unordered_map>
+
+namespace db {
+
+class system_distributed_keyspace {
+public:
+    static constexpr auto NAME = "system_distributed";
+    static constexpr auto VIEW_BUILD_STATUS = "view_build_status";
+
+private:
+    cql3::query_processor& _qp;
+    service::migration_manager& _mm;
+
+public:
+    system_distributed_keyspace(cql3::query_processor&, service::migration_manager&);
+
+    future<> start();
+    future<> stop();
+
+    future<std::unordered_map<utils::UUID, sstring>> view_status(sstring ks_name, sstring view_name) const;
+    future<> start_view_build(sstring ks_name, sstring view_name) const;
+    future<> view_built(sstring ks_name, sstring view_name) const;
+    future<> remove_view(sstring ks_name, sstring view_name) const;
+};
+
+}

scylladb/scylladb

Fixes #3237
(cherry picked from commit 78b232d98f2438d6b5c880bd2e040a40de4e758e)

db/system_distributed_keyspace.hh
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2018 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "bytes.hh"
+#include "cql3/query_processor.hh"
+#include "schema.hh"
+#include "service/migration_manager.hh"
+#include "utils/UUID.hh"
+
+#include <seastar/core/future.hh>
+#include <seastar/core/sstring.hh>
+
+#include <unordered_map>
+
+namespace db {
+
+class system_distributed_keyspace {
+public:
+    static constexpr auto NAME = "system_distributed";
+    static constexpr auto VIEW_BUILD_STATUS = "view_build_status";
+
+private:
+    cql3::query_processor& _qp;
+    service::migration_manager& _mm;
+
+public:
+    system_distributed_keyspace(cql3::query_processor&, service::migration_manager&);
+
+    future<> start();
+    future<> stop();
+
+    future<std::unordered_map<utils::UUID, sstring>> view_status(sstring ks_name, sstring view_name) const;
+    future<> start_view_build(sstring ks_name, sstring view_name) const;
+    future<> finish_view_build(sstring ks_name, sstring view_name) const;
+    future<> remove_view(sstring ks_name, sstring view_name) const;
+};
+
+}