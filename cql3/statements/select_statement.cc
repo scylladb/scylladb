d145a6cb97dc36f9038022ccec84fd42ee3194ad
Nadav Har'El <nyh@scylladb.com>
2018-05-24 15:46:12 +0300

    select_statement.cc: fix variable lifetime errors
    do_with() provides code a *reference* to an object which will be kept
    alive. It is a mistake to make a copy of this object or of parts of it,
    because then the lifetime of this copy will have to be maintained as well.
    
    In particular, it is a mistake to do do_with(..., [] (auto x) { ... }) -
    note how "auto x" appears instead of the correct "auto& x". This causes
    the object to be copied, and its lifetime not maintained.
    
    This patch fixes several cases where this rule was broken in
    select_statement.cc. I could not reproduce actual crashes caused by
    these mistakes, but in theory they could have happened.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    (cherry picked from commit 7dc9b77682b2299f246048e912ee43b578222610)

--- cql3/statements/select_statement.cc
+++ cql3/statements/select_statement.cc
@@ -322,10 +322,10 @@ select_statement::execute(distributed<service::storage_proxy>& proxy,
     // is no way to tell which of these rows belong to the query result before
     // doing post-query ordering.
     if (needs_post_query_ordering() && _limit) {
-        return do_with(std::forward<dht::partition_range_vector>(partition_ranges), [this, &proxy, &state, &options, cmd](auto prs) {
+        return do_with(std::forward<dht::partition_range_vector>(partition_ranges), [this, &proxy, &state, &options, cmd](auto& prs) {
             assert(cmd->partition_limit == query::max_partitions);
             query::result_merger merger(cmd->row_limit * prs.size(), query::max_partitions);
-            return map_reduce(prs.begin(), prs.end(), [this, &proxy, &state, &options, cmd] (auto pr) {
+            return map_reduce(prs.begin(), prs.end(), [this, &proxy, &state, &options, cmd] (auto& pr) {
                 dht::partition_range_vector prange { pr };
                 auto command = ::make_lw_shared<query::read_command>(*cmd);
                 return proxy.local().query(_schema,
@@ -367,10 +367,10 @@ select_statement::execute_internal(distributed<service::storage_proxy>& proxy,
     ++_stats.reads;
 
     if (needs_post_query_ordering() && _limit) {
-        return do_with(std::move(partition_ranges), [this, &proxy, &state, command] (auto prs) {
+        return do_with(std::move(partition_ranges), [this, &proxy, &state, command] (auto& prs) {
             assert(command->partition_limit == query::max_partitions);
             query::result_merger merger(command->row_limit * prs.size(), query::max_partitions);
-            return map_reduce(prs.begin(), prs.end(), [this, &proxy, &state, command] (auto pr) {
+            return map_reduce(prs.begin(), prs.end(), [this, &proxy, &state, command] (auto& pr) {
                 dht::partition_range_vector prange { pr };
                 auto cmd = ::make_lw_shared<query::read_command>(*command);
                 return proxy.local().query(_schema, cmd, std::move(prange), db::consistency_level::ONE, state.get_trace_state(),
