tgrabiec/scylla

tests: Improve perf_row_cache_update
We now test more kinds of workloads:
 - small partitions with no clustering key
 - large partition with lots of small rows
 - large partition with lots of range tombstones

We also collect statistics about scheduling latency induced by cache
update.

Example output:

Small partitions, no overwrites:
update: 356.809113 [ms], stall: {ticks: 396, min: 0.006867 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.358102 [ms]}, cache: 257/257 [MB] LSA: 257/257 [MB] std free: 83 [MB]
update: 337.542999 [ms], stall: {ticks: 373, min: 0.001598 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.358102 [ms]}, cache: 514/514 [MB] LSA: 514/514 [MB] std free: 83 [MB]
update: 383.485291 [ms], stall: {ticks: 425, min: 0.001598 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.131752 [ms]}, cache: 771/788 [MB] LSA: 771/788 [MB] std free: 83 [MB]
update: 574.968811 [ms], stall: {ticks: 634, min: 0.001598 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.629722 [ms], max: 1.955666 [ms]}, cache: 879/917 [MB] LSA: 879/917 [MB] std free: 83 [MB]
update: 411.541138 [ms], stall: {ticks: 455, min: 0.001598 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.358102 [ms]}, cache: 787/835 [MB] LSA: 787/835 [MB] std free: 83 [MB]
update: 368.491211 [ms], stall: {ticks: 408, min: 0.001332 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.131752 [ms]}, cache: 750/790 [MB] LSA: 750/790 [MB] std free: 83 [MB]
update: 343.671967 [ms], stall: {ticks: 380, min: 0.001598 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.131752 [ms]}, cache: 734/769 [MB] LSA: 734/769 [MB] std free: 83 [MB]
update: 320.277283 [ms], stall: {ticks: 357, min: 0.001598 [ms], 50%: 1.131752 [ms], 90%: 1.131752 [ms], 99%: 1.131752 [ms], max: 1.131752 [ms]}, cache: 724/753 [MB] LSA: 724/753 [MB] std free: 83 [MB]
update: 310.583282 [ms], stall: {ticks: 344, m

tests/perf_row_cache_update.cc
@@ -19,10 +19,12 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <chrono>
 #include <core/distributed.hh>
 #include <core/app-template.hh>
 #include <core/sstring.hh>
 #include <core/thread.hh>
+#include <seastar/core/weak_ptr.hh>
 
 #include "utils/managed_bytes.hh"
 #include "utils/logalloc.hh"
@@ -32,68 +34,199 @@
 #include "memtable.hh"
 #include "tests/perf/perf.hh"
 
-static
-dht::decorated_key new_key(schema_ptr s) {
-    static thread_local int next = 0;
-    return dht::global_partitioner().decorate_key(*s,
-        partition_key::from_single_value(*s, to_bytes(sprint("key%d", next++))));
+static const int update_iterations = 16;
+static const int cell_size = 128;
+static bool cancelled = false;
+
+template<typename Func>
+auto duration_in_seconds(Func&& f) {
+    using clk = std::chrono::steady_clock;
+    auto start = clk::now();
+    f();
+    auto end = clk::now();
+    return std::chrono::duration_cast<std::chrono::duration<float>>(end - start);
 }
 
-static
-clustering_key new_ckey(schema_ptr s) {
-    static thread_local int next = 0;
-    return clustering_key::from_single_value(*s, to_bytes(sprint("ckey%d", next++)));
+class scheduling_latency_measurer : public weakly_referencable<scheduling_latency_measurer> {
+    using clk = std::chrono::steady_clock;
+    clk::time_point _last = clk::now();
+    utils::estimated_histogram _hist{300};
+    bool _stop = false;
+private:
+    void schedule_tick();
+    void tick() {
+        auto old = _last;
+        _last = clk::now();
+        auto latency = _last - old;
+        _hist.add(latency.count());
+        if (!_stop) {
+            schedule_tick();
+        }
+    }
+public:
+    void start() {
+        schedule_tick();
+    }
+    void stop() {
+        _stop = true;
+        later().get(); // so that the last scheduled tick is counted
+    }
+    const utils::estimated_histogram& histogram() const {
+        return _hist;
+    }
+};
+
+void scheduling_latency_measurer::schedule_tick() {
+    seastar::schedule(make_task(default_scheduling_group(), [self = weak_from_this()] () mutable {
+        if (self) {
+            self->tick();
+        }
+    }));
+}
+
+std::ostream& operator<<(std::ostream& out, const scheduling_latency_measurer& slm) {
+    auto to_ms = [] (int64_t nanos) {
+        return float(nanos) / 1e6;
+    };
+    return out << sprint("{ticks: %d, "
+                         "min: %.6f [ms], "
+                         "50%%: %.6f [ms], "
+                         "90%%: %.6f [ms], "
+                         "99%%: %.6f [ms], "
+                         "max: %.6f [ms]}",
+        slm.histogram().count(),
+        to_ms(slm.histogram().min()),
+        to_ms(slm.histogram().percentile(0.5)),
+        to_ms(slm.histogram().percentile(0.9)),
+        to_ms(slm.histogram().percentile(0.99)),
+        to_ms(slm.histogram().max()));
+}
+
+template<typename MutationGenerator>
+void run_test(const sstring& name, schema_ptr s, MutationGenerator&& gen) {
+    cache_tracker tracker;
+    row_cache cache(s, make_empty_snapshot_source(), tracker, is_continuous::yes);
+
+    size_t memtable_size = seastar::memory::stats().total_memory() / 4;
+
+    std::cout << name << ":\n";
+
+    for (int i = 0; i < update_iterations; ++i) {
+        auto mt = make_lw_shared<memtable>(s);
+        while (mt->occupancy().total_space() < memtable_size) {
+            auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+                data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+            mutation m = gen();
+            mt->apply(m);
+            if (cancelled) {
+                return;
+            }
+        }
+
+        scheduling_latency_measurer slm;
+        slm.start();
+        auto d = duration_in_seconds([&] {
+            cache.update([] {}, *mt).get();
+        });
+        slm.stop();
+
+        auto MB = 1024 * 1024;
+        std::cout << sprint("update: %.6f [ms], stall: %s, cache: %d/%d [MB] LSA: %d/%d [MB] std free: %d [MB]\n",
+            d.count() * 1000,
+            slm,
+            tracker.region().occupancy().used_space() / MB,
+            tracker.region().occupancy().total_space() / MB,
+            logalloc::shard_tracker().region_occupancy().used_space() / MB,
+            logalloc::shard_tracker().region_occupancy().total_space() / MB,
+            seastar::memory::stats().free_memory() / MB);
+    }
+
+    auto d = duration_in_seconds([&] {
+        cache.invalidate([] {}).get();
+    });
+
+    std::cout << sprint("invalidation: %.6f [ms]", d.count() * 1000) << "\n";
+}
+
+void test_small_partitions() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    run_test("Small partitions, no overwrites", s, [&] {
+        auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+            data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+        m.set_clustered_cell(clustering_key::make_empty(), "v1", val, api::new_timestamp());
+        m.set_clustered_cell(clustering_key::make_empty(), "v2", val, api::new_timestamp());
+        m.set_clustered_cell(clustering_key::make_empty(), "v3", val, api::new_timestamp());
+        return m;
+    });
+}
+
+void test_partition_with_lots_of_small_rows() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("ck", reversed_type_impl::get_instance(int32_type), column_kind::clustering_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+        data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+    int ck_idx = 0;
+
+    run_test("Large partition, lots of small rows", s, [&] {
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+        auto ck = clustering_key::from_single_value(*s, data_value(ck_idx++).serialize());
+        m.set_clustered_cell(ck, "v1", val, api::new_timestamp());
+        m.set_clustered_cell(ck, "v2", val, api::new_timestamp());
+        m.set_clustered_cell(ck, "v3", val, api::new_timestamp());
+        return m;
+    });
+}
+
+void test_partition_with_lots_of_range_tombstones() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("ck", reversed_type_impl::get_instance(int32_type), column_kind::clustering_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+        data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+    int ck_idx = 0;
+
+    run_test("Large partition, lots of range tombstones", s, [&] {
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+        auto ck = clustering_key::from_single_value(*s, data_value(ck_idx++).serialize());
+        auto r = query::clustering_range::make({ck}, {ck});
+        tombstone tomb(api::new_timestamp(), gc_clock::now());
+        m.partition().apply_row_tombstone(*s, range_tombstone(bound_view::from_range_start(r), bound_view::from_range_end(r), tomb));
+        return m;
+    });
 }
 
 int main(int argc, char** argv) {
-    namespace bpo = boost::program_options;
     app_template app;
-    app.add_options()
-        ("debug", "enable debug logging")
-        ("partitions", bpo::value<unsigned>()->default_value(128), "number of partitions per memtable")
-        ("cell-size", bpo::value<unsigned>()->default_value(1024), "cell size in bytes")
-        ("rows", bpo::value<unsigned>()->default_value(128), "row count per partition")
-        ("no-cache", "do not update cache");
-
     return app.run(argc, argv, [&app] {
-        if (app.configuration().count("debug")) {
-            logging::logger_registry().set_all_loggers_level(logging::log_level::debug);
-        }
-
         return seastar::async([&] {
-            auto s = schema_builder("ks", "cf")
-                .with_column("pk", bytes_type, column_kind::partition_key)
-                .with_column("ck", bytes_type, column_kind::clustering_key)
-                .with_column("v", bytes_type, column_kind::regular_column)
-                .build();
-
-            cache_tracker tracker;
-            row_cache cache(s, make_empty_snapshot_source(), tracker);
-
-            size_t partitions = app.configuration()["partitions"].as<unsigned>();
-            size_t cell_size = app.configuration()["cell-size"].as<unsigned>();
-            size_t row_count = app.configuration()["rows"].as<unsigned>();
-            bool update_cache = !app.configuration().count("no-cache");
-
-            std::vector<mutation> mutations;
-            for (unsigned i = 0; i < partitions; ++i) {
-                mutation m(s, new_key(s));
-                for (size_t j = 0; j < row_count; j++) {
-                    m.set_clustered_cell(new_ckey(s), "v", data_value(bytes(bytes::initialized_later(), cell_size)), 2);
-                }
-                mutations.emplace_back(std::move(m));
-            }
-
-            time_it([&] {
-                auto mt = make_lw_shared<memtable>(s);
-                for (auto&& m : mutations) {
-                    mt->apply(m);
-                }
-
-                if (update_cache) {
-                    cache.update([] {}, *mt).get();
-                }
-            }, 5, 1);
+            engine().at_exit([] {
+                cancelled = true;
+                return make_ready_future();
+            });
+            test_small_partitions();
+            test_partition_with_lots_of_small_rows();
+            test_partition_with_lots_of_range_tombstones();
         });
     });
 }

scylladb/scylladb

tests/perf_row_cache_update.cc
@@ -19,12 +19,15 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <chrono>
 #include <core/distributed.hh>
 #include <core/app-template.hh>
 #include <core/sstring.hh>
 #include <core/thread.hh>
+#include <seastar/core/weak_ptr.hh>
 
 #include "utils/managed_bytes.hh"
+#include "utils/extremum_tracking.hh"
 #include "utils/logalloc.hh"
 #include "row_cache.hh"
 #include "log.hh"
@@ -32,68 +35,253 @@
 #include "memtable.hh"
 #include "tests/perf/perf.hh"
 
-static
-dht::decorated_key new_key(schema_ptr s) {
-    static thread_local int next = 0;
-    return dht::global_partitioner().decorate_key(*s,
-        partition_key::from_single_value(*s, to_bytes(sprint("key%d", next++))));
+static const int update_iterations = 16;
+static const int cell_size = 128;
+static bool cancelled = false;
+
+template<typename Func>
+auto duration_in_seconds(Func&& f) {
+    using clk = std::chrono::steady_clock;
+    auto start = clk::now();
+    f();
+    auto end = clk::now();
+    return std::chrono::duration_cast<std::chrono::duration<float>>(end - start);
 }
 
-static
-clustering_key new_ckey(schema_ptr s) {
-    static thread_local int next = 0;
-    return clustering_key::from_single_value(*s, to_bytes(sprint("ckey%d", next++)));
+class scheduling_latency_measurer : public weakly_referencable<scheduling_latency_measurer> {
+    using clk = std::chrono::steady_clock;
+    clk::time_point _last = clk::now();
+    utils::estimated_histogram _hist{300};
+    min_max_tracker<clk::duration> _minmax;
+    bool _stop = false;
+private:
+    void schedule_tick();
+    void tick() {
+        auto old = _last;
+        _last = clk::now();
+        auto latency = _last - old;
+        _minmax.update(latency);
+        _hist.add(latency.count());
+        if (!_stop) {
+            schedule_tick();
+        }
+    }
+public:
+    void start() {
+        schedule_tick();
+    }
+    void stop() {
+        _stop = true;
+        later().get(); // so that the last scheduled tick is counted
+    }
+    const utils::estimated_histogram& histogram() const {
+        return _hist;
+    }
+    clk::duration min() const { return _minmax.min(); }
+    clk::duration max() const { return _minmax.max(); }
+};
+
+void scheduling_latency_measurer::schedule_tick() {
+    seastar::schedule(make_task(default_scheduling_group(), [self = weak_from_this()] () mutable {
+        if (self) {
+            self->tick();
+        }
+    }));
+}
+
+std::ostream& operator<<(std::ostream& out, const scheduling_latency_measurer& slm) {
+    auto to_ms = [] (int64_t nanos) {
+        return float(nanos) / 1e6;
+    };
+    return out << sprint("{count: %d, "
+                         //"min: %.6f [ms], "
+                         //"50%%: %.6f [ms], "
+                         //"90%%: %.6f [ms], "
+                         "99%%: %.6f [ms], "
+                         "max: %.6f [ms]}",
+        slm.histogram().count(),
+        //to_ms(slm.min().count()),
+        //to_ms(slm.histogram().percentile(0.5)),
+        //to_ms(slm.histogram().percentile(0.9)),
+        to_ms(slm.histogram().percentile(0.99)),
+        to_ms(slm.max().count()));
+}
+
+template<typename MutationGenerator>
+void run_test(const sstring& name, schema_ptr s, MutationGenerator&& gen) {
+    cache_tracker tracker;
+    row_cache cache(s, make_empty_snapshot_source(), tracker, is_continuous::yes);
+
+    size_t memtable_size = seastar::memory::stats().total_memory() / 4;
+
+    std::cout << name << ":\n";
+
+    for (int i = 0; i < update_iterations; ++i) {
+        auto MB = 1024 * 1024;
+        auto prefill_compacted = logalloc::memory_compacted();
+        auto prefill_allocated = logalloc::memory_allocated();
+
+        auto mt = make_lw_shared<memtable>(s);
+        while (mt->occupancy().total_space() < memtable_size) {
+            auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+                data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+            mutation m = gen();
+            mt->apply(m);
+            if (cancelled) {
+                return;
+            }
+        }
+
+        auto prev_compacted = logalloc::memory_compacted();
+        auto prev_allocated = logalloc::memory_allocated();
+        auto prev_rows_processed_from_memtable = tracker.get_stats().rows_processed_from_memtable;
+        auto prev_rows_merged_from_memtable = tracker.get_stats().rows_merged_from_memtable;
+        auto prev_rows_dropped_from_memtable = tracker.get_stats().rows_dropped_from_memtable;
+
+        std::cout << sprint("cache: %d/%d [MB], memtable: %d/%d [MB], alloc/comp: %d/%d [MB] (amp: %.3f)\n",
+            tracker.region().occupancy().used_space() / MB,
+            tracker.region().occupancy().total_space() / MB,
+            mt->occupancy().used_space() / MB,
+            mt->occupancy().total_space() / MB,
+            (prev_allocated - prefill_allocated) / MB,
+            (prev_compacted - prefill_compacted) / MB,
+            float((prev_compacted - prefill_compacted)) / (prev_allocated - prefill_allocated)
+        );
+
+        scheduling_latency_measurer slm;
+        slm.start();
+        auto d = duration_in_seconds([&] {
+            cache.update([] {}, *mt).get();
+        });
+        slm.stop();
+
+        auto compacted = logalloc::memory_compacted() - prev_compacted;
+        auto allocated = logalloc::memory_allocated() - prev_allocated;
+
+        std::cout << sprint("update: %.6f [ms], stall: %s, cache: %d/%d [MB], alloc/comp: %d/%d [MB] (amp: %.3f), pr/me/dr %d/%d/%d\n",
+            d.count() * 1000,
+            slm,
+            tracker.region().occupancy().used_space() / MB,
+            tracker.region().occupancy().total_space() / MB,
+            allocated / MB, compacted / MB, float(compacted)/allocated,
+            tracker.get_stats().rows_processed_from_memtable - prev_rows_processed_from_memtable,
+            tracker.get_stats().rows_merged_from_memtable - prev_rows_merged_from_memtable,
+            tracker.get_stats().rows_dropped_from_memtable - prev_rows_dropped_from_memtable);
+    }
+
+    auto d = duration_in_seconds([&] {
+        cache.invalidate([] {}).get();
+    });
+
+    std::cout << sprint("invalidation: %.6f [ms]", d.count() * 1000) << "\n";
+}
+
+void test_small_partitions() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    run_test("Small partitions, no overwrites", s, [&] {
+        auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+            data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+        m.set_clustered_cell(clustering_key::make_empty(), "v1", val, api::new_timestamp());
+        m.set_clustered_cell(clustering_key::make_empty(), "v2", val, api::new_timestamp());
+        m.set_clustered_cell(clustering_key::make_empty(), "v3", val, api::new_timestamp());
+        return m;
+    });
+}
+
+void test_partition_with_lots_of_small_rows() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("ck", reversed_type_impl::get_instance(int32_type), column_kind::clustering_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+        data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+    int ck_idx = 0;
+
+    run_test("Large partition, lots of small rows", s, [&] {
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+        auto ck = clustering_key::from_single_value(*s, data_value(ck_idx++).serialize());
+        m.set_clustered_cell(ck, "v1", val, api::new_timestamp());
+        m.set_clustered_cell(ck, "v2", val, api::new_timestamp());
+        m.set_clustered_cell(ck, "v3", val, api::new_timestamp());
+        return m;
+    });
+}
+
+void test_partition_with_few_small_rows() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("ck", reversed_type_impl::get_instance(int32_type), column_kind::clustering_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    run_test("Small partition with a few rows", s, [&] {
+        auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+            data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+
+        for (int i = 0; i < 3; ++i) {
+            auto ck = clustering_key::from_single_value(*s, data_value(i).serialize());
+            m.set_clustered_cell(ck, "v1", val, api::new_timestamp());
+            m.set_clustered_cell(ck, "v2", val, api::new_timestamp());
+            m.set_clustered_cell(ck, "v3", val, api::new_timestamp());
+        }
+        return m;
+    });
+}
+
+void test_partition_with_lots_of_range_tombstones() {
+    auto s = schema_builder("ks", "cf")
+        .with_column("pk", uuid_type, column_kind::partition_key)
+        .with_column("ck", reversed_type_impl::get_instance(int32_type), column_kind::clustering_key)
+        .with_column("v1", bytes_type, column_kind::regular_column)
+        .with_column("v2", bytes_type, column_kind::regular_column)
+        .with_column("v3", bytes_type, column_kind::regular_column)
+        .build();
+
+    auto pk = dht::global_partitioner().decorate_key(*s, partition_key::from_single_value(*s,
+        data_value(utils::UUID_gen::get_time_UUID()).serialize()));
+    int ck_idx = 0;
+
+    run_test("Large partition, lots of range tombstones", s, [&] {
+        mutation m(s, pk);
+        auto val = data_value(bytes(bytes::initialized_later(), cell_size));
+        auto ck = clustering_key::from_single_value(*s, data_value(ck_idx++).serialize());
+        auto r = query::clustering_range::make({ck}, {ck});
+        tombstone tomb(api::new_timestamp(), gc_clock::now());
+        m.partition().apply_row_tombstone(*s, range_tombstone(bound_view::from_range_start(r), bound_view::from_range_end(r), tomb));
+        return m;
+    });
 }
 
 int main(int argc, char** argv) {
-    namespace bpo = boost::program_options;
     app_template app;
-    app.add_options()
-        ("debug", "enable debug logging")
-        ("partitions", bpo::value<unsigned>()->default_value(128), "number of partitions per memtable")
-        ("cell-size", bpo::value<unsigned>()->default_value(1024), "cell size in bytes")
-        ("rows", bpo::value<unsigned>()->default_value(128), "row count per partition")
-        ("no-cache", "do not update cache");
-
     return app.run(argc, argv, [&app] {
-        if (app.configuration().count("debug")) {
-            logging::logger_registry().set_all_loggers_level(logging::log_level::debug);
-        }
-
         return seastar::async([&] {
-            auto s = schema_builder("ks", "cf")
-                .with_column("pk", bytes_type, column_kind::partition_key)
-                .with_column("ck", bytes_type, column_kind::clustering_key)
-                .with_column("v", bytes_type, column_kind::regular_column)
-                .build();
-
-            cache_tracker tracker;
-            row_cache cache(s, make_empty_snapshot_source(), tracker);
-
-            size_t partitions = app.configuration()["partitions"].as<unsigned>();
-            size_t cell_size = app.configuration()["cell-size"].as<unsigned>();
-            size_t row_count = app.configuration()["rows"].as<unsigned>();
-            bool update_cache = !app.configuration().count("no-cache");
-
-            std::vector<mutation> mutations;
-            for (unsigned i = 0; i < partitions; ++i) {
-                mutation m(s, new_key(s));
-                for (size_t j = 0; j < row_count; j++) {
-                    m.set_clustered_cell(new_ckey(s), "v", data_value(bytes(bytes::initialized_later(), cell_size)), 2);
-                }
-                mutations.emplace_back(std::move(m));
-            }
-
-            time_it([&] {
-                auto mt = make_lw_shared<memtable>(s);
-                for (auto&& m : mutations) {
-                    mt->apply(m);
-                }
-
-                if (update_cache) {
-                    cache.update([] {}, *mt).get();
-                }
-            }, 5, 1);
+            engine().at_exit([] {
+                cancelled = true;
+                return make_ready_future();
+            });
+            test_small_partitions();
+            test_partition_with_few_small_rows();
+            test_partition_with_lots_of_small_rows();
+            test_partition_with_lots_of_range_tombstones();
         });
     });
 }