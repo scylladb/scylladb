pdziepak/scylla

tests: add test for new in memory representation

configure.py
@@ -243,6 +243,7 @@ scylla_tests = [
     'tests/duration_test',
     'tests/vint_serialization_test',
     'tests/compress_test',
+    'tests/in_memory_representation_test',
 ]
 
 apps = [
@@ -628,6 +629,7 @@ pure_boost_tests = set([
     'tests/duration_test',
     'tests/vint_serialization_test',
     'tests/compress_test',
+    'tests/in_memory_representation_test',
 ])
 
 tests_not_using_seastar_test_framework = set([
@@ -670,6 +672,7 @@ deps['tests/murmur_hash_test'] = ['bytes.cc', 'utils/murmur_hash.cc', 'tests/mur
 deps['tests/allocation_strategy_test'] = ['tests/allocation_strategy_test.cc', 'utils/logalloc.cc', 'utils/dynamic_bitset.cc']
 deps['tests/log_histogram_test'] = ['tests/log_histogram_test.cc']
 deps['tests/anchorless_list_test'] = ['tests/anchorless_list_test.cc']
+deps['tests/in_memory_representation_test'] = ['tests/in_memory_representation_test.cc']
 
 warnings = [
     '-Wno-mismatched-tags',  # clang-only

scylladb/scylladb

configure.py
@@ -300,6 +300,7 @@ scylla_tests = [
     'tests/querier_cache',
     'tests/limiting_data_source_test',
     'tests/meta_test',
+    'tests/imr_test',
 ]
 
 perf_tests = [
@@ -729,6 +730,7 @@ pure_boost_tests = set([
     'tests/enum_set_test',
     'tests/cql_auth_syntax_test',
     'tests/meta_test',
+    'tests/imr_test',
 ])
 
 tests_not_using_seastar_test_framework = set([
@@ -780,6 +782,7 @@ deps['tests/log_heap_test'] = ['tests/log_heap_test.cc']
 deps['tests/anchorless_list_test'] = ['tests/anchorless_list_test.cc']
 deps['tests/perf/perf_fast_forward'] += ['release.cc']
 deps['tests/meta_test'] = ['tests/meta_test.cc']
+deps['tests/imr_test'] = ['tests/imr_test.cc']
 
 warnings = [
     '-Wno-mismatched-tags',  # clang-only