093c8512dbefb2d659c7ed998d809bb62447cde8
Avi Kivity <avi@scylladb.com>
2018-06-27 14:28:27 +0300

    Merge "Disable sstable filtering based on min/max clustering key components" from Tomasz
    "
    With DateTiered and TimeWindow, there is a read optimization enabled
    which excludes sstables based on overlap with recorded min/max values
    of clustering key components. The problem is that it doesn't take into
    account partition tombstones and static rows, which should still be
    returned by the reader even if there is no overlap in the query's
    clustering range. A read which returns no clustering rows can
    mispopulate cache, which will appear as partition deletion or writes
    to the static row being lost. Until node restart or eviction of the
    partition entry.
    
    There is also a bad interaction between cache population on read and
    that optimization. When the clustering range of the query doesn't
    overlap with any sstable, the reader will return no partition markers
    for the read, which leads cache populator to assume there is no
    partition in sstables and it will cache an empty partition. This will
    cause later reads of that partition to miss prior writes to that
    partition until it is evicted from cache or node is restarted.
    
    Disable until a more elaborate fix is implemented.
    
    Fixes #3552
    Fixes #3553
    "
    
    * tag 'tgrabiec/disable-min-max-sstable-filtering-v1' of github.com:tgrabiec/scylla:
      tests: Add test for slicing a mutation source with date tiered compaction strategy
      tests: Check that database conforms to mutation source
      database: Disable sstable filtering based on min/max clustering key components
    
    (cherry picked from commit e1efda8b0c10aa71a425e1854e1e8e3d5f0f1efe)

--- tests/database_test.cc
+++ tests/database_test.cc
@@ -29,6 +29,9 @@
 #include "database.hh"
 #include "partition_slice_builder.hh"
 #include "frozen_mutation.hh"
+#include "mutation_source_test.hh"
+#include "schema_registry.hh"
+#include "service/migration_manager.hh"
 
 #include "disk-error-handler.hh"
 
@@ -77,5 +80,35 @@ SEASTAR_TEST_CASE(test_querying_with_limits) {
                 assert_that(query::result_set::from_raw_result(s, cmd.slice, *result)).has_size(3);
             }
         });
+    });
+}
+
+SEASTAR_TEST_CASE(test_database_with_data_in_sstables_is_a_mutation_source) {
+    return do_with_cql_env_thread([] (cql_test_env& e) {
+        run_mutation_source_tests([&] (schema_ptr s, const std::vector<mutation>& partitions) -> mutation_source {
+            try {
+                e.local_db().find_column_family(s->ks_name(), s->cf_name());
+                service::get_local_migration_manager().announce_column_family_drop(s->ks_name(), s->cf_name(), true).get();
+            } catch (const no_such_column_family&) {
+                // expected
+            }
+            service::get_local_migration_manager().announce_new_column_family(s, true).get();
+            column_family& cf = e.local_db().find_column_family(s);
+            for (auto&& m : partitions) {
+                e.local_db().apply(cf.schema(), freeze(m)).get();
+            }
+            cf.flush().get();
+            cf.get_row_cache().invalidate([] {}).get();
+            return mutation_source([&] (schema_ptr s,
+                    const dht::partition_range& range,
+                    const query::partition_slice& slice,
+                    const io_priority_class& pc,
+                    tracing::trace_state_ptr trace_state,
+                    streamed_mutation::forwarding fwd,
+                    mutation_reader::forwarding fwd_mr) {
+                return cf.make_reader(s, range, slice, pc, std::move(trace_state), fwd, fwd_mr);
+            });
+        });
+        return make_ready_future<>();
     });
 }
