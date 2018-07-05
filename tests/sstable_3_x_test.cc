c7285abc9e63d696029b02baef40cd73c000fbb0
Vladimir Krivopalov <vladimir@scylladb.com>
2018-07-05 13:52:55 -0700

    tests: Add test reading index for a partition comprised of RT markers of boundary types.
    Signed-off-by: Vladimir Krivopalov <vladimir@scylladb.com>

--- tests/sstable_3_x_test.cc
+++ tests/sstable_3_x_test.cc
@@ -2856,3 +2856,29 @@ SEASTAR_THREAD_TEST_CASE(test_read_range_tombstones_only_index) {
     assert_that(get_index_reader(sst)).has_monotonic_positions(*s);
 }
 
+/*
+ * Generated with the following code:
+ *  query = SimpleStatement("""
+	DELETE FROM range_tombstone_boundaries_index USING TIMESTAMP %(ts)s
+	WHERE pk = 'key1' AND ck1 > %(a)s AND ck1 <= %(b)s
+        """, consistency_level=ConsistencyLevel.ONE)
+
+    for i in range(1024):
+        session.execute(query, dict(a="%s%d" % ('a' * 1024, i), b="%s%d" %('a' * 1024, i + 1), ts=(1525385507816568 + i)))
+
+ */
+SEASTAR_THREAD_TEST_CASE(test_read_range_tombstone_boundaries_index) {
+    sstring table_name = "range_tombstone_boundaries_index";
+    // CREATE TABLE range_tombstone_boundaries_index (pk text, ck1 text, ck2 text, PRIMARY KEY (pk, ck1, ck2) WITH compression = {'sstable_compression': ''};
+    schema_builder builder("sst3", table_name);
+    builder.with_column("pk", utf8_type, column_kind::partition_key);
+    builder.with_column("ck1", utf8_type, column_kind::clustering_key);
+    builder.with_column("ck2", utf8_type, column_kind::clustering_key);
+    builder.set_compressor_params(compression_parameters());
+    schema_ptr s = builder.build(schema_builder::compact_storage::no);
+
+    auto sst = sstables::make_sstable(s, get_read_index_test_path(table_name), 1, sstable_version_types::mc , sstable_format_types::big);
+    sst->load().get0();
+    assert_that(get_index_reader(sst)).has_monotonic_positions(*s);
+}
+
