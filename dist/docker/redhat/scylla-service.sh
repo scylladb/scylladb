b52d647de2d2fbcce514934e2f9c62a00de7e6e1
Avi Kivity <avi@scylladb.com>
2018-08-02 18:30:17 +0300

    docker: adjust for script conversion to Python
    Since our scripts were converted to Python, we can no longer
    source them from a shell. Execute them directly instead. Also,
    we now need to import configuration variables ourselves, since
    scylla_prepare, being an independent process, won't do it for
    us.
    
    Fixes #3647
    Message-Id: <20180802153017.11112-1-avi@scylladb.com>
    
    (cherry picked from commit c9caaa8e6e1a29791728e56e3eed055467a626ef)

--- dist/docker/redhat/scylla-service.sh
+++ dist/docker/redhat/scylla-service.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-. /usr/lib/scylla/scylla_prepare
+/usr/lib/scylla/scylla_prepare
+
+. /etc/sysconfig/scylla-server
 
 export SCYLLA_HOME SCYLLA_CONF
+
+for f in /etc/scylla.d/*.conf; do
+    . "$f"
+done
 
 exec /usr/bin/scylla $SCYLLA_ARGS $SEASTAR_IO $DEV_MODE $CPUSET $SCYLLA_DOCKER_ARGS
