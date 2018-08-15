b1652823aae7e566e4e5e98101ec5f57cddedaa3
Eliran Sinvani <eliransin@scylladb.com>
2018-08-15 11:38:28 +0300

    cql3: ensure repeated values in IN clauses don't return repeated rows
    When the list of values in the IN list of a single column contains
    duplicates, multiple executors are activated since the assumption
    is that each value in the IN list corresponds to a different partition.
    this results in the same row appearing in the result number times
    corresponding to the duplication of the partition value.
    
    Added queries for the in restriction unitest and fixed with a bad result check.
    
    Fixes #2837
    Tests: Queries as in the usecase from the GitHub issue in both forms ,
    prepared and plain (using python driver),Unitest.
    
    Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>
    Message-Id: <ad88b7218fa55466be7bc4303dc50326a3d59733.1534322238.git.eliransin@scylladb.com>
    (cherry picked from commit d734d316a6b32a5e317d1229fff32b4ce1cb2c96)

--- tests/cql_query_test.cc
+++ tests/cql_query_test.cc
@@ -2047,10 +2047,9 @@ SEASTAR_TEST_CASE(test_in_restriction) {
             assert_that(msg).is_rows().with_size(0);
             return e.execute_cql("select r1 from tir where p1 in (2, 0, 2, 1);");
         }).then([&e] (shared_ptr<cql_transport::messages::result_message> msg) {
-            assert_that(msg).is_rows().with_rows({
+            assert_that(msg).is_rows().with_rows_ignore_order({
                 {int32_type->decompose(4)},
                 {int32_type->decompose(0)},
-                {int32_type->decompose(4)},
                 {int32_type->decompose(1)},
                 {int32_type->decompose(2)},
                 {int32_type->decompose(3)},
@@ -2071,6 +2070,22 @@ SEASTAR_TEST_CASE(test_in_restriction) {
                 {int32_type->decompose(3)},
                 {int32_type->decompose(2)},
                 {int32_type->decompose(1)},
+            });
+            return e.prepare("select r1 from tir where p1 in ?");
+        }).then([&e] (cql3::prepared_cache_key_type prepared_id){
+            auto my_list_type = list_type_impl::get_instance(int32_type, true);
+            std::vector<cql3::raw_value> raw_values;
+            auto in_values_list = my_list_type->decompose(make_list_value(my_list_type,
+                    list_type_impl::native_type{{int(2), int(0), int(2), int(1)}}));
+            raw_values.emplace_back(cql3::raw_value::make_value(in_values_list));
+            return e.execute_prepared(prepared_id,raw_values);
+        }).then([&e] (shared_ptr<cql_transport::messages::result_message> msg) {
+            assert_that(msg).is_rows().with_rows_ignore_order({
+                {int32_type->decompose(4)},
+                {int32_type->decompose(0)},
+                {int32_type->decompose(1)},
+                {int32_type->decompose(2)},
+                {int32_type->decompose(3)},
             });
         });
     });
