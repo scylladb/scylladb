e9fbc277409ca4d3f381f1a1f2ba502187a2fe22
Asias He <asias@scylladb.com>
2018-09-27 09:43:12 +0800

    idl: Add bound_weight
    It will be used by the row level repair code.

--- idl/partition_checksum.idl.hh
+++ idl/partition_checksum.idl.hh
@@ -32,6 +32,12 @@ class repair_hash {
     uint64_t hash;
 };
 
+enum class bound_weight : int8_t {
+    before_all_prefixed = -1,
+    equal = 0,
+    after_all_prefixed = 1,
+};
+
 enum class partition_region : uint8_t {
     partition_start,
     static_row,
