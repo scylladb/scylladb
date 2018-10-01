bhalevy/scylla-old

sstables stats: partition seeks
Fixes: #251

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

sstables/sstables.cc
@@ -4270,6 +4270,8 @@ future<> init_metrics() {
             sm::description("Number of range tombstones written")),
         sm::make_derive("partition_reads", [] { return sstables_stats::shard_stats().partition_reads; },
             sm::description("Number of partitions read")),
+        sm::make_derive("partition_seeks", [] { return sstables_stats::shard_stats().partition_seeks; },
+            sm::description("Number of partitions seeked")),
     });
   });
 }

scylladb/scylladb

sstables/sstables.cc
@@ -4300,6 +4300,8 @@ future<> init_metrics() {
             sm::description("Number of whole sstable flat mutation reads")),
         sm::make_derive("partition_reads", [] { return sstables_stats::get_shard_stats().partition_reads; },
             sm::description("Number of partitions read")),
+        sm::make_derive("partition_seeks", [] { return sstables_stats::get_shard_stats().partition_seeks; },
+            sm::description("Number of partitions seeked")),
     });
   });
 }