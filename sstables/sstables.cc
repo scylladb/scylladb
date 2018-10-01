bhalevy/scylla-old

sstables stats: row reads
Fixes: #251

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

sstables/sstables.cc
@@ -4272,6 +4272,8 @@ future<> init_metrics() {
             sm::description("Number of partitions read")),
         sm::make_derive("partition_seeks", [] { return sstables_stats::shard_stats().partition_seeks; },
             sm::description("Number of partitions seeked")),
+        sm::make_derive("row_reads", [] { return sstables_stats::shard_stats().row_reads; },
+            sm::description("Number of rows read")),
     });
   });
 }

scylladb/scylladb

sstables/sstables.cc
@@ -4302,6 +4302,8 @@ future<> init_metrics() {
             sm::description("Number of partitions read")),
         sm::make_derive("partition_seeks", [] { return sstables_stats::get_shard_stats().partition_seeks; },
             sm::description("Number of partitions seeked")),
+        sm::make_derive("row_reads", [] { return sstables_stats::get_shard_stats().row_reads; },
+            sm::description("Number of rows read")),
     });
   });
 }