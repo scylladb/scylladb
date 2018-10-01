bhalevy/scylla-old

sstables stats: partition reads
Fixes: #251

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

sstables/sstables.cc
@@ -4268,6 +4268,8 @@ future<> init_metrics() {
             sm::description("Number of tombstones written")),
         sm::make_derive("range_tombstone_writes", [] { return sstables_stats::shard_stats().range_tombstone_writes; },
             sm::description("Number of range tombstones written")),
+        sm::make_derive("partition_reads", [] { return sstables_stats::shard_stats().partition_reads; },
+            sm::description("Number of partitions read")),
     });
   });
 }

scylladb/scylladb

sstables/sstables.cc
@@ -4298,6 +4298,8 @@ future<> init_metrics() {
             sm::description("Number of partition range flat mutation reads")),
         sm::make_derive("sstable_partition_reads", [] { return sstables_stats::get_shard_stats().sstable_partition_reads; },
             sm::description("Number of whole sstable flat mutation reads")),
+        sm::make_derive("partition_reads", [] { return sstables_stats::get_shard_stats().partition_reads; },
+            sm::description("Number of partitions read")),
     });
   });
 }