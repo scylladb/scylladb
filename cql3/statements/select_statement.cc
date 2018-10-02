de48966abcd5593ad3573658ad7811fb58c61fd1
Pekka Enberg <penberg@scylladb.com>
2018-10-02 16:26:37 +0300

    cql3: Move as_json_function class to separate file
    The as_json_function class is not registered as a function, but we can
    still keep it cql3/functions, as per its namespace, to reduce the size
    of select_statement.cc.
    Message-Id: <20181002132637.30233-1-penberg@scylladb.com>

--- cql3/statements/select_statement.cc
+++ cql3/statements/select_statement.cc
@@ -43,6 +43,7 @@
 #include "cql3/statements/raw/select_statement.hh"
 
 #include "transport/messages/result_message.hh"
+#include "cql3/functions/as_json_function.hh"
 #include "cql3/selection/selection.hh"
 #include "cql3/util.hh"
 #include "cql3/restrictions/single_column_primary_key_restrictions.hh"
@@ -58,107 +59,6 @@
 #include <boost/algorithm/cxx11/any_of.hpp>
 
 namespace cql3 {
-
-namespace functions {
-
-/*
- * This function is used for handling 'SELECT JSON' statement.
- * 'SELECT JSON' is supposed to return a single column named '[json]'
- * with JSON representation of the query result as its value.
- * In order to achieve it, selectors from 'SELECT' are wrapped with
- * 'as_json' function, which also keeps information about underlying
- * selector names and types. This function is not registered in functions.cc,
- * because it should not be invoked directly from CQL.
- * Case-sensitive column names are wrapped in additional quotes,
- * as stated in CQL-JSON documentation.
- */
-class as_json_function : public scalar_function {
-    std::vector<sstring> _selector_names;
-    std::vector<data_type> _selector_types;
-public:
-    as_json_function(std::vector<sstring>&& selector_names, std::vector<data_type> selector_types)
-        : _selector_names(std::move(selector_names)), _selector_types(std::move(selector_types)) {
-    }
-
-    virtual bytes_opt execute(cql_serialization_format sf, const std::vector<bytes_opt>& parameters) override {
-        bytes_ostream encoded_row;
-        encoded_row.write("{", 1);
-        for (size_t i = 0; i < _selector_names.size(); ++i) {
-            if (i > 0) {
-                encoded_row.write(", ", 2);
-            }
-            bool has_any_upper = boost::algorithm::any_of(_selector_names[i], [](unsigned char c) { return std::isupper(c); });
-            encoded_row.write("\"", 1);
-            if (has_any_upper) {
-                encoded_row.write("\\\"", 2);
-            }
-            encoded_row.write(_selector_names[i].c_str(), _selector_names[i].size());
-            if (has_any_upper) {
-                encoded_row.write("\\\"", 2);
-            }
-            encoded_row.write("\": ", 3);
-            sstring row_sstring = _selector_types[i]->to_json_string(parameters[i]);
-            encoded_row.write(row_sstring.c_str(), row_sstring.size());
-        }
-        encoded_row.write("}", 1);
-        return encoded_row.linearize().to_string();
-    }
-
-    virtual const function_name& name() const override {
-        static const function_name f_name = function_name::native_function("as_json");
-        return f_name;
-    }
-
-    virtual const std::vector<data_type>& arg_types() const override {
-        return _selector_types;
-    }
-
-    virtual data_type return_type() const override {
-        return utf8_type;
-    }
-
-    virtual bool is_pure() override {
-        return true;
-    }
-
-    virtual bool is_native() override {
-        return true;
-    }
-
-    virtual bool is_aggregate() override {
-        // Aggregates of aggregates are currently not supported, but JSON handles them
-        return false;
-    }
-
-    virtual void print(std::ostream& os) const override {
-        os << "as_json(";
-        bool first = true;
-        for (const sstring&  selector_name: _selector_names) {
-            if (first) {
-                first = false;
-            } else {
-                os << ", ";
-            }
-            os << selector_name;
-        }
-        os << ") -> " << utf8_type->as_cql3_type()->to_string();
-    }
-
-    virtual bool uses_function(const sstring& ks_name, const sstring& function_name) override {
-        return false;
-    }
-
-    virtual bool has_reference_to(function& f) override {
-        return false;
-    }
-
-    virtual sstring column_name(const std::vector<sstring>& column_names) override {
-        return "[json]";
-    }
-
-};
-
-}
 
 namespace statements {
 
