8c1e6fcee830296d0107cea9949d8494bc14a70d
Duarte Nunes <duarte@scylladb.com>
2018-10-06 15:58:28 +0100

    db/timeout_clock: Define timeout_semaphore_units
    Defines the type of semaphore_units<> associated with
    timeout_semaphore.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- db/timeout_clock.hh
+++ db/timeout_clock.hh
@@ -28,5 +28,6 @@
 namespace db {
 using timeout_clock = seastar::lowres_clock;
 using timeout_semaphore = seastar::basic_semaphore<seastar::default_timeout_exception_factory, timeout_clock>;
+using timeout_semaphore_units = seastar::semaphore_units<seastar::default_timeout_exception_factory, timeout_clock>;
 static constexpr timeout_clock::time_point no_timeout = timeout_clock::time_point::max();
 }
