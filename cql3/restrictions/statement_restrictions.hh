7258f3206654418b375a9fe7c500ba3c7e4b3973
Eliran Sinvani <eliransin@scylladb.com>
2018-10-07 11:39:12 +0300

    cql3 : ensure retrieval of columns for filtering
    When a query that needs filtering is executed, the columns
    that the coordinator is filtering by have to be retrieved.The
    columns should be retrieved even if they are not used for
    ordering or named in the actual select clause.
    If the columns are missing from the result set, then any
    filtering that restricts the missing column will not take
    place.
    
    Fixes #3803
    
    Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>

--- cql3/restrictions/statement_restrictions.hh
+++ cql3/restrictions/statement_restrictions.hh
@@ -164,6 +164,13 @@ public:
     }
 
     /**
+     * Builds a possibly empty collection of column definitions that will be used for filtering
+     * @param db - the database context
+     * @return A list with the column definitions needed for filtering.
+     */
+    std::vector<const column_definition*> get_column_defs_for_filtering(database& db) const;
+
+    /**
      * Determines the index to be used with the restriction.
      * @param db - the database context (for extracting index manager)
      * @return If an index can be used, an optional containing this index, otherwise an empty optional.
