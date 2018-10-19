scylladb/scylladb

commit a35136533d9fb080979f4cdacfde02935d63aad2

Convert ring_position and token ranges to be nonwrapping
Wrapping ranges are a pain, so we are moving wrap handling to the edges.

Since cql can't generate wrapping ranges, this means thrift and the ring
maintenance code; also range->ring transformations need to merge the first
and last ranges.

Message-Id: <1478105905-31613-1-git-send-email-avi@scylladb.com>

locator/abstract_replication_strategy.cc
@@ -22,6 +22,7 @@
 #include "locator/abstract_replication_strategy.hh"
 #include "utils/class_registrator.hh"
 #include "exceptions/exceptions.hh"
+#include "stdx.hh"
 
 namespace locator {
 
@@ -38,19 +39,6 @@ abstract_replication_strategy::abstract_replication_strategy(
         , _token_metadata(token_metadata)
         , _snitch(snitch)
         , _my_type(my_type) {}
-
-static void unwrap_first_range(std::vector<range<token>>& ret) {
-    if (ret.empty()) {
-        return;
-    }
-    // Make ret contain no wrap-around range by unwrapping the first element.
-    auto& r = ret.front();
-    if (r.is_wrap_around(dht::token_comparator())) {
-        auto split_ranges = r.unwrap();
-        r = std::move(split_ranges.first);
-        ret.push_back(std::move(split_ranges.second));
-    }
-}
 
 std::unique_ptr<abstract_replication_strategy> abstract_replication_strategy::create_replication_strategy(const sstring& ks_name, const sstring& strategy_name, token_metadata& tk_metadata, const std::map<sstring, sstring>& config_options) {
     assert(locator::i_endpoint_snitch::get_local_snitch_ptr());
@@ -124,95 +112,96 @@ abstract_replication_strategy::get_cached_endpoints() {
     return _cached_endpoints;
 }
 
-std::vector<range<token>>
+static
+void
+insert_token_range_to_sorted_container_while_unwrapping(
+        const dht::token& prev_tok,
+        const dht::token& tok,
+        std::vector<nonwrapping_range<dht::token>>& ret) {
+    if (prev_tok < tok) {
+        ret.emplace_back(
+                nonwrapping_range<token>::bound(prev_tok, false),
+                nonwrapping_range<token>::bound(tok, true));
+    } else {
+        ret.emplace_back(
+                nonwrapping_range<token>::bound(prev_tok, false),
+                stdx::nullopt);
+        // Insert in front to maintain sorded order
+        ret.emplace(
+                ret.begin(),
+                stdx::nullopt,
+                nonwrapping_range<token>::bound(tok, true));
+    }
+}
+
+std::vector<nonwrapping_range<token>>
 abstract_replication_strategy::get_ranges(inet_address ep) const {
-    std::vector<range<token>> ret;
+    std::vector<nonwrapping_range<token>> ret;
     auto prev_tok = _token_metadata.sorted_tokens().back();
     for (auto tok : _token_metadata.sorted_tokens()) {
         for (inet_address a : calculate_natural_endpoints(tok, _token_metadata)) {
             if (a == ep) {
-                ret.emplace_back(
-                        range<token>::bound(prev_tok, false),
-                        range<token>::bound(tok, true));
+                insert_token_range_to_sorted_container_while_unwrapping(prev_tok, tok, ret);
                 break;
             }
         }
         prev_tok = tok;
     }
-    unwrap_first_range(ret);
     return ret;
 }
 
-std::vector<range<token>>
+std::vector<nonwrapping_range<token>>
 abstract_replication_strategy::get_primary_ranges(inet_address ep) {
-    std::vector<range<token>> ret;
+    std::vector<nonwrapping_range<token>> ret;
     auto prev_tok = _token_metadata.sorted_tokens().back();
     for (auto tok : _token_metadata.sorted_tokens()) {
         auto&& eps = calculate_natural_endpoints(tok, _token_metadata);
         if (eps.size() > 0 && eps[0] == ep) {
-            ret.emplace_back(
-                    range<token>::bound(prev_tok, false),
-                    range<token>::bound(tok, true));
+            insert_token_range_to_sorted_container_while_unwrapping(prev_tok, tok, ret);
         }
         prev_tok = tok;
     }
-    unwrap_first_range(ret);
     return ret;
 }
 
-std::unordered_multimap<inet_address, range<token>>
+std::unordered_multimap<inet_address, nonwrapping_range<token>>
 abstract_replication_strategy::get_address_ranges(token_metadata& tm) const {
-    std::unordered_multimap<inet_address, range<token>> ret;
+    std::unordered_multimap<inet_address, nonwrapping_range<token>> ret;
     for (auto& t : tm.sorted_tokens()) {
-        range<token> r = tm.get_primary_range_for(t);
+        std::vector<nonwrapping_range<token>> r = tm.get_primary_ranges_for(t);
         auto eps = calculate_natural_endpoints(t, tm);
         logger.debug("token={}, primary_range={}, address={}", t, r, eps);
-        bool wrap = r.is_wrap_around(dht::token_comparator());
-        if (wrap) {
-            auto split_ranges = r.unwrap();
-            for (auto ep : eps) {
-                ret.emplace(ep, split_ranges.first);
-                ret.emplace(ep, split_ranges.second);
-            }
-        } else {
-            for (auto ep : eps) {
-                ret.emplace(ep, r);
+        for (auto ep : eps) {
+            for (auto&& rng : r) {
+                ret.emplace(ep, rng);
             }
         }
     }
     return ret;
 }
 
-std::unordered_multimap<range<token>, inet_address>
+std::unordered_multimap<nonwrapping_range<token>, inet_address>
 abstract_replication_strategy::get_range_addresses(token_metadata& tm) const {
-    std::unordered_multimap<range<token>, inet_address> ret;
+    std::unordered_multimap<nonwrapping_range<token>, inet_address> ret;
     for (auto& t : tm.sorted_tokens()) {
-        range<token> r = tm.get_primary_range_for(t);
+        std::vector<nonwrapping_range<token>> r = tm.get_primary_ranges_for(t);
         auto eps = calculate_natural_endpoints(t, tm);
-        bool wrap = r.is_wrap_around(dht::token_comparator());
-        if (wrap) {
-            auto split_ranges = r.unwrap();
-            for (auto ep : eps) {
-                ret.emplace(split_ranges.first, ep);
-                ret.emplace(split_ranges.second, ep);
-            }
-        } else {
-            for (auto ep : eps) {
-                ret.emplace(r, ep);
-            }
+        for (auto ep : eps) {
+            for (auto&& rng : r)
+                ret.emplace(rng, ep);
         }
     }
     return ret;
 }
 
-std::vector<range<token>>
+std::vector<nonwrapping_range<token>>
 abstract_replication_strategy::get_pending_address_ranges(token_metadata& tm, token pending_token, inet_address pending_address) {
     return get_pending_address_ranges(tm, std::unordered_set<token>{pending_token}, pending_address);
 }
 
-std::vector<range<token>>
+std::vector<nonwrapping_range<token>>
 abstract_replication_strategy::get_pending_address_ranges(token_metadata& tm, std::unordered_set<token> pending_tokens, inet_address pending_address) {
-    std::vector<range<token>> ret;
+    std::vector<nonwrapping_range<token>> ret;
     auto temp = tm.clone_only_token_map();
     temp.update_normal_tokens(pending_tokens, pending_address);
     for (auto& x : get_address_ranges(temp)) {

database.cc
@@ -572,11 +572,6 @@ column_family::make_reader(schema_ptr s,
                            const query::partition_slice& slice,
                            const io_priority_class& pc,
                            tracing::trace_state_ptr trace_state) const {
-    if (query::is_wrap_around(range, *s)) {
-        // make_combined_reader() can't handle streams that wrap around yet.
-        fail(unimplemented::cause::WRAP_AROUND);
-    }
-
     std::vector<mutation_reader> readers;
     readers.reserve(_memtables->size() + 1);
 
@@ -618,10 +613,6 @@ column_family::make_streaming_reader(schema_ptr s,
                            const query::partition_range& range) const {
     auto& slice = query::full_slice;
     auto& pc = service::get_local_streaming_read_priority();
-    if (query::is_wrap_around(range, *s)) {
-        // make_combined_reader() can't handle streams that wrap around yet.
-        fail(unimplemented::cause::WRAP_AROUND);
-    }
 
     std::vector<mutation_reader> readers;
     readers.reserve(_memtables->size() + 1);
@@ -1357,13 +1348,13 @@ column_family::compact_sstables(sstables::compaction_descriptor descriptor, bool
 }
 
 static bool needs_cleanup(const lw_shared_ptr<sstables::sstable>& sst,
-                   const lw_shared_ptr<std::vector<range<dht::token>>>& owned_ranges,
+                   const lw_shared_ptr<std::vector<nonwrapping_range<dht::token>>>& owned_ranges,
                    schema_ptr s) {
     auto first = sst->get_first_partition_key();
     auto last = sst->get_last_partition_key();
     auto first_token = dht::global_partitioner().get_token(*s, first);
     auto last_token = dht::global_partitioner().get_token(*s, last);
-    range<dht::token> sst_token_range = range<dht::token>::make(first_token, last_token);
+    nonwrapping_range<dht::token> sst_token_range = nonwrapping_range<dht::token>::make(first_token, last_token);
 
     // return true iff sst partition range isn't fully contained in any of the owned ranges.
     for (auto& r : *owned_ranges) {
@@ -1375,8 +1366,8 @@ static bool needs_cleanup(const lw_shared_ptr<sstables::sstable>& sst,
 }
 
 future<> column_family::cleanup_sstables(sstables::compaction_descriptor descriptor) {
-    std::vector<range<dht::token>> r = service::get_local_storage_service().get_local_ranges(_schema->ks_name());
-    auto owned_ranges = make_lw_shared<std::vector<range<dht::token>>>(std::move(r));
+    std::vector<nonwrapping_range<dht::token>> r = service::get_local_storage_service().get_local_ranges(_schema->ks_name());
+    auto owned_ranges = make_lw_shared<std::vector<nonwrapping_range<dht::token>>>(std::move(r));
     auto sstables_to_cleanup = make_lw_shared<std::vector<sstables::shared_sstable>>(std::move(descriptor.sstables));
 
     return parallel_for_each(*sstables_to_cleanup, [this, owned_ranges = std::move(owned_ranges), sstables_to_cleanup] (auto& sst) {

db/size_estimates_recorder.cc
@@ -71,25 +71,16 @@ static std::vector<db::system_keyspace::range_estimates> estimates_for(const col
     std::vector<db::system_keyspace::range_estimates> estimates;
     estimates.reserve(local_ranges.size());
 
-    std::vector<query::partition_range> unwrapped;
-    // Each range defines both bounds.
-    for (auto& range : local_ranges) {
+    // Each range defines both bounds (with the call to compat::wrap)
+    for (auto& range : compat::wrap(local_ranges)) {
         int64_t count{0};
         utils::estimated_histogram hist{0};
-        unwrapped.clear();
-        if (range.is_wrap_around(dht::ring_position_comparator(*cf.schema()))) {
-            auto uw = range.unwrap();
-            unwrapped.push_back(std::move(uw.first));
-            unwrapped.push_back(std::move(uw.second));
-        } else {
-            unwrapped.push_back(range);
-        }
-        for (auto&& uwr : unwrapped) {
-            for (auto&& sstable : cf.select_sstables(uwr)) {
-                nonwrapping_range<dht::token> r(std::move(uwr).transform([](auto&& rp) { return rp.token(); }));
-                count += sstable->estimated_keys_for_range(r);
-                hist.merge(sstable->get_stats_metadata().estimated_row_size);
-            }
+        for (auto uwr : std::vector<query::partition_range>(compat::unwrap(range, *cf.schema()))) {
+          for (auto&& sstable : cf.select_sstables(uwr)) {
+            nonwrapping_range<dht::token> r(std::move(uwr).transform([](auto&& rp) { return rp.token(); }));
+            count += sstable->estimated_keys_for_range(r);
+            hist.merge(sstable->get_stats_metadata().estimated_row_size);
+          }
         }
         estimates.emplace_back(db::system_keyspace::range_estimates{
                 range.start()->value().token(),
@@ -97,7 +88,6 @@ static std::vector<db::system_keyspace::range_estimates> estimates_for(const col
                 count,
                 count > 0 ? hist.mean() : 0});
     }
-
     return estimates;
 }
 

dht/boot_strapper.cc
@@ -54,7 +54,7 @@ future<> boot_strapper::bootstrap() {
     for (const auto& keyspace_name : _db.local().get_non_system_keyspaces()) {
         auto& ks = _db.local().find_keyspace(keyspace_name);
         auto& strategy = ks.get_replication_strategy();
-        std::vector<range<token>> ranges = strategy.get_pending_address_ranges(_token_metadata, _tokens, _address);
+        std::vector<nonwrapping_range<token>> ranges = strategy.get_pending_address_ranges(_token_metadata, _tokens, _address);
         logger.debug("Will stream keyspace={}, ranges={}", keyspace_name, ranges);
         streamer->add_ranges(keyspace_name, ranges);
     }

dht/i_partitioner.cc
@@ -297,9 +297,9 @@ int ring_position::tri_compare(const schema& s, const ring_position& o) const {
     }
 }
 
-range<ring_position>
-to_partition_range(range<dht::token> r) {
-    using bound_opt = std::experimental::optional<range<ring_position>::bound>;
+nonwrapping_range<ring_position>
+to_partition_range(nonwrapping_range<dht::token> r) {
+    using bound_opt = std::experimental::optional<nonwrapping_range<ring_position>::bound>;
     auto start = r.start()
                  ? bound_opt(dht::ring_position(r.start()->value(),
                                                 r.start()->is_inclusive()

dht/i_partitioner.hh
@@ -422,7 +422,7 @@ i_partitioner& global_partitioner();
 
 unsigned shard_of(const token&);
 
-range<ring_position> to_partition_range(range<dht::token>);
+nonwrapping_range<ring_position> to_partition_range(nonwrapping_range<dht::token>);
 
 } // dht
 

dht/range_streamer.cc
@@ -53,22 +53,22 @@ logging::logger logger("range_streamer");
 
 using inet_address = gms::inet_address;
 
-static std::unordered_map<range<token>, std::unordered_set<inet_address>>
-unordered_multimap_to_unordered_map(const std::unordered_multimap<range<token>, inet_address>& multimap) {
-    std::unordered_map<range<token>, std::unordered_set<inet_address>> ret;
+static std::unordered_map<nonwrapping_range<token>, std::unordered_set<inet_address>>
+unordered_multimap_to_unordered_map(const std::unordered_multimap<nonwrapping_range<token>, inet_address>& multimap) {
+    std::unordered_map<nonwrapping_range<token>, std::unordered_set<inet_address>> ret;
     for (auto x : multimap) {
         ret[x.first].emplace(x.second);
     }
     return ret;
 }
 
-std::unordered_multimap<inet_address, range<token>>
-range_streamer::get_range_fetch_map(const std::unordered_multimap<range<token>, inet_address>& ranges_with_sources,
+std::unordered_multimap<inet_address, nonwrapping_range<token>>
+range_streamer::get_range_fetch_map(const std::unordered_multimap<nonwrapping_range<token>, inet_address>& ranges_with_sources,
                                     const std::unordered_set<std::unique_ptr<i_source_filter>>& source_filters,
                                     const sstring& keyspace) {
-    std::unordered_multimap<inet_address, range<token>> range_fetch_map_map;
+    std::unordered_multimap<inet_address, nonwrapping_range<token>> range_fetch_map_map;
     for (auto x : unordered_multimap_to_unordered_map(ranges_with_sources)) {
-        const range<token>& range_ = x.first;
+        const nonwrapping_range<token>& range_ = x.first;
         const std::unordered_set<inet_address>& addresses = x.second;
         bool found_source = false;
         for (auto address : addresses) {
@@ -103,8 +103,8 @@ range_streamer::get_range_fetch_map(const std::unordered_multimap<range<token>, 
     return range_fetch_map_map;
 }
 
-std::unordered_multimap<range<token>, inet_address>
-range_streamer::get_all_ranges_with_sources_for(const sstring& keyspace_name, std::vector<range<token>> desired_ranges) {
+std::unordered_multimap<nonwrapping_range<token>, inet_address>
+range_streamer::get_all_ranges_with_sources_for(const sstring& keyspace_name, std::vector<nonwrapping_range<token>> desired_ranges) {
     logger.debug("{} ks={}", __func__, keyspace_name);
 
     auto& ks = _db.local().find_keyspace(keyspace_name);
@@ -113,7 +113,7 @@ range_streamer::get_all_ranges_with_sources_for(const sstring& keyspace_name, st
     auto tm = _metadata.clone_only_token_map();
     auto range_addresses = unordered_multimap_to_unordered_map(strat.get_range_addresses(tm));
 
-    std::unordered_multimap<range<token>, inet_address> range_sources;
+    std::unordered_multimap<nonwrapping_range<token>, inet_address> range_sources;
     auto& snitch = locator::i_endpoint_snitch::get_local_snitch_ptr();
     for (auto& desired_range : desired_ranges) {
         auto found = false;
@@ -137,8 +137,8 @@ range_streamer::get_all_ranges_with_sources_for(const sstring& keyspace_name, st
     return range_sources;
 }
 
-std::unordered_multimap<range<token>, inet_address>
-range_streamer::get_all_ranges_with_strict_sources_for(const sstring& keyspace_name, std::vector<range<token>> desired_ranges) {
+std::unordered_multimap<nonwrapping_range<token>, inet_address>
+range_streamer::get_all_ranges_with_strict_sources_for(const sstring& keyspace_name, std::vector<nonwrapping_range<token>> desired_ranges) {
     logger.debug("{} ks={}", __func__, keyspace_name);
     assert (_tokens.empty() == false);
 
@@ -154,7 +154,7 @@ range_streamer::get_all_ranges_with_strict_sources_for(const sstring& keyspace_n
     auto pending_range_addresses  = unordered_multimap_to_unordered_map(strat.get_range_addresses(metadata_clone));
 
     //Collects the source that will have its range moved to the new node
-    std::unordered_multimap<range<token>, inet_address> range_sources;
+    std::unordered_multimap<nonwrapping_range<token>, inet_address> range_sources;
 
     for (auto& desired_range : desired_ranges) {
         for (auto& x : range_addresses) {
@@ -211,7 +211,7 @@ bool range_streamer::use_strict_sources_for_ranges(const sstring& keyspace_name)
            && _metadata.get_all_endpoints().size() != strat.get_replication_factor();
 }
 
-void range_streamer::add_ranges(const sstring& keyspace_name, std::vector<range<token>> ranges) {
+void range_streamer::add_ranges(const sstring& keyspace_name, std::vector<nonwrapping_range<token>> ranges) {
     auto ranges_for_keyspace = use_strict_sources_for_ranges(keyspace_name)
         ? get_all_ranges_with_strict_sources_for(keyspace_name, ranges)
         : get_all_ranges_with_sources_for(keyspace_name, ranges);
@@ -222,7 +222,7 @@ void range_streamer::add_ranges(const sstring& keyspace_name, std::vector<range<
         }
     }
 
-    std::unordered_map<inet_address, std::vector<range<token>>> range_fetch_map;
+    std::unordered_map<inet_address, std::vector<nonwrapping_range<token>>> range_fetch_map;
     for (auto& x : get_range_fetch_map(ranges_for_keyspace, _source_filters, keyspace_name)) {
         range_fetch_map[x.first].emplace_back(x.second);
     }
@@ -252,8 +252,8 @@ future<streaming::stream_state> range_streamer::fetch_async() {
     return _stream_plan.execute();
 }
 
-std::unordered_multimap<inet_address, range<token>>
-range_streamer::get_work_map(const std::unordered_multimap<range<token>, inet_address>& ranges_with_source_target,
+std::unordered_multimap<inet_address, nonwrapping_range<token>>
+range_streamer::get_work_map(const std::unordered_multimap<nonwrapping_range<token>, inet_address>& ranges_with_source_target,
              const sstring& keyspace) {
     auto filter = std::make_unique<dht::range_streamer::failure_detector_source_filter>(gms::get_local_failure_detector());
     std::unordered_set<std::unique_ptr<i_source_filter>> source_filters;

dht/range_streamer.hh
@@ -118,22 +118,22 @@ public:
         _source_filters.emplace(std::move(filter));
     }
 
-    void add_ranges(const sstring& keyspace_name, std::vector<range<token>> ranges);
+    void add_ranges(const sstring& keyspace_name, std::vector<nonwrapping_range<token>> ranges);
 private:
     bool use_strict_sources_for_ranges(const sstring& keyspace_name);
     /**
      * Get a map of all ranges and their respective sources that are candidates for streaming the given ranges
      * to us. For each range, the list of sources is sorted by proximity relative to the given destAddress.
      */
-    std::unordered_multimap<range<token>, inet_address>
-    get_all_ranges_with_sources_for(const sstring& keyspace_name, std::vector<range<token>> desired_ranges);
+    std::unordered_multimap<nonwrapping_range<token>, inet_address>
+    get_all_ranges_with_sources_for(const sstring& keyspace_name, std::vector<nonwrapping_range<token>> desired_ranges);
     /**
      * Get a map of all ranges and the source that will be cleaned up once this bootstrapped node is added for the given ranges.
      * For each range, the list should only contain a single source. This allows us to consistently migrate data without violating
      * consistency.
      */
-    std::unordered_multimap<range<token>, inet_address>
-    get_all_ranges_with_strict_sources_for(const sstring& keyspace_name, std::vector<range<token>> desired_ranges);
+    std::unordered_multimap<nonwrapping_range<token>, inet_address>
+    get_all_ranges_with_strict_sources_for(const sstring& keyspace_name, std::vector<nonwrapping_range<token>> desired_ranges);
 private:
     /**
      * @param rangesWithSources The ranges we want to fetch (key) and their potential sources (value)
@@ -141,14 +141,14 @@ private:
      *                      here, we always exclude ourselves.
      * @return
      */
-    static std::unordered_multimap<inet_address, range<token>>
-    get_range_fetch_map(const std::unordered_multimap<range<token>, inet_address>& ranges_with_sources,
+    static std::unordered_multimap<inet_address, nonwrapping_range<token>>
+    get_range_fetch_map(const std::unordered_multimap<nonwrapping_range<token>, inet_address>& ranges_with_sources,
                         const std::unordered_set<std::unique_ptr<i_source_filter>>& source_filters,
                         const sstring& keyspace);
 
 public:
-    static std::unordered_multimap<inet_address, range<token>>
-    get_work_map(const std::unordered_multimap<range<token>, inet_address>& ranges_with_source_target,
+    static std::unordered_multimap<inet_address, nonwrapping_range<token>>
+    get_work_map(const std::unordered_multimap<nonwrapping_range<token>, inet_address>& ranges_with_source_target,
                  const sstring& keyspace);
 #if 0
 
@@ -166,7 +166,7 @@ private:
     std::unordered_set<token> _tokens;
     inet_address _address;
     sstring _description;
-    std::unordered_multimap<sstring, std::unordered_map<inet_address, std::vector<range<token>>>> _to_fetch;
+    std::unordered_multimap<sstring, std::unordered_map<inet_address, std::vector<nonwrapping_range<token>>>> _to_fetch;
     std::unordered_set<std::unique_ptr<i_source_filter>> _source_filters;
     stream_plan _stream_plan;
 };

commit 1ce52d54324305759b0db31b26b23c97f836437a

locator: fix abstract_replication_strategy::get_ranges() and friends violating sort order
get_ranges() is supposed to return ranges in sorted order. However, a35136533d9f
broke this and returned the range that was supposed to be last in the second
position (e.g. [0, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9]). The broke cleanup, which
relied on the sort order to perform a binary search. Other users of the
get_ranges() family did not rely on the sort order.

Fixes #3872.
Message-Id: <20181019113613.1895-1-avi@scylladb.com>

locator/abstract_replication_strategy.cc
@@ -119,9 +119,17 @@ insert_token_range_to_sorted_container_while_unwrapping(
         const dht::token& tok,
         dht::token_range_vector& ret) {
     if (prev_tok < tok) {
-        ret.emplace_back(
-                dht::token_range::bound(prev_tok, false),
-                dht::token_range::bound(tok, true));
+        auto pos = ret.end();
+        if (!ret.empty() && !std::prev(pos)->end()) {
+            // We inserted a wrapped range (a, b] previously as
+            // (-inf, b], (a, +inf). So now we insert in the next-to-last
+            // position to keep the last range (a, +inf) at the end.
+            pos = std::prev(pos);
+        }
+        ret.insert(pos,
+                dht::token_range{
+                        dht::token_range::bound(prev_tok, false),
+                        dht::token_range::bound(tok, true)});
     } else {
         ret.emplace_back(
                 dht::token_range::bound(prev_tok, false),

tests/network_topology_strategy_test.cc
@@ -30,6 +30,7 @@
 #include <map>
 #include <iostream>
 #include <sstream>
+#include <boost/range/algorithm/adjacent_find.hpp>
 
 static logging::logger nlogger("NetworkTopologyStrategyLogger");
 
@@ -50,6 +51,27 @@ void print_natural_endpoints(double point, const std::vector<inet_address> v) {
     }
 
     nlogger.debug("{}", strm.str());
+}
+
+#ifndef SEASTAR_DEBUG
+static void verify_sorted(const dht::token_range_vector& trv) {
+    auto not_strictly_before = [] (const dht::token_range a, const dht::token_range b) {
+        return !b.start()
+                || !a.end()
+                || a.end()->value() > b.start()->value()
+                || (a.end()->value() == b.start()->value() && a.end()->is_inclusive() && b.start()->is_inclusive());
+    };
+    BOOST_CHECK(boost::adjacent_find(trv, not_strictly_before) == trv.end());
+}
+#endif
+
+static void check_ranges_are_sorted(abstract_replication_strategy* ars, gms::inet_address ep) {
+    // Too slow in debug mode
+#ifndef SEASTAR_DEBUG
+    verify_sorted(ars->get_ranges(ep));
+    verify_sorted(ars->get_primary_ranges(ep));
+    verify_sorted(ars->get_primary_ranges_within_dc(ep));
+#endif
 }
 
 void strategy_sanity_check(
@@ -150,6 +172,7 @@ void full_ring_check(const std::vector<ring_point>& ring_points,
         auto endpoints2 = ars_ptr->get_natural_endpoints(t2);
 
         endpoints_check(ars_ptr, endpoints2);
+        check_ranges_are_sorted(ars_ptr, rp.host);
         BOOST_CHECK(cache_hit_count + 1 == ars_ptr->get_cache_hits_count());
         BOOST_CHECK(endpoints1 == endpoints2);
     }