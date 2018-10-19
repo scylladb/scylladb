819b6f3406d89b7ceb43a3c11665915c2e4e4b4f
Duarte Nunes <duarte@scylladb.com>
2018-10-19 14:26:00 +0100

    service/storage_proxy: Add counters for delayed base writes
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- service/storage_proxy.cc
+++ service/storage_proxy.cc
@@ -728,8 +728,11 @@ storage_proxy::storage_proxy(distributed<database>& db, storage_proxy::config cf
         sm::make_queue_length("current_throttled_writes", [this] { return _throttled_writes.size(); },
                        sm::description("number of currently throttled write requests")),
 
+        sm::make_queue_length("current_throttled_base_writes", [this] { return _stats.throttled_base_writes; },
+                       sm::description("number of currently throttled base replica write requests")),
+
         sm::make_total_operations("throttled_writes", [this] { return _stats.throttled_writes; },
-                       sm::description("number of throttled write requests")),
+                                  sm::description("number of throttled write requests")),
 
         sm::make_current_bytes("queued_write_bytes", [this] { return _stats.queued_write_bytes; },
                        sm::description("number of bytes in pending write requests")),
