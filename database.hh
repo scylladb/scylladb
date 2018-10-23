d7849e6ea4bfa3dd4c8f549f5c8831f6e70d86d0
Piotr Sarna <sarna@scylladb.com>
2018-10-23 12:38:32 +0200

    database: add get_staging_sstable method
    This method can be used to check if sstable is staging,
    i.e. it shouldn't be compacted and it will not be used
    for generating view updates from other staging tables,
    and return proper shared_sstable pointer if it is.

--- database.hh
+++ database.hh
@@ -489,6 +489,10 @@ private:
 public:
     future<> add_sstable_and_update_cache(sstables::shared_sstable sst);
     void move_sstable_from_staging_in_thread(sstables::shared_sstable sst);
+    sstables::shared_sstable get_staging_sstable(uint64_t generation) {
+        auto it = _sstables_staging.find(generation);
+        return it != _sstables_staging.end() ? it->second : nullptr;
+    }
 private:
     void update_stats_for_new_sstable(uint64_t disk_space_used_by_sstable, const std::vector<unsigned>& shards_for_the_sstable) noexcept;
     // Adds new sstable to the set of sstables
