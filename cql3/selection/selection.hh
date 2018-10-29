65f21cc518491db65c4bb07ba98c52affc60aef1
Piotr Sarna <sarna@scylladb.com>
2018-10-29 12:58:32 +0100

    cql3: check filtering restrictions only if applicable
    Primary key restrictions should be checked only when they need
    filtering - otherwise it's superfluous, since they were already
    applied on query level.

--- cql3/selection/selection.hh
+++ cql3/selection/selection.hh
@@ -263,11 +263,17 @@ public:
     class restrictions_filter {
         ::shared_ptr<restrictions::statement_restrictions> _restrictions;
         const query_options& _options;
+        const bool _skip_pk_restrictions;
+        const bool _skip_ck_restrictions;
         mutable bool _current_partition_key_does_not_match = false;
         mutable bool _current_static_row_does_not_match = false;
     public:
-        restrictions_filter() = default;
-        explicit restrictions_filter(::shared_ptr<restrictions::statement_restrictions> restrictions, const query_options& options) : _restrictions(restrictions), _options(options) {}
+        explicit restrictions_filter(::shared_ptr<restrictions::statement_restrictions> restrictions, const query_options& options)
+                : _restrictions(restrictions)
+                , _options(options)
+                , _skip_pk_restrictions(!_restrictions->pk_restrictions_need_filtering())
+                , _skip_ck_restrictions(!_restrictions->ck_restrictions_need_filtering())
+        { }
         bool operator()(const selection& selection, const std::vector<bytes>& pk, const std::vector<bytes>& ck, const query::result_row_view& static_row, const query::result_row_view& row) const;
         void reset() {
             _current_partition_key_does_not_match = false;
