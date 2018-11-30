a6e25e4bd0d577e61995cecca0b9302e4e4ee337
Rafael Ávila de Espíndola <espindola@scylladb.com>
2018-11-29 17:33:06 -0800

    Try to read the full sst in broken_sst.
    With this patch we use data_consume_rows to try to read the entire
    sstable. The patch also adds a test with a particular corruption that
    would not be found without parsing the file.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>

--- tests/broken_sstable_test.cc
+++ tests/broken_sstable_test.cc
@@ -21,22 +21,58 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "sstables/data_consume_context.hh"
+#include "sstables/mp_row_consumer.hh"
 #include "tests/test-utils.hh"
 #include "sstable_test.hh"
 
 using namespace sstables;
 
+struct my_mp_row_consumer_reader : public mp_row_consumer_reader {
+    my_mp_row_consumer_reader(schema_ptr s) : mp_row_consumer_reader(std::move(s)) {}
+    virtual future<> fill_buffer(db::timeout_clock::time_point) override {
+        BOOST_FAIL("unexpected");
+        abort();
+    }
+    virtual void next_partition() override { BOOST_FAIL("unexpected"); }
+    virtual future<> fast_forward_to(const dht::partition_range&, db::timeout_clock::time_point) {
+        BOOST_FAIL("unexpected");
+        abort();
+    }
+    virtual void on_end_of_stream() override { BOOST_FAIL("unexpected"); }
+    virtual future<> fast_forward_to(position_range, db::timeout_clock::time_point) {
+        BOOST_FAIL("unexpected");
+        abort();
+    }
+};
+
 static void broken_sst(sstring dir, unsigned long generation, sstring msg) {
     // Using an empty schema for this function, which is only about loading
     // a malformed component and checking that it fails.
     auto s = make_lw_shared(schema({}, "ks", "cf", {}, {}, {}, {}, utf8_type));
-    auto sst = std::make_unique<sstable>(s, dir, generation, la, big);
     try {
-        sst->load().get();
+        sstable_ptr sstp = std::get<0>(reusable_sst(s, dir, generation).get());
+        auto r = std::make_unique<my_mp_row_consumer_reader>(s);
+        auto c = std::make_unique<mp_row_consumer_k_l>(r.get(), s, default_priority_class(),
+                                                       no_resource_tracking(),
+                                                       streamed_mutation::forwarding::no, sstp);
+        auto ctx = data_consume_rows<data_consume_rows_context>(*s, sstp, *c);
+        auto fut = repeat([&ctx] {
+            return ctx.read().then([&ctx] {
+                return ctx.eof() ? stop_iteration::yes : stop_iteration::no;
+            });
+        });
+        fut.get();
         BOOST_FAIL("expecting exception");
     } catch (malformed_sstable_exception& e) {
         BOOST_REQUIRE_EQUAL(sstring(e.what()), msg);
     }
+}
+
+SEASTAR_THREAD_TEST_CASE(bad_column_name) {
+    broken_sst("tests/sstables/bad_column_name", 58,
+               "Found 3 clustering elements in column name. Was not expecting that! in sstable "
+               "tests/sstables/bad_column_name/la-58-big-Data.db");
 }
 
 SEASTAR_THREAD_TEST_CASE(empty_toc) {
