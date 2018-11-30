b22ed754168f40c966f01a8569204f9ab67e9440
Tomasz Grabiec <tgrabiec@scylladb.com>
2018-11-30 12:13:48 +0100

    tests: libdeflate: Check both libdeflate and default checksummers

--- tests/libdeflate_test.cc
+++ tests/libdeflate_test.cc
@@ -26,25 +26,34 @@
 #include "make_random_string.hh"
 #include <seastar/core/print.hh>
 
-BOOST_AUTO_TEST_CASE(test_checksumming_matches_zlib) {
-    using reference_impl = zlib_crc32_checksummer;
-    auto rolling = libdeflate_crc32_checksummer::init_checksum();
-    BOOST_REQUIRE_EQUAL(rolling, reference_impl::init_checksum());
+template<typename ReferenceImpl, typename Impl>
+static
+void test() {
+    auto rolling = Impl::init_checksum();
+    BOOST_REQUIRE_EQUAL(rolling, ReferenceImpl::init_checksum());
 
     for (auto size : {0, 1, 2, 10, 13, 16, 17, 22, 31, 1024, 2000, 80000}) {
         auto data = make_random_string(size);
 
-        auto current = libdeflate_crc32_checksummer::checksum(data.cbegin(), data.size());
-        auto ref_current = reference_impl::checksum(data.cbegin(), data.size());
+        auto current = Impl::checksum(data.cbegin(), data.size());
+        auto ref_current = ReferenceImpl::checksum(data.cbegin(), data.size());
         BOOST_REQUIRE_EQUAL(current, ref_current);
 
-        auto new_rolling = libdeflate_crc32_checksummer::checksum(rolling, data.cbegin(), data.size());
-        auto ref_new_rolling = reference_impl::checksum(rolling, data.cbegin(), data.size());
+        auto new_rolling = Impl::checksum(rolling, data.cbegin(), data.size());
+        auto ref_new_rolling = ReferenceImpl::checksum(rolling, data.cbegin(), data.size());
         BOOST_REQUIRE_EQUAL(new_rolling, ref_new_rolling);
 
-        auto new_rolling_via_combine = libdeflate_crc32_checksummer::checksum_combine(rolling, current, data.size());
+        auto new_rolling_via_combine = Impl::checksum_combine(rolling, current, data.size());
         BOOST_REQUIRE_EQUAL(new_rolling, new_rolling_via_combine);
 
         rolling = new_rolling;
     }
+}
+
+BOOST_AUTO_TEST_CASE(test_libdeflate_matches_zlib) {
+    test<zlib_crc32_checksummer, libdeflate_crc32_checksummer>();
+}
+
+BOOST_AUTO_TEST_CASE(test_default_matches_zlib) {
+    test<zlib_crc32_checksummer, crc32_utils>();
 }
