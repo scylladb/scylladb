7febdb5a5cb93341618790801762e67b81110d78
Tomasz Grabiec <tgrabiec@scylladb.com>
2018-11-30 12:14:25 +0100

    tests: libdeflate: Add more tests for checksum_combine()

--- tests/libdeflate_test.cc
+++ tests/libdeflate_test.cc
@@ -28,6 +28,36 @@
 
 template<typename ReferenceImpl, typename Impl>
 static
+void test_combine() {
+    auto check = [](size_t len) {
+        auto c1 = Impl::checksum_combine(0x12381237, 0x73747474, len);
+        auto c2 = ReferenceImpl::checksum_combine(0x12381237, 0x73747474, len);
+        BOOST_REQUIRE_EQUAL(c1, c2);
+    };
+
+    check(0);
+    check(1);
+    check(2);
+    check(3);
+    check(8);
+    check(255);
+    check(256);
+    check(1023);
+    check(1024);
+    check(1025);
+    check(0xffff);
+    check(0x10000);
+    check(0xdeadbeef);
+    check(0xffffffff);
+    check(0x100000000);
+    check(0x200000000);
+    check(0x10000000001);
+    check(0x7eadbeefcafebabe);
+    check(0x7fffffffffffffff);
+}
+
+template<typename ReferenceImpl, typename Impl>
+static
 void test() {
     auto rolling = Impl::init_checksum();
     BOOST_REQUIRE_EQUAL(rolling, ReferenceImpl::init_checksum());
@@ -48,6 +78,8 @@ void test() {
 
         rolling = new_rolling;
     }
+
+    test_combine<ReferenceImpl, Impl>();
 }
 
 BOOST_AUTO_TEST_CASE(test_libdeflate_matches_zlib) {
