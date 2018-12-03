b1190c58ecbf9f28c43abdb95fea5a217044e2a6
Rafael Ávila de Espíndola <espindola@scylladb.com>
2018-12-03 13:26:06 -0800

    Convert tests to SEASTAR_THREAD_TEST_CASE.
    This will simplify future changes to broken_sst.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>

--- tests/broken_sstable_test.cc
+++ tests/broken_sstable_test.cc
@@ -26,49 +26,45 @@
 
 using namespace sstables;
 
-static future<> broken_sst(sstring dir, unsigned long generation, sstring msg) {
+static void broken_sst(sstring dir, unsigned long generation, sstring msg) {
     // Using an empty schema for this function, which is only about loading
     // a malformed component and checking that it fails.
     auto s = make_lw_shared(schema({}, "ks", "cf", {}, {}, {}, {}, utf8_type));
     auto sst = std::make_unique<sstable>(s, dir, generation, la, big);
-    auto fut = sst->load();
-    return std::move(fut).then_wrapped([sst = std::move(sst), msg = std::move(msg)] (future<> f) mutable {
-        try {
-            f.get();
-            BOOST_FAIL("expecting exception");
-        } catch (malformed_sstable_exception& e) {
-            BOOST_REQUIRE_EQUAL(sstring(e.what()), msg);
-        }
-        return make_ready_future<>();
-    });
+    try {
+        sst->load().get();
+        BOOST_FAIL("expecting exception");
+    } catch (malformed_sstable_exception& e) {
+        BOOST_REQUIRE_EQUAL(sstring(e.what()), msg);
+    }
 }
 
-SEASTAR_TEST_CASE(empty_toc) {
-    return broken_sst("tests/sstables/badtoc", 1,
-                      "Empty TOC in sstable tests/sstables/badtoc/la-1-big-TOC.txt");
+SEASTAR_THREAD_TEST_CASE(empty_toc) {
+    broken_sst("tests/sstables/badtoc", 1,
+               "Empty TOC in sstable tests/sstables/badtoc/la-1-big-TOC.txt");
 }
 
-SEASTAR_TEST_CASE(alien_toc) {
-    return broken_sst("tests/sstables/badtoc", 2,
-                      "tests/sstables/badtoc/la-2-big-Statistics.db: file not found");
+SEASTAR_THREAD_TEST_CASE(alien_toc) {
+    broken_sst("tests/sstables/badtoc", 2,
+               "tests/sstables/badtoc/la-2-big-Statistics.db: file not found");
 }
 
-SEASTAR_TEST_CASE(truncated_toc) {
-    return broken_sst("tests/sstables/badtoc", 3,
-                      "tests/sstables/badtoc/la-3-big-Statistics.db: file not found");
+SEASTAR_THREAD_TEST_CASE(truncated_toc) {
+    broken_sst("tests/sstables/badtoc", 3,
+               "tests/sstables/badtoc/la-3-big-Statistics.db: file not found");
 }
 
-SEASTAR_TEST_CASE(wrong_format_toc) {
-    return broken_sst("tests/sstables/badtoc", 4,
-                      "tests/sstables/badtoc/la-4-big-TOC.txt: file not found");
+SEASTAR_THREAD_TEST_CASE(wrong_format_toc) {
+    broken_sst("tests/sstables/badtoc", 4,
+               "tests/sstables/badtoc/la-4-big-TOC.txt: file not found");
 }
 
-SEASTAR_TEST_CASE(compression_truncated) {
-    return broken_sst("tests/sstables/badcompression", 1,
-                      "tests/sstables/badcompression/la-1-big-Statistics.db: file not found");
+SEASTAR_THREAD_TEST_CASE(compression_truncated) {
+    broken_sst("tests/sstables/badcompression", 1,
+               "tests/sstables/badcompression/la-1-big-Statistics.db: file not found");
 }
 
-SEASTAR_TEST_CASE(compression_bytes_flipped) {
-    return broken_sst("tests/sstables/badcompression", 2,
-                      "tests/sstables/badcompression/la-2-big-Statistics.db: file not found");
+SEASTAR_THREAD_TEST_CASE(compression_bytes_flipped) {
+    broken_sst("tests/sstables/badcompression", 2,
+               "tests/sstables/badcompression/la-2-big-Statistics.db: file not found");
 }
