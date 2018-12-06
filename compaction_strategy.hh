raphaelsc/scylla

compaction: introduce notion of compaction-strategy-aware major compaction
That's only the very first step which introduces the machinery for making
major compaction aware of all strategies. By the time being, default
implementation is used for them all which only suits size tiered.

Refs #1431.

Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

compaction_strategy.hh
@@ -61,6 +61,8 @@ public:
     // Return a list of sstables to be compacted after applying the strategy.
     compaction_descriptor get_sstables_for_compaction(column_family& cfs, std::vector<shared_sstable> candidates);
 
+    compaction_descriptor get_sstables_for_major_compaction(column_family& cf, std::vector<shared_sstable> candidates);
+
     std::vector<resharding_descriptor> get_resharding_jobs(column_family& cf, std::vector<shared_sstable> candidates);
 
     // Some strategies may look at the compacted and resulting sstables to

sstables/compaction_manager.cc
@@ -246,17 +246,16 @@ future<> compaction_manager::submit_major_compaction(column_family* cf) {
 
             // candidates are sstables that aren't being operated on by other compaction types.
             // those are eligible for major compaction.
-            // FIXME: we need to make major compaction compaction strategy aware. For example,
-            // leveled strategy may want to promote the merged sstables of a level N.
-            auto sstables = get_candidates(*cf);
-            auto compacting = compacting_sstable_registration(this, sstables);
+            sstables::compaction_strategy cs = cf->get_compaction_strategy();
+            sstables::compaction_descriptor descriptor = cs.get_sstables_for_major_compaction(*cf, get_candidates(*cf));
+            auto compacting = compacting_sstable_registration(this, descriptor.sstables);
 
             cmlog.info0("User initiated compaction started on behalf of {}.{}", cf->schema()->ks_name(), cf->schema()->cf_name());
             compaction_backlog_tracker user_initiated(std::make_unique<user_initiated_backlog_tracker>(_compaction_controller.backlog_of_shares(200), _available_memory));
-            return do_with(std::move(user_initiated), [this, cf, sstables = std::move(sstables)] (compaction_backlog_tracker& bt) mutable {
+            return do_with(std::move(user_initiated), [this, cf, descriptor = std::move(descriptor)] (compaction_backlog_tracker& bt) mutable {
                 register_backlog_tracker(bt);
-                return with_scheduling_group(_scheduling_group, [this, cf, sstables = std::move(sstables)] () mutable {
-                    return cf->compact_sstables(sstables::compaction_descriptor(std::move(sstables)));
+                return with_scheduling_group(_scheduling_group, [this, cf, descriptor = std::move(descriptor)] () mutable {
+                    return cf->compact_sstables(std::move(descriptor));
                 });
             }).then([compacting = std::move(compacting)] {});
         });

sstables/compaction_strategy.cc
@@ -662,6 +662,10 @@ compaction_descriptor compaction_strategy::get_sstables_for_compaction(column_fa
     return _compaction_strategy_impl->get_sstables_for_compaction(cfs, std::move(candidates));
 }
 
+compaction_descriptor compaction_strategy::get_sstables_for_major_compaction(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
+    return _compaction_strategy_impl->get_sstables_for_major_compaction(cf, std::move(candidates));
+}
+
 std::vector<resharding_descriptor> compaction_strategy::get_resharding_jobs(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
     return _compaction_strategy_impl->get_resharding_jobs(cf, std::move(candidates));
 }

sstables/compaction_strategy_impl.hh
@@ -67,6 +67,9 @@ protected:
 public:
     virtual ~compaction_strategy_impl() {}
     virtual compaction_descriptor get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) = 0;
+    virtual compaction_descriptor get_sstables_for_major_compaction(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
+        return compaction_descriptor(std::move(candidates));
+    }
     virtual std::vector<resharding_descriptor> get_resharding_jobs(column_family& cf, std::vector<sstables::shared_sstable> candidates);
     virtual void notify_completion(const std::vector<shared_sstable>& removed, const std::vector<shared_sstable>& added) { }
     virtual compaction_strategy_type type() const = 0;

scylladb/scylladb

compaction_strategy.hh
@@ -61,6 +61,8 @@ public:
     // Return a list of sstables to be compacted after applying the strategy.
     compaction_descriptor get_sstables_for_compaction(column_family& cfs, std::vector<shared_sstable> candidates);
 
+    compaction_descriptor get_major_compaction_job(column_family& cf, std::vector<shared_sstable> candidates);
+
     std::vector<resharding_descriptor> get_resharding_jobs(column_family& cf, std::vector<shared_sstable> candidates);
 
     // Some strategies may look at the compacted and resulting sstables to

sstables/compaction_manager.cc
@@ -260,17 +260,16 @@ future<> compaction_manager::submit_major_compaction(column_family* cf) {
 
             // candidates are sstables that aren't being operated on by other compaction types.
             // those are eligible for major compaction.
-            // FIXME: we need to make major compaction compaction strategy aware. For example,
-            // leveled strategy may want to promote the merged sstables of a level N.
-            auto sstables = get_candidates(*cf);
-            auto compacting = compacting_sstable_registration(this, sstables);
+            sstables::compaction_strategy cs = cf->get_compaction_strategy();
+            sstables::compaction_descriptor descriptor = cs.get_major_compaction_job(*cf, get_candidates(*cf));
+            auto compacting = compacting_sstable_registration(this, descriptor.sstables);
 
             cmlog.info0("User initiated compaction started on behalf of {}.{}", cf->schema()->ks_name(), cf->schema()->cf_name());
             compaction_backlog_tracker user_initiated(std::make_unique<user_initiated_backlog_tracker>(_compaction_controller.backlog_of_shares(200), _available_memory));
-            return do_with(std::move(user_initiated), [this, cf, sstables = std::move(sstables)] (compaction_backlog_tracker& bt) mutable {
+            return do_with(std::move(user_initiated), [this, cf, descriptor = std::move(descriptor)] (compaction_backlog_tracker& bt) mutable {
                 register_backlog_tracker(bt);
-                return with_scheduling_group(_scheduling_group, [this, cf, sstables = std::move(sstables)] () mutable {
-                    return cf->compact_sstables(sstables::compaction_descriptor(std::move(sstables)));
+                return with_scheduling_group(_scheduling_group, [this, cf, descriptor = std::move(descriptor)] () mutable {
+                    return cf->compact_sstables(std::move(descriptor));
                 });
             }).then([compacting = std::move(compacting)] {});
         });

sstables/compaction_strategy.cc
@@ -712,6 +712,10 @@ compaction_descriptor compaction_strategy::get_sstables_for_compaction(column_fa
     return _compaction_strategy_impl->get_sstables_for_compaction(cfs, std::move(candidates));
 }
 
+compaction_descriptor compaction_strategy::get_major_compaction_job(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
+    return _compaction_strategy_impl->get_major_compaction_job(cf, std::move(candidates));
+}
+
 std::vector<resharding_descriptor> compaction_strategy::get_resharding_jobs(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
     return _compaction_strategy_impl->get_resharding_jobs(cf, std::move(candidates));
 }

sstables/compaction_strategy_impl.hh
@@ -67,6 +67,9 @@ protected:
 public:
     virtual ~compaction_strategy_impl() {}
     virtual compaction_descriptor get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) = 0;
+    virtual compaction_descriptor get_major_compaction_job(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
+        return compaction_descriptor(std::move(candidates));
+    }
     virtual std::vector<resharding_descriptor> get_resharding_jobs(column_family& cf, std::vector<sstables::shared_sstable> candidates);
     virtual void notify_completion(const std::vector<shared_sstable>& removed, const std::vector<shared_sstable>& added) { }
     virtual compaction_strategy_type type() const = 0;