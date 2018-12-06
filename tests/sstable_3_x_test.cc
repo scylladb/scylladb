d07ab3b3efe3a23984d32aa97338e4c8ca1ff94d
Vladimir Krivopalov <vladimir@scylladb.com>
2018-12-06 15:51:21 -0800

    tests: Add stats metadata validation to write_range_tombstone_same_end_with_row
    Signed-off-by: Vladimir Krivopalov <vladimir@scylladb.com>

--- tests/sstable_3_x_test.cc
+++ tests/sstable_3_x_test.cc
@@ -4219,7 +4219,7 @@ SEASTAR_THREAD_TEST_CASE(test_write_range_tombstone_same_end_with_row) {
 
     // DELETE FROM range_tombstone_same_end_with_row  USING TIMESTAMP 1525385507816568 WHERE pk = 0 AND ck1 = 'aaa' AND ck2 <= 'bbb';
     {
-        gc_clock::time_point tp = gc_clock::time_point{} + gc_clock::duration{1529099073};
+        gc_clock::time_point tp = gc_clock::time_point{} + gc_clock::duration{1544091863};
         tombstone tomb{ts, tp};
         range_tombstone rt{clustering_key_prefix::from_single_value(*s, bytes("aaa")),
                            clustering_key_prefix::from_deeply_exploded(*s, {"aaa", "bbb"}), tomb};
@@ -4236,7 +4236,9 @@ SEASTAR_THREAD_TEST_CASE(test_write_range_tombstone_same_end_with_row) {
     lw_shared_ptr<memtable> mt = make_lw_shared<memtable>(s);
     mt->apply(mut);
 
-    write_and_compare_sstables(s, mt, table_name);
+    tmpdir tmp = write_and_compare_sstables(s, mt, table_name);
+    auto written_sst = validate_read(s, tmp.path, {mut});
+    validate_stats_metadata(s, written_sst, table_name);
 }
 
 SEASTAR_THREAD_TEST_CASE(test_write_overlapped_start_range_tombstones) {
