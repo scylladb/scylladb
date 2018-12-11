0b86ab0d2af8ba85fc3ed9d374a7c85c5e46162c
Avi Kivity <avi@scylladb.com>
2018-12-11 16:56:04 +0200

    build: build libdeflate with user selected C compiler
    If the user specified a C compiler, use it to build libdeflate.
    
    Fixes #3978.
    Message-Id: <20181211145604.14847-1-avi@scylladb.com>
    
    (cherry picked from commit 34a31a807d02c13a3ca7df717d6c9ce5206160fe)

--- configure.py
+++ configure.py
@@ -1288,7 +1288,7 @@ with open(buildfile, 'w') as f:
         f.write('build build/$mode/scylla-package.tar: package build/{mode}/scylla build/{mode}/iotune\n'.format(**locals()))
         f.write('    mode = {mode}\n'.format(**locals()))
         f.write('rule libdeflate.{mode}\n'.format(**locals()))
-        f.write('    command = make -C libdeflate BUILD_DIR=../build/{mode}/libdeflate/ CFLAGS="{libdeflate_cflags}"\n'.format(**locals()))
+        f.write('    command = make -C libdeflate BUILD_DIR=../build/{mode}/libdeflate/ CFLAGS="{libdeflate_cflags}" CC={args.cc}\n'.format(**locals()))
         f.write('build build/{mode}/libdeflate/libdeflate.a: libdeflate.{mode}\n'.format(**locals()))
 
     f.write('build {}: phony\n'.format(seastar_deps))
