scylladb/scylladb

commit fee4d2eb9b32ba2293b0db79747322e0a49d1eb9

compaction_manager: delay initialization of the compaction manager.
If the compaction manager is started, compactions may start (this is
regardless of whether or not we trigger them). The problem with that is
that they start at a time in which we are flushing the commitlog and the
initialization procedure waits for the commitlog to be fully flushed and
the resulting memtables flushed before we move on.

Because there are no incoming writes, the amount of shares in memtable
flushes decrease as memory used decreases and that can cause the startup
procedure to take a long time.

We have recently started to bump the shares manually for manual flushes.
While that guarantees that we will not drive the shares to zero, I will
make the argument that we can do better by making sure that those things
are, at this point, running alone: user experience is affected by
startup times and the bump we give to user-triggered operations will
only do so much. Even if we increase the shares a lot flushes will still
be fighting for resources with compactions and startup will take longer
than it could.

By making sure that flushes are this point running alone we improve the
user experience by making sure the startup is as fast as it can be.

Signed-off-by: Glauber Costa <glauber@scylladb.com>

database.cc
@@ -2228,7 +2228,6 @@ database::database(const db::config& cfg, database_config dbcfg)
     , _result_memory_limiter(dbcfg.available_memory / 10)
 {
     local_schema_registry().init(*this); // TODO: we're never unbound.
-    _compaction_manager->start();
     setup_metrics();
 
     _row_cache_tracker.set_compaction_scheduling_group(dbcfg.memory_compaction_scheduling_group);

main.cc
@@ -685,6 +685,11 @@ int main(int ac, char** av) {
                     cl->delete_segments(std::move(paths));
                 }
             }
+
+            db.invoke_on_all([&proxy] (database& db) {
+                db.get_compaction_manager().start();
+            }).get();
+
             // If the same sstable is shared by several shards, it cannot be
             // deleted until all shards decide to compact it. So we want to
             // start these compactions now. Note we start compacting only after

commit 245a0d953a1acecc8fe9ed95db7420d1ccfb65da

tests: cql_test_env: Start the compaction manager
Broken in fee4d2e

Not doing this results in compaction requests being ignored.

One effect of this is that perf_fast_forward produces many sstables instead of one.

Refs #3984
Refs #3983

Message-Id: <1544719540-10178-1-git-send-email-tgrabiec@scylladb.com>

tests/cql_test_env.cc
@@ -30,6 +30,7 @@
 #include <seastar/core/shared_ptr.hh>
 #include "utils/UUID_gen.hh"
 #include "service/migration_manager.hh"
+#include "sstables/compaction_manager.hh"
 #include "message/messaging_service.hh"
 #include "service/storage_service.hh"
 #include "auth/service.hh"
@@ -358,6 +359,10 @@ public:
             auto stop_db = defer([db] {
                 db->stop().get();
             });
+
+            db->invoke_on_all([] (database& db) {
+                db.get_compaction_manager().start();
+            }).get();
 
             // FIXME: split
             tst_init_ms_fd_gossiper(*feature_service, db::config::seed_provider_type()).get();