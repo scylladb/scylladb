201196130d7aa2221999dd96b595e0f0e98147c8
Duarte Nunes <duarte@scylladb.com>
2018-12-28 16:16:20 +0000

    db/view/view_update_from_staging_generator: Avoid creating more than one fiber
    If view_update_from_staging_generator::maybe_generate_view_updates()
    is called before view_update_from_staging_generator::start(), as can
    happen in main.cc, then we can potentially create more than one fiber,
    which leads to corrupted state and conflicting operations.
    
    To avoid this, use just one fiber and be explicit about notifying it
    that more work is needed, by leveraging a condition-variable.
    
    Fixes #4021
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- db/view/view_update_from_staging_generator.cc
+++ db/view/view_update_from_staging_generator.cc
@@ -23,44 +23,42 @@
 
 namespace db::view {
 
-
 future<> view_update_from_staging_generator::start() {
     _started = seastar::async([this]() mutable {
+      while (!_as.abort_requested()) {
+        if (_sstables_with_tables.empty()) {
+            _pending_sstables.wait().get();
+        }
         while (!_sstables_with_tables.empty()) {
             auto& entry = _sstables_with_tables.front();
             schema_ptr s = entry.t->schema();
-            if (_as.abort_requested()) {
-                return;
-            }
             flat_mutation_reader staging_sstable_reader = entry.sst->read_rows_flat(s);
             auto result = staging_sstable_reader.consume_in_thread(view_updating_consumer(s, _proxy, entry.sst, _as), db::no_timeout);
-            if (result == stop_iteration::no) {
+            if (result == stop_iteration::yes) {
+                break;
+            }
                 entry.t->move_sstable_from_staging_in_thread(entry.sst);
                 _registration_sem.signal();
                 _sstables_with_tables.pop_front();
-            }
         }
+      }
     });
     return make_ready_future<>();
 }
 
 future<> view_update_from_staging_generator::stop() {
     _as.request_abort();
+    _pending_sstables.signal();
     return std::move(_started);
 }
 
 future<> view_update_from_staging_generator::register_staging_sstable(sstables::shared_sstable sst, lw_shared_ptr<table> table) {
-    _sstables_with_tables.emplace_back(std::move(sst), std::move(table));
     if (_as.abort_requested()) {
         return make_ready_future<>();
     }
-    future<> restart = make_ready_future<>();
-    if (_started.available()) {
-        restart = start();
-    }
-    return restart.then([this] () {
-        return _registration_sem.wait(1);
-    });
+    _sstables_with_tables.emplace_back(std::move(sst), std::move(table));
+    _pending_sstables.signal();
+    return _registration_sem.wait(1);
 }
 
 }
