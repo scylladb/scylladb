9d467156131feaf5a3e93a92167bec9912d43fc9
Piotr Sarna <sarna@scylladb.com>
2019-01-02 15:20:48 +0100

    streaming,view: move view update checks to separate file
    Checking if view update path should be used for sstables
    is going to be reused in row level repair code,
    so relevant functions are moved to a separate header.

---
+++ db/view/view_update_checks.hh
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "db/system_distributed_keyspace.hh"
+#include "streaming/stream_reason.hh"
+#include <boost/range/adaptor/map.hpp>
+#include <boost/algorithm/cxx11/any_of.hpp>
+
+namespace db::view {
+
+future<bool> check_view_build_ongoing(db::system_distributed_keyspace& sys_dist_ks, const sstring& ks_name, const sstring& cf_name);
+future<bool> check_needs_view_update_path(db::system_distributed_keyspace& sys_dist_ks, const table& t, streaming::stream_reason reason);
+
+}
