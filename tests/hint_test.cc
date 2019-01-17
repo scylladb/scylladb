401684503d62d2a05b53d013fe72f6ca345454cf
Avi Kivity <avi@scylladb.com>
2019-01-17 19:03:31 +0200

    hint_test: de-template
    While cl_test is duplicated with commitlog_test, at least deduplicate it internally
    by converting it to an ordinary function.

--- tests/hint_test.cc
+++ tests/hint_test.cc
@@ -47,12 +47,11 @@
 
 using namespace db;
 
-template<typename Func>
-static future<> cl_test(commitlog::config cfg, Func && f) {
+static future<> cl_test(commitlog::config cfg, noncopyable_function<future<> (commitlog& log)> f) {
     tmpdir tmp;
     cfg.commit_log_location = tmp.path;
-    return commitlog::create_commitlog(cfg).then([f = std::forward<Func>(f)](commitlog log) mutable {
-        return do_with(std::move(log), [f = std::forward<Func>(f)](commitlog& log) {
+    return commitlog::create_commitlog(cfg).then([f = std::move(f)](commitlog log) mutable {
+        return do_with(std::move(log), [f = std::move(f)](commitlog& log) {
             return futurize_apply(f, log).finally([&log] {
                 return log.shutdown().then([&log] {
                     return log.clear();
