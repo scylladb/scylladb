859a1d8f36ca43c901273bdd8a8bcca6f7fdfcf2
Calle Wilund <calle@scylladb.com>
2019-02-06 15:27:13 +0000

    cql_test_env: Add truncation migration to init

--- tests/cql_test_env.cc
+++ tests/cql_test_env.cc
@@ -428,6 +428,8 @@ public:
             db::system_keyspace::init_local_cache().get();
             auto stop_local_cache = defer([] { db::system_keyspace::deinit_local_cache().get(); });
 
+            db::system_keyspace::migrate_truncation_records().get();
+
             service::get_local_storage_service().init_messaging_service_part().get();
             service::get_local_storage_service().init_server_without_the_messaging_service_part(service::bind_messaging_port(false)).get();
             auto deinit_storage_service_server = defer([auth_service] {
