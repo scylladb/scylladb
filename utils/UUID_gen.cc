a309f952d28710025085c3103bc202252d462a4b
Rafael Ávila de Espíndola <espindola@scylladb.com>
2019-02-13 14:04:43 -0800

    Delete dead code
    This code would have be to refactored by the next patch. Since it is
    commented out, just delete it.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>

--- utils/UUID_gen.cc
+++ utils/UUID_gen.cc
@@ -46,55 +46,6 @@
 
 namespace utils {
 
-
-#if 0
-private static byte[] hash(Collection<InetAddress> data)
-{
-    try
-    {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        for(InetAddress addr : data)
-            messageDigest.update(addr.getAddress());
-
-        return messageDigest.digest();
-    }
-    catch (NoSuchAlgorithmException nsae)
-    {
-        throw new RuntimeException("MD5 digest algorithm is not available", nsae);
-    }
-}
-
-
-static int64_t make_node()
-{
-   /*
-    * We don't have access to the MAC address but need to generate a node part
-    * that identify this host as uniquely as possible.
-    * The spec says that one option is to take as many source that identify
-    * this node as possible and hash them together. That's what we do here by
-    * gathering all the ip of this host.
-    * Note that FBUtilities.getBroadcastAddress() should be enough to uniquely
-    * identify the node *in the cluster* but it triggers DatabaseDescriptor
-    * instanciation and the UUID generator is used in Stress for instance,
-    * where we don't want to require the yaml.
-    */
-    Collection<InetAddress> localAddresses = FBUtilities.getAllLocalAddresses();
-    if (localAddresses.isEmpty())
-        throw new RuntimeException("Cannot generate the node component of the UUID because cannot retrieve any IP addresses.");
-
-    // ideally, we'd use the MAC address, but java doesn't expose that.
-    byte[] hash = hash(localAddresses);
-    long node = 0;
-    for (int i = 0; i < Math.min(6, hash.length); i++)
-        node |= (0x00000000000000ff & (long)hash[i]) << (5-i)*8;
-    assert (0xff00000000000000L & node) == 0;
-
-    // Since we don't use the mac address, the spec says that multicast
-    // bit (least significant bit of the first octet of the node ID) must be 1.
-    return node | 0x0000010000000000L;
-}
-#endif
-
 static int64_t make_node()
 {
     // FIXME: Mix-in node's address. See the above commented-out code
