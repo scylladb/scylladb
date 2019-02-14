460fb260cbc3f2318fa3d53c80fa8cf7117e0fb6
Piotr Jastrzebski <piotr@scylladb.com>
2019-02-14 15:46:34 +0100

    feature: add when_enabled callbacks
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- gms/feature.hh
+++ gms/feature.hh
@@ -21,7 +21,12 @@
 
 #pragma once
 
+#include <boost/signals2.hpp>
+#include <boost/signals2/dummy_mutex.hpp>
+
 #include <seastar/core/shared_future.hh>
+
+namespace bs2 = boost::signals2;
 
 namespace gms {
 
@@ -34,12 +39,33 @@ class feature_service;
  * A feature should only be created once the gossiper is available.
  */
 class feature final {
+    using signal_type = bs2::signal_type<void (), bs2::keywords::mutex_type<bs2::dummy_mutex>>::type;
+
     feature_service* _service = nullptr;
     sstring _name;
     bool _enabled = false;
     mutable shared_promise<> _pr;
+    mutable signal_type _s;
     friend class gossiper;
 public:
+    class listener {
+        friend class feature;
+        bs2::scoped_connection _conn;
+        signal_type::slot_type _slot;
+        const signal_type::slot_type& get_slot() const { return _slot; }
+        void set_connection(bs2::scoped_connection&& conn) { _conn = std::move(conn); }
+        void callback() {
+            _conn.disconnect();
+            on_enabled();
+        }
+    public:
+        listener() : _slot(signal_type::slot_type(&listener::callback, this)) {}
+        listener(const listener&) = delete;
+        listener(listener&&) = delete;
+        listener& operator=(const listener&) = delete;
+        listener& operator=(listener&&) = delete;
+        virtual void on_enabled() = 0;
+    };
     explicit feature(feature_service& service, sstring name, bool enabled = false);
     feature() = default;
     ~feature();
@@ -56,6 +82,12 @@ public:
         return os << "{ gossip feature = " << f._name << " }";
     }
     future<> when_enabled() const { return _pr.get_shared_future(); }
+    void when_enabled(listener& callback) {
+        callback.set_connection(_s.connect(callback.get_slot()));
+        if (_enabled) {
+            _s();
+        }
+    }
 };
 
 } // namespace gms
