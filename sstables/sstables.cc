bhalevy/scylla-old

database: maybe_delete_large_partitions_entry: do not access sstable and do not mask exceptions
1. We would like to be able to call maybe_delete_large_partitions_entry
from the sstable destructor path in the future so the sstable might go away
while the large data entries are being deleted.

2. We would like the caller to handle any exception on this path,
especially in the prepatation part, before calling delete_large_partitions_entry().

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

sstables/sstables.cc
@@ -3139,7 +3139,8 @@ future<>
 maybe_delete_large_data_entry(shared_sstable sst, const db::large_data_handler& large_data_handler)
 {
     auto toc = sst->toc_filename();
-    return large_data_handler.maybe_delete_large_partitions_entry(*sst).then_wrapped([toc = std::move(toc)] (future<> f) {
+    return large_data_handler.maybe_delete_large_partitions_entry(*sst->get_schema(), sst->get_filename(), sst->data_size())
+            .then_wrapped([toc = std::move(toc)] (future<> f) {
         try {
             f.get();
         } catch (...) {

scylladb/scylladb

sstables/sstables.cc
@@ -3145,7 +3145,8 @@ static future<>
 maybe_delete_large_data_entry(shared_sstable sst, const db::large_data_handler& large_data_handler)
 {
     auto name = sst->get_filename();
-    return large_data_handler.maybe_delete_large_partitions_entry(*sst).then_wrapped([name = std::move(name)] (future<> f) {
+    return large_data_handler.maybe_delete_large_partitions_entry(*sst->get_schema(), name, sst->data_size())
+            .then_wrapped([name = std::move(name)] (future<> f) {
         if (f.failed()) {
             // Just log and ignore failures to delete large data entries.
             // They are not critical to the operation of the database.