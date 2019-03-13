6a6871aa0ed06af9ee40ab15e8e7d47def580abf
Piotr Sarna <sarna@scylladb.com>
2019-03-13 13:06:17 +0100

    view: check for computed columns in view
    Currently, having a 'computed' column in view update generation
    indicates that token value needs to be generated and assigned to it.

--- view_info.hh
+++ view_info.hh
@@ -58,8 +58,6 @@ public:
         return _raw.where_clause();
     }
 
-    bool is_index() const;
-
     cql3::statements::select_statement& select_statement() const;
     const query::partition_slice& partition_slice() const;
     const dht::partition_range_vector& partition_ranges() const;
