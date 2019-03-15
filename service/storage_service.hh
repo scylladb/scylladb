a0e02df36ace1caa685975ae8ade4454096bb46d
Piotr Sarna <sarna@scylladb.com>
2019-03-15 09:40:45 +0100

    service: add computed columns feature
    Computed columns feature should be checked before creating
    index schemas the new way - by adding computed column names
    to system_schema.computed_columns.

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -332,6 +332,7 @@ private:
     gms::feature _unbounded_range_tombstones_feature;
     gms::feature _view_virtual_columns;
     gms::feature _digest_insensitive_to_expiry;
+    gms::feature _computed_columns;
 
     sstables::sstable_version_types _sstables_format = sstables::sstable_version_types::ka;
     seastar::semaphore _feature_listeners_sem = {1};
@@ -2344,14 +2345,21 @@ public:
     bool cluster_supports_unbounded_range_tombstones() const {
         return bool(_unbounded_range_tombstones_feature);
     }
+
     const gms::feature& cluster_supports_view_virtual_columns() const {
         return _view_virtual_columns;
     }
     const gms::feature& cluster_supports_digest_insensitive_to_expiry() const {
         return _digest_insensitive_to_expiry;
     }
+
+    bool cluster_supports_computed_columns() const {
+        return bool(_computed_columns);
+    }
+
     // Returns schema features which all nodes in the cluster advertise as supported.
     db::schema_features cluster_schema_features() const;
+
 private:
     future<> set_cql_ready(bool ready);
 private:
