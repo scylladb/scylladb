psarna/scylla

index: fix indentation

index/secondary_index_manager.cc
@@ -120,26 +120,26 @@ view_ptr secondary_index_manager::create_view_for_index(const index_metadata& im
     }
 
     // For local indexing, start with base partition key
-  if (im.local()) {
-      if (index_target->is_partition_key()) {
-          throw std::runtime_error("Local indexing based on partition key column is not allowed,"
-                  " since whole base partition key must be used in queries anyway. Use global indexing instead.");
-      }
-      for (auto& col : schema->partition_key_columns()) {
-          builder.with_column(col.name(), col.type, column_kind::partition_key);
-      }
-      builder.with_column(index_target->name(), index_target->type, column_kind::clustering_key);
-  } else {
-    builder.with_column(index_target->name(), index_target->type, column_kind::partition_key);
-    // Additional token column is added to ensure token order on secondary index queries
-    bytes token_column_name = get_available_token_column_name(*schema);
-    builder.with_column(token_column_name, bytes_type, column_kind::clustering_key);
-    for (auto& col : schema->partition_key_columns()) {
-        if (col == *index_target) {
-            continue;
+    if (im.local()) {
+        if (index_target->is_partition_key()) {
+            throw std::runtime_error("Local indexing based on partition key column is not allowed,"
+                    " since whole base partition key must be used in queries anyway. Use global indexing instead.");
         }
-        builder.with_column(col.name(), col.type, column_kind::clustering_key);
-  }
+        for (auto& col : schema->partition_key_columns()) {
+            builder.with_column(col.name(), col.type, column_kind::partition_key);
+        }
+        builder.with_column(index_target->name(), index_target->type, column_kind::clustering_key);
+    } else {
+        builder.with_column(index_target->name(), index_target->type, column_kind::partition_key);
+        // Additional token column is added to ensure token order on secondary index queries
+        bytes token_column_name = get_available_token_column_name(*schema);
+        builder.with_column(token_column_name, bytes_type, column_kind::clustering_key);
+        for (auto& col : schema->partition_key_columns()) {
+            if (col == *index_target) {
+                continue;
+            }
+            builder.with_column(col.name(), col.type, column_kind::clustering_key);
+        }
     }
 
     for (auto& col : schema->clustering_key_columns()) {

scylladb/scylladb

index/secondary_index_manager.cc
@@ -121,26 +121,26 @@ view_ptr secondary_index_manager::create_view_for_index(const index_metadata& im
     }
 
     // For local indexing, start with base partition key
-  if (im.local()) {
-      if (index_target->is_partition_key()) {
-          throw exceptions::invalid_request_exception("Local indexing based on partition key column is not allowed,"
-                  " since whole base partition key must be used in queries anyway. Use global indexing instead.");
-      }
-      for (auto& col : schema->partition_key_columns()) {
-          builder.with_column(col.name(), col.type, column_kind::partition_key);
-      }
-      builder.with_column(index_target->name(), index_target->type, column_kind::clustering_key);
-  } else {
-    builder.with_column(index_target->name(), index_target->type, column_kind::partition_key);
-    // Additional token column is added to ensure token order on secondary index queries
-    bytes token_column_name = get_available_token_column_name(*schema);
-    builder.with_column(token_column_name, bytes_type, column_kind::clustering_key);
-    for (auto& col : schema->partition_key_columns()) {
-        if (col == *index_target) {
-            continue;
+    if (im.local()) {
+        if (index_target->is_partition_key()) {
+            throw exceptions::invalid_request_exception("Local indexing based on partition key column is not allowed,"
+                    " since whole base partition key must be used in queries anyway. Use global indexing instead.");
         }
-        builder.with_column(col.name(), col.type, column_kind::clustering_key);
-  }
+        for (auto& col : schema->partition_key_columns()) {
+            builder.with_column(col.name(), col.type, column_kind::partition_key);
+        }
+        builder.with_column(index_target->name(), index_target->type, column_kind::clustering_key);
+    } else {
+        builder.with_column(index_target->name(), index_target->type, column_kind::partition_key);
+        // Additional token column is added to ensure token order on secondary index queries
+        bytes token_column_name = get_available_token_column_name(*schema);
+        builder.with_column(token_column_name, bytes_type, column_kind::clustering_key);
+        for (auto& col : schema->partition_key_columns()) {
+            if (col == *index_target) {
+                continue;
+            }
+            builder.with_column(col.name(), col.type, column_kind::clustering_key);
+        }
     }
 
     for (auto& col : schema->clustering_key_columns()) {