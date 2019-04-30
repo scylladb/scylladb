830805e608f99f0df6c79f7bfca86c40d3834d56
Nadav Har'El <nyh@scylladb.com>
2019-04-30 14:55:49 +0300

    test_item.py: basic string put and get test
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- alternator-test/test_item.py
+++ alternator-test/test_item.py
@@ -1,9 +1,26 @@
 # Tests for the CRUD item operations: PutItem, GetItem, UpdateItem, DeleteItem
 
+import random
+import string
 
-# Basic test for creating a new item with a random name, and reading it back.
-def test_basic_put_and_get(test_table):
-    # TODO: random key
-    response = test_table.put_item(
-        Item={'p': 'hello', 'c': 'hi'})
-    print(response)
+def random_string(len=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(len))
+
+# Basic test for creating a new item with a random name, and reading it back
+# with strong consistency.
+# Only the string type is used for keys and attributes. None of the various
+# optional PutItem features (Expected, ReturnValues, ReturnConsumedCapacity,
+# ReturnItemCollectionMetrics, ConditionalOperator, ConditionExpression,
+# ExpressionAttributeNames, ExpressionAttributeValues) are used, and
+# for GetItem strong consistency is requested as well as all attributes,
+# but no other optional features (AttributesToGet, ReturnConsumedCapacity,
+# ProjectionExpression, ExpressionAttributeNames)
+def test_basic_string_put_and_get(test_table):
+    p = random_string()
+    c = random_string()
+    val = random_string()
+    test_table.put_item(Item={'p': p, 'c': c, 'attribute': val})
+    item = test_table.get_item(Key={'p': p, 'c': c}, ConsistentRead=True)['Item']
+    assert item['p'] == p
+    assert item['c'] == c
+    assert item['attribute'] == val
