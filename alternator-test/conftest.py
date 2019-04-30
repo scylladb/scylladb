4d7d871930a4e68871e64ab3be0a741e435694b8
Nadav Har'El <nyh@scylladb.com>
2019-04-30 12:53:03 +0300

    Start to use "test fixtures"
    Start to use "test fixtures" defined in conftest.py: The connection to
    the DynamoDB API, and also temporary tables, can be reused between multiple
    tests.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

---
+++ alternator-test/conftest.py
@@ -0,0 +1,66 @@
+# This file contains "test fixtures", a pytest concept described in
+# https://docs.pytest.org/en/latest/fixture.html.
+# A "fixture" is some sort of setup which an invididual test requires to run.
+# The fixture has setup code and teardown code, and if multiple tests
+# require the same fixture, it can be set up only once - while still allowing
+# the user to run individual tests and automatically set up the fixtures they need.
+
+
+
+import pytest
+import boto3
+import time
+
+# "dynamodb" fixture: set up client object for communicating with the DynamoDB
+# API. Currently, this connects to the real Amazon on us-east-1, but we should
+# make it optional whether to connect to AWS or a local Scylla installation.
+# We use scope="session" so that all tests will reuse the same client object.
+@pytest.fixture(scope="session")
+def dynamodb():
+    return boto3.resource('dynamodb',region_name='us-east-1') 
+
+test_table_prefix = 'alternator_test_'
+def test_table_name():
+    current_ms = int(round(time.time() * 1000))
+    # In the off chance that test_table_name() is called twice in the same millisecond...
+    if test_table_name.last_ms >= current_ms:
+        current_ms = test_table_name.last_ms + 1
+    test_table_name.last_ms = current_ms
+    return test_table_prefix + str(current_ms)
+test_table_name.last_ms = 0
+
+# "test_table" fixture:
+# TODO: explain: creates a table with a certain schema.
+# TODO: require a fixture which on teardown removes all tables with a particular
+# prefix.
+# We use scope="session" so that all tests will reuse the same client object.
+@pytest.fixture(scope="session")
+def test_table(dynamodb):
+    name = test_table_name()
+    print("test_table() fixture creating new table {}".format(name))
+    table = dynamodb.create_table(
+        TableName=name,
+        BillingMode='PAY_PER_REQUEST',
+        KeySchema=[
+            {
+                'AttributeName': 'p',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'c',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'p',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'c',
+                'AttributeType': 'S'
+            },
+        ],
+    )
+    table.meta.client.get_waiter('table_exists').wait(TableName=name)
+    return table
--- alternator-test/test_table.py
+++ alternator-test/test_table.py
@@ -1,14 +1,11 @@
 # Tests for the basic table operations: CreateTable, DeleteTable, DescribeTable.
 
 import pytest
-import boto3
-
-dynamodb = boto3.resource('dynamodb',region_name='us-east-1') 
 
 # Utility function for create a table with a given name and some valid
 # schema.. This function initiates the table's creation, but doesn't
 # wait for the table to actually become ready.
-def create_table(name):
+def create_table(dynamodb, name):
     return dynamodb.create_table(
         TableName=name,
         BillingMode='PAY_PER_REQUEST',
@@ -40,8 +37,8 @@ def create_table(name):
 # and DeleteTable to work correctly.
 # Note that in DynamoDB, table deletion takes a very long time, so tests
 # successfully using this function are very slow.
-def create_and_delete_table(name):
-    table = create_table(name)
+def create_and_delete_table(dynamodb, name):
+    table = create_table(dynamodb, name)
     table.meta.client.get_waiter('table_exists').wait(TableName=name)
     table.delete()
     table.meta.client.get_waiter('table_not_exists').wait(TableName=name)
@@ -54,13 +51,13 @@ def create_and_delete_table(name):
 # function properly in their basic use cases.
 # Unfortunately, this test is extremely slow with DynamoDB because deleting
 # a table is extremely slow until it really happens.
-def test_create_and_delete_table():
-    create_and_delete_table('nyh_test')
+def test_create_and_delete_table(dynamodb):
+    create_and_delete_table(dynamodb, 'alternator_test')
 
 # DynamoDB documentation specifies that table names must be 3-255 characters,
 # and match the regex [a-zA-Z0-9._-]+. Names not matching these rules should
 # be rejected, and no table be created.
-def test_create_table_unsupported_names():
+def test_create_table_unsupported_names(dynamodb):
     from botocore.exceptions import ParamValidationError, ClientError
     # Intererstingly, the boto library tests for names shorter than the
     # minimum length (3 characters) immediately, and failure results in
@@ -68,17 +65,17 @@ def test_create_table_unsupported_names():
     # DynamoDB, which returns an HTTP response code, which results in a
     # CientError exception.
     with pytest.raises(ParamValidationError):
-        create_table('n')
+        create_table(dynamodb, 'n')
     with pytest.raises(ParamValidationError):
-        create_table('nn')
+        create_table(dynamodb, 'nn')
     with pytest.raises(ClientError, match='at most 255 characters long'):
-        create_table('n' * 256)
+        create_table(dynamodb, 'n' * 256)
     with pytest.raises(ClientError, match='validation error detected'):
-        create_table('nyh@test')
+        create_table(dynamodb, 'nyh@test')
 
 # On the other hand, names following the above rules should be accepted. Even
 # names which the Scylla rules forbid, such as a name starting with .
-def test_create_and_delete_table_non_scylla_name():
-    create_and_delete_table('.hello.there')
+def test_create_and_delete_table_non_scylla_name(dynamodb):
+    create_and_delete_table(dynamodb, '.alternator_test')
 
 # TODO: check case sensitivity
