e6ff06094445425d3165cd34c2c099d9bccf6c11
Nadav Har'El <nyh@scylladb.com>
2019-05-02 15:54:58 +0300

    alternator: errors should be output from server as Dynamo drivers expect
    Exceptions from the handlers need to be output in a certain way - as
    a JSON with specific fields - as DynamoDB drivers expect them to be.
    If a handler throws an alternator::api_error with these specific fields,
    they are output, but any other exception is converted into the same
    format as an "Internal Error".
    
    After this patch, executor code can throw an alternator::api_error and
    the client will receive this error in the right format.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- alternator/server.cc
+++ alternator/server.cc
@@ -32,9 +32,68 @@ inline std::vector<sstring> split(const sstring& text, const char* separator) {
     return boost::split(tokens, text, boost::is_any_of(separator));
 }
 
+// DynamoDB HTTP error responses are structured as follows
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.Errors.html
+// Our handlers throw an exception to report an error. If the exception
+// is of type alternator_api_error, it unwrapped and properly reported to
+// the user directly. Other exceptions are unexpected, and reported as
+// Internal Server Error.
+class api_handler : public handler_base {
+public:
+    api_handler(const future_json_function& _handle) : _f_handle(
+         [_handle](std::unique_ptr<request> req, std::unique_ptr<reply> rep) {
+         return seastar::futurize_apply(_handle, std::move(req)).then_wrapped([rep = std::move(rep)](future<json::json_return_type> resf) mutable {
+             if (resf.failed()) {
+                 // Exceptions of type api_error are wrapped as JSON and
+                 // returned to the client as expected. Other types of
+                 // exceptions are unexpected, and returned to the user
+                 // as an internal server error:
+                 api_error ret;
+                 try {
+                     resf.get();
+                 } catch (api_error &ae) {
+                     ret = ae;
+                 } catch (...) {
+                     ret = api_error(reply::status_type::internal_server_error,
+                             "Internal Server Error",
+                             format("Internal server error: {}", std::current_exception()));
+                 }
+                 // FIXME: what is this version number?
+                 rep->_content += "{\"__type\":\"com.amazonaws.dynamodb.v20120810#" + ret._type + "\"," +
+                         "\"message\":\"" + ret._msg + "\"}";
+                 rep->_status = ret._http_code;
+                 slogger.trace("api_handler error case: {}", rep->_content);
+                 return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
+             }
+             slogger.trace("api_handler success case");
+             auto res = resf.get0();
+             if (res._body_writer) {
+                 rep->write_body("json", std::move(res._body_writer));
+             } else {
+                 rep->_content += res._res;
+             }
+             return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
+         });
+    }), _type("json") { }
+
+    api_handler(const api_handler&) = default;
+    future<std::unique_ptr<reply>> handle(const sstring& path,
+            std::unique_ptr<request> req, std::unique_ptr<reply> rep) override {
+        return _f_handle(std::move(req), std::move(rep)).then(
+                [this](std::unique_ptr<reply> rep) {
+                    rep->done(_type);
+                    return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
+                });
+    }
+
+protected:
+    future_handler_function _f_handle;
+    sstring _type;
+};
+
 
 void server::set_routes(routes& r) {
-    function_handler* handler = new function_handler([this](std::unique_ptr<request> req) -> future<json::json_return_type> {
+    api_handler* handler = new api_handler([this](std::unique_ptr<request> req) -> future<json::json_return_type> {
         slogger.warn("REQ {} {}", req->content, req->content_length);
         sstring target = req->get_header(TARGET);
         std::vector<sstring> split_target = split(target, ".");
