db49bc6141e29e28f25f79427c84d7dfdbd313c7
Nadav Har'El <nyh@scylladb.com>
2019-05-02 15:35:54 +0300

    alternator: add alternator::api_error exception type
    DynamoDB error messages are returned in JSON format and expect specific
    information: Some HTTP error code (often but not always 400), a string
    error "type" and a user-readable message. Code that wants to return
    user-visible exceptions should use this type, and in the next patch we
    will translate it to the appropriate JSON string.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- alternator/server.hh
+++ alternator/server.hh
@@ -28,5 +28,24 @@ private:
     void set_routes(seastar::httpd::routes& r);
 };
 
+// DynamoDB's error messages are described in detail in
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.Errors.html
+// An error message has a "type", e.g., "ResourceNotFoundException", a coarser
+// HTTP code (in the above case, 400), and a human readable message. Eventually these
+// will be wrapped into a JSON object returned to the client.
+class api_error : public std::exception {
+public:
+    reply::status_type _http_code;
+    sstring _type;
+    sstring _msg;
+    api_error(reply::status_type http_code, sstring type, sstring msg)
+        : _http_code(std::move(http_code))
+        , _type(std::move(type))
+        , _msg(std::move(msg))
+    { }
+    api_error() { }
+    virtual const char* what() const noexcept override { return _msg.begin(); }
+};
+
 }
 
