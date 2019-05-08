ce27949797ee9f1fb4715c4ccc27ffc71a3acd0c
Glauber Costa <glauber@scylladb.com>
2019-05-08 13:08:31 -0400

    Support AWS i3en instances
    AWS just released their new instances, the i3en instances.  The instance
    is verified already to work well with scylla, the only adjustments that
    we need is advertise that we support it, and pre-fill the disk
    information according to the performance numbers obtained by running the
    instance.
    
    Fixes #4486
    Branches: 3.1
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>
    Message-Id: <20190508170831.6003-1-glauber@scylladb.com>
    (cherry picked from commit a23531ebd50fb30c31e915b3755088514c734b50)

--- dist/common/scripts/scylla_util.py
+++ dist/common/scripts/scylla_util.py
@@ -119,7 +119,7 @@ class aws_instance:
         return self._type.split(".")[0]
 
     def is_supported_instance_class(self):
-        if self.instance_class() in ['i2', 'i3']:
+        if self.instance_class() in ['i2', 'i3', 'i3en']:
             return True
         return False
 
@@ -128,7 +128,7 @@ class aws_instance:
         instance_size = self.instance_size()
         if instance_class in ['c3', 'c4', 'd2', 'i2', 'r3']:
             return 'ixgbevf'
-        if instance_class in ['c5', 'c5d', 'f1', 'g3', 'h1', 'i3', 'm5', 'm5d', 'p2', 'p3', 'r4', 'x1']:
+        if instance_class in ['c5', 'c5d', 'f1', 'g3', 'h1', 'i3', 'i3en', 'm5', 'm5d', 'p2', 'p3', 'r4', 'x1']:
             return 'ena'
         if instance_class == 'm4':
             if instance_size == '16xlarge':
