c4c71989bfb067783d5ecee4f960f246d074b701
Nadav Har'El <nyh@scylladb.com>
2019-05-12 18:37:20 +0300

    alternator-test: test all attribute types
    Although we planned to initially support only string types, it turns out
    for the attributes (*not* the key), we actually support all types already,
    including all scalar types (string, number, bool, binary and null) and
    more complex types (list, nested document, and sets).
    
    This adds a tests which PutItem's these types and verifies that we can
    retrieve them.
    
    Note that this test deals with top-level attributes only. There is no
    attempt to modify only a nested attribute (and with the current code,
    it wouldn't work).
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- alternator-test/test_item.py
+++ alternator-test/test_item.py
@@ -5,6 +5,7 @@ import string
 
 import pytest
 from botocore.exceptions import ClientError
+from decimal import Decimal
 
 def random_string(len=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(len))
@@ -44,6 +45,46 @@ def test_basic_string_update_and_get(test_table):
     assert item['attribute'] == val
     assert item['another'] == val2
 
+# Test put_item and get_item of various types for the *attributes*,
+# including both scalars as well as nested documents, lists and sets.
+# The full list of types tested here:
+#    number, boolean, bytes, null, list, map, string set, number set,
+#    binary set.
+# The keys are still strings.
+# See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/customizations/dynamodb.html
+# on how to pass these various types to Boto3's put_item().
+def test_put_and_get_attribute_types(test_table):
+    p = random_string()
+    c = random_string()
+    num1 = Decimal("12.345")
+    num2 = 42
+    bool1 = True
+    bool2 = False
+    binary1 = b'xyz'
+    null1 = None
+    list1 = ['hello', 'world', 42]
+    map1 = {'hello': 'world', 'life': 42}
+    map2 = {'hello': {'test': 'hi', 'hello': True, 'list': [1, 2, 'hi']}}
+    ss1 = set(['hello', 'world', 'hi'])
+    ns1 = set([1, 42, Decimal("3.14")])
+    bs1 = set([b'xyz', b'hi'])
+    test_table.put_item(Item={'p': p, 'c': c, 'num1': num1, 'num2': num2,
+        'bool1': bool1, 'bool2': bool2, 'binary1': binary1, 'null1': null1, 'list1': list1,
+        'map1': map1, 'map2': map2, 'ss1': ss1, 'ns1': ns1, 'bs1': bs1 })
+    item = test_table.get_item(Key={'p': p, 'c': c}, ConsistentRead=True)['Item']
+    assert item['p'] == p
+    assert item['c'] == c
+    assert item['num1'] == num1
+    assert item['num2'] == num2
+    assert item['bool1'] == bool1
+    assert item['bool2'] == bool2
+    assert item['binary1'] == binary1
+    assert item['null1'] == null1
+    assert item['list1'] == list1
+    assert item['map1'] == map1
+    assert item['map2'] == map2
+    assert item['ss1'] == ss1
+    assert item['ns1'] == ns1
 
 # Test error handling of UpdateItem passed a bad "Action" field.
 def test_update_bad_action(test_table):
