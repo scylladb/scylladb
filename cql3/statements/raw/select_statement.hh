6a682dc5a204cf6486478873c38bb451b2e36048
Benny Halevy <bhalevy@scylladb.com>
2019-05-21 17:33:00 +0300

    cql3: select_statement: provide default initializer for parameters::_bypass_cache
    Fixes #4503
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    Message-Id: <20190521143300.22753-1-bhalevy@scylladb.com>
    (cherry picked from commit fae4ca756c02c3b3965324676c02cfbb9a1c0bda)

--- cql3/statements/raw/select_statement.hh
+++ cql3/statements/raw/select_statement.hh
@@ -76,7 +76,7 @@ public:
         const bool _is_distinct;
         const bool _allow_filtering;
         const bool _is_json;
-        bool _bypass_cache;
+        bool _bypass_cache = false;
     public:
         parameters();
         parameters(orderings_type orderings,
