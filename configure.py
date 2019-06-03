52810d1103bcf709450455b7643a7286320f88c2
Nadav Har'El <nyh@scylladb.com>
2019-06-03 13:20:38 +0300

    configure.py: move alternator source files to separate list
    For some unknown reason we put the list of alternator source files
    in configure.py inside the "api" list. Let's move it into a separate
    list.
    
    We could have just put it in the scylla_core list, but that would cause
    frequent and annoying patch conflicts when people add alternator source
    files and Scylla core source files concurrently.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- configure.py
+++ configure.py
@@ -771,11 +771,14 @@ api = ['api/api.cc',
        'api/system.cc',
        'api/config.cc',
        'api/api-doc/config.json',
+       ]
+
+alternator = [
        'alternator/server.cc',
        'alternator/executor.cc',
        'alternator/stats.cc',
        'alternator/base64.cc',
-       ]
+]
 
 idls = ['idl/gossip_digest.idl.hh',
         'idl/uuid.idl.hh',
@@ -821,7 +824,7 @@ scylla_tests_dependencies = scylla_core + idls + scylla_tests_generic_dependenci
 ]
 
 deps = {
-    'scylla': idls + ['main.cc', 'release.cc'] + scylla_core + api,
+    'scylla': idls + ['main.cc', 'release.cc'] + scylla_core + api + alternator,
 }
 
 pure_boost_tests = set([
