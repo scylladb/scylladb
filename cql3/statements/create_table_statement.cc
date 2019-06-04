6e29f5e826d9838cf19a3e409b891c4d89a36d69
Piotr Jastrzebski <piotr@scylladb.com>
2019-06-04 11:40:14 +0200

    create_table_statement: prepare announce_migration for cdc
    This patch wrapps announce_migration logic into a lambda
    that will be used both when cdc is used and when it's not.
    
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- cql3/statements/create_table_statement.cc
+++ cql3/statements/create_table_statement.cc
@@ -97,24 +97,27 @@ std::vector<column_definition> create_table_statement::get_columns()
 }
 
 future<shared_ptr<cql_transport::event::schema_change>> create_table_statement::announce_migration(service::storage_proxy& proxy, bool is_local_only) {
-    return make_ready_future<>().then([this, is_local_only, &proxy] {
-        return service::get_local_migration_manager().announce_new_column_family(get_cf_meta_data(proxy.get_db().local()), is_local_only);
-    }).then_wrapped([this] (auto&& f) {
-        try {
-            f.get();
-            using namespace cql_transport;
-            return make_shared<event::schema_change>(
-                    event::schema_change::change_type::CREATED,
-                    event::schema_change::target_type::TABLE,
-                    this->keyspace(),
-                    this->column_family());
-        } catch (const exceptions::already_exists_exception& e) {
-            if (_if_not_exists) {
-                return ::shared_ptr<cql_transport::event::schema_change>();
+    auto create_table = [this, is_local_only, &proxy] {
+        return make_ready_future<>().then([this, is_local_only, &proxy] {
+            return service::get_local_migration_manager().announce_new_column_family(get_cf_meta_data(proxy.get_db().local()), is_local_only);
+        }).then_wrapped([this] (auto&& f) {
+            try {
+                f.get();
+                using namespace cql_transport;
+                return make_shared<event::schema_change>(
+                        event::schema_change::change_type::CREATED,
+                        event::schema_change::target_type::TABLE,
+                        this->keyspace(),
+                        this->column_family());
+            } catch (const exceptions::already_exists_exception& e) {
+                if (_if_not_exists) {
+                    return ::shared_ptr<cql_transport::event::schema_change>();
+                }
+                throw e;
             }
-            throw e;
-        }
-    });
+        });
+    };
+    return create_table();
 }
 
 /**
