scylladb/scylladb

commit 513d01d53eff59b490ce313290b35e3bf9ec068d

build-ami: use current git branch for shallow-clone of other repos
We want to use the same branch on the other repos build-ami needs
as the one we're building for.  Automatically find the current branch
using the `git branch` command.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
Message-Id: <20190606133648.15877-1-bhalevy@scylladb.com>

dist/ami/build_ami.sh
@@ -82,12 +82,19 @@ if [ $LOCALRPM -eq 1 ]; then
         cp build/redhat/RPMS/x86_64/$PRODUCT-server-`cat build/SCYLLA-VERSION-FILE`-`cat build/SCYLLA-RELEASE-FILE`.*.x86_64.rpm dist/ami/files/$PRODUCT-server.x86_64.rpm
         cp build/redhat/RPMS/x86_64/$PRODUCT-debuginfo-`cat build/SCYLLA-VERSION-FILE`-`cat build/SCYLLA-RELEASE-FILE`.*.x86_64.rpm dist/ami/files/$PRODUCT-debuginfo.x86_64.rpm
     fi
+    branch_arg=${BRANCH:-$(git rev-parse --abbrev-ref HEAD))}
+    if [ -n "$branch_arg" ]; then
+        if [[ "$b" == '(HEAD detached at'* ]]; then
+            branch_arg=$(echo "$branch_arg" | sed -e 's/^(HEAD detached at.*\///' -e 's/)$//')
+        fi
+        branch_arg="-b $branch_arg"
+    fi
     if [ ! -f dist/ami/files/$PRODUCT-jmx.noarch.rpm ]; then
         cd build
         if [ ! -f $PRODUCT-jmx/reloc/build_reloc.sh ]; then
             # directory exists but file is missing, so need to try clone again
             rm -rf $PRODUCT-jmx
-            git clone --depth 1 https://github.com/scylladb/$PRODUCT-jmx.git
+            git clone $branch_arg --depth 1 https://github.com/scylladb/$PRODUCT-jmx.git
         else
             git pull
         fi
@@ -102,7 +109,7 @@ if [ $LOCALRPM -eq 1 ]; then
         if [ ! -f $PRODUCT-tools-java/reloc/build_reloc.sh ]; then
             # directory exists but file is missing, so need to try clone again
             rm -rf $PRODUCT-tools-java
-            git clone --depth 1 https://github.com/scylladb/$PRODUCT-tools-java.git
+            git clone $branch_arg --depth 1 https://github.com/scylladb/$PRODUCT-tools-java.git
         else
             git pull
         fi
@@ -118,7 +125,7 @@ if [ $LOCALRPM -eq 1 ]; then
         if [ ! -f $PRODUCT-ami/dist/redhat/build_rpm.sh ]; then
             # directory exists but file is missing, so need to try clone again
             rm -rf $PRODUCT-ami
-            git clone --depth 1 https://github.com/scylladb/$PRODUCT-ami.git
+            git clone $branch_arg --depth 1 https://github.com/scylladb/$PRODUCT-ami.git
         else
             git pull
         fi

commit 3ad005ba1782f595dd8d8cb70d2d90eabf0cdbf8

build-ami: fix branch detection failure when not in git tree
Introduced in 513d01d53eff59b490ce313290b35e3bf9ec068d

The script is trying to determine the branch to shallow clone
when an rpm is missing and has to be built.
This functionality in the current implementation assumes it is being run inside
a git repository, but that must not be the case if the script is triggered after
local rpms were placed on the local directory.
This happens when putting all necessary rpm files in: dist/ami/files
And then running: dist/ami/build_ami.sh --localrpm
The dist/ami/ and dist/ami/files are the only ones required for this action so
querying the git repository in that situation makes no sense.

Fixes #4535

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
Message-Id: <20190611112455.13862-1-bhalevy@scylladb.com>

dist/ami/build_ami.sh
@@ -82,7 +82,7 @@ if [ $LOCALRPM -eq 1 ]; then
         cp build/redhat/RPMS/x86_64/$PRODUCT-server-`cat build/SCYLLA-VERSION-FILE`-`cat build/SCYLLA-RELEASE-FILE`.*.x86_64.rpm dist/ami/files/$PRODUCT-server.x86_64.rpm
         cp build/redhat/RPMS/x86_64/$PRODUCT-debuginfo-`cat build/SCYLLA-VERSION-FILE`-`cat build/SCYLLA-RELEASE-FILE`.*.x86_64.rpm dist/ami/files/$PRODUCT-debuginfo.x86_64.rpm
     fi
-    branch_arg=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+    branch_arg=${BRANCH:-$(git rev-parse --abbrev-ref HEAD || echo -n)}
     if [ -n "$branch_arg" ]; then
         if [[ "$b" == '(HEAD detached at'* ]]; then
             branch_arg=$(echo "$branch_arg" | sed -e 's/^(HEAD detached at.*\///' -e 's/)$//')