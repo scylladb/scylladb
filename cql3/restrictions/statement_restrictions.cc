23df964b969b6ba2ff33d38c19cf3822f8627e65
Piotr Sarna <sarna@scylladb.com>
2019-06-12 08:59:57 +0200

    cql3: fix fetching clustering key columns for filtering
    When a column is not present in the select clause, but used for
    filtering, it usually needs to be fetched from replicas.
    Sometimes it can be avoided, e.g. if primary key columns form a valid
    prefix - then, they will be optimized out before filtering itself.
    However, clustering key prefix can only be qualified for this
    optimization if the whole partition key is restricted - otherwise
    the clustering columns still need to be present for filtering.
    
    This commit also fixes tests in cql_query_test suite, because they now
    expect more values - columns fetched for filtering will be present as
    well (only internally, the clients receive only data they asked for).
    
    Fixes #4541

--- cql3/restrictions/statement_restrictions.cc
+++ cql3/restrictions/statement_restrictions.cc
@@ -365,8 +365,9 @@ std::vector<const column_definition*> statement_restrictions::get_column_defs_fo
                 }
             }
         }
-        if (_clustering_columns_restrictions->needs_filtering(*_schema)) {
-            column_id first_filtering_id = _schema->clustering_key_columns().begin()->id +
+        const bool pk_has_unrestricted_components = _partition_key_restrictions->has_unrestricted_components(*_schema);
+        if (pk_has_unrestricted_components || _clustering_columns_restrictions->needs_filtering(*_schema)) {
+            column_id first_filtering_id = pk_has_unrestricted_components ? 0 : _schema->clustering_key_columns().begin()->id +
                     _clustering_columns_restrictions->num_prefix_columns_that_need_not_be_filtered();
             for (auto&& cdef : _clustering_columns_restrictions->get_column_defs()) {
                 if (cdef->id >= first_filtering_id && !column_uses_indexing(cdef)) {
