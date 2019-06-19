73aff1fc95b44ec26acb25a6a417ca1dcabb2bc5
Glauber Costa <glauber@scylladb.com>
2019-06-19 07:08:50 -0400

    api: export system uptime via REST
    This will be useful for tools like nodetool that want to query the uptime
    of the system.
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>
    Message-Id: <20190619110850.14206-1-glauber@scylladb.com>

--- api/system.cc
+++ api/system.cc
@@ -30,6 +30,10 @@ namespace api {
 namespace hs = httpd::system_json;
 
 void set_system(http_context& ctx, routes& r) {
+    hs::get_system_uptime.set(r, [](const_req req) {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(engine().uptime()).count();
+    });
+
     hs::get_all_logger_names.set(r, [](const_req req) {
         return logging::logger_registry().get_all_logger_names();
     });
