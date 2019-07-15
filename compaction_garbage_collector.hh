5002ebb73f4313f10934879d16ed218917ae4de8
Botond Dénes <bdenes@scylladb.com>
2019-07-15 13:54:54 +0300

    Introduce compaction_garbage_collector interface
    This interface can be used to implement a garbage collector that
    collects atoms that are purged due to expiry during compaction.
    The intended usage is collecting purged atoms for safekeeping until the
    compaction process finishes safely, to be dropped only at the end when
    the compaction is known to have finished successfully.

---
+++ compaction_garbage_collector.hh
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "schema.hh"
+#include "types/collection.hh"
+
+class atomic_cell;
+class row_marker;
+
+class compaction_garbage_collector {
+public:
+    virtual ~compaction_garbage_collector() = default;
+    virtual void collect(column_id id, atomic_cell) = 0;
+    virtual void collect(column_id id, collection_type_impl::mutation) = 0;
+    virtual void collect(row_marker) = 0;
+};
