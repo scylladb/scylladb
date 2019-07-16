dbc3499fd15aa1cb7cc9cfac73442e68e11ad303
Calle Wilund <calle@scylladb.com>
2019-07-16 13:42:54 +0000

    server: Fix cql notification inet address serialization
    Fixes #4717
    
    Bug in ipv6 support series caused inet_address serialization
    to include an additional "size" parameter in the address chunk.
    
    Message-Id: <20190716134254.20708-1-calle@scylladb.com>

--- transport/server.cc
+++ transport/server.cc
@@ -1452,7 +1452,7 @@ void cql_server::response::write_inet(socket_address inet)
     auto addr = inet.addr();
     write_byte(uint8_t(addr.size()));
     auto * p = static_cast<const int8_t*>(addr.data());
-    write_bytes(bytes(p, p + addr.size()));
+    _body.write(bytes_view(p, addr.size()));
     write_int(inet.port());
 }
 
