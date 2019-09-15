6e807a56e1b43530bd377dc94191edb261dadf93
Benny Halevy <bhalevy@scylladb.com>
2019-09-15 17:06:53 +0300

    scyllatop: livedata: update all metrics based on new discovered list
    Update current results dictionary using the Metric.discover method.
    
    New results are added and missing results are marked as absent.
    (Both full metrics or specific keys)
    
    Previously, with prometheous, each metric.update called query_list
    resulting in O(n^2) when all metric were updated, like in the scylla_top
    dtest - causing test timeout when testing debug build.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tools/scyllatop/livedata.py
+++ tools/scyllatop/livedata.py
@@ -17,7 +17,7 @@ class LiveData(object):
             self._metricPatterns = metricPatterns
         else:
             self._metricPatterns = defaults.DEFAULT_METRIC_PATTERNS
-        self._initializeMetrics()
+        self._results = self._discoverMetrics()
         self._views = []
         self._stop = False
 
@@ -33,13 +33,14 @@ class LiveData(object):
     def measurements(self):
         return self._results.values()
 
-    def _initializeMetrics(self):
-        self._results = metric.Metric.discover(self._metric_source)
-        logging.debug('_initializeMetrics: {} results discovered'.format(len(self._results)))
-        for symbol in self._results:
+    def _discoverMetrics(self):
+        results = metric.Metric.discover(self._metric_source)
+        logging.debug('_discoverMetrics: {} results discovered'.format(len(results)))
+        for symbol in results:
             if not self._matches(symbol, self._metricPatterns):
-                self._results.pop(symbol)
-        logging.debug('_initializeMetrics: {} results matched'.format(len(self._results)))
+                results.pop(symbol)
+        logging.debug('_initializeMetrics: {} results matched'.format(len(results)))
+        return results
 
     def _matches(self, symbol, metricPatterns):
         for pattern in metricPatterns:
@@ -49,10 +50,20 @@ class LiveData(object):
         return False
 
     def go(self, mainLoop):
+        num_updated = 0
+        num_absent = 0
+        num_added = 0
         while not self._stop:
-            for metric_obj in self.measurements:
-                self._update(metric_obj)
-            logging.debug('go: updated {} measurements'.format(len(self.measurements)))
+            new_results = self._discoverMetrics();
+            for metric in self._results:
+                if not metric in new_results:
+                    metric_obj = self._results[metric]
+                    metric_obj.markAbsent()
+                    num_absent += 1
+            num_updated = len(self._results) - num_absent
+            num_added = len(new_results) - num_updated
+            self._results.update(new_results)
+            logging.debug('go: updated {} measurements, added {}, {} marked absent'.format(num_updated, num_added, num_absent))
 
             for view in self._views:
                 logging.debug('go: updating view {}'.format(view))
