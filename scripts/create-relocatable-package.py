d77171e10e306cd908482caffc4e2db9a25dfdd1
Avi Kivity <avi@scylladb.com>
2019-09-16 15:48:42 +0300

    build: adjust libthread_db file name to match gdb expectations
    gdb searches for libthread_db.so using its canonical name of libthread_db.so.1 rather
    than the file name of libthread_db-1.0.so, so use that name to store the file in the
    archive.
    
    Fixes #4996.

--- scripts/create-relocatable-package.py
+++ scripts/create-relocatable-package.py
@@ -78,7 +78,7 @@ for exe in executables:
     libs.update(ldd(exe))
 
 # manually add libthread_db for debugging thread
-libs.update({'libthread_db-1.0.so': '/lib64/libthread_db-1.0.so'})
+libs.update({'libthread_db.so.1': '/lib64/libthread_db-1.0.so'})
 
 ld_so = libs['ld.so']
 
