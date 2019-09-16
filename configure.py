8d6201a23b5dbf5808a1c0cebb514be81f7da558
Gleb Natapov <gleb@scylladb.com>
2019-09-16 15:21:01 +0300

    lwt: Add RPC verbs needed for paxos implementation
    Paxos protocol has three stages: prepare, accept, learn. This patch adds
    rpc verb for each of those stages. To be term compatible with Cassandra
    the patch calls those stages: prepare, propose, commit.

--- configure.py
+++ configure.py
@@ -819,6 +819,7 @@ idls = ['idl/gossip_digest.idl.hh',
         'idl/cache_temperature.idl.hh',
         'idl/view.idl.hh',
         'idl/messaging_service.idl.hh',
+        'idl/paxos.idl.hh',
         ]
 
 headers = find_headers('.', excluded_dirs=['idl', 'build', 'seastar', '.git'])
