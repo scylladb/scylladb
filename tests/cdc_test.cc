elcallio/scylla

test: check that drop table with cdc removes log and desc
Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

tests/cdc_test.cc
@@ -57,6 +57,8 @@ SEASTAR_THREAD_TEST_CASE(test_with_cdc_parameter) {
             alter_table_and_assert(!expected_cdc_value);
             alter_table_and_assert(expected_cdc_value);
             e.execute_cql("DROP TABLE ks.tbl").get();
+            e.require_table_dont_exists("ks", cdc::log_name("tbl")).get();
+            e.require_table_dont_exists("ks", cdc::desc_name("tbl")).get();
         };
         test("CREATE TABLE ks.tbl (a int PRIMARY KEY)", false);
         test("CREATE TABLE ks.tbl (a int PRIMARY KEY) WITH cdc = 'false'", false);

scylladb/scylladb

tests/cdc_test.cc
@@ -71,6 +71,8 @@ SEASTAR_THREAD_TEST_CASE(test_with_cdc_parameter) {
             e.execute_cql(format("ALTER TABLE ks.tbl WITH cdc = {}", alter2_prop)).get();
             assert_cdc(alter2_expected);
             e.execute_cql("DROP TABLE ks.tbl").get();
+            e.require_table_does_not_exist("ks", cdc::log_name("tbl")).get();
+            e.require_table_does_not_exist("ks", cdc::desc_name("tbl")).get();
         };
 
         test("", "{'enabled':'true'}", "{'enabled':'false'}", {false}, {true}, {false});