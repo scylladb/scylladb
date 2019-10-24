41db19c269fb3f3d6c83b2d0ed64a72aea9317a9
Piotr Sarna <sarna@scylladb.com>
2019-10-24 14:54:37 +0200

    alternator: add alternator health check
    The health check is performed simply by issuing a GET request
    to the alternator port - it returns the following status 200
    response when the server is healthy:
    
    $ curl -i localhost:8000
    HTTP/1.1 200 OK
    Content-Type: text/plain
    Content-Length: 23
    Server: Seastar httpd
    Date: 21 Oct 2019 12:55:33 GMT
    
    healthy: localhost:8000
    
    This commit comes with a test.
    Fixes #5050
    Message-Id: <3050b3819661ee19640c78372e655470c1e1089c.1571921618.git.sarna@scylladb.com>

---
+++ alternator-test/test_health.py
@@ -0,0 +1,34 @@
+# Copyright 2019 ScyllaDB
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+
+# Tests for the health check
+
+import requests
+
+# Test that a health check can be performed with a GET packet
+def test_health_works(dynamodb):
+    url = dynamodb.meta.client._endpoint.host
+    response = requests.get(url)
+    assert response.ok
+    assert response.content.decode('utf-8').strip()  == 'healthy: {}'.format(url.replace('https://', '').replace('http://', ''))
+
+# Test that a health check only works for the root URL ('/')
+def test_health_only_works_for_root_path(dynamodb):
+    url = dynamodb.meta.client._endpoint.host
+    for suffix in ['/abc', '/..', '/-', '/index.htm', '/health']:
+        response = requests.get(url + suffix)
+        assert response.status_code in range(400, 405)
