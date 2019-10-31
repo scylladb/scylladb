41cb16a526650fbafa1169e641cffbde7653f0d3
Piotr Dulikowski <piodul@scylladb.com>
2019-10-31 15:24:15 +0100

    tests/cql_query_test: add aggregate functions test
    Adds a test for min, max and avg functions for those primitive types for
    which those functions are working at the moment.

--- tests/cql_query_test.cc
+++ tests/cql_query_test.cc
@@ -1464,6 +1464,159 @@ SEASTAR_TEST_CASE(test_functions) {
     });
 }
 
+struct aggregate_function_test {
+    private:
+    cql_test_env& _e;
+    shared_ptr<const abstract_type> _column_type;
+    std::vector<data_value> _sorted_values;
+
+    sstring table_name() {
+        return "cf_" + _column_type->cql3_type_name();
+    }
+    void call_function_and_expect(const char* fname, data_type type, data_value expected) {
+        auto msg = _e.execute_cql(format("select {}(value) from {}", fname, table_name())).get0();
+        assert_that(msg).is_rows()
+            .with_size(1)
+            .with_column_types({type})
+            .with_row({
+                {expected.serialize()}
+            });
+    }
+public:
+    template<typename... T>
+    explicit aggregate_function_test(cql_test_env& e, shared_ptr<const abstract_type> column_type, T... sorted_values)
+        : _e(e), _column_type(column_type), _sorted_values{data_value(sorted_values)...}
+    {
+        const auto cf_name = table_name();
+        _e.create_table([column_type, cf_name] (sstring ks_name) {
+            return schema({}, ks_name, cf_name,
+                {{"pk", utf8_type}}, {{"ck", int32_type}}, {{"value", column_type}}, {}, utf8_type);
+        }).get();
+
+        auto prepared = _e.prepare(format("insert into {} (pk, ck, value) values ('key1', ?, ?)", cf_name)).get0();
+        for (int i = 0; i < (int)_sorted_values.size(); i++) {
+            const auto& value = _sorted_values[i];
+            BOOST_ASSERT(&*value.type() == &*_column_type);
+            std::vector<cql3::raw_value> raw_values {
+                cql3::raw_value::make_value(int32_type->decompose(int32_t(i))),
+                cql3::raw_value::make_value(value.serialize())
+            };
+            _e.execute_prepared(prepared, std::move(raw_values)).get();
+        }
+    }
+    aggregate_function_test& test_min() {
+        call_function_and_expect("min", _column_type, _sorted_values.front());
+        return *this;
+    }
+    aggregate_function_test& test_max() {
+        call_function_and_expect("max", _column_type, _sorted_values.back());
+        return *this;
+    }
+    aggregate_function_test& test_count() {
+        call_function_and_expect("count", long_type, int64_t(_sorted_values.size()));
+        return *this;
+    }
+    aggregate_function_test& test_sum(data_value expected_result) {
+        call_function_and_expect("sum", _column_type, expected_result);
+        return *this;
+    }
+    aggregate_function_test& test_avg(data_value expected_result) {
+        call_function_and_expect("avg", _column_type, expected_result);
+        return *this;
+    }
+    aggregate_function_test& test_min_max_count() {
+        test_min();
+        test_max();
+        test_count();
+        return *this;
+    }
+};
+
+SEASTAR_TEST_CASE(test_aggregate_functions) {
+    return do_with_cql_env_thread([] (cql_test_env& e) {
+        // Numeric types
+        aggregate_function_test(e, byte_type, int8_t(1), int8_t(2), int8_t(3))
+            .test_min_max_count()
+            .test_sum(int8_t(6))
+            .test_avg(int8_t(2));
+        aggregate_function_test(e, short_type, int16_t(1), int16_t(2), int16_t(3))
+            .test_min_max_count()
+            .test_sum(int16_t(6))
+            .test_avg(int16_t(2));
+        aggregate_function_test(e, int32_type, int32_t(1), int32_t(2), int32_t(3))
+            .test_min_max_count()
+            .test_sum(int32_t(6))
+            .test_avg(int32_t(2));
+        aggregate_function_test(e, long_type, int64_t(1), int64_t(2), int64_t(3))
+            .test_min_max_count()
+            .test_sum(int64_t(6))
+            .test_avg(int64_t(2));
+
+        aggregate_function_test(e, varint_type,
+            boost::multiprecision::cpp_int(1),
+            boost::multiprecision::cpp_int(2),
+            boost::multiprecision::cpp_int(3)
+        ).test_min_max_count()
+            .test_sum(boost::multiprecision::cpp_int(6))
+            .test_avg(boost::multiprecision::cpp_int(2));
+
+        aggregate_function_test(e, decimal_type,
+            big_decimal("1.0"),
+            big_decimal("2.0"),
+            big_decimal("3.0")
+        ).test_min_max_count()
+            .test_sum(big_decimal("6.0"))
+            .test_avg(big_decimal("2.0"));
+
+        aggregate_function_test(e, float_type, 1.0f, 2.0f, 3.0f)
+            .test_min_max_count()
+            .test_sum(6.0f)
+            .test_avg(2.0f);
+        aggregate_function_test(e, double_type, 1.0, 2.0, 3.0)
+            .test_min_max_count()
+            .test_sum(6.0)
+            .test_avg(2.0);
+
+        // Ordered types
+        aggregate_function_test(e, utf8_type, sstring("abcd"), sstring("efgh"), sstring("ijkl"))
+            .test_min_max_count();
+        aggregate_function_test(e, bytes_type, bytes("abcd"), bytes("efgh"), bytes("ijkl"))
+            .test_min_max_count();
+        aggregate_function_test(e, ascii_type,
+            ascii_native_type{"abcd"},
+            ascii_native_type{"efgh"},
+            ascii_native_type{"ijkl"}
+        ).test_min_max_count();
+
+        aggregate_function_test(e, simple_date_type,
+            simple_date_native_type{1},
+            simple_date_native_type{2},
+            simple_date_native_type{3}
+        ).test_min_max_count();
+
+        const db_clock::time_point now = db_clock::now();
+        aggregate_function_test(e, timestamp_type,
+            now,
+            now + std::chrono::seconds(1),
+            now + std::chrono::seconds(2)
+        ).test_min_max_count();
+
+        aggregate_function_test(e, timeuuid_type,
+            timeuuid_native_type{utils::UUID("00000000-0000-1000-0000-000000000000")},
+            timeuuid_native_type{utils::UUID("00000000-0000-1000-0000-000000000001")},
+            timeuuid_native_type{utils::UUID("00000000-0000-1000-0000-000000000002")}
+        ).test_min_max_count();
+
+        aggregate_function_test(e, uuid_type,
+            utils::UUID("00000000-0000-1000-0000-000000000000"),
+            utils::UUID("00000000-0000-1000-0000-000000000001"),
+            utils::UUID("00000000-0000-1000-0000-000000000002")
+        ).test_min_max_count();
+
+        aggregate_function_test(e, boolean_type, false, true).test_min_max_count();
+    });
+}
+
 static const api::timestamp_type the_timestamp = 123456789;
 SEASTAR_TEST_CASE(test_writetime_and_ttl) {
     return do_with_cql_env([] (cql_test_env& e) {
