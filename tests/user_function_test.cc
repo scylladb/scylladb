91754029661623fc4b8fa969d2ea07521813468f
Rafael Ávila de Espíndola <espindola@scylladb.com>
2019-11-25 12:53:23 -0800

    lua: fix test with boost 1.66
    The boost 1.67 release notes says
    
    Changed maximum supported year from 10000 to 9999 to resolve various issues
    
    So change the test to use a larger number so that we get an exception
    with both boost 1.66 and boost 1.67.
    
    Fixes #5344
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20191125205323.92300-1-espindola@scylladb.com>

--- tests/user_function_test.cc
+++ tests/user_function_test.cc
@@ -605,7 +605,7 @@ SEASTAR_TEST_CASE(test_user_function_timestamp_return) {
             {serialized(db_clock::time_point(db_clock::duration(int64_t(0x12de9b1e550))))}
         });
 
-        e.execute_cql("CREATE FUNCTION my_func6(val varint) CALLED ON NULL INPUT RETURNS timestamp LANGUAGE Lua AS 'return {year = 10000, month = 2, day = 3, hour = 4, min = 5, sec = 6 }';").get();
+        e.execute_cql("CREATE FUNCTION my_func6(val varint) CALLED ON NULL INPUT RETURNS timestamp LANGUAGE Lua AS 'return {year = 10001, month = 2, day = 3, hour = 4, min = 5, sec = 6 }';").get();
         auto fut = e.execute_cql("SELECT my_func6(val) FROM my_table;");
         BOOST_REQUIRE_EXCEPTION(fut.get(), boost::gregorian::bad_year, message_equals("Year is out of valid range: 1400..9999"));
 
