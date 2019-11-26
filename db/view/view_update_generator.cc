5d7c80c148ecbcd2c734cd8611eb68d3377c07f2
Benny Halevy <bhalevy@scylladb.com>
2019-11-26 13:18:02 +0200

    view_update_generator::start: fix indentation
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- db/view/view_update_generator.cc
+++ db/view/view_update_generator.cc
@@ -35,23 +35,23 @@ future<> view_update_generator::start() {
             }
             while (!_sstables_with_tables.empty()) {
                 auto& entry = _sstables_with_tables.front();
-              try {
-                schema_ptr s = entry.t->schema();
-                flat_mutation_reader staging_sstable_reader = entry.sst->read_rows_flat(s);
-                auto result = staging_sstable_reader.consume_in_thread(view_updating_consumer(s, _proxy, entry.sst, _as), db::no_timeout);
-                if (result == stop_iteration::yes) {
+                try {
+                    schema_ptr s = entry.t->schema();
+                    flat_mutation_reader staging_sstable_reader = entry.sst->read_rows_flat(s);
+                    auto result = staging_sstable_reader.consume_in_thread(view_updating_consumer(s, _proxy, entry.sst, _as), db::no_timeout);
+                    if (result == stop_iteration::yes) {
+                        break;
+                    }
+                } catch (...) {
+                    vug_logger.warn("Processing {} failed: {}. Will retry...", entry.sst->get_filename(), std::current_exception());
                     break;
                 }
-              } catch (...) {
-                vug_logger.warn("Processing {} failed: {}. Will retry...", entry.sst->get_filename(), std::current_exception());
-                break;
-              }
-              try {
-                entry.t->move_sstable_from_staging_in_thread(entry.sst);
-              } catch (...) {
-                // Move from staging will be retried upon restart.
-                vug_logger.warn("Moving {} from staging failed: {}. Ignoring...", entry.sst->get_filename(), std::current_exception());
-              }
+                try {
+                    entry.t->move_sstable_from_staging_in_thread(entry.sst);
+                } catch (...) {
+                    // Move from staging will be retried upon restart.
+                    vug_logger.warn("Moving {} from staging failed: {}. Ignoring...", entry.sst->get_filename(), std::current_exception());
+                }
                 _registration_sem.signal();
                 _sstables_with_tables.pop_front();
             }
