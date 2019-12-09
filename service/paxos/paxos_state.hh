898d2330a2026027ed464cde3104c6c805655b30
Gleb Natapov <gleb@scylladb.com>
2019-12-09 12:31:46 +0200

    lwt: drop invoke_on in paxos_state prepare and accept
    Since lwt requests are now running on an owning shard there is no longer
    a need to invoke cross shard call.

--- service/paxos/paxos_state.hh
+++ service/paxos/paxos_state.hh
@@ -80,12 +80,6 @@ class paxos_state {
     std::optional<proposal> _accepted_proposal;
     std::optional<proposal> _most_recent_commit;
 
-    static future<prepare_response> prepare_impl(tracing::trace_state_ptr tr_state, schema_ptr schema,
-            const query::read_command& cmd, const dht::token& token, const partition_key& key, utils::UUID ballot,
-            bool only_digest, query::digest_algorithm da, clock_type::time_point timeout);
-    static future<bool> accept_impl(tracing::trace_state_ptr tr_state, schema_ptr schema,
-            const dht::token& token, const proposal& proposal, clock_type::time_point timeout);
-
 public:
     static logging::logger logger;
 
