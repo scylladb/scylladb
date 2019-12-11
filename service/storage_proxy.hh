6bc18ba713dd16fc7bca8ff9952da351bdd04500
Pekka Enberg <penberg@scylladb.com>
2019-12-11 14:16:52 +0200

    storage_proxy: Remove reference to MBean interface
    The JMX interface is implemented by the scylla-jmx project, not scylla.
    Therefore, let's remove this historical reference to MBeans from
    storage_proxy.
    
    Message-Id: <20191211121652.22461-1-penberg@scylladb.com>

--- service/storage_proxy.hh
+++ service/storage_proxy.hh
@@ -143,7 +143,7 @@ public:
             const query::partition_slice& slice, api::timestamp_type ts) = 0;
 };
 
-class storage_proxy : public seastar::async_sharded_service<storage_proxy>, public service::endpoint_lifecycle_subscriber /*implements StorageProxyMBean*/ {
+class storage_proxy : public seastar::async_sharded_service<storage_proxy>, public service::endpoint_lifecycle_subscriber {
 public:
     using clock_type = lowres_clock;
     struct config {
