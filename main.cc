bc07b877a567dc75725a39f46e83f7e72d1e0f85
Rafael Ávila de Espíndola <espindola@scylladb.com>
2020-01-06 15:18:36 -0800

    main: Explicitly allow scylla core dumps
    I have not looked into the security reason for disabling it when
    a program has file capabilities.
    
    Fixes #5560
    
    [avi: remove extraneous semicolon]
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20200106231836.99052-1-espindola@scylladb.com>
    (cherry picked from commit b80852c447c7dfadf091c0d2601041dfe0332bfe)

--- main.cc
+++ main.cc
@@ -54,6 +54,7 @@
 #include <seastar/core/file.hh>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/prctl.h>
 #include "disk-error-handler.hh"
 #include "tracing/tracing.hh"
 #include "tracing/tracing_backend_registry.hh"
@@ -464,6 +465,15 @@ inline auto defer_with_log_on_error(Func&& func) {
 }
 
 int main(int ac, char** av) {
+    // Allow core dumps. The would be disabled by default if
+    // CAP_SYS_NICE was added to the binary, as is suggested by the
+    // epoll backend.
+    int r = prctl(PR_SET_DUMPABLE, 1, 0, 0, 0);
+    if (r) {
+        std::cerr << "Could not make scylla dumpable\n";
+        exit(1);
+    }
+
   int return_value = 0;
   try {
     // early check to avoid triggering
