scylladb/scylladb

commit 21dec3881cb8aceafee93c19df078fc91bac0670

debian-reloc: rename buld product to the name specified in SCYLLA-VERSION-GEN
When the product name is other than "scylla", the debian
packaging scripts go over all files that starts with "scylla-"
and change the prefix to be the actual product name.
However, if there are no such files in the directory
the script will fail since the renaming command will
get the wildcard string instrad of an actual file name.
This patch replaces the command with a command with
an equivalent desired effect that only operates on files
if there are any.

Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>
Message-Id: <20191230143250.18101-1-eliransin@scylladb.com>

dist/debian/build_deb.sh
@@ -132,9 +132,9 @@ ln -fv $RELOC_PKG_FULLPATH ../$PRODUCT-server_$SCYLLA_VERSION-$SCYLLA_RELEASE.or
 
 cp -al dist/debian/debian debian
 if [ "$PRODUCT" != "scylla" ]; then
-    for i in debian/scylla-*;do
-        mv $i ${i/scylla-/$PRODUCT-}
-    done
+    # rename all 'scylla-' prefixed artifacts in the debian folder to have the 
+    # product name as a prefix
+    find debian -maxdepth 1 -name "scylla-*" -exec bash -c 'mv $1 ${1/scylla-/$PRODUCT-}' _ {} \;
 fi
 ln -fv dist/common/sysconfig/scylla-server debian/$PRODUCT-server.default
 REVISION="1"

dist/debian/python3/build_deb.sh
@@ -126,9 +126,9 @@ ln -fv $RELOC_PKG_FULLPATH ../$PRODUCT-python3_$SCYLLA_VERSION-$SCYLLA_RELEASE.o
 
 cp -al dist/debian/python3/debian debian
 if [ "$PRODUCT" != "scylla" ]; then
-    for i in debian/scylla-*;do
-        mv $i ${i/scylla-/$PRODUCT-}
-    done
+    # rename all 'scylla-' prefixed artifacts in the debian folder to have the 
+    # product name as a prefix
+    find debian -maxdepth 1 -name "scylla-*" -exec bash -c 'mv $1 ${1/scylla-/$PRODUCT-}' _ {} \;
 fi
 REVISION="1"
 MUSTACHE_DIST="\"debian\": true, \"product\": \"$PRODUCT\", \"$PRODUCT\": true"

commit e64445d7e5fa3e0a25a7bd0b8de388c15392aa5d

debian-reloc: Propagate PRODUCT variable to renaming command in debian pkg
commit 21dec3881cb8aceafee93c19df078fc91bac0670 introduced
a bug that will cause scylla debian build to fail. This is
because the commit relied on the environment PRODUCT variable
to be exported (and as a result, to propogate to the rename
command that is executed by find in a subshell)
This commit fixes it by explicitly passing the PRODUCT variable
into the rename command.

Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>
Message-Id: <20200106102229.24769-1-eliransin@scylladb.com>

dist/debian/build_deb.sh
@@ -134,7 +134,7 @@ cp -al dist/debian/debian debian
 if [ "$PRODUCT" != "scylla" ]; then
     # rename all 'scylla-' prefixed artifacts in the debian folder to have the 
     # product name as a prefix
-    find debian -maxdepth 1 -name "scylla-*" -exec bash -c 'mv $1 ${1/scylla-/$PRODUCT-}' _ {} \;
+    find debian -maxdepth 1 -name "scylla-*" -exec bash -c 'mv $1 ${1/scylla-/$2-}' _ {} "$PRODUCT" \;
 fi
 ln -fv dist/common/sysconfig/scylla-server debian/$PRODUCT-server.default
 REVISION="1"

dist/debian/python3/build_deb.sh
@@ -128,7 +128,7 @@ cp -al dist/debian/python3/debian debian
 if [ "$PRODUCT" != "scylla" ]; then
     # rename all 'scylla-' prefixed artifacts in the debian folder to have the 
     # product name as a prefix
-    find debian -maxdepth 1 -name "scylla-*" -exec bash -c 'mv $1 ${1/scylla-/$PRODUCT-}' _ {} \;
+    find debian -maxdepth 1 -name "scylla-*" -exec bash -c 'mv $1 ${1/scylla-/$2-}' _ {} "$PRODUCT" \;
 fi
 REVISION="1"
 MUSTACHE_DIST="\"debian\": true, \"product\": \"$PRODUCT\", \"$PRODUCT\": true"