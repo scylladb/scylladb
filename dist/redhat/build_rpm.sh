ef88e1e822863140c278bcf6e08462149955de82
Hagit Segev <hagit.segev@scylladb.com>
2020-01-09 16:48:32 +0200

    CentOS RPMs: Remove target to enable general centos.

--- dist/redhat/build_rpm.sh
+++ dist/redhat/build_rpm.sh
@@ -4,15 +4,13 @@ PRODUCT=$(cat SCYLLA-PRODUCT-FILE)
 
 . /etc/os-release
 print_usage() {
-    echo "build_rpm.sh --rebuild-dep --target centos7 --reloc-pkg build/release/scylla-package.tar.gz"
+    echo "build_rpm.sh --rebuild-dep --reloc-pkg build/release/scylla-package.tar.gz"
     echo "  --dist  create a public distribution rpm"
-    echo "  --target target distribution in mock cfg name"
     echo "  --xtrace print command traces before executing command"
     echo "  --reloc-pkg specify relocatable package path"
     exit 1
 }
 DIST=false
-TARGET=
 RELOC_PKG=
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -20,7 +18,7 @@ while [ $# -gt 0 ]; do
             DIST=true
             shift 1
             ;;
-        "--target")
+        "--target") # This is obsolete, but I keep this in order not to break people's scripts.
             TARGET=$2
             shift 2
             ;;
@@ -56,26 +54,6 @@ if [ ! -e SCYLLA-RELOCATABLE-FILE ]; then
     exit 1
 fi
 
-if [ -z "$TARGET" ] || [ "$TARGET" = "centos" ]; then
-    TARGET=centos7
-fi
-
-if [ "$TARGET" = "fedora" ]; then
-    TARGET=fedora28
-fi
-
-if [[ ! $TARGET =~ fedora.*|centos.* ]]; then
-    echo "unknown distribution specified."
-    echo "can be used following target:"
-    echo "  centos"
-    echo "  centos7"
-    echo "  fedora"
-    echo "  fedora28"
-    echo
-    echo "when --target parameter not specified, centos7 will use as default value."
-    exit 1
-fi
-
 if [ -z "$RELOC_PKG" ]; then
     print_usage
     exit 1
@@ -102,7 +80,6 @@ fi
 RELOC_PKG_BASENAME=$(basename $RELOC_PKG)
 SCYLLA_VERSION=$(cat SCYLLA-VERSION-FILE)
 SCYLLA_RELEASE=$(cat SCYLLA-RELEASE-FILE)
-MUSTACHE_DIST="\"$TARGET\": true, \"target\": \"$TARGET\""
 
 RPMBUILD=$(readlink -f ../)
 mkdir -p $RPMBUILD/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
@@ -116,9 +93,5 @@ fi
 rpm_payload_opts=(--define "_binary_payload w2${xz_thread_param}.xzdio")
 
 ln -fv $RELOC_PKG $RPMBUILD/SOURCES/
-pystache dist/redhat/scylla.spec.mustache "{ \"version\": \"$SCYLLA_VERSION\", \"release\": \"$SCYLLA_RELEASE\", \"housekeeping\": $DIST, \"product\": \"$PRODUCT\", \"$PRODUCT\": true, \"reloc_pkg\": \"$RELOC_PKG_BASENAME\", $MUSTACHE_DIST }" > $RPMBUILD/SPECS/scylla.spec
-if [ "$TARGET" = "centos7" ]; then
-    rpmbuild -ba "${rpm_payload_opts[@]}" --define "_topdir $RPMBUILD" --define "dist .el7" $RPMBUILD/SPECS/scylla.spec
-else
-    rpmbuild -ba "${rpm_payload_opts[@]}" --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla.spec
-fi
+pystache dist/redhat/scylla.spec.mustache "{ \"version\": \"$SCYLLA_VERSION\", \"release\": \"$SCYLLA_RELEASE\", \"housekeeping\": $DIST, \"product\": \"$PRODUCT\", \"$PRODUCT\": true, \"reloc_pkg\": \"$RELOC_PKG_BASENAME\" }" > $RPMBUILD/SPECS/scylla.spec
+rpmbuild -ba "${rpm_payload_opts[@]}" --define "_topdir $RPMBUILD" $RPMBUILD/SPECS/scylla.spec
