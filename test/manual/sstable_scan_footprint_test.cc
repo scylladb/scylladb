e4616f92fe4c8b1bb2350547c5baaf87f856b48d
Botond Dénes <bdenes@scylladb.com>
2020-01-27 16:52:58 +0200

    test/manual/sstable_scan_footprint_test: improve memory consumption diagnostics
    This test is all about tracking measured memory consumption vs. real
    memory consumption. To make this easier add additional diagnostics:
    * enable seastar heap profiler for the duration of the reads (seastar
      has to be compiled with `-DSEASTAR_HEAPPROF`).
    * Add a stats collector, which periodically collects stats such as
      non-LSA free/used memory, LSA free/used memory and memory tracked by
      the reader concurrency semaphore. These stats are written to a `.csv`
      file, allowing importing them into a spreadsheet and processing them.

--- test/manual/sstable_scan_footprint_test.cc
+++ test/manual/sstable_scan_footprint_test.cc
@@ -33,6 +33,7 @@
 #include <seastar/core/reactor.hh>
 #include <seastar/core/units.hh>
 #include <seastar/core/future-util.hh>
+#include <seastar/core/timer.hh>
 #include <seastar/util/log.hh>
 
 static seastar::logger testlog("test");
@@ -50,6 +51,110 @@ thread_local std::default_random_engine local_random_engine;
 }
 
 namespace {
+
+class stats_collector {
+public:
+    struct params {
+        sstring output_file;
+        std::chrono::milliseconds period;
+    };
+
+    class collect_guard {
+        timer<> _t;
+    private:
+        explicit collect_guard(stats_collector& sc, std::chrono::milliseconds period)
+            : _t(std::bind(&stats_collector::capture_snapshot, std::ref(sc))) {
+            if (period.count()) {
+                _t.arm_periodic(period);
+                testlog.info("Start collecting stats");
+                sc.capture_snapshot();
+            }
+        }
+    private:
+        friend class stats_collector;
+    public:
+        ~collect_guard() {
+            testlog.info("Finish collecting stats");
+        }
+    };
+
+private:
+    struct data_point {
+        size_t lsa_used_memory;
+        size_t lsa_free_memory;
+        size_t non_lsa_used_memory;
+        size_t non_lsa_free_memory;
+        size_t reads_memory_consumption;
+        size_t reads;
+    };
+
+private:
+    std::optional<params> _params;
+    reader_concurrency_semaphore& _sem;
+    const reader_resources _initial_res;
+    utils::chunked_vector<data_point> _data_points;
+
+private:
+    void capture_snapshot() {
+        const auto mem_stats = memory::stats();
+        const auto lsa_stats = logalloc::shard_tracker().region_occupancy();
+        const auto res = _sem.available_resources();
+
+        data_point dp;
+        dp.lsa_used_memory = lsa_stats.used_space();
+        dp.lsa_free_memory = lsa_stats.total_space() - lsa_stats.used_space();
+        dp.non_lsa_used_memory = mem_stats.total_memory() - mem_stats.free_memory() - dp.lsa_used_memory;
+        dp.non_lsa_free_memory = mem_stats.free_memory() - dp.lsa_free_memory;
+        dp.reads_memory_consumption = _initial_res.memory - res.memory;
+        dp.reads = _initial_res.count - res.count;
+
+        _data_points.push_back(dp);
+    }
+
+public:
+    static std::optional<params> parse_params(boost::program_options::variables_map& app_config) {
+        if (!app_config.count("collect-stats")) {
+            return {};
+        }
+        return params{
+            app_config["stats-file"].as<sstring>(),
+            std::chrono::milliseconds(app_config["stats-period-ms"].as<unsigned>())};
+    }
+    stats_collector(table& tab, std::optional<params> p)
+        : _params(std::move(p))
+        , _sem(tab.read_concurrency_semaphore())
+        , _initial_res(_sem.available_resources()) {
+    }
+    stats_collector(const stats_collector&) = delete;
+    stats_collector(stats_collector&&) = delete;
+    collect_guard collect() {
+        return collect_guard{*this, _params ? _params->period : std::chrono::milliseconds(0)};
+    }
+    future<> write_stats() {
+        if (!_params) {
+            return make_ready_future<>();
+        }
+        return seastar::async([this] {
+            auto f = open_file_dma(_params->output_file, open_flags::create | open_flags::wo).get0();
+            auto os = make_file_output_stream(f, file_output_stream_options{});
+
+            {
+                const auto header = "lsa_used_memory,lsa_free_memory,non_lsa_used_memory,non_lsa_free_memory,reads_memory_consumption,reads\n";
+                os.write(header).get();
+            }
+
+            for (const auto& dp : _data_points) {
+                const auto line = format("{},{},{},{},{},{}\n", dp.lsa_used_memory, dp.lsa_free_memory, dp.non_lsa_used_memory, dp.non_lsa_free_memory,
+                        dp.reads_memory_consumption, dp.reads);
+                os.write(line.c_str()).get();
+            }
+
+            os.close().get();
+
+            testlog.info("Stats written to {}", _params->output_file);
+        });
+    }
+};
 
 void execute_reads(reader_concurrency_semaphore& sem, unsigned reads, unsigned concurrency, std::function<future<>(unsigned)> read) {
     const reader_resources initial_res = sem.available_resources();
@@ -107,6 +212,9 @@ int main(int argc, char** argv) {
         ("sstables", bpo::value<uint64_t>()->default_value(100), "")
         ("sstable-size", bpo::value<uint64_t>()->default_value(10000000), "")
         ("sstable-format", bpo::value<std::string>()->default_value("mc"), "Sstable format version to use during population")
+        ("collect-stats", "Enable collecting statistics.")
+        ("stats-file", bpo::value<sstring>()->default_value("stats.csv"), "Store statistics in the specified file.")
+        ("stats-period-ms", bpo::value<unsigned>()->default_value(100), "Tick period of the stats collection.")
         ;
 
     testing::local_random_engine.seed(std::random_device()());
@@ -136,6 +244,14 @@ int main(int argc, char** argv) {
             uint64_t sstables = app.configuration()["sstables"].as<uint64_t>();
             auto reads = app.configuration()["reads"].as<unsigned>();
             auto read_concurrency = app.configuration()["read-concurrency"].as<unsigned>();
+
+            std::optional<stats_collector::params> stats_collector_params;
+            try {
+                stats_collector_params = stats_collector::parse_params(app.configuration());
+            } catch (...) {
+                testlog.error("Error parsing stats collection parameters: {}", std::current_exception());
+                return;
+            }
 
             env.execute_cql(format("{} WITH compression = {{ 'sstable_compression': '{}' }} "
                                    "AND compaction = {{'class' : 'NullCompactionStrategy'}};",
@@ -193,7 +309,10 @@ int main(int argc, char** argv) {
             testlog.info("Occupancy before: {}", prev_occupancy);
 
             testlog.info("Reading");
+            stats_collector sc(tab, stats_collector_params);
             try {
+                auto _ = sc.collect();
+                memory::set_heap_profiling_enabled(true);
                 execute_reads(tab.read_concurrency_semaphore(), reads, read_concurrency, [&] (unsigned i) {
                     return env.execute_cql(format("select * from ks.test where pk = 0 and ck > {} limit 100;",
                             tests::random::get_int(rows / 2))).discard_result();
@@ -201,6 +320,8 @@ int main(int argc, char** argv) {
             } catch (...) {
                 testlog.error("Reads aborted due to exception: {}", std::current_exception());
             }
+            memory::set_heap_profiling_enabled(false);
+            sc.write_stats().get();
 
             auto occupancy = logalloc::shard_tracker().occupancy();
             testlog.info("Occupancy after: {}", occupancy);
