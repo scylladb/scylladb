c6ddd21c50cf461da2c720b4998b16320d6a804b
Pavel Emelyanov <xemul@scylladb.com>
2020-02-10 11:50:52 +0300

    repair_writer: Use db from repair_meta
    The caller of repair_writer.create_writer al ready
    have the needed reference on database, no need to
    get it from global storage_service instance.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- repair/row_level.cc
+++ repair/row_level.cc
@@ -485,7 +485,7 @@ public:
         _partition_opened.resize(_nr_peer_nodes, false);
     }
 
-    void create_writer(unsigned node_idx) {
+    void create_writer(database& db, unsigned node_idx) {
         if (_writer_done[node_idx]) {
             return;
         }
@@ -493,12 +493,11 @@ public:
         auto get_next_mutation_fragment = [this, node_idx] () mutable {
             return _mq[node_idx]->pop_eventually();
         };
-        auto& db = service::get_local_storage_service().db();
-        table& t = db.local().find_column_family(_schema->id());
+        table& t = db.find_column_family(_schema->id());
         _writer_done[node_idx] = mutation_writer::distribute_reader_and_consume_on_shards(_schema, dht::global_partitioner(),
                 make_generating_reader(_schema, std::move(get_next_mutation_fragment)),
                 [&db, estimated_partitions = this->_estimated_partitions] (flat_mutation_reader reader) {
-            auto& t = db.local().find_column_family(reader.schema());
+            auto& t = db.find_column_family(reader.schema());
             return db::view::check_needs_view_update_path(_sys_dist_ks->local(), t, streaming::stream_reason::repair).then([t = t.shared_from_this(), estimated_partitions, reader = std::move(reader)] (bool use_view_update_path) mutable {
                 //FIXME: for better estimations this should be transmitted from remote
                 auto metadata = mutation_source_metadata{};
@@ -1192,7 +1191,7 @@ private:
             _peer_row_hash_sets[node_idx] = boost::copy_range<std::unordered_set<repair_hash>>(row_diff |
                     boost::adaptors::transformed([] (repair_row& r) { thread::maybe_yield(); return r.hash(); }));
         }
-        _repair_writer.create_writer(node_idx);
+        _repair_writer.create_writer(_db.local(), node_idx);
         for (auto& r : row_diff) {
             if (update_buf) {
                 _working_row_buf_combined_hash.add(r.hash());
@@ -1214,7 +1213,7 @@ private:
         return to_repair_rows_list(rows).then([this] (std::list<repair_row> row_diff) {
             return do_with(std::move(row_diff), [this] (std::list<repair_row>& row_diff) {
                 unsigned node_idx = 0;
-                _repair_writer.create_writer(node_idx);
+                _repair_writer.create_writer(_db.local(), node_idx);
                 return do_for_each(row_diff, [this, node_idx] (repair_row& r) {
                     // The repair_row here is supposed to have
                     // mutation_fragment attached because we have stored it in
