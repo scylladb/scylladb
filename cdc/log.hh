9baea88086e3b278a03fdd43295f90427781a632
Piotr Dulikowski <piodul@scylladb.com>
2020-02-19 15:33:14 +0100

    cdc: distinguish open and closed ranges for range delete
    This patch causes inclusive and exclusive range deletes to be
    distinguished in cdc log. Previously, operations `range_delete_start`
    and `range_delete_end` were used for both inclusive and exclusive bounds
    in range deletes. Now, old operations were renamed to
    `range_delete_*_inclusive`, and for exclusive deletes, new operations
    `range_delete_*_exclusive` are used.
    
    Tests: unit(dev)

--- cdc/log.hh
+++ cdc/log.hh
@@ -123,7 +123,8 @@ struct db_context final {
 enum class operation : int8_t {
     // note: these values will eventually be read by a third party, probably not privvy to this
     // enum decl, so don't change the constant values (or the datatype).
-    pre_image = 0, update = 1, row_delete = 2, range_delete_start = 3, range_delete_end = 4, partition_delete = 5
+    pre_image = 0, update = 1, row_delete = 2, partition_delete = 3,
+    range_delete_start_inclusive = 4, range_delete_start_exclusive = 5, range_delete_end_inclusive = 6, range_delete_end_exclusive = 7
 };
 
 // cdc log data column operation
