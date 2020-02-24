0298a6270e687a850ad44789f7c33c672e7f91ac
Pavel Emelyanov <xemul@scylladb.com>
2020-02-24 22:46:56 +0300

    storage_service: Forget cql_config
    It needs the config purely to feed one into thrift/transport
    server, since the latter two no longer needs one, neither does
    the former.
    
    As a nice side effect -- some tests no longer have to carry
    the cql_config on board.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -74,10 +74,6 @@ class view_update_generator;
 }
 }
 
-namespace cql3 {
-class cql_config;
-}
-
 namespace cql_transport {
     class cql_server;
 }
@@ -157,7 +153,6 @@ private:
     distributed<database>& _db;
     gms::gossiper& _gossiper;
     sharded<auth::service>& _auth_service;
-    sharded<cql3::cql_config>& _cql_config;
     sharded<service::migration_notifier>& _mnotifier;
     // Note that this is obviously only valid for the current shard. Users of
     // this facility should elect a shard to be the coordinator based on any
@@ -187,7 +182,7 @@ private:
      */
     bool _for_testing;
 public:
-    storage_service(abort_source& as, distributed<database>& db, gms::gossiper& gossiper, sharded<auth::service>&, sharded<cql3::cql_config>& cql_config, sharded<db::system_distributed_keyspace>&, sharded<db::view::view_update_generator>&, gms::feature_service& feature_service, storage_service_config config, sharded<service::migration_notifier>& mn, locator::token_metadata& tm, /* only for tests */ bool for_testing = false);
+    storage_service(abort_source& as, distributed<database>& db, gms::gossiper& gossiper, sharded<auth::service>&, sharded<db::system_distributed_keyspace>&, sharded<db::view::view_update_generator>&, gms::feature_service& feature_service, storage_service_config config, sharded<service::migration_notifier>& mn, locator::token_metadata& tm, /* only for tests */ bool for_testing = false);
     void isolate_on_error();
     void isolate_on_commit_error();
 
@@ -927,7 +922,7 @@ public:
 };
 
 future<> init_storage_service(sharded<abort_source>& abort_sources, distributed<database>& db, sharded<gms::gossiper>& gossiper, sharded<auth::service>& auth_service,
-        sharded<cql3::cql_config>& cql_config, sharded<db::system_distributed_keyspace>& sys_dist_ks,
+        sharded<db::system_distributed_keyspace>& sys_dist_ks,
         sharded<db::view::view_update_generator>& view_update_generator, sharded<gms::feature_service>& feature_service,
         storage_service_config config, sharded<service::migration_notifier>& mn, sharded<locator::token_metadata>& tm);
 future<> deinit_storage_service();
--- test/lib/test_services.cc
+++ test/lib/test_services.cc
@@ -30,7 +30,6 @@
 #include "gms/gossiper.hh"
 #include "message/messaging_service.hh"
 #include "service/storage_service.hh"
-#include "cql3/cql_config.hh"
 
 
 class storage_service_for_tests::impl {
@@ -40,7 +39,6 @@ class storage_service_for_tests::impl {
     distributed<database> _db;
     db::config _cfg;
     sharded<auth::service> _auth_service;
-    sharded<cql3::cql_config> _cql_config;
     sharded<locator::token_metadata> _token_metadata;
     sharded<service::migration_notifier> _mnotif;
     sharded<db::system_distributed_keyspace> _sys_dist_ks;
@@ -60,15 +58,13 @@ public:
         netw::get_messaging_service().start(gms::inet_address("127.0.0.1"), 7000, false).get();
         service::storage_service_config sscfg;
         sscfg.available_memory = memory::stats().total_memory();
-        _cql_config.start().get();
-        service::get_storage_service().start(std::ref(_abort_source), std::ref(_db), std::ref(_gossiper), std::ref(_auth_service), std::ref(_cql_config), std::ref(_sys_dist_ks), std::ref(_view_update_generator), std::ref(_feature_service), sscfg, std::ref(_mnotif), std::ref(_token_metadata), true).get();
+        service::get_storage_service().start(std::ref(_abort_source), std::ref(_db), std::ref(_gossiper), std::ref(_auth_service), std::ref(_sys_dist_ks), std::ref(_view_update_generator), std::ref(_feature_service), sscfg, std::ref(_mnotif), std::ref(_token_metadata), true).get();
         service::get_storage_service().invoke_on_all([] (auto& ss) {
             ss.enable_all_features();
         }).get();
     }
     ~impl() {
         service::get_storage_service().stop().get();
-        _cql_config.stop().get();
         netw::get_messaging_service().stop().get();
         _db.stop().get();
         _gossiper.stop().get();
