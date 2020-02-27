72509911d9a79aaff21bc4ff94163941662098af
Botond Dénes <bdenes@scylladb.com>
2020-02-27 18:45:15 +0200

    multishard_mutation_query: query_mutations_on_all_shards(): remove default timeout

--- multishard_mutation_query.hh
+++ multishard_mutation_query.hh
@@ -68,4 +68,4 @@ future<std::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_tempera
         const dht::partition_range_vector& ranges,
         tracing::trace_state_ptr trace_state,
         uint64_t max_size,
-        db::timeout_clock::time_point timeout = db::no_timeout);
+        db::timeout_clock::time_point timeout);
