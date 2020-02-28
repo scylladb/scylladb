0859ecc22be4f079cb2f12a933151de84555e209
Juliusz Stasiewicz <juliusz.stasiewicz@scylladb.com>
2020-02-28 17:34:35 +0100

    cdc: distinguishing update from insert
    When incoming mutation contains live row marker the `operation` is
    described as "insert", not as an "update".
    
    Fixes #5723

--- cdc/log.hh
+++ cdc/log.hh
@@ -124,7 +124,8 @@ enum class operation : int8_t {
     // note: these values will eventually be read by a third party, probably not privvy to this
     // enum decl, so don't change the constant values (or the datatype).
     pre_image = 0, update = 1, row_delete = 2, partition_delete = 3,
-    range_delete_start_inclusive = 4, range_delete_start_exclusive = 5, range_delete_end_inclusive = 6, range_delete_end_exclusive = 7
+    range_delete_start_inclusive = 4, range_delete_start_exclusive = 5, range_delete_end_inclusive = 6, range_delete_end_exclusive = 7,
+    insert = 8,
 };
 
 // cdc log data column operation
