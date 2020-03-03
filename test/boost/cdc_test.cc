1046a9188353c55e2a5cd34dae5bf3fb02b2d666
Juliusz Stasiewicz <juliusz.stasiewicz@scylladb.com>
2020-03-03 17:36:19 +0100

    tests/cdc: TEMPORARY, NONWORKING added test to CDC from LWT

--- test/boost/cdc_test.cc
+++ test/boost/cdc_test.cc
@@ -50,6 +50,15 @@ static cql_test_config mk_cdc_test_config() {
     return cql_test_config(std::move(cfg));
 };
 
+static cql_test_config mk_lwt_cdc_test_config() {
+    shared_ptr<db::config> cfg(make_shared<db::config>());
+    auto features = cfg->experimental_features();
+    features.emplace_back(db::experimental_features_t::CDC);
+    features.emplace_back(db::experimental_features_t::LWT);
+    cfg->experimental_features(features);
+    return cql_test_config(std::move(cfg));
+};
+
 namespace cdc {
 api::timestamp_type find_timestamp(const schema&, const mutation&);
 utils::UUID generate_timeuuid(api::timestamp_type);
@@ -852,4 +861,52 @@ SEASTAR_THREAD_TEST_CASE(test_update_insert_delete_distinction) {
         BOOST_REQUIRE_EQUAL(results[3].size(), 1);
         BOOST_REQUIRE_EQUAL(*results[3].front(), data_value(cdc::operation::row_delete).serialize_nonnull()); // log entry from (3)
     }, mk_cdc_test_config()).get();
+}
+
+SEASTAR_THREAD_TEST_CASE(test_lwt_with_cdc) {
+    // Creates temporary `query_options` with `consistency_level::SERIAL`:
+    auto qo_sc = [] () {
+        using cqo = cql3::query_options;
+        using cqoso = cqo::specific_options;
+        return std::make_unique<cqo>(
+                cqo::DEFAULT.get_consistency(),
+                cqo::DEFAULT.get_timeout_config(),
+                std::vector<cql3::raw_value>{},
+                cqoso{cqoso::DEFAULT.page_size, cqoso::DEFAULT.state, db::consistency_level::SERIAL, cqoso::DEFAULT.timestamp});
+    };
+
+    do_with_cql_env_thread([qo_sc] (cql_test_env& e) {
+        const auto base_tbl_name = "tbl_lwt";
+        const int pk = 1, ck = 11;
+
+        cquery_nofail(e, format("CREATE TABLE ks.{} (pk int, ck int, val int, PRIMARY KEY(pk, ck)) WITH cdc = {{'enabled':'true'}}", base_tbl_name));
+        // (0) successful insert:
+        cquery_nofail(e, format("INSERT INTO ks.{} (pk, ck, val) VALUES ({}, {}, 111) IF NOT EXISTS", base_tbl_name, pk, ck), qo_sc());
+        // (1) failed insert:
+        cquery_nofail(e, format("INSERT INTO ks.{} (pk, ck, val) VALUES ({}, {}, 222) IF NOT EXISTS", base_tbl_name, pk, ck), qo_sc());
+        // (2) successful update:
+        cquery_nofail(e, format("UPDATE ks.{} set val=333 WHERE pk = {} and ck = {} IF EXISTS", base_tbl_name, pk, ck), qo_sc());
+        // (3) failed update:
+        cquery_nofail(e, format("UPDATE ks.{} set val=444 WHERE pk = 888 and ck = 777 IF EXISTS", base_tbl_name, pk, ck), qo_sc());
+        // (4) successful row delete:
+        cquery_nofail(e, format("DELETE FROM ks.{} WHERE pk = {} AND ck = {} IF EXISTS", base_tbl_name, pk, ck), qo_sc());
+        // (5) failed row delete:
+        cquery_nofail(e, format("DELETE FROM ks.{} WHERE pk = {} AND ck = {} IF EXISTS", base_tbl_name, pk, ck), qo_sc());
+
+        const sstring query = format("SELECT operation FROM ks.{}", cdc::log_name(base_tbl_name));
+        auto msg = e.execute_cql(query).get0();
+        auto rows = dynamic_pointer_cast<cql_transport::messages::result_message::rows>(msg);
+        BOOST_REQUIRE(rows);
+        auto results = to_bytes(*rows);
+        BOOST_REQUIRE_EQUAL(results.size(), 3);  // 1 successful insert + 1 successful update + 1 successful row delete == 3
+
+        BOOST_REQUIRE_EQUAL(results[0].size(), 1);
+        BOOST_REQUIRE_EQUAL(*results[0].front(), data_value(cdc::operation::insert).serialize_nonnull()); // log entry from (0)
+
+        BOOST_REQUIRE_EQUAL(results[1].size(), 1);
+        BOOST_REQUIRE_EQUAL(*results[1].front(), data_value(cdc::operation::update).serialize_nonnull()); // log entry from (2)
+
+        BOOST_REQUIRE_EQUAL(results[2].size(), 1);
+        BOOST_REQUIRE_EQUAL(*results[2].front(), data_value(cdc::operation::row_delete).serialize_nonnull()); // log entry from (4)
+    }, mk_lwt_cdc_test_config()).get();
 }
