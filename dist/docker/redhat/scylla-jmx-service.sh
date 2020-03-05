43b0ebf360e3e3e3d0e2fcac7b68046df64683ea
Takuya ASADA <syuu@scylladb.com>
2020-03-05 16:56:52 +0900

    docker: apply scylla-jmx sysconfig file on scylla-jmx service
    Apply scylla-jmx sysconfig file on scyla-jmx service, to allow customize
    jmx parameter.
    
    Fixes #5939

--- dist/docker/redhat/scylla-jmx-service.sh
+++ dist/docker/redhat/scylla-jmx-service.sh
@@ -2,4 +2,4 @@
 
 source /etc/sysconfig/scylla-jmx
 
-exec /opt/scylladb/jmx/scylla-jmx -l /opt/scylladb/jmx
+exec /opt/scylladb/jmx/scylla-jmx $SCYLLA_JMX_PORT $SCYLLA_API_PORT $SCYLLA_API_ADDR $SCYLLA_JMX_ADDR $SCYLLA_JMX_FILE $SCYLLA_JMX_LOCAL $SCYLLA_JMX_REMOTE $SCYLLA_JMX_DEBUG
