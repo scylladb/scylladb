eec2c515b5bf1cc284d15c45607621306654c0a2
Glauber Costa <glauber@scylladb.com>
2020-03-05 09:02:39 -0500

    mutation_writer: introduce shard_based splitting writer
    This is similar to the timestamp based splitting writer, except
    that it splits data based on the shard where the partition key
    is supposed to be placed.
    
    It is similar to the multishard_writer, in the sense that it
    creates n streams for n shards, but it does not want to process
    the streams in the owner shards. We want to use that in processes
    like resharding where it is fine for a foreign shard to deal
    with a mutation.
    
    One option would be to augment the multishard_writer to optionally
    achieve these properties, but having a separate splitter is both
    simpler and faster.
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

---
+++ mutation_writer/shard_based_splitting_writer.hh
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <seastar/util/noncopyable_function.hh>
+
+#include "feed_writers.hh"
+
+namespace mutation_writer {
+
+// Given a producer that may contain data for all shards, consume it in a per-shard
+// manner. This is useful, for instance, in the resharding process where a user changes
+// the amount of CPU assigned to Scylla and we have to rewrite the SSTables to their new
+// owners.
+future<> segregate_by_shard(flat_mutation_reader producer, reader_consumer consumer);
+
+} // namespace mutation_writer
