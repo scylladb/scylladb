scylladb/scylladb

commit fd49fd773cec1fba5ae7e4c4c5d0448bb8e2769b

db,view: move putting view updates to background to mutate_MV
Currently, launching view updates as an asynchronous background job
is done via not waiting for mutate_MV() future in
table::generate_and_propagate_view_updates. That has a big downside,
since mutate_MV() handles *all* view updates for *all* views of a table,
so it's not possible to wait for each view independently.
Per-view granularity is required in order to implement synchronous
view updates of local views - because then we'll synchronously
wait for all views that write to a local node (due to having a matching
partition key with the base), while remote view updates will still
be sent asynchronously.
In order to do that, instead of not waiting for mutate_MV,
we do wait for it properly, but instead launch the asynchronous,
unwaited-for futures inside mutate_MV.
Effectively that means no changes for view updates so far - all updates
will be fired in the background. Later, another patch will introduce
a way to wait for selected updates to finish.

db/view/view.cc
@@ -1113,13 +1113,16 @@ future<> mutate_MV(
                 // writes but mutate_locally() doesn't, so we need to do that here.
                 ++stats.writes;
                 auto mut_ptr = std::make_unique<frozen_mutation>(std::move(mut.fm));
-                fs->push_back(service::get_local_storage_proxy().mutate_locally(mut.s, *mut_ptr, db::commitlog::force_sync::no).then_wrapped(
+                future<> local_view_update = service::get_local_storage_proxy().mutate_locally(mut.s, *mut_ptr, db::commitlog::force_sync::no).then_wrapped(
                         [&stats,
                          maybe_account_failure = std::move(maybe_account_failure),
                          mut_ptr = std::move(mut_ptr)] (future<>&& f) {
                     --stats.writes;
                     return maybe_account_failure(std::move(f), utils::fb_utilities::get_broadcast_address(), true, 0);
-                }));
+                });
+                // The update is sent to background in order to preserve availability,
+                // its parallelism is limited by view_update_concurrency_semaphore
+                (void)local_view_update;
             } else {
                 vlogger.debug("Sending view update to endpoint {}, with pending endpoints = {}", *paired_endpoint, pending_endpoints);
                 // Note we don't wait for the asynchronous operation to complete
@@ -1129,7 +1132,7 @@ future<> mutate_MV(
                 // to send the update there. Currently, we do this from *each* of
                 // the base replicas, but this is probably excessive - see
                 // See https://issues.apache.org/jira/browse/CASSANDRA-14262/
-                fs->push_back(service::get_local_storage_proxy().send_to_endpoint(
+                future<> view_update = service::get_local_storage_proxy().send_to_endpoint(
                         std::move(mut),
                         *paired_endpoint,
                         std::move(pending_endpoints),
@@ -1141,7 +1144,10 @@ future<> mutate_MV(
                                  updates_pushed_remote,
                                  maybe_account_failure = std::move(maybe_account_failure)] (future<>&& f) mutable {
                     return maybe_account_failure(std::move(f), std::move(*paired_endpoint), is_endpoint_local, updates_pushed_remote);
-                }));
+                });
+                // The update is sent to background in order to preserve availability,
+                // its parallelism is limited by view_update_concurrency_semaphore
+                (void)view_update;
             }
         } else if (!pending_endpoints.empty()) {
             // If there is no paired endpoint, it means there's a range movement going on (decommission or move),
@@ -1158,7 +1164,7 @@ future<> mutate_MV(
             cf_stats.total_view_updates_pushed_remote += updates_pushed_remote;
             auto target = pending_endpoints.back();
             pending_endpoints.pop_back();
-            fs->push_back(service::get_local_storage_proxy().send_to_endpoint(
+            future<> view_update = service::get_local_storage_proxy().send_to_endpoint(
                     std::move(mut),
                     target,
                     std::move(pending_endpoints),
@@ -1168,7 +1174,10 @@ future<> mutate_MV(
                              updates_pushed_remote,
                              maybe_account_failure = std::move(maybe_account_failure)] (future<>&& f) {
                 return maybe_account_failure(std::move(f), std::move(target), false, updates_pushed_remote);
-            }));
+            });
+            // The update is sent to background in order to preserve availability,
+            // its parallelism is limited by view_update_concurrency_semaphore
+            (void)view_update;
         }
     }
     auto f = seastar::when_all_succeed(fs->begin(), fs->end());

table.cc
@@ -2107,8 +2107,7 @@ future<> table::generate_and_propagate_view_updates(const schema_ptr& base,
             flat_mutation_reader_from_mutations({std::move(m)}),
             std::move(existings)).then([this, base_token = std::move(base_token)] (std::vector<frozen_mutation_and_schema>&& updates) mutable {
         auto units = seastar::consume_units(*_config.view_update_concurrency_semaphore, memory_usage_of(updates));
-        //FIXME: discarded future.
-        (void)db::view::mutate_MV(std::move(base_token), std::move(updates), _view_stats, *_config.cf_stats, std::move(units), service::allow_hints::yes).handle_exception([] (auto ignored) { });
+        return db::view::mutate_MV(std::move(base_token), std::move(updates), _view_stats, *_config.cf_stats, std::move(units), service::allow_hints::yes).handle_exception([] (auto ignored) { });
     });
 }
 

commit 38668768a998b5dce435183477c054d4707fadff

view,table: fix waiting for view updates during building
View updates sent as part of the view building process should never
be ignored, but fd49fd7 introduced a bug which may cause exactly that:
the updates are mistakenly sent to background, so the view builder
will not receive negative feedback if an update failed, which will
in turn not cause a retry. Consequently, view building may report
that it "finished" building a view, while some of the updates were
lost. A simple fix is to restore previous behaviour - all updates
triggered by view building are now waited for.

Fixes #6038
Tests: unit(dev),
dtest: interrupt_build_process_with_resharding_low_to_half_test

db/view/view.cc
@@ -1073,7 +1073,8 @@ future<> mutate_MV(
         db::view::stats& stats,
         cf_stats& cf_stats,
         db::timeout_semaphore_units pending_view_updates,
-        service::allow_hints allow_hints)
+        service::allow_hints allow_hints,
+        wait_for_all_updates wait_for_all)
 {
     auto fs = std::make_unique<std::vector<future<>>>();
     fs->reserve(view_updates.size());
@@ -1152,9 +1153,13 @@ future<> mutate_MV(
                                  maybe_account_failure = std::move(maybe_account_failure)] (future<>&& f) mutable {
                     return maybe_account_failure(std::move(f), std::move(*paired_endpoint), is_endpoint_local, updates_pushed_remote);
                 });
-                // The update is sent to background in order to preserve availability,
-                // its parallelism is limited by view_update_concurrency_semaphore
-                (void)view_update;
+                if (wait_for_all) {
+                    fs->push_back(std::move(view_update));
+                } else {
+                    // The update is sent to background in order to preserve availability,
+                    // its parallelism is limited by view_update_concurrency_semaphore
+                    (void)view_update;
+                }
             }
         } else if (!pending_endpoints.empty()) {
             // If there is no paired endpoint, it means there's a range movement going on (decommission or move),
@@ -1182,9 +1187,13 @@ future<> mutate_MV(
                              maybe_account_failure = std::move(maybe_account_failure)] (future<>&& f) {
                 return maybe_account_failure(std::move(f), std::move(target), false, updates_pushed_remote);
             });
-            // The update is sent to background in order to preserve availability,
-            // its parallelism is limited by view_update_concurrency_semaphore
-            (void)view_update;
+            if (wait_for_all) {
+                fs->push_back(std::move(view_update));
+            } else {
+                // The update is sent to background in order to preserve availability,
+                // its parallelism is limited by view_update_concurrency_semaphore
+                (void)view_update;
+            }
         }
     }
     auto f = seastar::when_all_succeed(fs->begin(), fs->end());

db/view/view.hh
@@ -116,13 +116,16 @@ query::clustering_row_ranges calculate_affected_clustering_ranges(
         const mutation_partition& mp,
         const std::vector<view_ptr>& views);
 
+struct wait_for_all_updates_tag {};
+using wait_for_all_updates = bool_class<wait_for_all_updates_tag>;
 future<> mutate_MV(
         const dht::token& base_token,
         std::vector<frozen_mutation_and_schema> view_updates,
         db::view::stats& stats,
         cf_stats& cf_stats,
         db::timeout_semaphore_units pending_view_updates,
-        service::allow_hints allow_hints);
+        service::allow_hints allow_hints,
+        wait_for_all_updates wait_for_all);
 
 /**
  * create_virtual_column() adds a "virtual column" to a schema builder.

table.cc
@@ -2107,7 +2107,8 @@ future<> table::generate_and_propagate_view_updates(const schema_ptr& base,
             flat_mutation_reader_from_mutations({std::move(m)}),
             std::move(existings)).then([this, base_token = std::move(base_token)] (std::vector<frozen_mutation_and_schema>&& updates) mutable {
         auto units = seastar::consume_units(*_config.view_update_concurrency_semaphore, memory_usage_of(updates));
-        return db::view::mutate_MV(std::move(base_token), std::move(updates), _view_stats, *_config.cf_stats, std::move(units), service::allow_hints::yes).handle_exception([] (auto ignored) { });
+        return db::view::mutate_MV(std::move(base_token), std::move(updates), _view_stats, *_config.cf_stats,
+                std::move(units), service::allow_hints::yes, db::view::wait_for_all_updates::no).handle_exception([] (auto ignored) { });
     });
 }
 
@@ -2221,7 +2222,8 @@ future<> table::populate_views(
                  units_to_consume = update_size - units_to_wait_for,
                  this] (db::timeout_semaphore_units&& units) mutable {
             units.adopt(seastar::consume_units(*_config.view_update_concurrency_semaphore, units_to_consume));
-            return db::view::mutate_MV(std::move(base_token), std::move(updates), _view_stats, *_config.cf_stats, std::move(units), service::allow_hints::no);
+            return db::view::mutate_MV(std::move(base_token), std::move(updates), _view_stats,
+                    *_config.cf_stats, std::move(units), service::allow_hints::no, db::view::wait_for_all_updates::yes);
         });
     });
 }