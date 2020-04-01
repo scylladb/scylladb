5e32ecb51416f93f961151c0dbf49e1d7a146226
Avi Kivity <avi@scylladb.com>
2020-04-01 12:38:43 +0300

    test: sstable-utils: deinline do_make_keys()
    This hides a call to engine_is_ready() which is only available in
    reactor.hh.
    
    Dependencies are adjusted so tests link.
    
    Ref #1.

--- test/lib/sstable_utils.hh
+++ test/lib/sstable_utils.hh
@@ -49,30 +49,7 @@ inline future<> write_memtable_to_sstable_for_test(memtable& mt, sstables::share
 //
 // Make set of keys sorted by token for current or remote shard.
 //
-static std::vector<sstring> do_make_keys(unsigned n, const schema_ptr& s, size_t min_key_size = 1, local_shard_only lso = local_shard_only::yes) {
-    std::vector<std::pair<sstring, dht::decorated_key>> p;
-    p.reserve(n);
-
-    auto key_id = 0U;
-    auto generated = 0U;
-    while (generated < n) {
-        auto raw_key = sstring(std::max(min_key_size, sizeof(key_id)), int8_t(0));
-        std::copy_n(reinterpret_cast<int8_t*>(&key_id), sizeof(key_id), raw_key.begin());
-        auto dk = dht::decorate_key(*s, partition_key::from_single_value(*s, to_bytes(raw_key)));
-        key_id++;
-        if (lso) {
-            if (engine_is_ready() && this_shard_id() != shard_of(*s, dk.token())) {
-                continue;
-            }
-        }
-        generated++;
-        p.emplace_back(std::move(raw_key), std::move(dk));
-    }
-    boost::sort(p, [&] (auto& p1, auto& p2) {
-        return p1.second.less_compare(*s, p2.second);
-    });
-    return boost::copy_range<std::vector<sstring>>(p | boost::adaptors::map_keys);
-}
+std::vector<sstring> do_make_keys(unsigned n, const schema_ptr& s, size_t min_key_size = 1, local_shard_only lso = local_shard_only::yes);
 
 inline std::vector<sstring> make_local_keys(unsigned n, const schema_ptr& s, size_t min_key_size = 1) {
     return do_make_keys(n, s, min_key_size, local_shard_only::yes);
