glommer/scylla

compaction_manager: carve out a drain method
We want stop() to be callable just once. Having the compaction manager
stopped twice is a potential indication that something is wrong.

Still there are places where we want to stop all ongoing compactions
and prevent new from running - like the drain operation. Today the
only operation that allows for cancellation of all existing compations
is stop(). To unweave this, we will split those two things.

A drain operation is carved out, and it should be safe to be called many
times. The compaction manager is usable after this, and new compactions
can even be sent if it happen to be enabled again (we currently don't)

A stop operation, which includes a drain, will only be allowed once. After
a stop() the compaction_manager object is no longer usable.

Signed-off-by: Glauber Costa <glauber@scylladb.com>

sstables/compaction_manager.cc
@@ -457,19 +457,15 @@ void compaction_manager::postpone_compaction_for_column_family(column_family* cf
     _postponed.push_back(cf);
 }
 
-future<> compaction_manager::stop() {
-    if (_state == state::none || _state == state::destroyed) {
-        return make_ready_future<>();
-    }
+future<> compaction_manager::drain() {
+    _state = state::disabled;
+    cmlog.info("Draining {} ongoing compactions", _compactions.size());
 
-    cmlog.info("Asked to stop");
-    _state = state::destroyed;
-    // Reset the metrics registry
-    _metrics.clear();
     // Stop all ongoing compaction.
     for (auto& info : _compactions) {
-        info->stop("shutdown");
+        info->stop("drain");
     }
+
     // Wait for each task handler to stop. Copy list because task remove itself
     // from the list when done.
     auto tasks = _tasks;
@@ -477,10 +473,22 @@ future<> compaction_manager::stop() {
         return parallel_for_each(tasks, [this] (auto& task) {
             return this->task_stop(task);
         });
-    }).then([this] () mutable {
+    });
+}
+
+future<> compaction_manager::stop() {
+    if (_state == state::none || _state == state::destroyed) {
+        return make_ready_future<>();
+    }
+
+    cmlog.info("Asked to stop");
+    // Reset the metrics registry
+    _metrics.clear();
+    return drain().then([this] () mutable {
         reevaluate_postponed_compactions();
         return std::move(_waiting_reevalution);
     }).then([this] {
+        _state = state::destroyed;
         _weight_tracker.clear();
         _compaction_submission_timer.cancel();
         cmlog.info("Stopped");

scylladb/scylladb

sstables/compaction_manager.cc
@@ -461,19 +461,14 @@ void compaction_manager::postpone_compaction_for_column_family(column_family* cf
     _postponed.push_back(cf);
 }
 
-future<> compaction_manager::stop() {
-    if (_state == state::none || _state == state::stopped) {
-        return make_ready_future<>();
-    }
+future<> compaction_manager::stop_ongoing_compactions(sstring reason) {
+    cmlog.info("Stopping {} ongoing compactions", _compactions.size());
 
-    cmlog.info("Asked to stop");
-    _state = state::stopped;
-    // Reset the metrics registry
-    _metrics.clear();
     // Stop all ongoing compaction.
     for (auto& info : _compactions) {
-        info->stop("shutdown");
+        info->stop(reason);
     }
+
     // Wait for each task handler to stop. Copy list because task remove itself
     // from the list when done.
     auto tasks = _tasks;
@@ -481,7 +476,24 @@ future<> compaction_manager::stop() {
         return parallel_for_each(tasks, [this] (auto& task) {
             return this->task_stop(task);
         });
-    }).then([this] () mutable {
+    });
+}
+
+future<> compaction_manager::drain() {
+    _state = state::disabled;
+    return stop_ongoing_compactions("drain");
+}
+
+future<> compaction_manager::stop() {
+    if (_state == state::none || _state == state::stopped) {
+        return make_ready_future<>();
+    }
+
+    _state = state::stopped;
+    cmlog.info("Asked to stop");
+    // Reset the metrics registry
+    _metrics.clear();
+    return stop_ongoing_compactions("shutdown").then([this] () mutable {
         reevaluate_postponed_compactions();
         return std::move(_waiting_reevalution);
     }).then([this] {