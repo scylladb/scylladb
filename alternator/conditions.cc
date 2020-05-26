b2ca7f6fc0a571d9f7d8057a1f9c7f26b4a10268
Nadav Har'El <nyh@scylladb.com>
2020-05-26 21:58:11 +0300

    alternator: another check base64 begins_with without decoding
    In commit cb7d3c6b55f7639d6b381110fc6888ecb12c0ee2 we started to check
    if two base64-encoded strings begin with each other without decoding
    the strings first.
    
    However, we missed the check_BEGINS_WITH function which does the same
    thing. So this patch fixes this function as well.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <20200526185812.165038-1-nyh@scylladb.com>

--- alternator/conditions.cc
+++ alternator/conditions.cc
@@ -222,11 +222,7 @@ static bool check_BEGINS_WITH(const rjson::value* v1, const rjson::value& v2) {
         std::string_view val2(it2->value.GetString(), it2->value.GetStringLength());
         return val1.substr(0, val2.size()) == val2;
     } else /* it2->name == "B" */ {
-        // TODO (optimization): Check the begins_with condition directly on
-        // the base64-encoded string, without making a decoded copy.
-        bytes val1 = base64_decode(it1->value);
-        bytes val2 = base64_decode(it2->value);
-        return val1.substr(0, val2.size()) == val2;
+        return base64_begins_with(rjson::to_string_view(it1->value), rjson::to_string_view(it2->value));
     }
 }
 
