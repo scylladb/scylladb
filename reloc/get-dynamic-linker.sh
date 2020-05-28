48e7ee374a3d81830399326fca53adf79417e06d
Rafael Ávila de Espíndola <espindola@scylladb.com>
2020-05-28 13:27:41 -0700

    configure: Reduce the dynamic linker path size
    gdb has a SO_NAME_MAX_PATH_SIZE of 512, so we use that as the path
    size.
    
    Fixes: #6494
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20200528202741.398695-2-espindola@scylladb.com>
    (cherry picked from commit aa778ec152d2609d1eabd923c266e7a5c2618f9e)

--- reloc/get-dynamic-linker.sh
+++ reloc/get-dynamic-linker.sh
@@ -12,7 +12,11 @@
 # At the end of the build we check that the build-id is indeed in the
 # first page. At install time we check that patchelf doesn't modify
 # the program headers.
+
+# gdb has a SO_NAME_MAX_PATH_SIZE of 512, so limit the path size to
+# that. The 512 includes the null at the end, hence the 511 bellow.
+
 ORIGINAL_DYNAMIC_LINKER=$(gcc -### /dev/null -o t 2>&1 | perl -n  -e '/-dynamic-linker ([^ ]*) / && print $1')
-DYNAMIC_LINKER=$(printf "%2000s$ORIGINAL_DYNAMIC_LINKER" | sed 's| |/|g')
+DYNAMIC_LINKER=$(printf "%511s$ORIGINAL_DYNAMIC_LINKER" | sed 's| |/|g')
 
 echo $DYNAMIC_LINKER
