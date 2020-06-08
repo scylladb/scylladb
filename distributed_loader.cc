glommer/scylla

distributed_load: initial handling of off-strategy SSTables
Off-strategy SSTables are SSTables that do not conform to the invariants
that the compaction strategies define. Examples of offstrategy SSTables
are SSTables acquired over bootstrap, resharding when the cpu count
changes or imported from other databases through our upload directory.

This patch introduces a new class, sstable_directory, that will
handle SSTables that are present in a directory that is not one of the
directories where the table expects its SSTables.

There is much to be done to support off-strategy compactions fully. To
make sure we make incremental progress, this patch implements enough
code to handle resharding of SSTables in the upload directory. SSTables
are resharded in place, before we start accessing the files.

Later, we will take other steps before we finally move the SSTables into
the main directory. But for now, starting with resharding will not only
allow us to start small, but it will also allow us to start unleashing
much needed cleanups in many places. For instance, once we start
resharding on boot before making the SSTables available, we will be able
to expurge all places in Scylla where, during normal operations, we have
extra handler code for the fact that SSTables could be shared.

Tests: a new test is added and it passes in debug mode.

Signed-off-by: Glauber Costa <glauber@scylladb.com>

distributed_loader.cc
@@ -239,6 +239,147 @@ future<> distributed_loader::verify_owner_and_mode(fs::path path) {
     });
 };
 
+future<>
+distributed_loader::process_sstable_dir(sharded<sstables::sstable_directory>& dir) {
+    return dir.invoke_on_all([&dir] (sstables::sstable_directory& d) {
+        // Supposed to be called with the node either down or on behalf of maintenance tasks
+        // like nodetool refresh
+        return d.process_sstable_dir(service::get_local_streaming_read_priority()).then([&dir, &d] {
+            return d.move_foreign_sstables(dir);
+        });
+    }).then([&dir] {
+        return dir.invoke_on_all([&dir] (sstables::sstable_directory& d) {
+            return d.commit_directory_changes();
+        });
+    });
+}
+
+future<>
+distributed_loader::lock_table(sharded<sstables::sstable_directory>& dir, sharded<database>& db, sstring ks_name, sstring cf_name) {
+    return dir.invoke_on_all([&db, ks_name, cf_name] (sstables::sstable_directory& d) {
+        auto& table = db.local().find_column_family(ks_name, cf_name);
+        d.store_phaser(table.write_in_progress());
+        return make_ready_future<>();
+    });
+}
+
+// Helper structure for resharding.
+//
+// Describes the sstables (represented by their foreign_sstable_open_info) that are shared and
+// need to be resharded. Each shard will keep one such descriptor, that contains the list of
+// SSTables assigned to it, and their total size. The total size is used to make sure we are
+// fairly balancing SSTables among shards.
+struct reshard_shard_descriptor {
+    sstables::sstable_directory::sstable_info_vector info_vec;
+    uint64_t uncompressed_data_size = 0;
+
+    bool total_size_smaller(const reshard_shard_descriptor& rhs) const {
+        return uncompressed_data_size < rhs.uncompressed_data_size;
+    }
+
+    uint64_t size() const {
+        return uncompressed_data_size;
+    }
+};
+
+// Collects shared SSTables from all shards and returns a vector containing them all.
+// This function assumes that the list of SSTables can be fairly big so it is careful to
+// manipulate it in a do_for_each loop (which yields) instead of using standard accumulators.
+future<sstables::sstable_directory::sstable_info_vector>
+collect_all_shared_sstables(sharded<sstables::sstable_directory>& dir) {
+    return do_with(sstables::sstable_directory::sstable_info_vector(), [&dir] (sstables::sstable_directory::sstable_info_vector& info_vec) {
+        // We want to make sure that each distributed object reshards about the same amount of data.
+        // Each sharded object has its own shared SSTables. We can use a clever algorithm in which they
+        // all distributely figure out which SSTables to exchange, but we'll keep it simple and move all
+        // their foreign_sstable_open_info to a coordinator (the shard who called this function). We can
+        // move in bulk and that's efficient. That shard can then distribute the work among all the
+        // others who will reshard.
+        auto coordinator = this_shard_id();
+        // We will first move all of the foreign open info to temporary storage so that we can sort
+        // them. We want to distribute bigger sstables first.
+        return dir.invoke_on_all([&info_vec, coordinator] (sstables::sstable_directory& d) {
+            return smp::submit_to(coordinator, [&info_vec, info = d.retrieve_shared_sstables()] () mutable {
+                // We want do_for_each here instead of a loop to avoid stalls. Resharding can be
+                // called during node operations too. For example, if it is called to load new
+                // SSTables into the system.
+                return do_for_each(info, [&info_vec] (sstables::foreign_sstable_open_info& info) {
+                    info_vec.push_back(std::move(info));
+                });
+            });
+        }).then([&info_vec] () mutable {
+            return make_ready_future<sstables::sstable_directory::sstable_info_vector>(std::move(info_vec));
+        });
+    });
+}
+
+// Given a vector of shared sstables to be resharded, distribute it among all shards.
+// The vector is first sorted to make sure that we are moving the biggest SSTables first.
+//
+// Returns a reshard_shard_descriptor per shard indicating the work that each shard has to do.
+future<std::vector<reshard_shard_descriptor>>
+distribute_reshard_jobs(sstables::sstable_directory::sstable_info_vector source) {
+    return do_with(std::move(source), std::vector<reshard_shard_descriptor>(smp::count),
+            [] (sstables::sstable_directory::sstable_info_vector& source, std::vector<reshard_shard_descriptor>& destinations) mutable {
+        std::sort(source.begin(), source.end(), [] (const sstables::foreign_sstable_open_info& a, const sstables::foreign_sstable_open_info& b) {
+            // Sort on descending SSTable sizes.
+            return a.uncompressed_data_size > b.uncompressed_data_size;
+        });
+        return do_for_each(source, [&destinations] (sstables::foreign_sstable_open_info& info) mutable {
+            auto shard_it = boost::min_element(destinations, std::mem_fn(&reshard_shard_descriptor::total_size_smaller));
+            shard_it->uncompressed_data_size += info.uncompressed_data_size;
+            shard_it->info_vec.push_back(std::move(info));
+        }).then([&destinations] () mutable {
+            return make_ready_future<std::vector<reshard_shard_descriptor>>(std::move(destinations));
+        });
+    });
+}
+
+future<> run_resharding_jobs(sharded<sstables::sstable_directory>& dir, std::vector<reshard_shard_descriptor> reshard_jobs,
+                             sharded<database>& db, sstring ks_name, sstring table_name, sstables::creator_fn creator) {
+
+    uint64_t total_size = boost::accumulate(reshard_jobs | boost::adaptors::transformed(std::mem_fn(&reshard_shard_descriptor::size)), uint64_t(0));
+    if (total_size == 0) {
+        return make_ready_future<>();
+    }
+
+    return do_with(std::move(reshard_jobs), [&dir, &db, ks_name, table_name, creator = std::move(creator), total_size] (std::vector<reshard_shard_descriptor>& reshard_jobs) {
+        auto total_size_mb = total_size / 1000000.0;
+        auto start = std::chrono::steady_clock::now();
+        dblog.info("{}", fmt::format("Resharding {:.2f} MB", total_size_mb));
+
+        return dir.invoke_on_all([&dir, &db, &reshard_jobs, ks_name, table_name, creator] (sstables::sstable_directory& d) mutable {
+            auto& table = db.local().find_column_family(ks_name, table_name);
+            auto info_vec = std::move(reshard_jobs[this_shard_id()].info_vec);
+            auto& cm = table.get_compaction_manager();
+            auto max_threshold = table.schema()->max_compaction_threshold();
+            auto& iop = service::get_local_streaming_read_priority();
+            return d.reshard(std::move(info_vec), cm, table, max_threshold, creator, iop).then([&d, &dir] {
+                return d.move_foreign_sstables(dir);
+            });
+        }).then([start, total_size_mb] {
+            // add a microsecond to prevent division by zero
+            auto now = std::chrono::steady_clock::now() + 1us;
+            auto seconds = std::chrono::duration_cast<std::chrono::duration<float>>(now - start).count();
+            dblog.info("{}", fmt::format("Resharded {:.2f} MB in {:.2f} seconds, {:.2f} MB/s", total_size_mb, seconds, (total_size_mb / seconds)));
+            return make_ready_future<>();
+        });
+    });
+}
+
+// Global resharding function. Done in two parts:
+//  - The first part spreads the foreign_sstable_open_info across shards so that all of them are
+//    resharding about the same amount of data
+//  - The second part calls each shard's distributed object to reshard the SSTables they were
+//    assigned.
+future<>
+distributed_loader::reshard(sharded<sstables::sstable_directory>& dir, sharded<database>& db, sstring ks_name, sstring table_name, sstables::creator_fn creator) {
+    return collect_all_shared_sstables(dir).then([] (sstables::sstable_directory::sstable_info_vector all_jobs) mutable {
+        return distribute_reshard_jobs(std::move(all_jobs));
+    }).then([&dir, &db, ks_name, table_name, creator = std::move(creator)] (std::vector<reshard_shard_descriptor> destinations) mutable {
+        return run_resharding_jobs(dir, std::move(destinations), db, ks_name, table_name, creator = std::move(creator));
+    });
+}
+
 // This function will iterate through upload directory in column family,
 // and will do the following for each sstable found:
 // 1) Mutate sstable level to 0.

scylladb/scylladb

distributed_loader.cc
@@ -239,6 +239,147 @@ future<> distributed_loader::verify_owner_and_mode(fs::path path) {
     });
 };
 
+future<>
+distributed_loader::process_sstable_dir(sharded<sstables::sstable_directory>& dir) {
+    return dir.invoke_on_all([&dir] (sstables::sstable_directory& d) {
+        // Supposed to be called with the node either down or on behalf of maintenance tasks
+        // like nodetool refresh
+        return d.process_sstable_dir(service::get_local_streaming_read_priority()).then([&dir, &d] {
+            return d.move_foreign_sstables(dir);
+        });
+    }).then([&dir] {
+        return dir.invoke_on_all([&dir] (sstables::sstable_directory& d) {
+            return d.commit_directory_changes();
+        });
+    });
+}
+
+future<>
+distributed_loader::lock_table(sharded<sstables::sstable_directory>& dir, sharded<database>& db, sstring ks_name, sstring cf_name) {
+    return dir.invoke_on_all([&db, ks_name, cf_name] (sstables::sstable_directory& d) {
+        auto& table = db.local().find_column_family(ks_name, cf_name);
+        d.store_phaser(table.write_in_progress());
+        return make_ready_future<>();
+    });
+}
+
+// Helper structure for resharding.
+//
+// Describes the sstables (represented by their foreign_sstable_open_info) that are shared and
+// need to be resharded. Each shard will keep one such descriptor, that contains the list of
+// SSTables assigned to it, and their total size. The total size is used to make sure we are
+// fairly balancing SSTables among shards.
+struct reshard_shard_descriptor {
+    sstables::sstable_directory::sstable_info_vector info_vec;
+    uint64_t uncompressed_data_size = 0;
+
+    bool total_size_smaller(const reshard_shard_descriptor& rhs) const {
+        return uncompressed_data_size < rhs.uncompressed_data_size;
+    }
+
+    uint64_t size() const {
+        return uncompressed_data_size;
+    }
+};
+
+// Collects shared SSTables from all shards and returns a vector containing them all.
+// This function assumes that the list of SSTables can be fairly big so it is careful to
+// manipulate it in a do_for_each loop (which yields) instead of using standard accumulators.
+future<sstables::sstable_directory::sstable_info_vector>
+collect_all_shared_sstables(sharded<sstables::sstable_directory>& dir) {
+    return do_with(sstables::sstable_directory::sstable_info_vector(), [&dir] (sstables::sstable_directory::sstable_info_vector& info_vec) {
+        // We want to make sure that each distributed object reshards about the same amount of data.
+        // Each sharded object has its own shared SSTables. We can use a clever algorithm in which they
+        // all distributely figure out which SSTables to exchange, but we'll keep it simple and move all
+        // their foreign_sstable_open_info to a coordinator (the shard who called this function). We can
+        // move in bulk and that's efficient. That shard can then distribute the work among all the
+        // others who will reshard.
+        auto coordinator = this_shard_id();
+        // We will first move all of the foreign open info to temporary storage so that we can sort
+        // them. We want to distribute bigger sstables first.
+        return dir.invoke_on_all([&info_vec, coordinator] (sstables::sstable_directory& d) {
+            return smp::submit_to(coordinator, [&info_vec, info = d.retrieve_shared_sstables()] () mutable {
+                // We want do_for_each here instead of a loop to avoid stalls. Resharding can be
+                // called during node operations too. For example, if it is called to load new
+                // SSTables into the system.
+                return do_for_each(info, [&info_vec] (sstables::foreign_sstable_open_info& info) {
+                    info_vec.push_back(std::move(info));
+                });
+            });
+        }).then([&info_vec] () mutable {
+            return make_ready_future<sstables::sstable_directory::sstable_info_vector>(std::move(info_vec));
+        });
+    });
+}
+
+// Given a vector of shared sstables to be resharded, distribute it among all shards.
+// The vector is first sorted to make sure that we are moving the biggest SSTables first.
+//
+// Returns a reshard_shard_descriptor per shard indicating the work that each shard has to do.
+future<std::vector<reshard_shard_descriptor>>
+distribute_reshard_jobs(sstables::sstable_directory::sstable_info_vector source) {
+    return do_with(std::move(source), std::vector<reshard_shard_descriptor>(smp::count),
+            [] (sstables::sstable_directory::sstable_info_vector& source, std::vector<reshard_shard_descriptor>& destinations) mutable {
+        std::sort(source.begin(), source.end(), [] (const sstables::foreign_sstable_open_info& a, const sstables::foreign_sstable_open_info& b) {
+            // Sort on descending SSTable sizes.
+            return a.uncompressed_data_size > b.uncompressed_data_size;
+        });
+        return do_for_each(source, [&destinations] (sstables::foreign_sstable_open_info& info) mutable {
+            auto shard_it = boost::min_element(destinations, std::mem_fn(&reshard_shard_descriptor::total_size_smaller));
+            shard_it->uncompressed_data_size += info.uncompressed_data_size;
+            shard_it->info_vec.push_back(std::move(info));
+        }).then([&destinations] () mutable {
+            return make_ready_future<std::vector<reshard_shard_descriptor>>(std::move(destinations));
+        });
+    });
+}
+
+future<> run_resharding_jobs(sharded<sstables::sstable_directory>& dir, std::vector<reshard_shard_descriptor> reshard_jobs,
+                             sharded<database>& db, sstring ks_name, sstring table_name, sstables::compaction_sstable_creator_fn creator) {
+
+    uint64_t total_size = boost::accumulate(reshard_jobs | boost::adaptors::transformed(std::mem_fn(&reshard_shard_descriptor::size)), uint64_t(0));
+    if (total_size == 0) {
+        return make_ready_future<>();
+    }
+
+    return do_with(std::move(reshard_jobs), [&dir, &db, ks_name, table_name, creator = std::move(creator), total_size] (std::vector<reshard_shard_descriptor>& reshard_jobs) {
+        auto total_size_mb = total_size / 1000000.0;
+        auto start = std::chrono::steady_clock::now();
+        dblog.info("{}", fmt::format("Resharding {:.2f} MB", total_size_mb));
+
+        return dir.invoke_on_all([&dir, &db, &reshard_jobs, ks_name, table_name, creator] (sstables::sstable_directory& d) mutable {
+            auto& table = db.local().find_column_family(ks_name, table_name);
+            auto info_vec = std::move(reshard_jobs[this_shard_id()].info_vec);
+            auto& cm = table.get_compaction_manager();
+            auto max_threshold = table.schema()->max_compaction_threshold();
+            auto& iop = service::get_local_streaming_read_priority();
+            return d.reshard(std::move(info_vec), cm, table, max_threshold, creator, iop).then([&d, &dir] {
+                return d.move_foreign_sstables(dir);
+            });
+        }).then([start, total_size_mb] {
+            // add a microsecond to prevent division by zero
+            auto now = std::chrono::steady_clock::now() + 1us;
+            auto seconds = std::chrono::duration_cast<std::chrono::duration<float>>(now - start).count();
+            dblog.info("{}", fmt::format("Resharded {:.2f} MB in {:.2f} seconds, {:.2f} MB/s", total_size_mb, seconds, (total_size_mb / seconds)));
+            return make_ready_future<>();
+        });
+    });
+}
+
+// Global resharding function. Done in two parts:
+//  - The first part spreads the foreign_sstable_open_info across shards so that all of them are
+//    resharding about the same amount of data
+//  - The second part calls each shard's distributed object to reshard the SSTables they were
+//    assigned.
+future<>
+distributed_loader::reshard(sharded<sstables::sstable_directory>& dir, sharded<database>& db, sstring ks_name, sstring table_name, sstables::compaction_sstable_creator_fn creator) {
+    return collect_all_shared_sstables(dir).then([] (sstables::sstable_directory::sstable_info_vector all_jobs) mutable {
+        return distribute_reshard_jobs(std::move(all_jobs));
+    }).then([&dir, &db, ks_name, table_name, creator = std::move(creator)] (std::vector<reshard_shard_descriptor> destinations) mutable {
+        return run_resharding_jobs(dir, std::move(destinations), db, ks_name, table_name, creator = std::move(creator));
+    });
+}
+
 // This function will iterate through upload directory in column family,
 // and will do the following for each sstable found:
 // 1) Mutate sstable level to 0.