304b90d88b52e87fc9202ff891da3ba3fb68934f
Takuya ASADA <syuu@scylladb.com>
2020-06-10 06:35:44 +0900

    supervisor: drop unused Upstart code, always use libsystemd
    Since we don't support Ubuntu 14.04 anymore, we can drop Upstart related code
    from supervisor.[cc|hh].
    Also, "#ifdef HAVE_LIBSYSTEMD" was for compiling Scylla on older distribution
    which does not provide libsystemd, we nolonger need this since we always build
    Scylla on latest Fedora.
    Dropping HAVE_LIBSYSTEMD also means removing libsystemd from optional_packages
    in configure.py, make it required library.
    
    Note that we still may run Scylla without systemd such as our Docker image,
    but sd_notify() does nothing when systemd does not detected, so we can ignore
    such case.
    Reference: https://www.freedesktop.org/software/systemd/man/sd_notify.html
    Reference: https://github.com/systemd/systemd/blob/master/src/libsystemd/sd-daemon/sd-daemon.c

--- supervisor.hh
+++ supervisor.hh
@@ -22,25 +22,35 @@
 #pragma once
 
 #include <seastar/core/sstring.hh>
+#include <seastar/core/print.hh>
 #include "seastarx.hh"
+#include <systemd/sd-daemon.h>
+#include "log.hh"
+
+extern logger startlog;
 
 class supervisor {
 public:
-    static const sstring scylla_upstart_job_str;
-    static const sstring upstart_job_env;
-    static const sstring systemd_ready_msg;
+    static constexpr auto systemd_ready_msg = "READY=1";
     /** A systemd status message has a format <status message prefix>=<message> */
-    static const sstring systemd_status_msg_prefix;
+    static constexpr auto systemd_status_msg_prefix = "STATUS";
 public:
     /**
      * @brief Notify the Supervisor with the given message.
      * @param msg message to notify the Supervisor with
      * @param ready set to TRUE when scylla service becomes ready
      */
-    static void notify(sstring msg, bool ready = false);
+    static inline void notify(sstring msg, bool ready = false) {
+        startlog.info("{}", msg);
+        try_notify_systemd(msg, ready);
+    }
 
 private:
-    static void try_notify_systemd(sstring msg, bool ready);
-    static bool try_notify_upstart(sstring msg, bool ready);
-    static sstring get_upstart_job_env();
+    static inline void try_notify_systemd(sstring msg, bool ready) {
+        if (ready) {
+            sd_notify(0, format("{}\n{}={}\n", systemd_ready_msg, systemd_status_msg_prefix, msg).c_str());
+        } else {
+            sd_notify(0, format("{}={}\n", systemd_status_msg_prefix, msg).c_str());
+        }
+    }
 };
