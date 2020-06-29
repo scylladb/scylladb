9abaf9bc2e6fc9638f80eb404d017eeb094fa747
Asias He <asias@scylladb.com>
2020-06-29 15:55:33 +0800

    boot_strapper: Ignore node to be replaced explicitly as stream source
    After commit 7d86a3b208659e92489216e71df13fb33b1a4c3a (storage_service:
    Make replacing node take writes), during replace operation, tokens in
    _token_metadata for node being replaced are updated only after the replace
    operation is finished. As a result, in range_streamer::add_ranges, the
    node being replaced will be considered as a source to stream data from.
    
    Before commit 7d86a3b208659e92489216e71df13fb33b1a4c3a, the node being
    replaced will not be considered as a source node because it is already
    replaced by the replacing node before the replace operation is finished.
    This is the reason why it works in the past.
    
    To fix, filter out the node being replaced as a source node explicitly.
    
    Tests: replace_first_boot_test and replace_stopped_node_test
    Backports: 4.1
    Fixes: #6728

--- dht/boot_strapper.cc
+++ dht/boot_strapper.cc
@@ -59,7 +59,12 @@ future<> boot_strapper::bootstrap(streaming::stream_reason reason) {
         return make_exception_future<>(std::runtime_error("Wrong stream_reason provided: it can only be replace or bootstrap"));
     }
     auto streamer = make_lw_shared<range_streamer>(_db, _token_metadata, _abort_source, _tokens, _address, description, reason);
-    streamer->add_source_filter(std::make_unique<range_streamer::failure_detector_source_filter>(gms::get_local_gossiper().get_unreachable_members()));
+    auto nodes_to_filter = gms::get_local_gossiper().get_unreachable_members();
+    if (reason == streaming::stream_reason::replace && _db.local().get_replace_address()) {
+        nodes_to_filter.insert(_db.local().get_replace_address().value());
+    }
+    blogger.debug("nodes_to_filter={}", nodes_to_filter);
+    streamer->add_source_filter(std::make_unique<range_streamer::failure_detector_source_filter>(nodes_to_filter));
     auto keyspaces = make_lw_shared<std::vector<sstring>>(_db.local().get_non_system_keyspaces());
     return do_for_each(*keyspaces, [this, keyspaces, streamer] (sstring& keyspace_name) {
         auto& ks = _db.local().find_keyspace(keyspace_name);
