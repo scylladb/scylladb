scylladb/scylladb

commit 4e6400293e8cb6e7729d30cfb7e27dc03a3190f9

staging: potentially read many SSTables at the same time
There is no reason to read a single SSTable at a time from the staging
directory. Moving SSTables from staging directory essentially involves
scanning input SSTables and creating new SSTables (albeit in a different
directory).

We have a mechanism that does that: compactions. In a follow up patch, I
will introduce a new specialization of compaction that moves SSTables
from staging (potentially compacting them if there are plenty).

In preparation for that, some signatures have to be changed and the
view_updating_consumer has to be more compaction friendly. Meaning:
- Operating with an sstable vector
- taking a table reference, not a database

Because this code is a bit fragile and the reviewer set is fundamentally
different from anything compaction related, I am sending this separately

Signed-off-by: Glauber Costa <glauber@scylladb.com>

db/view/view_update_generator.cc
@@ -19,7 +19,9 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <boost/range/adaptor/map.hpp>
 #include "view_update_generator.hh"
+#include "service/priority_manager.hh"
 
 static logging::logger vug_logger("view_update_generator");
 
@@ -33,28 +35,53 @@ future<> view_update_generator::start() {
             if (_sstables_with_tables.empty()) {
                 _pending_sstables.wait().get();
             }
-            while (!_sstables_with_tables.empty()) {
-                auto& [sst, t] = _sstables_with_tables.front();
+
+            // If we got here, we will process all tables we know about so far eventually so there
+            // is no starvation
+            for (auto& t : _sstables_with_tables | boost::adaptors::map_keys) {
+                schema_ptr s = t->schema();
+
+                // Copy what we have so far so we don't miss new updates
+                auto sstables = std::exchange(_sstables_with_tables[t], {});
+
                 try {
-                    schema_ptr s = t->schema();
-                    flat_mutation_reader staging_sstable_reader = sst->read_rows_flat(s, no_reader_permit());
-                    auto result = staging_sstable_reader.consume_in_thread(view_updating_consumer(s, _db, sst, _as), db::no_timeout);
+                    // temporary: need an sstable set for the flat mutation reader, but the
+                    // compaction_descriptor takes a vector. Soon this will become a compaction
+                    // so the transformation to the SSTable set will not be needed.
+                    auto ssts = make_lw_shared(t->get_compaction_strategy().make_sstable_set(s));
+                    for (auto& sst : sstables) {
+                        ssts->insert(sst);
+                    }
+
+                    flat_mutation_reader staging_sstable_reader = ::make_range_sstable_reader(s,
+                            no_reader_permit(),
+                            std::move(ssts),
+                            query::full_partition_range,
+                            s->full_slice(),
+                            service::get_local_streaming_read_priority(),
+                            nullptr,
+                            ::streamed_mutation::forwarding::no,
+                            ::mutation_reader::forwarding::no);
+
+                    auto result = staging_sstable_reader.consume_in_thread(view_updating_consumer(s, *t, sstables, _as), db::no_timeout);
                     if (result == stop_iteration::yes) {
                         break;
                     }
                 } catch (...) {
-                    vug_logger.warn("Processing {} failed: {}. Will retry...", sst->get_filename(), std::current_exception());
+                    vug_logger.warn("Processing {} failed for table {}:{}. Will retry...", s->ks_name(), s->cf_name(), std::current_exception());
+                    // Need to add sstables back to the set so we can retry later. By now it may
+                    // have had other updates.
+                    std::move(sstables.begin(), sstables.end(), std::back_inserter(_sstables_with_tables[t]));
                     break;
                 }
                 try {
                     // collect all staging sstables to move in a map, grouped by table.
-                    _sstables_to_move[t].push_back(sst);
+                    std::move(sstables.begin(), sstables.end(), std::back_inserter(_sstables_to_move[t]));
                 } catch (...) {
                     // Move from staging will be retried upon restart.
-                    vug_logger.warn("Moving {} from staging failed: {}. Ignoring...", sst->get_filename(), std::current_exception());
+                    vug_logger.warn("Moving {} from staging failed: {}:{}. Ignoring...", s->ks_name(), s->cf_name(), std::current_exception());
                 }
                 _registration_sem.signal();
-                _sstables_with_tables.pop_front();
             }
             // For each table, move the processed staging sstables into the table's base dir.
             for (auto it = _sstables_to_move.begin(); it != _sstables_to_move.end(); ) {
@@ -88,7 +115,8 @@ future<> view_update_generator::register_staging_sstable(sstables::shared_sstabl
     if (_as.abort_requested()) {
         return make_ready_future<>();
     }
-    _sstables_with_tables.emplace_back(std::move(sst), std::move(table));
+    _sstables_with_tables[table].push_back(std::move(sst));
+
     _pending_sstables.signal();
     if (should_throttle()) {
         return _registration_sem.wait(1);

db/view/view_update_generator.hh
@@ -43,7 +43,7 @@ class view_update_generator {
         lw_shared_ptr<table> t;
         sstable_with_table(sstables::shared_sstable sst, lw_shared_ptr<table> t) : sst(std::move(sst)), t(std::move(t)) { }
     };
-    std::deque<sstable_with_table> _sstables_with_tables;
+    std::unordered_map<lw_shared_ptr<table>, std::vector<sstables::shared_sstable>> _sstables_with_tables;
     std::unordered_map<lw_shared_ptr<table>, std::vector<sstables::shared_sstable>> _sstables_to_move;
 public:
     view_update_generator(database& db) : _db(db) { }

database.hh
@@ -707,8 +707,8 @@ public:
             tracing::trace_state_ptr trace_state = nullptr,
             streamed_mutation::forwarding fwd = streamed_mutation::forwarding::no,
             mutation_reader::forwarding fwd_mr = mutation_reader::forwarding::yes) const;
-    flat_mutation_reader make_reader_excluding_sstable(schema_ptr schema,
-            sstables::shared_sstable sst,
+    flat_mutation_reader make_reader_excluding_sstables(schema_ptr schema,
+            std::vector<sstables::shared_sstable>& sst,
             const dht::partition_range& range,
             const query::partition_slice& slice,
             const io_priority_class& pc = default_priority_class(),
@@ -744,7 +744,7 @@ public:
     }
 
     mutation_source as_mutation_source() const;
-    mutation_source as_mutation_source_excluding(sstables::shared_sstable sst) const;
+    mutation_source as_mutation_source_excluding(std::vector<sstables::shared_sstable>& sst) const;
 
     void set_virtual_reader(mutation_source virtual_reader) {
         _virtual_reader = std::move(virtual_reader);
@@ -992,7 +992,10 @@ public:
     const std::vector<view_ptr>& views() const;
     future<row_locker::lock_holder> push_view_replica_updates(const schema_ptr& s, const frozen_mutation& fm, db::timeout_clock::time_point timeout) const;
     future<row_locker::lock_holder> push_view_replica_updates(const schema_ptr& s, mutation&& m, db::timeout_clock::time_point timeout) const;
-    future<row_locker::lock_holder> stream_view_replica_updates(const schema_ptr& s, mutation&& m, db::timeout_clock::time_point timeout, sstables::shared_sstable excluded_sstable) const;
+    future<row_locker::lock_holder>
+    stream_view_replica_updates(const schema_ptr& s, mutation&& m, db::timeout_clock::time_point timeout,
+            std::vector<sstables::shared_sstable>& excluded_sstables) const;
+
     void add_coordinator_read_latency(utils::estimated_histogram::duration latency);
     std::chrono::milliseconds get_coordinator_read_latency_percentile(double percentile);
 

db/view/view_updating_consumer.hh
@@ -36,14 +36,14 @@ namespace db::view {
 class view_updating_consumer {
     schema_ptr _schema;
     lw_shared_ptr<table> _table;
-    sstables::shared_sstable _excluded_sstable;
+    std::vector<sstables::shared_sstable> _excluded_sstables;
     const seastar::abort_source& _as;
     std::optional<mutation> _m;
 public:
-    view_updating_consumer(schema_ptr schema, database& db, sstables::shared_sstable excluded_sstable, const seastar::abort_source& as)
+    view_updating_consumer(schema_ptr schema, table& table, std::vector<sstables::shared_sstable> excluded_sstables, const seastar::abort_source& as)
             : _schema(std::move(schema))
-            , _table(db.find_column_family(_schema->id()).shared_from_this())
-            , _excluded_sstable(excluded_sstable)
+            , _table(table.shared_from_this())
+            , _excluded_sstables(std::move(excluded_sstables))
             , _as(as)
             , _m()
     { }

table.cc
@@ -2473,8 +2473,8 @@ table::disable_auto_compaction() {
 }
 
 flat_mutation_reader
-table::make_reader_excluding_sstable(schema_ptr s,
-        sstables::shared_sstable sst,
+table::make_reader_excluding_sstables(schema_ptr s,
+        std::vector<sstables::shared_sstable>& excluded,
         const dht::partition_range& range,
         const query::partition_slice& slice,
         const io_priority_class& pc,
@@ -2489,7 +2489,9 @@ table::make_reader_excluding_sstable(schema_ptr s,
     }
 
     auto effective_sstables = ::make_lw_shared<sstables::sstable_set>(*_sstables);
-    effective_sstables->erase(sst);
+    for (auto& sst : excluded) {
+        effective_sstables->erase(sst);
+    }
 
     readers.emplace_back(make_sstable_reader(s, std::move(effective_sstables), range, slice, pc, std::move(trace_state), fwd, fwd_mr));
     return make_combined_reader(s, std::move(readers), fwd, fwd_mr);
@@ -2591,13 +2593,15 @@ future<row_locker::lock_holder> table::push_view_replica_updates(const schema_pt
     return do_push_view_replica_updates(s, std::move(m), timeout, as_mutation_source(), service::get_local_sstable_query_read_priority());
 }
 
-future<row_locker::lock_holder> table::stream_view_replica_updates(const schema_ptr& s, mutation&& m, db::timeout_clock::time_point timeout, sstables::shared_sstable excluded_sstable) const {
-    return do_push_view_replica_updates(s, std::move(m), timeout, as_mutation_source_excluding(std::move(excluded_sstable)), service::get_local_streaming_write_priority());
+future<row_locker::lock_holder>
+table::stream_view_replica_updates(const schema_ptr& s, mutation&& m, db::timeout_clock::time_point timeout,
+        std::vector<sstables::shared_sstable>& excluded_sstables) const {
+    return do_push_view_replica_updates(s, std::move(m), timeout, as_mutation_source_excluding(excluded_sstables), service::get_local_streaming_write_priority());
 }
 
 mutation_source
-table::as_mutation_source_excluding(sstables::shared_sstable sst) const {
-    return mutation_source([this, sst = std::move(sst)] (schema_ptr s,
+table::as_mutation_source_excluding(std::vector<sstables::shared_sstable>& ssts) const {
+    return mutation_source([this, &ssts] (schema_ptr s,
                                    reader_permit,
                                    const dht::partition_range& range,
                                    const query::partition_slice& slice,
@@ -2605,7 +2609,7 @@ table::as_mutation_source_excluding(sstables::shared_sstable sst) const {
                                    tracing::trace_state_ptr trace_state,
                                    streamed_mutation::forwarding fwd,
                                    mutation_reader::forwarding fwd_mr) {
-        return this->make_reader_excluding_sstable(std::move(s), std::move(sst), range, slice, pc, std::move(trace_state), fwd, fwd_mr);
+        return this->make_reader_excluding_sstables(std::move(s), ssts, range, slice, pc, std::move(trace_state), fwd, fwd_mr);
     });
 }
 
@@ -2614,7 +2618,7 @@ stop_iteration db::view::view_updating_consumer::consume_end_of_partition() {
         return stop_iteration::yes;
     }
     try {
-        auto lock_holder = _table->stream_view_replica_updates(_schema, std::move(*_m), db::no_timeout, _excluded_sstable).get();
+        auto lock_holder = _table->stream_view_replica_updates(_schema, std::move(*_m), db::no_timeout, _excluded_sstables).get();
     } catch (...) {
         tlogger.warn("Failed to push replica updates for table {}.{}: {}", _schema->ks_name(), _schema->cf_name(), std::current_exception());
     }

commit 5ebe2c28d145cc55d8f8fc86837cef5f5936aa7c

db/view: view_update_generator: re-balance wait/signal on the register semaphore
The view update generator has a semaphore to limit concurrency. This
semaphore is waited on in `register_staging_sstable()` and later the
unit is returned after the sstable is processed in the loop inside
`start()`.
This was broken by 4e64002, which changed the loop inside `start()` to
process sstables in per table batches, however didn't change the
`signal()` call to return the amount of units according to the number of
sstables processed. This can cause the semaphore units to dry up, as the
loop can process multiple sstables per table but return just a single
unit. This can also block callers of `register_staging_sstable()`
indefinitely as some waiters will never be released as under the right
circumstances the units on the semaphore can permanently go below 0.
In addition to this, 4e64002 introduced another bug: table entries from
the `_sstables_with_tables` are never removed, so they are processed
every turn. If the sstable list is empty, there won't be any update
generated but due to the unconditional `signal()` described above, this
can cause the units on the semaphore to grow to infinity, allowing
future staging sstables producers to register a huge amount of sstables,
causing memory problems due to the amount of sstable readers that have
to be opened (#6603, #6707).
Both outcomes are equally bad. This patch fixes both issues and modifies
the `test_view_update_generator` unit test to reproduce them and hence
to verify that this doesn't happen in the future.

Fixes: #6774
Refs: #6707
Refs: #6603

Tests: unit(dev)
Signed-off-by: Botond DÃ©nes <bdenes@scylladb.com>
Message-Id: <20200706135108.116134-1-bdenes@scylladb.com>

db/view/view_update_generator.cc
@@ -44,11 +44,16 @@ future<> view_update_generator::start() {
 
             // If we got here, we will process all tables we know about so far eventually so there
             // is no starvation
-            for (auto& t : _sstables_with_tables | boost::adaptors::map_keys) {
+            for (auto table_it = _sstables_with_tables.begin(); table_it != _sstables_with_tables.end(); table_it = _sstables_with_tables.erase(table_it)) {
+                auto& [t, t_sstables] = *table_it;
                 schema_ptr s = t->schema();
 
+                vug_logger.trace("Processing {}.{}: {} sstables", s->ks_name(), s->cf_name(), t_sstables.size());
+
                 // Copy what we have so far so we don't miss new updates
-                auto sstables = std::exchange(_sstables_with_tables[t], {});
+                auto sstables = std::exchange(t_sstables, {});
+
+                const auto num_sstables = sstables.size();
 
                 try {
                     // Exploit the fact that sstables in the staging directory
@@ -89,7 +94,7 @@ future<> view_update_generator::start() {
                     // Move from staging will be retried upon restart.
                     vug_logger.warn("Moving {} from staging failed: {}:{}. Ignoring...", s->ks_name(), s->cf_name(), std::current_exception());
                 }
-                _registration_sem.signal();
+                _registration_sem.signal(num_sstables);
             }
             // For each table, move the processed staging sstables into the table's base dir.
             for (auto it = _sstables_to_move.begin(); it != _sstables_to_move.end(); ) {

db/view/view_update_generator.hh
@@ -32,7 +32,10 @@
 namespace db::view {
 
 class view_update_generator {
+public:
     static constexpr size_t registration_queue_size = 5;
+
+private:
     database& _db;
     seastar::abort_source _as;
     future<> _started = make_ready_future<>();
@@ -51,6 +54,8 @@ public:
     future<> start();
     future<> stop();
     future<> register_staging_sstable(sstables::shared_sstable sst, lw_shared_ptr<table> table);
+
+    ssize_t available_register_units() const { return _registration_sem.available_units(); }
 private:
     bool should_throttle() const;
 };

test/boost/view_build_test.cc
@@ -421,23 +421,49 @@ SEASTAR_TEST_CASE(test_view_update_generator) {
         auto& view_update_generator = e.local_view_update_generator();
         auto s = test_table_schema();
 
+        std::vector<shared_sstable> ssts;
+
+        lw_shared_ptr<table> t = e.local_db().find_column_family("ks", "t").shared_from_this();
+
+        auto write_to_sstable = [&] (mutation m) {
+            auto sst = t->make_streaming_staging_sstable();
+            sstables::sstable_writer_config sst_cfg = test_sstables_manager.configure_writer();
+            auto& pc = service::get_local_streaming_priority();
+
+            sst->write_components(flat_mutation_reader_from_mutations({m}), 1ul, s, sst_cfg, {}, pc).get();
+            sst->open_data().get();
+            t->add_sstable_and_update_cache(sst).get();
+            return sst;
+        };
+
         auto key = partition_key::from_exploded(*s, {to_bytes(key1)});
         mutation m(s, key);
         auto col = s->get_column_definition("v");
         for (int i = 1024; i < 1280; ++i) {
             auto& row = m.partition().clustered_row(*s, clustering_key::from_exploded(*s, {to_bytes(fmt::format("c{}", i))}));
             row.cells().apply(*col, atomic_cell::make_live(*col->type, 2345, col->type->decompose(sstring(fmt::format("v{}", i)))));
+            // Scatter the data in a bunch of different sstables, so we
+            // can test the registration semaphore of the view update
+            // generator
+            if (!(i % 10)) {
+                ssts.push_back(write_to_sstable(std::exchange(m, mutation(s, key))));
+            }
         }
-        lw_shared_ptr<table> t = e.local_db().find_column_family("ks", "t").shared_from_this();
+        ssts.push_back(write_to_sstable(std::move(m)));
 
-        auto sst = t->make_streaming_staging_sstable();
-        sstables::sstable_writer_config sst_cfg = test_sstables_manager.configure_writer();
-        auto& pc = service::get_local_streaming_priority();
+        BOOST_REQUIRE_EQUAL(view_update_generator.available_register_units(), db::view::view_update_generator::registration_queue_size);
 
-        sst->write_components(flat_mutation_reader_from_mutations({m}), 1ul, s, sst_cfg, {}, pc).get();
-        sst->open_data().get();
-        t->add_sstable_and_update_cache(sst).get();
-        view_update_generator.register_staging_sstable(sst, t).get();
+        parallel_for_each(ssts.begin(), ssts.begin() + 10, [&] (shared_sstable& sst) {
+            return view_update_generator.register_staging_sstable(sst, t);
+        }).get();
+
+        BOOST_REQUIRE_EQUAL(view_update_generator.available_register_units(), db::view::view_update_generator::registration_queue_size);
+
+        parallel_for_each(ssts.begin() + 10, ssts.end(), [&] (shared_sstable& sst) {
+            return view_update_generator.register_staging_sstable(sst, t);
+        }).get();
+
+        BOOST_REQUIRE_EQUAL(view_update_generator.available_register_units(), db::view::view_update_generator::registration_queue_size);
 
         eventually([&, key1, key2] {
             auto msg = e.execute_cql(fmt::format("SELECT * FROM t WHERE p = '{}'", key1)).get0();
@@ -464,5 +490,7 @@ SEASTAR_TEST_CASE(test_view_update_generator) {
 
             }
         });
+
+        BOOST_REQUIRE_EQUAL(view_update_generator.available_register_units(), db::view::view_update_generator::registration_queue_size);
     });
 }