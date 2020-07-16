1cd6f50806e0cfea6b4785e38ad78e8eedd35db0
Raphael S. Carvalho <raphaelsc@scylladb.com>
2020-07-16 15:07:37 -0300

    table: Fix Staging SSTables being incorrectly added or removed from the backlog tracker
    Staging SSTables can be incorrectly added or removed from the backlog tracker,
    after an ALTER TABLE or TRUNCATE, because the add and removal don't take
    into account if the SSTable requires view building, so a Staging SSTable can
    be added to the tracker after a ALTER table, or removed after a TRUNCATE,
    even though not added previously, potentially causing the backlog to
    become negative.
    
    Fixes #6798.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>
    Message-Id: <20200716180737.944269-1-raphaelsc@scylladb.com>
    (cherry picked from commit b67066cae27d9ab7620e2f9c60dc58c7e29a14f1)

--- sstables/compaction_manager.cc
+++ sstables/compaction_manager.cc
@@ -795,7 +795,7 @@ double compaction_backlog_tracker::backlog() const {
 }
 
 void compaction_backlog_tracker::add_sstable(sstables::shared_sstable sst) {
-    if (_disabled) {
+    if (_disabled || !sstable_belongs_to_tracker(sst)) {
         return;
     }
     _ongoing_writes.erase(sst);
@@ -808,7 +808,7 @@ void compaction_backlog_tracker::add_sstable(sstables::shared_sstable sst) {
 }
 
 void compaction_backlog_tracker::remove_sstable(sstables::shared_sstable sst) {
-    if (_disabled) {
+    if (_disabled || !sstable_belongs_to_tracker(sst)) {
         return;
     }
 
@@ -819,6 +819,10 @@ void compaction_backlog_tracker::remove_sstable(sstables::shared_sstable sst) {
         cmlog.warn("Disabling backlog tracker due to exception {}", std::current_exception());
         disable();
     }
+}
+
+bool compaction_backlog_tracker::sstable_belongs_to_tracker(const sstables::shared_sstable& sst) {
+    return !sst->requires_view_building();
 }
 
 void compaction_backlog_tracker::register_partially_written_sstable(sstables::shared_sstable sst, backlog_write_progress_manager& wp) {
