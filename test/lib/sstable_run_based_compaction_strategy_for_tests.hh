aa2476d7ac95b944381141d77d8ffb57fee0ec83
Rafael Ávila de Espíndola <espindola@scylladb.com>
2020-08-11 11:35:41 -0700

    test: Move code in sstable_run_based_compaction_strategy_for_tests.hh out of line
    Most of this is virtual and it is all test code.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>

--- test/lib/sstable_run_based_compaction_strategy_for_tests.hh
+++ test/lib/sstable_run_based_compaction_strategy_for_tests.hh
@@ -34,53 +34,15 @@ namespace sstables {
 class sstable_run_based_compaction_strategy_for_tests : public compaction_strategy_impl {
     static constexpr size_t static_fragment_size_for_run = 1024*1024;
 public:
-    sstable_run_based_compaction_strategy_for_tests() = default;
+    sstable_run_based_compaction_strategy_for_tests();
 
-    virtual compaction_descriptor get_sstables_for_compaction(column_family& cf, std::vector<sstables::shared_sstable> uncompacting_sstables) override {
-        // Get unique runs from all uncompacting sstables
-        std::vector<sstable_run> runs = cf.get_sstable_set().select(uncompacting_sstables);
+    virtual compaction_descriptor get_sstables_for_compaction(column_family& cf, std::vector<sstables::shared_sstable> uncompacting_sstables) override;
 
-        // Group similar sized runs into a bucket.
-        std::map<uint64_t, std::vector<sstable_run>> similar_sized_runs;
-        for (auto& run : runs) {
-            bool found = false;
-            for (auto& e : similar_sized_runs) {
-                if (run.data_size() >= e.first*0.5 && run.data_size() <= e.first*1.5) {
-                    e.second.push_back(run);
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                similar_sized_runs[run.data_size()].push_back(run);
-            }
-        }
-        // Get the most interesting bucket, if any, and return sstables from all its runs.
-        for (auto& entry : similar_sized_runs) {
-            auto& runs = entry.second;
-            if (runs.size() < size_t(cf.schema()->min_compaction_threshold())) {
-                continue;
-            }
-            auto all = boost::accumulate(runs, std::vector<shared_sstable>(), [&] (std::vector<shared_sstable>&& v, const sstable_run& run) {
-                v.insert(v.end(), run.all().begin(), run.all().end());
-                return std::move(v);
-            });
-            return sstables::compaction_descriptor(std::move(all), std::nullopt, default_priority_class(), 0, static_fragment_size_for_run);
-        }
-        return sstables::compaction_descriptor();
-    }
+    virtual int64_t estimated_pending_compactions(column_family& cf) const override;
 
-    virtual int64_t estimated_pending_compactions(column_family& cf) const override {
-        throw std::runtime_error("unimplemented");
-    }
+    virtual compaction_strategy_type type() const;
 
-    virtual compaction_strategy_type type() const {
-        throw std::runtime_error("unimplemented");
-    }
-
-    virtual compaction_backlog_tracker& get_backlog_tracker() override {
-        throw std::runtime_error("unimplemented");
-    }
+    virtual compaction_backlog_tracker& get_backlog_tracker() override;
 };
 
 }
