scylladb/scylladb

commit 971711a5461c09421f1c9b1b3b2d9f6209a425da

storage proxy: migrate to per scheduling group statistics
This commit builds on top of the introduced per scheduling group
statistics template and employs it for achieving a per scheduling
group statistics in storage_proxy.

Some of the statistics also had meaning as a global - per
shard one. Those are the ones for determining if to
throttle the write request. This was handled by creating a
global stats struct that will hold those stats and by changing
the stat update to also include the global one.

One point that complicated it is an already existing aggregation
over the per shard stats that now became a per scheduling group
per shard stats, converting the aggregation to a two-dimensional
aggregation.

One thing this commit doesn't handle is validating that an individual
statistic didn't "cross a scheduling group boundary", such validation
is possible but it can easily be added in the future. There is a
subtlety to doing so since if the operation did cross to other
scheduling group two connected statistics can lose balance
for example written bytes and completed write transactions.

Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>

service/storage_proxy.hh
@@ -47,6 +47,7 @@
 #include "query-result-set.hh"
 #include <seastar/core/distributed.hh>
 #include <seastar/core/execution_stage.hh>
+#include <seastar/core/scheduling_specific.hh>
 #include "db/consistency_level_type.hh"
 #include "db/read_repair_decision.hh"
 #include "db/write_type.hh"
@@ -181,6 +182,7 @@ public:
 
     using write_stats = storage_proxy_stats::write_stats;
     using stats = storage_proxy_stats::stats;
+    using global_stats = storage_proxy_stats::global_stats;
 
     class coordinator_query_options {
         clock_type::time_point _timeout;
@@ -249,7 +251,8 @@ private:
     db::hints::resource_manager _hints_resource_manager;
     std::optional<db::hints::manager> _hints_manager;
     db::hints::manager _hints_for_views_manager;
-    stats _stats;
+    scheduling_group_key _stats_key;
+    storage_proxy_stats::global_stats _global_stats;
     static constexpr float CONCURRENT_SUBREQUESTS_MARGIN = 0.10;
     // for read repair chance calculation
     std::default_random_engine _urandom;
@@ -397,7 +400,8 @@ private:
     template<typename Range>
     future<> mutate_counters(Range&& mutations, db::consistency_level cl, tracing::trace_state_ptr tr_state, service_permit permit, clock_type::time_point timeout);
 public:
-    storage_proxy(distributed<database>& db, config cfg, db::view::node_update_backlog& max_view_update_backlog);
+    storage_proxy(distributed<database>& db, config cfg, db::view::node_update_backlog& max_view_update_backlog,
+            scheduling_group_key stats_key);
     ~storage_proxy();
     const distributed<database>& get_db() const {
         return _db;
@@ -541,7 +545,20 @@ public:
     future<> drain_on_shutdown();
 
     const stats& get_stats() const {
-        return _stats;
+        return scheduling_group_get_specific<storage_proxy_stats::stats>(_stats_key);
+    }
+    stats& get_stats() {
+        return scheduling_group_get_specific<storage_proxy_stats::stats>(_stats_key);
+    }
+    const global_stats& get_global_stats() const {
+        return _global_stats;
+    }
+    global_stats& get_global_stats() {
+        return _global_stats;
+    }
+
+    scheduling_group_key get_stats_key() const {
+        return _stats_key;
     }
 
     static unsigned cas_shard(dht::token token);

commit dfa0f1fd7cd7cb0aef607966afb7b3ff8cd36dd6

Storage Proxy: Maintain metrics in response handler classes
There is a known problem introduced by 971711a, the problem
is that when some callbacks for a created response handler
are called from another scheduling groups an imbalance is
introduced to statistics which are now instantiated per
scheduling group. This can be easily solved by "remembering"
the stats of the scheduling group where the response handler
was created. This will mask out another problem where certain
operations are done outside of the "native" scheduling group.
This is a complex issue that needs to be addressed in separate
commit (see 971711a commit message).

Fixes #5797

service/storage_proxy.cc
@@ -846,9 +846,9 @@ paxos_response_handler::begin_and_repair_paxos(client_state& cs, unsigned& conte
                     paxos::paxos_state::logger.debug("CAS[{}] Finishing incomplete paxos round {}", _id, *in_progress);
                     tracing::trace(tr_state, "Finishing incomplete paxos round {}", *in_progress);
                     if (is_write) {
-                        ++_proxy->get_stats().cas_write_unfinished_commit;
+                        ++_stats.cas_write_unfinished_commit;
                     } else {
-                        ++_proxy->get_stats().cas_read_unfinished_commit;
+                        ++_stats.cas_read_unfinished_commit;
                     }
 
                     auto refreshed_in_progress = make_lw_shared<paxos::proposal>(ballot, std::move(in_progress->update));
@@ -1189,7 +1189,7 @@ future<bool> paxos_response_handler::accept_proposal(lw_shared_ptr<paxos::propos
                         // accepted, either.
                         paxos::paxos_state::logger.trace("CAS[{}] accept_proposal: proposal is partially rejected", _id);
                         tracing::trace(tr_state, "accept_proposal: proposal is partially rejected");
-                        _proxy->get_stats().cas_write_timeout_due_to_uncertainty++;
+                        _stats.cas_write_timeout_due_to_uncertainty++;
                         // TODO: we report write timeout exception to be compatible with Cassandra,
                         // which uses write_timeout_exception to signal any "unknown" state.
                         // To be changed in scope of work on https://issues.apache.org/jira/browse/CASSANDRA-15350
@@ -1255,12 +1255,12 @@ future<> paxos_response_handler::learn_decision(lw_shared_ptr<paxos::proposal> d
 }
 
 void paxos_response_handler::prune(utils::UUID ballot) {
-    if ( _proxy->get_stats().cas_now_pruning >= pruning_limit) {
-        _proxy->get_stats().cas_coordinator_dropped_prune++;
+    if ( _stats.cas_now_pruning >= pruning_limit) {
+        _stats.cas_coordinator_dropped_prune++;
         return;
     }
-     _proxy->get_stats().cas_now_pruning++;
-    _proxy->get_stats().cas_prune++;
+    _stats.cas_now_pruning++;
+    _stats.cas_prune++;
     // running in the background, but the amount of the bg job is limited by pruning_limit
     // it is waited by holding shared pointer to storage_proxy which guaranties
     // that storage_proxy::stop() will wait for this to complete
@@ -1274,7 +1274,7 @@ void paxos_response_handler::prune(utils::UUID ballot) {
             return ms.send_paxos_prune(peer, _timeout, _schema->version(), _key.key(), ballot, tracing::make_trace_info(tr_state));
         }
     }).finally([h = shared_from_this()] {
-        h->_proxy->get_stats().cas_now_pruning--;
+        h->_stats.cas_now_pruning--;
     });
 }
 
@@ -3393,24 +3393,24 @@ protected:
     lw_shared_ptr<column_family> _cf;
     bool _foreground = true;
     service_permit _permit; // holds admission permit until operation completes
-
+    storage_proxy::stats &_stats;
 private:
     void on_read_resolved() noexcept {
         // We could have !_foreground if this is called on behalf of background reconciliation.
-        _proxy->get_stats().foreground_reads -= int(_foreground);
+        _stats.foreground_reads -= int(_foreground);
         _foreground = false;
     }
 public:
     abstract_read_executor(schema_ptr s, lw_shared_ptr<column_family> cf, shared_ptr<storage_proxy> proxy, lw_shared_ptr<query::read_command> cmd, dht::partition_range pr, db::consistency_level cl, size_t block_for,
             std::vector<gms::inet_address> targets, tracing::trace_state_ptr trace_state, service_permit permit) :
                            _schema(std::move(s)), _proxy(std::move(proxy)), _cmd(std::move(cmd)), _partition_range(std::move(pr)), _cl(cl), _block_for(block_for), _targets(std::move(targets)), _trace_state(std::move(trace_state)),
-                           _cf(std::move(cf)), _permit(std::move(permit)) {
-        _proxy->get_stats().reads++;
-        _proxy->get_stats().foreground_reads++;
+                           _cf(std::move(cf)), _permit(std::move(permit)), _stats(_proxy->get_stats()) {
+        _stats.reads++;
+        _stats.foreground_reads++;
     }
     virtual ~abstract_read_executor() {
-        _proxy->get_stats().reads--;
-        _proxy->get_stats().foreground_reads -= int(_foreground);
+        _stats.reads--;
+        _stats.foreground_reads -= int(_foreground);
     }
 
     /// Targets that were successfully ised for data and/or digest requests.
@@ -3423,7 +3423,7 @@ public:
 
 protected:
     future<rpc::tuple<foreign_ptr<lw_shared_ptr<reconcilable_result>>, cache_temperature>> make_mutation_data_request(lw_shared_ptr<query::read_command> cmd, gms::inet_address ep, clock_type::time_point timeout) {
-        ++_proxy->get_stats().mutation_data_read_attempts.get_ep_stat(ep);
+        ++_stats.mutation_data_read_attempts.get_ep_stat(ep);
         if (fbu::is_me(ep)) {
             tracing::trace(_trace_state, "read_mutation_data: querying locally");
             return _proxy->query_mutations_locally(_schema, cmd, _partition_range, timeout, _trace_state);
@@ -3438,7 +3438,7 @@ protected:
         }
     }
     future<rpc::tuple<foreign_ptr<lw_shared_ptr<query::result>>, cache_temperature>> make_data_request(gms::inet_address ep, clock_type::time_point timeout, bool want_digest) {
-        ++_proxy->get_stats().data_read_attempts.get_ep_stat(ep);
+        ++_stats.data_read_attempts.get_ep_stat(ep);
         auto opts = want_digest
                   ? query::result_options{query::result_request::result_and_digest, digest_algorithm(*_proxy)}
                   : query::result_options{query::result_request::only_result, query::digest_algorithm::none};
@@ -3456,7 +3456,7 @@ protected:
         }
     }
     future<rpc::tuple<query::result_digest, api::timestamp_type, cache_temperature>> make_digest_request(gms::inet_address ep, clock_type::time_point timeout) {
-        ++_proxy->get_stats().digest_read_attempts.get_ep_stat(ep);
+        ++_stats.digest_read_attempts.get_ep_stat(ep);
         if (fbu::is_me(ep)) {
             tracing::trace(_trace_state, "read_digest: querying locally");
             return _proxy->query_result_local_digest(_schema, _cmd, _partition_range, _trace_state,
@@ -3480,9 +3480,9 @@ protected:
                     auto v = f.get0();
                     _cf->set_hit_rate(ep, std::get<1>(v));
                     resolver->add_mutate_data(ep, std::get<0>(std::move(v)));
-                    ++_proxy->get_stats().mutation_data_read_completed.get_ep_stat(ep);
+                    ++_stats.mutation_data_read_completed.get_ep_stat(ep);
                 } catch(...) {
-                    ++_proxy->get_stats().mutation_data_read_errors.get_ep_stat(ep);
+                    ++_stats.mutation_data_read_errors.get_ep_stat(ep);
                     resolver->error(ep, std::current_exception());
                 }
             });
@@ -3495,10 +3495,10 @@ protected:
                     auto v = f.get0();
                     _cf->set_hit_rate(ep, std::get<1>(v));
                     resolver->add_data(ep, std::get<0>(std::move(v)));
-                    ++_proxy->get_stats().data_read_completed.get_ep_stat(ep);
+                    ++_stats.data_read_completed.get_ep_stat(ep);
                     _used_targets.push_back(ep);
                 } catch(...) {
-                    ++_proxy->get_stats().data_read_errors.get_ep_stat(ep);
+                    ++_stats.data_read_errors.get_ep_stat(ep);
                     resolver->error(ep, std::current_exception());
                 }
             });
@@ -3511,10 +3511,10 @@ protected:
                     auto v = f.get0();
                     _cf->set_hit_rate(ep, std::get<2>(v));
                     resolver->add_digest(ep, std::get<0>(v), std::get<1>(v));
-                    ++_proxy->get_stats().digest_read_completed.get_ep_stat(ep);
+                    ++_stats.digest_read_completed.get_ep_stat(ep);
                     _used_targets.push_back(ep);
                 } catch(...) {
-                    ++_proxy->get_stats().digest_read_errors.get_ep_stat(ep);
+                    ++_stats.digest_read_errors.get_ep_stat(ep);
                     resolver->error(ep, std::current_exception());
                 }
             });
@@ -3580,7 +3580,7 @@ protected:
                         on_read_resolved();
                     });
                 } else {
-                    _proxy->get_stats().read_retries++;
+                    _stats.read_retries++;
                     _retry_cmd = make_lw_shared<query::read_command>(*cmd);
                     // We asked t (= cmd->get_row_limit()) live columns and got l (=data_resolver->total_live_count) ones.
                     // From that, we can estimate that on this row, for x requested
@@ -3664,14 +3664,14 @@ public:
                         auto write_timeout = exec->_proxy->_db.local().get_config().write_request_timeout_in_ms() * 1000;
                         auto delta = int64_t(digest_resolver->last_modified()) - int64_t(exec->_cmd->read_timestamp);
                         if (std::abs(delta) <= write_timeout) {
-                            exec->_proxy->get_stats().global_read_repairs_canceled_due_to_concurrent_write++;
+                            exec->_stats.global_read_repairs_canceled_due_to_concurrent_write++;
                             // if CL is local and non matching data is modified less then write_timeout ms ago do only local repair
                             auto i = boost::range::remove_if(exec->_targets, std::not_fn(std::cref(db::is_local)));
                             exec->_targets.erase(i, exec->_targets.end());
                         }
                     }
                     exec->reconcile(exec->_cl, timeout);
-                    exec->_proxy->get_stats().read_repair_repaired_blocking++;
+                    exec->_stats.read_repair_repaired_blocking++;
                 }
             } catch (...) {
                 exec->_result_promise.set_exception(std::current_exception());
@@ -3681,7 +3681,7 @@ public:
             // Waited on indirectly.
             (void)digest_resolver->done().then([exec, digest_resolver, timeout, background_repair_check] () mutable {
                 if (background_repair_check && !digest_resolver->digests_match()) {
-                    exec->_proxy->get_stats().read_repair_repaired_background++;
+                    exec->_stats.read_repair_repaired_background++;
                     exec->_result_promise = promise<foreign_ptr<lw_shared_ptr<query::result>>>();
                     exec->reconcile(exec->_cl, timeout);
                     return exec->_result_promise.get_future().discard_result();
@@ -3735,10 +3735,10 @@ public:
                 // FIXME: consider disabling for CL=*ONE
                 auto send_request = [&] (bool has_data) {
                     if (has_data) {
-                        _proxy->get_stats().speculative_digest_reads++;
+                        _stats.speculative_digest_reads++;
                         return make_digest_requests(resolver, _targets.end() - 1, _targets.end(), timeout);
                     } else {
-                        _proxy->get_stats().speculative_data_reads++;
+                        _stats.speculative_data_reads++;
                         return make_data_requests(resolver, _targets.end() - 1, _targets.end(), timeout, true);
                     }
                 };

service/storage_proxy.hh
@@ -658,6 +658,8 @@ private:
     service_permit _permit;
     // how many replicas replied to learn
     uint64_t _learned = 0;
+    //The origin sg stats in order to avoid unbalance
+    storage_proxy::stats& _stats;
 
     // Unique request id generator.
     static thread_local uint64_t next_id;
@@ -687,19 +689,20 @@ public:
         , _cas_timeout(cas_timeout_arg)
         , _key(std::move(key_arg))
         , _permit(std::move(permit_arg))
+        , _stats(_proxy->get_stats())
         , tr_state(tr_state_arg) {
         storage_proxy::paxos_participants pp = _proxy->get_paxos_participants(_schema->ks_name(), _key.token(), _cl_for_paxos);
         _live_endpoints = std::move(pp.endpoints);
         _required_participants = pp.required_participants;
         tracing::trace(tr_state, "Create paxos_response_handler for token {} with live: {} and required participants: {}",
                 _key.token(), _live_endpoints, _required_participants);
-        _proxy->get_stats().cas_foreground++;
-        _proxy->get_stats().cas_total_running++;
-        _proxy->get_stats().cas_total_operations++;
+        _stats.cas_foreground++;
+        _stats.cas_total_running++;
+        _stats.cas_total_operations++;
     }
 
     ~paxos_response_handler() {
-        _proxy->get_stats().cas_total_running--;
+        _stats.cas_total_running--;
     }
 
     // Result of PREPARE step, i.e. begin_and_repair_paxos().