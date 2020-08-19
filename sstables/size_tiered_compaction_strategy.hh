db6303dba0d90392a0825ffaf59ed50d3c5fbf54
Avi Kivity <avi@scylladb.com>
2020-08-19 11:34:41 +0300

    Merge "Fix TWCS compaction aggressiveness due to data segregation" from Raphael
    "
    After data segregation feature, anything that cause out-of-order writes,
    like read repair, can result in small updates to past time windows.
    This causes compaction to be very aggressive because whenever a past time
    window is updated like that, that time window is recompacted into a
    single SSTable.
    Users expect that once a window is closed, it will no longer be written
    to, but that has changed since the introduction of the data segregation
    future. We didn't anticipate the write amplification issues that the
    feature would cause. To fix this problem, let's perform size-tiered
    compaction on the windows that are no longer active and were updated
    because data was segregated. The current behavior where the last active
    window is merged into one file is kept. But thereafter, that same
    window will only be compacted using STCS.
    
    Fixes #6928.
    "
    
    * 'fix_twcs_agressiveness_after_data_segregation_v2' of github.com:raphaelsc/scylla:
      compaction/twcs: improve further debug messages
      compaction/twcs: Improve debug log which shows all windows
      test: Check that TWCS properly performs size-tiered compaction on past windows
      compaction/twcs: Make task estimation take into account the size-tiered behavior
      compaction/stcs: Export static function that estimates pending tasks
      compaction/stcs: Make get_buckets() static
      compact/twcs: Perform size-tiered compaction on past time windows
      compaction/twcs: Make strategy easier to extend by removing duplicated knowledge
      compaction/twcs: Make newest_bucket() non-static
      compaction/twcs: Move TWCS implementation into source file
    
    (cherry picked from commit 6f986df4587f11bcae415eb72dd0b46f3097c14a)

--- sstables/size_tiered_compaction_strategy.hh
+++ sstables/size_tiered_compaction_strategy.hh
@@ -116,9 +116,11 @@ class size_tiered_compaction_strategy : public compaction_strategy_impl {
     compaction_backlog_tracker _backlog_tracker;
 
     // Return a list of pair of shared_sstable and its respective size.
-    std::vector<std::pair<sstables::shared_sstable, uint64_t>> create_sstable_and_length_pairs(const std::vector<sstables::shared_sstable>& sstables) const;
+    static std::vector<std::pair<sstables::shared_sstable, uint64_t>> create_sstable_and_length_pairs(const std::vector<sstables::shared_sstable>& sstables);
 
     // Group files of similar size into buckets.
+    static std::vector<std::vector<sstables::shared_sstable>> get_buckets(const std::vector<sstables::shared_sstable>& sstables, size_tiered_compaction_strategy_options options);
+
     std::vector<std::vector<sstables::shared_sstable>> get_buckets(const std::vector<sstables::shared_sstable>& sstables) const;
 
     // Maybe return a bucket of sstables to compact
@@ -154,6 +156,8 @@ public:
 
     virtual compaction_descriptor get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) override;
 
+    static int64_t estimated_pending_compactions(const std::vector<sstables::shared_sstable>& sstables,
+        int min_threshold, int max_threshold, size_tiered_compaction_strategy_options options);
     virtual int64_t estimated_pending_compactions(column_family& cf) const override;
 
     virtual compaction_strategy_type type() const {
