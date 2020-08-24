c36bbc37c94fa5661df414cf8829a8aceba60a3d
Pavel Emelyanov <xemul@scylladb.com>
2020-08-24 13:41:58 +0300

    view_builder: Wakeup barrier on exception
    If an exception pops up during the view_builder::start while some
    shards wait for the status-read barrier, these shards are not woken
    up, thus causing the shutdown to stuck.
    
    Fix this by setting exception on the barrier promise, resolving all
    pending and on-going futures.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- db/view/view.cc
+++ db/view/view.cc
@@ -1255,7 +1255,7 @@ future<> view_builder::start(service::migration_manager& mm) {
             auto built = system_keyspace::load_built_views().get0();
             auto in_progress = system_keyspace::load_view_build_progress().get0();
             setup_shard_build_step(vbi, std::move(built), std::move(in_progress));
-        }).then([this] {
+        }).then_wrapped([this] (future<>&& f) {
             // All shards need to arrive at the same decisions on whether or not to
             // restart a view build at some common token (reshard), and which token
             // to restart at. So we need to wait until all shards have read the view
@@ -1263,9 +1263,21 @@ future<> view_builder::start(service::migration_manager& mm) {
             // If we don't synchronize here, a fast shard may make a decision, start
             // building and finish a build step - before the slowest shard even read
             // the view build information.
-            return container().invoke_on(0, [] (view_builder& builder) {
-                if (++builder._shards_finished_read == smp::count) {
-                    builder._shards_finished_read_promise.set_value();
+            std::exception_ptr eptr;
+            if (f.failed()) {
+                eptr = f.get_exception();
+            }
+
+            return container().invoke_on(0, [eptr = std::move(eptr)] (view_builder& builder) {
+                if (!eptr) {
+                    if (++builder._shards_finished_read == smp::count) {
+                        builder._shards_finished_read_promise.set_value();
+                    }
+                } else {
+                    if (builder._shards_finished_read < smp::count) {
+                        builder._shards_finished_read = smp::count;
+                        builder._shards_finished_read_promise.set_exception(std::move(eptr));
+                    }
                 }
                 return builder._shards_finished_read_promise.get_shared_future();
             });
