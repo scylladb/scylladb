edb4f464fad6231ab47c5e062f83c01e59d6d4cd
Piotr Sarna <sarna@scylladb.com>
2020-09-07 16:33:42 +0200

    reader_concurrency_semaphore: add linear backpressure
    Once the reader concurrency queue is full, the old behavior was to
    immediately refuse all surplus requests. Given very high client
    concurrency, it's possible that Scylla would be very busy sending
    refusals, causing the successful requests to have very high latency.
    To make the issue less severe, requests are now refused with the
    following heuristics, given max queue length Qmax:
     - requests before Qmax are not throttled
     - request Qmax+nth has n% chance to get throttled
     - when throttled, the request is refused, but it's first delayed
       by 10*n milliseconds.
    For instance, if max queue is 100, request 142 has 42% chance
    to get throttled, in which case it will be refused after being
    delayed for 420ms.
    The delays reduce the concurrency of refused requests, while still
    allowing the successful ones to have decent latency.
    The delay is capped to 1s in order to avoid ridiculous values.
    The limit is based on local cassandra-stress tests, in which the delay
    stabilized around 700ms.
    Metrics in local tests showed that while the average latency for all
    requests (including errors) raises, the latency of successful requests,
    which is more meaningful, drops from >1s to sub-500ms. Also, the number
    of failed requests drops, while the total number of served requests
    remains the same.

--- reader_concurrency_semaphore.cc
+++ reader_concurrency_semaphore.cc
@@ -24,7 +24,10 @@
 
 #include "reader_concurrency_semaphore.hh"
 #include "utils/exceptions.hh"
+#include <random>
 
+#include <seastar/core/sleep.hh>
+#include "exceptions/exceptions.hh"
 
 reader_permit::resource_units::resource_units(reader_concurrency_semaphore& semaphore, reader_resources res) noexcept
         : _semaphore(&semaphore), _resources(res) {
@@ -149,14 +152,33 @@ bool reader_concurrency_semaphore::try_evict_one_inactive_read() {
     return true;
 }
 
+class queue_throttler {
+    static constexpr size_t max_error_response_delay_ms = 1000;
+    size_t _severity;
+public:
+    queue_throttler(size_t current, size_t max)
+            : _severity(current > max ? current - max : 0) {}
+    bool should_throttle() {
+        static thread_local std::default_random_engine random_engine{std::random_device{}()};
+        static thread_local std::uniform_int_distribution<size_t> random_dist{0, 99};
+        return _severity > 0 && (_severity >= 100 || random_dist(random_engine) < _severity);
+    }
+    future<reader_permit::resource_units> throttle(sstring queue_name) {
+        return seastar::sleep(std::chrono::milliseconds(std::min(10 * _severity, max_error_response_delay_ms))).then([queue_name = std::move(queue_name)] {
+            return make_exception_future<reader_permit::resource_units>(
+                    std::make_exception_ptr(std::runtime_error(
+                            format("{}: restricted mutation reader queue overload", queue_name))));
+        });
+    }
+};
+
 future<reader_permit::resource_units> reader_concurrency_semaphore::do_wait_admission(size_t memory, db::timeout_clock::time_point timeout) {
-    if (_wait_list.size() >= _max_queue_length) {
+    queue_throttler throttler(_wait_list.size(), _max_queue_length);
+    if (throttler.should_throttle()) {
         if (_prethrow_action) {
             _prethrow_action();
         }
-        return make_exception_future<reader_permit::resource_units>(
-                std::make_exception_ptr(std::runtime_error(
-                        format("{}: restricted mutation reader queue overload", _name))));
+        return throttler.throttle(_name);
     }
     auto r = resources(1, static_cast<ssize_t>(memory));
     auto it = _inactive_reads.begin();
