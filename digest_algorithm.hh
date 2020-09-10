psarna/scylla

digest: add null values to row digest
With the new hashing routine, null values are taken into account
when computing row digest. Previous behavior had a regression
which stopped computing the hash after the first null value
is encountered, but the original behavior was also prone
to errors - e.g. row [1, NULL, 2] was not distinguishable
from [1, 2, NULL], because their hashes were identical.
This hashing is not yet active - it will only be used after
the next commit introduces a proper cluster feature for it.

digest_algorithm.hh
@@ -28,7 +28,8 @@ namespace query {
 enum class digest_algorithm : uint8_t {
     none = 0,  // digest not required
     MD5 = 1,
-    xxHash = 2,// default algorithm
+    xxHash = 2,
+    xxHash_with_null_digest = 3, // default algorithm
 };
 
 }

digester.hh
@@ -36,7 +36,7 @@ struct noop_hasher {
 };
 
 class digester final {
-    std::variant<noop_hasher, md5_hasher, xx_hasher> _impl;
+    std::variant<noop_hasher, md5_hasher, xx_hasher, xx_hasher_with_null_digest> _impl;
 
 public:
     explicit digester(digest_algorithm algo) {
@@ -46,6 +46,9 @@ public:
             break;
         case digest_algorithm::xxHash:
             _impl = xx_hasher();
+            break;
+        case digest_algorithm::xxHash_with_null_digest:
+            _impl = xx_hasher_with_null_digest();
             break;
         case digest_algorithm ::none:
             _impl = noop_hasher();

scylladb/scylladb

digest_algorithm.hh
@@ -28,7 +28,8 @@ namespace query {
 enum class digest_algorithm : uint8_t {
     none = 0,  // digest not required
     MD5 = 1,
-    xxHash = 2,// default algorithm
+    legacy_xxHash_without_null_digest = 2,
+    xxHash = 3, // default algorithm
 };
 
 }

digester.hh
@@ -36,7 +36,7 @@ struct noop_hasher {
 };
 
 class digester final {
-    std::variant<noop_hasher, md5_hasher, xx_hasher> _impl;
+    std::variant<noop_hasher, md5_hasher, xx_hasher, legacy_xx_hasher_without_null_digest> _impl;
 
 public:
     explicit digester(digest_algorithm algo) {
@@ -46,6 +46,9 @@ public:
             break;
         case digest_algorithm::xxHash:
             _impl = xx_hasher();
+            break;
+        case digest_algorithm::legacy_xxHash_without_null_digest:
+            _impl = legacy_xx_hasher_without_null_digest();
             break;
         case digest_algorithm ::none:
             _impl = noop_hasher();