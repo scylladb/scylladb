e19e86a6e79d71a7caa17a4a76e5d2189d10c27d
Piotr Sarna <sarna@scylladb.com>
2020-09-14 12:13:03 +0200

    service: drop checks for xxhash support
    xxhash algorithm is supported for over 2 years and upgrades are only
    allowed from versions which already have the support, so the checks
    are hereby dropped.

--- service/storage_proxy.cc
+++ service/storage_proxy.cc
@@ -126,9 +126,7 @@ static inline
 query::digest_algorithm digest_algorithm(service::storage_proxy& proxy) {
     return proxy.features().cluster_supports_digest_for_null_values()
             ? query::digest_algorithm::xxHash
-            : proxy.features().cluster_supports_xxhash_digest_algorithm()
-                    ? query::digest_algorithm::legacy_xxHash_without_null_digest
-                    : query::digest_algorithm::MD5;
+            : query::digest_algorithm::legacy_xxHash_without_null_digest;
 }
 
 static inline
