2f69e90fc90cf0c70eb5a768c932ef57da9eedde
Pavel Emelyanov <xemul@scylladb.com>
2020-09-18 15:48:40 +0300

    table_helper: Use local db variable
    The .setup_keyspace() method already has the db variable in this
    continuation lambda.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- table_helper.cc
+++ table_helper.cc
@@ -135,7 +135,7 @@ future<> table_helper::setup_keyspace(const sstring& keyspace_name, sstring repl
                 service::get_local_migration_manager().announce_new_keyspace(ksm, api::min_timestamp, false).get();
             }
 
-            qs.get_client_state().set_keyspace(cql3::get_local_query_processor().db(), keyspace_name);
+            qs.get_client_state().set_keyspace(db, keyspace_name);
 
 
             // Create tables
