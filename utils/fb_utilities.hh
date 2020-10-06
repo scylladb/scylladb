87c3fd9cd8c151f2987f826826d13f3a7e25861a
Benny Halevy <bhalevy@scylladb.com>
2020-10-06 08:57:30 +0300

    fb_utilities.hh: mark methods noexcept
    Now that gms::inet_address assignment is marked as noexcept.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/fb_utilities.hh
+++ utils/fb_utilities.hh
@@ -50,12 +50,12 @@ using inet_address = gms::inet_address;
 
 class fb_utilities {
 private:
-    static std::optional<inet_address>& broadcast_address() {
+    static std::optional<inet_address>& broadcast_address() noexcept {
         static std::optional<inet_address> _broadcast_address;
 
         return _broadcast_address;
     }
-    static std::optional<inet_address>& broadcast_rpc_address() {
+    static std::optional<inet_address>& broadcast_rpc_address() noexcept {
         static std::optional<inet_address> _broadcast_rpc_address;
 
         return _broadcast_rpc_address;
@@ -63,26 +63,26 @@ private:
 public:
    static constexpr int32_t MAX_UNSIGNED_SHORT = 0xFFFF;
 
-   static void set_broadcast_address(inet_address addr) {
+   static void set_broadcast_address(inet_address addr) noexcept {
        broadcast_address() = addr;
    }
 
-   static void set_broadcast_rpc_address(inet_address addr) {
+   static void set_broadcast_rpc_address(inet_address addr) noexcept {
        broadcast_rpc_address() = addr;
    }
 
 
-   static const inet_address get_broadcast_address() {
+   static const inet_address get_broadcast_address() noexcept {
        assert(broadcast_address());
        return *broadcast_address();
    }
 
-   static const inet_address get_broadcast_rpc_address() {
+   static const inet_address get_broadcast_rpc_address() noexcept {
        assert(broadcast_rpc_address());
        return *broadcast_rpc_address();
    }
 
-    static bool is_me(gms::inet_address addr) {
+    static bool is_me(gms::inet_address addr) noexcept {
         return addr == get_broadcast_address();
     }
 };
