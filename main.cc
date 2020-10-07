c887d0df4c9506ab79566a6a31906161d0b2cdbe
Pavel Emelyanov <xemul@scylladb.com>
2020-10-07 21:22:44 +0300

    thrift: Keep sharded query processor reference on controller
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- main.cc
+++ main.cc
@@ -1209,7 +1209,7 @@ int main(int ac, char** av) {
                 api::unset_transport_controller(ctx).get();
             });
 
-            ::thrift_controller thrift_ctl(db, auth_service);
+            ::thrift_controller thrift_ctl(db, auth_service, qp);
 
             ss.register_client_shutdown_hook("rpc server", [&thrift_ctl] {
                 thrift_ctl.stop().get();
