40c547402252c94c6d41f82cfce1251da788d43a
Botond Dénes <bdenes@scylladb.com>
2020-10-08 14:53:55 +0300

    querier_cache_test: test_immediate_evict_on_insert: use two permits
    The test currently uses a single permit shared between two simulated
    reads (to wait admission twice). This is not a supported way of using a
    permit and will stop working soon as we make the states the permit is in
    more pronounced.

--- test/boost/querier_cache_test.cc
+++ test/boost/querier_cache_test.cc
@@ -749,15 +749,16 @@ SEASTAR_THREAD_TEST_CASE(test_immediate_evict_on_insert) {
     test_querier_cache t;
 
     auto& sem = t.get_semaphore();
-    auto permit = sem.make_permit();
+    auto permit1 = sem.make_permit();
+    auto permit2 = sem.make_permit();
 
-    permit.wait_admission(0, db::no_timeout).get();
+    permit1.wait_admission(0, db::no_timeout).get();
 
-    auto resources = permit.consume_resources(reader_resources(sem.available_resources().count, 0));
+    auto resources = permit1.consume_resources(reader_resources(sem.available_resources().count, 0));
 
     BOOST_CHECK_EQUAL(sem.available_resources().count, 0);
 
-    auto fut = permit.wait_admission(1, db::no_timeout);
+    auto fut = permit2.wait_admission(1, db::no_timeout);
 
     BOOST_CHECK_EQUAL(sem.waiters(), 1);
 
