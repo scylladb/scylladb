ce8a0f388645cd3af54469b940cd8b90067ceab0
Calle Wilund <calle@scylladb.com>
2020-10-14 10:46:38 +0000

    partition_version: Change range_tombstones() to return chunked_vector
    Refs #7364
    
    The number of tombstones can be large. As a stopgap measure to
    just returning a source range (with keepalive), we can at least
    alleviate the problem by using a chunked vector.
    
    Closes #7433
    
    (cherry picked from commit 4b65d67a1aaf672a291e891da115163a75e29aa0)

--- partition_version.cc
+++ partition_version.cc
@@ -650,12 +650,12 @@ partition_snapshot_ptr partition_entry::read(logalloc::region& r,
     return partition_snapshot_ptr(std::move(snp));
 }
 
-std::vector<range_tombstone>
+partition_snapshot::range_tombstone_result
 partition_snapshot::range_tombstones(position_in_partition_view start, position_in_partition_view end)
 {
     partition_version* v = &*version();
     if (!v->next()) {
-        return boost::copy_range<std::vector<range_tombstone>>(
+        return boost::copy_range<range_tombstone_result>(
             v->partition().row_tombstones().slice(*_schema, start, end));
     }
     range_tombstone_list list(*_schema);
@@ -665,10 +665,10 @@ partition_snapshot::range_tombstones(position_in_partition_view start, position_
         }
         v = v->next();
     }
-    return boost::copy_range<std::vector<range_tombstone>>(list.slice(*_schema, start, end));
+    return boost::copy_range<range_tombstone_result>(list.slice(*_schema, start, end));
 }
 
-std::vector<range_tombstone>
+partition_snapshot::range_tombstone_result
 partition_snapshot::range_tombstones()
 {
     return range_tombstones(
