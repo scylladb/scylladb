e96ef0d62978ef2ff1571c78a0821babf4a93140
Piotr Grabowski <piotr.grabowski@scylladb.com>
2020-10-29 13:29:13 +0100

    tests: Cleanup select_statement_utils
    Add additional comments to select_statement_utils, fix formatting, add
    missing #pragma once and introduce set_internal_paging_size_guard to
    set internal_paging in RAII fashion.
    
    Closes #7507

--- test/lib/select_statement_utils.hh
+++ test/lib/select_statement_utils.hh
@@ -19,15 +19,40 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 #include <seastar/core/future.hh>
 
 namespace cql3 {
 
 namespace statements {
 
-	future<> set_internal_paging_size(int internal_paging_size);
-	future<> reset_internal_paging_size();
+    // In certain SELECT statements, such as statements with
+    // aggregates or GROUP BYs, internal paging is done with
+    // different paging size than specified in query options.
+    //
+    // set_internal_paging_size allows to override the default
+    // paging_size of this type of paging.
+    future<> set_internal_paging_size(int internal_paging_size);
 
+    // reset_internal_paging_size sets internal_paging_size
+    // to default value, which was set on startup - before 
+    // any calls to set_internal_paging_size.
+    future<> reset_internal_paging_size();
+
+    // Wrapper around set_internal_paging_size to set it
+    // to desired value on construction and reset it to 
+    // the default value on destruction. Should only be
+    // used inside Seastar thread.
+    struct set_internal_paging_size_guard {
+        set_internal_paging_size_guard(int internal_paging_size) {
+            set_internal_paging_size(internal_paging_size).get();
+        }
+
+        ~set_internal_paging_size_guard() {
+            reset_internal_paging_size().get();
+        }
+    };
 }
 
 }
