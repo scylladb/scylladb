64eef0a4f729f6f68955559f5d1bff62ab2e0b87
Pavel Emelyanov <xemul@scylladb.com>
2020-11-10 14:46:40 +0300

    test: Use cql_test_env::evecute_cql instead of qctx version
    Similar to previous patch, but for tests. Since cql_test_env
    does't have qctx on board, the patch makes one step forward
    and calls what is called by qctx::execute_cql.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/boost/cql_query_test.cc
+++ test/boost/cql_query_test.cc
@@ -4560,11 +4560,11 @@ SEASTAR_TEST_CASE(test_internal_schema_changes_on_a_distributed_table) {
         return seastar::async([&e] {
             cquery_nofail(e, "create table t (p int primary key, v int)");
             const auto local_err = exception_predicate::message_contains("internal query");
-            BOOST_REQUIRE_EXCEPTION(db::execute_cql("alter table ks.t add col abcd").get(), std::logic_error, local_err);
-            BOOST_REQUIRE_EXCEPTION(db::execute_cql("create table ks.t2 (id int primary key)").get(), std::logic_error, local_err);
-            BOOST_REQUIRE_EXCEPTION(db::execute_cql("create index on ks.t(v)").get(), std::logic_error, local_err);
-            BOOST_REQUIRE_EXCEPTION(db::execute_cql("drop table ks.t").get(), std::logic_error, local_err);
-            BOOST_REQUIRE_EXCEPTION(db::execute_cql("drop keyspace ks").get(), std::logic_error, local_err);
+            BOOST_REQUIRE_EXCEPTION(e.local_qp().execute_internal("alter table ks.t add col abcd").get(), std::logic_error, local_err);
+            BOOST_REQUIRE_EXCEPTION(e.local_qp().execute_internal("create table ks.t2 (id int primary key)").get(), std::logic_error, local_err);
+            BOOST_REQUIRE_EXCEPTION(e.local_qp().execute_internal("create index on ks.t(v)").get(), std::logic_error, local_err);
+            BOOST_REQUIRE_EXCEPTION(e.local_qp().execute_internal("drop table ks.t").get(), std::logic_error, local_err);
+            BOOST_REQUIRE_EXCEPTION(e.local_qp().execute_internal("drop keyspace ks").get(), std::logic_error, local_err);
         });
     });
 }
