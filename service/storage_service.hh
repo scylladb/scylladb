bhalevy/scylla

token_metadata: add clone_async
Clone token_metadata object using async continuation to
prevent reactor stalls.

Refs https://github.com/scylladb/scylla/issues/7220

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

service/storage_service.hh
@@ -178,8 +178,10 @@ private:
     future<> publish_schema_version();
     void install_schema_version_change_listener();
 
-    token_metadata_ptr get_mutable_token_metadata_ptr() {
-        return _shared_token_metadata.clone();
+    future<token_metadata_ptr> get_mutable_token_metadata_ptr() noexcept {
+        return _shared_token_metadata.get()->clone_async().then([] (token_metadata tm) {
+            return make_ready_future<token_metadata_ptr>(make_token_metadata_ptr(std::move(tm)));
+        });
     }
 
 public:

scylladb/scylladb

service/storage_service.hh
@@ -181,8 +181,10 @@ private:
     future<> publish_schema_version();
     void install_schema_version_change_listener();
 
-    mutable_token_metadata_ptr get_mutable_token_metadata_ptr() {
-        return _shared_token_metadata.clone();
+    future<mutable_token_metadata_ptr> get_mutable_token_metadata_ptr() noexcept {
+        return _shared_token_metadata.get()->clone_async().then([] (token_metadata tm) {
+            return make_ready_future<mutable_token_metadata_ptr>(make_token_metadata_ptr(std::move(tm)));
+        });
     }
 public:
     static future<> update_topology(inet_address endpoint);