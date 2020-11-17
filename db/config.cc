piodul/scylla

config: plug in hints::host_filter object into configuration
Uses db::hints::host_filter as the type of hinted_handoff_enabled
configuration option.

Previously, hinted_handoff_enabled used to be a string option, and it
was parsed later in a separate function during startup. The function
returned a std::optional<std::unordered_set<sstring>>, whose meaning in
the context of hints is rather enigmatic for an observer not familiar
with hints.

Now, hinted_handoff_enabled has type of db::hints::host_filter, and it
is plugged into the config parsing framework, so there is no need for
later post-processing.

db/config.cc
@@ -67,6 +67,12 @@ seed_provider_to_json(const db::seed_provider_type& spt) {
     return value_to_json("seed_provider_type");
 }
 
+static
+json::json_return_type
+hinted_handoff_enabled_to_json(const db::config::hinted_handoff_enabled_type& h) {
+    return value_to_json(h.to_configuration_string());
+}
+
 template <>
 const config_type config_type_for<bool> = config_type("bool", value_to_json<bool>);
 
@@ -113,6 +119,9 @@ template <>
 const config_type config_type_for<std::vector<enum_option<db::experimental_features_t>>> = config_type(
         "experimental features", value_to_json<std::vector<sstring>>);
 
+template <>
+const config_type config_type_for<db::config::hinted_handoff_enabled_type> = config_type("hinted handoff enabled", hinted_handoff_enabled_to_json);
+
 }
 
 namespace YAML {
@@ -154,6 +163,18 @@ struct convert<db::config::seed_provider_type> {
                 }
             }
         }
+        return true;
+    }
+};
+
+template<>
+struct convert<db::config::hinted_handoff_enabled_type> {
+    static bool decode(const Node& node, db::config::hinted_handoff_enabled_type& rhs) {
+        std::string opt;
+        if (!convert<std::string>::decode(node, opt)) {
+            return false;
+        }
+        rhs = db::hints::host_filter::parse_from_config_string(std::move(opt));
         return true;
     }
 };
@@ -567,7 +588,7 @@ db::config::config(std::shared_ptr<db::extensions> exts)
         "Time interval in milliseconds to reset all node scores, which allows a bad node to recover.")
     , dynamic_snitch_update_interval_in_ms(this, "dynamic_snitch_update_interval_in_ms", value_status::Unused, 100,
         "The time interval for how often the snitch calculates node scores. Because score calculation is CPU intensive, be careful when reducing this interval.")
-    , hinted_handoff_enabled(this, "hinted_handoff_enabled", value_status::Used, "true",
+    , hinted_handoff_enabled(this, "hinted_handoff_enabled", value_status::Used, db::hints::host_filter(db::hints::host_filter::enabled_for_all_tag()),
         "Enable or disable hinted handoff. To enable per data center, add data center list. For example: hinted_handoff_enabled: DC1,DC2. A hint indicates that the write needs to be replayed to an unavailable node. "
         "Related information: About hinted handoff writes")
     , hinted_handoff_throttle_in_kb(this, "hinted_handoff_throttle_in_kb", value_status::Unused, 1024,

service/storage_proxy.hh
@@ -63,6 +63,7 @@
 #include "service_permit.hh"
 #include "service/client_state.hh"
 #include "cdc/stats.hh"
+#include "db/hints/host_filter.hh"
 
 class reconcilable_result;
 class frozen_mutation_and_schema;
@@ -158,7 +159,7 @@ class storage_proxy : public seastar::async_sharded_service<storage_proxy>, publ
 public:
     using clock_type = lowres_clock;
     struct config {
-        std::optional<std::vector<sstring>> hinted_handoff_enabled = {};
+        db::hints::host_filter hinted_handoff_enabled = {};
         size_t available_memory;
         smp_service_group read_smp_service_group = default_smp_service_group();
         smp_service_group write_smp_service_group = default_smp_service_group();

scylladb/scylladb

db/config.cc
@@ -67,6 +67,12 @@ seed_provider_to_json(const db::seed_provider_type& spt) {
     return value_to_json("seed_provider_type");
 }
 
+static
+json::json_return_type
+hinted_handoff_enabled_to_json(const db::config::hinted_handoff_enabled_type& h) {
+    return value_to_json(h.to_configuration_string());
+}
+
 template <>
 const config_type config_type_for<bool> = config_type("bool", value_to_json<bool>);
 
@@ -113,6 +119,9 @@ template <>
 const config_type config_type_for<std::vector<enum_option<db::experimental_features_t>>> = config_type(
         "experimental features", value_to_json<std::vector<sstring>>);
 
+template <>
+const config_type config_type_for<db::config::hinted_handoff_enabled_type> = config_type("hinted handoff enabled", hinted_handoff_enabled_to_json);
+
 }
 
 namespace YAML {
@@ -154,6 +163,18 @@ struct convert<db::config::seed_provider_type> {
                 }
             }
         }
+        return true;
+    }
+};
+
+template<>
+struct convert<db::config::hinted_handoff_enabled_type> {
+    static bool decode(const Node& node, db::config::hinted_handoff_enabled_type& rhs) {
+        std::string opt;
+        if (!convert<std::string>::decode(node, opt)) {
+            return false;
+        }
+        rhs = db::hints::host_filter::parse_from_config_string(std::move(opt));
         return true;
     }
 };
@@ -571,7 +592,7 @@ db::config::config(std::shared_ptr<db::extensions> exts)
         "Time interval in milliseconds to reset all node scores, which allows a bad node to recover.")
     , dynamic_snitch_update_interval_in_ms(this, "dynamic_snitch_update_interval_in_ms", value_status::Unused, 100,
         "The time interval for how often the snitch calculates node scores. Because score calculation is CPU intensive, be careful when reducing this interval.")
-    , hinted_handoff_enabled(this, "hinted_handoff_enabled", value_status::Used, "true",
+    , hinted_handoff_enabled(this, "hinted_handoff_enabled", value_status::Used, db::config::hinted_handoff_enabled_type(db::config::hinted_handoff_enabled_type::enabled_for_all_tag()),
         "Enable or disable hinted handoff. To enable per data center, add data center list. For example: hinted_handoff_enabled: DC1,DC2. A hint indicates that the write needs to be replayed to an unavailable node. "
         "Related information: About hinted handoff writes")
     , hinted_handoff_throttle_in_kb(this, "hinted_handoff_throttle_in_kb", value_status::Unused, 1024,

service/storage_proxy.hh
@@ -64,6 +64,8 @@
 #include "service/client_state.hh"
 #include "cdc/stats.hh"
 #include "locator/token_metadata.hh"
+#include "db/hints/host_filter.hh"
+#include "db/config.hh"
 
 class reconcilable_result;
 class frozen_mutation_and_schema;
@@ -153,7 +155,7 @@ class storage_proxy : public seastar::async_sharded_service<storage_proxy>, publ
 public:
     using clock_type = lowres_clock;
     struct config {
-        std::optional<std::vector<sstring>> hinted_handoff_enabled = {};
+        db::config::hinted_handoff_enabled_type hinted_handoff_enabled = {};
         size_t available_memory;
         smp_service_group read_smp_service_group = default_smp_service_group();
         smp_service_group write_smp_service_group = default_smp_service_group();