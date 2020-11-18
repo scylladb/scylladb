5b79d6ca4c267c6bfac76d876b22c55a873029a0
Michał Chojnowski <michal.chojnowski@scylladb.com>
2020-11-18 20:43:28 +0100

    test: mutation_test: remove an obsolete assertion
    Due to small value optimizations, the removed assertions are not true in
    general. Until now, atomic_cell did not use small value optimizations, but
    it will after upcoming changes.

--- test/boost/mutation_test.cc
+++ test/boost/mutation_test.cc
@@ -2002,7 +2002,6 @@ SEASTAR_THREAD_TEST_CASE(test_cell_external_memory_usage) {
             auto before = alloc.allocated_bytes();
             auto ac = atomic_cell_or_collection(atomic_cell::make_live(*dt, 1, bv));
             auto after = alloc.allocated_bytes();
-            BOOST_CHECK_GE(ac.external_memory_usage(*dt), bv.size());
             BOOST_CHECK_EQUAL(ac.external_memory_usage(*dt), after - before);
         });
     };
@@ -2030,7 +2029,6 @@ SEASTAR_THREAD_TEST_CASE(test_cell_external_memory_usage) {
             auto before = alloc.allocated_bytes();
             auto cell2 = cell.copy(*collection_type);
             auto after = alloc.allocated_bytes();
-            BOOST_CHECK_GE(cell2.external_memory_usage(*collection_type), bv.size());
             BOOST_CHECK_EQUAL(cell2.external_memory_usage(*collection_type), cell.external_memory_usage(*collection_type));
             BOOST_CHECK_EQUAL(cell2.external_memory_usage(*collection_type), after - before);
         });
