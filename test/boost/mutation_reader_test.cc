0b36c5e116a19c4b152e2b94a607f9c9dc0a89ad
Kamil Braun <kbraun@scylladb.com>
2020-11-18 12:24:53 +0100

    mutation_reader: introduce clustering_order_reader_merger
    This abstraction is used to merge the output of multiple readers, each
    opened for a single partition query, into a non-decreasing stream
    of mutation_fragments.
    
    It is similar to `mutation_reader_merger`,
    an important difference is that the new merger may select new readers
    in the middle of a partition after it already returned some fragments
    from that partition.  It uses the new `position_reader_queue` abstraction
    to select new readers. It doesn't support multi-partition (ring range) queries.
    
    The new merger will be later used when reading from sstable sets created
    by TimeWindowCompactionStrategy. This strategy creates many sstables
    that are mostly disjoint w.r.t the contained clustering keys, so we can
    delay opening sstable readers when querying a partition until after we have
    processed all mutation fragments with positions before the keys
    contained by these sstables.

--- test/boost/mutation_reader_test.cc
+++ test/boost/mutation_reader_test.cc
@@ -27,6 +27,7 @@
 #include <seastar/core/sleep.hh>
 #include <seastar/core/do_with.hh>
 #include <seastar/core/thread.hh>
+#include <seastar/core/coroutine.hh>
 
 #include <seastar/testing/test_case.hh>
 #include <seastar/testing/thread_test_case.hh>
