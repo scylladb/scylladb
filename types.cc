41b889d0c80fe1c9d15401602f939edc89a366d1
Michał Chojnowski <michal.chojnowski@scylladb.com>
2020-12-04 14:35:07 +0100

    types: serialize lists to bytes_ostream
    Avoids linearization by serializing to a fragmented type.
    It's still linearized at the very end, this will be changed in the near future.

--- types.cc
+++ types.cc
@@ -3072,15 +3072,17 @@ static bytes serialize_for_cql_aux(const set_type_impl&, collection_mutation_vie
 }
 
 static bytes serialize_for_cql_aux(const list_type_impl&, collection_mutation_view_description mut, cql_serialization_format sf) {
-    std::vector<bytes> linearized_values;
-    std::vector<bytes_view> tmp;
-    tmp.reserve(mut.cells.size());
+    bytes_ostream out;
+    auto len_slot = out.write_place_holder(collection_size_len(sf));
+    int elements = 0;
     for (auto&& e : mut.cells) {
         if (e.second.is_live(mut.tomb, false)) {
-            tmp.push_back(linearized(e.second.value(), linearized_values));
+            write_collection_value(out, sf, e.second.value());
+            elements += 1;
         }
     }
-    return collection_type_impl::pack(tmp.begin(), tmp.end(), tmp.size(), sf);
+    write_collection_size(len_slot, elements, sf);
+    return linearized(out);
 }
 
 static bytes serialize_for_cql_aux(const user_type_impl& type, collection_mutation_view_description mut, cql_serialization_format) {
