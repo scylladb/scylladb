psarna/scylla

treewide: remove timeout config from query options
Timeout config is now stored in each connection, so there's no point
in tracking it inside each query as well. This patch removes
timeout_config from query_options and follows by removing now
unnecessary parameters of many functions and constructors.

service/client_state.hh
@@ -206,12 +206,15 @@ public:
         return _timeout_config;
     }
 
-    client_state(internal_tag)
+    client_state(internal_tag) : client_state(internal_tag{}, infinite_timeout_config)
+    {}
+
+    client_state(internal_tag, const timeout_config& config)
             : _keyspace("system")
             , _is_internal(true)
             , _is_thrift(false)
-            , _global_timeout_config_ref(infinite_timeout_config)
-            , _timeout_config(infinite_timeout_config)
+            , _global_timeout_config_ref(config)
+            , _timeout_config(config)
     {}
 
     client_state(const client_state&) = delete;

scylladb/scylladb

service/client_state.hh
@@ -198,12 +198,15 @@ public:
         return _timeout_config;
     }
 
-    client_state(internal_tag)
+    client_state(internal_tag) : client_state(internal_tag{}, infinite_timeout_config)
+    {}
+
+    client_state(internal_tag, const timeout_config& config)
             : _keyspace("system")
             , _is_internal(true)
             , _is_thrift(false)
-            , _default_timeout_config(infinite_timeout_config)
-            , _timeout_config(infinite_timeout_config)
+            , _default_timeout_config(config)
+            , _timeout_config(config)
     {}
 
     client_state(const client_state&) = delete;