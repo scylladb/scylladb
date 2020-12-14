6712429736f3f552f01d57b0513046c9c62feb30
Piotr Sarna <sarna@scylladb.com>
2020-12-14 14:44:31 +0100

    exceptions: add a default constructor for overloaded_exception

--- exceptions/exceptions.hh
+++ exceptions/exceptions.hh
@@ -228,6 +228,7 @@ struct read_failure_exception : public request_failure_exception {
 };
 
 struct overloaded_exception : public cassandra_exception {
+    overloaded_exception() noexcept : cassandra_exception(exception_code::OVERLOADED, "") {};
     explicit overloaded_exception(size_t c) noexcept :
         cassandra_exception(exception_code::OVERLOADED, prepare_message("Too many in flight hints: %lu", c)) {}
     explicit overloaded_exception(sstring msg) noexcept :
