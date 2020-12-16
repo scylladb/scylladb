2a6a0728575f79af3283df271c690ce8dbf70056
Gleb Natapov <gleb@scylladb.com>
2020-12-16 11:00:38 +0200

    mutation_writer: pass exceptions through feed_writer
    feed_writer() eats exception and transforms it into an end of stream
    instead. Downstream validators hate when this happens.
    
    Fixes #7482
    Message-Id: <20201216090038.GB3244976@scylladb.com>
    
    (cherry picked from commit 61520a33d64687477934f8f011efe4300dff3bca)

--- mutation_writer/feed_writers.hh
+++ mutation_writer/feed_writers.hh
@@ -38,8 +38,14 @@ future<> feed_writer(flat_mutation_reader&& rd, Writer&& wr) {
                 auto f2 = rd.is_buffer_empty() ? rd.fill_buffer(db::no_timeout) : make_ready_future<>();
                 return when_all_succeed(std::move(f1), std::move(f2));
             });
-        }).finally([&wr] {
-            return wr.consume_end_of_stream();
+        }).then_wrapped([&wr] (future<> f) {
+            if (f.failed()) {
+                auto ex = f.get_exception();
+                wr.abort(ex);
+                return make_exception_future<>(ex);
+            } else {
+                return wr.consume_end_of_stream();
+            }
         });
     });
 }
