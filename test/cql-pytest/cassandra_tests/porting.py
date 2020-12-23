ed31dd1742faac322c682834748bd4a6b4299358
Nadav Har'El <nyh@scylladb.com>
2020-12-23 20:13:25 +0200

    cql-pytest: port Cassandra's unit test validation/entities/counters_test
    In this patch, we port validation/entities/collection_test.java, containing
    7 tests for CQL counters. Happily, these tests did not uncover any bugs in
    Scylla and all pass on both Cassandra and Scylla.
    
    There is one small difference that I decided to ignore instead of reporting
    a bug. If you try a CREATE TABLE with both counter and non-counter columns,
    Scylla gives a ConfigurationException error, while Cassandra gives a more
    reasonable InvalidRequest. The ported test currently allows both.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <20201223181325.3148928-1-nyh@scylladb.com>

--- test/cql-pytest/cassandra_tests/porting.py
+++ test/cql-pytest/cassandra_tests/porting.py
@@ -23,6 +23,7 @@
 
 import pytest
 import re
+import collections
 from util import unique_name
 from contextlib import contextmanager
 from cassandra.protocol import SyntaxException, InvalidRequest
@@ -61,12 +62,19 @@ def create_function(cql, keyspace, arg):
     yield function_name
     cql.execute("DROP FUNCTION " + function_name)
 
+# utility function to substitute table name in command.
+# For easy conversion of Java code, support %s as used in Java. Also support
+# it *multiple* times (always interpolating the same table name). In Java,
+# a %1$s is needed for this.
+def subs_table(cmd, table):
+    return cmd.replace('%s', table)
+
 def execute(cql, table, cmd, *args):
     if args:
-        prepared = cql.prepare(cmd % table)
+        prepared = cql.prepare(subs_table(cmd, table))
         return cql.execute(prepared, args)
     else:
-        return cql.execute(cmd % table)
+        return cql.execute(subs_table(cmd, table))
 
 def assert_invalid_throw(cql, table, type, cmd, *args):
     with pytest.raises(type):
@@ -87,6 +95,10 @@ def assert_invalid_message(cql, table, message, cmd, *args):
     with pytest.raises(InvalidRequest, match=re.escape(message)):
         execute(cql, table, cmd, *args)
 
+def assert_invalid_throw_message(cql, table, message, typ, cmd, *args):
+    with pytest.raises(typ, match=re.escape(message)):
+        execute(cql, table, cmd, *args)
+
 # Cassandra's CQLTester.java has a much more elaborate implementation
 # of these functions, which carefully prints out the differences when
 # the assertion fails. We can also do this in the future, but pytest's
@@ -103,6 +115,24 @@ def assert_rows(result, *expected):
     assert len(allresults) == len(expected)
     for r,e in zip(allresults, expected):
         assert list(r) == e
+
+# To compare two lists of items (each is a dict) without regard for order,
+# The following function, multiset() converts the list into a multiset
+# (set with duplicates) where order doesn't matter, so the multisets can
+# be compared.
+def freeze(item):
+    if isinstance(item, dict):
+        return frozenset((key, freeze(value)) for key, value in item.items())
+    elif isinstance(item, list):
+        return tuple(freeze(value) for value in item)
+    return item
+def multiset(items):
+    return collections.Counter([freeze(item) for item in items])
+
+def assert_rows_ignoring_order(result, *expected):
+    allresults = list(result)
+    assert len(allresults) == len(expected)
+    assert multiset(allresults) == multiset(expected)
 
 # Note that the Python driver has a function _clean_column_name() which
 # "cleans" column names in result sets by dropping any non-alphanumeric
---
+++ test/cql-pytest/cassandra_tests/validation/entities/counters_test.py
@@ -0,0 +1,190 @@
+# This file was translated from the original Java test from the Apache
+# Cassandra source repository, as of commit 6ca34f81386dc8f6020cdf2ea4246bca2a0896c5
+#
+# The original Apache Cassandra license:
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cassandra_tests.porting import *
+
+from cassandra.protocol import SyntaxException, AlreadyExists, InvalidRequest, ConfigurationException
+from cassandra.query import UNSET_VALUE
+
+# Test for the validation bug of CASSANDRA-4706,
+# migrated from cql_tests.py:TestCQL.validate_counter_regular_test()
+def testRegularCounters(cql, test_keyspace):
+    # The Cassandra and Scylla error messages are different: Cassandra says
+    # "Cannot mix counter and non counter columns in the same table", Scylla
+    # "Cannot add a non counter column (things) in a counter column family".
+    # Let's just check the common phrase "non counter".
+    # Cassandra changed the exception it throws on dropping a nonexistent keyspace.
+    # FIXME: Cassandra throws InvalidRequest here, but Scylla uses
+    # ConfigurationException. We shouldn't have done that... But I consider
+    # this difference to be so trivial I didn't want to consider this a bug.
+    # Maybe we should reconsider, and not allow ConfigurationException...
+    assert_invalid_throw_message(cql, test_keyspace + "." + unique_name(),
+                                 "non counter",
+                                 (InvalidRequest, ConfigurationException),
+                                 "CREATE TABLE %s (id bigint PRIMARY KEY, count counter, things set<text>)")
+
+# Migrated from cql_tests.py:TestCQL.collection_counter_test()
+def testCountersOnCollections(cql, test_keyspace):
+    assert_invalid_throw(cql, test_keyspace + "." + unique_name(),
+                         InvalidRequest,
+                         "CREATE TABLE %s (k int PRIMARY KEY, l list<counter>)")
+
+    assert_invalid_throw(cql, test_keyspace + "." + unique_name(),
+                         InvalidRequest,
+                         "CREATE TABLE %s (k int PRIMARY KEY, s set<counter>)")
+
+    assert_invalid_throw(cql, test_keyspace + "." + unique_name(),
+                         InvalidRequest,
+                         "CREATE TABLE %s (k int PRIMARY KEY, m map<text, counter>)")
+
+def testCounterUpdatesWithUnset(cql, test_keyspace):
+    with create_table(cql, test_keyspace, "(k int PRIMARY KEY, c counter)") as table:
+        # set up
+        execute(cql, table, "UPDATE %s SET c = c + 1 WHERE k = 10")
+        assert_rows(execute(cql, table, "SELECT c FROM %s WHERE k = 10"), [1])
+        # increment
+        execute(cql, table, "UPDATE %s SET c = c + ? WHERE k = 10", 1)
+        assert_rows(execute(cql, table, "SELECT c FROM %s WHERE k = 10"), [2])
+        execute(cql, table, "UPDATE %s SET c = c + ? WHERE k = 10", UNSET_VALUE)
+        assert_rows(execute(cql, table, "SELECT c FROM %s WHERE k = 10"), [2]) # no change to the counter value
+        # decrement
+        execute(cql, table, "UPDATE %s SET c = c - ? WHERE k = 10", 1)
+        assert_rows(execute(cql, table, "SELECT c FROM %s WHERE k = 10"), [1])
+        execute(cql, table, "UPDATE %s SET c = c - ? WHERE k = 10", UNSET_VALUE)
+        assert_rows(execute(cql, table, "SELECT c FROM %s WHERE k = 10"), [1]) # no change to the counter value
+
+
+def testCounterFiltering(cql, test_keyspace):
+    with create_table(cql, test_keyspace, "(k int PRIMARY KEY, a counter)") as table:
+        for i in range(10):
+            execute(cql, table, "UPDATE %s SET a = a + ? WHERE k = ?", i, i)
+
+        execute(cql, table, "UPDATE %s SET a = a + ? WHERE k = ?", 6, 10)
+
+        # GT
+        assert_rows_ignoring_order(execute(cql, table, "SELECT * FROM %s WHERE a > ? ALLOW FILTERING", 5),
+                                [6, 6], [7, 7], [8, 8], [9, 9], [10, 6])
+
+        # GTE
+        assert_rows_ignoring_order(execute(cql, table, "SELECT * FROM %s WHERE a >= ? ALLOW FILTERING", 6),
+                                [6, 6], [7, 7], [8, 8], [9, 9], [10, 6])
+
+        # LT
+        assert_rows_ignoring_order(execute(cql, table, "SELECT * FROM %s WHERE a < ? ALLOW FILTERING", 3),
+                                [0,0], [1, 1], [2, 2])
+
+        # LTE
+        assert_rows_ignoring_order(execute(cql, table, "SELECT * FROM %s WHERE a <= ? ALLOW FILTERING", 3),
+                                [0,0], [1, 1], [2, 2], [3, 3])
+
+        # EQ
+        assert_rows_ignoring_order(execute(cql, table, "SELECT * FROM %s WHERE a = ? ALLOW FILTERING", 6),
+                                [6, 6], [10, 6])
+
+def testCounterFilteringWithNull(cql, test_keyspace):
+    with create_table(cql, test_keyspace, "(k int PRIMARY KEY, a counter, b counter)") as table:
+        execute(cql, table, "UPDATE %s SET a = a + ? WHERE k = ?", 1, 1)
+
+        assert_rows(execute(cql, table, "SELECT * FROM %s WHERE a > ? ALLOW FILTERING", 0), [1, 1, None])
+        # GT
+        assert_empty(execute(cql, table, "SELECT * FROM %s WHERE b > ? ALLOW FILTERING", 1))
+        # GTE
+        assert_empty(execute(cql, table, "SELECT * FROM %s WHERE b >= ? ALLOW FILTERING", 1))
+        # LT
+        assert_empty(execute(cql, table, "SELECT * FROM %s WHERE b < ? ALLOW FILTERING", 1))
+        # LTE
+        assert_empty(execute(cql, table, "SELECT * FROM %s WHERE b <= ? ALLOW FILTERING", 1))
+        # EQ
+        assert_empty(execute(cql, table, "SELECT * FROM %s WHERE b = ? ALLOW FILTERING", 1))
+        # with null
+        assert_invalid_message(cql, table, "Invalid null value for counter increment/decrement",
+                             "SELECT * FROM %s WHERE b = null ALLOW FILTERING")
+
+# Test for the validation bug of CASSANDRA-9395.
+def testProhibitReversedCounterAsPartOfPrimaryKey(cql, test_keyspace):
+    # The Cassandra message and Scylla message differ slightly -
+    # counter type is not supported for PRIMARY KEY column 'a'"
+    # counter type is not supported for PRIMARY KEY part a
+    # respectively.
+    assert_invalid_throw_message(cql, test_keyspace + "." + unique_name(),
+                                 "counter type is not supported for PRIMARY KEY",
+                                 InvalidRequest,
+                                 "CREATE TABLE %s (a counter, b int, PRIMARY KEY (b, a)) WITH CLUSTERING ORDER BY (a desc);")
+
+# Check that a counter batch works as intended
+def testCounterBatch(cql, test_keyspace):
+    with create_table(cql, test_keyspace, "(userid int, url text, total counter, PRIMARY KEY (userid, url))") as table:
+        # Ensure we handle updates to the same CQL row in the same partition properly
+        execute(cql, table, "BEGIN UNLOGGED BATCH " +
+                "UPDATE %s SET total = total + 1 WHERE userid = 1 AND url = 'http://foo.com'; " +
+                "UPDATE %s SET total = total + 1 WHERE userid = 1 AND url = 'http://foo.com'; " +
+                "UPDATE %s SET total = total + 1 WHERE userid = 1 AND url = 'http://foo.com'; " +
+                "APPLY BATCH; ")
+        assert_rows(execute(cql, table, "SELECT total FROM %s WHERE userid = 1 AND url = 'http://foo.com'"), [3])
+
+        # Ensure we handle different CQL rows in the same partition properly
+        execute(cql, table, "BEGIN UNLOGGED BATCH " +
+                "UPDATE %s SET total = total + 1 WHERE userid = 1 AND url = 'http://bar.com'; " +
+                "UPDATE %s SET total = total + 1 WHERE userid = 1 AND url = 'http://baz.com'; " +
+                "UPDATE %s SET total = total + 1 WHERE userid = 1 AND url = 'http://bad.com'; " +
+                "APPLY BATCH; ")
+        assert_rows(execute(cql, table, "SELECT url, total FROM %s WHERE userid = 1"),
+                ["http://bad.com", 1],
+                ["http://bar.com", 1],
+                ["http://baz.com", 1],
+                ["http://foo.com", 3]) # from previous batch
+
+    with create_table(cql, test_keyspace, "(userid int, url text, first counter, second counter, third counter, PRIMARY KEY (userid, url))") as table:
+        # Different counters in the same CQL Row
+        execute(cql, table, "BEGIN UNLOGGED BATCH " +
+                "UPDATE %s SET first = first + 1 WHERE userid = 1 AND url = 'http://foo.com'; " +
+                "UPDATE %s SET first = first + 1 WHERE userid = 1 AND url = 'http://foo.com'; " +
+                "UPDATE %s SET second = second + 1 WHERE userid = 1 AND url = 'http://foo.com'; " +
+                "APPLY BATCH; ")
+        assert_rows(execute(cql, table, "SELECT first, second, third FROM %s WHERE userid = 1 AND url = 'http://foo.com'"),
+                [2, 1, None])
+
+        # Different counters in different CQL Rows
+        execute(cql, table, "BEGIN UNLOGGED BATCH " +
+                "UPDATE %s SET first = first + 1 WHERE userid = 1 AND url = 'http://bad.com'; " +
+                "UPDATE %s SET first = first + 1, second = second + 1 WHERE userid = 1 AND url = 'http://bar.com'; " +
+                "UPDATE %s SET first = first - 1, second = second - 1 WHERE userid = 1 AND url = 'http://bar.com'; " +
+                "UPDATE %s SET second = second + 1 WHERE userid = 1 AND url = 'http://baz.com'; " +
+                "APPLY BATCH; ")
+        assert_rows(execute(cql, table, "SELECT url, first, second, third FROM %s WHERE userid = 1"),
+                ["http://bad.com", 1, None, None],
+                ["http://bar.com", 0, 0, None],
+                ["http://baz.com", None, 1, None],
+                ["http://foo.com", 2, 1, None]) # from previous batch
+
+
+        # Different counters in different partitions
+        execute(cql, table, "BEGIN UNLOGGED BATCH " +
+                "UPDATE %s SET first = first + 1 WHERE userid = 2 AND url = 'http://bad.com'; " +
+                "UPDATE %s SET first = first + 1, second = second + 1 WHERE userid = 3 AND url = 'http://bar.com'; " +
+                "UPDATE %s SET first = first - 1, second = second - 1 WHERE userid = 4 AND url = 'http://bar.com'; " +
+                "UPDATE %s SET second = second + 1 WHERE userid = 5 AND url = 'http://baz.com'; " +
+                "APPLY BATCH; ")
+        assert_rows_ignoring_order(execute(cql, table, "SELECT userid, url, first, second, third FROM %s WHERE userid IN (2, 3, 4, 5)"),
+                [2, "http://bad.com", 1, None, None],
+                [3, "http://bar.com", 1, 1, None],
+                [4, "http://bar.com", -1, -1, None],
+                [5, "http://baz.com", None, 1, None])
