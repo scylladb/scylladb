93a2c523383fd1903e2912c783efdd1d27dc81ee
Nadav Har'El <nyh@scylladb.com>
2020-12-30 15:23:50 +0200

    cql-pytest: add tests for inserting rows with missing key columns
    This patch adds two simple tests for what happens when a user tries to
    insert a row with one of the key column missing. The first tests confirms
    that if the column is completely missing, we correctly print an error
    (this was issue #3665, that was already marked fixed).
    
    However, the second test demonstrates that we still have a bug when
    the key column appears on the command, but with a null value.
    In this case, instead of failing the insert (as Cassandra does),
    we silently ignore it. This is the proper behavior for UNSET_VALUE,
    but not for null. So the second test is marked xfail, and I opened
    issue #7852 about it.
    
    Refs #3665
    Refs #7852
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <20201230132350.3463906-1-nyh@scylladb.com>

---
+++ test/cql-pytest/test_null.py
@@ -0,0 +1,65 @@
+# Copyright 2020 ScyllaDB
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+
+#############################################################################
+# Tests for finer points of the meaning of "null" in various places
+#############################################################################
+
+import pytest
+import re
+from cassandra.protocol import SyntaxException, AlreadyExists, InvalidRequest, ConfigurationException, ReadFailure
+from util import unique_name, random_string
+
+
+@pytest.fixture(scope="session")
+def table1(cql, test_keyspace):
+    table = test_keyspace + "." + unique_name()
+    cql.execute(f"CREATE TABLE {table} (p text, c text, v text, primary key (p, c))")
+    yield table
+    cql.execute("DROP TABLE " + table)
+
+# An item cannot be inserted without a key. Verify that before we get into
+# the really interesting test below - trying to pass "null" as the value of
+# the key.
+# See also issue #3665.
+def test_insert_missing_key(cql, table1):
+    s = random_string()
+    # A clustering key is missing. Cassandra uses the message "Some clustering
+    # keys are missing: c", and Scylla: "Missing mandatory PRIMARY KEY part c"
+    with pytest.raises(InvalidRequest, match=re.compile('missing', re.IGNORECASE)):
+        cql.execute(f"INSERT INTO {table1} (p) VALUES ('{s}')")
+    # Similarly, a missing partition key
+    with pytest.raises(InvalidRequest, match=re.compile('missing', re.IGNORECASE)):
+        cql.execute(f"INSERT INTO {table1} (c) VALUES ('{s}')")
+
+# A null key, like a missing one, is also not allowed.
+# This reproduces issue #7852.
+@pytest.mark.xfail(reason="issue #7852")
+def test_insert_null_key(cql, table1):
+    s = random_string()
+    with pytest.raises(InvalidRequest, match='null value'):
+        cql.execute(f"INSERT INTO {table1} (p,c) VALUES ('{s}', null)")
+    with pytest.raises(InvalidRequest, match='null value'):
+        cql.execute(f"INSERT INTO {table1} (p,c) VALUES (null, '{s}')")
+    # Try the same thing with prepared statement, where a "None" stands for
+    # a null. Note that this is completely different from UNSET_VALUE - only
+    # with the latter should the insertion be ignored.
+    stmt = cql.prepare(f"INSERT INTO {table1} (p,c) VALUES (?, ?)")
+    with pytest.raises(InvalidRequest, match='null value'):
+        cql.execute(stmt, [s, None])
+    with pytest.raises(InvalidRequest, match='null value'):
+        cql.execute(stmt, [None, s])
