7bb9230cfa4c72e56aa67dd0750189b11835d5f5
Avi Kivity <avi@scylladb.com>
2021-01-20 13:11:33 +0200

    Merge "mutation_writer: explicitly close writers" from Benny
    "
    _consumer_fut is expected to return an exception
    on the abort path.  Wait for it and drop any exception
    so it won't be abandoned as seen in #7904.
    
    A future<> close() method was added to return
    _consumer_fut.  It is called both after abort()
    in the error path, and after consume_end_of_stream,
    on the success path.
    
    With that, consume_end_of_stream was made void
    as it doesn't return a future<> anymore.
    
    Fixes #7904
    Test: unit(release)
    "
    
    * tag 'close-bucket-writer-v5' of github.com:bhalevy/scylla:
      mutation_writer: bucket_writer: add close
      mutation_writer/feed_writers: refactor bucket/shard writers
      mutation_writer: update bucket/shard writers consume_end_of_stream
    
    (cherry picked from commit f11a0700a829c6c3de3bd3450823688b8f88252b)

---
+++ mutation_writer/feed_writers.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2021 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "feed_writers.hh"
+
+namespace mutation_writer {
+
+bucket_writer::bucket_writer(schema_ptr schema, std::pair<flat_mutation_reader, queue_reader_handle> queue_reader, reader_consumer& consumer)
+    : _schema(schema)
+    , _handle(std::move(queue_reader.second))
+    , _consume_fut(consumer(std::move(queue_reader.first)))
+{ }
+
+bucket_writer::bucket_writer(schema_ptr schema, reader_permit permit, reader_consumer& consumer)
+    : bucket_writer(schema, make_queue_reader(schema, std::move(permit)), consumer)
+{ }
+
+future<> bucket_writer::consume(mutation_fragment mf) {
+    return _handle.push(std::move(mf));
+}
+
+void bucket_writer::consume_end_of_stream() {
+    _handle.push_end_of_stream();
+}
+
+void bucket_writer::abort(std::exception_ptr ep) noexcept {
+    _handle.abort(std::move(ep));
+}
+
+future<> bucket_writer::close() noexcept {
+    return std::move(_consume_fut);
+}
+
+} // mutation_writer
