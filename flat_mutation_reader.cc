e2e642b1b162a4c0dbad61c3560190e529b6768d
Benny Halevy <bhalevy@scylladb.com>
2021-01-26 23:57:26 +0200

    flat_mutation_reader: make_forwardable, make_nonforwardable: close underlying reader
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- flat_mutation_reader.cc
+++ flat_mutation_reader.cc
@@ -316,6 +316,9 @@ flat_mutation_reader make_forwardable(flat_mutation_reader m) {
             };
             return _underlying.fast_forward_to(pr, timeout);
         }
+        virtual future<> close() noexcept override {
+            return _underlying.close();
+        }
     };
     return make_flat_mutation_reader<reader>(std::move(m));
 }
@@ -378,6 +381,9 @@ flat_mutation_reader make_nonforwardable(flat_mutation_reader r, bool single_par
             _end_of_stream = false;
             clear_buffer();
             return _underlying.fast_forward_to(pr, timeout);
+        }
+        virtual future<> close() noexcept override {
+            return _underlying.close();
         }
     };
     return make_flat_mutation_reader<reader>(std::move(r), single_partition);
