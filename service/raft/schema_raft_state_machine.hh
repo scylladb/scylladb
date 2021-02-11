3b50cdf1ed33b3524ee1e6a1caca563ce134d29f
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2021-02-11 04:07:01 +0300

    raft: use a named constant for pre-defined schema raft group
    Introduce a static `schema_raft_state_machine::group_id` constant,
    which denotes the raft group id for the schema changes server.
    
    Also fix the comment on the state machine class declaration
    to emphasize that the instance will be managed by shard 0.
    
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- service/raft/schema_raft_state_machine.hh
+++ service/raft/schema_raft_state_machine.hh
@@ -22,9 +22,13 @@
 
 #include "raft/raft.hh"
 
-// Dummy implementation for schema changes raft state machine
+// Raft state machine implementation for managing schema changes.
+// NOTE: schema raft server is always instantiated on shard 0.
 class schema_raft_state_machine : public raft::state_machine {
 public:
+    // schema raft server always belongs to a special pre-defined raft group with id = 0
+    static constexpr uint64_t group_id = 0u;
+
     future<> apply(std::vector<raft::command_cref> command) override;
     future<raft::snapshot_id> take_snapshot() override;
     void drop_snapshot(raft::snapshot_id id) override;
