57f921de4f455ba2c1862c50fd438d69a20a5afb
Benny Halevy <bhalevy@scylladb.com>
2021-02-14 12:31:17 +0200

    database: streaming_reader_lifecycle_policy: destroy_reader: close inactive reader
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- database.cc
+++ database.cc
@@ -2265,7 +2265,8 @@ flat_mutation_reader make_multishard_streaming_reader(distributed<database>& db,
         virtual future<> destroy_reader(shard_id shard, future<stopped_reader> reader_fut) noexcept override {
             return reader_fut.then([this, zis = shared_from_this(), shard] (stopped_reader&& reader) mutable {
                 return smp::submit_to(shard, [ctx = std::move(_contexts[shard]), handle = std::move(reader.handle)] () mutable {
-                    ctx.semaphore->unregister_inactive_read(std::move(*handle));
+                    auto reader_opt = ctx.semaphore->unregister_inactive_read(std::move(*handle));
+                    return reader_opt ? reader_opt->close() : make_ready_future<>();
                 });
             }).handle_exception([shard] (std::exception_ptr e) {
                 dblog.warn("Failed to destroy shard reader of streaming multishard reader on shard {}: {}", shard, e);
