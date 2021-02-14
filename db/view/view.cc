dad6c94476389c6d99b78251f9ce95692840d732
Benny Halevy <bhalevy@scylladb.com>
2021-02-14 13:22:11 +0200

    view_builder: stop: close all build_step readers
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- db/view/view.cc
+++ db/view/view.cc
@@ -1411,6 +1411,10 @@ future<> view_builder::stop() {
             // ignored
         }).handle_exception_type([] (const semaphore_timed_out&) {
             // ignored
+        }).finally([this] {
+            return parallel_for_each(_base_to_build_step, [] (std::pair<const utils::UUID, build_step>& p) {
+                return p.second.reader.close();
+            });
         });
     });
 }
