767253fe24483297f6757010fcab88b445c8ea06
Pavel Emelyanov <xemul@scylladb.com>
2021-02-15 16:34:58 +0300

    test/perf_mutation: Add option to run with more than 1 columns
    The --column-count makes the test generate schema with
    the given numbers of columns and make mutation maker
    fill random column with the value on each iteration.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/perf/perf_mutation.cc
+++ test/perf/perf_mutation.cc
@@ -31,8 +31,12 @@ static atomic_cell make_atomic_cell(data_type dt, bytes value) {
 };
 
 int main(int argc, char* argv[]) {
-    return app_template().run_deprecated(argc, argv, [] {
-        size_t column_count = 1;
+    namespace bpo = boost::program_options;
+    app_template app;
+    app.add_options()
+        ("column-count", bpo::value<size_t>()->default_value(1), "column count");
+    return app.run_deprecated(argc, argv, [&] {
+        size_t column_count = app.configuration()["column-count"].as<size_t>();
         auto builder = schema_builder("ks", "cf")
             .with_column("p1", utf8_type, column_kind::partition_key)
             .with_column("c1", int32_type, column_kind::clustering_key);
