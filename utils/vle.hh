1782b0c6b94bc9c1bbb8dd7e465306f49cacda52
Pavel Emelyanov <xemul@scylladb.com>
2021-02-17 13:47:15 +0300

    uleb64: Add express encoding
    Standard encoding is compiled into a loop that puts values
    into memory byte-by-byte. This works slowly, but reliably.
    When allocating an object LSA uses ubel64 encoder with 2
    features that allow to optimize the encoder:
    
    1. the value is migrator.index() which is small enough
       to fit 2 bytes when encoded
    2. After the descriptor there usually comes an object
       which is of 8+ bytes in size
    
    Feature #1 makes it possible to encode the value with just
    a few instructions. In O3 level clang makes it like
    
      mov    %esi,%ecx
      and    $0xfc0,%ecx
      and    $0x3f,%esi
      lea    (%rsi,%rcx,4),%ecx
      add    $0x40,%ecx
    
    Next, the encoder needs to put the value into a gap whose
    size depends on the alignment of previous and current objects,
    so the classical algo loops through this size. Feature #2
    makes it possible to put the encoded value and the needed
    amount of zeros by using 2 64-bit movs. In this case the
    encoded value gets off the needed size and overwrites some
    memory after. That's OK, as this overwritten memory is where
    the allocated object _will_ be, so the contents there is not
    of any interest.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- utils/vle.hh
+++ utils/vle.hh
@@ -22,8 +22,16 @@
 #pragma once
 
 #include <seastar/core/bitops.hh>
+#include <seastar/core/byteorder.hh>
 
 namespace utils {
+
+/*
+ * The express encoder below is optimized to encode a value
+ * that may only have non-zeroes in its first 12 bits
+ */
+static constexpr size_t uleb64_express_bits = 12;
+static constexpr uint32_t uleb64_express_supreme = 1 << uleb64_express_bits;
 
 // Returns the number of bytes needed to encode the value
 // The value cannot be 0 (not checked)
@@ -66,6 +74,36 @@ static inline void uleb64_encode(char*& pos, uint32_t val, size_t encoded_size, 
     } while (encoded_size);
     poison(start, pos - start);
 }
+
+#if !defined(SEASTAR_ASAN_ENABLED)
+static inline void uleb64_express_encode_impl(char*& pos, uint64_t val, size_t size) noexcept {
+    static_assert(uleb64_express_bits == 12);
+
+    if (size > sizeof(uint64_t)) {
+        static uint64_t zero = 0;
+        std::copy_n(reinterpret_cast<char*>(&zero), sizeof(zero), pos + size - sizeof(uint64_t));
+    }
+    seastar::write_le(pos, uint64_t(((val & 0xfc0) << 2) | ((val & 0x3f) | 64)));
+    pos += size;
+    pos[-1] |= 0x80;
+}
+
+template <typename Poison, typename Unpoison>
+requires std::is_invocable<Poison, const char*, size_t>::value && std::is_invocable<Unpoison, const char*, size_t>::value
+static inline void uleb64_express_encode(char*& pos, uint32_t val, size_t encoded_size, size_t gap, Poison&& poison, Unpoison&& unpoison) noexcept {
+    if (encoded_size + gap > sizeof(uint64_t)) {
+        uleb64_express_encode_impl(pos, val, encoded_size);
+    } else {
+        uleb64_encode(pos, val, encoded_size, poison, unpoison);
+    }
+}
+#else
+template <typename Poison, typename Unpoison>
+requires std::is_invocable<Poison, const char*, size_t>::value && std::is_invocable<Unpoison, const char*, size_t>::value
+static inline void uleb64_express_encode(char*& pos, uint32_t val, size_t encoded_size, size_t gap, Poison&& poison, Unpoison&& unpoison) noexcept {
+    uleb64_encode(pos, val, encoded_size, poison, unpoison);
+}
+#endif
 
 template <typename Poison, typename Unpoison>
 requires std::is_invocable<Poison, const char*, size_t>::value && std::is_invocable<Unpoison, const char*, size_t>::value
