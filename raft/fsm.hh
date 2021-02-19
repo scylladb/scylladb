4dd66edae5db926134630246418824c4ce77df78
Konstantin Osipov <kostja@scylladb.com>
2021-02-19 22:49:42 +0300

    raft: add fsm_output::empty() helper to aid testing
    Used in testing to implement trivial transport.

--- raft/fsm.hh
+++ raft/fsm.hh
@@ -35,6 +35,13 @@ struct fsm_output {
     // Entries to apply.
     std::vector<log_entry_ptr> committed;
     std::optional<snapshot> snp;
+
+    // True if there is no new output
+    bool empty() const {
+        return !term_and_vote.has_value() &&
+            log_entries.size() == 0 && messages.size() == 0 &&
+            committed.size() == 0 && !snp.has_value();
+    }
 };
 
 struct fsm_config {
