af0a23e75cdeccd2d3567bb06d07fc6b0b11122c
Botond Dénes <bdenes@scylladb.com>
2021-03-01 13:27:46 +0200

    test/cql_test_env: do_with_cql_test_env(): add thread_attributes parameter
    To allow conveniently setting the scheduling group `func` is to be run
    in.

--- test/lib/cql_test_env.hh
+++ test/lib/cql_test_env.hh
@@ -135,4 +135,4 @@ public:
 };
 
 future<> do_with_cql_env(std::function<future<>(cql_test_env&)> func, cql_test_config = {});
-future<> do_with_cql_env_thread(std::function<void(cql_test_env&)> func, cql_test_config = {});
+future<> do_with_cql_env_thread(std::function<void(cql_test_env&)> func, cql_test_config = {}, thread_attributes thread_attr = {});
