9fbe4850c9c0c28033eed4a5d154a2a6508108ef
Avi Kivity <avi@scylladb.com>
2021-03-05 18:21:26 +0200

    utils: collection-concepts: prepare for std::strong_ordering
    collection-concepts includes a Comparable concept for a trichotomic
    comparator function, used in intrusive btree and double_decker. Prepare
    for std::strong_ordering by also allowing std::strong_ordering as a
    return type. Once we've cleaned the code base, we can tighten it to
    only allow std::strong_ordering.

--- utils/collection-concepts.hh
+++ utils/collection-concepts.hh
@@ -22,6 +22,7 @@
 #pragma once
 #include <type_traits>
 #include <seastar/util/concepts.hh>
+#include <compare>
 
 template <typename Func, typename T>
 concept Disposer = requires (Func f, T* val) { 
@@ -37,11 +38,16 @@ concept LessComparable = requires (const Key1& a, const Key2& b, Less less) {
 template <typename Key1, typename Key2, typename Less>
 concept LessNothrowComparable = LessComparable<Key1, Key2, Less> && std::is_nothrow_invocable_v<Less, Key1, Key2>;
 
+// Temporary scaffolding for supporting trichotomic compares
+// that return either int or std::strong_ordering
+template <typename T>
+concept int_or_strong_ordering = std::same_as<T, int> || std::same_as<T, std::strong_ordering>;
+
 template <typename T1, typename T2, typename Compare>
 concept Comparable = requires (const T1& a, const T2& b, Compare cmp) {
     // The Comparable is trichotomic comparator that should return 
     //   negative value when a < b
     //   zero when a == b
     //   positive value when a > b
-    { cmp(a, b) } -> std::same_as<int>;
+    { cmp(a, b) } -> int_or_strong_ordering;
 };
