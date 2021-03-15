xemul/scylla

query_processor: Keep migration manager onboard
The query processor sits upper than the migration manager,
in the services layering, it's started after and (will be)
stopped before the migration manager.

The migration manager is needed in schema altering statements
which are called with query processor argument. They will
later get the migration manager from the query processor.

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

cql3/query_processor.hh
@@ -58,6 +58,10 @@
 #include "service/query_state.hh"
 #include "transport/messages/result_message.hh"
 
+namespace service {
+class migration_manager;
+}
+
 namespace cql3 {
 
 namespace statements {
@@ -115,6 +119,7 @@ private:
     service::storage_proxy& _proxy;
     database& _db;
     service::migration_notifier& _mnotifier;
+    service::migration_manager& _mm;
     const cql_config& _cql_config;
 
     struct stats {
@@ -149,7 +154,7 @@ public:
 
     static std::unique_ptr<statements::raw::parsed_statement> parse_statement(const std::string_view& query);
 
-    query_processor(service::storage_proxy& proxy, database& db, service::migration_notifier& mn, memory_config mcfg, cql_config& cql_cfg);
+    query_processor(service::storage_proxy& proxy, database& db, service::migration_notifier& mn, service::migration_manager& mm, memory_config mcfg, cql_config& cql_cfg);
 
     ~query_processor();
 
@@ -164,6 +169,9 @@ public:
     service::storage_proxy& proxy() {
         return _proxy;
     }
+
+    const service::migration_manager& get_mm() const noexcept { return _mm; }
+    service::migration_manager& get_mm() noexcept { return _mm; }
 
     cql_stats& get_cql_stats() {
         return _cql_stats;

scylladb/scylladb

cql3/query_processor.hh
@@ -58,6 +58,10 @@
 #include "service/query_state.hh"
 #include "transport/messages/result_message.hh"
 
+namespace service {
+class migration_manager;
+}
+
 namespace cql3 {
 
 namespace statements {
@@ -115,6 +119,7 @@ private:
     service::storage_proxy& _proxy;
     database& _db;
     service::migration_notifier& _mnotifier;
+    service::migration_manager& _mm;
     const cql_config& _cql_config;
 
     struct stats {
@@ -149,7 +154,7 @@ public:
 
     static std::unique_ptr<statements::raw::parsed_statement> parse_statement(const std::string_view& query);
 
-    query_processor(service::storage_proxy& proxy, database& db, service::migration_notifier& mn, memory_config mcfg, cql_config& cql_cfg);
+    query_processor(service::storage_proxy& proxy, database& db, service::migration_notifier& mn, service::migration_manager& mm, memory_config mcfg, cql_config& cql_cfg);
 
     ~query_processor();
 
@@ -164,6 +169,9 @@ public:
     service::storage_proxy& proxy() {
         return _proxy;
     }
+
+    const service::migration_manager& get_migration_manager() const noexcept { return _mm; }
+    service::migration_manager& get_migration_manager() noexcept { return _mm; }
 
     cql_stats& get_cql_stats() {
         return _cql_stats;
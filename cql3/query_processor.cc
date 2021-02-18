1de235f4dafc9eb7f62c5e992a2b6f55d3ef9aef
Pavel Emelyanov <xemul@scylladb.com>
2021-02-18 11:27:30 +0300

    query_processor: Keep migration manager onboard
    The query processor sits upper than the migration manager,
    in the services layering, it's started after and (will be)
    stopped before the migration manager.
    
    The migration manager is needed in schema altering statements
    which are called with query processor argument. They will
    later get the migration manager from the query processor.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- cql3/query_processor.cc
+++ cql3/query_processor.cc
@@ -84,11 +84,12 @@ public:
     }
 };
 
-query_processor::query_processor(service::storage_proxy& proxy, database& db, service::migration_notifier& mn, query_processor::memory_config mcfg, cql_config& cql_cfg)
+query_processor::query_processor(service::storage_proxy& proxy, database& db, service::migration_notifier& mn, service::migration_manager& mm, query_processor::memory_config mcfg, cql_config& cql_cfg)
         : _migration_subscriber{std::make_unique<migration_subscriber>(this)}
         , _proxy(proxy)
         , _db(db)
         , _mnotifier(mn)
+        , _mm(mm)
         , _cql_config(cql_cfg)
         , _internal_state(new internal_state())
         , _prepared_cache(prep_cache_log, mcfg.prepared_statment_cache_size)
