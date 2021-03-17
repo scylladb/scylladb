efe734c575b74235ccfa7fe04e43fdbbd215f7ab
Piotr Sarna <sarna@scylladb.com>
2021-03-17 12:09:20 +0100

    alternator: add test for slow query logging
    The test checks whether slow queries are properly logged
    in the system_traces.node_slow_log system table.
    The test is deterministic because it uses the threshold of 0ms
    to qualify a query as slow, which effectively makes all queries
    "slow enough".

--- test/alternator/test_tracing.py
+++ test/alternator/test_tracing.py
@@ -31,6 +31,7 @@ import pytest
 import requests
 import re
 import time
+import json
 from botocore.exceptions import ClientError
 from util import random_string, full_scan, full_query, create_test_table
 
@@ -69,6 +70,46 @@ def with_tracing(dynamodb):
     response = requests.get(url+'/storage_service/trace_probability')
     if response.status_code != 200 or response.content.decode('utf-8') != '0':
         pytest.skip('Failed to verify tracing disabled')
+
+# Similarly to the fixture above, slow query logging is enabled only for the run of the
+# test function. Slow logging is set up with threshold equal to 0 microseconds,
+# which effectively means that each query will be qualified as slow and logged.
+@pytest.fixture(scope="function")
+def with_slow_query_logging(dynamodb):
+    print("with_slow_query_logging enabling slow query logging")
+    if dynamodb.meta.client._endpoint.host.endswith('.amazonaws.com'):
+        pytest.skip('Scylla-only feature not supported by AWS')
+    url = dynamodb.meta.client._endpoint.host
+    # The REST API is on port 10000, and always http, not https.
+    url = re.sub(r':[0-9]+(/|$)', ':10000', url)
+    url = re.sub(r'^https:', 'http:', url)
+    slow_query_info = requests.get(url+'/storage_service/slow_query')
+    if slow_query_info.status_code != 200:
+        pytest.skip('Failed to fetch slow query logging info')
+    slow_query_json = json.loads(slow_query_info.text)
+    print(slow_query_json)
+    response = requests.post(url+'/storage_service/slow_query?enable=true')
+    if response.status_code != 200:
+        pytest.skip('Failed to enable slow query logging')
+    response = requests.post(url+'/storage_service/slow_query?threshold=0')
+    if response.status_code != 200:
+        pytest.skip('Failed to enable slow query logging threshold')
+    # verify that logging is really enabled
+    response = requests.get(url+'/storage_service/slow_query')
+    if response.status_code != 200:
+        pytest.skip('Failed to verify slow query logging')
+    response_json = json.loads(response.text)
+    if response_json['enable'] != True or response_json['threshold'] != 0:
+        pytest.skip('Failed to verify slow query logging values')
+    print(response_json)
+    yield
+    print("with_slow_query_logging restoring slow query logging")
+    response = requests.post(url+'/storage_service/slow_query?enable='+str(slow_query_json['enable']))
+    if response.status_code != 200:
+        pytest.fail('Failed to restore slow query logging')
+    response = requests.post(url+'/storage_service/slow_query?threshold='+str(slow_query_json['threshold']))
+    if response.status_code != 200:
+        pytest.fail('Failed to restore slow query logging threshold')
 
 # Unfortunately, we currently have no way of directly finding the tracing
 # session of a specific Alternator request. We could have returned the
@@ -212,3 +253,24 @@ def test_tracing_all(with_tracing, test_table_s_isolation_always, dynamodb):
 # We could use traces to show that the right things actually happen during a
 # request. In issue #6747 we suspected that maybe GetItem doesn't read just
 # the requested item, and a tracing test can prove or disprove that hunch.
+
+def test_slow_query_log(with_slow_query_logging, test_table_s, dynamodb):
+    table = test_table_s
+    p = random_string(20)
+    print(f"Traced key: {p}")
+    table.put_item(Item={'p': p})
+    table.delete_item(Key={'p': p})
+    # Verify that the operations got logged. Each operation taking more than 0 microseconds is logged,
+    # which effectively logs all requests as slow.
+    slow_query_table = dynamodb.Table('.scylla.alternator.system_traces.node_slow_log')
+    delay = 0.2
+    while delay < 30:
+        results = full_scan(slow_query_table, ConsistentRead=False)
+        put_item_found = any("PutItem" in result['parameters'] and p in result['parameters'] for result in results)
+        delete_item_found = any("DeleteItem" in result['parameters'] and p in result['parameters'] for result in results)
+        if put_item_found and delete_item_found:
+            return
+        else:
+            time.sleep(delay)
+            delay += 1
+    pytest.fail("Slow query entries not found")
