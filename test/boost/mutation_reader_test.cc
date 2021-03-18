ad02f313dd75c1d723e71c2e06932136c1361b44
Botond Dénes <bdenes@scylladb.com>
2021-03-18 16:18:20 +0200

    test: mutation_reader_test: add test for permit cleanup
    Check that a permit correctly restores the units on the semaphore in
    each state it can be destroyed in.

--- test/boost/mutation_reader_test.cc
+++ test/boost/mutation_reader_test.cc
@@ -928,6 +928,48 @@ SEASTAR_TEST_CASE(test_reader_concurrency_semaphore_clear_inactive_reads) {
     return make_ready_future<>();
 }
 
+SEASTAR_THREAD_TEST_CASE(test_reader_concurrency_semaphore_destroyed_permit_releases_units) {
+    simple_schema s;
+    const auto initial_resources = reader_concurrency_semaphore::resources{10, 1024 * 1024};
+    reader_concurrency_semaphore semaphore(initial_resources.count, initial_resources.memory, get_name());
+
+    // Not admitted, active
+    {
+        auto permit = semaphore.make_permit(s.schema().get(), get_name());
+        auto units2 = permit.consume_memory(1024);
+    }
+    BOOST_REQUIRE(semaphore.available_resources() == initial_resources);
+
+    // Not admitted, inactive
+    {
+        auto permit = semaphore.make_permit(s.schema().get(), get_name());
+        auto units2 = permit.consume_memory(1024);
+
+        auto handle = semaphore.register_inactive_read(make_empty_flat_reader(s.schema(), permit));
+        BOOST_REQUIRE(semaphore.try_evict_one_inactive_read());
+    }
+    BOOST_REQUIRE(semaphore.available_resources() == initial_resources);
+
+    // Admitted, active
+    {
+        auto permit = semaphore.make_permit(s.schema().get(), get_name());
+        auto units1 = permit.wait_admission(1024, db::no_timeout).get0();
+        auto units2 = permit.consume_memory(1024);
+    }
+    BOOST_REQUIRE(semaphore.available_resources() == initial_resources);
+
+    // Admitted, inactive
+    {
+        auto permit = semaphore.make_permit(s.schema().get(), get_name());
+        auto units1 = permit.wait_admission(1024, db::no_timeout).get0();
+        auto units2 = permit.consume_memory(1024);
+
+        auto handle = semaphore.register_inactive_read(make_empty_flat_reader(s.schema(), permit));
+        BOOST_REQUIRE(semaphore.try_evict_one_inactive_read());
+    }
+    BOOST_REQUIRE(semaphore.available_resources() == initial_resources);
+}
+
 static
 sstables::shared_sstable create_sstable(sstables::test_env& env, schema_ptr s, std::vector<mutation> mutations) {
     static thread_local auto tmp = tmpdir();
