61a0314952b057d89545ebf3d6b98eae470763e6
Juliusz Stasiewicz <juliusz.stasiewicz@scylladb.com>
2021-03-24 12:34:24 +0100

    db: Add virtual tables interface
    This change introduces the basic interface we expect each virtual
    table to implement. More specific implementations will then expand
    upon it if needed.

---
+++ db/virtual_table.hh
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "mutation_reader.hh"
+#include "schema.hh"
+#include "database_fwd.hh"
+
+namespace db {
+
+class virtual_table {
+protected:
+    schema_ptr _s;
+    database* _db = nullptr; // Always valid when attached to a database.
+
+protected: // opt-ins
+    // If set to true, the implementation ensures that produced data
+    // only contains partitions owned by the current shard.
+    // Implementations can do this by checking the result of this_shard_owns().
+    // If set to false, data will be filtered out automatically.
+    bool _shard_aware = false;
+
+protected:
+    void set_cell(row&, const bytes& column_name, data_value);
+    bool contains_key(const dht::partition_range&, const dht::decorated_key&) const;
+    bool this_shard_owns(const dht::decorated_key&) const;
+
+public:
+    explicit virtual_table(schema_ptr s) : _s(std::move(s)) {}
+
+    const schema_ptr& schema() const { return _s; }
+
+    // Keep this object alive as long as the returned mutation_source is alive.
+    virtual mutation_source as_mutation_source() = 0;
+
+    void set_database(database& db) { _db = &db; }
+};
+
+}
