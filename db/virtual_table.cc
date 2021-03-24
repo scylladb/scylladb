7f18f06aa0bed39c800872a1e257ee1668bb7f68
Juliusz Stasiewicz <juliusz.stasiewicz@scylladb.com>
2021-03-24 12:34:24 +0100

    db: Add virtual tables interface
    This change introduces the basic interface we expect each virtual
    table to implement. More specific implementations will then expand
    upon it if needed.

---
+++ db/virtual_table.cc
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Modified by ScyllaDB
+ * Copyright (C) 2021 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "db/virtual_table.hh"
+
+namespace db {
+
+void virtual_table::set_cell(row& cr, const bytes& column_name, data_value value) {
+    auto ts = api::new_timestamp();
+    auto cdef = schema()->get_column_definition(column_name);
+    if (!cdef) {
+        throw_with_backtrace<std::runtime_error>(format("column not found: {}", column_name));
+    }
+    if (!value.is_null()) {
+        cr.apply(*cdef, atomic_cell::make_live(*cdef->type, ts, value.serialize_nonnull()));
+    }
+}
+
+bool virtual_table::this_shard_owns(const dht::decorated_key& dk) const {
+    return dht::shard_of(*_s, dk.token()) == this_shard_id();
+}
+
+bool virtual_table::contains_key(const dht::partition_range& pr, const dht::decorated_key& dk) const {
+    return pr.contains(dk, dht::ring_position_comparator(*_s));
+}
+
+}
