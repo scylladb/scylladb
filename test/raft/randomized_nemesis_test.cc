905126acc36baf301aab5d8150de61e927e67be7
Kamil Braun <kbraun@scylladb.com>
2021-04-02 22:35:48 +0200

    raft: randomized_nemesis_test: impure_state_machine
    To replicate a state machine, our Raft implementation requires it to
    be represented with the `raft::state_machine` interface.
    
    `impure_state_machine` is an implementation of `raft::state_machine`
    that wraps a `PureStateMachine`. It keeps a variable of type `state_t`
    representing the current state. In `apply` it deserializes the given
    command into `input_t`, uses the transition (`delta`) function to
    produce the next state and output, replaces its current state with the
    obtained state and returns the output (more on that below); it does so
    sequentially for every given command. We can think of `PureStateMachine`
    as the actual state machine - the business logic, and
    `impure_state_machine` as the ``boilerplate'' that allows the pure machine
    to be replicated by Raft and communicate with the external world.
    
    The interface also requires maintainance of snapshots. We introduce the
    `snapshots_t` type representing a set of snapshots known by a state
    machine. `impure_state_machine` keeps a reference to `snapshots_t`
    because it will share it with an implementation of `raft::persistence`
    coming with a later commit.
    
    Returning outputs is a bit tricky because apply is ``write-only'' - it
    returns `future<>`. We use the following technique:
    
    1. Before sending a command to a Raft leader through `server::add_entry`,
       one must first directly contact the instance of `impure_state_machine`
       replicated by the leader, asking it to allocate an ``output channel''.
    2. On such a request, `impure_state_machine` creates a channel
       (represented by a promise-future pair) and a unique ID; it stores the
       input side of the channel (the promise) with this ID internally and returns
       the ID and the output side of the channel (the future) to the requester.
    3. After obtaining the ID, one serializes the ID together with the input
       and sends it as a command to Raft. Thus commands are (ID, machine input)
       pairs.
    4. When `impure_state_machine` applies a command, it looks for a promise
       with the given ID. If it finds one, it sends the output through this
       channel.
    5. The command sender waits for the output on the obtained future.
    
    The allocation and deallocation of channels is done using the
    `impure_state_machine::with_output_channel` function. The `call`
    function is an implementation of the above technique.
    
    Note that only the leader will attempt to send the output - other
    replicas won't find the ID in their internal data structure. The set of
    IDs and channels is not a part of the replicated state.
    
    A failure may cause the output to never arrive (or even the command to
    never be applied) so `call` waits for a limited time. It may also
    mistakenly `call` a server which is not currently the leader, but it
    is prepared to handle this error.

--- test/raft/randomized_nemesis_test.cc
+++ test/raft/randomized_nemesis_test.cc
@@ -21,8 +21,16 @@
 
 #include <seastar/testing/test_case.hh>
 #include <seastar/core/timed_out_error.hh>
+#include <seastar/core/coroutine.hh>
+#include <seastar/core/gate.hh>
+#include <seastar/util/defer.hh>
 
+#include "raft/server.hh"
 #include "raft/logical_clock.hh"
+#include "serializer.hh"
+#include "serializer_impl.hh"
+#include "idl/uuid.dist.hh"
+#include "idl/uuid.dist.impl.hh"
 
 using namespace seastar;
 
@@ -189,6 +197,172 @@ public:
         return f;
     }
 };
+
+// Used to uniquely identify commands passed into `apply` in order to return
+// the outputs of these commands. See `impure_state_machine` and `call`.
+using cmd_id_t = utils::UUID;
+
+// A set of in-memory snapshots maintained by a single Raft server.
+// The different parts of the server (the state machine, persistence,
+// rpc) will share a single `snapshots_t`.
+template <typename State>
+using snapshots_t = std::unordered_map<raft::snapshot_id, State>;
+
+// To replicate a state machine, our Raft implementation requires it to
+// be represented with the `raft::state_machine` interface.
+//
+// `impure_state_machine` is an implementation of `raft::state_machine`
+// that wraps a `PureStateMachine`. It keeps a variable of type `state_t`
+// representing the current state. In `apply` it deserializes the given
+// command into `input_t`, uses the transition (`delta`) function to
+// produce the next state and output, replaces its current state with the
+// obtained state and returns the output (more on that below); it does so
+// sequentially for every given command. We can think of `PureStateMachine`
+// as the actual state machine - the business logic, and `impure_state_machine`
+// as the ``boilerplate'' that allows the pure machine to be replicated
+// by Raft and communicate with the external world.
+//
+// The interface also requires maintainance of snapshots. We use the
+// `snapshots_t` introduced above; `impure_state_machine` keeps a reference to `snapshots_t`
+// because it will share it with an implementation of `raft::persistence`.
+template <PureStateMachine M>
+class impure_state_machine : public raft::state_machine {
+    typename M::state_t _val;
+    snapshots_t<typename M::state_t>& _snapshots;
+
+    // Used to ensure that when `abort()` returns there are
+    // no more in-progress methods running on this object.
+    seastar::gate _gate;
+
+    // To obtain output from an applied command, the client (see `call`)
+    // first allocates a channel in this data structure by calling `with_output_channel`
+    // and makes the returned command ID a part of the command passed to Raft.
+    // When (if) we eventually apply the command, we use the ID to find the output channel
+    // here and push the output to the client waiting on the other end.
+    // The channel is allocated only on the local server where `with_output_channel`
+    // was called; other replicas of the state machine will therefore not find the ID
+    // in their instances of `_output_channels` so they just drop the output.
+    std::unordered_map<cmd_id_t, promise<typename M::output_t>> _output_channels;
+
+public:
+    impure_state_machine(snapshots_t<typename M::state_t>& snapshots)
+        : _val(M::init), _snapshots(snapshots) {}
+
+    future<> apply(std::vector<raft::command_cref> cmds) override {
+        co_await with_gate(_gate, [this, cmds = std::move(cmds)] () mutable -> future<> {
+            for (auto& cref : cmds) {
+                _gate.check();
+
+                auto is = ser::as_input_stream(cref);
+                auto cmd_id = ser::deserialize(is, boost::type<cmd_id_t>{});
+                auto input = ser::deserialize(is, boost::type<typename M::input_t>{});
+                auto [new_state, output] = M::delta(std::move(_val), std::move(input));
+                _val = std::move(new_state);
+
+                auto it = _output_channels.find(cmd_id);
+                if (it != _output_channels.end()) {
+                    // We are on the leader server where the client submitted the command
+                    // and waits for the output. Send it to them.
+                    it->second.set_value(std::move(output));
+                } else {
+                    // This is not the leader on which the command was submitted,
+                    // or it is but the client already gave up on us and deallocated the channel.
+                    // In any case we simply drop the output.
+                }
+
+                co_await make_ready_future<>(); // maybe yield
+            }
+        });
+    }
+
+    future<raft::snapshot_id> take_snapshot() override {
+        auto id = raft::snapshot_id{utils::make_random_uuid()};
+        _snapshots[id] = _val;
+        co_return id;
+    }
+
+    void drop_snapshot(raft::snapshot_id id) override {
+        _snapshots.erase(id);
+    }
+
+    future<> load_snapshot(raft::snapshot_id id) override {
+        auto it = _snapshots.find(id);
+        assert(it != _snapshots.end()); // dunno if the snapshot can actually be missing
+        _val = it->second;
+        co_return;
+    }
+
+    future<> abort() override {
+        return _gate.close();
+    }
+
+    // Before sending a command to Raft, the client must obtain a command ID
+    // and an output channel using this function.
+    template <typename F>
+    future<typename M::output_t> with_output_channel(F f) {
+        return with_gate(_gate, [this, f = std::move(f)] () mutable -> future<typename M::output_t> {
+            promise<typename M::output_t> p;
+            auto fut = p.get_future();
+            auto cmd_id = utils::make_random_uuid();
+            assert(_output_channels.emplace(cmd_id, std::move(p)).second);
+
+            auto guard = defer([this, cmd_id] { _output_channels.erase(cmd_id); });
+            return f(cmd_id, std::move(fut)).finally([guard = std::move(guard)] {});
+        });
+    }
+};
+
+// TODO: serializable concept?
+template <typename Input>
+raft::command make_command(const cmd_id_t& cmd_id, const Input& input) {
+    raft::command cmd;
+    ser::serialize(cmd, cmd_id);
+    ser::serialize(cmd, input);
+    return cmd;
+}
+
+// TODO: handle other errors?
+template <PureStateMachine M>
+using call_result_t = std::variant<typename M::output_t, timed_out_error, raft::not_a_leader>;
+
+// Sends a given `input` as a command to `server`, waits until the command gets replicated
+// and applied on that server and returns the produced output.
+//
+// The wait time is limited using `timeout` which is a logical time point referring to the
+// logical clock used by `timer`. Standard way to use is to pass `timer.now() + X_t`
+// as the time point, where `X` is the maximum number of ticks that we wait for.
+//
+// `sm` must be a reference to the state machine owned by `server`.
+//
+// The `server` may currently be a follower, in which case it will return a `not_a_leader` error.
+template <PureStateMachine M>
+future<call_result_t<M>> call(
+        typename M::input_t input,
+        raft::logical_clock::time_point timeout,
+        logical_timer& timer,
+        raft::server& server,
+        impure_state_machine<M>& sm) {
+    return sm.with_output_channel([&, input = std::move(input), timeout] (cmd_id_t cmd_id, future<typename M::output_t> f) {
+        return timer.with_timeout(timeout, [&] (typename M::input_t input, future<typename M::output_t> f) {
+            return server.add_entry(
+                    make_command(std::move(cmd_id), std::move(input)),
+                    raft::wait_type::applied
+            ).then([f = std::move(f)] () mutable {
+                return std::move(f);
+            });
+        }(std::move(input), std::move(f)));
+    }).then([] (typename M::output_t output) {
+        return make_ready_future<call_result_t<M>>(std::move(output));
+    }).handle_exception([] (std::exception_ptr eptr) {
+        try {
+            std::rethrow_exception(eptr);
+        } catch (raft::not_a_leader e) {
+            return make_ready_future<call_result_t<M>>(e);
+        } catch (timed_out_error e) {
+            return make_ready_future<call_result_t<M>>(e);
+        }
+    });
+}
 
 SEASTAR_TEST_CASE(dummy_test) {
     return seastar::make_ready_future<>();
