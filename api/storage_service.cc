34643ac997a7243a0f40f940eb45d9ad0b7f9bbf
Botond Dénes <bdenes@scylladb.com>
2021-04-29 17:55:13 +0300

    api: /storage_service/keyspace_scrub: add scrub mode param
    Add direct support to the newly added scrub mode enum. Instead of the
    legacy `skip_corrupted` flag, one can now select the desired mode from
    the mode enum. `skip_corrupted` is still supported for backwards
    compatibility but it is ignored when the mode enum is set.

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -1244,9 +1244,21 @@ void set_snapshot(http_context& ctx, routes& r, sharded<db::snapshot_ctl>& snap_
     ss::scrub.set(r, wrap_ks_cf(ctx, [&snap_ctl] (http_context& ctx, std::unique_ptr<request> req, sstring keyspace, std::vector<sstring> column_families) {
         auto scrub_mode = sstables::compaction_options::scrub::mode::abort;
 
-        const auto skip_corrupted = req_param<bool>(*req, "skip_corrupted", false);
-        if (skip_corrupted) {
-            scrub_mode = sstables::compaction_options::scrub::mode::skip;
+        const sstring scrub_mode_str = req_param<sstring>(*req, "scrub_mode", "");
+        if (scrub_mode_str == "") {
+            const auto skip_corrupted = req_param<bool>(*req, "skip_corrupted", false);
+
+            if (skip_corrupted) {
+                scrub_mode = sstables::compaction_options::scrub::mode::skip;
+            }
+        } else {
+            if (scrub_mode_str == "ABORT") {
+                scrub_mode = sstables::compaction_options::scrub::mode::abort;
+            } else if (scrub_mode_str == "SKIP") {
+                scrub_mode = sstables::compaction_options::scrub::mode::skip;
+            } else {
+                throw std::invalid_argument(fmt::format("Unknown argument for 'scrub_mode' parameter: {}", scrub_mode_str));
+            }
         }
 
         auto f = make_ready_future<>();
