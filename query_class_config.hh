e84c31fab8a0f6fb2778d0599d0c3856478dc24e
Botond Dénes <bdenes@scylladb.com>
2021-05-05 11:20:16 +0300

    query_class_config: add operator== for max_result_size

--- query_class_config.hh
+++ query_class_config.hh
@@ -36,6 +36,10 @@ struct max_result_size {
     explicit max_result_size(uint64_t soft_limit, uint64_t hard_limit) : soft_limit(soft_limit), hard_limit(hard_limit) { }
 };
 
+inline bool operator==(const max_result_size& a, const max_result_size& b) {
+    return a.soft_limit == b.soft_limit && a.hard_limit == b.hard_limit;
+}
+
 struct query_class_config {
     reader_concurrency_semaphore& semaphore;
     max_result_size max_memory_for_unlimited_query;
