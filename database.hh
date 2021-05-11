77cb6596c42e07086aac32f2e3d6e132d08878ff
Benny Halevy <bhalevy@scylladb.com>
2021-05-11 19:14:07 +0300

    utils: phased_barrier: advance_and_await: make noexcept
    As a function returning a future, simplify
    its interface by handling any exceptions and
    returning an exceptional future instead of
    propagating the exception.
    
    In this specific case, throwing from advance_and_await()
    will propagate through table::await_pending_* calls
    short-circuiting a .finally clause in table::stop().
    
    Also, mark as noexcept methods of class table calling
    advance_and_await and table::await_pending_ops that depends on them.
    
    Fixes #8636
    
    A followup patch will convert advance_and_await to a coroutine.
    This is done separately to facilitate backporting of this patch.
    
    Test: unit(dev)
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    Message-Id: <20210511161407.218402-1-bhalevy@scylladb.com>
    (cherry picked from commit c0dafa75d9f334c1ac932aa7de5618ac5c80f188)

--- database.hh
+++ database.hh
@@ -897,7 +897,7 @@ public:
         return _pending_writes_phaser.start();
     }
 
-    future<> await_pending_writes() {
+    future<> await_pending_writes() noexcept {
         return _pending_writes_phaser.advance_and_await();
     }
 
@@ -909,7 +909,7 @@ public:
         return _pending_reads_phaser.start();
     }
 
-    future<> await_pending_reads() {
+    future<> await_pending_reads() noexcept {
         return _pending_reads_phaser.advance_and_await();
     }
 
@@ -921,7 +921,7 @@ public:
         return _pending_streams_phaser.start();
     }
 
-    future<> await_pending_streams() {
+    future<> await_pending_streams() noexcept {
         return _pending_streams_phaser.advance_and_await();
     }
 
@@ -929,11 +929,11 @@ public:
         return _pending_streams_phaser.operations_in_progress();
     }
 
-    future<> await_pending_flushes() {
+    future<> await_pending_flushes() noexcept {
         return _pending_flushes_phaser.advance_and_await();
     }
 
-    future<> await_pending_ops() {
+    future<> await_pending_ops() noexcept {
         return when_all(await_pending_reads(), await_pending_writes(), await_pending_streams(), await_pending_flushes()).discard_result();
     }
 
