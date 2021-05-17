03ad111beb1e3a0bdc8c8311ead2bf058312fdc2
Kamil Braun <kbraun@scylladb.com>
2021-05-17 15:35:05 +0200

    tree-wide: comments on deprecated functions to access global variables
    Closes #8665

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -101,9 +101,13 @@ class storage_service;
 class migration_manager;
 
 extern distributed<storage_service> _the_storage_service;
+// DEPRECATED, DON'T USE!
+// Pass references to services through constructor/function parameters. Don't use globals.
 inline distributed<storage_service>& get_storage_service() {
     return _the_storage_service;
 }
+// DEPRECATED, DON'T USE!
+// Pass references to services through constructor/function parameters. Don't use globals.
 inline storage_service& get_local_storage_service() {
     return _the_storage_service.local();
 }
