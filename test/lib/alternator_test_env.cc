b6d6247a7487efa5a398e95936abb75e98d11051
Piotr Sarna <sarna@scylladb.com>
2021-05-17 10:24:39 +0200

    test: add minimal alternator_test_env
    A minimal implementation of alternator test env, a younger cousin
    of cql_test_env, is implemented. Note that using this environment
    for unit tests is strongly discouraged in favor of the official
    test/alternator pytest suite. Still, alternator_test_env has its uses
    for microbenchmarks.

---
+++ test/lib/alternator_test_env.cc
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "test/lib/alternator_test_env.hh"
+#include "alternator/rmw_operation.hh"
+#include "database.hh"
+#include <seastar/core/coroutine.hh>
+
+future<> alternator_test_env::start(std::string_view isolation_level) {
+    smp_service_group_config c;
+    c.max_nonlocal_requests = 5000;
+    smp_service_group ssg = co_await create_smp_service_group(c);
+
+    co_await _sdks.start(std::ref(_qp), std::ref(_mm), std::ref(_proxy));
+    co_await _cdc_metadata.start();
+    co_await _executor.start(std::ref(_proxy),
+            std::ref(_mm),
+            // parameters below are only touched by alternator streams;
+            //  not really interesting for this use case
+            std::ref(_sdks),
+            std::ref(_storage_service),
+            std::ref(_cdc_metadata),
+            // end-of-streams-parameters
+            ssg);
+    try {
+        alternator::rmw_operation::set_default_write_isolation(isolation_level);
+    } catch (const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+        throw;
+    }
+}
+
+future<> alternator_test_env::stop() {
+    co_await _executor.stop();
+    co_await _cdc_metadata.stop();
+    co_await _sdks.stop();
+}
+
+future<> alternator_test_env::flush_memtables() {
+    return _proxy.local().get_db().invoke_on_all(&database::flush_all_memtables);
+}
---
+++ test/lib/alternator_test_env.hh
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2021 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <seastar/core/sharded.hh>
+#include <seastar/core/future.hh>
+
+#include "alternator/executor.hh"
+#include "utils/rjson.hh"
+#include "cql3/query_processor.hh"
+#include "service/storage_service.hh"
+#include "service/storage_proxy.hh"
+#include "cdc/log.hh"
+#include "cdc/metadata.hh"
+#include "cdc/generation.hh"
+#include "db/system_distributed_keyspace.hh"
+
+// Test environment for alternator frontend.
+// The interface is minimal and does not cover alternator streams,
+// because this environment has limited use as well - microbenchmarks.
+// Regular unit tests should be performed by the test/alternator pytest
+// suite, which has several advantages:
+//  - it's not coded in raw C++, which is not great for unit tests
+//  - it actually boots Scylla, which exercises more paths
+//  - it's designed to run the tests against DynamoDB as well
+// Because of that, the helper class above should only be used
+// for benchmarks which need more low-level access to Seastar metrics.
+class alternator_test_env {
+    sharded<service::storage_proxy>& _proxy;
+    sharded<service::migration_manager>& _mm;
+    sharded<service::storage_service>& _storage_service;
+    sharded<cql3::query_processor>& _qp;
+
+    // Dummy service, only needed for alternator streams
+    sharded<db::system_distributed_keyspace> _sdks;
+    // Dummy service, only needed for alternator streams
+    sharded<cdc::metadata> _cdc_metadata;
+
+    sharded<alternator::executor> _executor;
+public:
+    alternator_test_env(sharded<service::storage_proxy>& proxy,
+            sharded<service::migration_manager>& mm,
+            sharded<service::storage_service>& ss,
+            sharded<cql3::query_processor>& qp)
+        : _proxy(proxy)
+        , _mm(mm)
+        , _storage_service(ss)
+        , _qp(qp)
+    {}
+
+    future<> start(std::string_view isolation_level);
+    future<> stop();
+    future<> flush_memtables();
+
+    alternator::executor& executor() {
+        return _executor.local();
+    }
+};
