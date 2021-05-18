dbb6851d4d0462b03db700cd44cf45f067dbe252
Botond Dénes <bdenes@scylladb.com>
2021-05-18 17:09:13 +0300

    test/manual/sstable_scan_footprint: don't double close the semaphore
    The semaphore `stats_collector` references is the one obtained from the
    database object, which is already stopped by `database::stop()`, making
    the stop in `~stats_collector()` redundant, and even worse, as it
    triggers an assert failure. Remove it.
    
    Signed-off-by: Botond Dénes <bdenes@scylladb.com>
    Message-Id: <20210518140913.276368-1-bdenes@scylladb.com>

--- test/manual/sstable_scan_footprint_test.cc
+++ test/manual/sstable_scan_footprint_test.cc
@@ -126,9 +126,6 @@ public:
     }
     stats_collector(const stats_collector&) = delete;
     stats_collector(stats_collector&&) = delete;
-    ~stats_collector() {
-        _sem.stop().get();
-    }
     collect_guard collect() {
         return collect_guard{*this, _params ? _params->period : std::chrono::milliseconds(0)};
     }
