db62fb690368fa5d41cdb67000c7327c6514c6b5
Takuya ASADA <syuu@scylladb.com>
2021-05-26 18:28:46 +0900

    reloc: add arch to relocatable package filename
    Add architecture name for relocatable packages, to support distributing
    both x86_64 version and aarch64 version.
    
    Also create symlink from new filename to old filename to keep
    compatibility with older scripts.
    
    Fixes #8675

--- unified/build_unified.sh
+++ unified/build_unified.sh
@@ -35,7 +35,7 @@ fi
 PRODUCT=`cat build/SCYLLA-PRODUCT-FILE`
 
 MODE="release"
-UNIFIED_PKG="build/release/$PRODUCT-unified-package.tar.gz"
+UNIFIED_PKG="build/release/$PRODUCT-unified-$(arch)-package.tar.gz"
 while [ $# -gt 0 ]; do
     case "$1" in
         "--mode")
@@ -53,7 +53,7 @@ while [ $# -gt 0 ]; do
 done
 
 UNIFIED_PKG="$(realpath -s $UNIFIED_PKG)"
-PKGS="build/$MODE/dist/tar/$PRODUCT-package.tar.gz build/$MODE/dist/tar/$PRODUCT-python3-package.tar.gz build/$MODE/dist/tar/$PRODUCT-jmx-package.tar.gz build/$MODE/dist/tar/$PRODUCT-tools-package.tar.gz"
+PKGS="build/$MODE/dist/tar/$PRODUCT-$(arch)-package.tar.gz build/$MODE/dist/tar/$PRODUCT-python3-$(arch)-package.tar.gz build/$MODE/dist/tar/$PRODUCT-jmx-package.tar.gz build/$MODE/dist/tar/$PRODUCT-tools-package.tar.gz"
 
 rm -rf build/"$MODE"/unified/
 mkdir -p build/"$MODE"/unified/
@@ -65,7 +65,7 @@ for pkg in $PKGS; do
     fi
     pkg="$(readlink -f $pkg)"
     tar -C build/"$MODE"/unified/ -xpf "$pkg"
-    dirname=$(basename "$pkg"| sed -e "s/-package.tar.gz//")
+    dirname=$(basename "$pkg"| sed -e "s/-$(arch)-package.tar.gz//" -e "s/-package.tar.gz//")
     dirname=${dirname/#$PRODUCT/scylla}
     if [ ! -d build/"$MODE"/unified/"$dirname" ]; then
         echo "Directory $dirname not found in $pkg, the pacakge may corrupted."
