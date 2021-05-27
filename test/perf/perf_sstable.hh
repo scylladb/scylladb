c3614f6e016f4ca121b3a2671a8ef90803c364a0
Wojciech Mitros <wojciech.mitros@scylladb.com>
2021-05-27 15:50:30 +0200

    test/perf: close test_env to pass an assert in sstables_manager destructor
    When destroying an perf_sstable_test_env, an assert in sstables_manager
    destructor fails, because it hasn't been closed.
    Fix by removing all references to sstables from perf_sstable_test_env,
    and then closing the test_env(as well as the sstables_manager)
    
    Fixes #8736
    
    Signed-off-by: Wojciech Mitros <wojciech.mitros@scylladb.com>

--- test/perf/perf_sstable.hh
+++ test/perf/perf_sstable.hh
@@ -110,7 +110,10 @@ public:
            , _mt(make_lw_shared<memtable>(s))
     {}
 
-    future<> stop() { return make_ready_future<>(); }
+    future<> stop() {
+        _sst.clear();
+        return _env.stop();
+    }
 
     future<> fill_memtable() {
         auto idx = boost::irange(0, int(_cfg.partitions / _cfg.sstables));
