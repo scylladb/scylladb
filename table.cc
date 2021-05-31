f08c5abbef08c25fb9e2ba004c38d6ccad1088df
Calle Wilund <calle@scylladb.com>
2021-05-31 16:18:27 +0000

    table: Always use explicit commitlog discard + clear out rp_set
    Fixes #8733
    
    If a memtable flush is still pending when we call table::clear(),
    we can end up doing a "discard-all" call to commitlog, followed
    by a per-segment-count (using rp_set) _later_. This will foobar
    our internal usage counts and quite probably cause assertion
    failures.
    Fixed by always doing per-memtable explicit discard call. But to
    ensure this works, since a memtable being flushed remains on
    memtable list for a while (why?), we must also ensure we clear
    out the rp_set on discard.
    
    v2:
    * Rename rp_set getter to more omnious name, since we actually
      clear rp_set.

--- table.cc
+++ table.cc
@@ -598,7 +598,7 @@ table::seal_active_memtable(flush_permit&& permit) {
         }
 
         if (_commitlog) {
-            _commitlog->discard_completed_segments(_schema->id(), old->rp_set());
+            _commitlog->discard_completed_segments(_schema->id(), old->get_and_discard_rp_set());
         }
         return previous_flush.finally([op = std::move(op)] { });
     });
@@ -1504,7 +1504,9 @@ bool table::can_flush() const {
 future<> table::clear() {
     _memtables->clear_and_add();
     if (_commitlog) {
-        _commitlog->discard_completed_segments(_schema->id());
+        for (auto& t : *_memtables) {
+            _commitlog->discard_completed_segments(_schema->id(), t->get_and_discard_rp_set());
+        }
     }
     return _cache.invalidate(row_cache::external_updater([] { /* There is no underlying mutation source */ }));
 }
