77c618f46ee62364d63f6be6573eb40a22bf2d80
Tomasz Grabiec <tgrabiec@scylladb.com>
2021-06-02 16:30:06 +0200

    clustering_ranges_walker: Emit range tombstone changes while walking
    The walker will now emit range tombstone change fragments while
    walking.  This is in order to support the guarantee of
    flat_mutation_reader_v2 saying that clustering range tombstone
    information must be trimmed to clustering key restrictions.
    
    For example, for ranges:
    
       [1, 3) [5, 9) [10, 11)
    
    advancing generates the following changes:
    
      using rtc = range_tombstone_change;
    
      advance_to(0, {})  ->  []
      advance_to(2, t1)  ->  [ rtc(2, t1) ]
      advance_to(4, t2)  ->  [ rtc(3, {}) ]
      advance_to(15, t3)  ->  [ rtc(5, t2), rtc(9, {}), rtc(10, t2), rtc(11, {}) ]

--- clustering_ranges_walker.hh
+++ clustering_ranges_walker.hh
@@ -26,6 +26,7 @@
 #include "schema.hh"
 #include "query-request.hh"
 #include "mutation_fragment.hh"
+#include "mutation_fragment_v2.hh"
 
 // Utility for in-order checking of overlap with position ranges.
 class clustering_ranges_walker {
@@ -33,21 +34,26 @@ class clustering_ranges_walker {
     const query::clustering_row_ranges& _ranges;
     boost::iterator_range<query::clustering_row_ranges::const_iterator> _current_range;
     bool _in_current; // next position is known to be >= _current_start
+    bool _past_current; // next position is known to be >= _current_end
+    bool _using_clustering_range; // Whether current range comes from _current_range
     bool _with_static_row;
     position_in_partition_view _current_start;
     position_in_partition_view _current_end;
     std::optional<position_in_partition> _trim;
     size_t _change_counter = 1;
+    tombstone _tombstone;
 private:
     bool advance_to_next_range() {
         _in_current = false;
-        if (!_current_start.is_static_row()) {
+        _past_current = false;
+        if (_using_clustering_range) {
             if (!_current_range) {
                 return false;
             }
             _current_range.advance_begin(1);
         }
         ++_change_counter;
+        _using_clustering_range = true;
         if (!_current_range) {
             _current_end = _current_start = position_in_partition_view::after_all_clustered_rows();
             return false;
@@ -58,17 +64,20 @@ private:
     }
 
     void set_current_positions() {
+        _using_clustering_range = false;
          if (!_with_static_row) {
             if (!_current_range) {
                 _current_start = position_in_partition_view::before_all_clustered_rows();
             } else {
                 _current_start = position_in_partition_view::for_range_start(_current_range.front());
                 _current_end = position_in_partition_view::for_range_end(_current_range.front());
+                _using_clustering_range = true;
             }
         } else {
              // If the first range is contiguous with the static row, then advance _current_end as much as we can
              if (_current_range && !_current_range.front().start()) {
                  _current_end = position_in_partition_view::for_range_end(_current_range.front());
+                 _using_clustering_range = true;
              }
         }
     }
@@ -79,6 +88,7 @@ public:
             , _ranges(ranges)
             , _current_range(ranges)
             , _in_current(with_static_row)
+            , _past_current(false)
             , _with_static_row(with_static_row)
             , _current_start(position_in_partition_view::for_static_row())
             , _current_end(position_in_partition_view::before_all_clustered_rows()) {
@@ -91,11 +101,33 @@ public:
     clustering_ranges_walker& operator=(const clustering_ranges_walker&) = delete;
     clustering_ranges_walker& operator=(clustering_ranges_walker&&) = delete;
 
+    using range_tombstones = utils::small_vector<range_tombstone_change, 1>;
+
+    // Result of advancing to a given position.
+    struct progress {
+        // True iff the position is contained in requested ranges.
+        bool contained;
+
+        // Range tombstone changes to emit which reflect current range tombstone
+        // trimmed to requested ranges, up to the advanced-to position (inclusive).
+        //
+        // It is guaranteed that the sequence of tombstones returned from all
+        // advance_to() calls will be the same for a given ranges no matter at
+        // which positions you call advance_to(), provided that you change
+        // the current tombstone at the same positions.
+        // Redundant changes will not be generated.
+        // This is to support the guarantees of flat_mutation_reader_v2.
+        range_tombstones rts;
+    };
+
     // Excludes positions smaller than pos from the ranges.
     // pos should be monotonic.
     // No constraints between pos and positions passed to advance_to().
     //
     // After the invocation, when !out_of_range(), lower_bound() returns the smallest position still contained.
+    //
+    // After this, advance_to(lower_bound()) will always emit a range tombstone change for pos
+    // if there is an active range tombstone and !out_of_range().
     void trim_front(position_in_partition pos) {
         position_in_partition::less_compare less(_schema);
 
@@ -117,22 +149,51 @@ public:
     // Must be called with monotonic positions.
     // Idempotent.
     bool advance_to(position_in_partition_view pos) {
+        return advance_to(pos, _tombstone).contained;
+    }
+
+    // Returns result of advancing over clustering restrictions.
+    // Must be called with monotonic positions.
+    //
+    // The walker tracks current clustered tombstone.
+    // The new_tombstone will be the current clustered tombstone after advancing, starting from pos (inclusive).
+    // The returned progress object contains range_tombstone_change fragments which reflect changes of
+    // the current clustered tombstone trimmed to the boundaries of requested ranges, up to the
+    // advanced-to position (inclusive).
+    progress advance_to(position_in_partition_view pos, tombstone new_tombstone) {
         position_in_partition::less_compare less(_schema);
+        range_tombstones rts;
+
+        auto prev_tombstone = _tombstone;
+        _tombstone = new_tombstone;
 
         do {
             if (!_in_current && less(pos, _current_start)) {
                 break;
             }
+
+            if (!_in_current && prev_tombstone) {
+                rts.push_back(range_tombstone_change(_current_start, prev_tombstone));
+            }
+
             // All subsequent clustering keys are larger than the start of this
             // range so there is no need to check that again.
             _in_current = true;
 
             if (less(pos, _current_end)) {
-                return true;
+                if (prev_tombstone != new_tombstone) {
+                    rts.push_back(range_tombstone_change(pos, new_tombstone));
+                }
+                return progress{.contained = true, .rts = std::move(rts)};
+            } else {
+                if (!_past_current && prev_tombstone) {
+                    rts.push_back(range_tombstone_change(_current_end, {}));
+                }
+                _past_current = true;
             }
         } while (advance_to_next_range());
 
-        return false;
+        return progress{.contained = false, .rts = std::move(rts)};
     }
 
     // Returns true if the range expressed by start and end (as in position_range) overlaps
@@ -140,6 +201,8 @@ public:
     // Must be called with monotonic start position. That position must also be greater than
     // the last position passed to the other advance_to() overload.
     // Idempotent.
+    // Breaks the tracking of current range tombstone, so don't use if you also use the advance_to()
+    // overload which tracks tombstones.
     bool advance_to(position_in_partition_view start, position_in_partition_view end) {
         position_in_partition::less_compare less(_schema);
 
@@ -215,6 +278,14 @@ public:
         return first;
     }
 
+    tombstone current_tombstone() const {
+        return _tombstone;
+    }
+
+    void set_tombstone(tombstone t) {
+        _tombstone = t;
+    }
+
     // Returns true if advanced past all contained positions. Any later advance_to() until reset() will return false.
     bool out_of_range() const {
         return !_in_current && !_current_range;
@@ -225,6 +296,7 @@ public:
     void reset() {
         _current_range = _ranges;
         _in_current = _with_static_row;
+        _past_current = false;
         _current_start = position_in_partition_view::for_static_row();
         _current_end = position_in_partition_view::before_all_clustered_rows();
         set_current_positions();
