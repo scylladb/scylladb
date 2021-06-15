774ef653b1ee4bd34e2e2872af7e2ef961000ffa
Kamil Braun <kbraun@scylladb.com>
2021-06-15 13:02:50 +0200

    test: raft: randomized_nemesis_test: move `ticker` to its own header

--- test/raft/randomized_nemesis_test.cc
+++ test/raft/randomized_nemesis_test.cc
@@ -37,6 +37,7 @@
 #include "idl/uuid.dist.impl.hh"
 
 #include "test/raft/logical_timer.hh"
+#include "test/raft/ticker.hh"
 
 using namespace seastar;
 using namespace std::chrono_literals;
@@ -1048,78 +1049,6 @@ public:
             co_await r._server->abort();
         }
         _stopped = true;
-    }
-};
-
-// Given a set of functions and associated positive natural numbers,
-// calls the functions with periods defined by their corresponding numbers,
-// yielding in between.
-//
-// Define a "tick" to be a (possibly empty) set of calls to some of the given functions
-// followed by a yield. The ticker executes a sequence of ticks. Given {n, f}, where n
-// is a number and f is a function, f will be called each nth tick.
-//
-// For example, suppose the ticker was started with the set {{2, f}, {4, g}, {4, h}}.
-// Then the functions called in each tick are:
-// tick 1: f, g, h tick 2: none, tick 3: f, tick 4: none, tick 5: f, g, h tick 2: none, and so on.
-//
-// The order of calls within a single tick is unspecified.
-//
-// The number of ticks can be limited. We crash if the ticker reaches the limit before it's `abort()`ed.
-//
-// Call `start` to provide the distribution and start the ticking.
-class ticker {
-    bool _stop = false;
-    std::optional<future<>> _ticker;
-    seastar::logger& _logger;
-
-public:
-    ticker(seastar::logger& l) : _logger(l) {}
-    ticker(const ticker&) = delete;
-    ticker(ticker&&) = delete;
-
-    ~ticker() {
-        assert(!_ticker);
-    }
-
-    using on_tick_t = noncopyable_function<void()>;
-
-    template <size_t N>
-    void start(std::pair<size_t, on_tick_t> (&&tick_funs)[N], uint64_t limit = std::numeric_limits<uint64_t>::max()) {
-        assert(!_ticker);
-        _ticker = tick(std::move(tick_funs), limit);
-    }
-
-    future<> abort() {
-        if (_ticker) {
-            _stop = true;
-            co_await *std::exchange(_ticker, std::nullopt);
-        }
-    }
-
-private:
-    template <size_t N>
-    future<> tick(std::pair<size_t, on_tick_t> (&&tick_funs)[N], uint64_t limit) {
-        static_assert(N > 0);
-
-        auto funs = std::to_array(std::move(tick_funs));
-        for (uint64_t tick = 0; tick < limit; ++tick) {
-            if (_stop) {
-                _logger.debug("ticker: finishing after {} ticks", tick);
-                co_return;
-            }
-
-            for (auto& [n, f] : funs) {
-                if (tick % n == 0) {
-                    f();
-                }
-            }
-
-            co_await seastar::later();
-        }
-
-        _logger.error("ticker: limit reached");
-        assert(false);
     }
 };
 
