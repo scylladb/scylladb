tgrabiec/scylla

sstables, cached_file: Evict cache gently when sstable is destroyed
We must evict before the _cached_index_file associated with the
sstable goes away. Better to do it gently to avoid stalls.

sstables/sstables.cc
@@ -2993,7 +2993,13 @@ void sstable::unused() {
 
 future<> sstable::destroy() {
     return close_files().finally([this] {
-        return _index_cache->destroy();
+        return _index_cache->destroy().then([this] {
+            if (_cached_index_file) {
+                return _cached_index_file->evict_gently();
+            } else {
+                return make_ready_future<>();
+            }
+        });
     });
 }
 

scylladb/scylladb

sstables/sstables.cc
@@ -2995,7 +2995,13 @@ void sstable::unused() {
 
 future<> sstable::destroy() {
     return close_files().finally([this] {
-        return _index_cache->evict_gently();
+        return _index_cache->evict_gently().then([this] {
+            if (_cached_index_file) {
+                return _cached_index_file->evict_gently();
+            } else {
+                return make_ready_future<>();
+            }
+        });
     });
 }
 