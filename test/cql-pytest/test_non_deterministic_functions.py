a130921120812ce1110937bcf520aa29bab7bbfe
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2021-07-05 13:08:16 +0300

    test: test_non_deterministic_functions: test more non-pure functions
    Check that all existing non-pure functions (except for
    `currentdate()`) work correctly with or without prepared
    statements.
    
    Tests: cql-pytest/test_non_deterministic_functions.py(dev)
    
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- test/cql-pytest/test_non_deterministic_functions.py
+++ test/cql-pytest/test_non_deterministic_functions.py
@@ -15,37 +15,59 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
 
+#############################################################################
+# Non-deterministic CQL functions should be evaluated just before query
+# execution.
+# Check that these functions (e.g. `now()`, `uuid()` and `current*()`) work
+# consistently both for un-prepared and prepared statements for LWT queries
+# (e.g. execution order is correct and `bounce_to_shard` messages forward the
+# same value for the execution on another shard).
+#############################################################################
+
+from time import sleep
 from util import new_test_table
 
-def test_lwt_uuid_fn_pk_insert(cql, test_keyspace):
-    '''Non-deterministic CQL functions should be evaluated just before query execution.
-       Check that these functions (on the example of `uuid()`) work consistently both
-       for un-prepared and prepared statements for LWT queries (e.g. execution order
-       is correct and `bounce_to_shard` messages forward the same value for the execution
-       on another shard).
-    '''
-    with new_test_table(cql, test_keyspace, "pk uuid PRIMARY KEY") as table:
+def nondeterm_fn_repeated_execute(cql, test_keyspace, pk_type, fn):
+    with new_test_table(cql, test_keyspace, f"pk {pk_type} PRIMARY KEY") as table:
         # Test that both unprepared and prepared statements work exactly the
         # same way as described above.
         # The number of inserted rows should be exactly the same as the number
-        # of query executions to show that each time `uuid()` call yields
-        # a different value.
+        # of query executions to show that each time non-deterministic function
+        # call yields a different value (sleep 1ms to make sure time-dependent
+        # functions demonstrate correct behavior).
         # Insert statements should not crash and handle `bounce_to_shard`
         # cross-shard request correctly by forwarding cached results
-        # from the initial shard to the target (until #8604 is fixed, the
-        # test would crash).
+        # from the initial shard to the target (the test used to crash
+        # before #8604 was resolved).
 
-        insert_str = f"INSERT INTO {table} (pk) VALUES (uuid()) IF NOT EXISTS"
+        insert_str = f"INSERT INTO {table} (pk) VALUES ({fn}()) IF NOT EXISTS"
         select_str = f"SELECT * FROM {table}"
         num_iterations = 10
 
         for i in range(num_iterations):
             cql.execute(insert_str)
+            sleep(0.001)
         rows = list(cql.execute(select_str))
         assert len(rows) == num_iterations
 
         insert_stmt = cql.prepare(insert_str)
         for i in range(num_iterations):
             cql.execute(insert_stmt, [])
+            sleep(0.001)
         rows = list(cql.execute(select_str))
         assert len(rows) == num_iterations * 2
+
+def test_uuid_fn_pk_insert(cql, test_keyspace):
+    nondeterm_fn_repeated_execute(cql, test_keyspace, "uuid", "uuid")
+
+def test_currenttimestamp_fn_pk_insert(cql, test_keyspace):
+    nondeterm_fn_repeated_execute(cql, test_keyspace, "timestamp", "currenttimestamp")
+
+def test_currenttime_fn_pk_insert(cql, test_keyspace):
+    nondeterm_fn_repeated_execute(cql, test_keyspace, "time", "currenttime")
+
+def test_currenttimeuuid_fn_pk_insert(cql, test_keyspace):
+    nondeterm_fn_repeated_execute(cql, test_keyspace, "timeuuid", "currenttimeuuid")
+
+def test_now_fn_pk_insert(cql, test_keyspace):
+    nondeterm_fn_repeated_execute(cql, test_keyspace, "timeuuid", "now")
