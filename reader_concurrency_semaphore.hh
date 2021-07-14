denesb/scylla

reader_concurrency_semaphore: make it an execution stage
The execution stage functionality is exposed via two new member
functions, `with_permit()` and `with_ready_permit()`. Both accept a
function to be run. The former obtains a permit then runs the passed in
function through the execution stage. The latter allows an already
obtained permit to be passed in.

reader_concurrency_semaphore.hh
@@ -22,6 +22,7 @@
 #pragma once
 
 #include <boost/intrusive/list.hpp>
+#include <seastar/core/execution_stage.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/gate.hh>
 #include "reader_permit.hh"
@@ -51,7 +52,11 @@ using namespace seastar;
 /// type `std::runtime_error` is thrown. Optionally, some additional
 /// code can be executed just before throwing (`prethrow_action` 
 /// constructor parameter).
-class reader_concurrency_semaphore {
+///
+/// The semaphore also acts as an execution stage for reads. This
+/// functionality is exposed via \ref with_permit() and \ref
+/// with_ready_permit().
+class reader_concurrency_semaphore : public execution_stage {
 public:
     using resources = reader_resources;
 
@@ -89,12 +94,15 @@ public:
 
     class inactive_read_handle;
 
+    using read_func = noncopyable_function<future<>(reader_permit)>;
+
 private:
     struct entry {
         promise<> pr;
         reader_permit permit;
-        entry(promise<>&& pr, reader_permit permit)
-            : pr(std::move(pr)), permit(std::move(permit)) {}
+        read_func func;
+        entry(promise<>&& pr, reader_permit permit, read_func func)
+            : pr(std::move(pr)), permit(std::move(permit)), func(std::move(func)) {}
     };
 
     class expiry_handler {
@@ -168,6 +176,7 @@ private:
     resources _resources;
 
     expiring_fifo<entry, expiry_handler, db::timeout_clock> _wait_list;
+    circular_buffer<entry> _ready_list;
 
     sstring _name;
     size_t _max_queue_length = std::numeric_limits<size_t>::max();
@@ -187,9 +196,9 @@ private:
 
     // Add the permit to the wait queue and return the future which resolves when
     // the permit is admitted (popped from the queue).
-    future<> enqueue_waiter(reader_permit permit, db::timeout_clock::time_point timeout);
+    future<> enqueue_waiter(reader_permit permit, db::timeout_clock::time_point timeout, read_func func);
     void evict_readers_in_background();
-    future<> do_wait_admission(reader_permit permit, db::timeout_clock::time_point timeout);
+    future<> do_wait_admission(reader_permit permit, db::timeout_clock::time_point timeout, read_func func = {});
     void maybe_admit_waiters() noexcept;
 
     void on_permit_created(reader_permit::impl&) noexcept;
@@ -205,6 +214,8 @@ private:
 
     // closes reader in the background.
     void close_reader(flat_mutation_reader reader);
+
+    virtual void do_flush() noexcept override;
 
 public:
     struct no_limits { };
@@ -342,6 +353,31 @@ public:
     /// the schema parameter is allowed.
     reader_permit make_tracking_only_permit(const schema* const schema, const char* const op_name);
     reader_permit make_tracking_only_permit(const schema* const schema, sstring&& op_name);
+
+    /// Run the function through the semaphore's execution stage with an admitted permit
+    ///
+    /// First a permit is obtained via the normal admission route, as if
+    /// it was created  with \ref obtain_permit(), then func is enqueued on
+    /// the semaphore's execution stage.
+    ///
+    /// The permit is associated with a schema, which is the schema of the table
+    /// the read is executed against, and the operation name, which should be a
+    /// name such that we can identify the operation which created this permit.
+    /// Ideally this should be a unique enough name that we not only can identify
+    /// the kind of read, but the exact code-path that was taken.
+    ///
+    /// Some permits cannot be associated with any table, so passing nullptr as
+    /// the schema parameter is allowed.
+    future<> with_permit(const schema* const schema, const char* const op_name, size_t memory, db::timeout_clock::time_point timeout, read_func func);
+
+    /// Run the function through the semaphore's execution stage with a pre-admitted permit
+    ///
+    /// Same as \ref with_permit(), but it uses an already admitted
+    /// permit. Should only be used when a permit is already readily
+    /// available, e.g. when resuming a saved read. Using
+    /// \ref obtain_permit(), then \ref with_ready_permit() is less
+    /// optimal then just using \ref with_permit().
+    future<> with_ready_permit(reader_permit permit, read_func func);
 
     const resources initial_resources() const {
         return _initial_resources;

scylladb/scylladb

reader_concurrency_semaphore.hh
@@ -51,6 +51,10 @@ using namespace seastar;
 /// type `std::runtime_error` is thrown. Optionally, some additional
 /// code can be executed just before throwing (`prethrow_action` 
 /// constructor parameter).
+///
+/// The semaphore also acts as an execution stage for reads. This
+/// functionality is exposed via \ref with_permit() and \ref
+/// with_ready_permit().
 class reader_concurrency_semaphore {
 public:
     using resources = reader_resources;
@@ -94,12 +98,15 @@ public:
 
     class inactive_read_handle;
 
+    using read_func = noncopyable_function<future<>(reader_permit)>;
+
 private:
     struct entry {
         promise<> pr;
         reader_permit permit;
-        entry(promise<>&& pr, reader_permit permit)
-            : pr(std::move(pr)), permit(std::move(permit)) {}
+        read_func func;
+        entry(promise<>&& pr, reader_permit permit, read_func func)
+            : pr(std::move(pr)), permit(std::move(permit)), func(std::move(func)) {}
     };
 
     class expiry_handler {
@@ -173,6 +180,7 @@ private:
     resources _resources;
 
     expiring_fifo<entry, expiry_handler, db::timeout_clock> _wait_list;
+    queue<entry> _ready_list;
 
     sstring _name;
     size_t _max_queue_length = std::numeric_limits<size_t>::max();
@@ -183,6 +191,7 @@ private:
     bool _stopped = false;
     gate _close_readers_gate;
     gate _permit_gate;
+    std::optional<future<>> _execution_loop_future;
 
 private:
     [[nodiscard]] flat_mutation_reader detach_inactive_reader(inactive_read&, evict_reason reason) noexcept;
@@ -190,11 +199,13 @@ private:
 
     bool has_available_units(const resources& r) const;
 
+    [[nodiscard]] std::exception_ptr check_queue_size(std::string_view queue_name);
+
     // Add the permit to the wait queue and return the future which resolves when
     // the permit is admitted (popped from the queue).
-    future<> enqueue_waiter(reader_permit permit, db::timeout_clock::time_point timeout);
+    future<> enqueue_waiter(reader_permit permit, db::timeout_clock::time_point timeout, read_func func);
     void evict_readers_in_background();
-    future<> do_wait_admission(reader_permit permit, db::timeout_clock::time_point timeout);
+    future<> do_wait_admission(reader_permit permit, db::timeout_clock::time_point timeout, read_func func = {});
     void maybe_admit_waiters() noexcept;
 
     void on_permit_created(reader_permit::impl&);
@@ -210,6 +221,8 @@ private:
 
     // closes reader in the background.
     void close_reader(flat_mutation_reader reader);
+
+    future<> execution_loop() noexcept;
 
 public:
     struct no_limits { };
@@ -347,6 +360,36 @@ public:
     /// the schema parameter is allowed.
     reader_permit make_tracking_only_permit(const schema* const schema, const char* const op_name);
     reader_permit make_tracking_only_permit(const schema* const schema, sstring&& op_name);
+
+    /// Run the function through the semaphore's execution stage with an admitted permit
+    ///
+    /// First a permit is obtained via the normal admission route, as if
+    /// it was created  with \ref obtain_permit(), then func is enqueued to be
+    /// run by the semaphore's execution loop. This emulates an execution stage,
+    /// as it allows batching multiple funcs to be run together. Unlike an
+    /// execution stage, with_permit() accepts a type-erased function, which
+    /// allows for more flexibility in what functions are batched together.
+    /// Use only functions that share most of their code to benefit from the
+    /// instruction-cache warm-up!
+    ///
+    /// The permit is associated with a schema, which is the schema of the table
+    /// the read is executed against, and the operation name, which should be a
+    /// name such that we can identify the operation which created this permit.
+    /// Ideally this should be a unique enough name that we not only can identify
+    /// the kind of read, but the exact code-path that was taken.
+    ///
+    /// Some permits cannot be associated with any table, so passing nullptr as
+    /// the schema parameter is allowed.
+    future<> with_permit(const schema* const schema, const char* const op_name, size_t memory, db::timeout_clock::time_point timeout, read_func func);
+
+    /// Run the function through the semaphore's execution stage with a pre-admitted permit
+    ///
+    /// Same as \ref with_permit(), but it uses an already admitted
+    /// permit. Should only be used when a permit is already readily
+    /// available, e.g. when resuming a saved read. Using
+    /// \ref obtain_permit(), then \ref with_ready_permit() is less
+    /// optimal then just using \ref with_permit().
+    future<> with_ready_permit(reader_permit permit, read_func func);
 
     const resources initial_resources() const {
         return _initial_resources;