f866bc2802a2c4f98e2077e532839a48309269d0
Laura Novich <36125151+lauranovich@users.noreply.github.com>
2021-07-20 10:37:57 +0300

    Update docs/conf.py
    Co-authored-by: David Garcia <hi@davidgarcia.dev>

--- docs/conf.py
+++ docs/conf.py
@@ -159,7 +159,7 @@ notfound_urls_prefix = ''
 # -- Options for redirect extension ---------------------------------------
 
 # Read a YAML dictionary of redirections and generate an HTML file for each
-redirects_file = "../_utils/redirections.yaml"
+redirects_file = "_utils/redirections.yaml"
 
 # -- Options for multiversion extension ----------------------------------
 # Whitelist pattern for tags (set to None to ignore all tags)
