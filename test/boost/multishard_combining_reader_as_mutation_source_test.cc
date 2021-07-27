e184ed2b9cc50e11e5a52d196ac4b2603c1f1265
Pavel Emelyanov <xemul@scylladb.com>
2021-07-27 08:42:45 +0300

    test: Fix indentation after previous patch
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/boost/multishard_combining_reader_as_mutation_source_test.cc
+++ test/boost/multishard_combining_reader_as_mutation_source_test.cc
@@ -45,69 +45,69 @@
 static std::list<dummy_sharder> keep_alive_sharder;
 
 static auto make_populate(bool evict_paused_readers, bool single_fragment_buffer) {
-            return [&, evict_paused_readers, single_fragment_buffer] (schema_ptr s, const std::vector<mutation>& mutations) mutable {
-                // We need to group mutations that have the same token so they land on the same shard.
-                std::map<dht::token, std::vector<frozen_mutation>> mutations_by_token;
+    return [evict_paused_readers, single_fragment_buffer] (schema_ptr s, const std::vector<mutation>& mutations) mutable {
+        // We need to group mutations that have the same token so they land on the same shard.
+        std::map<dht::token, std::vector<frozen_mutation>> mutations_by_token;
 
-                for (const auto& mut : mutations) {
-                    mutations_by_token[mut.token()].push_back(freeze(mut));
-                }
+        for (const auto& mut : mutations) {
+            mutations_by_token[mut.token()].push_back(freeze(mut));
+        }
 
-                dummy_sharder sharder(s->get_sharder(), mutations_by_token);
+        dummy_sharder sharder(s->get_sharder(), mutations_by_token);
 
-                auto merged_mutations = boost::copy_range<std::vector<std::vector<frozen_mutation>>>(mutations_by_token | boost::adaptors::map_values);
+        auto merged_mutations = boost::copy_range<std::vector<std::vector<frozen_mutation>>>(mutations_by_token | boost::adaptors::map_values);
 
-                auto remote_memtables = make_lw_shared<std::vector<foreign_ptr<lw_shared_ptr<memtable>>>>();
-                for (unsigned shard = 0; shard < sharder.shard_count(); ++shard) {
-                    auto remote_mt = smp::submit_to(shard, [shard, gs = global_schema_ptr(s), &merged_mutations, sharder] {
-                        auto s = gs.get();
-                        auto mt = make_lw_shared<memtable>(s);
+        auto remote_memtables = make_lw_shared<std::vector<foreign_ptr<lw_shared_ptr<memtable>>>>();
+        for (unsigned shard = 0; shard < sharder.shard_count(); ++shard) {
+            auto remote_mt = smp::submit_to(shard, [shard, gs = global_schema_ptr(s), &merged_mutations, sharder] {
+                auto s = gs.get();
+                auto mt = make_lw_shared<memtable>(s);
 
-                        for (unsigned i = shard; i < merged_mutations.size(); i += sharder.shard_count()) {
-                            for (auto& mut : merged_mutations[i]) {
-                                mt->apply(mut.unfreeze(s));
-                            }
-                        }
-
-                        return make_foreign(mt);
-                    }).get0();
-                    remote_memtables->emplace_back(std::move(remote_mt));
-                }
-                keep_alive_sharder.push_back(sharder);
-
-                return mutation_source([&, remote_memtables, evict_paused_readers, single_fragment_buffer] (schema_ptr s,
-                        reader_permit permit,
-                        const dht::partition_range& range,
-                        const query::partition_slice& slice,
-                        const io_priority_class& pc,
-                        tracing::trace_state_ptr trace_state,
-                        streamed_mutation::forwarding fwd_sm,
-                        mutation_reader::forwarding fwd_mr) mutable {
-                    auto factory = [remote_memtables, single_fragment_buffer] (
-                            schema_ptr s,
-                            reader_permit permit,
-                            const dht::partition_range& range,
-                            const query::partition_slice& slice,
-                            const io_priority_class& pc,
-                            tracing::trace_state_ptr trace_state,
-                            mutation_reader::forwarding fwd_mr) {
-                            auto reader = remote_memtables->at(this_shard_id())->make_flat_reader(s, std::move(permit), range, slice, pc, std::move(trace_state),
-                                    streamed_mutation::forwarding::no, fwd_mr);
-                            if (single_fragment_buffer) {
-                                reader.set_max_buffer_size(1);
-                            }
-                            return reader;
-                    };
-
-                    auto lifecycle_policy = seastar::make_shared<test_reader_lifecycle_policy>(std::move(factory), evict_paused_readers);
-                    auto mr = make_multishard_combining_reader_for_tests(keep_alive_sharder.back(), std::move(lifecycle_policy), s,
-                            std::move(permit), range, slice, pc, trace_state, fwd_mr);
-                    if (fwd_sm == streamed_mutation::forwarding::yes) {
-                        return make_forwardable(std::move(mr));
+                for (unsigned i = shard; i < merged_mutations.size(); i += sharder.shard_count()) {
+                    for (auto& mut : merged_mutations[i]) {
+                        mt->apply(mut.unfreeze(s));
                     }
-                    return mr;
-                });
+                }
+
+                return make_foreign(mt);
+            }).get0();
+            remote_memtables->emplace_back(std::move(remote_mt));
+        }
+        keep_alive_sharder.push_back(sharder);
+
+        return mutation_source([&, remote_memtables, evict_paused_readers, single_fragment_buffer] (schema_ptr s,
+                reader_permit permit,
+                const dht::partition_range& range,
+                const query::partition_slice& slice,
+                const io_priority_class& pc,
+                tracing::trace_state_ptr trace_state,
+                streamed_mutation::forwarding fwd_sm,
+                mutation_reader::forwarding fwd_mr) mutable {
+            auto factory = [remote_memtables, single_fragment_buffer] (
+                    schema_ptr s,
+                    reader_permit permit,
+                    const dht::partition_range& range,
+                    const query::partition_slice& slice,
+                    const io_priority_class& pc,
+                    tracing::trace_state_ptr trace_state,
+                    mutation_reader::forwarding fwd_mr) {
+                    auto reader = remote_memtables->at(this_shard_id())->make_flat_reader(s, std::move(permit), range, slice, pc, std::move(trace_state),
+                            streamed_mutation::forwarding::no, fwd_mr);
+                    if (single_fragment_buffer) {
+                        reader.set_max_buffer_size(1);
+                    }
+                    return reader;
             };
+
+            auto lifecycle_policy = seastar::make_shared<test_reader_lifecycle_policy>(std::move(factory), evict_paused_readers);
+            auto mr = make_multishard_combining_reader_for_tests(keep_alive_sharder.back(), std::move(lifecycle_policy), s,
+                    std::move(permit), range, slice, pc, trace_state, fwd_mr);
+            if (fwd_sm == streamed_mutation::forwarding::yes) {
+                return make_forwardable(std::move(mr));
+            }
+            return mr;
+        });
+    };
 }
 
 // Best run with SMP >= 2
