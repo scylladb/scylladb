cb190481863cdd4d90e8aae1e1b0f976a4a7f9c3
Takuya ASADA <syuu@scylladb.com>
2021-08-04 20:18:33 +0900

    docker: use dist/common/supervisor script for docker
    supervisor scripts for Docker and supervisor scripts for offline
    installer are almost same, drop Docker one and share same code to
    deduplicate them.
    
    Closes #9143
    
    Fixes #9194

--- dist/docker/node-exporter-service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/node_exporter  --collector.interrupts
-
-
--- dist/docker/scylla-jmx-service.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /etc/default/scylla-jmx
-
-exec /opt/scylladb/jmx/scylla-jmx $SCYLLA_JMX_PORT $SCYLLA_API_PORT $SCYLLA_API_ADDR $SCYLLA_JMX_ADDR $SCYLLA_JMX_FILE $SCYLLA_JMX_LOCAL $SCYLLA_JMX_REMOTE $SCYLLA_JMX_DEBUG
