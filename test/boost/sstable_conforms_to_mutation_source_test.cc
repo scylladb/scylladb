77a8fee513bcc0e8a79373b0452bf0fead551c05
Pavel Emelyanov <xemul@scylladb.com>
2021-08-27 19:12:09 +0300

    tests: Fix indentation after previous patch
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/boost/sstable_conforms_to_mutation_source_test.cc
+++ test/boost/sstable_conforms_to_mutation_source_test.cc
@@ -97,22 +97,22 @@ void test_cache_population_with_range_tombstone_adjacent_to_population_range(pop
 
 static future<> test_sstable_conforms_to_mutation_source(sstable_version_types version, int index_block_size) {
     return sstables::test_env::do_with_async([version, index_block_size] (sstables::test_env& env) {
-                sstable_writer_config cfg = env.manager().configure_writer();
-                cfg.promoted_index_block_size = index_block_size;
+        sstable_writer_config cfg = env.manager().configure_writer();
+        cfg.promoted_index_block_size = index_block_size;
 
-                std::vector<tmpdir> dirs;
-                auto populate = [&env, &dirs, &cfg, version] (schema_ptr s, const std::vector<mutation>& partitions,
-                                                              gc_clock::time_point query_time) -> mutation_source {
-                    dirs.emplace_back();
-                    return make_sstable_mutation_source(env, s, dirs.back().path().string(), partitions, cfg, version, query_time);
-                };
+        std::vector<tmpdir> dirs;
+        auto populate = [&env, &dirs, &cfg, version] (schema_ptr s, const std::vector<mutation>& partitions,
+                                                      gc_clock::time_point query_time) -> mutation_source {
+            dirs.emplace_back();
+            return make_sstable_mutation_source(env, s, dirs.back().path().string(), partitions, cfg, version, query_time);
+        };
 
-                run_mutation_source_tests(populate);
+        run_mutation_source_tests(populate);
 
-                if (index_block_size == 1) {
-                    // The tests below are not sensitive to index bock size so run once.
-                    test_cache_population_with_range_tombstone_adjacent_to_population_range(populate);
-                }
+        if (index_block_size == 1) {
+            // The tests below are not sensitive to index bock size so run once.
+            test_cache_population_with_range_tombstone_adjacent_to_population_range(populate);
+        }
     });
 }
 
