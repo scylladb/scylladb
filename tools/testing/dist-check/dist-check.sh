4fe4aa190d3b2d0ab3d45f0e247e08b890c6c32e
Beni Peled <beni.peled@scylladb.com>
2021-08-30 09:39:50 +0300

    dist-check: add podman support
    ...and use container term instead of docker
    
    Closes #9265

--- tools/testing/dist-check/dist-check.sh
+++ tools/testing/dist-check/dist-check.sh
@@ -30,6 +30,15 @@ print_usage() {
     exit 1
 }
 
+if which podman > /dev/null 2>&1 ; then
+	contool=podman
+elif which docker > /dev/null 2>&1 ; then
+	contool=docker
+else
+	echo "Please make sure you have either podman or docker installed on this host in order to use dist-chec"
+	exit 1
+fi
+
 while [ $# -gt 0 ]; do
     case "$1" in
         "--mode")
@@ -50,22 +59,22 @@ if [ -z "$MODE" ]; then
 fi
 
 if [ -f /.dockerenv ]; then
-    echo "error: running $PROGRAM in Docker is not supported, please run on host."
+    echo "error: running $PROGRAM in container is not supported, please run on host."
     exit 1
 fi
 
-docker_images=(
+container_images=(
     docker.io/centos:7
 )
 
-for docker_image in "${docker_images[@]}"
+for container_image in "${container_images=[@]}"
 do
-    docker_script="${docker_image//:/-}"
-    install_sh="$(pwd)/tools/testing/dist-check/$docker_script.sh"
+    container_script="${container_image//:/-}"
+    install_sh="$(pwd)/tools/testing/dist-check/$container_script.sh"
     if [ -f "$install_sh" ]; then
-        docker run -i --rm -v $(pwd):$(pwd) $docker_image /bin/bash -c "cd $(pwd) && $install_sh --mode $MODE"
+        $contool run -i --rm -v $(pwd):$(pwd) $container_image /bin/bash -c "cd $(pwd) && $install_sh --mode $MODE"
     else
-        echo "internal error: $install_sh does not exist, please create one to verify packages on $docker_image."
+        echo "internal error: $install_sh does not exist, please create one to verify packages on $container_image."
         exit 1
     fi
 done
