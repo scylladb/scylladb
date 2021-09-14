f3a9f361b9914b063c7e4745d96bc8045d1d9f30
Calle Wilund <calle@scylladb.com>
2021-09-14 12:36:53 +0000

    commitlog::allocate_segment_ex: Don't re-throw out of function
    Fixes #9342
    
    commitlog_error_handler rethrows. But we want to not. And run post-handler
    cleanup (co_await)

--- db/commitlog/commitlog.cc
+++ db/commitlog/commitlog.cc
@@ -1525,8 +1525,11 @@ future<db::commitlog::segment_manager::sseg_ptr> db::commitlog::segment_manager:
 
         f = make_checked_file(commit_error_handler, std::move(f));
     } catch (...) {
-        ep = std::current_exception();
-        commit_error_handler(ep);
+        try {
+            commit_error_handler(std::current_exception());
+        } catch (...) {
+            ep = std::current_exception();
+        }
     }
     if (ep && f) {
         co_await f.close();
