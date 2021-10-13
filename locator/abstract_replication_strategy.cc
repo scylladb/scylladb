bhalevy/scylla

storage_service: update keyspaces effective_replication_map on token_metadata change
Every time the token_metadata changes we need to update the
effective_replication_map on all non-system keyspaces.

Do that in replicate_to_all_cores after the updated token_metadata
has been replicated to all cores.

We first prepare and clone the token_metadata, then prepare
and clone the new effective_replication_maps.  Any failure
at this stage is recoverable, handle via rollback and the exception
is returned.

Note that any failure to _apply_ the pending token_metadata or the
effective_replication_map will cause scylla to abort.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

locator/abstract_replication_strategy.cc
@@ -24,6 +24,7 @@
 #include "exceptions/exceptions.hh"
 #include <boost/range/algorithm/remove_if.hpp>
 #include <seastar/core/coroutine.hh>
+#include <seastar/coroutine/maybe_yield.hh>
 #include "utils/stall_free.hh"
 
 namespace locator {
@@ -356,6 +357,17 @@ future<effective_replication_map_ptr> abstract_replication_strategy::make_effect
 future<> effective_replication_map::clear_gently() noexcept {
     co_await utils::clear_gently(_all_endpoints);
     co_await utils::clear_gently(_tmptr);
+}
+
+future<replication_map> effective_replication_map::clone_endpoints_gently() const {
+    replication_map cloned_endpoints;
+
+    for (auto& i : _all_endpoints) {
+        cloned_endpoints.emplace(i.first, i.second);
+        co_await coroutine::maybe_yield();
+    }
+
+    co_return cloned_endpoints;
 }
 
 inet_address_vector_replica_set effective_replication_map::get_natural_endpoints(const token& search_token) const {

scylladb/scylladb

locator/abstract_replication_strategy.cc
@@ -24,6 +24,7 @@
 #include "exceptions/exceptions.hh"
 #include <boost/range/algorithm/remove_if.hpp>
 #include <seastar/core/coroutine.hh>
+#include <seastar/coroutine/maybe_yield.hh>
 #include "utils/stall_free.hh"
 
 namespace locator {
@@ -351,6 +352,17 @@ future<mutable_effective_replication_map_ptr> calculate_effective_replication_ma
     }
 
     co_return make_effective_replication_map(std::move(rs), std::move(tmptr), std::move(replication_map));
+}
+
+future<replication_map> effective_replication_map::clone_endpoints_gently() const {
+    replication_map cloned_endpoints;
+
+    for (auto& i : _replication_map) {
+        cloned_endpoints.emplace(i.first, i.second);
+        co_await coroutine::maybe_yield();
+    }
+
+    co_return cloned_endpoints;
 }
 
 inet_address_vector_replica_set effective_replication_map::get_natural_endpoints(const token& search_token) const {