75bab2beec797a4d9a03ef4eb637b95278d95bf3
Kamil Braun <kbraun@scylladb.com>
2021-10-25 15:39:43 +0200

    raft: server: print the ID of aborted server

--- raft/server.cc
+++ raft/server.cc
@@ -1080,7 +1080,7 @@ void server_impl::abort_snapshot_transfers() {
 }
 
 future<> server_impl::abort() {
-    logger.trace("abort() called");
+    logger.trace("[{}]: abort() called", _id);
     _fsm->stop();
     _apply_entries.abort(std::make_exception_ptr(stop_apply_fiber()));
 
