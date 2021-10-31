5d7064e00e83a205881dcc3dd0354b8830b6cef8
Nadav Har'El <nyh@scylladb.com>
2021-10-31 11:46:21 +0200

    alternator: return the correct Content-Type header
    Although the DynamoDB API responses are JSON, additional conventions apply
    to these responses - such as how error codes are encoded in JSON. For this
    reason, DynamoDB uses the content type `application/x-amz-json-1.0` instead
    of the standard `application/json` in its responses.
    
    Until this patch, Scylla used `application/json` in its responses. This
    unexpected content-type didn't bother any of the AWS libraries which we
    tested, but it does bother the aiodynamo library (see HENNGE/aiodynamo#27).
    
    Moreover, we should return the x-amz-json-1.0 content type for future
    proofing: It turns out that AWS already defined x-amz-json-1.1 - see:
    https://awslabs.github.io/smithy/1.0/spec/aws/aws-json-1_1-protocol.html
    The 1.1 content type differs (only) in how it encodes error replies.
    If one day DynamoDB starts to use this new reply format (it doesn't yet)
    and if DynamoDB libraries will need to differenciate between the two
    reply formats, Alternator better return the right one.
    
    This patch also includes a new test that the Content-Type header is
    returned with the expected value. The test passes on DynamoDB, and
    after this patch it starts to pass on Alternator as well.
    
    Fixes #9554.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <20211031094621.1193387-1-nyh@scylladb.com>
    (cherry picked from commit 6ae0ea0c4880e3fad8ec8c9ffff28283a02a185d)

--- alternator/server.cc
+++ alternator/server.cc
@@ -129,6 +129,10 @@ public:
                  [&] (const json::json_return_type& json_return_value) {
                      slogger.trace("api_handler success case");
                      if (json_return_value._body_writer) {
+                         // Unfortunately, write_body() forces us to choose
+                         // from a fixed and irrelevant list of "mime-types"
+                         // at this point. But we'll override it with the
+                         // one (application/x-amz-json-1.0) below.
                          rep->write_body("json", std::move(json_return_value._body_writer));
                      } else {
                          rep->_content += json_return_value._res;
@@ -141,7 +145,7 @@ public:
 
              return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
          });
-    }), _type("json") { }
+    }) { }
 
     api_handler(const api_handler&) = default;
     future<std::unique_ptr<reply>> handle(const sstring& path,
@@ -149,7 +153,8 @@ public:
         handle_CORS(*req, *rep, false);
         return _f_handle(std::move(req), std::move(rep)).then(
                 [this](std::unique_ptr<reply> rep) {
-                    rep->done(_type);
+                    rep->set_mime_type("application/x-amz-json-1.0");
+                    rep->done();
                     return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
                 });
     }
@@ -163,7 +168,6 @@ protected:
     }
 
     future_handler_function _f_handle;
-    sstring _type;
 };
 
 class gated_handler : public handler_base {
