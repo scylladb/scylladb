raphaelsc/scylla

compaction: switch to table_state in compaction_strategy::get_sstables_for_compaction()
From now on, get_sstables_for_compaction() will use table_state.
With table_state, we avoid layer violations like strategy using
manager and also makes testing easier.

Compaction unit tests were temporarily disabled to avoid a giant
commit which is hard to parse.

Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

compaction/leveled_compaction_strategy.cc
@@ -27,20 +27,20 @@
 
 namespace sstables {
 
-compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) {
+compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(table_state& table_s, std::vector<sstables::shared_sstable> candidates) {
     // NOTE: leveled_manifest creation may be slightly expensive, so later on,
     // we may want to store it in the strategy itself. However, the sstable
     // lists managed by the manifest may become outdated. For example, one
     // sstable in it may be marked for deletion after compacted.
     // Currently, we create a new manifest whenever it's time for compaction.
-    leveled_manifest manifest = leveled_manifest::create(cfs, candidates, _max_sstable_size_in_mb, _stcs_options);
+    leveled_manifest manifest = leveled_manifest::create(table_s, candidates, _max_sstable_size_in_mb, _stcs_options);
     if (!_last_compacted_keys) {
         generate_last_compacted_keys(manifest);
     }
     auto candidate = manifest.get_compaction_candidates(*_last_compacted_keys, _compaction_counter);
 
     if (!candidate.sstables.empty()) {
-        leveled_manifest::logger.debug("leveled: Compacting {} out of {} sstables", candidate.sstables.size(), cfs.get_sstables()->size());
+        leveled_manifest::logger.debug("leveled: Compacting {} out of {} sstables", candidate.sstables.size(), table_s.sstable_set().all()->size());
         return candidate;
     }
 
@@ -48,7 +48,7 @@ compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(c
     // unlike stcs, lcs can look for sstable with highest droppable tombstone ratio, so as not to choose
     // a sstable which droppable data shadow data in older sstable, by starting from highest levels which
     // theoretically contain oldest non-overlapping data.
-    auto gc_before = gc_clock::now() - cfs.schema()->gc_grace_seconds();
+    auto gc_before = gc_clock::now() - table_s.schema()->gc_grace_seconds();
     for (auto level = int(manifest.get_level_count()); level >= 0; level--) {
         auto& sstables = manifest.get_level(level);
         // filter out sstables which droppable tombstone ratio isn't greater than the defined threshold.
@@ -62,7 +62,7 @@ compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(c
         auto& sst = *std::max_element(sstables.begin(), sstables.end(), [&] (auto& i, auto& j) {
             return i->estimate_droppable_tombstone_ratio(gc_before) < j->estimate_droppable_tombstone_ratio(gc_before);
         });
-        return sstables::compaction_descriptor({ sst }, cfs.get_sstable_set(), service::get_local_compaction_priority(), sst->get_sstable_level());
+        return sstables::compaction_descriptor({ sst }, table_s.sstable_set(), service::get_local_compaction_priority(), sst->get_sstable_level());
     }
     return {};
 }

compaction/size_tiered_compaction_strategy.cc
@@ -158,11 +158,11 @@ size_tiered_compaction_strategy::most_interesting_bucket(std::vector<std::vector
 }
 
 compaction_descriptor
-size_tiered_compaction_strategy::get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) {
+size_tiered_compaction_strategy::get_sstables_for_compaction(table_state& table_s, std::vector<sstables::shared_sstable> candidates) {
     // make local copies so they can't be changed out from under us mid-method
-    int min_threshold = cfs.min_compaction_threshold();
-    int max_threshold = cfs.schema()->max_compaction_threshold();
-    auto gc_before = gc_clock::now() - cfs.schema()->gc_grace_seconds();
+    int min_threshold = table_s.min_compaction_threshold();
+    int max_threshold = table_s.schema()->max_compaction_threshold();
+    auto gc_before = gc_clock::now() - table_s.schema()->gc_grace_seconds();
 
     // TODO: Add support to filter cold sstables (for reference: SizeTieredCompactionStrategy::filterColdSSTables).
 
@@ -170,13 +170,13 @@ size_tiered_compaction_strategy::get_sstables_for_compaction(column_family& cfs,
 
     if (is_any_bucket_interesting(buckets, min_threshold)) {
         std::vector<sstables::shared_sstable> most_interesting = most_interesting_bucket(std::move(buckets), min_threshold, max_threshold);
-        return sstables::compaction_descriptor(std::move(most_interesting), cfs.get_sstable_set(), service::get_local_compaction_priority());
+        return sstables::compaction_descriptor(std::move(most_interesting), table_s.sstable_set(), service::get_local_compaction_priority());
     }
 
     // If we are not enforcing min_threshold explicitly, try any pair of SStables in the same tier.
-    if (!cfs.compaction_enforce_min_threshold() && is_any_bucket_interesting(buckets, 2)) {
+    if (!table_s.enforce_min_compaction_threshold() && is_any_bucket_interesting(buckets, 2)) {
         std::vector<sstables::shared_sstable> most_interesting = most_interesting_bucket(std::move(buckets), 2, max_threshold);
-        return sstables::compaction_descriptor(std::move(most_interesting), cfs.get_sstable_set(), service::get_local_compaction_priority());
+        return sstables::compaction_descriptor(std::move(most_interesting), table_s.sstable_set(), service::get_local_compaction_priority());
     }
 
     // if there is no sstable to compact in standard way, try compacting single sstable whose droppable tombstone
@@ -196,7 +196,7 @@ size_tiered_compaction_strategy::get_sstables_for_compaction(column_family& cfs,
         auto it = std::min_element(sstables.begin(), sstables.end(), [] (auto& i, auto& j) {
             return i->get_stats_metadata().min_timestamp < j->get_stats_metadata().min_timestamp;
         });
-        return sstables::compaction_descriptor({ *it }, cfs.get_sstable_set(), service::get_local_compaction_priority());
+        return sstables::compaction_descriptor({ *it }, table_s.sstable_set(), service::get_local_compaction_priority());
     }
     return sstables::compaction_descriptor();
 }

compaction/time_window_compaction_strategy.cc
@@ -221,8 +221,8 @@ time_window_compaction_strategy::get_reshaping_job(std::vector<shared_sstable> i
 }
 
 compaction_descriptor
-time_window_compaction_strategy::get_sstables_for_compaction(column_family& cf, std::vector<shared_sstable> candidates) {
-    auto gc_before = gc_clock::now() - cf.schema()->gc_grace_seconds();
+time_window_compaction_strategy::get_sstables_for_compaction(table_state& table_s, std::vector<shared_sstable> candidates) {
+    auto gc_before = gc_clock::now() - table_s.schema()->gc_grace_seconds();
 
     if (candidates.empty()) {
         return compaction_descriptor();
@@ -233,7 +233,7 @@ time_window_compaction_strategy::get_sstables_for_compaction(column_family& cf, 
 
     if (db_clock::now() - _last_expired_check > _options.expired_sstable_check_frequency) {
         clogger.debug("TWCS expired check sufficiently far in the past, checking for fully expired SSTables");
-        expired = get_fully_expired_sstables(cf, candidates, gc_before);
+        expired = table_s.fully_expired_sstables(candidates);
         _last_expired_check = db_clock::now();
     } else {
         clogger.debug("TWCS skipping check for fully expired SSTables");
@@ -241,11 +241,11 @@ time_window_compaction_strategy::get_sstables_for_compaction(column_family& cf, 
 
     if (!expired.empty()) {
         clogger.debug("Going to compact {} expired sstables", expired.size());
-        return compaction_descriptor(has_only_fully_expired::yes, std::vector<shared_sstable>(expired.begin(), expired.end()), cf.get_sstable_set(), service::get_local_compaction_priority());
+        return compaction_descriptor(has_only_fully_expired::yes, std::vector<shared_sstable>(expired.begin(), expired.end()), table_s.sstable_set(), service::get_local_compaction_priority());
     }
 
-    auto compaction_candidates = get_next_non_expired_sstables(cf, std::move(candidates), gc_before);
-    return compaction_descriptor(std::move(compaction_candidates), cf.get_sstable_set(), service::get_local_compaction_priority());
+    auto compaction_candidates = get_next_non_expired_sstables(table_s, std::move(candidates), gc_before);
+    return compaction_descriptor(std::move(compaction_candidates), table_s.sstable_set(), service::get_local_compaction_priority());
 }
 
 time_window_compaction_strategy::bucket_compaction_mode
@@ -264,9 +264,9 @@ time_window_compaction_strategy::compaction_mode(const bucket_t& bucket, timesta
 }
 
 std::vector<shared_sstable>
-time_window_compaction_strategy::get_next_non_expired_sstables(column_family& cf,
+time_window_compaction_strategy::get_next_non_expired_sstables(table_state& table_s,
         std::vector<shared_sstable> non_expiring_sstables, gc_clock::time_point gc_before) {
-    auto most_interesting = get_compaction_candidates(cf, non_expiring_sstables);
+    auto most_interesting = get_compaction_candidates(table_s, non_expiring_sstables);
 
     if (!most_interesting.empty()) {
         return most_interesting;
@@ -288,14 +288,14 @@ time_window_compaction_strategy::get_next_non_expired_sstables(column_family& cf
 }
 
 std::vector<shared_sstable>
-time_window_compaction_strategy::get_compaction_candidates(column_family& cf, std::vector<shared_sstable> candidate_sstables) {
+time_window_compaction_strategy::get_compaction_candidates(table_state& table_s, std::vector<shared_sstable> candidate_sstables) {
     auto p = get_buckets(std::move(candidate_sstables), _options);
     // Update the highest window seen, if necessary
     _highest_window_seen = std::max(_highest_window_seen, p.second);
 
-    update_estimated_compaction_by_tasks(p.first, cf.min_compaction_threshold(), cf.schema()->max_compaction_threshold());
+    update_estimated_compaction_by_tasks(p.first, table_s.min_compaction_threshold(), table_s.schema()->max_compaction_threshold());
 
-    return newest_bucket(std::move(p.first), cf.min_compaction_threshold(), cf.schema()->max_compaction_threshold(),
+    return newest_bucket(std::move(p.first), table_s.min_compaction_threshold(), table_s.schema()->max_compaction_threshold(),
         _options.sstable_window_size, _highest_window_seen, _stcs_options);
 }
 

test/lib/sstable_run_based_compaction_strategy_for_tests.cc
@@ -26,9 +26,9 @@ namespace sstables {
 
 sstable_run_based_compaction_strategy_for_tests::sstable_run_based_compaction_strategy_for_tests() = default;
 
-compaction_descriptor sstable_run_based_compaction_strategy_for_tests::get_sstables_for_compaction(column_family& cf, std::vector<sstables::shared_sstable> uncompacting_sstables) {
+compaction_descriptor sstable_run_based_compaction_strategy_for_tests::get_sstables_for_compaction(table_state& table_s, std::vector<sstables::shared_sstable> uncompacting_sstables) {
     // Get unique runs from all uncompacting sstables
-    std::vector<sstable_run> runs = cf.get_sstable_set().select_sstable_runs(uncompacting_sstables);
+    std::vector<sstable_run> runs = table_s.sstable_set().select_sstable_runs(uncompacting_sstables);
 
     // Group similar sized runs into a bucket.
     std::map<uint64_t, std::vector<sstable_run>> similar_sized_runs;
@@ -48,7 +48,7 @@ compaction_descriptor sstable_run_based_compaction_strategy_for_tests::get_sstab
     // Get the most interesting bucket, if any, and return sstables from all its runs.
     for (auto& entry : similar_sized_runs) {
         auto& runs = entry.second;
-        if (runs.size() < size_t(cf.schema()->min_compaction_threshold())) {
+        if (runs.size() < size_t(table_s.schema()->min_compaction_threshold())) {
             continue;
         }
         auto all = boost::accumulate(runs, std::vector<shared_sstable>(), [&] (std::vector<shared_sstable>&& v, const sstable_run& run) {

scylladb/scylladb

compaction/leveled_compaction_strategy.cc
@@ -27,20 +27,20 @@
 
 namespace sstables {
 
-compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) {
+compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(table_state& table_s, std::vector<sstables::shared_sstable> candidates) {
     // NOTE: leveled_manifest creation may be slightly expensive, so later on,
     // we may want to store it in the strategy itself. However, the sstable
     // lists managed by the manifest may become outdated. For example, one
     // sstable in it may be marked for deletion after compacted.
     // Currently, we create a new manifest whenever it's time for compaction.
-    leveled_manifest manifest = leveled_manifest::create(cfs, candidates, _max_sstable_size_in_mb, _stcs_options);
+    leveled_manifest manifest = leveled_manifest::create(table_s, candidates, _max_sstable_size_in_mb, _stcs_options);
     if (!_last_compacted_keys) {
         generate_last_compacted_keys(manifest);
     }
     auto candidate = manifest.get_compaction_candidates(*_last_compacted_keys, _compaction_counter);
 
     if (!candidate.sstables.empty()) {
-        leveled_manifest::logger.debug("leveled: Compacting {} out of {} sstables", candidate.sstables.size(), cfs.get_sstables()->size());
+        leveled_manifest::logger.debug("leveled: Compacting {} out of {} sstables", candidate.sstables.size(), table_s.get_sstable_set().all()->size());
         return candidate;
     }
 
@@ -48,7 +48,7 @@ compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(c
     // unlike stcs, lcs can look for sstable with highest droppable tombstone ratio, so as not to choose
     // a sstable which droppable data shadow data in older sstable, by starting from highest levels which
     // theoretically contain oldest non-overlapping data.
-    auto gc_before = gc_clock::now() - cfs.schema()->gc_grace_seconds();
+    auto gc_before = gc_clock::now() - table_s.schema()->gc_grace_seconds();
     for (auto level = int(manifest.get_level_count()); level >= 0; level--) {
         auto& sstables = manifest.get_level(level);
         // filter out sstables which droppable tombstone ratio isn't greater than the defined threshold.
@@ -62,7 +62,7 @@ compaction_descriptor leveled_compaction_strategy::get_sstables_for_compaction(c
         auto& sst = *std::max_element(sstables.begin(), sstables.end(), [&] (auto& i, auto& j) {
             return i->estimate_droppable_tombstone_ratio(gc_before) < j->estimate_droppable_tombstone_ratio(gc_before);
         });
-        return sstables::compaction_descriptor({ sst }, cfs.get_sstable_set(), service::get_local_compaction_priority(), sst->get_sstable_level());
+        return sstables::compaction_descriptor({ sst }, table_s.get_sstable_set(), service::get_local_compaction_priority(), sst->get_sstable_level());
     }
     return {};
 }

compaction/size_tiered_compaction_strategy.cc
@@ -158,11 +158,11 @@ size_tiered_compaction_strategy::most_interesting_bucket(std::vector<std::vector
 }
 
 compaction_descriptor
-size_tiered_compaction_strategy::get_sstables_for_compaction(column_family& cfs, std::vector<sstables::shared_sstable> candidates) {
+size_tiered_compaction_strategy::get_sstables_for_compaction(table_state& table_s, std::vector<sstables::shared_sstable> candidates) {
     // make local copies so they can't be changed out from under us mid-method
-    int min_threshold = cfs.min_compaction_threshold();
-    int max_threshold = cfs.schema()->max_compaction_threshold();
-    auto gc_before = gc_clock::now() - cfs.schema()->gc_grace_seconds();
+    int min_threshold = table_s.min_compaction_threshold();
+    int max_threshold = table_s.schema()->max_compaction_threshold();
+    auto gc_before = gc_clock::now() - table_s.schema()->gc_grace_seconds();
 
     // TODO: Add support to filter cold sstables (for reference: SizeTieredCompactionStrategy::filterColdSSTables).
 
@@ -170,13 +170,13 @@ size_tiered_compaction_strategy::get_sstables_for_compaction(column_family& cfs,
 
     if (is_any_bucket_interesting(buckets, min_threshold)) {
         std::vector<sstables::shared_sstable> most_interesting = most_interesting_bucket(std::move(buckets), min_threshold, max_threshold);
-        return sstables::compaction_descriptor(std::move(most_interesting), cfs.get_sstable_set(), service::get_local_compaction_priority());
+        return sstables::compaction_descriptor(std::move(most_interesting), table_s.get_sstable_set(), service::get_local_compaction_priority());
     }
 
     // If we are not enforcing min_threshold explicitly, try any pair of SStables in the same tier.
-    if (!cfs.compaction_enforce_min_threshold() && is_any_bucket_interesting(buckets, 2)) {
+    if (!table_s.compaction_enforce_min_threshold() && is_any_bucket_interesting(buckets, 2)) {
         std::vector<sstables::shared_sstable> most_interesting = most_interesting_bucket(std::move(buckets), 2, max_threshold);
-        return sstables::compaction_descriptor(std::move(most_interesting), cfs.get_sstable_set(), service::get_local_compaction_priority());
+        return sstables::compaction_descriptor(std::move(most_interesting), table_s.get_sstable_set(), service::get_local_compaction_priority());
     }
 
     // if there is no sstable to compact in standard way, try compacting single sstable whose droppable tombstone
@@ -196,7 +196,7 @@ size_tiered_compaction_strategy::get_sstables_for_compaction(column_family& cfs,
         auto it = std::min_element(sstables.begin(), sstables.end(), [] (auto& i, auto& j) {
             return i->get_stats_metadata().min_timestamp < j->get_stats_metadata().min_timestamp;
         });
-        return sstables::compaction_descriptor({ *it }, cfs.get_sstable_set(), service::get_local_compaction_priority());
+        return sstables::compaction_descriptor({ *it }, table_s.get_sstable_set(), service::get_local_compaction_priority());
     }
     return sstables::compaction_descriptor();
 }

compaction/time_window_compaction_strategy.cc
@@ -221,8 +221,8 @@ time_window_compaction_strategy::get_reshaping_job(std::vector<shared_sstable> i
 }
 
 compaction_descriptor
-time_window_compaction_strategy::get_sstables_for_compaction(column_family& cf, std::vector<shared_sstable> candidates) {
-    auto gc_before = gc_clock::now() - cf.schema()->gc_grace_seconds();
+time_window_compaction_strategy::get_sstables_for_compaction(table_state& table_s, std::vector<shared_sstable> candidates) {
+    auto gc_before = gc_clock::now() - table_s.schema()->gc_grace_seconds();
 
     if (candidates.empty()) {
         return compaction_descriptor();
@@ -233,7 +233,7 @@ time_window_compaction_strategy::get_sstables_for_compaction(column_family& cf, 
 
     if (db_clock::now() - _last_expired_check > _options.expired_sstable_check_frequency) {
         clogger.debug("TWCS expired check sufficiently far in the past, checking for fully expired SSTables");
-        expired = get_fully_expired_sstables(cf, candidates, gc_before);
+        expired = table_s.fully_expired_sstables(candidates);
         _last_expired_check = db_clock::now();
     } else {
         clogger.debug("TWCS skipping check for fully expired SSTables");
@@ -241,11 +241,11 @@ time_window_compaction_strategy::get_sstables_for_compaction(column_family& cf, 
 
     if (!expired.empty()) {
         clogger.debug("Going to compact {} expired sstables", expired.size());
-        return compaction_descriptor(has_only_fully_expired::yes, std::vector<shared_sstable>(expired.begin(), expired.end()), cf.get_sstable_set(), service::get_local_compaction_priority());
+        return compaction_descriptor(has_only_fully_expired::yes, std::vector<shared_sstable>(expired.begin(), expired.end()), table_s.get_sstable_set(), service::get_local_compaction_priority());
     }
 
-    auto compaction_candidates = get_next_non_expired_sstables(cf, std::move(candidates), gc_before);
-    return compaction_descriptor(std::move(compaction_candidates), cf.get_sstable_set(), service::get_local_compaction_priority());
+    auto compaction_candidates = get_next_non_expired_sstables(table_s, std::move(candidates), gc_before);
+    return compaction_descriptor(std::move(compaction_candidates), table_s.get_sstable_set(), service::get_local_compaction_priority());
 }
 
 time_window_compaction_strategy::bucket_compaction_mode
@@ -264,9 +264,9 @@ time_window_compaction_strategy::compaction_mode(const bucket_t& bucket, timesta
 }
 
 std::vector<shared_sstable>
-time_window_compaction_strategy::get_next_non_expired_sstables(column_family& cf,
+time_window_compaction_strategy::get_next_non_expired_sstables(table_state& table_s,
         std::vector<shared_sstable> non_expiring_sstables, gc_clock::time_point gc_before) {
-    auto most_interesting = get_compaction_candidates(cf, non_expiring_sstables);
+    auto most_interesting = get_compaction_candidates(table_s, non_expiring_sstables);
 
     if (!most_interesting.empty()) {
         return most_interesting;
@@ -288,14 +288,14 @@ time_window_compaction_strategy::get_next_non_expired_sstables(column_family& cf
 }
 
 std::vector<shared_sstable>
-time_window_compaction_strategy::get_compaction_candidates(column_family& cf, std::vector<shared_sstable> candidate_sstables) {
+time_window_compaction_strategy::get_compaction_candidates(table_state& table_s, std::vector<shared_sstable> candidate_sstables) {
     auto p = get_buckets(std::move(candidate_sstables), _options);
     // Update the highest window seen, if necessary
     _highest_window_seen = std::max(_highest_window_seen, p.second);
 
-    update_estimated_compaction_by_tasks(p.first, cf.min_compaction_threshold(), cf.schema()->max_compaction_threshold());
+    update_estimated_compaction_by_tasks(p.first, table_s.min_compaction_threshold(), table_s.schema()->max_compaction_threshold());
 
-    return newest_bucket(std::move(p.first), cf.min_compaction_threshold(), cf.schema()->max_compaction_threshold(),
+    return newest_bucket(std::move(p.first), table_s.min_compaction_threshold(), table_s.schema()->max_compaction_threshold(),
         _options.sstable_window_size, _highest_window_seen, _stcs_options);
 }
 

test/lib/sstable_run_based_compaction_strategy_for_tests.cc
@@ -26,9 +26,9 @@ namespace sstables {
 
 sstable_run_based_compaction_strategy_for_tests::sstable_run_based_compaction_strategy_for_tests() = default;
 
-compaction_descriptor sstable_run_based_compaction_strategy_for_tests::get_sstables_for_compaction(column_family& cf, std::vector<sstables::shared_sstable> uncompacting_sstables) {
+compaction_descriptor sstable_run_based_compaction_strategy_for_tests::get_sstables_for_compaction(table_state& table_s, std::vector<sstables::shared_sstable> uncompacting_sstables) {
     // Get unique runs from all uncompacting sstables
-    std::vector<sstable_run> runs = cf.get_sstable_set().select_sstable_runs(uncompacting_sstables);
+    std::vector<sstable_run> runs = table_s.get_sstable_set().select_sstable_runs(uncompacting_sstables);
 
     // Group similar sized runs into a bucket.
     std::map<uint64_t, std::vector<sstable_run>> similar_sized_runs;
@@ -48,7 +48,7 @@ compaction_descriptor sstable_run_based_compaction_strategy_for_tests::get_sstab
     // Get the most interesting bucket, if any, and return sstables from all its runs.
     for (auto& entry : similar_sized_runs) {
         auto& runs = entry.second;
-        if (runs.size() < size_t(cf.schema()->min_compaction_threshold())) {
+        if (runs.size() < size_t(table_s.schema()->min_compaction_threshold())) {
             continue;
         }
         auto all = boost::accumulate(runs, std::vector<shared_sstable>(), [&] (std::vector<shared_sstable>&& v, const sstable_run& run) {