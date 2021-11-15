b136746040c51f4be26f8c402ccbbcef91b33d4a
Botond Dénes <bdenes@scylladb.com>
2021-11-15 11:15:13 +0200

    mutation_partition: deletable_row::apply(shadowable_tombstone): remove redundant maybe_shadow()
    Shadowing is already checked by the underlying row_tombstone::apply().
    This redundant check was introduced by a previous fix to #9483
    (6a76e127683355e4d621dfd4905231884b36f7be). The rest of that patch is
    good.
    
    Refs: #9483
    Signed-off-by: Botond Dénes <bdenes@scylladb.com>
    Message-Id: <20211115091513.181233-1-bdenes@scylladb.com>

--- mutation_partition.hh
+++ mutation_partition.hh
@@ -843,7 +843,6 @@ public:
 
     void apply(shadowable_tombstone deleted_at) {
         _deleted_at.apply(deleted_at, _marker);
-        maybe_shadow();
     }
 
     void apply(row_tombstone deleted_at) {
