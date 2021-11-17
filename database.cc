cb240ffbae076ba7beea9276469ea354f40a8c89
Benny Halevy <bhalevy@scylladb.com>
2021-11-17 11:24:20 +0200

    keyspace: use erm_factory to create effective_replication_map
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- database.cc
+++ database.cc
@@ -1062,7 +1062,8 @@ keyspace::create_replication_strategy(const locator::shared_token_metadata& stm,
             abstract_replication_strategy::create_replication_strategy(
                 _metadata->strategy_name(), options);
 
-    update_effective_replication_map(co_await calculate_effective_replication_map(_replication_strategy, stm.get()));
+    auto erm = co_await get_erm_factory().create_effective_replication_map(_replication_strategy, stm.get());
+    update_effective_replication_map(std::move(erm));
 }
 
 void
