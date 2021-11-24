e904236cd43ee623709fbe53dfbbe6cd3d3c9687
Gleb Natapov <gleb@scylladb.com>
2021-11-24 11:04:39 +0200

    cql3: move DROP FUNCTION statement to prepare_schema_mutations() api

--- cql3/statements/drop_function_statement.cc
+++ cql3/statements/drop_function_statement.cc
@@ -19,12 +19,14 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <seastar/core/coroutine.hh>
 #include "cql3/statements/drop_function_statement.hh"
 #include "cql3/functions/functions.hh"
 #include "cql3/functions/user_function.hh"
 #include "prepared_statement.hh"
 #include "service/migration_manager.hh"
 #include "cql3/query_processor.hh"
+#include "mutation.hh"
 
 namespace cql3 {
 
@@ -32,6 +34,28 @@ namespace statements {
 
 std::unique_ptr<prepared_statement> drop_function_statement::prepare(database& db, cql_stats& stats) {
     return std::make_unique<prepared_statement>(make_shared<drop_function_statement>(*this));
+}
+
+future<std::pair<::shared_ptr<cql_transport::event::schema_change>, std::vector<mutation>>>
+drop_function_statement::prepare_schema_mutations(query_processor& qp) const {
+    ::shared_ptr<cql_transport::event::schema_change> ret;
+    std::vector<mutation> m;
+
+    auto func = validate_while_executing(qp.proxy());
+
+    if (func) {
+        auto user_func = dynamic_pointer_cast<functions::user_function>(func);
+        if (!user_func) {
+            throw exceptions::invalid_request_exception(format("'{}' is not a user defined function", func));
+        }
+        if (auto aggregate = functions::functions::used_by_user_aggregate(user_func->name()); bool(aggregate)) {
+            throw exceptions::invalid_request_exception(format("Cannot delete function {}, as it is used by user-defined aggregate {}", func, *aggregate));
+        }
+        m = co_await qp.get_migration_manager().prepare_function_drop_announcement(user_func);
+        ret = create_schema_change(*func, false);
+    }
+
+    co_return std::make_pair(std::move(ret), std::move(m));
 }
 
 future<shared_ptr<cql_transport::event::schema_change>> drop_function_statement::announce_migration(
