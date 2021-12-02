67661b6e669e0cf950327edec566bc5b59265f71
Gleb Natapov <gleb@scylladb.com>
2021-12-02 15:15:27 +0200

    cql3: move CREATE TYPE statement to prepare_schema_mutations() api

--- cql3/statements/create_type_statement.hh
+++ cql3/statements/create_type_statement.hh
@@ -69,6 +69,8 @@ public:
     virtual const sstring& keyspace() const override;
 
     virtual future<shared_ptr<cql_transport::event::schema_change>> announce_migration(query_processor& qp) const override;
+    future<std::pair<::shared_ptr<cql_transport::event::schema_change>, std::vector<mutation>>> prepare_schema_mutations(query_processor& qp) const override;
+    virtual bool has_prepare_schema_mutations() const override { return true; }
 
     virtual std::unique_ptr<prepared_statement> prepare(database& db, cql_stats& stats) override;
 
@@ -76,6 +78,7 @@ public:
 
 private:
     bool type_exists_in(::keyspace& ks) const;
+    std::optional<user_type> make_type(query_processor& qp) const;
 
 public:
     user_type create_type(database& db) const;
