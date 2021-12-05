bhalevy/scylla

compaction_manager: perform_sstable_scrub: get the whole compaction_type_options::scrub
So we can pass additional options on top of the scrub mode.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

compaction/compaction_manager.hh
@@ -227,7 +227,7 @@ public:
     future<> perform_sstable_upgrade(database& db, column_family* cf, bool exclude_current_version);
 
     // Submit a column family to be scrubbed and wait for its termination.
-    future<> perform_sstable_scrub(column_family* cf, sstables::compaction_type_options::scrub::mode scrub_mode);
+    future<> perform_sstable_scrub(column_family* cf, sstables::compaction_type_options::scrub opts);
 
     // Submit a column family for major compaction.
     future<> submit_major_compaction(column_family* cf);

scylladb/scylladb

compaction/compaction_manager.hh
@@ -259,7 +259,7 @@ public:
     future<> perform_sstable_upgrade(database& db, table* t, bool exclude_current_version);
 
     // Submit a table to be scrubbed and wait for its termination.
-    future<> perform_sstable_scrub(table* t, sstables::compaction_type_options::scrub::mode scrub_mode);
+    future<> perform_sstable_scrub(table* t, sstables::compaction_type_options::scrub opts);
 
     // Submit a table for major compaction.
     future<> perform_major_compaction(table* t);