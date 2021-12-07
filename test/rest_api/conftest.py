5eb32aa57cc779871d3b421ce3b7b05e9b7b1eb8
Benny Halevy <bhalevy@scylladb.com>
2021-12-07 16:54:58 +0200

    test: rest_api: add storage_service test
    FIXME: negative tests for not-found tables
    should result in a requests.codes.bad_request
    but currently result in requests.codes.internal_server_error.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/rest_api/conftest.py
+++ test/rest_api/conftest.py
@@ -24,6 +24,16 @@
 
 import pytest
 import requests
+import ssl
+import sys
+
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster, ConsistencyLevel, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra.policies import RoundRobinPolicy
+
+# Use the util.py library from ../cql-pytest:
+sys.path.insert(1, sys.path[0] + '/../cql-pytest')
+from util import unique_name
 
 # By default, tests run against a Scylla server listening
 # on localhost:9042 for CQL and localhost:10000 for the REST API.
@@ -63,3 +73,49 @@ def rest_api(request):
     host = request.config.getoption('host')
     port = request.config.getoption('api_port')
     return RestApiSession(host, port)
+
+# "cql" fixture: set up client object for communicating with the CQL API.
+# The host/port combination of the server are determined by the --host and
+# --port options, and defaults to localhost and 9042, respectively.
+# We use scope="session" so that all tests will reuse the same client object.
+@pytest.fixture(scope="session")
+def cql(request):
+    profile = ExecutionProfile(
+        load_balancing_policy=RoundRobinPolicy(),
+        consistency_level=ConsistencyLevel.LOCAL_QUORUM,
+        serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
+        # The default timeout (in seconds) for execute() commands is 10, which
+        # should have been more than enough, but in some extreme cases with a
+        # very slow debug build running on a very busy machine and a very slow
+        # request (e.g., a DROP KEYSPACE needing to drop multiple tables)
+        # 10 seconds may not be enough, so let's increase it. See issue #7838.
+        request_timeout = 120)
+    if request.config.getoption('ssl'):
+        # Scylla does not support any earlier TLS protocol. If you try,
+        # you will get mysterious EOF errors (see issue #6971) :-(
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    else:
+        ssl_context = None
+    cluster = Cluster(execution_profiles={EXEC_PROFILE_DEFAULT: profile},
+        contact_points=[request.config.getoption('host')],
+        port=request.config.getoption('port'),
+        # TODO: make the protocol version an option, to allow testing with
+        # different versions. If we drop this setting completely, it will
+        # mean pick the latest version supported by the client and the server.
+        protocol_version=4,
+        # Use the default superuser credentials, which work for both Scylla and Cassandra
+        auth_provider=PlainTextAuthProvider(username='cassandra', password='cassandra'),
+        ssl_context=ssl_context,
+    )
+    return cluster.connect()
+
+# Until Cassandra 4, NetworkTopologyStrategy did not support the option
+# replication_factor (https://issues.apache.org/jira/browse/CASSANDRA-14303).
+# We want to allow these tests to run on Cassandra 3.* (for the convenience
+# of developers who happen to have it installed), so we'll use the older
+# syntax that needs to specify a DC name explicitly. For this, will have
+# a "this_dc" fixture to figure out the name of the current DC, so it can be
+# used in NetworkTopologyStrategy.
+@pytest.fixture(scope="session")
+def this_dc(cql):
+    yield cql.execute("SELECT data_center FROM system.local").one()[0]
