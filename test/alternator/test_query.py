fdf3d18b41d21bac17e7c33297a4a3adb3b51cbd
Nadav Har'El <nyh@scylladb.com>
2021-12-15 17:45:08 +0200

    alternator: doc, test: fix mentions of reverse queries
    Now that issues #7586 and #9487 were fixed, reverse queries - even in
    long partitions - work well, we can drop the claim in
    alternator/docs/compatibility.md that reverse queries are buggy for
    large partitions.
    
    We can also remove the "xfail" mark from the tes that checks this
    feature, as it now passes.
    
    Refs #7586
    Refs #9487
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- test/alternator/test_query.py
+++ test/alternator/test_query.py
@@ -437,7 +437,6 @@ def test_query_reverse_paging(test_table_sn):
 # several seconds), so we mark it with "veryslow" - so it's not run unless
 # the "--runveryslow" option is passed to pytest.
 @pytest.mark.veryslow
-@pytest.mark.xfail(reason="issue #7586")
 def test_query_reverse_long(test_table_sn):
     # Insert many big strings into one partition sized over 100MB:
     p = random_string()
