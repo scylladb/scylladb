adf7138b3bca2e3ad52ab9bb80328547005059e1
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2021-12-24 11:34:53 +0300

    utils: atomic_vector: introduce future-returning `for_each` function
    Introduce a variant of `for_each` function not requiring
    `seastar::async` context.
    
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- utils/atomic_vector.hh
+++ utils/atomic_vector.hh
@@ -24,6 +24,7 @@
 #include <seastar/core/rwlock.hh>
 #include <seastar/util/defer.hh>
 #include <seastar/util/noncopyable_function.hh>
+#include <seastar/core/coroutine.hh>
 
 #include <vector>
 
@@ -60,6 +61,21 @@ public:
         // reallocated.
         for (size_t i = 0, n = _vec.size(); i < n; ++i) {
             func(_vec[i]);
+        }
+    }
+
+    // The callback function must not call remove.
+    //
+    // We would take callbacks that take a T&, but we had bugs in the
+    // past with some of those callbacks holding that reference past a
+    // preemption.
+    seastar::future<> for_each(seastar::noncopyable_function<seastar::future<>(T)> func) {
+        auto holder = co_await _vec_lock.hold_read_lock();
+        // We grab a lock in remove(), but not in add(), so we
+        // iterate using indexes to guard against the vector being
+        // reallocated.
+        for (size_t i = 0, n = _vec.size(); i < n; ++i) {
+            co_await func(_vec[i]);
         }
     }
 };
