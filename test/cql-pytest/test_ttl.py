5fbeae90161de783104c4ecd65216849ed920ed1
Nadav Har'El <nyh@scylladb.com>
2021-12-27 11:15:02 +0200

    cql-pytest: add a couple of default-TTL tests
    This patch adds a new cql-pytest test file - test_ttl.py - with
    currently just a couple of tests for the "with default_time_to_live"
    feature. One is a basic test, and second reproduces issue #9842 -
    that "using ttl 0" should override the default time to live, but
    doesn't.
    
    The test for #9842, test_default_ttl_0_override, fails on Scylla and
    passes on Cassandra, and is marked "xfail".
    
    Refs #9842.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <20211227091502.553577-1-nyh@scylladb.com>

---
+++ test/cql-pytest/test_ttl.py
@@ -0,0 +1,68 @@
+# Copyright 2021-present ScyllaDB
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+
+#############################################################################
+# Various tests for Scylla's ttl feature - USING TTL and DEFAULT_TIME_TO_LIVE
+#############################################################################
+
+from util import new_test_table
+import pytest
+import random
+import time
+
+# Fixture with a table with a default TTL set to 1, so new data would be
+# inserted by default with TTL of 1.
+@pytest.fixture(scope="module")
+def table_ttl_1(cql, test_keyspace):
+    with new_test_table(cql, test_keyspace, 'p int primary key, v int', 'with default_time_to_live = 1') as table:
+        yield table
+
+@pytest.fixture(scope="module")
+def table_ttl_100(cql, test_keyspace):
+    with new_test_table(cql, test_keyspace, 'p int primary key, v int', 'with default_time_to_live = 100') as table:
+        yield table
+
+
+# Basic test that data inserted *without* an explicit ttl into a table with
+# default TTL inherits this TTL, but an explicit TTL overrides it.
+def test_basic_default_ttl(cql, table_ttl_1):
+    p1 = random.randint(1,1000000000)
+    p2 = random.randint(1,1000000000)
+    cql.execute(f'INSERT INTO {table_ttl_1} (p, v) VALUES ({p1}, 1) USING TTL 1000')
+    cql.execute(f'INSERT INTO {table_ttl_1} (p, v) VALUES ({p2}, 1)')
+    # p2 item should expire in *less* than one second (it will expire
+    # in the next whole second), 
+    start = time.time()
+    while len(list(cql.execute(f'SELECT * from {table_ttl_1} where p={p2}'))):
+        assert time.time() < start + 2
+        time.sleep(0.1)
+    # p1 should not have expired yet. By the way, its current ttl(v) would
+    # normally be exactly 999 now, but theoretically could be a bit lower in
+    # case of delays in the test.
+    assert len(list(cql.execute(f'SELECT *from {table_ttl_1} where p={p1}'))) == 1
+
+# Above we tested that explicitly setting "using ttl" overrides the default
+# ttl set for the table. Here we check that also in the special case of
+# "using ttl 0" (which means the item should never expire) it also overrides
+# the default TTL. Reproduces issue #9842.
+@pytest.mark.xfail(reason="Issue #9842")
+def test_default_ttl_0_override(cql, table_ttl_100):
+    p = random.randint(1,1000000000)
+    cql.execute(f'INSERT INTO {table_ttl_100} (p, v) VALUES ({p}, 1) USING TTL 0')
+    # We can immediately check that this item's TTL is "null", meaning it
+    # will never expire. There's no need to have any sleeps.
+    assert list(cql.execute(f'SELECT ttl(v) from {table_ttl_100} where p={p}')) == [(None,)]
