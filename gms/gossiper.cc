49a603af395d831218d389f4d59e6d9052dafb63
Avi Kivity <avi@scylladb.com>
2021-12-28 21:13:59 +0200

    gossip: fix lowres_clock::duration assumption
    The variable diff is assigned a type of std::chrono::milliseconds
    but later used to store the difference between two
    lowres_clock::time_point samples. This works now because the two
    types are the same, but fails if lowres_clock::duration changes.
    
    Remove the assumption by using lowres_clock::duration.

--- gms/gossiper.cc
+++ gms/gossiper.cc
@@ -763,7 +763,7 @@ future<> gossiper::update_live_endpoints_version() {
 
 future<> gossiper::failure_detector_loop_for_node(gms::inet_address node, int64_t gossip_generation, uint64_t live_endpoints_version) {
     auto last = gossiper::clk::now();
-    auto diff = std::chrono::milliseconds(0);
+    auto diff = gossiper::clk::duration(0);
     auto echo_interval = std::chrono::milliseconds(2000);
     auto max_duration = echo_interval + std::chrono::milliseconds(_cfg.failure_detector_timeout_in_ms());
     while (is_enabled()) {
