41dec2dd506d1c0b738ec5da2a6def2c64ec043c
Botond Dénes <bdenes@scylladb.com>
2022-01-07 08:33:47 +0200

    tools/utils: remove now unused get_selected_operation() overload

--- tools/utils.hh
+++ tools/utils.hh
@@ -68,27 +68,6 @@ const Op& get_selected_operation(int& ac, char**& av, const std::vector<Op>& ope
     exit(1);
 }
 
-template <Operation Op>
-const Op& get_selected_operation(boost::program_options::variables_map& app_config, const std::vector<Op>& operations, std::string_view alias) {
-    std::vector<const Op*> found_operations;
-    for (const auto& op : operations) {
-        if (app_config.contains(op.name())) {
-            found_operations.push_back(&op);
-        }
-    }
-    if (found_operations.size() == 1) {
-        return *found_operations.front();
-    }
-
-    const auto all_operation_names = boost::algorithm::join(operations | boost::adaptors::transformed(
-            [] (const Op& op) { return format("--{}", op.name()); } ), ", ");
-
-    if (found_operations.empty()) {
-        throw std::invalid_argument(fmt::format("error: missing {}, exactly one of {} should be specified", alias, all_operation_names));
-    }
-    throw std::invalid_argument(fmt::format("error: need exactly one {}, cannot specify more then one of {}", alias, all_operation_names));
-}
-
 // Configure seastar with defaults more appropriate for a tool.
 // Make seastar not act as if it owns the place, taking over all system resources.
 // Set ERROR as the default log level, except for the logger \p logger_name, which
