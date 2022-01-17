d7a993043de250f39c3881017b83fbb1e789e6fb
Michael Livshin <michael.livshin@scylladb.com>
2022-01-17 14:04:05 +0200

    shard_reader: check that _reader is valid before dereferencing
    After fc729a804, `shard_reader::close()` is not interrupted with an
    exception any more if read-ahead fails, so `_reader` may in fact be
    null.
    
    Fixes #9923
    
    Signed-off-by: Michael Livshin <michael.livshin@scylladb.com>
    Message-Id: <20220117120405.152927-1-michael.livshin@scylladb.com>

--- mutation_reader.cc
+++ mutation_reader.cc
@@ -2091,12 +2091,6 @@ public:
 };
 
 future<> shard_reader::close() noexcept {
-    // Nothing to do if there was no reader created, nor is there a background
-    // read ahead in progress which will create one.
-    if (!_reader && !_read_ahead) {
-        co_return;
-    }
-
     if (_read_ahead) {
         try {
             co_await *std::exchange(_read_ahead, std::nullopt);
@@ -2107,6 +2101,10 @@ future<> shard_reader::close() noexcept {
 
     try {
         co_await smp::submit_to(_shard, [this] {
+            if (!_reader) {
+                return make_ready_future<>();
+            }
+
             auto irh = std::move(*_reader).inactive_read_handle();
             return with_closeable(flat_mutation_reader(_reader.release()), [this] (flat_mutation_reader& reader) mutable {
                 auto permit = reader.permit();
@@ -2326,12 +2324,6 @@ public:
 };
 
 future<> shard_reader_v2::close() noexcept {
-    // Nothing to do if there was no reader created, nor is there a background
-    // read ahead in progress which will create one.
-    if (!_reader && !_read_ahead) {
-        co_return;
-    }
-
     if (_read_ahead) {
         try {
             co_await *std::exchange(_read_ahead, std::nullopt);
@@ -2342,6 +2334,10 @@ future<> shard_reader_v2::close() noexcept {
 
     try {
         co_await smp::submit_to(_shard, [this] {
+            if (!_reader) {
+                return make_ready_future<>();
+            }
+
             auto irh = std::move(*_reader).inactive_read_handle();
             return with_closeable(flat_mutation_reader_v2(_reader.release()), [this] (flat_mutation_reader_v2& reader) mutable {
                 auto permit = reader.permit();
