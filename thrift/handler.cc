scylladb/scylladb

commit d9c315891a55694b699698807632ab93ba02e66b

thrift: move system_update_keyspace() to raft

thrift/handler.cc
@@ -978,16 +978,23 @@ public:
 
             co_await t._query_state.get_client_state().has_keyspace_access(t._db.local(), ks_def.name, auth::permission::ALTER);
 
-            if (!t._db.local().has_keyspace(ks_def.name)) {
-                throw NotFoundException();
-            }
-            if (!ks_def.cf_defs.empty()) {
-                throw make_exception<InvalidRequestException>("Keyspace update must not contain any column family definitions.");
-            }
+            auto func = [&] (replica::database& db) -> future<std::string> {
+                auto& mm = t._query_processor.local().get_migration_manager();
 
-            auto ksm = keyspace_from_thrift(ks_def);
-            co_await t._query_processor.local().get_migration_manager().announce_keyspace_update(std::move(ksm));
-            co_return std::string(t._db.local().get_version().to_sstring());
+                co_await mm.schema_read_barrier();
+
+                if (db.has_keyspace(ks_def.name)) {
+                    throw NotFoundException();
+                }
+                if (!ks_def.cf_defs.empty()) {
+                    throw make_exception<InvalidRequestException>("Keyspace update must not contain any column family definitions.");
+                }
+
+                auto ksm = keyspace_from_thrift(ks_def);
+                co_await mm.announce(mm.prepare_keyspace_update_announcement(std::move(ksm)));
+                co_return std::string(db.get_version().to_sstring());
+            };
+            co_return co_await t._db.invoke_on(0, func);
         });
     }
 

commit d65427ad8128e00e76664908295233f4cfe7f23f

thrift: correctly check for keyspace existence
d9c315891a55694b699698807632ab93ba02e66b broke the check for keyspace
existence. The condition is opposite. Fix it.

Fixes #9927

Message-Id: <YeUhtESDHQeMHiUW@scylladb.com>

thrift/handler.cc
@@ -971,7 +971,7 @@ public:
             co_await t._query_state.get_client_state().has_keyspace_access(t._db, ks_def.name, auth::permission::ALTER);
 
             co_return co_await t.execute_schema_command([&ks_def] (service::migration_manager& mm, data_dictionary::database db) -> future<std::vector<mutation>> {
-                if (db.has_keyspace(ks_def.name)) {
+                if (!db.has_keyspace(ks_def.name)) {
                     throw NotFoundException();
                 }
                 if (!ks_def.cf_defs.empty()) {