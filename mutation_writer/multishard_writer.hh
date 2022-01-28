b2061688a582c3ba1b87782ba0cf01a876b046aa
Botond Dénes <bdenes@scylladb.com>
2022-01-28 13:14:25 +0200

    mutation_writer/multishard_writer: remove now unused v1 factory overloads

--- mutation_writer/multishard_writer.hh
+++ mutation_writer/multishard_writer.hh
@@ -15,14 +15,6 @@
 
 namespace mutation_writer {
 
-// Helper to use multishard_writer to distribute mutation_fragments from the
-// producer to the correct shard and consume with the consumer.
-// It returns number of partitions consumed.
-future<uint64_t> distribute_reader_and_consume_on_shards(schema_ptr s,
-    flat_mutation_reader producer,
-    std::function<future<> (flat_mutation_reader)> consumer,
-    utils::phased_barrier::operation&& op = {});
-
 future<uint64_t> distribute_reader_and_consume_on_shards(schema_ptr s,
     flat_mutation_reader_v2 producer,
     std::function<future<> (flat_mutation_reader_v2)> consumer,
