8d52ceca5070f54a217985d5064927826d7d0af0
Piotr Dulikowski <piodul@scylladb.com>
2022-02-04 20:17:11 +0100

    utils: add result_try and result_futurize_try
    Adds result_try and result_futurize_try - functions which allow to
    convert existing try..catch blocks into a version which handles C++
    exceptions, failed results with exception containers and, depending on
    the function variant, exceptional futures.

--- test/boost/result_utils_test.cc
+++ test/boost/result_utils_test.cc
@@ -10,10 +10,12 @@
 #include <stdexcept>
 #include "utils/exception_container.hh"
 #include "utils/result.hh"
+#include "utils/result_try.hh"
 
 #include <seastar/testing/test_case.hh>
 #include <seastar/core/sstring.hh>
 #include <seastar/core/map_reduce.hh>
+#include <seastar/util/later.hh>
 #include <seastar/testing/thread_test_case.hh>
 
 using namespace seastar;
@@ -124,4 +126,234 @@ SEASTAR_THREAD_TEST_CASE(test_result_parallel_for_each) {
     BOOST_REQUIRE_THROW(reduce(bo::success(), foo_exc()), foo_exception);
     BOOST_REQUIRE_THROW(reduce(foo_exc(), bar_exc()), foo_exception);
     BOOST_REQUIRE_THROW(reduce(bar_exc(), foo_exc()), bar_exception);
+}
+
+namespace test_policies {
+
+struct result_try {
+    template<typename... Args>
+    static auto do_try(Args&&... args) {
+        return utils::result_try(std::forward<Args>(args)...);
+    }
+
+    static result<sstring> value(sstring value)   { return bo::success(std::move(value)); }
+    static result<sstring> error(auto ex)         { return bo::failure(std::move(ex)); }
+    static result<sstring> exception(auto ex)     { throw ex; }
+    static result<sstring> rethrow(auto&& handle) { return handle.into_result(); }
+};
+
+struct result_futurize_try_exceptional_futures {
+    template<typename... Args>
+    static auto do_try(Args&&... args) {
+        return utils::result_futurize_try(std::forward<Args>(args)...).get();
+    }
+
+    static future<result<sstring>> value(sstring value)   { return make_ready_future<result<sstring>>(bo::success(std::move(value))); }
+    static future<result<sstring>> error(auto ex)         { return make_ready_future<result<sstring>>(bo::failure(std::move(ex))); }
+    static future<result<sstring>> exception(auto ex)     { return make_exception_future<result<sstring>>(std::move(ex)); }
+    static future<result<sstring>> rethrow(auto&& handle) { return handle.into_future(); }
+};
+
+struct result_futurize_try_exceptions {
+    template<typename... Args>
+    static auto do_try(Args&&... args) {
+        return utils::result_futurize_try(std::forward<Args>(args)...).get();
+    }
+
+    static future<result<sstring>> value(sstring value)   { return make_ready_future<result<sstring>>(bo::success(std::move(value))); }
+    static future<result<sstring>> error(auto ex)         { return make_ready_future<result<sstring>>(bo::failure(std::move(ex))); }
+    static future<result<sstring>> exception(auto ex)     { throw ex; }
+    static future<result<sstring>> rethrow(auto&& handle) { return make_ready_future<result<sstring>>(handle.into_result()); }
+};
+
+template<typename Base>
+struct with_delay : public Base {
+    using base = Base;
+
+private:
+    static future<result<sstring>> with_yield(future<result<sstring>> f) {
+        return seastar::yield().then([f = std::move(f)] () mutable {
+            return std::move(f);
+        });
+    }
+
+public:
+    static future<result<sstring>> value(sstring value)   { return with_yield(base::value(std::move(value))); }
+    static future<result<sstring>> error(auto ex)         { return with_yield(base::error(std::move(ex))); }
+    static future<result<sstring>> exception(auto ex)     { return with_yield(base::exception(std::move(ex))); }
+    static future<result<sstring>> rethrow(auto&& handle) { return with_yield(base::rethrow(std::move(handle))); }
+};
+
+}
+
+template<typename TestTemplate>
+auto test_result_try_with_policies(TestTemplate&& template_fn) {
+    BOOST_TEST_MESSAGE("Running test for result_try");
+    template_fn(test_policies::result_try());
+
+    BOOST_TEST_MESSAGE("Running test for result_futurize_try (exceptions as futures)");
+    template_fn(test_policies::result_futurize_try_exceptional_futures());
+
+    BOOST_TEST_MESSAGE("Running test for result_futurize_try (exceptions as futures, yielded futures)");
+    template_fn(test_policies::with_delay<test_policies::result_futurize_try_exceptional_futures>());
+
+    BOOST_TEST_MESSAGE("Running test for result_futurize_try (thrown exceptions)");
+    template_fn(test_policies::result_futurize_try_exceptions());
+
+    BOOST_TEST_MESSAGE("Running test for result_futurize_try (thrown exceptions, yielded futures)");
+    template_fn(test_policies::with_delay<test_policies::result_futurize_try_exceptions>());
+}
+
+SEASTAR_THREAD_TEST_CASE(test_result_try_success_on_exception) {
+    test_result_try_with_policies([] (auto policy) {
+        auto handle = [&] (auto f) {
+            return policy.do_try(
+                std::move(f),
+                utils::result_catch<foo_exception>([&] (const auto& ex) {
+                    return policy.value("foo");
+                }),
+                utils::result_catch<bar_exception>([&] (const auto& ex) {
+                    return policy.value("bar");
+                })
+            );
+        };
+
+        BOOST_CHECK_EQUAL(handle([&] { return policy.value("success"); }).value(), "success");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(bar_exception()); }).value(), "bar");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(bar_exception()); }).value(), "bar");
+    });
+}
+
+SEASTAR_THREAD_TEST_CASE(test_result_try_rethrow_exception) {
+    test_result_try_with_policies([] (auto policy) {
+        bool was_handled = false;
+
+        auto handle = [&] (auto f) {
+            was_handled = false;
+            return policy.do_try(
+                std::move(f),
+                utils::result_catch<foo_exception>([&] (const auto& ex, auto&& handle) {
+                    was_handled = true;
+                    return policy.rethrow(std::move(handle));
+                }),
+                utils::result_catch<bar_exception>([&] (const auto& ex, auto&& handle) {
+                    was_handled = true;
+                    return policy.rethrow(std::move(handle));
+                })
+            );
+        };
+
+        BOOST_CHECK_EQUAL(handle([&] { return policy.value("success"); }).value(), "success");
+        BOOST_CHECK(!was_handled);
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(foo_exception()); }).error(), exc_container(foo_exception()));
+        BOOST_CHECK(was_handled);
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(bar_exception()); }).error(), exc_container(bar_exception()));
+        BOOST_CHECK(was_handled);
+        BOOST_CHECK_THROW((void)handle([&] { return policy.exception(foo_exception()); }), foo_exception);
+        BOOST_CHECK(was_handled);
+        BOOST_CHECK_THROW((void)handle([&] { return policy.exception(bar_exception()); }), bar_exception);
+        BOOST_CHECK(was_handled);
+    });
+}
+
+SEASTAR_THREAD_TEST_CASE(test_result_try_handler_precedence) {
+    test_result_try_with_policies([] (auto policy) {
+        auto handle = [&] (auto f) {
+            return policy.do_try(
+                std::move(f),
+                utils::result_catch<foo_exception>([&] (const auto& ex) {
+                    return policy.value("foo");
+                }),
+                utils::result_catch<std::exception>([&] (const auto& ex) {
+                    return policy.value("std::exception");
+                })
+            );
+        };
+
+        BOOST_CHECK_EQUAL(handle([&] { return policy.value("success"); }).value(), "success");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(bar_exception()); }).value(), "std::exception");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(bar_exception()); }).value(), "std::exception");
+    });
+}
+
+SEASTAR_THREAD_TEST_CASE(test_result_try_unhandled_exception) {
+    test_result_try_with_policies([] (auto policy) {
+        auto handle = [&] (auto f) {
+            return policy.do_try(
+                std::move(f),
+                utils::result_catch<foo_exception>([&] (const auto& ex) {
+                    return policy.value("foo");
+                })
+            );
+        };
+
+        BOOST_CHECK_EQUAL(handle([&] { return policy.value("success"); }).value(), "success");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(bar_exception()); }).error(), exc_container(bar_exception()));
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_THROW((void)handle([&] { return policy.exception(bar_exception()); }), bar_exception);
+    });
+}
+
+SEASTAR_THREAD_TEST_CASE(test_result_try_catch_dots) {
+    test_result_try_with_policies([] (auto policy) {
+        auto handle = [&] (auto f) {
+            return policy.do_try(
+                std::move(f),
+                utils::result_catch<foo_exception>([&] (const auto& ex) {
+                    return policy.value("foo");
+                }),
+                utils::result_catch_dots([&] () {
+                    return policy.value("...");
+                })
+            );
+        };
+
+        BOOST_CHECK_EQUAL(handle([&] { return policy.value("success"); }).value(), "success");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(bar_exception()); }).value(), "...");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(foo_exception()); }).value(), "foo");
+        BOOST_CHECK_EQUAL(handle([&] { return policy.exception(bar_exception()); }).value(), "...");
+    });
+}
+
+SEASTAR_THREAD_TEST_CASE(test_result_try_catch_forward_to_promise) {
+    test_result_try_with_policies([] (auto policy) {
+        auto handle = [&] (auto f) -> result<int> {
+            // Handle error/failed result in catch handler and forward it to
+            // promise of a different type
+            promise<result<int>> prom;
+            bool promise_set = false;
+            try {
+                policy.do_try(
+                    std::move(f),
+                    utils::result_catch_dots([&] (auto&& result) {
+                        result.forward_to_promise(prom);
+                        promise_set = true;
+                        return policy.value("should not see that");
+                    })
+                ).value();
+            } catch (...) {
+                BOOST_CHECK_MESSAGE(false, "Exception or failed result was not forwarded to promise");
+                throw;
+            }
+
+            // If not set by a handler, set the promise manually
+            if (!promise_set) {
+                prom.set_value(bo::success(123));
+            }
+
+            return prom.get_future().get();
+        };
+
+        BOOST_CHECK_EQUAL(handle([&] { return policy.value("success"); }).value(), 123);
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(foo_exception()); }).error(), exc_container(foo_exception()));
+        BOOST_CHECK_EQUAL(handle([&] { return policy.error(bar_exception()); }).error(), exc_container(bar_exception()));
+        BOOST_CHECK_THROW((void)handle([&] { return policy.exception(foo_exception()); }), foo_exception);
+        BOOST_CHECK_THROW((void)handle([&] { return policy.exception(bar_exception()); }), bar_exception);
+    });
 }
