d8cc5352979ea482ac992e9d4580d8ba79eda1c0
Michael Livshin <michael.livshin@scylladb.com>
2022-02-10 22:23:18 +0200

    database_test: ensure host id continuity
    The "populate_from_quarantine_works" test case creates sstables with
    one db config, then reads them with another.  Ensure that both configs
    have the same host id so the sstables pass validation.
    
    Signed-off-by: Michael Livshin <michael.livshin@scylladb.com>

--- test/boost/database_test.cc
+++ test/boost/database_test.cc
@@ -928,10 +928,12 @@ SEASTAR_TEST_CASE(upgrade_sstables) {
 
 SEASTAR_TEST_CASE(populate_from_quarantine_works) {
     auto tmpdir_for_data = make_lw_shared<tmpdir>();
+    utils::UUID host_id;
 
     // populate tmpdir_for_data and
     // move a random sstable to quarantine
-    co_await do_with_some_data({"cf"}, [] (cql_test_env& e) -> future<> {
+    co_await do_with_some_data({"cf"}, [&host_id] (cql_test_env& e) -> future<> {
+        host_id = e.local_db().get_config().host_id;
         auto& db = e.db();
         co_await db.invoke_on_all([] (replica::database& db) {
             auto& cf = db.find_column_family("ks", "cf");
@@ -962,6 +964,7 @@ SEASTAR_TEST_CASE(populate_from_quarantine_works) {
     // verify that all rows are still there
     auto db_cfg_ptr = make_shared<db::config>();
     db_cfg_ptr->data_file_directories(std::vector<sstring>({ tmpdir_for_data->path().string() }));
+    db_cfg_ptr->host_id = host_id;
     size_t row_count = 0;
     co_await do_with_cql_env([&row_count] (cql_test_env& e) -> future<> {
         auto res = co_await e.execute_cql("select * from ks.cf;");
