841b982e51fc4d93c77d83670d1e9491fa231016
Botond Dénes <bdenes@scylladb.com>
2022-02-16 09:02:04 +0200

    test/lib/test_utils: return OK from check() variants
    The various require() and check() methods in test_utils.hh were
    introduced to replace BOOST_REQUIRE() and BOOST_CHECK() respectively in
    multi-shard concurrent tests, specifically those in
    tests/boost/multishard_mutation_query_test.cc.
    This was done literally, just replacing BOOST_REQUIRE() with require()
    and BOOST_CHECK() with check(). The problem is that check() is missing a
    feature BOOST_CHECK() had: while BOOST_CHECK() doesn't cause an
    immediate test failure, just logging an error if the condition fails, it
    remembers this failure and will fail the test in the end. check() did
    not have this feature and this caused potential errors to just be logged
    while the test could still pass fine, causing false-positive tests
    passes. This patch fixes this by returning a [[nodiscard]] bool from the
    check() methods. The caller can & these together over all calls to
    check() methods and manually fail the test in the end. We choose this
    method over a hidden global (like BOOST_CHECK() does) for simplicity
    sake.

--- test/lib/test_utils.hh
+++ test/lib/test_utils.hh
@@ -20,16 +20,16 @@
 
 namespace tests {
 
-void do_check(bool condition, std::experimental::source_location sl, std::string_view msg);
+[[nodiscard]] bool do_check(bool condition, std::experimental::source_location sl, std::string_view msg);
 
-inline void check(bool condition, std::experimental::source_location sl = std::experimental::source_location::current()) {
-    do_check(condition, sl, {});
+[[nodiscard]] inline bool check(bool condition, std::experimental::source_location sl = std::experimental::source_location::current()) {
+    return do_check(condition, sl, {});
 }
 
 template <typename LHS, typename RHS>
-void check_equal(const LHS& lhs, const RHS& rhs, std::experimental::source_location sl = std::experimental::source_location::current()) {
+[[nodiscard]] bool check_equal(const LHS& lhs, const RHS& rhs, std::experimental::source_location sl = std::experimental::source_location::current()) {
     const auto condition = (lhs == rhs);
-    do_check(condition, sl, fmt::format("{} {}= {}", lhs, condition ? "=" : "!", rhs));
+    return do_check(condition, sl, fmt::format("{} {}= {}", lhs, condition ? "=" : "!", rhs));
 }
 
 void do_require(bool condition, std::experimental::source_location sl, std::string_view msg);
