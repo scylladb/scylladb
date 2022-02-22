2afacf96096e5e7f1712a7351b115aec1d22f2f6
Botond Dénes <bdenes@scylladb.com>
2022-02-22 09:19:25 +0200

    mutation_reader: drop now unused v1 multishard_combining_reader and friends
    Friends: shard_reader and evictable_reader. All these have been
    supplanted by their respective v2 variants.
    
    Tests: unit(dev)
    
    Signed-off-by: Botond Dénes <bdenes@scylladb.com>
    Message-Id: <20220222071925.223718-1-bdenes@scylladb.com>

--- mutation_reader.hh
+++ mutation_reader.hh
@@ -375,64 +375,6 @@ flat_mutation_reader make_foreign_reader(schema_ptr schema,
 /// transparent to its user.
 /// Parameters passed by reference have to be kept alive while the reader is
 /// alive.
-flat_mutation_reader make_auto_paused_evictable_reader(
-        mutation_source ms,
-        schema_ptr schema,
-        reader_permit permit,
-        const dht::partition_range& pr,
-        const query::partition_slice& ps,
-        const io_priority_class& pc,
-        tracing::trace_state_ptr trace_state,
-        mutation_reader::forwarding fwd_mr);
-
-class evictable_reader;
-
-class evictable_reader_handle {
-    friend std::pair<flat_mutation_reader, evictable_reader_handle> make_manually_paused_evictable_reader(mutation_source, schema_ptr, reader_permit,
-            const dht::partition_range&, const query::partition_slice&, const io_priority_class&, tracing::trace_state_ptr, mutation_reader::forwarding);
-
-private:
-    evictable_reader* _r;
-
-private:
-    explicit evictable_reader_handle(evictable_reader& r);
-
-public:
-    void pause();
-};
-
-/// Make a manually-paused evictable reader.
-///
-/// The reader can be paused via the evictable reader handle when desired. The
-/// intended usage is subsequent reads done in bursts, after which the reader is
-/// not used for some time. When paused, the reader is made evictable, that is,
-/// it is registered with reader concurrency semaphore as an inactive read.
-/// The reader is resumed automatically on the next use. If it was evicted, it
-/// will be recreated at the position it left off reading. This is all
-/// transparent to its user.
-/// Parameters passed by reference have to be kept alive while the reader is
-/// alive.
-std::pair<flat_mutation_reader, evictable_reader_handle> make_manually_paused_evictable_reader(
-        mutation_source ms,
-        schema_ptr schema,
-        reader_permit permit,
-        const dht::partition_range& pr,
-        const query::partition_slice& ps,
-        const io_priority_class& pc,
-        tracing::trace_state_ptr trace_state,
-        mutation_reader::forwarding fwd_mr);
-
-/// Make an auto-paused evictable reader.
-///
-/// The reader is paused after each use, that is after each call to any of its
-/// members that cause actual reading to be done (`fill_buffer()` and
-/// `fast_forward_to()`). When paused, the reader is made evictable, that it is
-/// it is registered with reader concurrency semaphore as an inactive read.
-/// The reader is resumed automatically on the next use. If it was evicted, it
-/// will be recreated at the position it left off reading. This is all
-/// transparent to its user.
-/// Parameters passed by reference have to be kept alive while the reader is
-/// alive.
 flat_mutation_reader_v2 make_auto_paused_evictable_reader_v2(
         mutation_source ms,
         schema_ptr schema,
@@ -479,88 +421,6 @@ std::pair<flat_mutation_reader_v2, evictable_reader_handle_v2> make_manually_pau
         const io_priority_class& pc,
         tracing::trace_state_ptr trace_state,
         mutation_reader::forwarding fwd_mr);
-
-/// Reader lifecycle policy for the mulitshard combining reader.
-///
-/// This policy is expected to make sure any additional resource the readers
-/// might need is kept alive for the lifetime of the readers, not that
-/// of the multishard reader. This is a very important distinction. As
-/// destructors cannot return futures, the multishard reader will be
-/// destroyed before all it's shard readers could stop properly. Hence it
-/// is the duty of this policy to make sure all objects the shard readers
-/// depend on stay alive until they are properly destroyed on their home
-/// shards. Note that this also includes the passed in `range` and `slice`
-/// parameters because although client code is required to keep them alive as
-/// long as the top level reader lives, the shard readers might outlive the
-/// multishard reader itself.
-class reader_lifecycle_policy {
-public:
-    struct stopped_reader {
-        reader_concurrency_semaphore::inactive_read_handle handle;
-        flat_mutation_reader::tracked_buffer unconsumed_fragments;
-    };
-
-public:
-    /// Create an appropriate reader on the shard it is called on.
-    ///
-    /// Will be called when the multishard reader visits a shard for the
-    /// first time or when a reader has to be recreated after having been
-    /// evicted (while paused). This method should also enter gates, take locks
-    /// or whatever is appropriate to make sure resources it is using on the
-    /// remote shard stay alive, during the lifetime of the created reader.
-    ///
-    /// The \c permit parameter shall be obtained via `obtain_reader_permit()`
-    virtual flat_mutation_reader create_reader(
-            schema_ptr schema,
-            reader_permit permit,
-            const dht::partition_range& range,
-            const query::partition_slice& slice,
-            const io_priority_class& pc,
-            tracing::trace_state_ptr trace_state,
-            mutation_reader::forwarding fwd_mr) = 0;
-
-    /// Updates the read-range of the shard reader.
-    ///
-    /// Gives the lifecycle-policy a chance to update its stored read-range (if
-    /// the case). Called after any modification to the read range (typically
-    /// after fast_forward_to()). The range is identical to the one the reader
-    /// holds a reference to after the modification happened. When this method
-    /// is called, it is safe to destroy the previous range instance.
-    ///
-    /// This method has to be called on the shard the reader lives on.
-    virtual void update_read_range(lw_shared_ptr<const dht::partition_range> pr) = 0;
-
-    /// Destroy the shard reader.
-    ///
-    /// Will be called when the multishard reader is being destroyed. It will be
-    /// called for each of the shard readers.
-    /// This method is expected to do a proper cleanup, that is, leave any gates,
-    /// release any locks or whatever is appropriate for the shard reader.
-    ///
-    /// This method has to be called on the shard the reader lives on.
-    /// This method will be called from a destructor so it cannot throw.
-    virtual future<> destroy_reader(stopped_reader reader) noexcept = 0;
-
-    /// Get the relevant semaphore for this read.
-    ///
-    /// The semaphore is used to register paused readers with as inactive
-    /// readers. The semaphore then can evict these readers when resources are
-    /// in-demand.
-    /// The multishard reader will pause and resume readers via the `pause()`
-    /// and `try_resume()` helper methods. Clients can resume any paused readers
-    /// after the multishard reader is destroyed via the same helper methods.
-    ///
-    /// This method will be called on the shard where the relevant reader lives.
-    virtual reader_concurrency_semaphore& semaphore() = 0;
-
-    /// Obtain an admitted permit.
-    ///
-    /// The permit will be associated with the semaphore returned by
-    /// `semaphore()`.
-    ///
-    /// This method will be called on the shard where the relevant reader lives.
-    virtual future<reader_permit> obtain_reader_permit(schema_ptr schema, const char* const description, db::timeout_clock::time_point timeout) = 0;
-};
 
 /// Reader lifecycle policy for the mulitshard combining reader.
 ///
@@ -643,45 +503,6 @@ public:
     /// This method will be called on the shard where the relevant reader lives.
     virtual future<reader_permit> obtain_reader_permit(schema_ptr schema, const char* const description, db::timeout_clock::time_point timeout) = 0;
 };
-
-/// Make a multishard_combining_reader.
-///
-/// multishard_combining_reader takes care of reading a range from all shards
-/// that own a subrange in the range. Shard reader are created on-demand, when
-/// the shard is visited for the first time.
-///
-/// The read starts with a concurrency of one, that is the reader reads from a
-/// single shard at a time. The concurrency is exponentially increased (to a
-/// maximum of the number of shards) when a reader's buffer is empty after
-/// moving the next shard. This condition is important as we only wan't to
-/// increase concurrency for sparse tables that have little data and the reader
-/// has to move between shards often. When concurrency is > 1, the reader
-/// issues background read-aheads to the next shards so that by the time it
-/// needs to move to them they have the data ready.
-/// For dense tables (where we rarely cross shards) we rely on the
-/// foreign_reader to issue sufficient read-aheads on its own to avoid blocking.
-///
-/// The readers' life-cycles are managed through the supplied lifecycle policy.
-flat_mutation_reader make_multishard_combining_reader(
-        shared_ptr<reader_lifecycle_policy> lifecycle_policy,
-        schema_ptr schema,
-        reader_permit permit,
-        const dht::partition_range& pr,
-        const query::partition_slice& ps,
-        const io_priority_class& pc,
-        tracing::trace_state_ptr trace_state = nullptr,
-        mutation_reader::forwarding fwd_mr = mutation_reader::forwarding::no);
-
-flat_mutation_reader make_multishard_combining_reader_for_tests(
-        const dht::sharder& sharder,
-        shared_ptr<reader_lifecycle_policy> lifecycle_policy,
-        schema_ptr schema,
-        reader_permit permit,
-        const dht::partition_range& pr,
-        const query::partition_slice& ps,
-        const io_priority_class& pc,
-        tracing::trace_state_ptr trace_state = nullptr,
-        mutation_reader::forwarding fwd_mr = mutation_reader::forwarding::no);
 
 /// Make a multishard_combining_reader.
 ///
