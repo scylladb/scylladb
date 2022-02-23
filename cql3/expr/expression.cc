02c3b78e25cbf4ec4f4649d29223ae51add6cd7f
Jan Ciolek <jan.ciolek@scylladb.com>
2022-02-24 00:07:02 +0100

    cql3: expr: Handle subscript in possible_lhs_values
    possible_lhs_values returns set of possible values
    for a column given some restrictions.
    
    Current behaviour in case of a subscripted column
    is to just ignore the subscript and treat
    the restriction as if it were on just the column.
    
    This seems wrong, or at least confusing,
    but I won't change it in this patch to preserve the existing behaviour.
    
    Trying to change this to something more reasonable
    breaks other code which assumes that possible_lhs_values
    returns a list of values.
    (See partition_ranges_from_EQs() in cql3/restrictions/statement_restrictions.cc)
    
    Signed-off-by: Jan Ciolek <jan.ciolek@scylladb.com>

--- cql3/expr/expression.cc
+++ cql3/expr/expression.cc
@@ -840,6 +840,28 @@ value_set possible_lhs_values(const column_definition* cdef, const expression& e
                             }
                             throw std::logic_error(format("possible_lhs_values: unhandled operator {}", oper));
                         },
+                        [&] (const subscript& s) -> value_set {
+                            // FIXME: This looks wrong, the restriction is for the subscripted value, not the column.
+                            // Code copied from column_value& handler to preserve behaviour.
+                            // I didn't want to introduce changes in a refactor PR.
+                            // Will be fixed in another patch soon.
+                            const column_value& col = get_subscripted_column(s);
+
+                            if (!cdef || cdef != col.col) {
+                                return unbounded_value_set;
+                            }
+                            if (is_compare(oper.op)) {
+                                managed_bytes_opt val = evaluate(oper.rhs, options).value.to_managed_bytes_opt();
+                                if (!val) {
+                                    return empty_value_set; // All NULL comparisons fail; no column values match.
+                                }
+                                return oper.op == oper_t::EQ ? value_set(value_list{*val})
+                                        : to_range(oper.op, std::move(*val));
+                            } else if (oper.op == oper_t::IN) {
+                                return get_IN_values(oper.rhs, options, type->as_less_comparator(), cdef->name_as_text());
+                            }
+                            throw std::logic_error(format("possible_lhs_values: unhandled operator {}", oper));
+                        },
                         [&] (const tuple_constructor& tuple) -> value_set {
                             if (!cdef) {
                                 return unbounded_value_set;
@@ -940,6 +962,9 @@ value_set possible_lhs_values(const column_definition* cdef, const expression& e
             },
             [] (const column_value&) -> value_set {
                 on_internal_error(expr_logger, "possible_lhs_values: a column cannot serve as a restriction by itself");
+            },
+            [] (const subscript&) -> value_set {
+                on_internal_error(expr_logger, "possible_lhs_values: a subscript cannot serve as a restriction by itself");
             },
             [] (const token&) -> value_set {
                 on_internal_error(expr_logger, "possible_lhs_values: the token function cannot serve as a restriction by itself");
