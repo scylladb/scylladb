089d4442d8027288b145412aeb18d674fba198c7
Benny Halevy <bhalevy@scylladb.com>
2022-03-03 09:55:17 +0200

    compaction_manager: register_metrics: expose failed_compactions
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- compaction/compaction_manager.cc
+++ compaction/compaction_manager.cc
@@ -474,6 +474,8 @@ void compaction_manager::register_metrics() {
                        sm::description("Holds the number of compaction tasks waiting for an opportunity to run.")),
         sm::make_derive("completed_compactions", [this] { return _stats.completed_tasks; },
                        sm::description("Holds the number of completed compaction tasks.")),
+        sm::make_derive("failed_compactions", [this] { return _stats.errors; },
+                       sm::description("Holds the number of failed compaction tasks.")),
         sm::make_gauge("backlog", [this] { return _last_backlog; },
                        sm::description("Holds the sum of compaction backlog for all tables in the system.")),
         sm::make_gauge("normalized_backlog", [this] { return _last_backlog / _available_memory; },
