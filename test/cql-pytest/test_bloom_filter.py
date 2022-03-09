1438be53118a615a6ba72ecd19fd2b1c24fc454e
Jadw1 <michal.jadwiszczak@scylladb.com>
2022-03-09 18:06:13 +0100

    CQL3: Bloom filter efficacy test
    Added CQL pytest to check bloom filter efficacy by
    inserting `N` rows and reading `M` non-existing keys.
    
    Added `bloom_filter_false_positives` method to Python `nodetool`
    module. Method gets fp number by calling Scylla's API.
    
    Fixes: #1055
    
    Closes #10186

---
+++ test/cql-pytest/test_bloom_filter.py
@@ -0,0 +1,31 @@
+# Copyright 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import pytest
+import rest_api
+import nodetool
+from util import new_test_table
+
+# Test inserts `N` rows into table, flushes it 
+# and tries to read `M` non-existing keys.
+# Then bloom filter's false-positive ratio is checked.
+@pytest.mark.parametrize("N,M,fp_chance", [(500, 1000, 0.1)])
+def test_bloom_filter(scylla_only, cql, test_keyspace, N, M, fp_chance):
+    with new_test_table(cql, test_keyspace, "a int PRIMARY KEY", 
+        f"WITH bloom_filter_fp_chance = {fp_chance}") as table:
+        
+        stmt = cql.prepare(f"INSERT INTO {table} (a) VALUES(?)")
+        for k in range(N):
+            cql.execute(stmt, [k])
+        nodetool.flush(cql, table)
+        
+        read_stmt = cql.prepare(f"SELECT * FROM {table} WHERE a = ? BYPASS CACHE")
+        for k in range(N, N+M):
+            cql.execute(read_stmt, [k])
+
+        fp = rest_api.get_column_family_metric(cql, 
+          "bloom_filter_false_positives", table)
+        ratio = fp / M
+        assert ratio >= fp_chance * 0.7 and ratio <= fp_chance * 1.15
+            
