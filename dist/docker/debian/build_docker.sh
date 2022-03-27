1315135fca46ec9f3f3405a9da8829a6ec4df294
Takuya ASADA <syuu@scylladb.com>
2022-03-27 14:33:09 +0900

    docker: enable --log-to-stdout which mistakenly disabled
    Since our Docker image moved to Ubuntu, we mistakenly copy
    dist/docker/etc/sysconfig/scylla-server to /etc/sysconfig, which is not
    used in Ubuntu (it should be /etc/default).
    So /etc/default/scylla-server is just default configuration of
    scylla-server .deb package, --log-to-stdout is 0, same as normal installation.
    
    We don't want keep the duplicated configuration file anyway,
    so let's drop dist/docker/etc/sysconfig/scylla-server and configure
    /etc/default/scylla-server in build_docker.sh.
    
    Fixes #10270
    
    Closes #10280
    
    (cherry picked from commit bdefea7c82bbd2674b8c133212921d0a5fbecab5)

--- dist/docker/debian/build_docker.sh
+++ dist/docker/debian/build_docker.sh
@@ -89,6 +89,7 @@ run bash -ec "cat /scylla_bashrc >> /etc/bash.bashrc"
 run mkdir -p /etc/supervisor.conf.d
 run mkdir -p /var/log/scylla
 run chown -R scylla:scylla /var/lib/scylla
+run sed -i -e 's/^SCYLLA_ARGS=".*"$/SCYLLA_ARGS="--log-to-syslog 0 --log-to-stdout 1 --default-log-level info --network-stack posix"/' /etc/default/scylla-server
 
 run mkdir -p /opt/scylladb/supervisor
 run touch /opt/scylladb/SCYLLA-CONTAINER-FILE
