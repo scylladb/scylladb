c0fd53a9d7d8016b489e8fca19f40a98e103d57a
Piotr Sarna <sarna@scylladb.com>
2022-03-30 18:26:50 +0200

    cql3: fix qualifying restrictions with IN for indexing
    When a query contains IN restriction on its partition key,
    it's currently not eligible for indexing. It was however
    erroneously qualified as such, which lead to fetching incorrect
    results. This commit fixes the issue by not allowing such queries
    to undergo indexing, and comes with a regression test.
    
    Fixes #10300
    
    Closes #10302

--- test/cql-pytest/test_filtering.py
+++ test/cql-pytest/test_filtering.py
@@ -130,3 +130,16 @@ def test_filter_like_on_desc_column(cql, test_keyspace, cassandra_bug):
         cql.execute(f"INSERT INTO {table} (a, b) VALUES (1, 'one')")
         res = cql.execute(f"SELECT b FROM {table} WHERE b LIKE '%%%' ALLOW FILTERING")
         assert res.one().b == "one"
+
+# Test that the fact that a column is indexed does not cause us to fetch
+# incorrect results from a filtering query (issue #10300).
+def test_index_with_in_relation(scylla_only, cql, test_keyspace):
+    schema = 'p int, c int, v boolean, primary key (p,c)'
+    with new_test_table(cql, test_keyspace, schema) as table:
+        cql.execute(f"create index on {table}(v)")
+        for p, c, v in [(0,0,True),(0,1,False),(0,2,True),(0,3,False),
+                (1,0,True),(1,1,False),(1,2,True),(1,3,False),
+                (2,0,True),(2,1,False),(2,2,True),(2,3,False)]:
+            cql.execute(f"insert into {table} (p,c,v) values ({p}, {c}, {v})")
+        res = cql.execute(f"select * from {table} where p in (0,1) and v = False ALLOW FILTERING")
+        assert set(res) == set([(0,1,False),(0,3,False),(1,1,False), (1,3,False)])
