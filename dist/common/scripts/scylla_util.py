b6003989f9e193ebed342ced8dc737c8f6925364
Takuya ASADA <syuu@scylladb.com>
2022-05-11 10:52:43 +0900

    scylla_setup: stop using sudo -u, use user/group parameter on subprocess module
    To run scylla-housekeeping we currently use "sudo -u scylla <cmd>" to switch
    scylla user, but it fails on some environment.
    Since recent version of Python 3 supports to switch user on subprocess module,
    let's use python native way and drop sudo.
    
    Fixes #10483
    
    Closes #10538

--- dist/common/scripts/scylla_util.py
+++ dist/common/scripts/scylla_util.py
@@ -38,8 +38,8 @@ def scylla_excepthook(etype, value, tb):
 sys.excepthook = scylla_excepthook
 
 
-def out(cmd, shell=True, timeout=None, encoding='utf-8'):
-    res = subprocess.run(cmd, capture_output=True, shell=shell, timeout=timeout, check=False, encoding=encoding)
+def out(cmd, shell=True, timeout=None, user=None, group=None, encoding='utf-8'):
+    res = subprocess.run(cmd, capture_output=True, shell=shell, timeout=timeout, user=user, group=group, check=False, encoding=encoding)
     if res.returncode != 0:
         print(f'Command \'{cmd}\' returned non-zero exit status: {res.returncode}')
         print('----------  stdout  ----------')
