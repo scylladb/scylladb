a2b9a17927d851b368d3ef8e869a295c8bd2e86b
Nadav Har'El <nyh@scylladb.com>
2022-05-15 14:50:30 +0300

    test/cql-pytest: add test for default clustering order of SELECT
    Our documentation for SELECT,
    https://docs.scylladb.com/getting-started/dml/#ordering-results
    says that:
    
      "The ORDER BY clause lets you select the order of the returned results.
       It takes as argument a list of column names along with the order for
       the column (ASC for ascendant and DESC for descendant,
       **omitting the order being equivalent to ASC**)."
    
    The test in this patch confirms that the last emphasized line is not
    accurate - The default order for SELECT is the default order of the table
    being read - NOT always ascending order. If the table was created with
    descending WITH CLUSTERING ORDER BY, then a SELECT not specifying
    an ORDER BY will get this descending order by default.
    
    The test passes on both Scylla and Cassandra, demonstrating that this
    behavior is expected and correct - regardless of what our docs say.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <20220515115030.775813-1-nyh@scylladb.com>

---
+++ test/cql-pytest/test_clustering_order.py
@@ -0,0 +1,48 @@
+# Copyright 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+#############################################################################
+# Tests for clustering key ordering, namely the WITH CLUSTERING ORDER BY
+# setting in the table schema, and ORDER BY in select.
+#
+# We have many other tests for this feature - in C++ tests, in translated
+# unit tests from Cassandra (cassandra_tests), and its interaction with
+# other features (filtering, secondary indexes, etc.) in other test files.
+
+import pytest
+
+from util import new_test_table, unique_key_int
+
+@pytest.fixture(scope="module")
+def table_int_desc(cql, test_keyspace):
+    schema="k INT, c INT, PRIMARY KEY (k, c)"
+    order="WITH CLUSTERING ORDER BY (c DESC)"
+    with new_test_table(cql, test_keyspace, schema, order) as table:
+        yield table
+
+# Verify that if a table is created with descending order for its
+# clustering key, the default ordering of SELECT is changed to descending
+# order. This was contrary to our documentation which used to suggest
+# that SELECT always defaults to ascending order.
+def test_select_default_order(cql, table_int_desc):
+    k = unique_key_int()
+    stmt = cql.prepare(f'INSERT INTO {table_int_desc} (k, c) VALUES ({k}, ?)')
+    numbers = range(5)
+    for i in numbers:
+        cql.execute(stmt, [i])
+    # In a table created with descending sort order, the default select
+    # order is descending:
+    rows = [(i,) for i in numbers]
+    reverse_rows = [(i,) for i in reversed(numbers)]
+    assert reverse_rows == list(cql.execute(f'SELECT c FROM {table_int_desc} WHERE k = {k}'))
+    # Confirm that when specifying the order explicitly, both work:
+    assert rows == list(cql.execute(f'SELECT c FROM {table_int_desc} WHERE k = {k} ORDER BY c ASC'))
+    assert reverse_rows == list(cql.execute(f'SELECT c FROM {table_int_desc} WHERE k = {k} ORDER BY c DESC'))
+    # Repeat the same three assertions as above, adding a "limit" of N=3:
+    N=3
+    rows = rows[0:N]
+    reverse_rows = reverse_rows[0:N]
+    assert reverse_rows == list(cql.execute(f'SELECT c FROM {table_int_desc} WHERE k = {k} LIMIT {N}'))
+    assert rows == list(cql.execute(f'SELECT c FROM {table_int_desc} WHERE k = {k} ORDER BY c ASC LIMIT {N}'))
+    assert reverse_rows == list(cql.execute(f'SELECT c FROM {table_int_desc} WHERE k = {k} ORDER BY c DESC LIMIT {N}'))
