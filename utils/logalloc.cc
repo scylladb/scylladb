3ba85c3bbd1ac0aaf278cf253de9286bcd7118f2
Benny Halevy <bhalevy@scylladb.com>
2022-05-29 12:31:57 +0300

    logalloc: region_impl: mark functions noexcept
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/logalloc.cc
+++ utils/logalloc.cc
@@ -1501,7 +1501,7 @@ private:
     struct compaction_lock {
         region_impl& _region;
         bool _prev;
-        compaction_lock(region_impl& r)
+        compaction_lock(region_impl& r) noexcept
             : _region(r)
             , _prev(r._reclaiming_enabled)
         {
@@ -1734,7 +1734,7 @@ private:
         _buf_active_offset = 0;
     }
 
-    static uint64_t next_id() {
+    static uint64_t next_id() noexcept {
         static std::atomic<uint64_t> id{0};
         return id.fetch_add(1);
     }
@@ -1845,7 +1845,7 @@ public:
     //
     //    while (is_compactible()) { compact(); }
     //
-    bool is_compactible() const {
+    bool is_compactible() const noexcept {
         return _reclaiming_enabled
             // We require 2 segments per allocation segregation group to ensure forward progress during compaction.
             // There are currently two fixed groups, one for the allocation_strategy implementation and one for lsa_buffer:s.
@@ -1853,7 +1853,7 @@ public:
             && _segment_descs.contains_above_min();
     }
 
-    bool is_idle_compactible() {
+    bool is_idle_compactible() const noexcept {
         return is_compactible();
     }
 
@@ -2011,7 +2011,7 @@ public:
     }
 
     // Returns occupancy of the sparsest compactible segment.
-    occupancy_stats min_occupancy() const {
+    occupancy_stats min_occupancy() const noexcept {
         if (_segment_descs.empty()) {
             return {};
         }
@@ -2019,7 +2019,7 @@ public:
     }
 
     // Compacts a single segment, most appropriate for it
-    void compact() {
+    void compact() noexcept {
         compaction_lock _(*this);
         auto& desc = _segment_descs.one_of_largest();
         _segment_descs.pop_one_of_largest();
@@ -2058,16 +2058,16 @@ public:
         compact_segment_locked(seg, desc);
     }
 
-    allocation_strategy& allocator() {
+    allocation_strategy& allocator() noexcept {
         return *this;
     }
 
-    uint64_t id() const {
+    uint64_t id() const noexcept {
         return _id;
     }
 
     // Returns true if this pool is evictable, so that evict_some() can be called.
-    bool is_evictable() const {
+    bool is_evictable() const noexcept {
         return _evictable && _reclaiming_enabled;
     }
 
@@ -2079,17 +2079,17 @@ public:
         return ret;
     }
 
-    void make_not_evictable() {
+    void make_not_evictable() noexcept {
         _evictable = false;
         _eviction_fn = {};
     }
 
-    void make_evictable(eviction_fn fn) {
+    void make_evictable(eviction_fn fn) noexcept {
         _evictable = true;
         _eviction_fn = std::move(fn);
     }
 
-    const eviction_fn& evictor() const {
+    const eviction_fn& evictor() const noexcept {
         return _eviction_fn;
     }
 
