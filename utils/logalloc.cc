888e225113512b5a29cdb3a385f29aa541787374
Benny Halevy <bhalevy@scylladb.com>
2022-05-29 12:25:08 +0300

    logalloc: tracker::impl: mark functions noexcept
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/logalloc.cc
+++ utils/logalloc.cc
@@ -444,7 +444,7 @@ private:
     // object is not re-entered while inside one of the tracker's methods.
     struct reclaiming_lock {
         impl& _ref;
-        reclaiming_lock(impl& ref)
+        reclaiming_lock(impl& ref) noexcept
             : _ref(ref)
         {
             _ref.disable_reclaim();
@@ -464,10 +464,10 @@ public:
             return make_ready_future<>();
         }
     }
-    void disable_reclaim() {
+    void disable_reclaim() noexcept {
         ++_reclaiming_disabled_depth;
     }
-    void enable_reclaim() {
+    void enable_reclaim() noexcept {
         --_reclaiming_disabled_depth;
     }
     void register_region(region::impl*);
@@ -483,15 +483,15 @@ public:
     size_t compact_and_evict(size_t reserve_segments, size_t bytes, is_preemptible p);
     void full_compaction();
     void reclaim_all_free_segments();
-    occupancy_stats region_occupancy();
-    occupancy_stats occupancy();
-    size_t non_lsa_used_space();
+    occupancy_stats region_occupancy() noexcept;
+    occupancy_stats occupancy() noexcept;
+    size_t non_lsa_used_space() noexcept;
     // Set the minimum number of segments reclaimed during single reclamation cycle.
-    void set_reclamation_step(size_t step_in_segments) { _reclamation_step = step_in_segments; }
-    size_t reclamation_step() const { return _reclamation_step; }
+    void set_reclamation_step(size_t step_in_segments) noexcept { _reclamation_step = step_in_segments; }
+    size_t reclamation_step() const noexcept { return _reclamation_step; }
     // Abort on allocation failure from LSA
-    void enable_abort_on_bad_alloc() { _abort_on_bad_alloc = true; }
-    bool should_abort_on_bad_alloc() const { return _abort_on_bad_alloc; }
+    void enable_abort_on_bad_alloc() noexcept { _abort_on_bad_alloc = true; }
+    bool should_abort_on_bad_alloc() const noexcept { return _abort_on_bad_alloc; }
     void setup_background_reclaim(scheduling_group sg) {
         assert(!_background_reclaimer);
         _background_reclaimer.emplace(sg, [this] (size_t target) {
@@ -2252,7 +2252,7 @@ std::ostream& operator<<(std::ostream& out, const occupancy_stats& stats) {
         stats.used_fraction() * 100, stats.used_space(), stats.total_space());
 }
 
-occupancy_stats tracker::impl::region_occupancy() {
+occupancy_stats tracker::impl::region_occupancy() noexcept {
     reclaiming_lock _(*this);
     occupancy_stats total{};
     for (auto&& r: _regions) {
@@ -2261,7 +2261,7 @@ occupancy_stats tracker::impl::region_occupancy() {
     return total;
 }
 
-occupancy_stats tracker::impl::occupancy() {
+occupancy_stats tracker::impl::occupancy() noexcept {
     reclaiming_lock _(*this);
     auto occ = region_occupancy();
     {
@@ -2271,7 +2271,7 @@ occupancy_stats tracker::impl::occupancy() {
     return occ;
 }
 
-size_t tracker::impl::non_lsa_used_space() {
+size_t tracker::impl::non_lsa_used_space() noexcept {
 #ifdef SEASTAR_DEFAULT_ALLOCATOR
     return 0;
 #else
