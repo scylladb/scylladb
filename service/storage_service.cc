scylladb/scylladb

commit 89163a3be4ba3cd2161f1caf6214e19da7cca4d0

main, storage_service: Move early group0 join call
It happens right after the prepare to join, moving it at the end of the
latter call doesn't change the code logic. A side effect -- this removes
a silly join_group0() one-line helper.

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

service/storage_service.cc
@@ -354,6 +354,18 @@ future<> storage_service::prepare_to_join(
     auto generation_number = co_await db::system_keyspace::increment_and_get_generation();
     auto advertise = gms::advertise_myself(!replacing_a_node_with_same_ip);
     co_await _gossiper.start_gossiping(generation_number, app_states, advertise);
+
+    // Raft group0 can be joined before we wait for gossip to settle
+    // if one of the following applies:
+    //  - it's a fresh node start (in a fresh cluster)
+    //  - it's a restart of an existing node, which have already joined some group0
+    const bool can_join_with_raft =
+        _db.local().get_config().check_experimental(db::experimental_features_t::RAFT) && (
+            _sys_ks.local().bootstrap_needed() ||
+            !_sys_ks.local().get_raft_group0_id().get().is_null());
+    if (can_join_with_raft) {
+        co_await _group0->join_group0();
+    }
 }
 
 future<> storage_service::start_sys_dist_ks() {
@@ -3660,11 +3672,6 @@ future<> storage_service::node_ops_abort_thread() {
         }
     }
     slogger.info("Stopped node_ops_abort_thread");
-}
-
-future<> storage_service::join_group0() {
-    assert(_group0);
-    return _group0->join_group0();
 }
 
 } // namespace service

main.cc
@@ -1276,20 +1276,6 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
                 return ss.local().init_server(qp.local(), group0_client);
             }).get();
 
-            // Raft group0 can be joined before we wait for gossip to settle
-            // if one of the following applies:
-            //  - it's a fresh node start (in a fresh cluster)
-            //  - it's a restart of an existing node, which have already joined some group0
-            const bool can_join_with_raft =
-                cfg->check_experimental(db::experimental_features_t::RAFT) && (
-                    sys_ks.local().bootstrap_needed() ||
-                    !sys_ks.local().get_raft_group0_id().get().is_null());
-            if (can_join_with_raft) {
-                with_scheduling_group(maintenance_scheduling_group, [&ss] {
-                    return ss.local().join_group0();
-                }).get();
-            }
-
             auto schema_change_announce = db.local().observable_schema_version().observe([&mm] (utils::UUID schema_version) mutable {
                 mm.local().passive_announce(std::move(schema_version));
             });

service/storage_service.hh
@@ -380,9 +380,6 @@ public:
 
     future<> stop_transport();
 
-    // delegates work to `raft_group0::join_group0`
-    future<> join_group0();
-
 private:
     bool should_bootstrap();
     std::optional<gms::inet_address> get_replace_address();

commit 443eb8fa8721db65b4923198fadc4932f8080b8c

service: storage_service: remove get() call not in thread
Regression introduced by code movement in
89163a3be4ba3cd2161f1caf6214e19da7cca4d0.

Fixes #10679.

service/storage_service.cc
@@ -432,7 +432,7 @@ future<> storage_service::join_token_ring(cdc::generation_service& cdc_gen_servi
     const bool can_join_with_raft =
         _db.local().get_config().check_experimental(db::experimental_features_t::RAFT) && (
             _sys_ks.local().bootstrap_needed() ||
-            !_sys_ks.local().get_raft_group0_id().get().is_null());
+            !(co_await _sys_ks.local().get_raft_group0_id()).is_null());
     if (can_join_with_raft) {
         co_await _group0->join_group0();
     }