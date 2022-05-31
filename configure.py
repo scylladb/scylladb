69cfa16eddbc1b914da5d586eea23dcbfa844d62
Michał Chojnowski <michal.chojnowski@scylladb.com>
2022-05-31 12:24:16 +0200

    configure.py: hoist the pkg_config() call for seastar-testing.pc
    Put the pkg_config() for seastar-testing.pc in the same area as the call
    for seastar.pc, outside of the loop.
    This is a cosmetic change aimed at making following commits cleaner.

--- configure.py
+++ configure.py
@@ -1657,6 +1657,7 @@ for mode in build_modes:
     seastar_pc_cflags, seastar_pc_libs = query_seastar_flags(pc[mode], link_static_cxx=args.staticcxx)
     modes[mode]['seastar_cflags'] = seastar_pc_cflags
     modes[mode]['seastar_libs'] = seastar_pc_libs
+    modes[mode]['seastar_testing_libs'] = pkg_config(pc[mode].replace('seastar.pc', 'seastar-testing.pc'), '--libs', '--static')
 
 def configure_abseil(build_dir, mode, mode_config):
     abseil_build_dir = os.path.join(build_dir, mode, 'abseil')
@@ -1815,6 +1816,7 @@ with open(buildfile, 'w') as f:
             cxxflags_{mode} = $cxx_ld_flags_{mode} {cxxflags} -iquote. -iquote $builddir/{mode}/gen
             libs_{mode} = -l{fmt_lib}
             seastar_libs_{mode} = {seastar_libs}
+            seastar_testing_libs_{mode} = {seastar_testing_libs}
             rule cxx.{mode}
               command = $cxx -MD -MT $out -MF $out.d {seastar_cflags} $cxxflags_{mode} $cxxflags $obj_cxxflags -c -o $out $in
               description = CXX $out
@@ -1923,8 +1925,7 @@ with open(buildfile, 'w') as f:
                 if binary in pure_boost_tests:
                     local_libs += ' ' + maybe_static(args.staticboost, '-lboost_unit_test_framework')
                 if binary not in tests_not_using_seastar_test_framework:
-                    pc_path = pc[mode].replace('seastar.pc', 'seastar-testing.pc')
-                    local_libs += ' ' + pkg_config(pc_path, '--libs', '--static')
+                    local_libs += ' ' + "$seastar_testing_libs_{}".format(mode)
                 # Our code's debugging information is huge, and multiplied
                 # by many tests yields ridiculous amounts of disk space.
                 # So we strip the tests by default; The user can very
