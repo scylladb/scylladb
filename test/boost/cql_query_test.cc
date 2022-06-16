scylladb/scylladb

commit 5dee55d433118003d46d2b5c5b79509977a57613

cql3: expr: Change how prepare_expression handles bind_variable
The situation with preparing bind_variable is a bit strange,
there are four shapes of bind variables and receiver behaviour
is not in line with other types.

To prepare a bind_variable for a list of IN values for an int column
the current code requires us to pass a receiver of type int.
This is counterintuitive, to prepare a string we pass
a receiver with string type, so to prepare list<int> we should
pass a receiver of type list<int>, not just int.

This commit changes the behaviour in two ways:
- Shape of bind_variable doesn't matter anymore
- The bind_variable gets the receiver passed to prepare_expression,
  no more list<receiver> magic.

Other variants of bind_variable_x_prepare_expression are not removed yet
because they are needed by prepare_expression_mutlti_column.
They will be removed later, along with bind_variable::shape_type.

Signed-off-by: cvybhu <jan.ciolek@scylladb.com>

cql3/column_condition.cc
@@ -315,7 +315,9 @@ column_condition::raw::prepare(data_dictionary::database db, const sstring& keys
 
     if (_in_marker) {
         assert(_in_values.empty());
-        expr::expression multi_item_term = prepare_expression(*_in_marker, db, keyspace, value_spec);
+        auto in_name = ::make_shared<column_identifier>(format("in({})", value_spec->name->text()), true);
+        lw_shared_ptr<column_specification> in_list_receiver = make_lw_shared<column_specification>(value_spec->ks_name, value_spec->cf_name, in_name, list_type_impl::get_instance(receiver.type, false));
+        expr::expression multi_item_term = prepare_expression(*_in_marker, db, keyspace, in_list_receiver);
         return column_condition::in_condition(receiver, collection_element_expression, std::move(multi_item_term), {});
     }
     // Both _in_values and in _in_marker can be missing in case of empty IN list: "a IN ()"

cql3/expr/prepare_expr.cc
@@ -649,23 +649,6 @@ bind_variable_scalar_prepare_expression(const bind_variable& bv, data_dictionary
 
 static
 lw_shared_ptr<column_specification>
-bind_variable_scalar_in_make_receiver(const column_specification& receiver) {
-    auto in_name = ::make_shared<column_identifier>(sstring("in(") + receiver.name->to_string() + sstring(")"), true);
-    return make_lw_shared<column_specification>(receiver.ks_name, receiver.cf_name, in_name, list_type_impl::get_instance(receiver.type, false));
-}
-
-static
-bind_variable
-bind_variable_scalar_in_prepare_expression(const bind_variable& bv, data_dictionary::database db, const sstring& keyspace, lw_shared_ptr<column_specification> receiver) {
-    return bind_variable {
-        .shape = bind_variable::shape_type::scalar,
-        .bind_index = bv.bind_index,
-        .receiver = bind_variable_scalar_in_make_receiver(*receiver)
-    };
-}
-
-static
-lw_shared_ptr<column_specification>
 bind_variable_tuple_make_receiver(const std::vector<lw_shared_ptr<column_specification>>& receivers) {
     std::vector<data_type> types;
     types.reserve(receivers.size());
@@ -922,13 +905,7 @@ prepare_expression(const expression& expr, data_dictionary::database db, const s
             return null_prepare_expression(db, keyspace, receiver);
         },
         [&] (const bind_variable& bv) -> expression {
-            switch (bv.shape) {
-            case expr::bind_variable::shape_type::scalar:  return bind_variable_scalar_prepare_expression(bv, db, keyspace, receiver);
-            case expr::bind_variable::shape_type::scalar_in: return bind_variable_scalar_in_prepare_expression(bv, db, keyspace, receiver);
-            case expr::bind_variable::shape_type::tuple: on_internal_error(expr_logger, "prepare_expression(bind_variable(tuple))");
-            case expr::bind_variable::shape_type::tuple_in: on_internal_error(expr_logger, "prepare_expression(bind_variable(tuple_in))");
-            }
-            on_internal_error(expr_logger, "unexpected shape in bind_variable");
+            return bind_variable_scalar_prepare_expression(bv, db, keyspace, receiver);
         },
         [&] (const untyped_constant& uc) -> expression {
             return untyped_constant_prepare_expression(uc, db, keyspace, receiver);

cql3/single_column_relation.cc
@@ -13,6 +13,7 @@
 #include "cql3/statements/request_validations.hh"
 #include "cql3/cql3_type.hh"
 #include "cql3/lists.hh"
+#include "seastar/core/on_internal_error.hh"
 #include "unimplemented.hh"
 #include "types/map.hh"
 #include "types/list.hh"
@@ -22,6 +23,9 @@
 using namespace cql3::expr;
 
 namespace cql3 {
+namespace expr {
+    extern logging::logger expr_logger;
+}
 
 expression
 single_column_relation::to_expression(const std::vector<lw_shared_ptr<column_specification>>& receivers,
@@ -58,6 +62,28 @@ single_column_relation::new_EQ_restriction(data_dictionary::database db, schema_
     return r;
 }
 
+expression
+static prepare_in_bind_marker(const std::vector<lw_shared_ptr<column_specification>>& receivers,
+                              const expr::expression& raw,
+                              data_dictionary::database db,
+                              const sstring& keyspace,
+                              prepare_context& ctx) {
+    if(receivers.size() != 1) {
+        on_internal_error(expr_logger, format("prepare_in_bind_marker - bad receivers.size(): {}", receivers.size()));
+    }
+    shared_ptr<column_identifier> in_name =
+        ::make_shared<column_identifier>(format("in({})", receivers[0]->name->text()), true);
+    data_type in_list_type = list_type_impl::get_instance(receivers[0]->type->underlying_type(), false);
+    lw_shared_ptr<column_specification> in_receiver =
+        make_lw_shared<column_specification>(receivers[0]->ks_name,
+                                             receivers[0]->cf_name,
+                                             std::move(in_name),
+                                             std::move(in_list_type));
+    auto expr = prepare_expression(raw, db, keyspace, in_receiver);
+    expr::fill_prepare_context(expr, ctx);
+    return expr;
+}
+
 ::shared_ptr<restrictions::restriction>
 single_column_relation::new_IN_restriction(data_dictionary::database db, schema_ptr schema, prepare_context& ctx) {
     using namespace restrictions;
@@ -69,7 +95,7 @@ single_column_relation::new_IN_restriction(data_dictionary::database db, schema_
     auto receivers = to_receivers(*schema, column_def);
     assert(_in_values.empty() || !_value);
     if (_value) {
-        auto e = to_expression(receivers, *_value, db, schema->ks_name(), ctx);
+        auto e = prepare_in_bind_marker(receivers, *_value, db, schema->ks_name(), ctx);
         auto r = ::make_shared<single_column_restriction>(column_def);
         r->expression = binary_operator{column_value{&column_def}, expr::oper_t::IN, std::move(e)};
         return r;

commit 8ed10a5836bc675bcdc77b854f3bb08546dd48f9

cql3::column_condition.cc: fix _in_marker handling The commit scylladb@5dee55d introduced a regression: type of in_list_receiver was taken from receiver instead of value_spec as it was before. This regression was caught by dtest test_lwt_update_prepared_listlike_and_tuples. This commit reverts to original behavior and adds a specific boost-test for this scenario.
Fixes: #10821

cql3/column_condition.cc
@@ -331,7 +331,7 @@ column_condition::raw::prepare(data_dictionary::database db, const sstring& keys
     if (_in_marker) {
         assert(_in_values.empty());
         auto in_name = ::make_shared<column_identifier>(format("in({})", value_spec->name->text()), true);
-        lw_shared_ptr<column_specification> in_list_receiver = make_lw_shared<column_specification>(value_spec->ks_name, value_spec->cf_name, in_name, list_type_impl::get_instance(receiver.type, false));
+        lw_shared_ptr<column_specification> in_list_receiver = make_lw_shared<column_specification>(value_spec->ks_name, value_spec->cf_name, in_name, list_type_impl::get_instance(value_spec->type, false));
         expr::expression multi_item_term = prepare_expression(*_in_marker, db, keyspace, nullptr, in_list_receiver);
         return column_condition::in_condition(receiver, collection_element_expression, std::move(multi_item_term), {});
     }

test/boost/cql_query_test.cc
@@ -4539,6 +4539,28 @@ SEASTAR_TEST_CASE(test_like_parameter_marker) {
     });
 }
 
+SEASTAR_TEST_CASE(test_list_parameter_marker) {
+    return do_with_cql_env_thread([] (cql_test_env& e) {
+        cquery_nofail(e, "CREATE TABLE t (k int PRIMARY KEY, v list<int>)");
+        cquery_nofail(e, "INSERT INTO  t (k, v) VALUES (1, [10, 20, 30])");
+
+        const sstring query("UPDATE t SET v=:upd_v WHERE k=1 IF v[:i] in :v");
+
+        auto list_of = [](const std::vector<data_value>& data) -> bytes {
+            auto list_type_int = list_type_impl::get_instance(int32_type, false);
+            return make_list_value(list_type_int, data).serialize_nonnull();
+        };
+
+        prepared_on_shard(e,
+            query,
+            {list_of({100, 200, 300}), I(1), list_of({21, 22, 23})},
+            {{B(false), list_of({10, 20, 30})}});
+        prepared_on_shard(e, query,
+            {list_of({100, 200, 300}), I(1), list_of({20, 21, 22})},
+            {{B(true), list_of({10, 20, 30})}});
+    });
+}
+
 SEASTAR_TEST_CASE(test_select_serial_consistency) {
     return do_with_cql_env_thread([] (cql_test_env& e) {
         cquery_nofail(e, "CREATE TABLE t (a int, b int, primary key (a,b))");