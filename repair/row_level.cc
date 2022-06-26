b4383a389bfc8161782ca145cc18ed0cc5479e43
Benny Halevy <bhalevy@scylladb.com>
2022-06-26 16:12:30 +0300

    repair_reader: construct _reader_handle before _reader
    Currently, the `_reader` member is explicitly
    initialized with the result of the call to `make_reader`.
    And `make_reader`, as a side effect, assigns a value
    to the `_reader_handle` member.
    
    Since C++ initializes class members sequentially,
    in the order they are defined, the assignment to `_reader_handle`
    in `make_reader()` happens before `_reader_handle` is initialized.
    
    This patch fixes that by changing the definition order,
    and consequently, the member initialization order
    in the constructor so that `_reader_handle` will be (default-)initialized
    before the call to `make_reader()`, avoiding the undefined behavior.
    
    Fixes #10882
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    
    Closes #10883
    
    (cherry picked from commit 9c231ad0ce587c0eb874ff94696a6257b72fad72)

--- repair/row_level.cc
+++ repair/row_level.cc
@@ -347,9 +347,9 @@ private:
     // Only needed for local readers, the multishard reader takes care
     // of pinning tables on used shards.
     std::optional<utils::phased_barrier::operation> _local_read_op;
+    std::optional<evictable_reader_handle> _reader_handle;
     // Local reader or multishard reader to read the range
     flat_mutation_reader _reader;
-    std::optional<evictable_reader_handle> _reader_handle;
     // Current partition read from disk
     lw_shared_ptr<const decorated_key_with_hash> _current_dk;
     uint64_t _reads_issued = 0;
