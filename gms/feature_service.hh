25fec0acced25311571aeffd62ff39bc274c528d
Piotr Dulikowski <piodul@scylladb.com>
2022-08-03 15:36:22 +0200

    gms/feature_service: introduce SECONDARY_INDEXES_ON_STATIC_COLUMNS cluster feature
    The new feature will prevent secondary indexes on static columns from
    being created unless the whole cluster is ready to support them.

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -112,6 +112,7 @@ public:
     gms::feature aggregate_storage_options { *this, "AGGREGATE_STORAGE_OPTIONS"sv };
     gms::feature collection_indexing { *this, "COLLECTION_INDEXING"sv };
     gms::feature large_collection_detection { *this, "LARGE_COLLECTION_DETECTION"sv };
+    gms::feature secondary_indexes_on_static_columns { *this, "SECONDARY_INDEXES_ON_STATIC_COLUMNS"sv };
 
 public:
 
