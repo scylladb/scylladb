ebd765ebd8759adfc466a806d72d74b924a2acd3
Botond Dénes <bdenes@scylladb.com>
2022-08-12 07:59:09 +0300

    replica/table: query(): drop _async_gate usage
    Caller already required to take read_in_progress(), which already
    ensures table lifecycle.

--- replica/table.cc
+++ replica/table.cc
@@ -2155,13 +2155,11 @@ table::query(schema_ptr s,
         co_return make_lw_shared<query::result>();
     }
 
-    _async_gate.enter();
     utils::latency_counter lc;
     _stats.reads.set_latency(lc);
 
     auto finally = defer([&] () noexcept {
         _stats.reads.mark(lc);
-        _async_gate.leave();
     });
 
     const auto short_read_allowed = query::short_read(cmd.slice.options.contains<query::partition_slice::option::allow_short_read>());
