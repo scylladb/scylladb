d3fd09042980142ef9005903315e46814bdf59ae
Nadav Har'El <nyh@scylladb.com>
2022-08-15 23:41:30 +0300

    alternator: return ProvisionedThroughput in DescribeTable
    DescribeTable is currently hard-coded to return PAY_PER_REQUEST billing
    mode. Nevertheless, even in PAY_PER_REQUEST mode, the DescribeTable
    operation must return a ProvisionedThroughput structure, listing both
    ReadCapacityUnits and WriteCapacityUnits as 0. This requirement is not
    stated in some DynamoDB documentation but is explictly mentioned in
    https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_ProvisionedThroughput.html
    Also in empirically, DynamoDB returns ProvisionedThroughput with zeros
    even in PAY_PER_REQUEST mode. We even had an xfailing test to confirm this.
    
    The ProvisionedThroughput structure being missing was a problem for
    applications like DynamoDB connectors for Spark, if they implicitly
    assume that ProvisionedThroughput is returned by DescribeTable, and
    fail (as described in issue #11222) if it's outright missing.
    
    So this patch adds the missing ProvisionedThroughput structure, and
    the xfailing test starts to pass.
    
    Note that this patch doesn't change the fact that attempting to set
    a table to PROVISIONED billing mode is ignored: DescribeTable continues
    to always return PAY_PER_REQUEST as the billing mode and zero as the
    provisioned capacities.
    
    Fixes #11222
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    
    Closes #11298
    
    (cherry picked from commit 941c719a23896d44daacbaafbf59850f4f1ad1f5)

--- test/alternator/test_describe_table.py
+++ test/alternator/test_describe_table.py
@@ -94,9 +94,9 @@ def test_describe_table_size(test_table):
 # Test the ProvisionedThroughput attribute returned by DescribeTable.
 # This is a very partial test: Our test table is configured without
 # provisioned throughput, so obviously it will not have interesting settings
-# for it. DynamoDB returns zeros for some of the attributes, even though
-# the documentation suggests missing values should have been fine too.
-@pytest.mark.xfail(reason="DescribeTable does not return provisioned throughput")
+# for it. But DynamoDB documents that zeros be returned for WriteCapacityUnits
+# and ReadCapacityUnits, and does this in practice as well - and some
+# applications assume these numbers are always there (even if 0).
 def test_describe_table_provisioned_throughput(test_table):
     got = test_table.meta.client.describe_table(TableName=test_table.name)['Table']
     assert got['ProvisionedThroughput']['NumberOfDecreasesToday'] == 0
