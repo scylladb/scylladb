bhalevy/scylla

mutation_partition_view: make mutation_partition_view_virtual_visitor stoppable
So that the frozen_mutation consumer can return
stop_iteration::yes if it wishes to stop consuming at
some clustering position.

In this case, on_end_of_partition must still be called
so a closing range_tombstone_change can be emitted to the consumer.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

mutation_partition_view.cc
@@ -324,13 +324,15 @@ mutation_partition_view::accept_ordered_result mutation_partition_view::do_accep
     auto consume_rt = [&] (range_tombstone&& rt) {
         cookie.iterators->rts_begin = rt_it;
         mpvlog.trace("do_accept_ordered: accept range_tombstone: {}", rt);
-        visitor.accept_row_tombstone(std::move(rt));
+        return visitor.accept_row_tombstone(std::move(rt));
     };
     auto consume_cr = [&] (ser::deletable_row_view&& cr, clustering_key_prefix&& cr_key) {
         cookie.iterators->crs_begin = cr_it;
         auto t = row_tombstone(cr.deleted_at(), shadowable_tombstone(cr.shadowable_deleted_at()));
         mpvlog.trace("do_accept_ordered: accept clustering_row: {}", cr_key);
-        visitor.accept_row(position_in_partition_view::for_key(cr_key), t, read_row_marker(cr.marker()), is_dummy::no, is_continuous::yes);
+        if (visitor.accept_row(position_in_partition_view::for_key(cr_key), t, read_row_marker(cr.marker()), is_dummy::no, is_continuous::yes)) {
+            return stop_iteration::yes;
+        }
 
         struct cell_visitor {
             mutation_partition_view_virtual_visitor& _visitor;
@@ -343,6 +345,7 @@ mutation_partition_view::accept_ordered_result mutation_partition_view::do_accep
             }
         };
         read_and_visit_row(cr.cells(), cm, column_kind::regular_column, cell_visitor{visitor});
+        return stop_iteration::no;
     };
 
     std::optional<range_tombstone> rt;
@@ -376,32 +379,27 @@ mutation_partition_view::accept_ordered_result mutation_partition_view::do_accep
     position_in_partition::tri_compare cmp{s};
 
     for (;;) {
-        if (preempt && need_preempt()) {
-            mpvlog.trace("do_accept_ordered: preempt");
-            return accept_ordered_result{stop_iteration::no, std::move(cookie)};
-        }
         next_rt();
         next_cr();
-        if (rt) {
-            bool emit_rt = !cr;
-            if (cr) {
-                auto rt_pos = rt->position();
-                auto cr_pos = position_in_partition_view::for_key(*cr_key);
-                emit_rt = (cmp(rt_pos, cr_pos) < 0);
-            }
-            if (emit_rt) {
-                consume_rt(std::move(*std::exchange(rt, std::nullopt)));
-                // FIXME: allow visitor to return stop_iteration
-                continue;
-            }
+        bool emit_rt = bool(rt);
+        stop_iteration stop = stop_iteration::no;
+        if (rt && cr) {
+            auto rt_pos = rt->position();
+            auto cr_pos = position_in_partition_view::for_key(*cr_key);
+            emit_rt = (cmp(rt_pos, cr_pos) < 0);
         }
-        if (cr) {
-            consume_cr(std::move(*std::exchange(cr, std::nullopt)), std::move(*cr_key));
-            // FIXME: allow visitor to return stop_iteration
-            continue;
+        if (emit_rt) {
+            stop = consume_rt(std::move(*std::exchange(rt, std::nullopt)));
+        } else if (cr) {
+            stop = consume_cr(std::move(*std::exchange(cr, std::nullopt)), std::move(*cr_key));
+        } else {
+            mpvlog.trace("do_accept_ordered: done");
+            return accept_ordered_result{stop_iteration::yes, accept_ordered_cookie{}};
         }
-        mpvlog.trace("do_accept_ordered: done");
-        return accept_ordered_result{stop_iteration::yes, accept_ordered_cookie{}};
+        if (stop || (preempt && need_preempt())) {
+            mpvlog.trace("do_accept_ordered: {}", stop ? "stop_iteration" : "preempt");
+            return accept_ordered_result{stop, std::move(cookie)};
+        }
     }
 }
 

scylladb/scylladb

mutation_partition_view.cc
@@ -315,12 +315,14 @@ mutation_partition_view::accept_ordered_result mutation_partition_view::do_accep
 
     auto consume_rt = [&] (range_tombstone&& rt) {
         cookie.iterators->rts_begin = rt_it;
-        visitor.accept_row_tombstone(std::move(rt));
+        return visitor.accept_row_tombstone(std::move(rt));
     };
     auto consume_cr = [&] (ser::deletable_row_view&& cr, clustering_key_prefix&& cr_key) {
         cookie.iterators->crs_begin = cr_it;
         auto t = row_tombstone(cr.deleted_at(), shadowable_tombstone(cr.shadowable_deleted_at()));
-        visitor.accept_row(position_in_partition_view::for_key(cr_key), t, read_row_marker(cr.marker()), is_dummy::no, is_continuous::yes);
+        if (visitor.accept_row(position_in_partition_view::for_key(cr_key), t, read_row_marker(cr.marker()), is_dummy::no, is_continuous::yes)) {
+            return stop_iteration::yes;
+        }
 
         struct cell_visitor {
             mutation_partition_view_virtual_visitor& _visitor;
@@ -333,6 +335,7 @@ mutation_partition_view::accept_ordered_result mutation_partition_view::do_accep
             }
         };
         read_and_visit_row(cr.cells(), cm, column_kind::regular_column, cell_visitor{visitor});
+        return stop_iteration::no;
     };
 
     std::optional<range_tombstone> rt;
@@ -361,21 +364,21 @@ mutation_partition_view::accept_ordered_result mutation_partition_view::do_accep
         next_rt();
         next_cr();
         bool emit_rt = bool(rt);
+        stop_iteration stop;
         if (rt && cr) {
             auto rt_pos = rt->position();
             auto cr_pos = position_in_partition_view::for_key(*cr_key);
             emit_rt = (cmp(rt_pos, cr_pos) < 0);
         }
-        // FIXME: allow visitor to return stop_iteration
         if (emit_rt) {
-            consume_rt(std::move(*std::exchange(rt, std::nullopt)));
+            stop = consume_rt(std::move(*std::exchange(rt, std::nullopt)));
         } else if (cr) {
-            consume_cr(std::move(*std::exchange(cr, std::nullopt)), std::move(*cr_key));
+            stop = consume_cr(std::move(*std::exchange(cr, std::nullopt)), std::move(*cr_key));
         } else {
             return accept_ordered_result{stop_iteration::yes, accept_ordered_cookie{}};
         }
-        if (is_preemptible && need_preempt()) {
-            return accept_ordered_result{stop_iteration::no, std::move(cookie)};
+        if (stop || (is_preemptible && need_preempt())) {
+            return accept_ordered_result{stop, std::move(cookie)};
         }
     }
 }