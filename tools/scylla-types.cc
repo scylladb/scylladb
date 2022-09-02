21ef0c64f1e93130b5a0adf67f9ef730bd86df57
Botond Dénes <bdenes@scylladb.com>
2022-09-02 07:41:42 +0300

    tools/scylla-types: add shardof action
    Decorates a partition key and calculates which shard it belongs to,
    given the shard count (--shards) and the ignore msb bits
    (--ignore-msb-bits) parameters. The latter is optional and is defaulted to
    12.
    
    Example:
    
        $ scylla types shardof --full-compound -t UTF8Type -t SimpleDateType -t UUIDType --shards=7 000d66696c655f696e7374616e63650004800049190010c61a3321045941c38e5675255feb0196
        (file_instance, 2021-03-27, c61a3321-0459-41c3-8e56-75255feb0196): token: -5043005771368701888, shard: 1

--- tools/scylla-types.cc
+++ tools/scylla-types.cc
@@ -163,6 +163,35 @@ void tokenof_handler(type_variant type, std::vector<bytes> values, const bpo::va
     }
 }
 
+void shardof_handler(type_variant type, std::vector<bytes> values, const bpo::variables_map& vm) {
+    struct shardof_visitor {
+        bytes_view value;
+        const bpo::variables_map& vm;
+
+        void operator()(const data_type& type) {
+            throw std::invalid_argument("shardof action requires full-compound input");
+        }
+        void operator()(const compound_type<allow_prefixes::yes>& type) {
+            throw std::invalid_argument("shardof action requires full-compound input");
+        }
+        void operator()(const compound_type<allow_prefixes::no>& type) {
+            auto s = build_dummy_partition_key_schema(type);
+            auto pk = partition_key::from_bytes(value);
+            auto dk = dht::decorate_key(*s, pk);
+            auto shard = dht::shard_of(vm["shards"].as<unsigned>(), vm["ignore-msb-bits"].as<unsigned>(), dk.token());
+            fmt::print("{}: token: {}, shard: {}\n", to_printable_string(type, value), dk.token(), shard);
+        }
+    };
+
+    if (!vm.count("shards")) {
+        throw std::invalid_argument("error: missing mandatory argument --shards");
+    }
+
+    for (const auto& value : values) {
+        std::visit(shardof_visitor{value, vm}, type);
+    }
+}
+
 using action_handler_func = void(*)(type_variant, std::vector<bytes>, const bpo::variables_map& vm);
 
 class action_handler {
@@ -235,6 +264,19 @@ Examples:
 $ scylla types tokenof --full-compound -t UTF8Type -t SimpleDateType -t UUIDType 000d66696c655f696e7374616e63650004800049190010c61a3321045941c38e5675255feb0196
 (file_instance, 2021-03-27, c61a3321-0459-41c3-8e56-75255feb0196): -5043005771368701888
 )"},
+    {"shardof", "calculate which shard the partition-key belongs to", shardof_handler,
+R"(
+Decorate the key and calculate which shard its token belongs to.
+Only supports --full-compound. Use --shards and --ignore-msb-bits to specify
+sharding parameters.
+
+Arguments: 1 or more serialized values.
+
+Examples:
+
+$ scylla types shardof --full-compound -t UTF8Type -t SimpleDateType -t UUIDType --shards=7 000d66696c655f696e7374616e63650004800049190010c61a3321045941c38e5675255feb0196
+(file_instance, 2021-03-27, c61a3321-0459-41c3-8e56-75255feb0196): token: -5043005771368701888, shard: 1
+)"},
 };
 
 }
@@ -291,6 +333,8 @@ $ scylla types {{action}} --help
                 "note that the order of the types on the command line will be their order in the compound too")
         ("prefix-compound", "values are prefixable compounds (e.g. clustering key), composed of multiple values of possibly different types")
         ("full-compound", "values are full compounds (e.g. partition key), composed of multiple values of possibly different types")
+        ("shards", bpo::value<unsigned>(), "number of shards (only relevant for shardof action)")
+        ("ignore-msb-bits", bpo::value<unsigned>()->default_value(12), "number of shards (only relevant for shardof action)")
         ;
 
     app.add_positional_options({
