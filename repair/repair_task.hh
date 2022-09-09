576277384a53bf5a2e90d9eca2a6206be5f2bd87
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2022-09-09 10:13:29 +0200

    repair: create repair module and task
    Create repair_task_impl and repair_module inheriting from respectively
    task manager task_impl and module to integrate repair operations with
    task manager.

---
+++ repair/repair_task.hh
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2022-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "repair/repair.hh"
+#include "tasks/task_manager.hh"
+
+class repair_task_impl : public tasks::task_manager::task::impl {
+public:
+    repair_task_impl(tasks::task_manager::module_ptr module, tasks::task_id id, unsigned sequence_number, std::string keyspace, std::string table, std::string type, std::string entity, tasks::task_id parent_id)
+        : tasks::task_manager::task::impl(module, id, sequence_number, std::move(keyspace), std::move(table), std::move(type), std::move(entity), parent_id) {
+        _status.progress_units = "ranges";
+    }
+
+    virtual future<> run() override = 0;
+};
+
+class repair_module : public tasks::task_manager::module {
+private:
+    repair_service& _rs;
+public:
+    repair_module(tasks::task_manager& tm, repair_service& rs) noexcept : module(tm, "repair"), _rs(rs) {}
+
+    repair_service& get_repair_service() noexcept {
+        return _rs;
+    }
+};
