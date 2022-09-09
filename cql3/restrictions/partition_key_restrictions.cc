a4265abd8dd8bf8b3cb65c922c322b1428b9ca10
Jan Ciolek <jan.ciolek@scylladb.com>
2022-09-09 19:59:18 +0200

    restrictions: analyze partition_key_restrictions
    Perform analysis of the restrictions held inside
    partition_key_restrictions.
    
    They are split into three groups:
    * restrictions that allow to find a list of values for each
      partiiton key column.
    * restrictions that allow to find a range of token values.
    * restrictions used for filtering.
    
    The first two groups can be used
    to generate partition ranges.
    
    All restrictions that aren't used to
    generate partition ranges end up
    inside filtering_restrictions.
    
    Signed-off-by: Jan Ciolek <jan.ciolek@scylladb.com>

--- cql3/restrictions/partition_key_restrictions.cc
+++ cql3/restrictions/partition_key_restrictions.cc
@@ -37,6 +37,7 @@ partition_key_restrictions::partition_key_restrictions(expr::expression partitio
     assert_contains_only_partition_key_columns(_partition_restrictions);
 
     _single_column_partition_key_restrictions = expr::get_single_column_restrictions_map(_partition_restrictions);
+    analyze_restrictions();
 }
 
 const expr::expression& partition_key_restrictions::get_partition_key_restrictions() const {
@@ -51,6 +52,71 @@ const expr::single_column_restrictions_map& partition_key_restrictions::get_sing
 // Check if there are no partition key restrictions.
 bool partition_key_restrictions::partition_key_restrictions_is_empty() const {
     return is_empty_restriction(_partition_restrictions);
+}
+
+static bool contains_column(const expression& e) {
+    return find_in_expression<column_value>(e, [](const column_value&) { return true; }) != nullptr;
+}
+
+void partition_key_restrictions::analyze_restrictions() {
+    std::map<const column_definition*, std::vector<expression>> column_eq_restrictions;
+    std::vector<expression> token_range_restrictions;
+    std::vector<expression> filtering_restrictions;
+
+    for_each_boolean_factor(_partition_restrictions, [&](const expression& e) {
+        const binary_operator* binop = as_if<binary_operator>(&e);
+        if (binop == nullptr) {
+            filtering_restrictions.push_back(e);
+            return;
+        }
+
+        if (auto lhs_col = as_if<column_value>(&binop->lhs)) {
+            if (lhs_col->col->is_partition_key() && (binop->op == oper_t::EQ || binop->op == oper_t::IN) &&
+                !contains_column(binop->rhs)) {
+                column_eq_restrictions[lhs_col->col].push_back(e);
+                return;
+            }
+        }
+
+        if (auto lhs_token = as_if<token>(&binop->lhs)) {
+            if ((binop->op == oper_t::EQ || binop->op == oper_t::IN || is_slice(binop->op)) &&
+                !contains_column(binop->rhs)) {
+                token_range_restrictions.push_back(e);
+                return;
+            }
+        }
+
+        filtering_restrictions.push_back(e);
+    });
+
+    for (auto& [col_def, col_def_eq_restrictions] : column_eq_restrictions) {
+        _column_eq_restrictions[col_def] = conjunction{std::move(col_def_eq_restrictions)};
+    }
+
+    _token_range_restrictions = conjunction{std::move(token_range_restrictions)};
+
+    if (all_columns_have_eq_restrictions()) {
+        // _column_eq_restrictions will be used to generate partition ranges.
+        // Add token range restrictions to filtering restrictions.
+        // In the future it might be possible to do an intersection
+        // of ranges generated using both of these restrictions,
+        // but this isn't supported at the moment.
+        for_each_boolean_factor(_token_range_restrictions,
+                                [&](const expression& eq_restr) { filtering_restrictions.push_back(eq_restr); });
+    } else {
+        // Token range restrictions will be used to generate partition ranges.
+        // Add column equality restrictions to filtering restrictions.
+        for (auto&& [col_def, col_def_eq_restrictions] : _column_eq_restrictions) {
+            for_each_boolean_factor(col_def_eq_restrictions,
+                                    [&](const expression& eq_restr) { filtering_restrictions.push_back(eq_restr); });
+        }
+    }
+
+    _filtering_restrictions = conjunction{std::move(filtering_restrictions)};
+}
+
+bool partition_key_restrictions::all_columns_have_eq_restrictions() const {
+    return _column_eq_restrictions.size() == _table_schema->partition_key_size();
 }
 }  // namespace restrictions
 }  // namespace cql3
--- cql3/restrictions/partition_key_restrictions.hh
+++ cql3/restrictions/partition_key_restrictions.hh
@@ -20,6 +20,19 @@ class partition_key_restrictions {
     expr::expression _partition_restrictions;
     expr::single_column_restrictions_map _single_column_partition_key_restrictions;
 
+    // Analyzed restrictions
+
+    // Single column restrictions for each column which restrict it to a list of values.
+    // If each partition key column is restricted to a list of values
+    // then we can generate a number of token values as partition range.
+    expr::single_column_restrictions_map _column_eq_restrictions;
+
+    // Restrictions which restrict the partition token to a range of values.
+    expr::expression _token_range_restrictions;
+
+    // Restrictions that will be used for filtering.
+    expr::expression _filtering_restrictions;
+
 public:
     partition_key_restrictions(expr::expression partition_restrictions, schema_ptr table_schema);
 
@@ -31,6 +44,16 @@ public:
 
     // Check if there are no partition key restrictions.
     bool partition_key_restrictions_is_empty() const;
+
+private:
+    // Analyze _partition_restrictions to fill in _column_eq_restrictions, _token_range_restrictions
+    // and _filtering_restrictions.
+    void analyze_restrictions();
+
+    // If each partition key column has a restriction that restricts it to a list of values
+    // we can use these restrictions to generate a list of partition ranges.
+    // Otherwise these restrictions have to be filtered.
+    bool all_columns_have_eq_restrictions() const;
 };
 }  // namespace restrictions
 }  // namespace cql3
