8efea45f4af7c232fad6371a730cf23fe04f4c9b
Kamil Braun <kbraun@scylladb.com>
2022-09-29 13:56:26 +0200

    test/pylib: scylla_cluster: mark `server_remove` as not implemented
    The `server_remove` function did a very weird thing: it shut down a
    server and made the framework 'forget' about it. From the point of view
    of the Scylla cluster and the driver the server was still there.
    
    Replace the function's body with `raise NotImplementedError`. In the
    future it can be replaced with an implementation that calls
    `removenode` on the Scylla cluster.
    
    Remove `test_remove_server_add_column` from `test_topology`. It
    effectively does the same thing as `test_stop_server_add_column`, except
    that the framework also 'forgets' about the stopped server. This could
    lead to weird situations because the forgotten server's IP could be
    reused in another test that was running concurrently with this test.

--- test/topology/test_topology.py
+++ test/topology/test_topology.py
@@ -37,14 +37,3 @@ async def test_restart_server_add_column(manager, random_tables):
     ret = await manager.server_restart(servers[1])
     await table.add_column()
     await random_tables.verify_schema()
-
-
-@pytest.mark.asyncio
-async def test_remove_server_add_column(manager, random_tables):
-    """Add a node, remove an original node, add a column"""
-    servers = await manager.servers()
-    table = await random_tables.add_table(ncolumns=5)
-    await manager.server_add()
-    await manager.server_remove(servers[1])
-    await table.add_column()
-    await random_tables.verify_schema()
