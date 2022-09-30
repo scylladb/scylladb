50a3cc6b901702111fb7390e642644c9976b04ce
Botond Dénes <bdenes@scylladb.com>
2022-09-30 08:31:02 +0300

    compatible_ring_position_or_view: make it cheap to copy
    This class exists for one purpose only: to serve as glue code between
    dht::ring_position and boost::icl::interval_map. The latter requires
    that keys in its intervals are:
    * default constructible
    * copyable
    * have standalone compare operations
    
    For this reason we have to wrap `dht::ring_position` in a class,
    together with a schema to provide all this. This is
    `compatible_ring_position`. There is one further requirement by code
    using the interval map: it wants to do lookups without copying the
    lookup key(s). To solve this, we came up with
    `compatible_ring_position_or_view` which is a union of a key or a key
    view + schema. As we recently found out, boost::icl copies its keys **a
    lot**. It seems to assume these keys are cheap to copy and carelessly
    copies them around even when iterating over the map. But
    `compatible_ring_position_or_view` is not cheap to copy as it copies a
    `dht::ring_position` which allocates, and it does that via an
    `std::optional` and `std::variant` to add insult to injury.
    This patch make said class cheap to copy, by getting rid of the variant
    and storing the `dht::ring_position` via a shared pointer. The view is
    stored separately and either points to the ring position stored in the
    shared pointer or to an outside ring position (for lookups).
    
    Fixes: #11669
    
    Closes #11670
    
    (cherry picked from commit 169a8a66f282cb420197f446134c61760f1a087b)

--- test/boost/sstable_datafile_test.cc
+++ test/boost/sstable_datafile_test.cc
@@ -2740,7 +2740,7 @@ static dht::token token_from_long(int64_t value) {
 
 SEASTAR_TEST_CASE(basic_interval_map_testing_for_sstable_set) {
     using value_set = std::unordered_set<int64_t>;
-    using interval_map_type = boost::icl::interval_map<compatible_ring_position, value_set>;
+    using interval_map_type = boost::icl::interval_map<compatible_ring_position_or_view, value_set>;
     using interval_type = interval_map_type::interval_type;
 
     interval_map_type map;
@@ -2750,8 +2750,8 @@ SEASTAR_TEST_CASE(basic_interval_map_testing_for_sstable_set) {
                 .with_column("value", int32_type);
         auto s = builder.build();
 
-    auto make_pos = [&] (int64_t token) -> compatible_ring_position {
-        return compatible_ring_position(s, dht::ring_position::starting_at(token_from_long(token)));
+    auto make_pos = [&] (int64_t token) -> compatible_ring_position_or_view {
+        return compatible_ring_position_or_view(s, dht::ring_position::starting_at(token_from_long(token)));
     };
 
     auto add = [&] (int64_t start, int64_t end, int gen) {
