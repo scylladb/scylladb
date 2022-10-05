scylladb/scylladb

commit aba475fe1d24d5c2581bc5763f40924bbf688500

storage_service: Remove bool from do_drain
The do_drain() today tells shutdown drain from API drain. The reason
is that compaction manager subscribes on the main's abort signal and
drains itself early. Thus, on regular drain it needs this extra kick
that would crash if called from shutdown drain.

This differentiation should sit in the compaction manager itself.

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

compaction/compaction_manager.cc
@@ -508,6 +508,10 @@ future<> compaction_manager::stop_ongoing_compactions(sstring reason) {
 }
 
 future<> compaction_manager::drain() {
+    if (!*_early_abort_subscription) {
+        return make_ready_future<>();
+    }
+
     _state = state::disabled;
     return stop_ongoing_compactions("drain");
 }

service/storage_service.cc
@@ -1304,7 +1304,7 @@ future<> storage_service::stop_transport() {
 future<> storage_service::drain_on_shutdown() {
     assert(this_shard_id() == 0);
     return (_operation_mode == mode::DRAINING || _operation_mode == mode::DRAINED) ?
-        _drain_finished.get_future() : do_drain(true);
+        _drain_finished.get_future() : do_drain();
 }
 
 future<> storage_service::init_messaging_service_part() {
@@ -2645,25 +2645,23 @@ future<> storage_service::drain() {
         }
 
         ss.set_mode(mode::DRAINING, "starting drain process", true);
-        return ss.do_drain(false).then([&ss] {
+        return ss.do_drain().then([&ss] {
             ss._drain_finished.set_value();
             ss.set_mode(mode::DRAINED, true);
         });
     });
 }
 
-future<> storage_service::do_drain(bool on_shutdown) {
-    return seastar::async([this, on_shutdown] {
+future<> storage_service::do_drain() {
+    return seastar::async([this] {
         stop_transport().get();
 
         tracing::tracing::tracing_instance().invoke_on_all(&tracing::tracing::shutdown).get();
 
-        if (!on_shutdown) {
-            // Interrupt on going compaction and shutdown to prevent further compaction
-            _db.invoke_on_all([] (auto& db) {
-                return db.get_compaction_manager().drain();
-            }).get();
-        }
+        // Interrupt on going compaction and shutdown to prevent further compaction
+        _db.invoke_on_all([] (auto& db) {
+            return db.get_compaction_manager().drain();
+        }).get();
 
         set_mode(mode::DRAINING, "flushing column families", false);
         flush_column_families();

service/storage_service.hh
@@ -802,7 +802,7 @@ public:
 private:
     promise<> _drain_finished;
     std::optional<shared_promise<>> _transport_stopped;
-    future<> do_drain(bool on_shutdown);
+    future<> do_drain();
     /**
      * Seed data to the endpoints that will be responsible for it at the future
      *

commit 1560d5874c8a115b1b5ed6a78559d4f96e6e2c39

compaction: Make compaction_manager stop more robust
Commit aba475fe1d24d5c accidentally fixed a race, which happens in
the following sequence of events:

1) storage service starts drain() via API for example
2) main's abort source is triggered, calling compaction_manager's do_stop()
via subscription.
        2.1) do_stop() initiates the stop but doesn't wait for it.
        2.2) compaction_manager's state is set to stopped, such that
        compaction_manager::stop() called in defer_verbose_shutdown()
        will wait for the stop and not start a new one.
3) drain() calls compaction_manager::drain() changing the state from
stopped to disabled.
4) main calls compaction_manager::stop() (as described in 2.2) and
incorrectly tries to stop the manager again, because the state was
changed in step 3.

aba475fe1d24d5c accidentally fixed this problem because drain() will no
longer take place if it detects the shutdown process was initiated
(it does so by ignoring drain request if abort source's subscription
was unlinked).

This shows us that looking at the state to determine if stop should be
performed is fragile, because once the state changes from A to B,
manager doesn't know the state was A. To make it robust, we can instead
check if the future that stores stop's promise is engaged, meaning that
the stop was already initiated and we don't have to start a new one.

Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

compaction/compaction_manager.cc
@@ -863,7 +863,7 @@ auto swallow_enospc(const Ex& ex) noexcept {
 }
 
 void compaction_manager::do_stop() noexcept {
-    if (_state == state::none || _state == state::stopped) {
+    if (_state == state::none || _stop_future) {
         return;
     }
 

test/boost/sstable_compaction_test.cc
@@ -5232,3 +5232,22 @@ SEASTAR_TEST_CASE(check_table_sstable_set_includes_maintenance_sstables) {
         BOOST_REQUIRE(cf->get_sstable_set().all()->size() == 1);
     });
 }
+
+// Without commit aba475fe1d24d5c, scylla will fail miserably (either with abort or segfault; depends on the version).
+SEASTAR_TEST_CASE(compaction_manager_stop_and_drain_race_test) {
+    abort_source as;
+
+    auto cfg = compaction_manager::config{ .available_memory = 1 };
+
+    auto cm = compaction_manager(cfg, as);
+    cm.enable();
+
+    testlog.info("requesting abort");
+    as.request_abort();
+
+    testlog.info("draining compaction manager");
+    co_await cm.drain();
+
+    testlog.info("stopping compaction manager");
+    co_await cm.stop();
+}