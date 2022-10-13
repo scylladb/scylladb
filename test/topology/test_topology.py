954849799dc3ebfd27457ab027aa8b88c8872b4c
Kamil Braun <kbraun@scylladb.com>
2022-10-13 17:45:44 +0200

    test/topology: disable flaky `test_decommission_add_column`
    Flaky due to #11780, causes next promotion failures.
    We can reenable it after the issue is fixed or a workaround is found.

--- test/topology/test_topology.py
+++ test/topology/test_topology.py
@@ -53,6 +53,7 @@ async def test_remove_node_add_column(manager, random_tables):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip(reason="Flaky due to #11780")
 async def test_decommission_node_add_column(manager, random_tables):
     """Add a node, remove an original node, add a column"""
     servers = await manager.running_servers()
