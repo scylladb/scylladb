580a8bf92daf36a7698f377531695df82106a042
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2022-10-20 17:31:12 +0200

    tasks: move task_id to tasks/types.hh

---
+++ tasks/types.hh
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2022-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "utils/UUID.hh"
+
+namespace tasks {
+
+using task_id = utils::tagged_uuid<struct task_id_tag>;
+
+}
