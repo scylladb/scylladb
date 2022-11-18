286f182a8c6c8f6dfa739dc2ea49ee2cbe6f7058
Jan Ciolek <jan.ciolek@scylladb.com>
2022-11-18 15:27:42 +0100

    cql-pytest: add a reproducer for #12014, verify that filtering multi column and regular restrictions works
    In issue #12014 a user has encountered an instance of #6200.
    When filtering a WHERE clause which contained
    both multi-column and regular restrictions,
    the regular restrictions were ignored.
    
    Add a test which reproduces the issue
    using a reproducer provided by the user.
    
    This problem is tested in another similar test,
    but this one reproduces the issue in the exact
    way it was found by the user.
    
    Signed-off-by: Jan Ciolek <jan.ciolek@scylladb.com>

--- test/cql-pytest/test_scan.py
+++ test/cql-pytest/test_scan.py
@@ -77,6 +77,24 @@ def test_multi_column_restrictions_and_filtering(cql, test_keyspace):
         # Reproduces #6200:
         assert list(cql.execute(f"SELECT c1,c2,r FROM {table} WHERE p=1 AND (c1, c2) = (0,1) AND r=0 ALLOW FILTERING")) == []
 
+# Test that if we have a range multi-column restrictions on the clustering key
+# and additional filtering on regular columns, both restrictions are obeyed.
+# Similar to test_multi_column_restrictions_and_filtering, but uses a range
+# restriction on the clustering key columns.
+# Reproduces #12014, the code is taken from a reproducer provided by a user.
+def test_multi_column_range_restrictions_and_filtering(cql, test_keyspace):
+    with new_test_table(cql, test_keyspace, "pk int, ts timestamp, id int, processed boolean, PRIMARY KEY (pk, ts, id)") as table:
+        cql.execute(f"INSERT INTO {table} (pk, ts, id, processed) VALUES (0, currentTimestamp(), 0, true)")
+        cql.execute(f"INSERT INTO {table} (pk, ts, id, processed) VALUES (0, currentTimestamp(), 1, true)")
+        cql.execute(f"INSERT INTO {table} (pk, ts, id, processed) VALUES (0, currentTimestamp(), 2, false)")
+        cql.execute(f"INSERT INTO {table} (pk, ts, id, processed) VALUES (0, currentTimestamp(), 3, false)")
+        # This select doesn't use multi-column restrictions, the result shouldn't change when it does.
+        rows1 = list(cql.execute(f"SELECT id, processed FROM {table} WHERE pk = 0 AND ts >= 0 AND processed = false ALLOW FILTERING"))
+        assert rows1 == [(2, False), (3, False)]
+        # Reproduces #12014
+        rows2 = list(cql.execute(f"SELECT id, processed FROM {table} WHERE pk = 0 AND (ts, id) >= (0, 0) AND processed = false ALLOW FILTERING"))
+        assert rows1 == rows2
+
 # Like the previous test, just in cases that require ALLOW FILTERING.
 # We add another clustering key column to ensure that filtering *in*
 # a long partition is really needed - not just filtering on the partitions
