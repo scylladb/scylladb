c61083852cb68c504a9d487ba0d7885af19cce1b
Benny Halevy <bhalevy@scylladb.com>
2022-11-25 19:15:09 +0200

    storage_service: handle_state_normal: calculate candidates_for_removal when replacing tokens
    We currently try to detect a replaced node so to insert it to
    endpoints_to_remove when it has no owned tokens left.
    However, for each token we first generate a multimap using
    get_endpoint_to_token_map_for_reading().
    
    There are 2 problems with that:
    
    1. unless the replaced node owns a single token, this map will not
       be empty after erasing one token out of it, since the
       token metadata has not changed yet (this is done later with
       update_normal_tokens(owned_tokens, endpoint)).
    2. generating this map for each token is inefficient, turning this
       algorithm complexity to quadratic in the number of tokens...
    
    This change copies the current token_to_endpoint map
    to temporary map and erases replaced tokens from it,
    while maintaining a set of candidates_for_removal.
    
    After traversing all replaced tokens, we check again
    the `token_to_endpoint_map` erasing from `candidates_for_removal`
    any endpoint that still owns tokens.
    The leftover candidates are endpoints the own no tokens
    and so they are added to `hosts_to_remove`.
    
    Fixes #12082
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    
    Closes #12141

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -940,37 +940,61 @@ future<> storage_service::handle_state_normal(inet_address endpoint) {
     // the collision is resolved by assigning the token to the endpoint which started later.
     std::unordered_set<token> owned_tokens;
 
+    // token_to_endpoint_map is used to track the current token owners for the purpose of removing replaced endpoints.
+    // when any token is replaced by a new owner, we track the existing owner in `candidates_for_removal`
+    // and eventually, if any candidate for removal ends up owning no tokens, it is removed from token_metadata.
+    std::unordered_map<token, inet_address> token_to_endpoint_map = get_token_metadata().get_token_to_endpoint();
+    std::unordered_set<inet_address> candidates_for_removal;
+
     for (auto t : tokens) {
         // we don't want to update if this node is responsible for the token and it has a later startup time than endpoint.
-        auto current_owner = tmptr->get_endpoint(t);
-        if (!current_owner) {
+        auto current = token_to_endpoint_map.find(t);
+        if (current == token_to_endpoint_map.end()) {
             slogger.debug("handle_state_normal: New node {} at token {}", endpoint, t);
             owned_tokens.insert(t);
-        } else if (endpoint == *current_owner) {
-            slogger.debug("handle_state_normal: endpoint={} == current_owner={} token {}", endpoint, *current_owner, t);
+            continue;
+        }
+        auto current_owner = current->second;
+        if (endpoint == current_owner) {
+            slogger.debug("handle_state_normal: endpoint={} == current_owner={} token {}", endpoint, current_owner, t);
             // set state back to normal, since the node may have tried to leave, but failed and is now back up
             owned_tokens.insert(t);
-        } else if (_gossiper.compare_endpoint_startup(endpoint, *current_owner) > 0) {
-            slogger.debug("handle_state_normal: endpoint={} > current_owner={}, token {}", endpoint, *current_owner, t);
+        } else if (_gossiper.compare_endpoint_startup(endpoint, current_owner) > 0) {
+            slogger.debug("handle_state_normal: endpoint={} > current_owner={}, token {}", endpoint, current_owner, t);
             owned_tokens.insert(t);
+            slogger.info("handle_state_normal: remove endpoint={} token={}", current_owner, t);
             // currentOwner is no longer current, endpoint is.  Keep track of these moves, because when
             // a host no longer has any tokens, we'll want to remove it.
-            std::multimap<inet_address, token> ep_to_token_copy = get_token_metadata().get_endpoint_to_token_map_for_reading();
-            auto rg = ep_to_token_copy.equal_range(*current_owner);
-            for (auto it = rg.first; it != rg.second; it++) {
-                if (it->second == t) {
-                    slogger.info("handle_state_normal: remove endpoint={} token={}", *current_owner, t);
-                    ep_to_token_copy.erase(it);
+            token_to_endpoint_map.erase(current);
+            candidates_for_removal.insert(current_owner);
+            slogger.info("handle_state_normal: Nodes {} and {} have the same token {}. {} is the new owner", endpoint, current_owner, t, endpoint);
+        } else {
+            // current owner of this token is kept and endpoint attempt to own it is rejected.
+            // Keep track of these moves, because when a host no longer has any tokens, we'll want to remove it.
+            token_to_endpoint_map.erase(current);
+            candidates_for_removal.insert(endpoint);
+            slogger.info("handle_state_normal: Nodes {} and {} have the same token {}. Ignoring {}", endpoint, current_owner, t, endpoint);
+        }
+    }
+
+    // After we replace all tokens owned by current_owner
+    // We check for each candidate for removal if it still owns any tokens,
+    // and remove it if it doesn't anymore.
+    if (!candidates_for_removal.empty()) {
+        for (const auto& [t, ep] : token_to_endpoint_map) {
+            if (candidates_for_removal.contains(ep)) {
+                slogger.debug("handle_state_normal: endpoint={} still owns tokens, will not be removed", ep);
+                candidates_for_removal.erase(ep);
+                if (candidates_for_removal.empty()) {
+                    break;
                 }
             }
-            if (!ep_to_token_copy.contains(*current_owner)) {
-                slogger.info("handle_state_normal: endpoints_to_remove endpoint={}", *current_owner);
-                endpoints_to_remove.insert(*current_owner);
-            }
-            slogger.info("handle_state_normal: Nodes {} and {} have the same token {}. {} is the new owner", endpoint, *current_owner, t, endpoint);
-        } else {
-            slogger.info("handle_state_normal: Nodes {} and {} have the same token {}. Ignoring {}", endpoint, *current_owner, t, endpoint);
         }
+    }
+
+    for (const auto& ep : candidates_for_removal) {
+        slogger.info("handle_state_normal: endpoints_to_remove endpoint={}", ep);
+        endpoints_to_remove.insert(ep);
     }
 
     bool is_normal_token_owner = tmptr->is_normal_token_owner(endpoint);
