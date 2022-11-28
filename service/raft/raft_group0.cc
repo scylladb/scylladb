4429885543282c48fdbc4041b036bce67fee7f81
Kamil Braun <kbraun@scylladb.com>
2022-11-28 16:58:38 +0100

    service: handle replace correctly with Raft enabled
    We must place the Raft ID obtained during the shadow round in the
    address map. It won't be placed by the regular gossiping route if we're
    replacing using the same IP, because we override the application state
    of the replaced node. Even if we replace a node with a different IP, it
    is not guaranteed that background gossiping manages to update the
    address map before we need it, especially in tests where we set
    ring_delay to 0 and disable wait_for_gossip_to_settle. The shadow round,
    on the other hand, performs a synchronous request (and if it fails
    during bootstrap, bootstrap will fail - because we also won't be able to
    obtain the tokens and Host ID of the replaced node).
    
    Fetch the Raft ID of the replaced node in `prepare_replacement_info`,
    which runs the shadow round. Return it in `replacement_info`. Then
    `join_token_ring` passes it to `setup_group0`, which stores it in the
    address map. It does that after `join_group0` so the entry is
    non-expiring (the replaced node is a member of group 0). Later in the
    replace procedure, we call `remove_from_group0` for the replaced node.
    `remove_from_group0` will be able to reverse-translate the IP of the
    replaced node to its Raft ID using the address map.

--- service/raft/raft_group0.cc
+++ service/raft/raft_group0.cc
@@ -488,7 +488,9 @@ static future<bool> synchronize_schema(
         const noncopyable_function<future<bool>()>& can_finish_early,
         abort_source&);
 
-future<> raft_group0::setup_group0(db::system_keyspace& sys_ks, const std::unordered_set<gms::inet_address>& initial_contact_nodes) {
+future<> raft_group0::setup_group0(
+        db::system_keyspace& sys_ks, const std::unordered_set<gms::inet_address>& initial_contact_nodes,
+        std::optional<replace_info> replace_info) {
     assert(this_shard_id() == 0);
 
     if (!_raft_gr.is_enabled()) {
@@ -524,6 +526,13 @@ future<> raft_group0::setup_group0(db::system_keyspace& sys_ks, const std::unord
         co_return;
     }
 
+    if (replace_info && !replace_info->raft_id) {
+        auto msg = format("Cannot perform replace operation: Raft ID of the replaced server (IP: {}) is missing.",
+                          replace_info->ip_addr);
+        group0_log.error("{}", msg);
+        throw std::runtime_error{std::move(msg)};
+    }
+
     std::vector<gms::inet_address> seeds;
     for (auto& addr: initial_contact_nodes) {
         if (addr != _gossiper.get_broadcast_address()) {
@@ -534,6 +543,25 @@ future<> raft_group0::setup_group0(db::system_keyspace& sys_ks, const std::unord
     group0_log.info("setup_group0: joining group 0...");
     co_await join_group0(std::move(seeds), false /* non-voter */);
     group0_log.info("setup_group0: successfully joined group 0.");
+
+    if (replace_info) {
+        // Insert the replaced node's (Raft ID, IP address) pair into `raft_address_map`.
+        // In general, the mapping won't be obtained through the regular gossiping route:
+        // if we (the replacing node) use the replaced node's IP address, the replaced node's
+        // application states are gone by this point (our application states overrode them
+        // - the application states map is using the IP address as the key).
+        // Even when we use a different IP, there's no guarantee the IPs were exchanged by now.
+        // Instead, we obtain `replace_info` during the shadow round (which guarantees to contact
+        // another node and fetch application states from it) and pass it to `setup_group0`.
+
+        // Checked earlier.
+        assert(replace_info->raft_id);
+        group0_log.info("Replacing a node with Raft ID: {}, IP address: {}",
+                        *replace_info->raft_id, replace_info->ip_addr);
+
+        // `opt_add_entry` is shard-local, but that's fine - we only need this info on shard 0.
+        _raft_gr.address_map().opt_add_entry(*replace_info->raft_id, replace_info->ip_addr);
+    }
 
     // Enter `synchronize` upgrade state in case the cluster we're joining has recently enabled Raft
     // and is currently in the middle of `upgrade_to_group0()`. For that procedure to finish
