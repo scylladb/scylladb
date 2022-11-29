d35cf88f0981ab64d78292c37a07974c1dcf82a8
Raphael S. Carvalho <raphaelsc@scylladb.com>
2022-11-29 16:08:44 -0300

    replica: database: stop and disable compaction on behalf of all groups
    With compaction group model, truncate_table_on_all_shards() needs
    to stop and disable compaction for all groups.
    replica::table::as_table_state() will be removed once no user
    remains, as each table may map to multiple groups.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- replica/database.cc
+++ replica/database.cc
@@ -2335,10 +2335,14 @@ future<> database::truncate_table_on_all_shards(sharded<database>& sharded_db, c
             st->cres.reserve(1 + cf.views().size());
             auto& db = sharded_db.local();
             auto& cm = db.get_compaction_manager();
-            st->cres.emplace_back(co_await cm.stop_and_disable_compaction(cf.as_table_state()));
+            co_await cf.parallel_foreach_table_state([&cm, &st] (compaction::table_state& ts) -> future<> {
+                st->cres.emplace_back(co_await cm.stop_and_disable_compaction(ts));
+            });
             co_await coroutine::parallel_for_each(cf.views(), [&] (view_ptr v) -> future<> {
                 auto& vcf = db.find_column_family(v);
-                st->cres.emplace_back(co_await cm.stop_and_disable_compaction(vcf.as_table_state()));
+                co_await vcf.parallel_foreach_table_state([&cm, &st] (compaction::table_state& ts) -> future<> {
+                    st->cres.emplace_back(co_await cm.stop_and_disable_compaction(ts));
+                });
             });
 
             co_return make_foreign(std::move(st));
