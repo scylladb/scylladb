63e1d33da1906f21d6c5f0f969cb00179593748a
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-11-29 20:18:32 +0100

    WIP no repro

--- test/topology/test_alter_delete_regression.py
+++ test/topology/test_alter_delete_regression.py
@@ -15,10 +15,10 @@ import pytest
 logger = logging.getLogger("test_alter_delete")
 
 
-LOOPS          =     4    # Attempts to trigger failure
-NROWS          =  1000    # Total rows on table
-MAX_S          =    10.0  # Max run seconds
-ALTER_TABLE_S  =     0.1  # Alter table at 100ms
+LOOPS          =     4     # Attempts to trigger failure
+INITIAL_ROWS   =    10     # Total rows on table
+MAX_S          =  600.0   # Max run seconds
+ALTER_TABLE_S  =     0.001 # Alter table every 1ms
 
 
 @pytest.mark.asyncio
@@ -32,26 +32,32 @@ async def test_new_table(manager, random_tables):
                                                    Column('v', IntType),
                                                    Column('d1', IntType),
                                                    Column('d2', IntType)])
-    await asyncio.gather(*(table.insert_seq() for _ in range(NROWS)))
+    await asyncio.gather(*(table.insert_seq() for _ in range(INITIAL_ROWS)))
     logger.debug("Done prologue")
 
-    async def delete_rows():
+    async def delete_add_rows():
         logger.debug("delete_rows: start")
-        for i in range(0, NROWS - 1):
+        for i in range(2**22):
             table.delete_pk(i)
             await asyncio.sleep(.001)
+            table.insert_seq()
         logger.debug("delete_rows: done")
 
     async def alter_table():
-        await asyncio.sleep(ALTER_TABLE_S)
         logger.debug("alter_table: start")
-        await table.drop_column("d2")
-        await table.add_column(Column("d2", BigIntType))
+        ctypes = [BigIntType, IntType]
+        for i in range(0, INITIAL_ROWS - 1):
+            await asyncio.sleep(ALTER_TABLE_S)
+            logger.debug(f"XXX drop d2 iter {i}")
+            await table.drop_column("d2")
+            logger.debug(f"XXX adding d2 iter {i}: type {i % 2}")
+            await table.add_column("d2", ctypes[i % 2])
         logger.debug("alter_table: done")
 
     # Test woud fail with asyncio.TimeoutError on regression
     # TODO: change to asyncio.timeout for Python 3.11
     try:
-        await asyncio.wait_for(asyncio.gather(alter_table(), delete_rows()), timeout = MAX_S)
+        await asyncio.wait_for(asyncio.gather(alter_table(), delete_add_rows()), timeout = MAX_S)
     except asyncio.TimeoutError as exc:
-        raise RuntimeError("LWT regression: deletes hang on alter table") from exc
+        pass
+        # raise RuntimeError("LWT regression: deletes hang on alter table") from exc
