89a1fb89a1d34d62c30de7cce8549674ab7cccc2
Benny Halevy <bhalevy@scylladb.com>
2022-12-04 08:51:05 +0200

    view: row_locker: lock_ck: simplify error handling
    There is no need to start locking the partition
    early before we may create a map entry for the row
    since we do that synchronously without yielding.
    
    Extend the try catch block to the whole function body
    to convert any exception to an exceptional future.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- db/view/row_locking.cc
+++ db/view/row_locking.cc
@@ -78,33 +78,20 @@ row_locker::lock_pk(const dht::decorated_key& pk, bool exclusive, db::timeout_cl
 
 future<row_locker::lock_holder>
 row_locker::lock_ck(const dht::decorated_key& pk, const clustering_key_prefix& cpk, bool exclusive, db::timeout_clock::time_point timeout, stats& stats) {
+    try {
+    // FIXME: indentation
     mylog.debug("taking shared lock on partition {}, and {} lock on row {} in it", pk, (exclusive ? "exclusive" : "shared"), cpk);
     auto i = _two_level_locks.try_emplace(pk, this).first;
-    future<lock_type::holder> lock_partition = i->second._partition_lock.hold_read_lock(timeout);
     auto j = i->second._row_locks.find(cpk);
     if (j == i->second._row_locks.end()) {
         // Not yet locked, need to create the lock. This makes a copy of cpk.
-        try {
             j = i->second._row_locks.emplace(cpk, lock_type()).first;
-        } catch(...) {
-            // If this emplace() failed, e.g., out of memory, we fail. We
-            // could do nothing - the partition lock we already started
-            // taking will be unlocked automatically after being locked.
-            // But it's better form to wait for the work we started, and it
-            // will also allow us to remove the hash-table row we added.
-            return lock_partition.then([ex = std::current_exception()] (auto lock) {
-                // The lock is automatically released when "lock" goes out of scope.
-                // TODO: unlock (lock = {}) now, search for the partition in the
-                // hash table (we know it's still there, because we held the lock until
-                // now) and remove the unused lock from the hash table if still unused.
-                return make_exception_future<row_locker::lock_holder>(std::current_exception());
-            });
-        }
     }
     single_lock_stats &single_lock_stats = exclusive ? stats.exclusive_row : stats.shared_row;
     single_lock_stats.operations_currently_waiting_for_lock++;
     utils::latency_counter waiting_latency;
     waiting_latency.start();
+    future<lock_type::holder> lock_partition = i->second._partition_lock.hold_read_lock(timeout);
     future<lock_type::holder> lock_row = exclusive ? j->second.hold_write_lock(timeout) : j->second.hold_read_lock(timeout);
     return when_all_succeed(std::move(lock_partition), std::move(lock_row))
     .then_unpack([this, pk = &i->first, cpk = &j->first, exclusive, &single_lock_stats, waiting_latency = std::move(waiting_latency)] (auto lock1, auto lock2) mutable {
@@ -116,6 +103,9 @@ row_locker::lock_ck(const dht::decorated_key& pk, const clustering_key_prefix& c
         single_lock_stats.operations_currently_waiting_for_lock--;
         return lock_holder(this, pk, cpk, exclusive ? lock_state::exclusive : lock_state::shared);
     });
+    } catch (...) {
+        return make_exception_future<row_locker::lock_holder>(std::current_exception());
+    }
 }
 
 row_locker::lock_holder::lock_holder(row_locker::lock_holder&& old) noexcept
