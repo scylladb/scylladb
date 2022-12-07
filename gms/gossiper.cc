1dfeb308bc01d4c3c26d226292b62732a83fea35
Amnon Heiman <amnon@scylladb.com>
2022-12-07 18:45:02 +0200

    gms/gossiper.cc: label metrics with basic_level
    The following metrics will be marked with basic_level label:
    scylla_gossip_heart_beat
    scylla_gossip_live
    scylla_gossip_unreachable
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>

--- gms/gossiper.cc
+++ gms/gossiper.cc
@@ -46,6 +46,7 @@
 #include "utils/error_injection.hh"
 #include "idl/gossip.dist.hh"
 #include <csignal>
+#include "utils/labels.hh"
 
 namespace gms {
 
@@ -112,15 +113,15 @@ gossiper::gossiper(abort_source& as, const locator::shared_token_metadata& stm, 
                 } else {
                     return 0;
                 }
-            }, sm::description("Heartbeat of the current Node.")),
+            }, sm::description("Heartbeat of the current Node."))(basic_level),
         sm::make_gauge("live",
             [this] {
                 return _live_endpoints.size();
-            }, sm::description("How many live nodes the current node sees")),
+            }, sm::description("How many live nodes the current node sees"))(basic_level),
         sm::make_gauge("unreachable",
             [this] {
                 return _unreachable_endpoints.size();
-            }, sm::description("How many unreachable nodes the current node sees")),
+            }, sm::description("How many unreachable nodes the current node sees"))(basic_level),
     });
 
     // Add myself to the map on start
