5dbe2363395f7ef89f44cf5d7a43b31ec6a8fc1b
Kamil Braun <kbraun@scylladb.com>
2022-12-12 14:15:17 +0100

    Revert "gms/gossiper: fetch RAFT_SERVER_ID during shadow round"
    This reverts commit 60217d7f50cd47449a8ffb39c92d742db246b35b.
    We no longer need RAFT_SERVER_ID.

--- gms/gossiper.cc
+++ gms/gossiper.cc
@@ -1895,8 +1895,7 @@ future<> gossiper::do_shadow_round(std::unordered_set<gms::inet_address> nodes) 
             gms::application_state::DC,
             gms::application_state::RACK,
             gms::application_state::SUPPORTED_FEATURES,
-            gms::application_state::SNITCH_NAME,
-            gms::application_state::RAFT_SERVER_ID}};
+            gms::application_state::SNITCH_NAME}};
         logger.info("Gossip shadow round started with nodes={}", nodes);
         std::unordered_set<gms::inet_address> nodes_talked;
         size_t nodes_down = 0;
