90faeedb777576e2a59c9534c93d6b8f5d606361
Benny Halevy <bhalevy@scylladb.com>
2022-12-14 15:13:16 +0200

    test: test_topology: test replace using host_id
    Add test cases exercising the --replace-node-first-boot option
    by replacing nodes using their host_id rather
    than ip address.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/pylib/scylla_cluster.py
+++ test/pylib/scylla_cluster.py
@@ -44,6 +44,7 @@ from cassandra.policies import WhiteListRoundRobinPolicy  # type: ignore
 class ReplaceConfig(NamedTuple):
     replaced_id: ServerNum
     reuse_ip_addr: bool
+    use_host_id: bool
 
 
 def make_scylla_conf(workdir: pathlib.Path, host_addr: str, seed_addrs: List[str], cluster_name: str) -> dict[str, object]:
@@ -604,7 +605,10 @@ class ScyllaCluster:
                 f"add_server: replaced id {replaced_id} not found in existing servers"
 
             replaced_srv = self.servers[replaced_id]
-            extra_config['replace_address_first_boot'] = replaced_srv.ip_addr
+            if replace_cfg.use_host_id:
+                extra_config['replace_node_first_boot'] = replaced_srv.host_id
+            else:
+                extra_config['replace_address_first_boot'] = replaced_srv.ip_addr
 
             assert replaced_id not in self.removed, \
                 f"add_server: cannot replace removed server {replaced_srv}"
