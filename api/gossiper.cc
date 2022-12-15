d3995a05d8c38a8fd5bc729ee5c698734dc08a39
Kamil Braun <kbraun@scylladb.com>
2022-12-15 11:55:41 +0100

    api: gossiper: allow querying specific shard for alive/dead nodes
    Introduce an optional "shard" parameter to `/gossiper/endpoint/live/`
    and `/gossiper/endpoint/down/` endpoints which allows to query the
    live/dead node set on a specific shard. If "shard" is not provided we
    query shard 0 as before.
    
    We'll use it in tests to wait until nodes mark other nodes as dead/alive
    on every shard, e.g. before starting an operation which requires
    some other nodes to be alive. Waiting on shard 0 only does not guarantee
    that other shards have the same information yet (it takes a while for
    this information to replicate between shards).

--- api/gossiper.cc
+++ api/gossiper.cc
@@ -13,15 +13,41 @@
 namespace api {
 using namespace json;
 
+static unsigned parse_shard_number(const std::string& str) {
+    try {
+        long s = std::stol(str);
+        if (s < 0 || s >= smp::count) {
+            throw std::runtime_error{format(
+                "Shard number {} outside valid shard range [0, {})", s, smp::count)};
+        }
+        return s;
+    } catch (...) {
+        throw std::runtime_error{format(
+            "Invalid shard number \"{}\": {}", str, std::current_exception())};
+    }
+}
+
 void set_gossiper(http_context& ctx, routes& r, gms::gossiper& g) {
-    httpd::gossiper_json::get_down_endpoint.set(r, [&g] (const_req req) {
-        auto res = g.get_unreachable_members();
-        return container_to_vec(res);
+    httpd::gossiper_json::get_down_endpoint.set(r, [&g] (std::unique_ptr<request> req) {
+        std::string shard_str = req->get_query_param("shard");
+        unsigned shard = shard_str.empty() ? 0 : parse_shard_number(shard_str);
+
+        return g.container().invoke_on(shard, [] (gms::gossiper& g) {
+            return g.get_unreachable_members();
+        }).then([] (std::set<gms::inet_address> s) {
+            return make_ready_future<json::json_return_type>(container_to_vec(s));
+        });
     });
 
-    httpd::gossiper_json::get_live_endpoint.set(r, [&g] (const_req req) {
-        auto res = g.get_live_members();
-        return container_to_vec(res);
+    httpd::gossiper_json::get_live_endpoint.set(r, [&g] (std::unique_ptr<request> req) {
+        std::string shard_str = req->get_query_param("shard");
+        unsigned shard = shard_str.empty() ? 0 : parse_shard_number(shard_str);
+
+        return g.container().invoke_on(shard, [] (gms::gossiper& g) {
+            return g.get_live_members();
+        }).then([] (std::set<gms::inet_address> s) {
+            return make_ready_future<json::json_return_type>(container_to_vec(s));
+        });
     });
 
     httpd::gossiper_json::get_endpoint_downtime.set(r, [&g] (const_req req) {
