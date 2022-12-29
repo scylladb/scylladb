690697961c73823436eebfdc4e3d7e8d7d60312e
Benny Halevy <bhalevy@scylladb.com>
2022-12-29 15:52:03 +0200

    compaction_manager: compacting_sstable_registration: keep a ref to the compaction_state
    To be used for managing sstables requiring cleanup.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- compaction/compaction_manager.cc
+++ compaction/compaction_manager.cc
@@ -32,14 +32,16 @@ using namespace compaction;
 
 class compacting_sstable_registration {
     compaction_manager& _cm;
+    compaction_manager::compaction_state& _cs;
     std::unordered_set<sstables::shared_sstable> _compacting;
 public:
-    explicit compacting_sstable_registration(compaction_manager& cm) noexcept
+    explicit compacting_sstable_registration(compaction_manager& cm, compaction_manager::compaction_state& cs) noexcept
         : _cm(cm)
+        , _cs(cs)
     { }
 
-    compacting_sstable_registration(compaction_manager& cm, std::vector<sstables::shared_sstable> compacting)
-        : compacting_sstable_registration(cm)
+    compacting_sstable_registration(compaction_manager& cm, compaction_manager::compaction_state& cs, std::vector<sstables::shared_sstable> compacting)
+        : compacting_sstable_registration(cm, cs)
     {
         register_compacting(compacting);
     }
@@ -57,6 +59,7 @@ public:
 
     compacting_sstable_registration(compacting_sstable_registration&& other) noexcept
         : _cm(other._cm)
+        , _cs(other._cs)
         , _compacting(std::move(other._compacting))
     { }
 
@@ -422,7 +425,7 @@ protected:
         table_state* t = _compacting_table;
         sstables::compaction_strategy cs = t->get_compaction_strategy();
         sstables::compaction_descriptor descriptor = cs.get_major_compaction_job(*t, _cm.get_candidates(*t));
-        auto compacting = compacting_sstable_registration(_cm, descriptor.sstables);
+        auto compacting = compacting_sstable_registration(_cm, _cm.get_compaction_state(t), descriptor.sstables);
         auto release_exhausted = [&compacting] (const std::vector<sstables::shared_sstable>& exhausted_sstables) {
             compacting.release_compacting(exhausted_sstables);
         };
@@ -1020,7 +1023,7 @@ protected:
                 _cm.postpone_compaction_for_table(&t);
                 co_return std::nullopt;
             }
-            auto compacting = compacting_sstable_registration(_cm, descriptor.sstables);
+            auto compacting = compacting_sstable_registration(_cm, _cm.get_compaction_state(&t), descriptor.sstables);
             auto weight_r = compaction_weight_registration(&_cm, weight);
             auto release_exhausted = [&compacting] (const std::vector<sstables::shared_sstable>& exhausted_sstables) {
                 compacting.release_compacting(exhausted_sstables);
@@ -1364,7 +1367,7 @@ future<compaction_manager::compaction_stats_opt> compaction_manager::perform_tas
     // in the re-write, we need to barrier out any previously running
     // compaction.
     std::vector<sstables::shared_sstable> sstables;
-    compacting_sstable_registration compacting(*this);
+    compacting_sstable_registration compacting(*this, get_compaction_state(&t));
     co_await run_with_compaction_disabled(t, [&sstables, &compacting, get_func = std::move(get_func)] () -> future<> {
         // Getting sstables and registering them as compacting must be atomic, to avoid a race condition where
         // regular compaction runs in between and picks the same files.
