551366c9d304de2c9520988ee1519c5bab4ac00f
Benny Halevy <bhalevy@scylladb.com>
2022-12-29 15:48:15 +0200

    compaction_manager: keep owned_ranges_ptr in compaction_state
    When perform_cleanup adds sstables to sstables_requiring_cleanup,
    also save the owned_ranges_ptr in the compaction_state so
    it could be used by other compaction types like
    regular, reshape, or major compaction.
    
    When the exhausted sstables are released, check
    if sstables_requiring_cleanup is empty, and if it is,
    clear also the owned_ranges_ptr.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    (cherry picked from commit d87925d9fc524e2371a7873405371490e36d290f)

--- compaction/compaction_manager.cc
+++ compaction/compaction_manager.cc
@@ -83,6 +83,9 @@ public:
             _compacting.erase(sst);
             _cs.sstables_requiring_cleanup.erase(sst);
         }
+        if (_cs.sstables_requiring_cleanup.empty()) {
+            _cs.owned_ranges_ptr = nullptr;
+        }
     }
 
     class update_me : public compaction_manager::task::on_replacement {
@@ -1577,6 +1580,9 @@ future<> compaction_manager::perform_cleanup(owned_ranges_ptr sorted_owned_range
             // for later processing if they can't be cleaned up right now.
             // They are erased from sstables_requiring_cleanup by compacting.release_compacting
             auto& cs = get_compaction_state(&t);
+            if (!cs.sstables_requiring_cleanup.empty()) {
+                cs.owned_ranges_ptr = std::move(sorted_owned_ranges);
+            }
             return get_candidates(t, cs.sstables_requiring_cleanup);
         });
     };
