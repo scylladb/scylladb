5ba5371631c7b5ab49c5b4c4963dfffae5d28651
Benny Halevy <bhalevy@scylladb.com>
2023-01-01 16:13:51 +0200

    locator: endpoint_dc_rack: define default operator==
    and get rid of the ad-hoc implementation in network_topology_strategy.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/types.hh
+++ locator/types.hh
@@ -25,6 +25,8 @@ using inet_address = gms::inet_address;
 struct endpoint_dc_rack {
     sstring dc;
     sstring rack;
+
+    bool operator==(const endpoint_dc_rack&) const = default;
 };
 
 using dc_rack_fn = seastar::noncopyable_function<endpoint_dc_rack(inet_address)>;
