scylladb/scylladb

commit 23e4c83155332d3f0607f16ee8829ca4cd75d9be

position_in_partition: Make after_key() work with non-full keys
This fixes a long standing bug related to handling of non-full
clustering keys, issue #1446.

after_key() was creating a position which is after all keys prefixed
by a non-full key, rather than a position which is right after that
key.

This will issue will be caught by cql_query_test::test_compact_storage
in debug mode when mutation_partition_v2 merging starts inserting
sentinels at position after_key() on preemption.

It probably already causes problems for such keys.

position_in_partition.hh
@@ -80,6 +80,8 @@ enum class partition_region : uint8_t {
     partition_end,
 };
 
+struct view_and_holder;
+
 std::ostream& operator<<(std::ostream&, partition_region);
 std::string_view to_string(partition_region);
 partition_region parse_partition_region(std::string_view);
@@ -168,12 +170,18 @@ public:
         return {clustering_row_tag_t(), ck};
     }
 
-    static position_in_partition_view after_key(const clustering_key& ck) {
+    // Returns a view, as the first element of the returned pair, to before_key(pos._ck)
+    // if pos.is_clustering_row() else returns pos as-is.
+    // The second element of the pair needs to be kept alive as long as the first element is used.
+    // The returned view is valid as long as the view passed to this method is valid.
+    static view_and_holder after_key(const schema&, position_in_partition_view);
+
+    static position_in_partition_view after_all_prefixed(const clustering_key& ck) {
         return {partition_region::clustered, bound_weight::after_all_prefixed, &ck};
     }
 
-    // Returns a view to after_key(pos._ck) if pos.is_clustering_row() else returns pos as-is.
-    static position_in_partition_view after_key(position_in_partition_view pos) {
+    // Returns a view to after_all_prefixed(pos._ck) if pos.is_clustering_row() else returns pos as-is.
+    static position_in_partition_view after_all_prefixed(position_in_partition_view pos) {
         return {partition_region::clustered, pos._bound_weight == bound_weight::equal ? bound_weight::after_all_prefixed : pos._bound_weight, pos._ck};
     }
 
@@ -266,13 +274,27 @@ public:
     explicit position_in_partition(static_row_tag_t) : _type(partition_region::static_row) { }
     position_in_partition(clustering_row_tag_t, clustering_key_prefix ck)
         : _type(partition_region::clustered), _ck(std::move(ck)) { }
-    position_in_partition(after_clustering_row_tag_t, clustering_key_prefix ck)
-        // FIXME: Use lexicographical_relation::before_strictly_prefixed here. Refs #1446
-        : _type(partition_region::clustered), _bound_weight(bound_weight::after_all_prefixed), _ck(std::move(ck)) { }
-    position_in_partition(after_clustering_row_tag_t, position_in_partition_view pos)
+    position_in_partition(after_clustering_row_tag_t, const schema& s, clustering_key_prefix ck)
+        : _type(partition_region::clustered)
+        , _bound_weight(bound_weight::after_all_prefixed)
+        , _ck(std::move(ck))
+    {
+        if (clustering_key::make_full(s, *_ck)) { // Refs #1446
+            _bound_weight = bound_weight::before_all_prefixed;
+        }
+    }
+    position_in_partition(after_clustering_row_tag_t, const schema& s, position_in_partition_view pos)
+        : position_in_partition(after_clustering_row_tag_t(), s, position_in_partition(pos))
+    { }
+    position_in_partition(after_clustering_row_tag_t, const schema& s, position_in_partition&& pos)
         : _type(partition_region::clustered)
         , _bound_weight(pos._bound_weight != bound_weight::equal ? pos._bound_weight : bound_weight::after_all_prefixed)
-        , _ck(*pos._ck) { }
+        , _ck(std::move(pos._ck))
+    {
+        if (pos._bound_weight == bound_weight::equal && _ck && clustering_key::make_full(s, *_ck)) { // Refs #1446
+            _bound_weight = bound_weight::before_all_prefixed;
+        }
+    }
     position_in_partition(before_clustering_row_tag_t, clustering_key_prefix ck)
         : _type(partition_region::clustered), _bound_weight(bound_weight::before_all_prefixed), _ck(std::move(ck)) { }
     position_in_partition(before_clustering_row_tag_t, position_in_partition_view pos)
@@ -323,15 +345,19 @@ public:
         return {before_clustering_row_tag_t(), std::move(ck)};
     }
 
-    static position_in_partition after_key(clustering_key ck) {
-        return {after_clustering_row_tag_t(), std::move(ck)};
+    static position_in_partition after_key(const schema& s, clustering_key ck) {
+        return {after_clustering_row_tag_t(), s, std::move(ck)};
     }
 
     // If given position is a clustering row position, returns a position
     // right after it. Otherwise returns it unchanged.
     // The position "pos" must be a clustering position.
-    static position_in_partition after_key(position_in_partition_view pos) {
-        return {after_clustering_row_tag_t(), pos};
+    static position_in_partition after_key(const schema& s, position_in_partition_view pos) {
+        return {after_clustering_row_tag_t(), s, pos};
+    }
+
+    static position_in_partition after_key(const schema& s, position_in_partition&& pos) noexcept {
+        return {after_clustering_row_tag_t(), s, std::move(pos)};
     }
 
     static position_in_partition for_key(clustering_key ck) {
@@ -571,6 +597,25 @@ public:
         return position_in_partition(_type, ::reversed(_bound_weight), std::move(_ck));
     }
 };
+
+struct view_and_holder {
+    position_in_partition_view view;
+    std::optional<position_in_partition> holder;
+};
+
+inline
+view_and_holder position_in_partition_view::after_key(const schema& s, position_in_partition_view pos) {
+    if (!pos.is_clustering_row()) {
+        return {pos, std::nullopt};
+    }
+    if (pos.key().is_full(s)) {
+        return {position_in_partition_view::after_all_prefixed(pos.key()), std::nullopt};
+    }
+    // FIXME: This wouldn't be needed if we had a bound weight to represent this.
+    auto res = position_in_partition::after_key(s, clustering_key(pos.key()));
+    position_in_partition_view res_view = res;
+    return {std::move(res_view), std::move(res)};
+}
 
 inline
 position_in_partition position_in_partition::for_range_start(const query::clustering_range& r) {

test/boost/mutation_test.cc
@@ -2679,15 +2679,15 @@ SEASTAR_THREAD_TEST_CASE(test_position_in_partition_reversal) {
     auto rev_s = ss.schema()->make_reversed();
     position_in_partition::tri_compare rev_cmp(*rev_s);
 
-    BOOST_REQUIRE(fwd_cmp(p_i_p::before_key(ss.make_ckey(1)), p_i_p::after_key(ss.make_ckey(1))) < 0);
+    BOOST_REQUIRE(fwd_cmp(p_i_p::before_key(ss.make_ckey(1)), p_i_p::after_key(*ss.schema(), ss.make_ckey(1))) < 0);
     BOOST_REQUIRE(fwd_cmp(p_i_p::before_key(ss.make_ckey(1)), ss.make_ckey(0)) > 0);
-    BOOST_REQUIRE(fwd_cmp(p_i_p::after_key(ss.make_ckey(1)), p_i_p::before_key(ss.make_ckey(1))) > 0);
-    BOOST_REQUIRE(fwd_cmp(p_i_p::after_key(ss.make_ckey(1)), ss.make_ckey(2)) < 0);
+    BOOST_REQUIRE(fwd_cmp(p_i_p::after_key(*ss.schema(), ss.make_ckey(1)), p_i_p::before_key(ss.make_ckey(1))) > 0);
+    BOOST_REQUIRE(fwd_cmp(p_i_p::after_key(*ss.schema(), ss.make_ckey(1)), ss.make_ckey(2)) < 0);
 
-    BOOST_REQUIRE(rev_cmp(p_i_p::before_key(ss.make_ckey(1)).reversed(), p_i_p::after_key(ss.make_ckey(1)).reversed()) > 0);
+    BOOST_REQUIRE(rev_cmp(p_i_p::before_key(ss.make_ckey(1)).reversed(), p_i_p::after_key(*ss.schema(), ss.make_ckey(1)).reversed()) > 0);
     BOOST_REQUIRE(rev_cmp(p_i_p::before_key(ss.make_ckey(1)).reversed(), ss.make_ckey(0)) < 0);
-    BOOST_REQUIRE(rev_cmp(p_i_p::after_key(ss.make_ckey(1)).reversed(), p_i_p::before_key(ss.make_ckey(1)).reversed()) < 0);
-    BOOST_REQUIRE(rev_cmp(p_i_p::after_key(ss.make_ckey(1)).reversed(), ss.make_ckey(2)) > 0);
+    BOOST_REQUIRE(rev_cmp(p_i_p::after_key(*ss.schema(), ss.make_ckey(1)).reversed(), p_i_p::before_key(ss.make_ckey(1)).reversed()) < 0);
+    BOOST_REQUIRE(rev_cmp(p_i_p::after_key(*ss.schema(), ss.make_ckey(1)).reversed(), ss.make_ckey(2)) > 0);
 
     // Test reversal-invariant positions
 
@@ -2699,6 +2699,97 @@ SEASTAR_THREAD_TEST_CASE(test_position_in_partition_reversal) {
 
     BOOST_REQUIRE(rev_cmp(p_i_p::for_static_row().reversed(),
                           p_i_p::for_static_row()) == 0);
+}
+
+SEASTAR_THREAD_TEST_CASE(test_position_in_partition_order_with_prefix_keys) {
+    using pip = position_in_partition;
+    using pipv = position_in_partition_view;
+
+    schema_ptr s = schema_builder("ks", "cf")
+        .with_column("pk", utf8_type, column_kind::partition_key)
+        .with_column("ck1", utf8_type, column_kind::clustering_key)
+        .with_column("ck2", utf8_type, column_kind::clustering_key)
+        .with_column("v", utf8_type)
+        .build();
+
+    position_in_partition::tri_compare cmp(*s);
+
+    auto make_ck = [&] (sstring ck1, std::optional<sstring> ck2 = {}) {
+        if (ck2) {
+            return clustering_key::from_exploded(*s, {serialized(ck1), serialized(*ck2)});
+        }
+        return clustering_key::from_exploded(*s, {serialized(ck1)});
+    };
+
+    auto prefix_a = make_ck("a");
+    auto full_a = prefix_a;
+    clustering_key::make_full(*s, full_a);
+    auto full_a_a = make_ck("a", "a");
+
+    BOOST_REQUIRE(cmp(pip::before_key(prefix_a), prefix_a) < 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), prefix_a) > 0);
+
+    BOOST_REQUIRE(cmp(prefix_a, full_a_a) < 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), prefix_a) > 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), full_a_a) < 0);
+    BOOST_REQUIRE(cmp(pipv::after_all_prefixed(prefix_a), full_a_a) > 0);
+
+    BOOST_REQUIRE(cmp(prefix_a, full_a) < 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), full_a) < 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), pip::before_key(full_a)) <= 0);
+
+    // before_key()/after_key() applied to dummy does not change the position.
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), pip::after_key(*s, pip::after_key(*s, prefix_a))) == 0);
+    BOOST_REQUIRE(cmp(pip::before_key(prefix_a), pip::before_key(pip::before_key(prefix_a))) == 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), pip::after_key(*s, pip::after_key(*s, prefix_a))) == 0);
+
+    BOOST_REQUIRE(cmp(pip::for_key(prefix_a), pip::for_key(full_a_a)) < 0);
+    BOOST_REQUIRE(cmp(pip::for_key(full_a_a), pip::for_key(prefix_a)) > 0);
+    BOOST_REQUIRE(cmp(pip::for_key(prefix_a), pip::for_key(full_a)) < 0);
+    BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), pip::for_key(prefix_a)) > 0);
+
+    // Check reversed schema
+
+    auto rev_s = s->make_reversed();
+    position_in_partition::tri_compare rev_cmp(*rev_s);
+
+    BOOST_REQUIRE(rev_cmp(pip::before_key(prefix_a).reversed(),
+                          pip::for_key(full_a_a).reversed()) > 0);
+
+    BOOST_REQUIRE(rev_cmp(pip::before_key(prefix_a).reversed(),
+                          pip::for_key(full_a).reversed()) > 0);
+
+    BOOST_REQUIRE(rev_cmp(pip::before_key(prefix_a).reversed(),
+                          pip::for_key(full_a_a).reversed()) > 0);
+
+    BOOST_REQUIRE(rev_cmp(pip::before_key(prefix_a).reversed(),
+                          pip::for_key(full_a).reversed()) > 0);
+
+    BOOST_REQUIRE(rev_cmp(pipv::after_all_prefixed(prefix_a).reversed(),
+                          pip::for_key(full_a_a).reversed()) < 0);
+
+    BOOST_REQUIRE(rev_cmp(pipv::after_all_prefixed(prefix_a).reversed(),
+                          pip::for_key(full_a).reversed()) < 0);
+
+    BOOST_REQUIRE(rev_cmp(pip::after_key(*rev_s, prefix_a).reversed(),
+                          pip::for_key(full_a_a).reversed()) > 0);
+
+    BOOST_REQUIRE(rev_cmp(pip::after_key(*rev_s, prefix_a).reversed(),
+                          pip::for_key(full_a).reversed()) > 0);
+
+// FIXME: Below don't work due to https://github.com/scylladb/scylladb/issues/12258
+//
+//    BOOST_REQUIRE(rev_cmp(pip::for_key(prefix_a).reversed(),
+//                          pip::for_key(full_a_a).reversed()) > 0);
+//
+//    BOOST_REQUIRE(rev_cmp(pip::for_key(full_a_a).reversed(),
+//                          pip::for_key(prefix_a).reversed()) < 0);
+//
+//    BOOST_REQUIRE(rev_cmp(pip::for_key(prefix_a).reversed(),
+//                          pip::for_key(full_a).reversed()) > 0);
+//
+//    BOOST_REQUIRE(rev_cmp(pip::after_key(*rev_s, prefix_a).reversed(),
+//                          pip::for_key(prefix_a).reversed()) < 0);
 }
 
 SEASTAR_THREAD_TEST_CASE(test_compactor_range_tombstone_spanning_many_pages) {
@@ -2730,7 +2821,7 @@ SEASTAR_THREAD_TEST_CASE(test_compactor_range_tombstone_spanning_many_pages) {
             frags.emplace_back(mutation_fragment_v2(*s, permit, std::move(row)));
         }
 
-        frags.emplace_back(*s, permit, range_tombstone_change(position_in_partition::after_key(ss.make_ckey(10)), tombstone{}));
+        frags.emplace_back(*s, permit, range_tombstone_change(position_in_partition::after_key(*s, ss.make_ckey(10)), tombstone{}));
 
         frags.emplace_back(*s, permit, partition_end{});
 

test/boost/row_cache_test.cc
@@ -2121,7 +2121,7 @@ SEASTAR_TEST_CASE(test_scan_with_partial_partitions_reversed) {
                     .produces_range_tombstone_change(start_change(reversed(rt2)))
                     .produces_range_tombstone_change(range_tombstone_change(reversed(rt2).end_position(), rt1.tomb))
                     .produces_row_with_key(s.make_ckey(2))
-                    .produces_range_tombstone_change(range_tombstone_change(position_in_partition::after_key(s.make_ckey(1)), {}))
+                    .produces_range_tombstone_change(range_tombstone_change(position_in_partition::after_key(*s.schema(), s.make_ckey(1)), {}))
                     .produces_partition_end()
                     .produces_end_of_stream();
         }

cache_flat_mutation_reader.hh
@@ -572,7 +572,7 @@ void cache_flat_mutation_reader::maybe_add_to_cache(const clustering_row& cr) {
         _read_context.cache().on_mispopulate();
         return;
     }
-    auto rt_opt = _rt_assembler.flush(*_schema, position_in_partition::after_key(cr.key()));
+    auto rt_opt = _rt_assembler.flush(*_schema, position_in_partition::after_key(*_schema, cr.key()));
     clogger.trace("csm {}: populate({})", fmt::ptr(this), clustering_row::printer(*_schema, cr));
     _lsa_manager.run_in_update_section_with_allocator([this, &cr, &rt_opt] {
         mutation_partition& mp = _snp->version()->partition();
@@ -634,8 +634,8 @@ inline
 void cache_flat_mutation_reader::copy_from_cache_to_buffer() {
     clogger.trace("csm {}: copy_from_cache, next={}, next_row_in_range={}", fmt::ptr(this), _next_row.position(), _next_row_in_range);
     _next_row.touch();
-    position_in_partition_view next_lower_bound = _next_row.dummy() ? _next_row.position() : position_in_partition_view::after_key(_next_row.key());
-    auto upper_bound = _next_row_in_range ? next_lower_bound : _upper_bound;
+    auto next_lower_bound = position_in_partition_view::after_key(table_schema(), _next_row.position());
+    auto upper_bound = _next_row_in_range ? next_lower_bound.view : _upper_bound;
     if (_snp->range_tombstones(_lower_bound, upper_bound, [&] (range_tombstone rts) {
         add_range_tombstone_to_buffer(std::move(rts));
         return stop_iteration(_lower_bound_changed && is_buffer_full());
@@ -774,14 +774,14 @@ void cache_flat_mutation_reader::move_to_next_entry() {
     }
 }
 
-void cache_flat_mutation_reader::flush_tombstones(position_in_partition_view pos, bool end_of_range) {
+void cache_flat_mutation_reader::flush_tombstones(position_in_partition_view pos_, bool end_of_range) {
     // Ensure position is appropriate for range tombstone bound
-    pos = position_in_partition_view::after_key(pos);
-    clogger.trace("csm {}: flush_tombstones({}) end_of_range: {}", fmt::ptr(this), pos, end_of_range);
-    _rt_gen.flush(pos, [this] (range_tombstone_change&& rtc) {
+    auto pos = position_in_partition_view::after_key(*_schema, pos_);
+    clogger.trace("csm {}: flush_tombstones({}) end_of_range: {}", fmt::ptr(this), pos.view, end_of_range);
+    _rt_gen.flush(pos.view, [this] (range_tombstone_change&& rtc) {
         add_to_buffer(std::move(rtc), source::cache);
     }, end_of_range);
-    if (auto rtc_opt = _rt_merger.flush(pos, end_of_range)) {
+    if (auto rtc_opt = _rt_merger.flush(pos.view, end_of_range)) {
         do_add_to_buffer(std::move(*rtc_opt));
     }
 }
@@ -832,7 +832,7 @@ inline
 void cache_flat_mutation_reader::add_clustering_row_to_buffer(mutation_fragment_v2&& mf) {
     clogger.trace("csm {}: add_clustering_row_to_buffer({})", fmt::ptr(this), mutation_fragment_v2::printer(*_schema, mf));
     auto& row = mf.as_clustering_row();
-    auto new_lower_bound = position_in_partition::after_key(row.key());
+    auto new_lower_bound = position_in_partition::after_key(*_schema, row.key());
     push_mutation_fragment(std::move(mf));
     _lower_bound = std::move(new_lower_bound);
     _lower_bound_changed = true;

compaction/compaction.cc
@@ -907,7 +907,7 @@ void compacted_fragments_writer::split_large_partition() {
     // will result in current fragment storing an inclusive end bound for last pos, and the
     // next fragment storing an exclusive start bound for last pos. This is very important
     // for not losing information on the range tombstone.
-    auto after_last_pos = position_in_partition::after_key(_current_partition.last_pos.key());
+    auto after_last_pos = position_in_partition::after_key(*_c.schema(), _current_partition.last_pos.key());
     if (_current_partition.current_emitted_tombstone) {
         auto rtc = range_tombstone_change(after_last_pos, tombstone{});
         _c.log_debug("Closing active tombstone {} with {} for partition {}", _current_partition.current_emitted_tombstone, rtc, *_current_partition.dk);

mutation_compactor.hh
@@ -446,7 +446,7 @@ public:
     requires CompactedFragmentsConsumerV2<Consumer> && CompactedFragmentsConsumerV2<GCConsumer>
     stop_iteration consume_end_of_partition(Consumer& consumer, GCConsumer& gc_consumer) {
         if (_effective_tombstone) {
-            auto rtc = range_tombstone_change(position_in_partition::after_key(_last_pos), tombstone{});
+            auto rtc = range_tombstone_change(position_in_partition::after_key(_schema, _last_pos), tombstone{});
             // do_consume() overwrites _effective_tombstone with {}, so save and restore it.
             auto prev_tombstone = _effective_tombstone;
             do_consume(std::move(rtc), consumer, gc_consumer);
@@ -539,7 +539,7 @@ public:
             consume(*std::exchange(_last_static_row, {}), consumer, nc);
         }
         if (_effective_tombstone) {
-            auto rtc = range_tombstone_change(position_in_partition_view::after_key(_last_pos), _effective_tombstone);
+            auto rtc = range_tombstone_change(position_in_partition::after_key(_schema, _last_pos), _effective_tombstone);
             do_consume(std::move(rtc), consumer, nc);
         }
     }
@@ -574,7 +574,7 @@ public:
         partition_start ps(std::move(_last_dk), _partition_tombstone);
         if (_effective_tombstone) {
             return detached_compaction_state{std::move(ps), std::move(_last_static_row),
-                    range_tombstone_change(position_in_partition_view::after_key(_last_pos), _effective_tombstone)};
+                    range_tombstone_change(position_in_partition::after_key(_schema, _last_pos), _effective_tombstone)};
         } else {
             return detached_compaction_state{std::move(ps), std::move(_last_static_row), std::optional<range_tombstone_change>{}};
         }

mutation_fragment.cc
@@ -258,12 +258,12 @@ position_in_partition_view mutation_fragment::position() const
     return visit([] (auto& mf) -> position_in_partition_view { return mf.position(); });
 }
 
-position_range mutation_fragment::range() const {
+position_range mutation_fragment::range(const schema& s) const {
     switch (_kind) {
     case kind::static_row:
         return position_range::for_static_row();
     case kind::clustering_row:
-        return position_range(position_in_partition(position()), position_in_partition::after_key(key()));
+        return position_range(position_in_partition(position()), position_in_partition::after_key(s, key()));
     case kind::partition_start:
         return position_range(position_in_partition(position()), position_in_partition::for_static_row());
     case kind::partition_end:

mutation_fragment.hh
@@ -361,7 +361,7 @@ public:
     position_in_partition_view position() const;
 
     // Returns the range of positions for which this fragment holds relevant information.
-    position_range range() const;
+    position_range range(const schema& s) const;
 
     // Checks if this fragment may be relevant for any range starting at given position.
     bool relevant_for_range(const schema& s, position_in_partition_view pos) const;

commit d196efce8eccd9124d37d73e3f26c2eef0857f40

cache: Fix undefined behavior when populating with non-full keys
Regression introduced in 23e4c8315.

view_and_holder position_in_partiton::after_key() triggers undefined
behavior when the key was not full because the holder is moved, which invalidates the view.

Fixes #12367

position_in_partition.hh
@@ -599,22 +599,40 @@ public:
 };
 
 struct view_and_holder {
-    position_in_partition_view view;
     std::optional<position_in_partition> holder;
+    position_in_partition_view view;
+
+    view_and_holder(position_in_partition pos)
+        : holder(std::move(pos))
+        , view(*holder)
+    { }
+
+    explicit view_and_holder(position_in_partition_view pos)
+        : view(pos)
+    { }
+
+    view_and_holder(view_and_holder&& other) noexcept
+        : holder(std::move(other.holder))
+        , view(holder ? *holder : other.view)
+    { }
+
+    view_and_holder& operator=(view_and_holder&& other) noexcept {
+        holder = std::move(other.holder);
+        view = holder ? *holder: other.view;
+        return *this;
+    }
 };
 
 inline
 view_and_holder position_in_partition_view::after_key(const schema& s, position_in_partition_view pos) {
     if (!pos.is_clustering_row()) {
-        return {pos, std::nullopt};
+        return view_and_holder(pos);
     }
     if (pos.key().is_full(s)) {
-        return {position_in_partition_view::after_all_prefixed(pos.key()), std::nullopt};
+        return view_and_holder(position_in_partition_view::after_all_prefixed(pos.key()));
     }
     // FIXME: This wouldn't be needed if we had a bound weight to represent this.
-    auto res = position_in_partition::after_key(s, clustering_key(pos.key()));
-    position_in_partition_view res_view = res;
-    return {std::move(res_view), std::move(res)};
+    return view_and_holder(position_in_partition::after_key(s, clustering_key(pos.key())));
 }
 
 inline

test/boost/mutation_test.cc
@@ -2728,6 +2728,22 @@ SEASTAR_THREAD_TEST_CASE(test_position_in_partition_order_with_prefix_keys) {
     clustering_key::make_full(*s, full_a);
     auto full_a_a = make_ck("a", "a");
 
+    auto holder = pipv::after_key(*s, prefix_a);
+    BOOST_REQUIRE(holder.holder);
+    {
+        auto holder2 = std::move(holder);
+        BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), holder2.view) == 0);
+        BOOST_REQUIRE(holder2.holder);
+    }
+
+    holder = pipv::after_key(*s, full_a_a);
+    BOOST_REQUIRE(!holder.holder);
+    {
+        auto holder2 = std::move(holder);
+        BOOST_REQUIRE(cmp(pip::after_key(*s, full_a_a), holder2.view) == 0);
+        BOOST_REQUIRE(!holder2.holder);
+    }
+
     BOOST_REQUIRE(cmp(pip::before_key(prefix_a), prefix_a) < 0);
     BOOST_REQUIRE(cmp(pip::after_key(*s, prefix_a), prefix_a) > 0);
 

test/boost/row_cache_test.cc
@@ -4084,3 +4084,52 @@ SEASTAR_TEST_CASE(test_eviction_of_upper_bound_of_population_range) {
         read(0, 3);
     });
 }
+
+SEASTAR_TEST_CASE(test_reading_of_nonfull_keys) {
+        return seastar::async([] {
+            schema_ptr s = schema_builder("ks", "cf")
+                    .with_column("pk", utf8_type, column_kind::partition_key)
+                    .with_column("ck1", utf8_type, column_kind::clustering_key)
+                    .with_column("ck2", utf8_type, column_kind::clustering_key)
+                    .with_column("v", utf8_type)
+                    .build();
+
+            auto pkey = dht::decorate_key(*s, partition_key::from_single_value(*s, serialized("pk1")));
+
+            auto make_ck = [&] (sstring ck1, std::optional<sstring> ck2 = {}) {
+                if (ck2) {
+                    return clustering_key::from_exploded(*s, {serialized(ck1), serialized(*ck2)});
+                }
+                return clustering_key::from_exploded(*s, {serialized(ck1)});
+            };
+
+            auto prefix_a = make_ck("a");
+            auto full_a = prefix_a;
+            clustering_key::make_full(*s, full_a);
+            auto full_a_a = make_ck("a", "a");
+
+            tests::reader_concurrency_semaphore_wrapper semaphore;
+
+            auto pr = dht::partition_range::make_singular(pkey);
+
+            memtable_snapshot_source underlying(s);
+
+            auto t1 = tombstone(api::new_timestamp(), gc_clock::now());
+            mutation m1(s, pkey);
+            m1.partition().clustered_row(*s, prefix_a).apply(t1);
+            m1.partition().clustered_row(*s, full_a).apply(t1);
+            m1.partition().clustered_row(*s, full_a_a).apply(t1);
+            underlying.apply(m1);
+
+            cache_tracker tracker;
+            row_cache cache(s, snapshot_source([&] { return underlying(); }), tracker);
+
+            // populating read
+            assert_that(cache.make_reader(s, semaphore.make_permit(), pr))
+                    .produces(m1);
+
+            // non-populating read
+            assert_that(cache.make_reader(s, semaphore.make_permit(), pr))
+                    .produces(m1);
+        });
+}