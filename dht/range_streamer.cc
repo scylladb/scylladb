775c6b9697042c2717a72cd0b7182d34b30e27d4
Benny Halevy <bhalevy@scylladb.com>
2023-01-17 00:18:31 +0200

    dht/range_streamer: stream_async: do_streaming: move ranges downstream
    The ranges can be moved rather than copied to both
    `request_ranges` and `transfer_ranges` as they are only cleared
    after this point.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- dht/range_streamer.cc
+++ dht/range_streamer.cc
@@ -276,9 +276,9 @@ future<> range_streamer::stream_async() {
                             nr_ranges_streamed, nr_ranges_streamed + ranges_to_stream.size(), nr_ranges_total);
                     auto ranges_streamed = ranges_to_stream.size();
                     if (_nr_rx_added) {
-                        sp.request_ranges(source, keyspace, ranges_to_stream);
+                        sp.request_ranges(source, keyspace, std::move(ranges_to_stream));
                     } else if (_nr_tx_added) {
-                        sp.transfer_ranges(source, keyspace, ranges_to_stream);
+                        sp.transfer_ranges(source, keyspace, std::move(ranges_to_stream));
                     }
                     sp.execute().discard_result().get();
                     ranges_to_stream.clear();
