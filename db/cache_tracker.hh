tgrabiec/scylla

db: cache_tracker: Introduce insert() variant which positions before existing entry in the LRU

db/cache_tracker.hh
@@ -92,6 +92,8 @@ public:
     void insert(partition_entry&) noexcept;
     void insert(partition_version&) noexcept;
     void insert(rows_entry&) noexcept;
+    // Inserts e such that it will be evicted right before more_recent in the absence of later touches.
+    void insert(rows_entry& e, rows_entry& more_recent) noexcept;
     void on_remove() noexcept;
     void clear_continuity(cache_entry& ce) noexcept;
     void on_partition_erase() noexcept;
@@ -133,6 +135,13 @@ void cache_tracker::insert(rows_entry& entry) noexcept {
     ++_stats.row_insertions;
     ++_stats.rows;
     _lru.add(entry);
+}
+
+inline
+void cache_tracker::insert(rows_entry& entry, rows_entry& more_recent) noexcept {
+    ++_stats.row_insertions;
+    ++_stats.rows;
+    _lru.add_before(entry, more_recent);
 }
 
 inline

utils/lru.hh
@@ -63,6 +63,11 @@ public:
         _list.push_back(e);
     }
 
+    // Like add(e) but makes sure that e is evicted right before "more_recent" in the absence of later touches.
+    void add_before(evictable& e, evictable& more_recent) noexcept {
+        _list.insert(_list.iterator_to(more_recent), e);
+    }
+
     void touch(evictable& e) noexcept {
         remove(e);
         add(e);

scylladb/scylladb

db/cache_tracker.hh
@@ -93,6 +93,8 @@ public:
     void insert(partition_version&) noexcept;
     void insert(rows_entry&) noexcept;
     void remove(rows_entry&) noexcept;
+    // Inserts e such that it will be evicted right before more_recent in the absence of later touches.
+    void insert(rows_entry& more_recent, rows_entry& e) noexcept;
     void clear_continuity(cache_entry& ce) noexcept;
     void on_partition_erase() noexcept;
     void on_partition_merge() noexcept;
@@ -136,6 +138,13 @@ void cache_tracker::insert(rows_entry& entry) noexcept {
     ++_stats.row_insertions;
     ++_stats.rows;
     _lru.add(entry);
+}
+
+inline
+void cache_tracker::insert(rows_entry& more_recent, rows_entry& entry) noexcept {
+    ++_stats.row_insertions;
+    ++_stats.rows;
+    _lru.add_before(more_recent, entry);
 }
 
 inline

utils/lru.hh
@@ -71,6 +71,11 @@ public:
         _list.push_back(e);
     }
 
+    // Like add(e) but makes sure that e is evicted right before "more_recent" in the absence of later touches.
+    void add_before(evictable& more_recent, evictable& e) noexcept {
+        _list.insert(_list.iterator_to(more_recent), e);
+    }
+
     void touch(evictable& e) noexcept {
         remove(e);
         add(e);