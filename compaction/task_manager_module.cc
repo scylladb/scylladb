b18806053538c4979d7418e8f753153c5bae5372
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-01-28 14:46:08 +0100

    compaction: create task manager's task for major keyspace compaction on one shard
    Implementation of task_manager's task that covers major keyspace compaction
    on one shard.

--- compaction/task_manager_module.cc
+++ compaction/task_manager_module.cc
@@ -32,18 +32,28 @@ future<> run_on_existing_tables(sstring op, replica::database& db, std::string_v
 
 future<> major_keyspace_compaction_task_impl::run() {
     co_await _db.invoke_on_all([&] (replica::database& db) -> future<> {
-        auto local_tables = _table_infos;
-        // major compact smaller tables first, to increase chances of success if low on space.
-        std::ranges::sort(local_tables, std::less<>(), [&] (const table_id& ti) {
-            try {
-                return db.find_column_family(ti).get_stats().live_disk_space_used;
-            } catch (const replica::no_such_column_family& e) {
-                return int64_t(-1);
-            }
-        });
-        co_await run_on_existing_tables("force_keyspace_compaction", db, _status.keyspace, local_tables, [] (replica::table& t) {
-            return t.compact_all_sstables();
-        });
+        tasks::task_info parent_info{_status.id, _status.shard};
+        auto& module = db.get_compaction_manager().get_task_manager_module();
+        auto task = co_await module.make_and_start_task<shard_major_keyspace_compaction_task_impl>(parent_info, _status.keyspace, _status.id, db, _table_infos);
+        co_await task->done();
+    });
+}
+
+tasks::is_internal shard_major_keyspace_compaction_task_impl::is_internal() const noexcept {
+    return tasks::is_internal::yes;
+}
+
+future<> shard_major_keyspace_compaction_task_impl::run() {
+    // Major compact smaller tables first, to increase chances of success if low on space.
+    std::ranges::sort(_local_tables, std::less<>(), [&] (const table_id& ti) {
+        try {
+            return _db.find_column_family(ti).get_stats().live_disk_space_used;
+        } catch (const replica::no_such_column_family& e) {
+            return int64_t(-1);
+        }
+    });
+    co_await run_on_existing_tables("force_keyspace_compaction", _db, _status.keyspace, _local_tables, [] (replica::table& t) {
+        return t.compact_all_sstables();
     });
 }
 
