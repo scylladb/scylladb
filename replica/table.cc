dba0e604a74461332dfc507b28a526f5364c0ef2
Raphael S. Carvalho <raphaelsc@scylladb.com>
2023-02-02 16:38:45 -0300

    table: Fix disk-space related metrics
    total disk space used metric is incorrectly telling the amount of
    disk space ever used, which is wrong. It should tell the size of
    all sstables being used + the ones waiting to be deleted.
    live disk space used, by this defition, shouldn't account the
    ones waiting to be deleted.
    and live sstable count, shouldn't account sstables waiting to
    be deleted.
    
    Fix all that.
    
    Fixes #12717.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>
    (cherry picked from commit 529a1239a98eefd2b1aa6a6785e8c4b88d43de60)

--- replica/table.cc
+++ replica/table.cc
@@ -997,17 +997,16 @@ void table::set_metrics() {
 }
 
 void table::rebuild_statistics() {
-    // zeroing live_disk_space_used and live_sstable_count because the
-    // sstable list was re-created
     _stats.live_disk_space_used = 0;
     _stats.live_sstable_count = 0;
+    _stats.total_disk_space_used = 0;
 
     _sstables->for_each_sstable([this] (const sstables::shared_sstable& tab) {
         update_stats_for_new_sstable(tab->bytes_on_disk());
     });
     for (const compaction_group_ptr& cg : compaction_groups()) {
         for (auto& tab: cg->compacted_undeleted_sstables()) {
-            update_stats_for_new_sstable(tab->bytes_on_disk());
+            _stats.total_disk_space_used += tab->bytes_on_disk();
         }
     }
 }
