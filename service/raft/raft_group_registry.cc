e420dbf10b464ea0f70749938809d2a1035aa172
Andrii Patsula <andriy.patsula@gmail.com>
2023-02-03 19:04:51 +0100

    service/raft: raft_group_registry: use recent_entries_map to store rate_limits in pinger. Fixes #12309

--- service/raft/raft_group_registry.cc
+++ service/raft/raft_group_registry.cc
@@ -455,10 +455,11 @@ future<bool> direct_fd_pinger::ping(direct_failure_detector::pinger::endpoint_id
     auto dst_id = raft::server_id{std::move(id)};
     auto addr = _address_map.find(dst_id);
     if (!addr) {
-        auto [it, _] = _rate_limits.try_emplace(id, std::chrono::minutes(5));
-        auto& rate_limit = it->second;
-
-        rslog.log(log_level::warn, rate_limit, "Raft server id {} cannot be translated to an IP address.", id);
+        {
+            auto& rate_limit = _rate_limits.try_get_recent_entry(id, std::chrono::minutes(5));
+            rslog.log(log_level::warn, rate_limit, "Raft server id {} cannot be translated to an IP address.", id);
+        }
+        _rate_limits.remove_least_recent_entries(std::chrono::minutes(30));
         co_return false;
     }
 
