06d5557c42ee1714de6601b65e19d1c6046bcea5
Petr Gusev <petr.gusev@scylladb.com>
2023-02-08 00:07:02 +0400

    transport server: fix unexpected server errors handling
    If request processing ended with an error, it is worth
    sending the error to the client through
    make_error/write_response. Previously in this case we
    just wrote a message to the log and didn't handle the
    client connection in any way. As a result, the only
    thing the client got in this case was timeout error.
    
    A new test_batch_with_error is added. It is quite
    difficult to reproduce error condition in a test,
    so we use error injection instead. Passing injection_key
    in the body of the request ensures that the exception
    will be thrown only for this test request and
    will not affect other requests that
    the driver may send in the background.
    
    Closes: scylladb#12104
    
    (cherry picked from commit a4cf509)

--- test/cql-pytest/test_batch.py
+++ test/cql-pytest/test_batch.py
@@ -7,8 +7,10 @@
 # Tests for batch operations
 #############################################################################
 from cassandra import InvalidRequest
-
+from cassandra.cluster import NoHostAvailable
 from util import new_test_table
+from rest_api import scylla_inject_error
+
 
 import pytest
 
@@ -50,3 +52,27 @@ def test_error_is_raised_for_batch_size_above_threshold(cql, table1):
     from scylla.yaml."""
     with pytest.raises(InvalidRequest, match="Batch too large"):
         cql.execute(generate_big_batch(table1, 1025))
+
+# Test checks unexpected errors handling in CQL server.
+#
+# The original problem was that std::bad_alloc exception occurred while parsing a large batch request.
+# This exception was caught by try/catch in cql_server::connection::process_request_one and
+# an attempt was made to construct the error response message via make_error function.
+# This attempt failed since the error message contained entire query and exceeded the limit of 64K
+# in cql_server::response::write_string, causing "Value too large" exception to be thrown.
+# This new exception reached the general handler in cql_server::connection::process_request, where
+# it was just logged and no information about the problem was sent to the client.
+# As a result, the client received a timeout exception after a while and
+# no other information about the cause of the error.
+#
+# It is quite difficult to reproduce OOM in a test, so we use error injection instead.
+# Passing injection_key in the body of the request ensures that the exception will be
+# thrown only for this test request and will not affect other requests that
+# the driver may send in the background.
+@pytest.mark.asyncio
+async def test_batch_with_error(cql, table1):
+    injection_key = 'query_processor-parse_statement-test_failure'
+    with scylla_inject_error(cql, injection_key, one_shot=False):
+        # exceptions::exception_code::SERVER_ERROR, it gets converted to NoHostAvailable by the driver
+        with pytest.raises(NoHostAvailable, match="Value too large"):
+            cql.execute(generate_big_batch(table1, 100) + injection_key)
