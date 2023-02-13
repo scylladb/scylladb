e29671ca178b48da0aa47c5645d2b43c9f10eb1e
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-02-13 16:22:47 +0100

    compaction: add table_major_compaction_task_impl
    Implementation of task_manager's task that covers major keyspace compaction
    of one table.

--- compaction/task_manager_module.cc
+++ compaction/task_manager_module.cc
@@ -48,16 +48,71 @@ tasks::is_internal shard_major_keyspace_compaction_task_impl::is_internal() cons
     return tasks::is_internal::yes;
 }
 
+struct table_tasks_info {
+    tasks::task_manager::task_ptr task;
+    table_info ti;
+
+    table_tasks_info(tasks::task_manager::task_ptr t, table_info info)
+        : task(t)
+        , ti(info)
+    {}
+};
+
 future<> shard_major_keyspace_compaction_task_impl::run() {
-    // Major compact smaller tables first, to increase chances of success if low on space.
-    std::ranges::sort(_local_tables, std::less<>(), [&] (const table_info& ti) {
+    std::exception_ptr ex;
+    seastar::condition_variable cv;
+    tasks::task_manager::task_ptr current_task;
+    tasks::task_info parent_info{_status.id, _status.shard};
+    std::vector<table_tasks_info> table_tasks;
+    for (auto& ti : _local_tables) {
+        table_tasks.emplace_back(co_await _module->make_and_start_task<table_major_keyspace_compaction_task_impl>(parent_info, _status.keyspace, ti.name, _status.id, _db, ti, cv, current_task), ti);
+    }
+
+    // While compaction is run on one table, the size of tables may significantly change.
+    // Thus, they are sorted before each invidual compaction and the smallest table is chosen.
+    while (!table_tasks.empty()) {
         try {
-            return _db.find_column_family(ti.id).get_stats().live_disk_space_used;
+    // FIXME: fix indentation
+    // Major compact smaller tables first, to increase chances of success if low on space.
+    // Tables will be kept in descending order.
+    std::ranges::sort(table_tasks, std::greater<>(), [&] (const table_tasks_info& tti) {
+        try {
+            return _db.find_column_family(tti.ti.id).get_stats().live_disk_space_used;
         } catch (const replica::no_such_column_family& e) {
             return int64_t(-1);
         }
     });
-    co_await run_on_existing_tables("force_keyspace_compaction", _db, _status.keyspace, _local_tables, [] (replica::table& t) {
+            // Task responsible for the smallest table.
+            current_task = table_tasks.back().task;
+            table_tasks.pop_back();
+            cv.broadcast();
+            co_await current_task->done();
+        } catch (...) {
+            ex = std::current_exception();
+            current_task = nullptr;
+            cv.broken(ex);
+            break;
+        }
+    }
+
+    if (ex) {
+        // Wait for all tasks even on failure.
+        for (auto& tti: table_tasks) {
+            co_await tti.task->done();
+        }
+        co_await coroutine::return_exception_ptr(std::move(ex));
+    }
+}
+
+tasks::is_internal table_major_keyspace_compaction_task_impl::is_internal() const noexcept {
+    return tasks::is_internal::yes;
+}
+
+future<> table_major_keyspace_compaction_task_impl::run() {
+    co_await _cv.wait([&] {
+        return _current_task && _current_task->id() == _status.id;
+    });
+    co_await run_on_table("force_keyspace_compaction", _db, _status.keyspace, _ti, [] (replica::table& t) {
         return t.compact_all_sstables();
     });
 }
