36a6f283cb0616fb53f63ace5391d8711d76af47
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-02-13 13:17:19 +0100

    compaction: add run_on_table
    Extract code which runs a function on a particular table from
    run_on_existing_tables to run_on_table.

--- compaction/task_manager_module.cc
+++ compaction/task_manager_module.cc
@@ -11,10 +11,9 @@
 
 namespace compaction {
 
-// Run on all tables, skipping dropped tables
-future<> run_on_existing_tables(sstring op, replica::database& db, std::string_view keyspace, const std::vector<table_id> local_tables, std::function<future<> (replica::table&)> func) {
+future<> run_on_table(sstring op, replica::database& db, std::string_view keyspace, table_id ti, std::function<future<> (replica::table&)> func) {
     std::exception_ptr ex;
-    for (const auto& ti : local_tables) {
+        // FIXME: fix indentation
         tasks::tmlogger.debug("Starting {} on {}.{}", op, keyspace, ti);
         try {
             co_await func(db.find_column_family(ti));
@@ -27,6 +26,12 @@ future<> run_on_existing_tables(sstring op, replica::database& db, std::string_v
         if (ex) {
             co_await coroutine::return_exception_ptr(std::move(ex));
         }
+}
+
+// Run on all tables, skipping dropped tables
+future<> run_on_existing_tables(sstring op, replica::database& db, std::string_view keyspace, const std::vector<table_id> local_tables, std::function<future<> (replica::table&)> func) {
+    for (const auto& ti : local_tables) {
+        co_await run_on_table(op, db, keyspace, ti, func);
     }
 }
 
