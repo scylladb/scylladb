607ccdd1e1b5684d8092ff35db6a6ef9fa17af51
Michał Chojnowski <michal.chojnowski@scylladb.com>
2023-02-23 07:36:01 +0100

    mutation_partition: add an upgrading variant of row::row
    It will be used in upcoming commits.
    
    A factory function is used, rather than an actual constructor,
    because we want to delegate the (easy) case of equal schemas
    to the existing single-schema constructor.
    And that's impossible (at least without invoking a copy/move
    constructor) to do with only constructors.

--- mutation/mutation_partition.cc
+++ mutation/mutation_partition.cc
@@ -1559,6 +1559,16 @@ row::row(const schema& s, column_kind kind, const row& o) : _size(o._size)
     _cells.clone_from(o._cells, clone_cell_and_hash);
 }
 
+row row::construct_row(const schema& our_schema, const schema& their_schema, column_kind kind, const row& o) {
+    if (our_schema.version() == their_schema.version()) {
+        return row(our_schema, kind, o);
+    } else {
+        row r;
+        r.apply(our_schema, their_schema, kind, o);
+        return r;
+    }
+}
+
 row::~row() {
 }
 
