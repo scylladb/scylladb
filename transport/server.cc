ecf068779bd9abe401701540641c58ee4b1d1d9e
Kefu Chai <kefu.chai@scylladb.com>
2023-02-23 11:20:48 +0800

    compress, transport: do not detect LZ4_compress_default()
    `LZ4_compress_default()` was introduced in liblz4 v1.7.3, despite
    that the release note (https://github.com/lz4/lz4/releases/tag/v1.7.3)
    of v1.7.3 didn't mention this. if we check the commit which added
    this API, we can find all releases including it: see
    ```
    $ git tag --contains 1b17bf2ab8cf66dd2b740eca376e2d46f7ad7041
    lz4-r130
    r129
    r130
    r131
    rc129v0
    v1.7.3
    v1.7.4
    v1.7.4.2
    v1.7.5
    v1.8.0
    v1.8.1
    v1.8.1.2
    v1.8.2
    v1.8.3
    v1.9.0
    v1.9.1
    v1.9.2
    v1.9.3
    v1.9.4
    ```
    
    and v1.7.3 was released in Nov 17, 2016. some popular distros
    releases also package new enough liblz4:
    
    - fedora 35 ships lz4-devel 1.9.3,
    - CentOS 7 ships lz4-devel 1.8.3
    - debian 10 ships liblz4-dev 1.8.3
    - ubuntu 18.04 ships liblz4-dev r131
    
    so, in this change, we drop the support of liblz4 < 1.7.3 for better
    code readability.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- transport/server.cc
+++ transport/server.cc
@@ -1556,11 +1556,7 @@ void cql_server::response::compress_lz4()
         output[1] = (input_len >> 16) & 0xFF;
         output[2] = (input_len >> 8) & 0xFF;
         output[3] = input_len & 0xFF;
-#ifdef HAVE_LZ4_COMPRESS_DEFAULT
         auto ret = LZ4_compress_default(input, output + 4, input_len, LZ4_compressBound(input_len));
-#else
-        auto ret = LZ4_compress(input, output + 4, input_len);
-#endif
         if (ret == 0) {
             throw std::runtime_error("CQL frame LZ4 compression failure");
         }
