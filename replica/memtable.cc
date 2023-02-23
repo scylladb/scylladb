a6fcd4c289dfde9aa397a16d43574c3d1a2d8416
Michał Chojnowski <michal.chojnowski@scylladb.com>
2023-02-23 07:30:55 +0100

    memtable: remove _schema from memtable_entry
    After adding a _schema field to each partition version,
    the field in memtable_entry is redundant. It can be always recovered
    from the latest version. Remove it.

--- replica/memtable.cc
+++ replica/memtable.cc
@@ -696,7 +696,7 @@ public:
 };
 
 partition_snapshot_ptr memtable_entry::snapshot(memtable& mtbl) {
-    return _pe.read(mtbl.region(), mtbl.cleaner(), _schema, no_cache_tracker);
+    return _pe.read(mtbl.region(), mtbl.cleaner(), schema(), no_cache_tracker);
 }
 
 flat_mutation_reader_v2_opt
@@ -821,8 +821,7 @@ mutation_source memtable::as_data_source() {
 }
 
 memtable_entry::memtable_entry(memtable_entry&& o) noexcept
-    : _schema(std::move(o._schema))
-    , _key(std::move(o._key))
+    : _key(std::move(o._key))
     , _pe(std::move(o._pe))
     , _flags(o._flags)
 { }
@@ -840,14 +839,13 @@ bool memtable::is_flushed() const noexcept {
 }
 
 void memtable_entry::upgrade_schema(const schema_ptr& s, mutation_cleaner& cleaner) {
-    if (_schema != s) {
-        partition().upgrade(_schema, s, cleaner, no_cache_tracker);
-        _schema = s;
+    if (schema() != s) {
+        partition().upgrade(schema(), s, cleaner, no_cache_tracker);
     }
 }
 
 void memtable::upgrade_entry(memtable_entry& e) {
-    if (e._schema != _schema) {
+    if (e.schema() != _schema) {
         assert(!reclaiming_enabled());
         with_allocator(allocator(), [this, &e] {
             e.upgrade_schema(_schema, cleaner());
