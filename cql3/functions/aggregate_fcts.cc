bfd70c192e1e21a6ef2c4783c908aa1d2f6bea50
Avi Kivity <avi@scylladb.com>
2023-02-27 12:31:26 +0200

    cql3: functions: reimplement min/max statelessly
    min() and max() had two implementations: one static (for each type in
    a select list) and one dynamic (for compound types). Since the
    dynamic implementation is sufficient, we only reimplement that. This
    means we don't use the automarshalling helpers, since we don't do any
    arithemetic on values apart from comparison, which is conveniently
    provided by abstract_type.

--- cql3/functions/aggregate_fcts.cc
+++ cql3/functions/aggregate_fcts.cc
@@ -494,288 +494,6 @@ struct aggregate_type_for<time_native_type> {
     using type = time_native_type::primary_type;
 };
 
-// WARNING: never invoke this on temporary values; it will return a dangling reference.
-template <typename Type>
-const Type& max_wrapper(const Type& t1, const Type& t2) {
-    using std::max;
-    return max(t1, t2);
-}
-
-inline const net::inet_address& max_wrapper(const net::inet_address& t1, const net::inet_address& t2) {
-    using family = seastar::net::inet_address::family;
-    const size_t len =
-            (t1.in_family() == family::INET || t2.in_family() == family::INET)
-            ? sizeof(::in_addr) : sizeof(::in6_addr);
-    return std::memcmp(t1.data(), t2.data(), len) >= 0 ? t1 : t2;
-}
-
-inline const timeuuid_native_type& max_wrapper(const timeuuid_native_type& t1, const timeuuid_native_type& t2) {
-    return t1.uuid.timestamp() > t2.uuid.timestamp() ? t1 : t2;
-}
-
-template <typename Type>
-class impl_max_function_for final : public aggregate_function::aggregate {
-   std::optional<typename aggregate_type_for<Type>::type> _max{};
-public:
-    virtual void reset() override {
-        _max = {};
-    }
-    virtual opt_bytes compute() override {
-        if (!_max) {
-            return {};
-        }
-        return data_type_for<Type>()->decompose(data_value(Type{*_max}));
-    }
-    virtual void add_input(const std::vector<opt_bytes>& values) override {
-        if (!values[0]) {
-            return;
-        }
-        auto val = value_cast<typename aggregate_type_for<Type>::type>(data_type_for<Type>()->deserialize(*values[0]));
-        if (!_max) {
-            _max = val;
-        } else {
-            _max = max_wrapper(*_max, val);
-        }
-    }
-    virtual void set_accumulator(const opt_bytes& acc) override {
-        if (acc) {
-            _max = value_cast<typename aggregate_type_for<Type>::type>(data_type_for<Type>()->deserialize(*acc));
-        } else {
-            reset();
-        }
-    }
-    virtual opt_bytes get_accumulator() const override {
-        if (_max) {
-            return data_type_for<Type>()->decompose(data_value(Type{*_max}));
-        }
-        return {};
-    }
-    virtual void reduce(const opt_bytes& acc) override {
-        return add_input({acc});
-    }
-};
-
-/// The same as `impl_max_function_for' but without compile-time dependency on `Type'.
-class impl_max_dynamic_function final : public aggregate_function::aggregate {
-    data_type _io_type;
-    opt_bytes _max;
-public:
-    impl_max_dynamic_function(data_type io_type) : _io_type(std::move(io_type)) {}
-
-    virtual void reset() override {
-        _max = {};
-    }
-    virtual opt_bytes compute() override {
-        return _max.value_or(bytes{});
-    }
-    virtual void add_input(const std::vector<opt_bytes>& values) override {
-        if (values.empty() || !values[0]) {
-            return;
-        }
-        if (!_max || !_max->length() || _io_type->less(*_max, *values[0])) {
-            _max = values[0];
-        }
-    }
-    virtual void set_accumulator(const opt_bytes& acc) override {
-        _max = acc;
-    }
-    virtual opt_bytes get_accumulator() const override {
-        return _max;
-    }
-    virtual void reduce(const opt_bytes& acc) override {
-        if (acc && !acc->length()) {
-            return;
-        }
-        return add_input({acc});
-    }
-};
-
-template <typename Type>
-class max_function_for final : public native_aggregate_function {
-public:
-    max_function_for() : native_aggregate_function("max", data_type_for<Type>(), { data_type_for<Type>() }) {}
-    virtual bool is_reducible() const override {
-        return true;
-    }
-    virtual std::unique_ptr<aggregate> new_aggregate() override {
-        return std::make_unique<impl_max_function_for<Type>>();
-    }
-    virtual ::shared_ptr<aggregate_function> reducible_aggregate_function() override {
-        return ::make_shared<max_function_for<Type>>();
-    };
-};
-
-class max_dynamic_function final : public native_aggregate_function {
-    data_type _io_type;
-public:
-    max_dynamic_function(data_type io_type)
-            : native_aggregate_function("max", io_type, { io_type })
-            , _io_type(std::move(io_type)) {}
-    virtual bool is_reducible() const override {
-        return true;
-    }            
-    virtual std::unique_ptr<aggregate> new_aggregate() override {
-        return std::make_unique<impl_max_dynamic_function>(_io_type);
-    }
-    virtual ::shared_ptr<aggregate_function> reducible_aggregate_function() override {
-        return ::make_shared<max_dynamic_function>(_io_type);
-    };
-};
-
-/**
- * Creates a MAX function for the specified type.
- *
- * @param inputType the function input and output type
- * @return a MAX function for the specified type.
- */
-template <typename Type>
-static
-shared_ptr<aggregate_function>
-make_max_function() {
-    return make_shared<max_function_for<Type>>();
-}
-
-// WARNING: never invoke this on temporary values; it will return a dangling reference.
-template <typename Type>
-const Type& min_wrapper(const Type& t1, const Type& t2) {
-    using std::min;
-    return min(t1, t2);
-}
-
-inline const net::inet_address& min_wrapper(const net::inet_address& t1, const net::inet_address& t2) {
-    using family = seastar::net::inet_address::family;
-    const size_t len =
-            (t1.in_family() == family::INET || t2.in_family() == family::INET)
-            ? sizeof(::in_addr) : sizeof(::in6_addr);
-    return std::memcmp(t1.data(), t2.data(), len) <= 0 ? t1 : t2;
-}
-
-inline timeuuid_native_type min_wrapper(timeuuid_native_type t1, timeuuid_native_type t2) {
-    return t1.uuid.timestamp() < t2.uuid.timestamp() ? t1 : t2;
-}
-
-template <typename Type>
-class impl_min_function_for final : public aggregate_function::aggregate {
-   std::optional<typename aggregate_type_for<Type>::type> _min{};
-public:
-    virtual void reset() override {
-        _min = {};
-    }
-    virtual opt_bytes compute() override {
-        if (!_min) {
-            return {};
-        }
-        return data_type_for<Type>()->decompose(data_value(Type{*_min}));
-    }
-    virtual void add_input(const std::vector<opt_bytes>& values) override {
-        if (!values[0]) {
-            return;
-        }
-        auto val = value_cast<typename aggregate_type_for<Type>::type>(data_type_for<Type>()->deserialize(*values[0]));
-        if (!_min) {
-            _min = val;
-        } else {
-            _min = min_wrapper(*_min, val);
-        }
-    }
-    virtual void set_accumulator(const opt_bytes& acc) override {
-        if (acc) {
-            _min = value_cast<typename aggregate_type_for<Type>::type>(data_type_for<Type>()->deserialize(*acc));
-        } else {
-            reset();
-        }
-    }
-    virtual opt_bytes get_accumulator() const override {
-        if (_min) {
-            return data_type_for<Type>()->decompose(data_value(Type{*_min}));
-        }
-        return {};
-    }
-    virtual void reduce(const opt_bytes& acc) override {
-        return add_input({acc});
-    }
-};
-
-/// The same as `impl_min_function_for' but without compile-time dependency on `Type'.
-class impl_min_dynamic_function final : public aggregate_function::aggregate {
-    data_type _io_type;
-    opt_bytes _min;
-public:
-    impl_min_dynamic_function(data_type io_type) : _io_type(std::move(io_type)) {}
-
-    virtual void reset() override {
-        _min = {};
-    }
-    virtual opt_bytes compute() override {
-        return _min.value_or(bytes{});
-    }
-    virtual void add_input(const std::vector<opt_bytes>& values) override {
-        if (values.empty() || !values[0]) {
-            return;
-        }
-        if (!_min || !_min->length() || _io_type->less(*values[0], *_min)) {
-            _min = values[0];
-        }
-    }
-    virtual void set_accumulator(const opt_bytes& acc) override {
-        _min = acc;
-    }
-    virtual opt_bytes get_accumulator() const override {
-        return _min;
-    }
-    virtual void reduce(const opt_bytes& acc) override {
-        if (acc && !acc->length()) {
-            return;
-        }
-        return add_input({acc});
-    }
-};
-
-template <typename Type>
-class min_function_for final : public native_aggregate_function {
-public:
-    min_function_for() : native_aggregate_function("min", data_type_for<Type>(), { data_type_for<Type>() }) {}
-    virtual bool is_reducible() const override {
-        return true;
-    }
-    virtual std::unique_ptr<aggregate> new_aggregate() override {
-        return std::make_unique<impl_min_function_for<Type>>();
-    }
-    virtual ::shared_ptr<aggregate_function> reducible_aggregate_function() override {
-        return ::make_shared<min_function_for<Type>>();
-    };
-};
-
-class min_dynamic_function final : public native_aggregate_function {
-    data_type _io_type;
-public:
-    min_dynamic_function(data_type io_type)
-            : native_aggregate_function("min", io_type, { io_type })
-            , _io_type(std::move(io_type)) {}
-    virtual bool is_reducible() const override {
-        return true;
-    }
-    virtual std::unique_ptr<aggregate> new_aggregate() override {
-        return std::make_unique<impl_min_dynamic_function>(_io_type);
-    }
-    virtual ::shared_ptr<aggregate_function> reducible_aggregate_function() override {
-        return ::make_shared<min_dynamic_function>(_io_type);
-    };
-};
-
-/**
- * Creates a MIN function for the specified type.
- *
- * @param inputType the function input and output type
- * @return a MIN function for the specified type.
- */
-template <typename Type>
-static
-shared_ptr<aggregate_function>
-make_min_function() {
-    return make_shared<min_function_for<Type>>();
-}
-
 template <typename Type>
 class impl_count_function_for final : public aggregate_function::aggregate {
    int64_t _count = 0;
@@ -931,88 +649,97 @@ aggregate_fcts::make_count_rows_function() {
 
 shared_ptr<aggregate_function>
 aggregate_fcts::make_max_dynamic_function(data_type io_type) {
-    return make_shared<max_dynamic_function>(io_type);
+    auto max = ::make_shared<internal_scalar_function>("max_step", io_type, std::vector({io_type, io_type}), [io_type] (const std::vector<bytes_opt>& args) -> bytes_opt {
+        if (!args[0]) {
+            return args[1];
+        }
+        if (!args[1]) {
+            return args[0];
+        }
+        return std::max(*args[0], *args[1], io_type->as_less_comparator());
+    });
+    return ::make_shared<db::functions::stateless_aggregate_function_adapter>(
+        db::functions::stateless_aggregate_function{
+            .name = function_name::native_function("max"),
+            .state_type = io_type,
+            .result_type = io_type,
+            .argument_types = {io_type},
+            .initial_state = std::nullopt,
+            .aggregation_function = max,
+            .state_to_result_function = ::make_shared<internal_scalar_function>("max_finalizer", io_type, std::vector({io_type}), [] (const std::vector<bytes_opt>& args) {
+                return args[0];
+            }),
+            .state_reduction_function = max,
+        }
+    );
 }
 
 shared_ptr<aggregate_function>
 aggregate_fcts::make_min_dynamic_function(data_type io_type) {
-    return make_shared<min_dynamic_function>(io_type);
+    auto min = ::make_shared<internal_scalar_function>("min_step", io_type, std::vector({io_type, io_type}), [io_type] (const std::vector<bytes_opt>& args) -> bytes_opt {
+        if (!args[0]) {
+            return args[1];
+        }
+        if (!args[1]) {
+            return args[0];
+        }
+        return std::min(*args[0], *args[1], io_type->as_less_comparator());
+    });
+    return ::make_shared<db::functions::stateless_aggregate_function_adapter>(
+        db::functions::stateless_aggregate_function{
+            .name = function_name::native_function("min"),
+            .state_type = io_type,
+            .result_type = io_type,
+            .argument_types = {io_type},
+            .initial_state = std::nullopt,
+            .aggregation_function = min,
+            .state_to_result_function = ::make_shared<internal_scalar_function>("min_finalizer", io_type, std::vector({io_type}), [] (const std::vector<bytes_opt>& args) {
+                return args[0];
+            }),
+            .state_reduction_function = min,
+        }
+    );
 }
+
 
 void cql3::functions::add_agg_functions(declared_t& funcs) {
     auto declare = [&funcs] (shared_ptr<function> f) { funcs.emplace(f->name(), f); };
 
     declare(make_count_function<int8_t>());
-    declare(make_max_function<int8_t>());
-    declare(make_min_function<int8_t>());
 
     declare(make_count_function<int16_t>());
-    declare(make_max_function<int16_t>());
-    declare(make_min_function<int16_t>());
 
     declare(make_count_function<int32_t>());
-    declare(make_max_function<int32_t>());
-    declare(make_min_function<int32_t>());
 
     declare(make_count_function<int64_t>());
-    declare(make_max_function<int64_t>());
-    declare(make_min_function<int64_t>());
 
     declare(make_count_function<utils::multiprecision_int>());
-    declare(make_max_function<utils::multiprecision_int>());
-    declare(make_min_function<utils::multiprecision_int>());
 
     declare(make_count_function<big_decimal>());
-    declare(make_max_function<big_decimal>());
-    declare(make_min_function<big_decimal>());
 
     declare(make_count_function<float>());
-    declare(make_max_function<float>());
-    declare(make_min_function<float>());
 
     declare(make_count_function<double>());
-    declare(make_max_function<double>());
-    declare(make_min_function<double>());
 
     declare(make_count_function<sstring>());
-    declare(make_max_function<sstring>());
-    declare(make_min_function<sstring>());
 
     declare(make_count_function<ascii_native_type>());
-    declare(make_max_function<ascii_native_type>());
-    declare(make_min_function<ascii_native_type>());
 
     declare(make_count_function<simple_date_native_type>());
-    declare(make_max_function<simple_date_native_type>());
-    declare(make_min_function<simple_date_native_type>());
 
     declare(make_count_function<db_clock::time_point>());
-    declare(make_max_function<db_clock::time_point>());
-    declare(make_min_function<db_clock::time_point>());
 
     declare(make_count_function<timeuuid_native_type>());
-    declare(make_max_function<timeuuid_native_type>());
-    declare(make_min_function<timeuuid_native_type>());
 
     declare(make_count_function<time_native_type>());
-    declare(make_max_function<time_native_type>());
-    declare(make_min_function<time_native_type>());
 
     declare(make_count_function<utils::UUID>());
-    declare(make_max_function<utils::UUID>());
-    declare(make_min_function<utils::UUID>());
 
     declare(make_count_function<bytes>());
-    declare(make_max_function<bytes>());
-    declare(make_min_function<bytes>());
 
     declare(make_count_function<bool>());
-    declare(make_max_function<bool>());
-    declare(make_min_function<bool>());
 
     declare(make_count_function<net::inet_address>());
-    declare(make_max_function<net::inet_address>());
-    declare(make_min_function<net::inet_address>());
 
     // FIXME: more count/min/max
 
