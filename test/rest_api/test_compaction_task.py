4e84c05b5fa7a7eca3a0a06e9b19210665cadbb4
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-03-01 18:32:35 +0100

    test: extend test_compaction_task.py
    test/rest_api/test_compaction_task.py is extended so that it checks
    validity of major compaction run from column family api.

--- test/rest_api/test_compaction_task.py
+++ test/rest_api/test_compaction_task.py
@@ -6,7 +6,7 @@ from util import new_test_table, new_test_keyspace
 from rest_util import set_tmp_task_ttl
 from task_manager_utils import wait_for_task, list_tasks, check_child_parent_relationship
 
-def test_major_keyspace_compaction_task(cql, this_dc, rest_api):
+def check_compaction_task(cql, this_dc, rest_api, run_compaction):
     long_time = 1000000000
     with set_tmp_task_ttl(rest_api, long_time):
         with new_test_keyspace(cql, f"WITH REPLICATION = {{ 'class' : 'NetworkTopologyStrategy', '{this_dc}' : 1 }}") as keyspace:
@@ -17,7 +17,8 @@ def test_major_keyspace_compaction_task(cql, this_dc, rest_api):
                 cql.execute(stmt, [1, 'world'])
 
                 # Run major compaction.
-                resp = rest_api.send("POST", f"storage_service/keyspace_compaction/{keyspace}")
+                [_, table] = t0.split(".")
+                resp = run_compaction(keyspace, table)
                 resp.raise_for_status()
 
                 # Get list of compaction tasks.
@@ -31,3 +32,9 @@ def test_major_keyspace_compaction_task(cql, this_dc, rest_api):
 
                 for top_level_task in statuses:
                     check_child_parent_relationship(rest_api, top_level_task)
+
+def test_major_keyspace_compaction_task(cql, this_dc, rest_api):
+    check_compaction_task(cql, this_dc, rest_api, lambda keyspace, _: rest_api.send("POST", f"storage_service/keyspace_compaction/{keyspace}"))
+
+def test_major_column_family_compaction_task(cql, this_dc, rest_api):
+    check_compaction_task(cql, this_dc, rest_api, lambda keyspace, table: rest_api.send("POST", f"column_family/major_compaction/{keyspace}:{table}"))
