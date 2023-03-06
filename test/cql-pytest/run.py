f0f48520cea8c1ca7929c518312799e7b88ebe03
Nadav Har'El <nyh@scylladb.com>
2023-03-06 11:21:28 +0200

    test: drop our "pytest" wrapper script
    When Fedora 37 came out, we discovered that its "pytest" script started
    to run Python with the "-s" option, which caused problems for packages
    installed personally via pip. We fixed this by adding our own wrapper
    script test/pytest.
    
    But this bug (https://bugzilla.redhat.com/show_bug.cgi?id=2152171) was
    already fixed in Fedora 37, and the new version already reached our
    dbuild. So we no longer need this wrapper script. Let's remove it.
    
    Fixes #12412
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- test/cql-pytest/run.py
+++ test/cql-pytest/run.py
@@ -365,7 +365,7 @@ def run_pytest(pytest_dir, additional_parameters):
         run_pytest_pids = set()
         os.chdir(pytest_dir)
         os.setsid()
-        os.execvp(os.path.join(source_path, 'test/pytest'), ['pytest',
+        os.execvp('pytest', ['pytest',
             '-o', 'junit_family=xunit2'] + additional_parameters)
         exit(1)
     # parent:
