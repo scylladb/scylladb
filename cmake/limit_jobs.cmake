b25a6d5a9c2db9a2572a8889aee6fd371a1372da
Kefu Chai <kefu.chai@scylladb.com>
2023-03-07 15:22:54 +0800

    build: cmake: limit the number of link job
    this mirrors the settings in `configure.py`.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

---
+++ cmake/limit_jobs.cmake
@@ -0,0 +1,16 @@
+set(Seastar_LINK_MEM_PER_JOB 1024 CACHE INTERNAL "Maximum memory used by each link job in (in MiB)")
+
+cmake_host_system_information(
+  RESULT _total_mem
+  QUERY AVAILABLE_PHYSICAL_MEMORY)
+math(EXPR _link_pool_depth "${_total_mem} / ${Seastar_LINK_MEM_PER_JOB}")
+if(_link_pool_depth EQUAL 0)
+  set(_link_pool_depth 1)
+endif()
+set_property(
+  GLOBAL
+  APPEND
+  PROPERTY JOB_POOLS
+    link_pool=${_link_pool_depth}
+    submodule_pool=1)
+set(CMAKE_JOB_POOL_LINK link_pool)
