378e8761b98d9fcc11104e170f901838f78eb038
Jan Ciolek <jan.ciolek@scylladb.com>
2023-03-09 13:26:52 +0100

    cql-pytest/test_unset: test unset value in INSERTs with IF NOT EXISTS
    Add tests which test INSERT statements with IF NOT EXISTS,
    when an UNSET_VLAUE is passed for some column.
    The test are similar to the previous ones done for simple
    INSERTs without IF NOT EXISTS.
    
    Signed-off-by: Jan Ciolek <jan.ciolek@scylladb.com>

--- test/cql-pytest/test_unset.py
+++ test/cql-pytest/test_unset.py
@@ -83,3 +83,77 @@ def test_insert_unset_regular_col(cql, table4):
     # INSERT (p, 1, 5, UNSET)
     insert(c=1, s=5, r=UNSET_VALUE)
     assert select_rows() == [(p, 1, 5, 3)]
+
+# Test INSERT with UNSET_VALUE for the clustering column value, using IF NOT EXISTS
+def test_insert_unset_clustering_col_if_not_exists(cql, table4):
+    p = unique_key_int()
+    def insert(c, s, r):
+        cql.execute(cql.prepare(f"INSERT INTO {table4} (p, c, s, r) VALUES ({p}, ?, ?, ?) IF NOT EXISTS"), [c, s, r])
+    def select_rows():
+        return sorted(list(cql.execute(f"SELECT p, c, s, r FROM {table4} WHERE p = {p}")))
+
+    # INSERT (p, UNSET, 2, 3)
+    with pytest.raises(InvalidRequest, match='unset'):
+        insert(c=UNSET_VALUE, s=2, r=3)
+
+    # INSERT (p, 1, 2, 3)
+    insert(c=1, s=2, r=3)
+    assert select_rows() == [(p, 1, 2, 3)]
+
+    # INSERT (p, UNSET, 2, 3)
+    with pytest.raises(InvalidRequest, match='unset'):
+        insert(c=UNSET_VALUE, s=2, r=3)
+
+    # INSERT (p, UNSET, 5, 6)
+    with pytest.raises(InvalidRequest, match='unset'):
+        insert(c=UNSET_VALUE, s=5, r=6)
+
+    assert select_rows() == [(p, 1, 2, 3)]
+
+# Test INSERT with UNSET_VALUE for the static column value, using IF NOT EXISTS
+def test_insert_unset_static_col_if_not_exists(cql, table4):
+    p = unique_key_int()
+    def insert(c, s, r):
+        cql.execute(cql.prepare(f"INSERT INTO {table4} (p, c, s, r) VALUES ({p}, ?, ?, ?) IF NOT EXISTS"), [c, s, r])
+    def select_rows():
+        return sorted(list(cql.execute(f"SELECT p, c, s, r FROM {table4} WHERE p = {p}")))
+
+    # INSERT (p, 1, UNSET, 3)
+    insert(c=1, s=UNSET_VALUE, r=3)
+    assert select_rows() == [(p, 1, None, 3)]
+
+    # INSERT (p, 1, 2, 3)
+    insert(c=1, s=2, r=3)
+    assert select_rows() == [(p, 1, None, 3)]
+
+    # INSERT (p, 1, UNSET, 3)
+    insert(c=1, s=UNSET_VALUE, r=3)
+    assert select_rows() == [(p, 1, None, 3)]
+
+    # INSERT (p, 1, UNSET, 4)
+    insert(c=1, s=UNSET_VALUE, r=4)
+    assert select_rows() == [(p, 1, None, 3)]
+
+# Test INSERT with UNSET_VALUE for the regular column value, using IF NOT EXISTS
+def test_insert_unset_regular_col_if_not_exists(cql, table4):
+    p = unique_key_int()
+    def insert(c, s, r):
+        cql.execute(cql.prepare(f"INSERT INTO {table4} (p, c, s, r) VALUES ({p}, ?, ?, ?) IF NOT EXISTS"), [c, s, r])
+    def select_rows():
+        return list(cql.execute(f"SELECT p, c, s, r FROM {table4} WHERE p = {p}"))
+
+    # INSERT (p, 1, 2, UNSET)
+    insert(c=1, s=2, r=UNSET_VALUE)
+    assert select_rows() == [(p, 1, 2, None)]
+
+    # INSERT (p, 1, 2, 3)
+    insert(c=1, s=2, r=3)
+    assert select_rows() == [(p, 1, 2, None)]
+
+    # INSERT (p, 1, 2, UNSET)
+    insert(c=1, s=2, r=UNSET_VALUE)
+    assert select_rows() == [(p, 1, 2, None)]
+
+    # INSERT (p, 1, 5, UNSET)
+    insert(c=1, s=5, r=UNSET_VALUE)
+    assert select_rows() == [(p, 1, 2, None)]
