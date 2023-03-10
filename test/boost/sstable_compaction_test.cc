b27910cff2dda46b6905fb91ff36eaff4a6b866c
Benny Halevy <bhalevy@scylladb.com>
2023-03-10 16:04:37 +0200

    test: sstable_compaction_test: sstable_rewrite: do_with_async
    simplify flow using seastar thread.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/boost/sstable_compaction_test.cc
+++ test/boost/sstable_compaction_test.cc
@@ -1170,7 +1170,7 @@ SEASTAR_TEST_CASE(tombstone_purge_test) {
 
 SEASTAR_TEST_CASE(sstable_rewrite) {
     BOOST_REQUIRE(smp::count == 1);
-    return test_env::do_with([] (test_env& env) {
+    return test_env::do_with_async([] (test_env& env) {
         auto s = make_shared_schema({}, some_keyspace, some_column_family,
             {{"p1", utf8_type}}, {{"c1", utf8_type}}, {{"r1", utf8_type}}, {}, utf8_type);
 
@@ -1188,9 +1188,10 @@ SEASTAR_TEST_CASE(sstable_rewrite) {
         apply_key(key_for_this_shard[0]);
 
         auto sst = env.make_sstable(s, 51, sstables::get_highest_sstable_version(), big);
-        return write_memtable_to_sstable_for_test(*mt, sst).then([&env, s, sst] {
-            return env.reusable_sst(s, 51);
-        }).then([&env, s, key = key_for_this_shard[0]] (auto sstp) mutable {
+        write_memtable_to_sstable_for_test(*mt, sst).get();
+        auto sstp = env.reusable_sst(s, 51).get();
+        auto key = key_for_this_shard[0];
+        // FIXME: indentation
             auto new_tables = make_lw_shared<std::vector<sstables::shared_sstable>>();
             auto creator = [&env, new_tables, s] {
                 auto sst = env.make_sstable(s, 52, sstables::get_highest_sstable_version(), big);
@@ -1198,28 +1199,23 @@ SEASTAR_TEST_CASE(sstable_rewrite) {
                 return sst;
             };
             auto cf = make_lw_shared<table_for_tests>(env.manager(), s);
+            auto stop_cf = deferred_stop(*cf);
             std::vector<shared_sstable> sstables;
             sstables.push_back(std::move(sstp));
 
-            return compact_sstables(sstables::compaction_descriptor(std::move(sstables), default_priority_class()), *cf, creator).then([&env, s, key, new_tables] (auto) {
+            compact_sstables(sstables::compaction_descriptor(std::move(sstables), default_priority_class()), *cf, creator).get();
                 BOOST_REQUIRE(new_tables->size() == 1);
                 auto newsst = (*new_tables)[0];
                 BOOST_REQUIRE(generation_value(newsst->generation()) == 52);
                 auto reader = make_lw_shared<flat_mutation_reader_v2>(sstable_reader(newsst, s, env.make_reader_permit()));
-                return (*reader)().then([s, reader, key] (mutation_fragment_v2_opt m) {
+                auto close_reader = deferred_close(*reader);
+                auto m = (*reader)().get();
                     BOOST_REQUIRE(m);
                     BOOST_REQUIRE(m->is_partition_start());
                     BOOST_REQUIRE(m->as_partition_start().key().equal(*s, key));
-                    return reader->next_partition();
-                }).then([reader] {
-                    return (*reader)();
-                }).then([reader] (mutation_fragment_v2_opt m) {
+                    reader->next_partition().get();
+                m = (*reader)().get();
                     BOOST_REQUIRE(!m);
-                }).finally([reader] {
-                    return reader->close();
-                });
-            }).finally([cf] () mutable { return cf->stop_and_keep_alive(); });
-        }).then([sst, mt, s] {});
     });
 }
 
