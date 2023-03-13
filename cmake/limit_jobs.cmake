269cce4c2c69b33103dcda8cecd0d23b88fd36f7
Kefu Chai <tchaikov@gmail.com>
2023-03-14 00:34:26 +0800

    build: cmake: remove Seastar from the option name
    change the option name to "LINK_MEM_PER_JOB" as this is not
    a Seastar option, but a top-level project option.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- cmake/limit_jobs.cmake
+++ cmake/limit_jobs.cmake
@@ -1,9 +1,9 @@
-set(Seastar_LINK_MEM_PER_JOB 1024 CACHE INTERNAL "Maximum memory used by each link job in (in MiB)")
+set(LINK_MEM_PER_JOB 1024 CACHE INTERNAL "Maximum memory used by each link job in (in MiB)")
 
 cmake_host_system_information(
   RESULT _total_mem
   QUERY AVAILABLE_PHYSICAL_MEMORY)
-math(EXPR _link_pool_depth "${_total_mem} / ${Seastar_LINK_MEM_PER_JOB}")
+math(EXPR _link_pool_depth "${_total_mem} / ${LINK_MEM_PER_JOB}")
 if(_link_pool_depth EQUAL 0)
   set(_link_pool_depth 1)
 endif()
