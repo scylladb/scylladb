1cc28679bcb11675abdb5750cd9a0a0eea18bf39
Kefu Chai <kefu.chai@scylladb.com>
2023-03-19 14:22:13 +0800

    transport: mark cql_server::timeout_config() const
    this function returns a const reference to member variable, so we
    can mark it with the `const` specifier for better readability.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- transport/server.hh
+++ transport/server.hh
@@ -280,7 +280,7 @@ private:
     future<> advertise_new_connection(shared_ptr<generic_server::connection> conn) override;
     future<> unadvertise_connection(shared_ptr<generic_server::connection> conn) override;
 
-    const ::timeout_config& timeout_config() { return _config.timeout_config; }
+    const ::timeout_config& timeout_config() const { return _config.timeout_config; }
 };
 
 class cql_server::event_notifier : public service::migration_listener,
