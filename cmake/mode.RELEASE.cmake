e0ca80d21fef4e589776a15faf5f335c362a8117
Kefu Chai <kefu.chai@scylladb.com>
2023-03-21 16:13:43 +0800

    build: cmake: port more cxxflags from configure.py
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- cmake/mode.RELEASE.cmake
+++ cmake/mode.RELEASE.cmake
@@ -7,6 +7,17 @@ set(CMAKE_CXX_FLAGS_RELEASE
 string(APPEND CMAKE_CXX_FLAGS_RELEASE
   " -O${Seastar_OptimizationLevel_RELEASE}")
 
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "arm64|aarch64")
+  set(clang_inline_threshold 300)
+else()
+  set(clang_inline_threshold 2500)
+endif()
+string(APPEND CMAKE_CXX_FLAGS_RELEASE
+  " $<$<CXX_COMPILER_ID:GNU>:--param inline-unit-growth=300"
+  " $<$<CXX_COMPILER_ID:Clang>:-mllvm -inline-threshold=${clang_inline_threshold}>"
+  # clang generates 16-byte loads that break store-to-load forwarding
+  # gcc also has some trouble: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=103554
+  " -fno-slp-vectorize")
 set(Seastar_DEFINITIONS_DEBUG
   SCYLLA_BUILD_MODE=release)
 
