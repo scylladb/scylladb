a892e144cc70de1cce87cfb0b80eebf182a1e664
Tomasz Grabiec <tgrabiec@scylladb.com>
2023-03-23 14:37:01 +0100

    db: Log replication strategy for debugging purposes

--- replica/database.cc
+++ replica/database.cc
@@ -1201,7 +1201,7 @@ keyspace::create_replication_strategy(const locator::shared_token_metadata& stm,
     _replication_strategy =
             abstract_replication_strategy::create_replication_strategy(
                 _metadata->strategy_name(), options);
-
+    rslogger.debug("replication strategy for keyspace {} is {}, opts={}", _metadata->name(), _metadata->strategy_name(), options);
     auto erm = co_await get_erm_factory().create_effective_replication_map(_replication_strategy, stm.get());
     update_effective_replication_map(std::move(erm));
 }
