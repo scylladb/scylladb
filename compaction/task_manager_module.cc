d0c4028d6428dfd76c83314cfb7516acd78859ee
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-04-21 13:43:34 +0200

    compaction: add helpers for table tasks scheduling
    In shard compaction tasks per table tasks will be created all at once
    and then they will wait for their turn to run.
    
    A function that allows waking up tasks one after another and a function
    that makes the task wait for its turn are added.

--- compaction/task_manager_module.cc
+++ compaction/task_manager_module.cc
@@ -12,6 +12,16 @@
 
 namespace compaction {
 
+struct table_tasks_info {
+    tasks::task_manager::task_ptr task;
+    table_info ti;
+
+    table_tasks_info(tasks::task_manager::task_ptr t, table_info info)
+        : task(t)
+        , ti(info)
+    {}
+};
+
 future<> run_on_table(sstring op, replica::database& db, std::string keyspace, table_info ti, std::function<future<> (replica::table&)> func) {
     std::exception_ptr ex;
         // FIXME: fix indentation
@@ -33,6 +43,52 @@ future<> run_on_table(sstring op, replica::database& db, std::string keyspace, t
 future<> run_on_existing_tables(sstring op, replica::database& db, std::string keyspace, const std::vector<table_info> local_tables, std::function<future<> (replica::table&)> func) {
     for (const auto& ti : local_tables) {
         co_await run_on_table(op, db, keyspace, ti, func);
+    }
+}
+
+future<> wait_for_your_turn(seastar::condition_variable& cv, tasks::task_manager::task_ptr& current_task, tasks::task_id id) {
+    co_await cv.wait([&] {
+        return current_task && current_task->id() == id;
+    });
+}
+
+future<> run_table_tasks(replica::database& db, std::vector<table_tasks_info> table_tasks, seastar::condition_variable& cv, tasks::task_manager::task_ptr& current_task, bool sort) {
+    std::exception_ptr ex;
+
+    // While compaction is run on one table, the size of tables may significantly change.
+    // Thus, they are sorted before each invidual compaction and the smallest table is chosen.
+    while (!table_tasks.empty()) {
+        try {
+            if (sort) {
+                // Major compact smaller tables first, to increase chances of success if low on space.
+                // Tables will be kept in descending order.
+                std::ranges::sort(table_tasks, std::greater<>(), [&] (const table_tasks_info& tti) {
+                    try {
+                        return db.find_column_family(tti.ti.id).get_stats().live_disk_space_used;
+                    } catch (const replica::no_such_column_family& e) {
+                        return int64_t(-1);
+                    }
+                });
+            }
+            // Task responsible for the smallest table.
+            current_task = table_tasks.back().task;
+            table_tasks.pop_back();
+            cv.broadcast();
+            co_await current_task->done();
+        } catch (...) {
+            ex = std::current_exception();
+            current_task = nullptr;
+            cv.broken(ex);
+            break;
+        }
+    }
+
+    if (ex) {
+        // Wait for all tasks even on failure.
+        for (auto& tti: table_tasks) {
+            co_await tti.task->done();
+        }
+        co_await coroutine::return_exception_ptr(std::move(ex));
     }
 }
 
