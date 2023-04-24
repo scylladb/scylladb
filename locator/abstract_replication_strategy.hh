tgrabiec/scylla

locator: effective_replication_map: Abstract get_pending_endpoints()

locator/abstract_replication_strategy.hh
@@ -230,6 +230,7 @@ public:
     inet_address_vector_replica_set get_natural_endpoints(const token& search_token) const;
     stop_iteration for_each_natural_endpoint_until(const token& search_token, const noncopyable_function<stop_iteration(const inet_address&)>& func) const;
     inet_address_vector_replica_set get_natural_endpoints_without_node_being_replaced(const token& search_token) const;
+    inet_address_vector_replica_set get_pending_endpoints(const token& search_token, const sstring& ks_name) const;
 
     // get_ranges() returns the list of ranges held by the given endpoint.
     // The list is sorted, and its elements are non overlapping and non wrap-around.

scylladb/scylladb

locator/abstract_replication_strategy.hh
@@ -205,6 +205,7 @@ public:
     inet_address_vector_replica_set get_natural_endpoints(const token& search_token) const;
     stop_iteration for_each_natural_endpoint_until(const token& search_token, const noncopyable_function<stop_iteration(const inet_address&)>& func) const;
     inet_address_vector_replica_set get_natural_endpoints_without_node_being_replaced(const token& search_token) const;
+    inet_address_vector_topology_change get_pending_endpoints(const token& search_token, const sstring& ks_name) const;
 
     // get_ranges() returns the list of ranges held by the given endpoint.
     // The list is sorted, and its elements are non overlapping and non wrap-around.