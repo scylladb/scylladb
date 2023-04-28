066371adfa0f9e15849e13e523613e5108cdb783
Kefu Chai <kefu.chai@scylladb.com>
2023-04-28 15:17:05 +0800

    db_clock: specialize fmt::formatter<db_clock::time_point>
    this is a part of a series to migrating from `operator<<(ostream&, ..)`
    based formatting to fmtlib based formatting. the goal here is to enable
    fmtlib to print `db_clock::time_point` without the help of `operator<<`.
    
    the corresponding `operator<<()` is removed in this change, as all its
    callers are now using fmtlib for formatting now.
    
    Refs #13245
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- clocks-impl.cc
+++ clocks-impl.cc
@@ -15,12 +15,6 @@
 
 std::atomic<int64_t> clocks_offset;
 
-std::ostream& operator<<(std::ostream& os, db_clock::time_point tp) {
-    auto t = db_clock::to_time_t(tp);
-    ::tm t_buf;
-    return os << std::put_time(::gmtime_r(&t, &t_buf), "%Y/%m/%d %T");
-}
-
 std::string format_timestamp(api::timestamp_type ts) {
     auto t = std::time_t(std::chrono::duration_cast<std::chrono::seconds>(api::timestamp_clock::duration(ts)).count());
     ::tm t_buf;
