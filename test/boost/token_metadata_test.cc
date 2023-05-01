d4f004f5c7a332d18f1fb574548fadbed9921901
Petr Gusev <petr.gusev@scylladb.com>
2023-05-01 16:12:11 +0400

    token_metadata_test.cc: create token_metadata and replication_strategy as shared pointers
    We want to switch token_metadata_test to the new
    implementation of pending_endpoints and read_endpoints in erm.
    To do this, it is convenient to have token_metadata and
    replication_strategy as shared pointers, as it fits better with the signature
    of calculate_effective_replication_map. In this commit we don't
    change the logic of the tests, we just migrate them to use pointers.

--- test/boost/token_metadata_test.cc
+++ test/boost/token_metadata_test.cc
@@ -24,8 +24,8 @@ namespace {
         };
     }
 
-    token_metadata create_token_metadata(inet_address this_endpoint) {
-        return token_metadata({
+    mutable_token_metadata_ptr create_token_metadata(inet_address this_endpoint) {
+        return make_lw_shared<token_metadata>(token_metadata::config {
             topology::config {
                 .this_host_id = host_id::create_random_id(),
                 .this_endpoint = this_endpoint,
@@ -41,17 +41,17 @@ SEASTAR_THREAD_TEST_CASE(test_pending_endpoints_for_bootstrap_first_node) {
     const auto e1 = inet_address("192.168.0.1");
     const auto t1 = dht::token::from_int64(1);
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "1"}
     });
-    token_metadata.add_bootstrap_token(t1, e1);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(0), ks_name),
+    token_metadata->add_bootstrap_token(t1, e1);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(0), ks_name),
         inet_address_vector_topology_change{e1});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1), ks_name),
         inet_address_vector_topology_change{e1});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(2), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(2), ks_name),
         inet_address_vector_topology_change{e1});
 }
 
@@ -63,23 +63,23 @@ SEASTAR_THREAD_TEST_CASE(test_pending_endpoints_for_bootstrap_second_node) {
     const auto e2 = inet_address("192.168.0.2");
     const auto t2 = dht::token::from_int64(100);
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    token_metadata.update_topology(e2, get_dc_rack(e2));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    token_metadata->update_topology(e2, get_dc_rack(e2));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "1"}
     });
-    token_metadata.update_normal_tokens({t1}, e1).get();
-    token_metadata.add_bootstrap_token(t2, e2);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(0), ks_name),
+    token_metadata->update_normal_tokens({t1}, e1).get();
+    token_metadata->add_bootstrap_token(t2, e2);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(0), ks_name),
         inet_address_vector_topology_change{});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1), ks_name),
         inet_address_vector_topology_change{});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(2), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(2), ks_name),
         inet_address_vector_topology_change{e2});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(100), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(100), ks_name),
         inet_address_vector_topology_change{e2});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(101), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(101), ks_name),
         inet_address_vector_topology_change{});
 }
 
@@ -94,25 +94,25 @@ SEASTAR_THREAD_TEST_CASE(test_pending_endpoints_for_bootstrap_with_replicas) {
     const auto e2 = inet_address("192.168.0.2");
     const auto e3 = inet_address("192.168.0.3");
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    token_metadata.update_topology(e2, get_dc_rack(e2));
-    token_metadata.update_topology(e3, get_dc_rack(e3));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    token_metadata->update_topology(e2, get_dc_rack(e2));
+    token_metadata->update_topology(e3, get_dc_rack(e3));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "2"}
     });
-    token_metadata.update_normal_tokens({t1, t1000}, e2).get();
-    token_metadata.update_normal_tokens({t10}, e3).get();
-    token_metadata.add_bootstrap_token(t100, e1);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1), ks_name),
+    token_metadata->update_normal_tokens({t1, t1000}, e2).get();
+    token_metadata->update_normal_tokens({t10}, e3).get();
+    token_metadata->add_bootstrap_token(t100, e1);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1), ks_name),
         inet_address_vector_topology_change{});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(2), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(2), ks_name),
         inet_address_vector_topology_change{e1});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(11), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(11), ks_name),
         inet_address_vector_topology_change{e1});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(100), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(100), ks_name),
         inet_address_vector_topology_change{e1});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(101), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(101), ks_name),
         inet_address_vector_topology_change{});
 }
 
@@ -127,26 +127,26 @@ SEASTAR_THREAD_TEST_CASE(test_pending_endpoints_for_leave_with_replicas) {
     const auto e2 = inet_address("192.168.0.2");
     const auto e3 = inet_address("192.168.0.3");
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    token_metadata.update_topology(e2, get_dc_rack(e2));
-    token_metadata.update_topology(e3, get_dc_rack(e3));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    token_metadata->update_topology(e2, get_dc_rack(e2));
+    token_metadata->update_topology(e3, get_dc_rack(e3));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "2"}
     });
-    token_metadata.update_normal_tokens({t1, t1000}, e2).get();
-    token_metadata.update_normal_tokens({t10}, e3).get();
-    token_metadata.update_normal_tokens({t100}, e1).get();
-    token_metadata.add_leaving_endpoint(e1);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1), ks_name),
+    token_metadata->update_normal_tokens({t1, t1000}, e2).get();
+    token_metadata->update_normal_tokens({t10}, e3).get();
+    token_metadata->update_normal_tokens({t100}, e1).get();
+    token_metadata->add_leaving_endpoint(e1);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1), ks_name),
         inet_address_vector_topology_change{});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(2), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(2), ks_name),
         inet_address_vector_topology_change{e2});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(11), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(11), ks_name),
         inet_address_vector_topology_change{e3});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(100), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(100), ks_name),
         inet_address_vector_topology_change{e3});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(101), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(101), ks_name),
         inet_address_vector_topology_change{});
 }
 
@@ -162,31 +162,31 @@ SEASTAR_THREAD_TEST_CASE(test_pending_endpoints_for_replace_with_replicas) {
     const auto e3 = inet_address("192.168.0.3");
     const auto e4 = inet_address("192.168.0.4");
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    token_metadata.update_topology(e2, get_dc_rack(e2));
-    token_metadata.update_topology(e3, get_dc_rack(e3));
-    token_metadata.update_topology(e4, get_dc_rack(e4));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    token_metadata->update_topology(e2, get_dc_rack(e2));
+    token_metadata->update_topology(e3, get_dc_rack(e3));
+    token_metadata->update_topology(e4, get_dc_rack(e4));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "2"}
     });
-    token_metadata.update_normal_tokens({t1000}, e1).get();
-    token_metadata.update_normal_tokens({t1, t100}, e2).get();
-    token_metadata.update_normal_tokens({t10}, e3).get();
-    token_metadata.add_replacing_endpoint(e3, e4);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(100), ks_name),
+    token_metadata->update_normal_tokens({t1000}, e1).get();
+    token_metadata->update_normal_tokens({t1, t100}, e2).get();
+    token_metadata->update_normal_tokens({t10}, e3).get();
+    token_metadata->add_replacing_endpoint(e3, e4);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(100), ks_name),
         inet_address_vector_topology_change{});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1000), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1000), ks_name),
         inet_address_vector_topology_change{});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1001), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1001), ks_name),
         inet_address_vector_topology_change{e4});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1), ks_name),
         inet_address_vector_topology_change{e4});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(2), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(2), ks_name),
         inet_address_vector_topology_change{e4});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(10), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(10), ks_name),
         inet_address_vector_topology_change{e4});
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(11), ks_name),
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(11), ks_name),
         inet_address_vector_topology_change{});
 }
 
@@ -201,15 +201,15 @@ SEASTAR_THREAD_TEST_CASE(test_endpoints_for_reading_when_bootstrap_with_replicas
     const auto e2 = inet_address("192.168.0.2");
     const auto e3 = inet_address("192.168.0.3");
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    token_metadata.update_topology(e2, get_dc_rack(e2));
-    token_metadata.update_topology(e3, get_dc_rack(e3));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    token_metadata->update_topology(e2, get_dc_rack(e2));
+    token_metadata->update_topology(e3, get_dc_rack(e3));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "2"}
     });
-    token_metadata.update_normal_tokens({t1, t1000}, e2).get();
-    token_metadata.update_normal_tokens({t10}, e3).get();
-    token_metadata.add_bootstrap_token(t100, e1);
+    token_metadata->update_normal_tokens({t1, t1000}, e2).get();
+    token_metadata->update_normal_tokens({t10}, e3).get();
+    token_metadata->add_bootstrap_token(t100, e1);
 
     auto check_endpoints = [&](int64_t t, inet_address_vector_replica_set expected_replicas,
         seastar::compat::source_location sl = seastar::compat::source_location::current())
@@ -217,7 +217,7 @@ SEASTAR_THREAD_TEST_CASE(test_endpoints_for_reading_when_bootstrap_with_replicas
         BOOST_TEST_INFO("line: " << sl.line());
         const auto expected_set = std::unordered_set<inet_address>(expected_replicas.begin(),
             expected_replicas.end());
-        const auto actual_replicas = token_metadata.endpoints_for_reading(dht::token::from_int64(t), ks_name);
+        const auto actual_replicas = token_metadata->endpoints_for_reading(dht::token::from_int64(t), ks_name);
         BOOST_REQUIRE(actual_replicas.has_value());
         const auto actual_set = std::unordered_set<inet_address>(actual_replicas->begin(),
             actual_replicas->end());
@@ -228,14 +228,14 @@ SEASTAR_THREAD_TEST_CASE(test_endpoints_for_reading_when_bootstrap_with_replicas
         seastar::compat::source_location sl = seastar::compat::source_location::current())
     {
         BOOST_TEST_INFO("line: " << sl.line());
-        BOOST_REQUIRE(!token_metadata.endpoints_for_reading(dht::token::from_int64(t), ks_name).has_value());
+        BOOST_REQUIRE(!token_metadata->endpoints_for_reading(dht::token::from_int64(t), ks_name).has_value());
     };
 
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
     check_no_endpoints(2);
 
-    token_metadata.set_read_new(locator::token_metadata::read_new_t::yes);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
+    token_metadata->set_read_new(locator::token_metadata::read_new_t::yes);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
 
     check_endpoints(2, {e3, e1});
     check_endpoints(10, {e3, e1});
@@ -252,14 +252,14 @@ SEASTAR_THREAD_TEST_CASE(test_replace_node_with_same_endpoint) {
     const auto t1 = dht::token::from_int64(1);
     const auto e1 = inet_address("192.168.0.1");
     auto token_metadata = create_token_metadata(e1);
-    token_metadata.update_topology(e1, get_dc_rack(e1));
-    const auto replication_strategy = simple_strategy(replication_strategy_config_options {
+    token_metadata->update_topology(e1, get_dc_rack(e1));
+    const auto replication_strategy = seastar::make_shared<simple_strategy>(replication_strategy_config_options {
         {"replication_factor", "2"}
     });
-    token_metadata.update_normal_tokens({t1}, e1).get();
-    token_metadata.add_replacing_endpoint(e1, e1);
-    token_metadata.update_pending_ranges(replication_strategy, ks_name, get_dc_rack_fn).get();
-    BOOST_REQUIRE_EQUAL(token_metadata.pending_endpoints_for(dht::token::from_int64(1), ks_name),
+    token_metadata->update_normal_tokens({t1}, e1).get();
+    token_metadata->add_replacing_endpoint(e1, e1);
+    token_metadata->update_pending_ranges(*replication_strategy, ks_name, get_dc_rack_fn).get();
+    BOOST_REQUIRE_EQUAL(token_metadata->pending_endpoints_for(dht::token::from_int64(1), ks_name),
         inet_address_vector_topology_change{e1});
-    BOOST_REQUIRE_EQUAL(token_metadata.get_endpoint(t1), e1);
+    BOOST_REQUIRE_EQUAL(token_metadata->get_endpoint(t1), e1);
 }
