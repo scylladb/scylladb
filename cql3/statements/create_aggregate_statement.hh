29d26573ad54ea822d1f4a6ac22d8e92e831594f
Wojciech Mitros <wojciech.mitros@scylladb.com>
2023-05-09 11:30:21 +0200

    cql: check permissions for used functions when creating a UDA
    Currently, when creating a UDA, we only check for permissions
    for creating functions. However, the creator gains all permissions
    to the UDA, including the EXECUTE permission. This enables the
    user to also execute the state/reduce/final functions that were
    used in the UDA, even if they don't have the EXECUTE permissions
    on them.
    
    This patch adds checks for the missing EXECUTE permissions, so
    that the UDA can be only created if the user has all required
    permissions.
    
    The new permissions that are now required when creating a UDA
    are now granted in the existing UDA test.
    
    Fixes #13818

--- cql3/statements/create_aggregate_statement.hh
+++ cql3/statements/create_aggregate_statement.hh
@@ -26,6 +26,7 @@ namespace statements {
 class create_aggregate_statement final : public create_function_statement_base {
     virtual std::unique_ptr<prepared_statement> prepare(data_dictionary::database db, cql_stats& stats) override;
     future<std::pair<::shared_ptr<cql_transport::event::schema_change>, std::vector<mutation>>> prepare_schema_mutations(query_processor& qp, api::timestamp_type) const override;
+    virtual future<> check_access(query_processor& qp, const service::client_state& state) const override;
 
     virtual seastar::future<shared_ptr<db::functions::function>> create(query_processor& qp, db::functions::function* old) const override;
 
