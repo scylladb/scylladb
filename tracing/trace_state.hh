5aebbedaba292931940380ee0c9dfd17657750c9
Pavel Emelyanov <xemul@scylladb.com>
2023-05-12 14:20:08 +0300

    tracing: List-initialize trace_state::_props
    It takes props from constructor args and tunes them according to the
    constructing "flavor" -- primary or secondary state. Adding two static
    helpers code-document the intent and make list-initialization possible
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- tracing/trace_state.hh
+++ tracing/trace_state.hh
@@ -97,37 +97,40 @@ private:
         }
     } _params_ptr;
 
+    static trace_state_props_set make_primary(trace_state_props_set props) {
+        if (!props.contains(trace_state_props::full_tracing) && !props.contains(trace_state_props::log_slow_query)) {
+            throw std::logic_error("A primary session has to be created for either full tracing or a slow query logging");
+        }
+        props.set(trace_state_props::primary);
+        return props;
+    }
+
+    static trace_state_props_set make_secondary(trace_state_props_set props) noexcept {
+        props.remove(trace_state_props::primary);
+        // Default a secondary session to a full tracing.
+        // We may get both zeroes for a full_tracing and a log_slow_query if a
+        // primary session is created with an older server version.
+        props.set_if<trace_state_props::full_tracing>(!props.contains(trace_state_props::full_tracing) && !props.contains(trace_state_props::log_slow_query));
+        return props;
+    }
+
 public:
     trace_state(trace_type type, trace_state_props_set props)
         : _local_tracing_ptr(tracing::get_local_tracing_instance().shared_from_this())
-        , _state_props(props)
+        , _state_props(make_primary(props))
         , _slow_query_threshold(_local_tracing_ptr->slow_query_threshold())
     {
         auto slow_query_ttl = _local_tracing_ptr->slow_query_record_ttl();
-        if (!full_tracing() && !log_slow_query()) {
-            throw std::logic_error("A primary session has to be created for either full tracing or a slow query logging");
-        }
-
-        // This is a primary session
-        _state_props.set(trace_state_props::primary);
         _records = make_lw_shared<one_session_records>(type, ttl_by_type(type, slow_query_ttl), slow_query_ttl);
     }
 
     trace_state(const trace_info& info)
         : _local_tracing_ptr(tracing::get_local_tracing_instance().shared_from_this())
-        , _state_props(info.state_props)
+        , _state_props(make_secondary(info.state_props))
         , _slow_query_threshold(info.slow_query_threshold_us)
     {
         // inherit the slow query threshold and ttl from the coordinator
         auto slow_query_ttl = std::chrono::seconds(info.slow_query_ttl_sec);
-        // This is a secondary session
-        _state_props.remove(trace_state_props::primary);
-
-        // Default a secondary session to a full tracing.
-        // We may get both zeroes for a full_tracing and a log_slow_query if a
-        // primary session is created with an older server version.
-        _state_props.set_if<trace_state_props::full_tracing>(!full_tracing() && !log_slow_query());
-
         _records = make_lw_shared<one_session_records>(info.type, ttl_by_type(info.type, slow_query_ttl), slow_query_ttl, info.session_id, info.parent_id);
 
         if (info.state_props.contains<trace_state_props::log_slow_query>() && info.start_ts_us > 0u) {
