bhalevy/scylla

table: signal compaction_manager when staging sstables become eligible for cleanup
perform_cleanup may be waiting for those sstables
to become eligible for cleanup so signal it
when table::move_sstables_from_staging detects an
sstable that requires cleanup.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

compaction/table_state.hh
@@ -9,6 +9,8 @@
 
 #pragma once
 
+#include <seastar/core/condition-variable.hh>
+
 #include "schema/schema_fwd.hh"
 #include "compaction_descriptor.hh"
 
@@ -51,6 +53,7 @@ public:
     virtual const tombstone_gc_state& get_tombstone_gc_state() const noexcept = 0;
     virtual compaction_backlog_tracker& get_backlog_tracker() = 0;
     virtual const std::string& get_group_id() const noexcept = 0;
+    virtual seastar::condition_variable& get_staging_condition() noexcept = 0;
 };
 
 } // namespace compaction

replica/table.cc
@@ -2582,6 +2582,7 @@ table::make_reader_v2_excluding_sstables(schema_ptr s,
 future<> table::move_sstables_from_staging(std::vector<sstables::shared_sstable> sstables) {
     auto units = co_await get_units(_sstable_deletion_sem, 1);
     sstables::sstable::delayed_commit_changes delay_commit;
+    std::unordered_set<compaction_group*> compaction_groups_to_notify;
     for (auto sst : sstables) {
         try {
             // Off-strategy can happen in parallel to view building, so the SSTable may be deleted already if the former
@@ -2589,17 +2590,25 @@ future<> table::move_sstables_from_staging(std::vector<sstables::shared_sstable>
             // The _sstable_deletion_sem prevents list update on off-strategy completion and move_sstables_from_staging()
             // from stepping on each other's toe.
             co_await sst->change_state(sstables::normal_dir, &delay_commit);
+            auto& cg = compaction_group_for_sstable(sst);
+            if (get_compaction_manager().requires_cleanup(cg.as_table_state(), sst)) {
+                compaction_groups_to_notify.insert(&cg);
+            }
             // If view building finished faster, SSTable with repair origin still exists.
             // It can also happen the SSTable is not going through reshape, so it doesn't have a repair origin.
             // That being said, we'll only add this SSTable to tracker if its origin is other than repair.
             // Otherwise, we can count on off-strategy completion to add it when updating lists.
             if (sst->get_origin() != sstables::repair_origin) {
-                add_sstable_to_backlog_tracker(compaction_group_for_sstable(sst).get_backlog_tracker(), sst);
+                add_sstable_to_backlog_tracker(cg.get_backlog_tracker(), sst);
             }
         } catch (...) {
             tlogger.warn("Failed to move sstable {} from staging: {}", sst->get_filename(), std::current_exception());
             throw;
         }
+    }
+
+    for (auto* cg : compaction_groups_to_notify) {
+        cg->get_staging_condition().broadcast();
     }
 
     co_await delay_commit.commit();
@@ -2803,6 +2812,10 @@ public:
     }
     const std::string& get_group_id() const noexcept override {
         return _cg.get_group_id();
+    }
+
+    seastar::condition_variable& get_staging_condition() noexcept override {
+        return _cg.get_staging_condition();
     }
 };
 

test/lib/test_services.cc
@@ -52,6 +52,7 @@ class table_for_tests::table_state : public compaction::table_state {
     mutable compaction_backlog_tracker _backlog_tracker;
     compaction::compaction_strategy_state _compaction_strategy_state;
     std::string _group_id;
+    seastar::condition_variable _staging_condition;
 private:
     replica::table& table() const noexcept {
         return *_data.cf;
@@ -123,6 +124,9 @@ public:
     }
     const std::string& get_group_id() const noexcept override {
         return _group_id;
+    }
+    seastar::condition_variable& get_staging_condition() noexcept override {
+        return _staging_condition;
     }
 };
 

scylladb/scylladb

compaction/table_state.hh
@@ -9,6 +9,8 @@
 
 #pragma once
 
+#include <seastar/core/condition-variable.hh>
+
 #include "schema/schema_fwd.hh"
 #include "compaction_descriptor.hh"
 
@@ -52,6 +54,7 @@ public:
     virtual const tombstone_gc_state& get_tombstone_gc_state() const noexcept = 0;
     virtual compaction_backlog_tracker& get_backlog_tracker() = 0;
     virtual const std::string& get_group_id() const noexcept = 0;
+    virtual seastar::condition_variable& get_staging_done_condition() noexcept = 0;
 };
 
 } // namespace compaction

replica/table.cc
@@ -2590,6 +2590,7 @@ table::make_reader_v2_excluding_sstables(schema_ptr s,
 future<> table::move_sstables_from_staging(std::vector<sstables::shared_sstable> sstables) {
     auto units = co_await get_units(_sstable_deletion_sem, 1);
     sstables::delayed_commit_changes delay_commit;
+    std::unordered_set<compaction_group*> compaction_groups_to_notify;
     for (auto sst : sstables) {
         try {
             // Off-strategy can happen in parallel to view building, so the SSTable may be deleted already if the former
@@ -2597,12 +2598,16 @@ future<> table::move_sstables_from_staging(std::vector<sstables::shared_sstable>
             // The _sstable_deletion_sem prevents list update on off-strategy completion and move_sstables_from_staging()
             // from stepping on each other's toe.
             co_await sst->change_state(sstables::normal_dir, &delay_commit);
+            auto& cg = compaction_group_for_sstable(sst);
+            if (get_compaction_manager().requires_cleanup(cg.as_table_state(), sst)) {
+                compaction_groups_to_notify.insert(&cg);
+            }
             // If view building finished faster, SSTable with repair origin still exists.
             // It can also happen the SSTable is not going through reshape, so it doesn't have a repair origin.
             // That being said, we'll only add this SSTable to tracker if its origin is other than repair.
             // Otherwise, we can count on off-strategy completion to add it when updating lists.
             if (sst->get_origin() != sstables::repair_origin) {
-                add_sstable_to_backlog_tracker(compaction_group_for_sstable(sst).get_backlog_tracker(), sst);
+                add_sstable_to_backlog_tracker(cg.get_backlog_tracker(), sst);
             }
         } catch (...) {
             tlogger.warn("Failed to move sstable {} from staging: {}", sst->get_filename(), std::current_exception());
@@ -2611,6 +2616,10 @@ future<> table::move_sstables_from_staging(std::vector<sstables::shared_sstable>
     }
 
     co_await delay_commit.commit();
+
+    for (auto* cg : compaction_groups_to_notify) {
+        cg->get_staging_done_condition().broadcast();
+    }
 
     // Off-strategy timer will be rearmed, so if there's more incoming data through repair / streaming,
     // the timer can be updated once again. In practice, it allows off-strategy compaction to kick off
@@ -2814,6 +2823,10 @@ public:
     }
     const std::string& get_group_id() const noexcept override {
         return _cg.get_group_id();
+    }
+
+    seastar::condition_variable& get_staging_done_condition() noexcept override {
+        return _cg.get_staging_done_condition();
     }
 };
 

test/lib/test_services.cc
@@ -52,6 +52,7 @@ class table_for_tests::table_state : public compaction::table_state {
     mutable compaction_backlog_tracker _backlog_tracker;
     compaction::compaction_strategy_state _compaction_strategy_state;
     std::string _group_id;
+    seastar::condition_variable _staging_condition;
 private:
     replica::table& table() const noexcept {
         return *_data.cf;
@@ -126,6 +127,9 @@ public:
     }
     const std::string& get_group_id() const noexcept override {
         return _group_id;
+    }
+    seastar::condition_variable& get_staging_done_condition() noexcept override {
+        return _staging_condition;
     }
 };
 