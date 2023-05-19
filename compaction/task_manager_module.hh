e486f4eba60866138f2e93f7a174138e598edb3e
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-05-19 17:10:40 +0200

    compaction: create resharding_compaction_task_impl
    resharding_compaction_task_impl serves as a base class of all
    concrete resharding compaction task classes.

--- compaction/task_manager_module.hh
+++ compaction/task_manager_module.hh
@@ -550,6 +550,28 @@ protected:
     virtual future<> run() override;
 };
 
+
+class resharding_compaction_task_impl : public compaction_task_impl {
+public:
+    resharding_compaction_task_impl(tasks::task_manager::module_ptr module,
+            tasks::task_id id,
+            unsigned sequence_number,
+            std::string keyspace,
+            std::string table,
+            std::string entity,
+            tasks::task_id parent_id) noexcept
+        : compaction_task_impl(module, id, sequence_number, std::move(keyspace), std::move(table), std::move(entity), parent_id)
+    {
+        // FIXME: add progress units
+    }
+
+    virtual std::string type() const override {
+        return "resharding compaction";
+    }
+protected:
+    virtual future<> run() override = 0;
+};
+
 class task_manager_module : public tasks::task_manager::module {
 public:
     task_manager_module(tasks::task_manager& tm) noexcept : tasks::task_manager::module(tm, "compaction") {}
