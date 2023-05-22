scylladb/scylladb

commit 0d4ffe1d69d70848446129d47570808d618112d7

scripts/refresh-submodules.sh: include all commits in summary
before this change, we suse `git submodule summary ${submodule}`
for collecting the titles of commits in between current HEAD and
origin/master. normally, this works just fine. but it fails to
collect all commits if the origin/master happens to reference
a merge commit. for instance, if we have following history like:

1. merge foo
2. bar
3. foo
4. baz  <--- submodule is pointing here.

`git submodule summary` would just print out the titles of commits
of 1 and 3.

so, in this change, instead of relying on `git submodule summary`,
we just collect the commits using `git log`. but we preserve the
output format used by `git submodule summary` to be consistent with
the previous commits bumping up the submodules. please note, in
this change instead of matching the output of `git submodule summary`,
we use `git merge-base --is-ancestor HEAD origin/master` to check
if we are going to create a fastforward change, this is less fragile.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

Closes #13366

scripts/refresh-submodules.sh
@@ -25,12 +25,28 @@ submodules=(
 for ent in "${@:-${submodules[@]}}"; do
     submodule=${ent%%:*}
     [ ${submodule} == ${ent} ] && branch="master" || branch=${ent#*:}
-    GIT_DIR="$submodule/.git" git pull --ff-only origin ${branch}
-    SUMMARY=$(git submodule summary $submodule)
-    if grep '^ *<' <<< "$SUMMARY"; then
+    bump_to=origin/${branch}
+
+    pushd .
+    cd $submodule
+    git fetch origin ${branch}
+    if ! git merge-base --is-ancestor HEAD ${bump_to}; then
         echo "Non fast-forward changes detected! Fire three red flares from your flare pistol."
         exit 1
     fi
+    # collect the summary
+    head_ref=$(git rev-parse --short=8 HEAD)
+    branch_ref=$(git rev-parse --short=8 ${branch})
+    count=$(git rev-list --no-merges --count HEAD..${bump_to})
+    # create a summary using the output format of "git submodule summary"
+    SUMMARY="
+* ${submodule} ${head_ref}...${branch_ref} (${count}):
+$(git log --pretty='format:  > %s' --no-merges HEAD..${bump_to})"
+
+    # fast-forward to origin/master
+    git merge --ff-only ${bump_to}
+    popd
+
     if [ ! -z "$SUMMARY" ]; then
         git commit --edit -m "Update $submodule submodule" -m "$SUMMARY" $submodule
     fi

commit 15eed3bc7f9c7f0a8825ba492d35746300a7c986

scripts/refresh-submodules.sh: use the correct sha1 in title
0d4ffe1d69d70848446129d47570808d618112d7 introduced a regression where
it used the sha1 of the local "master" branch instead of the remote's
"master" branch in the title of the commit message.

in this change, let's use the origin/${branch}'s sha1 in the title.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

scripts/refresh-submodules.sh
@@ -36,7 +36,7 @@ for ent in "${@:-${submodules[@]}}"; do
     fi
     # collect the summary
     head_ref=$(git rev-parse --short=8 HEAD)
-    branch_ref=$(git rev-parse --short=8 ${branch})
+    branch_ref=$(git rev-parse --short=8 origin/${branch})
     count=$(git rev-list --no-merges --count HEAD..${bump_to})
     # create a summary using the output format of "git submodule summary"
     SUMMARY="