acc035b504f87116c36628d15e65705d148eb5af
Gleb Natapov <gleb@scylladb.com>
2023-05-28 16:20:21 +0300

    storage_service: do not allow override_decommission flag if consistent cluster management is enabled
    If consistent cluster management is enabled it is not possible to
    restart decommissioned node since it will not be part of the grouup0.

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -1562,11 +1562,11 @@ future<> storage_service::join_token_ring(cdc::generation_service& cdc_gen_servi
     std::optional<cdc::generation_id> cdc_gen_id;
 
     if (_sys_ks.local().was_decommissioned()) {
-        if (_db.local().get_config().override_decommission()) {
+        if (_db.local().get_config().override_decommission() && !_db.local().get_config().consistent_cluster_management()) {
             slogger.warn("This node was decommissioned, but overriding by operator request.");
             co_await _sys_ks.local().set_bootstrap_state(db::system_keyspace::bootstrap_state::COMPLETED);
         } else {
-            auto msg = sstring("This node was decommissioned and will not rejoin the ring unless override_decommission=true has been set,"
+            auto msg = sstring("This node was decommissioned and will not rejoin the ring unless override_decommission=true has been set and consistent cluster management is not in use,"
                                "or all existing data is removed and the node is bootstrapped again");
             slogger.error("{}", msg);
             throw std::runtime_error(msg);
