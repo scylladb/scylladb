7c545826c0c9429005d6dc1a08ad9e8ed4fca75e
Pavel Emelyanov <xemul@scylladb.com>
2023-06-02 15:31:32 +0300

    compaction: Drop shared-sstable formatter
    No it's unused, all code uses fmt::range, boost adaptors and
    formatter<sstable> the way they like
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- sstables/shared_sstable.hh
+++ sstables/shared_sstable.hh
@@ -38,22 +38,4 @@ namespace sstables {
 using shared_sstable = seastar::lw_shared_ptr<sstable>;
 using sstable_list = std::unordered_set<shared_sstable>;
 
-std::string to_string(const shared_sstable& sst, bool include_origin = true);
-
 } // namespace sstables
-
-template <>
-struct fmt::formatter<sstables::shared_sstable> : fmt::formatter<std::string_view> {
-    template <typename FormatContext>
-    auto format(const sstables::shared_sstable& sst, FormatContext& ctx) const {
-        return fmt::format_to(ctx.out(), "{}", sstables::to_string(sst));
-    }
-};
-
-namespace std {
-
-inline std::ostream& operator<<(std::ostream& os, const sstables::shared_sstable& sst) {
-    return os << fmt::format("{}", sst);
-}
-
-} // namespace std
