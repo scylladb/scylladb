a12d2d5f168e0104cf91b86a6bace5726e47895a
Aarav Arora <aaravarora.works@gmail.com>
2023-06-02 18:10:53 +0530

    fix: keyspace spell
    Closes #14121

--- db/system_distributed_keyspace.cc
+++ db/system_distributed_keyspace.cc
@@ -286,7 +286,7 @@ future<> system_distributed_keyspace::start() {
                     "Create system_distributed keyspace");
         } catch (exceptions::already_exists_exception&) {}
     } else {
-        dlogger.info("{} keyspase is already present. Not creating", NAME);
+        dlogger.info("{} keyspace is already present. Not creating", NAME);
     }
 
     if (!_sp.get_db().local().has_keyspace(NAME_EVERYWHERE)) {
@@ -303,7 +303,7 @@ future<> system_distributed_keyspace::start() {
                     "Create system_distributed_everywhere keyspace");
         } catch (exceptions::already_exists_exception&) {}
     } else {
-        dlogger.info("{} keyspase is already present. Not creating", NAME_EVERYWHERE);
+        dlogger.info("{} keyspace is already present. Not creating", NAME_EVERYWHERE);
     }
 
     auto tables = ensured_tables();
