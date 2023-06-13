8848c3b809f3ea251fc945e32c6db0546b3e7e76
Kamil Braun <kbraun@scylladb.com>
2023-06-13 15:03:17 +0200

    replica: database: write a comment for `parse_system_tables`

--- replica/database.hh
+++ replica/database.hh
@@ -1456,7 +1456,10 @@ public:
         }
     };
 
+    // Load the schema definitions kept in schema tables from disk and initialize in-memory schema data structures
+    // (keyspace/table definitions, column mappings etc.)
     future<> parse_system_tables(distributed<service::storage_proxy>&, sharded<db::system_keyspace>&);
+
     database(const db::config&, database_config dbcfg, service::migration_notifier& mn, gms::feature_service& feat, const locator::shared_token_metadata& stm,
             compaction_manager& cm, sstables::storage_manager& sstm, sharded<sstables::directory_semaphore>& sst_dir_sem, utils::cross_shard_barrier barrier = utils::cross_shard_barrier(utils::cross_shard_barrier::solo{}) /* for single-shard usage */);
     database(database&&) = delete;
