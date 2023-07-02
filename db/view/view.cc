fc7b02c8e4cd07a57b042bdb944ca27872343424
Michał Chojnowski <michal.chojnowski@scylladb.com>
2023-07-02 10:58:37 +0200

    view: fix range tombstone handling on flushes in view_updating_consumer
    View update routines accept `mutation` objects.
    But what comes out of staging sstable readers is a stream of
    mutation_fragment_v2 objects.
    To build view updates after a repair/streaming, we have to
    convert the fragment stream into `mutation`s. This is done by piping
    the stream to mutation_rebuilder_v2.
    
    To keep memory usage limited, the stream for a single partition might
    have to be split into multiple partial `mutation` objects.
    view_update_consumer does that, but in improper way -- when the
    split/flush happens inside an active range tombstone, the range
    tombstone isn't closed properly. This is illegal, and triggers an
    internal error.
    
    This patch fixes the problem by closing the active range tombstone
    (and reopening in the same position in the next `mutation` object).
    
    The tombstone is closed just after the last seen clustered position.
    This is not necessary for correctness -- for example we could delay
    all processing of the range tombstone until we see its end
    bound -- but it seems like the most natural semantic.
    
    Fixes #14503

--- db/view/view.cc
+++ db/view/view.cc
@@ -2577,6 +2577,10 @@ void view_updating_consumer::do_flush_buffer() {
 }
 
 void view_updating_consumer::flush_builder() {
+    _buffer.emplace_back(_mut_builder->flush());
+}
+
+void view_updating_consumer::end_builder() {
     _mut_builder->consume_end_of_partition();
     if (auto mut_opt = _mut_builder->consume_end_of_stream()) {
         _buffer.emplace_back(std::move(*mut_opt));
@@ -2587,9 +2591,7 @@ void view_updating_consumer::flush_builder() {
 void view_updating_consumer::maybe_flush_buffer_mid_partition() {
     if (_buffer_size >= buffer_size_hard_limit) {
         flush_builder();
-        auto dk = _buffer.back().decorated_key();
         do_flush_buffer();
-        consume_new_partition(dk);
     }
 }
 
