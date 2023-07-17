d560dd807c118e762c5bd725f80479b6793b7e46
Raphael S. Carvalho <raphaelsc@scylladb.com>
2023-07-17 13:42:44 -0300

    Fix test.py with compaction groups
    test.py with --x-log2-compaction-groups option rotted a little bit.
    Some boost tests added later didn't use the correct header which
    parses the option or they didn't adjust suite.yaml.
    Perhaps it's time to set up a weekly (or bi-weekly) job to verify
    there are no regressions with it. It's important as it stresses
    the data plane for tablets reusing the existing tests available.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- test/boost/recent_entries_map_test.cc
+++ test/boost/recent_entries_map_test.cc
@@ -7,7 +7,7 @@
  */
 
 #include <boost/test/unit_test.hpp>
-#include <seastar/testing/thread_test_case.hh>
+#include "test/lib/scylla_test_case.hh"
 #include <seastar/core/manual_clock.hh>
 #include <chrono>
 
--- test/boost/row_cache_test.cc
+++ test/boost/row_cache_test.cc
@@ -14,8 +14,7 @@
 #include <boost/algorithm/cxx11/any_of.hpp>
 #include <seastar/util/closeable.hh>
 
-#include <seastar/testing/test_case.hh>
-#include <seastar/testing/thread_test_case.hh>
+#include "test/lib/scylla_test_case.hh"
 #include "test/lib/mutation_assertions.hh"
 #include "test/lib/flat_mutation_reader_assertions.hh"
 #include "test/lib/mutation_source_test.hh"
