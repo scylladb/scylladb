d67e22b87657cb1e022301968b1fe6e07a6c7e7f
Botond Dénes <bdenes@scylladb.com>
2023-07-26 10:32:59 -0400

    tools/scylla-nodetool: implement basic scylla_rest_api_client
    Add --host and --port parameters, parse and resolve these and
    establish a connection to the provided host.
    Add a simple get() and post() method, parsing the returned data as json.
    
    Add the following compatibility arguments:
    * password,pw
    * password-file,pwf
    * username,u
    * print-port,pp
    
    These are parsed and silently ignored, as they are specific to JMX and
    aren't needed when connecting to the REST API.
    Since boost program options doesn't support multi-char short-form
    switches, as well as the -short=value syntax, the argv has to be massaged
    into a form which boost program options can digest. This is achieved by
    substituting all incompatible option formats and syntax with the
    equivalent boost program options compatible one.
    This mechanism is also used to make sure -h is translated to --host, not
    --help. The help message is unfortunately still ambiguous, displaying
    both with -h. This will be addressed in a follow-up.

--- tools/scylla-nodetool.cc
+++ tools/scylla-nodetool.cc
@@ -9,9 +9,13 @@
 #include <boost/algorithm/string/join.hpp>
 #include <boost/range/adaptor/map.hpp>
 #include <seastar/core/thread.hh>
+#include <seastar/http/request.hh>
+#include <seastar/util/short_streams.hh>
 
 #include "log.hh"
 #include "tools/utils.hh"
+#include "utils/http.hh"
+#include "utils/rjson.hh"
 
 namespace bpo = boost::program_options;
 
@@ -24,21 +28,127 @@ const auto app_name = "scylla-nodetool";
 logging::logger nlog(app_name);
 
 class scylla_rest_client {
+    sstring _host;
+    uint16_t _port;
+    sstring _host_name;
+
+    rjson::value do_request(sstring type, sstring path, std::unordered_map<sstring, sstring> params) {
+        auto req = http::request::make(type, _host_name, path);
+        req.query_parameters = std::move(params);
+
+        nlog.trace("Making {} request to {} with parameters {}", type, req.get_url(), req.query_parameters);
+
+        sstring res;
+
+        http::experimental::client api_client(std::make_unique<utils::http::dns_connection_factory>(_host, _port, false, nlog), 1);
+        try {
+            api_client.make_request(std::move(req), seastar::coroutine::lambda([&] (const http::reply&, input_stream<char> body) -> future<> {
+                res = co_await util::read_entire_stream_contiguous(body);
+            })).get();
+        } catch (...) {
+            api_client.close().get();
+            throw;
+        }
+        api_client.close().get();
+
+        if (res.empty()) {
+            return rjson::null_value();
+        } else {
+            return rjson::parse(res);
+        }
+    }
+
+public:
+    scylla_rest_client(sstring host, uint16_t port)
+        : _host(std::move(host))
+        , _port(port)
+        , _host_name(format("{}:{}", _host, _port))
+    { }
+
+    rjson::value post(sstring path, std::unordered_map<sstring, sstring> params = {}) {
+        return do_request("POST", std::move(path), std::move(params));
+    }
+
+    rjson::value get(sstring path, std::unordered_map<sstring, sstring> params = {}) {
+        return do_request("GET", std::move(path), std::move(params));
+    }
 };
 
 using operation_func = void(*)(scylla_rest_client&, const bpo::variables_map&);
 
 const std::vector<operation_option> global_options{
+    typed_option<sstring>("host,h", "localhost", "the hostname or ip address of the ScyllaDB node"),
+    typed_option<uint16_t>("port,p", 10000, "the port of the REST API of the ScyllaDB node"),
+    typed_option<sstring>("password", "Remote jmx agent password (unused)"),
+    typed_option<sstring>("password-file", "Path to the JMX password file (unused)"),
+    typed_option<sstring>("username,u", "Remote jmx agent username (unused)"),
+    typed_option<>("print-port", "Operate in 4.0 mode with hosts disambiguated by port number (unused)"),
+};
+
+const std::map<std::string_view, std::string_view> option_substitutions{
+    {"-h", "--host"},
+    {"-pw", "--password"},
+    {"-pwf", "--password-file"},
+    {"-pp", "--print-port"},
 };
 
 const std::map<operation, operation_func> operations_with_func{
 };
+
+// boost::program_options doesn't allow multi-char option short-form,
+// e.g. -pw, that C*'s nodetool uses. We silently map these to the
+// respective long-form and pass the transformed argv to tool_app_template.
+// Furthermore, C* nodetool allows for assigning values to short-form
+// arguments with =, e.g. -h=localhost, something which boost::program_options
+// also doesn't support. We silently replace all = with space to support this.
+// So, e.g. "-h=localhost" becomes "-h localhost".
+std::vector<char*> massage_argv(int argc, char** argv) {
+    static std::vector<std::string> argv_holder;
+    argv_holder.reserve(argc);
+
+    for (size_t i = 0; i < argc; ++i) {
+        if (argv[i][0] != '-') {
+            argv_holder.push_back(argv[i]);
+            continue;
+        }
+
+        std::string arg = argv[i];
+        std::string arg_key;
+        std::optional<std::string> arg_value;
+
+        if (auto pos = arg.find('='); pos == std::string::npos) {
+            arg_key = std::move(arg);
+        } else {
+            arg_key = arg.substr(0, pos);
+            arg_value = arg.substr(pos + 1);
+        }
+
+        const auto it = option_substitutions.find(arg_key);
+        if (it != option_substitutions.end()) {
+            nlog.trace("Substituting cmd-line arg {} with {}", arg_key, it->second);
+            arg_key = it->second;
+        }
+
+        argv_holder.push_back(std::move(arg_key));
+        if (arg_value) {
+            argv_holder.push_back(std::move(*arg_value));
+        }
+    }
+
+    std::vector<char*> new_argv;
+    new_argv.reserve(argv_holder.size());
+    std::ranges::transform(argv_holder, std::back_inserter(new_argv), [] (std::string& arg) -> char* { return arg.data(); });
+    return new_argv;
+}
 
 } // anonymous namespace
 
 namespace tools {
 
 int scylla_nodetool_main(int argc, char** argv) {
+    auto replacement_argv = massage_argv(argc, argv);
+    nlog.debug("replacement argv: {}", replacement_argv);
+
     const auto description_template =
 R"(scylla-nodetool - a command-line tool to administer local or remote ScyllaDB nodes
 
@@ -66,8 +176,8 @@ For more information, see: https://opensource.docs.scylladb.com/stable/operating
             .global_options = &global_options};
     tool_app_template app(std::move(app_cfg));
 
-    return app.run_async(argc, argv, [] (const operation& operation, const bpo::variables_map& app_config) {
-        scylla_rest_client client;
+    return app.run_async(replacement_argv.size(), replacement_argv.data(), [] (const operation& operation, const bpo::variables_map& app_config) {
+        scylla_rest_client client(app_config["host"].as<sstring>(), app_config["port"].as<uint16_t>());
 
         try {
             operations_with_func.at(operation)(client, app_config);
