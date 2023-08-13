07f5b3da47c7213650b1a0e2818dc4c83984abce
Amnon Heiman <amnon@scylladb.com>
2023-08-13 08:49:21 -0400

    estimated_histogram.hh: reuse approx_exponential_histogram
    Seastar now holds an estimated histogram implementation; this patch
    reuses the seater implementation by aliasing the names in ScyllaDB.
    This first remove unnecessary code duplication, but it's also important
    for the future Prometheus native histogram support.
    
    Making specific changes in a code closer to the metrics and Prometheus
    implementation is cleaner than mixing Scylla code and seastar
    metric-specific details.
    
    Because Seastar code was originally in ScyllaDB aside from the aliasing,
    no other changes are needed.
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>

--- utils/histogram_metrics_helper.hh
+++ utils/histogram_metrics_helper.hh
@@ -18,22 +18,7 @@
 
 template<uint64_t Min, uint64_t Max, size_t Precision>
 seastar::metrics::histogram to_metrics_histogram(const utils::approx_exponential_histogram<Min, Max, Precision>& hist) {
-    seastar::metrics::histogram res;
-    res.buckets.resize(hist.size() - 1);
-    uint64_t cummulative_count = 0;
-    res.sample_sum = 0;
-
-    for (size_t i = 0; i < hist.NUM_BUCKETS - 1; i++) {
-        auto& v = res.buckets[i];
-        v.upper_bound = hist.get_bucket_lower_limit(i + 1);
-        cummulative_count += hist.get(i);
-        v.count = cummulative_count;
-        res.sample_sum += hist.get(i) * v.upper_bound;
-    }
-    // The count serves as the infinite bucket
-    res.sample_count = cummulative_count + hist.get(hist.NUM_BUCKETS - 1);
-    res.sample_sum += hist.get(hist.NUM_BUCKETS - 1) * hist.get_bucket_lower_limit(hist.NUM_BUCKETS - 1);
-    return res;
+    return hist.to_metrics_histogram();
 }
 
 /*!
