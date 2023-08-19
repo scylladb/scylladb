3ab3a2cc1bf232d5d9e90c4718016677bf6b6abc
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2023-08-19 14:18:52 +0200

    cql3: statements: pass data_dictionary::database to check_restricted_table_properties
    Pass data_dictionary::database to check_restricted_table_properties
    as an arguemnt instead of query_processor as the method will be called
    from a context which does not have access to query processor.
    
    (cherry picked from commit ec98b182c829136d2a8feffc220766a8aeb1f50c)

--- cql3/statements/alter_table_statement.cc
+++ cql3/statements/alter_table_statement.cc
@@ -410,7 +410,7 @@ cql3::statements::alter_table_statement::prepare(data_dictionary::database db, c
 future<::shared_ptr<messages::result_message>>
 alter_table_statement::execute(query_processor& qp, service::query_state& state, const query_options& options) const {
     auto s = validation::validate_column_family(qp.db(), keyspace(), column_family());
-    std::optional<sstring> warning = check_restricted_table_properties(qp, s, keyspace(), column_family(), *_properties);
+    std::optional<sstring> warning = check_restricted_table_properties(qp.db(), s, keyspace(), column_family(), *_properties);
     return schema_altering_statement::execute(qp, state, options).then([this, warning = std::move(warning)] (::shared_ptr<messages::result_message> msg) {
         if (warning) {
             msg->add_warning(*warning);
