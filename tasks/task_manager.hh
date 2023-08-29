f9a763539052b44612c4d40b1a15f4d6cd687dcc
Benny Halevy <bhalevy@scylladb.com>
2023-08-29 19:28:09 +0300

    task_manager: module: make_task: enter gate when the task is created
    Passing the gate_closed_exception to the task promise in start()
    ends up with abandoned exception since no-one is waiting
    for it.
    
    Instead, enter the gate when the task is made
    so it will fail make_task if the gate is already closed.
    
    Fixes scylladb/scylladb#15211
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tasks/task_manager.hh
+++ tasks/task_manager.hh
@@ -139,8 +139,10 @@ public:
         using task_impl_ptr = shared_ptr<impl>;
     protected:
         task_impl_ptr _impl;
+    private:
+        gate::holder _gate_holder;
     public:
-        task(task_impl_ptr&& impl) noexcept;
+        task(task_impl_ptr&& impl, gate::holder) noexcept;
 
         task_id id();
         std::string type() const;
