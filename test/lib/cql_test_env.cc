9ec0368a3b7415d352a1df6e50e55d1c4036d0ce
Pavel Emelyanov <xemul@scylladb.com>
2023-08-31 19:43:25 +0300

    storage_service: Remove query processor arg from join_cluster()
    The s.service since d42685d0cb6 is having on-board query processor ref^w
    pointer and can use it to join cluster
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/lib/cql_test_env.cc
+++ test/lib/cql_test_env.cc
@@ -841,7 +841,7 @@ private:
             _ss.local().set_group0(group0_service, raft_topology_change_enabled);
 
             try {
-                _ss.local().join_cluster(_sys_dist_ks, _proxy, _qp.local()).get();
+                _ss.local().join_cluster(_sys_dist_ks, _proxy).get();
             } catch (std::exception& e) {
                 // if any of the defers crashes too, we'll never see
                 // the error
