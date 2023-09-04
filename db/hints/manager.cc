679614cc1d27bca086af75dc86af2ed93cd7e1a9
Dawid Medrek <dawid.medrek@scylladb.com>
2023-09-04 22:03:02 +0200

    db/hints: Extract host_manager from manager.hh
    This is the second step in the modularization of the manager.hh.
    We move the logic of end_point_hints_manager to a separate file
    to improve readability, better isolate the code, and finally
    help maintaining the module by splitting it into smaller units,
    which are easier to reason about.
    
    This commit also renames the data structure to host_manager, which
    is shorter while still conveying its purpose.

--- db/hints/manager.cc
+++ db/hints/manager.cc
@@ -155,7 +155,7 @@ void manager::forbid_hints() {
 void manager::forbid_hints_for_eps_with_pending_hints() {
     manager_logger.trace("space_watchdog: Going to block hints to: {}", _eps_with_pending_hints);
     boost::for_each(_ep_managers, [this] (auto& pair) {
-        end_point_hints_manager& ep_man = pair.second;
+        host_manager& ep_man = pair.second;
         if (has_ep_with_pending_hints(ep_man.end_point_key())) {
             ep_man.forbid_hints();
         } else {
@@ -169,7 +169,7 @@ sync_point::shard_rps manager::calculate_current_sync_point(const std::vector<en
     for (auto addr : target_hosts) {
         auto it = _ep_managers.find(addr);
         if (it != _ep_managers.end()) {
-            const end_point_hints_manager& ep_man = it->second;
+            const host_manager& ep_man = it->second;
             rps[ep_man.end_point_key()] = ep_man.last_written_replay_position();
         }
     }
@@ -222,139 +222,11 @@ future<> manager::wait_for_sync_point(abort_source& as, const sync_point::shard_
     co_return;
 }
 
-bool manager::end_point_hints_manager::store_hint(schema_ptr s, lw_shared_ptr<const frozen_mutation> fm, tracing::trace_state_ptr tr_state) noexcept {
-    try {
-        // Future is waited on indirectly in `stop()` (via `_store_gate`).
-        (void)with_gate(_store_gate, [this, s = std::move(s), fm = std::move(fm), tr_state] () mutable {
-            ++_hints_in_progress;
-            size_t mut_size = fm->representation().size();
-            shard_stats().size_of_hints_in_progress += mut_size;
-
-            return with_shared(file_update_mutex(), [this, fm, s, tr_state] () mutable -> future<> {
-                return get_or_load().then([this, fm = std::move(fm), s = std::move(s), tr_state] (hints_store_ptr log_ptr) mutable {
-                    commitlog_entry_writer cew(s, *fm, db::commitlog::force_sync::no);
-                    return log_ptr->add_entry(s->id(), cew, db::timeout_clock::now() + _shard_manager.hint_file_write_timeout);
-                }).then([this, tr_state] (db::rp_handle rh) {
-                    auto rp = rh.release();
-                    if (_last_written_rp < rp) {
-                        _last_written_rp = rp;
-                        manager_logger.debug("[{}] Updated last written replay position to {}", end_point_key(), rp);
-                    }
-                    ++shard_stats().written;
-
-                    manager_logger.trace("Hint to {} was stored", end_point_key());
-                    tracing::trace(tr_state, "Hint to {} was stored", end_point_key());
-                }).handle_exception([this, tr_state] (std::exception_ptr eptr) {
-                    ++shard_stats().errors;
-
-                    manager_logger.debug("store_hint(): got the exception when storing a hint to {}: {}", end_point_key(), eptr);
-                    tracing::trace(tr_state, "Failed to store a hint to {}: {}", end_point_key(), eptr);
-                });
-            }).finally([this, mut_size, fm, s] {
-                --_hints_in_progress;
-                shard_stats().size_of_hints_in_progress -= mut_size;
-            });;
-        });
-    } catch (...) {
-        manager_logger.trace("Failed to store a hint to {}: {}", end_point_key(), std::current_exception());
-        tracing::trace(tr_state, "Failed to store a hint to {}: {}", end_point_key(), std::current_exception());
-
-        ++shard_stats().dropped;
-        return false;
-    }
-    return true;
-}
-
-future<> manager::end_point_hints_manager::populate_segments_to_replay() {
-    return with_lock(file_update_mutex(), [this] {
-        return get_or_load().discard_result();
-    });
-}
-
-void manager::end_point_hints_manager::start() {
-    clear_stopped();
-    allow_hints();
-    _sender.start();
-}
-
-future<> manager::end_point_hints_manager::stop(drain should_drain) noexcept {
-    if(stopped()) {
-        return make_exception_future<>(std::logic_error(format("ep_manager[{}]: stop() is called twice", _key).c_str()));
-    }
-
-    return seastar::async([this, should_drain] {
-        std::exception_ptr eptr;
-
-        // This is going to prevent further storing of new hints and will break all sending in progress.
-        set_stopping();
-
-        _store_gate.close().handle_exception([&eptr] (auto e) { eptr = std::move(e); }).get();
-        _sender.stop(should_drain).handle_exception([&eptr] (auto e) { eptr = std::move(e); }).get();
-
-        with_lock(file_update_mutex(), [this] {
-            if (_hints_store_anchor) {
-                hints_store_ptr tmp = std::exchange(_hints_store_anchor, nullptr);
-                return tmp->shutdown().finally([tmp] {
-                    return tmp->release();
-                }).finally([tmp] {});
-            }
-            return make_ready_future<>();
-        }).handle_exception([&eptr] (auto e) { eptr = std::move(e); }).get();
-
-        if (eptr) {
-            manager_logger.error("ep_manager[{}]: exception: {}", _key, eptr);
-        }
-
-        set_stopped();
-    });
-}
-
-manager::end_point_hints_manager::end_point_hints_manager(const endpoint_id& key, manager& shard_manager)
-    : _key(key)
-    , _shard_manager(shard_manager)
-    , _file_update_mutex_ptr(make_lw_shared<seastar::shared_mutex>())
-    , _file_update_mutex(*_file_update_mutex_ptr)
-    , _state(state_set::of<state::stopped>())
-    , _hints_dir(_shard_manager.hints_dir() / format("{}", _key).c_str())
-    // Approximate the position of the last written hint by using the same formula as for segment id calculation in commitlog
-    // TODO: Should this logic be deduplicated with what is in the commitlog?
-    , _last_written_rp(this_shard_id(), std::chrono::duration_cast<std::chrono::milliseconds>(runtime::get_boot_time().time_since_epoch()).count())
-    , _sender(*this, _shard_manager.local_storage_proxy(), _shard_manager.local_db(), _shard_manager.local_gossiper())
-{}
-
-manager::end_point_hints_manager::end_point_hints_manager(end_point_hints_manager&& other)
-    : _key(other._key)
-    , _shard_manager(other._shard_manager)
-    , _file_update_mutex_ptr(std::move(other._file_update_mutex_ptr))
-    , _file_update_mutex(*_file_update_mutex_ptr)
-    , _state(other._state)
-    , _hints_dir(std::move(other._hints_dir))
-    , _last_written_rp(other._last_written_rp)
-    , _sender(other._sender, *this)
-{}
-
-manager::end_point_hints_manager::~end_point_hints_manager() {
-    assert(stopped());
-}
-
-future<hints_store_ptr> manager::end_point_hints_manager::get_or_load() {
-    if (!_hints_store_anchor) {
-        return _shard_manager.store_factory().get_or_load(_key, [this] (const endpoint_id&) noexcept {
-            return add_store();
-        }).then([this] (hints_store_ptr log_ptr) {
-            _hints_store_anchor = log_ptr;
-            return make_ready_future<hints_store_ptr>(std::move(log_ptr));
-        });
-    }
-
-    return make_ready_future<hints_store_ptr>(_hints_store_anchor);
-}
-
-manager::end_point_hints_manager& manager::get_ep_manager(endpoint_id ep) {
+manager::host_manager& manager::get_ep_manager(endpoint_id ep) {
     auto it = find_ep_manager(ep);
     if (it == ep_managers_end()) {
         manager_logger.trace("Creating an ep_manager for {}", ep);
-        manager::end_point_hints_manager& ep_man = _ep_managers.emplace(ep, end_point_hints_manager(ep, *this)).first->second;
+        manager::host_manager& ep_man = _ep_managers.emplace(ep, host_manager(ep, *this)).first->second;
         ep_man.start();
         return ep_man;
     }
@@ -384,131 +256,6 @@ bool manager::store_hint(endpoint_id ep, schema_ptr s, lw_shared_ptr<const froze
         ++_stats.errors;
         return false;
     }
-}
-
-future<db::commitlog> manager::end_point_hints_manager::add_store() noexcept {
-    manager_logger.trace("Going to add a store to {}", _hints_dir.c_str());
-
-    return futurize_invoke([this] {
-        return io_check([name = _hints_dir.c_str()] { return recursive_touch_directory(name); }).then([this] () {
-            commitlog::config cfg;
-
-            cfg.commit_log_location = _hints_dir.c_str();
-            cfg.commitlog_segment_size_in_mb = resource_manager::hint_segment_size_in_mb;
-            cfg.commitlog_total_space_in_mb = resource_manager::max_hints_per_ep_size_mb;
-            cfg.fname_prefix = manager::FILENAME_PREFIX;
-            cfg.extensions = &_shard_manager.local_db().extensions();
-
-            // HH leaves segments on disk after commitlog shutdown, and later reads
-            // them when commitlog is re-created. This is expected to happen regularly
-            // during standard HH workload, so no need to print a warning about it.
-            cfg.warn_about_segments_left_on_disk_after_shutdown = false;
-            // Allow going over the configured size limit of the commitlog
-            // (resource_manager::max_hints_per_ep_size_mb). The commitlog will
-            // be more conservative with its disk usage when going over the limit.
-            // On the other hand, HH counts used space using the space_watchdog
-            // in resource_manager, so its redundant for the commitlog to apply
-            // a hard limit.
-            cfg.allow_going_over_size_limit = true;
-            // The API for waiting for hint replay relies on replay positions
-            // monotonically increasing. When there are no segments on disk,
-            // by default the commitlog will calculate the first segment ID
-            // based on the boot time. This may cause the following sequence
-            // of events to occur:
-            //
-            // 1. Node starts with empty hints queue
-            // 2. Some hints are written and some segments are created
-            // 3. All hints are replayed
-            // 4. Hint sync point is created
-            // 5. Commitlog instance gets re-created and resets it segment ID counter
-            // 6. New hint segment has the first ID as the first (deleted by now) segment
-            // 7. Waiting for the sync point commences but resolves immediately
-            //    before new hints are replayed - since point 5., `_last_written_rp`
-            //    and `_sent_upper_bound_rp` are not updated because RPs of new
-            //    hints are much lower than both of those marks.
-            //
-            // In order to prevent this situation, we override the base segment ID
-            // of the newly created commitlog instance - it should start with an ID
-            // which is larger than the segment ID of the RP of the last written hint.
-            cfg.base_segment_id = _last_written_rp.base_id();
-
-            return commitlog::create_commitlog(std::move(cfg)).then([this] (commitlog l) -> future<commitlog> {
-                // add_store() is triggered every time hint files are forcefully flushed to I/O (every hints_flush_period).
-                // When this happens we want to refill _sender's segments only if it has finished with the segments he had before.
-                if (_sender.have_segments()) {
-                    co_return l;
-                }
-
-                std::vector<sstring> segs_vec = co_await l.get_segments_to_replay();
-
-                if (segs_vec.empty()) {
-                    // If the segs_vec is empty, this means that there are no more
-                    // hints to be replayed. We can safely skip to the position of the
-                    // last written hint.
-                    //
-                    // This is necessary: remember that we artificially set
-                    // the last replayed position based on the creation time
-                    // of the endpoint manager. If we replay all segments from
-                    // previous runtimes but won't write any new hints during
-                    // this runtime, then without the logic below the hint replay
-                    // tracker won't reach the hint written tracker.
-                    auto rp = _last_written_rp;
-                    rp.pos++;
-                    _sender.rewind_sent_replay_position_to(rp);
-                    co_return l;
-                }
-
-                std::vector<std::pair<db::segment_id_type, sstring>> local_segs_vec;
-                local_segs_vec.reserve(segs_vec.size());
-
-                // Divide segments into those that were created on this shard
-                // and those which were moved to it during rebalancing.
-                for (auto& seg : segs_vec) {
-                    db::commitlog::descriptor desc(seg, manager::FILENAME_PREFIX);
-                    unsigned shard_id = db::replay_position(desc).shard_id();
-                    if (shard_id == this_shard_id()) {
-                        local_segs_vec.emplace_back(desc.id, std::move(seg));
-                    } else {
-                        _sender.add_foreign_segment(std::move(seg));
-                    }
-                }
-
-                // Sort local segments by their segment ids, which should
-                // correspond to the chronological order.
-                std::sort(local_segs_vec.begin(), local_segs_vec.end());
-
-                for (auto& [segment_id, seg] : local_segs_vec) {
-                    _sender.add_segment(std::move(seg));
-                }
-
-                co_return l;
-            });
-        });
-    });
-}
-
-future<> manager::end_point_hints_manager::flush_current_hints() noexcept {
-    // flush the currently created hints to disk
-    if (_hints_store_anchor) {
-        return futurize_invoke([this] {
-            return with_lock(file_update_mutex(), [this]() -> future<> {
-                return get_or_load().then([] (hints_store_ptr cptr) {
-                    return cptr->shutdown().finally([cptr] {
-                        return cptr->release();
-                    }).finally([cptr] {});
-                }).then([this] {
-                    // Un-hold the commitlog object. Since we are under the exclusive _file_update_mutex lock there are no
-                    // other hints_store_ptr copies and this would destroy the commitlog shared value.
-                    _hints_store_anchor = nullptr;
-
-                    // Re-create the commitlog instance - this will re-populate the _segments_to_replay if needed.
-                    return get_or_load().discard_result();
-                });
-            });
-        });
-    }
-
-    return make_ready_future<>();
 }
 
 bool manager::too_many_in_flight_hints_for(endpoint_id ep) const noexcept {
--- db/hints/manager.hh
+++ db/hints/manager.hh
@@ -30,6 +30,7 @@
 #include "locator/abstract_replication_strategy.hh"
 #include "db/hints/internal/common.hh"
 #include "db/hints/internal/hint_sender.hh"
+#include "db/hints/internal/host_manager.hh"
 
 class fragmented_temporary_buffer;
 
@@ -46,6 +47,7 @@ namespace hints {
 
 namespace internal {
 class hint_sender;
+class host_manager;
 } // namespace internal
 
 using node_to_hint_store_factory_type = utils::loading_shared_values<internal::endpoint_id, db::commitlog>;
@@ -84,182 +86,12 @@ private:
     using drain = internal::drain;
 
     friend class internal::hint_sender;
+    friend class internal::host_manager;
     friend class space_watchdog;
 
-public:
-    class end_point_hints_manager {
-    private:
-        friend class internal::hint_sender;
-        using hint_sender = internal::hint_sender;
-    private:
-        endpoint_id _key;
-        manager& _shard_manager;
-        hints_store_ptr _hints_store_anchor;
-        seastar::gate _store_gate;
-        lw_shared_ptr<seastar::shared_mutex> _file_update_mutex_ptr;
-        seastar::shared_mutex& _file_update_mutex;
+    using host_manager = internal::host_manager;
 
-        enum class state {
-            can_hint,               // hinting is currently allowed (used by the space_watchdog)
-            stopping,               // stopping is in progress (stop() method has been called)
-            stopped                 // stop() has completed
-        };
-
-        using state_set = enum_set<super_enum<state,
-            state::can_hint,
-            state::stopping,
-            state::stopped>>;
-
-        state_set _state;
-        const fs::path _hints_dir;
-        uint64_t _hints_in_progress = 0;
-        db::replay_position _last_written_rp;
-        hint_sender _sender;
-
-    public:
-        end_point_hints_manager(const endpoint_id& key, manager& shard_manager);
-        end_point_hints_manager(end_point_hints_manager&&);
-        ~end_point_hints_manager();
-
-        const endpoint_id& end_point_key() const noexcept {
-            return _key;
-        }
-
-        /// \brief Get the corresponding hints_store object. Create it if needed.
-        /// \note Must be called under the \ref _file_update_mutex.
-        /// \return The corresponding hints_store object.
-        future<hints_store_ptr> get_or_load();
-
-        /// \brief Store a single mutation hint.
-        /// \param s column family descriptor
-        /// \param fm frozen mutation object
-        /// \param tr_state trace_state handle
-        /// \return FALSE if hint is definitely not going to be stored
-        bool store_hint(schema_ptr s, lw_shared_ptr<const frozen_mutation> fm, tracing::trace_state_ptr tr_state) noexcept;
-
-        /// \brief Populates the _segments_to_replay list.
-        ///  Populates the _segments_to_replay list with the names of the files in the <manager hints files directory> directory
-        ///  in the order they should be sent out.
-        ///
-        /// \return Ready future when end point hints manager is initialized.
-        future<> populate_segments_to_replay();
-
-        /// \brief Waits till all writers complete and shuts down the hints store. Drains hints if needed.
-        ///
-        /// If "draining" is requested - sends all pending hints out.
-        ///
-        /// When hints are being drained we will not stop sending after a single hint sending has failed and will continue sending hints
-        /// till the end of the current segment. After that we will remove the current segment and move to the next one till
-        /// there isn't any segment left.
-        ///
-        /// \param should_drain is drain::yes - drain all pending hints
-        /// \return Ready future when all operations are complete
-        future<> stop(drain should_drain = drain::no) noexcept;
-
-        /// \brief Start the timer.
-        void start();
-
-        /// \return Number of in-flight (towards the file) hints.
-        uint64_t hints_in_progress() const noexcept {
-            return _hints_in_progress;
-        }
-
-        bool replay_allowed() const noexcept {
-            return _shard_manager.replay_allowed();
-        }
-
-        bool can_hint() const noexcept {
-            return _state.contains(state::can_hint);
-        }
-
-        void allow_hints() noexcept {
-            _state.set(state::can_hint);
-        }
-
-        void forbid_hints() noexcept {
-            _state.remove(state::can_hint);
-        }
-
-        void set_stopping() noexcept {
-            _state.set(state::stopping);
-        }
-
-        bool stopping() const noexcept {
-            return _state.contains(state::stopping);
-        }
-
-        void set_stopped() noexcept {
-            _state.set(state::stopped);
-        }
-
-        void clear_stopped() noexcept {
-            _state.remove(state::stopped);
-        }
-
-        bool stopped() const noexcept {
-            return _state.contains(state::stopped);
-        }
-
-        /// \brief Returns replay position of the most recently written hint.
-        ///
-        /// If there weren't any hints written during this endpoint manager's lifetime, a zero replay_position is returned.
-        db::replay_position last_written_replay_position() const {
-            return _last_written_rp;
-        }
-
-        /// \brief Waits until hints are replayed up to a given replay position, or given abort source is triggered.
-        future<> wait_until_hints_are_replayed_up_to(abort_source& as, db::replay_position up_to_rp) {
-            return _sender.wait_until_hints_are_replayed_up_to(as, up_to_rp);
-        }
-
-        /// \brief Safely runs a given functor under the file_update_mutex of \ref ep_man
-        ///
-        /// Runs a given functor under the file_update_mutex of the given end_point_hints_manager instance.
-        /// This function is safe even if \ref ep_man gets destroyed before the future this function returns resolves
-        /// (as long as the \ref func call itself is safe).
-        ///
-        /// \tparam Func Functor type.
-        /// \param ep_man end_point_hints_manager instance which file_update_mutex we want to lock.
-        /// \param func Functor to run under the lock.
-        /// \return Whatever \ref func returns.
-        template <typename Func>
-        friend inline auto with_file_update_mutex(end_point_hints_manager& ep_man, Func&& func) {
-            return with_lock(*ep_man._file_update_mutex_ptr, std::forward<Func>(func)).finally([lock_ptr = ep_man._file_update_mutex_ptr] {});
-        }
-
-        const fs::path& hints_dir() const noexcept {
-            return _hints_dir;
-        }
-
-    private:
-        seastar::shared_mutex& file_update_mutex() noexcept {
-            return _file_update_mutex;
-        }
-
-        /// \brief Creates a new hints store object.
-        ///
-        /// - Creates a hints store directory if doesn't exist: <shard_hints_dir>/<ep_key>
-        /// - Creates a store object.
-        /// - Populate _segments_to_replay if it's empty.
-        ///
-        /// \return A new hints store object.
-        future<commitlog> add_store() noexcept;
-
-        /// \brief Flushes all hints written so far to the disk.
-        ///  - Repopulates the _segments_to_replay list if needed.
-        ///
-        /// \return Ready future when the procedure above completes.
-        future<> flush_current_hints() noexcept;
-
-        hint_stats& shard_stats() {
-            return _shard_manager._stats;
-        }
-
-        resource_manager& shard_resource_manager() {
-            return _shard_manager._resource_manager;
-        }
-    };
-
+private:
     enum class state {
         started,                // hinting is currently allowed (start() call is complete)
         replay_allowed,         // replaying (hints sending) is allowed
@@ -274,7 +106,7 @@ public:
         state::stopping>>;
 
 private:
-    using ep_managers_map_type = std::unordered_map<endpoint_id, end_point_hints_manager>;
+    using ep_managers_map_type = std::unordered_map<endpoint_id, host_manager>;
 
 public:
     static const std::string FILENAME_PREFIX;
@@ -513,7 +345,7 @@ private:
         return _local_db;
     }
 
-    end_point_hints_manager& get_ep_manager(endpoint_id ep);
+    host_manager& get_ep_manager(endpoint_id ep);
     bool have_ep_manager(endpoint_id ep) const noexcept;
 
 public:
@@ -523,7 +355,7 @@ public:
     /// Otherwise drains hints to the node that has left.
     ///
     /// In both cases - removes the corresponding hints' directories after all hints have been drained and erases the
-    /// corresponding end_point_hints_manager objects.
+    /// corresponding host_manager objects.
     ///
     /// \param endpoint node that left the cluster
     void drain_for(endpoint_id endpoint);
