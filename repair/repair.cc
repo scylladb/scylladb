620273899b4228ec516ba4e8ab95ab7f8e9f3711
Pavel Emelyanov <xemul@scylladb.com>
2023-09-05 11:13:21 +0300

    repair: Use full endpoint location instead of datacenter part
    There are several places in repair code that get datacenter from the
    topology. Nearby there are calls to update_topology() which, in turn,
    needs full location ({dc, rack} pair). This patch makes the former
    places obtain full location from topology and get the dc part from it.
    This is needed as a preparation to let latter places use that location.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- repair/repair.cc
+++ repair/repair.cc
@@ -1401,7 +1401,7 @@ future<> repair_service::bootstrap_with_repair(locator::token_metadata_ptr tmptr
         auto& db = get_db().local();
         auto ks_erms = db.get_non_local_strategy_keyspaces_erms();
         auto& topology = tmptr->get_topology();
-        auto local_dc = topology.get_datacenter();
+        auto myloc = topology.get_location();
         auto myip = utils::fb_utilities::get_broadcast_address();
         auto reason = streaming::stream_reason::bootstrap;
         // Calculate number of ranges to sync data
@@ -1495,14 +1495,14 @@ future<> repair_service::bootstrap_with_repair(locator::token_metadata_ptr tmptr
                                     throw std::runtime_error(format("bootstrap_with_repair: keyspace={}, range={}, failed to cast to network_topology_strategy",
                                             keyspace_name, desired_range));
                                 }
-                                rf_in_local_dc = nts->get_replication_factor(local_dc);
+                                rf_in_local_dc = nts->get_replication_factor(myloc.dc);
                             }
                             return rf_in_local_dc;
                         };
                         auto get_old_endpoints_in_local_dc = [&] () {
                             return boost::copy_range<std::vector<gms::inet_address>>(old_endpoints |
                                 boost::adaptors::filtered([&] (const gms::inet_address& node) {
-                                    return topology.get_datacenter(node) == local_dc;
+                                    return topology.get_datacenter(node) == myloc.dc;
                                 })
                             );
                         };
@@ -1889,14 +1889,14 @@ future<> repair_service::replace_with_repair(locator::token_metadata_ptr tmptr, 
     auto cloned_tm = co_await tmptr->clone_async();
     auto op = sstring("replace_with_repair");
     auto& topology = tmptr->get_topology();
-    auto source_dc = topology.get_datacenter();
+    auto myloc = topology.get_location();
     auto reason = streaming::stream_reason::replace;
     // update a cloned version of tmptr
     // no need to set the original version
     auto cloned_tmptr = make_token_metadata_ptr(std::move(cloned_tm));
     cloned_tmptr->update_topology(utils::fb_utilities::get_broadcast_address(), _sys_ks.local().local_dc_rack(), locator::node::state::replacing);
     co_await cloned_tmptr->update_normal_tokens(replacing_tokens, utils::fb_utilities::get_broadcast_address());
-    co_return co_await do_rebuild_replace_with_repair(std::move(cloned_tmptr), std::move(op), std::move(source_dc), reason, std::move(ignore_nodes));
+    co_return co_await do_rebuild_replace_with_repair(std::move(cloned_tmptr), std::move(op), myloc.dc, reason, std::move(ignore_nodes));
 }
 
 node_ops_cmd_category categorize_node_ops_cmd(node_ops_cmd cmd) noexcept {
