111d20958e874d5abd4d40756e32d525cf249833
Kefu Chai <kefu.chai@scylladb.com>
2023-09-12 15:45:35 +0800

    build: cmake: build python3 dist tarball with arch postfix
    now that `configure.py` always generate python3 dist tarball with
    ${arch} postfix, let's mirror this behavior. as `build_unified.sh`
    uses this naming convention.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- cmake/build_submodule.cmake
+++ cmake/build_submodule.cmake
@@ -1,13 +1,20 @@
 function(build_submodule name dir)
+  cmake_parse_arguments(parsed_args "NOARCH" "" "" ${ARGN})
   string(REPLACE "-" "~" scylla_version_tilde ${Scylla_VERSION})
   set(version "${scylla_version_tilde}-${Scylla_RELEASE}")
   set(scylla_version
     "${Scylla_PRODUCT}-${scylla_version_tilde}-${Scylla_RELEASE}")
   set(working_dir ${CMAKE_CURRENT_SOURCE_DIR}/${dir})
-  set(reloc_pkg "${working_dir}/build/${Scylla_PRODUCT}-${name}-${version}.noarch.tar.gz")
+  if(parsed_args_NOARCH)
+    set(arch "noarch")
+  else()
+    set(arch "${CMAKE_SYSTEM_PROCESSOR}")
+  endif()
+  set(reloc_args ${parsed_args_UNPARSED_ARGUMENTS})
+  set(reloc_pkg "${working_dir}/build/${Scylla_PRODUCT}-${name}-${version}.${arch}.tar.gz")
   add_custom_command(
     OUTPUT ${reloc_pkg}
-    COMMAND reloc/build_reloc.sh --version ${scylla_version} --nodeps ${ARGN}
+    COMMAND reloc/build_reloc.sh --version ${scylla_version} --nodeps ${reloc_args}
     WORKING_DIRECTORY "${working_dir}"
     JOB_POOL submodule_pool)
   add_custom_target(dist-${name}-tar
