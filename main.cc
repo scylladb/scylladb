0ae0f75a0457898aa0606638824965db845fca9d
Pavel Emelyanov <xemul@scylladb.com>
2023-09-18 20:47:36 +0300

    view_update_generator: Stop for real later
    Now the v.u.g.::stop() code waits for the generator bacground fiber to
    stop for real. This can happen much later, all the necessary precautions
    not to produce more work for the generator had been taken in do_abort().
    
    This keeps the v.u.g. start-stop in one place, except for the call to
    invoke_on_all(v.u.g.::start()) which will be handler separately.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- main.cc
+++ main.cc
@@ -1434,6 +1434,9 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
 
             supervisor::notify("starting view update generator");
             view_update_generator.start(std::ref(db), std::ref(proxy), std::ref(stop_signal.as_sharded_abort_source())).get();
+            auto stop_view_update_generator = defer_verbose_shutdown("view update generator", [] {
+                view_update_generator.stop().get();
+            });
 
             supervisor::notify("starting commit log");
             auto cl = db.local().commitlog();
@@ -1887,10 +1890,6 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
                     return lifecycle_notifier.local().unregister_subscriber(&controller);
                 }).get();
                 sl_controller.invoke_on_all(&qos::service_level_controller::drain).get();
-            });
-
-            auto stop_view_update_generator = defer_verbose_shutdown("view update generator", [] {
-                view_update_generator.stop().get();
             });
 
             auto do_drain = defer_verbose_shutdown("local storage", [&ss] {
