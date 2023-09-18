da7de887d6b537d96b658aac6e15cb11db6b0275
Kefu Chai <kefu.chai@scylladb.com>
2023-09-18 09:52:34 +0800

    build: cmake: bump the minimum required CMake version
    because we should have a frozeon toolchain built with fedora38, and f38
    provides cmake v3.27.4, we can assume the availability of cmake v3.27.4
    when building scylla with the toolchain.
    
    in this change, the minimum required CMake version is changed to
    3.27.
    
    this also allows us to simplify the implementation of
    `add_whole_archive()`, and remove the buggy branch for supporting
    CMake < 3.24, as we should have used `${name}` in place of `auth` there.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    
    Closes scylladb/scylladb#15446

--- cmake/add_whole_archive.cmake
+++ cmake/add_whole_archive.cmake
@@ -5,15 +5,6 @@
 # actually compiling a sample program.
 function(add_whole_archive name library)
   add_library(${name} INTERFACE)
-  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
-    target_link_libraries(${name} INTERFACE
-      "$<LINK_LIBRARY:WHOLE_ARCHIVE,${library}>")
-  else()
-    add_dependencies(${name} ${library})
-    target_include_directories(${name} INTERFACE
-      ${CMAKE_SOURCE_DIR})
-    target_link_options(auth INTERFACE
-      "$<$<CXX_COMPILER_ID:Clang>:SHELL:LINKER:-force_load $<TARGET_LINKER_FILE:${library}>>"
-      "$<$<CXX_COMPILER_ID:GNU>:SHELL:LINKER:--whole-archive $<TARGET_LINKER_FILE:${library}> LINKER:--no-whole-archive>")
-  endif()
+  target_link_libraries(${name} INTERFACE
+    "$<LINK_LIBRARY:WHOLE_ARCHIVE,${library}>")
 endfunction()
