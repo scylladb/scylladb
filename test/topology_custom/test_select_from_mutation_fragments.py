f2df8cf484ae04f737d70ca7201dc6120d136ad4
Botond Dénes <bdenes@scylladb.com>
2023-09-20 10:33:28 -0400

    test/topology_custom: add copyright/license blurb to tests
    Most tests were missing this, fix it.

--- test/topology_custom/test_select_from_mutation_fragments.py
+++ test/topology_custom/test_select_from_mutation_fragments.py
@@ -1,3 +1,10 @@
+#
+# Copyright (C) 2023-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This file configures pytest for all tests in this directory, and also
+
 import pytest
 
 from cassandra.protocol import InvalidRequest  # type: ignore
