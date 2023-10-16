059d7c795ea23be1117288d15f2b5b6e53fc6340
Pavel Emelyanov <xemul@scylladb.com>
2023-10-16 16:17:33 +0300

    db,sstables: Move storage init for system keyspace to table creation
    User and system keyspaces are created and populated slightly
    differently.
    
    System keyspace is created via system_keyspace::make() which eventually
    calls calls add_column_family(). Then it's populated via
    init_system_keyspace() which calls sstable_directory::prepare() which,
    in turn, optionally creates directories in datadir/ or checks the
    directory permissions if it exists
    
    User keyspaces are created with the help of
    add_column_family_and_make_directory() call which calls the
    add_column_family() mentioned above _and_ calls table::init_storage() to
    create directories. When it's populated with init_non_system_keyspaces()
    it also calls sstable_directory::prepare() which notices that the
    directory exists and then checks the permissions.
    
    As a result, sstable_directory::prepare() initializes storage for system
    keyspace only and there's a BUG (#15708) that the upload/ subdir is not
    created.
    
    This patch makes the directories creation for _all_ keyspaces with the
    table::init_storage(). The change only touches system keyspace by moving
    the creation of directories from sstable_directory::prepare() into
    system_keyspace::make().
    
    Indentation is deliberately left broken.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- sstables/sstable_directory.cc
+++ sstables/sstable_directory.cc
@@ -208,12 +208,12 @@ future<> sstable_directory::prepare(process_flags flags) {
 }
 
 future<> sstable_directory::filesystem_components_lister::prepare(sstable_directory& dir, process_flags flags, storage& st) {
-    if (!co_await file_exists(_directory.native())) {
-        if (dir._state == sstable_state::quarantine) {
+    if (dir._state == sstable_state::quarantine) {
+        if (!co_await file_exists(_directory.native())) {
             co_return;
         }
-        co_await io_check([this] { return recursive_touch_directory(_directory.native()); });
-    } else {
+    }
+
         // verify owner and mode on the sstables directory
         // and all its subdirectories, except for "snapshots"
         // as there could be a race with scylla-manager that might
@@ -224,7 +224,6 @@ future<> sstable_directory::filesystem_components_lister::prepare(sstable_direct
                 co_await utils::directories::verify_owner_and_mode(dir / de.name, utils::directories::recursive::yes);
             }
         });
-    }
 
     if (flags.garbage_collect) {
         co_await garbage_collect(st);
