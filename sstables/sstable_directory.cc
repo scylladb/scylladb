c3b3e5b107009b7509c34fe01d41c16a3e0bd513
Pavel Emelyanov <xemul@scylladb.com>
2023-10-16 16:18:02 +0300

    sstable_directory: Indentation fix after previous patch
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- sstables/sstable_directory.cc
+++ sstables/sstable_directory.cc
@@ -214,16 +214,16 @@ future<> sstable_directory::filesystem_components_lister::prepare(sstable_direct
         }
     }
 
-        // verify owner and mode on the sstables directory
-        // and all its subdirectories, except for "snapshots"
-        // as there could be a race with scylla-manager that might
-        // delete snapshots concurrently
-        co_await utils::directories::verify_owner_and_mode(_directory, utils::directories::recursive::no);
-        co_await lister::scan_dir(_directory, lister::dir_entry_types::of<directory_entry_type::directory>(), [] (fs::path dir, directory_entry de) -> future<> {
-            if (de.name != sstables::snapshots_dir) {
-                co_await utils::directories::verify_owner_and_mode(dir / de.name, utils::directories::recursive::yes);
-            }
-        });
+    // verify owner and mode on the sstables directory
+    // and all its subdirectories, except for "snapshots"
+    // as there could be a race with scylla-manager that might
+    // delete snapshots concurrently
+    co_await utils::directories::verify_owner_and_mode(_directory, utils::directories::recursive::no);
+    co_await lister::scan_dir(_directory, lister::dir_entry_types::of<directory_entry_type::directory>(), [] (fs::path dir, directory_entry de) -> future<> {
+        if (de.name != sstables::snapshots_dir) {
+            co_await utils::directories::verify_owner_and_mode(dir / de.name, utils::directories::recursive::yes);
+        }
+    });
 
     if (flags.garbage_collect) {
         co_await garbage_collect(st);
