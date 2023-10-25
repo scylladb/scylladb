7e3a78d73d01881872d02526ecf41e9534f19ad2
Botond Dénes <bdenes@scylladb.com>
2023-10-25 07:37:20 -0400

    test/nodetool: rest_api_mock: add more options for multiple requests
    Change the current bool multiple param to a weak enum, allowing for a
    third value: ANY, which allows for 0 matches too.

--- test/nodetool/test_compact.py
+++ test/nodetool/test_compact.py
@@ -10,14 +10,16 @@ import utils
 
 def test_all_keyspaces(nodetool):
     nodetool("compact", expected_requests=[
-        expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system", "system_schema"]),
+        expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                         response=["system", "system_schema"]),
         expected_request("POST", "/storage_service/keyspace_compaction/system"),
         expected_request("POST", "/storage_service/keyspace_compaction/system_schema")])
 
 
 def test_keyspace(nodetool):
     nodetool("compact", "system_schema", expected_requests=[
-            expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system", "system_schema"]),
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
             expected_request("POST", "/storage_service/keyspace_compaction/system_schema")])
 
 
@@ -26,7 +28,8 @@ def test_nonexistent_keyspace(nodetool):
             nodetool,
             ("compact", "non_existent_ks"),
             {"expected_requests": [
-                expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system"]),
+                expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                                 response=["system"]),
                 expected_request("POST", "/storage_service/keyspace_compaction/non_existent_ks")]},
             ["nodetool: Keyspace [non_existent_ks] does not exist.",
              "error processing arguments: keyspace non_existent_ks does not exist"])
@@ -34,11 +37,13 @@ def test_nonexistent_keyspace(nodetool):
 
 def test_table(nodetool):
     nodetool("compact", "system_schema", "columns", expected_requests=[
-            expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system", "system_schema"]),
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
             expected_request("POST", "/storage_service/keyspace_compaction/system_schema", params={"cf": "columns"})])
 
     nodetool("compact", "system_schema", "columns", "computed_columns", expected_requests=[
-            expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system", "system_schema"]),
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
             expected_request("POST",
                              "/storage_service/keyspace_compaction/system_schema",
                              params={"cf": "columns,computed_columns"})])
@@ -46,7 +51,8 @@ def test_table(nodetool):
 
 def test_split_output_compatibility_argument(nodetool):
     dummy_request = [
-            expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system", "system_schema"]),
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
             expected_request("POST", "/storage_service/keyspace_compaction/system_schema")]
 
     nodetool("compact", "system_schema", "-s", expected_requests=dummy_request)
@@ -55,7 +61,8 @@ def test_split_output_compatibility_argument(nodetool):
 
 def test_token_range_compatibility_argument(nodetool):
     dummy_request = [
-            expected_request("GET", "/storage_service/keyspaces", multiple=True, response=["system", "system_schema"]),
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
             expected_request("POST", "/storage_service/keyspace_compaction/system_schema")]
 
     nodetool("compact", "system_schema", "-st", "0", "-et", "1000", expected_requests=dummy_request)
