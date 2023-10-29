bhalevy/scylla

migration_manager: propagate listener notification exceptions
1e29b07e4065f4a8683f2970b9ee6e86b737a94c claimed
to make event notification exception safe,
but swallawing the exceptions isn't safe at all,
as this might leave the node in an inconsistent state
if e.g. storage_service::keyspace_changed fails on any of the
shards.  Propagating the exception here will cause abort,
but it is better than leaving the node up, but in an
inconsistent state.

We keep notifying other listeners even if any of them failed
Based on 1e29b07e4065f4a8683f2970b9ee6e86b737a94c:
```
If one of the listeners throws an exception, we must ensure that other
listeners are still notified.
```

The decision about swallowing exceptions can't be
made in such a generic layer.
Specific notification listeners that may ignore exceptions,
like in transport/evenet_notifier, may decide to swallow their
local exceptions on their own (as done in this patch).

Refs #3389

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

service/storage_service.cc
@@ -4824,10 +4824,7 @@ future<> storage_service::keyspace_changed(const sstring& ks_name) {
     }
     // Update pending ranges since keyspace can be changed after we calculate pending ranges.
     sstring reason = ::format("keyspace {}", ks_name);
-    // FIXME: indentation
-        return update_topology_change_info(reason, acquire_merge_lock::no).handle_exception([reason = std::move(reason)] (auto ep) {
-            slogger.warn("Failure to update pending ranges for {} ignored", reason);
-        });
+    return update_topology_change_info(reason, acquire_merge_lock::no);
 }
 
 void storage_service::on_update_tablet_metadata() {

scylladb/scylladb

(cherry picked from commit 825d617a5312236520f271159f08b3249925d51b)

service/storage_service.cc
@@ -3335,10 +3335,7 @@ future<> storage_service::keyspace_changed(const sstring& ks_name) {
     }
     // Update pending ranges since keyspace can be changed after we calculate pending ranges.
     sstring reason = format("keyspace {}", ks_name);
-    // FIXME: indentation
-        return update_pending_ranges(reason, acquire_merge_lock::no).handle_exception([reason = std::move(reason)] (auto ep) {
-            slogger.warn("Failure to update pending ranges for {} ignored", reason);
-        });
+    return update_pending_ranges(reason, acquire_merge_lock::no);
 }
 
 future<> storage_service::snitch_reconfigured() {