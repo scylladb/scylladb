bhalevy/scylla

database: create_keyspace_on_all_shards
Part of moving the responsibility for applying
and notifying keyspace schema changes from
schema_tables to the database so that the
database can control the order of applying the changes
across shards and when to notify its listeners.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

replica/database.hh
@@ -1447,6 +1447,7 @@ private:
     void remove(table&) noexcept;
     void drop_keyspace(const sstring& name);
     future<> update_keyspace(sharded<service::storage_proxy>& proxy, const sstring& name);
+    future<> create_keyspace(sharded<service::storage_proxy>& proxy, const sstring& name);
 public:
     static table_schema_version empty_version;
 
@@ -1528,7 +1529,7 @@ public:
      *
      * @return ready future when the operation is complete
      */
-    future<> create_keyspace(const lw_shared_ptr<keyspace_metadata>&, locator::effective_replication_map_factory& erm_factory);
+    static future<> create_keyspace_on_all_shards(sharded<database>& sharded_db, sharded<service::storage_proxy>& proxy, const sstring& name);
     /* below, find_keyspace throws no_such_<type> on fail */
     keyspace& find_keyspace(std::string_view name);
     const keyspace& find_keyspace(std::string_view name) const;

scylladb/scylladb

(cherry picked from commit 3520c786bd8eaf15c556370bce8e37a306738017)

replica/database.hh
@@ -1499,7 +1499,7 @@ public:
      *
      * @return ready future when the operation is complete
      */
-    future<> create_keyspace(const lw_shared_ptr<keyspace_metadata>&, locator::effective_replication_map_factory& erm_factory);
+    static future<> create_keyspace_on_all_shards(sharded<database>& sharded_db, sharded<service::storage_proxy>& proxy, const keyspace_metadata& ksm);
     /* below, find_keyspace throws no_such_<type> on fail */
     keyspace& find_keyspace(std::string_view name);
     const keyspace& find_keyspace(std::string_view name) const;