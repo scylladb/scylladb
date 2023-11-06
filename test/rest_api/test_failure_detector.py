315c69cec2cacd55b0ca584a6c28dade2e9d31ca
Kamil Braun <kbraun@scylladb.com>
2023-11-06 17:24:53 +0100

    test: rest_api: test that generation is nonzero in `failure_detector/endpoints`

--- test/rest_api/test_failure_detector.py
+++ test/rest_api/test_failure_detector.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import re
+import time
 
 def test_failure_detector_endpoints(rest_api):
     resp = rest_api.send('GET', "failure_detector/endpoints")
@@ -16,3 +17,13 @@ def test_failure_detector_endpoint_phi_values(rest_api):
     # so just check it doesn't return an error or crash
     resp = rest_api.send('GET', "failure_detector/endpoint_phi_values")
     resp.raise_for_status()
+
+def test_nonzero_generation(rest_api):
+    # In older versions of Scylla, shards other than 0 would return generation=0.
+    # Call the endpoint multiple times to increase the chance of hitting nonzero shard.
+    for _ in range(100):
+        resp = rest_api.send('GET', "failure_detector/endpoints")
+        resp.raise_for_status()
+        assert len(resp.json()) == 1
+        gen = int(resp.json()[0]['generation'])
+        assert gen > 0
