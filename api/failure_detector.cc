a89c69007ed543dbb37f8d3246748679111556f7
Kamil Braun <kbraun@scylladb.com>
2023-11-06 17:03:36 +0100

    api: failure_detector: invoke on shard 0
    These APIs may return stale or simply incorrect data on shards
    other than 0. Newer versions of Scylla are better at maintaining
    cross-shard consistency, but we need a simple fix that can be easily and
    without risk be backported to older versions; this is the fix.
    
    Fixes: scylladb/scylladb#15816

--- api/failure_detector.cc
+++ api/failure_detector.cc
@@ -18,6 +18,7 @@ namespace fd = httpd::failure_detector_json;
 
 void set_failure_detector(http_context& ctx, routes& r, gms::gossiper& g) {
     fd::get_all_endpoint_states.set(r, [&g](std::unique_ptr<request> req) {
+        return g.container().invoke_on(0, [] (gms::gossiper& g) {
         std::vector<fd::endpoint_state> res;
         res.reserve(g.num_endpoints());
         g.for_each_endpoint_state([&] (const gms::inet_address& addr, const gms::endpoint_state& eps) {
@@ -39,16 +40,21 @@ void set_failure_detector(http_context& ctx, routes& r, gms::gossiper& g) {
             res.emplace_back(std::move(val));
         });
         return make_ready_future<json::json_return_type>(res);
+        });
     });
 
     fd::get_up_endpoint_count.set(r, [&g](std::unique_ptr<request> req) {
+        return g.container().invoke_on(0, [] (gms::gossiper& g) {
         int res = g.get_up_endpoint_count();
         return make_ready_future<json::json_return_type>(res);
+        });
     });
 
     fd::get_down_endpoint_count.set(r, [&g](std::unique_ptr<request> req) {
+        return g.container().invoke_on(0, [] (gms::gossiper& g) {
         int res = g.get_down_endpoint_count();
         return make_ready_future<json::json_return_type>(res);
+        });
     });
 
     fd::get_phi_convict_threshold.set(r, [] (std::unique_ptr<request> req) {
@@ -56,11 +62,13 @@ void set_failure_detector(http_context& ctx, routes& r, gms::gossiper& g) {
     });
 
     fd::get_simple_states.set(r, [&g] (std::unique_ptr<request> req) {
+        return g.container().invoke_on(0, [] (gms::gossiper& g) {
         std::map<sstring, sstring> nodes_status;
         g.for_each_endpoint_state([&] (const gms::inet_address& node, const gms::endpoint_state&) {
             nodes_status.emplace(node.to_sstring(), g.is_alive(node) ? "UP" : "DOWN");
         });
         return make_ready_future<json::json_return_type>(map_to_key_value<fd::mapper>(nodes_status));
+        });
     });
 
     fd::set_phi_convict_threshold.set(r, [](std::unique_ptr<request> req) {
@@ -71,6 +79,7 @@ void set_failure_detector(http_context& ctx, routes& r, gms::gossiper& g) {
     });
 
     fd::get_endpoint_state.set(r, [&g] (std::unique_ptr<request> req) {
+        return g.container().invoke_on(0, [req = std::move(req)] (gms::gossiper& g) {
         auto state = g.get_endpoint_state_ptr(gms::inet_address(req->param["addr"]));
         if (!state) {
             return make_ready_future<json::json_return_type>(format("unknown endpoint {}", req->param["addr"]));
@@ -78,6 +87,7 @@ void set_failure_detector(http_context& ctx, routes& r, gms::gossiper& g) {
         std::stringstream ss;
         g.append_endpoint_state(ss, *state);
         return make_ready_future<json::json_return_type>(sstring(ss.str()));
+        });
     });
 
     fd::get_endpoint_phi_values.set(r, [](std::unique_ptr<request> req) {
