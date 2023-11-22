scylladb/scylladb

commit 781b7de502c01560fa580d9d226004820d5ab36a

build: enable -Wignore-qualifiers
`-Wignore-qualifiers` is included by -Wextra. but we are not there yet,
with this change, we can keep the changes introducing -Wignore-qualifiers
warnings out of the repo, before applying `-Wextra`.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

cmake/mode.common.cmake
@@ -16,6 +16,7 @@ add_compile_options(
   "-Werror"
   "-Wno-error=deprecated-declarations"
   "-Wimplicit-fallthrough"
+  "-Wignore-qualifiers"
   ${_supported_warnings})
 
 function(default_target_arch arch)

configure.py
@@ -1506,6 +1506,7 @@ def get_warning_options(cxx):
         '-Wall',
         '-Werror',
         '-Wimplicit-fallthrough',
+        '-Wignore-qualifiers',
         '-Wno-mismatched-tags',  # clang-only
         '-Wno-c++11-narrowing',
         '-Wno-overloaded-virtual',

commit 2e00cf5a746bf66772e64018c1c17a757c62e1db

build: s/-Wignore-qualifiers/-Wignored-qualifiers/
this was a typo introduced by 781b7de5. which intended to add
-Wignored-qualifiers to the compiling options, but it ended up
adding -Wignore-qualifiers.

in this change, the typo is corrected.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

cmake/mode.common.cmake
@@ -16,7 +16,7 @@ add_compile_options(
   "-Werror"
   "-Wno-error=deprecated-declarations"
   "-Wimplicit-fallthrough"
-  "-Wignore-qualifiers"
+  "-Wignored-qualifiers"
   ${_supported_warnings})
 
 function(default_target_arch arch)

configure.py
@@ -1506,7 +1506,7 @@ def get_warning_options(cxx):
         '-Wall',
         '-Werror',
         '-Wimplicit-fallthrough',
-        '-Wignore-qualifiers',
+        '-Wignored-qualifiers',
         '-Wno-mismatched-tags',  # clang-only
         '-Wno-c++11-narrowing',
         '-Wno-overloaded-virtual',
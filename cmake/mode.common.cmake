2e00cf5a746bf66772e64018c1c17a757c62e1db
Kefu Chai <kefu.chai@scylladb.com>
2023-11-21 16:37:46 +0800

    build: s/-Wignore-qualifiers/-Wignored-qualifiers/
    this was a typo introduced by 781b7de5. which intended to add
    -Wignored-qualifiers to the compiling options, but it ended up
    adding -Wignore-qualifiers.
    
    in this change, the typo is corrected.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- cmake/mode.common.cmake
+++ cmake/mode.common.cmake
@@ -16,7 +16,7 @@ add_compile_options(
   "-Werror"
   "-Wno-error=deprecated-declarations"
   "-Wimplicit-fallthrough"
-  "-Wignore-qualifiers"
+  "-Wignored-qualifiers"
   ${_supported_warnings})
 
 function(default_target_arch arch)
