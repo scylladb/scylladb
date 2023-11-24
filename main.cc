scylladb/scylladb

commit f4626f6b8e54a11d80e6412d8022a0f8aca830c3

storage_service: Drop (un)init_messaging_service_part() pair
It's no longer needed.

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

main.cc
@@ -1591,12 +1591,6 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
                 cdc.stop().get();
             });
 
-            supervisor::notify("starting storage service", true);
-            ss.local().init_messaging_service_part(sys_dist_ks, raft_topology_change_enabled).get();
-            auto stop_ss_msg = defer_verbose_shutdown("storage service messaging", [&ss] {
-                ss.local().uninit_messaging_service_part().get();
-            });
-
             api::set_server_messaging_service(ctx, messaging).get();
             auto stop_messaging_api = defer_verbose_shutdown("messaging service API", [&ctx] {
                 api::unset_server_messaging_service(ctx).get();

service/storage_service.cc
@@ -3983,14 +3983,6 @@ future<> storage_service::drain_on_shutdown() {
         _drain_finished.get_future() : do_drain();
 }
 
-future<> storage_service::init_messaging_service_part(sharded<db::system_distributed_keyspace>& sys_dist_ks, bool raft_topology_change_enabled) {
-    return make_ready_future<>();
-}
-
-future<> storage_service::uninit_messaging_service_part() {
-    return make_ready_future<>();
-}
-
 void storage_service::set_group0(raft_group0& group0, bool raft_topology_change_enabled) {
     _group0 = &group0;
     _raft_topology_change_enabled = raft_topology_change_enabled;

service/storage_service.hh
@@ -323,34 +323,6 @@ public:
     future<> check_for_endpoint_collision(std::unordered_set<gms::inet_address> initial_contact_nodes,
             const std::unordered_map<gms::inet_address, sstring>& loaded_peer_features);
 
-    /*!
-     * \brief Init the messaging service part of the service.
-     *
-     * This is the first part of the initialization, call this method
-     * first.
-     *
-     * After this method is completed, it is ok to start the storage_service
-     * API.
-     * \see init_server_without_the_messaging_service_part
-     */
-    future<> init_messaging_service_part(sharded<db::system_distributed_keyspace>& sys_dist_ks, bool raft_topology_change_enabled);
-    /*!
-     * \brief Uninit the messaging service part of the service.
-     */
-    future<> uninit_messaging_service_part();
-
-    /*!
-     * \brief complete the server initialization
-     *
-     * The storage_service initialization is done in two parts.
-     *
-     * It is safe to start the API after init_messaging_service_part
-     * completed
-     *
-     * Must be called on shard 0.
-     *
-     * \see init_messaging_service_part
-     */
     future<> join_cluster(sharded<db::system_distributed_keyspace>& sys_dist_ks, sharded<service::storage_proxy>& proxy);
 
     void set_group0(service::raft_group0&, bool raft_topology_change_enabled);

commit da09252dc454b71e4a0fa90e97b840aeba9a72a1

main: notify systemd that the service is ready
this change addresses a regression introduced by
f4626f6b8e54a11d80e6412d8022a0f8aca830c3, which stopped notifying
systemd with the status that scylla is READY. without the
notification, systemd would wait in vain for the readiness of
scylla.

Refs f4626f6b8e54a11d80e6412d8022a0f8aca830c3

Fixes #16159
Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

main.cc
@@ -1591,6 +1591,8 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
                 cdc.stop().get();
             });
 
+            supervisor::notify("starting storage service", true);
+
             api::set_server_messaging_service(ctx, messaging).get();
             auto stop_messaging_api = defer_verbose_shutdown("messaging service API", [&ctx] {
                 api::unset_server_messaging_service(ctx).get();