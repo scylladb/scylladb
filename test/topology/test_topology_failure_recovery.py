b1c0b57acf0e72a9bfc71894f19509f303d09146
Gleb Natapov <gleb@scylladb.com>
2023-11-27 14:26:04 +0200

    test: fix test_topology_streaming_failure test to not grep the whole file
    A cluster can be reused between tests, so lets grep only the part of the
    log that is relevant for the test itself.

--- test/topology/test_topology_failure_recovery.py
+++ test/topology/test_topology_failure_recovery.py
@@ -16,24 +16,26 @@ async def test_topology_streaming_failure(request, manager: ManagerClient):
     """Fail streaming while doing a topology operation"""
     # decommission failure
     servers = await manager.running_servers()
+    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
+    marks = [await log.mark() for log in logs]
     await manager.api.enable_injection(servers[2].ip_addr, 'stream_ranges_fail', one_shot=True)
     await manager.decommission_node(servers[2].server_id, expected_error="Decommission failed. See earlier errors")
     servers = await manager.running_servers()
     assert len(servers) == 3
-    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
-    matches = [await log.grep("storage_service - rollback.*after decommissioning failure to state rollback_to_normal") for log in logs]
+    matches = [await log.grep("storage_service - rollback.*after decommissioning failure to state rollback_to_normal", from_mark=mark) for log, mark in zip(logs, marks)]
     assert sum(len(x) for x in matches) == 1
     # remove failure
+    marks = [await log.mark() for log in logs]
     await manager.server_add()
     servers = await manager.running_servers()
     await manager.server_stop_gracefully(servers[3].server_id)
     await manager.api.enable_injection(servers[2].ip_addr, 'stream_ranges_fail', one_shot=True)
     await manager.remove_node(servers[0].server_id, servers[3].server_id, expected_error="Removenode failed. See earlier errors")
-    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
-    matches = [await log.grep("storage_service - rollback.*after removing failure to state rollback_to_normal") for log in logs]
+    matches = [await log.grep("storage_service - rollback.*after removing failure to state rollback_to_normal", from_mark=mark) for log, mark in zip(logs, marks)]
     assert sum(len(x) for x in matches) == 1
     await manager.server_start(servers[3].server_id)
     # bootstrap failure
+    marks = [await log.mark() for log in logs]
     servers = await manager.running_servers()
     s = await manager.server_add(start=False, config={
         'error_injections_at_startup': ['stream_ranges_fail']
@@ -41,10 +43,11 @@ async def test_topology_streaming_failure(request, manager: ManagerClient):
     await manager.server_start(s.server_id, expected_error="Bootstrap failed. See earlier errors")
     servers = await manager.running_servers()
     assert s not in servers
-    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
-    matches = [await log.grep("storage_service - rollback.*after bootstrapping failure to state left_token_ring") for log in logs]
+    matches = [await log.grep("storage_service - rollback.*after bootstrapping failure to state left_token_ring", from_mark=mark) for log, mark in zip(logs, marks)]
     assert sum(len(x) for x in matches) == 1
     # replace failure
+    marks = [await log.mark() for log in logs]
+    servers = await manager.running_servers()
     await manager.server_stop_gracefully(servers[2].server_id)
     replace_cfg = ReplaceConfig(replaced_id = servers[2].server_id, reuse_ip_addr = False, use_host_id = True)
     s = await manager.server_add(start=False, replace_cfg=replace_cfg, config={
@@ -53,6 +56,5 @@ async def test_topology_streaming_failure(request, manager: ManagerClient):
     await manager.server_start(s.server_id, expected_error="Replace failed. See earlier errors")
     servers = await manager.running_servers()
     assert s not in servers
-    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
-    matches = [await log.grep("storage_service - rollback.*after replacing failure to state left_token_ring") for log in logs]
+    matches = [await log.grep("storage_service - rollback.*after replacing failure to state left_token_ring", from_mark=mark) for log, mark in zip(logs, marks)]
     assert sum(len(x) for x in matches) == 1
