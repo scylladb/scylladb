0e4c90dca66f7ab19589eaa49c65db696e8feb48
Petr Gusev <petr.gusev@scylladb.com>
2023-11-27 21:01:14 +0400

    api/token_metadata: switch to new version

--- api/token_metadata.cc
+++ api/token_metadata.cc
@@ -20,25 +20,34 @@ using namespace json;
 
 void set_token_metadata(http_context& ctx, routes& r, sharded<locator::shared_token_metadata>& tm) {
     ss::local_hostid.set(r, [&tm](std::unique_ptr<http::request> req) {
-        auto id = tm.local().get()->get_my_id();
+        auto id = tm.local().get()->get_new()->get_my_id();
         return make_ready_future<json::json_return_type>(id.to_sstring());
     });
 
     ss::get_tokens.set(r, [&tm] (std::unique_ptr<http::request> req) {
-        return make_ready_future<json::json_return_type>(stream_range_as_array(tm.local().get()->sorted_tokens(), [](const dht::token& i) {
+        return make_ready_future<json::json_return_type>(stream_range_as_array(tm.local().get()->get_new()->sorted_tokens(), [](const dht::token& i) {
            return fmt::to_string(i);
         }));
     });
 
     ss::get_node_tokens.set(r, [&tm] (std::unique_ptr<http::request> req) {
         gms::inet_address addr(req->param["endpoint"]);
-        return make_ready_future<json::json_return_type>(stream_range_as_array(tm.local().get()->get_tokens(addr), [](const dht::token& i) {
-           return fmt::to_string(i);
-       }));
+        auto& local_tm = *tm.local().get()->get_new();
+        const auto host_id = local_tm.get_host_id_if_known(addr);
+        return make_ready_future<json::json_return_type>(stream_range_as_array(host_id ? local_tm.get_tokens(*host_id): std::vector<dht::token>{}, [](const dht::token& i) {
+            return fmt::to_string(i);
+        }));
     });
 
     ss::get_leaving_nodes.set(r, [&tm](const_req req) {
-        return container_to_vec(tm.local().get()->get_leaving_endpoints());
+        const auto& local_tm = *tm.local().get()->get_new();
+        const auto& leaving_host_ids = local_tm.get_leaving_endpoints();
+        std::unordered_set<gms::inet_address> eps;
+        eps.reserve(leaving_host_ids.size());
+        for (const auto host_id: leaving_host_ids) {
+            eps.insert(local_tm.get_endpoint_for_host_id(host_id));
+        }
+        return container_to_vec(eps);
     });
 
     ss::get_moving_nodes.set(r, [](const_req req) {
@@ -47,17 +56,19 @@ void set_token_metadata(http_context& ctx, routes& r, sharded<locator::shared_to
     });
 
     ss::get_joining_nodes.set(r, [&tm](const_req req) {
-        auto points = tm.local().get()->get_bootstrap_tokens();
-        std::unordered_set<sstring> addr;
-        for (auto i: points) {
-            addr.insert(fmt::to_string(i.second));
+        const auto& local_tm = *tm.local().get()->get_new();
+        const auto& points = local_tm.get_bootstrap_tokens();
+        std::unordered_set<gms::inet_address> eps;
+        eps.reserve(points.size());
+        for (const auto& [token, host_id]: points) {
+            eps.insert(local_tm.get_endpoint_for_host_id(host_id));
         }
-        return container_to_vec(addr);
+        return container_to_vec(eps);
     });
 
     ss::get_host_id_map.set(r, [&tm](const_req req) {
         std::vector<ss::mapper> res;
-        return map_to_key_value(tm.local().get()->get_endpoint_to_host_id_map_for_reading(), res);
+        return map_to_key_value(tm.local().get()->get_new()->get_endpoint_to_host_id_map_for_reading(), res);
     });
 
     static auto host_or_broadcast = [&tm](const_req req) {
@@ -66,7 +77,7 @@ void set_token_metadata(http_context& ctx, routes& r, sharded<locator::shared_to
     };
 
     httpd::endpoint_snitch_info_json::get_datacenter.set(r, [&tm](const_req req) {
-        auto& topology = tm.local().get()->get_topology();
+        auto& topology = tm.local().get()->get_new()->get_topology();
         auto ep = host_or_broadcast(req);
         if (!topology.has_endpoint(ep)) {
             // Cannot return error here, nodetool status can race, request
@@ -77,7 +88,7 @@ void set_token_metadata(http_context& ctx, routes& r, sharded<locator::shared_to
     });
 
     httpd::endpoint_snitch_info_json::get_rack.set(r, [&tm](const_req req) {
-        auto& topology = tm.local().get()->get_topology();
+        auto& topology = tm.local().get()->get_new()->get_topology();
         auto ep = host_or_broadcast(req);
         if (!topology.has_endpoint(ep)) {
             // Cannot return error here, nodetool status can race, request
