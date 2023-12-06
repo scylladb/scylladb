9b6db6e73b7ede5d3d47d1268b19fe7d5d28cdfa
Asias He <asias@scylladb.com>
2023-12-06 09:08:39 +0800

    api: Reject unsupported http api options for repair
    If an option is not supported, reject the request instead of silently
    ignoring the unsupported options.
    
    It prevents the user thinks the option is supported but it is ignored by
    scylla core.
    
    Fixes #16299

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -323,9 +323,22 @@ void unset_rpc_controller(http_context& ctx, routes& r) {
 
 void set_repair(http_context& ctx, routes& r, sharded<repair_service>& repair) {
     ss::repair_async.set(r, [&ctx, &repair](std::unique_ptr<http::request> req) {
-        static std::vector<sstring> options = {"primaryRange", "parallelism", "incremental",
+        static std::unordered_set<sstring> options = {"primaryRange", "parallelism", "incremental",
                 "jobThreads", "ranges", "columnFamilies", "dataCenters", "hosts", "ignore_nodes", "trace",
                 "startToken", "endToken", "ranges_parallelism", "small_table_optimization"};
+
+        // Nodetool still sends those unsupported options. Ignore them to avoid failing nodetool repair.
+        static std::unordered_set<sstring> legacy_options_to_ignore = {"pullRepair", "ignoreUnreplicatedKeyspaces"};
+
+        for (auto& x : req->query_parameters) {
+            if (legacy_options_to_ignore.contains(x.first)) {
+                continue;
+            }
+            if (!options.contains(x.first)) {
+                return make_exception_future<json::json_return_type>(
+                        httpd::bad_param_exception(format("option {} is not supported", x.first)));
+            }
+        }
         std::unordered_map<sstring, sstring> options_map;
         for (auto o : options) {
             auto s = req->get_query_param(o);
