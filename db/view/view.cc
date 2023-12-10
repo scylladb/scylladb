37b5c038655866639a6e4df7369d381117548aea
Nadav Har'El <nyh@scylladb.com>
2023-12-10 13:55:15 +0200

    mv: coroutinize wait code for remote view updates
    In the previous patch we added a delay injection point (for testing)
    in the view update code. Because the code was using continuation style,
    this resulted in increased indentation and ugly repetition of captures.
    
    So in this patch we coroutinize the code that waits for remote view
    updates, making it simpler, shorter, and less indented.
    
    Note that this function still uses continuations in one place:
    The remote view update is still composed of two steps that need
    to happen one after another, but we don't necessarily need to wait
    for them to happen. This is easiest to do with chaining continuations,
    and then either waiting or not waiting for the resulting future.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- db/view/view.cc
+++ db/view/view.cc
@@ -1737,7 +1737,6 @@ future<> view_update_generator::mutate_MV(
             remote_endpoints.pop_back();
         }
 
-        future<> remote_view_update = make_ready_future<>();
         // If target_endpoint is engaged by this point, then either the update
         // is not local, or the local update was already applied but we still
         // have pending endpoints to send to.
@@ -1749,40 +1748,38 @@ future<> view_update_generator::mutate_MV(
             // The "delay_before_remote_view_update" injection point can be
             // used to add a short delay (currently 0.5 seconds) before a base
             // replica sends its update to the remove view replica.
-            future<> view_update = utils::get_local_injector().inject("delay_before_remote_view_update", 500ms).then(
-                [this, s = std::move(s), &stats, &cf_stats, tr_state, base_token, view_token, target_endpoint,
-                 updates_pushed_remote, units = sem_units.split(sem_units.count()), apply_update_synchronously,
-                 view_ermp = std::move(view_ermp), remote_endpoints = std::move(remote_endpoints), mut = std::move(mut), allow_hints] () mutable {
-                return apply_to_remote_endpoints(_proxy.local(), std::move(view_ermp), *target_endpoint, std::move(remote_endpoints), std::move(mut), base_token, view_token, allow_hints, tr_state).then_wrapped(
-                    [s = std::move(s), &stats, &cf_stats, tr_state, base_token, view_token, target_endpoint, updates_pushed_remote, units = std::move(units), apply_update_synchronously] (future<>&& f) mutable {
-                    if (f.failed()) {
-                        stats.view_updates_failed_remote += updates_pushed_remote;
-                        cf_stats.total_view_updates_failed_remote += updates_pushed_remote;
-                        auto ep = f.get_exception();
-                        tracing::trace(tr_state, "Failed to apply view update for {} and {} remote endpoints",
-                            *target_endpoint, updates_pushed_remote);
-
-                        // Printing an error on every failed view mutation would cause log spam, so a rate limit is needed.
-                        static thread_local logger::rate_limit view_update_error_rate_limit(std::chrono::seconds(4));
-                        vlogger.log(log_level::warn, view_update_error_rate_limit,
-                                "Error applying view update to {} (view: {}.{}, base token: {}, view token: {}): {}",
-                                *target_endpoint, s->ks_name(), s->cf_name(), base_token, view_token, ep);
-                        return apply_update_synchronously ? make_exception_future<>(std::move(ep)) : make_ready_future<>();
-                    }
-                    tracing::trace(tr_state, "Successfully applied view update for {} and {} remote endpoints",
+            co_await utils::get_local_injector().inject("delay_before_remote_view_update", 500ms);
+            future<> remote_view_update = apply_to_remote_endpoints(_proxy.local(), std::move(view_ermp), *target_endpoint, std::move(remote_endpoints), std::move(mut), base_token, view_token, allow_hints, tr_state).then_wrapped(
+                [s = std::move(s), &stats, &cf_stats, tr_state, base_token, view_token, target_endpoint, updates_pushed_remote,
+                 units = sem_units.split(sem_units.count()), apply_update_synchronously] (future<>&& f) mutable {
+                if (f.failed()) {
+                    stats.view_updates_failed_remote += updates_pushed_remote;
+                    cf_stats.total_view_updates_failed_remote += updates_pushed_remote;
+                    auto ep = f.get_exception();
+                    tracing::trace(tr_state, "Failed to apply view update for {} and {} remote endpoints",
                         *target_endpoint, updates_pushed_remote);
-                    return make_ready_future<>();
-                });
+
+                    // Printing an error on every failed view mutation would cause log spam, so a rate limit is needed.
+                    static thread_local logger::rate_limit view_update_error_rate_limit(std::chrono::seconds(4));
+                    vlogger.log(log_level::warn, view_update_error_rate_limit,
+                        "Error applying view update to {} (view: {}.{}, base token: {}, view token: {}): {}",
+                        *target_endpoint, s->ks_name(), s->cf_name(), base_token, view_token, ep);
+                    return apply_update_synchronously ? make_exception_future<>(std::move(ep)) : make_ready_future<>();
+                }
+                tracing::trace(tr_state, "Successfully applied view update for {} and {} remote endpoints",
+                    *target_endpoint, updates_pushed_remote);
+                return make_ready_future<>();
             });
             if (apply_update_synchronously) {
-                remote_view_update = std::move(view_update);
+                co_return co_await when_all_succeed(
+                    std::move(local_view_update), std::move(remote_view_update)).discard_result();
             } else {
                 // The update is sent to background in order to preserve availability,
                 // its parallelism is limited by view_update_concurrency_semaphore
-                (void)view_update;
+                (void)remote_view_update;
             }
         }
-        return when_all_succeed(std::move(local_view_update), std::move(remote_view_update)).discard_result();
+        co_return co_await std::move(local_view_update);
     });
 }
 
