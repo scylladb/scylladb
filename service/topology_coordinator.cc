e8833c6f2a6e6fad2797e2451de46ea1b11857f3
Aleksandr Bykov <alex.bykov@scylladb.com>
2023-12-11 16:42:10 +0700

    test: Kill coordinator during topology operation
    If coordinator node was killed, restarted, become not
    operatable during topology operation, new coordinator should be elected,
    operation should be aborted and cluster should be rolled back
    
    Error injection will be used to kill the coordinator before streaming
    starts
    
    Closes scylladb/scylladb#16197

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -1649,7 +1649,7 @@ class topology_coordinator : public endpoint_lifecycle_subscriber {
                         co_await _group0.make_nonvoter(replaced_node_id, _as);
                     }
                 }
-
+                utils::get_local_injector().inject("crash_coordinator_before_stream", [] { abort(); });
                 raft_topology_cmd cmd{raft_topology_cmd::command::stream_ranges};
                 auto state = node.rs->state;
                 try {
