db160487618b3b0bdba3d1121bfa75436eb82516
Kefu Chai <kefu.chai@scylladb.com>
2023-12-12 20:14:43 +0800

    test/pylib: avoid using asyncio.get_event_loop()
    asyncio.get_event_loop() returns the current event loop. but if there
    is not, the result of `get_event_loop_policy().get_event_loop()` is
    returned. but this behavior is deprecated since Python 3.12, so let's
    use asyncio.run() as recommended by
    https://docs.python.org/3/library/asyncio-eventloop.html.
    asyncio.run() was introduced by Python 3.7, so we should be able to
    use it.
    
    this change should silence the waring when running this script
    as a stand-alone script with Python 3.12.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    
    Closes scylladb/scylladb#16385

--- test/pylib/minio_server.py
+++ test/pylib/minio_server.py
@@ -296,5 +296,4 @@ async def main():
             await server.stop()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
