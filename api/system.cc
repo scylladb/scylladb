4c60804c4c6b6e28cc59e13897b4f80706aabaeb
Eliran Sinvani <eliransin@scylladb.com>
2023-12-25 16:19:34 +0200

    rest api: Add an api for profile dumping
    As part of code coverage support we need to work with dumped profiles
    for ScyllaDB executables.
    Those profiles are created on two occasions:
    1. When an application exits notmaly (which will trigger
       __llvm_dump_profile registered in the exit hooks.
    2. For ScyllaDB commit d7b524cf10 introduced a manual call to
       __llvm_dump_profile upon receiving a SIGTERM signal.
    
    This commit adds a third option, a rest API to dump the profile.
    In addition the target file is logged and the counters are reset, which
    enables incremental dumping of the profile.
    Except for logging, if the executable is not instrumented, this API call
    becomes a no-op so it bears minimal risk in keeping it in our releases.
    Specifically for code coverage, the gain will be that we will not be
    required to change the entire test run to shut down clusters gracefully
    and this will cause minimal effect to the actual test behavior.
    
    The change was tested by manually triggering the API in with and
    without instrumentation as well as re triggering it with write
    permissions for the profile file disabled (to test fault tolerance).
    
    Signed-off-by: Eliran Sinvani <eliransin@scylladb.com>

--- api/system.cc
+++ api/system.cc
@@ -30,6 +30,10 @@ using namespace seastar::httpd;
 namespace hs = httpd::system_json;
 namespace hm = httpd::metrics_json;
 
+extern "C" void __attribute__((weak)) __llvm_profile_dump();
+extern "C" const char * __attribute__((weak)) __llvm_profile_get_filename();
+extern "C" void __attribute__((weak)) __llvm_profile_reset_counters();
+
 void set_system(http_context& ctx, routes& r) {
     hm::get_metrics_config.set(r, [](const_req req) {
         std::vector<hm::metrics_config> res;
@@ -158,6 +162,27 @@ void set_system(http_context& ctx, routes& r) {
             return json::json_return_type(json::json_void());
         });
     });
+
+    hs::dump_profile.set(r, [](std::unique_ptr<request> req) {
+        if (!__llvm_profile_dump) {
+            apilog.info("Profile will not be dumped, executable is not instrumented with profile dumping.");
+            return make_ready_future<json::json_return_type>(json::json_return_type(json::json_void()));
+        }
+        sstring profile_dest(__llvm_profile_get_filename ? __llvm_profile_get_filename() : "disk");
+        apilog.info("Dumping profile to {}", profile_dest);
+        __llvm_profile_dump();
+        if (__llvm_profile_reset_counters) {
+            // If counters are not reset the profile dumping mechanism will issue a warning and exit
+            // next time it is attempted. If the counters are reset, profiles can be accumulated
+            // (if %m is present in LLVM_PROFILE_FILE pattern) so it can be dumped in stages or
+            // multiple times during runtime.
+            __llvm_profile_reset_counters();
+        } else {
+            apilog.warn("Could not reset profile counters, profile dumping will be skipped next time it is attempted");
+        }
+        apilog.info("Profile dumped to {}", profile_dest);
+        return make_ready_future<json::json_return_type>(json::json_return_type(json::json_void()));
+    }) ;
 }
 
 }
