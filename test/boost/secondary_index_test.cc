ac3dd4bf5d275130442d4ef8aa14d3572ae0b308
Pavel Emelyanov <xemul@scylladb.com>
2023-12-25 13:12:35 +0300

    test: Coroutinize some secondary_index_test cases
    Now they are long then-chains that are hard to read
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>
    
    Closes scylladb/scylladb#16547

--- test/boost/secondary_index_test.cc
+++ test/boost/secondary_index_test.cc
@@ -6,6 +6,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+#include <seastar/core/coroutine.hh>
 #include "test/lib/scylla_test_case.hh"
 #include "test/lib/cql_test_env.hh"
 #include "test/lib/cql_assertions.hh"
@@ -22,58 +23,43 @@
 using namespace std::chrono_literals;
 
 SEASTAR_TEST_CASE(test_secondary_index_regular_column_query) {
-    return do_with_cql_env([] (cql_test_env& e) {
-        return e.execute_cql("CREATE TABLE users (userid int, name text, email text, country text, PRIMARY KEY (userid));").discard_result().then([&e] {
-            return e.execute_cql("CREATE INDEX ON users (email);").discard_result();
-        }).then([&e] {
-            return e.execute_cql("CREATE INDEX ON users (country);").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (0, 'Bondie Easseby', 'beassebyv@house.gov', 'France');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (1, 'Demetri Curror', 'dcurrorw@techcrunch.com', 'France');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (2, 'Langston Paulisch', 'lpaulischm@reverbnation.com', 'United States');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (3, 'Channa Devote', 'cdevote14@marriott.com', 'Denmark');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("SELECT email FROM users WHERE country = 'France';");
-        }).then([] (shared_ptr<cql_transport::messages::result_message> msg) {
-            assert_that(msg).is_rows().with_rows({
-                { utf8_type->decompose(sstring("dcurrorw@techcrunch.com")) },
-                { utf8_type->decompose(sstring("beassebyv@house.gov")) },
-            });
+    return do_with_cql_env([] (cql_test_env& e) -> future<> {
+        co_await e.execute_cql("CREATE TABLE users (userid int, name text, email text, country text, PRIMARY KEY (userid));");
+        co_await e.execute_cql("CREATE INDEX ON users (email);");
+        co_await e.execute_cql("CREATE INDEX ON users (country);");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (0, 'Bondie Easseby', 'beassebyv@house.gov', 'France');");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (1, 'Demetri Curror', 'dcurrorw@techcrunch.com', 'France');");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (2, 'Langston Paulisch', 'lpaulischm@reverbnation.com', 'United States');");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (3, 'Channa Devote', 'cdevote14@marriott.com', 'Denmark');");
+
+        shared_ptr<cql_transport::messages::result_message> msg = co_await e.execute_cql("SELECT email FROM users WHERE country = 'France';");
+        assert_that(msg).is_rows().with_rows({
+            { utf8_type->decompose(sstring("dcurrorw@techcrunch.com")) },
+            { utf8_type->decompose(sstring("beassebyv@house.gov")) },
         });
     });
 }
 
 SEASTAR_TEST_CASE(test_secondary_index_clustering_key_query) {
-    return do_with_cql_env([] (cql_test_env& e) {
-        return e.execute_cql("CREATE TABLE users (userid int, name text, email text, country text, PRIMARY KEY (userid, country));").discard_result().then([&e] {
-            return e.execute_cql("CREATE INDEX ON users (country);").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (0, 'Bondie Easseby', 'beassebyv@house.gov', 'France');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (1, 'Demetri Curror', 'dcurrorw@techcrunch.com', 'France');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (2, 'Langston Paulisch', 'lpaulischm@reverbnation.com', 'United States');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (3, 'Channa Devote', 'cdevote14@marriott.com', 'Denmark');").discard_result();
-        }).then([&e] {
-            return e.execute_cql("SELECT email FROM users WHERE country = 'France';");
-        }).then([] (auto msg) {
-            assert_that(msg).is_rows().with_rows({
-                { utf8_type->decompose(sstring("dcurrorw@techcrunch.com")) },
-                { utf8_type->decompose(sstring("beassebyv@house.gov")) },
-            });
-        }).then([&e] {
-            return e.execute_cql("select country from users where country='France' and country='Denmark'"); // #7772
-        }).then([] (shared_ptr<cql_transport::messages::result_message> msg) {
-            assert_that(msg).is_rows().is_empty();
-        }).then([&e] {
-            return e.execute_cql("select country from users where country='Denmark' and country='Denmark'");
-        }).then([] (shared_ptr<cql_transport::messages::result_message> msg) {
-            assert_that(msg).is_rows().with_rows({{utf8_type->decompose(sstring("Denmark"))}});
+    return do_with_cql_env([] (cql_test_env& e) -> future<> {
+        co_await e.execute_cql("CREATE TABLE users (userid int, name text, email text, country text, PRIMARY KEY (userid, country));");
+        co_await e.execute_cql("CREATE INDEX ON users (country);");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (0, 'Bondie Easseby', 'beassebyv@house.gov', 'France');");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (1, 'Demetri Curror', 'dcurrorw@techcrunch.com', 'France');");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (2, 'Langston Paulisch', 'lpaulischm@reverbnation.com', 'United States');");
+        co_await e.execute_cql("INSERT INTO users (userid, name, email, country) VALUES (3, 'Channa Devote', 'cdevote14@marriott.com', 'Denmark');");
+
+        auto msg = co_await e.execute_cql("SELECT email FROM users WHERE country = 'France';");
+        assert_that(msg).is_rows().with_rows({
+            { utf8_type->decompose(sstring("dcurrorw@techcrunch.com")) },
+            { utf8_type->decompose(sstring("beassebyv@house.gov")) },
         });
+
+        msg = co_await e.execute_cql("select country from users where country='France' and country='Denmark'"); // #7772
+        assert_that(msg).is_rows().is_empty();
+
+        msg = co_await e.execute_cql("select country from users where country='Denmark' and country='Denmark'");
+        assert_that(msg).is_rows().with_rows({{utf8_type->decompose(sstring("Denmark"))}});
     });
 }
 
