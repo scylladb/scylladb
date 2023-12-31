bhalevy/scylla

gms: endpoint_state: define application_state_map
Have a central definition for the map held
in the endpoint_state (before changing it to
std::unordered_map).

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

gms/endpoint_state.cc
@@ -19,7 +19,7 @@ static_assert(std::is_default_constructible_v<heart_beat_state>);
 static_assert(std::is_nothrow_copy_constructible_v<heart_beat_state>);
 static_assert(std::is_nothrow_move_constructible_v<heart_beat_state>);
 
-static_assert(std::is_nothrow_default_constructible_v<std::map<application_state, versioned_value>>);
+static_assert(std::is_nothrow_default_constructible_v<application_states_map>);
 
 // Note: although std::map::find is not guaranteed to be noexcept
 // it depends on the comperator used and in this case comparing application_state

gms/endpoint_state.hh
@@ -19,6 +19,8 @@
 
 namespace gms {
 
+using application_states_map = std::map<application_state, versioned_value>;
+
 /**
  * This abstraction represents both the HeartBeatState and the ApplicationState in an EndpointState
  * instance. Any state for a given endpoint can be retrieved from this instance.
@@ -28,7 +30,7 @@ public:
     using clk = seastar::lowres_system_clock;
 private:
     heart_beat_state _heart_beat_state;
-    std::map<application_state, versioned_value> _application_state;
+    application_states_map _application_state;
     /* fields below do not get serialized */
     clk::time_point _update_timestamp;
     bool _is_normal = false;
@@ -55,7 +57,7 @@ public:
     }
 
     endpoint_state(heart_beat_state&& initial_hb_state,
-            const std::map<application_state, versioned_value>& application_state)
+            const application_states_map& application_state)
         : _heart_beat_state(std::move(initial_hb_state))
         , _application_state(application_state)
         , _update_timestamp(clk::now())
@@ -84,11 +86,11 @@ public:
      * TODO replace this with operations that don't expose private state
      */
     // @Deprecated
-    std::map<application_state, versioned_value>& get_application_state_map() noexcept {
+    application_states_map& get_application_state_map() noexcept {
         return _application_state;
     }
 
-    const std::map<application_state, versioned_value>& get_application_state_map() const noexcept {
+    const application_states_map& get_application_state_map() const noexcept {
         return _application_state;
     }
 

gms/gossiper.cc
@@ -474,7 +474,7 @@ gossiper::handle_get_endpoint_states_msg(gossip_get_endpoint_states_request requ
     for (const auto& [node, state] : _endpoint_state_map) {
         const heart_beat_state& hbs = state->get_heart_beat_state();
         auto state_wanted = endpoint_state(hbs);
-        const std::map<application_state, versioned_value>& apps = state->get_application_state_map();
+        auto& apps = state->get_application_state_map();
         for (const auto& app : apps) {
             if (application_states_wanted.count(app.first) > 0) {
                 state_wanted.get_application_state_map().emplace(app);
@@ -1945,7 +1945,7 @@ void gossiper::examine_gossiper(utils::chunked_vector<gossip_digest>& g_digest_l
     }
 }
 
-future<> gossiper::start_gossiping(gms::generation_type generation_nbr, std::map<application_state, versioned_value> preload_local_states, gms::advertise_myself advertise) {
+future<> gossiper::start_gossiping(gms::generation_type generation_nbr, application_states_map preload_local_states, gms::advertise_myself advertise) {
     auto permit = co_await lock_endpoint(get_broadcast_address(), null_permit_id);
     co_await container().invoke_on_all([advertise] (gossiper& g) {
         if (!advertise) {

gms/gossiper.hh
@@ -588,7 +588,7 @@ public:
      * existing nodes can talk to the replacing node. So the probability of
      * replacing node being talked to is pretty high.
      */
-    future<> start_gossiping(gms::generation_type generation_nbr, std::map<application_state, versioned_value> preload_local_states = {},
+    future<> start_gossiping(gms::generation_type generation_nbr, application_states_map preload_local_states = {},
             gms::advertise_myself advertise = gms::advertise_myself::yes);
 
 public:

service/storage_service.cc
@@ -2998,7 +2998,7 @@ future<> storage_service::join_token_ring(sharded<db::system_distributed_keyspac
         std::unordered_map<gms::inet_address, sstring> loaded_peer_features,
         std::chrono::milliseconds delay) {
     std::unordered_set<token> bootstrap_tokens;
-    std::map<gms::application_state, gms::versioned_value> app_states;
+    gms::application_states_map app_states;
     /* The timestamp of the CDC streams generation that this node has proposed when joining.
      * This value is nullopt only when:
      * 1. this node is being upgraded from a non-CDC version,

test/manual/gossip.cc
@@ -88,7 +88,7 @@ int main(int ac, char ** av) {
 
             std::cout << "Start gossiper service ...\n";
 
-            std::map<gms::application_state, gms::versioned_value> app_states = {
+            gms::application_states_map app_states = {
                 { gms::application_state::LOAD, gms::versioned_value::load(0.5) },
             };
 

scylladb/scylladb

gms/endpoint_state.cc
@@ -19,7 +19,7 @@ static_assert(std::is_default_constructible_v<heart_beat_state>);
 static_assert(std::is_nothrow_copy_constructible_v<heart_beat_state>);
 static_assert(std::is_nothrow_move_constructible_v<heart_beat_state>);
 
-static_assert(std::is_nothrow_default_constructible_v<std::map<application_state, versioned_value>>);
+static_assert(std::is_nothrow_default_constructible_v<application_state_map>);
 
 // Note: although std::map::find is not guaranteed to be noexcept
 // it depends on the comperator used and in this case comparing application_state

gms/endpoint_state.hh
@@ -19,6 +19,8 @@
 
 namespace gms {
 
+using application_state_map = std::map<application_state, versioned_value>;
+
 /**
  * This abstraction represents both the HeartBeatState and the ApplicationState in an EndpointState
  * instance. Any state for a given endpoint can be retrieved from this instance.
@@ -28,7 +30,7 @@ public:
     using clk = seastar::lowres_system_clock;
 private:
     heart_beat_state _heart_beat_state;
-    std::map<application_state, versioned_value> _application_state;
+    application_state_map _application_state;
     /* fields below do not get serialized */
     clk::time_point _update_timestamp;
     bool _is_normal = false;
@@ -55,7 +57,7 @@ public:
     }
 
     endpoint_state(heart_beat_state&& initial_hb_state,
-            const std::map<application_state, versioned_value>& application_state)
+            const application_state_map& application_state)
         : _heart_beat_state(std::move(initial_hb_state))
         , _application_state(application_state)
         , _update_timestamp(clk::now())
@@ -84,11 +86,11 @@ public:
      * TODO replace this with operations that don't expose private state
      */
     // @Deprecated
-    std::map<application_state, versioned_value>& get_application_state_map() noexcept {
+    application_state_map& get_application_state_map() noexcept {
         return _application_state;
     }
 
-    const std::map<application_state, versioned_value>& get_application_state_map() const noexcept {
+    const application_state_map& get_application_state_map() const noexcept {
         return _application_state;
     }
 

gms/gossiper.cc
@@ -474,7 +474,7 @@ gossiper::handle_get_endpoint_states_msg(gossip_get_endpoint_states_request requ
     for (const auto& [node, state] : _endpoint_state_map) {
         const heart_beat_state& hbs = state->get_heart_beat_state();
         auto state_wanted = endpoint_state(hbs);
-        const std::map<application_state, versioned_value>& apps = state->get_application_state_map();
+        auto& apps = state->get_application_state_map();
         for (const auto& app : apps) {
             if (application_states_wanted.count(app.first) > 0) {
                 state_wanted.get_application_state_map().emplace(app);
@@ -1945,7 +1945,7 @@ void gossiper::examine_gossiper(utils::chunked_vector<gossip_digest>& g_digest_l
     }
 }
 
-future<> gossiper::start_gossiping(gms::generation_type generation_nbr, std::map<application_state, versioned_value> preload_local_states, gms::advertise_myself advertise) {
+future<> gossiper::start_gossiping(gms::generation_type generation_nbr, application_state_map preload_local_states, gms::advertise_myself advertise) {
     auto permit = co_await lock_endpoint(get_broadcast_address(), null_permit_id);
     co_await container().invoke_on_all([advertise] (gossiper& g) {
         if (!advertise) {

gms/gossiper.hh
@@ -588,7 +588,7 @@ public:
      * existing nodes can talk to the replacing node. So the probability of
      * replacing node being talked to is pretty high.
      */
-    future<> start_gossiping(gms::generation_type generation_nbr, std::map<application_state, versioned_value> preload_local_states = {},
+    future<> start_gossiping(gms::generation_type generation_nbr, application_state_map preload_local_states = {},
             gms::advertise_myself advertise = gms::advertise_myself::yes);
 
 public:

service/storage_service.cc
@@ -3000,7 +3000,7 @@ future<> storage_service::join_token_ring(sharded<db::system_distributed_keyspac
         std::unordered_map<gms::inet_address, sstring> loaded_peer_features,
         std::chrono::milliseconds delay) {
     std::unordered_set<token> bootstrap_tokens;
-    std::map<gms::application_state, gms::versioned_value> app_states;
+    gms::application_state_map app_states;
     /* The timestamp of the CDC streams generation that this node has proposed when joining.
      * This value is nullopt only when:
      * 1. this node is being upgraded from a non-CDC version,

test/manual/gossip.cc
@@ -88,7 +88,7 @@ int main(int ac, char ** av) {
 
             std::cout << "Start gossiper service ...\n";
 
-            std::map<gms::application_state, gms::versioned_value> app_states = {
+            gms::application_state_map app_states = {
                 { gms::application_state::LOAD, gms::versioned_value::load(0.5) },
             };
 