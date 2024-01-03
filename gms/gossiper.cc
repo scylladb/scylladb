scylladb/scylladb

commit 147f30caff9fa8ca4fd0b6f7da766e61eb27021e

gossiper: mutate_live_and_unreachable_endpoints: make exception safe
Change the mutate_live_and_unreachable_endpoints procedure
so that the called `func` would mutate a cloned
`live_and_unreachable_endpoints` object in place.

Those are replicated to temporary copies on all shards
using `foreign<unique_ptr<>>` so that the would be
automatically freed on exception.

Only after all copies are made, they are applied
on all gossiper shards in a noexcept loop
and finally, a `on_success` function is called
to apply further side effects if everything else
was replicated successfully.

The latter is still susceptible to exceptions,
but we can live with those as long as `_live_endpoints`
and `_unreachable_endpoints` are synchronized on all shards.

With that, the read-only methods:
`get_live_members_synchronized` and
`get_unreachable_members_synchronized`
become trivial and they just return the required data
from shard 0.

Fixes #15089

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

Closes scylladb/scylladb#16597

gms/gossiper.cc
@@ -702,10 +702,10 @@ future<> gossiper::remove_endpoint(inet_address endpoint, permit_id pid) {
 
     auto state = get_endpoint_state_ptr(endpoint);
 
-    bool was_alive;
-    co_await mutate_live_and_unreachable_endpoints([endpoint, &was_alive] (gossiper& g) {
-        was_alive = g._live_endpoints.erase(endpoint);
-        g._unreachable_endpoints.erase(endpoint);
+    bool was_alive = false;
+    co_await mutate_live_and_unreachable_endpoints([endpoint, &was_alive] (live_and_unreachable_endpoints& data) {
+        was_alive = data.live.erase(endpoint);
+        data.unreachable.erase(endpoint);
     });
     _syn_handlers.erase(endpoint);
     _ack_handlers.erase(endpoint);
@@ -853,34 +853,31 @@ future<semaphore_units<>> gossiper::lock_endpoint_update_semaphore() {
     return get_units(_endpoint_update_semaphore, 1, _abort_source);
 }
 
-future<> gossiper::mutate_live_and_unreachable_endpoints(std::function<void(gossiper&)> func) {
+future<> gossiper::mutate_live_and_unreachable_endpoints(std::function<void(live_and_unreachable_endpoints&)> func,
+        std::function<void(gossiper&)> on_success) {
     auto lock = co_await lock_endpoint_update_semaphore();
-    func(*this);
+    auto cloned = std::make_unique<live_and_unreachable_endpoints>(_live_endpoints, _unreachable_endpoints);
+    func(*cloned);
 
     // Bump the _live_endpoints_version unconditionally,
     // even if only _unreachable_endpoints changed.
     // It will trigger a new round in the failure_detector_loop
     // but that's not too bad as changing _unreachable_endpoints
     // is rare enough.
-    _live_endpoints_version++;
-    co_await replicate_live_endpoints_on_change();
+    co_await replicate_live_endpoints_on_change(make_foreign(std::move(cloned)), _live_endpoints_version + 1);
+
+    on_success(*this);
 }
 
 future<std::set<inet_address>> gossiper::get_live_members_synchronized() {
-    return container().invoke_on(0, [] (gms::gossiper& g) -> future<std::set<inet_address>> {
-        auto lock = co_await g.lock_endpoint_update_semaphore();
-        std::set<inet_address> live_members = g.get_live_members();
-        co_await g.replicate_live_endpoints_on_change();
-        co_return live_members;
+    return container().invoke_on(0, [] (gms::gossiper& g) {
+        return g.get_live_members();
     });
 }
 
 future<std::set<inet_address>> gossiper::get_unreachable_members_synchronized() {
-    return container().invoke_on(0, [] (gms::gossiper& g) -> future<std::set<inet_address>> {
-        auto lock = co_await g.lock_endpoint_update_semaphore();
-        std::set<inet_address> unreachable_members = g.get_unreachable_members();
-        co_await g.replicate_live_endpoints_on_change();
-        co_return unreachable_members;
+    return container().invoke_on(0, [] (gms::gossiper& g) {
+        return g.get_unreachable_members();
     });
 }
 
@@ -984,8 +981,9 @@ future<> gossiper::failure_detector_loop() {
 }
 
 // This needs to be run with a lock
-future<> gossiper::replicate_live_endpoints_on_change() {
-    assert(this_shard_id() == 0);
+future<> gossiper::replicate_live_endpoints_on_change(foreign_ptr<std::unique_ptr<live_and_unreachable_endpoints>> data0, uint64_t new_version) {
+    auto coordinator = this_shard_id();
+    assert(coordinator == 0);
     //
     // Gossiper task runs only on CPU0:
     //
@@ -997,18 +995,31 @@ future<> gossiper::replicate_live_endpoints_on_change() {
     //
     logger.debug("replicating live and unreachable endpoints to other shards");
 
-    co_await container().invoke_on_others([this] (gossiper& local_gossiper) {
-        if (local_gossiper._live_endpoints_version != _live_endpoints_version) {
-            local_gossiper._live_endpoints = _live_endpoints;
-            local_gossiper._unreachable_endpoints = _unreachable_endpoints;
+    std::vector<foreign_ptr<std::unique_ptr<live_and_unreachable_endpoints>>> per_shard_data;
+    per_shard_data.resize(smp::count);
+    per_shard_data[coordinator] = std::move(data0);
 
-            // Set local _live_endpoints_version last to protect against exceptions
-            // on this shards.  In this case replicate_live_endpoints_on_change will
-            // retry setting the above next time around.
-            local_gossiper._live_endpoints_version = _live_endpoints_version;
-        } else {
-            logger.trace("shard already has the latest live and unreachable endpoints");
+    // Prepare copies on each other shard
+    co_await coroutine::parallel_for_each(boost::irange(0u, smp::count), [&per_shard_data, coordinator] (auto shard) -> future<> {
+        if (shard != this_shard_id()) {
+            const auto& src = *per_shard_data[coordinator];
+            per_shard_data[shard] = co_await smp::submit_to(shard, [&] {
+                return make_foreign(std::make_unique<live_and_unreachable_endpoints>(src));
+            });
         }
+    });
+
+    // Apply copies on each other shard
+    co_await container().invoke_on_all([&] (gossiper& local_gossiper) noexcept {
+        if (local_gossiper._live_endpoints_version >= new_version) {
+            on_fatal_internal_error(logger, fmt::format("shard already has unexpected live_endpoints_version {} > {}",
+                    local_gossiper._live_endpoints_version, new_version));
+        }
+
+        auto data = per_shard_data[this_shard_id()].release();
+        local_gossiper._live_endpoints = std::move(data->live);
+        local_gossiper._unreachable_endpoints = std::move(data->unreachable);
+        local_gossiper._live_endpoints_version = new_version;
     });
 }
 
@@ -1205,9 +1216,9 @@ version_type gossiper::get_max_endpoint_state_version(const endpoint_state& stat
 
 future<> gossiper::evict_from_membership(inet_address endpoint, permit_id pid) {
     verify_permit(endpoint, pid);
-    co_await mutate_live_and_unreachable_endpoints([endpoint] (gossiper& g) {
-        g._unreachable_endpoints.erase(endpoint);
-        g._live_endpoints.erase(endpoint);
+    co_await mutate_live_and_unreachable_endpoints([endpoint] (live_and_unreachable_endpoints& data) {
+        data.unreachable.erase(endpoint);
+        data.live.erase(endpoint);
     });
     co_await container().invoke_on_all([endpoint] (auto& g) {
         g._endpoint_state_map.erase(endpoint);
@@ -1641,12 +1652,12 @@ future<> gossiper::real_mark_alive(inet_address addr) {
 
     logger.debug("removing expire time for endpoint : {}", addr);
     bool was_live = false;
-    co_await mutate_live_and_unreachable_endpoints([addr, &was_live] (gossiper& g) {
-        g._unreachable_endpoints.erase(addr);
-        g._expire_time_endpoint_map.erase(addr);
-
-        auto [it_, inserted] = g._live_endpoints.insert(addr);
+    co_await mutate_live_and_unreachable_endpoints([addr, &was_live] (live_and_unreachable_endpoints& data) {
+        data.unreachable.erase(addr);
+        auto [it_, inserted] = data.live.insert(addr);
         was_live = !inserted;
+    }, [addr] (gossiper& g) {
+        g._expire_time_endpoint_map.erase(addr);
     });
     if (was_live) {
         co_return;
@@ -1671,9 +1682,9 @@ future<> gossiper::real_mark_alive(inet_address addr) {
 future<> gossiper::mark_dead(inet_address addr, endpoint_state_ptr state, permit_id pid) {
     logger.trace("marking as down {}", addr);
     verify_permit(addr, pid);
-    co_await mutate_live_and_unreachable_endpoints([addr] (gossiper& g) {
-        g._live_endpoints.erase(addr);
-        g._unreachable_endpoints[addr] = now();
+    co_await mutate_live_and_unreachable_endpoints([addr] (live_and_unreachable_endpoints& data) {
+        data.live.erase(addr);
+        data.unreachable[addr] = now();
     });
     logger.info("InetAddress {} is now DOWN, status = {}", addr, get_gossip_status(*state));
     co_await do_on_dead_notifications(addr, std::move(state), pid);

gms/gossiper.hh
@@ -248,22 +248,26 @@ private:
     // Must be called on shard 0.
     future<semaphore_units<>> lock_endpoint_update_semaphore();
 
+    struct live_and_unreachable_endpoints {
+        std::unordered_set<inet_address> live;
+        std::unordered_map<inet_address, clk::time_point> unreachable;
+    };
+
     // Must be called on shard 0.
     // Update _live_endpoints and/or _unreachable_endpoints
     // on shard 0, after acquiring `lock_endpoint_update_semaphore`.
-    // replicate_live_endpoints_on_change is called to replicate the changes to
-    // all shards and _live_endpoints_version is bumped unconditionally.
     //
-    // FIXME: The function is not exception safe so some shards
-    // may be left with stale _live_endpoints or _unreachable_endpoints.
-    // In this case, the _live_endpoints_version on that shard will not be updated.
-    // That can be examined under the endpoint_update_semaphore to make sure
-    // they all shards are consistent with each other.
-    future<> mutate_live_and_unreachable_endpoints(std::function<void(gossiper&)>);
+    // The called function modifies a copy of live_and_unreachable_endpoints
+    // which is then copied to temporary copies for all shards
+    // and then applied atomcically on all shards.
+    //
+    // Finally, the `on_success` callback is called on shard 0 iff replication was successful.
+    future<> mutate_live_and_unreachable_endpoints(std::function<void(live_and_unreachable_endpoints&)> func,
+            std::function<void(gossiper&)> on_success = [] (gossiper&) {});
 
     // replicate shard 0 live and unreachable endpoints sets across all other shards.
     // _endpoint_update_semaphore must be held for the whole duration
-    future<> replicate_live_endpoints_on_change();
+    future<> replicate_live_endpoints_on_change(foreign_ptr<std::unique_ptr<live_and_unreachable_endpoints>>, uint64_t new_live_endpoints_version);
 
     void run();
     // Replicates given endpoint_state to all other shards.

commit b2be13f668edc263deb24cd974245c89cef77324

gossiper: get_*_members_synchronized: acquire endpoint update semaphore
To ensure that the value they return is synchronized on all shards.

This got broken recently by 147f30caff9fa8ca4fd0b6f7da766e61eb27021e.

Refs https://github.com/scylladb/scylladb/pull/16597#discussion_r1440445432

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

gms/gossiper.cc
@@ -870,14 +870,18 @@ future<> gossiper::mutate_live_and_unreachable_endpoints(std::function<void(live
 }
 
 future<std::set<inet_address>> gossiper::get_live_members_synchronized() {
-    return container().invoke_on(0, [] (gms::gossiper& g) {
-        return g.get_live_members();
+    return container().invoke_on(0, [] (gms::gossiper& g) -> future<std::set<inet_address>> {
+        // Make sure the value we return is synchronized on all shards
+        auto lock = co_await g.lock_endpoint_update_semaphore();
+        co_return g.get_live_members();
     });
 }
 
 future<std::set<inet_address>> gossiper::get_unreachable_members_synchronized() {
-    return container().invoke_on(0, [] (gms::gossiper& g) {
-        return g.get_unreachable_members();
+    return container().invoke_on(0, [] (gms::gossiper& g) -> future<std::set<inet_address>> {
+        // Make sure the value we return is synchronized on all shards
+        auto lock = co_await g.lock_endpoint_update_semaphore();
+        co_return g.get_unreachable_members();
     });
 }
 