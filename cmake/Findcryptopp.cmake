scylladb/scylladb

commit 2508d3394631e78f488415732f14bed096f7d2c9

build: cmake: add Findcryptopp.cmake
seastar dropped the dependency to Crypto++, and it also removed
Findcryptopp.cmake from its `cmake` directory. but scylladb still
depends on this library. and it has been using the `Findcryptopp.cmake`
in seastar submodule for finding it.

after the removal of this file, scylladb would not be able to
use it anymore. so, we have to provide our own `Findcryptopp.cmake`.

Findcryptopp.cmake is copied from the Seastar project. So its
date of copyright is preserved. and it was licensed under Apache 2.0,
since we are creating a derivative work from it. let's relicense
it under Apache 2.0 and AGPL 3.0 or later.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

Closes scylladb/scylladb#16601

cmake/Findcryptopp.cmake
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2018-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: (AGPL-3.0-or-later and Apache-2.0)
+ */
+
+find_library (cryptopp_LIBRARY
+  NAMES cryptopp)
+
+find_path (cryptopp_INCLUDE_DIR
+  NAMES cryptopp/aes.h
+  PATH_SUFFIXES cryptopp)
+
+mark_as_advanced (
+  cryptopp_LIBRARY
+  cryptopp_INCLUDE_DIR)
+
+include (FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args (cryptopp
+  REQUIRED_VARS
+    cryptopp_LIBRARY
+    cryptopp_INCLUDE_DIR)
+
+set (cryptopp_LIBRARIES ${cryptopp_LIBRARY})
+set (cryptopp_INCLUDE_DIRS ${cryptopp_INCLUDE_DIR})
+
+if (cryptopp_FOUND AND NOT (TARGET cryptopp::cryptopp))
+  add_library (cryptopp::cryptopp UNKNOWN IMPORTED)
+
+  set_target_properties (cryptopp::cryptopp
+    PROPERTIES
+      IMPORTED_LOCATION ${cryptopp_LIBRARIES}
+      INTERFACE_INCLUDE_DIRECTORIES ${cryptopp_INCLUDE_DIRS})
+endif ()

commit a67e58509d6747131b298046e24a4a431eb3cd4b

build: cmake: use # for line comment
it was a copy-pasta error introduced by 2508d339. the copyright
blob was copied from a C++ source code, but the CMake language
define the block comment is different from the C++ language.

let's use the line comment of CMake.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

cmake/Findcryptopp.cmake
@@ -1,10 +1,8 @@
-/*
- * Copyright (C) 2018-present ScyllaDB
- */
-
-/*
- * SPDX-License-Identifier: (AGPL-3.0-or-later and Apache-2.0)
- */
+#
+# Copyright (C) 2018-present ScyllaDB
+#
+# SPDX-License-Identifier: (AGPL-3.0-or-later and Apache-2.0)
+#
 
 find_library (cryptopp_LIBRARY
   NAMES cryptopp)