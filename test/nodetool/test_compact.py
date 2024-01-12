bhalevy/scylla

api: compaction: add flush_memtables option
When flushing is done externally, e.g. by running
`nodetool flush` prior to `nodetool compact`,
flush_memtables=false can be passed to skip flushing
of tables right before they are major-compacted.

This is useful to prevent creation of small sstables
due to excessive memtable flushing.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

test/nodetool/test_compact.py
@@ -6,6 +6,7 @@
 
 from rest_api_mock import expected_request
 import utils
+import pytest
 
 
 def test_all_keyspaces(nodetool):
@@ -76,3 +77,22 @@ def test_user_defined(nodetool, scylla_only):
              "me-3g8w_11cg_4317k2ppfb6d5vgp0w-big-Data.db"),
             {},
             ["error processing arguments: --user-defined flag is unsupported"])
+
+
+@pytest.mark.parametrize("flush", ("true", "false"))
+def test_keyspace_flush(nodetool, scylla_only, flush):
+    params = {"flush_memtables": flush}
+    nodetool("compact", "system_schema", "--flush-memtables", flush, expected_requests=[
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
+            expected_request("POST", "/storage_service/keyspace_compaction/system_schema", params=params)])
+
+
+@pytest.mark.parametrize("flush", ("true", "false"))
+def test_all_keyspaces_flush(nodetool, scylla_only, flush):
+    params = {"flush_memtables": flush}
+    nodetool("compact", "--flush-memtables", flush, expected_requests=[
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                            response=["system", "system_schema"]),
+            expected_request("POST", "/storage_service/keyspace_compaction/system", params=params),
+            expected_request("POST", "/storage_service/keyspace_compaction/system_schema", params=params)])

scylladb/scylladb

(cherry picked from commit 1fd85bd37bbc451518c4bcea2376081907cf7af7)

test/nodetool/test_compact.py
@@ -6,6 +6,7 @@
 
 from rest_api_mock import expected_request
 import utils
+import pytest
 
 
 def test_all_keyspaces(nodetool):
@@ -76,3 +77,24 @@ def test_user_defined(nodetool, scylla_only):
              "me-3g8w_11cg_4317k2ppfb6d5vgp0w-big-Data.db"),
             {},
             ["error processing arguments: --user-defined flag is unsupported"])
+
+
+@pytest.mark.parametrize("flush", ("true", "false"))
+# The `--flush-memtables` option to `nodetool compact` is available only with `scylla_nodetool`
+def test_keyspace_flush_memtables_option(nodetool, scylla_only, flush):
+    params = {"flush_memtables": flush}
+    nodetool("compact", "system_schema", "--flush-memtables", flush, expected_requests=[
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                             response=["system", "system_schema"]),
+            expected_request("POST", "/storage_service/keyspace_compaction/system_schema", params=params)])
+
+
+@pytest.mark.parametrize("flush", ("true", "false"))
+# The `--flush-memtables` option to `nodetool compact` is available only with `scylla_nodetool`
+def test_all_keyspaces_flush_memtables_option(nodetool, scylla_only, flush):
+    params = {"flush_memtables": flush}
+    nodetool("compact", "--flush-memtables", flush, expected_requests=[
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                            response=["system", "system_schema"]),
+            expected_request("POST", "/storage_service/keyspace_compaction/system", params=params),
+            expected_request("POST", "/storage_service/keyspace_compaction/system_schema", params=params)])