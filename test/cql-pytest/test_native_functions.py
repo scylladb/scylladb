827c20467c9d0709e0dd4b0bbee31ba6087b9210
Nadav Har'El <nyh@scylladb.com>
2024-01-30 21:15:26 +0200

    utils: add a timeuuid minimum, like we had maximum
    Our time-handling code in UUID_gen.hh is very fragile for very large
    timestamps, because the different types - such as Cassandra "timestamp"
    and Timeuuid use very different resolution and ranges.
    
    In issue #17035 we discovered a situation where a certain CQL
    "timestamp"-type value could cause an assertion-failure and a crash
    in the create_time() function that creates a timeuuid - because that
    timestamp didn't fit the place we have in timeuuid.
    
    We already added in the past a limit, UUID_UNIXTIME_MAX, beyond which
    we refuse timestamps, to avoid these assertions failure. However, we
    missed the possibility of *negative* timestamps (which are allowed in
    CQL), and indeed a negative timestamp (or a timestamp which was "wrapped"
    to a negative value) is what caused issue #17035.
    
    So this patch adds a second limit, UUID_UNIXTIME_MIN - limiting the
    most negative timestamp that we support to well below the area which
    causes problems, and adds tests that reproduce #17035 and that we
    didn't break anything else (e.g., negative timestamps are still
    allowed - just not extremely negative timestamps).
    
    Fixes #17035.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- test/cql-pytest/test_native_functions.py
+++ test/cql-pytest/test_native_functions.py
@@ -72,3 +72,52 @@ def test_blobas_wrong_size(cql, table1):
     # value 0x0000007b is not a valid binary representation for type bigint".
     with pytest.raises(InvalidRequest, match='blobasbigint'):
         cql.execute(f"SELECT blobasbigint(intasblob(i)) FROM {table1} WHERE p={p}")
+
+# The mintimeuuid() function works with valid "timestamp"-type values, which
+# are 64-bit *signed* integers representing milliseconds since the epoch.
+# In particular, negative timestamps are allowed and should not be rejected.
+# The function tounixtimestamp can be used to extract the timestamp back from
+# the timeuuid. In this test we use "reasonably"-low timestamps (referring
+# to dates in this and the previous century - not to the age of the
+# dinosaurs). The test that follows will look at extreme, not-useful-in-
+# practice, timestamp values.
+@pytest.mark.parametrize("timestamp", [-1706638779000, -123, 123, 1706638779000])
+def test_mintimeuuid_reasonable(cql, table1, timestamp):
+    p = unique_key_int()
+    cql.execute(f"INSERT INTO {table1} (p, t) VALUES ({p}, {timestamp})")
+    assert [(timestamp,)] == list(cql.execute(f"SELECT tounixtimestamp(mintimeuuid(t)) FROM {table1} WHERE p={p}"))
+
+# The "timestamp" column type stores a 64-bit number of milliseconds, but
+# Scylla's and Cassandra's implementation of timeuuids cannot store this
+# entire range. This test expects mintimeuuid() to either return the *right*
+# thing (converted back to the original timestamp via tounixtimestamp),
+# or to throw an exception. Crashing is not acceptable (reproduces #17035),
+# and so is returning a value, but it having the wrong timestamp (this
+# happens in Cassandra, so this test is marked a cassandra_bug)
+@pytest.mark.parametrize("timestamp", [-2**63+123, -2**62, -2**58, 2**62, 2**63-123])
+def test_mintimeuuid_extreme(cql, table1, timestamp, cassandra_bug):
+    p = unique_key_int()
+    cql.execute(f"INSERT INTO {table1} (p, t) VALUES ({p}, {timestamp})")
+    try:
+        res = list(cql.execute(f"SELECT tounixtimestamp(mintimeuuid(t)) FROM {table1} WHERE p={p}"))
+    except:
+        # If mintimeuuid() refuses these extreme values, it's fine.
+        # The real bug is to return something, which is wrong (the
+        # assert below), or to crash the server (issue #17035).
+        pass
+    else:
+        assert [(timestamp,)] == res
+
+# An example of an extreme negative timestamp is what totimestamp(123)
+# returns: As shown in test_type_date.py, the number "123" is converted to
+# a date very long in the past (2**31 is the day of the epoch), and results
+# in a very negative timestamp.
+# We don't expect much from this test - it may report strange errors (this
+# happens in both Cassandra and Scylla), but it mustn't crash as it did in
+# issue #17035.
+def test_mintimeuuid_extreme_from_totimestamp(cql, table1):
+    p = unique_key_int()
+    try:
+        cql.execute(f"SELECT * FROM {table1} WHERE p={p} and u < mintimeuuid(totimestamp(123)) ALLOW FILTERING")
+    except:
+        pass
