94af1df2cf2f5b6e75c3ff5bc59d1a1c958e1380
Botond Dénes <bdenes@scylladb.com>
2024-02-01 10:48:48 +0200

    Merge 'Fix mintimeuuid() call that could crash Scylla' from Nadav Har'El
    This PR fixes the bug of certain calls to the `mintimeuuid()` CQL function which large negative timestamps could crash Scylla. It turns out we already had protections in place against very positive timestamps, but very negative timestamps could still cause bugs.
    
    The actual fix in this series is just a few lines, but the bigger effort was improving the test coverage in this area. I added tests for the "date" type (the original reproducer for this bug used totimestamp() which takes a date parameter), and also reproducers for this bug directly, without totimestamp() function, and one with that function.
    
    Finally this PR also replaces the assert() which made this molehill-of-a-bug into a mountain, by a throw.
    
    Fixes #17035
    
    Closes scylladb/scylladb#17073
    
    * github.com:scylladb/scylladb:
      utils: replace assert() by on_internal_error()
      utils: add on_internal_error with common logger
      utils: add a timeuuid minimum, like we had maximum
      test/cql-pytest: tests for "date" type
    
    (cherry picked from commit 2a4b99177253da1df9df72ae89ea3d2abbe1bf29)

--- configure.py
+++ configure.py
@@ -698,6 +698,7 @@ scylla_core = (['message/messaging_service.cc',
                 'mutation_partition.cc',
                 'mutation_partition_view.cc',
                 'mutation_partition_serializer.cc',
+                'utils/on_internal_error.cc',
                 'converting_mutation_partition_applier.cc',
                 'readers/combined.cc',
                 'readers/multishard.cc',
@@ -1271,7 +1272,7 @@ deps['test/boost/bytes_ostream_test'] = [
     "test/lib/log.cc",
 ]
 deps['test/boost/input_stream_test'] = ['test/boost/input_stream_test.cc']
-deps['test/boost/UUID_test'] = ['utils/UUID_gen.cc', 'test/boost/UUID_test.cc', 'utils/uuid.cc', 'utils/dynamic_bitset.cc', 'hashers.cc']
+deps['test/boost/UUID_test'] = ['utils/UUID_gen.cc', 'test/boost/UUID_test.cc', 'utils/uuid.cc', 'utils/dynamic_bitset.cc', 'hashers.cc', 'utils/on_internal_error.cc']
 deps['test/boost/murmur_hash_test'] = ['bytes.cc', 'utils/murmur_hash.cc', 'test/boost/murmur_hash_test.cc']
 deps['test/boost/allocation_strategy_test'] = ['test/boost/allocation_strategy_test.cc', 'utils/logalloc.cc', 'utils/dynamic_bitset.cc']
 deps['test/boost/log_heap_test'] = ['test/boost/log_heap_test.cc']
