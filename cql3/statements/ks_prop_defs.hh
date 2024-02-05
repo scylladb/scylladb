3906f8e91a2f5b535ac02cca1d2e5209f207d62e
Pavel Emelyanov <xemul@scylladb.com>
2024-02-05 15:59:24 +0300

    ks_prop_defs: Remove preprocessor-guarded java code
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- cql3/statements/ks_prop_defs.hh
+++ cql3/statements/ks_prop_defs.hh
@@ -56,20 +56,6 @@ public:
     data_dictionary::storage_options get_storage_options() const;
     lw_shared_ptr<data_dictionary::keyspace_metadata> as_ks_metadata(sstring ks_name, const locator::token_metadata&, const gms::feature_service&);
     lw_shared_ptr<data_dictionary::keyspace_metadata> as_ks_metadata_update(lw_shared_ptr<data_dictionary::keyspace_metadata> old, const locator::token_metadata&, const gms::feature_service&);
-
-#if 0
-    public KSMetaData asKSMetadataUpdate(KSMetaData old) throws RequestValidationException
-    {
-        String sClass = strategyClass;
-        Map<String, String> sOptions = getReplicationOptions();
-        if (sClass == null)
-        {
-            sClass = old.strategyClass.getName();
-            sOptions = old.strategyOptions;
-        }
-        return KSMetaData.newKeyspace(old.name, sClass, sOptions, getBoolean(KW_DURABLE_WRITES, old.durableWrites));
-    }
-#endif
 };
 
 }
