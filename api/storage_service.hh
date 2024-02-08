9b6a66826c859225cfbf3b78d878d4c9f9bbf70f
Kefu Chai <kefu.chai@scylladb.com>
2024-02-08 13:32:27 +0800

    api/storage_service: add more constness to http_context parameter
    when we just want to perform read access to `http_context`, there
    is no need to use a non-const reference. so let's add `const` specifier
    to make this explicit. this shoudl help with the readability and
    maintainability.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    
    Closes scylladb/scylladb#17219

--- api/storage_service.hh
+++ api/storage_service.hh
@@ -36,27 +36,27 @@ namespace api {
 
 // verify that the keyspace is found, otherwise a bad_param_exception exception is thrown
 // containing the description of the respective keyspace error.
-sstring validate_keyspace(http_context& ctx, sstring ks_name);
+sstring validate_keyspace(const http_context& ctx, sstring ks_name);
 
 // verify that the keyspace parameter is found, otherwise a bad_param_exception exception is thrown
 // containing the description of the respective keyspace error.
-sstring validate_keyspace(http_context& ctx, const httpd::parameters& param);
+sstring validate_keyspace(const http_context& ctx, const httpd::parameters& param);
 
 // splits a request parameter assumed to hold a comma-separated list of table names
 // verify that the tables are found, otherwise a bad_param_exception exception is thrown
 // containing the description of the respective no_such_column_family error.
 // Returns an empty vector if no parameter was found.
 // If the parameter is found and empty, returns a list of all table names in the keyspace.
-std::vector<sstring> parse_tables(const sstring& ks_name, http_context& ctx, const std::unordered_map<sstring, sstring>& query_params, sstring param_name);
+std::vector<sstring> parse_tables(const sstring& ks_name, const http_context& ctx, const std::unordered_map<sstring, sstring>& query_params, sstring param_name);
 
 // splits a request parameter assumed to hold a comma-separated list of table names
 // verify that the tables are found, otherwise a bad_param_exception exception is thrown
 // containing the description of the respective no_such_column_family error.
 // Returns a vector of all table infos given by the parameter, or
 // if the parameter is not found or is empty, returns a list of all table infos in the keyspace.
-std::vector<table_info> parse_table_infos(const sstring& ks_name, http_context& ctx, const std::unordered_map<sstring, sstring>& query_params, sstring param_name);
+std::vector<table_info> parse_table_infos(const sstring& ks_name, const http_context& ctx, const std::unordered_map<sstring, sstring>& query_params, sstring param_name);
 
-std::vector<table_info> parse_table_infos(const sstring& ks_name, http_context& ctx, sstring value);
+std::vector<table_info> parse_table_infos(const sstring& ks_name, const http_context& ctx, sstring value);
 
 struct scrub_info {
     sstables::compaction_type_options::scrub opts;
@@ -64,7 +64,7 @@ struct scrub_info {
     std::vector<sstring> column_families;
 };
 
-future<scrub_info> parse_scrub_options(http_context& ctx, sharded<db::snapshot_ctl>& snap_ctl, std::unique_ptr<http::request> req);
+future<scrub_info> parse_scrub_options(const http_context& ctx, sharded<db::snapshot_ctl>& snap_ctl, std::unique_ptr<http::request> req);
 
 void set_storage_service(http_context& ctx, httpd::routes& r, sharded<service::storage_service>& ss, service::raft_group0_client&);
 void unset_storage_service(http_context& ctx, httpd::routes& r);
