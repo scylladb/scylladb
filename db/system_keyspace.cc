piodul/scylla

topology_state_machine: introduce upgrade_state
`upgrade_state` is a static column which will be used to track the
progress of building the topology state machine.

db/system_keyspace.cc
@@ -240,6 +240,7 @@ schema_ptr system_keyspace::topology() {
             .with_column("enabled_features", set_type_impl::get_instance(utf8_type, true), column_kind::static_column)
             .with_column("session", uuid_type, column_kind::static_column)
             .with_column("tablet_balancing_enabled", boolean_type, column_kind::static_column)
+            .with_column("upgrade_state", utf8_type, column_kind::static_column)
             .set_comment("Current state of topology change machine")
             .with_version(generate_schema_version(id))
             .build();
@@ -2663,6 +2664,12 @@ future<service::topology> system_keyspace::load_topology_state() {
     }
 
     for (auto& row : *rs) {
+        if (!row.has("host_id")) {
+            // There are no clustering rows, only the static row.
+            // Skip the whole loop, the static row is handled later.
+            break;
+        }
+
         raft::server_id host_id{row.get_as<utils::UUID>("host_id")};
         auto datacenter = row.get_as<sstring>("datacenter");
         auto rack = row.get_as<sstring>("rack");
@@ -2906,6 +2913,12 @@ future<service::topology> system_keyspace::load_topology_state() {
         } else {
             ret.tablet_balancing_enabled = true;
         }
+
+        if (some_row.has("upgrade_state")) {
+            ret.ustate = service::upgrade_state_from_string(some_row.get_as<sstring>("upgrade_state"));
+        } else {
+            ret.ustate = service::topology::upgrade_state::not_upgraded;
+        }
     }
 
     co_return ret;
@@ -2927,6 +2940,12 @@ service::topology_features system_keyspace::decode_topology_features_state(::sha
     }
 
     for (auto& row : *rs) {
+        if (!row.has("host_id")) {
+            // There are no clustering rows, only the static row.
+            // Skip the whole loop, the static row is handled later.
+            break;
+        }
+
         raft::server_id host_id{row.get_as<utils::UUID>("host_id")};
         service::node_state nstate = service::node_state_from_string(row.get_as<sstring>("node_state"));
         if (row.has("supported_features") && nstate == service::node_state::normal) {

service/topology_mutation.cc
@@ -224,6 +224,10 @@ topology_mutation_builder& topology_mutation_builder::set_global_topology_reques
     return apply_atomic("global_topology_request", ::format("{}", value));
 }
 
+topology_mutation_builder& topology_mutation_builder::set_upgrade_state(topology::upgrade_state value) {
+    return apply_atomic("upgrade_state", ::format("{}", value));
+}
+
 topology_mutation_builder& topology_mutation_builder::add_enabled_features(const std::set<sstring>& features) {
     return apply_set("enabled_features", collection_apply_mode::update, features | boost::adaptors::transformed([] (const auto& f) { return sstring(f); }));
 }

service/topology_mutation.hh
@@ -116,6 +116,7 @@ public:
     topology_mutation_builder& set_new_cdc_generation_data_uuid(const utils::UUID& value);
     topology_mutation_builder& set_unpublished_cdc_generations(const std::vector<cdc::generation_id_v2>& values);
     topology_mutation_builder& set_global_topology_request(global_topology_request);
+    topology_mutation_builder& set_upgrade_state(topology::upgrade_state);
     topology_mutation_builder& add_enabled_features(const std::set<sstring>& value);
     topology_mutation_builder& add_unpublished_cdc_generation(const cdc::generation_id_v2& value);
     topology_mutation_builder& del_transition_state();

scylladb/scylladb

db/system_keyspace.cc
@@ -240,6 +240,7 @@ schema_ptr system_keyspace::topology() {
             .with_column("enabled_features", set_type_impl::get_instance(utf8_type, true), column_kind::static_column)
             .with_column("session", uuid_type, column_kind::static_column)
             .with_column("tablet_balancing_enabled", boolean_type, column_kind::static_column)
+            .with_column("upgrade_state", utf8_type, column_kind::static_column)
             .set_comment("Current state of topology change machine")
             .with_version(generate_schema_version(id))
             .build();
@@ -2663,6 +2664,12 @@ future<service::topology> system_keyspace::load_topology_state() {
     }
 
     for (auto& row : *rs) {
+        if (!row.has("host_id")) {
+            // There are no clustering rows, only the static row.
+            // Skip the whole loop, the static row is handled later.
+            break;
+        }
+
         raft::server_id host_id{row.get_as<utils::UUID>("host_id")};
         auto datacenter = row.get_as<sstring>("datacenter");
         auto rack = row.get_as<sstring>("rack");
@@ -2889,6 +2896,12 @@ future<service::topology> system_keyspace::load_topology_state() {
         } else {
             ret.tablet_balancing_enabled = true;
         }
+
+        if (some_row.has("upgrade_state")) {
+            ret.upgrade_state = service::upgrade_state_from_string(some_row.get_as<sstring>("upgrade_state"));
+        } else {
+            ret.upgrade_state = service::topology::upgrade_state_type::not_upgraded;
+        }
     }
 
     co_return ret;
@@ -2910,6 +2923,12 @@ service::topology_features system_keyspace::decode_topology_features_state(::sha
     }
 
     for (auto& row : *rs) {
+        if (!row.has("host_id")) {
+            // There are no clustering rows, only the static row.
+            // Skip the whole loop, the static row is handled later.
+            break;
+        }
+
         raft::server_id host_id{row.get_as<utils::UUID>("host_id")};
         service::node_state nstate = service::node_state_from_string(row.get_as<sstring>("node_state"));
         if (row.has("supported_features") && nstate == service::node_state::normal) {

service/topology_mutation.cc
@@ -224,6 +224,10 @@ topology_mutation_builder& topology_mutation_builder::set_global_topology_reques
     return apply_atomic("global_topology_request", ::format("{}", value));
 }
 
+topology_mutation_builder& topology_mutation_builder::set_upgrade_state(topology::upgrade_state_type value) {
+    return apply_atomic("upgrade_state", ::format("{}", value));
+}
+
 topology_mutation_builder& topology_mutation_builder::add_enabled_features(const std::set<sstring>& features) {
     return apply_set("enabled_features", collection_apply_mode::update, features | boost::adaptors::transformed([] (const auto& f) { return sstring(f); }));
 }

service/topology_mutation.hh
@@ -116,6 +116,7 @@ public:
     topology_mutation_builder& set_new_cdc_generation_data_uuid(const utils::UUID& value);
     topology_mutation_builder& set_unpublished_cdc_generations(const std::vector<cdc::generation_id_v2>& values);
     topology_mutation_builder& set_global_topology_request(global_topology_request);
+    topology_mutation_builder& set_upgrade_state(topology::upgrade_state_type);
     topology_mutation_builder& add_enabled_features(const std::set<sstring>& value);
     topology_mutation_builder& add_unpublished_cdc_generation(const cdc::generation_id_v2& value);
     topology_mutation_builder& del_transition_state();