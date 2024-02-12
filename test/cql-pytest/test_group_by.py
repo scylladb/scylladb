scylladb/scylladb

commit 432cb02d643973aa0267a1dc42ccfe7feb99d153

cql3: select_statement: force aggregation if GROUP BY is used
GROUP BY is typically used with aggregation. In one case the aggregation
is implicit:

    SELECT a, b, c
    FROM tab
    GROUP BY x, y, z

One row will appear from each group, even though no aggregation
was specified. To avoid this irregularity, rewrite this query as

    SELECT first(a), first(b), first(c)
    FROM tab
    GROUP BY x, y, z

This allows us to have different paths for aggregations and
non-aggregations, without worrying about this special case.

cql3/statements/select_statement.cc
@@ -1699,6 +1699,12 @@ std::unique_ptr<prepared_statement> select_statement::prepare(data_dictionary::d
     prepared_selectors = maybe_jsonize_select_clause(std::move(prepared_selectors), db, schema);
 
     auto aggregation_depth = 0u;
+
+    // Force aggregation if GROUP BY is used. This will wrap every column x as first(x).
+    if (!_group_by_columns.empty()) {
+        aggregation_depth = std::max(aggregation_depth, 1u);
+    }
+
     for (auto& ps : prepared_selectors) {
         aggregation_depth = std::max(aggregation_depth, expr::aggregation_depth(ps.expr));
     }

commit dad5cb54fef6216f90d6d84241e4596532d75117

test/cql-pytest: reproducer for GROUP BY regression
This patch adds a simple reproducer for a regression in Scylla 5.4 caused
by commit 432cb02, breaking LIMIT support in GROUP BY.

Refs #17237

Signed-off-by: Nadav Har'El <nyh@scylladb.com>

test/cql-pytest/test_group_by.py
@@ -36,6 +36,15 @@ def table1(cql, test_keyspace):
 def test_group_by_partition(cql, table1):
     assert {(0,0,0,1), (1,0,0,5)} == set(cql.execute(f'SELECT p,c1,c2,v FROM {table1} GROUP BY p'))
 
+# Adding a LIMIT should be honored.
+# Reproduces #17237 - more results than the limit were generated.
+@pytest.mark.xfail(reason="issue #17237")
+def test_group_by_partition_with_limit(cql, table1):
+    # "LIMIT 1" should return only one of the two matches (0,0,0,1) and
+    # (1,0,0,5). The partition key 1 happens to be the first one in
+    # murmur3 order, so this is what should be returned.
+    assert {(1,0,0,5)} == set(cql.execute(f'SELECT p,c1,c2,v FROM {table1} GROUP BY p LIMIT 1'))
+
 # Try the same restricting the scan to a single partition instead of a
 # whole-table scan. We should get just one row (the first row in the
 # clustering order).