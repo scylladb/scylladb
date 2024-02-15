1d6caa42b94441f3aee3456d20df4bfd2bca28ea
Petr Gusev <petr.gusev@scylladb.com>
2024-02-15 19:41:15 +0400

    join_cluster: move was_decommissioned check earlier
    Before the patch if a decommissioned node tries
    to restart, it calls _group0->discover_group0 first
    in join_cluster, which hangs since decommissioned
    nodes are banned and other nodes don't respond
    to their discovering requests.
    
    We fix the problem by checking was_decommissioned()
    flag before calling discover_group0.
    
    fixes scylladb/scylladb#17282
    
    Closes scylladb/scylladb#17358

---
+++ test/topology_custom/test_decommission.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2024-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+import logging
+
+import pytest
+from test.pylib.manager_client import ManagerClient
+from test.topology.util import check_token_ring_and_group0_consistency
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.asyncio
+async def test_decommissioned_node_cant_rejoin(request, manager: ManagerClient):
+    # This a regression test for #17282.
+
+    logger.info("Bootstrapping a leader node")
+    servers = [await manager.server_add()]
+
+    logger.info(f"Bootstrapping second node")
+    servers += [await manager.server_add()]
+
+    # It's important that we decommission a node which is not a leader.
+    # We want to check the case when after restart the node needs
+    # to communicate with other nodes to discover a leader.
+    logger.info(f"Decommissioning node {servers[1]}")
+    await manager.decommission_node(servers[1].server_id)
+    await check_token_ring_and_group0_consistency(manager)
+    logger.info(f"huj1")
+    await manager.server_start(servers[1].server_id,
+                               expected_error='This node was decommissioned and will not rejoin the ring')
+    logger.info(f"huj2")
