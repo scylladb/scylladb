53996e25571bd31477cf6d83799044bacb730940
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2024-02-26 14:08:26 +0100

    test: add auth-v2 snapshot transfer test

--- test/auth_cluster/test_auth_no_quorum.py
+++ test/auth_cluster/test_auth_no_quorum.py
@@ -6,6 +6,7 @@
 
 import asyncio
 import time
+from test.pylib.internal_types import ServerInfo
 from test.pylib.manager_client import ManagerClient
 import pytest
 from cassandra.auth import PlainTextAuthProvider
@@ -55,3 +56,45 @@ async def test_auth_no_quorum(manager: ManagerClient) -> None:
             auth_provider=PlainTextAuthProvider(username=role, password=role))
     names = [row.role for row in await manager.get_cql().run_async(f"LIST ROLES", execution_profile='whitelist')]
     assert set(names) == set(roles)
+
+
+async def trigger_snapshot(manager: ManagerClient, server: ServerInfo) -> None:
+    cql = manager.get_cql()
+    host = cql.cluster.metadata.get_host(server.ip_addr)
+    group0_id = (await cql.run_async(
+        "select value from system.scylla_local where key = 'raft_group0_id'",
+        host=host))[0].value
+    await manager.api.client.post(f"/raft/trigger_snapshot/{group0_id}", host=server.ip_addr)
+
+
+"""
+Tests raft snapshot transfer of auth data.
+"""
+@pytest.mark.asyncio
+async def test_auth_raft_snapshot_transfer(manager: ManagerClient) -> None:
+    servers = await manager.servers_add(1)
+
+    cql = manager.get_cql()
+    await wait_for_cql_and_get_hosts(cql, servers, time.time() + 60)
+    await manager.servers_see_each_other(servers)
+
+    roles = ["ro" + unique_name() for _ in range(10)]
+    for role in roles:
+        # if not exists due to https://github.com/scylladb/python-driver/issues/296
+        await cql.run_async(f"CREATE ROLE IF NOT EXISTS {role}")
+
+    await trigger_snapshot(manager, servers[0])
+
+    # on startup node should receive snapshot
+    snapshot_receiving_server = await manager.server_add()
+    servers.append(snapshot_receiving_server)
+    await wait_for_cql_and_get_hosts(cql, servers, time.time() + 60)
+    await manager.servers_see_each_other(servers)
+
+    # simulate lost quorum but snapshot with data should have been transferred already
+    await manager.server_stop_gracefully(servers[0].server_id)
+
+    await manager.driver_connect(server=snapshot_receiving_server)
+    cql = manager.get_cql()
+    names = [row.role for row in await cql.run_async(f"LIST ROLES", execution_profile='whitelist')]
+    assert(set(names) == set(['cassandra'] + roles))
