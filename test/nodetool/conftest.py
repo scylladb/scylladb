764e9a344d691567ff9e33c0483c970198ecc1de
Botond Dénes <bdenes@scylladb.com>
2024-03-07 05:52:16 -0500

    test/nodetool: nodetool(): add check_return_code param
    When set to false, the returncode is not checked, this is left to the
    caller. This in turn allows for checking the expected and unexpected
    requests which is not checked when the nodetool process fails.
    This is used by utils._do_check_nodetool_fails_with(), so that expected
    and unexpected requests are checked even for failed invocations.
    
    Some test need adjustment to the stricter checks.

--- test/nodetool/conftest.py
+++ test/nodetool/conftest.py
@@ -191,7 +191,7 @@ def cassandra_only(request):
 
 @pytest.fixture(scope="module")
 def nodetool(request, jmx, nodetool_path, rest_api_mock_server):
-    def invoker(method, *args, expected_requests=None):
+    def invoker(method, *args, expected_requests=None, check_return_code=True):
         with rest_api_mock.expected_requests(rest_api_mock_server, expected_requests or []):
             if request.config.getoption("nodetool") == "scylla":
                 api_ip, api_port = rest_api_mock_server
@@ -213,7 +213,8 @@ def nodetool(request, jmx, nodetool_path, rest_api_mock_server):
             unexpected_requests = rest_api_mock.get_unexpected_requests(rest_api_mock_server)
 
             # Check the return-code first, if the command failed probably not all requests were consumed
-            res.check_returncode()
+            if check_return_code:
+                res.check_returncode()
             assert len(expected_requests) == 0, ''.join(str(r) for r in expected_requests)
             assert unexpected_requests == 0
 
