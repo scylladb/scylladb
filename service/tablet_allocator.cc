bhalevy/scylla

migration_manager: notify before_drop_column_family before dropping views
Call the before_drop_column_family notifications
before dropping the views to allow the tablet_allocator
to delete the view's tablets.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

service/tablet_allocator.cc
@@ -1148,6 +1148,7 @@ public:
         auto rs = abstract_replication_strategy::create_replication_strategy(ksm.strategy_name(), params);
         if (auto&& tablet_rs = rs->maybe_as_tablet_aware()) {
             auto tm = _db.get_shared_token_metadata().get();
+            lblogger.info("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
             auto map = tablet_rs->allocate_tablets_for_new_table(s.shared_from_this(), tm, _config.initial_tablets_scale).get();
             muts.emplace_back(tablet_map_to_mutation(map, s.id(), s.keypace_name(), s.cf_name(), ts).get());
         }
@@ -1158,6 +1159,7 @@ public:
         auto&& rs = ks.get_replication_strategy();
         if (rs.uses_tablets()) {
             auto tm = _db.get_shared_token_metadata().get();
+            lblogger.info("Dropping tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
             muts.emplace_back(make_drop_tablet_map_mutation(s.id(), ts));
         }
     }
@@ -1166,6 +1168,7 @@ public:
         keyspace& ks = _db.find_keyspace(keyspace_name);
         auto&& rs = ks.get_replication_strategy();
         if (rs.uses_tablets()) {
+            lblogger.info("Dropping tablets for keyspace {}", keyspace_name);
             auto tm = _db.get_shared_token_metadata().get();
             for (auto&& [name, s] : ks.metadata()->cf_meta_data()) {
                 muts.emplace_back(make_drop_tablet_map_mutation(s->id(), ts));

scylladb/scylladb

service/tablet_allocator.cc
@@ -1148,6 +1148,7 @@ public:
         auto rs = abstract_replication_strategy::create_replication_strategy(ksm.strategy_name(), params);
         if (auto&& tablet_rs = rs->maybe_as_tablet_aware()) {
             auto tm = _db.get_shared_token_metadata().get();
+            lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
             auto map = tablet_rs->allocate_tablets_for_new_table(s.shared_from_this(), tm, _config.initial_tablets_scale).get();
             muts.emplace_back(tablet_map_to_mutation(map, s.id(), s.keypace_name(), s.cf_name(), ts).get());
         }
@@ -1158,6 +1159,7 @@ public:
         auto&& rs = ks.get_replication_strategy();
         if (rs.uses_tablets()) {
             auto tm = _db.get_shared_token_metadata().get();
+            lblogger.debug("Dropping tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
             muts.emplace_back(make_drop_tablet_map_mutation(s.id(), ts));
         }
     }
@@ -1166,6 +1168,7 @@ public:
         keyspace& ks = _db.find_keyspace(keyspace_name);
         auto&& rs = ks.get_replication_strategy();
         if (rs.uses_tablets()) {
+            lblogger.debug("Dropping tablets for keyspace {}", keyspace_name);
             auto tm = _db.get_shared_token_metadata().get();
             for (auto&& [name, s] : ks.metadata()->cf_meta_data()) {
                 muts.emplace_back(make_drop_tablet_map_mutation(s->id(), ts));