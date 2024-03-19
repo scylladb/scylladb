e26b0f34a0e3d9121eacb05e8c0d45cb45e26f0a
Gleb Natapov <gleb@scylladb.com>
2024-03-19 13:34:33 +0200

    raft topology: update rack/dc info in topology state on reboot if changed
    It is allowed to change a snitch after cluster is already running.
    Changing a snitch may cause dc and/or rack names to be changed and
    gossiper handles it by gossiping new names on restart. The patch changes
    raft mode to update the names on restart as well.

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -1193,7 +1193,9 @@ future<> storage_service::update_topology_with_local_metadata(raft::server& raft
         return replica_state.shard_count == local_shard_count
             && replica_state.ignore_msb == local_ignore_msb
             && replica_state.release_version == local_release_version
-            && replica_state.supported_features == local_supported_features;
+            && replica_state.supported_features == local_supported_features
+            && replica_state.datacenter == _snitch.local()->get_datacenter()
+            && replica_state.rack == _snitch.local()->get_rack();
     };
 
     // We avoid performing a read barrier if we're sure that our metadata stored in topology
@@ -1244,7 +1246,10 @@ future<> storage_service::update_topology_with_local_metadata(raft::server& raft
                .set("shard_count", local_shard_count)
                .set("ignore_msb", local_ignore_msb)
                .set("release_version", local_release_version)
-               .set("supported_features", local_supported_features);
+               .set("supported_features", local_supported_features)
+               .set("datacenter", _snitch.local()->get_datacenter())
+               .set("rack", _snitch.local()->get_rack());
+
         topology_change change{{builder.build()}};
         group0_command g0_cmd = _group0->client().prepare_command(
                 std::move(change), guard, ::format("{}: update topology with local metadata", raft_server.id()));
