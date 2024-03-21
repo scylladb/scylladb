2ca4b0a7a2fc1d691042fd1c6d2a538f477645eb
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-03-21 18:19:23 +0530

    sstables_manager: store available memory size
    The available memory size is required to calculate the reclaim memory
    threshold, so store that within the sstables manager.
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- sstables/sstables_manager.hh
+++ sstables/sstables_manager.hh
@@ -79,6 +79,7 @@ class sstables_manager {
             boost::intrusive::constant_time_size<false>>;
 private:
     storage_manager* _storage;
+    size_t _available_memory;
     db::large_data_handler& _large_data_handler;
     const db::config& _db_config;
     gms::feature_service& _features;
