59d49c5219c6dab24a8655506597a73b25926d2a
Dawid Medrek <dawid.medrek@scylladb.com>
2024-03-22 13:18:24 +0100

    db/hints: Coroutinize space_watchdog::scan_one_ep_dir()

--- db/hints/resource_manager.cc
+++ db/hints/resource_manager.cc
@@ -92,27 +92,23 @@ future<> space_watchdog::stop() noexcept {
 
 // Called under the end_point_hints_manager::file_update_mutex() of the corresponding end_point_hints_manager instance.
 future<> space_watchdog::scan_one_ep_dir(fs::path path, manager& shard_manager, endpoint_id ep_key) {
-    return do_with(std::move(path), [this, ep_key, &shard_manager] (fs::path& path) {
-        // It may happen that we get here and the directory has already been deleted in the context of manager::drain_for().
-        // In this case simply bail out.
-        return file_exists(path.native()).then([this, ep_key, &shard_manager, &path] (bool exists) {
-            if (!exists) {
-                return make_ready_future<>();
-            } else {
-                return lister::scan_dir(path, lister::dir_entry_types::of<directory_entry_type::regular>(), [this, ep_key, &shard_manager] (fs::path dir, directory_entry de) {
-                    // Put the current end point ID to state.eps_with_pending_hints when we see the second hints file in its directory
-                    if (_files_count == 1) {
-                        shard_manager.add_ep_with_pending_hints(ep_key);
-                    }
-                    ++_files_count;
+    // It may happen that we get here and the directory has already been deleted in the context of manager::drain_for().
+    // In this case simply bail out.
+    if (!co_await file_exists(path.native())) {
+        co_return;
+    }
 
-                    return io_check(file_size, (dir / de.name.c_str()).c_str()).then([this] (uint64_t fsize) {
-                        _total_size += fsize;
-                    });
-                });
-            }
-        });
-    });
+    co_await lister::scan_dir(path, lister::dir_entry_types::of<directory_entry_type::regular>(),
+            coroutine::lambda([this, ep_key, &shard_manager] (fs::path dir, directory_entry de) -> future<> {
+        // Put the current end point ID to state.eps_with_pending_hints when we see the second hints file in its directory
+        if (_files_count == 1) {
+            shard_manager.add_ep_with_pending_hints(ep_key);
+        }
+        ++_files_count;
+
+        const auto filename = (std::move(dir) / std::move(de.name)).native();
+        _total_size += co_await io_check(file_size, filename);
+    }));
 }
 
 // Called from the context of a seastar::thread.
