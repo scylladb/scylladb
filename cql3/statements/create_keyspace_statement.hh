0adee264be2ac2fe5b0eab2a8f97973e1cccc70d
Kurashkin Nikita <jager822@yandex.ru>
2024-03-23 18:11:26 +0300

    Implement support for optional replication strategy and replication factor properties in CREATE KEYSPACE statements Things to change: presumably change default replication strategy and/or replication factor
    Fixes #16028, #16029

--- cql3/statements/create_keyspace_statement.hh
+++ cql3/statements/create_keyspace_statement.hh
@@ -51,6 +51,8 @@ public:
      */
     create_keyspace_statement(const sstring& name, shared_ptr<ks_prop_defs> attrs, bool if_not_exists);
 
+    create_keyspace_statement(const sstring& name, bool if_not_exists);
+
     virtual bool has_keyspace() const override {
         return true;
     }
