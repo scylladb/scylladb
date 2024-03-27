1a5d3ff94200f08eb9260ea78b8e4e30b5d421ee
Botond Dénes <bdenes@scylladb.com>
2024-03-27 11:39:27 -0400

    tools/scylla-nodetool: implement the checkAndRepairCdcStreams command

---
+++ test/nodetool/test_check_and_repair_cdc_streams.py
@@ -0,0 +1,13 @@
+#
+# Copyright 2024-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+
+from rest_api_mock import expected_request
+
+
+def test_checkandrepaircdcstreams(nodetool):
+    nodetool("checkAndRepairCdcStreams", expected_requests=[
+        expected_request("POST", "/storage_service/cdc_streams_check_and_repair")
+    ])
