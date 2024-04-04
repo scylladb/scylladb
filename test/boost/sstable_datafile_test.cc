c15e72695da1d6928360cb3672bc64768e0b7b42
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-04-04 18:32:47 +0530

    sstable_datafile_test: add test to verify reclaimed components reload
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
    (cherry picked from commit 69b2a127b0345dbbc5ead4509515877dc3fe0ea3)

--- test/boost/sstable_datafile_test.cc
+++ test/boost/sstable_datafile_test.cc
@@ -3494,12 +3494,21 @@ SEASTAR_TEST_CASE(test_sstable_reclaim_memory_from_components) {
             // create a bloom filter
             auto sst_test = sstables::test(sst);
             sst_test.create_bloom_filter(100);
+            sst_test.write_filter();
             auto total_reclaimable_memory = sst_test.total_reclaimable_memory_size();
 
             // Test sstable::reclaim_memory_from_components() :
             BOOST_REQUIRE_EQUAL(sst_test.reclaim_memory_from_components(), total_reclaimable_memory);
+            // No more memory to reclaim in the sstable
             BOOST_REQUIRE_EQUAL(sst_test.total_reclaimable_memory_size(), 0);
             BOOST_REQUIRE_EQUAL(sst->filter_memory_size(), 0);
+
+            // Test sstable::reload_reclaimed_components() :
+            // Reloading should load the bloom filter back into memory
+            sst_test.reload_reclaimed_components();
+            // SSTable should have reclaimable memory from the bloom filter
+            BOOST_REQUIRE_EQUAL(sst_test.total_reclaimable_memory_size(), total_reclaimable_memory);
+            BOOST_REQUIRE_EQUAL(sst->filter_memory_size(), total_reclaimable_memory);
         });
     });
 }
