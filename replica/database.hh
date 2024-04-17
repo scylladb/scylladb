ba58b71eea7832676bc4f97e14b19afcd4ed48f6
Pavel Emelyanov <xemul@scylladb.com>
2024-04-17 16:00:50 +0300

    database: Keep local directory_semaphore to initialize sstables managers
    Now database is constructed with sharded<directory_semaphore>, but it no
    longer needs sharded, local is enough.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- replica/database.hh
+++ replica/database.hh
@@ -1599,7 +1599,7 @@ public:
     future<> parse_system_tables(distributed<service::storage_proxy>&, sharded<db::system_keyspace>&);
 
     database(const db::config&, database_config dbcfg, service::migration_notifier& mn, gms::feature_service& feat, const locator::shared_token_metadata& stm,
-            compaction_manager& cm, sstables::storage_manager& sstm, wasm::manager& wasm, sharded<sstables::directory_semaphore>& sst_dir_sem, utils::cross_shard_barrier barrier = utils::cross_shard_barrier(utils::cross_shard_barrier::solo{}) /* for single-shard usage */);
+            compaction_manager& cm, sstables::storage_manager& sstm, wasm::manager& wasm, sstables::directory_semaphore& sst_dir_sem, utils::cross_shard_barrier barrier = utils::cross_shard_barrier(utils::cross_shard_barrier::solo{}) /* for single-shard usage */);
     database(database&&) = delete;
     ~database();
 
