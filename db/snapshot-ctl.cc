67a408447f14890fd3d583e6f1f5bf48fd4bfa8b
Pavel Emelyanov <xemul@scylladb.com>
2024-04-19 21:06:06 +0300

    snapshot_ctl: Brush up true_snapshots_size() internals
    Previous patches broke indentation in this method. Fix it by shortening
    the summation loop with the help of std::accumulate()
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- db/snapshot-ctl.cc
+++ db/snapshot-ctl.cc
@@ -120,9 +120,7 @@ future<int64_t> snapshot_ctl::true_snapshots_size() {
     co_return co_await run_snapshot_list_operation(coroutine::lambda([this] () -> future<int64_t> {
         int64_t total = 0;
         for (auto& [name, details] : co_await _db.local().get_snapshot_details()) {
-          for (auto& r : details) {
-            total += r.details.live;
-          }
+            total += std::accumulate(details.begin(), details.end(), int64_t(0), [] (int64_t sum, const auto& d) { return sum + d.details.live; });
         }
         co_return total;
     }));
