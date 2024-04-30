b6068cdb45191d3f5bc958f15487974c1a3efe46
Piotr Smaron <piotr.smaron@scylladb.com>
2024-04-30 14:10:05 +0200

    Return response only when tablets are reallocated
    Up until now we waited until mutations are in place and then returned
    directly to the caller of the ALTER statement, but that doesn't imply
    that tablets were deleted/created, so we must wait until the whole
    processing is done and return only then.
    
    (cherry picked from commit 39181c4bf2648e30fab575472c1b65e968d0f247)

--- cql3/query_processor.cc
+++ cql3/query_processor.cc
@@ -1039,6 +1039,7 @@ query_processor::execute_schema_statement(const statements::schema_altering_stat
             co_await remote_.get().mm.announce<service::topology_change>(std::move(m), std::move(*guard), description);
             // TODO: eliminate timeout from alter ks statement on the cqlsh/driver side
             auto error = co_await remote_.get().ss.wait_for_topology_request_completion(request_id);
+            co_await remote_.get().ss.wait_for_topology_not_busy();
             if (!error.empty()) {
                 log.error("CQL statement \"{}\" with topology request_id \"{}\" failed with error: \"{}\"", stmt.raw_cql_statement, request_id, error);
                 throw exceptions::request_execution_exception(exceptions::exception_code::INVALID, error);
