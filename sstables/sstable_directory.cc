608762adda3b9f8f2c7af4377f014bfc19589717
Pavel Emelyanov <xemul@scylladb.com>
2024-05-02 13:07:39 +0300

    sstable_directory: Create sstable path with make_path() when logging
    The sstable_directory::sstable_filename() should generate a name of an
    sstable for log messages. It's not accurate, because it silently assumes
    that the filename is on local storage, which might not be the case.
    Fixing it is large chage, so for now replace _sstable_dir with explicit
    call to make_path(). The change is idempotent, as _sstable_dir is
    initialized with the result of make_path() call in constructor.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- sstables/sstable_directory.cc
+++ sstables/sstable_directory.cc
@@ -227,7 +227,7 @@ sstable_directory::sort_sstable(sstables::entry_descriptor desc, process_flags f
 }
 
 sstring sstable_directory::sstable_filename(const sstables::entry_descriptor& desc) const {
-    return sstable::filename(_sstable_dir.native(), _schema->ks_name(), _schema->cf_name(), desc.version, desc.generation, desc.format, component_type::Data);
+    return sstable::filename(make_path(_table_dir, _state).native(), _schema->ks_name(), _schema->cf_name(), desc.version, desc.generation, desc.format, component_type::Data);
 }
 
 generation_type
