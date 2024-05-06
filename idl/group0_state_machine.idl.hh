bd4b781dc8d97ae45bed2f853dacc0a8b9875a65
Piotr Smaron <piotr.smaron@scylladb.com>
2024-05-06 18:17:32 +0200

    New raft cmd for both schema & topo changes
    Allows executing combined topology & schema mutations under a single RAFT command

--- idl/group0_state_machine.idl.hh
+++ idl/group0_state_machine.idl.hh
@@ -27,12 +27,16 @@ struct topology_change {
     std::vector<canonical_mutation> mutations;
 };
 
+struct mixed_change {
+    std::vector<canonical_mutation> mutations;
+};
+
 struct write_mutations {
     std::vector<canonical_mutation> mutations;
 };
 
 struct group0_command {
-    std::variant<service::schema_change, service::broadcast_table_query, service::topology_change, service::write_mutations> change;
+    std::variant<service::schema_change, service::broadcast_table_query, service::topology_change, service::write_mutations, service::mixed_change> change;
     canonical_mutation history_append;
 
     std::optional<utils::UUID> prev_state_id;
