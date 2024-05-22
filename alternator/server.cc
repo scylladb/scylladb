44e036c53ce149eb01e1b851781174cbceae1273
Nadav Har'El <nyh@scylladb.com>
2024-05-22 16:06:53 +0300

    alternator: fix "/localnodes" to use broadcast_rpc_address
    Alternator's non-standard "/localnodes" HTTP request returns a list of
    live nodes on this DC, to consider for load balancing. The returned
    node addresses should be external IP addresses usable by the clients.
    Scylla has a configuration parameter - broadcast_rpc_address - which
    defines for a node an external IP address. If such a configuration
    exists, we need to use those external IP addresses, not the internal
    ones.
    
    Finding these broadcast_rpc_address of all nodes is easy, because the
    gossiper already gossips them.
    
    This patch also tests the new feature:
    1. The existing single-node test is extended to verify that without
       broadcast_rpc_address we get the usual IP address.
    2. A new two-node test is added to check that when broadcast_rpc_address
       is configured, we get that address and not the usual internal IP
       addresses.
    
    Fixes #18711.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- alternator/server.cc
+++ alternator/server.cc
@@ -211,7 +211,9 @@ protected:
         std::unordered_set<gms::inet_address> local_dc_nodes = topology.get_datacenter_endpoints().at(local_dc);
         for (auto& ip : local_dc_nodes) {
             if (_gossiper.is_alive(ip)) {
-                rjson::push_back(results, rjson::from_string(fmt::to_string(ip)));
+                // Use the gossiped broadcast_rpc_address if available instead
+                // of the internal IP address "ip". See discussion in #18711.
+                rjson::push_back(results, rjson::from_string(_gossiper.get_rpc_address(ip)));
             }
         }
         rep->set_status(reply::status_type::ok);
--- test/alternator/test_scylla.py
+++ test/alternator/test_scylla.py
@@ -8,11 +8,12 @@
 import pytest
 import requests
 import json
+import urllib.parse
 
 # Test that the "/localnodes" request works, returning at least the one node.
-# TODO: A more through test would need to start a cluster with multiple nodes
-# in multiple data centers, and check that we can get a list of nodes in each
-# data center. But this test framework cannot yet test that.
+# See more elaborate tests for /localnodes, requiring multiple nodes,
+# datacenters, or different configurations, in
+# test_topology_experimental_raft/test_alternator.py
 def test_localnodes(scylla_only, dynamodb):
     url = dynamodb.meta.client._endpoint.host
     response = requests.get(url + '/localnodes', verify=False)
@@ -20,3 +21,10 @@ def test_localnodes(scylla_only, dynamodb):
     j = json.loads(response.content.decode('utf-8'))
     assert isinstance(j, list)
     assert len(j) >= 1
+    # The host we used to reach Alternator ("url") should normally be one
+    # of the nodes returned by /localnodes. In theory, they may be different
+    # addresses or aliases of the same node, but in practice, in all ways
+    # we normally run this test, it will be the same address.
+    # We have a separate test, test_localnodes_broadcast_rpc_address, on how
+    # this changes if "broadcast_rpc_address" is configured.
+    assert urllib.parse.urlparse(url).hostname in j
