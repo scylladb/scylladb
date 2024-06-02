8c97f88274eb356c67f3cf74a50043640aafcd1b
Yaron Kaikov <yaronkaikov@scylladb.com>
2024-06-02 11:16:09 +0300

    script/pull_github_pr.sh: merge PR with single commit
    Today for a single commit PR we do a rebase to next using pull_github_pr.sh script rather than merge, making our PR closed instead of merging, which means we don't have a merge commit in GitHub API (Mergify is looking for this value)
    
    So instead of doing rebase, let's merge the single commit and add the `Closes` prefix as a git note (this will apply for both single commit and multiple commits PR).
    
    This will allow us to have the proper reference with `git log` but the PR will be merged instead of closed when the commit is promoted
    
    Fixes: https://github.com/scylladb/scylladb/issues/18973

--- scripts/pull_github_pr.sh
+++ scripts/pull_github_pr.sh
@@ -72,23 +72,13 @@ nr_commits=$(git log --pretty=oneline HEAD..FETCH_HEAD | wc -l)
 closes="${NL}${NL}Closes ${PROJECT}#${PR_NUM}${NL}"
 
 if [[ $nr_commits == 1 ]]; then
-	commit=$(git log --pretty=oneline HEAD..FETCH_HEAD | awk '{print $1}')
-	message="$(git log -1 "$commit" --format="format:%s%n%n%b")"
-	if ! git cherry-pick $commit
-	then
-		echo "Cherry-pick failed. You are now in a subshell. Either resolve with git cherry-pick --continue or git cherry-pick --abort, then exit the subshell"
-		head_before=$(git rev-parse HEAD)
-		bash
-		head_after=$(git rev-parse HEAD)
-		if [[ "$head_before" = "$head_after" ]]; then
-			exit 1
-		fi
-	fi
-	git commit --amend -m "${message}${closes}"
+	git merge --no-squash FETCH_HEAD
 else
-	git merge --no-ff --log=1000 FETCH_HEAD -m "Merge '$PR_TITLE' from $USER_NAME" -m "${PR_DESCR}${closes}"
+	git merge --no-ff --log=1000 FETCH_HEAD -m "Merge '$PR_TITLE' from $USER_NAME" -m "${PR_DESCR}"
+	git commit --amend # for a manual double-check
 fi
-git commit --amend # for a manual double-check
+
+git notes add -f -m "${closes}"
 
 # Check PR tests status
 PR_HEAD_SHA=$(jq -r .head.sha <<< $PR_DATA)
