b824e73418ec73de670a6293436a448cfc2c588d
Michael Litvak <michael.litvak@scylladb.com>
2024-06-10 09:10:29 +0200

    db/hints: rename sync point structures with _v1 suffix to _v1_v2
    rename sync point types and variables to have v1/v2 suffix according to
    their use.

--- idl/hinted_handoff.idl.hh
+++ idl/hinted_handoff.idl.hh
@@ -18,20 +18,20 @@ namespace hints {
 
 // Contains per-endpoint and per-shard information about replay positions
 // for a particular type of hint queues (regular mutation hints or MV update hints)
-struct per_manager_sync_point_v1 {
+struct per_manager_sync_point_v1_or_v2 {
     std::vector<gms::inet_address> addresses;
     std::vector<db::replay_position> flattened_rps;
 };
 
-struct sync_point_v1 {
+struct sync_point_v1_or_v2 {
     locator::host_id host_id;
     uint16_t shard_count;
 
     // Sync point information for regular mutation hints
-    db::hints::per_manager_sync_point_v1 regular_sp;
+    db::hints::per_manager_sync_point_v1_or_v2 regular_sp;
 
     // Sync point information for materialized view hints
-    db::hints::per_manager_sync_point_v1 mv_sp;
+    db::hints::per_manager_sync_point_v1_or_v2 mv_sp;
 };
 
 }
