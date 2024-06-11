5811df4d4bc69460ec000d18dca3ef899fd735fc
Pavel Emelyanov <xemul@scylladb.com>
2024-06-11 11:56:40 +0300

    config: Mark tablets feature as unused
    This features used to be there for a while, but then it was removed by
    83d491af025951da6d5994ed4164a3290e147d31. This patch partially takes it
    back, but maps to UNUSED, so that if met in config, it's warned, but
    other features are parsed as well.
    
    refs: #18968
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>
    (cherry picked from commit b2520b8185d8b4c40a72ea467237de0231f9ffea)

--- db/config.cc
+++ db/config.cc
@@ -1348,6 +1348,7 @@ std::map<sstring, db::experimental_features_t::feature> db::experimental_feature
         {"consistent-topology-changes", feature::UNUSED},
         {"broadcast-tables", feature::BROADCAST_TABLES},
         {"keyspace-storage-options", feature::KEYSPACE_STORAGE_OPTIONS},
+        {"tablets", feature::UNUSED},
     };
 }
 
