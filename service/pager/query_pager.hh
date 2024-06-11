c1fcd6b247ac076e9b5f1bfa5dd2cde76fdc9379
Łukasz Paszkowski <lukasz.paszkowski@scylladb.com>
2024-06-11 13:17:11 +0200

    select_statement: Execute reversed query in native format
    Use a reversed schema and a native reversed slice when constructing
    a read_command and executing a reversed select statement.
    
    Such a created read_command is passed further down to query_pagers::pager
    and storage::proxy::query_result that transform it to the format
    they accept/know, i.e. lagacy.

--- service/pager/query_pager.hh
+++ service/pager/query_pager.hh
@@ -87,7 +87,7 @@ protected:
     query_function _query_function;
 
 public:
-    query_pager(service::storage_proxy& p, schema_ptr s, shared_ptr<const cql3::selection::selection> selection,
+    query_pager(service::storage_proxy& p, schema_ptr query_schema, shared_ptr<const cql3::selection::selection> selection,
                 service::query_state& state,
                 const cql3::query_options& options,
                 lw_shared_ptr<query::read_command> cmd,
