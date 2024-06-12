00895ab549c65b75ab3ba00b8dd4cc05b5f19393
Benny Halevy <bhalevy@scylladb.com>
2024-06-12 09:03:38 +0300

    hint_sender: use get_replicas rather get_natural_endpoints
    As part of scylladb/scylladb#6403, we would
    like to deprecate the use of `get_natural_endpoints`
    and friends and use `get_replicas` instead.
    
    do_send_one_mutation already validates the `end_point_key`
    which is the destination host_id and makes sure it
    can be mapped into an inet_address, so it would be
    just fine if that host already left the cluster
    (as in the case of get_replicas vs. get_natural_endpoints).
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- db/hints/internal/hint_sender.cc
+++ db/hints/internal/hint_sender.cc
@@ -76,15 +76,16 @@ future<timespec> hint_sender::get_last_file_modification(const sstring& fname) {
     });
 }
 
-future<> hint_sender::do_send_one_mutation(frozen_mutation_and_schema m, locator::effective_replication_map_ptr ermp, const inet_address_vector_replica_set& natural_endpoints) {
+future<> hint_sender::do_send_one_mutation(frozen_mutation_and_schema m, locator::effective_replication_map_ptr ermp, const host_id_vector_replica_set& natural_endpoints) {
     return futurize_invoke([this, m = std::move(m), ermp = std::move(ermp), &natural_endpoints] () mutable -> future<> {
         // The fact that we send with CL::ALL in both cases below ensures that new hints are not going
         // to be generated as a result of hints sending.
         const auto& tm = ermp->get_token_metadata();
-        const auto maybe_addr = tm.get_endpoint_for_host_id_if_known(end_point_key());
+        auto ep = end_point_key();
+        const auto maybe_addr = tm.get_endpoint_for_host_id_if_known(ep);
 
-        if (maybe_addr && boost::range::find(natural_endpoints, *maybe_addr) != natural_endpoints.end()) {
-            manager_logger.trace("Sending directly to {}", end_point_key());
+        if (maybe_addr && boost::range::find(natural_endpoints, ep) != natural_endpoints.end()) {
+            manager_logger.trace("Sending directly to {}/{}", end_point_key(), *maybe_addr);
             return _proxy.send_hint_to_endpoint(std::move(m), std::move(ermp), *maybe_addr);
         } else {
             manager_logger.trace("Endpoints set has changed and {} is no longer a replica. Mutating from scratch...", end_point_key());
@@ -276,7 +277,7 @@ void hint_sender::start() {
 future<> hint_sender::send_one_mutation(frozen_mutation_and_schema m) {
     auto erm = _db.find_column_family(m.s).get_effective_replication_map();
     auto token = dht::get_token(*m.s, m.fm.key());
-    inet_address_vector_replica_set natural_endpoints = erm->get_natural_endpoints(std::move(token));
+    auto natural_endpoints = erm->get_replicas(std::move(token));
 
     return do_send_one_mutation(std::move(m), std::move(erm), std::move(natural_endpoints));
 }
--- db/hints/internal/hint_sender.hh
+++ db/hints/internal/hint_sender.hh
@@ -22,7 +22,6 @@
 #include "db/commitlog/replay_position.hh"
 #include "db/hints/internal/common.hh"
 #include "db/hints/internal/hint_storage.hh"
-#include "gms/inet_address.hh"
 #include "locator/abstract_replication_strategy.hh"
 #include "mutation/frozen_mutation.hh"
 #include "schema/schema.hh"
@@ -242,7 +241,7 @@ private:
     /// \param ermp points to the effective_replication_map used to obtain \c natural_endpoints
     /// \param natural_endpoints current replicas for the given mutation
     /// \return future that resolves when the operation is complete
-    future<> do_send_one_mutation(frozen_mutation_and_schema m, locator::effective_replication_map_ptr ermp, const inet_address_vector_replica_set& natural_endpoints);
+    future<> do_send_one_mutation(frozen_mutation_and_schema m, locator::effective_replication_map_ptr ermp, const host_id_vector_replica_set& natural_endpoints);
 
     /// \brief Send one mutation out.
     ///
