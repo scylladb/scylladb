181213d9ea04377415ddd131a91a9506ae2a58fc
Botond Dénes <bdenes@scylladb.com>
2024-06-14 15:31:00 +0300

    [Backport 5.4] : Merge 'Fix usage of utils/chunked_vector::reserve_partial' from Lakshmi Narayanan Sreethar
    utils/chunked_vector::reserve_partial: fix usage in callers
    
    The method reserve_partial(), when used as documented, quits before the
    intended capacity can be reserved fully. This can lead to overallocation
    of memory in the last chunk when data is inserted to the chunked vector.
    The method itself doesn't have any bug but the way it is being used by
    the callers needs to be updated to get the desired behaviour.
    
    Instead of calling it repeatedly with the value returned from the
    previous call until it returns zero, it should be repeatedly called with
    the intended size until the vector's capacity reaches that size.
    
    This PR updates the method comment and all the callers to use the
    right way.
    
    Fixes #19254
    
    Closes scylladb/scylladb#19279
    
    * github.com:scylladb/scylladb:
      utils/large_bitset: remove unused includes identified by clangd
      utils/large_bitset: use thread::maybe_yield()
      test/boost/chunked_managed_vector_test: fix testcase tests_reserve_partial
      utils/lsa/chunked_managed_vector: fix reserve_partial()
      utils/chunked_vector: return void from reserve_partial and make_room
      test/boost/chunked_vector_test: fix testcase tests_reserve_partial
      utils/chunked_vector::reserve_partial: fix usage in callers
    
    (cherry picked from commit b2ebc172d05dcfe817282a6859d08574e45d2194)
    
    Backported from #19308 to 5.4

--- utils/large_bitset.cc
+++ utils/large_bitset.cc
@@ -7,31 +7,23 @@
  */
 
 #include "large_bitset.hh"
-#include <algorithm>
 #include <seastar/core/align.hh>
 #include <seastar/core/thread.hh>
-#include "seastarx.hh"
 
 using namespace seastar;
 
 large_bitset::large_bitset(size_t nr_bits) : _nr_bits(nr_bits) {
     assert(thread::running_in_thread());
 
-    const size_t orig_nr_ints = align_up(nr_bits, bits_per_int()) / bits_per_int();
-    auto nr_ints = orig_nr_ints;
-    while (nr_ints) {
-        nr_ints = _storage.reserve_partial(nr_ints);
-        if (need_preempt()) {
-            thread::yield();
-        }
+    size_t nr_ints = align_up(nr_bits, bits_per_int()) / bits_per_int();
+    while (_storage.capacity() != nr_ints) {
+        _storage.reserve_partial(nr_ints);
+        thread::maybe_yield();
     }
-    nr_ints = orig_nr_ints;
     while (nr_ints) {
         _storage.push_back(0);
         --nr_ints;
-        if (need_preempt()) {
-            thread::yield();
-        }
+        thread::maybe_yield();
     }
 }
 
@@ -40,8 +32,6 @@ large_bitset::clear() {
     assert(thread::running_in_thread());
     for (auto&& pos: _storage) {
         pos = 0;
-        if (need_preempt()) {
-            thread::yield();
-        }
+        thread::maybe_yield();
     }
 }
