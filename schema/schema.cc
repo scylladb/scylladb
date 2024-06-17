d27620e1462597e4c3cbbeb5df524e4528c85cf5
Calle Wilund <calle@scylladb.com>
2024-06-17 13:27:05 +0000

    schema_extensions: Add an option to string method
    Allow an extension to describe itself as the CQL property
    string that created it (and is serialized to schema tables)
    
    Only paxos extension requires override.

--- schema/schema.cc
+++ schema/schema.cc
@@ -801,6 +801,16 @@ std::ostream& operator<<(std::ostream& os, const schema& s) {
     return os;
 }
 
+// default impl assumes options are in a map.
+// implementations should override if not
+std::string schema_extension::options_to_string() const {
+    std::ostringstream ss;
+    ss << '{';
+    map_as_cql_param(ss, ser::deserialize_from_buffer(serialize(), boost::type<default_map_type>(), 0));
+    ss << '}';
+    return ss.str();
+}
+
 static std::ostream& column_definition_as_cql_key(std::ostream& os, const column_definition & cd) {
     os << cd.name_as_cql_string();
     os << " " << cd.type->cql3_type_name();
