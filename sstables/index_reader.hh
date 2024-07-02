64dadd5ec2aaece29293d4bf864dc1f10ca0accf
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-07-02 20:27:17 +0530

    sstables/index_reader: stop consuming index when abort has been requested
    When an abort is requested, stop further reading of the index file and
    throw and exception from index_consume_entry_context::process_state().
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- sstables/index_reader.hh
+++ sstables/index_reader.hh
@@ -186,6 +186,8 @@ public:
     }
 
     processing_result process_state(temporary_buffer<char>& data) {
+        _abort.check();
+
         auto current_pos = [&] { return this->position() - data.size(); };
         auto read_vint_or_uint64 = [this] (temporary_buffer<char>& data) {
             return is_mc_format() ? this->read_unsigned_vint(data) : this->read_64(data);
