603125dcc001bc4a80aed22139cfc09e948fa75b
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2024-07-09 19:50:54 +0200

    db: add class encapsulating schema merging
    This commit doesn't yet change how schema merging
    works but it prepares the ground for it.
    
    We split merging code into several functions.
    Main reasons for it are that:
    
    - We want to generalize and create some interface
    which each subsystem would use.
    
    - We need to pull mutation's apply() out
    of the code because raft will call it directly,
    and it will contain a mix of mutations from more
    than one subsystem. This is needed because we have
    the need to update multiple subsystems atomically
    (e.g. auth and schema during auto-grant when creating
    a table).
    
    In this commit do_merge_schema() code is split between
    prepare(), adjust(), update(), commit(), notify(). The idea
    behind each of these phases is described in comments. The
    last 2 phases are not yet implemented as it requires more
    code changes but adding schema_applier enclosing class
    will help to create some copied state in the future and
    implement commit() and notify() phases.

--- db/schema_applier.hh
+++ db/schema_applier.hh
@@ -33,6 +33,68 @@ future<> merge_schema(sharded<db::system_keyspace>& sys_ks, distributed<service:
 // of feature_service and schema tables.
 future<> recalculate_schema_version(sharded<db::system_keyspace>& sys_ks, distributed<service::storage_proxy>& proxy, gms::feature_service& feat);
 
+enum class table_kind { table, view };
+
+struct table_selector {
+    bool all_in_keyspace = false; // If true, selects all existing tables in a keyspace plus what's in "tables";
+    std::unordered_map<table_kind, std::unordered_set<sstring>> tables;
+
+    table_selector& operator+=(table_selector&& o);
+    void add(table_kind t, sstring name);
+    void add(sstring name);
+};
+
+struct schema_complete_view {
+    schema_tables::schema_result keyspaces;
+    std::map<table_id, schema_mutations> tables;
+    schema_tables::schema_result types;
+    std::map<table_id, schema_mutations> views;
+    schema_tables::schema_result functions;
+    schema_tables::schema_result aggregates;
+    schema_tables::schema_result scylla_aggregates;
+};
+
+class schema_applier {
+    using keyspace_name = sstring;
+
+    sharded<service::storage_proxy>& _proxy;
+    sharded<db::system_keyspace>& _sys_ks;
+    bool _reload;
+
+    std::set<sstring> _keyspaces;
+    std::unordered_map<keyspace_name, table_selector> _affected_tables;
+    bool _has_tablet_mutations = false;
+
+    schema_complete_view _before;
+    schema_complete_view _after;
+
+    future<schema_complete_view> get_schema_complete_view();
+public:
+    schema_applier(
+            sharded<service::storage_proxy>& proxy,
+            sharded<db::system_keyspace>& sys_ks,
+            bool reload = false)
+            : _proxy(proxy), _sys_ks(sys_ks), _reload(reload) {};
+
+    // Gets called before mutations are applied,
+    // preferably no work should be done here but subsystem
+    // may do some snapshot of 'before' data.
+    future<> prepare(const std::vector<mutation>& muts);
+    // Allows to modify mutations before they are applied,
+    // preferably no work is done here but sometimes we need to adjust
+    // for legacy things.
+    future<> adjust(std::vector<mutation>& muts);
+    // Update is called after mutations are applied, it should create
+    // all updates but not yet commit them to a subsystem (i.e. copy on write style).
+    future<> update(const std::vector<mutation>& muts);
+    // Makes updates visible, it should be something very simple like a pointer switch and not yield so that it can be atomically composed with other subsystems.
+    void commit();
+    // Notify is called after commit and allows to trigger code which can't provide
+    // atomicity either for legacy reasons or causes side effects to an external system
+    // (e.g. informing client's driver).
+    future<> notify();
+};
+
 }
 
 }
