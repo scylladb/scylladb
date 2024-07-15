5fb84e5231ef34fcdcd652b330cf2c478d5b87b5
Nadav Har'El <nyh@scylladb.com>
2024-07-15 14:22:38 +0300

    alternator: do not allow authentication with a non-"login" role
    Alternator allows authentication into the existing CQL roles, but
    roles which have the flag "login=false" should be refused in
    authentication, and this patch adds the missing check.
    
    The patch also adds a regression test for this feature in the
    test/alternator test framework, in a new test file
    test/alternator/cql_rbac.py. This test file will later include more
    tests of how the CQL RBAC commands (CREATE ROLE, GRANT, REVOKE)
    affect authentication and authorization in Alternator.
    In particular, these tests need to use not just the DynamoDB API but
    also CQL, so this new test file includes the "cql" fixture that allows
    us to run CQL commands, to create roles, to retrieve their secret keys,
    and so on.
    
    Fixes #19735
    
    (cherry picked from commit 14cd7b5095f9a2ed8b99cf1f0566a014c8708989)

--- alternator/auth.cc
+++ alternator/auth.cc
@@ -21,6 +21,7 @@
 #include "cql3/selection/selection.hh"
 #include "query-result-set.hh"
 #include "cql3/result_set.hh"
+#include "types/types.hh"
 #include <seastar/core/coroutine.hh>
 
 namespace alternator {
@@ -33,11 +34,12 @@ future<std::string> get_key_from_roles(service::storage_proxy& proxy, std::strin
     dht::partition_range_vector partition_ranges{dht::partition_range(dht::decorate_key(*schema, pk))};
     std::vector<query::clustering_range> bounds{query::clustering_range::make_open_ended_both_sides()};
     const column_definition* salted_hash_col = schema->get_column_definition(bytes("salted_hash"));
-    if (!salted_hash_col) {
+    const column_definition* can_login_col = schema->get_column_definition(bytes("can_login"));
+    if (!salted_hash_col || !can_login_col) {
         co_await coroutine::return_exception(api_error::unrecognized_client(format("Credentials cannot be fetched for: {}", username)));
     }
-    auto selection = cql3::selection::selection::for_columns(schema, {salted_hash_col});
-    auto partition_slice = query::partition_slice(std::move(bounds), {}, query::column_id_vector{salted_hash_col->id}, selection->get_query_options());
+    auto selection = cql3::selection::selection::for_columns(schema, {salted_hash_col, can_login_col});
+    auto partition_slice = query::partition_slice(std::move(bounds), {}, query::column_id_vector{salted_hash_col->id, can_login_col->id}, selection->get_query_options());
     auto command = ::make_lw_shared<query::read_command>(schema->id(), schema->version(), partition_slice,
             proxy.get_max_result_size(partition_slice), query::tombstone_limit(proxy.get_tombstone_limit()));
     auto cl = auth::password_authenticator::consistency_for_user(username);
@@ -53,7 +55,14 @@ future<std::string> get_key_from_roles(service::storage_proxy& proxy, std::strin
     if (result_set->empty()) {
         co_await coroutine::return_exception(api_error::unrecognized_client(format("User not found: {}", username)));
     }
-    const managed_bytes_opt& salted_hash = result_set->rows().front().front(); // We only asked for 1 row and 1 column
+    const auto& result = result_set->rows().front();
+    bool can_login = result[1] && value_cast<bool>(boolean_type->deserialize(*result[1]));
+    if (!can_login) {
+        // This is a valid role name, but has "login=False" so should not be
+        // usable for authentication (see #19735).
+        co_await coroutine::return_exception(api_error::unrecognized_client(format("Role {} has login=false so cannot be used for login", username)));
+    }
+    const managed_bytes_opt& salted_hash = result.front();
     if (!salted_hash) {
         co_await coroutine::return_exception(api_error::unrecognized_client(format("No password found for user: {}", username)));
     }
