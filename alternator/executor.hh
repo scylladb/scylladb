71227580416a65f1a14efe86c803d010d9c6d90c
Amnon Heiman <amnon@scylladb.com>
2024-07-21 13:55:30 +0300

    alternator/executor.hh: white-space cleanup
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>

--- alternator/executor.hh
+++ alternator/executor.hh
@@ -67,7 +67,7 @@ public:
  * (very) large objects as there are overhead issues with this
  * as well, but for massive lists of return objects this can
  * help avoid large allocations/many re-allocs
- */ 
+ */
 json::json_return_type make_streamed(rjson::value&&);
 
 struct json_string : public json::jsonable {
@@ -224,7 +224,7 @@ private:
     friend class rmw_operation;
 
     static void describe_key_schema(rjson::value& parent, const schema&, std::unordered_map<std::string,std::string> * = nullptr);
-    
+
 public:
     static void describe_key_schema(rjson::value& parent, const schema& schema, std::unordered_map<std::string,std::string>&);
 
