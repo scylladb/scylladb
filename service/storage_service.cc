78d7c953b01e242479d91c3b4dfbc0ec378ae1ea
Nadav Har'El <nyh@scylladb.com>
2024-07-29 01:24:54 +0300

    test: increase timeouts for /localnodes test
    In commit bac7c3331377ad7bb85524d295e438230d61001e we introduced a new
    test for the Alternator "/localnodes" request, checking that a node
    that is still joining does not get returned. The tests used what I
    thought were "very high" timeouts - we had a timeout of 10 seconds
    for starting a single node, and injected a 20 second sleep to leave
    us 10 seconds after the first sleep.
    
    But the test failed in one extremely slow run (a debug build on
    aarch64), where starting just a single node took more than 15 seconds!
    
    So in this patch I increase the timeouts significantly: We increase
    the wait for the node to 60 seconds, and the sleeping injection to
    120 seconds. These should definitely be enough for anyone (famous
    last words...).
    
    The test doesn't actually wait for these timeouts, so the ridiculously
    high timeouts shouldn't affect the normal runtime of this test.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    (cherry picked from commit ca8b91f641ac9a506a7f18604417507c06a10787)
    
    Closes scylladb/scylladb#19940

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -1782,7 +1782,7 @@ future<> storage_service::join_token_ring(sharded<db::system_distributed_keyspac
 
     set_mode(mode::JOINING);
 
-    co_await utils::get_local_injector().inject("delay_bootstrap_20s", std::chrono::seconds(20));
+    co_await utils::get_local_injector().inject("delay_bootstrap_120s", std::chrono::seconds(120));
 
     if (raft_server) { // Raft is enabled. Check if we need to bootstrap ourself using raft
         rtlogger.info("topology changes are using raft");
@@ -3822,7 +3822,7 @@ void storage_service::run_bootstrap_ops(std::unordered_set<token>& bootstrap_tok
         // Step 3: Prepare to sync data
         ctl.prepare(node_ops_cmd::bootstrap_prepare).get();
 
-        utils::get_local_injector().inject("delay_bootstrap_20s", std::chrono::seconds(20)).get();
+        utils::get_local_injector().inject("delay_bootstrap_120s", std::chrono::seconds(120)).get();
 
         // Step 5: Sync data for bootstrap
         _repair.local().bootstrap_with_repair(get_token_metadata_ptr(), bootstrap_tokens).get();
@@ -5501,7 +5501,7 @@ future<raft_topology_cmd_result> storage_service::raft_topology_cmd_handler(raft
                             if (!_topology_state_machine._topology.normal_nodes.empty()) { // stream only if there is a node in normal state
                                 co_await retrier(_bootstrap_result, coroutine::lambda([&] () -> future<> {
                                     if (is_repair_based_node_ops_enabled(streaming::stream_reason::bootstrap)) {
-                                        co_await utils::get_local_injector().inject("delay_bootstrap_20s", std::chrono::seconds(20));
+                                        co_await utils::get_local_injector().inject("delay_bootstrap_120s", std::chrono::seconds(120));
 
                                         co_await _repair.local().bootstrap_with_repair(get_token_metadata_ptr(), rs.ring.value().tokens);
                                     } else {
