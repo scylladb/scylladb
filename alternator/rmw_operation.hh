2bbb1d86b02b0fd4bc311f5f17bdfe2519c65e76
Amnon Heiman <amnon@scylladb.com>
2024-07-30 14:33:19 +0300

    Alternator: add WCU support
    This patch adds functionality to track Write Capacity Units (WCU).
    Currently for the put_item operation.
    
    This enhancement allows for standardized measurement of write
    operations, aligning with DynamoDB-like metrics.
    
    Additionally, the WCU value is now included in the response to provide
    immediate feedback on the write capacity usage.
    
    The implmenetation adds a consumed_capacity_counter member to
    rmw_operation, this will allow to add WCU functionality to update_item
    and delete_item

--- alternator/rmw_operation.hh
+++ alternator/rmw_operation.hh
@@ -15,6 +15,8 @@
 
 namespace alternator {
 
+class consumed_capacity;
+
 // An rmw_operation encapsulates the common logic of all the item update
 // operations which may involve a read of the item before the write
 // (so-called Read-Modify-Write operations). These operations include PutItem,
@@ -63,7 +65,7 @@ protected:
     partition_key _pk = partition_key::make_empty();
     clustering_key _ck = clustering_key::make_empty();
     write_isolation _write_isolation;
-
+    std::unique_ptr<consumed_capacity_counter> _consumed_capacity;
     // All RMW operations can have a ReturnValues parameter from the following
     // choices. But note that only UpdateItem actually supports all of them:
     enum class returnvalues {
