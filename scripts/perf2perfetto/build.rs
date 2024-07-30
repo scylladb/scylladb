bf657edfaf0309b05869c7ade596191f6329965e
Michał Chojnowski <michocho98@gmail.com>
2024-07-30 15:06:19 +0200

    scripts: add intel_pt_trace.py
    Adds a script which sets up a cluster (via test/pylib),
    runs something on it under Intel PT recording,
    and converts the resulting trace to the FTF format, viewable
    for example in Chromium's trace viewer (Perfetto).
    
    It can be used to explore how some things in Scylla are implemented,
    and how many instructions they take.
    (However, without additional setup, it's only suitable for short workloads,
    like a single query. Long operations, which, for example, rewrite system tables,
    are likely to produce a trace that's too big for the tooling to handle.

---
+++ scripts/perf2perfetto/build.rs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: Copyright 2024 Michał Chojnowski <michal.chojnowski@scylladb.com>
+
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=include/wrapper.h");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("include/wrapper.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
