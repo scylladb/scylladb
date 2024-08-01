0ea212814012b3828d0a06a711d51299d99103c1
Piotr Smaron <piotr.smaron@scylladb.com>
2024-08-01 14:35:36 +0200

    cql: refactor rf_change indentation

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -772,38 +772,38 @@ class topology_coordinator : public endpoint_lifecycle_subscriber {
                 std::vector<canonical_mutation> updates;
                 sstring error;
                 if (_db.has_keyspace(ks_name)) {
-                auto& ks = _db.find_keyspace(ks_name);
-                auto tmptr = get_token_metadata_ptr();
-                size_t unimportant_init_tablet_count = 2; // must be a power of 2
-                locator::tablet_map new_tablet_map{unimportant_init_tablet_count};
+                    auto& ks = _db.find_keyspace(ks_name);
+                    auto tmptr = get_token_metadata_ptr();
+                    size_t unimportant_init_tablet_count = 2; // must be a power of 2
+                    locator::tablet_map new_tablet_map{unimportant_init_tablet_count};
 
-                for (const auto& table : ks.metadata()->tables()) {
-                    try {
-                        locator::tablet_map old_tablets = tmptr->tablets().get_tablet_map(table->id());
-                        locator::replication_strategy_params params{repl_opts, old_tablets.tablet_count()};
-                        auto new_strategy = locator::abstract_replication_strategy::create_replication_strategy("NetworkTopologyStrategy", params);
-                        new_tablet_map = co_await new_strategy->maybe_as_tablet_aware()->reallocate_tablets(table, tmptr, old_tablets);
-                    } catch (const std::exception& e) {
-                        error = e.what();
-                        rtlogger.error("Couldn't process global_topology_request::keyspace_rf_change, error: {},"
-                                       "desired new ks opts: {}", error, new_ks_props.get_replication_options());
-                        updates.clear(); // remove all tablets mutations ...
-                        break;           // ... and only create mutations deleting the global req
+                    for (const auto& table : ks.metadata()->tables()) {
+                        try {
+                            locator::tablet_map old_tablets = tmptr->tablets().get_tablet_map(table->id());
+                            locator::replication_strategy_params params{repl_opts, old_tablets.tablet_count()};
+                            auto new_strategy = locator::abstract_replication_strategy::create_replication_strategy("NetworkTopologyStrategy", params);
+                            new_tablet_map = co_await new_strategy->maybe_as_tablet_aware()->reallocate_tablets(table, tmptr, old_tablets);
+                        } catch (const std::exception& e) {
+                            error = e.what();
+                            rtlogger.error("Couldn't process global_topology_request::keyspace_rf_change, error: {},"
+                                           "desired new ks opts: {}", error, new_ks_props.get_replication_options());
+                            updates.clear(); // remove all tablets mutations ...
+                            break;           // ... and only create mutations deleting the global req
+                        }
+
+                        replica::tablet_mutation_builder tablet_mutation_builder(guard.write_timestamp(), table->id());
+                        co_await new_tablet_map.for_each_tablet([&](locator::tablet_id tablet_id, const locator::tablet_info& tablet_info) -> future<> {
+                            auto last_token = new_tablet_map.get_last_token(tablet_id);
+                            updates.emplace_back(co_await make_canonical_mutation_gently(
+                                    replica::tablet_mutation_builder(guard.write_timestamp(), table->id())
+                                            .set_new_replicas(last_token, tablet_info.replicas)
+                                            .set_stage(last_token, locator::tablet_transition_stage::allow_write_both_read_old)
+                                            .set_transition(last_token, locator::tablet_transition_kind::rebuild)
+                                            .build()
+                            ));
+                            co_await coroutine::maybe_yield();
+                        });
                     }
-
-                    replica::tablet_mutation_builder tablet_mutation_builder(guard.write_timestamp(), table->id());
-                    co_await new_tablet_map.for_each_tablet([&](locator::tablet_id tablet_id, const locator::tablet_info& tablet_info) -> future<> {
-                        auto last_token = new_tablet_map.get_last_token(tablet_id);
-                        updates.emplace_back(co_await make_canonical_mutation_gently(
-                                replica::tablet_mutation_builder(guard.write_timestamp(), table->id())
-                                        .set_new_replicas(last_token, tablet_info.replicas)
-                                        .set_stage(last_token, locator::tablet_transition_stage::allow_write_both_read_old)
-                                        .set_transition(last_token, locator::tablet_transition_kind::rebuild)
-                                        .build()
-                        ));
-                        co_await coroutine::maybe_yield();
-                    });
-                }
                 } else {
                     error = "Can't ALTER keyspace " + ks_name + ", keyspace doesn't exist";
                 }
