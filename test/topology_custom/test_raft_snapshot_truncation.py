2cf0014208f1630b32efcda0d4e7cf4405b30012
Sergey Zolotukhin <sergey.zolotukhin@scylladb.com>
2024-08-09 16:11:07 +0200

    raft: Invoke store_snapshot_descriptor with actually preserved items instead of _config.snapshot_trailing value; Remove incorrect check for truncated number of items in store_snapshot_descriptor. Fixes scylladb#16817 Fixes scylladb#20080

--- test/topology_custom/test_raft_snapshot_truncation.py
+++ test/topology_custom/test_raft_snapshot_truncation.py
@@ -29,7 +29,6 @@ async def get_raft_snap_id(cql, host) -> str:
 
 
 @pytest.mark.asyncio
-@pytest.mark.xfail(reason="issue ##16817")
 async def test_raft_snapshot_truncation(manager: ManagerClient):
     # Check that after snapshot creation, snapshot_trailing_size is taken in to consideration,
     # issue https://github.com/scylladb/scylladb/issues/16817
@@ -40,6 +39,12 @@ async def test_raft_snapshot_truncation(manager: ManagerClient):
     #    desired number of log entries after log trancation to 0
     # 3. Generate a schema change event
     # 4. Check that truncated log contains 0 entries
+    #
+    # Check regression:
+    # 1. Set snapshot threshold to 2
+    # 2. Generate 2 schema change events
+    # 3. Trigger snapshot creation by setting snapshot threshold to 0
+    # 4. Check that truncated log contains 2 entries
 
     cmdline = [
         '--logger-log-level', 'raft=trace',
@@ -74,3 +79,31 @@ async def test_raft_snapshot_truncation(manager: ManagerClient):
 
     # Verify that after snapshot was created, log size is 0 due to setting 'snapshot_trailing_size': '0'
     assert (log_size == 0)
+
+    # Now test, that after truncation the records are preserved if snapshot_trailing higher than zero
+
+    # Set snapshot thresholds to a higher level
+    errs = [inject_error_one_shot(manager.api, s.ip_addr, "raft_server_set_snapshot_thresholds",
+                                  parameters={'snapshot_threshold': '1024', 'snapshot_threshold_log_size': '2000000',
+                                              'snapshot_trailing': '2', 'snapshot_trailing_size': '2000000'})
+            for s in servers]
+    await asyncio.gather(*errs)
+
+    await cql.run_async("create keyspace ks with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}")
+
+    log_size = await get_raft_log_size(cql, h1)
+    logger.info(f"After add table Log size on {s1}: {log_size}")
+
+    # Set snapshot thresholds to 0 to trigger on the next schema change
+    errs = [inject_error_one_shot(manager.api, s.ip_addr, "raft_server_set_snapshot_thresholds",
+                                  parameters={'snapshot_threshold': '0', 'snapshot_threshold_log_size': '0'})
+            for s in servers]
+
+    # Change schema - trigger log truncation
+    await cql.run_async("drop keyspace ks")
+
+    log_size = await get_raft_log_size(cql, h1)
+    logger.info(f"After drop table Log size on {s1}: {log_size}")
+
+    # check the log size, which should be 2, as we set snapshot_trailing to 2 and we generated 2 schema chenges
+    assert (log_size == 2)
