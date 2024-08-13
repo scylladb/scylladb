4368d318398e7094a0abff670185cf921f3bf56f
Pavel Emelyanov <xemul@scylladb.com>
2024-08-13 20:00:37 +0300

    distributed_loader: Split get_sstables_from_upload_dir()
    Next patches will need this method to initialize sstable_directory
    differently and then do its regular processing. For that, split the
    method into two, next patch will re-use the common part it needs.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- replica/distributed_loader.hh
+++ replica/distributed_loader.hh
@@ -74,6 +74,9 @@ class distributed_loader {
             sharded<replica::database>& db, sharded<db::view::view_builder>& vb,
             bool needs_view_update, sstring ks, sstring cf);
     static future<> populate_keyspace(distributed<replica::database>& db, sharded<db::system_keyspace>& sys_ks, keyspace& ks, sstring ks_name);
+    static future<std::tuple<table_id, std::vector<std::vector<sstables::shared_sstable>>>>
+        get_sstables_from(distributed<replica::database>& db, sstring ks, sstring cf, sstables::sstable_open_config cfg,
+        noncopyable_function<future<>(global_table_ptr&, sharded<sstables::sstable_directory>&)> start_dir);
 
 public:
     static future<> init_system_keyspace(sharded<db::system_keyspace>&, distributed<locator::effective_replication_map_factory>&, distributed<replica::database>&);
