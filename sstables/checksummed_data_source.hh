b7dfba4c18a735f8ce563542060aa73ba30a3a1b
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2024-08-14 18:04:32 +0300

    sstables: Introduce checksummed file data source implementation
    Introduce a new data source implementation for uncompressed SSTables.
    
    This is just a thin wrapper for a raw data source that also performs
    checksum validation for each chunk. This way we can have consistent
    behavior for compressed and uncompressed SSTables.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

---
+++ sstables/checksummed_data_source.hh
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2024-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <seastar/core/seastar.hh>
+#include <seastar/core/iostream.hh>
+
+#include "sstables/types.hh"
+
+namespace sstables {
+
+input_stream<char> make_checksummed_file_k_l_format_input_stream(file f,
+                uint64_t file_len, const sstables::checksum& checksum,
+                uint64_t offset, size_t len, class file_input_stream_options options);
+
+input_stream<char> make_checksummed_file_m_format_input_stream(file f,
+                uint64_t file_len, const sstables::checksum& checksum,
+                uint64_t offset, size_t len, class file_input_stream_options options);
+}
