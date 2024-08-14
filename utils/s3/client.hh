8d4385bdd91eeb070eb4af4e4d6df2d77bad224c
Pavel Emelyanov <xemul@scylladb.com>
2024-08-14 13:44:44 +0300

    s3-client: Add support for lister::filter
    Directory lister comes with a filter function that tells lister which
    entries to skip by its .get() method. For uniformity, add the same to
    S3 bucket_lister.
    
    After this change the lister reports shorter name in the returned
    directory entry (with the prefix cut), so also need to tune up the unit
    test respectively.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- utils/s3/client.hh
+++ utils/s3/client.hh
@@ -138,6 +138,7 @@ public:
         sstring _prefix;
         sstring _max_keys;
         std::optional<future<>> _opt_done_fut;
+        lister::filter_type _filter;
         seastar::queue<std::optional<directory_entry>> _queue;
 
         future<> start_listing();
@@ -145,6 +146,7 @@ public:
     public:
 
         bucket_lister(shared_ptr<client> client, sstring bucket, sstring prefix = "", size_t objects_per_page = 64, size_t entries_batch = 512 / sizeof(std::optional<directory_entry>));
+        bucket_lister(shared_ptr<client> client, sstring bucket, sstring prefix, lister::filter_type filter, size_t objects_per_page = 64, size_t entries_batch = 512 / sizeof(std::optional<directory_entry>));
 
         future<std::optional<directory_entry>> get() override;
         future<> close() noexcept override;
