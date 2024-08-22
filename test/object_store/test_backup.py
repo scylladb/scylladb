de874504538c8a486a1a933a31f1419a4cd090b1
Pavel Emelyanov <xemul@scylladb.com>
2024-08-22 15:27:40 +0300

    test/object_store: Test that backup task is abortable
    It starts similarly to simpl backup test, but injects a pause into the
    task once a single file is scheduled for upload, then aborts the task,
    waits for it to fail, and check that _not_ all files are uploaded.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/object_store/test_backup.py
+++ test/object_store/test_backup.py
@@ -9,6 +9,7 @@ import logging
 from test.pylib.manager_client import ManagerClient
 from test.object_store.conftest import format_tuples
 from test.object_store.conftest import get_s3_resource
+from test.topology.conftest import skip_mode
 
 logger = logging.getLogger(__name__)
 
@@ -76,3 +77,46 @@ async def test_simple_backup(manager: ManagerClient, s3_server):
     log = await manager.server_open_log(server.server_id)
     res = await log.grep(r'INFO.*\[shard [0-9]:([a-z]+)\] .* Backup sstables from .* to')
     assert len(res) == 1 and res[0][1].group(1) == 'strm'
+
+
+@pytest.mark.asyncio
+@skip_mode('release', 'error injections are not supported in release mode')
+async def test_backup_is_abortable(manager: ManagerClient, s3_server):
+    '''check that backing up a snapshot for a keyspace works'''
+
+    cfg = {'enable_user_defined_functions': False,
+           'object_storage_config_file': str(s3_server.config_file),
+           'experimental_features': ['keyspace-storage-options'],
+           'task_ttl_in_seconds': 300
+           }
+    cmd = [ '--logger-log-level', 'snapshots=trace:task_manager=trace' ]
+    server = await manager.server_add(config=cfg, cmdline=cmd)
+    ks, cf = await prepare_snapshot_for_backup(manager, server)
+
+    workdir = await manager.server_get_workdir(server.server_id)
+    cf_dir = os.listdir(f'{workdir}/data/{ks}')[0]
+    files = set(os.listdir(f'{workdir}/data/{ks}/{cf_dir}/snapshots/backup'))
+    assert len(files) > 1
+
+    await manager.api.enable_injection(server.ip_addr, "backup_task_pause", one_shot=True)
+    log = await manager.server_open_log(server.server_id)
+    mark = await log.mark()
+
+    print('Backup snapshot')
+    tid = await manager.api.backup(server.ip_addr, ks, 'backup', s3_server.address, s3_server.bucket_name)
+
+    print(f'Started task {tid}, aborting it early')
+    await log.wait_for('backup task: waiting', from_mark=mark)
+    await manager.api.abort_task(server.ip_addr, tid)
+    await manager.api.message_injection(server.ip_addr, "backup_task_pause")
+    status = await manager.api.wait_task(server.ip_addr, tid)
+    print(f'Status: {status}')
+    assert (status is not None) and (status['state'] == 'failed')
+
+    objects = set([ o.key for o in get_s3_resource(s3_server).Bucket(s3_server.bucket_name).objects.all() ])
+    uploaded_count = 0
+    for f in files:
+        print(f'Check {f} is in backup')
+        if f'{cf}/backup/{f}' in objects:
+            uploaded_count += 1
+    assert uploaded_count > 0 and uploaded_count < len(files)
