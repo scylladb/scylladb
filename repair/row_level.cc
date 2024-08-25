a35942e09a7cf3d4cb5dad8b91b0795797c74bbb
Avi Kivity <avi@scylladb.com>
2024-08-25 16:19:38 +0300

    repair: row_level: coroutinize repair_meta::get_full_row_hashes_sink_op()
    Extra care is needed for exception handling.

--- repair/row_level.cc
+++ repair/row_level.cc
@@ -1530,13 +1530,17 @@ private:
     }
 
     future<> get_full_row_hashes_sink_op(rpc::sink<repair_stream_cmd>& sink) {
-        return sink(repair_stream_cmd::get_full_row_hashes).then([&sink] {
-            return sink.flush();
-        }).handle_exception([&sink] (std::exception_ptr ep) {
-            return sink.close().then([ep = std::move(ep)] () mutable {
-                return make_exception_future<>(std::move(ep));
-            });
-        });
+        std::exception_ptr ep;
+        try {
+            co_await sink(repair_stream_cmd::get_full_row_hashes);
+            co_await sink.flush();
+        } catch (...) {
+            ep = std::current_exception();
+        }
+        if (ep) {
+            co_await sink.close();
+            std::rethrow_exception(ep);
+        }
     }
 
 public:
