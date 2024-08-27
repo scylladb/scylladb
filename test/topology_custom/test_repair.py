ac70669bce55105a2f57108f2945cf53ba580aef
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2024-08-27 11:09:43 +0200

    test: add test to ensure repair won't fail with uninitialized bm
    (cherry picked from commit f38bb6483a15119312f5785b798453ff221ccc20)
    
    # Conflicts:
    #	test/topology_custom/test_repair.py

--- test/topology_custom/test_repair.py
+++ test/topology_custom/test_repair.py
@@ -67,3 +67,103 @@ async def test_enable_compacting_data_for_streaming_and_repair_live_update(manag
     # After the update to the config above, the next repair should pick up the updated value.
     await manager.api.repair(node1.ip_addr, "ks", "tbl")
     assert (await get_injection_params(manager, node1.ip_addr, "maybe_compact_for_streaming"))["compaction_enabled"] == "true"
+<<<<<<< HEAD
+=======
+
+
+@pytest.mark.asyncio
+@skip_mode('release', 'error injections are not supported in release mode')
+async def test_tombstone_gc_for_streaming_and_repair(manager):
+    """
+    Check that:
+    * enable_tombstone_gc_for_streaming_and_repair=1 works as expected
+    * enable_tombstone_gc_for_streaming_and_repair=0 works as expected
+    * enable_tombstone_gc_for_streaming_and_repair is live-update
+    """
+    cmdline = [
+            "--enable-compacting-data-for-streaming-and-repair", "1",
+            "--enable-tombstone-gc-for-streaming-and-repair", "1",
+            "--enable-cache", "0",
+            "--hinted-handoff-enabled", "0",
+            "--smp", "1",
+            "--logger-log-level", "api=trace:database=trace"]
+    node1 = await manager.server_add(cmdline=cmdline)
+    node2 = await manager.server_add(cmdline=cmdline)
+
+    cql = manager.get_cql()
+
+    cql.execute("CREATE KEYSPACE ks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 2}")
+    cql.execute("CREATE TABLE ks.tbl (pk int, ck int, PRIMARY KEY (pk, ck)) WITH compaction = {'class': 'NullCompactionStrategy'}")
+
+    await manager.server_stop_gracefully(node2.server_id)
+
+    stmt = SimpleStatement("DELETE FROM ks.tbl WHERE pk = 0 AND ck = 0", consistency_level=ConsistencyLevel.ONE)
+    cql.execute(stmt)
+
+    await manager.server_start(node2.server_id, wait_others=1)
+
+    # Flush memtables and remove commitlog, so we can freely GC tombstones.
+    await manager.server_restart(node1.server_id, wait_others=1)
+
+    host1, host2 = await wait_for_cql_and_get_hosts(cql, [node1, node2], time.time() + 30)
+
+    config_item = "enable_tombstone_gc_for_streaming_and_repair"
+
+    def check_nodes_have_data(node1_has_data, node2_has_data):
+        for (host, host_has_data) in ((host1, node1_has_data), (host2, node2_has_data)):
+            res = list(cql.execute("SELECT * FROM MUTATION_FRAGMENTS(ks.tbl) WHERE pk = 0", host=host))
+            print(res)
+            if host_has_data:
+                assert len(res) == 3
+            else:
+                assert len(res) < 3
+
+    # Initial start-condition check
+    check_nodes_have_data(True, False)
+
+    await manager.api.enable_injection(node1.ip_addr, "maybe_compact_for_streaming", False, {})
+
+    # Make the tombstone purgeable
+    cql.execute("ALTER TABLE ks.tbl WITH tombstone_gc = {'mode': 'immediate'}")
+
+    # With enable_tombstone_gc_for_streaming_and_repair=1, repair
+    # should not find any differences and thus not replicate the GCable
+    # tombstone.
+    await manager.api.repair(node1.ip_addr, "ks", "tbl")
+    assert (await get_injection_params(manager, node1.ip_addr, "maybe_compact_for_streaming")) == {
+            "compaction_enabled": "true", "compaction_can_gc": "true"}
+    check_nodes_have_data(True, False)
+
+    for host in (host1, host2):
+        cql.execute(f"UPDATE system.config SET value = '0' WHERE name = '{config_item}'", host=host)
+
+    # With enable_tombstone_gc_for_streaming_and_repair=0, repair
+    # should find the differences and replicate the GCable tombstone.
+    await manager.api.repair(node1.ip_addr, "ks", "tbl")
+    assert (await get_injection_params(manager, node1.ip_addr, "maybe_compact_for_streaming")) == {
+            "compaction_enabled": "true", "compaction_can_gc": "false"}
+    check_nodes_have_data(True, True)
+
+@pytest.mark.asyncio
+@skip_mode('release', 'error injections are not supported in release mode')
+async def test_repair_succeeds_with_unitialized_bm(manager):
+    await manager.server_add()
+    await manager.server_add()
+    servers = await manager.running_servers()
+
+    cql = manager.get_cql()
+
+    cql.execute("CREATE KEYSPACE ks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 2}")
+    cql.execute("CREATE TABLE ks.tbl (pk int, ck int, PRIMARY KEY (pk, ck)) WITH tombstone_gc = {'mode': 'repair'}")
+
+    await manager.api.enable_injection(servers[1].ip_addr, "repair_flush_hints_batchlog_handler_bm_uninitialized", True, {})
+    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
+    marks = [await log.mark() for log in logs]
+
+    await manager.api.repair(servers[0].ip_addr, "ks", "tbl")
+
+    matches = await logs[1].grep("Failed to process repair_flush_hints_batchlog_request", from_mark=marks[1])
+    assert len(matches) == 1
+    matches = await logs[0].grep("failed, continue to run repair", from_mark=marks[0])
+    assert len(matches) == 1
+>>>>>>> f38bb6483a (test: add test to ensure repair won't fail with uninitialized bm)
