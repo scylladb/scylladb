f38bb6483a15119312f5785b798453ff221ccc20
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2024-08-27 11:09:43 +0200

    test: add test to ensure repair won't fail with uninitialized bm

--- test/topology_custom/test_repair.py
+++ test/topology_custom/test_repair.py
@@ -144,3 +144,26 @@ async def test_tombstone_gc_for_streaming_and_repair(manager):
     assert (await get_injection_params(manager, node1.ip_addr, "maybe_compact_for_streaming")) == {
             "compaction_enabled": "true", "compaction_can_gc": "false"}
     check_nodes_have_data(True, True)
+
+@pytest.mark.asyncio
+@skip_mode('release', 'error injections are not supported in release mode')
+async def test_repair_succeeds_with_unitialized_bm(manager):
+    await manager.server_add()
+    await manager.server_add()
+    servers = await manager.running_servers()
+
+    cql = manager.get_cql()
+
+    cql.execute("CREATE KEYSPACE ks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 2}")
+    cql.execute("CREATE TABLE ks.tbl (pk int, ck int, PRIMARY KEY (pk, ck)) WITH tombstone_gc = {'mode': 'repair'}")
+
+    await manager.api.enable_injection(servers[1].ip_addr, "repair_flush_hints_batchlog_handler_bm_uninitialized", True, {})
+    logs = [await manager.server_open_log(srv.server_id) for srv in servers]
+    marks = [await log.mark() for log in logs]
+
+    await manager.api.repair(servers[0].ip_addr, "ks", "tbl")
+
+    matches = await logs[1].grep("Failed to process repair_flush_hints_batchlog_request", from_mark=marks[1])
+    assert len(matches) == 1
+    matches = await logs[0].grep("failed, continue to run repair", from_mark=marks[0])
+    assert len(matches) == 1
