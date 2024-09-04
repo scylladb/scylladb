3356ce8231b9c816731362a323df5a56093bdcfa
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2024-09-04 11:12:20 +0200

    db: store tables and views schema diff in schema_applier
    It will be used in subsequent commit for moving
    notifications code.

--- db/schema_applier.hh
+++ db/schema_applier.hh
@@ -15,6 +15,7 @@
 #include "query-result-set.hh"
 #include "db/schema_tables.hh"
 #include "data_dictionary/user_types_metadata.hh"
+#include "schema/schema_registry.hh"
 
 #include <seastar/core/distributed.hh>
 
@@ -90,6 +91,31 @@ public:
     in_progress_types_storage_per_shard& local();
 };
 
+// schema_diff represents what is happening with tables or views during schema merge
+struct schema_diff {
+    struct dropped_schema {
+        global_schema_ptr schema;
+    };
+
+    struct altered_schema {
+        global_schema_ptr old_schema;
+        global_schema_ptr new_schema;
+    };
+
+    std::vector<global_schema_ptr> created;
+    std::vector<altered_schema> altered;
+    std::vector<dropped_schema> dropped;
+
+    size_t size() const {
+        return created.size() + altered.size() + dropped.size();
+    }
+};
+
+struct affected_tables_and_views {
+    schema_diff tables;
+    schema_diff views;
+};
+
 // Schema_applier encapsulates intermediate state needed to construct schema objects from
 // set of rows read from system tables (see struct schema_state). It does atomic (per shard)
 // application of a new schema.
@@ -109,6 +135,7 @@ class schema_applier {
 
     affected_keyspaces _affected_keyspaces;
     affected_user_types _affected_user_types;
+    affected_tables_and_views _affected_tables_and_views;
 
     future<schema_persisted_state> get_schema_persisted_state();
 public:
