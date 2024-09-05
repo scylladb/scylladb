f02a686115b0cad40384e04e9100b22ca1ae0013
Pavel Emelyanov <xemul@scylladb.com>
2024-09-05 19:04:38 +0300

    schema: Ditch make_shared_schema() helper
    Now it's unused
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- schema/schema.hh
+++ schema/schema.hh
@@ -969,10 +969,6 @@ public:
     schema_ptr get_reversed() const;
 };
 
-lw_shared_ptr<const schema> make_shared_schema(std::optional<table_id> id, std::string_view ks_name, std::string_view cf_name,
-    std::vector<schema::column> partition_key, std::vector<schema::column> clustering_key, std::vector<schema::column> regular_columns,
-    std::vector<schema::column> static_columns, data_type regular_column_name_type, sstring comment = "");
-
 bool operator==(const schema&, const schema&);
 
 /**
