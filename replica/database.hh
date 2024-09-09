8158b1d2a5e72f62b7a0d49333c2f9efd1d0f9ff
Pavel Emelyanov <xemul@scylladb.com>
2024-09-09 15:12:29 +0300

    table: Remove unused database& argument from take_snapshot() method
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- replica/database.hh
+++ replica/database.hh
@@ -937,7 +937,7 @@ public:
 private:
     using snapshot_file_set = foreign_ptr<std::unique_ptr<std::unordered_set<sstring>>>;
 
-    future<snapshot_file_set> take_snapshot(database& db, sstring jsondir);
+    future<snapshot_file_set> take_snapshot(sstring jsondir);
     // Writes the table schema and the manifest of all files in the snapshot directory.
     future<> finalize_snapshot(database& db, sstring jsondir, std::vector<snapshot_file_set> file_sets);
     static future<> seal_snapshot(sstring jsondir, std::vector<snapshot_file_set> file_sets);
