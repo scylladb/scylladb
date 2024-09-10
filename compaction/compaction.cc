50c9d8612cf527901f78c2d92ad024e21012fc88
Łukasz Paszkowski <lukasz.paszkowski@scylladb.com>
2024-09-10 13:17:55 +0200

    compaction: Update compaction history with collected histogram
    A new field has been added to the compaction_stats structure to hold
    collected combined reader statistics. The struct is than used to update
    the compaction_history table.

--- compaction/compaction.cc
+++ compaction/compaction.cc
@@ -503,6 +503,7 @@ protected:
     uint64_t _estimated_partitions = 0;
     double _estimated_droppable_tombstone_ratio = 0;
     uint64_t _bloom_filter_checks = 0;
+    combined_reader_statistics _reader_statistics;
     db::replay_position _rp;
     encoding_stats_collector _stats_collector;
     const bool _can_split_large_partition = false;
@@ -899,6 +900,7 @@ protected:
                 .start_size = _start_size,
                 .end_size = _end_size,
                 .bloom_filter_checks = _bloom_filter_checks,
+                .reader_statistics = std::move(_reader_statistics),
             },
         };
 
@@ -1158,7 +1160,9 @@ public:
                 std::move(trace),
                 sm_fwd,
                 mr_fwd,
-                unwrap_monitor_generator());
+                unwrap_monitor_generator(),
+                default_sstable_predicate(),
+                &_reader_statistics);
     }
 
     std::string_view report_start_desc() const override {
