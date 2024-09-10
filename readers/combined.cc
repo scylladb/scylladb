58d9f84bde099ae9c9dc05b5b3668bbeef9b3b5d
Łukasz Paszkowski <lukasz.paszkowski@scylladb.com>
2024-09-10 12:49:31 +0200

    mutation_fragment_merger: Create histogram while consuming mutation fragment batches
    The mutation_fragment_merger takes one additional parameter in its
    constructor, that is a pointer to a combined_reader_statistics
    used to collect various statistics.
    
    The histogram is populated with data while the merger consumes
    batches from the producer and merges them into seperate mutation
    fragments. The size of the batch, that represents the number of streams
    the mutation fragment originates from, is used as a key in the
    historgam and its corresponding value is increased by one.

--- readers/combined.cc
+++ readers/combined.cc
@@ -83,12 +83,14 @@ class mutation_fragment_merger {
     Producer _producer;
     range_tombstone_change_merger<stream_id_t> _tombstone_merger;
     mutation_fragment_v2_opt _result;
+    combined_reader_statistics* _statistics{ nullptr };
 
 public:
-    mutation_fragment_merger(schema_ptr schema, reader_permit permit, Producer&& producer)
+    mutation_fragment_merger(schema_ptr schema, reader_permit permit, Producer&& producer, combined_reader_statistics* statistics = nullptr)
         : _schema(std::move(schema))
         , _permit(std::move(permit))
-        , _producer(std::move(producer)) {
+        , _producer(std::move(producer))
+        , _statistics(statistics) {
     }
 
     future<mutation_fragment_v2_opt> operator()() {
@@ -114,6 +116,10 @@ public:
                     }
                     return stop_iteration::no;
                 } else {
+                    if (_statistics && begin->fragment.is_clustering_row()) {
+                        ++_statistics->rows_merged_histogram[fragments.size()];
+                    }
+
                     for (auto it = begin + 1; it != end; ++it) {
                         begin->fragment.apply(*_schema, std::move(it->fragment));
                     }
--- readers/combined.hh
+++ readers/combined.hh
@@ -34,6 +34,14 @@ public:
     }
 };
 
+struct combined_reader_statistics {
+    // Histogram describing a distribution of clustering keys. The vector
+    // gathers a number of clustering keys merged (value) from a given
+    // number of sstable files (index). The length of the vector is equal
+    // to the number of compacted sstables + 1
+    std::vector<int64_t> rows_merged_histogram;
+};
+
 // Creates a mutation reader which combines data return by supplied readers.
 // Returns mutation of the same schema only when all readers return mutations
 // of the same schema.
