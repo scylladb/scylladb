2daa630938dc8cf188e54ef471e1a3f6eb8efc74
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2024-09-13 11:39:39 +0200

    replica: split update keyspace into two phases
    - first phase is preemptive (prepare_update_keyspace)
    - second phase is non-preemptive (update_keyspace)
    
    This is done so that schema change can be applied atomically.
    
    Aditionally create keyspace code was changed to share common
    part with update keyspace flow.
    
    This commit doesn't yet change the behaviour of the code,
    as it doesn't guarantee atomicity, it will be done in following
    commits.

--- replica/database.hh
+++ replica/database.hh
@@ -1327,6 +1327,13 @@ using user_types_metadata = data_dictionary::user_types_metadata;
 
 using keyspace_metadata = data_dictionary::keyspace_metadata;
 
+// Encapsulates objects needed to update keyspace schema
+struct keyspace_change {
+    lw_shared_ptr<keyspace_metadata> metadata;
+    locator::replication_strategy_ptr strategy;
+    locator::vnode_effective_replication_map_ptr erm;
+};
+
 class keyspace {
 public:
     struct config {
@@ -1358,14 +1365,14 @@ private:
     locator::effective_replication_map_factory& _erm_factory;
 
 public:
-    explicit keyspace(lw_shared_ptr<keyspace_metadata> metadata, config cfg, locator::effective_replication_map_factory& erm_factory);
+    explicit keyspace(config cfg, locator::effective_replication_map_factory& erm_factory);
     keyspace(const keyspace&) = delete;
     void operator=(const keyspace&) = delete;
     keyspace(keyspace&&) = default;
 
     future<> shutdown() noexcept;
 
-    future<> update_from(const locator::shared_token_metadata& stm, lw_shared_ptr<keyspace_metadata>);
+    void apply(keyspace_change kc);
 
     future<> init_storage();
 
@@ -1374,7 +1381,12 @@ public:
      * boom, it is replaced.
      */
     lw_shared_ptr<keyspace_metadata> metadata() const;
-    future<> create_replication_strategy(const locator::shared_token_metadata& stm);
+
+    static locator::replication_strategy_ptr create_replication_strategy(
+            lw_shared_ptr<keyspace_metadata> metadata);
+    future<locator::vnode_effective_replication_map_ptr> create_effective_replication_map(
+            locator::replication_strategy_ptr strategy,
+            const locator::shared_token_metadata& stm) const;
     void update_effective_replication_map(locator::vnode_effective_replication_map_ptr erm);
 
     /**
@@ -1657,7 +1669,8 @@ private:
     void insert_keyspace(std::unique_ptr<keyspace> ks);
     future<> remove(table&) noexcept;
     void drop_keyspace(const sstring& name);
-    future<> update_keyspace(const keyspace_metadata& tmp_ksm);
+    future<keyspace_change> prepare_update_keyspace(const keyspace& ks, lw_shared_ptr<keyspace_metadata> metadata) const;
+    void update_keyspace(keyspace_change change);
     static future<> modify_keyspace_on_all_shards(sharded<database>& sharded_db, std::function<future<>(replica::database&)> func);
 
     future<> foreach_reader_concurrency_semaphore(std::function<future<>(reader_concurrency_semaphore&)> func);
