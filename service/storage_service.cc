d565cb35015e1ad1cf791043c842550cdd18c80a
Gleb Natapov <gleb@scylladb.com>
2024-09-19 14:44:30 +0300

    topology coordinator:: mark node as being replaced earlier
    Before 17f4a151ce27114a66a83874705c223e0dba9ceb the node was marked as
    been replaced in join_group0 state, before it actually joins the group0,
    so by the time it actually joins and starts transferring snapshot/log no
    traffic is sent to it. The commit changed this to mark the node as
    being replaced after the snapshot/log is already transferred so we can
    get the traffic to the node while it sill did not caught up with a
    leader and this may causes problems since the state is not complete.
    Mark the node as being replaced earlier, but still add the new node to
    the topology later as the commit above intended.
    
    (cherry picked from commit c0939d86f920ceb7434f8167c13ac4d8ebcf9987)

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -559,11 +559,11 @@ future<storage_service::nodes_to_notify_after_sync> storage_service::sync_raft_t
                 on_fatal_internal_error(rtlogger, ::format("Cannot map id of a node being replaced {} to its ip", replaced_id));
             }
             assert(existing_ip);
+            const auto replaced_host_id = locator::host_id(replaced_id.uuid());
+            tmptr->update_topology(replaced_host_id, std::nullopt, locator::node::state::being_replaced);
+            tmptr->add_replacing_endpoint(replaced_host_id, host_id);
             if (rs.ring.has_value()) {
-                const auto replaced_host_id = locator::host_id(replaced_id.uuid());
-                tmptr->update_topology(replaced_host_id, std::nullopt, locator::node::state::being_replaced);
                 update_topology(host_id, ip, rs);
-                tmptr->add_replacing_endpoint(replaced_host_id, host_id);
                 co_await update_topology_change_info(tmptr, ::format("replacing {}/{} by {}/{}", replaced_id, *existing_ip, id, ip));
             } else {
                 // After adding replacing endpoint above the node will no longer be reported for reads and writes,
