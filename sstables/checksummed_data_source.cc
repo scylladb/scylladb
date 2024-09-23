77285f24c87c8d62afa3716958d5b8181a694599
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2024-09-23 18:50:11 +0300

    sstables: Add error handler in checksummed data source
    Currently, the checksummed data source treats an invalid checksum or
    digest as an unrecoverable error by throwing a
    `malformed_sstable_exception`. This does not allow to use this data
    source in places where it is required to resume after a failed checksum
    (e.g., in `validate_checksums()`).
    
    Make the error handling logic customizable via a callback function.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- sstables/checksummed_data_source.cc
+++ sstables/checksummed_data_source.cc
@@ -17,6 +17,7 @@
 #include "exceptions.hh"
 #include "checksum_utils.hh"
 #include "data_source_types.hh"
+#include "checksummed_data_source.hh"
 
 namespace sstables {
 
@@ -29,6 +30,7 @@ class checksummed_file_data_source_impl : public data_source_impl {
     std::optional<input_stream<char>> _input_stream;
     const checksum& _checksum;
     [[no_unique_address]] digest_members<check_digest> _digests;
+    integrity_error_handler _error_handler;
     uint64_t _chunk_size_trailing_zeros;
     uint64_t _file_len;
     uint64_t _underlying_pos;
@@ -39,8 +41,10 @@ public:
     checksummed_file_data_source_impl(file f, uint64_t file_len,
                 const checksum& checksum, uint64_t pos, size_t len,
                 file_input_stream_options options,
-                std::optional<uint32_t> digest)
+                std::optional<uint32_t> digest,
+                integrity_error_handler error_handler)
             : _checksum(checksum)
+            , _error_handler(error_handler)
             , _file_len(file_len)
             , _pos(pos)
             , _beg_pos(pos)
@@ -118,7 +122,9 @@ public:
             auto expected_checksum = _checksum.checksums[chunk_index];
             auto actual_checksum = ChecksumType::checksum(buf.get(), buf.size());
             if (expected_checksum != actual_checksum) {
-                throw sstables::malformed_sstable_exception(format("Checksummed chunk of size {} at file offset {} failed checksum: expected={}, actual={}", buf.size(), _underlying_pos, expected_checksum, actual_checksum));
+                _error_handler(seastar::format(
+                        "Checksummed chunk of size {} at file offset {} failed checksum: expected={}, actual={}",
+                        buf.size(), _underlying_pos, expected_checksum, actual_checksum));
             }
 
             if constexpr (check_digest) {
@@ -133,7 +139,7 @@ public:
 
             if constexpr (check_digest) {
                 if (_digests.can_calculate_digest && _pos == _file_len && _digests.expected_digest != _digests.actual_digest) {
-                    throw malformed_sstable_exception(seastar::format("Digest mismatch: expected={}, actual={}", _digests.expected_digest, _digests.actual_digest));
+                    _error_handler(seastar::format("Digest mismatch: expected={}, actual={}", _digests.expected_digest, _digests.actual_digest));
                 }
             }
             return buf;
@@ -176,39 +182,48 @@ class checksummed_file_data_source : public data_source {
 public:
     checksummed_file_data_source(file f, uint64_t file_len, const checksum& checksum,
             uint64_t offset, size_t len, file_input_stream_options options,
-            std::optional<uint32_t> digest)
+            std::optional<uint32_t> digest, integrity_error_handler error_handler)
         : data_source(std::make_unique<checksummed_file_data_source_impl<ChecksumType, check_digest>>(
-                std::move(f), file_len, checksum, offset, len, std::move(options), digest))
+                std::move(f), file_len, checksum, offset, len, std::move(options), digest,
+                error_handler))
     {}
 };
 
 template <ChecksumUtils ChecksumType>
 inline input_stream<char> make_checksummed_file_input_stream(
         file f, uint64_t file_len, const checksum& checksum, uint64_t offset,
-        size_t len, file_input_stream_options options, std::optional<uint32_t> digest)
+        size_t len, file_input_stream_options options, std::optional<uint32_t> digest,
+        integrity_error_handler error_handler)
 {
     if (digest) {
         return input_stream<char>(checksummed_file_data_source<ChecksumType, true>(
-            std::move(f), file_len, checksum, offset, len, std::move(options), digest));
+            std::move(f), file_len, checksum, offset, len, std::move(options), digest,
+            error_handler));
     }
     return input_stream<char>(checksummed_file_data_source<ChecksumType, false>(
-        std::move(f), file_len, checksum, offset, len, std::move(options), digest));
+        std::move(f), file_len, checksum, offset, len, std::move(options), digest, error_handler));
 }
 
 input_stream<char> make_checksummed_file_k_l_format_input_stream(
         file f, uint64_t file_len, const checksum& checksum, uint64_t offset,
-        size_t len, file_input_stream_options options, std::optional<uint32_t> digest)
+        size_t len, file_input_stream_options options, std::optional<uint32_t> digest,
+        integrity_error_handler error_handler)
 {
     return make_checksummed_file_input_stream<adler32_utils>(std::move(f), file_len,
-            checksum, offset, len, std::move(options), digest);
+            checksum, offset, len, std::move(options), digest, error_handler);
 }
 
 input_stream<char> make_checksummed_file_m_format_input_stream(
         file f, uint64_t file_len, const checksum& checksum, uint64_t offset,
-        size_t len, file_input_stream_options options, std::optional<uint32_t> digest)
+        size_t len, file_input_stream_options options, std::optional<uint32_t> digest,
+        integrity_error_handler error_handler)
 {
     return make_checksummed_file_input_stream<crc32_utils>(std::move(f), file_len,
-            checksum, offset, len, std::move(options), digest);
+            checksum, offset, len, std::move(options), digest, error_handler);
 }
+
+void throwing_integrity_error_handler(sstring msg) {
+        throw sstables::malformed_sstable_exception(msg);
+};
 
 }
