1cfdfa9858517ea2349e638c70331ec2bd00002c
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2024-10-01 00:24:32 +0300

    sstables: Allow validate_checksums() to report missing digests
    Currently, `validate_checksums()` expects the SSTable to have a digest
    component and fails immediately otherwise. This is suboptimal since data
    integrity verification could still be carried out partially via checksum
    checking.
    
    Lift this restriction by allowing the function to perform checksum
    checking in any case, and treat digest checking as best effort. Add a
    separate boolean flag in the response to indicate the presence or
    absence of the digest component, so that the user can deduce if a valid
    result involved digest checking or not.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- test/boost/sstable_datafile_test.cc
+++ test/boost/sstable_datafile_test.cc
@@ -2824,7 +2824,8 @@ SEASTAR_TEST_CASE(test_validate_checksums) {
                 testlog.info("Validating intact {}", sst->get_filename());
 
                 res = sstables::validate_checksums(sst, permit).get();
-                BOOST_REQUIRE(res == validate_checksums_result::valid);
+                BOOST_REQUIRE(res.status == validate_checksums_status::valid);
+                BOOST_REQUIRE(res.has_digest);
 
                 auto sst_file = open_file_dma(test(sst).filename(sstables::component_type::Data).native(), open_flags::wo).get();
                 auto close_sst_file = defer([&sst_file] { sst_file.close().get(); });
@@ -2839,7 +2840,8 @@ SEASTAR_TEST_CASE(test_validate_checksums) {
                 }
 
                 res = sstables::validate_checksums(sst, permit).get();
-                BOOST_REQUIRE(res == validate_checksums_result::invalid);
+                BOOST_REQUIRE(res.status == validate_checksums_status::invalid);
+                BOOST_REQUIRE(res.has_digest);
 
                 testlog.info("Validating truncated {}", sst->get_filename());
 
@@ -2848,13 +2850,15 @@ SEASTAR_TEST_CASE(test_validate_checksums) {
                 }
 
                 res = sstables::validate_checksums(sst, permit).get();
-                BOOST_REQUIRE(res == validate_checksums_result::invalid);
+                BOOST_REQUIRE(res.status == validate_checksums_status::invalid);
+                BOOST_REQUIRE(res.has_digest);
 
                 if (compression_params == no_compression_params) {
                     testlog.info("Validating with no checksums {}", sst->get_filename());
                     sstables::test(sst).rewrite_toc_without_component(component_type::CRC);
                     auto res = sstables::validate_checksums(sst, permit).get();
-                    BOOST_REQUIRE(res == validate_checksums_result::no_checksum);
+                    BOOST_REQUIRE(res.status == validate_checksums_status::no_checksum);
+                    BOOST_REQUIRE(res.has_digest);
                 }
             }
         }
