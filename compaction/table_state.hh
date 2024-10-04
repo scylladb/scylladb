f7ad21d07c51631ca239759f7d2248dfa2d51b01
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-10-04 14:51:38 +0530

    compaction::table_state: implement `get_token_range_after_split()` wrapper
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- compaction/table_state.hh
+++ compaction/table_state.hh
@@ -59,6 +59,7 @@ public:
     virtual compaction_backlog_tracker& get_backlog_tracker() = 0;
     virtual const std::string get_group_id() const noexcept = 0;
     virtual seastar::condition_variable& get_staging_done_condition() noexcept = 0;
+    virtual dht::token_range get_token_range_after_split(const dht::token& t) const noexcept = 0;
 };
 
 } // namespace compaction
