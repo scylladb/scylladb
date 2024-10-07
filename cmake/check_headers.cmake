scylladb/scylladb

commit efd65aebb28dc07d313e3435001a4f166cdbb9b7

build: cmake: add check-header target
to have feature parity with `configure.py`. we won't need this
once we migrate to C++20 modules. but before that day comes, we
need to stick with C++ headers.

we generate a rule for each .hh files to create a corresponding
.cc and then compile it, in order to verify the self-containness of
that header. so the number of rule is quite large, to avoid the
unnecessary overhead. the check-header target is enabled only if
`Scylla_CHECK_HEADERS` option is enabled.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

Closes scylladb/scylladb#15913

cmake/check_headers.cmake
@@ -1,0 +1,104 @@
+function (check_headers check_headers_target target)
+  if(NOT TARGET ${check_headers_target})
+    return()
+  endif()
+
+  # "target" is required, so we can get the compiling options for compiling
+  # the headers.
+  cmake_parse_arguments (
+    parsed_args
+    ""
+    "GLOB;GLOB_RECURSE;EXCLUDE;INCLUDE"
+    ""
+    ${ARGN})
+
+  set(sources "")
+  if(DEFINED parsed_args_GLOB)
+    file(GLOB sources
+      LIST_DIRECTORIES false
+      RELATIVE ${CMAKE_SOURCE_DIR}
+      "${parsed_args_GLOB}")
+  elseif(DEFINED parsed_args_GLOB_RECURSE)
+    file(GLOB_RECURSE sources
+      LIST_DIRECTORIES false
+      RELATIVE ${CMAKE_SOURCE_DIR}
+      "${parsed_args_RECURSIVE}")
+  else()
+    message(FATAL_ERROR "Please specify GLOB or GLOB_RECURSE.")
+  endif()
+  if(DEFINED parsed_args_INCLUDE)
+    list(FILTER sources INCLUDE REGEX "${parsed_args_INCLUDE}")
+  endif()
+  if(DEFINED parsed_args_EXCLUDE)
+    list(FILTER sources EXCLUDE REGEX "${parsed_args_EXCLUDE}")
+  endif()
+
+  foreach(fn ${sources})
+    get_filename_component(file_dir ${fn} DIRECTORY)
+    list(APPEND includes "${CMAKE_SOURCE_DIR}/${file_dir}")
+    set(src_dir "${CMAKE_BINARY_DIR}/check-headers/${file_dir}")
+    file(MAKE_DIRECTORY "${src_dir}")
+    get_filename_component(file_name ${fn} NAME)
+    set(src "${src_dir}/${file_name}.cc")
+    # CMake refuses to compile .hh files, so we need to rename them first.
+    add_custom_command(
+      OUTPUT ${src}
+      DEPENDS ${CMAKE_SOURCE_DIR}/${fn}
+      # silence "-Wpragma-once-outside-header"
+      COMMAND sed
+            -e "s/^#pragma once//"
+            "${fn}" > "${src}"
+      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+      VERBATIM)
+    list(APPEND srcs "${src}")
+  endforeach()
+
+  if(NOT srcs)
+    # not headers to checks
+    return()
+  endif()
+
+  set(check_lib "${check_headers_target}-${target}")
+  add_library(${check_lib} EXCLUDE_FROM_ALL)
+  target_sources(${check_lib}
+    PRIVATE ${srcs})
+  # use ${target} as an interface library by consuming all of its
+  # compile time options
+  get_target_property(libraries ${target} LINK_LIBRARIES)
+  if (libraries)
+    target_link_libraries(${check_lib}
+      PRIVATE ${libraries})
+  endif()
+
+  # if header includes other header files with relative path,
+  # we should satisfy it.
+  list(REMOVE_DUPLICATES includes)
+  target_include_directories(${check_lib}
+    PRIVATE ${includes})
+  get_target_property(includes ${target} INCLUDE_DIRECTORIES)
+  if(includes)
+    target_include_directories(${check_lib}
+      PRIVATE ${includes})
+  endif()
+
+  get_target_property(compile_options ${target} COMPILE_OPTIONS)
+  if(compile_options)
+    target_compile_options(${check_lib}
+      PRIVATE ${compile_options})
+  endif ()
+  # symbols in header file should always be referenced, but these
+  # are just pure headers, so unused variables should be tolerated.
+  target_compile_options(${check_lib}
+    PRIVATE
+      -Wno-unused-const-variable
+      -Wno-unused-function
+      -Wno-unused-variable)
+
+  get_target_property(compile_definitions ${target} COMPILE_DEFINITIONS)
+  if(compile_definitions)
+    target_compile_definitions(${check_lib}
+      PRIVATE ${compile_definitions})
+  endif()
+
+  add_dependencies(${check_headers_target} ${check_lib})
+endfunction ()

node_ops/CMakeLists.txt
@@ -11,3 +11,6 @@ target_link_libraries(node_ops
     Seastar::seastar
   PRIVATE
     service)
+
+check_headers(check-headers api
+  GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

sstables/CMakeLists.txt
@@ -33,3 +33,6 @@ target_link_libraries(sstables
     tracing
     libdeflate::libdeflate
     ZLIB::ZLIB)
+
+check_headers(check-headers streaming
+  GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

CMakeLists.txt
@@ -121,6 +121,17 @@ target_link_libraries(scylla-main
     Snappy::snappy
     systemd
     ZLIB::ZLIB)
+
+option(Scylla_CHECK_HEADERS
+  "Add check-headers target for checking the self-containness of headers")
+if(Scylla_CHECK_HEADERS)
+    add_custom_target(check-headers)
+endif()
+
+include(check_headers)
+check_headers(check-headers scylla-main
+  GLOB ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)
+
 add_subdirectory(api)
 add_subdirectory(alternator)
 add_subdirectory(db)

alternator/CMakeLists.txt
@@ -28,3 +28,6 @@ target_link_libraries(alternator
   idl
   Seastar::seastar
   xxHash::xxhash)
+
+check_headers(check-headers alternator
+  GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

api/CMakeLists.txt
@@ -67,6 +67,8 @@ target_include_directories(api
 target_link_libraries(api
   idl
   wasmtime_bindings
-
   Seastar::seastar
   xxHash::xxhash)
+
+check_headers(check-headers api
+  GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

auth/CMakeLists.txt
@@ -35,3 +35,6 @@ target_link_libraries(scylla_auth
     libxcrypt::libxcrypt)
 
 add_whole_archive(auth scylla_auth)
+
+check_headers(check-headers scylla_auth
+  GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

cdc/CMakeLists.txt
@@ -15,3 +15,6 @@ target_link_libraries(cdc
     xxHash::xxhash
   PRIVATE
     replica)
+
+check_headers(check-headers cdc
+  GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

commit 33e33006b535eb39439d24c7b1b243a912448f70

cmake/check_headers: correct typos
Commit efd65aebb28d ("build: cmake: add check-header target", 2023-11-13)
introduced three typos:

- In "cmake/check_headers.cmake", it checked whether the
  "parsed_args_GLOB_RECURSE" argument was defined, but then it referenced
  the same under the wrong name "parsed_args_RECURSIVE".

- The above error masked two further typos; namely the duplicate use of
  "api" and "streaming" each, as targets. With "parsed_args_GLOB_RECURSE"
  above fixed, CMake now reports these conflicting arguments (target
  names). They should have been "node_ops" and "sstables", respectively.

Correct the typos.

Signed-off-by: Laszlo Ersek <laszlo.ersek@scylladb.com>

cmake/check_headers.cmake
@@ -22,7 +22,7 @@ function (check_headers check_headers_target target)
     file(GLOB_RECURSE sources
       LIST_DIRECTORIES false
       RELATIVE ${CMAKE_SOURCE_DIR}
-      "${parsed_args_RECURSIVE}")
+      "${parsed_args_GLOB_RECURSE}")
   else()
     message(FATAL_ERROR "Please specify GLOB or GLOB_RECURSE.")
   endif()

node_ops/CMakeLists.txt
@@ -12,5 +12,5 @@ target_link_libraries(node_ops
   PRIVATE
     service)
 
-check_headers(check-headers api
+check_headers(check-headers node_ops
   GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)

sstables/CMakeLists.txt
@@ -36,5 +36,5 @@ target_link_libraries(sstables
     libdeflate::libdeflate
     ZLIB::ZLIB)
 
-check_headers(check-headers streaming
+check_headers(check-headers sstables
   GLOB_RECURSE ${CMAKE_CURRENT_SOURCE_DIR}/*.hh)