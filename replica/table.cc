62e6d00b09de5ddfce2eab0b7182128914cd7aa8
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-10-07 16:35:01 +0530

    replica/table: check memtable before discarding tombstone during read
    On the read path, the compacting reader is applied only to the sstable
    reader. This can cause an expired tombstone from an sstable to be purged
    from the request before it has a chance to merge with deleted data in
    the memtable leading to data resurrection.
    
    Fix this by checking the memtables before deciding to purge tombstones
    from the request on the read path. A tombstone will not be purged if a
    key exists in any of the table's memtables with a minimum live timestamp
    that is lower than the maximum purgeable timestamp.
    
    Fixes #20916
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
    (cherry picked from commit 5a9327790437991759794f86b8a4158d28ff9973)
    
    # Conflicts:
    #	replica/table.cc

--- replica/table.cc
+++ replica/table.cc
@@ -2471,6 +2471,32 @@ table::make_partition_presence_checker(lw_shared_ptr<const sstables::sstable_set
     };
 }
 
+max_purgeable_fn table::get_max_purgeable_fn_for_cache_underlying_reader() const {
+    return [this](const dht::decorated_key& dk, ::is_shadowable is_shadowable) {
+        auto& sg = storage_group_for_token(dk.token());
+        auto max_purgeable_timestamp = api::max_timestamp;
+
+        sg.for_each_compaction_group([&dk, is_shadowable, &max_purgeable_timestamp] (const compaction_group_ptr& cg) {
+            const auto& mt = cg->memtables()->active_memtable();
+            // see get_max_purgeable_timestamp() in compaction.cc for comments on choosing min timestamp
+            api::timestamp_type memtable_min_timestamp = is_shadowable ? mt.get_min_live_row_marker_timestamp() : mt.get_min_live_timestamp();
+            if (memtable_min_timestamp > cg->max_seen_timestamp()) {
+                // All the entries in the memtable are newer than the entries in the
+                // SSTable within this compaction group. So, no need to check further.
+                return;
+            }
+
+            // If a memtable with a minimum timestamp lower than the current maximum
+            // purgeable timestamp has the given key, the tombstone should not be purged.
+            if (memtable_min_timestamp < max_purgeable_timestamp && mt.contains_partition(dk)) {
+                max_purgeable_timestamp = memtable_min_timestamp;
+            }
+        });
+
+        return max_purgeable_timestamp;
+    };
+}
+
 snapshot_source
 table::sstables_as_snapshot_source() {
     return snapshot_source([this] () {
@@ -2486,7 +2512,11 @@ table::sstables_as_snapshot_source() {
             return make_compacting_reader(
                 std::move(reader),
                 gc_clock::now(),
+<<<<<<< HEAD
                 [](const dht::decorated_key&) { return api::max_timestamp; },
+=======
+                get_max_purgeable_fn_for_cache_underlying_reader(),
+>>>>>>> 5a93277904 (replica/table: check memtable before discarding tombstone during read)
                 _compaction_manager.get_tombstone_gc_state(),
                 fwd);
         }, [this, sst_set] {
