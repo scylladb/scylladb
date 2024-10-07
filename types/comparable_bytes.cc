b6ff3f530467bb97b68b44c81e3f2d777d9ae803
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-10-07 12:29:49 +0530

    types/comparable_bytes: support bigint cql3 type
    The bigint type, internally implemented as a long data type, is encoded
    using a variable-length encoding similar to UTF-8. This enables a
    significant amount of space to be saved when smaller numbers are
    frequently used, while still permitting large values to be efficiently
    encoded.
    
    The first bit of the encoding represents the inverted sign (i.e., 1 for
    positive, 0 for negative), followed by length encoded as a sequence of
    bits matching the inverted sign. This is then followed by a differing
    bit (except for 9-byte encodings) and the bits of the number's two's
    complement.
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- types/comparable_bytes.cc
+++ types/comparable_bytes.cc
@@ -16,6 +16,17 @@
 
 logging::logger cblogger("comparable_bytes");
 
+static constexpr uint8_t BYTE_SIGN_MASK = 0x80;
+
+static void read_fragmented_checked(managed_bytes_view& view, size_t bytes_to_read, bytes::value_type* out) {
+    if (view.size_bytes() < bytes_to_read) {
+        throw_with_backtrace<marshal_exception>(
+            format("read_fragmented_checked - not enough bytes (expected {:d}, got {:d})", bytes_to_read, view.size_bytes()));
+    }
+
+    return read_fragmented(view, bytes_to_read, out);
+}
+
 template <std::integral T>
 static void write_native_int(bytes_ostream& out, T value) {
     out.write(bytes_view(reinterpret_cast<const signed char*>(&value), sizeof(T)));
@@ -36,6 +47,104 @@ static void convert_signed_fixed_length_integer(managed_bytes_view& src, bytes_o
     write_native_int<unsigned_type>(out, flipped_value);
 }
 
+/**
+ * Variable-length encoding for signed long data type (int64_t).
+ * The first bit of the coding stores the inverted sign bit followed by as many matching bits as
+ * there are additional bytes in the encoding, followed by the two's complement of the number.
+ *
+ * (i.e) <n bits with inverted sign bit><2's complement of the number>
+ *       where n = number of bytes in encoding and n >= 1
+ *
+ * Because of the inverted sign bit, negative numbers compare smaller than positives, and because the length
+ * bits match the sign, longer positive numbers compare greater and longer negative ones compare smaller.
+ *
+ * Examples:
+ *      0              encodes as           80
+ *      1              encodes as           81
+ *     -1              encodes as           7F
+ *     63              encodes as           BF
+ *     64              encodes as           C040
+ *    -64              encodes as           40
+ *    -65              encodes as           3FBF
+ *   2^20-1            encodes as           EFFFFF
+ *   2^20              encodes as           F0100000
+ *  -2^20              encodes as           100000
+ *   2^63-1            encodes as           FFFFFFFFFFFFFFFFFF
+ *  -2^63              encodes as           000000000000000000
+ *
+ * The encoded value will have a maximum of 9 bytes including the length bits.
+ * As the number of bytes is specified in bits 2-9, no value is a prefix of another.
+ */
+static void encode_signed_long_type(int64_t value, bytes_ostream& out) {
+    // Create a negative mask : -1 for negative numbers and 0 for positive
+    int64_t negative_mask = value >> 63;
+    // If the value is negative, flip all bits so we can treat it like a positive number.
+    // This allows us to unify the transformation logic for both positive and negative values.
+    auto encoded_value = uint64_t(value ^ negative_mask);
+
+    // Count number of significant bits (1-63) that actually needed to be encoded.
+    // It can never be 64 as we have already flipped negative numbers.
+    int num_bits = 64 - std::countl_zero(uint64_t(encoded_value | 1));
+    // Calculate number of bytes required to represent the bits in comparable format.
+    // The encoding includes the encoded length as inverted sign bits and the
+    // actual value is stored as a 2's complement with at least one sign bit preserved.
+    // For example, 32 (0b00100000) can be encoded in a single byte 0b10100000,
+    //          but 64 (0b01000000) requires two bytes : 0b00000001 0b01000000.
+    // So, 0-6 bits => 1 byte; 7-13 => 2 bytes; etc to 56-63 => 9 bytes.
+    // Note that 63 bits require 10 bytes but we handle it as a special case and
+    // encode it within 9 bytes - no sign bit is preserved and the decoding logic
+    // accounts for this scenario.
+    int num_bytes = std::min(9, num_bits / 7 + 1);
+    // Encode the number of bytes as inverted sign bits in the comparable format
+    std::array<char, 9> encoded_buffer;
+    if (num_bytes == 9) {
+        // An extra byte full of inverted sign bits has to be included in the comparable format
+        encoded_buffer[0] = uint8_t(negative_mask) ^ 0xFF;
+        // Incorporate the 9th length bit into the value
+        encoded_value |= 0x8000000000000000L;
+    } else {
+        // Incorporate the length bits into the first non-zero byte of the value
+        int64_t mask = (-0x100 >> num_bytes) & 0xFF;
+        encoded_value = encoded_value | (mask << ((num_bytes * 8) - 8));
+    }
+
+    // Flip the value back to original form and copy the big endian form to the buffer.
+    seastar::write_be(encoded_buffer.data() + 1, encoded_value ^ negative_mask);
+    // Write out the non-zero bytes from the big endian form
+    out.write(encoded_buffer.data() + 9 - num_bytes, num_bytes);
+}
+
+// Refer encode_signed_long_type() for the encoding details
+static void decode_signed_long_type(managed_bytes_view& src, bytes_ostream& out) {
+    const int8_t first_byte = static_cast<int8_t>(src[0]);
+    // Count the number of bytes to read by counting the inverted sign bits in the first byte
+    const int8_t inverted_sign_mask = first_byte >> 7;
+    auto num_bytes = std::countl_zero(uint8_t(first_byte ^ inverted_sign_mask));
+    // Use a buffer to read the serialized bytes, which is in big endian order.
+    std::array<int8_t, 9> buffer;
+    // The buffer[0] value is used only to read the extra sign byte in case of a 9-byte encoding.
+    // The actual value is always read into the buffer starting from buffer[1].
+    auto value_ptr = buffer.data() + 1;
+    if (num_bytes == 8 && ((static_cast<int8_t>(src[1]) >> 7) == inverted_sign_mask)) {
+        // This is a 9-byte encoding. Read one more additional byte into buffer
+        // and ignore the extra byte in the beginning during write.
+        read_fragmented_checked(src, num_bytes + 1, buffer.data());
+        // Flip the 9th inverted sign bit
+        buffer[1] ^= BYTE_SIGN_MASK;
+    } else {
+        // We read the serialized bytes into their positions w.r.to the big endian
+        // order and then fill with sign bytes at the front.
+        int bytes_pos = sizeof(int64_t) - num_bytes;
+        std::memset(value_ptr, ~inverted_sign_mask, bytes_pos);
+        read_fragmented_checked(src, num_bytes, value_ptr + bytes_pos);
+        // Flip the inverted sign bits in first significant byte of the value
+        value_ptr[bytes_pos] ^= int8_t((-0x100 >> num_bytes) & 0xFF);
+    }
+
+    // Write out the entire long value which is in the big endian order.
+    out.write(bytes_view(value_ptr, sizeof(int64_t)));
+}
+
 // to_comparable_bytes_visitor provides methods to
 // convert serialized bytes into byte comparable format.
 struct to_comparable_bytes_visitor {
@@ -53,9 +162,9 @@ struct to_comparable_bytes_visitor {
         convert_signed_fixed_length_integer<T>(serialized_bytes_view, out);
     }
 
+    // The long type uses variable-length encoding, unlike other smaller fixed-length signed integers.
     void operator()(const long_type_impl&) {
-        // Unimplemented
-        SCYLLA_ASSERT(false);
+        encode_signed_long_type(read_simple<int64_t>(serialized_bytes_view), out);
     }
 
     // TODO: Handle other types
@@ -101,8 +210,7 @@ struct from_comparable_bytes_visitor {
     }
 
     void operator()(const long_type_impl&) {
-        // Unimplemented
-        SCYLLA_ASSERT(false);
+        decode_signed_long_type(comparable_bytes_view, out);
     }
 
     // TODO: Handle other types
