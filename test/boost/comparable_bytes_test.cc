192fa8f7d9df384ec88736e83d665016feab0248
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-10-07 12:29:49 +0530

    types/comparable_bytes: support bigint cql3 type
    The bigint type, internally implemented as a long data type, is encoded
    using a variable-length encoding similar to UTF-8. This enables a
    significant amount of space to be saved when smaller numbers are
    frequently used, while still permitting large values to be efficiently
    encoded.
    
    The first bit of the encoding represents the inverted sign (i.e., 1 for
    positive, 0 for negative), followed by length encoded as a sequence of
    bits matching the inverted sign. This is then followed by a differing
    bit (except for 9-byte encodings) and the bits of the number's two's
    complement.
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- test/boost/comparable_bytes_test.cc
+++ test/boost/comparable_bytes_test.cc
@@ -20,6 +20,23 @@ BOOST_AUTO_TEST_CASE(test_comparable_bytes_opt) {
     BOOST_REQUIRE(comparable_bytes::from_serialized_bytes(*int32_type, managed_bytes_opt()) == comparable_bytes_opt());
 }
 
+BOOST_AUTO_TEST_CASE(test_bool) {
+    auto test_bool_value = [] (comparable_bytes_opt& comparable_bytes, bool value) {
+        BOOST_REQUIRE_EQUAL(comparable_bytes->size(), 1);
+        BOOST_REQUIRE_MESSAGE(comparable_bytes->as_managed_bytes_view().front() == uint8_t(value),
+                              fmt::format("comparable bytes encode failed for bool value : {}", value));
+        BOOST_REQUIRE_MESSAGE(value == comparable_bytes->to_data_value(boolean_type),
+                              fmt::format("comparable bytes decode failed for bool value : {}", value));
+    };
+
+    auto cb_false = comparable_bytes::from_data_value(false);
+    test_bool_value(cb_false, false);
+    auto cb_true = comparable_bytes::from_data_value(true);
+    test_bool_value(cb_true, true);
+    // Verify order
+    BOOST_REQUIRE(cb_false < cb_true);
+}
+
 void byte_comparable_test(std::vector<data_value>&& test_data) {
     struct test_item {
         managed_bytes serialized_bytes;
@@ -101,4 +118,8 @@ BOOST_AUTO_TEST_CASE(test_smallint) {
 
 BOOST_AUTO_TEST_CASE(test_int) {
     byte_comparable_test(generate_integer_test_data<int32_t>());
+}
+
+BOOST_AUTO_TEST_CASE(test_bigint) {
+    byte_comparable_test(generate_integer_test_data<int64_t>());
 }
