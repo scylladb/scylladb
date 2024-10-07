f38704eefb08c8ce5ef285b93830c39ccc3fc006
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-10-07 12:29:49 +0530

    types/comparable_bytes: support bigint cql3 type
    The bigint type, internally implemented as a long data type, is encoded
    using a variable-length encoding similar to UTF-8. This enables a
    significant amount of space to be saved when smaller numbers are
    frequently used, while still permitting large values to be efficiently
    encoded.
    
    The first bit of the encoding represents the inverted sign (i.e., 1 for
    positive, 0 for negative), followed by length encoded as a sequence of
    bits matching the inverted sign. This is then followed by a differing
    bit (except for 9-byte encodings) and the bits of the number's two's
    complement.
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- test/boost/comparable_bytes_test.cc
+++ test/boost/comparable_bytes_test.cc
@@ -119,3 +119,7 @@ BOOST_AUTO_TEST_CASE(test_smallint) {
 BOOST_AUTO_TEST_CASE(test_int) {
     byte_comparable_test(generate_integer_test_data<int32_t>());
 }
+
+BOOST_AUTO_TEST_CASE(test_bigint) {
+    byte_comparable_test(generate_integer_test_data<int64_t>());
+}
