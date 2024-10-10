8dbe3518887b65cbc202c7440148df579d25fae0
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2024-10-10 16:24:59 +0300

    client: Introduce retry strategy
    Add `retry_strategy` interface and default implementation for exponential back-off retry strategy

---
+++ utils/s3/retry_strategy.cc
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "retry_strategy.hh"
+#include "aws_error.hh"
+#include "utils/log.hh"
+
+using namespace std::chrono_literals;
+
+namespace aws {
+
+static logging::logger rs_logger("default_retry_strategy");
+
+default_retry_strategy::default_retry_strategy(unsigned max_retries, unsigned scale_factor) : _max_retries(max_retries), _scale_factor(scale_factor) {
+}
+
+bool default_retry_strategy::should_retry(const aws_error& error, unsigned attempted_retries) const {
+    if (attempted_retries >= _max_retries) {
+        rs_logger.error("Retries exhausted. Retry# {}", attempted_retries);
+        return false;
+    }
+    bool should_retry = error.is_retryable() == retryable::yes;
+    if (should_retry) {
+        rs_logger.debug("S3 client request failed. Reason: {}. Retry# {}", error.get_error_message(), attempted_retries);
+    } else {
+        rs_logger.error("S3 client encountered non-retryable error. Reason: {}. Code: {}. Retry# {}",
+                        error.get_error_message(),
+                        std::to_underlying(error.get_error_type()),
+                        attempted_retries);
+    }
+    return should_retry;
+}
+
+std::chrono::milliseconds default_retry_strategy::delay_before_retry(const aws_error&, unsigned attempted_retries) const {
+    if (attempted_retries == 0) {
+        return 0ms;
+    }
+
+    return std::chrono::milliseconds((1UL << attempted_retries) * _scale_factor);
+}
+
+} // namespace aws
---
+++ utils/s3/retry_strategy.hh
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2024-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#pragma once
+#include <chrono>
+
+namespace aws {
+
+class aws_error;
+
+class retry_strategy {
+public:
+    virtual ~retry_strategy() = default;
+    // Returns true if the error can be retried given the error and the number of times already tried.
+    [[nodiscard]] virtual bool should_retry(const aws_error& error, unsigned attempted_retries) const = 0;
+
+    // Calculates the time in milliseconds the client should wait before attempting another request based on the error and attemptedRetries count.
+    [[nodiscard]] virtual std::chrono::milliseconds delay_before_retry(const aws_error& error, unsigned attempted_retries) const = 0;
+
+    [[nodiscard]] virtual unsigned get_max_retries() const = 0;
+};
+
+class default_retry_strategy : public retry_strategy {
+    unsigned _max_retries;
+    unsigned _scale_factor;
+
+public:
+    explicit default_retry_strategy(unsigned max_retries = 10, unsigned scale_factor = 25);
+
+    [[nodiscard]] bool should_retry(const aws_error& error, unsigned attempted_retries) const override;
+
+    [[nodiscard]] std::chrono::milliseconds delay_before_retry(const aws_error& error, unsigned attempted_retries) const override;
+
+    [[nodiscard]] unsigned get_max_retries() const override { return _max_retries; }
+};
+
+} // namespace aws
