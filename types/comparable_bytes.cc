fa371ed21b85f61e34431b0f922a72073117ecfd
Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>
2024-10-11 19:41:18 +0530

    types/comparable_bytes: support date, time and timestamp cql3 types
    Both the date and time cql3 types are internally unsigned fixed length
    integers. Their serialized form is already byte comparable, so the
    encoder and decoder return the serialized bytes as it is.
    
    The timestamp type is encoded using the fixed length signed integer
    encoding.
    
    Signed-off-by: Lakshmi Narayanan Sreethar <lakshmi.sreethar@scylladb.com>

--- types/comparable_bytes.cc
+++ types/comparable_bytes.cc
@@ -167,6 +167,18 @@ struct to_comparable_bytes_visitor {
         encode_signed_long_type(read_simple<int64_t>(serialized_bytes_view), out);
     }
 
+    // Encoding for simple_date_type_impl and time_type_impl
+    // They are both fixed length unsigned integers and are already byte comparable in their serialized form
+    template <std::integral T>
+    void operator()(const simple_type_impl<T>&) {
+        out.write(serialized_bytes_view, sizeof(T));
+    }
+
+    // timestamp_type is encoded as fixed length signed integer
+    void operator()(const timestamp_type_impl&) {
+        convert_signed_fixed_length_integer<db_clock::rep>(serialized_bytes_view, out);
+    }
+
     // TODO: Handle other types
 
     void operator()(const abstract_type& type) {
@@ -210,6 +222,16 @@ struct from_comparable_bytes_visitor {
 
     void operator()(const long_type_impl&) {
         decode_signed_long_type(comparable_bytes_view, out);
+    }
+
+    // Decoder for simple_date_type_impl and time_type_impl; they are written as it is.
+    template <std::integral T>
+    void operator()(const simple_type_impl<T>&) {
+        out.write(comparable_bytes_view, sizeof(T));
+    }
+
+    void operator()(const timestamp_type_impl&) {
+        convert_signed_fixed_length_integer<db_clock::rep>(comparable_bytes_view, out);
     }
 
     // TODO: Handle other types
