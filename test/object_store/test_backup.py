972ff80fada849005c83180dacc71414f9810fbe
Pavel Emelyanov <xemul@scylladb.com>
2024-10-18 11:02:44 +0300

    test: Add scope-streaming test (for restore from backup)
    - create
      - a cluster with given topology
      - keyspace with tablets and given rf value
      - table with some data
    - backup
      - flush all nodes
      - kick backup API on every node
    - re-create keyspace and table
      - drop it first
      - create again with the same parameters and schema, but don't
        populate table with data
    - restore
      - collect nodes to contact and corresponding list of TOCs
        according to the preferred "scope"
      - ask selected nodes to restore, limiting its streaming scope
        and providing the specific list of sstables
    - check
      - select mutation fragments from all nodes for random keys
      - make sure that the number of non-empty responses equals the
        expected rf value
    
    Specific topologies, RFs and stream scopes used are:
    
        rf = 1, nodes = 3, racks = 1, dcs = 1, scope = node
        rf = 3, nodes = 5, racks = 1, dcs = 1, scope = node
        rf = 1, nodes = 4, racks = 2, dcs = 1, scope = rack
        rf = 3, nodes = 6, racks = 2, dcs = 1, scope = rack
        rf = 3, nodes = 6, racks = 3, dcs = 1, scope = rack
        rf = 2, nodes = 8, racks = 4, dcs = 2, scope = dc
    
    nodes and racks are evenly distributed in racks and dcs respectively
    in the last topo RF effectively becomes 4 (2 in each dc)
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- test/object_store/test_backup.py
+++ test/object_store/test_backup.py
@@ -2,13 +2,20 @@
 
 import os
 import logging
+import asyncio
 import pytest
+import time
+import random
 
 from test.pylib.manager_client import ManagerClient
 from test.object_store.conftest import format_tuples
 from test.object_store.conftest import get_s3_resource
 from test.topology.conftest import skip_mode
+from test.topology.util import wait_for_cql_and_get_hosts
+from test.pylib.rest_client import read_barrier
 from test.pylib.util import unique_name
+from cassandra.cluster import ConsistencyLevel, Session
+from cassandra.query import SimpleStatement              # type: ignore # pylint: disable=no-name-in-module
 
 logger = logging.getLogger(__name__)
 
@@ -317,3 +324,123 @@ async def test_simple_backup_and_restore(manager: ManagerClient, s3_server):
 async def test_abort_simple_backup_and_restore(manager: ManagerClient, s3_server):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
     await do_test_simple_backup_and_restore(manager, s3_server, True)
+
+
+# Helper class to parametrize the test below
+class topo:
+    def __init__(self, rf, nodes, racks, dcs):
+        self.rf = rf
+        self.nodes = nodes
+        self.racks = racks
+        self.dcs = dcs
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("topology", [
+        topo(rf = 1, nodes = 3, racks = 1, dcs = 1),
+        topo(rf = 3, nodes = 5, racks = 1, dcs = 1),
+        topo(rf = 1, nodes = 4, racks = 2, dcs = 1),
+        topo(rf = 3, nodes = 6, racks = 2, dcs = 1),
+        topo(rf = 3, nodes = 6, racks = 3, dcs = 1),
+        topo(rf = 2, nodes = 8, racks = 4, dcs = 2)
+    ])
+async def test_restore_with_streaming_scopes(manager: ManagerClient, s3_server, topology):
+    '''Check that restoring of a cluster with stream scopes works'''
+
+    logger.info(f'Start cluster with {topology.nodes} nodes in {topology.dcs} DCs, {topology.racks} racks')
+    cfg = { 'object_storage_config_file': str(s3_server.config_file), 'task_ttl_in_seconds': 300 }
+    cmd = [ '--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug' ]
+    servers = []
+    host_ids = {}
+    for s in range(topology.nodes):
+        dc = f'dc{s % topology.dcs}'
+        rack = f'rack{s % topology.racks}'
+        s = await manager.server_add(config=cfg, cmdline=cmd, property_file={'dc': dc, 'rack': rack})
+        logger.info(f'Created node {s.ip_addr} in {dc}.{rack}')
+        servers.append(s)
+        host_ids[s.server_id] = await manager.get_host_id(s.server_id)
+
+    await manager.api.disable_tablet_balancing(servers[0].ip_addr)
+    cql = manager.get_cql()
+
+    logger.info(f'Create keyspace, rf={topology.rf}')
+    keys = range(256)
+    ks = 'ks'
+    cf = 'cf'
+    replication_opts = format_tuples({'class': 'NetworkTopologyStrategy', 'replication_factor': f'{topology.rf}'})
+    cql.execute((f"CREATE KEYSPACE {ks} WITH REPLICATION = {replication_opts};"))
+
+    schema = f"CREATE TABLE {ks}.{cf} ( pk int primary key, value text );"
+    cql.execute(schema)
+    for k in keys:
+        cql.execute(f"INSERT INTO {ks}.{cf} ( pk, value ) VALUES ({k}, '{k}');")
+
+    logger.info(f'Collect sstables lists')
+    sstables = []
+    for s in servers:
+        await manager.api.flush_keyspace(s.ip_addr, ks)
+        workdir = await manager.server_get_workdir(s.server_id)
+        cf_dir = os.listdir(f'{workdir}/data/{ks}')[0]
+        tocs = [ f.name for f in os.scandir(f'{workdir}/data/{ks}/{cf_dir}') if f.is_file() and f.name.endswith('TOC.txt') ]
+        logger.info(f'Collected sstables from {s.ip_addr}:{cf_dir}: {tocs}')
+        sstables += tocs
+
+    snap_name = unique_name('backup_')
+    logger.info(f'Backup to {snap_name}')
+    prefix = f'{cf}/{snap_name}'
+    async def do_backup(s):
+        await manager.api.take_snapshot(s.ip_addr, ks, snap_name)
+        tid = await manager.api.backup(s.ip_addr, ks, cf, snap_name, s3_server.address, s3_server.bucket_name, prefix)
+        await manager.api.wait_task(s.ip_addr, tid)
+
+    await asyncio.gather(*(do_backup(s) for s in servers))
+
+    logger.info(f'Re-initialize keyspace')
+    cql.execute(f'DROP KEYSPACE {ks}')
+    cql.execute((f"CREATE KEYSPACE {ks} WITH REPLICATION = {replication_opts};"))
+    cql.execute(schema)
+
+    logger.info(f'Restore')
+    async def do_restore(s, toc_names, scope):
+        logger.info(f'Restore {s.ip_addr} with {toc_names}, scope={scope}')
+        tid = await manager.api.restore(s.ip_addr, ks, cf, s3_server.address, s3_server.bucket_name, prefix, toc_names, scope)
+        await manager.api.wait_task(s.ip_addr, tid)
+
+    if topology.dcs > 1:
+        scope = 'dc'
+        r_servers = servers[:topology.dcs]
+    elif topology.racks > 1:
+        scope = 'rack'
+        r_servers = servers[:topology.racks]
+    else:
+        scope = 'node'
+        r_servers = servers
+
+    await asyncio.gather(*(do_restore(s, sstables, scope) for s in r_servers))
+
+    logger.info(f'Check the data is back')
+    async def check_mutations(server, key):
+        host = await wait_for_cql_and_get_hosts(cql, [server], time.time() + 30)
+        await read_barrier(manager.api, server.ip_addr)  # scylladb/scylladb#18199
+        res = await cql.run_async(f"SELECT partition_region FROM MUTATION_FRAGMENTS({ks}.{cf}) WHERE pk={key}", host=host[0])
+        for fragment in res:
+            if fragment.partition_region == 0: # partition start
+                return True
+        return False
+
+    for k in random.sample(keys, 17):
+        res = await asyncio.gather(*(check_mutations(s, k) for s in servers))
+        assert res.count(True) == topology.rf * topology.dcs
+
+    logger.info(f'Validate streaming directions')
+    for i, s in enumerate(r_servers):
+        log = await manager.server_open_log(s.server_id)
+        res = await log.grep(r'INFO.*sstables_loader - load_and_stream:.*target_node=([0-9a-z-]+),.*num_bytes_sent=([0-9]+)')
+        streamed_to = set([ str(host_ids[s.server_id]) ] + [ r[1].group(1) for r in res ])
+        scope_nodes = set([ str(host_ids[s.server_id]) ])
+        # See comment near merge_tocs() above for explanation of servers list filtering below
+        if scope == 'rack':
+            scope_nodes.update([ str(host_ids[s.server_id]) for s in servers[i::topology.racks] ])
+        elif scope == 'dc':
+            scope_nodes.update([ str(host_ids[s.server_id]) for s in servers[i::topology.dcs] ])
+        logger.info(f'{s.ip_addr} streamed to {streamed_to}, expected {scope_nodes}')
+        assert streamed_to == scope_nodes
