67122e580ce07d5c191c1ea4eea3ea7464172f2f
Łukasz Paszkowski <lukasz.paszkowski@scylladb.com>
2024-10-18 15:13:46 +0200

    api/compaction_manager: Extend compaction_history api
    Extend api of /compaction_manager/compaction_history to include
    newly added columns to the compaction history table from the previous
    patches.

--- test/rest_api/test_compactionhistory.py
+++ test/rest_api/test_compactionhistory.py
@@ -3,13 +3,70 @@
 # SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
 
 import requests
-import sys
 import time
 from collections import defaultdict
+from dataclasses import dataclass, field
 
-# Use the util.py library from ../cqlpy:
-sys.path.insert(1, sys.path[0] + '/test/cqlpy')
-from ..cqlpy.util import new_test_table, new_test_keyspace
+from ..cqlpy.util import new_test_table, new_test_keyspace, sleep_till_whole_second
+
+@dataclass
+class TombstonePurgeStats:
+    attempts: int = 0
+    failures_due_to_memtables: int = 0
+    failures_due_to_other_sstables: int = 0
+
+
+@dataclass
+class SStablesStats:
+    input: list[dict] = field(default_factory=list)
+    output: list[dict] = field(default_factory=list)
+
+
+def waitAndGetCompleteCompactionHistory(rest_api, table):
+    # cql-pytest/run.py::run_scylla_cmd() passes "--smp 2" to scylla, so we
+    # use this value to ensure compaction results from all shards arrived
+    # to the table.
+    SCYLLA_SMP_COUNT = 2
+    ks, cf = table.split('.')
+    while True:
+        response = rest_api.send("GET", "compaction_manager/compaction_history")
+        assert response.status_code == requests.codes.ok
+
+        table_entry_count = sum(1 for data in response.json() if data["ks"] == ks and data["cf"] == cf)
+        if table_entry_count == SCYLLA_SMP_COUNT:
+            return response
+
+        assert table_entry_count < SCYLLA_SMP_COUNT
+        time.sleep(0.2)
+
+
+def extractTombstonePurgeStatistics(response, ks) -> TombstonePurgeStats:
+    '''
+    Extract compaction history statistics for a given keyspace ks from the response and squash
+    it as there will be as many items as the number of shards.
+    '''
+    stats = TombstonePurgeStats()
+    for data in response.json():
+        if data["ks"] == ks:
+            stats.attempts += data["total_tombstone_purge_attempt"]
+            stats.failures_due_to_memtables += data["total_tombstone_purge_failure_due_to_overlapping_with_memtable"]
+            stats.failures_due_to_other_sstables += data["total_tombstone_purge_failure_due_to_overlapping_with_uncompacting_sstable"]
+
+    return stats
+
+
+def extractSStablesStatistics(response, ks) -> SStablesStats:
+    '''
+    Extract compaction history statistics for a given keyspace ks from the response and squash
+    it as there will be as many items as the number of shards.
+    '''
+    stats = SStablesStats()
+    for data in response.json():
+        if data["ks"] == ks:
+            stats.input += data["sstables_in"]
+            stats.output += data["sstables_out"]
+
+    return stats
 
 
 def extractRowsMergedAsSortedList(response, ks):
@@ -27,77 +84,153 @@ def extractRowsMergedAsSortedList(response, ks):
     return [{"key": key, "value": value} for key, value in sorted(total.items())]
 
 
+def populateSomeData(cql, cf: str, pk_range: tuple[int], timestamp: int | None = None, step: int = 0):
+    stmt = cql.prepare(f"INSERT INTO {cf} (pk, ck, v) VALUES (?, ?, ?) {'USING TIMESTAMP ?' if timestamp else ''}")
+    for pk in range(*pk_range):
+        for ck in range(1, 6):
+            timestamp = timestamp + step if timestamp is not None else None
+            cql.execute(stmt, [pk, ck*11+100, 0], timestamp)
+
+
+def alterSomeData(cql, cf: str, timestamp: int | None = None):
+    using_timestamp = f"USING TIMESTAMP {timestamp}" if timestamp else ''
+
+    cql.execute(f"DELETE FROM {cf} {using_timestamp} WHERE pk=1 and ck=122")
+    cql.execute(f"DELETE FROM {cf} {using_timestamp} WHERE pk=5 and ck=155")
+    cql.execute(f"DELETE FROM {cf} {using_timestamp} WHERE pk=3 and ck>111 AND ck<144")
+    cql.execute(f"UPDATE {cf} {using_timestamp} SET v=100 WHERE pk=2 AND ck=122")
+    cql.execute(f"DELETE FROM {cf} {using_timestamp} WHERE pk=5")
+
+
 def test_compactionhistory_rows_merged_null_compaction_strategy(cql, rest_api):
     with new_test_keyspace(cql, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
         with new_test_table(cql, ks, "pk int, ck int, v int, PRIMARY KEY (pk, ck)", "WITH compaction = {'class': 'NullCompactionStrategy'};") as cf:
-            stmt = cql.prepare(f"INSERT INTO {cf} (pk, ck, v) VALUES (?, ?, ?)")
-            cql.execute(stmt, [1, 111, 0])
-            cql.execute(stmt, [1, 122, 0])
-            cql.execute(stmt, [1, 133, 0])
-            cql.execute(stmt, [2, 222, 0])
-            cql.execute(stmt, [3, 333, 0])
-            cql.execute(stmt, [3, 344, 0])
-            cql.execute(stmt, [3, 355, 0])
-            cql.execute(stmt, [3, 366, 0])
-            cql.execute(stmt, [3, 377, 0])
-            cql.execute(stmt, [4, 444, 0])
-            cql.execute(stmt, [5, 555, 0])
-
+            populateSomeData(cql, cf, (1, 6))
             response = rest_api.send("POST", f"storage_service/keyspace_flush/{ks}")
             assert response.status_code == requests.codes.ok
 
-            cql.execute(f"DELETE FROM {cf} WHERE pk=1 and ck=122")
-            cql.execute(f"DELETE FROM {cf} WHERE pk=5 and ck=555")
-            cql.execute(f"DELETE FROM {cf} WHERE pk=3 and ck>333 AND ck <366")
-            cql.execute(f"UPDATE {cf} SET v=100 WHERE pk=2 AND ck=222")
-            cql.execute(f"DELETE FROM {cf} WHERE pk=5")
+            alterSomeData(cql, cf)
             response = rest_api.send("POST", f"storage_service/keyspace_flush/{ks}")
             assert response.status_code == requests.codes.ok
 
-            response = rest_api.send("POST", "storage_service/compact")
+            response = rest_api.send("POST", f"storage_service/keyspace_compaction/{ks}")
             assert response.status_code == requests.codes.ok
 
-            response = rest_api.send("GET", "compaction_manager/compaction_history")
-            assert response.status_code == requests.codes.ok
-            assert extractRowsMergedAsSortedList(response, ks) == [{"key": 1, "value": 13}, {"key": 2, "value": 10}]
+            response = waitAndGetCompleteCompactionHistory(rest_api, cf)
+            assert extractRowsMergedAsSortedList(response, ks) == [{"key": 1, "value": 27}, {"key": 2, "value": 10}]
 
 
 def test_compactionhistory_rows_merged_time_window_compaction_strategy(cql, rest_api):
     with new_test_keyspace(cql, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
-        with new_test_table(cql, ks, "pk int, ck int, v int, PRIMARY KEY (pk, ck)", "WITH compaction = {'class': 'TimeWindowCompactionStrategy', 'compaction_window_unit': 'MINUTES', 'compaction_window_size': 1};") as cf:
-            rest_api.send("POST", f"/column_family/autocompaction/{ks}:{cf.split('.')[-1]}")
-            current_time = int(time.time())
+        compaction_opt = "{'class': 'TimeWindowCompactionStrategy', 'compaction_window_unit': 'MINUTES', 'compaction_window_size': 1}"
+        with new_test_table(cql, ks, "pk int, ck int, v int, PRIMARY KEY (pk, ck)",
+                            f"WITH compaction = {compaction_opt};") as cf:
+            timestamp = int(time.time())
 
             # Spread data across 2 windows by simulating a write process. `USING TIMESTAMP` is
             # provided to distribute the writes in the first one-minute window while updates and
             # deletes are propagated into the second 1-minute window.
-            stmt = cql.prepare(f"INSERT INTO {cf} (pk, ck, v) VALUES (?, ?, ?) USING TIMESTAMP ?")
-            cql.execute(stmt, [1, 111, 0, current_time - 60 + 1])
-            cql.execute(stmt, [1, 122, 0, current_time - 60 + 2])
-            cql.execute(stmt, [1, 133, 0, current_time - 60 + 3])
-            cql.execute(stmt, [2, 222, 0, current_time - 60 + 4])
-            cql.execute(stmt, [3, 333, 0, current_time - 60 + 5])
-            cql.execute(stmt, [3, 344, 0, current_time - 60 + 6])
-            cql.execute(stmt, [3, 355, 0, current_time - 60 + 7])
-            cql.execute(stmt, [3, 366, 0, current_time - 60 + 8])
-            cql.execute(stmt, [3, 377, 0, current_time - 60 + 9])
-            cql.execute(stmt, [4, 444, 0, current_time - 60 + 10])
-            cql.execute(stmt, [5, 555, 0, current_time - 60 + 11])
-
+            populateSomeData(cql, cf, (1, 6), timestamp - 60, 1)
             response = rest_api.send("POST", f"storage_service/keyspace_flush/{ks}")
             assert response.status_code == requests.codes.ok
 
-            cql.execute(f"DELETE FROM {cf} WHERE pk=1 and ck=122")
-            cql.execute(f"DELETE FROM {cf} WHERE pk=5 AND ck=555")
-            cql.execute(f"DELETE FROM {cf} WHERE pk=3 and ck>333 AND ck <366")
-            cql.execute(f"UPDATE {cf} SET v=100 WHERE pk=2 AND ck=222")
-            cql.execute(f"DELETE FROM {cf} WHERE pk=5")
+            alterSomeData(cql, cf)
             response = rest_api.send("POST", f"storage_service/keyspace_flush/{ks}")
             assert response.status_code == requests.codes.ok
 
-            response = rest_api.send("POST", "storage_service/compact")
+            response = rest_api.send("POST", f"storage_service/keyspace_compaction/{ks}")
             assert response.status_code == requests.codes.ok
 
-            response = rest_api.send("GET", "compaction_manager/compaction_history")
+            response = waitAndGetCompleteCompactionHistory(rest_api, cf)
+            assert extractRowsMergedAsSortedList(response, ks) == [{"key": 1, "value": 27}, {"key": 2, "value": 10}]
+
+
+def test_compactionhistory_tombstone_purge_statistics(cql, rest_api):
+    with new_test_keyspace(cql, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
+        with new_test_table(cql, ks, "pk int, ck int, v int, PRIMARY KEY (pk, ck)",
+                            "WITH compaction = {'class': 'NullCompactionStrategy'} AND tombstone_gc = {'mode': 'immediate'};") as cf:
+            timestamp = int(time.time())
+
+            populateSomeData(cql, cf, (1, 11), timestamp - 10)
+            response = rest_api.send("POST", "storage_service/flush")
             assert response.status_code == requests.codes.ok
-            assert extractRowsMergedAsSortedList(response, ks) == [{"key": 1, "value": 13}, {"key": 2, "value": 10}]
+
+            populateSomeData(cql, cf, (11, 21), timestamp - 5)
+            alterSomeData(cql, cf, timestamp - 5)
+            response = rest_api.send("POST", "storage_service/flush")
+            assert response.status_code == requests.codes.ok
+
+            # Sleep a second to let the commitlog minimum gc time, in seconds, be greater than the tombstone deletion time
+            sleep_till_whole_second(1)
+            response = rest_api.send("POST", f"storage_service/keyspace_compaction/{ks}")
+            assert response.status_code == requests.codes.ok
+
+            response = waitAndGetCompleteCompactionHistory(rest_api, cf)
+            stats = extractTombstonePurgeStatistics(response, ks)
+            assert stats == TombstonePurgeStats(5, 0, 0)
+
+            stats = extractSStablesStatistics(response, ks)
+            assert len(stats.input) == 4 and len(stats.output) == 2
+
+
+def test_compactionhistory_tombstone_purge_statistics_overlapping_with_memtable(cql, rest_api):
+    with new_test_keyspace(cql, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
+        with new_test_table(cql, ks, "pk int, ck int, v int, PRIMARY KEY (pk, ck)",
+                            "WITH compaction = {'class': 'NullCompactionStrategy'} AND tombstone_gc = {'mode': 'immediate'};") as cf:
+            timestamp = int(time.time())
+
+            populateSomeData(cql, cf, (11, 21), timestamp - 5)
+            alterSomeData(cql, cf, timestamp - 5)
+            response = rest_api.send("POST", "storage_service/flush")
+            assert response.status_code == requests.codes.ok
+
+            # Sleep a second to let the commitlog minimum gc time, in seconds, be greater than the tombstone deletion time
+            sleep_till_whole_second(1)
+            populateSomeData(cql, cf, (1, 11), timestamp - 10)
+            response = rest_api.send("POST", "storage_service/flush")
+            assert response.status_code == requests.codes.ok
+
+            # Do not flush to keep it in memtable
+            cql.execute(f"UPDATE {cf} USING TIMESTAMP {timestamp - 7} SET v=100 WHERE pk=1 AND ck=122")
+            cql.execute(f"UPDATE {cf} USING TIMESTAMP {timestamp - 7} SET v=100 WHERE pk=7 AND ck=122")
+
+            response = rest_api.send("POST", f"storage_service/keyspace_compaction/{ks}", {"flush_memtables": "false"})
+            assert response.status_code == requests.codes.ok
+
+            response = waitAndGetCompleteCompactionHistory(rest_api, cf)
+            stats = extractTombstonePurgeStatistics(response, ks)
+            assert stats == TombstonePurgeStats(5, 1, 0)
+
+
+def test_compactionhistory_tombstone_purge_statistics_overlapping_with_other_sstables(cql, rest_api):
+    with new_test_keyspace(cql, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
+        compaction_opt = "{'class': 'SizeTieredCompactionStrategy', 'min_threshold': 2, 'min_sstable_size': 0}"
+        with new_test_table(cql, ks, "pk int, ck int, v int, PRIMARY KEY (pk, ck)",
+                            f"WITH compaction = {compaction_opt} AND tombstone_gc = {{'mode': 'immediate'}};") as cf:
+            timestamp = int(time.time())
+
+            cql.execute(f"UPDATE {cf} USING TIMESTAMP {timestamp - 7} SET v=100 WHERE pk=1 AND ck=122")
+            cql.execute(f"UPDATE {cf} USING TIMESTAMP {timestamp - 7} SET v=100 WHERE pk=7 AND ck=122")
+            response = rest_api.send("POST", "storage_service/flush")
+            assert response.status_code == requests.codes.ok
+
+            # Now produce two additional sstable that will get into the same bucket
+            # and hence be compacted together but not with the sstable from above.
+            populateSomeData(cql, cf, (11, 21), timestamp - 5)
+            alterSomeData(cql, cf, timestamp - 5)
+            response = rest_api.send("POST", "storage_service/flush")
+            assert response.status_code == requests.codes.ok
+
+            # Sleep a second to let the commitlog minimum gc time, in seconds, be greater than the tombstone deletion time
+            sleep_till_whole_second(1)
+            populateSomeData(cql, cf, (1, 11), timestamp - 10)
+            response = rest_api.send("POST", "storage_service/flush")
+            assert response.status_code == requests.codes.ok
+
+            response = waitAndGetCompleteCompactionHistory(rest_api, cf)
+
+            stats = extractTombstonePurgeStatistics(response, ks)
+            assert stats == TombstonePurgeStats(5, 0, 1)
+
+            stats = extractSStablesStatistics(response, ks)
+            assert len(stats.input) == 4 and len(stats.output) == 2
