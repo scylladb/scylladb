cbd38f643cb47aa988006c488a2840c9a9ca5382
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2024-10-20 13:45:15 +0300

    client: Add retries
    Add retries to the s3 client, all retries are coordinated by an instance of `retry_strategy`. In a case of error also parse response body in attempt to retrieve additional and more focused error information as suggested by AWS. See https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html.

--- utils/s3/client.hh
+++ utils/s3/client.hh
@@ -17,6 +17,7 @@
 #include <filesystem>
 #include "utils/lister.hh"
 #include "utils/s3/creds.hh"
+#include "retry_strategy.hh"
 
 using namespace seastar;
 class memory_data_sink_buffers;
@@ -76,6 +77,7 @@ class client : public enable_shared_from_this<client> {
     using global_factory = std::function<shared_ptr<client>(std::string)>;
     global_factory _gf;
     semaphore& _memory;
+    std::unique_ptr<aws::retry_strategy> _retry_strategy;
 
     struct private_tag {};
 
@@ -83,14 +85,22 @@ class client : public enable_shared_from_this<client> {
 
     void authorize(http::request&);
     group_client& find_or_create_client();
-    future<> make_request(http::request req, http::experimental::client::reply_handler handle = ignore_reply, http::reply::status_type expected = http::reply::status_type::ok);
+
+    using multipart_upload_completion = bool_class<struct is_multipart_upload_completion_req>;
+    static http::experimental::client::reply_handler make_s3_error_handler(http::experimental::client::reply_handler&& handler,
+                                                                           multipart_upload_completion is_mpu_completion);
+    static future<aws::aws_error> look_for_error(const http::reply& reply, input_stream<char>& in_, multipart_upload_completion is_mpu_completion);
+
+    future<> make_request(http::request req, http::experimental::client::reply_handler handle = ignore_reply,
+            http::reply::status_type expected = http::reply::status_type::ok,
+            multipart_upload_completion is_mpu_completion_req = multipart_upload_completion::no);
     using reply_handler_ext = noncopyable_function<future<>(group_client&, const http::reply&, input_stream<char>&& body)>;
     future<> make_request(http::request req, reply_handler_ext handle, http::reply::status_type expected = http::reply::status_type::ok);
-
+    future<> do_retryable_request(group_client& gc, http::request req, http::experimental::client::reply_handler handler) const;
     future<> get_object_header(sstring object_name, http::experimental::client::reply_handler handler);
 public:
 
-    explicit client(std::string host, endpoint_config_ptr cfg, semaphore& mem, global_factory gf, private_tag);
+    client(std::string host, endpoint_config_ptr cfg, semaphore& mem, global_factory gf, private_tag, std::unique_ptr<aws::retry_strategy> rs = std::make_unique<aws::default_retry_strategy>());
     static shared_ptr<client> make(std::string endpoint, endpoint_config_ptr cfg, semaphore& memory, global_factory gf = {});
 
     future<uint64_t> get_object_size(sstring object_name);
