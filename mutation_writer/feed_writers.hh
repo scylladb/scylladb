6a7eaea9f4eca6d6b7f99eaa2f398916b8833b26
Kefu Chai <kefu.chai@scylladb.com>
2024-10-21 17:49:37 +0800

    mutation_writer/feed_writer: remove redundant check
    `mutation_reader::is_end_of_stream()` returns
    `_impl->is_end_of_stream() && is_buffer_empty()`, so
    `!is_end_of_stream()` equals to `
    `!_impl->is_end_of_stream() || !is_buffer_empty()`, which in turn
    always equals to
    `!_impl->is_end_of_stream() || !is_buffer_empty() || !is_buffer_empty()`.
    hence there is no need to check `rd.is_buffer_empty()` again.
    
    in this change, the redundant condition is dropped. simpler this way.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    
    Closes scylladb/scylladb#21224

--- mutation_writer/feed_writers.hh
+++ mutation_writer/feed_writers.hh
@@ -40,7 +40,7 @@ future<> feed_writer(mutation_reader&& rd_ref, Writer wr) {
     auto rd = std::move(rd_ref);
     std::exception_ptr ex;
     try {
-        while (!rd.is_end_of_stream() || !rd.is_buffer_empty()) {
+        while (!rd.is_end_of_stream()) {
             co_await rd.fill_buffer();
             while (!rd.is_buffer_empty()) {
                 co_await rd.pop_mutation_fragment().consume(wr);
