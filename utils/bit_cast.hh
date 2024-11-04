6176ef0b6744101a37524b1dcfefd945cda37293
Michał Chojnowski <michocho98@gmail.com>
2024-11-04 18:03:54 +0100

    utils/bit_cast: add a std::byte* version of write_unaligned

--- utils/bit_cast.hh
+++ utils/bit_cast.hh
@@ -23,8 +23,14 @@ To read_unaligned(const void* src) {
 }
 
 template <TriviallyCopyable From>
-void write_unaligned(void* dst, const From& src) {
+std::byte* write_unaligned(std::byte* dst, const From& src) {
     std::memcpy(dst, &src, sizeof(From));
+    return dst + sizeof(From);
+}
+
+template <TriviallyCopyable From>
+void write_unaligned(void* dst, const From& src) {
+    write_unaligned(reinterpret_cast<std::byte*>(dst), src);
 }
 
 std::span<const std::byte> object_representation(const TriviallyCopyable auto& x) {
