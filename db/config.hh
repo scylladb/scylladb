a248520201b911f09624fa2a8382f83964cadc01
Botond Dénes <bdenes@scylladb.com>
2024-11-06 09:06:18 -0500

    db/config: introduce repair_multishard_reader_enable_read_ahead
    Not used yet.

--- db/config.hh
+++ db/config.hh
@@ -343,6 +343,7 @@ public:
     named_value<double> repair_partition_count_estimation_ratio;
     named_value<uint32_t> repair_hints_batchlog_flush_cache_time_in_ms;
     named_value<uint64_t> repair_multishard_reader_buffer_hint_size;
+    named_value<uint64_t> repair_multishard_reader_enable_read_ahead;
     named_value<uint32_t> ring_delay_ms;
     named_value<uint32_t> shadow_round_ms;
     named_value<uint32_t> fd_max_interval_ms;
