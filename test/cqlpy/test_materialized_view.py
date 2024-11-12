dd8f56ad3a7aa550d01bc6c02fbb53ded4100126
Pavel Emelyanov <xemul@scylladb.com>
2024-11-12 14:57:26 +0300

    test: Move test_query_built_indexes_virtual_table from boost to cqlpy
    And split it into two -- one for materialized view, another for
    secondary index. This is to fit current cqlpy layout that has different
    files for views and indexes.
    
    refs: #21552
    refs: #21551 (detached this patch from there, as that PR needs fix in
    the core code)
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>
    
    Closes scylladb/scylladb#21677

--- test/cqlpy/test_materialized_view.py
+++ test/cqlpy/test_materialized_view.py
@@ -1569,3 +1569,13 @@ def test_views_with_future_tombstones(cql, test_keyspace):
             cql.execute(f'insert into {table} (a,b,c,d,e) values (3,4,5,6,7) using timestamp 18')
             assert [] == list(cql.execute(f'select * from {table}'))
             assert [] == list(cql.execute(f'select * from {mv}'))
+
+# Test view representation in system.* tables
+def test_view_in_system_tables(cql, test_keyspace):
+    with new_test_table(cql, test_keyspace, "p int PRIMARY KEY, v int") as base:
+        with new_materialized_view(cql, base, '*', 'v,p', 'v is not null and p is not null') as view:
+            wait_for_view_built(cql, view)
+            res = [ f'{r.keyspace_name}.{r.view_name}' for r in cql.execute('select * from system.built_views')]
+            assert view in res
+            res = [ f'{r.table_name}.{r.index_name}' for r in cql.execute('select * from system."IndexInfo"')]
+            assert view not in res
