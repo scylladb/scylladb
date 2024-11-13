6a2a18a2fc934deaf543fcf89e5513fb990422b6
Calle Wilund <calle@scylladb.com>
2024-11-13 09:50:05 +0000

    distributed_loader: Add optional abort_source to get_sstables_from_object_store

--- replica/distributed_loader.hh
+++ replica/distributed_loader.hh
@@ -22,6 +22,10 @@
 #include "db/system_keyspace.hh"
 #include "sstables/sstable_directory.hh"
 
+namespace seastar {
+    class abort_source;
+}
+
 namespace replica {
 class database;
 class table;
@@ -89,7 +93,7 @@ public:
     static future<std::tuple<table_id, std::vector<std::vector<sstables::shared_sstable>>>>
             get_sstables_from_upload_dir(distributed<replica::database>& db, sstring ks, sstring cf, sstables::sstable_open_config cfg);
     static future<std::tuple<table_id, std::vector<std::vector<sstables::shared_sstable>>>>
-            get_sstables_from_object_store(distributed<replica::database>& db, sstring ks, sstring cf, std::vector<sstring> sstables, sstring endpoint, sstring bucket, sstring prefix, sstables::sstable_open_config cfg);
+            get_sstables_from_object_store(distributed<replica::database>& db, sstring ks, sstring cf, std::vector<sstring> sstables, sstring endpoint, sstring bucket, sstring prefix, sstables::sstable_open_config cfg, std::function<seastar::abort_source*()> = {});
     static future<> process_upload_dir(distributed<replica::database>& db, sharded<db::view::view_builder>& vb, sstring ks_name, sstring cf_name);
 };
 
