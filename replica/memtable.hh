dbb3f5ab17af613a8e6524bb664945a1fd9a1230
Michał Chojnowski <michocho98@gmail.com>
2024-11-15 12:01:17 +0100

    memtable: ensure _flushed_memory doesn't grow above total memory usage
    dirty_memory_manager tracks two quantities about memtable memory usage:
    "real" and "unspooled" memory usage.
    
    "real" is the total memory usage (sum of `occupancy().total_space()`)
    by all memtable LSA regions, plus a upper-bound estimate of the size of
    memtable data which has already moved to the cache region but isn't
    evictable (merged into the cache) yet.
    
    "unspooled" is the difference between total memory usage by all memtable
    LSA regions, and the total flushed memory (sum of `_flushed_memory`)
    of memtables.
    
    dirty_memory_manager controls the shares of compaction and/or blocks
    writes when these quantities cross various thresholds.
    
    "Total flushed memory" isn't a well defined notion,
    since the actual consumption of memory by the same data can vary over
    time due to LSA compactions, and even the data present in memtable can
    change over the course of the flush due to removals of outdated MVCC versions.
    So `_flushed_memory` is merely an approximation computed by `flush_reader`
    based on the data passing through it.
    
    This approximation is supposed to be a conservative lower bound.
    In particular, `_flushed_memory` should be not greater than
    `occupancy().total_space()`. Otherwise, for example, "unspooled" memory
    could become negative (and/or wrap around) and weird things could happen.
    There is an assertion in ~flush_memory_accounter which checks that
    `_flushed_memory < occupancy().total_space()` at the end of flush.
    
    But it can fail. Without additional treatment, the memtable reader sometimes emits
    data which is already deleted. (In particular, it emites rows covered by
    a partition tombstone in a newer MVCC version.)
    This data is seen `flush_reader` and accounted in `_flushed_memory`.
    But this data can be garbage-collected by the mutation_cleaner later during the
    flush and decrease `total_memory` below `_flushed_memory`.
    
    There is a piece of code in mutation_cleaner intended to prevent that.
    If `total_memory` decreases during a `mutation_cleaner` run,
    `_flushed_memory` is lowered by the same amount, just to preserve the
    asserted property. (This could also make `_flushed_memory` quite inaccurate,
    but that's considered acceptable).
    
    But that only works if `total_memory` is decreased during that run. It doesn't
    work if the `total_memory` decrease (enabled by the new allocator holes made
    by `mutation_cleaner`'s garbage collection work) happens asynchronously
    (due to memory reclaim for whatever reason) after the run.
    
    This patch fixes that by tracking the decreases of `total_memory` closer to the
    source. Instead of relying on `mutation_cleaner` to notify the memtable if it
    lowers `total_memory`, the memtable itself listens for notifications about
    LSA segment deallocations. It keeps `_flushed_memory` equal to the reader's
    estimate of flushed memory decreased by the change in `total_memory` since the
    beginning of flush (if it was positive), and it keeps the amount of "spooled"
    memory reported to the `dirty_memory_manager` at `max(0, _flushed_memory)`.
    
    (cherry picked from commit 975e7e405a3ab3e41f362e6c9fa5f4eec10fb3ff)

--- replica/memtable.hh
+++ replica/memtable.hh
@@ -130,8 +130,25 @@ private:
     // mutation_source is necessary for the combined mutation source to be
     // monotonic. That combined source in this case is cache + memtable.
     mutation_source_opt _underlying;
-    uint64_t _flushed_memory = 0;
     bool _merging_into_cache = false;
+    // Tracks the difference between the amount of memory "spooled" during the flush
+    // and the memory freed during the flush.
+    //
+    // If positive, this is equal to the difference between the amount of "spooled" memory
+    // registered in dirty_memory_manager with account_potentially_cleaned_up_memory
+    // and unregistered with revert_potentially_cleaned_up_memory.
+    // If negative, the above difference is 0.
+    int64_t _flushed_memory = 0;
+    // For most of the time, this is equal to occupancy().total_memory.
+    // But we want to know the current memory usage in our logalloc::region_listener
+    // handlers, and at that point in time occupancy() is undefined. (LSA can choose to
+    // update it before or after the handler). So we track it ourselves, based on the deltas
+    // passed to the handlers.
+    uint64_t _total_memory = 0;
+    // During LSA compaction, _total_memory can fluctuate up and down.
+    // But we are only interested in the maximal total decrease since the beginning of flush.
+    // This tracks the lowest value of _total_memory seen during the flush.
+    uint64_t _total_memory_low_watermark_during_flush = 0;
     bool _merged_into_cache = false;
     replica::table_stats& _table_stats;
 
@@ -199,7 +216,6 @@ private:
     void add_flushed_memory(uint64_t);
     void remove_flushed_memory(uint64_t);
     void clear() noexcept;
-    uint64_t dirty_size() const;
 public:
     explicit memtable(schema_ptr schema, dirty_memory_manager&,
             memtable_table_shared_data& shared_data,
