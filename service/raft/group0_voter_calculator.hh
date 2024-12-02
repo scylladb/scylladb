32f937dc21bae1ea2e642bf916601a805fd3f9c6
Emil Maskovsky <emil.maskovsky@scylladb.com>
2024-12-02 22:24:06 +0100

    raft: implement the limited voters feature
    Currently if raft is enabled all nodes are voters in group0. However it
    is not necessary to have all nodes to be voters - it only slows down
    the raft group operation (since the quorum is large) and makes
    deployments with asymmetrical DCs problematic (2 DCs with 5 nodes along
    1 DC with 10 nodes will lose the majority if large DC is isolated).
    
    The topology coordinator will now maintain a state where there are only
    limited number of voters, evenly distributed across the DCs and racks.
    
    After each node addition or removal the voters are recalculated and
    rebalanced if necessary. That means:
    * When a new node is added, it might become a voter depending on the
      current distribution of voters - either if there are still some voter
      "slots" available, or if the new node is a better candidate than some
      existing voter (in which case the existing node voter status might be
      revoked).
    * When a voter node is removed or stopped (shut down), its voter status
      is revoked and another node might become a voter instead (this can also
      depend on other circumstances, like e.g. changing the number of DCs).
    * If a node addition or removal causes a change in number of datacenters
      (DCs) or racks, the rebalance action might become wider (as there are
      some special rules applying to 1 vs 2 vs more DCs, also changing the
      number of racks might cause similar effects in the voters distribution)
    
    Special conditions for various number of DCs:
    * 1 DC: Can have up to the maximum allowed number of voters (5 - see below)
    * 2 DCs: The distribution of the voters will be asymmetric (if possible),
      meaning that we can tolerate a loss of the DC with the smaller number
      of voters (if both would have the same number of voters we'd lose the
      majority if any of the DCs is lost).
      For example, if we have 2 DCs with 2 nodes each, one of them will only
      have 1 voter (despite the limit of 5). Also, if one of the 2 DCs has
      more racks than the other and the node count allows it, the DC with
      the more racks will have more voters.
    * 3 and more DCs: The distribution of the voters will be so that every
      DC has strictly less than half of the total voters (so a loss of any
      of the DCs cannot lead to the majority loss). Again, DCs with more
      racks are being preferred in the voter distribution.
    
    At the moment we will be handling the zero-token nodes in the same way
    as the regular nodes (i.e. the zero-token nodes will not take any
    priority in the voter distribution). Technically it doesn't make much
    sense to have a zero-token node that is not a voter (when there are
    regular nodes in the same DC being voters), but currently the intended
    purpose of zero-token nodes is to form an "arbiter DC" (in case of 2 DCs,
    creating a third DC with zero-token nodes only), so for that intended
    purpose no special handling is needed and will work out of the box.
    If a preference of zero token nodes will eventually be needed/requested,
    it will be added separately from this PR.
    
    Currently the voter limits will not be configurable (we might introduce
    configurable limits later if that would be needed/requested).
    
    The feature is enabled by the `group0_limited_voters` feature flag
    to avoid issues with cluster upgrade (the feature will be only enabled
    once all nodes in the cluster are upgraded to the version supporting
    the feature).
    
    Fixes: scylladb/scylladb#18793

---
+++ service/raft/group0_voter_calculator.hh
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include "raft/raft.hh"
+
+namespace service {
+
+// The voter calculator is responsible for distributing voters in group 0.
+//
+// It calculates the optimal distribution of voters across datacenters and racks, ensuring:
+// 1. The number of voters in each datacenter is balanced.
+// 2. No single datacenter or rack holds at least half of the total voters (when the number of DCs/racks >= 3).
+//
+// For scenarios with 2 datacenters (or 2 racks in a single datacenter), the calculator ensures an asymmetric
+// distribution of voters. This allows the system to tolerate the loss of the datacenter or rack with fewer voters.
+//
+// Additionally, the calculator maximizes the "distance" between voters in group 0. The "distance" is based on
+// node properties, such as datacenter or rack location. For example, nodes in different datacenters are considered
+// farther apart than nodes in the same datacenter. This approach ensures an even distribution of voters across
+// datacenters and racks.
+class group0_voter_calculator {
+
+    size_t _voters_max;
+
+public:
+    // Constructs a new voter calculator instance.
+    //
+    // The voter registry is initialized with the `voters_max` parameter that specifies the maximum number of voters
+    // allowed in group 0. If not provided, the default behavior allows the default maximum number of voters.
+    explicit group0_voter_calculator(std::optional<size_t> voters_max = std::nullopt);
+
+    struct node_descriptor {
+        sstring datacenter;
+        sstring rack;
+        bool is_voter;
+        bool is_alive;
+    };
+
+    using nodes_list_t = std::unordered_map<raft::server_id, node_descriptor>;
+
+    using voters_set_t = std::unordered_set<raft::server_id>;
+
+    // Calculates the optimal distribution of voters across datacenters and racks.
+    //
+    // Parameters:
+    // - `nodes`: A map where each entry represents a node in group 0. The key is the node's
+    //            unique server ID, and the value is a descriptor containing the node's
+    //            datacenter, rack, voter status, and liveness information.
+    //
+    // Returns:
+    // A set of server IDs representing the nodes selected as voters. The returned set ensures compliance
+    // with the balancing and fault-tolerance rules for group 0.
+    [[nodiscard]] voters_set_t distribute_voters(const nodes_list_t& nodes) const;
+};
+
+} // namespace service
