cd5d1d3c990afeacdf0ef4fddf3a27829e4ad3d6
Raphael S. Carvalho <raphaelsc@scylladb.com>
2024-12-03 21:16:08 -0300

    gms: Add cluster feature for tablet merge
    The reason we need it is that tablet merge can only be finalized
    when the cluster agrees on the feature, otherwise unpatched
    nodes would fail to handle merge finalization, potentially
    crashing.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -145,6 +145,7 @@ public:
     gms::feature maintenance_tenant { *this, "MAINTENANCE_TENANT"sv };
 
     gms::feature tablet_repair_scheduler { *this, "TABLET_REPAIR_SCHEDULER"sv };
+    gms::feature tablet_merge { *this, "TABLET_MERGE"sv };
 
     // A feature just for use in tests. It must not be advertised unless
     // the "features_enable_test_feature" injection is enabled.
