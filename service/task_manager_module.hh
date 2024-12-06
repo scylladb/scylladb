d66697041c88d710586e8b7ead022134c31c0045
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2024-12-06 11:03:53 +0100

    service: tasks: make get_table_id a method of virtual_task_hint

--- service/task_manager_module.hh
+++ service/task_manager_module.hh
@@ -42,7 +42,6 @@ public:
 private:
     std::vector<table_id> get_table_ids() const;
     future<std::optional<tasks::task_status>> get_status_helper(tasks::task_id id, utils::chunked_vector<locator::tablet_id>& tablets, tasks::virtual_task_hint hint);
-    table_id get_table_id(const tasks::virtual_task_hint& hint) const;
 };
 
 class task_manager_module : public tasks::task_manager::module {
