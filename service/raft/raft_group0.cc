1213c977fe283c8c63e9903368a00fdee1f62df3
Patryk Jędrzejczak <patryk.jedrzejczak@scylladb.com>
2024-12-12 19:30:59 +0100

    treewide: allow recreating group 0 in the Raft-based recovery procedure
    This patch adds support for recreating group 0 after losing
    majority. This is the only part of the new Raft-based recovery
    procedure that touches Scylla core.
    
    The following steps are necessary to recreate group 0:
    1. Determine the new group 0 members. These are alive nodes that
    are normal or rebuilding.
    2. Choose the recovery leader - the node which will become the
    new group 0 leader. This must be one of the nodes with the
    latest persistent group 0 state.
    3. Remove `raft_group_id` from `system.scylla_local` and truncate
    `system.discovery` on each live node.
    4. Set the new scylla.yaml parameter - `recovery_leader` - to Host
    ID of the recovery leader on each live node.
    5. Rolling restart all live nodes, but the recovery leader must be
    restarted first.
    
    In the implementation, restarts in step 5 are very similar to normal
    restarts with the Raft-based topology enabled. The only differences
    are:
    1. Steps 3-4 make the restarting node discover the new group 0
    in `join_cluster`.
    2. The group 0 server is started in `join_group0`, not
    `setup_group0_if_exists`.
    3. The restarting node joins the new group 0 in `join_topology` using
    `legacy_handshaker`. There is no reason to contact the topology
    coordinator since the node has already joined the topology.
    
    Unfortunately, this patch creates another execution path for the
    starting logic. `join_cluster` becomes even messier. However, there
    is nothing we can do about it. Joining group 0 without joining
    topology is something completely new. Having a few small changes
    without touching other execution paths is the best we can do.
    We will start removing the old stuff soon, after making the
    Raft-based topology mandatory, and the situation will improve.
    
    (cherry picked from commit fd51d7e4481988b166a635dd8a1f14a803e18cd0)

--- service/raft/raft_group0.cc
+++ service/raft/raft_group0.cc
@@ -25,6 +25,7 @@
 #include "direct_failure_detector/failure_detector.hh"
 #include "gms/gossiper.hh"
 #include "gms/feature_service.hh"
+#include "db/config.hh"
 #include "db/system_keyspace.hh"
 #include "replica/database.hh"
 #include "utils/assert.hh"
@@ -529,6 +530,26 @@ future<> raft_group0::join_group0(std::vector<gms::inet_address> seeds, shared_p
                 // We should start a new group with this node as voter.
                 group0_log.info("Server {} chosen as discovery leader; bootstrapping group 0 from scratch", my_id);
                 initial_configuration.current.emplace(my_addr, true);
+<<<<<<< HEAD
+||||||| parent of fd51d7e448 (treewide: allow recreating group 0 in the Raft-based recovery procedure)
+
+                // Initializes system tables for the first group 0 member. Nodes joining group 0 henceforth would apply them via snapshots.
+                if (topology_change_enabled) {
+                    co_await ss.raft_initialize_discovery_leader(params);
+                }
+
+=======
+
+                // Initializes system tables for the first group 0 member. Nodes joining group 0 henceforth would apply them via snapshots.
+                // We should not change system tables on the recovery leader (the discovery leader of the new group 0
+                // created in the Raft-based recovery procedure). The persistent topology state is present on that node
+                // when it creates the new group 0. Also, it joins the new group 0 using legacy_handshaker, so there is
+                // no need to create a join request.
+                if (topology_change_enabled && qp.db().get_config().recovery_leader().empty()) {
+                    co_await ss.raft_initialize_discovery_leader(params);
+                }
+
+>>>>>>> fd51d7e448 (treewide: allow recreating group 0 in the Raft-based recovery procedure)
                 // Force snapshot transfer from us to subsequently joining servers.
                 // This is important for upgrade and recovery, where the group 0 state machine
                 // (schema tables in particular) is nonempty.
@@ -694,8 +715,9 @@ future<> raft_group0::setup_group0_if_exist(db::system_keyspace& sys_ks, service
         } else {
             // We'll disable them once we complete the upgrade procedure.
         }
-    } else {
-        // Scylla has bootstrapped earlier but group 0 ID not present. This means we're upgrading.
+    } else if (qp.db().get_config().recovery_leader().empty()) {
+        // Scylla has bootstrapped earlier but group 0 ID is not present and we are not recovering from majority loss
+        // using the Raft-based procedure. This means we're upgrading.
         // Upgrade will start through a feature listener created after we enter NORMAL state.
         //
         // See `raft_group0::finish_setup_after_join`.
@@ -710,13 +732,25 @@ future<> raft_group0::setup_group0(
         db::system_keyspace& sys_ks, const std::unordered_set<gms::inet_address>& initial_contact_nodes, shared_ptr<group0_handshaker> handshaker,
         std::optional<replace_info> replace_info, service::storage_service& ss, cql3::query_processor& qp, service::migration_manager& mm, bool topology_change_enabled) {
     if (!co_await use_raft()) {
+        // The node is in the RECOVERY mode. We are in Maintenence Mode or the gossip-based recovery procedure.
         co_return;
     }
 
-    if (sys_ks.bootstrap_complete()) {
-        // If the node is bootstrapped the group0 server should be setup already
+    if (_sys_ks.bootstrap_complete() && !ss.raft_topology_change_enabled()) {
+        // The node is restarting in the gossip-based topology. There are two possible cases:
+        // - the node is performing a normal restart and group 0 has already been set up,
+        // - the node has just left the RECOVERY mode (a part of the gossip-based recovery procedure) and the internal
+        // upgrade-to-raft procedure will set up group 0 in finish_setup_after_join.
         co_return;
     }
+
+    if (joined_group0()) {
+        // Group 0 is already set up, there is nothing to do.
+        co_return;
+    }
+    // Reaching this point is possible only in two cases:
+    // - the node is bootstrapping,
+    // - the node is restarting in the Raft-based recovery procedure and has not joined the new group 0 yet.
 
     std::vector<gms::inet_address> seeds(initial_contact_nodes.begin(), initial_contact_nodes.end());
 
