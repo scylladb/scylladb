3e2c0663787cbd14d3b1702ebefdf6d7829b0fc3
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2024-12-15 14:15:09 +0200

    aws creds: add STS and Instance Metadata service credentials providers
    This commit introduces two new credentials providers: STS and Instance Metadata Service. The S3 client's provider chain has been updated to incorporate these new providers. Additionally, unit tests have been added to ensure coverage of the new functionality.

--- main.cc
+++ main.cc
@@ -177,6 +177,7 @@ struct convert<::object_storage_endpoint_param> {
         ep.config.port = node["port"].as<unsigned>();
         ep.config.use_https = node["https"].as<bool>(false);
         ep.config.region = node["aws_region"] ? node["aws_region"].as<std::string>() : std::getenv("AWS_DEFAULT_REGION");
+        ep.config.role_arn = node["iam_role_arn"] ? std::make_optional(node["iam_role_arn"].as<std::string>()) : std::nullopt;
         return true;
     }
 };
