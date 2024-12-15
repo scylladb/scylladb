b00f5a6070be0ee1b06c4bff026cb43983ac0161
Nadav Har'El <nyh@scylladb.com>
2024-12-15 12:25:15 +0200

    test/alternator: test that DescribeTable doesn't return IndexStatus for LSI
    Whereas GSIs have an IndexStatus when described by DescribeTable,
    LSIs do not. The purpose of IndexStatus is to tell when the index is live,
    and this is not needed for LSIs because they cannot be added to a base
    table that already exists.
    
    We already had a test for this, but it was hidden in an xfailing test
    for many different DescribeTable attributes - so let's move it into it's
    own, *passing*, test. The new tests passes on both Alternator and
    Amazon DynamoDB.
    
    This test is an important regression test for when we later add
    IndexStatus support to GSI, and this test will ensure that we don't
    accidentally introduce IndexStatus to LSIs as well - DynamoDB doesn't
    generate it for LSIs so neither should Alternator.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- test/alternator/test_lsi.py
+++ test/alternator/test_lsi.py
@@ -248,13 +248,21 @@ def test_lsi_describe(test_table_lsi_4):
         assert lsi['IndexArn'] == desc['Table']['TableArn'] + '/index/' + lsi['IndexName']
         assert lsi['Projection'] == {'ProjectionType': 'ALL'}
 
+# Whereas GSIs have an IndexStatus when described by DescribeTable,
+# LSIs do not. IndexStatus is not needed because LSIs cannot be added
+# after the base table is created.
+def test_lsi_describe_indexstatus(test_table_lsi_1):
+    desc = test_table_lsi_1.meta.client.describe_table(TableName=test_table_lsi_1.name)
+    assert 'Table' in desc
+    assert 'LocalSecondaryIndexes' in desc['Table']
+    lsis = desc['Table']['LocalSecondaryIndexes']
+    assert len(lsis) == 1
+    lsi = lsis[0]
+    assert not 'IndexStatus' in lsi
+
 # In addition to the basic listing of an LSI in DescribeTable tested above,
 # in this test we check additional fields that should appear in each LSI's
 # description.
-# Note that whereas GSIs also have IndexStatus and ProvisionedThroughput
-# fields, LSIs do not. IndexStatus is not needed because LSIs cannot be
-# added after the base table is created, and ProvisionedThroughput isn't
-# needed because an LSI shares its provisioning with the base table.
 @pytest.mark.xfail(reason="issues #7550, #11466")
 def test_lsi_describe_fields(test_table_lsi_1):
     desc = test_table_lsi_1.meta.client.describe_table(TableName=test_table_lsi_1.name)
@@ -266,7 +274,8 @@ def test_lsi_describe_fields(test_table_lsi_1):
     assert lsi['IndexName'] == 'hello'
     assert 'IndexSizeBytes' in lsi     # actual size depends on content
     assert 'ItemCount' in lsi
-    assert not 'IndexStatus' in lsi
+    # Whereas GSIs has ProvisionedThroughput, LSIs do not. An LSI shares
+    # its provisioning with the base table.
     assert not 'ProvisionedThroughput' in lsi
     assert lsi['KeySchema'] == [{'KeyType': 'HASH', 'AttributeName': 'p'},
                                 {'KeyType': 'RANGE', 'AttributeName': 'b'}]
