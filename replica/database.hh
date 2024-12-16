6e1635bc2459cc110c9dec96f19bd78043da5127
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2024-12-16 13:40:39 +0100

    compaction: set _child_count value for global cleanup task
    Filter out local keyspaces and keyspaces that use tablets before
    the cleanup loop. Set global_cleanup_compaction_task_impl::_child_count
    based on the number of those keyspaces.

--- replica/database.hh
+++ replica/database.hh
@@ -1792,6 +1792,7 @@ public:
     std::vector<sstring> get_user_keyspaces() const;
     std::vector<sstring> get_all_keyspaces() const;
     std::vector<sstring> get_non_local_strategy_keyspaces() const;
+    std::vector<sstring> get_non_local_strategy_non_tablets_keyspaces() const;
     std::vector<sstring> get_non_local_vnode_based_strategy_keyspaces() const;
     std::unordered_map<sstring, locator::vnode_effective_replication_map_ptr> get_non_local_strategy_keyspaces_erms() const;
     std::vector<sstring> get_tablets_keyspaces() const;
