55ce2c6e61a945ecd06240a6631120c215718f1f
Asias He <asias@scylladb.com>
2024-12-19 21:26:12 +0800

    repair: Add repair_hosts_filter and repair_dcs_filter
    They will be useful for hosts and DCs selection for the repair
    scheduler. It is not implemented yet. Adding it earlier, so we do not
    need to change the system tabler later.

--- locator/tablets.hh
+++ locator/tablets.hh
@@ -158,6 +158,8 @@ struct tablet_task_info {
     db_clock::time_point request_time;
     int64_t sched_nr = 0;
     db_clock::time_point sched_time;
+    sstring repair_hosts_filter;
+    sstring repair_dcs_filter;
     bool operator==(const tablet_task_info&) const = default;
     bool is_valid() const;
     bool is_user_request() const;
