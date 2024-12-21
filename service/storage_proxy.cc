9fbf5673426cf9557687ccbdde8c260098ddc98a
Benny Halevy <bhalevy@scylladb.com>
2024-12-21 11:51:49 +0200

    storage_proxy: stop: close _pending_writes_phaser
    We take care to start_write() in the write response handlers,
    but we don't await them on stop().  await_pending_writes() is
    currently only used by storage_service::sstable_cleanup_fiber
    to wait for all local writes to complete before cleanup.
    
    close the _pending_writes_phaser in stop() to make sure there are no
    pending writes hanging around after storage_proxy is stopped
    and prevent any new writes from starting.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- service/storage_proxy.cc
+++ service/storage_proxy.cc
@@ -6958,7 +6958,7 @@ future<> storage_proxy::abort_view_writes() {
 
 future<>
 storage_proxy::stop() {
-    return make_ready_future<>();
+    return _pending_writes_phaser.close();
 }
 
 locator::token_metadata_ptr storage_proxy::get_token_metadata_ptr() const noexcept {
