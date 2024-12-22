f3667eb937685de51b4f685ceda01619629747d5
Benny Halevy <bhalevy@scylladb.com>
2024-12-22 17:09:34 +0200

    replica: table: make generate_and_propagate_view_updates abortable
    Pass a reference to the table's abort_source to
    `generate_and_propagate_view_updates` so to abort
    it when abort_requested, similar to the way it can
    timeout today.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- db/view/view_update_generator.cc
+++ db/view/view_update_generator.cc
@@ -6,6 +6,8 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+#include <seastar/core/sleep.hh>
+
 #include "db/view/view_update_backlog.hh"
 #include <seastar/core/timed_out_error.hh>
 #include "gms/inet_address.hh"
@@ -411,7 +413,8 @@ future<> view_update_generator::generate_and_propagate_view_updates(const replic
         mutation_reader_opt existings,
         tracing::trace_state_ptr tr_state,
         gc_clock::time_point now,
-        db::timeout_clock::time_point timeout) {
+        db::timeout_clock::time_point timeout,
+        abort_source& abort) {
     auto base_token = m.token();
     auto m_schema = m.schema();
     view_update_builder builder = make_view_update_builder(
@@ -456,7 +459,7 @@ future<> view_update_generator::generate_and_propagate_view_updates(const replic
             update_backlog local_backlog = _db.get_view_update_backlog();
             std::chrono::microseconds throttle_delay =  calculate_view_update_throttling_delay(local_backlog, timeout, _db.get_config().view_flow_control_delay_limit_in_ms());
 
-            co_await seastar::sleep(throttle_delay);
+            co_await seastar::sleep_abortable(throttle_delay, abort);
 
             if (utils::get_local_injector().enter("view_update_limit") && _db.view_update_sem().current() == 0) {
                 err = std::make_exception_ptr(std::runtime_error("View update backlog exceeded the limit"));
@@ -467,6 +470,8 @@ future<> view_update_generator::generate_and_propagate_view_updates(const replic
                 err = std::make_exception_ptr(view_update_generation_timeout_exception());
                 break;
             }
+        } else {
+            abort.check();
         }
 
         try {
