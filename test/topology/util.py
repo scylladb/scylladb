dbaba3eca55b872f6438bac9a8c88bebba963d82
Patryk Jędrzejczak <patryk.jedrzejczak@scylladb.com>
2024-12-24 11:13:24 +0100

    test: topology: util: extend start_writes
    We extend `start_writes` to allow:
    - providing `ks_name` from the test,
    - restarting it (by starting it again with the same `ks_name`),
    - running it in the presence of shutdowns.
    
    We use these features in a new test in one of the following patches.

--- test/topology/util.py
+++ test/topology/util.py
@@ -285,15 +285,17 @@ async def check_node_log_for_failed_mutations(manager: ManagerClient, server: Se
     assert len(occurrences) == 0
 
 
-async def start_writes(cql: Session, rf: int, cl: ConsistencyLevel, concurrency: int = 3):
+async def start_writes(cql: Session, rf: int, cl: ConsistencyLevel, concurrency: int = 3,
+                       ks_name: Optional[str] = None, node_shutdowns: bool = False):
     logging.info(f"Starting to asynchronously write, concurrency = {concurrency}")
 
     stop_event = asyncio.Event()
 
-    ks_name = unique_name()
-    await cql.run_async(f"CREATE KEYSPACE {ks_name} WITH replication = {{'class': 'NetworkTopologyStrategy', 'replication_factor': {rf}}}")
+    if ks_name is None:
+        ks_name = unique_name()
+    await cql.run_async(f"CREATE KEYSPACE IF NOT EXISTS {ks_name} WITH replication = {{'class': 'NetworkTopologyStrategy', 'replication_factor': {rf}}}")
     await cql.run_async(f"USE {ks_name}")
-    await cql.run_async(f"CREATE TABLE tbl (pk int PRIMARY KEY, v int)")
+    await cql.run_async(f"CREATE TABLE IF NOT EXISTS tbl (pk int PRIMARY KEY, v int)")
 
     # In the test we only care about whether operations report success or not
     # and whether they trigger errors in the nodes' logs. Inserting the same
@@ -307,6 +309,12 @@ async def start_writes(cql: Session, rf: int, cl: ConsistencyLevel, concurrency:
             try:
                 await cql.run_async(stmt)
                 write_count += 1
+            except NoHostAvailable as e:
+                for _, err in e.errors.items():
+                    # ConnectionException can be raised when the node is shutting down.
+                    if not node_shutdowns or not isinstance(err, ConnectionException):
+                        logger.error(f"Write started {time.time() - start_time}s ago failed: {e}")
+                        raise
             except Exception as e:
                 logging.error(f"Write started {time.time() - start_time}s ago failed: {e}")
                 raise
