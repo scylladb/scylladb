32c86b69128ee9b7654cffdacaf9126d4aba5753
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-01-09 11:35:39 +0100

    message/messaging_service: add view building RPCs
    This patch adds:
    - `build_views_range` - to build particular range on given host and
      shard
    - `abort_vbc_work` - to abort work on a particular shard on given host

--- message/messaging_service.hh
+++ message/messaging_service.hh
@@ -202,7 +202,9 @@ enum class messaging_verb : int32_t {
     TASKS_GET_CHILDREN = 74,
     TABLET_REPAIR = 75,
     TRUNCATE_WITH_TABLETS = 76,
-    LAST = 77,
+    BUILD_VIEWS_RANGE = 77,
+    ABORT_VBC_WORK = 78,
+    LAST = 79,
 };
 
 } // namespace netw
