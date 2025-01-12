d1f960eee257bcf0c59be8f6f1dfc0ac615544a2
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-01-13 00:30:03 +0100

    test/topology_custom: Add test for Scylla with disabled view building
    Before this commit, there doesn't seem to have been a test verifying that
    starting and shutting down Scylla behave correctly when the configuration
    option `view_building` is set to false. In these changes, we add one.

--- test/topology_custom/test_mv_building.py
+++ test/topology_custom/test_mv_building.py
@@ -42,3 +42,15 @@ async def test_view_building_scheduling_group(manager: ManagerClient):
     ratio = ms_statement / ms_streaming
     print(f"ms_streaming: {ms_streaming}, ms_statement: {ms_statement}, ratio: {ratio}")
     assert ratio < 0.1
+
+# A sanity check test ensures that starting and shutting down Scylla when view building is
+# disabled is conducted properly and we don't run into any issues.
+@pytest.mark.asyncio
+async def test_start_scylla_with_view_building_disabled(manager: ManagerClient):
+    server = await manager.server_add(config={"view_building": "false"})
+    await manager.server_stop_gracefully(server_id=server.server_id)
+
+    # Make sure there have been no errors.
+    log = await manager.server_open_log(server.server_id)
+    res = await log.grep(r"ERROR.*\[shard [0-9]+:[a-z]+\]")
+    assert len(res) == 0
