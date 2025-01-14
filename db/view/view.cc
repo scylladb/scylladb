b1be2d3c41b2ee9c3aaccf0b09ffd0a7bf4fc7cb
Michael Litvak <michael.litvak@scylladb.com>
2025-01-14 11:42:31 +0200

    view_builder: write status to tables before starting to build
    When adding a new view for building, first write the status to the
    system tables and then add the view building step that will start
    building it.
    
    Otherwise, if we start building it before the status is written to the
    table, it may happen that we complete building the view, write the
    SUCCESS status, and then overwrite it with the STARTED status. The
    view_build_status table will remain in incorrect state indicating the
    view building is not complete.
    
    Fixes scylladb/scylladb#20638

--- db/view/view.cc
+++ db/view/view.cc
@@ -2448,11 +2448,11 @@ view_builder::view_build_statuses(sstring keyspace, sstring view_name) const {
 
 future<> view_builder::add_new_view(view_ptr view, build_step& step) {
     vlogger.info0("Building view {}.{}, starting at token {}", view->ks_name(), view->cf_name(), step.current_token());
+    if (this_shard_id() == 0) {
+        co_await mark_view_build_started(view->ks_name(), view->cf_name());
+    }
+    co_await _sys_ks.register_view_for_building(view->ks_name(), view->cf_name(), step.current_token());
     step.build_status.emplace(step.build_status.begin(), view_build_status{view, step.current_token(), std::nullopt});
-    auto f = this_shard_id() == 0 ? mark_view_build_started(view->ks_name(), view->cf_name()) : make_ready_future<>();
-    return when_all_succeed(
-            std::move(f),
-            _sys_ks.register_view_for_building(view->ks_name(), view->cf_name(), step.current_token())).discard_result();
 }
 
 static future<> flush_base(lw_shared_ptr<replica::column_family> base, abort_source& as) {
