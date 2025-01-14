27ac13eb48167b2c45788782eef06b44c70ea1a6
Wojciech Mitros <wojciech.mitros@scylladb.com>
2025-01-14 04:08:49 +0100

    schema_applier: make updates to tables and views atomic
    Currently, when processing a schema update of multiple tables, we
    may yield between updating different tables. In particular, we can
    yield between updates to base tables and materialized views based
    on them, contrary to the `db::schema_tables::merge_tables_and_views`
    comment description.
    The yielding was originally introduced when trying to remove the
    stalls when processing many tables in https://github.com/scylladb/scylladb/issues/11574
    However, no stalls were identified directly in `update_column_family()`
    and atomic updates of base tables and their views is needed for
    keeping the base_info in the view schemas consistent with base schemas.
    
    Additionally, the schema updates are moved before view schema creations.
    This is done to avoid the scenario where we create a view with base_info
    based on an updated base schema, while the base schema wasn't updated yet.
    We shouldn't cause issues even when we create and alter a view schema
    at the same time - in that case, the schema update will be merged with
    the schema creation and will appear only in the views_diff.created list,
    not the views_diff.altered list.
    
    If we observe stalls after this change, we should either at least
    group base tables with their views and only yield between the groups,
    or preprocess some of the `update_column_family` work before actually
    applying the updates.

--- db/schema_applier.cc
+++ db/schema_applier.cc
@@ -639,17 +639,14 @@ static future<> merge_tables_and_views(distributed<service::storage_proxy>& prox
         co_await max_concurrent_for_each(tables_diff.created, max_concurrent, [&] (global_schema_ptr& gs) -> future<> {
             co_await db.add_column_family_and_make_directory(gs, replica::database::is_new_cf::yes);
         });
-        co_await max_concurrent_for_each(views_diff.created, max_concurrent, [&] (global_schema_ptr& gs) -> future<> {
-            co_await db.add_column_family_and_make_directory(gs, replica::database::is_new_cf::yes);
-        });
-    });
-    co_await db.invoke_on_all([&](replica::database& db) -> future<> {
         std::vector<bool> columns_changed;
         columns_changed.reserve(tables_diff.altered.size() + views_diff.altered.size());
         for (auto&& altered : boost::range::join(tables_diff.altered, views_diff.altered)) {
             columns_changed.push_back(db.update_column_family(altered.new_schema));
-            co_await coroutine::maybe_yield();
         }
+        co_await max_concurrent_for_each(views_diff.created, max_concurrent, [&] (global_schema_ptr& gs) -> future<> {
+            co_await db.add_column_family_and_make_directory(gs, replica::database::is_new_cf::yes);
+        });
         auto it = columns_changed.begin();
         auto notify = [&] (auto& r, auto&& f) -> future<> {
             co_await max_concurrent_for_each(r, max_concurrent, std::move(f));
