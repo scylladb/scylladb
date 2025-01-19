0aeb4d353aaffec0167f674e0caa6ab782224864
Benny Halevy <bhalevy@scylladb.com>
2025-01-19 08:52:50 +0200

    topology_custom/test_major_compaction: use new_test_keyspace
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    (cherry picked from commit 0e11aad9c5b25aace005a10c9c8b1ad0105c551b)

--- test/topology_custom/test_major_compaction.py
+++ test/topology_custom/test_major_compaction.py
@@ -11,6 +11,7 @@ import asyncio
 from test.pylib.manager_client import ManagerClient
 from test.pylib.rest_client import inject_error_one_shot
 from test.topology.conftest import skip_mode
+from test.topology.util import new_test_keyspace, reconnect_driver
 
 logger = logging.getLogger(__name__)
 
@@ -42,53 +43,52 @@ async def test_major_compaction_consider_only_existing_data(manager: ManagerClie
     server = (await manager.servers_add(1))[0]
 
     logger.info("Creating table")
-    ks = "test_consider_only_existing_data"
-    cf = "t1"
+    cf = "test_consider_only_existing_data"
     cql = manager.get_cql()
-    await cql.run_async(f"CREATE KEYSPACE {ks} WITH replication = {{'class': 'NetworkTopologyStrategy', 'replication_factor': 1}}")
-    await cql.run_async(f"CREATE TABLE {ks}.{cf} (pk int PRIMARY KEY) WITH tombstone_gc = {{'mode': 'immediate'}}")
-    await disable_autocompaction_across_keyspaces(manager, server.ip_addr, ks)
+    async with new_test_keyspace(manager, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
+        await cql.run_async(f"CREATE TABLE {ks}.{cf} (pk int PRIMARY KEY) WITH tombstone_gc = {{'mode': 'immediate'}}")
+        await disable_autocompaction_across_keyspaces(manager, server.ip_addr, ks)
 
-    logger.info("Populating table")
-    await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k});") for k in range(20)])
-    await asyncio.gather(*[cql.run_async(f"DELETE FROM {ks}.{cf} WHERE pk = {k};") for k in range(10)])
-    await manager.api.keyspace_flush(server.ip_addr, ks, cf)
+        logger.info("Populating table")
+        await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k});") for k in range(20)])
+        await asyncio.gather(*[cql.run_async(f"DELETE FROM {ks}.{cf} WHERE pk = {k};") for k in range(10)])
+        await manager.api.keyspace_flush(server.ip_addr, ks, cf)
 
-    # let a second pass, so that the tombstones are eligible for gc
-    await asyncio.sleep(1)
+        # let a second pass, so that the tombstones are eligible for gc
+        await asyncio.sleep(1)
 
-    # error injection to make compaction wait after collecting sstables
-    injection = "major_compaction_wait"
-    injection_handler = await inject_error_one_shot(manager.api, server.ip_addr, injection)
+        # error injection to make compaction wait after collecting sstables
+        injection = "major_compaction_wait"
+        injection_handler = await inject_error_one_shot(manager.api, server.ip_addr, injection)
 
-    logger.info("Start major compaction")
-    log = await manager.server_open_log(server.server_id)
-    mark = await log.mark()
-    compaction_task = asyncio.create_task(manager.api.keyspace_compaction(server.ip_addr, ks, cf, consider_only_existing_data=consider_only_existing_data))
-    # wait for the injection to pause the compaction
-    await log.wait_for("major_compaction_wait: waiting", from_mark=mark, timeout=30)
+        logger.info("Start major compaction")
+        log = await manager.server_open_log(server.server_id)
+        mark = await log.mark()
+        compaction_task = asyncio.create_task(manager.api.keyspace_compaction(server.ip_addr, ks, cf, consider_only_existing_data=consider_only_existing_data))
+        # wait for the injection to pause the compaction
+        await log.wait_for("major_compaction_wait: waiting", from_mark=mark, timeout=30)
 
-    # insert new backdated rows with deleted keys and flush them
-    # into a new sstable that will not be part of the major compaction
-    logger.info("Insert backdated data into the table")
-    await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k}) USING TIMESTAMP 1;") for k in range(5)])
-    await manager.api.keyspace_flush(server.ip_addr, ks, cf)
+        # insert new backdated rows with deleted keys and flush them
+        # into a new sstable that will not be part of the major compaction
+        logger.info("Insert backdated data into the table")
+        await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k}) USING TIMESTAMP 1;") for k in range(5)])
+        await manager.api.keyspace_flush(server.ip_addr, ks, cf)
 
-    # insert few more rows with deleted keys with backdated data into memtable
-    await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k}) USING TIMESTAMP 1;") for k in range(5, 10)])
+        # insert few more rows with deleted keys with backdated data into memtable
+        await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k}) USING TIMESTAMP 1;") for k in range(5, 10)])
 
-    # resume compaction
-    await injection_handler.message()
-    await compaction_task
+        # resume compaction
+        await injection_handler.message()
+        await compaction_task
 
-    # evict cache to make backdated data visible for consider_only_existing_data mode
-    if consider_only_existing_data:
-        await manager.api.drop_sstable_caches(server.ip_addr)
+        # evict cache to make backdated data visible for consider_only_existing_data mode
+        if consider_only_existing_data:
+            await manager.api.drop_sstable_caches(server.ip_addr)
 
-    logger.info("Verify major compaction results")
-    expected_count = 1 if consider_only_existing_data else 0
-    for k in range(10):
-        assert len(await cql.run_async(f"SELECT * FROM {ks}.{cf} WHERE pk = {k}")) == expected_count
+        logger.info("Verify major compaction results")
+        expected_count = 1 if consider_only_existing_data else 0
+        for k in range(10):
+            assert len(await cql.run_async(f"SELECT * FROM {ks}.{cf} WHERE pk = {k}")) == expected_count
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("compaction_flush_all_tables_before_major_seconds", [0, 2, 10])
@@ -110,37 +110,36 @@ async def test_major_compaction_flush_all_tables(manager: ManagerClient, compact
     server = (await manager.servers_add(1, config=cfg, cmdline=['--smp=1']))[0]
 
     logger.info("Creating table")
-    ks = "test_flush_all_tables"
-    cf = "t1"
+    cf = "test_flush_all_tables"
     cql = manager.get_cql()
-    await cql.run_async(f"CREATE KEYSPACE {ks} WITH replication = {{'class': 'NetworkTopologyStrategy', 'replication_factor': 1}}")
-    await cql.run_async(f"CREATE TABLE {ks}.{cf} (pk int PRIMARY KEY)")
-    await disable_autocompaction_across_keyspaces(manager, server.ip_addr, ks)
+    async with new_test_keyspace(manager, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
+        await cql.run_async(f"CREATE TABLE {ks}.{cf} (pk int PRIMARY KEY)")
+        await disable_autocompaction_across_keyspaces(manager, server.ip_addr, ks)
 
-    logger.info("Populating table")
-    await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k});") for k in range(256)])
-    await manager.api.keyspace_flush(server.ip_addr, ks, cf)
-    log = await manager.server_open_log(server.server_id)
+        logger.info("Populating table")
+        await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k});") for k in range(256)])
+        await manager.api.keyspace_flush(server.ip_addr, ks, cf)
+        log = await manager.server_open_log(server.server_id)
 
-    async def check_all_table_flush_in_major_compaction(expect_all_table_flush: bool):
-        mark = await log.mark()
+        async def check_all_table_flush_in_major_compaction(expect_all_table_flush: bool):
+            mark = await log.mark()
 
-        logger.info("Start major compaction")
-        await manager.api.keyspace_compaction(server.ip_addr, ks, cf)
+            logger.info("Start major compaction")
+            await manager.api.keyspace_compaction(server.ip_addr, ks, cf)
 
-        flush_log = await log.grep("Forcing new commitlog segment and flushing all tables", from_mark=mark)
-        assert len(flush_log) == (1 if expect_all_table_flush else 0)
+            flush_log = await log.grep("Forcing new commitlog segment and flushing all tables", from_mark=mark)
+            assert len(flush_log) == (1 if expect_all_table_flush else 0)
 
-    # all tables should be flushed the first time unless compaction_flush_all_tables_before_major_seconds == 0
-    await check_all_table_flush_in_major_compaction(compaction_flush_all_tables_before_major_seconds != 0)
+        # all tables should be flushed the first time unless compaction_flush_all_tables_before_major_seconds == 0
+        await check_all_table_flush_in_major_compaction(compaction_flush_all_tables_before_major_seconds != 0)
 
-    if compaction_flush_all_tables_before_major_seconds == 2:
-        # let 2 seconds pass before trying again
-        await asyncio.sleep(compaction_flush_all_tables_before_major_seconds)
+        if compaction_flush_all_tables_before_major_seconds == 2:
+            # let 2 seconds pass before trying again
+            await asyncio.sleep(compaction_flush_all_tables_before_major_seconds)
 
-    # for the second time, all tables should be flushed only if
-    # compaction_flush_all_tables_before_major_seconds == 2 as only 2 seconds have passed
-    await check_all_table_flush_in_major_compaction(compaction_flush_all_tables_before_major_seconds == 2)
+        # for the second time, all tables should be flushed only if
+        # compaction_flush_all_tables_before_major_seconds == 2 as only 2 seconds have passed
+        await check_all_table_flush_in_major_compaction(compaction_flush_all_tables_before_major_seconds == 2)
 
 # Testcase for https://github.com/scylladb/scylladb/issues/20197
 @pytest.mark.asyncio
@@ -159,38 +158,41 @@ async def test_shutdown_drain_during_compaction(manager: ManagerClient):
     server = await manager.server_add(cmdline=['--smp=1'])
 
     logger.info("Creating table")
-    ks = "test_shutdown_drain_during_compaction"
-    cf = "t1"
+    cf = "test_shutdown_drain_during_compaction"
     cql = manager.get_cql()
-    await cql.run_async(f"CREATE KEYSPACE {ks} WITH replication = {{'class': 'NetworkTopologyStrategy', 'replication_factor': 1}}")
-    await cql.run_async(f"CREATE TABLE {ks}.{cf} (pk int PRIMARY KEY);")
-    await disable_autocompaction_across_keyspaces(manager, server.ip_addr, ks)
+    async with new_test_keyspace(manager, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1}") as ks:
+        await cql.run_async(f"CREATE TABLE {ks}.{cf} (pk int PRIMARY KEY);")
+        await disable_autocompaction_across_keyspaces(manager, server.ip_addr, ks)
 
-    logger.info("Populating table")
-    await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k});") for k in range(100)])
-    await manager.api.keyspace_flush(server.ip_addr, ks, cf)
+        logger.info("Populating table")
+        await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.{cf} (pk) VALUES ({k});") for k in range(100)])
+        await manager.api.keyspace_flush(server.ip_addr, ks, cf)
 
-    # inject error to make compaction wait just before it updates the compaction_history table
-    injection = "update_history_wait"
-    injection_handler = await inject_error_one_shot(manager.api, server.ip_addr, injection)
+        # inject error to make compaction wait just before it updates the compaction_history table
+        injection = "update_history_wait"
+        injection_handler = await inject_error_one_shot(manager.api, server.ip_addr, injection)
 
-    log = await manager.server_open_log(server.server_id)
-    mark = await log.mark()
-    # start compaction and wait for it to pause at the injection point
-    logger.info("Start compaction")
-    compaction_task = asyncio.create_task(manager.api.keyspace_compaction(server.ip_addr, ks, cf))
-    await log.wait_for("update_history_wait: waiting", mark, 30)
+        log = await manager.server_open_log(server.server_id)
+        mark = await log.mark()
+        # start compaction and wait for it to pause at the injection point
+        logger.info("Start compaction")
+        compaction_task = asyncio.create_task(manager.api.keyspace_compaction(server.ip_addr, ks, cf))
+        await log.wait_for("update_history_wait: waiting", mark, 30)
 
-    mark = await log.mark()
-    # Start server shutdown
-    logger.info("Shutdown server")
-    stop_task = asyncio.create_task(manager.server_stop_gracefully(server.server_id))
-    # wait until the shutdown drain request is sent to compaction_manager
-    await log.wait_for("Asked to drain", mark, 30)
-    # now resume compaction and let shutdown complete
-    await injection_handler.message()
-    # wait server to shutdown
-    await stop_task
-    # During shutdown, errors mentioning 'seastar::abort_requested_exception' is expected as we do abort the compaction midway.
-    # Verify that the shutdown completed without any other unexpected errors
-    assert len(await log.grep(expr="ERROR .*", filter_expr=".* seastar::abort_requested_exception \(abort requested\)", from_mark=mark)) == 0
+        mark = await log.mark()
+        # Start server shutdown
+        logger.info("Shutdown server")
+        stop_task = asyncio.create_task(manager.server_stop_gracefully(server.server_id))
+        # wait until the shutdown drain request is sent to compaction_manager
+        await log.wait_for("Asked to drain", mark, 30)
+        # now resume compaction and let shutdown complete
+        await injection_handler.message()
+        # wait server to shutdown
+        await stop_task
+        # During shutdown, errors mentioning 'seastar::abort_requested_exception' is expected as we do abort the compaction midway.
+        # Verify that the shutdown completed without any other unexpected errors
+        assert len(await log.grep(expr="ERROR .*", filter_expr=".* seastar::abort_requested_exception \(abort requested\)", from_mark=mark)) == 0
+
+        # For dropping the keyspace
+        await manager.server_start(server.server_id)
+        await reconnect_driver(manager)
