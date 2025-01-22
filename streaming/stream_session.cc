asias/scylla

Introduce file stream for tablet
File based stream is a new feature that optimizes tablet movement
significantly. It streams the entire SSTable files without deserializing
SSTable files into mutation fragments and re-serializing them back into
SSTables on receiving nodes. As a result, less data is streamed over the
network, and less CPU is consumed, especially for data models that
contain small cells.

The following patches are imported from the scylla enterprise:

*) Merge 'Introduce file stream for tablet' from Asias He

    This patch uses Seastar RPC stream interface to stream sstable files on
    network for tablet migration.

    It streams sstables instead of mutation fragments. The file based
    stream has multiple advantages over the mutation streaming.

    - No serialization or deserialization for mutation fragments
    - No need to read and process each mutation fragments
    - On wire data is more compact and smaller

    In the test below, a significant speed up is observed.

    Two nodes, 1 shard per node, 1 initial_tablets:

    - Start node 1
    - Insert 10M rows of data with c-s
    - Bootstrap node 2

    Node 1 will migration data to node2 with the file stream.

    Test results:

    1) File stream: bytes on wire = 1132006250 bytes, bw = 836MB/s

    [shard 0:stre] stream_blob - stream_sstables[eadaa8e0-a4f2-4cc6-bf10-39ad1ce106b0]
	Finished sending sstable_nr=2 files_nr=18 files={} range=(-1,9223372036854775807] bytes_sent=1132006250 stream_bw=836MB/s
    [shard 0:stre] storage_service - Streaming for tablet migration of a4f68900-568a-11ee-b7b9-c2b13945eed2:1 took 1.08004s seconds

    2) Mutation stream: bytes on wire = 3030004736 bytes, bw = 125410.87 KiB/s = 128MB/s

    [shard 0:stre] stream_session - [Stream #406dc8b0-56b5-11ee-bc2d-000bf4871058]
	Streaming plan for Tablet migration-ks1-index-0 succeeded, peers={127.0.0.1}, tx=0 KiB, 0.00 KiB/s, rx=2958989 KiB, 125410.87 KiB/s
    [shard 0:stre] storage_service - Streaming for tablet mi

streaming/stream_session.cc
@@ -19,6 +19,7 @@
 #include "dht/auto_refreshing_sharder.hh"
 #include <seastar/core/sleep.hh>
 #include <seastar/core/thread.hh>
+#include "streaming/stream_blob.hh"
 #include "streaming/stream_session_state.hh"
 #include "service/migration_manager.hh"
 #include "mutation_writer/multishard_writer.hh"
@@ -278,13 +279,22 @@ void stream_manager::init_messaging_service_handler(abort_source& as) {
             return make_ready_future<>();
         }
     });
+    ms.register_stream_blob([this] (const rpc::client_info& cinfo, streaming::stream_blob_meta meta, rpc::source<streaming::stream_blob_cmd_data> source) {
+        auto from = netw::messaging_service::get_source(cinfo).addr;
+        auto sink = _ms.local().make_sink_for_stream_blob(source);
+        (void)stream_blob_handler(_db.local(), _ms.local(), from, meta, sink, source).handle_exception([ms = _ms.local().shared_from_this()] (std::exception_ptr eptr) {
+            sslog.warn("Failed to run stream blob handler: {}", eptr);
+        });
+        return make_ready_future<rpc::sink<streaming::stream_blob_cmd_data>>(sink);
+    });
 }
 
 future<> stream_manager::uninit_messaging_service_handler() {
     auto& ms = _ms.local();
     return when_all_succeed(
         ser::streaming_rpc_verbs::unregister(&ms),
-        ms.unregister_stream_mutation_fragments()).discard_result();
+        ms.unregister_stream_mutation_fragments(),
+        ms.unregister_stream_blob()).discard_result();
 }
 
 stream_session::stream_session(stream_manager& mgr, locator::host_id peer_)

scylladb/scylladb

streaming/stream_session.cc
@@ -19,6 +19,7 @@
 #include "dht/auto_refreshing_sharder.hh"
 #include <seastar/core/sleep.hh>
 #include <seastar/core/thread.hh>
+#include "streaming/stream_blob.hh"
 #include "streaming/stream_session_state.hh"
 #include "service/migration_manager.hh"
 #include "mutation_writer/multishard_writer.hh"
@@ -278,12 +279,21 @@ void stream_manager::init_messaging_service_handler(abort_source& as) {
             return make_ready_future<>();
         }
     });
+    ms.register_stream_blob([this] (const rpc::client_info& cinfo, streaming::stream_blob_meta meta, rpc::source<streaming::stream_blob_cmd_data> source) {
+        auto from = netw::messaging_service::get_source(cinfo).addr;
+        auto sink = _ms.local().make_sink_for_stream_blob(source);
+        (void)stream_blob_handler(_db.local(), _ms.local(), from, meta, sink, source).handle_exception([ms = _ms.local().shared_from_this()] (std::exception_ptr eptr) {
+            sslog.warn("Failed to run stream blob handler: {}", eptr);
+        });
+        return make_ready_future<rpc::sink<streaming::stream_blob_cmd_data>>(sink);
+    });
 }
 
 future<> stream_manager::uninit_messaging_service_handler() {
     auto& ms = _ms.local();
     return when_all_succeed(
         ser::streaming_rpc_verbs::unregister(&ms),
+        ms.unregister_stream_blob(),
         ms.unregister_stream_mutation_fragments()).discard_result();
 }
 