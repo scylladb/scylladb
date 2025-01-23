6f36e1fcdad222448be3a3e5c93ecad5a9c8d1db
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-01-23 17:19:04 +0100

    service/vb_coordinator: add currently processing base
    The view building coordinator will be building all views
    of one base table at a time.
    Select first available base table as currently processing base
    and save this information to `system.scylla_local`.

--- service/view_building_coordinator.cc
+++ service/view_building_coordinator.cc
@@ -58,8 +58,11 @@ future<> view_building_coordinator::await_event() {
 
 future<view_building_coordinator::vbc_state> view_building_coordinator::load_coordinator_state() {
     auto tasks = co_await _sys_ks.get_view_building_coordinator_tasks();
+    auto processing_base = co_await _sys_ks.get_vbc_processing_base();
+
     co_return vbc_state {
-        .tasks = std::move(tasks)
+        .tasks = std::move(tasks),
+        .processing_base = std::move(processing_base),
     };
 }
 
@@ -110,7 +113,20 @@ future<std::optional<view_building_coordinator::vbc_state>> view_building_coordi
         for (auto& view: to_remove) {
             auto muts = co_await remove_view(guard, view);
             cmuts.insert(cmuts.end(), std::make_move_iterator(muts.begin()), std::make_move_iterator(muts.end()));
+
+            if (state.processing_base && *state.processing_base == get_base_id(view)) {
+                auto mut = co_await _sys_ks.make_vbc_delete_processing_base_mutation(guard.write_timestamp());
+                cmuts.emplace_back(std::move(mut));
+                state.processing_base = std::nullopt;
+            }
         }
+    } else if (!state.processing_base && !state.tasks.empty()) {
+        // select base table to process
+        auto& base_id = state.tasks.cbegin()->first;
+        vbc_logger.info("Start building views for base table: {}", base_id);
+
+        auto mut = co_await _sys_ks.make_vbc_processing_base_mutation(guard.write_timestamp(), base_id);
+        cmuts.emplace_back(std::move(mut));
     }
 
     if (!cmuts.empty()) {
