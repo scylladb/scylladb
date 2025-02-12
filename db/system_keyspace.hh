0e8ecbaebf987bca6e251b6b27b9bf78502d688a
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-02-12 15:30:53 +0100

    db/view: move cql statements execution to system_keyspace
    They will be also used in view building coordinator.

--- db/system_keyspace.hh
+++ db/system_keyspace.hh
@@ -14,6 +14,7 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
+#include "db/view/view_build_status.hh"
 #include "gms/gossiper.hh"
 #include "schema/schema_fwd.hh"
 #include "timestamp.hh"
@@ -548,6 +549,11 @@ public:
     future<> remove_built_view(sstring ks_name, sstring view_name);
     future<std::vector<view_name>> load_built_views();
     future<std::vector<view_build_progress>> load_view_build_progress();
+
+    future<mutation> make_view_build_status_mutation(api::timestamp_type ts, system_keyspace_view_name view_name, locator::host_id host_id, view::build_status status);
+    future<mutation> make_view_build_status_update_mutation(api::timestamp_type ts, system_keyspace_view_name view_name, locator::host_id host_id, view::build_status status);
+    future<mutation> make_remove_view_build_status_mutation(api::timestamp_type ts, system_keyspace_view_name view_name);
+    future<mutation> make_remove_view_build_status_on_host_mutation(api::timestamp_type ts, system_keyspace_view_name view_name, locator::host_id host_id);
 
     future<service::vbc::vbc_tasks> get_view_building_coordinator_tasks();
     future<mutation> make_vbc_task_mutation(api::timestamp_type ts, system_keyspace_view_name view_name, locator::host_id host_id, shard_id shard, dht::token_range range);
