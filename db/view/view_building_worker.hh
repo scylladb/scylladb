65e80747b777fb2c59987e81cf9b538f266f6b9c
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-02-12 01:11:48 +0100

    db/view/view_building_worker: detect staging sstables
    Add a fiber which detects local staging sstable
    in currently processing (by view building coordinator) base table.
    
    The fiber starts its work when notified and finishes by waiting on
    conditional variable when the information is successfully saved to
    group0.

--- db/view/view_building_worker.hh
+++ db/view/view_building_worker.hh
@@ -11,8 +11,14 @@
 #include "raft/raft.hh"
 #include "schema/schema_fwd.hh"
 #include "seastar/core/abort_source.hh"
+#include "seastar/core/condition-variable.hh"
+#include "seastar/core/future.hh"
 #include "seastar/core/sharded.hh"
 #include "dht/i_partitioner_fwd.hh"
+
+namespace db {
+class system_keyspace;
+}
 
 namespace replica {
 class database;
@@ -20,6 +26,10 @@ class database;
 
 namespace netw {
 class messaging_service;
+}
+
+namespace service {
+class raft_group0_client;
 }
 
 using namespace seastar;
@@ -30,20 +40,30 @@ class view_update_generator;
 
 class view_building_worker : public seastar::peering_sharded_service<view_building_worker> {
     replica::database& _db;
+    service::raft_group0_client& _group0_client;
+    db::system_keyspace& _sys_ks;
     view_update_generator& _vug;
     sharded<netw::messaging_service>& _messaging;
 
     std::optional<raft::term_t> _work_term;
-    abort_source _as;
+    abort_source _view_building_as;
+
+    condition_variable _cond;
+    abort_source _detector_as;
+    future<> _staging_detector_fiber = make_ready_future();
 
 public:
-    view_building_worker(replica::database& db, view_update_generator& vug, sharded<netw::messaging_service>& messaging);
+    view_building_worker(replica::database& db, service::raft_group0_client& group0_client, db::system_keyspace& sys_ks, view_update_generator& vug, sharded<netw::messaging_service>& messaging);
 
+    future<> notify();
     future<> stop();
 
     class consumer;
 private:
     future<> build_views_range(table_id base_id, dht::token_range range, std::vector<table_id> views, raft::term_t term);
+
+    future<> start_staging_detector();
+    future<> detect_staging_sstables();
 
     void init_messaging_service();
     future<> uninit_messaging_service();
