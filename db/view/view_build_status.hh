cb03d3539f08e5bf70f2932a31008e8485f58892
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-02-12 15:30:53 +0100

    db/view: move cql statements execution to system_keyspace
    They will be also used in view building coordinator.

---
+++ db/view/view_build_status.hh
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include <fmt/core.h>
+#include <string_view>
+#include <seastar/core/sstring.hh>
+
+namespace db {
+
+namespace view {
+
+enum class build_status {
+    STARTED,
+    SUCCESS,
+};
+
+inline build_status build_status_from_string(std::string_view str) {
+    if (str == "STARTED") {
+        return build_status::STARTED;
+    }
+    if (str == "SUCCESS") {
+        return build_status::SUCCESS;
+    }
+    throw std::runtime_error(fmt::format("Unknown view build status: {}", str));
+}
+
+inline seastar::sstring build_status_to_sstring(build_status status) {
+    switch (status) {
+    case build_status::STARTED:
+        return "STARTED";
+    case build_status::SUCCESS:
+        return "SUCCESS";
+    }
+}
+
+}
+
+}
