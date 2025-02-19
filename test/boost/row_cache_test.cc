942ea97e8f11a3fe3eec31589aff7a334ad64085
Botond Dénes <bdenes@scylladb.com>
2025-02-19 09:35:04 -0500

    db/row_cache: migrate to tombstone_gc_before_getter
    tombstone_gc_before_getter has value semantics, so tombstone_gc_state*
    is replaced by std::optional<tombstone_gc_before_getter>.

--- test/boost/row_cache_test.cc
+++ test/boost/row_cache_test.cc
@@ -4575,13 +4575,14 @@ SEASTAR_TEST_CASE(test_cache_compacts_expired_tombstones_on_read) {
         }
 
         tombstone_gc_state gc_state(nullptr);
+        tombstone_gc_before_getter gc_before_getter(gc_state);
 
         // emulate commitlog behaivor
         gc_state.set_gc_time_min_source([&s](const table_id& id) {
                 return gc_clock::now() - (std::chrono::seconds(s->gc_grace_seconds().count() + 600));
         });
 
-        auto rd1 = cache.make_reader(s, semaphore.make_permit(), query::full_partition_range, &gc_state);
+        auto rd1 = cache.make_reader(s, semaphore.make_permit(), query::full_partition_range, gc_before_getter);
         auto close_rd = deferred_close(rd1);
         rd1.fill_buffer().get(); // cache_mutation_reader compacts cache on fill buffer
 
@@ -4633,6 +4634,7 @@ SEASTAR_TEST_CASE(test_compact_range_tombstones_on_read) {
         cache.populate(m);
 
         tombstone_gc_state gc_state(nullptr);
+        tombstone_gc_before_getter gc_before_getter(gc_state);
 
         cache_entry& entry = cache.lookup(pk);
         auto& cp = entry.partition().version()->partition();
@@ -4659,7 +4661,7 @@ SEASTAR_TEST_CASE(test_compact_range_tombstones_on_read) {
         set_cells_timestamp_to_min(cp.clustered_row(*s.schema(), ck3));
 
         {
-            auto rd1 = cache.make_reader(s.schema(), semaphore.make_permit(), pr, &gc_state);
+            auto rd1 = cache.make_reader(s.schema(), semaphore.make_permit(), pr, gc_before_getter);
             auto close_rd1 = deferred_close(rd1);
             rd1.fill_buffer().get();
 
@@ -4671,7 +4673,7 @@ SEASTAR_TEST_CASE(test_compact_range_tombstones_on_read) {
         }
 
         {
-            auto rd2 = cache.make_reader(s.schema(), semaphore.make_permit(), pr, &gc_state);
+            auto rd2 = cache.make_reader(s.schema(), semaphore.make_permit(), pr, gc_before_getter);
             auto close_rd2 = deferred_close(rd2);
             rd2.fill_buffer().get();
 
@@ -4710,7 +4712,6 @@ SEASTAR_THREAD_TEST_CASE(test_cache_reader_semaphore_oom_kill) {
     cache.populate(m);
 
     auto pr = dht::partition_range::make_singular(pk);
-    tombstone_gc_state gc_state(nullptr);
 
     BOOST_REQUIRE_EQUAL(semaphore.get_stats().total_reads_killed_due_to_kill_limit, 0);
     auto kill_limit_before = 0;
@@ -4720,7 +4721,7 @@ SEASTAR_THREAD_TEST_CASE(test_cache_reader_semaphore_oom_kill) {
         semaphore.set_resources({1, memory});
         auto permit = semaphore.obtain_permit(s.schema(), "read", 0, db::no_timeout, {}).get();
         auto create_reader_and_read_all = [&] {
-            auto rd = cache.make_reader(s.schema(), permit, pr, &gc_state);
+            auto rd = cache.make_reader(s.schema(), permit, pr, {});
             auto close_rd = deferred_close(rd);
             while (rd().get());
         };
@@ -4884,7 +4885,6 @@ SEASTAR_THREAD_TEST_CASE(test_reproduce_18045) {
     // Before the fix for issue #18045, this caused a (ASAN-triggering) use-after-free,
     // because _latest_it was deferenced during the population.
 
-    tombstone_gc_state gc_state(nullptr);
     auto slice = query::reverse_slice(*s, s->full_slice());
     auto rd = cache.make_reader(
         s->make_reversed(),
@@ -4894,7 +4894,7 @@ SEASTAR_THREAD_TEST_CASE(test_reproduce_18045) {
         nullptr,
         streamed_mutation::forwarding::no,
         mutation_reader::forwarding::no,
-        &gc_state);
+        {});
     auto close_rd = deferred_close(rd);
     read_mutation_from_mutation_reader(rd).get();
 }
