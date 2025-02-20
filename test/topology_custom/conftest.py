ef62dfa6a9a1cf7033107b2aae8a31f017adb8fc
Artsiom Mishuta <artsiom.mishuta@scylladb.com>
2025-02-20 12:26:12 +0100

    test.py delete conftest in topology_custom
    delete conftest in the sepatate commi for brtter diff listing during
    merge topology_custom and topology

--- test/topology_custom/conftest.py
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2023-present ScyllaDB
-#
-# SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
-#
-# This file configures pytest for all tests in this directory, and also
-# defines common test fixtures for all of them to use
-
-from test.topology.conftest import *
