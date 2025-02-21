400d4690974979b7808b0384773ae6a939bdea13
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2025-02-21 15:18:15 +0100

    nodetool: repair: repair only vnode keyspaces
    nodetool repair command repairs only vnode keyspaces.
    
    A nodetool tablet-repair command to repair tablet keyspaces will
    be added in the following patches.

--- test/nodetool/test_repair.py
+++ test/nodetool/test_repair.py
@@ -51,7 +51,8 @@ def _remove_log_timestamp(res):
 
 def test_repair_all_single_keyspace(nodetool):
     res = nodetool("repair", expected_requests=[
-        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy"}, response=["ks1"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "vnodes"}, response=["ks1"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "tablets"}, response=[]),
         expected_request("GET", "/storage_service/keyspaces", response=["ks1"], multiple=expected_request.ANY),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
@@ -78,8 +79,9 @@ Repair session 1 finished
 
 def test_repair_all_two_keyspaces(nodetool):
     res = nodetool("repair", expected_requests=[
-        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy"},
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "vnodes"},
                          response=["ks1", "ks2"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "tablets"}, response=[]),
         expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.ANY, response=["ks1", "ks2"]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
@@ -126,6 +128,7 @@ Repair session 4 finished
 def test_repair_keyspace(nodetool):
     res = nodetool("repair", "ks", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request(
@@ -152,6 +155,7 @@ Repair session 1 finished
 def test_repair_one_table(nodetool):
     res = nodetool("repair", "ks", "table1", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request(
@@ -179,6 +183,7 @@ Repair session 1 finished
 def test_repair_two_tables(nodetool):
     res = nodetool("repair", "ks", "table1", "table2", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request(
@@ -206,6 +211,7 @@ Repair session 1 finished
 def test_repair_long_progress(nodetool):
     res = nodetool("repair", "ks", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request(
@@ -237,6 +243,7 @@ def test_repair_failed(nodetool):
         ("repair", "ks"),
         {"expected_requests": [
             expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+            expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
             JMX_COLUMN_FAMILIES_REQUEST,
             JMX_STREAM_MANAGER_REQUEST,
             expected_request(
@@ -261,8 +268,10 @@ def test_repair_all_three_keyspaces_failed(nodetool):
     """Check that given three keyspaces to repair, if the second one fails, the
     third one isn't even started."""
     expected_requests = [
-        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy"},
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "vnodes"},
                          response=["ks1", "ks2", "ks3"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "tablets"},
+                         response=[]),
         expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.ANY,
                          response=["ks1", "ks2", "ks3"]),
         JMX_COLUMN_FAMILIES_REQUEST,
@@ -325,6 +334,7 @@ def _do_test_repair_options(
 
     expected_requests = [
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
     ]
 
     if trace is None:
@@ -466,6 +476,7 @@ def test_repair_options_hosts(nodetool, hosts):
 def test_repair_parallelism_precedence(nodetool):
     res = nodetool("repair", "ks", "--dc-parallel", "--sequential", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request(
@@ -492,6 +503,7 @@ Repair session 1 finished
 def test_repair_dc_precedence(nodetool):
     res = nodetool("repair", "ks", "--in-dc", "DC1", "-dc", "DC2", "--in-local-dc", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request("GET", "/snitch/datacenter", response="DC_local", multiple=expected_request.ANY),
@@ -535,6 +547,7 @@ def test_repair_unused_options(request, nodetool, jobs, full):
 
     res = nodetool(*args, expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         JMX_COLUMN_FAMILIES_REQUEST,
         JMX_STREAM_MANAGER_REQUEST,
         expected_request(
@@ -564,6 +577,7 @@ def test_repair_pr_and_dcs(nodetool):
         ("repair", "ks", "-pr", "-dc", "DC1"),
         {"expected_requests": [
                 expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+                expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         ]},
         ["error: Primary range repair should be performed on all nodes in the cluster.",
          "error processing arguments: primary range repair should be performed on all nodes in the cluster"])
@@ -575,6 +589,46 @@ def test_repair_pr_and_hosts(nodetool):
         ("repair", "ks", "-pr", "-hosts", "127.0.0.2"),
         {"expected_requests": [
                 expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+                expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=[]),
         ]},
         ["error: Primary range repair should be performed on all nodes in the cluster.",
          "error processing arguments: primary range repair should be performed on all nodes in the cluster"])
+
+def test_repair_all_with_tablet_keyspace(nodetool):
+    res = nodetool("repair", expected_requests=[
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "vnodes"}, response=["ks1"]),
+        expected_request("GET", "/storage_service/keyspaces", params={"type": "non_local_strategy", "replication": "tablets"}, response=["ks2"]),
+        expected_request("GET", "/storage_service/keyspaces", response=["ks1"], multiple=expected_request.ANY),
+        JMX_COLUMN_FAMILIES_REQUEST,
+        JMX_STREAM_MANAGER_REQUEST,
+        expected_request(
+            "POST",
+            "/storage_service/repair_async/ks1",
+            params={
+                "trace": "false",
+                "ignoreUnreplicatedKeyspaces": "false",
+                "parallelism": "parallel",
+                "incremental": "false",
+                "pullRepair": "false",
+                "primaryRange": "false",
+                "jobThreads": "1"},
+            response=1),
+        expected_request("GET", "/storage_service/repair_async/ks1", params={"id": "1"}, response="SUCCESSFUL")])
+
+    assert "Warning: only vnode keyspaces will be repaired" in res.stdout
+
+    assert _remove_log_timestamp(res.stdout) == """\
+Starting repair command #1, repairing 1 ranges for keyspace ks1 (parallelism=SEQUENTIAL, full=true)
+Repair session 1
+Repair session 1 finished
+"""
+
+def test_repair_keyspace(nodetool):
+    check_nodetool_fails_with(
+        nodetool,
+        ("repair", "ks"),
+        {"expected_requests": [
+                expected_request("GET", "/storage_service/keyspaces", response=["ks"]),
+                expected_request("GET", "/storage_service/keyspaces", params={"replication": "tablets"}, response=["ks"]),
+        ]},
+        ["error processing arguments: nodetool repair repairs only vnode keyspaces"])
--- tools/scylla-nodetool.cc
+++ tools/scylla-nodetool.cc
@@ -262,10 +262,13 @@ public:
     }
 };
 
-std::vector<sstring> get_keyspaces(scylla_rest_client& client, std::optional<sstring> type = {}) {
+std::vector<sstring> get_keyspaces(scylla_rest_client& client, std::optional<sstring> type = {}, std::optional<sstring> replication = {}) {
     std::unordered_map<sstring, sstring> params;
     if (type) {
         params["type"] = *type;
+    }
+    if (replication) {
+        params["replication"] = *replication;
     }
     auto keyspaces_json = client.get("/storage_service/keyspaces", std::move(params));
     std::vector<sstring> keyspaces;
@@ -1427,10 +1430,17 @@ void repair_operation(scylla_rest_client& client, const bpo::variables_map& vm) 
     std::vector<sstring> keyspaces, tables;
     if (vm.contains("keyspace")) {
         auto res = parse_keyspace_and_tables(client, vm, true);
+        auto uses_tablets = keyspace_uses_tablets(client, res.keyspace);
+        if (uses_tablets) {
+            throw std::invalid_argument("nodetool repair repairs only vnode keyspaces");
+        }
         keyspaces.push_back(std::move(res.keyspace));
         tables = std::move(res.tables);
     } else {
-        keyspaces = get_keyspaces(client, "non_local_strategy");
+        keyspaces = get_keyspaces(client, "non_local_strategy", "vnodes");
+        if (!get_keyspaces(client, "non_local_strategy", "tablets").empty()) {
+            fmt::print("Warning: only vnode keyspaces will be repaired");
+        }
     }
 
     if (vm.contains("partitioner-range") && (vm.contains("in-dc") || vm.contains("in-hosts"))) {
@@ -3952,6 +3962,8 @@ replicas until the master data subset is in-sync.
 
 To repair all of the data in the cluster, you need to run a repair on
 all of the nodes in the cluster, or let ScyllaDB Manager do it for you.
+
+Note that nodetool repair repairs only vnode keyspaces.
 
 For more information, see: {}"
 )", doc_link("operating-scylla/nodetool-commands/repair.html")),
