61316e29df70fb7be747c03311b961489217a8e2
Michał Chojnowski <michocho98@gmail.com>
2025-02-24 18:59:19 +0100

    gms/feature_service: add the SSTABLE_COMPRESSION_DICTS cluster feature
    This feature will guard against writing SSTables containing compression
    dictionaries before the entire cluster is able to understand them.

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -165,6 +165,7 @@ public:
     gms::feature compression_dicts { *this, "COMPRESSION_DICTS"sv };
     gms::feature tablet_options { *this, "TABLET_OPTIONS"sv };
     gms::feature tablet_load_stats_v2 { *this, "TABLET_LOAD_STATS_V2"sv };
+    gms::feature sstable_compression_dicts { *this, "SSTABLE_COMPRESSION_DICTS"sv };
 public:
 
     const std::unordered_map<sstring, std::reference_wrapper<feature>>& registered_features() const;
