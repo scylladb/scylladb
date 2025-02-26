6100ab3afc7490d5936fb7707e4c9e3cadf1d55c
Benny Halevy <bhalevy@scylladb.com>
2025-02-26 16:23:52 +0200

    compaction_group: cleanup: prepare: move error injection after sstable deletion
    To be used for reproducing #23049
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- replica/table.cc
+++ replica/table.cc
@@ -3971,12 +3971,12 @@ future<> compaction_group::cleanup() {
             set->for_each_sstable([&all_sstables] (const sstables::shared_sstable& sst) mutable {
                 all_sstables.push_back(sst);
             });
+            co_await _cg.delete_sstables_atomically(std::move(all_sstables));
             if (utils::get_local_injector().enter("tablet_cleanup_failure")) {
                 co_await sleep(std::chrono::seconds(1));
                 tlogger.info("Cleanup failed for tablet {}", _cg.group_id());
                 throw std::runtime_error("tablet cleanup failure");
             }
-            co_await _cg.delete_sstables_atomically(std::move(all_sstables));
         }
 
         virtual void execute() override {
