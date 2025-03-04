a62ab824e6d469eff0b7529149a0bdd1eadc3dc3
Avi Kivity <avi@scylladb.com>
2025-03-04 23:21:48 +0200

    schema: deprecate schema_extension
    schema_extension allows making invisible changes to system_schema
    that evade upgrade rollback tests. They appear in system_schema
    as an encoded blob which reduces serviceability, as they cannot
    be read.
    
    Deprecate it and point users to adding explicit columns in scylla_tables.
    
    We could probably make use of the data structure, after we teach it
    to encode its payload into proper named and typed columns instead of
    using IDL.
    
    Closes scylladb/scylladb#23151

--- tombstone_gc_extension.hh
+++ tombstone_gc_extension.hh
@@ -21,6 +21,10 @@ class tombstone_gc_extension : public schema_extension {
 public:
     static constexpr auto NAME = "tombstone_gc";
 
+    // tombstone_gc_extension was written before schema_extension was deprecated, so support it
+    // without warnings
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     tombstone_gc_extension() = default;
     tombstone_gc_extension(const tombstone_gc_options& opts) : _tombstone_gc_options(opts) {}
     explicit tombstone_gc_extension(std::map<seastar::sstring, seastar::sstring> tags) : _tombstone_gc_options(std::move(tags)) {}
@@ -28,6 +32,7 @@ public:
     explicit tombstone_gc_extension(const seastar::sstring& s) {
         throw std::logic_error("Cannot create tombstone_gc_extension info from string");
     }
+#pragma clang diagnostic pop
     bytes serialize() const override {
         return ser::serialize_to_buffer<bytes>(_tombstone_gc_options.to_map());
     }
