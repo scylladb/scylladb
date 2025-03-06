cd54fdaf0072193e3d8e352a23d8765f976929ab
Michael Litvak <michael.litvak@scylladb.com>
2025-03-06 10:59:57 +0200

    tablets: create tablet map from an existing base table
    When creating a tablet map for a new table, add the option to create the
    tablet map from an existing base table.
    
    In this case, we inherit the tablet count and tablet replicas from the
    tablet map of the base table, making them co-located, and set the
    base_table of the new tablet map to point to the base table.

--- service/tablet_allocator.cc
+++ service/tablet_allocator.cc
@@ -3067,16 +3067,33 @@ public:
         auto rs = abstract_replication_strategy::create_replication_strategy(ksm.strategy_name(), params);
         if (auto&& tablet_rs = rs->maybe_as_tablet_aware()) {
             auto tm = _db.get_shared_token_metadata().get();
-            lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
-            auto lb = make_load_balancer(tm, nullptr, {});
-            auto plan = lb.make_sizing_plan(s.shared_from_this(), tablet_rs).get();
-            auto& table_plan = plan.tables[s.id()];
-            if (table_plan.target_tablet_count_aligned != table_plan.target_tablet_count) {
-                lblogger.info("Rounding up tablet count from {} to {} for table {}.{}", table_plan.target_tablet_count,
-                        table_plan.target_tablet_count_aligned, s.ks_name(), s.cf_name());
-            }
-            auto tablet_count = table_plan.target_tablet_count_aligned;
-            auto map = tablet_rs->allocate_tablets_for_new_table(s.shared_from_this(), tm, tablet_count).get();
+
+            tablet_map map = std::invoke([&] {
+                if (auto base_table = _db.get_base_table_for_tablet_colocation(s); base_table && _db.features().colocated_tablets) {
+                    auto base_id = *base_table;
+                    auto base_map = tm->tablets().get_tablet_map(base_id);
+                    lblogger.debug("Creating tablets for {}.{} id={} with base={}", s.ks_name(), s.cf_name(), s.id(), base_id);
+
+                    // TODO MICHAEL is it ok to copy the entire map including transitions and other fields?
+                    // what if base is during transition?
+                    tablet_map map(base_map);
+                    map.set_base_table(base_id);
+                    return map;
+                } else {
+                    lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
+                    auto lb = make_load_balancer(tm, nullptr, {});
+                    auto plan = lb.make_sizing_plan(s.shared_from_this(), tablet_rs).get();
+                    auto& table_plan = plan.tables[s.id()];
+                    if (table_plan.target_tablet_count_aligned != table_plan.target_tablet_count) {
+                        lblogger.info("Rounding up tablet count from {} to {} for table {}.{}", table_plan.target_tablet_count,
+                                table_plan.target_tablet_count_aligned, s.ks_name(), s.cf_name());
+                    }
+                    auto tablet_count = table_plan.target_tablet_count_aligned;
+                    auto map = tablet_rs->allocate_tablets_for_new_table(s.shared_from_this(), tm, tablet_count).get();
+                    return map;
+                }
+            });
+
             muts.emplace_back(tablet_map_to_mutation(map, s.id(), s.ks_name(), s.cf_name(), ts, _db.features()).get());
         }
     }
