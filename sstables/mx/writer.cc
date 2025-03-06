87594d06caac8afdf68182a567b534eb82e364d4
Pavel Emelyanov <xemul@scylladb.com>
2025-03-06 14:50:20 +0300

    sstables: Make sstable::index_filename() return component_name
    Most of the method callers use it as log parameter. There are few more
    places that push it to malformed_sstable_exception, which immediately
    converts it to string, so this patch makes the exception be constructed
    with the component_name either.
    
    And there's one more place that passes this string to file_writer
    constructor. For now, convert it to string explicitly, but next patches
    fill fix that place to use pure component_name too.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- sstables/mx/writer.cc
+++ sstables/mx/writer.cc
@@ -894,7 +894,7 @@ void writer::init_file_writers() {
     }
 
     out = _sst._storage->make_data_or_index_sink(_sst, component_type::Index).get();
-    _index_writer = std::make_unique<file_writer>(output_stream<char>(std::move(out)), _sst.index_filename());
+    _index_writer = std::make_unique<file_writer>(output_stream<char>(std::move(out)), fmt::to_string(_sst.index_filename()));
 }
 
 std::unique_ptr<file_writer> writer::close_writer(std::unique_ptr<file_writer>& w) {
