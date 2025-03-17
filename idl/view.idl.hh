Jadw1/scylla

message/messaging_service: add view building RPCs
This patch adds:
- `build_views_range` - to build particular range on given host and
  shard
- `abort_vbc_work` - to abort work on a particular shard on given host

idl/view.idl.hh
@@ -6,11 +6,16 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+#include "dht/i_partitioner_fwd.hh"
+
 namespace db {
 namespace view {
 class update_backlog {
     size_t get_current_bytes();
     size_t get_max_bytes();
 };
+
+verb [[cancellable]] build_views_range(table_id base_id, unsigned shard, dht::token_range range, std::vector<table_id> views);
+verb [[one_way]] abort_vbc_work(unsigned shard);
 }
 }

message/messaging_service.cc
@@ -691,6 +691,8 @@ static constexpr unsigned do_get_rpc_client_idx(messaging_verb verb) {
     case messaging_verb::TABLET_CLEANUP:
     case messaging_verb::TABLET_REPAIR:
     case messaging_verb::TABLE_LOAD_STATS:
+    case messaging_verb::BUILD_VIEWS_RANGE:
+    case messaging_verb::ABORT_VBC_WORK:
         return 1;
     case messaging_verb::CLIENT_ID:
     case messaging_verb::MUTATION:

scylladb/scylladb

idl/view.idl.hh
@@ -6,11 +6,16 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+#include "dht/i_partitioner_fwd.hh"
+
 namespace db {
 namespace view {
 class update_backlog {
     size_t get_current_bytes();
     size_t get_max_bytes();
 };
+
+verb [[cancellable]] build_views_range(table_id base_id, unsigned shard, dht::token_range range, std::vector<table_id> views);
+verb [[one_way]] abort_view_building_work(unsigned shard);
 }
 }

message/messaging_service.cc
@@ -700,6 +700,8 @@ static constexpr unsigned do_get_rpc_client_idx(messaging_verb verb) {
     case messaging_verb::TABLET_REPAIR:
     case messaging_verb::TABLE_LOAD_STATS_V1:
     case messaging_verb::TABLE_LOAD_STATS:
+    case messaging_verb::BUILD_VIEWS_RANGE:
+    case messaging_verb::ABORT_VIEW_BUILDING_WORK:
         return 1;
     case messaging_verb::CLIENT_ID:
     case messaging_verb::MUTATION: