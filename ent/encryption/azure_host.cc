5228c36b90ff15d62ae01226debab7f0fff7a787
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-03-19 19:52:41 +0200

    encryption: azure: Add credentials in Azure host
    The Azure host needs credentials to communicate with Key Vault.
    
    First search for credentials in the host options, and then fall back to
    default credentials if the former are non-existent or incomplete.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- ent/encryption/azure_host.cc
+++ ent/encryption/azure_host.cc
@@ -12,6 +12,8 @@
 #include "utils/log.hh"
 #include "utils/hash.hh"
 #include "utils/loading_cache.hh"
+#include "utils/azure/identity/default_credentials.hh"
+#include "utils/azure/identity/service_principal_credentials.hh"
 #include "azure_host.hh"
 #include "encryption.hh"
 
@@ -33,7 +35,9 @@ public:
     future<key_ptr> get_key_by_id(const id_type&, const key_info&);
 private:
     const std::string _name;
+    const std::string _log_prefix;
     const host_options _options;
+    std::unique_ptr<azure::credentials> _credentials;
 
     struct attr_cache_key {
         seastar::sstring master_key;
@@ -65,12 +69,25 @@ private:
 
 azure_host::impl::impl(const std::string& name, const host_options& options)
     : _name(name)
+    , _log_prefix(fmt::format("AzureVault:{}", name))
     , _options(options)
+    , _credentials()
     , _attr_cache(utils::loading_cache_config{
         .max_size = std::numeric_limits<size_t>::max(),
         .expiry = options.key_cache_expiry.value_or(default_expiry),
         .refresh = options.key_cache_refresh.value_or(default_refresh)}, azlog, std::bind_front(&impl::create_key, this))
-{}
+{
+    if (!_options.tenant_id.empty() && !_options.client_id.empty() && (!_options.client_secret.empty() || !_options.client_cert.empty())) {
+        _credentials = std::make_unique<azure::service_principal_credentials>(
+                options.tenant_id, options.client_id, options.client_secret, options.client_cert,
+                options.authority, options.truststore, options.priority_string, _log_prefix);
+        return;
+    }
+    azlog.debug("[{}] No credentials configured. Falling back to default credentials.", _log_prefix);
+    _credentials = std::make_unique<azure::default_credentials>(
+            azure::default_credentials::all_sources, _options.imds_endpoint,
+            _options.truststore, _options.priority_string, _log_prefix);
+}
 
 future<> azure_host::impl::init() {
     throw std::logic_error("Not implemented");
