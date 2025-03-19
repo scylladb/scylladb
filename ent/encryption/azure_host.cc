470513b433937d5e9d43cc4aaa09834200903355
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-03-19 19:59:11 +0200

    encryption: azure: Implement get_or_create_key()
    Implement the `azure_host::get_or_create_key()` API, which returns a
    data encryption key for a given algorithm descriptor (cipher algorithm
    and key length).
    
    Use a loading cache to reduce the API calls to Key Vault. When the cache
    needs to refresh or reload a key, always create a new one and wrap it
    with the Vault key.
    
    For the REST API calls to Key Vault, use an ephemeral HTTP client and
    configure it to not wait for the server's response when terminating a
    TLS connection. Although the TLS protocol requires clients to wait on
    the server's response to a close_notify alert, the Key Vault service
    ignores this, causing the client to block for 10 seconds (hardcoded)
    before timing out.
    
    Use the following identifier for each key:
    <vault name>/<key name>/<key version>:<base64 encoded ciphertext of data encryption key>
    
    The key version is required to support Vault key rotations.
    
    Finally, define an exception for Vault errors.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- ent/encryption/azure_host.cc
+++ ent/encryption/azure_host.cc
@@ -9,19 +9,67 @@
 
 #include <stdexcept>
 
+#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include "db/config.hh"
 #include "utils/log.hh"
 #include "utils/hash.hh"
+#include "utils/rjson.hh"
+#include "utils/base64.hh"
 #include "utils/loading_cache.hh"
+#include "utils/rest/client.hh"
 #include "utils/azure/identity/default_credentials.hh"
 #include "utils/azure/identity/service_principal_credentials.hh"
 #include "azure_host.hh"
 #include "encryption.hh"
+#include "encryption_exceptions.hh"
 
 using namespace std::chrono_literals;
 
 static logging::logger azlog("azure_vault");
 
 namespace encryption {
+
+vault_error vault_error::make_error(const http::reply::status_type& status, std::string_view result) {
+    const auto& jres = rjson::parse(result);
+    const auto& error_details = rjson::get(jres, ERROR_KEY);
+    return vault_error(
+            status,
+            rjson::get<std::string>(error_details, ERROR_CODE_KEY),
+            rjson::get<std::string>(error_details, ERROR_MESSAGE_KEY)
+    );
+}
+
+class vault_log_filter : public rest::http_log_filter {
+public:
+    enum class op_type {
+        wrapkey,
+    };
+
+    explicit vault_log_filter(op_type op) : _op(op) {}
+
+    string_opt filter_header(std::string_view name, std::string_view value) const override {
+        if (boost::iequals(name, "Authorization") && value.starts_with("Bearer")) {
+            return REDACTED_VALUE;
+        }
+        return std::nullopt;
+    }
+
+    string_opt filter_body(body_type type, std::string_view body) const override {
+        if (_op == op_type::wrapkey && type == body_type::request) {
+            auto j = rjson::parse(body);
+            auto val = rjson::find(j, "value");
+            if (val) {
+                val->SetString(REDACTED_VALUE);
+                return rjson::print(j);
+            }
+        }
+        return std::nullopt;
+    }
+private:
+    op_type _op;
+};
 
 class azure_host::impl {
 public:
@@ -64,6 +112,17 @@ private:
     >;
     cache_type<attr_cache_key, key_and_id_type, attr_cache_key_hash> _attr_cache;
 
+    static constexpr char AKV_HOST_TEMPLATE[] = "{}.vault.azure.net";
+    static constexpr char AKV_PATH_TEMPLATE[] = "/keys/{}/{}/{}?api-version=7.4";
+    static constexpr char AKV_LATEST_VERSION[] = ""; // an empty version denotes the latest
+    static constexpr char AKV_WRAPKEY_OP[] = "wrapkey";
+    static constexpr char AKV_ENCRYPTION_ALG[] = "RSA-OAEP-256";
+    static constexpr char AKV_TOKEN_RESOURCE_URI[] = "https://vault.azure.net"; // no trailing slash
+
+    static std::tuple<std::string, std::string> parse_key(std::string_view);
+    static std::tuple<std::string, std::string, unsigned> parse_vault(std::string_view);
+    future<shared_ptr<tls::certificate_credentials>> make_creds();
+    future<rjson::value> send_request(const sstring& host, unsigned port, bool use_https, const sstring& path, const rjson::value& body, const rest::http_log_filter& filter);
     future<key_and_id_type> create_key(const attr_cache_key&);
 };
 
@@ -89,6 +148,25 @@ azure_host::impl::impl(const std::string& name, const host_options& options)
             _options.truststore, _options.priority_string, _log_prefix);
 }
 
+/**
+ * Wraps exceptions to encryption::base_error exceptions.
+ * Should be used in all public methods.
+ */
+template <typename T, typename Callable>
+static future<T> wrap_exceptions(const std::string& context, Callable&& func) {
+    try {
+        co_return co_await func();
+    } catch (base_error&) {
+        throw;
+    } catch (const std::invalid_argument& e) {
+        std::throw_with_nested(configuration_error(fmt::format("{}: {}", context, e.what())));
+    } catch (const rjson::malformed_value& e) {
+        std::throw_with_nested(malformed_response_error(fmt::format("{}: {}", context, e.what())));
+    } catch (...) {
+        std::throw_with_nested(service_error(fmt::format("{}: {}", context, std::current_exception())));
+    }
+}
+
 future<> azure_host::impl::init() {
     throw std::logic_error("Not implemented");
 }
@@ -98,15 +176,146 @@ const azure_host::host_options& azure_host::impl::options() const {
 }
 
 future<azure_host::key_and_id_type> azure_host::impl::get_or_create_key(const key_info& info) {
-    throw std::logic_error("Not implemented");
-}
+    attr_cache_key key {
+        .master_key = _options.master_key,
+        .info = info,
+    };
 
-future<azure_host::key_and_id_type> azure_host::impl::create_key(const attr_cache_key& key) {
-    throw std::logic_error("Not implemented");
+    if (key.master_key.empty()) {
+        throw configuration_error(fmt::format("[{}] No master key set in azure host config or encryption attributes", _log_prefix));
+    }
+    co_return co_await wrap_exceptions<key_and_id_type>("get_or_create_key", [this, &key] -> future<key_and_id_type> {
+        co_return co_await _attr_cache.get(key);
+    });
 }
 
 future<azure_host::key_ptr> azure_host::impl::get_key_by_id(const azure_host::id_type& id, const key_info& info) {
     throw std::logic_error("Not implemented");
+}
+
+std::tuple<std::string, std::string> azure_host::impl::parse_key(std::string_view spec) {
+    auto i = spec.find_last_of('/');
+    if (i == std::string_view::npos) {
+        throw std::invalid_argument(fmt::format("Invalid master key spec '{}'. Must be in format <vaultname>/<keyname>", spec));
+    }
+    if (i >= spec.size() - 1) {
+        throw std::invalid_argument(fmt::format("Invalid master key spec '{}'. Key name is missing. Expected format: <vaultname>/<keyname>", spec));
+    }
+    return std::make_tuple(std::string(spec.substr(0, i)), std::string(spec.substr(i + 1)));
+}
+
+std::tuple<std::string, std::string, unsigned> azure_host::impl::parse_vault(std::string_view vault) {
+    static const boost::regex vault_name_re(R"([a-zA-Z0-9-]+)");
+    static const boost::regex vault_endpoint_re(R"((https?)://([^/:]+)(?::(\d+))?)");
+
+    boost::smatch match;
+    std::string tmp{vault};
+
+    if (boost::regex_match(tmp, match, vault_name_re)) {
+        // If the vault is just a name, use the default Azure Key Vault endpoint.
+        return {"https", fmt::format(AKV_HOST_TEMPLATE, vault), 443};
+    }
+
+    if (boost::regex_match(tmp, match, vault_endpoint_re)) {
+        std::string scheme = match[1];
+        std::string host = match[2];
+        std::string port_str = match[3];
+
+        unsigned port = (port_str.empty()) ? (scheme == "https" ? 443 : 80) : std::stoi(port_str);
+        return {scheme, host, port};
+    }
+
+    throw std::invalid_argument(fmt::format("Invalid vault '{}'. Must be either a name or an endpoint in format: http(s)://<host>[:port]", vault));
+}
+
+future<shared_ptr<tls::certificate_credentials>> azure_host::impl::make_creds() {
+    auto creds = ::make_shared<tls::certificate_credentials>();
+    if (!_options.priority_string.empty()) {
+        creds->set_priority_string(_options.priority_string);
+    } else {
+        creds->set_priority_string(db::config::default_tls_priority);
+    }
+    if (!_options.truststore.empty()) {
+        co_await creds->set_x509_trust_file(_options.truststore, seastar::tls::x509_crt_format::PEM);
+    } else {
+        co_await creds->set_system_trust();
+    }
+    co_return creds;
+}
+
+future<rjson::value> azure_host::impl::send_request(const sstring& host, unsigned port, bool use_https, const sstring& path, const rjson::value& body, const rest::http_log_filter& filter) {
+    auto token = co_await _credentials->get_access_token(AKV_TOKEN_RESOURCE_URI);
+
+    shared_ptr<tls::certificate_credentials> creds;
+    std::optional<seastar::tls::tls_options> options;
+
+    if (use_https) {
+        creds = co_await make_creds();
+        // Do not wait when terminating the TLS connection. TLS close_notify
+        // alerts are ignored by the Key Vault service.
+        //
+        // Also, do not use numeric hosts as SNI hostnames. SNI works only with DNS hostnames.
+        // This by extension also disables hostname validation, but that's fine; numeric hosts
+        // may appear only in testing.
+        bool is_numeric_host = seastar::net::inet_address::parse_numerical(host).has_value();
+        sstring server_name = is_numeric_host ? sstring{} : host;
+        options = { .wait_for_eof_on_shutdown = false, .server_name = server_name };
+    }
+
+    rest::httpclient client(host, port, std::move(creds), options);
+    client.target(path);
+    client.method(httpd::operation_type::POST);
+    client.add_header("Authorization", fmt::format("Bearer {}", token.token));
+    client.add_header("Content-Type", "application/json");
+    client.content(std::move(rjson::print(body)));
+
+    azlog.trace("Sending request: {}", rest::redacted_request_type{ client.request(), filter });
+
+    auto res = co_await client.send();
+    if (res.result() == http::reply::status_type::ok) {
+        azlog.trace("Got response: {}", rest::redacted_result_type{ res, filter });
+        co_return rjson::parse(res.body());
+    } else {
+        azlog.trace("Got unexpected response: {}", rest::redacted_result_type{ res, filter });
+        throw vault_error::make_error(res.result(), res.body());
+    }
+}
+
+future<azure_host::key_and_id_type> azure_host::impl::create_key(const attr_cache_key& k) {
+    static const vault_log_filter filter{vault_log_filter::op_type::wrapkey};
+    auto& info = k.info;
+    azlog.debug("[{}] Creating new key: {}", _log_prefix, info);
+    auto [vault, keyname] = parse_key(k.master_key);
+    auto [scheme, host, port] = parse_vault(vault);
+    auto key = make_shared<symmetric_key>(info);
+    auto path = fmt::format(AKV_PATH_TEMPLATE, keyname, AKV_LATEST_VERSION, AKV_WRAPKEY_OP);
+    auto body = [&key] {
+        auto b = rjson::empty_object();
+        rjson::add(b, "alg", AKV_ENCRYPTION_ALG);
+        rjson::add(b, "value", base64url_encode(key->key()));
+        return b;
+    }();
+    rjson::value resp;
+    try {
+        resp = co_await send_request(host, port, scheme == "https", path, body, filter);
+    } catch (...) {
+        azlog.error("[{}] Failed to wrap key {} with master_key={}: {}", _log_prefix, info, k.master_key, std::current_exception());
+        throw;
+    }
+    auto key_id = rjson::get<std::string>(resp, "kid");
+    auto cipher = rjson::get<std::string>(resp, "value");
+    boost::regex version_regex(R"foo(.*/([^/]+)$)foo");
+    boost::smatch match;
+    if (!boost::regex_search(key_id, match, version_regex)) {
+        throw std::runtime_error(fmt::format("Failed to parse key version from key id {}", key_id));
+    }
+    auto key_version = match[1].str();
+
+    auto sid = fmt::format("{}/{}/{}:{}", vault, keyname, key_version, cipher);
+    bytes id(sid.begin(), sid.end());
+
+    azlog.trace("[{}] Created key id {}", _log_prefix, sid);
+    co_return key_and_id_type{ key, id };
 }
 
 // ==================== azure_host class implementation ====================
--- ent/encryption/azure_host.hh
+++ ent/encryption/azure_host.hh
@@ -11,6 +11,7 @@
 
 #include <memory>
 
+#include <seastar/http/reply.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/shared_ptr.hh>
 
@@ -19,6 +20,44 @@
 namespace encryption {
 
 class encryption_context;
+
+class vault_error : public std::exception {
+    // The error response always contains a code and a message.
+    // https://github.com/microsoft/api-guidelines/blob/vNext/azure/Guidelines.md#handling-errors
+    static constexpr char ERROR_KEY[] = "error";
+    static constexpr char ERROR_CODE_KEY[] = "code";
+    static constexpr char ERROR_MESSAGE_KEY[] = "message";
+    http::reply::status_type _status;
+    std::string _code, _msg;
+public:
+    vault_error(const http::reply::status_type& status, std::string_view code, std::string_view msg)
+        : _status(status)
+        , _code(code)
+        , _msg(fmt::format("{}: {}", code, msg))
+    {}
+    const http::reply::status_type& status() const {
+        return _status;
+    }
+    const std::string& code() const {
+        return _code;
+    }
+    const char* what() const noexcept override {
+        return _msg.c_str();
+    }
+    static vault_error make_error(const http::reply::status_type& status, std::string_view result);
+};
+
+// Common error codes from Azure Key Vault:
+// https://learn.microsoft.com/en-us/azure/key-vault/general/rest-error-codes
+//
+// Not to be confused with the more specialized 'innererror' codes:
+// https://learn.microsoft.com/en-us/azure/key-vault/general/common-error-codes
+namespace vault_errors {
+    [[maybe_unused]] static constexpr char Unauthorized[] = "Unauthorized";
+    [[maybe_unused]] static constexpr char Forbidden[] = "Forbidden";
+    [[maybe_unused]] static constexpr char Throttled[] = "Throttled";
+    [[maybe_unused]] static constexpr char KeyNotFound[] = "KeyNotFound";
+}
 
 class azure_host {
 public:
