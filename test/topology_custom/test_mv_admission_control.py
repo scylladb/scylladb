186166ae592e17a715f7d9a39dab8ec6a5b47e78
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-03-28 20:17:48 +0100

    test/cluster/mv: Adjust test to RF-rack-valid keyspaces
    We adjust the test in the directory so that all of the used
    keyspaces are RF-rack-valid throughout the their execution.
    
    Refs scylladb/scylladb#23428
    
    Closes scylladb/scylladb#23490
    
    (cherry picked from commit 10589e966facb07b381754322297a4436de954ee)

--- test/topology_custom/test_mv_admission_control.py
+++ test/topology_custom/test_mv_admission_control.py
@@ -67,8 +67,10 @@ async def test_mv_admission_control_exception(manager: ManagerClient) -> None:
 @skip_mode('release', "error injections aren't enabled in release mode")
 async def test_mv_retried_writes_reach_all_replicas(manager: ManagerClient) -> None:
     node_count = 4
-    servers = await manager.servers_add(node_count - 1, config={'error_injections_at_startup': ['update_backlog_immediately'], 'tablets_mode_for_new_keyspaces': 'enabled'})
-    server = await manager.server_add(config={'error_injections_at_startup': ['view_update_limit', 'delay_before_remote_view_update', 'update_backlog_immediately'], 'tablets_mode_for_new_keyspaces': 'enabled'})
+    cfg = {'error_injections_at_startup': ['update_backlog_immediately'], 'tablets_mode_for_new_keyspaces': 'enabled'}
+    cfg_slow = {'error_injections_at_startup': ['view_update_limit', 'delay_before_remote_view_update', 'update_backlog_immediately'], 'tablets_mode_for_new_keyspaces': 'enabled'}
+    servers = await manager.servers_add(node_count - 1, config=cfg, auto_rack_dc="dc1")
+    server = await manager.server_add(config=cfg_slow, property_file={"dc": servers[0].datacenter, "rack": servers[0].rack})
 
     cql, hosts = await manager.get_ready_cql(servers)
     async with new_test_keyspace(manager, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3} AND tablets = {'initial': 1}") as ks:
