c0d482c9fc9a6722b73af7af97960a7328490451
Michael Litvak <michael.litvak@scylladb.com>
2025-03-30 16:21:13 +0300

    cdc: virtual tables for cdc with tablets
    Define two new virtual tables in system keyspace: cdc_timestamps and
    cdc_streams. They expose the internal cdc metadata for tablets-enabled
    keyspace to be consumed by users consuming the CDC log.
    
    cdc_timestamps lists all timestamps for a table where a stream change
    occured.
    
    cdc_streams list additionally the current streams sets for each
    table and timestamp, as well as difference - closed and opened streams -
    from the previous stream set.

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -64,6 +64,7 @@ namespace cql_transport { class controller; }
 
 namespace cdc {
 class generation_service;
+class metadata;
 }
 
 namespace streaming {
@@ -346,6 +347,8 @@ public:
     }
 
     bool is_raft_leader() const noexcept;
+
+    const cdc::metadata& get_cdc_metadata() const noexcept;
 
 private:
     inet_address get_broadcast_address() const noexcept {
