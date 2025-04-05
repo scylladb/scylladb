d9aa2f3f5e02a8ef98e5712cef0046539a03e975
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-04-05 22:07:16 +0300

    utils: azure: Get access token with SP secret
    Implement token request for Service Principals with a secret.
    
    The token request requires a TLS connection. When closing the
    connection, do not wait for a response to the TLS `close_notify` alert.
    Azure's OAuth server would ignore it and the Seastar `connected_socket`
    would hang for 10 seconds.
    
    Add log redaction logic to not expose sensitive data from the request
    and response payloads.
    
    Add a token factory to parse the HTTP response. This cannot be shared
    with other credential types because the JSON format is not consistent.
    
    Finally, implement a fail-fast retry policy for short-lived transient
    errors.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- utils/azure/identity/service_principal_credentials.cc
+++ utils/azure/identity/service_principal_credentials.cc
@@ -9,9 +9,45 @@
 
 #include <boost/regex.hpp>
 
+#include "db/config.hh"
+#include "utils/rest/client.hh"
+#include "exceptions.hh"
 #include "service_principal_credentials.hh"
 
 namespace azure {
+
+class sp_log_filter : public rest::nop_log_filter {
+public:
+    string_opt filter_body(body_type type, std::string_view body) const override {
+        if (type == body_type::request) {
+            for (const auto& param : {"client_secret="}) {
+                size_t start_pos = body.find(param);
+                if (start_pos == std::string_view::npos) {
+                    continue;
+                }
+                size_t value_start_pos = start_pos + std::string(param).length();
+                size_t end_pos = body.find('&', value_start_pos);
+                if (end_pos == std::string_view::npos) {
+                    end_pos = body.length();
+                }
+                return fmt::format("{}{}{}",
+                        body.substr(0, value_start_pos),
+                        REDACTED_VALUE,
+                        body.substr(end_pos));
+            }
+        } else if (type == body_type::response) {
+            if (!body.empty()) {
+                auto j = rjson::parse(body);
+                auto val = rjson::find(j, "access_token");
+                if (val) {
+                    val->SetString(REDACTED_VALUE);
+                    return rjson::print(j);
+                }
+            }
+        }
+        return std::nullopt;
+    }
+};
 
 service_principal_credentials::service_principal_credentials(const sstring& tenant_id,
         const sstring& client_id, const sstring& client_secret, const sstring& client_cert,
@@ -46,8 +82,143 @@ service_principal_credentials::service_principal_credentials(const sstring& tena
     }
 }
 
+static future<::shared_ptr<tls::certificate_credentials>> make_creds(const sstring& truststore, const sstring& priority_string) {
+    auto creds = seastar::make_shared<tls::certificate_credentials>();
+    if (!priority_string.empty()) {
+        creds->set_priority_string(priority_string);
+    } else {
+        creds->set_priority_string(db::config::default_tls_priority);
+    }
+    if (!truststore.empty()) {
+        co_await creds->set_x509_trust_file(truststore, seastar::tls::x509_crt_format::PEM);
+    } else {
+        co_await creds->set_system_trust();
+    }
+    co_return creds;
+}
+
+/**
+ * @brief Retries for transient errors.
+ *
+ * Retries are performed for 408, 429, 500, 502, 503, and 504 errors, using an exponential backoff strategy.
+ * Three retries are attempted in total.
+ * The latencies between retries are 0, 100, and 300 milliseconds.
+ *
+ * The error codes are based on the generic retry policy of the Azure C++ SDK:
+ * https://github.com/Azure/azure-sdk-for-cpp/blob/126452efd30860263398a152f11f337007f529f4/sdk/core/azure-core/inc/azure/core/http/policies/policy.hpp#L133
+ *
+ * The exponential backoff strategy follows the one for the `managed_identity_credentials`.
+ *
+ * @param func The asynchronous function to execute and retry on failure.
+ * @return The result of the function if successful, or throws if all retries fail or a non-retriable error occurs.
+ */
+future<sstring> service_principal_credentials::with_retries(std::function<future<sstring>()> func) {
+    constexpr int MAX_RETRIES = 3;
+    constexpr std::chrono::milliseconds DELTA_BACKOFF {100};
+    std::chrono::milliseconds backoff;
+
+    int retries = 0;
+    while (true) {
+        try {
+            co_return co_await func();
+        } catch (const creds_auth_error& e) {
+            auto status = e.status();
+            bool should_retry =
+                    status == http::reply::status_type::request_timeout ||
+                    status == http::reply::status_type::too_many_requests ||
+                    status == http::reply::status_type::internal_server_error ||
+                    status == http::reply::status_type::bad_gateway ||
+                    status == http::reply::status_type::service_unavailable ||
+                    status == http::reply::status_type::gateway_timeout;
+
+            if (retries >= MAX_RETRIES || !should_retry) {
+                throw;
+            }
+
+            backoff = DELTA_BACKOFF * ((1 << retries) - 1);
+            az_creds_logger.debug("[{}] Token request failed with status {}. Reason: {}. Retrying in {} ms...",
+                    *this, static_cast<int>(status), e.what(), backoff.count());
+
+            retries++;
+        }
+        co_await seastar::sleep(backoff);
+    }
+}
+
+future<sstring> service_principal_credentials::post(const sstring& body) {
+    static const sp_log_filter filter{};
+    const auto op = httpd::operation_type::POST;
+    const auto path = seastar::format(AZURE_ENTRA_ID_TOKEN_PATH_TEMPLATE, _tenant_id);
+    const auto mime_type = MIME_TYPE;
+
+    shared_ptr<tls::certificate_credentials> creds;
+    std::optional<seastar::tls::tls_options> options;
+
+    if (_is_secured) {
+        creds = co_await make_creds(_truststore, _priority_string);
+        options = { .wait_for_eof_on_shutdown = false, .server_name = _host };
+    }
+
+    rest::httpclient client{_host, _port, std::move(creds), options};
+    client.target(path);
+    client.method(op);
+    client.add_header("Content-Type", mime_type);
+    client.content(std::move(body));
+
+    if (az_creds_logger.is_enabled(log_level::trace)) {
+        az_creds_logger.trace("[{}] Sending request: {}", *this, rest::redacted_request_type{ client.request(), filter });
+    }
+
+    auto res = co_await client.send();
+    if (res.result() == http::reply::status_type::ok) {
+        if (az_creds_logger.is_enabled(log_level::trace)) {
+            az_creds_logger.trace("[{}] Got response: {}", *this, rest::redacted_result_type{ res, filter });
+        }
+    } else {
+        if (az_creds_logger.is_enabled(log_level::trace)) {
+            az_creds_logger.trace("[{}] Got unexpected response: {}", *this, rest::redacted_result_type{ res, filter });
+        }
+        throw creds_auth_error::make_error(res.result(), res.body());
+    }
+    co_return res.body();
+}
+
+access_token service_principal_credentials::make_token(const rjson::value& json, const resource_type& resource_uri) {
+    auto token = rjson::get<std::string>(json, "access_token");
+    auto expires_in = timeout_clock::now() + std::chrono::seconds(rjson::get<int>(json, "expires_in"));
+    return { token, expires_in, resource_uri };
+}
+
 future<> service_principal_credentials::refresh(const resource_type& resource_uri) {
-    throw std::logic_error("Not implemented");
+    az_creds_logger.debug("[{}] Refreshing token for principal: {}", *this, _client_id);
+    if (_client_secret != "") {
+        co_await refresh_with_secret(resource_uri);
+    } else {
+        co_await refresh_with_certificate(resource_uri);
+    }
+}
+
+/**
+ * Token request with secret.
+ * Based on: https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-client-creds-grant-flow#first-case-access-token-request-with-a-shared-secret
+ */
+future<> service_principal_credentials::refresh_with_secret(const resource_type& resource_uri) {
+    // Scopes for the client credentials flow must contain only one resource
+    // identifier and only the .default scope.
+    auto scope = seastar::format("{}/.default", resource_uri);
+    sstring grant_type = "client_credentials";
+    sstring body = seastar::format(
+            "client_id={}&scope={}&client_secret={}&grant_type={}",
+            _client_id,
+            seastar::http::internal::url_encode(scope),
+            seastar::http::internal::url_encode(_client_secret),
+            grant_type);
+    auto resp = co_await with_retries([&] { return post(body); });
+    _token = make_token(rjson::parse(resp), resource_uri);
+}
+
+future<> service_principal_credentials::refresh_with_certificate(const resource_type& resource_uri) {
+     throw std::logic_error("Not implemented");
 }
 
 }
