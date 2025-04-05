14e262cf5f345f9cf7eec531cd1aa9d24be76c6e
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-04-05 22:22:55 +0300

    utils: azure: Get access token from IMDS
    Implement token request from IMDS.
    
    No credentials are required for that - just a plain HTTP request on the
    IMDS token endpoint.
    
    Since the IMDS endpoint is a raw IP, it's not possible to reliably
    determine whether IMDS is accessible or not (i.e., whether the node is
    an Azure VM). Azure provides no node-local indication either. In lack of
    a better choice, attempt to connect and declare failure if the
    connection is not established within 3 seconds. Use a raw TCP socket for
    this check, as the HTTP client currently lacks timeout or cancellation
    support. Perform the check only once, during the first token refresh.
    
    For the time being, do not support nodes with multiple user-assigned
    managed identities. Expect the token request to fail in this case (IMDS
    requires the identifier of the desired Managed Identity).
    
    Add a token factory to correctly parse the HTTP response. This addresses
    a discrepancy between token requests on IMDS and Azure Entra - the
    'expires_in' field is a string in the former and an integer in the
    latter.
    
    Finally, implement a fail-fast retry policy for short-lived transient
    errors.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- utils/azure/identity/managed_identity_credentials.hh
+++ utils/azure/identity/managed_identity_credentials.hh
@@ -9,6 +9,7 @@
 
 #pragma once
 
+#include "utils/rjson.hh"
 #include "credentials.hh"
 
 namespace azure {
@@ -16,12 +17,18 @@ namespace azure {
 class managed_identity_credentials : public credentials {
     static constexpr char NAME[] = "ManagedIdentityCredentials";
     static constexpr char IMDS_HOST[] = "169.254.169.254";
+    static constexpr char IMDS_API_VERSION[] = "2018-02-01";
+    static constexpr char IMDS_TOKEN_PATH_TEMPLATE[] = "/metadata/identity/oauth2/token?api-version={}&resource={}";
 
     sstring _host{IMDS_HOST};
     unsigned _port{80};
+    bool _connectivity_checked{false};
 
     std::string_view get_name() const override { return NAME; };
+    future<> with_retries(std::function<future<>()>);
+    future<> check_connectivity();
     future<> refresh(const resource_type& resource_uri) override;
+    access_token make_token(const rjson::value&, const resource_type&);
 public:
     managed_identity_credentials(const sstring& endpoint = "", const sstring& logctx = "");
 };
