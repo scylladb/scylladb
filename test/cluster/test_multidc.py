5ac07a6c724ce237ccdf25acee7c83693edaefe1
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-04-08 17:24:24 +0200

    test/cluster/test_multidc.py: Adjust to RF-rack-validity
    Instead of putting all of the nodes in a DC in the same rack
    in `test_putget_2dc_with_rf`, we assign them to different racks.
    The distribution of nodes in racks is orthogonal to what the test
    is doing and verifying, so the change is correct in that sense.
    At the same time, it ensures that the test never violates the
    invariant of RF-rack-valid keyspaces, so we can also run it
    with `rf_rack_valid_keyspaces` set to true.
    
    (cherry picked from commit 5b83304b3889c72034985821ae5f4ac5912a939e)

--- test/cluster/test_multidc.py
+++ test/cluster/test_multidc.py
@@ -58,10 +58,10 @@ async def test_putget_2dc_with_rf(
     table_name = "test_table_name"
     columns = [Column("name", TextType), Column("value", TextType)]
     logger.info("Create two servers in different DC's")
-    for i in nodes_list:
+    for rack_idx, dc_idx in enumerate(nodes_list):
         s_info = await manager.server_add(
             config=CONFIG,
-            property_file={"dc": f"dc{i}", "rack": "myrack"},
+            property_file={"dc": f"dc{dc_idx}", "rack": f"rack{rack_idx}"},
         )
         logger.info(s_info)
     conn = manager.get_cql()
