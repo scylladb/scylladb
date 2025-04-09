acc7f771456f7d5e571204905f0156c4017e3b6a
Andrei Chekun <andrei.chekun@scylladb.com>
2025-04-09 13:45:01 +0200

    test.py: clean code that isn't used anymore
    Clean code that is not used anymore

--- test/pylib/suite/__init__.py
+++ test/pylib/suite/__init__.py
@@ -4,11 +4,8 @@
 # SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
 #
 
-from test.pylib.suite.boost import BoostTestSuite
 from test.pylib.suite.cql_approval import CQLApprovalTestSuite
-from test.pylib.suite.ldap import LdapTestSuite
 from test.pylib.suite.python import PythonTestSuite
 from test.pylib.suite.run import RunTestSuite
 from test.pylib.suite.tool import ToolTestSuite
 from test.pylib.suite.topology import TopologyTestSuite
-from test.pylib.suite.unit import UnitTestSuite
