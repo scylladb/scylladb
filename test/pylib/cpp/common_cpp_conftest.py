01c139ddb0b6ba1096971c86bde9a7a61bd5d2da
Andrei Chekun <andrei.chekun@scylladb.com>
2025-04-11 13:45:01 +0200

    test.py: switch method of finding the root repo directory
    Switching to use constant defined in __init__ filet instead of getting
    the root directory from pytest's config

--- test/pylib/cpp/common_cpp_conftest.py
+++ test/pylib/cpp/common_cpp_conftest.py
@@ -95,7 +95,6 @@ def collect_items(file_path: PosixPath, parent: Collector, facade: CppTestFacade
         SCYLLA_TEST_ENV='yes',
     )
     run_id = parent.config.getoption('run_id')
-    project_root = Path(parent.session.config.rootpath).parent
     modes = get_modes_to_run(parent.session.config)
     suite_config = read_suite_config(file_path.parent)
     no_parallel_cases = suite_config.get('no_parallel_cases', [])
@@ -109,8 +108,8 @@ def collect_items(file_path: PosixPath, parent: Collector, facade: CppTestFacade
     if len(custom_args) > 1:
         return CppFile.from_parent(parent=parent, path=file_path, arguments=args, parameters=custom_args,
                                    no_parallel_run=no_parallel_run, modes=modes, disabled_tests=disabled_tests,
-                                   run_id=run_id, facade=facade, project_root=project_root, env=test_env)
+                                   run_id=run_id, facade=facade,  env=test_env)
     else:
         args.extend(custom_args)
         return CppFile.from_parent(parent=parent, path=file_path, arguments=args, no_parallel_run=no_parallel_run,
-                                   modes=modes, disabled_tests=disabled_tests, run_id=run_id, facade=facade, project_root=project_root, env=test_env)
+                                   modes=modes, disabled_tests=disabled_tests, run_id=run_id, facade=facade, env=test_env)
