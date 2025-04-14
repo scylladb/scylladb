193190fca8339e3d6ea317c50effb3861b82dc04
Kefu Chai <kefu.chai@scylladb.com>
2025-04-14 12:10:36 +0800

    s3/client: define a constant for security credential resource
    instead of repeating it, let's define a consstant and reuse it.
    less repeatings this way.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- utils/s3/credentials_providers/instance_profile_credentials_provider.cc
+++ utils/s3/credentials_providers/instance_profile_credentials_provider.cc
@@ -32,6 +32,8 @@ future<> instance_profile_credentials_provider::reload() {
     }
 }
 
+static constexpr auto EC2_SECURITY_CREDENTIALS_RESOURCE = "/latest/meta-data/iam/security-credentials";
+
 future<> instance_profile_credentials_provider::update_credentials() {
     auto factory = std::make_unique<utils::http::dns_connection_factory>(ec2_metadata_ip, port, false, ec2_md_logger);
     retryable_http_client http_client(std::move(factory), 1, retryable_http_client::ignore_exception, http::experimental::client::retry_requests::yes, retry_strategy);
@@ -49,7 +51,7 @@ future<> instance_profile_credentials_provider::update_credentials() {
         http::reply::status_type::ok);
 
     std::string role;
-    req = http::request::make("GET", ec2_metadata_ip, "/latest/meta-data/iam/security-credentials/");
+    req = http::request::make("GET", ec2_metadata_ip, seastar::format("{}/", EC2_SECURITY_CREDENTIALS_RESOURCE));
     req._headers["x-aws-ec2-metadata-token"] = token;
     co_await http_client.make_request(
         std::move(req),
@@ -59,7 +61,7 @@ future<> instance_profile_credentials_provider::update_credentials() {
         },
         http::reply::status_type::ok);
 
-    req = http::request::make("GET", ec2_metadata_ip, seastar::format("/latest/meta-data/iam/security-credentials/{}", role));
+    req = http::request::make("GET", ec2_metadata_ip, seastar::format("{}/{}", EC2_SECURITY_CREDENTIALS_RESOURCE, role));
     req._headers["x-aws-ec2-metadata-token"] = token;
     co_await http_client.make_request(
         std::move(req),
