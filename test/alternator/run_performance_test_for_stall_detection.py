353044800a9486c2dbd84d076c715b91f3567011
Radosław Cybulski <radoslaw.cybulski@scylladb.com>
2025-04-16 09:38:03 +0200

    test

--- test/alternator/run_performance_test_for_stall_detection.py
+++ test/alternator/run_performance_test_for_stall_detection.py
@@ -12,26 +12,11 @@ def parse_log_output_1(input_file, output_file):
 
     with open(input_file, 'r', encoding='utf8') as inp:
         data = inp.readlines()
-
-    for x in range(len(data)):
-        l = data[x]
-        if 'api - /system/log: test/alternator: Starting test_batch.py::' in l:
-            data2 = data[x + 1:]
-            break
-    else:
-        assert False, input
-
-    for x in range(len(data2)):
-        l = data2[x]
-        if 'api - /system/log: test/alternator: Ended test_batch.py::' in l:
-            data3 = data2[:x]
-            break
-    else:
-        assert False, input
-
+    
+    emited = False
     with open(output_file, 'w', encoding='utf8') as output:
-        for x in range(len(data3)):
-            l = data3[x].rstrip()
+        for x in range(len(data)):
+            l = data[x].rstrip()
             if not l: continue
             if l.startswith(' '):
                 continue
@@ -46,12 +31,14 @@ def parse_log_output_1(input_file, output_file):
             m = reactor_stalled_re.match(l)
             if m:
                 print(l, file=output)
-                if x + 1 < len(data3):
-                    l2 = data3[x + 1]
+                if x + 1 < len(data):
+                    l2 = data[x + 1]
                     if l2.startswith('kernel callstack: '):
                         print(l2.rstrip(), file=output)
+                emited = True
             else:
                 print(f'UNPARSED               -> {l[:120]}')
+    return emited
 
 def parse_log_output_2(input_file, output_file):
     with open(input_file, 'r', encoding='utf8') as input, open(output_file, 'w', encoding='utf8') as output:
@@ -97,8 +84,11 @@ def parse_log_output_2(input_file, output_file):
             print('', file=output)
             print('', file=output)
 
+
 if 1:
-    for x in (
+    os.makedirs('/tmp/test_o', exist_ok=True)
+    tries = 10
+    for test_name in (
             'test_performance_batch_write_item_1',
             'test_performance_batch_write_item_2',
             'test_performance_batch_write_item_3',
@@ -113,31 +103,54 @@ if 1:
             'test_performance_update_time_to_live_1',
             'test_performance_update_time_to_live_2',
             ):
-        subprocess.check_call(['rm', '-rf', '/tmp/scylla-*' ])
-        subprocess.check_call(['/home/y/work/scylladb/test/alternator/run', 'test_batch.py', '-k', x])
-        for y in os.listdir('/tmp'):
-            if y.startswith('scylla-'):
-                p = os.path.join('/tmp', y, 'log')
-                os.rename(p, f'/tmp/test_o/log_{x}.txt')
-                break
-        else:
-            assert False
+        dest_path = f'/tmp/test_o/log_{test_name}.txt'
+        if os.path.exists(dest_path):
+            os.remove(dest_path)
+        for attempt in range(tries):
+            subprocess.check_call(['rm', '-rf', '/tmp/scylla-*' ])
+            subprocess.check_call(['/home/y/work/scylladb/test/alternator/run', 'test_batch.py', '-k', test_name])
+            for y in os.listdir('/tmp'):
+                if y.startswith('scylla-'):
+                    s = os.path.join('/tmp', y, 'log')
+                    with open(dest_path, "a", encoding='utf8') as dest, open(s, 'r', encoding='utf8') as src:
+                        data = src.readlines()
 
-for f in os.listdir('.'):
+                        for x in range(len(data)):
+                            l = data[x]
+                            if 'api - /system/log: test/alternator: Starting test_batch.py::' in l:
+                                data2 = data[x + 1:]
+                                break
+                        else:
+                            assert False, input
+
+                        for x in range(len(data2)):
+                            l = data2[x]
+                            if 'api - /system/log: test/alternator: Ended test_batch.py::' in l:
+                                data3 = data2[:x]
+                                break
+                        else:
+                            assert False, input
+
+                        for d in data3:
+                            dest.write(d)
+                    break
+            else:
+                assert False
+
+for f in os.listdir('/tmp/test_o'):
     if f.startswith('log_'):
-        o = 'p1_' + f[4:]
-        o2 = 'p2_' + f[4:]
-        o3 = 'res_' + f[4:]
-        parse_log_output_1(f, o)
-
-        if 1:
+        o = os.path.join('/tmp/test_o', 'p1_' + f[4:])
+        o2 = os.path.join('/tmp/test_o', 'p2_' + f[4:])
+        o3 = os.path.join('/tmp/test_o', 'res_' + f[4:])
+        f = os.path.join('/tmp/test_o', f)
+        if parse_log_output_1(f, o):
             with open(o2, 'w', encoding='utf8') as output:
                 subprocess.check_call([
-                    '/home/y/work/scylladb/seastar/scripts/stall-analyser.py',
+                    '/home/y/work/scylla-testtest/seastar/scripts/stall-analyser.py',
                     os.path.join(os.getcwd(), o),
-                    '--format', 'trace',
+                    '--format', 'graph',
                     '-e', os.environ['SCYLLA'] ],
                     stdout=output)
         
-        parse_log_output_2(o2, o3)
+        #parse_log_output_2(o2, o3)
 
