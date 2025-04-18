e3763339f7008230176ac3c5fc186dc0ad48ae9e
Botond Dénes <bdenes@scylladb.com>
2025-04-18 03:17:25 -0400

    db/config: change batchlog_replay_cleanup_after_replays default to 1
    Now that batchlog cleanup is cheap, on account of memtable flush on the
    system.batchlog table garbage-collecting tombstones (previous patch), we
    can afford to do cleanup on each replay, keeping the memtable size small
    and more importantly -- the amount of tombstones in the memtable small.
    
    (cherry picked from commit e762027943765b6dcad08e161b3afff1d07c3fa8)

--- service/storage_proxy.cc
+++ service/storage_proxy.cc
@@ -4235,6 +4235,7 @@ storage_proxy::mutate_atomically_result(utils::chunked_vector<mutation> mutation
             return _p.mutate_prepare(_mutations, _cl, db::write_type::BATCH, _trace_state, _permit, db::allow_per_partition_rate_limit::no, _options).then(utils::result_wrap([this] (unique_response_handler_vector ids) {
                 return sync_write_to_batchlog().then(utils::result_wrap([this, ids = std::move(ids)] () mutable {
                     tracing::trace(_trace_state, "Sending batch mutations");
+                    utils::get_local_injector().inject("storage_proxy_fail_send_batch", [] { throw std::runtime_error("Error injection: failing to send batch"); });
                     _p.register_cdc_operation_result_tracker(ids, _cdc_tracker);
                     return _p.mutate_begin(std::move(ids), _cl, _trace_state, _timeout);
                 })).then(utils::result_wrap([this] {
