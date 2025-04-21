2b9982c3401c28c4dca1bb1687420c9520e77f50
Abhinav Jha <abhinav.jha@scylladb.com>
2025-04-21 13:17:21 +0530

    group0: modify `start_operation` logic to account for synchronize phase race condition
    In the present scenario, the bootstrapping node undergoes synchronize phase after
    initialization of group0, then enters post_raft phase and becomes fully ready for
    group0 operations. The topology coordinator is agnostic of this and issues stream
    ranges command as soon as the node successfully completes `join_group0`. Although for
    a node booting into an already upgraded cluster, the time duration for which, node
    remains in synchronize phase is negligible but this race condition causes trouble in a
    small percentage of cases, since the stream ranges operation fails and node fails to bootstrap.
    
    This commit addresses this issue and updates the error throw logic to account for this
    edge case and lets the node wait (with timeouts) for synchronize phase to get over instead of throwing
    error.
    
    A regression test is also added to confirm the working of this code change. The test adds a
    wait in synchronize phase for newly joining node and releases only after the program counter
    reaches the synchronize case in the `start_operation` function. Hence it indicates that in the
    updated code, the start_operation will wait for the node to get done with the
    synchronize phase instead of throwing error.
    
    This PR fixes a bug. Hence we need to backport it.
    
    Fixes: scylladb/scylladb#23536

--- test/cluster/test_view_build_status.py
+++ test/cluster/test_view_build_status.py
@@ -541,3 +541,30 @@ async def test_migration_on_existing_raft_topology(request, manager: ManagerClie
         log = await manager.server_open_log(srv.server_id)
         res = await log.grep(r'ERROR.*\[shard [0-9]: [a-z]+\] raft_topology - topology change coordinator fiber got error exceptions::unavailable_exception \(Cannot achieve consistency level for')
         assert len(res) == 0
+
+# Regression test for scylladb/scylladb#23536
+# New node addition with 'sleep_in_synchronize' injection shoehorns the execution path to
+# hit 'raft_group0_client::start_operation' when upgrade state is synchronize. The test
+# confirms successful execution in such cases.
+@skip_mode('release', 'error injections are not supported in release mode')
+@pytest.mark.asyncio
+async def test_view_build_status_with_synchronize_wait(manager: ManagerClient):
+    node_count = 4
+    servers = await manager.servers_add(node_count)
+    cql, hosts = await manager.get_ready_cql(servers)
+
+    ks = await create_keyspace(cql)
+    await create_table(cql, ks)
+    await create_mv(cql, ks, "vt1")
+
+    # add a node
+    replacing_server = await manager.server_add(start=False, config={
+        'error_injections_at_startup': ['sleep_in_synchronize']
+    })
+    task = asyncio.create_task(manager.server_start(replacing_server.server_id))
+    servers.append(replacing_server)
+    log = await manager.server_open_log(replacing_server.server_id)
+    await log.wait_for("start_operation: waiting until local node leaves synchronize state to start a group 0 operation")
+    await manager.api.message_injection(replacing_server.ip_addr, 'sleep_in_synchronize')
+
+    await task
