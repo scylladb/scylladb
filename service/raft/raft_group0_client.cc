6a6bb931650bda5153d6a3bb1dad1a0c79b50262
Abhinav Jha <abhinav.jha@scylladb.com>
2025-04-21 13:17:21 +0530

    group0: modify `start_operation` logic to account for synchronize phase race condition
    In the present scenario, the bootstrapping node undergoes synchronize phase after
    initialization of group0, then enters post_raft phase and becomes fully ready for
    group0 operations. The topology coordinator is agnostic of this and issues stream
    ranges command as soon as the node successfully completes `join_group0`. Although for
    a node booting into an already upgraded cluster, the time duration for which, node
    remains in synchronize phase is negligible but this race condition causes trouble in a
    small percentage of cases, since the stream ranges operation fails and node fails to bootstrap.
    
    This commit addresses this issue and updates the error throw logic to account for this
    edge case and lets the node wait for synchronize phase to get over instead of throwing
    error.
    
    The failing test is also updated to confirm the working of previous code change. The test adds a
    wait in synchronize phase and releases only after the program counter reaches the
    synchronize case in the `start_operation` function. Hence it indicates that in the
    updated code, the start_operation will wait for the node to get done with the
    synchronize phase instead of throwing error
    
    This PR fixes a bug. Hence we need to backport it.
    
    Fixes: scylladb/scylladb#23536

--- service/raft/raft_group0_client.cc
+++ service/raft/raft_group0_client.cc
@@ -257,6 +257,11 @@ future<group0_guard> raft_group0_client::start_operation(seastar::abort_source& 
 
     auto [upgrade_lock_holder, upgrade_state] = co_await get_group0_upgrade_state();
     switch (upgrade_state) {
+        case group0_upgrade_state::synchronize:
+            logger.info("start_operation is waiting in synchronize phase");
+            upgrade_lock_holder.release();
+            co_await wait_until_group0_upgraded(as);
+            [[fallthrough]];
         case group0_upgrade_state::use_post_raft_procedures: {
             auto operation_holder = co_await get_units(_operation_mutex, 1, as);
             co_await _raft_gr.group0_with_timeouts().read_barrier(&as, timeout);
@@ -280,12 +285,6 @@ future<group0_guard> raft_group0_client::start_operation(seastar::abort_source& 
                 )
             };
        }
-
-        case group0_upgrade_state::synchronize:
-            throw std::runtime_error{
-                "Cannot perform schema or topology changes during this time; the cluster is currently upgrading to use Raft for schema operations."
-                " If this error keeps happening, check the logs of your nodes to learn the state of upgrade. The upgrade procedure may get stuck"
-                " if there was a node failure."};
 
         case group0_upgrade_state::recovery:
             logger.warn("starting operation in RECOVERY mode (using old procedures)");
