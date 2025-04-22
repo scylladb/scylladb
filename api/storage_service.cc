c570941692fb4a9eacb1d60594a80649f7c90326
Robert Bindar <robert.bindar@scylladb.com>
2025-04-22 16:00:50 +0300

    Add nodetool refresh --scope option
    This change adds the --scope option to nodetool refresh.
    Like in the case of nodetool restore, you can pass either of:
    * node - On the local node.
    * rack - On the local rack.
    * dc - In the datacenter (DC) where the local node lives.
    * all (default) - Everywhere across the cluster.
    as scope.
    
    The feature is based on the existing load_and_stream paths, so it
    requires passing --load-and-stream to the refresh command.
    Also, it is not compatible with the --primary-replica-only option.
    
    Signed-off-by: Robert Bindar <robert.bindar@scylladb.com>
    
    Closes scylladb/scylladb#23861

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -465,13 +465,19 @@ void set_sstables_loader(http_context& ctx, routes& r, sharded<sstables_loader>&
         bool load_and_stream = stream == "true" || stream == "1";
         bool primary_replica_only = primary_replica == "true" || primary_replica == "1";
         bool skip_cleanup = skip_cleanup_p == "true" || skip_cleanup_p == "1";
+        auto scope = parse_stream_scope(req->get_query_param("scope"));
+
+        if (scope != sstables_loader::stream_scope::all && !load_and_stream) {
+            throw httpd::bad_param_exception("scope takes no effect without load-and-stream");
+        }
+
         // No need to add the keyspace, since all we want is to avoid always sending this to the same
         // CPU. Even then I am being overzealous here. This is not something that happens all the time.
         auto coordinator = std::hash<sstring>()(cf) % smp::count;
         return sst_loader.invoke_on(coordinator,
                 [ks = std::move(ks), cf = std::move(cf),
-                load_and_stream, primary_replica_only, skip_cleanup] (sstables_loader& loader) {
-            return loader.load_new_sstables(ks, cf, load_and_stream, primary_replica_only, skip_cleanup, sstables_loader::stream_scope::all);
+                load_and_stream, primary_replica_only, skip_cleanup, scope] (sstables_loader& loader) {
+            return loader.load_new_sstables(ks, cf, load_and_stream, primary_replica_only, skip_cleanup, scope);
         }).then_wrapped([] (auto&& f) {
             if (f.failed()) {
                 auto msg = fmt::format("Failed to load new sstables: {}", f.get_exception());
