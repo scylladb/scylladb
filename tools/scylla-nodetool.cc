9cb15bd4c9124ce0d4403ebddf7f0d39b9e08163
Robert Bindar <robert.bindar@scylladb.com>
2025-04-22 16:00:50 +0300

    Add nodetool refresh --scope option
    Signed-off-by: Robert Bindar <robert.bindar@scylladb.com>

--- tools/scylla-nodetool.cc
+++ tools/scylla-nodetool.cc
@@ -1489,6 +1489,15 @@ void refresh_operation(scylla_rest_client& client, const bpo::variables_map& vm)
         }
         params["primary_replica_only"] = "true";
     }
+    if (vm.contains("scope")) {
+        if (!vm.contains("load-and-stream")) {
+            throw std::invalid_argument("--scope takes no effect without --load-and-stream|-las");
+        }
+        if (vm.contains("primary-replica-only")) {
+            throw std::invalid_argument("Scoped streaming of primary replica only is not supported yet");
+        }
+        params["scope"] = vm["scope"].as<sstring>();
+    }
     client.post(format("/storage_service/sstables/{}", vm["keyspace"].as<sstring>()), std::move(params));
 }
 
@@ -4049,6 +4058,7 @@ For more information, see: {}"
                 {
                     typed_option<>("load-and-stream", "Allows loading sstables that do not belong to this node, in which case they are automatically streamed to the owning nodes"),
                     typed_option<>("primary-replica-only", "Load the sstables and stream to primary replica node that owns the data. Repair is needed after the load and stream process"),
+                    typed_option<sstring>("scope", "Load-and-stream scope (node, rack or dc)"),
                 },
                 {
                     typed_option<sstring>("keyspace", "The keyspace to load sstable(s) into", 1),
