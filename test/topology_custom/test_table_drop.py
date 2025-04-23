bfdf7c944b9e9773158dc043c5999029aa49cf22
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2025-04-23 14:24:50 +0200

    test: test table drop during flush
    (cherry picked from commit c1618c7de5b793619a2efdc6a875a728456d6d83)

--- test/topology_custom/test_table_drop.py
+++ test/topology_custom/test_table_drop.py
@@ -1,4 +1,6 @@
+import asyncio
 from test.pylib.manager_client import ManagerClient
+from test.topology.util import new_test_keyspace
 import pytest
 
 @pytest.mark.asyncio
@@ -10,3 +12,15 @@ async def test_drop_table_during_streaming_receiver_side(manager: ManagerClient)
         'force_gossip_topology_changes': True,
         'tablets_mode_for_new_keyspaces': 'disabled'
     }) for _ in range(2)]
+
+@pytest.mark.asyncio
+async def test_drop_table_during_flush(manager: ManagerClient):
+    servers = [await manager.server_add() for _ in range(2)]
+
+    await manager.api.enable_injection(servers[0].ip_addr, "flush_tables_on_all_shards_table_drop", True)
+
+    cql = manager.get_cql()
+    async with new_test_keyspace(cql, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 1};") as ks:
+        await cql.run_async(f"CREATE TABLE {ks}.test (pk int PRIMARY KEY, c int);")
+        await asyncio.gather(*[cql.run_async(f"INSERT INTO {ks}.test (pk, c) VALUES ({k}, {k%3});") for k in range(64)])
+        await manager.api.keyspace_flush(servers[0].ip_addr, ks, "test")
