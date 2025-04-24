90af439e4780a67649a59d6965ad575f876ba905
Piotr Smaron <piotr.smaron@scylladb.com>
2025-04-24 21:05:29 +0200

    cql: fix CREATE tablets KS warning msg
    Materialized Views and Secondary Indexes are yet another features that
    keyspaces with tablets do not support, but these were not listed in a
    warning message returned to the user on CREATE KEYSPACE statement. This
    commit adds the 2 missing features.
    
    Fixes: #24006
    
    Closes scylladb/scylladb#23902
    
    (cherry picked from commit f740f9f0e1f33def8cd52da52969187e486fa7c2)
    
    Closes scylladb/scylladb#24079

--- cql3/statements/create_keyspace_statement.cc
+++ cql3/statements/create_keyspace_statement.cc
@@ -113,10 +113,9 @@ future<std::tuple<::shared_ptr<cql_transport::event::schema_change>, std::vector
         if (rs->uses_tablets()) {
             warnings.push_back(
                 "Tables in this keyspace will be replicated using Tablets "
-                "and will not support CDC, LWT and counters features. "
-                "To use CDC, LWT or counters, drop this keyspace and re-create it "
-                "without tablets by adding AND TABLETS = {'enabled': false} "
-                "to the CREATE KEYSPACE statement.");
+                "and will not support Materialized Views, Secondary Indexes, CDC, LWT and counters features. "
+                "To use Materialized Views, Secondary Indexes, CDC, LWT or counters, drop this keyspace and re-create it "
+                "without tablets by adding AND TABLETS = {'enabled': false} to the CREATE KEYSPACE statement.");
         }
 
         // If `rf_rack_valid_keyspaces` is enabled, it's forbidden to create an RF-rack-invalid keyspace.
