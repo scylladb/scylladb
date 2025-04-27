elcallio/scylla

sstables: Start using `make_data_or_index_source` in `sstable`
Convert all necessary methods to be awaitable. Start using `make_data_or_index_source`
when creating data_source for data and index components.

For proper working of compressed/checksummed input streams, start passing
stream creator functors to `make_(checksummed/compressed)_file_(k_l/m)_format_input_stream`.

test/cluster/object_store/test_backup.py
@@ -248,7 +248,7 @@ async def test_backup_is_abortable_in_s3_client(manager: ManagerClient, s3_serve
     await do_test_backup_abort(manager, s3_server, breakpoint_name="backup_task_pre_upload", min_files=0, max_files=1)
 
 
-async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, do_abort = False):
+async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, tmpdir, do_encrypt = False, do_abort = False):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
 
     objconf = MinioServer.create_conf(s3_server.address, s3_server.port, s3_server.region)
@@ -257,7 +257,14 @@ async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, d
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug']
+    if do_encrypt:
+        d = tmpdir / "system_keys"
+        d.mkdir()
+        cfg = cfg | {
+            'system_key_directory': str(d),
+            'user_info_encryption': { 'enabled': True, 'key_provider': 'LocalFileSystemKeyProviderFactory' }
+        }
+    cmd = ['--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug:encryption=debug']
     server = await manager.server_add(config=cfg, cmdline=cmd)
 
     cql = manager.get_cql()
@@ -355,15 +362,20 @@ async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, d
     assert objects == post_objects
 
 @pytest.mark.asyncio
-async def test_simple_backup_and_restore(manager: ManagerClient, s3_server):
+async def test_simple_backup_and_restore(manager: ManagerClient, s3_server, tmp_path):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
-    await do_test_simple_backup_and_restore(manager, s3_server, False)
+    await do_test_simple_backup_and_restore(manager, s3_server, tmp_path, False, False)
 
 @pytest.mark.asyncio
-async def test_abort_simple_backup_and_restore(manager: ManagerClient, s3_server):
+async def test_abort_simple_backup_and_restore(manager: ManagerClient, s3_server, tmp_path):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
-    await do_test_simple_backup_and_restore(manager, s3_server, True)
+    await do_test_simple_backup_and_restore(manager, s3_server, tmp_path, False, True)
 
+
+@pytest.mark.asyncio
+async def test_simple_backup_and_restore_with_encryption(manager: ManagerClient, s3_server, tmp_path):
+    '''check that restoring from backed up snapshot for a keyspace:table works'''
+    await do_test_simple_backup_and_restore(manager, s3_server, tmp_path, True, False)
 
 # Helper class to parametrize the test below
 class topo:

scylladb/scylladb

test/cluster/object_store/test_backup.py
@@ -59,7 +59,7 @@ async def test_simple_backup(manager: ManagerClient, s3_server):
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace']
+    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
     ks, cf = await prepare_snapshot_for_backup(manager, server)
 
@@ -100,7 +100,7 @@ async def test_backup_move(manager: ManagerClient, s3_server, move_files):
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace']
+    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
     ks, cf = await prepare_snapshot_for_backup(manager, server)
 
@@ -134,7 +134,7 @@ async def test_backup_to_non_existent_bucket(manager: ManagerClient, s3_server):
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace']
+    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
     ks, cf = await prepare_snapshot_for_backup(manager, server)
 
@@ -186,7 +186,7 @@ async def do_test_backup_abort(manager: ManagerClient, s3_server,
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace']
+    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
     ks, cf = await prepare_snapshot_for_backup(manager, server)
 
@@ -239,7 +239,7 @@ async def test_backup_to_non_existent_snapshot(manager: ManagerClient, s3_server
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace']
+    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
     ks, cf = await prepare_snapshot_for_backup(manager, server)
 
@@ -275,7 +275,7 @@ async def test_backup_is_abortable_in_s3_client(manager: ManagerClient, s3_serve
     await do_test_backup_abort(manager, s3_server, breakpoint_name="backup_task_pre_upload", min_files=0, max_files=1)
 
 
-async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, do_abort = False):
+async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, tmpdir, do_encrypt = False, do_abort = False):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
 
     objconf = MinioServer.create_conf(s3_server.address, s3_server.port, s3_server.region)
@@ -284,7 +284,14 @@ async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, d
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug']
+    if do_encrypt:
+        d = tmpdir / "system_keys"
+        d.mkdir()
+        cfg = cfg | {
+            'system_key_directory': str(d),
+            'user_info_encryption': { 'enabled': True, 'key_provider': 'LocalFileSystemKeyProviderFactory' }
+        }
+    cmd = ['--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug:encryption=debug:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
 
     cql = manager.get_cql()
@@ -382,15 +389,20 @@ async def do_test_simple_backup_and_restore(manager: ManagerClient, s3_server, d
     assert objects == post_objects
 
 @pytest.mark.asyncio
-async def test_simple_backup_and_restore(manager: ManagerClient, s3_server):
+async def test_simple_backup_and_restore(manager: ManagerClient, s3_server, tmp_path):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
-    await do_test_simple_backup_and_restore(manager, s3_server, False)
+    await do_test_simple_backup_and_restore(manager, s3_server, tmp_path, False, False)
 
 @pytest.mark.asyncio
-async def test_abort_simple_backup_and_restore(manager: ManagerClient, s3_server):
+async def test_abort_simple_backup_and_restore(manager: ManagerClient, s3_server, tmp_path):
     '''check that restoring from backed up snapshot for a keyspace:table works'''
-    await do_test_simple_backup_and_restore(manager, s3_server, True)
+    await do_test_simple_backup_and_restore(manager, s3_server, tmp_path, False, True)
 
+
+@pytest.mark.asyncio
+async def test_simple_backup_and_restore_with_encryption(manager: ManagerClient, s3_server, tmp_path):
+    '''check that restoring from backed up snapshot for a keyspace:table works'''
+    await do_test_simple_backup_and_restore(manager, s3_server, tmp_path, True, False)
 
 # Helper class to parametrize the test below
 class topo:
@@ -415,7 +427,7 @@ async def test_restore_with_streaming_scopes(manager: ManagerClient, s3_server, 
     logger.info(f'Start cluster with {topology.nodes} nodes in {topology.dcs} DCs, {topology.racks} racks')
     objconf = MinioServer.create_conf(s3_server.address, s3_server.port, s3_server.region)
     cfg = { 'object_storage_endpoints': objconf, 'task_ttl_in_seconds': 300 }
-    cmd = [ '--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug' ]
+    cmd = [ '--logger-log-level', 'sstables_loader=debug:sstable_directory=trace:snapshots=trace:s3=trace:sstable=debug:http=debug:api=info' ]
     servers = []
     host_ids = {}
     for s in range(topology.nodes):
@@ -542,7 +554,7 @@ async def test_restore_with_non_existing_sstable(manager: ManagerClient, s3_serv
            'experimental_features': ['keyspace-storage-options'],
            'task_ttl_in_seconds': 300
            }
-    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace']
+    cmd = ['--logger-log-level', 'snapshots=trace:task_manager=trace:api=info']
     server = await manager.server_add(config=cfg, cmdline=cmd)
     cql = manager.get_cql()
     print('Create keyspace')