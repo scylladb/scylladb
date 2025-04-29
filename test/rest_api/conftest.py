2660dda9de48a2ac76f01f9fdf764f6c9b97236c
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2025-04-29 03:53:15 +0000

    test.py: python: remove dups of cql and this_dc fixtures
    Replace dups of `cql` and `this_dc` fixtures in `rest_api` and
    `pylib/cql_repl` with imports from `cqlpy`.

--- test/rest_api/conftest.py
+++ test/rest_api/conftest.py
@@ -11,16 +11,10 @@
 
 import pytest
 import requests
-import ssl
-import sys
 
-from cassandra.auth import PlainTextAuthProvider
-from cassandra.cluster import Cluster, ConsistencyLevel, ExecutionProfile, EXEC_PROFILE_DEFAULT
-from cassandra.policies import RoundRobinPolicy
-
-
+from test.cqlpy.conftest import cql, this_dc  # add required fixtures
+from test.cqlpy.util import unique_name, new_test_keyspace, keyspace_has_tablets, is_scylla
 from test.pylib.suite.python import add_host_option, add_cql_connection_options
-from ..cqlpy.util import unique_name, new_test_keyspace, keyspace_has_tablets, is_scylla
 
 # By default, tests run against a Scylla server listening
 # on localhost:9042 for CQL and localhost:10000 for the REST API.
@@ -56,59 +50,6 @@ def rest_api(request):
     host = request.config.getoption('host')
     port = request.config.getoption('api_port')
     return RestApiSession(host, port)
-
-# "cql" fixture: set up client object for communicating with the CQL API.
-# The host/port combination of the server are determined by the --host and
-# --port options, and defaults to localhost and 9042, respectively.
-# We use scope="session" so that all tests will reuse the same client object.
-@pytest.fixture(scope="session")
-def cql(request):
-    profile = ExecutionProfile(
-        load_balancing_policy=RoundRobinPolicy(),
-        consistency_level=ConsistencyLevel.LOCAL_QUORUM,
-        serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
-        # The default timeout (in seconds) for execute() commands is 10, which
-        # should have been more than enough, but in some extreme cases with a
-        # very slow debug build running on a very busy machine and a very slow
-        # request (e.g., a DROP KEYSPACE needing to drop multiple tables)
-        # 10 seconds may not be enough, so let's increase it. See issue #7838.
-        request_timeout = 120)
-    if request.config.getoption('ssl'):
-        # Scylla does not support any earlier TLS protocol. If you try,
-        # you will get mysterious EOF errors (see issue #6971) :-(
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    else:
-        ssl_context = None
-    cluster = Cluster(execution_profiles={EXEC_PROFILE_DEFAULT: profile},
-        contact_points=[request.config.getoption('host')],
-        port=int(request.config.getoption('port')),
-        # TODO: make the protocol version an option, to allow testing with
-        # different versions. If we drop this setting completely, it will
-        # mean pick the latest version supported by the client and the server.
-        protocol_version=4,
-        # Use the default superuser credentials, which work for both Scylla and Cassandra
-        auth_provider=PlainTextAuthProvider(username='cassandra', password='cassandra'),
-        ssl_context=ssl_context,
-        # The default timeout for new connections is 5 seconds, and for
-        # requests made by the control connection is 2 seconds. These should
-        # have been more than enough, but in some extreme cases with a very
-        # slow debug build running on a very busy machine, they may not be.
-        # so let's increase them to 60 seconds. See issue #11289.
-        connect_timeout = 60,
-        control_connection_timeout = 60,
-    )
-    return cluster.connect()
-
-# Until Cassandra 4, NetworkTopologyStrategy did not support the option
-# replication_factor (https://issues.apache.org/jira/browse/CASSANDRA-14303).
-# We want to allow these tests to run on Cassandra 3.* (for the convenience
-# of developers who happen to have it installed), so we'll use the older
-# syntax that needs to specify a DC name explicitly. For this, will have
-# a "this_dc" fixture to figure out the name of the current DC, so it can be
-# used in NetworkTopologyStrategy.
-@pytest.fixture(scope="session")
-def this_dc(cql):
-    yield cql.execute("SELECT data_center FROM system.local").one()[0]
 
 # The "scylla_only" fixture can be used by tests for Scylla-only features,
 # which do not exist on Apache Cassandra. A test using this fixture will be
