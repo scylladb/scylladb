5ae11746fa9691cb77164b64d515a192cef2907e
Amnon Heiman <amnon@scylladb.com>
2025-05-04 20:09:12 +0300

    Alternator: Change the WCU/RCU to use units
    This patch changes the RCU/WCU Alternator metrics to use whole units
    instead of half units. The change includes the following:
    
    Change the metrics documentation. Keep the RCU counter internally in
    half units, but return the actual (whole unit) value.
    Change the RCU name to be rcu_half_units_total to indicates that it
    counts half units.
    Change the WCU to count in whole units instead of half units.
    
    Update the tests accordingly.
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>

--- test/alternator/test_metrics.py
+++ test/alternator/test_metrics.py
@@ -174,7 +174,7 @@ def test_batch_get_item_count(test_table_s, metrics):
 
 KB = 1024
 def test_rcu(test_table_s, metrics):
-    with check_increases_metric_exact(metrics, 'scylla_alternator_rcu_total', [[4, None]]):
+    with check_increases_metric_exact(metrics, 'scylla_alternator_rcu_total', [[2, None]]):
         p = random_string()
         val = random_string()
         total_length = len(p) + len(val) + len("pattanother")
@@ -184,7 +184,7 @@ def test_rcu(test_table_s, metrics):
         test_table_s.get_item(Key={'p': p}, ConsistentRead=True)
 
 def test_wcu(test_table_s, metrics):
-    with check_increases_operation(metrics, ['PutItem'], 'scylla_alternator_wcu_total', 6):
+    with check_increases_operation(metrics, ['PutItem'], 'scylla_alternator_wcu_total', 3):
         p = random_string()
         val = random_string()
         total_length = len(p) + len(val) + len("pattanother")
