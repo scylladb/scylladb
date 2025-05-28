f12feabece3e5c74ee2cbb82688938fba4706371
Calle Wilund <calle@scylladb.com>
2025-05-28 11:27:19 +0000

    encryption/utils: Move encryption httpclient to "general" REST client
    Fixed #24296
    
    While the HTTP client used for REST calls in AWS/GCP KMS integration (EAR)
    is not general enough to be called a HTTP client as such, it is general
    enough to be called a REST client (limited to stateless, single-op REST
    calls).
    
    Other code, like general auth integrations (hello Azure) and similar
    could reuse this to lessen code duplication.
    
    This patch simply moves the httpclient class from encryption to "rest"
    namespace, and explicitly "limits" it to such usage. Making an alias
    in encryption to avoid touching more files than needed.

--- ent/encryption/utils.hh
+++ ent/encryption/utils.hh
@@ -7,84 +7,10 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
-#include <seastar/core/future.hh>
-#include <seastar/http/url.hh>
-#include <seastar/http/client.hh>
-#include <seastar/http/request.hh>
-#include <seastar/http/reply.hh>
-
-namespace seastar {
-namespace tls { class certificate_credentials; }
-namespace http {
-namespace experimental { class client; }
-struct request;
-struct reply;
-}
-}
+#include "utils/rest/client.hh"
 
 namespace encryption {
 
-class httpclient {
-public:
-    httpclient(std::string host, uint16_t port, seastar::shared_ptr<seastar::tls::certificate_credentials> = {});
-
-    httpclient& add_header(std::string_view key, std::string_view value);
-    void clear_headers();
-
-    using reply_status = seastar::http::reply::status_type;
-    using request_type = seastar::http::request;
-
-    struct result_type {
-        seastar::http::reply reply;
-        reply_status result() const {
-            return reply._status;
-        }
-        int result_int() const {
-            return int(result());
-        }
-        std::string_view body() const {
-            return reply._content;
-        }
-    };
-
-    using handler_func = std::function<void(const seastar::http::reply&, std::string_view)>;
-
-    seastar::future<result_type> send();
-    seastar::future<> send(const handler_func&);
-
-    using method_type = seastar::httpd::operation_type;
-
-    void method(method_type);
-    void content(std::string_view);
-    void target(std::string_view);
-
-    const request_type& request() const {
-        return _req;
-    }
-    const std::string& host() const {
-        return _host;
-    }
-    uint16_t port() const {
-        return _port;
-    }
-
-    static inline constexpr const char* CONTENT_TYPE_HEADER = "content-type";
-
-private:
-    std::string _host;
-    uint16_t _port;
-    seastar::shared_ptr<seastar::tls::certificate_credentials> _creds;
-    request_type _req;
-};
+using httpclient = rest::httpclient;
 
 }
-
-template <>
-struct fmt::formatter<encryption::httpclient::request_type> : fmt::formatter<std::string_view> {
-    auto format(const encryption::httpclient::request_type&, fmt::format_context& ctx) const -> decltype(ctx.out());
-};
-
-template <>
-struct fmt::formatter<encryption::httpclient::result_type> : fmt::formatter<std::string_view> {
-    auto format(const encryption::httpclient::result_type&, fmt::format_context& ctx) const -> decltype(ctx.out());
-};
