6a7573e4561bed1ce840c168e185e52c5e8ba051
Petr Gusev <petr.gusev@scylladb.com>
2025-05-28 14:01:31 +0200

    lwt: add test_lwt_state_is_preserved_on_tablet_migration
    This test verifies that Paxos state is correctly migrated when
    the base table's tablet is migrated. This test  fails if Paxos
    state is stored in system.paxos, as the final Paxos read would
    reflect conflicting outcomes from both prior LWT operations.

--- test/cluster/test_tablets2.py
+++ test/cluster/test_tablets2.py
@@ -30,16 +30,16 @@ import itertools
 logger = logging.getLogger(__name__)
 
 
-async def inject_error_one_shot_on(manager, error_name, servers):
+async def inject_error_one_shot_on(manager: ManagerClient, error_name, servers):
     errs = [inject_error_one_shot(manager.api, s.ip_addr, error_name) for s in servers]
     await asyncio.gather(*errs)
 
 
-async def inject_error_on(manager, error_name, servers):
+async def inject_error_on(manager: ManagerClient, error_name, servers):
     errs = [manager.api.enable_injection(s.ip_addr, error_name, False) for s in servers]
     await asyncio.gather(*errs)
 
-async def disable_injection_on(manager, error_name, servers):
+async def disable_injection_on(manager: ManagerClient, error_name, servers):
     errs = [manager.api.disable_injection(s.ip_addr, error_name) for s in servers]
     await asyncio.gather(*errs)
 
@@ -1999,3 +1999,99 @@ async def test_lwt(manager: ManagerClient):
 
         await cql.run_async(f"DROP TABLE {ks}.test")
         assert not await paxos_state_table_exists()
+
+
+@pytest.mark.asyncio
+@skip_mode('release', 'error injections are not supported in release mode')
+async def test_lwt_state_is_preserved_on_tablet_migration(manager: ManagerClient):
+    # Scenario:
+    # 1. Cells c1 and c2 of some partition are not set.
+    # 2. An LWT on {n1, n2} writes 1 to c1, stores accepts on {n1, n2} and learn on n1.
+    # 3. Tablet migrates from n2 to n4.
+    # 4. Another LWT comes, accepts/learns on {n3, n4} a value "2 if c1 != 1" for the cell c2.
+    #    If paxos state was not preserved, this LWT would succeed since the quorum {n3, n4} is completely
+    #    unaware of the decided value 1 for c1. We would miss this value and
+    #    commit a new write which contradicts it. Depending in the status of the first LWT, this could
+    #    be valid or. If the first LWT has failed (e.g. cl_learn == quorum == 2, learn succeeded on n1
+    #    and failed on n2) this is OK, since the user hasn't observed the effects of the first LWT.
+    #    In this test we use cl_learn == 1 for simplicity (see the comment below), so this ignoring
+    #    the effects of the first LWT is not valid -- this breaks linearizability.
+    # 5. Do the SERIAL (paxos) read of c1 and c2 from {n1, n4}. If paxos state was not preserved, this
+    #    read would see c1==1 (from learned value on n1) and c2 == 2 (from n4),
+    #    which contradicts the CQL from 4.
+
+    logger.info("Bootstrap a cluster with three nodes")
+    cmdline = [
+        '--logger-log-level', 'paxos=trace'
+    ]
+    config = {
+        'rf_rack_valid_keyspaces': False,
+        'experimental_features': ['lwt-with-tablets']
+    }
+    servers = await manager.servers_add(3, cmdline=cmdline, config=config)
+    cql = manager.get_cql()
+
+    async def set_injection(set_to: list[ServerInfo], injection: str):
+        logger.info(f"Injecting {injection} on {set_to}")
+        await inject_error_on(manager, injection, set_to)
+        unset_from = [s for s in servers if s not in set_to]
+        logger.info(f"Disabling {injection} on {unset_from}")
+        await disable_injection_on(manager, injection, unset_from)
+
+    logger.info("Resolve hosts")
+    hosts = await wait_for_cql_and_get_hosts(cql, servers, time.time() + 60)
+
+    logger.info("Disable tablet balancing")
+    await asyncio.gather(*(manager.api.disable_tablet_balancing(s.ip_addr) for s in servers))
+
+    logger.info("Create a keyspace")
+    async with new_test_keyspace(manager, "WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': 3} AND tablets = {'initial': 1}") as ks:
+        logger.info("Create a table")
+        await cql.run_async(f"CREATE TABLE {ks}.test (pk int PRIMARY KEY, c1 int, c2 int);")
+
+        # Step 2: Inject paxos_error_before_save_proposal - on [n3], paxos_error_before_learn on [n2, n3],
+        # so that the paxos proposal for the LWT would be accepted
+        # only on n1 and n2 and learnt only on n1. The new value for the CAS
+        # operation would be decided.
+        # Note: we use cl_learn=1 here to simplify the test, the same problem
+        # would be with the default cl_learn=quorum, if learn succeeded on n1 but
+        # failed on n2.
+        await set_injection([servers[2]], 'paxos_error_before_save_proposal')
+        await set_injection([servers[1], servers[2]], 'paxos_error_before_learn')
+        logger.info("Execute CAS(c1 := 1)")
+        lwt1 = SimpleStatement(f"INSERT INTO {ks}.test (pk, c1) VALUES (1, 1) IF NOT EXISTS",
+                               consistency_level=ConsistencyLevel.ONE)
+        await cql.run_async(lwt1, host=hosts[0])
+
+        # Step3: start n4, migrate the single table tablet from n2 to n4.
+        servers += [await manager.server_add(cmdline=cmdline, config=config)]
+        n4_host_id = await manager.get_host_id(servers[3].server_id)
+        logger.info("Migrating the tablet from n2 to n4")
+        n2_host_id = await manager.get_host_id(servers[1].server_id)
+        tablets = await get_all_tablet_replicas(manager, servers[0], ks, 'test')
+        assert len(tablets) == 1
+        tablet = tablets[0]
+        n2_replica = next(r for r in tablet.replicas if r[0] == n2_host_id)
+        await manager.api.move_tablet(servers[0].ip_addr, ks, "test", *n2_replica,
+                                      *(n4_host_id, 0), tablet.last_token)
+
+        # Step 4: execute LWT on n3, n4
+        await set_injection([servers[0]], 'paxos_error_before_save_promise')
+        await set_injection([servers[0]], 'paxos_error_before_save_proposal')
+        await set_injection([servers[0]], 'paxos_error_before_learn')
+        logger.info("Execute CAS(c2 := 2 IF c1 != 1)")
+        await cql.run_async(f"UPDATE {ks}.test SET c2 = 2 WHERE pk = 1 IF c1 != 1", host=hosts[2])
+
+        # Step 5. Do the SERIAL (paxos) read of c1, c2 from {n1, n4}.
+        await set_injection([servers[2]], 'paxos_error_before_save_promise')
+        await set_injection([servers[2]], 'paxos_error_before_save_proposal')
+        await set_injection([servers[2]], 'paxos_error_before_learn')
+        logger.info("Execute paxos read")
+        lwt_read = SimpleStatement(f"SELECT * FROM {ks}.test WHERE pk = 1;",
+                                   consistency_level=ConsistencyLevel.SERIAL)
+        rows = await cql.run_async(lwt_read, host=hosts[1])
+        assert len(rows) == 1
+        row = rows[0]
+        assert row.pk == 1
+        assert row.c1 == 1
+        assert row.c2 is None
