01dd4b7f307007811e2b73f6c357c3473c5e25ce
Gleb Natapov <gleb@scylladb.com>
2025-05-28 16:57:39 +0300

    topology coordinator: fix indentation after the previous patch

--- service/storage_proxy.cc
+++ service/storage_proxy.cc
@@ -1072,36 +1072,36 @@ private:
             const table_id table_id = _sp.local_db().find_uuid(ks_name, cf_name);
 
             if (!_sp._features.topology_global_request_queue) {
-            // Check if we already have a truncate queued for the same table. This can happen when a truncate has timed out
-            // and the client retried by issuing the same truncate again. In this case, instead of failing the request with
-            // an "Another global topology request is ongoing" error, we can wait for the already queued request to complete.
-            // Note that we can not do this for a truncate which the topology coordinator has already started processing,
-            // only for a truncate which is still waiting.
-            if (_topology_state_machine._topology.global_request || !_topology_state_machine._topology.global_requests_queue.empty()) {
-                utils::UUID ongoing_global_request_id;
-                if (!_topology_state_machine._topology.global_requests_queue.empty()) {
-                    ongoing_global_request_id = _topology_state_machine._topology.global_requests_queue[0];
-                } else {
-                    ongoing_global_request_id = _topology_state_machine._topology.global_request_id.value();
-                }
-                const auto topology_requests_entry = co_await _sys_ks.local().get_topology_request_entry(ongoing_global_request_id, true);
-                auto global_request = std::get<service::global_topology_request>(topology_requests_entry.request_type);
-                if (global_request == global_topology_request::truncate_table) {
-                    std::optional<topology::transition_state>& tstate = _topology_state_machine._topology.tstate;
-                    if (!tstate || *tstate != topology::transition_state::truncate_table) {
-                        if (topology_requests_entry.truncate_table_id == table_id) {
-                            global_request_id = ongoing_global_request_id;
-                            slogger.info("Ongoing TRUNCATE for table {}.{} (global request ID {}) detected; waiting for it to complete",
-                                                ks_name, cf_name, global_request_id);
-                            break;
+                // Check if we already have a truncate queued for the same table. This can happen when a truncate has timed out
+                // and the client retried by issuing the same truncate again. In this case, instead of failing the request with
+                // an "Another global topology request is ongoing" error, we can wait for the already queued request to complete.
+                // Note that we can not do this for a truncate which the topology coordinator has already started processing,
+                // only for a truncate which is still waiting.
+                if (_topology_state_machine._topology.global_request || !_topology_state_machine._topology.global_requests_queue.empty()) {
+                    utils::UUID ongoing_global_request_id;
+                    if (!_topology_state_machine._topology.global_requests_queue.empty()) {
+                        ongoing_global_request_id = _topology_state_machine._topology.global_requests_queue[0];
+                    } else {
+                        ongoing_global_request_id = _topology_state_machine._topology.global_request_id.value();
+                    }
+                    const auto topology_requests_entry = co_await _sys_ks.local().get_topology_request_entry(ongoing_global_request_id, true);
+                    auto global_request = std::get<service::global_topology_request>(topology_requests_entry.request_type);
+                    if (global_request == global_topology_request::truncate_table) {
+                        std::optional<topology::transition_state>& tstate = _topology_state_machine._topology.tstate;
+                        if (!tstate || *tstate != topology::transition_state::truncate_table) {
+                            if (topology_requests_entry.truncate_table_id == table_id) {
+                                global_request_id = ongoing_global_request_id;
+                                slogger.info("Ongoing TRUNCATE for table {}.{} (global request ID {}) detected; waiting for it to complete",
+                                                    ks_name, cf_name, global_request_id);
+                                break;
+                            }
                         }
                     }
+                    slogger.warn("Another global topology request ({}) is ongoing during attempt to TRUNCATE table {}.{}",
+                                    global_request, ks_name, cf_name);
+                    throw exceptions::invalid_request_exception(::format("Another global topology request is ongoing during attempt to TRUNCATE table {}.{}, please retry.",
+                                                                            ks_name, cf_name));
                 }
-                slogger.warn("Another global topology request ({}) is ongoing during attempt to TRUNCATE table {}.{}",
-                                global_request, ks_name, cf_name);
-                throw exceptions::invalid_request_exception(::format("Another global topology request is ongoing during attempt to TRUNCATE table {}.{}, please retry.",
-                                                                        ks_name, cf_name));
-            }
             }
 
             global_request_id = guard.new_group0_state_id();
