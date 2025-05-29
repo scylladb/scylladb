4d667fbd5dcbb7eb069d693e6f0b8ce92dbb7311
Łukasz Paszkowski <lukasz.paszkowski@scylladb.com>
2025-05-29 16:41:08 +0200

    database: Add critical_disk_utilization mode database can be moved to
    When database operates in the critical disk utilization mode, all
    mutation writes including inserts, updates, deletes, counter updates,
    hints, read+repair, lwt writes) to user tables and other associated
    with them tables like views, CDC log, audit are rejected, with a clear
    error exception returned.
    
    The mode is meant to be used with the disk space monitor in order
    to prevent any user writes when node's disk utilization is too high.

--- test/lib/cql_test_env.cc
+++ test/lib/cql_test_env.cc
@@ -160,7 +160,9 @@ private:
     sharded<locator::effective_replication_map_factory> _erm_factory;
     sharded<sstables::directory_semaphore> _sst_dir_semaphore;
     std::optional<utils::disk_space_monitor> _disk_space_monitor_shard0;
-    std::optional<replica::out_of_space_controller> _out_of_space_controller_shard0;
+    // Deliberately nullptr. We do not need the controller for the unit tests but
+    // passing _out_of_space_controller instead of nullptr directly is more readable.
+    replica::out_of_space_controller* _out_of_space_controller = nullptr;
     sharded<lang::manager> _lang_manager;
     sharded<cql3::cql_config> _cql_config;
     sharded<service::endpoint_lifecycle_notifier> _elc_notif;
@@ -669,7 +671,7 @@ private:
             });
 
             _db_config = &*cfg;
-            _db.start(std::ref(*cfg), dbcfg, std::ref(_mnotifier), std::ref(_feature_service), std::ref(_token_metadata), std::ref(_cm), std::ref(_sstm), std::ref(_lang_manager), std::ref(_sst_dir_semaphore), std::ref(_scf), std::ref(abort_sources), utils::cross_shard_barrier()).get();
+            _db.start(std::ref(*cfg), dbcfg, std::ref(_mnotifier), std::ref(_feature_service), std::ref(_token_metadata), std::ref(_cm), std::ref(_sstm), std::ref(_lang_manager), std::ref(_sst_dir_semaphore), std::ref(_scf), _out_of_space_controller, std::ref(abort_sources), utils::cross_shard_barrier()).get();
             auto stop_db = defer_verbose_shutdown("database", [this] {
                 _db.stop().get();
             });
