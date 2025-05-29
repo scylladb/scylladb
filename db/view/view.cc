886a0f09d1f5ffe79bbcb7460310bdbceca24922
libo2-yewu <libo2_yewu@cmss.chinamobile.com>
2025-05-29 15:41:29 +0800

    views: do not perform view update when `update` and `existing` is the same row in `update_entry()`.
    When repair is performed on the base table and the repair generates base
    table data, the data will be propagated to the corresponding view table.
    If there is no difference between the newly generated data and the
    existing data during the propagation process, the update of this view
    can be skipped.

--- db/view/view.cc
+++ db/view/view.cc
@@ -1042,6 +1042,12 @@ void view_updates::update_entry(data_dictionary::database db, const partition_ke
     auto view_rows = get_view_rows(base_key, update, std::nullopt, {});
 
     const auto kind = update.column_kind();
+
+    // We can skip if `update` and `existing` is the same row.
+    if (update.cells().equal(kind, *_base, existing.cells(), *_base)) {
+        return;
+    }
+
     for (const auto& [r, action] : view_rows) {
         if (auto rm = std::get_if<row_marker>(&action)) {
             r->apply(*rm);
