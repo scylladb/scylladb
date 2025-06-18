5d3fe2a4d58f3849d0f339c26f8315c1470b39b7
Petr Gusev <petr.gusev@scylladb.com>
2025-06-18 10:08:31 +0200

    storage_proxy: add cas_shard class
    The sp::cas method must be called on the correct shard,
    as determined by sp::cas_shard. Additionally, there must
    be no asynchronous yields between the shard check and
    capturing the erm strong pointer in sp::cas. While
    this condition currently holds, it's fragile and
    easy to break.
    
    To address this, future commits will move the capture of
    token_metadata_guard to the call sites of sp::cas, before
    performing the shard check.
    
    As a first step, this commit introduces a cas_shard class
    that wraps both the target shard and a token_metadata_guard
    instance. This ensures the returned shard remains valid for
    the given tablet as long as the guard is held.
    In the next commits, we’ll pass a cas_shard instance
    to sp::cas as a separate parameter.

---
+++ service/cas_shard.hh
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ *
+ * Modified by ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: (LicenseRef-ScyllaDB-Source-Available-1.0 and Apache-2.0)
+ */
+ #include "locator/tablet_metadata_guard.hh"
+
+#pragma once
+namespace service {
+    // A helper class for the storage_proxy::cas API.
+    //
+    // Represents the correct shard on which cas() must be called.
+    // The appropriate shard is selected based on the current topology,
+    // and a token_metadata_guard is acquired to ensure that the shard
+    // decision remains valid while cas_shard instance is alive.
+    class cas_shard {
+        locator::token_metadata_guard _token_guard;
+        unsigned _shard;
+    public:
+        cas_shard(const schema& s, dht::token token);
+        unsigned shard() const {
+            return _shard;
+        }
+        bool this_shard() const {
+            return _shard == this_shard_id();
+        }
+        locator::token_metadata_guard token_guard() && {
+            return std::move(_token_guard);
+        }
+    };
+}
