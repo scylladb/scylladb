8e783cc23a8a4bb7c8f548c21b452015f158220f
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2025-06-18 10:58:15 +0200

    test: add test for live updates of permissions cache config
    (cherry picked from commit dd01852341d3f2d4fca809ad7ec180f5700e138d)

--- test/cluster/test_config_live_updates.py
+++ test/cluster/test_config_live_updates.py
@@ -8,8 +8,17 @@ from test.pylib.util import wait_for
 
 @pytest.mark.asyncio
 async def test_config_live_updates(manager):
-    server = await manager.server_add()
+    config = {
+        "maintenance_socket": "ignore"  # bring back the default value
+    }
+    server = await manager.server_add(config=config)
     server_log = await manager.server_open_log(server.server_id)
+
+    await manager.server_update_config(server.server_id, "permissions_validity_in_ms", 20000)
+    await server_log.wait_for("Updating loading cache; max_size: 1000, expiry: 20000ms, refresh: 100ms")
+
+    await manager.server_update_config(server.server_id, "permissions_update_interval_in_ms", 30000)
+    await server_log.wait_for("Updating loading cache; max_size: 1000, expiry: 20000ms, refresh: 30000ms")
 
     await manager.server_update_config(server.server_id, "uninitialized_connections_semaphore_cpu_concurrency", 16)
     await server_log.wait_for("Updating uninitialized_connections_semaphore_cpu_concurrency from 8 to 16 due to config update")
