0fbafa3918ac39e9f22b0961010fa447bf04a915
Marcin Maliszkiewicz <marcinmal@scylladb.com>
2025-06-18 10:58:15 +0200

    test: add test for live updates of permissions cache config
    (cherry picked from commit dd01852341d3f2d4fca809ad7ec180f5700e138d)

---
+++ test/topology_custom/test_config_live_updates.py
@@ -0,0 +1,21 @@
+# Copyright 2025-present ScyllaDB
+#
+# SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+
+import pytest
+from test.pylib.util import wait_for
+
+
+@pytest.mark.asyncio
+async def test_config_live_updates(manager):
+    config = {
+        "maintenance_socket": "ignore"  # bring back the default value
+    }
+    server = await manager.server_add(config=config)
+    server_log = await manager.server_open_log(server.server_id)
+
+    await manager.server_update_config(server.server_id, "permissions_validity_in_ms", 20000)
+    await server_log.wait_for("Updating loading cache; max_size: 1000, expiry: 20000ms, refresh: 100ms")
+
+    await manager.server_update_config(server.server_id, "permissions_update_interval_in_ms", 30000)
+    await server_log.wait_for("Updating loading cache; max_size: 1000, expiry: 20000ms, refresh: 30000ms")
