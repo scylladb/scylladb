cf18469564c51ca64267216f99b96c7c97bb4405
Botond Dénes <bdenes@scylladb.com>
2025-06-25 16:35:56 +0300

    Introduce the ScyllaDB Web Shell
    A HTTP server allowing for simple interactive query workflow and a
    simple web application to serve as the default UI.
    
    The server is disabled by default for now, to enable, set webshell_port
    to a non-0 value.
    
    The web shell server has the following endpoints:
    * / - serves the web application client
    * /login - creates a session
    * /query - execute a query against the ScyllaDB cluster
    * /option - change configuration of the current session
    
    The web shell is stateful, the state is stored in the session. The
    session is created with the /login endpoint and all /query and /option
    calls require a valid session to be set up. The session id is stored as
    a cookie. The /option endpoint can be used to change the session's
    configuration and state.
    
    The web client is a simple HTML5 and "vanilla" Javascript client, that
    uses AJAX to communicate with the backend.
    
    The backend HTTP API was designed to be friendly to command-line clients,
    like curl.

---
+++ webshell/webshell.hh
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ *
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include <seastar/core/units.hh>
+#include <seastar/http/httpd.hh>
+#include "cql3/query_processor.hh"
+#include "timeout_config.hh"
+
+namespace webshell {
+
+struct config {
+    scheduling_group scheduling_group;
+    ::updateable_timeout_config timeout_config;
+};
+
+class session_manager;
+
+class server : public peering_sharded_service<server> {
+    static constexpr size_t content_length_limit = 16*MB;
+
+private:
+    config _cfg;
+    httpd::http_server _http_server;
+    named_gate _pending_requests;
+    std::unique_ptr<session_manager> _session_manager;
+
+private:
+    void set_routes(seastar::httpd::routes& r);
+
+public:
+    server(config cfg, cql3::query_processor& qp, auth::service& auth_service, qos::service_level_controller& sl_controller);
+    ~server();
+
+    future<> init(net::inet_address addr, uint16_t port);
+    future<> stop();
+};
+
+} // namespace webshell
