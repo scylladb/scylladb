18257823b932a5a805ae3d395a6e64dc6a37d3d0
Botond Dénes <bdenes@scylladb.com>
2025-06-26 10:22:47 +0300

    cql3/query_result_printer: improve handling of null values
    Also check for empty buffer and add missing continue, so a null value is
    not attempted to be converted to json.

--- cql3/result_set.cc
+++ cql3/result_set.cc
@@ -258,7 +258,7 @@ void print_query_results_json(std::ostream& os, const result& result) {
         writer.StartObject();
         for (size_t i = 0; i < row.size(); ++i) {
             writer.Key(column_metadata[i]->name->text());
-            if (!row[i]) {
+            if (!row[i] || row[i]->empty()) {
                 writer.Null();
                 continue;
             }
