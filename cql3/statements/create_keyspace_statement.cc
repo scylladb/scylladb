e900f870e68c251017aaf307ae79d7d0ba99c11b
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-07-01 17:26:52 +0200

    cql3/statements: Fix indentation

--- cql3/statements/create_keyspace_statement.cc
+++ cql3/statements/create_keyspace_statement.cc
@@ -124,11 +124,11 @@ future<std::tuple<::shared_ptr<cql_transport::event::schema_change>, utils::chun
         // If `rf_rack_valid_keyspaces` is enabled, it's forbidden to create an RF-rack-invalid keyspace.
         // Verify that it's RF-rack-valid.
         // For more context, see: scylladb/scylladb#23071.
-            try {
-                // We hold a group0_guard, so it's correct to check this here.
-                // The topology or schema cannot change while we're performing this query.
-                locator::assert_rf_rack_valid_keyspace(_name, tmptr, *rs);
-            } catch (const std::exception& e) {
+        try {
+            // We hold a group0_guard, so it's correct to check this here.
+            // The topology or schema cannot change while we're performing this query.
+            locator::assert_rf_rack_valid_keyspace(_name, tmptr, *rs);
+        } catch (const std::exception& e) {
             if (cfg.rf_rack_valid_keyspaces()) {
                 // There's no guarantee what the type of the exception will be, so we need to
                 // wrap it manually here in a type that can be passed to the user.
@@ -145,7 +145,7 @@ future<std::tuple<::shared_ptr<cql_transport::event::schema_change>, utils::chun
                     "https://docs.scylladb.com/manual/stable/reference/glossary.html#term-RF-rack-valid-keyspace.",
                     _name));
             }
-            }
+        }
     } catch (const exceptions::already_exists_exception& e) {
         if (!_if_not_exists) {
           co_return coroutine::exception(std::current_exception());
