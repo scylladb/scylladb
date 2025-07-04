84e1ac524809e56a60143582e4724563b39e67a1
Pavel Emelyanov <xemul@scylladb.com>
2025-07-04 15:13:53 +0300

    sstables: Move versions static-assertion check to .cc file
    Thiss check validates that static values of supported versions are "in
    sync" with each other. It's enough to do it once when compiling
    sstable_version.cc, not every time the header is included.
    
    refs: #1 (not that it helps noticeably, but technically it fits)
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>
    
    Closes scylladb/scylladb#24839

--- sstables/sstable_version.cc
+++ sstables/sstable_version.cc
@@ -6,11 +6,31 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
+#include "version.hh"
 #include "sstable_version.hh"
 #include "sstable_version_k_l.hh"
 #include "sstable_version_m.hh"
 
 namespace sstables {
+
+template <size_t S1, size_t S2>
+constexpr bool check_sstable_versions(const std::array<sstable_version_types, S1>& all_sstable_versions,
+        const std::array<sstable_version_types, S2>& writable_sstable_versions, sstable_version_types oldest_writable_sstable_format) {
+    for (auto v : writable_sstable_versions) {
+        if (v < oldest_writable_sstable_format) {
+            return false;
+        }
+    }
+    size_t expected = 0;
+    for (auto v : all_sstable_versions) {
+        if (v >= oldest_writable_sstable_format) {
+            ++expected;
+        }
+    }
+    return expected == S2;
+}
+
+static_assert(check_sstable_versions(all_sstable_versions, writable_sstable_versions, oldest_writable_sstable_format));
 
 const sstring sstable_version_constants::TOC_SUFFIX = "TOC.txt";
 const sstring sstable_version_constants::TEMPORARY_TOC_SUFFIX = "TOC.txt.tmp";
