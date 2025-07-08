18173a336be87096ea491bd4119aeb735248b7de
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2025-07-08 12:40:37 +0200

    replica: hold compaction group gate during flush
    Destructor of database_sstable_write_monitor, which is created
    in table::try_flush_memtable_to_sstable, tries to get the compaction
    state of the processed compaction group. If at this point
    the compaction group is already stopped (and the compaction state
    is removed), e.g. due to concurrent tablet merge, an exception is
    thrown and a node coredumps.
    
    Add flush gate to compaction group to wait for flushes in
    compaction_group::stop. Hold the gate in seal function in
    table::make_memtable_list. seal function is turned into
    a coroutine to ensure it won't throw.
    
    Wait until async_gate is closed before flushing, to ensure that
    all data is written into sstables. Stop ongoing compactions
    beforehand.
    
    Remove unnecessary flush in tablet_storage_group_manager::merge_completion_fiber.
    Stop method already flushes the compaction group.
    
    Fixes: #23911.
    
    Closes scylladb/scylladb#24582
    
    (cherry picked from commit 2ec54d4f1a7f5cf578d97cda23524a93a983f77d)

--- replica/compaction_group.hh
+++ replica/compaction_group.hh
@@ -58,6 +58,8 @@ class compaction_group {
     seastar::condition_variable _staging_done_condition;
     // Gates async operations confined to a single group.
     seastar::gate _async_gate;
+    // Gates flushes.
+    seastar::gate _flush_gate;
     bool _tombstone_gc_enabled = true;
 private:
     // Adds new sstable to the set of sstables
@@ -187,6 +189,10 @@ public:
 
     seastar::gate& async_gate() noexcept {
         return _async_gate;
+    }
+
+    seastar::gate& flush_gate() noexcept {
+        return _flush_gate;
     }
 
     compaction_manager& get_compaction_manager() noexcept;
