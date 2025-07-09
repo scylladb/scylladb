8ff0871c3299c54ce92293cad6a28e7f6c84d761
Petr Gusev <petr.gusev@scylladb.com>
2025-07-09 14:46:41 +0200

    migration_manager: add timeout to start_group0_operation and announce
    Pass a timeout parameter through to start_operation()
    and add_entry(), respectively.
    
    This is a preparatory change for the next commit, which
    will use the timeout to properly handle timeouts during
    lazy creation of Paxos state tables.

--- service/migration_manager.cc
+++ service/migration_manager.cc
@@ -935,7 +935,7 @@ future<> migration_manager::push_schema_mutation(locator::host_id id, const std:
 }
 
 template<typename mutation_type>
-future<> migration_manager::announce_with_raft(std::vector<mutation> schema, group0_guard guard, std::string_view description) {
+future<> migration_manager::announce_with_raft(std::vector<mutation> schema, group0_guard guard, std::string_view description, std::optional<raft_timeout> timeout) {
     SCYLLA_ASSERT(this_shard_id() == 0);
     auto schema_features = _feat.cluster_schema_features();
     auto adjusted_schema = db::schema_tables::adjust_schema_for_schema_features(std::move(schema), schema_features);
@@ -946,7 +946,7 @@ future<> migration_manager::announce_with_raft(std::vector<mutation> schema, gro
         },
         guard, std::move(description));
 
-    return _group0_client.add_entry(std::move(group0_cmd), std::move(guard), _as, raft_timeout{});
+    return _group0_client.add_entry(std::move(group0_cmd), std::move(guard), _as, timeout.value_or(raft_timeout{}));
 }
 
 future<> migration_manager::announce_without_raft(std::vector<mutation> schema, group0_guard guard) {
@@ -1007,31 +1007,31 @@ static void add_committed_by_group0_flag(std::vector<mutation>& schema, const gr
 
 // Returns a future on the local application of the schema
 template<typename mutation_type>
-future<> migration_manager::announce(std::vector<mutation> schema, group0_guard guard, std::string_view description) {
+future<> migration_manager::announce(std::vector<mutation> schema, group0_guard guard, std::string_view description, std::optional<raft_timeout> timeout) {
     if (_feat.group0_schema_versioning) {
         schema.push_back(make_group0_schema_version_mutation(_storage_proxy.data_dictionary(), guard));
         add_committed_by_group0_flag(schema, guard);
     }
 
     if (guard.with_raft()) {
-        return announce_with_raft<mutation_type>(std::move(schema), std::move(guard), std::move(description));
+        return announce_with_raft<mutation_type>(std::move(schema), std::move(guard), std::move(description), std::move(timeout));
     } else {
         return announce_without_raft(std::move(schema), std::move(guard));
     }
 }
 template
-future<> migration_manager::announce_with_raft<schema_change>(std::vector<mutation> schema, group0_guard, std::string_view description);
+future<> migration_manager::announce_with_raft<schema_change>(std::vector<mutation> schema, group0_guard, std::string_view description, std::optional<raft_timeout> timeout);
 template
-future<> migration_manager::announce_with_raft<topology_change>(std::vector<mutation> schema, group0_guard, std::string_view description);
+future<> migration_manager::announce_with_raft<topology_change>(std::vector<mutation> schema, group0_guard, std::string_view description, std::optional<raft_timeout> timeout);
 
 template
-future<> migration_manager::announce<schema_change>(std::vector<mutation> schema, group0_guard, std::string_view description);
+future<> migration_manager::announce<schema_change>(std::vector<mutation> schema, group0_guard, std::string_view description, std::optional<raft_timeout> timeout = std::nullopt);
 template
-future<> migration_manager::announce<topology_change>(std::vector<mutation> schema, group0_guard, std::string_view description);
+future<> migration_manager::announce<topology_change>(std::vector<mutation> schema, group0_guard, std::string_view description, std::optional<raft_timeout> timeout = std::nullopt);
 
-future<group0_guard> migration_manager::start_group0_operation() {
+future<group0_guard> migration_manager::start_group0_operation(std::optional<raft_timeout> timeout) {
     SCYLLA_ASSERT(this_shard_id() == 0);
-    return _group0_client.start_operation(_as, raft_timeout{});
+    return _group0_client.start_operation(_as, timeout.value_or(raft_timeout{}));
 }
 
 /**
