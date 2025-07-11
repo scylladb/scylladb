8f735bc4ebd09eacdf4f734e07e2bd5b6e2ccd73
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-07-11 23:05:20 +0200

    index, cql3: Remove secondary_index_manager::create_view_for_index
    After moving the function to `cql3/statements/create_index_statement`
    and adjusting it to compile in previous commits, we're replacing all
    of the references to the original by calls to the new function.
    We remove the old one.
    
    The rationale for doing this is that we want to give more control
    over the function's behavior to the code actually creating indexes.
    As of now, there is no other way to create an index than to perform
    a schema change via a CQL statement, so the migration makes sense.
    It also makes the code more natural: other CQL statements implement
    that logic within the scope of files in `cql3/` module.
    
    This is a stepping stone towards extending the capabilities of
    the CREATE INDEX statement.
    
    Refs scylladb/scylladb#16454

--- cql3/statements/create_index_statement.cc
+++ cql3/statements/create_index_statement.cc
@@ -16,6 +16,7 @@
 #include "prepared_statement.hh"
 #include "replica/database.hh"
 #include "types/types.hh"
+#include "utils/chunked_vector.hh"
 #include "validation.hh"
 #include "service/storage_proxy.hh"
 #include "service/migration_manager.hh"
@@ -574,7 +575,7 @@ create_index_statement::prepare_schema_mutations(query_processor& qp, const quer
 
         // Produce the underlying view for the index.
         if (db::schema_tables::view_should_exist(res->index)) {
-            view_ptr view = cf.get_index_manager().create_view_for_index(res->index);
+            view_ptr view = create_view_for_index(cf.schema(), res->index);
             utils::chunked_vector<mutation> view_muts = co_await service::prepare_new_view_announcement(qp.proxy(), std::move(view), ts);
 
             muts.reserve(muts.size() + view_muts.size());
