1c78e0a2f74c5d1d11a24a09517336a0cd1c2c7b
Botond Dénes <bdenes@scylladb.com>
2025-07-16 13:45:21 +0300

    compaction/compaction_garbage_collector: max_purgeable: add exploded timestamps
    Instead of a single [min live] timestamp, add a list of timestamps, each
    with its own source and expiry threshold. This will enable more advanced
    overlap checks, possibly avoiding overlap checks against certain
    timestamps, when it is known that the candidate tombstone cannot
    possibly cover data in the source with the current min live timestamp.

--- compaction/compaction_garbage_collector.hh
+++ compaction/compaction_garbage_collector.hh
@@ -28,6 +28,16 @@ enum class max_purgeable_timestamp_source {
     other_sstables_possibly_shadowing_data
 };
 
+struct timestamp_with_expiry {
+    api::timestamp_type timestamp;
+    max_purgeable_timestamp_source source;
+    std::optional<gc_clock::time_point> expiry_threshold;
+
+    timestamp_with_expiry(api::timestamp_type ts, max_purgeable_timestamp_source src, std::optional<gc_clock::time_point> expiry = std::nullopt)
+        : timestamp(ts), source(src), expiry_threshold(expiry)
+    { }
+};
+
 class max_purgeable {
 public:
     using timestamp_source = max_purgeable_timestamp_source;
@@ -35,17 +45,46 @@ public:
 private:
     api::timestamp_type _timestamp { api::missing_timestamp };
     timestamp_source _source { timestamp_source::none };
+    std::vector<timestamp_with_expiry> _exploded_timestamps;
+
+    void recalculate_timestamp_and_source();
 
 public:
     max_purgeable() = default;
     explicit max_purgeable(api::timestamp_type timestamp, timestamp_source source = timestamp_source::none)
         : _timestamp(timestamp), _source(source)
     { }
+    explicit max_purgeable(std::vector<timestamp_with_expiry>);
 
     operator bool() const { return _timestamp != api::missing_timestamp; }
 
     api::timestamp_type timestamp() const noexcept { return _timestamp; }
     timestamp_source source() const noexcept { return _source; }
+    const std::vector<timestamp_with_expiry>& exploded_timestamps() const noexcept { return _exploded_timestamps; }
+
+    max_purgeable& combine(max_purgeable other);
+
+    struct can_purge_result {
+        bool can_purge { true };
+        timestamp_source timestamp_source { timestamp_source::none };
+
+        // can purge?
+        operator bool() const noexcept {
+            return can_purge;
+        }
+        bool operator!() const noexcept {
+            return !can_purge;
+        }
+    };
+
+    // Determines whether the tombstone can be purged.
+    // Uses exploded timestamps and the provided expiry time to exclude certain
+    // timtestamps from the overlap check, increasing the chance of purging.
+    // NOTE: checking for purge by
+    //      tombstone.timestamp() >= max_purgeable.timestamp()
+    // is still correct, but is a stricter check: for all cases where can_purge()
+    // returns false, the above will also evaluate to false.
+    can_purge_result can_purge(tombstone, gc_clock::time_point) const;
 };
 
 template <>
