f69d858fefa8f13a2928a1d7e4245dc41beb30c3
Botond Dénes <bdenes@scylladb.com>
2025-07-16 13:45:21 +0300

    compaction/compaction_garbage_collector: max_purgeable: add expiry_treshold
    Allow possibly avoiding overlap checks in the case where the source of
    the min-live timestamp is known to only contain data which was written
    *after* expiry treshold. Expiry treshold is the upper bound of
    tombstone.deletion_time that was already expired at the time of
    obtaining this expiry treshold value. Meaning that any write originating
    from after this point in time, was generated at a time when such
    tombstone was already expired. Hence these writes are not relevant for
    the purposes of overlap checks with the tombstone and so their min-live
    timestamp can be ignored.
    This is important for MV workloads, where writes generated now can have
    timestamps going far back in time, possibly blocking tombstone GC of
    much older [shadowable] tombstones.

--- compaction/compaction_garbage_collector.hh
+++ compaction/compaction_garbage_collector.hh
@@ -22,7 +22,14 @@ using can_gc_fn = std::function<bool(tombstone, is_shadowable)>;
 extern can_gc_fn always_gc;
 extern can_gc_fn never_gc;
 
-// A tombstone is purgeable if: tombstone.deletion_time ∈ [-inf, max_purgeable._timestamp)
+// For the purposes of overlap with live data, a tombstone is purgeable if:
+//      tombstone.timestamp ∈ (max_purgeable._timestamp, +inf)
+//
+// The above overlap check can be omitted iff:
+//      tombstone.deletion_time ∈ (-inf, max_purgeable._expiry_threshold.value_or(gc_clock::time_point::min()))
+//
+// So in other words, a tombstone is purgeable iff:
+//      tombstone.deletion_time < max_purgeable._expiry_threshold.value_or(gc_clock::time_point::min()) || tombstone.timestamp > max_purgeable._timestamp
 class max_purgeable {
 public:
     enum class timestamp_source {
@@ -31,8 +38,11 @@ public:
         other_sstables_possibly_shadowing_data
     };
 
+    using expiry_threshold_opt = std::optional<gc_clock::time_point>;
+
 private:
     api::timestamp_type _timestamp { api::missing_timestamp };
+    expiry_threshold_opt _expiry_threshold;
     timestamp_source _source { timestamp_source::none };
 
 public:
@@ -40,11 +50,41 @@ public:
     explicit max_purgeable(api::timestamp_type timestamp, timestamp_source source = timestamp_source::none)
         : _timestamp(timestamp), _source(source)
     { }
+    explicit max_purgeable(api::timestamp_type timestamp, expiry_threshold_opt expiry_threshold, timestamp_source source = timestamp_source::none)
+        : _timestamp(timestamp), _expiry_threshold(expiry_threshold), _source(source)
+    { }
 
     operator bool() const { return _timestamp != api::missing_timestamp; }
+    bool operator==(const max_purgeable&) const = default;
+    bool operator!=(const max_purgeable&) const = default;
 
     api::timestamp_type timestamp() const noexcept { return _timestamp; }
+    expiry_threshold_opt expiry_threshold() const noexcept { return _expiry_threshold; }
     timestamp_source source() const noexcept { return _source; }
+
+    max_purgeable& combine(max_purgeable other);
+
+    struct can_purge_result {
+        bool can_purge { true };
+        timestamp_source timestamp_source { timestamp_source::none };
+
+        // can purge?
+        operator bool() const noexcept {
+            return can_purge;
+        }
+        bool operator!() const noexcept {
+            return !can_purge;
+        }
+    };
+
+    // Determines whether the tombstone can be purged.
+    // Uses the expiry treshold if available in conjuction to the provided expiry
+    // time to avoid the overlap check if possible, increasing the chance of purging.
+    // NOTE: checking for purge by
+    //      tombstone.timestamp() >= max_purgeable.timestamp()
+    // is still correct, but is a stricter check: for all cases where can_purge()
+    // returns false, the above will also evaluate to false.
+    can_purge_result can_purge(tombstone) const;
 };
 
 template <>
