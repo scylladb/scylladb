08b57f611e79f6c484ef739c7256829e54027efd
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-07-16 14:58:30 +0200

    service/view_building_coordinator: handle tablet operations
    If the view building coordinator is running, adjust view_building_tasks
    in case of tablet operations.
    
    The mutations are generated in the same batch as tablet mutations.
    At the start of tablet migration/resize/RF change, started
    view building tasks are aborted (by setting ABORTED state) if needed.
    Then, new adjusted tasks are created in group0 batch which ends the
    tablet operation and aborted tasks are removed from the table.
    
    In case the tablet operation fails or is revoked, aborted view building
    tasks are rollback by creating new copies of them and aborted ones are
    deleted from the table.
    
    View building tasks are not aborted/changed during tablet repair,
    because in this case, even if vb task is started, a staging sstable will
    be generated.

--- service/view_building_coordinator.hh
+++ service/view_building_coordinator.hh
@@ -10,9 +10,12 @@
 #pragma once
 
 #include <seastar/core/abort_source.hh>
+#include "db/system_keyspace.hh"
 #include "locator/tablets.hh"
+#include "mutation/canonical_mutation.hh"
 #include "raft/raft.hh"
 #include "service/endpoint_lifecycle_subscriber.hh"
+#include "schema/schema_fwd.hh"
 #include "service/topology_state_machine.hh"
 #include "service/view_building_state.hh"
 
@@ -61,6 +64,14 @@ public:
     future<> run();
     future<> stop();
 
+    void generate_tablet_migration_updates(utils::chunked_vector<canonical_mutation>& out, const group0_guard& guard, const locator::tablet_map& tmap, locator::global_tablet_id gid, const locator::tablet_transition_info& trinfo);
+    void generate_tablet_resize_updates(utils::chunked_vector<canonical_mutation>& out, const group0_guard& guard, table_id table_id, const locator::tablet_map& old_tmap, const locator::tablet_map& new_tmap);
+
+    void abort_tasks(utils::chunked_vector<canonical_mutation>& out, const group0_guard& guard, table_id table_id);
+    void abort_tasks(utils::chunked_vector<canonical_mutation>& out, const group0_guard& guard, table_id table_id, locator::tablet_replica replica, dht::token last_token);
+    void rollback_aborted_tasks(utils::chunked_vector<canonical_mutation>& out, const group0_guard& guard, table_id table_id);
+    void rollback_aborted_tasks(utils::chunked_vector<canonical_mutation>& out, const group0_guard& guard, table_id table_id, locator::tablet_replica replica, dht::token last_token);
+
     virtual void on_up(const gms::inet_address& endpoint, locator::host_id host_id) override;
 
 private:
@@ -85,6 +96,9 @@ private:
     future<std::optional<remote_work_results>> work_on_tasks(locator::tablet_replica replica, std::vector<utils::UUID> tasks);
     future<utils::chunked_vector<mutation>> update_state_after_work_is_done(const group0_guard& guard, const locator::tablet_replica& replica, remote_work_results results);
 };
+
+void abort_view_building_tasks(const view_building_state_machine& vb_sm,
+        utils::chunked_vector<canonical_mutation>& out, api::timestamp_type write_timestamp, table_id table_id, const locator::tablet_replica& replica, dht::token last_token);
 
 }
 
