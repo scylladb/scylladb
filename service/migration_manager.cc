085abef05d3cf0cb50bce86325617d9c3f377a24
Michael Litvak <michael.litvak@scylladb.com>
2025-07-17 20:20:21 +0200

    schema_registry: use extended_frozen_schema in schema load
    Change the schema loader type in the schema_registry to return a
    extended_frozen_schema instead of view_schema_and_base_info, and
    remove view_schema_and_base_info which is not used anymore.
    
    The casting between them is trivial.

--- service/migration_manager.cc
+++ service/migration_manager.cc
@@ -1220,7 +1220,7 @@ static future<schema_ptr> get_schema_definition(table_schema_version v, locator:
                 base_schema = db.find_schema(us->view_info()->base_id());
                 db::schema_tables::check_no_legacy_secondary_index_mv_schema(db, view_ptr(us), base_schema);
             }
-            return db::schema_tables::store_column_mapping(proxy, us, true).then([us, base_schema] -> view_schema_and_base_info {
+            return db::schema_tables::store_column_mapping(proxy, us, true).then([us, base_schema] -> extended_frozen_schema {
                 return extended_frozen_schema(us);
             });
         });
