afaad1c6b28fb1b6415775cff414bb6d898dad80
Michael Litvak <michael.litvak@scylladb.com>
2025-07-17 19:49:56 +0200

    schema_registry: replace frozen_schema+base_info with extended_frozen_schema
    The schema_registry_entry holds a frozen_schema and a base_info. The
    base_info is extracted from the schema_ptr on load of a schema_ptr, and
    it is used when unfreezing the schema.
    
    But this is exactly what extended_frozen_schema is doing, so we can
    just store an object of this type in the schema_registry_entry.
    
    This makes the code simpler because the schema registry doesn't need to
    be aware of the base_info.

--- frozen_schema.hh
+++ frozen_schema.hh
@@ -36,6 +36,7 @@ public:
 // with it to be used for unfreezing it.
 struct extended_frozen_schema {
     extended_frozen_schema(const schema_ptr& c);
+    extended_frozen_schema(frozen_schema fs, std::optional<db::view::base_dependent_view_info> base_info);
     schema_ptr unfreeze(const db::schema_ctxt& ctxt) const;
 
     frozen_schema fs;
