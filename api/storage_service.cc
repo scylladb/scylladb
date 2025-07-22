401a05ccd43e22d154133004f46e7c5850e20c22
Pavel Emelyanov <xemul@scylladb.com>
2025-07-22 11:58:08 +0300

    api: Simplify table_info::name extraction with std::views::transform
    Instead of using lambda, pass pointer to struct member. The result is
    the same, but the code is nicer.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- api/storage_service.cc
+++ api/storage_service.cc
@@ -232,7 +232,7 @@ future<scrub_info> parse_scrub_options(const http_context& ctx, sharded<db::snap
     scrub_info info;
     auto [ keyspace, table_infos ] = parse_table_infos(ctx, *req, "cf");
     info.keyspace = std::move(keyspace);
-    info.column_families = table_infos | std::views::transform([] (auto ti) { return ti.name; }) | std::ranges::to<std::vector>();
+    info.column_families = table_infos | std::views::transform(&table_info::name) | std::ranges::to<std::vector>();
     auto scrub_mode_str = req->get_query_param("scrub_mode");
     auto scrub_mode = sstables::compaction_type_options::scrub::mode::abort;
 
