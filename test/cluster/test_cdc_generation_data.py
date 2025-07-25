c98dde92db6e838269707d8ecb5afece9fb8c21b
Petr Gusev <petr.gusev@scylladb.com>
2025-07-25 18:08:43 +0200

    Revert "storage_service: test_group0_apply_while_node_is_being_shutdown"
    This reverts commit b1050944a3df28b3368e4328dfd506bcf8e1f424.

--- test/cluster/test_cdc_generation_data.py
+++ test/cluster/test_cdc_generation_data.py
@@ -1,13 +1,8 @@
 from test.pylib.manager_client import ManagerClient
 from test.pylib.rest_client import inject_error
 from test.cluster.util import check_token_ring_and_group0_consistency
-from test.cluster.conftest import skip_mode
-import logging
+
 import pytest
-import asyncio
-
-
-logger = logging.getLogger(__name__)
 
 """
 The injection forces the topology coordinator to send CDC generation data in multiple parts,
@@ -36,40 +31,3 @@ async def test_send_data_in_parts(manager: ManagerClient):
             break
     else:
         pytest.fail("No CDC generation data sent in parts was found")
-
-
-@pytest.mark.asyncio
-@skip_mode('release', 'error injections are not supported in release mode')
-async def test_group0_apply_while_node_is_being_shutdown(manager: ManagerClient):
-    # This a regression test for #24401.
-
-    logger.info("Starting s0")
-    s0 = await manager.server_add(cmdline=['--logger-log-level', 'raft_group0=debug'])
-
-    logger.info("Injecting topology_state_load_before_update_cdc into s0")
-    await manager.api.enable_injection(s0.ip_addr, "topology_state_load_before_update_cdc", False)
-
-    logger.info("Starting s1")
-    s1_start_task = asyncio.create_task(manager.server_add())
-
-    logger.info("Waiting for topology_state_load_before_update_cdc on s0")
-    log = await manager.server_open_log(s0.server_id)
-    await log.wait_for('topology_state_load_before_update_cdc hit, wait for message')
-
-    logger.info("Triggering s0 shutdown")
-    stop_s0_task = asyncio.create_task(manager.server_stop_gracefully(s0.server_id))
-
-    logger.info("Waiting for group0 to start aborting on s0")
-    await log.wait_for('Raft group0 service is aborting...')
-
-    logger.info("Releasing topology_state_load_before_update_cdc on s0")
-    await manager.api.message_injection(s0.ip_addr, 'topology_state_load_before_update_cdc')
-
-    await stop_s0_task
-    try:
-        await s1_start_task
-    except Exception:
-        pass  # ingore errors, since we don't care
-
-    errors = await log.grep_for_errors()
-    assert errors == []
