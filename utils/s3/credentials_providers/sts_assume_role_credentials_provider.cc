3ecef00b391af1253d6adcf3048aa6d96f1cdddb
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-07-30 17:24:49 +0300

    s3_creds: use Seastar HTTP client with retry strategy
    In AWS credentials providers, replace `retryable_http_client` with
    Seastar's native HTTP client. Integrate the newly added
    `default_aws_retry_strategy` to handle retries more efficiently and
    reduce dependency on external retry logic.

--- utils/s3/credentials_providers/sts_assume_role_credentials_provider.cc
+++ utils/s3/credentials_providers/sts_assume_role_credentials_provider.cc
@@ -44,8 +44,8 @@ future<> sts_assume_role_credentials_provider::update_credentials() {
     req.query_parameters["Action"] = "AssumeRole";
     req.query_parameters["RoleSessionName"] = format("{}", utils::make_random_uuid());
     req.query_parameters["RoleArn"] = role_arn;
-    auto factory = std::make_unique<utils::http::dns_connection_factory>(sts_host, port, is_secured, sts_logger);
-    retryable_http_client http_client(std::move(factory), 1, retryable_http_client::ignore_exception, http::experimental::client::retry_requests::yes, retry_strategy);
+    http::experimental::client http_client(
+        std::make_unique<utils::http::dns_connection_factory>(sts_host, port, false, sts_logger), 1, 1024, std::make_unique<default_aws_retry_strategy>());
     co_await http_client.make_request(
         std::move(req),
         [this](const http::reply&, input_stream<char>&& in) -> future<> {
--- utils/s3/credentials_providers/sts_assume_role_credentials_provider.hh
+++ utils/s3/credentials_providers/sts_assume_role_credentials_provider.hh
@@ -8,7 +8,6 @@
 
 #pragma once
 #include "aws_credentials_provider.hh"
-#include "utils/s3/retry_strategy.hh"
 
 namespace aws {
 
@@ -28,7 +27,6 @@ private:
     seastar::future<> update_credentials();
     s3::aws_credentials parse_creds(seastar::sstring& body);
 
-    default_retry_strategy retry_strategy;
     std::string sts_host;
     std::string role_arn;
     unsigned port{443};
