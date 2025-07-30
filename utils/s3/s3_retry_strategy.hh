561161e424687bdd87910f106d51d9b4cdd8646d
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-07-30 17:49:52 +0300

    s3_client: migrate s3_client to Seastar HTTP client
    Eliminate use of `retryable_http_client` in `s3_client` and adopt
    Seastar's native HTTP client.

--- utils/s3/s3_retry_strategy.hh
+++ utils/s3/s3_retry_strategy.hh
@@ -7,18 +7,18 @@
  */
 
 #pragma once
-#include "retry_strategy.hh"
+#include "default_aws_retry_strategy.hh"
 
 namespace aws {
 
 class aws_error;
 
-class s3_retry_strategy : public default_retry_strategy {
+class s3_retry_strategy : public default_aws_retry_strategy {
 public:
     using credentials_refresher = std::function<seastar::future<>()>;
-    s3_retry_strategy(credentials_refresher creds_refresher, unsigned max_retries = 10, unsigned scale_factor = 25);
+    explicit s3_retry_strategy(credentials_refresher creds_refresher, unsigned max_retries = 10, unsigned scale_factor = 25);
 
-    seastar::future<bool> should_retry(const aws_error& error, unsigned attempted_retries) const override;
+    seastar::future<bool> should_retry(std::exception_ptr error, unsigned attempted_retries) const override;
 
 private:
     credentials_refresher _creds_refresher;
