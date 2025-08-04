35c7d91bd0d1811ddd2db980c5c2fc60ebab2b24
Michał Hudobski <michal.hudobski@scylladb.com>
2025-08-04 09:57:58 +0200

    metrics, utils: move estimated_histogram_to_metrics to utils
    Currently a function that converts an estimated histogram to
    a metric is placed in alternator code. As it is more general
    and we want to use it in other places, we move it to the utils
    directory.

--- alternator/stats.cc
+++ alternator/stats.cc
@@ -14,20 +14,6 @@
 namespace alternator {
 
 const char* ALTERNATOR_METRICS = "alternator";
-static seastar::metrics::histogram estimated_histogram_to_metrics(const utils::estimated_histogram& histogram) {
-    seastar::metrics::histogram res;
-    res.buckets.resize(histogram.bucket_offsets.size());
-    uint64_t cumulative_count = 0;
-    res.sample_count = histogram._count;
-    res.sample_sum = histogram._sample_sum;
-    for (size_t i = 0; i < res.buckets.size(); i++) {
-        auto& v = res.buckets[i];
-        v.upper_bound = histogram.bucket_offsets[i];
-        cumulative_count += histogram.buckets[i];
-        v.count = cumulative_count;
-    }
-    return res;
-}
 
 static seastar::metrics::label column_family_label("cf");
 static seastar::metrics::label keyspace_label("ks");
