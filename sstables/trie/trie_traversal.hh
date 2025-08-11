0ffe336887b377a43f7f3408043d870f2646100a
Michał Chojnowski <michal.chojnowski@scylladb.com>
2025-08-11 15:36:41 +0200

    sstables/trie/trie_traversal: extract comparable_bytes_iterator to its own file
    In a later commit, this concept will be used in a place that's not
    dependent on trie traversal routines. So extract it to its own header.

--- sstables/trie/trie_traversal.hh
+++ sstables/trie/trie_traversal.hh
@@ -14,6 +14,7 @@
 #include <seastar/core/coroutine.hh>
 #include "common.hh"
 #include "utils/assert.hh"
+#include "comparable_bytes_iterator.hh"
 
 namespace sstables::trie {
 
@@ -45,33 +46,6 @@ struct trail_entry {
 // Note: for the partition index, the trail should never be longer than ~6 entries.
 // For the row index, it can get arbitrarily large.
 struct ancestor_trail : utils::small_vector<trail_entry, 8> {
-};
-
-template <typename T>
-concept bytespan_ref = std::same_as<T, std::span<std::byte>&&>
-    || std::same_as<T, std::span<const std::byte>&&>;
-
-// Converting keys from the regular Scylla format to the BTI byte-comparable format
-// can be expensive, so we allow the conversion to happen lazily.
-// To support that, the traversal routines take the key as an iterator/generator
-// of fragments (byte spans).
-//
-// Note: a std::generator<const_bytes>::iterator fulfils this concept.
-template <typename T>
-concept comparable_bytes_iterator = requires (T it) {
-    // Users of comparable_bytes_iterator want to modify the contents of `*it`
-    // (for their convenience, not due to necessity), so operator* returns a reference.
-    { *it } -> bytespan_ref;
-    // Allowed to destroy `*it`;
-    { ++it };
-    // Since this concept is designed for lazy evaluation,
-    // the "end" of the sequence is usually more naturally encoded
-    // as something in the iterator itself, rather than some external "end" object.
-    //
-    // So we don't bother with iterator pairs and we instead
-    // require the iterator itself to know if it's done.
-    { it == std::default_sentinel } -> std::same_as<bool>;
-    { it != std::default_sentinel } -> std::same_as<bool>;
 };
 
 // State of the "traverse" routine.
