7e8fd132085feb00509ccdda341627de15623540
Michał Chojnowski <michal.chojnowski@scylladb.com>
2025-08-12 17:07:26 +0200

    sstables/trie: add bti_row_index_writer.cc
    Implements the Rows.db writer, which for each partition that
    possesses an intra-partition index writes a trie of separators
    between clustering key blocks, and a header (footer?)
    with enough metadata (partition key, partition tombstone)
    to allow for a direct jump to a clustering row in Data.db.
    
    The format of Rows.db is supposed to be as described in:
    https://github.com/apache/cassandra/blob/f16fb6765b8a3ff8f49accf61c908791520c0d6e/src/java/org/apache/cassandra/io/sstable/format/bti/BtiFormat.md#row-index
    and the arbitrary details about the choice of clustering block separators
    follows Cassandra's choices.
    
    I didn't actually test it for compatibility with Cassandra (yet?).
    (I followed the docs linked above and Cassandra's source code,
    but it could be that I have made a mistake somewhere).

---
+++ sstables/trie/bti_index.hh
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2024-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include <memory>
+#include "sstables/abstract_index_reader.hh"
+
+namespace sstables {
+    class file_writer;
+    class clustering_info;
+    class deletion_time;
+}
+
+class schema;
+
+namespace sstables::trie {
+
+// IMPORTANT: the index reader is currently page-oriented and
+// it assumes that BTI nodes don't cross page boundaries.
+//
+// Therefore, modifying the page size is a breaking change,
+// because old sstables will violate the assumptions of new readers,
+// and new sstables will violate the assumptions of old readers.
+//
+// If you want to change the page size, you must teach new readers how to handle both
+// the old and the new page size, and new writers must keep writing with the old page size
+// until a cluster feature guarding the new page size is set.
+constexpr static uint64_t BTI_PAGE_SIZE = 4096;
+
+// Transforms a stream of clustering index block entries
+// into a stream of trie nodes fed into bti_trie_sink.
+// Used to populate the Partitions.db file of the BTI format
+// with trie-based indexes of clustering keys.
+class bti_row_index_writer {
+    class impl;
+    std::unique_ptr<impl> _impl;
+private:
+    friend class optimized_optional<bti_row_index_writer>;
+    bti_row_index_writer();
+public:
+    ~bti_row_index_writer();
+    // The trie will be written to the given file writer.
+    // Note: the file doesn't have to be empty,
+    // and it can be extended later.
+    explicit bti_row_index_writer(sstables::file_writer&);
+    // Add a new row index entry.
+    // Must be called in ascending order.
+    // (`first_ck` must be strictly greater than the previous `last_ck`).
+    void add(
+        const schema& s,
+        const sstables::clustering_info& first_ck,
+        const sstables::clustering_info& last_ck,
+        uint64_t offset_from_partition_start,
+        const sstables::deletion_time& range_tombstone_before_first_ck);
+    // Flushes all remaining contents, and returns the position of the root node in the output stream.
+    // If add() was never called, returns -1.
+    //
+    // Note: the writer can be reused after this.
+    // (This is unlike with the partition index writer, which can't be reused.
+    // This is because reusing the row index writer is useful for performance,
+    // (without an explicit reuse, it would have to be recreated for every partition)
+    // while reusing the partition index writer isn't).
+    //
+    // Note: `partition_data_start` is the start of the entire partition in Data.db,
+    // i.e. the position of the partition key.
+    // `partition_data_end` is the position of the END_OF_PARTITION flag byte in Data.db,
+    // which lies 1 byte before the start of the next partition.
+    // So `partition_data_end` is *NOT* the start position of the next partition.
+    int64_t finish(
+        sstable_version_types,
+        const schema&,
+        int64_t partition_data_start,
+        int64_t partition_data_end,
+        const sstables::key&,
+        const sstables::deletion_time& partition_tombstone);
+};
+
+} // namespace sstables::trie
---
+++ sstables/trie/bti_index_internal.hh
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include "bti_index.hh"
+
+// This file contains some declarations which aren't needed by users of BTI readers/writers,
+// but are exposed here for testing, or because they are shared between readers and writers.
+
+namespace sstables::trie {
+
+// Each row index trie in Rows.db is followed by a header containing some partition metadata.
+// The partition's entry in Partitions.db points into that header.
+struct row_index_header {
+    // The partiition key, in BIG serialization format (i.e. the same as in Data.db or Index.db).
+    sstables::key partition_key = bytes();
+    // The global position of the root node of this partition's row index within Rows.db.
+    uint64_t trie_root;
+    // The global position of the partition inside Data.db.
+    uint64_t data_file_offset;
+    // The partition tombstone of this partition.
+    sstables::deletion_time partition_tombstone;
+    uint64_t number_of_blocks = 0;
+};
+
+future<row_index_header> read_row_index_header(input_stream<char>&& input, uint64_t start, uint64_t maxlen, reader_permit rp);
+void write_row_index_header(
+    sstable_version_types sst_ver,
+    sstables::file_writer& fw,
+    const sstables::key& pk,
+    int64_t partition_data_start,
+    uint64_t added_blocks,
+    uint64_t root_pos,
+    const sstables::deletion_time& partition_tombstone
+);
+
+} // namespace sstables::trie
