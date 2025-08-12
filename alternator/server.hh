85141609746e497a91c8d328d73788e8bfe4baec
Nadav Har'El <nyh@scylladb.com>
2025-08-12 11:38:53 +0300

    alternator: add alternator_warn_authorization config
    Before this patch, the configuration alternator_enforce_authorization
    is a boolean: true means enforce authentication checks (i.e., each
    request is signed by a valid user) and authorization checks (the user
    who signed the request is allowed by RBAC to perform this request).
    
    This patch adds a second boolean configuration option,
    alternator_warn_authorization. When alternator_enforce_authorization
    is false but alternator_warn_authorization is true, authentication and
    authorization checks are performed as in enforce mode, but failures
    are ignored and counted in two new metrics:
    
        scylla_alternator_authentication_failures
        scylla_alternator_authorization_failures
    
    additionally,also each authentication or authorization error is logged as
    a WARN-level log message. Some users prefer those log messages over
    metrics, as the log messages contain additional information about the
    failure that can be useful - such as the address of the misconfigured
    client, or the username attempted in the request.
    
    All combinations of the two configuration options are allowed:
     * If just "enforce" is true, auth failures cause a request failure.
       The failures are counted, but not logged.
     * If both "enforce" and "warn" are true, auth failures cause a request
       failure. The failures are both counted and logged.
     * If just "warn" is true, auth failures are ignored (the request
       is allowed to compelete) but are counted and logged.
     * If neither "enforce" nor "warn" are true, no authentication or
       authorization check are done at all. So we don't know about failures,
       so naturally we don't count them and don't log them.
    
    This patch is fairly straightforward, doing mainly the following
    things:
    
    1. Add an alternator_warn_authorization config parameter.
    
    2. Make sure alternator_enforce_authorization is live-updatable (we'll
       use this in a test in the next patch). It "almost" was, but a typo
       prevented the live update from working properly.
    
    3. Add the two new metrics, and increment them in every type of
       authentication or authorization error.
       Some code that needs to increment these new metrics didn't have
       access to the "stats" object, so we had to pass it around more.
    
    4. Add log messages when alternator_warn_authorization is true.
    
    5. If alternator_enforce_authorization is false, allow the auth check
       to allow the request to proceed (after having counted and/or logged
       the auth error).
    
    A separate patch will follow and add documentation suggesting to users
    how to use the new "warn" options to safely switch between non-enforcing
    to enforcing mode. Another patch will add tests for the new configuration
    options, new metrics and new log messages.
    
    Fixes #25308.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    (cherry picked from commit 51186b2f2c66d5107e57d0dd54caed46a82ceda8)

--- alternator/server.hh
+++ alternator/server.hh
@@ -40,6 +40,13 @@ class server : public peering_sharded_service<server> {
 
     key_cache _key_cache;
     utils::updateable_value<bool> _enforce_authorization;
+<<<<<<< HEAD
+||||||| parent of 51186b2f2c (alternator: add alternator_warn_authorization config)
+    utils::updateable_value<uint64_t> _max_users_query_size_in_trace_output;
+=======
+    utils::updateable_value<bool> _warn_authorization;
+    utils::updateable_value<uint64_t> _max_users_query_size_in_trace_output;
+>>>>>>> 51186b2f2c (alternator: add alternator_warn_authorization config)
     utils::small_vector<std::reference_wrapper<seastar::httpd::http_server>, 2> _enabled_servers;
     named_gate _pending_requests;
     // In some places we will need a CQL updateable_timeout_config object even
@@ -78,7 +85,15 @@ public:
     server(executor& executor, service::storage_proxy& proxy, gms::gossiper& gossiper, auth::service& service, qos::service_level_controller& sl_controller);
 
     future<> init(net::inet_address addr, std::optional<uint16_t> port, std::optional<uint16_t> https_port, std::optional<tls::credentials_builder> creds,
+<<<<<<< HEAD
             utils::updateable_value<bool> enforce_authorization, semaphore* memory_limiter, utils::updateable_value<uint32_t> max_concurrent_requests);
+||||||| parent of 51186b2f2c (alternator: add alternator_warn_authorization config)
+            utils::updateable_value<bool> enforce_authorization, utils::updateable_value<uint64_t> max_users_query_size_in_trace_output,
+            semaphore* memory_limiter, utils::updateable_value<uint32_t> max_concurrent_requests);
+=======
+            utils::updateable_value<bool> enforce_authorization, utils::updateable_value<bool> warn_authorization, utils::updateable_value<uint64_t> max_users_query_size_in_trace_output,
+            semaphore* memory_limiter, utils::updateable_value<uint32_t> max_concurrent_requests);
+>>>>>>> 51186b2f2c (alternator: add alternator_warn_authorization config)
     future<> stop();
 private:
     void set_routes(seastar::httpd::routes& r);
