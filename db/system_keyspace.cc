6db52984b030284c4de9cf5c346a1f7273c68744
Botond Dénes <bdenes@scylladb.com>
2025-08-12 07:11:09 +0300

    db/system_keyspace: disable caching for the batchlog table
    All internal queries against this table already bypass cache, due to
    problems related to tombstone handling in cache, so there is not much
    point in keeping it enabled. Furthermore, cache interacts badly with
    memtable compaction on flush enabled in the previous patch.

--- db/system_keyspace.cc
+++ db/system_keyspace.cc
@@ -204,6 +204,7 @@ schema_ptr system_keyspace::batchlog() {
        );
        builder.set_gc_grace_seconds(0);
        builder.set_memtable_compact_flushed_data(true);
+       builder.set_caching_options(caching_options::get_disabled_caching_options());
        builder.with_hash_version();
        return builder.build(schema_builder::compact_storage::no);
     }();
