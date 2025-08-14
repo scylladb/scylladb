5d53c7d675da2649305cd5b0b48561d819a5b55f
Michael Litvak <michael.litvak@scylladb.com>
2025-08-14 18:27:14 +0200

    tablets: add per_table_tablet_map to tablet metadata
    Previously, in the tablet metadata, each table points to a tablet_map
    object, which can also be shared by colocated tables.
    
    We change it now so that for each table we have two pointers - one for a
    shared tablet_map object, and another pointer to a per_table_tablet_map
    object, which is unique for each table.

--- locator/load_sketch.hh
+++ locator/load_sketch.hh
@@ -136,7 +136,7 @@ public:
             }
         } else {
             for (const auto& [table, tmap] : _tm->tablets().all_tables_ungrouped()) {
-                co_await populate_table(*tmap, host, only_dc);
+                co_await populate_table(tmap, host, only_dc);
             }
         }
 
