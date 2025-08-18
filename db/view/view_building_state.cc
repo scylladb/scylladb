a1f1e70909c4178b440767cc2906690dbb7ea22c
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-08-18 10:27:42 +0200

    db/view: add view_building_state and views_state
    `view_building_state` holds mapping of `view_building_task`s for
    tablet-based views. The structure is a memory representation of data
    stored in group0 tables.
    
    `views_state` holds information about tablet-based views and their
    build status.

---
+++ db/view/view_building_state.cc
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ *
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#include "db/view/view_building_state.hh"
+
+namespace db {
+
+namespace view {
+
+view_building_task::view_building_task(utils::UUID id, task_type type, task_state state, table_id base_id, std::optional<table_id> view_id, locator::tablet_replica replica, dht::token last_token)
+        : id(id)
+        , type(type)
+        , state(state)
+        , base_id(base_id)
+        , view_id(view_id)
+        , replica(replica)
+        , last_token(last_token) {}
+
+view_building_state::view_building_state(building_tasks tasks_state, std::optional<table_id> processed_base_table)
+        : tasks_state(std::move(tasks_state))
+        , currently_processed_base_table(std::move(processed_base_table)) {}
+
+views_state::views_state(std::map<table_id, std::vector<table_id>> views_per_base, view_build_status_map status_map)
+        : views_per_base(std::move(views_per_base))
+        , status_map(std::move(status_map)) {}
+
+view_building_task::task_type task_type_from_string(std::string_view str) {
+    if (str == "BUILD_RANGE") {
+        return view_building_task::task_type::build_range;
+    }
+    if (str == "PROCESS_STAGING") {
+        return view_building_task::task_type::process_staging;
+    }
+    throw std::runtime_error(fmt::format("Unknown view building task type: {}", str));
+}
+
+seastar::sstring task_type_to_sstring(view_building_task::task_type type) {
+    switch (type) {
+    case view_building_task::task_type::build_range:
+        return "BUILD_RANGE";
+    case view_building_task::task_type::process_staging:
+        return "PROCESS_STAGING";
+    }
+}
+
+view_building_task::task_state task_state_from_string(std::string_view str) {
+    if (str == "IDLE") {
+        return view_building_task::task_state::idle;
+    }
+    if (str == "STARTED") {
+        return view_building_task::task_state::started;
+    }
+    if (str == "ABORTED") {
+        return view_building_task::task_state::aborted;
+    }
+    throw std::runtime_error(fmt::format("Unknown view building task state: {}", str));
+}
+
+seastar::sstring task_state_to_sstring(view_building_task::task_state state) {
+    switch (state) {
+    case view_building_task::task_state::idle:
+        return "IDLE";
+    case view_building_task::task_state::started:
+        return "STARTED";
+    case view_building_task::task_state::aborted:
+        return "ABORTED";
+    }
+}
+
+}
+
+}
