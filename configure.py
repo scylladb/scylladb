927c7ecbb028a604fe49f0c70f10a973f9e5bc68
Michał Chojnowski <michal.chojnowski@scylladb.com>
2025-08-18 19:02:39 +0200

    test/boost: add bti_index_test
    Adds a fat unit test (integration test?) for BTI index writers and readers.
    
    The test generates a small random dataset for the index writer, writes
    it to a BTI file, and then attempts to run all possible (and legal)
    sequences (up to a certain length) of index reader operations and check
    that the results match the expectation (dictated by a "simple"
    reference index reader).
    
    It is currently the only test for this new feature, but I think it's reasonably
    good. (I validated the coverage by looking at LLVM coverage reports
    and by manually adding bugs in various places and checking that the test
    catches it after a reasonably small number of runs).
    
    (Note that in a later series, when we hook up BTI indexes
    to Data.db readers/writers, the feature will also be indirectly
    tested by the corresponding integration tests).
    
    This is a randomized test. As such, its power grows with the number of runs.
    In particular, a single run has a decently high probability of
    not exercising parts of the code at all. (E.g. the generated dataset
    might have no clustering keys).
    
    Also this is a slow test. (With the default parameters,
    ~1s in release mode on my PC, several seconds in
    debug mode. (And note  that this is after a custom parameter
    downsizing for debug mode, because this test is slowed extremely
    badly by debug mode, due to the forced preemption after every future)).
    
    For those two reasons, I'm not glad to include it in the test suite that runs in CI.
    Instead of running it once in every CI run, I'd very much rather
    have it run 10000 times after the tested feature is touched,
    and before releases. Unfortunately we don't have a precedent for
    something like that.

--- configure.py
+++ configure.py
@@ -565,6 +565,7 @@ scylla_tests = set([
     'test/boost/token_metadata_test',
     'test/boost/top_k_test',
     'test/boost/transport_test',
+    'test/boost/bti_index_test',
     'test/boost/bti_key_translation_test',
     'test/boost/bti_node_sink_test',
     'test/boost/trie_traversal_test',
