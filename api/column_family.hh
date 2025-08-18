7933a68921db46c5a82b44b71a30b776a24294ec
Pavel Emelyanov <xemul@scylladb.com>
2025-08-18 11:43:37 +0300

    api: Drop CF map-reducers ability to work with http context
    This patch finalizes the change started by the previous patch of the
    similar title -- the map_reduce_cf(_raw) is switched to work only with
    sharded<replica::database> reference. All callers were updated by
    previous patches.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- api/column_family.hh
+++ api/column_family.hh
@@ -24,21 +24,13 @@ void unset_column_family(http_context& ctx, httpd::routes& r);
 
 table_info parse_table_info(const sstring& name, const replica::database& db);
 
-namespace {
-inline sharded<replica::database>& db(http_context& ctx) { return ctx.db; }
-inline sharded<replica::database>& db(sharded<replica::database>& db) { return db; }
-
-template <typename T>
-concept CtxOrDb = std::same_as<T, http_context> || std::same_as<T, sharded<replica::database>>;
-}
-
-template<CtxOrDb Ctx, class Mapper, class I, class Reducer>
-future<I> map_reduce_cf_raw(Ctx& ctx, const sstring& name, I init,
+template<class Mapper, class I, class Reducer>
+future<I> map_reduce_cf_raw(sharded<replica::database>& db, const sstring& name, I init,
         Mapper mapper, Reducer reducer) {
-    auto uuid = parse_table_info(name, db(ctx).local()).id;
+    auto uuid = parse_table_info(name, db.local()).id;
     using mapper_type = std::function<future<std::unique_ptr<std::any>>(replica::database&)>;
     using reducer_type = std::function<std::unique_ptr<std::any>(std::unique_ptr<std::any>, std::unique_ptr<std::any>)>;
-    return db(ctx).map_reduce0(mapper_type([mapper, uuid](replica::database& db) {
+    return db.map_reduce0(mapper_type([mapper, uuid](replica::database& db) {
         return futurize_invoke([mapper, &db, uuid] {
             return mapper(db.find_column_family(uuid));
         }).then([] (auto result) {
@@ -52,18 +44,18 @@ future<I> map_reduce_cf_raw(Ctx& ctx, const sstring& name, I init,
 }
 
 
-template<CtxOrDb Ctx, class Mapper, class I, class Reducer>
-future<json::json_return_type> map_reduce_cf(Ctx& ctx, const sstring& name, I init,
+template<class Mapper, class I, class Reducer>
+future<json::json_return_type> map_reduce_cf(sharded<replica::database>& db, const sstring& name, I init,
         Mapper mapper, Reducer reducer) {
-    return map_reduce_cf_raw(ctx, name, init, mapper, reducer).then([](const I& res) {
+    return map_reduce_cf_raw(db, name, init, mapper, reducer).then([](const I& res) {
         return make_ready_future<json::json_return_type>(res);
     });
 }
 
-template<CtxOrDb Ctx, class Mapper, class I, class Reducer, class Result>
-future<json::json_return_type> map_reduce_cf(Ctx& ctx, const sstring& name, I init,
+template<class Mapper, class I, class Reducer, class Result>
+future<json::json_return_type> map_reduce_cf(sharded<replica::database>& db, const sstring& name, I init,
         Mapper mapper, Reducer reducer, Result result) {
-    return map_reduce_cf_raw(ctx, name, init, mapper, reducer).then([result](const I& res) mutable {
+    return map_reduce_cf_raw(db, name, init, mapper, reducer).then([result](const I& res) mutable {
         result = res;
         return make_ready_future<json::json_return_type>(result);
     });
@@ -83,8 +75,8 @@ struct map_reduce_column_families_locally {
     }
 };
 
-template<CtxOrDb Ctx, class Mapper, class I, class Reducer>
-future<I> map_reduce_cf_raw(Ctx& ctx, I init,
+template<class Mapper, class I, class Reducer>
+future<I> map_reduce_cf_raw(sharded<replica::database>& db, I init,
         Mapper mapper, Reducer reducer) {
     using mapper_type = std::function<future<std::unique_ptr<std::any>>(replica::column_family&)>;
     using reducer_type = std::function<std::unique_ptr<std::any>(std::unique_ptr<std::any>, std::unique_ptr<std::any>)>;
@@ -98,17 +90,17 @@ future<I> map_reduce_cf_raw(Ctx& ctx, I init,
     auto wrapped_reducer = reducer_type([reducer = std::move(reducer)] (std::unique_ptr<std::any> a, std::unique_ptr<std::any> b) mutable {
         return std::make_unique<std::any>(I(reducer(std::any_cast<I>(std::move(*a)), std::any_cast<I>(std::move(*b)))));
     });
-    return db(ctx).map_reduce0(map_reduce_column_families_locally{init,
+    return db.map_reduce0(map_reduce_column_families_locally{init,
             std::move(wrapped_mapper), wrapped_reducer}, std::make_unique<std::any>(init), wrapped_reducer).then([] (std::unique_ptr<std::any> res) {
         return std::any_cast<I>(std::move(*res));
     });
 }
 
 
-template<CtxOrDb Ctx, class Mapper, class I, class Reducer>
-future<json::json_return_type> map_reduce_cf(Ctx& ctx, I init,
+template<class Mapper, class I, class Reducer>
+future<json::json_return_type> map_reduce_cf(sharded<replica::database>& db, I init,
         Mapper mapper, Reducer reducer) {
-    return map_reduce_cf_raw(ctx, init, mapper, reducer).then([](const I& res) {
+    return map_reduce_cf_raw(db, init, mapper, reducer).then([](const I& res) {
         return make_ready_future<json::json_return_type>(res);
     });
 }
