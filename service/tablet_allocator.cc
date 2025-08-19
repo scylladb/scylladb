0aa0a0d692b0241936cfd8fce5245e5ad1148891
Michael Litvak <michael.litvak@scylladb.com>
2025-08-19 10:24:38 +0200

    topology_coordinator: repair single table in colocation group
    Previously, if there is a repair request for a table that is colocated
    with other tables, the repair_task_info is written in the shared tablet
    map and the topology coordinator would repair all the tables in the
    colocation group.
    
    We change it to allow repairing a specific table, even if it's colocated
    with other tables.
    
    When repair is requeted, the repair_task_info is written to the
    non-shared, per-table tablet map of the requested table, and the table
    groups transitions to the "repair" stage. Then, the topology coordinator
    picks the single table that was requested and repairs only that table.

--- service/tablet_allocator.cc
+++ service/tablet_allocator.cc
@@ -888,8 +888,7 @@ public:
 
         utils::chunked_vector<repair_plan> plans;
         auto migration_tablet_ids = co_await mplan.get_migration_tablet_ids();
-        for (auto&& [table, tables] : _tm->tablets().all_table_groups()) {
-            const auto& tmap = _tm->tablets().get_tablet_map_view(table);
+        for (auto&& [table, tmap] : _tm->tablets().all_tables_ungrouped()) {
             co_await coroutine::maybe_yield();
             auto& config = tmap.repair_scheduler_config();
             auto now = db_clock::now();
@@ -903,7 +902,8 @@ public:
                 }
 
                 // Skip the tablet that is about to be in transition.
-                if (migration_tablet_ids.contains(gid)) {
+                auto base_gid = locator::global_tablet_id{_tm->tablets().get_base_table(table), id};
+                if (migration_tablet_ids.contains(base_gid)) {
                     co_return;
                 }
 
@@ -933,6 +933,13 @@ public:
                 auto range = tmap.get_token_range(id);
                 auto last_token = tmap.get_last_token(id);
                 plans.push_back(repair_plan{gid, info.shared, range, last_token, diff});
+
+                // don't schedule repairs of colocated tablets together.
+                // currently there are some technical limitations that prevent us from scheduling repairs
+                // of colocated tablets in parallel because the repair transition is shared.
+                // when some tablet is scheduled for repair, the tablet and all its colocated tablets will move to repair transition stage, so
+                // we can't schedule repair for them until the transition is finished.
+                migration_tablet_ids.insert(base_gid);
             });
         }
 
