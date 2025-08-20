20ca43c95bf3ba970c9111afaa93b321705e750f
Andrzej Jackowski <andrzej.jackowski@scylladb.com>
2025-08-20 19:16:25 +0200

    transport: whitespace only change in update_scheduling_group_to_user
    The indentation is changed because it will be required in the next
    commit of this patch series.

--- transport/server.cc
+++ transport/server.cc
@@ -991,11 +991,11 @@ void cql_server::connection::update_scheduling_group_to_driver() {
 }
 
 void cql_server::connection::update_scheduling_group_to_user() {
-    switch_tenant([this] (noncopyable_function<future<> ()> process_loop) -> future<> {
-        auto shg = co_await _server._sl_controller.get_user_scheduling_group(_client_state.user());
-        _current_scheduling_group = shg;
-        co_return co_await _server._sl_controller.with_user_service_level(_client_state.user(), std::move(process_loop));
-    });
+        switch_tenant([this] (noncopyable_function<future<> ()> process_loop) -> future<> {
+            auto shg = co_await _server._sl_controller.get_user_scheduling_group(_client_state.user());
+            _current_scheduling_group = shg;
+            co_return co_await _server._sl_controller.with_user_service_level(_client_state.user(), std::move(process_loop));
+        });
 }
 
 future<std::unique_ptr<cql_server::response>> cql_server::connection::process_auth_response(uint16_t stream, request_reader in, service::client_state& client_state,
