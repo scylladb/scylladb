df1abaf3c4a1b44645daf7d79c0d3cc6f884da13
Piotr Szymaniak <piotr.szymaniak@scylladb.com>
2025-08-21 13:15:53 +0200

    alternator: make number of backlog connections for server sockets configurable
    Add new config variable `alternator_listen_socket_backlog` that is
    subsequently passed as a parameter to calls to listen(). The parameter
    influences HTTP and HTTPS server sockets of Alternator. Before the
    patch, the default Seastar limit of up to 100 backlog connections
    was used.
    The default value (for Alternator) is now 1024.
    
    Fixes: scylladb/scylla-enterprise#5614

--- db/config.cc
+++ db/config.cc
@@ -1394,6 +1394,7 @@ db::config::config(std::shared_ptr<db::extensions> exts)
     , alternator_allow_system_table_write(this, "alternator_allow_system_table_write", liveness::LiveUpdate, value_status::Used,
         false,
         "Allow writing to system tables using the .scylla.alternator.system prefix")
+    , alternator_listen_socket_backlog(this, "alternator_listen_socket_backlog", value_status::Used, 1024, "The max. queue length of pending connections for socket listening to Alternator requests.")
     , vector_store_uri(this, "vector_store_uri", liveness::LiveUpdate, value_status::Used, "", "The URI of the vector store to use for vector search. If not set, vector search is disabled.")
     , abort_on_ebadf(this, "abort_on_ebadf", value_status::Used, true, "Abort the server on incorrect file descriptor access. Throws exception when disabled.")
     , redis_port(this, "redis_port", value_status::Used, 0, "Port on which the REDIS transport listens for clients.")
