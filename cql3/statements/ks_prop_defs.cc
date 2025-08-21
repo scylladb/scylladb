3c31e148c51c6d82e9eb0f1249bf188483488857
Tomasz Grabiec <tgrabiec@scylladb.com>
2025-08-21 14:06:17 +0200

    locator: Add debug logging

--- cql3/statements/ks_prop_defs.cc
+++ cql3/statements/ks_prop_defs.cc
@@ -23,6 +23,8 @@ namespace cql3 {
 
 namespace statements {
 
+static logging::logger logger("ks_prop_defs");
+
 static std::map<sstring, sstring> prepare_options(
         const sstring& strategy_class,
         const locator::token_metadata& tm,
@@ -30,7 +32,11 @@ static std::map<sstring, sstring> prepare_options(
         const std::map<sstring, sstring>& old_options = {}) {
     options.erase(ks_prop_defs::REPLICATION_STRATEGY_CLASS_KEY);
 
-    if (locator::abstract_replication_strategy::to_qualified_class_name(strategy_class) != "org.apache.cassandra.locator.NetworkTopologyStrategy") {
+    auto is_nts = locator::abstract_replication_strategy::to_qualified_class_name(strategy_class) == "org.apache.cassandra.locator.NetworkTopologyStrategy";
+
+    logger.debug("prepare_options: {}: is_nts={} old_options={} new_options={}", strategy_class, is_nts, old_options, options);
+
+    if (!is_nts) {
         return options;
     }
 
