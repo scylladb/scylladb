dfafc8e2510e2b2647e6ab2d0b3cb0e8815ae108
Abhinav Jha <abhinav.jha@scylladb.com>
2025-08-28 11:00:28 +0530

    raft_topology: Modify the conditional logic in remove node operation to enhance concurrency for raft enabled clusters.
    In the current scenario, the shard receiving the remove node REST api request
    performs condional lock depending on whether raft is enabled or not. Since
    non-zero shard returns false for `raft_topology_enabled()`, the requests
    routed to non zero shards are prone to this lock which is unnecessary and
    hampers the ability to perform concurrent operations, which is possible for
    raft enabled nodes.
    
    This pr modifies the conditional lock logic and orchestrates the remove node
    execution logic directly to the shard0, hence the `raft_topology_enabled()` is
    now checked on the shard0 and execution is performed accordingly.
    
    A test is also added to confirm the new behaviour, where concurrent remove
    node operations are now being performed seamlessly.
    
    This pr fixes a bug. Hence we need to backport it.
    
    Fixes: scylladb/scylladb#24737

---
+++ test/cluster/test_concurrent_remove_node_ops.py
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2025-present ScyllaDB
+#
+# SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+#
+
+import asyncio
+import pytest
+
+from test.pylib.manager_client import ManagerClient
+
+
+@pytest.mark.asyncio
+async def test_concurrent_removenode(manager: ManagerClient):
+    await manager.servers_add(5)
+    servers = await manager.running_servers()
+    assert len(servers) >= 5
+
+    await manager.server_stop_gracefully(servers[2].server_id)
+    await manager.server_stop_gracefully(servers[1].server_id)
+
+    await asyncio.gather(*[manager.remove_node(servers[0].server_id, servers[2].server_id),
+            manager.remove_node(servers[0].server_id, servers[1].server_id)])
