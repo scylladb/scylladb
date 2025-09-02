d1f5adebe9beeffdbccb0bb5a4c360dea76c0484
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-09-02 17:39:38 +0300

    s3_client: add `make_request` override with custom retry and error handler
    Introduce an override for `make_request` in `s3_client` to support
    custom retry strategies and error handlers, enabling flexibility
    beyond the default client behavior and improving control over request
    handling

--- utils/s3/client.hh
+++ utils/s3/client.hh
@@ -149,7 +149,14 @@ class client : public enable_shared_from_this<client> {
     group_client& find_or_create_client();
     future<> make_request(http::request req, http::experimental::client::reply_handler handle = ignore_reply, std::optional<http::reply::status_type> expected = std::nullopt, seastar::abort_source* = nullptr);
     using reply_handler_ext = noncopyable_function<future<>(group_client&, const http::reply&, input_stream<char>&& body)>;
+    using error_handler = std::function<void(std::exception_ptr)>;
     future<> make_request(http::request req, reply_handler_ext handle, std::optional<http::reply::status_type> expected = std::nullopt, seastar::abort_source* = nullptr);
+    future<> make_request(http::request req,
+                          reply_handler_ext handle,
+                          const seastar::http::experimental::retry_strategy& rs,
+                          error_handler err_handler,
+                          std::optional<http::reply::status_type> expected = std::nullopt,
+                          seastar::abort_source* = nullptr);
     future<> get_object_header(sstring object_name, http::experimental::client::reply_handler handler, seastar::abort_source* = nullptr);
 public:
 
