5cfa564bc2d638fbffd13bbf66bea89d573d2ee6
Sergey Zolotukhin <sergey.zolotukhin@scylladb.com>
2025-09-04 18:47:59 +0200

    gossiper: add test for a race condition in start_gossiping
    This change adds a test for a race condition in `start_gossiping` that
    can lead to an empty self state sent in `gossip_get_endpoint_states_response`.
    
    Test for scylladb/scylladb#25831

--- gms/gossiper.cc
+++ gms/gossiper.cc
@@ -2090,6 +2090,8 @@ future<> gossiper::start_gossiping(gms::generation_type generation_nbr, applicat
         local_state.add_application_state(entry.first, entry.second);
     }
 
+    co_await utils::get_local_injector().inject("gossiper_publish_local_state_pause", utils::wait_for_message(5min));
+
     co_await replicate(local_state, permit.id());
 
     logger.info("Gossip started with local state: {}", local_state);
