a4aa7c770d0d55b6eb37f1b0cdb81f5e69420cbe
Szymon Malewski <szymon.malewski@scylladb.com>
2025-09-07 01:52:30 +0200

    alternator/expressions: error on parsing empty update expression
    With this patch empty update expression is no longer accepted by the parser.
    So far it was rejected only after resolving, however it could pollute the expression cache.

--- alternator/executor.cc
+++ alternator/executor.cc
@@ -3731,9 +3731,6 @@ update_item_operation::update_item_operation(prepared_expression_cache& expressi
             resolve_update_expression(expr,
                     expression_attribute_names, expression_attribute_values,
                     used_attribute_names, used_attribute_values);
-            if (expr.empty()) {
-                throw api_error::validation("Empty expression in UpdateExpression is not allowed");
-            }
             for (auto& action : expr.actions()) {
                 // Unfortunately we need to copy the action's path, because
                 // we std::move the action object.
