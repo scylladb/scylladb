14dada350a5707a94f47663a921cb4523a2f0edb
Calle Wilund <calle@scylladb.com>
2025-09-08 08:50:20 +0000

    object_storage_endpoint_param: Add gs storage as option

--- db/object_storage_endpoint_param.cc
+++ db/object_storage_endpoint_param.cc
@@ -16,11 +16,16 @@
 #include "utils/s3/creds.hh"
 #include "object_storage_endpoint_param.hh"
 
+using namespace std::string_literals;
+
 db::object_storage_endpoint_param::object_storage_endpoint_param(s3_storage s)
     : _data(std::move(s))
 {}
 db::object_storage_endpoint_param::object_storage_endpoint_param(std::string endpoint, s3::endpoint_config config)
     : object_storage_endpoint_param(s3_storage{std::move(endpoint), std::move(config)})
+{}
+db::object_storage_endpoint_param::object_storage_endpoint_param(gs_storage s)
+    : _data(std::move(s))
 {}
 
 db::object_storage_endpoint_param::object_storage_endpoint_param() = default;
@@ -36,12 +41,40 @@ std::string db::object_storage_endpoint_param::s3_storage::key() const {
     return endpoint;
 }
 
+std::string db::object_storage_endpoint_param::gs_storage::to_json_string() const {
+    return fmt::format("{{ \"type\": \"gs\", \"credentials_file\": \"{}\" }}",
+        credentials_file
+    );
+}
+
+std::string db::object_storage_endpoint_param::gs_storage::key() const {
+    return endpoint;
+}
+
 bool db::object_storage_endpoint_param::is_s3_storage() const {
     return std::holds_alternative<s3_storage>(_data);
 }
 
+bool db::object_storage_endpoint_param::is_gs_storage() const {
+    return std::holds_alternative<gs_storage>(_data);
+}
+
+bool db::object_storage_endpoint_param::is_storage_of_type(std::string_view type) const {
+    if (type == s3_type) {
+        return is_s3_storage();
+    }
+    if (type == gs_type) {
+        return is_gs_storage();
+    }
+    return false;
+}
+
 const db::object_storage_endpoint_param::s3_storage& db::object_storage_endpoint_param::get_s3_storage() const {
     return std::get<db::object_storage_endpoint_param::s3_storage>(_data);
+}
+
+const db::object_storage_endpoint_param::gs_storage& db::object_storage_endpoint_param::get_gs_storage() const {
+    return std::get<db::object_storage_endpoint_param::gs_storage>(_data);
 }
 
 std::strong_ordering db::object_storage_endpoint_param::operator<=>(const object_storage_endpoint_param&) const = default;
@@ -55,13 +88,27 @@ std::string db::object_storage_endpoint_param::key() const {
     return std::visit([](auto& o) { return o.key(); }, _data);
 }
 
+const std::string& db::object_storage_endpoint_param::type() const {
+    if (is_s3_storage()) {
+        return s3_type;
+    } else if (is_gs_storage()) {
+        return gs_type;
+    }
+    throw std::runtime_error("Should not reach");
+}
+
 db::object_storage_endpoint_param db::object_storage_endpoint_param::decode(const YAML::Node& node) {
     auto name = node["name"];
     auto aws_region = node["aws_region"];
     auto iam_role_arn = node["iam_role_arn"];
     auto type = node["type"];
+
+    auto get_opt = [](auto& node, const std::string& key, auto def) {
+        auto tmp = node[key];
+        return tmp ? tmp.template as<std::decay_t<decltype(def)>>() : def;
+    };
     // aws s3 endpoint. 
-    if (!type || type.as<std::string>() == "s3" || aws_region || iam_role_arn) {
+    if (!type || type.as<std::string>() == s3_type || aws_region || iam_role_arn) {
         s3_storage ep;
         ep.endpoint = name.as<std::string>();
         ep.config.port = node["port"].as<unsigned>();
@@ -71,9 +118,20 @@ db::object_storage_endpoint_param db::object_storage_endpoint_param::decode(cons
 
         return object_storage_endpoint_param{std::move(ep)};
     }
+    // GCS endpoint
+    if (type.as<std::string>() == gs_type) {
+        gs_storage ep;
+        ep.endpoint = name.as<std::string>();
+        ep.credentials_file = get_opt(node, "credentials_file", ""s);
+
+        return object_storage_endpoint_param(std::move(ep));
+    }
     // TODO: other types
     throw std::invalid_argument(fmt::format("Could not decode object_storage_endpoint_param: {}", boost::lexical_cast<std::string>(node)));
 }
+
+const std::string db::object_storage_endpoint_param::s3_type = "s3";
+const std::string db::object_storage_endpoint_param::gs_type = "gs";
 
 auto fmt::formatter<db::object_storage_endpoint_param>::format(const db::object_storage_endpoint_param& e, fmt::format_context& ctx) const
     -> decltype(ctx.out()) {
--- db/object_storage_endpoint_param.hh
+++ db/object_storage_endpoint_param.hh
@@ -31,25 +31,42 @@ public:
         std::string to_json_string() const;
         std::string key() const;
     };
+    struct gs_storage {
+        std::string endpoint; // "default"/empty or an URI
+        std::string credentials_file; // optional
+
+        std::strong_ordering operator<=>(const gs_storage&) const = default;
+        std::string to_json_string() const;
+        std::string key() const;
+    };
 
     object_storage_endpoint_param();
     object_storage_endpoint_param(const object_storage_endpoint_param&);
     object_storage_endpoint_param(s3_storage);
     object_storage_endpoint_param(std::string endpoint, s3::endpoint_config config);
+    object_storage_endpoint_param(gs_storage);
 
     std::strong_ordering operator<=>(const object_storage_endpoint_param&) const;
     bool operator==(const object_storage_endpoint_param&) const;
 
     std::string to_json_string() const;
     std::string key() const;
+    const std::string& type() const;
+
     bool is_s3_storage() const;
+    bool is_gs_storage() const;
+
+    bool is_storage_of_type(std::string_view) const;
+
     const s3_storage& get_s3_storage() const;
+    const gs_storage& get_gs_storage() const;
 
     static object_storage_endpoint_param decode(const YAML::Node&);
-
+    static const std::string s3_type; // "s3"
+    static const std::string gs_type; // "gs"
 private:
     friend fmt::formatter<object_storage_endpoint_param>;
-    std::variant<s3_storage> _data;
+    std::variant<s3_storage, gs_storage> _data;
 };
 
 std::istream& operator>>(std::istream& is, object_storage_endpoint_param& f);
