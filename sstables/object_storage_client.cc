527a6df460d82fc07b5be4c61b6aaf93051ffbea
Calle Wilund <calle@scylladb.com>
2025-09-10 13:52:30 +0000

    sstables::object_storage_client: Add google storage implementation
    Allowing GS config to be honoured.

--- sstables/object_storage_client.cc
+++ sstables/object_storage_client.cc
@@ -11,19 +11,24 @@
 
 #include <seastar/core/future.hh>
 #include <seastar/core/semaphore.hh>
+#include <seastar/core/iostream.hh>
 #include <fmt/core.h>
 
 #include "db/object_storage_endpoint_param.hh"
 
 #include "object_storage_client.hh"
 #include "generation_type.hh"
+#include "utils/gcp/gcp_credentials.hh"
+#include "utils/gcp/object_storage.hh"
 #include "utils/s3/client.hh"
 #include "utils/s3/creds.hh"
 #include "utils/memory_data_sink.hh"
 #include "utils/lister.hh"
+#include "utils/io-wrappers.hh"
 
 using namespace seastar;
 using namespace sstables;
+using namespace utils;
 
 
 sstables::object_name::object_name(std::string_view bucket, std::string_view prefix, std::string_view type)
@@ -101,11 +106,143 @@ public:
     }
 };
 
+static shared_ptr<gcp::storage::client> make_gcs_client(const db::object_storage_endpoint_param& ep) {
+    auto& epc = ep.get_gs_storage();
+    auto host = epc.endpoint.empty() || epc.endpoint == "default"
+        ? gcp::storage::client::DEFAULT_ENDPOINT
+        : epc.endpoint
+        ;
+    if (epc.credentials_file == "none") {
+        return seastar::make_shared<gcp::storage::client>(host, std::nullopt);
+    }
+    auto credentials = epc.credentials_file.empty()
+        ? gcp::google_credentials::uninitialized_default_credentials()
+        : gcp::google_credentials::uninitialized_from_file(epc.credentials_file)
+        ;
+    return seastar::make_shared<gcp::storage::client>(host, std::move(credentials));
+}
+
+class gs_client_wrapper : public sstables::object_storage_client {
+    shared_ptr<gcp::storage::client> _client;
+    // TODO: semaphore& _memory;
+    std::function<shared_ptr<gcp::storage::client>()> _shard_client;
+public:
+    gs_client_wrapper(const db::object_storage_endpoint_param& ep, semaphore&, shard_client_factory cf)
+        : _client(make_gcs_client(ep))
+        , _shard_client([cf = std::move(cf), endpoint = ep.key()] {
+            auto lc = cf(endpoint);
+            if (!lc) {
+                throw std::runtime_error(fmt::format("Could not retrive shard client for {}", endpoint));
+            }
+            return dynamic_pointer_cast<gs_client_wrapper>(lc)->_client;
+        })
+    {}
+
+    future<> put_object(object_name name, ::memory_data_sink_buffers bufs, abort_source* as) override {
+        auto sink = _client->create_upload_sink(name.bucket(), name.object(), {}, as);
+        for (auto&& buf : bufs.buffers()) {
+            co_await sink.put(std::move(buf));
+        }
+        co_await sink.flush();
+        co_await sink.close();
+    }
+    future<> delete_object(object_name name) override {
+        return _client->delete_object(name.bucket(), name.object());
+    }
+    file make_readable_file(object_name name, abort_source* as) override {
+        auto src = _client->create_download_source(name.bucket(), name.object(), as);
+        return create_file_for_seekable_source(std::move(src), [scf = _shard_client, name] {
+            return scf()->create_download_source(name.bucket(), name.object());
+        });
+    }
+    data_sink make_data_upload_sink(object_name name, std::optional<unsigned> max_parts_per_piece, abort_source* as) override {
+        return make_upload_sink(std::move(name), as);
+    }
+    data_sink make_upload_sink(object_name name, abort_source* as) override {
+        return _client->create_upload_sink(name.bucket(), name.object(), {}, as);
+    }
+    data_source make_download_source(object_name name, abort_source* as) override {
+        return _client->create_download_source(name.bucket(), name.object(), as);
+    }
+    abstract_lister make_object_lister(std::string bucket, std::string prefix, lister::filter_type filter) override {
+        class list_impl : public abstract_lister::impl {
+            shared_ptr<gcp::storage::client> _client;
+            std::string _bucket, _prefix;
+            lister::filter_type _filter;
+            utils::gcp::storage::bucket_paging _paging;
+            utils::chunked_vector<utils::gcp::storage::object_info> _info;
+            size_t _pos;
+        public:
+            list_impl(shared_ptr<gcp::storage::client> client, std::string bucket, std::string prefix, lister::filter_type filter)
+                : _client(std::move(client))
+                , _bucket(std::move(bucket))
+                , _prefix(std::move(prefix))
+                , _filter(std::move(filter))
+                , _paging(100)
+                , _pos(0)
+            {}
+            future<std::optional<directory_entry>> get() override {
+                std::filesystem::path dir(_prefix);
+                do {
+                    if (_pos == _info.size()) {
+                        _info.clear();
+                        _info = co_await _client->list_objects(_bucket, _prefix, _paging);
+                    }
+                    if (_info.empty()) {
+                        break;
+                    }
+                    auto& item = _info[_pos++];
+                    directory_entry ent{item.name.substr(_prefix.size())};
+                    if (_filter && !_filter(dir, ent)) {
+                        continue;
+                    }
+                    co_return ent;
+                } while (false);
+
+                co_return std::nullopt;
+            }
+            future<> close() noexcept override {
+                co_return;
+            }
+        };
+        return abstract_lister::make<list_impl>(_client, std::move(bucket), std::move(prefix), std::move(filter));
+    }
+    future<> upload_file(std::filesystem::path path, object_name name, utils::upload_progress& up, seastar::abort_source* as) override {
+        auto f = co_await open_file_dma(path.string(), open_flags::ro);
+        auto s = co_await f.stat();
+        up.total += s.st_size;
+        uint64_t pos = 0;
+        auto sink = make_upload_sink(std::move(name), as);
+        for (;;) {
+            auto buf = co_await f.dma_read_bulk<char>(pos, 64*1024);
+            auto n = buf.size();
+            if (n == 0) {
+                break;
+            }
+            co_await sink.put(std::move(buf));
+            up.uploaded += n;
+            pos += n;
+        }
+        co_await sink.flush();
+        co_await sink.close();
+    }
+    future<> update_config(const db::object_storage_endpoint_param& ep) override {
+        auto client = std::exchange(_client, make_gcs_client(ep));
+        co_await client->close();
+    }
+    future<> close() override {
+        return _client->close();
+    }
+};
+
 shared_ptr<object_storage_client> sstables::make_object_storage_client(const db::object_storage_endpoint_param& ep, semaphore& memory, shard_client_factory cf) {
     if (ep.is_s3_storage()) {
         auto& epc = ep.get_s3_storage();
         auto s3_cfg = make_lw_shared<s3::endpoint_config>(epc.config);
         return seastar::make_shared<s3_client_wrapper>(epc.endpoint, std::move(s3_cfg), memory, std::move(cf));
     }
-    throw std::invalid_argument("Not implemented");
+    if (ep.is_gs_storage()) {
+        return seastar::make_shared<gs_client_wrapper>(ep, memory, std::move(cf));
+    }
+    throw std::invalid_argument(fmt::format("Not implemented: {}", ep));
 }
