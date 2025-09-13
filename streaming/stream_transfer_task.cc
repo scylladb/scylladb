378e0f3073c885b71c55bb33826e91ec780338c3
Michał Chojnowski <michal.chojnowski@scylladb.com>
2025-09-13 19:21:35 +0200

    streaming/stream_transfer_task: coroutinize `estimate_partitions()`
    In preparation for making `sstable::estimated_keys_for_range` asynchronous.

--- streaming/stream_transfer_task.cc
+++ streaming/stream_transfer_task.cc
@@ -34,6 +34,7 @@
 #include "gms/feature_service.hh"
 #include "utils/error_injection.hh"
 #include "idl/streaming.dist.hh"
+#include <seastar/coroutine/maybe_yield.hh>
 
 namespace streaming {
 
@@ -101,15 +102,15 @@ struct send_info {
         });
     }
     future<size_t> estimate_partitions() {
-        return do_with(cf->get_sstables(), size_t(0), [this] (auto& sstables, size_t& partition_count) {
-            return do_for_each(*sstables, [this, &partition_count] (auto& sst) {
-                return do_for_each(ranges, [&sst, &partition_count] (auto& range) {
-                    partition_count += sst->estimated_keys_for_range(range);
-                });
-            }).then([&partition_count] {
-                return partition_count;
-            });
-        });
+        auto sstables = cf->get_sstables();
+        size_t partition_count = 0;
+        for (auto& sst : *sstables) {
+            for (auto& range : ranges) {
+                partition_count += sst->estimated_keys_for_range(range);
+            }
+            co_await coroutine::maybe_yield();
+        }
+        co_return partition_count;
     }
 };
 
