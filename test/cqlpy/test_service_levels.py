d30590c1d098d938b1d3911a7afe8663e01de6a4
Andrzej Jackowski <andrzej.jackowski@scylladb.com>
2025-09-16 11:36:36 +0200

    test: add MAX_USER_SERVICE_LEVELS
    Previously, tests used the hardcoded value 7 for the maximum number of
    user service levels. This commit introduces a named variable that can
    be shared across tests to avoid cases where this magic number goes
    out of sync.

--- test/cqlpy/test_service_levels.py
+++ test/cqlpy/test_service_levels.py
@@ -18,6 +18,11 @@ from cassandra.util import Duration
 import pytest
 import time
 
+# MAX_USER_SERVICE_LEVELS represents the maximal number of service levels that users can create.
+# The value is documentented in `docs/features/workload-prioritization.rst`.
+# It's used for regression testing of user service level limit in this and other files.
+MAX_USER_SERVICE_LEVELS = 7
+
 @contextmanager
 def new_service_level(cql, timeout=None, workload_type=None, shares=None, role=None):
     params = ""
@@ -161,7 +166,7 @@ def test_scheduling_groups_limit(scylla_only, cql):
                 created_count = created_count + 1
 
     assert created_count > 0
-    assert created_count == 7 # regression check
+    assert created_count == MAX_USER_SERVICE_LEVELS # regression check
 
 def test_default_shares_in_listings(scylla_only, cql):
     with scylla_inject_error(cql, "create_service_levels_without_default_shares", one_shot=False), \
