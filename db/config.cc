eb9112a4a2357e74d187612b1efba88d95f31acd
Gleb Natapov <gleb@scylladb.com>
2025-09-17 12:49:22 +0300

    db: experimental consistent-tablets option
    The option will be used to hid consistent tablets feature until it is
    ready.

--- db/config.cc
+++ db/config.cc
@@ -1756,7 +1756,8 @@ std::map<sstring, db::experimental_features_t::feature> db::experimental_feature
         {"broadcast-tables", feature::BROADCAST_TABLES},
         {"keyspace-storage-options", feature::KEYSPACE_STORAGE_OPTIONS},
         {"tablets", feature::UNUSED},
-        {"views-with-tablets", feature::UNUSED}
+        {"views-with-tablets", feature::UNUSED},
+        {"strongly-consistent-tables", feature::STRONGLY_CONSISTENT_TABLES}
     };
 }
 
