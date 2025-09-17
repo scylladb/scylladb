667af2d71562a14a4656ffd0d88bf1368c4d65bc
Gleb Natapov <gleb@scylladb.com>
2025-09-17 12:49:22 +0300

    db: experimental consistent-tablets option
    The option will be used to hid consistent tablets feature until it is
    ready.

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -171,6 +171,7 @@ public:
     gms::feature lwt_with_tablets { *this, "LWT_WITH_TABLETS"sv };
     gms::feature repair_msg_split { *this, "REPAIR_MSG_SPLIT"sv };
     gms::feature view_building_coordinator { *this, "VIEW_BUILDING_COORDINATOR"sv };
+    gms::feature strongly_consistent_tables { *this, "STRONGLY_CONSISTENT_TABLES"sv };
 public:
 
     const std::unordered_map<sstring, std::reference_wrapper<feature>>& registered_features() const;
