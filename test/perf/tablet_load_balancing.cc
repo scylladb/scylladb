34cfe4a0171df85e4afcaa7cd9e234edaebe1cc1
Ferenc Szili <ferenc.szili@scylladb.com>
2025-09-17 11:40:37 +0200

    test, simulator: generate tablet sizes and update load_stats
    This change adds a random tablet size generator. The tablet sizes are
    created in load_stats.
    
    Further changes to the load balance simulator:
    
    - apply_plan() updates the load_stats after a migration plan is issued by the
    load balancer,
    
    - adds the option to set a command line option which controls the tablet size
    deviation factor.

--- test/perf/tablet_load_balancing.cc
+++ test/perf/tablet_load_balancing.cc
@@ -113,7 +113,7 @@ future<> apply_resize_plan(token_metadata& tm, const migration_plan& plan) {
 
 // Reflects the plan in a given token metadata as if the migrations were fully executed.
 static
-future<> apply_plan(token_metadata& tm, const migration_plan& plan) {
+future<> apply_plan(token_metadata& tm, const migration_plan& plan, locator::load_stats& load_stats) {
     for (auto&& mig : plan.migrations()) {
         co_await tm.tablets().mutate_tablet_map_async(mig.tablet.table, [&mig] (tablet_map& tmap) {
             auto tinfo = tmap.get_tablet_info(mig.tablet.tablet);
@@ -121,6 +121,32 @@ future<> apply_plan(token_metadata& tm, const migration_plan& plan) {
             tmap.set_tablet(mig.tablet.tablet, tinfo);
             return make_ready_future();
         });
+        // Move tablet size in load_stats to account for the migration
+        if (mig.src.host != mig.dst.host) {
+            if (!load_stats.tablet_stats.contains(mig.src.host)) {
+                throw std::runtime_error(format("Source host not found in load_stats for migration {}", mig));
+            }
+            if (!load_stats.tablet_stats.contains(mig.dst.host)) {
+                throw std::runtime_error(format("Destination host not found in load_stats for migration {}", mig));
+            }
+
+            auto& src_stats = load_stats.tablet_stats.at(mig.src.host);
+            auto& dst_stats = load_stats.tablet_stats.at(mig.dst.host);
+
+            auto& tmap = tm.tablets().get_tablet_map(mig.tablet.table);
+            const range_based_tablet_id rb_tid{mig.tablet.table, tmap.get_token_range(mig.tablet.tablet)};
+
+            if (!src_stats.tablet_sizes.contains(rb_tid.table) || src_stats.tablet_sizes.at(rb_tid.table).contains(rb_tid.range)) {
+                throw std::runtime_error(format("Tablet not found on source load_stats for migration: {}", mig));
+            }
+            if (dst_stats.tablet_sizes.contains(rb_tid.table) && dst_stats.tablet_sizes.at(rb_tid.table).contains(rb_tid.range)) {
+                throw std::runtime_error(format("Tablet already on destination load_stats for migration: {}", mig));
+            }
+
+            const uint64_t tablet_size = src_stats.tablet_sizes.at(rb_tid.table).at(rb_tid.range);
+            dst_stats.tablet_sizes[rb_tid.table][rb_tid.range] = tablet_size;
+            src_stats.tablet_sizes.at(rb_tid.table).erase(rb_tid.range);
+        }
     }
     co_await apply_resize_plan(tm, plan);
 }
@@ -192,7 +218,7 @@ rebalance_stats rebalance_tablets(cql_test_env& e, locator::load_stats& load_sta
             return stats;
         }
         stm.mutate_token_metadata([&] (token_metadata& tm) {
-            return apply_plan(tm, plan);
+            return apply_plan(tm, plan, load_stats);
         }).get();
     }
     throw std::runtime_error("rebalance_tablets(): convergence not reached within limit");
@@ -208,6 +234,7 @@ struct params {
     int shards;
     int scale1 = 1;
     int scale2 = 1;
+    double tablet_size_deviation_factor = 0.5;
 };
 
 struct table_balance {
@@ -252,13 +279,52 @@ struct fmt::formatter<params> : fmt::formatter<string_view> {
     auto format(const params& p, FormatContext& ctx) const {
         auto tablets1_per_shard = double(p.tablets1.value_or(0)) * p.rf1 / (p.nodes * p.shards);
         auto tablets2_per_shard = double(p.tablets2.value_or(0)) * p.rf2 / (p.nodes * p.shards);
-        return fmt::format_to(ctx.out(), "{{iterations={}, nodes={}, tablets1={} ({:0.1f}/sh), tablets2={} ({:0.1f}/sh), rf1={}, rf2={}, shards={}}}",
+        return fmt::format_to(ctx.out(), "{{iterations={}, nodes={}, tablets1={} ({:0.1f}/sh), tablets2={} ({:0.1f}/sh), rf1={}, rf2={}, shards={}, tablet_size_deviation_factor={}}}",
                          p.iterations, p.nodes,
                          p.tablets1.value_or(0), tablets1_per_shard,
                          p.tablets2.value_or(0), tablets2_per_shard,
-                         p.rf1, p.rf2, p.shards);
+                         p.rf1, p.rf2, p.shards, p.tablet_size_deviation_factor);
     }
 };
+
+class tablet_size_generator {
+    std::default_random_engine rnd_engine{std::random_device{}()};
+    std::normal_distribution<> dist;
+public:
+    explicit tablet_size_generator(double deviation_factor)
+        : dist(default_target_tablet_size, default_target_tablet_size * deviation_factor) {
+    }
+
+    uint64_t generate() {
+        // We can't have a negative tablet size, which is why we need to minimize it to 0 (with std::max()).
+        // One consequence of this is that the average generated tablet size will actually
+        // be larger than default_target_tablet_size.
+        // This will be especially pronounced as deviation_factor gets larger. For instance:
+        //
+        // deviation_factor | avg tablet size
+        // -----------------+----------------------------------------
+        //              1   | default_target_tablet_size * 1.08
+        //              1.5 | default_target_tablet_size * 1.22
+        //              2   | default_target_tablet_size * 1.39
+        //              3   | default_target_tablet_size * 1.76
+        return std::max(0.0, dist(rnd_engine));
+    }
+};
+
+void generate_tablet_sizes(double tablet_size_deviation_factor, locator::load_stats& stats, locator::shared_token_metadata& stm) {
+    tablet_size_generator tsg(tablet_size_deviation_factor);
+    for (auto&& [table, tmap] : stm.get()->tablets().all_tables_ungrouped()) {
+        tmap->for_each_tablet([&] (tablet_id tid, const tablet_info& ti) -> future<> {
+            for (const auto& replica : ti.replicas) {
+                const uint64_t tablet_size = tsg.generate();
+                locator::range_based_tablet_id rb_tid {table, tmap->get_token_range(tid)};
+                stats.tablet_stats[replica.host].tablet_sizes[rb_tid.table][rb_tid.range] = tablet_size;
+                testlog.trace("Generated tablet size {} for {}:{}", tablet_size, table, tid);
+            }
+            return make_ready_future<>();
+        }).get();
+    }
+}
 
 future<results> test_load_balancing_with_many_tables(params p, bool tablet_aware) {
     auto cfg = tablet_cql_test_config();
@@ -300,7 +366,9 @@ future<results> test_load_balancing_with_many_tables(params p, bool tablet_aware
         auto add_host = [&] (endpoint_dc_rack dc_rack) {
             auto host = topo.add_node(service::node_state::normal, shard_count, dc_rack);
             hosts.emplace_back(host, dc_rack);
-            stats.capacity[host] = default_target_tablet_size * shard_count;
+            const uint64_t capacity = default_target_tablet_size * shard_count * 100;
+            stats.capacity[host] = capacity;
+            stats.tablet_stats[host].effective_capacity = capacity;
             testlog.info("Added new node: {} / {}:{}", host, dc_rack.dc, dc_rack.rack);
         };
 
@@ -330,6 +398,7 @@ future<results> test_load_balancing_with_many_tables(params p, bool tablet_aware
             topo.set_node_state(host, service::node_state::left);
             testlog.info("Node decommissioned: {}", host);
             hosts.erase(it);
+            stats.tablet_stats.erase(host);
         };
 
         auto ks1 = add_keyspace(e, {{dc1, rf1}}, p.tablets1.value_or(1));
@@ -338,6 +407,8 @@ future<results> test_load_balancing_with_many_tables(params p, bool tablet_aware
         auto id2 = add_table(e, ks2).get();
         schema_ptr s1 = e.local_db().find_schema(id1);
         schema_ptr s2 = e.local_db().find_schema(id2);
+
+        generate_tablet_sizes(p.tablet_size_deviation_factor, stats, stm);
 
         auto check_balance = [&] () -> cluster_balance {
             cluster_balance res;
@@ -425,6 +496,7 @@ void test_parallel_scaleout(const bpo::variables_map& opts) {
     const int nr_racks = opts["racks"].as<int>();
     const int initial_nodes = nr_racks * opts["nodes-per-rack"].as<int>();
     const int extra_nodes = nr_racks * opts["extra-nodes-per-rack"].as<int>();
+    const bool tablet_size_deviation_factor = opts["tablet-size-deviation-factor"].as<double>();
 
     auto cfg = tablet_cql_test_config();
     cfg.db_config->rf_rack_valid_keyspaces(true);
@@ -441,7 +513,9 @@ void test_parallel_scaleout(const bpo::variables_map& opts) {
 
         auto add_host = [&] (endpoint_dc_rack rack) {
             auto host = topo.add_node(service::node_state::normal, shard_count, rack);
-            stats.capacity[host] = default_target_tablet_size * shard_count;
+            const uint64_t capacity = default_target_tablet_size * shard_count * 100;
+            stats.capacity[host] = capacity;
+            stats.tablet_stats[host].effective_capacity = capacity;
             testlog.info("Added new node: {}", host);
         };
 
@@ -458,6 +532,8 @@ void test_parallel_scaleout(const bpo::variables_map& opts) {
         seastar::parallel_for_each(std::views::iota(0, nr_tables), [&] (int) -> future<> {
             return add_table(e, ks1).discard_result();
         }).get();
+
+        generate_tablet_sizes(tablet_size_deviation_factor, stats, e.shared_token_metadata().local());
 
         testlog.info("Initial rebalancing");
         rebalance_tablets(e, stats);
@@ -517,6 +593,8 @@ future<> run_simulations(const boost::program_options::variables_map& app_cfg) {
         auto scale1 = 1 << tests::random::get_int(0, 5);
         auto scale2 = 1 << tests::random::get_int(0, 5);
         auto nodes = tests::random::get_int(rf1 + rf2, 2 *  MAX_RF);
+        // results in a deviation factor of 0.0 - 2.0
+        auto tablet_size_deviation_factor = tests::random::get_int(0, 200) / 100.0;
 
         params p {
             .iterations = app_cfg["iterations"].as<int>(),
@@ -528,6 +606,7 @@ future<> run_simulations(const boost::program_options::variables_map& app_cfg) {
             .shards = shards,
             .scale1 = scale1,
             .scale2 = scale2,
+            .tablet_size_deviation_factor = tablet_size_deviation_factor
         };
 
         auto name = format("#{}", i);
@@ -549,6 +628,7 @@ void run_add_dec(const bpo::variables_map& opts) {
             .rf1 = opts["rf1"].as<int>(),
             .rf2 = opts["rf2"].as<int>(),
             .shards = opts["shards"].as<int>(),
+            .tablet_size_deviation_factor = opts["tablet-size-deviation-factor"].as<double>(),
         };
         run_simulation(p).get();
     }
@@ -572,7 +652,8 @@ const std::map<operation, operation_func> operations_with_func{
             typed_option<int>("rf1", 1, "Replication factor for the first table."),
             typed_option<int>("rf2", 1, "Replication factor for the second table."),
             typed_option<int>("nodes", 3, "Number of nodes in the cluster."),
-            typed_option<int>("shards", 30, "Number of shards per node.")
+            typed_option<int>("shards", 30, "Number of shards per node."),
+            typed_option<double>("tablet-size-deviation-factor", 0.5, "Deviation factor for the tablet size random generator.")
           }
         }, &run_add_dec},
 
@@ -585,7 +666,8 @@ const std::map<operation, operation_func> operations_with_func{
             typed_option<int>("nodes-per-rack", 5, "Number of initial nodes per rack."),
             typed_option<int>("extra-nodes-per-rack", 3, "Number of nodes to add per rack."),
             typed_option<int>("racks", 2, "Number of racks."),
-            typed_option<int>("shards", 88, "Number of shards per node.")
+            typed_option<int>("shards", 88, "Number of shards per node."),
+            typed_option<double>("tablet-size-deviation-factor", 0.5, "Deviation factor for the tablet size random generator.")
           }
         }, &test_parallel_scaleout},
     }
