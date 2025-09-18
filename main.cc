denesb/scylla

commitlog,tombstone_gc: allow to restrict commitlog gc-before check
Add optional rp_set parameter, which when provided, will filter out
segments from the check, which are only referenced by the provided set.
This simulates the discard_completed_segments() done after memtable
flush, without actually modifying the commitlog segments.
This allows getting the min gc before time from commitlog for a
memtable, with the commitlog segments for said memtable removed from the
calculation. Otherwise, the memtable would always block its own
tombstones from GC, as the returned gc before time is always that of the
latest append into the memtable.

main.cc
@@ -1861,16 +1861,16 @@ sharded<locator::shared_token_metadata> token_metadata;
                 auto cl = db.local().commitlog();
                 auto scl = db.local().schema_commitlog();
                 if (cl && scl) {
-                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id) {
-                        return std::min(cl->min_gc_time(id), scl->min_gc_time(id));
+                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id, const db::rp_set* exclude) {
+                        return std::min(cl->min_gc_time(id, exclude), scl->min_gc_time(id, exclude));
                     });
                 } else if (cl) {
-                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id) {
-                        return cl->min_gc_time(id);
+                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id, const db::rp_set* exclude) {
+                        return cl->min_gc_time(id, exclude);
                     });
                 } else if (scl) {
-                    cm.get_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id) {
-                        return scl->min_gc_time(id);
+                    cm.get_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id, const db::rp_set* exclude) {
+                        return scl->min_gc_time(id, exclude);
                     });
                 }
             }).get();

test/boost/row_cache_test.cc
@@ -4387,7 +4387,7 @@ SEASTAR_TEST_CASE(test_populating_cache_with_expired_and_nonexpired_tombstones) 
         schema_ptr s = t.schema();
 
         // emulate commitlog behaivor
-        t.get_compaction_manager().get_tombstone_gc_state().set_gc_time_min_source([s](const table_id& id) {
+        t.get_compaction_manager().get_tombstone_gc_state().set_gc_time_min_source([s](const table_id& id, const db::rp_set*) {
             return gc_clock::now() - (std::chrono::seconds(s->gc_grace_seconds().count() + 600));
         });
 
@@ -4581,7 +4581,7 @@ SEASTAR_TEST_CASE(test_cache_compacts_expired_tombstones_on_read) {
         tombstone_gc_state gc_state(nullptr);
 
         // emulate commitlog behaivor
-        gc_state.set_gc_time_min_source([&s](const table_id& id) {
+        gc_state.set_gc_time_min_source([&s](const table_id& id, const db::rp_set*) {
                 return gc_clock::now() - (std::chrono::seconds(s->gc_grace_seconds().count() + 600));
         });
 

test/lib/cql_test_env.cc
@@ -958,16 +958,16 @@ private:
                 auto cl = _db.local().commitlog();
                 auto scl = _db.local().schema_commitlog();
                 if (cl && scl) {
-                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id) {
-                        return std::min(cl->min_gc_time(id), scl->min_gc_time(id));
+                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id, const db::rp_set* exclude) {
+                        return std::min(cl->min_gc_time(id, exclude), scl->min_gc_time(id, exclude));
                     });
                 } else if (cl) {
-                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id) {
-                        return cl->min_gc_time(id);
+                    cm.get_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id, const db::rp_set* exclude) {
+                        return cl->min_gc_time(id, exclude);
                     });
                 } else if (scl) {
-                    cm.get_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id) {
-                        return scl->min_gc_time(id);
+                    cm.get_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id, const db::rp_set* exclude) {
+                        return scl->min_gc_time(id, exclude);
                     });
                 }
             }).get();

scylladb/scylladb

main.cc
@@ -1890,16 +1890,16 @@ sharded<locator::shared_token_metadata> token_metadata;
                 auto cl = db.local().commitlog();
                 auto scl = db.local().schema_commitlog();
                 if (cl && scl) {
-                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id) {
-                        return std::min(cl->min_gc_time(id), scl->min_gc_time(id));
+                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id, const db::rp_set* exclude) {
+                        return std::min(cl->min_gc_time(id, exclude), scl->min_gc_time(id, exclude));
                     });
                 } else if (cl) {
-                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id) {
-                        return cl->min_gc_time(id);
+                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id, const db::rp_set* exclude) {
+                        return cl->min_gc_time(id, exclude);
                     });
                 } else if (scl) {
-                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id) {
-                        return scl->min_gc_time(id);
+                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id, const db::rp_set* exclude) {
+                        return scl->min_gc_time(id, exclude);
                     });
                 }
             }).get();

test/boost/row_cache_test.cc
@@ -4523,7 +4523,7 @@ SEASTAR_TEST_CASE(test_populating_cache_with_expired_and_nonexpired_tombstones) 
         schema_ptr s = t.schema();
 
         // emulate commitlog behaivor
-        t.get_compaction_manager().get_shared_tombstone_gc_state().set_gc_time_min_source([s](const table_id& id) {
+        t.get_compaction_manager().get_shared_tombstone_gc_state().set_gc_time_min_source([s](const table_id& id, const db::rp_set*) {
             return gc_clock::now() - (std::chrono::seconds(s->gc_grace_seconds().count() + 600));
         });
 
@@ -4718,7 +4718,7 @@ SEASTAR_TEST_CASE(test_cache_compacts_expired_tombstones_on_read) {
         tombstone_gc_state gc_state(gc_shared_state);
 
         // emulate commitlog behaivor
-        gc_shared_state.set_gc_time_min_source([&s](const table_id& id) {
+        gc_shared_state.set_gc_time_min_source([&s](const table_id& id, const db::rp_set*) {
                 return gc_clock::now() - (std::chrono::seconds(s->gc_grace_seconds().count() + 600));
         });
 

test/lib/cql_test_env.cc
@@ -980,16 +980,16 @@ private:
                 auto cl = _db.local().commitlog();
                 auto scl = _db.local().schema_commitlog();
                 if (cl && scl) {
-                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id) {
-                        return std::min(cl->min_gc_time(id), scl->min_gc_time(id));
+                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl, scl](const table_id& id, const db::rp_set* exclude) {
+                        return std::min(cl->min_gc_time(id, exclude), scl->min_gc_time(id, exclude));
                     });
                 } else if (cl) {
-                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id) {
-                        return cl->min_gc_time(id);
+                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([cl](const table_id& id, const db::rp_set* exclude) {
+                        return cl->min_gc_time(id, exclude);
                     });
                 } else if (scl) {
-                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id) {
-                        return scl->min_gc_time(id);
+                    cm.get_shared_tombstone_gc_state().set_gc_time_min_source([scl](const table_id& id, const db::rp_set* exclude) {
+                        return scl->min_gc_time(id, exclude);
                     });
                 }
             }).get();