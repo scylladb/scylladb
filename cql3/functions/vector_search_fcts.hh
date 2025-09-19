93bd4674cbd36a2e2f7387b30aade4b3e2a5459b
Dawid Pawlik <dawid.pawlik@scylladb.com>
2025-09-19 11:06:59 +0200

    cql3/functions: add `vector_similarity()` function placeholder
    This patch introduces a CQL function `vector_similarity()`
    which takes no arguments and should return the float - distance
    from ANN query correlated with the selected row's primary key.
    
    As for now the function is a placeholder and returns the `null`.
    Furthermore there are yet no restrictions introduced for the function
    so that it can be called with every SELECT query.

---
+++ cql3/functions/vector_search_fcts.hh
@@ -0,0 +1,35 @@
+/*
+ * Modified by ScyllaDB
+ *
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: (LicenseRef-ScyllaDB-Source-Available-1.0 and Apache-2.0)
+ */
+
+#pragma once
+
+#include "native_scalar_function.hh"
+
+namespace cql3 {
+namespace functions {
+
+class vector_similarity_fct: public native_scalar_function {
+public:
+    vector_similarity_fct()
+        : native_scalar_function("vector_similarity",
+            float_type, {}) {
+    }
+
+    virtual bool is_pure() const override {
+        return false;
+    }
+
+    bytes_opt execute(std::span<const bytes_opt> parameters) override {
+        return std::nullopt; // Unimplemented
+    }
+};
+
+}
+}
