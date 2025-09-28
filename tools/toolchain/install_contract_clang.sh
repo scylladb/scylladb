58d6d36f1e737376ee7ca8136b27704a6d0220f4
Ran Regev <ran.regev@scylladb.com>
2025-09-28 11:12:29 +0300

    env: docker image
    added a docker file to create an image
    with a contracts supported compiler
    
    To build the image one can run from scylla's root, for example:
    docker image build --network=host -f ./tools/toolchain/Dockerfile.contracts .

---
+++ tools/toolchain/install_contract_clang.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install contracts compiler
+CONTRACT_CMPILER_DEST_DIR=/opt/compiler-explorer/clang-contracts
+if [ ! -d ${CONTRACT_CMPILER_DEST_DIR} ]; then
+    echo "compiler-explorer..."
+    TEMP=$(mktemp -d)
+    pushd "$TEMP" > /dev/null || exit 1
+    INFRADIR=compiler-explorer-infra
+    echo "cloning from github.com/regevran/infra.git until https://github.com/compiler-explorer/infra/issues/1838 is fixed"
+    git clone --branch 1838_skip_missing --depth 1 https://github.com/regevran/infra.git $INFRADIR
+    #git clone --depth 1 https://github.com/compiler-explorer/infra.git $INFRADIR
+    echo compier-explorer-infra cloned to ${TEMP}/$INFRADIR
+    cd $INFRADIR || exit 1
+    make ce
+    ./bin/ce_install --dest ${CONTRACT_CMPILER_DEST_DIR} --enable nightly install compilers/c++/nightly/clang ericwf-contracts-trunk
+    if [ $? -ne 0 ]; then
+        echo "Error: ce_install failed"
+        popd > /dev/null || exit 1
+        exit 1
+    fi
+    echo "compiler-explorer contracts compiler installed to ${CONTRACT_CMPILER_DEST_DIR}"
+    rm -rf "$TEMP"
+    popd > /dev/null || exit 1
+fi
+
