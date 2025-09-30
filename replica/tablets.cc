bhalevy/scylla

tablets: tablet_map_to_mutations: accept process_func
Prepare for generating several mutations for the
tablet_map by calling process_func for each generated mutation.

This allows the caller to directly freeze those mutations
one at a time into a vector of frozen mutations or simililarly
convert them into canonical mutations.

Next patch will split large tablet mutations to prevent stalls.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

replica/tablets.cc
@@ -70,9 +70,9 @@ std::vector<data_value> replicas_to_data_value(const tablet_replica_set& replica
     return result;
 };
 
-future<mutation>
-tablet_map_to_mutation(const tablet_map& tablets, table_id id, const sstring& keyspace_name, const sstring& table_name,
-                       api::timestamp_type ts) {
+future<>
+tablet_map_to_mutations(const tablet_map& tablets, table_id id, const sstring& keyspace_name, const sstring& table_name,
+                       api::timestamp_type ts, std::function<void(mutation)> process_mutation) {
     auto s = db::system_keyspace::tablets();
     auto gc_now = gc_clock::now();
     auto tombstone_ts = ts - 1;
@@ -103,7 +103,7 @@ tablet_map_to_mutation(const tablet_map& tablets, table_id id, const sstring& ke
         tid = *tablets.next_tablet(tid);
         co_await coroutine::maybe_yield();
     }
-    co_return std::move(m);
+    process_mutation(std::move(m));
 }
 
 tablet_mutation_builder&
@@ -191,15 +191,16 @@ tablet_replica_set deserialize_replica_set(cql3::untyped_result_set_row::view_ty
 
 future<> save_tablet_metadata(replica::database& db, const tablet_metadata& tm, api::timestamp_type ts) {
     tablet_logger.trace("Saving tablet metadata: {}", tm);
-    std::vector<mutation> muts;
+    std::vector<frozen_mutation> muts;
     muts.reserve(tm.all_tables().size());
     for (auto&& [id, tablets] : tm.all_tables()) {
         // FIXME: Should we ignore missing tables? Currently doesn't matter because this is only used in tests.
         auto s = db.find_schema(id);
-        muts.emplace_back(
-                co_await tablet_map_to_mutation(tablets, id, s->ks_name(), s->cf_name(), ts));
+        co_await tablet_map_to_mutations(tablets, id, s->ks_name(), s->cf_name(), ts, [&] (mutation m) {
+            muts.emplace_back(freeze(m));
+        });
     }
-    co_await db.apply(freeze(muts), db::no_timeout);
+    co_await db.apply(muts, db::no_timeout);
 }
 
 future<tablet_metadata> read_tablet_metadata(cql3::query_processor& qp) {

scylladb/scylladb

replica/tablets.cc
@@ -117,9 +117,9 @@ data_value repair_scheduler_config_to_data_value(const locator::repair_scheduler
     return result;
 };
 
-future<mutation>
-tablet_map_to_mutation(const tablet_map& tablets, table_id id, const sstring& keyspace_name, const sstring& table_name,
-                       api::timestamp_type ts, const gms::feature_service& features) {
+future<>
+tablet_map_to_mutations(const tablet_map& tablets, table_id id, const sstring& keyspace_name, const sstring& table_name,
+                       api::timestamp_type ts, const gms::feature_service& features, std::function<future<>(mutation)> process_mutation) {
     auto s = db::system_keyspace::tablets();
     auto gc_now = gc_clock::now();
     auto tombstone_ts = ts - 1;
@@ -175,7 +175,7 @@ tablet_map_to_mutation(const tablet_map& tablets, table_id id, const sstring& ke
         tid = *tablets.next_tablet(tid);
         co_await coroutine::maybe_yield();
     }
-    co_return std::move(m);
+    co_await process_mutation(std::move(m));
 }
 
 mutation
@@ -409,14 +409,15 @@ locator::repair_scheduler_config deserialize_repair_scheduler_config(cql3::untyp
 
 future<> save_tablet_metadata(replica::database& db, const tablet_metadata& tm, api::timestamp_type ts) {
     tablet_logger.trace("Saving tablet metadata: {}", tm);
-    utils::chunked_vector<mutation> muts;
+    utils::chunked_vector<frozen_mutation> muts;
     muts.reserve(tm.all_tables_ungrouped().size());
     for (auto&& [base_id, tables] : tm.all_table_groups()) {
         // FIXME: Should we ignore missing tables? Currently doesn't matter because this is only used in tests.
         const auto& tablets = tm.get_tablet_map(base_id);
         auto s = db.find_schema(base_id);
-        muts.emplace_back(
-                co_await tablet_map_to_mutation(tablets, base_id, s->ks_name(), s->cf_name(), ts, db.features()));
+        co_await tablet_map_to_mutations(tablets, base_id, s->ks_name(), s->cf_name(), ts, db.features(), [&] (mutation m) -> future<> {
+            muts.emplace_back(co_await freeze_gently(m));
+        });
         for (auto id : tables) {
             if (id != base_id) {
                 auto s = db.find_schema(id);
@@ -425,7 +426,7 @@ future<> save_tablet_metadata(replica::database& db, const tablet_metadata& tm, 
             }
         }
     }
-    co_await db.apply(freeze(muts), db::no_timeout);
+    co_await db.apply(muts, db::no_timeout);
 }
 
 static table_id to_tablet_metadata_key(const schema& s, const partition_key& key) {