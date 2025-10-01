da36a9d78e0e5ad4f5e88b66d7eadec6a11a1ac7
Calle Wilund <calle@scylladb.com>
2025-10-01 09:59:40 +0000

    boost::gcs_storage_test: reindent
    Remove redundant indentation/moosewings.

--- test/boost/gcp_object_storage_test.cc
+++ test/boost/gcp_object_storage_test.cc
@@ -85,31 +85,29 @@ static future<> create_object_of_size(storage::client& c, std::string_view bucke
 
 static future<> test_read_write_helper(const local_gcs_wrapper& env, size_t dest_size) {
     auto& c = env.client();
-    {
-        auto uuid = fmt::format("{}", utils::UUID_gen::get_time_UUID());
-        std::vector<temporary_buffer<char>> written;
+    auto uuid = fmt::format("{}", utils::UUID_gen::get_time_UUID());
+    std::vector<temporary_buffer<char>> written;
 
-        // ensure we remove the object
-        env.objects_to_delete.emplace_back(uuid);
-        co_await create_object_of_size(c, env.bucket, uuid, dest_size, &written);
+    // ensure we remove the object
+    env.objects_to_delete.emplace_back(uuid);
+    co_await create_object_of_size(c, env.bucket, uuid, dest_size, &written);
 
-        auto source = c.create_download_source(env.bucket, uuid);
-        auto is1 = seastar::input_stream<char>(std::move(source));
-        auto is2 = seastar::input_stream<char>(create_memory_source(std::move(written)));
+    auto source = c.create_download_source(env.bucket, uuid);
+    auto is1 = seastar::input_stream<char>(std::move(source));
+    auto is2 = seastar::input_stream<char>(create_memory_source(std::move(written)));
 
-        uint64_t read = 0;
-        while (!is1.eof()) {
-            auto buf = co_await is1.read();
-            if (buf.empty()) {
-                break;
-            }
-            auto buf2 = co_await is2.read_exactly(buf.size());
-            BOOST_REQUIRE_EQUAL(buf, buf2);
-            read += buf.size();
+    uint64_t read = 0;
+    while (!is1.eof()) {
+        auto buf = co_await is1.read();
+        if (buf.empty()) {
+            break;
         }
-
-        BOOST_REQUIRE_EQUAL(read, dest_size);
+        auto buf2 = co_await is2.read_exactly(buf.size());
+        BOOST_REQUIRE_EQUAL(buf, buf2);
+        read += buf.size();
     }
+
+    BOOST_REQUIRE_EQUAL(read, dest_size);
 }
 
 BOOST_AUTO_TEST_SUITE(gcs_tests, *seastar::testing::async_fixture<gcs_fixture>())
@@ -125,98 +123,92 @@ SEASTAR_FIXTURE_TEST_CASE(test_gcp_storage_create_large_object, local_gcs_wrappe
 SEASTAR_FIXTURE_TEST_CASE(test_gcp_storage_list_objects, local_gcs_wrapper, *check_gcp_storage_test_enabled()) {
     auto& env = *this;
     auto& c = env.client();
-    {
-        std::unordered_map<std::string, uint64_t> names;
-        for (size_t i = 0; i < 10; ++i) {
-            auto name = fmt::format("{}", utils::UUID_gen::get_time_UUID());
-            auto size = tests::random::get_int(size_t(1), size_t(2*1024*1024));
-            env.objects_to_delete.emplace_back(name);
-            co_await create_object_of_size(c, env.bucket, name, size);
-            names.emplace(name, size);
-        }
-
-        auto infos = co_await c.list_objects(env.bucket);
-        size_t n_found = 0;
-
-        for (auto& info : infos) {
-            auto i = names.find(info.name);
-            if (i != names.end()) {
-                BOOST_REQUIRE_EQUAL(info.size, i->second);
-                ++n_found;
-            }
-        }
-        BOOST_REQUIRE_EQUAL(n_found, names.size());
+    std::unordered_map<std::string, uint64_t> names;
+    for (size_t i = 0; i < 10; ++i) {
+        auto name = fmt::format("{}", utils::UUID_gen::get_time_UUID());
+        auto size = tests::random::get_int(size_t(1), size_t(2*1024*1024));
+        env.objects_to_delete.emplace_back(name);
+        co_await create_object_of_size(c, env.bucket, name, size);
+        names.emplace(name, size);
     }
+
+    auto infos = co_await c.list_objects(env.bucket);
+    size_t n_found = 0;
+
+    for (auto& info : infos) {
+        auto i = names.find(info.name);
+        if (i != names.end()) {
+            BOOST_REQUIRE_EQUAL(info.size, i->second);
+            ++n_found;
+        }
+    }
+    BOOST_REQUIRE_EQUAL(n_found, names.size());
 }
 
 SEASTAR_FIXTURE_TEST_CASE(test_gcp_storage_delete_object, local_gcs_wrapper, *check_gcp_storage_test_enabled()) {
     auto& env = *this;
     auto& c = env.client();
+    auto name = fmt::format("{}", utils::UUID_gen::get_time_UUID());
+    env.objects_to_delete.emplace_back(name);
+    co_await create_object_of_size(c, env.bucket, name, 128);
     {
-        auto name = fmt::format("{}", utils::UUID_gen::get_time_UUID());
-        env.objects_to_delete.emplace_back(name);
-        co_await create_object_of_size(c, env.bucket, name, 128);
-        {
-            // validate object was created.
-            auto infos = co_await c.list_objects(env.bucket, name);
-            BOOST_REQUIRE(std::find_if(infos.begin(), infos.end(), [&](auto& info) {
-                return info.name == name;
-            }) != infos.end());
-        }
-
-        co_await c.delete_object(env.bucket, name);
-
+        // validate object was created.
         auto infos = co_await c.list_objects(env.bucket, name);
-        BOOST_REQUIRE(infos.empty());
+        BOOST_REQUIRE(std::find_if(infos.begin(), infos.end(), [&](auto& info) {
+            return info.name == name;
+        }) != infos.end());
     }
+
+    co_await c.delete_object(env.bucket, name);
+
+    auto infos = co_await c.list_objects(env.bucket, name);
+    BOOST_REQUIRE(infos.empty());
 }
 
 SEASTAR_FIXTURE_TEST_CASE(test_gcp_storage_skip_read, local_gcs_wrapper, *check_gcp_storage_test_enabled()) {
     auto& env = *this;
     auto& c = env.client();
-    {
-        auto name = fmt::format("{}", utils::UUID_gen::get_time_UUID());
-        std::vector<temporary_buffer<char>> bufs;
-        constexpr size_t file_size = 12*1024*1024 + 384*7 + 31;
+    auto name = fmt::format("{}", utils::UUID_gen::get_time_UUID());
+    std::vector<temporary_buffer<char>> bufs;
+    constexpr size_t file_size = 12*1024*1024 + 384*7 + 31;
 
-        co_await create_object_of_size(c, env.bucket, name, 12*1024*1024, &bufs);
-        for (size_t i = 0; i < 20; ++i) {
-            auto source = c.create_download_source(env.bucket, name);
-            auto copy = bufs | std::views::transform([](auto& buf) { return buf.share(); });
-            auto is1 = seastar::input_stream<char>(std::move(source));
-            auto is2 = seastar::input_stream<char>(create_memory_source(std::vector<temporary_buffer<char>>(copy.begin(), copy.end())));
+    co_await create_object_of_size(c, env.bucket, name, 12*1024*1024, &bufs);
+    for (size_t i = 0; i < 20; ++i) {
+        auto source = c.create_download_source(env.bucket, name);
+        auto copy = bufs | std::views::transform([](auto& buf) { return buf.share(); });
+        auto is1 = seastar::input_stream<char>(std::move(source));
+        auto is2 = seastar::input_stream<char>(create_memory_source(std::vector<temporary_buffer<char>>(copy.begin(), copy.end())));
 
-            std::exception_ptr p;
-            try {
+        std::exception_ptr p;
+        try {
 
-                size_t pos = 0; 
-                while (pos < file_size) {
-                    auto rem = file_size - pos;
-                    auto skip = tests::random::get_int(std::min(rem, size_t(100)), rem);
-                    auto read = std::min(rem - skip, size_t(tests::random::get_int(31, 2048)));
-                    co_await is1.skip(skip);
-                    co_await is2.skip(skip);
+            size_t pos = 0; 
+            while (pos < file_size) {
+                auto rem = file_size - pos;
+                auto skip = tests::random::get_int(std::min(rem, size_t(100)), rem);
+                auto read = std::min(rem - skip, size_t(tests::random::get_int(31, 2048)));
+                co_await is1.skip(skip);
+                co_await is2.skip(skip);
 
-                    auto b1 = co_await is1.read_exactly(read);
-                    auto b2 = co_await is2.read_exactly(read);
+                auto b1 = co_await is1.read_exactly(read);
+                auto b2 = co_await is2.read_exactly(read);
 
-                    BOOST_REQUIRE_EQUAL(b1.size(), b2.size());
-                    if (b1 != b2) {
-                        BOOST_TEST_MESSAGE(fmt::format("diff at {} ({} bytes)", pos + skip, read));
-                        auto i = std::mismatch(b1.begin(), b1.end(), b2.begin());
-                        BOOST_TEST_MESSAGE(fmt::format("offset {}", std::distance(b1.begin(), i.first)));
-                    }
-                    BOOST_REQUIRE_EQUAL(b1, b2);
-                    pos += (skip + read);
+                BOOST_REQUIRE_EQUAL(b1.size(), b2.size());
+                if (b1 != b2) {
+                    BOOST_TEST_MESSAGE(fmt::format("diff at {} ({} bytes)", pos + skip, read));
+                    auto i = std::mismatch(b1.begin(), b1.end(), b2.begin());
+                    BOOST_TEST_MESSAGE(fmt::format("offset {}", std::distance(b1.begin(), i.first)));
                 }
-            } catch (...) {
-                p = std::current_exception();
+                BOOST_REQUIRE_EQUAL(b1, b2);
+                pos += (skip + read);
             }
-            co_await is1.close();
-            co_await is2.close();
-            if (p) {
-                std::rethrow_exception(p);
-            }
+        } catch (...) {
+            p = std::current_exception();
+        }
+        co_await is1.close();
+        co_await is2.close();
+        if (p) {
+            std::rethrow_exception(p);
         }
     }
 }
