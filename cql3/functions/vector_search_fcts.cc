fb67d7cd71074846458c05c663ea66d334ff0d73
Dawid Pawlik <dawid.pawlik@scylladb.com>
2025-10-02 11:12:59 +0200

    vector_search_fcts: provide proper function arg types
    This patch provides the arg types for similarity functions.
    Newly introduced `provide_arg_types` function checks if
    the provided arguments are vectors of floats and if
    the vector value is of the same type as the vector column.

--- cql3/functions/vector_search_fcts.cc
+++ cql3/functions/vector_search_fcts.cc
@@ -7,9 +7,44 @@
  */
 
 #include "vector_search_fcts.hh"
+#include "cql3/column_identifier.hh"
+#include "types/vector.hh"
 
 namespace cql3 {
 namespace functions {
+
+std::vector<data_type> vector_similarity_fct::provide_arg_types(
+        const function_name& name, const std::vector<shared_ptr<assignment_testable>>& provided_args, const data_dictionary::database& db) {
+    if (provided_args.size() != 2) {
+        throw exceptions::invalid_request_exception(fmt::format("Invalid number of arguments for function {}(vector<float, n>, vector<float, n>)", name));
+    }
+
+    auto vector_column_type_opt = provided_args[0]->assignment_testable_type_opt();
+    if (!vector_column_type_opt) {
+        throw exceptions::invalid_request_exception("Vector similarity functions are only valid when the vector column is known");
+    }
+
+    // The first argument must be a vector<float, N> column.
+    auto vector_column_type = *vector_column_type_opt;
+    if (!vector_column_type->is_vector()) {
+        throw exceptions::invalid_request_exception(fmt::format("Function {} requires a float vector argument, but found argument {} of type {}", name,
+                provided_args[0], vector_column_type->cql3_type_name()));
+    }
+    auto elem_type = dynamic_cast<const vector_type_impl&>(*vector_column_type).get_elements_type();
+    if (elem_type != float_type) {
+        throw exceptions::invalid_request_exception(fmt::format("Function {} requires a float vector argument, but found argument {} of type {}", name,
+                provided_args[0], vector_column_type->cql3_type_name()));
+    }
+
+    // The second argument is a vector literal that should match the given vector column's type.
+    if (!is_assignable(provided_args[1]->test_assignment(
+                db, {}, {}, column_specification({}, {}, ::make_shared<column_identifier>("<vector_literal>", true), vector_column_type)))) {
+
+        throw exceptions::invalid_request_exception(fmt::format("Function {} requires a float vector argument, but found argument {}", name, provided_args[1]));
+    }
+
+    return {vector_column_type, vector_column_type};
+}
 
 bytes_opt vector_similarity_fct::execute(std::span<const bytes_opt> parameters) {
     if (std::any_of(parameters.begin(), parameters.end(), [](const auto& param) {
--- cql3/functions/vector_search_fcts.hh
+++ cql3/functions/vector_search_fcts.hh
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include "cql3/assignment_testable.hh"
 #include "native_scalar_function.hh"
 
 namespace cql3 {
@@ -15,8 +16,8 @@ namespace functions {
 
 class vector_similarity_fct : public native_scalar_function {
 public:
-    vector_similarity_fct(const sstring& name)
-        : native_scalar_function(name, float_type, {}) {
+    vector_similarity_fct(const std::vector<data_type>& arg_types, sstring name)
+        : native_scalar_function(name, float_type, arg_types) {
     }
 
     virtual bool is_pure() const override {
@@ -24,12 +25,15 @@ public:
     }
 
     virtual bytes_opt execute(std::span<const bytes_opt> parameters) override;
+
+    static std::vector<data_type> provide_arg_types(
+            const function_name& name, const std::vector<shared_ptr<assignment_testable>>& provided_args, const data_dictionary::database& db);
 };
 
 class similarity_cosine_fct : public vector_similarity_fct {
 public:
-    similarity_cosine_fct()
-        : vector_similarity_fct("similarity_cosine") {
+    similarity_cosine_fct(const std::vector<data_type>& arg_types)
+        : vector_similarity_fct(arg_types, "similarity_cosine") {
     }
 
     virtual bytes_opt execute(std::span<const bytes_opt> parameters) override;
@@ -38,8 +42,8 @@ public:
 
 class similarity_euclidean_fct : public vector_similarity_fct {
 public:
-    similarity_euclidean_fct()
-        : vector_similarity_fct("similarity_euclidean") {
+    similarity_euclidean_fct(const std::vector<data_type>& arg_types)
+        : vector_similarity_fct( arg_types, "similarity_euclidean") {
     }
 
     virtual bytes_opt execute(std::span<const bytes_opt> parameters) override;
@@ -47,8 +51,8 @@ public:
 
 class similarity_dot_product_fct : public vector_similarity_fct {
 public:
-    similarity_dot_product_fct()
-        : vector_similarity_fct("similarity_dot_product") {
+    similarity_dot_product_fct(const std::vector<data_type>& arg_types)
+        : vector_similarity_fct(arg_types, "similarity_dot_product") {
     }
 
     virtual bytes_opt execute(std::span<const bytes_opt> parameters) override;
