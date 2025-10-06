0294d1aeb01f9ed232e3d040f8fdf4c6d114e073
Karol Nowacki <karol.nowacki@scylladb.com>
2025-10-06 09:41:08 +0200

    vector_search: Move abort_source_timeout to util.hh
    The `abort_source_timeout` function is moved to the shared utility
    header `test/vector_search/util.hh`.
    
    This allows it to be reused by upcoming `node` unit tests, avoiding
    code duplication.

--- test/vector_search/util.hh
+++ test/vector_search/util.hh
@@ -11,9 +11,34 @@
 #include <seastar/core/future.hh>
 #include <seastar/core/coroutine.hh>
 #include <seastar/core/sstring.hh>
+#include <seastar/core/abort_source.hh>
+#include <seastar/core/timer.hh>
 #include <functional>
+#include <chrono>
 
 namespace test::vector_search {
+
+constexpr auto STANDARD_WAIT = std::chrono::seconds(10);
+
+class abort_source_timeout {
+    seastar::abort_source as;
+    seastar::timer<> t;
+
+public:
+    explicit abort_source_timeout(std::chrono::milliseconds timeout = STANDARD_WAIT)
+        : t(seastar::timer([&]() {
+            as.request_abort();
+        })) {
+        t.arm(timeout);
+    }
+
+    seastar::abort_source& reset(std::chrono::milliseconds timeout = STANDARD_WAIT) {
+        t.cancel();
+        as = seastar::abort_source();
+        t.arm(timeout);
+        return as;
+    }
+};
 
 inline seastar::future<> try_on_loopback_address(std::function<seastar::future<>(seastar::sstring)> func) {
     constexpr size_t MAX_LOCALHOST_ADDR_TO_TRY = 127;
@@ -27,5 +52,6 @@ inline seastar::future<> try_on_loopback_address(std::function<seastar::future<>
     }
     throw std::runtime_error("unable to perform action on any 127.0.0.x address");
 }
+
 
 } // namespace test::vector_search
