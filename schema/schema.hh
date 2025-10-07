5a0fddc9eee39b184961a5a2f3e883dac675799d
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-10-07 17:32:07 +0200

    schema: Extract user properties out of raw_schema
    The properties can be directly manipulated by the user
    via statements like `ALTER TABLE`. To better organize
    the structure of `raw_schema`, we encapsulate that data
    in the form of a dedicated struct. This change will be
    later used for applying multiple properties to `schema_builder`
    in one go.

--- schema/schema.hh
+++ schema/schema.hh
@@ -525,32 +525,20 @@ public:
         }
     };
     using extensions_map = std::map<sstring, ::shared_ptr<schema_extension>>;
-private:
-    // More complex fields are derived from these inside rebuild().
-    // Contains only fields which can be safely default-copied.
-    struct raw_schema {
-        raw_schema(table_id id);
-        table_id _id;
-        sstring _ks_name;
-        sstring _cf_name;
-        // regular columns are sorted by name
-        // static columns are sorted by name, but present only when there's any clustering column
-        std::vector<column_definition> _columns;
+
+    /// This structure represents the properties of schema entities that the user
+    /// can directly set or modify via CREATE/ALTER statements.
+    ///
+    /// Note that ID is absent. That's because it cannot be changed.
+    struct user_properties {
         sstring _comment;
         gc_clock::duration _default_time_to_live = gc_clock::duration::zero();
-        data_type _regular_column_name_type;
-        data_type _default_validation_class = bytes_type;
         double _bloom_filter_fp_chance = 0.01;
         compression_parameters _compressor_params;
         extensions_map _extensions;
-        bool _is_dense = false;
-        bool _is_compound = true;
-        bool _is_counter = false;
-        cf_type _type = cf_type::standard;
         int32_t _gc_grace_seconds = DEFAULT_GC_GRACE_SECONDS;
         std::optional<int32_t> _paxos_grace_seconds;
         double _crc_check_chance = 1;
-        db::per_partition_rate_limit_options _per_partition_rate_limit_options;
         int32_t _min_compaction_threshold = DEFAULT_MIN_COMPACTION_THRESHOLD;
         int32_t _max_compaction_threshold = DEFAULT_MAX_COMPACTION_THRESHOLD;
         int32_t _min_index_interval = DEFAULT_MIN_INDEX_INTERVAL;
@@ -562,6 +550,31 @@ private:
         std::map<sstring, sstring> _compaction_strategy_options;
         bool _compaction_enabled = true;
         ::caching_options _caching_options;
+        std::optional<std::map<sstring, sstring>> _tablet_options;
+
+        const cdc::options& get_cdc_options() const;
+        gc_clock::duration get_paxos_grace_seconds() const;
+        const ::tombstone_gc_options& get_tombstone_gc_options() const;
+    };
+
+private:
+    // More complex fields are derived from these inside rebuild().
+    // Contains only fields which can be safely default-copied.
+    struct raw_schema {
+        raw_schema(table_id id);
+        table_id _id;
+        sstring _ks_name;
+        sstring _cf_name;
+        // regular columns are sorted by name
+        // static columns are sorted by name, but present only when there's any clustering column
+        std::vector<column_definition> _columns;
+        data_type _regular_column_name_type;
+        data_type _default_validation_class = bytes_type;
+        bool _is_dense = false;
+        bool _is_compound = true;
+        bool _is_counter = false;
+        cf_type _type = cf_type::standard;
+        db::per_partition_rate_limit_options _per_partition_rate_limit_options;
         table_schema_version _version;
         std::unordered_map<sstring, dropped_column> _dropped_columns;
         std::map<bytes, data_type> _collections;
@@ -571,8 +584,9 @@ private:
         // schema digest. It is also not set locally on a schema tables.
         std::reference_wrapper<const dht::static_sharder> _sharder;
         bool _in_memory = false;
-        std::optional<std::map<sstring, sstring>> _tablet_options;
         std::optional<raw_view_info> _view_info;
+
+        user_properties _props;
     };
     raw_schema _raw;
     schema_static_props _static_props;
@@ -599,7 +613,7 @@ private:
     std::vector<const column_definition*> _all_columns_in_select_order;
 
     extensions_map& extensions() {
-        return _raw._extensions;
+        return _raw._props._extensions;
     }
 
     friend class db::extensions;
@@ -657,13 +671,13 @@ public:
         return _raw._version;
     }
     double bloom_filter_fp_chance() const {
-        return _raw._bloom_filter_fp_chance;
+        return _raw._props._bloom_filter_fp_chance;
     }
     const compression_parameters& get_compressor_params() const {
-        return _raw._compressor_params;
+        return _raw._props._compressor_params;
     }
     const extensions_map& extensions() const {
-        return _raw._extensions;
+        return _raw._props._extensions;
     }
     bool is_dense() const {
         return _raw._is_dense;
@@ -687,7 +701,7 @@ public:
         return _raw._id;
     }
     const sstring& comment() const {
-        return _raw._comment;
+        return _raw._props._comment;
     }
     bool is_counter() const {
         return _raw._is_counter;
@@ -702,66 +716,72 @@ public:
     }
 
     gc_clock::duration gc_grace_seconds() const {
-        auto seconds = std::chrono::seconds(_raw._gc_grace_seconds);
+        auto seconds = std::chrono::seconds(_raw._props._gc_grace_seconds);
         return std::chrono::duration_cast<gc_clock::duration>(seconds);
     }
 
-    gc_clock::duration paxos_grace_seconds() const;
+    gc_clock::duration paxos_grace_seconds() const {
+        return _raw._props.get_paxos_grace_seconds();
+    }
 
     double crc_check_chance() const {
-        return _raw._crc_check_chance;
+        return _raw._props._crc_check_chance;
     }
 
     int32_t min_compaction_threshold() const {
-        return _raw._min_compaction_threshold;
+        return _raw._props._min_compaction_threshold;
     }
 
     int32_t max_compaction_threshold() const {
-        return _raw._max_compaction_threshold;
+        return _raw._props._max_compaction_threshold;
     }
 
     int32_t min_index_interval() const {
-        return _raw._min_index_interval;
+        return _raw._props._min_index_interval;
     }
 
     int32_t max_index_interval() const {
-        return _raw._max_index_interval;
+        return _raw._props._max_index_interval;
     }
 
     int32_t memtable_flush_period() const {
-        return _raw._memtable_flush_period;
+        return _raw._props._memtable_flush_period;
     }
 
     compaction::compaction_strategy_type configured_compaction_strategy() const {
-        return _raw._compaction_strategy;
+        return _raw._props._compaction_strategy;
     }
 
     compaction::compaction_strategy_type compaction_strategy() const {
-        return _raw._compaction_enabled ? _raw._compaction_strategy : compaction::compaction_strategy_type::null;
+        return _raw._props._compaction_enabled ? _raw._props._compaction_strategy : compaction::compaction_strategy_type::null;
     }
 
     const std::map<sstring, sstring>& compaction_strategy_options() const {
-        return _raw._compaction_strategy_options;
+        return _raw._props._compaction_strategy_options;
     }
 
     bool compaction_enabled() const {
-        return _raw._compaction_enabled;
+        return _raw._props._compaction_enabled;
     }
 
-    const cdc::options& cdc_options() const;
+    const cdc::options& cdc_options() const {
+        return _raw._props.get_cdc_options();
+    }
 
-    const ::tombstone_gc_options& tombstone_gc_options() const;
+    const ::tombstone_gc_options& tombstone_gc_options() const {
+        return _raw._props.get_tombstone_gc_options();
+    }
 
     const db::per_partition_rate_limit_options& per_partition_rate_limit_options() const {
         return _raw._per_partition_rate_limit_options;
     }
 
     const ::speculative_retry& speculative_retry() const {
-        return _raw._speculative_retry;
+        return _raw._props._speculative_retry;
     }
 
     const ::caching_options& caching_options() const {
-        return _raw._caching_options;
+        return _raw._props._caching_options;
     }
 
     // Returns true iff the _tablet_options are initialized.
@@ -859,7 +879,7 @@ public:
     }
 
     gc_clock::duration default_time_to_live() const {
-        return _raw._default_time_to_live;
+        return _raw._props._default_time_to_live;
     }
 
     data_type make_legacy_default_validator() const;
--- schema/schema_builder.hh
+++ schema/schema_builder.hh
@@ -78,134 +78,134 @@ public:
         return _raw._cf_name;
     }
     schema_builder& set_comment(const sstring& s) {
-        _raw._comment = s;
+        _raw._props._comment = s;
         return *this;
     }
     const sstring& comment() const {
-        return _raw._comment;
+        return _raw._props._comment;
     }
     schema_builder& set_default_time_to_live(gc_clock::duration t) {
-        _raw._default_time_to_live = t;
+        _raw._props._default_time_to_live = t;
         return *this;
     }
     gc_clock::duration default_time_to_live() const {
-        return _raw._default_time_to_live;
+        return _raw._props._default_time_to_live;
     }
 
     schema_builder& set_gc_grace_seconds(int32_t gc_grace_seconds) {
-        _raw._gc_grace_seconds = gc_grace_seconds;
+        _raw._props._gc_grace_seconds = gc_grace_seconds;
         return *this;
     }
 
     int32_t get_gc_grace_seconds() const {
-        return _raw._gc_grace_seconds;
+        return _raw._props._gc_grace_seconds;
     }
 
     schema_builder& set_paxos_grace_seconds(int32_t seconds);
 
     schema_builder& set_crc_check_chance(double chance) {
-        _raw._crc_check_chance = chance;
+        _raw._props._crc_check_chance = chance;
         return *this;
     }
 
     double get_crc_check_chance() const {
-        return _raw._crc_check_chance;
+        return _raw._props._crc_check_chance;
     }
 
     schema_builder& set_min_compaction_threshold(int32_t t) {
-        _raw._min_compaction_threshold = t;
+        _raw._props._min_compaction_threshold = t;
         return *this;
     }
 
     int32_t get_min_compaction_threshold() const {
-        return _raw._min_compaction_threshold;
+        return _raw._props._min_compaction_threshold;
     }
 
     schema_builder& set_max_compaction_threshold(int32_t t) {
-        _raw._max_compaction_threshold = t;
+        _raw._props._max_compaction_threshold = t;
         return *this;
     }
 
     int32_t get_max_compaction_threshold() const {
-        return _raw._max_compaction_threshold;
+        return _raw._props._max_compaction_threshold;
     }
 
     schema_builder& set_compaction_enabled(bool enabled) {
-        _raw._compaction_enabled = enabled;
+        _raw._props._compaction_enabled = enabled;
         return *this;
     }
 
     bool compaction_enabled() const {
-        return _raw._compaction_enabled;
+        return _raw._props._compaction_enabled;
     }
 
     schema_builder& set_min_index_interval(int32_t t) {
-        _raw._min_index_interval = t;
+        _raw._props._min_index_interval = t;
         return *this;
     }
 
     int32_t get_min_index_interval() const {
-        return _raw._min_index_interval;
+        return _raw._props._min_index_interval;
     }
 
     schema_builder& set_max_index_interval(int32_t t) {
-        _raw._max_index_interval = t;
+        _raw._props._max_index_interval = t;
         return *this;
     }
 
     int32_t get_max_index_interval() const {
-        return _raw._max_index_interval;
+        return _raw._props._max_index_interval;
     }
 
     schema_builder& set_memtable_flush_period(int32_t t) {
-        _raw._memtable_flush_period = t;
+        _raw._props._memtable_flush_period = t;
         return *this;
     }
 
     int32_t get_memtable_flush_period() const {
-        return _raw._memtable_flush_period;
+        return _raw._props._memtable_flush_period;
     }
 
     schema_builder& set_speculative_retry(sstring retry_sstring) {
-        _raw._speculative_retry = speculative_retry::from_sstring(retry_sstring);
+        _raw._props._speculative_retry = speculative_retry::from_sstring(retry_sstring);
         return *this;
     }
 
     const speculative_retry& get_speculative_retry() const {
-        return _raw._speculative_retry;
+        return _raw._props._speculative_retry;
     }
 
     schema_builder& set_bloom_filter_fp_chance(double fp) {
-        _raw._bloom_filter_fp_chance = fp;
+        _raw._props._bloom_filter_fp_chance = fp;
         return *this;
     }
     double get_bloom_filter_fp_chance() const {
-        return _raw._bloom_filter_fp_chance;
+        return _raw._props._bloom_filter_fp_chance;
     }
     schema_builder& set_compressor_params(const compression_parameters& cp) {
-        _raw._compressor_params = cp;
+        _raw._props._compressor_params = cp;
         return *this;
     }
     schema_builder& set_extensions(schema::extensions_map exts) {
-        _raw._extensions = std::move(exts);
+        _raw._props._extensions = std::move(exts);
         return *this;
     }
     schema_builder& add_extension(const sstring& name, ::shared_ptr<schema_extension> ext) {
-        _raw._extensions[name] = std::move(ext);
+        _raw._props._extensions[name] = std::move(ext);
         return *this;
     }
     const schema::extensions_map& get_extensions() const {
-        return _raw._extensions;
+        return _raw._props._extensions;
     }
     schema_builder& set_compaction_strategy(compaction::compaction_strategy_type type) {
-        _raw._compaction_strategy = type;
+        _raw._props._compaction_strategy = type;
         return *this;
     }
 
     schema_builder& set_compaction_strategy_options(std::map<sstring, sstring>&& options);
 
     schema_builder& set_caching_options(caching_options c) {
-        _raw._caching_options = std::move(c);
+        _raw._props._caching_options = std::move(c);
         return *this;
     }
 
