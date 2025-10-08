67c4d980dd29b2a95b6b9bb010107ef786282540
Avi Kivity <avi@scylladb.com>
2025-10-08 21:44:37 +0300

    Revert "Merge 'auth: move passwords::check call to alien thread' from Andrzej Jackowski"
    This reverts commit 1fd82d32e09df55d8bdcb91b66be330e61d89488. It causes
    connection storms to snowball into a node crash via this mechanism:
    
    1. large node suffers mild connection storm
    2. password hash requests queue up on alien hash thread
    3. incoming hash requests queue faster than the alien thread can retire them.
    4. auth latency grows without bounds
    5. this encourages the clients to create new connections
    6. problem grows
    
    Reverting the patch restores the hash stall, but at least prevents node
    crashes.
    
    Fixes #26461 (2025.1)
    
    Closes scylladb/scylladb#26462

--- utils/alien_worker.cc
+++ utils/alien_worker.cc
@@ -13,24 +13,14 @@ using namespace seastar;
 
 namespace utils {
 
-std::thread alien_worker::spawn(seastar::logger& log, int niceness, const seastar::sstring& name_suffix) {
+std::thread alien_worker::spawn(seastar::logger& log, int niceness) {
     sigset_t newset;
     sigset_t oldset;
     sigfillset(&newset);
     auto r = ::pthread_sigmask(SIG_SETMASK, &newset, &oldset);
     assert(r == 0);
-    auto thread_name = fmt::format("alien-{}", name_suffix);
-    if (thread_name.size() > 15) {
-        log.warn("Thread name '{}' is longer than 15 characters, truncating to fit", thread_name);
-        thread_name.resize(15); // pthread_setname_np requires name to be <= 15 characters
-    }
-    auto thread = std::thread([this, &log, niceness, thread_name] () noexcept {
+    auto thread = std::thread([this, &log, niceness] () noexcept {
         errno = 0;
-        int setname_value = pthread_setname_np(pthread_self(), thread_name.c_str());
-        if (setname_value != 0) {
-            log.error("Unable to set worker thread name '{}', setname_value={}", thread_name, setname_value);
-            std::abort();
-        }
         int nice_value = nice(niceness);
         if (nice_value == -1 && errno != 0) {
             log.warn("Unable to renice worker thread (system error number {}); the thread will compete with reactor, which can cause latency spikes. Try adding CAP_SYS_NICE", errno);
@@ -53,8 +43,8 @@ std::thread alien_worker::spawn(seastar::logger& log, int niceness, const seasta
     return thread;
 }
 
-alien_worker::alien_worker(seastar::logger& log, int niceness, const seastar::sstring& name_suffix)
-    : _thread(spawn(log, niceness, name_suffix))
+alien_worker::alien_worker(seastar::logger& log, int niceness)
+    : _thread(spawn(log, niceness))
 {}
 
 alien_worker::~alien_worker() {
