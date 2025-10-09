89b3d0c582a6cbdd8e385052fc1d4132b04aa6bb
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-10-09 15:53:29 +0200

    cql3/statements/index_prop_defs: Mark methods as const-qualified

--- cql3/statements/index_prop_defs.cc
+++ cql3/statements/index_prop_defs.cc
@@ -22,7 +22,7 @@ void check_system_option_specified(const index_options_map& options, const sstri
     }
 }
 
-void cql3::statements::index_prop_defs::validate() {
+void cql3::statements::index_prop_defs::validate() const {
     static std::set<sstring> keywords({ sstring(KW_OPTIONS) });
 
     property_definitions::validate(keywords);
@@ -41,13 +41,13 @@ void cql3::statements::index_prop_defs::validate() {
 }
 
 index_options_map
-cql3::statements::index_prop_defs::get_raw_options() {
+cql3::statements::index_prop_defs::get_raw_options() const {
     auto options = get_map(KW_OPTIONS);
     return !options ? std::unordered_map<sstring, sstring>() : std::unordered_map<sstring, sstring>(options->begin(), options->end());
 }
 
 index_options_map
-cql3::statements::index_prop_defs::get_options() {
+cql3::statements::index_prop_defs::get_options() const {
     auto options = get_raw_options();
     options.emplace(db::index::secondary_index::custom_class_option_name, *custom_class);
     if (index_version.has_value()) {
