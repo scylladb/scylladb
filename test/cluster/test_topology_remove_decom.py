989b0a5da259cabc7db1bbc1bfdcdbae870218e2
Andrei Chekun <andrei.chekun@scylladb.com>
2025-10-09 14:22:01 +0200

    test.py: convert skip_mode function to pytest.mark
    Function skip_mode works only on function and only in cluster test. This if OK when we need to skip one test, but it's not possible to use it with pytestmark to automatically mark all tests in the file. The goal of this PR is to migrate skip_mode to be dynamic pytest.mark that can be used as ordinary mark.

--- test/cluster/test_topology_remove_decom.py
+++ test/cluster/test_topology_remove_decom.py
@@ -11,7 +11,6 @@ import asyncio
 import random
 import time
 from test import pylib
-from test.cluster.conftest import skip_mode
 from test.pylib.util import wait_for
 from test.pylib.manager_client import ManagerClient
 from test.pylib.random_tables import RandomTables
@@ -195,7 +194,7 @@ async def test_concurrent_removenode_two_initiators_two_dead_nodes(manager: Mana
             manager.remove_node(servers[3].server_id, servers[1].server_id, ignore_dead=ignore_nodes)])
 
 @pytest.mark.asyncio
-@skip_mode('release', 'error injection is not supported in release mode')
+@pytest.mark.skip_mode(mode='release', reason='error injection is not supported in release mode')
 async def test_decommission_left_token_ring_retry(manager: ManagerClient):
     """
     Tests the execution flow in case of performing decommission node
