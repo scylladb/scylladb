d301b88919e308807d5217406ea24b80f281cbd8
Ran Regev <ran.regev@scylladb.com>
2025-10-09 14:32:29 +0300

    wip

--- dbuild_configure_scylla_with_clang.sh
+++ dbuild_configure_scylla_with_clang.sh
@@ -2,6 +2,6 @@
 
 ./tools/toolchain/dbuild ./configure.py \
     --mode=dev \
-    --compiler /opt/compiler-explorer/clang-contracts/clang-ericwf-contracts-trunk/bin/clang++ \
+    --compiler "ccache /opt/compiler-explorer/clang-contracts/clang-ericwf-contracts-trunk/bin/clang++" \
     --cflags="--config=${PWD}/tools/toolchain/contracts_compiler.cfg" \
     --ldflags="-rpath /opt/compiler-explorer/clang-contracts/clang-ericwf-contracts-trunk/lib/x86_64-unknown-linux-gnu/ -L/opt/compiler-explorer/clang-contracts/clang-ericwf-contracts-trunk/lib/x86_64-unknown-linux-gnu/ -lc++" 
