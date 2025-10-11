4341082e6da301f9a12ac2ecd03dc8c6ff440167
Ran Regev <ran.regev@scylladb.com>
2025-10-11 16:19:45 +0300

    trying clang 22

--- tools/toolchain/install_clang_latest.sh
+++ tools/toolchain/install_clang_latest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install contracts compiler
-CONTRACT_CMPILER_DEST_DIR=/opt/compiler-explorer/clang-contracts
-if [ ! -d ${CONTRACT_CMPILER_DEST_DIR} ]; then
+COMPILER_DEST_DIR=/opt/compiler-explorer/clang-latest
+if [ ! -d ${COMPILER_DEST_DIR} ]; then
     echo "compiler-explorer..."
     TEMP=$(mktemp -d)
     pushd "$TEMP" > /dev/null || exit 1
@@ -11,13 +11,13 @@ if [ ! -d ${CONTRACT_CMPILER_DEST_DIR} ]; then
     echo compier-explorer-infra cloned to ${TEMP}/$INFRADIR
     cd $INFRADIR || exit 1
     make ce
-    ./bin/ce_install --dest ${CONTRACT_CMPILER_DEST_DIR} --enable nightly install compilers/c++/nightly/clang ericwf-contracts-trunk
+    ./bin/ce_install --dest ${COMPILER_DEST_DIR} --enable nightly install compilers/c++/nightly/clang trunk
     if [ $? -ne 0 ]; then
         echo "Error: ce_install failed"
         popd > /dev/null || exit 1
         exit 1
     fi
-    echo "compiler-explorer contracts compiler installed to ${CONTRACT_CMPILER_DEST_DIR}"
+    echo "compiler-explorer latest compiler installed to ${COMPILER_DEST_DIR}"
     rm -rf "$TEMP"
     popd > /dev/null || exit 1
 fi
