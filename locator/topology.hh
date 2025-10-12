7ea70e4e0b783bcb4319cbd5bed2579743aa3ea8
Michael Litvak <michael.litvak@scylladb.com>
2025-10-12 12:18:54 +0200

    locator: fix rf-rack validation during node join/remove
    If a keyspace is created while a node is joining or being removed, it could
    break the rf-rack invariant. For example:
    1. We have 3 nodes in 3 racks, no keyspaces
    2. A new node starts to join in a new rack - passes validation because
       there are no keyspaces
    3. Create a keyspace with rf=3 - passes validation because the joining
       node is not a normal token owner yet
    4. The new node becomes a normal token owner
    5. The rf-rack invariant is broken. We have rf=3 and 4 racks
    
    To fix this, we change the rf-rack check to consider a node as a token
    owner if it's either a normal token owner or it has bootstrap tokens and
    is about to become a normal token owner.
    
    Now the condition can't be broken. Consider keyspace creation at
    different stages of adding a node in our example:
    * Before the node is assigned bootstrap tokens: the node is not
      considered. We can create a keyspace with rf=3 as if the node doesn't
      exist, and then node join will fail in the group0 operation that
      assigns bootstrap tokens, because during this operation we check
      rf-rack validity.
    * Assigning bootstrap tokens is a single group0 operation that is
      serialized with keyspace creation. During this operation we check that
      adding the node as a token owner will maintain rf-rack validity for all
      keyspaces.
    * After the node is assigned bootstrap tokens and until it becomes a
      normal token owner: it is considered as a transitioning token owner by
      the rf-rack check and the rack is considered a transitioning rack. We
      can't count the rack as a normal rack because the node join may still
      fail and rollback. Trying to create a keyspace with either rf=3 or
      rf=4 will fail because we can end up with either 3 or 4 racks.
    
    Similarly, when removing a node, we validate that removing the node will
    maintain rf-rack validity in the same group0 operation that changes the
    node state to removing/decommissioning, after which the node becomes a
    leaving endpoint, and it's not considered a normal token owner anymore
    for the rf-rack check.

--- locator/topology.hh
+++ locator/topology.hh
@@ -124,6 +124,10 @@ public:
         }
     }
 
+    bool is_bootstrapping() const noexcept {
+        return _state == state::bootstrapping;
+    }
+
     bool is_normal() const noexcept {
         return _state == state::normal;
     }
