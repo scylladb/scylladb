ffc9a02de519100b4463a19745ea27cc96f84ca2
Dawid Pawlik <dawid.pawlik@scylladb.com>
2025-10-13 12:57:12 +0200

    cql3/statements/primary_key: make `primary_key` comparable
    Refactor the primary key struct to be a class.
    Add `<` operator to make it possible to use `primary_key` object
    in a sorted container (e.g. `std::map`).
    
    The change is motivited by the need of finding a certain key while
    matching the current rows primary key with `ann_results`.
    Using map, the effective complexity is reduced to O(nlogn).

--- cql3/statements/primary_key.hh
+++ cql3/statements/primary_key.hh
@@ -16,9 +16,26 @@ namespace cql3 {
 namespace statements {
 
 /// Encapsulates a partition key and clustering key prefix as a primary key.
-struct primary_key {
+class primary_key {
+public:
     dht::decorated_key partition;
     clustering_key_prefix clustering;
+
+    primary_key(const dht::decorated_key& partition, const clustering_key_prefix& clustering)
+        : partition(partition), clustering(clustering) {}
+
+    bool operator<(const primary_key& other) const {
+        auto partition_key = partition.key().explode();
+        auto other_partition_key = other.partition.key().explode();
+
+        if (partition_key < other_partition_key) {
+            return true;
+        }
+        if (other_partition_key < partition_key) {
+            return false;
+        }
+        return clustering.explode() < other.clustering.explode();
+    }
 };
 
 } // namespace statements
