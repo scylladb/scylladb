3851ee58d7591a04d7671bc2da31b56ce19a15c9
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-10-23 14:17:51 +0300

    retry_strategy: add exponential backoff to `default_aws_retry_strategy`
    Add exponential backoff to `default_aws_retry_strategy` and call it to `sleep` before returning `true`, no-op in case of non-retryable error

--- utils/s3/default_aws_retry_strategy.cc
+++ utils/s3/default_aws_retry_strategy.cc
@@ -8,6 +8,7 @@
 
 #include "default_aws_retry_strategy.hh"
 #include "aws_error.hh"
+#include "seastar/core/sleep.hh"
 #include "seastar/http/exception.hh"
 #include "seastar/util/short_streams.hh"
 #include "utils/log.hh"
@@ -21,6 +22,14 @@ using namespace seastar::http::experimental;
 
 namespace aws {
 
+static seastar::future<> sleep_before_retry(size_t attempted_retries) {
+    if (attempted_retries == 0) {
+        return seastar::make_ready_future();
+    }
+    constexpr size_t scale_factor = 25;
+    return seastar::sleep(std::chrono::milliseconds((1UL << attempted_retries) * scale_factor));
+}
+
 default_aws_retry_strategy::default_aws_retry_strategy(unsigned max_retries) : _max_retries(max_retries) {
 }
 
@@ -33,6 +42,7 @@ seastar::future<bool> default_aws_retry_strategy::should_retry(std::exception_pt
     bool should_retry = err.is_retryable() == retryable::yes;
     if (should_retry) {
         rs_logger.debug("AWS HTTP client request failed. Reason: {}. Retry# {}", err.get_error_message(), attempted_retries);
+        co_await sleep_before_retry(attempted_retries);
     } else {
         rs_logger.warn("AWS HTTP client encountered non-retryable error. Reason: {}. Code: {}. Retry# {}",
                        err.get_error_message(),
