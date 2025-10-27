316e702d45548a557cc6935e6b02f990c47495ab
Raphael S. Carvalho <raphaelsc@scylladb.com>
2025-10-27 20:34:25 -0300

    sstables_loader: Don't bypass synchronization with busy topology
    The patch c543059f86 fixed the synchronization issue between tablet
    split and load-and-stream. The synchronization worked only with
    raft topology, and therefore was disabled with gossip.
    To do the check, storage_service::raft_topology_change_enabled()
    but the topology kind is only available/set on shard 0, so it caused
    the synchronization to be bypassed when load-and-stream runs on
    any shard other than 0.
    
    The reason the reproducer didn't catch it is that it was restricted
    to single cpu. It will now run with multi cpu and catch the
    problem observed.
    
    Fixes #22707
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>
    
    Closes scylladb/scylladb#26730
    
    (cherry picked from commit 7f34366b9ded7cf0af006e835f4db4a5e07a7600)
    (cherry picked from commit 4c466ace4fced32e667bc487c8d95c321e2b0356)
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -7904,6 +7904,14 @@ void storage_service::set_topology_change_kind(topology_change_kind kind) {
     _gossiper.set_topology_state_machine(kind == topology_change_kind::raft ? & _topology_state_machine : nullptr);
 }
 
+bool storage_service::raft_topology_change_enabled() const {
+    return _topology_change_kind_enabled == topology_change_kind::raft;
+}
+
+bool storage_service::legacy_topology_change_enabled() const {
+    return _topology_change_kind_enabled == topology_change_kind::legacy;
+}
+
 future<> storage_service::register_protocol_server(protocol_server& server, bool start_instantly) {
     _protocol_servers.push_back(&server);
     if (start_instantly) {
