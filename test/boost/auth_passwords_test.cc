b56c27efa67a5fd8eee67d45973dfdf4de20ae66
Andrzej Jackowski <andrzej.jackowski@scylladb.com>
2025-11-03 09:50:23 +0100

    test/boost: coroutinize auth_passwords_test
    This commit prepares `auth_passwords_test` for using coroutines,
    because later in this patch series `auth::passwords::check` and other
    similar functions will return Seastar futures.
    
    Refs: scylladb/scylladb#26859

--- test/boost/auth_passwords_test.cc
+++ test/boost/auth_passwords_test.cc
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
-#define BOOST_TEST_MODULE core
+#include <seastar/testing/test_case.hh>
 
 #include <array>
 #include <random>
@@ -16,15 +16,21 @@
 
 #include <boost/test/unit_test.hpp>
 #include <seastar/core/sstring.hh>
+#include <seastar/core/coroutine.hh>
 
 #include "seastarx.hh"
+
+extern "C" {
+#include <crypt.h>
+#include <unistd.h>
+}
 
 static auto rng_for_salt = std::default_random_engine(std::random_device{}());
 
 //
 // The same password hashed multiple times will result in different strings because the salt will be different.
 //
-BOOST_AUTO_TEST_CASE(passwords_are_salted) {
+SEASTAR_TEST_CASE(passwords_are_salted) {
     const char* const cleartext = "my_excellent_password";
     std::unordered_set<sstring> observed_passwords{};
 
@@ -33,12 +39,13 @@ BOOST_AUTO_TEST_CASE(passwords_are_salted) {
         BOOST_REQUIRE(!observed_passwords.contains(e));
         observed_passwords.insert(e);
     }
+    co_return;
 }
 
 //
 // A hashed password will authenticate against the same password in cleartext.
 //
-BOOST_AUTO_TEST_CASE(correct_passwords_authenticate) {
+SEASTAR_TEST_CASE(correct_passwords_authenticate) {
     // Common passwords.
     std::array<const char*, 3> passwords{
         "12345",
@@ -49,12 +56,14 @@ BOOST_AUTO_TEST_CASE(correct_passwords_authenticate) {
     for (const char* p : passwords) {
         BOOST_REQUIRE(auth::passwords::check(p, auth::passwords::hash(p, rng_for_salt, auth::passwords::scheme::sha_512)));
     }
+    co_return;
 }
 
 //
 // A hashed password that does not match the password in cleartext does not authenticate.
 //
-BOOST_AUTO_TEST_CASE(incorrect_passwords_do_not_authenticate) {
+SEASTAR_TEST_CASE(incorrect_passwords_do_not_authenticate) {
     const sstring hashed_password = auth::passwords::hash("actual_password", rng_for_salt,auth::passwords::scheme::sha_512);
     BOOST_REQUIRE(!auth::passwords::check("password_guess", hashed_password));
+    co_return;
 }
