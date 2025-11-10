c0f7622d12c13b34b24186c05abdb916e70872ce
Dawid Mędrek <dawid.medrek@scylladb.com>
2025-11-10 18:41:35 +0100

    service/qos: Do not crash Scylla if auth_integration absent
    If the user connects to Scylla via the maintenance socket, it may happen
    that `auth_integration` has not been registered in the service level
    controller yet. One example is maintenance mode when that will never
    happen; another when the connection occurs before Scylla is fully
    initialized.
    
    To avoid unnecessary crashes, we add new branches if the passed user is
    absent or if it corresponds to the anonymous role. Since the role
    corresponding to a connection via the maintenance socket is the anonymous
    role, that solves the problem.
    
    In those cases, we completely circumvent any calls to `auth_integration`
    and handle them separately. The modified methods are:
    
    * `get_user_scheduling_group`,
    * `with_user_service_level`,
    * `describe_service_levels`.
    
    For the first two, the new behavior is in line with the previous
    implementation of those functions. The last behaves differently now,
    but since it's a soft error, crashing the node is not necessary anyway.
    We throw an exception instead, whose error message should give the user
    a hint of what might be wrong.
    
    The other uses of `auth_integration` within the service level controller
    are not problematic:
    
    * `find_effective_service_level`,
    * `find_cached_effective_service_level`.
    
    They take the name of a role as their argument. Since the anonymous role
    doesn't have a name, it's not possible to call them with it.
    
    Fixes scylladb/scylladb#26816

--- service/qos/service_level_controller.hh
+++ service/qos/service_level_controller.hh
@@ -17,6 +17,7 @@
 #include <seastar/core/with_scheduling_group.hh>
 #include <seastar/core/lowres_clock.hh>
 
+#include "auth/authenticated_user.hh"
 #include "seastarx.hh"
 #include "auth/service.hh"
 #include "cql3/description.hh"
@@ -288,6 +289,17 @@ public:
     template <typename Func, typename Ret = std::invoke_result_t<Func>>
     requires std::invocable<Func>
     futurize_t<Ret> with_user_service_level(const std::optional<auth::authenticated_user>& usr, Func&& func) {
+        // Special case:
+        // -------------
+        // The maintenance socket can communicate with Scylla before `auth_integration`
+        // is registered, and we need to prepare for it.
+        // For the discussion, see: scylladb/scylladb#26816.
+        //
+        // TODO: Get rid of this.
+        if (!usr.has_value() || auth::is_anonymous(usr.value())) {
+            return with_scheduling_group(get_default_scheduling_group(), std::forward<Func>(func));
+        }
+
         SCYLLA_ASSERT(_auth_integration != nullptr);
         return _auth_integration->with_user_service_level(usr, std::forward<Func>(func));
     }
