d9ed784fc315e5b2b94caef9e05a6f4ce75a525a
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-11-13 18:48:05 +0200

    streaming: refactor get_sstables_by_tablet_range to make it testable
    Make the `get_sstables_by_tablet_range` member function `static`. This
    is a step toward improved testability, allowing the function to be
    invoked directly without requiring a full instance of the streamer.

--- sstables_loader.cc
+++ sstables_loader.cc
@@ -179,8 +179,8 @@ private:
 
 class tablet_sstable_streamer : public sstable_streamer {
     const locator::tablet_map& _tablet_map;
-    future<std::tuple<std::vector<sstables::shared_sstable>, std::vector<sstables::shared_sstable>>>
-    get_sstables_by_tablet_range(const dht::token_range& tablet_range) const;
+    static future<std::tuple<std::vector<sstables::shared_sstable>, std::vector<sstables::shared_sstable>>>
+    get_sstables_by_tablet_range(const std::vector<sstables::shared_sstable>& sstables, const dht::token_range& tablet_range);
 public:
     tablet_sstable_streamer(netw::messaging_service& ms, replica::database& db, ::table_id table_id, locator::effective_replication_map_ptr erm,
                             std::vector<sstables::shared_sstable> sstables, primary_replica_only primary, unlink_sstables unlink, stream_scope scope)
@@ -346,11 +346,11 @@ public:
 };
 
 future<std::tuple<std::vector<sstables::shared_sstable>, std::vector<sstables::shared_sstable>>>
-tablet_sstable_streamer::get_sstables_by_tablet_range(const dht::token_range& tablet_range) const {
+tablet_sstable_streamer::get_sstables_by_tablet_range(const std::vector<sstables::shared_sstable>& sstables, const dht::token_range& tablet_range) {
     std::vector<sstables::shared_sstable> sstables_fully_contained;
     std::vector<sstables::shared_sstable> sstables_partially_contained;
     // sstables are sorted by first key in reverse order.
-    for (const auto& sst : std::ranges::reverse_view(_sstables)) {
+    for (const auto& sst : std::ranges::reverse_view(sstables)) {
         auto sst_first = sst->get_first_decorated_key().token();
         auto sst_last = sst->get_last_decorated_key().token();
         dht::token_range sst_token_range{sst_first, sst_last};
@@ -382,7 +382,7 @@ future<> tablet_sstable_streamer::stream(shared_ptr<stream_progress> progress) {
 
     for (auto tablet_id : _tablet_map.tablet_ids() | std::views::filter([this] (auto tid) { return tablet_in_scope(tid); })) {
         auto tablet_range = _tablet_map.get_token_range(tablet_id);
-        auto [sstables_fully_contained, sstables_partially_contained] = co_await get_sstables_by_tablet_range(tablet_range);
+        auto [sstables_fully_contained, sstables_partially_contained] = co_await get_sstables_by_tablet_range(_sstables, tablet_range);
         auto per_tablet_progress = make_shared<per_tablet_stream_progress>(
             progress,
             sstables_fully_contained.size() + sstables_partially_contained.size());
