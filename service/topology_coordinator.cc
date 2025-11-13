e0dbb8f778812319fc93d34260eb84cf4ee05e21
Michael Litvak <michael.litvak@scylladb.com>
2025-11-13 11:54:08 +0100

    topology_coordinator: include joining node in barrier
    Previously, only nodes in the 'normal' state and decommissioning nodes
    were included in the set of nodes participating in barrier and
    barrier_and_drain commands. Joining nodes are not included because they
    don't coordinate requests, given their cql port is closed.
    
    However, joining nodes may receive mutations from other nodes, for which
    they may generate and coordinate materialized view updates. If their
    group0 state is not synchronized it could cause lost view updates.
    For example:
    
    1. On the topology coordinator, the join completes and the joining node
       becomes normal, but the joining node's state lags behind. Since it's
       not synchronized by the barrier, it could be in an old state such as
       `write_both_read_old`.
    2. A normal node coordinates a write and sends it to the new node as the
       new replica.
    3. The new node applies the base mutation but doesn't generate a view
       update for it, because it calculates the base-view pairing according
       to its own state and replication map, and determines that it doesn't
       participate in the base-view pairing.
    
    Therefore, since the joining node participates as a coordinator for view
    updates, it should be included in these barriers as well. This ensures
    that before the join completes, the joining node's state is
    `write_both_read_new`, where it does generate view updates.
    
    Fixes scylladb/scylladb#26976
    
    (cherry picked from commit 13d94576e5fc6a5047b67f4148426087876d46e3)

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -437,9 +437,11 @@ class topology_coordinator : public endpoint_lifecycle_subscriber {
             | std::views::filter([&cmd, &exclude_nodes] (const std::pair<const raft::server_id, replica_state>& n) {
                 // We must send barrier and barrier_and_drain to the decommissioning node
                 // as it might be accepting or coordinating requests.
-                bool include_decommissioning_node = n.second.state == node_state::decommissioning
+                // A joining node might be receiving mutations and coordinating view updates.
+                bool include_transitioning_node =
+                        (n.second.state == node_state::decommissioning || n.second.state == node_state::bootstrapping || n.second.state == node_state::replacing)
                         && (cmd.cmd == raft_topology_cmd::command::barrier || cmd.cmd == raft_topology_cmd::command::barrier_and_drain);
-                return !exclude_nodes.contains(n.first) && (n.second.state == node_state::normal || include_decommissioning_node);
+                return !exclude_nodes.contains(n.first) && (n.second.state == node_state::normal || include_transitioning_node);
             })
             | std::views::keys;
         if (drop_and_retake) {
