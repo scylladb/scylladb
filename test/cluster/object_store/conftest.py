8b01c901c602320a3ce8b72d75bba4388771f1a5
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-11-18 09:32:07 +0200

    streaming: add pytest case to reproduce mutation loss issue
    Introduce a test that demonstrates mutation loss caused by premature
    loop termination in tablet_sstable_streamer::stream. The code broke
    out of the SSTable iteration when encountering a non-overlapping range,
    which skipped subsequent SSTables that should have been partially
    contained. This test showcases the problem only.
    
    Example:
    Tablet range: [4, 5]
    SSTable ranges:
    [0,5]
    [0, 3] <--- is considered exhausted, and causes skip to next tablet
    [2, 5] <--- is missed for range [4, 5]
    
    (cherry picked from commit 656ce27e7f1190986065f5f9c3643c573ba33cda)

--- test/cluster/object_store/conftest.py
+++ test/cluster/object_store/conftest.py
@@ -109,3 +109,265 @@ def get_s3_resource(s3_server):
         config=boto3.session.Config(signature_version='s3v4'),
         verify=False
     )
+<<<<<<< HEAD
+||||||| parent of 656ce27e7f (streaming: add pytest case to reproduce mutation loss issue)
+
+class GSFront:
+    def __init__(self, endpoint, bucket_name, credentials_file):
+        self.endpoint = endpoint
+        self.bucket_name = bucket_name
+        self.credentials_file = credentials_file
+
+    @property
+    def address(self):
+        return self.endpoint
+
+    @property
+    def type(self):
+        return 'GS'
+
+    def create_endpoint_conf(self):
+        endpoint = {'name': self.endpoint, 
+                    'type': 'gs',
+                    'credentials_file': self.credentials_file if self.credentials_file else 'none'
+                    }
+        return [endpoint]
+
+    def get_resource(self):
+        """Creates boto3.resource object that can be used to communicate to the given server"""
+        return boto3.resource('s3',
+            endpoint_url=self.endpoint,
+            config=boto3.session.Config(signature_version='s3v4'),
+            verify=False
+        )
+
+    async def start(self):
+        pass
+    async def stop(self):
+        pass
+
+class GSServer(GSFront):
+    def __init__(self, tmpdir):
+        super(GSServer, self).__init__(None, 'testbucket', None)
+        self.server = None
+        self.host = None
+        self.port = None
+        self.oldvars = {}
+        self.vars = { 'GS_SERVER_ADDRESS_FOR_TEST': attrgetter('endpoint'),
+                      'GS_BUCKET_FOR_TEST': attrgetter('bucket_name'), 
+                      'GS_CREDENTIALS_FILE': attrgetter('credentials_file'), 
+                      }
+        self.tmpdir = tmpdir
+
+    def publish(self):
+        self.endpoint = f'http://{self.host}:{self.port}'
+        self.oldvars = { k : os.environ.get(k) for k in self.vars }
+        for k, v in self.vars.items():
+            val = v(self)
+            if val:
+                os.environ[k] = val
+
+    def unpublish(self):
+        for k in self.vars:
+            v = self.oldvars[k]
+            if v:
+                os.environ[k] = v
+            elif os.environ.get(k):
+                del os.environ[k]
+
+    def _docker_args(self, host, port):
+        # pylint: disable=unused-argument
+        return ["-p", f'{port}:{port}']
+
+    def _image_args(self, host, port):
+        # pylint: disable=unused-argument
+        return ["-scheme", "http", "-log-level", "debug", "--port", f'{port}', '-public-host', f'127.0.0.1:{port}']
+
+    async def start(self):
+        self.server = DockerizedServer("docker.io/fsouza/fake-gcs-server:1.52.3", self.tmpdir, 
+                                       logfilenamebase="fake-gcs-server",
+                                       docker_args=self._docker_args,
+                                       image_args=self._image_args,
+                                       success_string="server started at",
+                                       failure_string="address already in use"
+                                       )
+        await self.server.start()
+        self.port = self.server.port
+        self.host = self.server.host
+        self.publish()
+
+        # create bucket. can't use boto, because fake server does not support xml syntax 
+        # for anything beyong listing
+        response = requests.post(f'{self.endpoint}/storage/v1/b?project=testproject', json = {
+            'name': self.bucket_name, 'location' : 'US', 'storageClass' : 'STANDARD',
+            'iamConfiguration': {
+                'uniformBucketLevelAccess' : {
+                    'enabled': True,
+                }
+            }
+        }, timeout = 10)
+        if response.status_code not in [200, 201]:
+            raise Exception(f'Could not create test bucket: {response}')
+
+    async def stop(self):
+        if self.server:
+            await self.server.stop()
+        self.unpublish()
+
+@pytest.fixture(scope="function", params=['s3','gs'])
+async def object_storage(request, pytestconfig, tmpdir):
+    server = None
+
+    if request.param == 'gs':
+        endpoint = os.environ.get('GS_SERVER_ADDRESS_FOR_TEST')
+        bucket = os.environ.get('GS_BUCKET_FOR_TEST')
+        credentials_file = os.environ.get('GS_CREDENTIALS_FILE')
+
+        if endpoint is not None and bucket is not None:
+            server = GSFront(endpoint, bucket, credentials_file)
+        else:
+            server = GSServer(tmpdir)
+    else:
+        server = create_s3_server(pytestconfig, tmpdir)
+
+    try:
+        await server.start()
+        yield server
+    finally:
+        await server.stop()
+=======
+
+class GSFront:
+    def __init__(self, endpoint, bucket_name, credentials_file):
+        self.endpoint = endpoint
+        self.bucket_name = bucket_name
+        self.credentials_file = credentials_file
+
+    @property
+    def address(self):
+        return self.endpoint
+
+    @property
+    def type(self):
+        return 'GS'
+
+    def create_endpoint_conf(self):
+        endpoint = {'name': self.endpoint, 
+                    'type': 'gs',
+                    'credentials_file': self.credentials_file if self.credentials_file else 'none'
+                    }
+        return [endpoint]
+
+    def get_resource(self):
+        """Creates boto3.resource object that can be used to communicate to the given server"""
+        return boto3.resource('s3',
+            endpoint_url=self.endpoint,
+            config=boto3.session.Config(signature_version='s3v4'),
+            verify=False
+        )
+
+    async def start(self):
+        pass
+    async def stop(self):
+        pass
+
+class GSServer(GSFront):
+    def __init__(self, tmpdir):
+        super(GSServer, self).__init__(None, 'testbucket', None)
+        self.server = None
+        self.host = None
+        self.port = None
+        self.oldvars = {}
+        self.vars = { 'GS_SERVER_ADDRESS_FOR_TEST': attrgetter('endpoint'),
+                      'GS_BUCKET_FOR_TEST': attrgetter('bucket_name'), 
+                      'GS_CREDENTIALS_FILE': attrgetter('credentials_file'), 
+                      }
+        self.tmpdir = tmpdir
+
+    def publish(self):
+        self.endpoint = f'http://{self.host}:{self.port}'
+        self.oldvars = { k : os.environ.get(k) for k in self.vars }
+        for k, v in self.vars.items():
+            val = v(self)
+            if val:
+                os.environ[k] = val
+
+    def unpublish(self):
+        for k in self.vars:
+            v = self.oldvars[k]
+            if v:
+                os.environ[k] = v
+            elif os.environ.get(k):
+                del os.environ[k]
+
+    def _docker_args(self, host, port):
+        # pylint: disable=unused-argument
+        return ["-p", f'{port}:{port}']
+
+    def _image_args(self, host, port):
+        # pylint: disable=unused-argument
+        return ["-scheme", "http", "-log-level", "debug", "--port", f'{port}', '-public-host', f'127.0.0.1:{port}']
+
+    async def start(self):
+        self.server = DockerizedServer("docker.io/fsouza/fake-gcs-server:1.52.3", self.tmpdir, 
+                                       logfilenamebase="fake-gcs-server",
+                                       docker_args=self._docker_args,
+                                       image_args=self._image_args,
+                                       success_string="server started at",
+                                       failure_string="address already in use"
+                                       )
+        await self.server.start()
+        self.port = self.server.port
+        self.host = self.server.host
+        self.publish()
+
+        # create bucket. can't use boto, because fake server does not support xml syntax 
+        # for anything beyong listing
+        response = requests.post(f'{self.endpoint}/storage/v1/b?project=testproject', json = {
+            'name': self.bucket_name, 'location' : 'US', 'storageClass' : 'STANDARD',
+            'iamConfiguration': {
+                'uniformBucketLevelAccess' : {
+                    'enabled': True,
+                }
+            }
+        }, timeout = 10)
+        if response.status_code not in [200, 201]:
+            raise Exception(f'Could not create test bucket: {response}')
+
+    async def stop(self):
+        if self.server:
+            await self.server.stop()
+        self.unpublish()
+
+@pytest.fixture(scope="function", params=['s3','gs'])
+async def object_storage(request, pytestconfig, tmpdir):
+    server = None
+
+    if request.param == 'gs':
+        endpoint = os.environ.get('GS_SERVER_ADDRESS_FOR_TEST')
+        bucket = os.environ.get('GS_BUCKET_FOR_TEST')
+        credentials_file = os.environ.get('GS_CREDENTIALS_FILE')
+
+        if endpoint is not None and bucket is not None:
+            server = GSFront(endpoint, bucket, credentials_file)
+        else:
+            server = GSServer(tmpdir)
+    else:
+        server = create_s3_server(pytestconfig, tmpdir)
+
+    try:
+        await server.start()
+        yield server
+    finally:
+        await server.stop()
+
+@pytest.fixture(scope="function")
+async def s3_storage(pytestconfig, tmpdir):
+    server = create_s3_server(pytestconfig, tmpdir)
+
+    try:
+        await server.start()
+        yield server
+    finally:
+        await server.stop()
+>>>>>>> 656ce27e7f (streaming: add pytest case to reproduce mutation loss issue)
