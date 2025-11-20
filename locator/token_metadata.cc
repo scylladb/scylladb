scylladb/scylladb

commit 6e4803a750e42ea7504ec7f69f2a4ed02782b3e4

token_metadata_impl: clear_gently: release version tracker early
No need to wait for all members to be cleared gently.
We can release the version earlier since the
held version may be awaited for in barriers.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

locator/token_metadata.cc
@@ -357,6 +357,7 @@ future<std::unique_ptr<token_metadata_impl>> token_metadata_impl::clone_only_tok
 }
 
 future<> token_metadata_impl::clear_gently() noexcept {
+    _version_tracker = {};
     co_await utils::clear_gently(_token_to_endpoint_map);
     co_await utils::clear_gently(_normal_token_owners);
     co_await utils::clear_gently(_bootstrap_tokens);

commit 92ba82b3e15973bbfcfce9df871687923ddce015

token_metadata: fix notification about expiring erm held for to long
Commit 6e4803a750e4 broke notification about expired erms held for too
long since it resets the tracker without calling its destructor (where
notification is triggered). Reset the tracker using std::exchange
instead.

locator/token_metadata.cc
@@ -358,7 +358,7 @@ future<std::unique_ptr<token_metadata_impl>> token_metadata_impl::clone_only_tok
 }
 
 future<> token_metadata_impl::clear_gently() noexcept {
-    _version_tracker = {};
+    std::exchange(_version_tracker, locator::token_metadata::version_tracker_t{});
     co_await utils::clear_gently(_token_to_endpoint_map);
     co_await utils::clear_gently(_normal_token_owners);
     co_await utils::clear_gently(_bootstrap_tokens);