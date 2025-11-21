d8d71b0a5e5daa80d37fc8872bc54387bec729a4
Dario Mirovic <dario.mirovic@scylladb.com>
2025-11-21 02:51:37 +0100

    auth: maintenance socket connection access rights and superuser status
    Maintenance auth service uses allow_all_authenticator. To allow
    role modification statements over maintenance socket connections,
    we need to treat maintenance socket connections as superusers and
    give them proper access rights. This patch introduces
    maintenance_socket_authenticator, and enables role modification statements
    when server node is not in maintenance mode.
    
    Refs scylladb/scylla-enterprise#5657

--- auth/maintenance_socket_role_manager.hh
+++ auth/maintenance_socket_role_manager.hh
@@ -11,6 +11,7 @@
 #include "auth/cache.hh"
 #include "auth/resource.hh"
 #include "auth/role_manager.hh"
+#include "auth/standard_role_manager.hh"
 #include <seastar/core/future.hh>
 
 namespace cql3 {
@@ -29,9 +30,16 @@ extern const std::string_view maintenance_socket_role_manager_name;
 // This role manager is used by the maintenance socket. It has disabled all role management operations to not depend on
 // system_auth keyspace, which may be not yet created when the maintenance socket starts listening.
 class maintenance_socket_role_manager final : public role_manager {
+    cql3::query_processor& _qp;
+    ::service::raft_group0_client& _group0_client;
+    ::service::migration_manager& _migration_manager;
+    cache& _cache;
+    std::optional<standard_role_manager> _std_mgr;
     bool _is_maintenance_mode;
 
 public:
+    future<> enable_role_operations();
+
     bool is_maintenance_mode() const;
     void set_maintenance_mode();
 
--- main.cc
+++ main.cc
@@ -17,6 +17,7 @@
 #include "gms/inet_address.hh"
 #include "auth/allow_all_authenticator.hh"
 #include "auth/allow_all_authorizer.hh"
+#include "auth/maintenance_socket_authenticator.hh"
 #include "auth/maintenance_socket_role_manager.hh"
 #include <seastar/core/future.hh>
 #include <seastar/core/signal.hh>
@@ -2076,7 +2077,7 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
                 checkpoint(stop_signal, "starting maintenance auth service");
                 auth::service_config maintenance_auth_config;
                 maintenance_auth_config.authorizer_java_name = sstring{auth::allow_all_authorizer_name};
-                maintenance_auth_config.authenticator_java_name = sstring{auth::allow_all_authenticator_name};
+                maintenance_auth_config.authenticator_java_name = sstring{auth::maintenance_socket_authenticator_name};
                 maintenance_auth_config.role_manager_java_name = sstring{auth::maintenance_socket_role_manager_name};
 
                 maintenance_auth_service.start(perm_cache_config, std::ref(qp), std::ref(group0_client),  std::ref(mm_notifier), std::ref(mm), maintenance_auth_config, maintenance_socket_enabled::yes, std::ref(auth_cache), std::ref(hashing_worker)).get();
@@ -2223,6 +2224,14 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
                 return ss.local().join_cluster(proxy, service::start_hint_manager::yes, generation_number);
             }).get();
             stop_signal.ready(false);
+
+            if (cfg->maintenance_socket() != "ignore") {
+                // Enable role operations now that node joined the cluster
+                maintenance_auth_service.invoke_on_all([](auth::service& svc) {
+                    auto& rm = dynamic_cast<auth::maintenance_socket_role_manager&>(svc.underlying_role_manager());
+                    return rm.enable_role_operations();
+                }).get();
+            }
 
             // At this point, `locator::topology` should be stable, i.e. we should have complete information
             // about the layout of the cluster (= list of nodes along with the racks/DCs).
