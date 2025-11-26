b779f15cdb581430ef7021597df8195e9c939eeb
Patryk Jędrzejczak <patryk.jedrzejczak@scylladb.com>
2025-11-26 10:07:16 +0100

    locator/node: preserve _excluded in clone()
    We currently ignore the `_excluded` field in `clone()`. Losing
    information about exclusion can have unpredictable consequences. One
    observed effect (that led to finding this issue) is that the
    `/storage_service/nodes/excluded` API endpoint sometimes misses excluded
    nodes.
    
    (cherry picked from commit 4160ae94c1d346f2e849d0e8b7f30b11bce43c69)

--- locator/topology.hh
+++ locator/topology.hh
@@ -74,6 +74,7 @@ public:
          endpoint_dc_rack dc_rack,
          state state,
          shard_id shard_count = 0,
+         bool excluded = false,
          this_node is_this_node = this_node::no,
          idx_type idx = -1);
 
@@ -161,6 +162,7 @@ private:
                             endpoint_dc_rack dc_rack,
                             state state,
                             shard_id shard_count = 0,
+                            bool excluded = false,
                             node::this_node is_this_node = this_node::no,
                             idx_type idx = -1);
     node_holder clone() const;
