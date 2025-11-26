d2d9a3254329bb6dc3d634cfeed04743bb1a1d07
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2025-11-26 16:30:23 +0100

    tasks: service: add global_topology_request_virtual_task
    Add a service::topo::global_topology_request_virtual_task, which
    covers the replication factor changes.
    
    Currently, the global_topology_request_virtual_task can be aborted
    only if it is paused.
    
    The progress of the rf change isn't counted.

--- service/task_manager_module.hh
+++ service/task_manager_module.hh
@@ -54,4 +54,33 @@ public:
 
     std::set<locator::host_id> get_nodes() const override;
 };
+
+namespace topo {
+
+class global_topology_request_virtual_task : public tasks::task_manager::virtual_task::impl {
+private:
+    service::storage_service& _ss;
+public:
+    global_topology_request_virtual_task(tasks::task_manager::module_ptr module,
+            service::storage_service& ss)
+        : tasks::task_manager::virtual_task::impl(std::move(module))
+        , _ss(ss)
+    {}
+    virtual tasks::task_manager::task_group get_group() const noexcept override;
+    virtual future<std::optional<tasks::virtual_task_hint>> contains(tasks::task_id task_id) const override;
+    virtual future<tasks::is_abortable> is_abortable(tasks::virtual_task_hint hint) const override;
+
+    virtual future<std::optional<tasks::task_status>> get_status(tasks::task_id id, tasks::virtual_task_hint hint) override;
+    virtual future<std::optional<tasks::task_status>> wait(tasks::task_id id, tasks::virtual_task_hint hint) override;
+    virtual future<> abort(tasks::task_id id, tasks::virtual_task_hint hint) noexcept override;
+    virtual future<std::vector<tasks::task_stats>> get_stats() override;
+};
+
+class task_manager_module : public tasks::task_manager::module {
+public:
+    task_manager_module(tasks::task_manager& tm) noexcept;
+};
+
+}
+
 }
