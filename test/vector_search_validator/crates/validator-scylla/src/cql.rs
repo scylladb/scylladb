c6051814013b71b6da570827b4fe4bb3e3998e4b
Pawel Pery <pawel.pery@scylladb.com>
2025-11-27 18:31:25 +0100

    vector_search_validator: move high availability tests from vector-store.git
    Initially, tests for high availability were implemented in vector-store.git
    repository. High availability is currently implemented in scylladb.git
    repository so this repository should be the better place to store them. This
    commit copies these tests into the scylladb.git.
    
    The commit copies validator-vector-store/src/high_availability.rs (tests logic)
    and validator-tests/src/common.rs (utils for tests) into the local crate
    validator-scylla. The common.rs should be copied to be able for reviewer to see
    common test code and this code most likely be frequent to change - it will be
    hard to maintain one common version between two repositories.
    
    The commit updates also README for vector_search_validator; it shortly describe
    the validator modules.
    
    The commit updates reference to the latest vector-store.git master.
    
    As a next step on the vector-store.git high_availability.rs would be removed
    and common.rs moved from validator-tests into validator-vector-store.
    
    References: VECTOR-394

--- test/vector_search_validator/crates/validator-scylla/src/cql.rs
+++ test/vector_search_validator/crates/validator-scylla/src/cql.rs
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
  */
 
-use std::time::Duration;
-use vector_search_validator_tests::common;
-use vector_search_validator_tests::*;
+use crate::common;
+use async_backtrace::framed;
+use vector_search_validator_tests::TestCase;
 
+#[framed]
 pub(crate) async fn new() -> TestCase {
-    let timeout = Duration::from_secs(30);
+    let timeout = common::DEFAULT_TEST_TIMEOUT;
     TestCase::empty()
         .with_init(timeout, common::init)
         .with_cleanup(timeout, common::cleanup)
