41d52393eff533e393d0ce41130826b906ba867d
Andrzej Jackowski <andrzej.jackowski@scylladb.com>
2025-11-28 14:48:48 +0100

    test: add test for CLIENT_ROUTES event
    Ref: scylladb/scylla-enterprise#5699

--- test/cluster/test_client_routes.py
+++ test/cluster/test_client_routes.py
@@ -19,6 +19,7 @@ from cassandra.protocol import EventMessage
 import cassandra.protocol as cass_proto
 
 logger = logging.getLogger(__name__)
+CLIENT_ROUTES_CHANGE_EVENT_NAME = "CLIENT_ROUTES_CHANGE"
 
 async def wait_for_expected_client_routes_size(cql, expected_routes_size):
     async def expected_client_routes_size(cql, expected_size):
@@ -129,3 +130,68 @@ async def test_client_routes_lost_quorum(request, manager: ManagerClient):
 
     await asyncio.gather(fail_post(), fail_delete())
     await wait_for_expected_client_routes_size(cql, 1)
+
+# The monkeypatching is a temporary solution. It will be removed very soon after a new Python Driver is released
+def monkeypatch_driver_to_handle_client_routes_event(monkeypatch, received_events):
+    _original_try_connect = ControlConnection._try_connect
+    _original_known_event_types = cass_proto.known_event_types
+
+    def _handle_event(arg1):
+        logger.info(f"monkeypatch_driver handle_event called with arg1={arg1}")
+        received_events.append(arg1)
+
+    def _recv_client_routes_change(f, arg2):
+        logger.info(f"monkeypatch_driver recv_client_routes_change, f={f} arg2={arg2}")
+        change_type = cass_proto.read_string(f)
+        connection_ids = [cass_proto.read_string(f) for _ in range(cass_proto.read_short(f))]
+        host_ids = [cass_proto.read_string(f) for _ in range(cass_proto.read_short(f))]
+        return {
+            "change_type": change_type,
+            "connection_ids": connection_ids,
+            "host_ids": host_ids
+        }
+
+    def _try_connect_register_client_routes_change_event(self, *args, **kwargs):
+        connection = _original_try_connect(self, *args, **kwargs)
+        connection.register_watchers({CLIENT_ROUTES_CHANGE_EVENT_NAME: _handle_event})
+        return connection   
+
+    monkeypatch.setattr(cass_proto, "known_event_types", _original_known_event_types.union([CLIENT_ROUTES_CHANGE_EVENT_NAME]), raising=True)
+    monkeypatch.setattr(EventMessage, "recv_client_routes_change", _recv_client_routes_change, raising=False)
+    monkeypatch.setattr(ControlConnection, "_try_connect", _try_connect_register_client_routes_change_event, raising=True)
+
+# This test verifies client routes change event in the following steps:
+# 1. Add one new entry to client_routes.
+# 2. Verify driver received one new event.
+# 3. Add two new entries to client_routes using one POST request.
+# 4. Verify driver received one new event with two updates.
+@pytest.mark.asyncio
+async def test_events(request, manager: ManagerClient, monkeypatch):
+    received_events = []
+    monkeypatch_driver_to_handle_client_routes_event(monkeypatch, received_events)
+
+    servers = await manager.servers_add(2)
+    cql, hosts = await manager.get_ready_cql(servers)
+
+    await manager.api.client.post("/v2/client-routes", host=servers[0].ip_addr, json=[generate_client_routes_entry(0)])
+
+    async def wait_for_expected_event_num(expected_num):
+        async def expected_event_num(num):
+            if len(received_events) == num:
+                return num
+            return None
+        await wait_for(lambda: expected_event_num(expected_num), time.time() + 10)
+
+    await wait_for_expected_event_num(1)
+    assert received_events[0]["change_type"] == "UPDATE_NODES"
+    assert received_events[0]["connection_ids"] == [generate_connection_id(0)]
+    assert received_events[0]["host_ids"] == [generate_host_id(0)]
+
+    await manager.api.client.post("/v2/client-routes", host=servers[0].ip_addr, json=[
+        generate_client_routes_entry(1),
+        generate_client_routes_entry(2),
+    ])
+    await wait_for_expected_event_num(2)
+    assert received_events[1]["change_type"] == "UPDATE_NODES"
+    assert received_events[1]["connection_ids"] == [generate_connection_id(1), generate_connection_id(2)]
+    assert received_events[1]["host_ids"] == [generate_host_id(1), generate_host_id(2)]
