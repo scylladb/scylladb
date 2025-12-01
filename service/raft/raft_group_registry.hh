37010db61a6472883f28d496a656c80dbb6f5cdd
Gleb Natapov <gleb@scylladb.com>
2025-12-01 11:55:44 +0200

    raft: drop invoke_on from the pinger verb handler
    Currently raft direct pinger verb jumps to shard 0 to check if group0 is
    alive before replying. The verb runs relatively often, so it is not very
    efficient. The patch distributes group0 liveness information (as it
    changes) to all shard instead, so that the handler itself does not need
    to jump to shard 0.
    
    (cherry picked from commit 6a6bbbf1a667ebba572f9f80ce26905ebfe5b9b7)

--- service/raft/raft_group_registry.hh
+++ service/raft/raft_group_registry.hh
@@ -128,6 +128,9 @@ private:
     // My Raft ID. Shared between different Raft groups.
     raft::server_id _my_id;
 
+    bool _group0_is_alive = false;
+
+    void ensure_aborted(raft_server_for_group& server_for_group, sstring reason);
 public:
     raft_group_registry(raft::server_id my_id, netw::messaging_service& ms,
             direct_failure_detector::failure_detector& fd);
@@ -182,6 +185,9 @@ public:
     unsigned shard_for_group(const raft::group_id& gid) const;
     shared_ptr<raft::failure_detector> failure_detector();
     direct_failure_detector::failure_detector& direct_fd() { return _direct_fd; }
+    bool is_group0_alive() const {
+        return _group0_is_alive;
+    }
 };
 
 // Implementation of `direct_failure_detector::pinger` which uses DIRECT_FD_PING verb for pinging.
