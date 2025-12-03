52fe3f1c4531bb6059db78afd7d363a429319b99
Dario Mirovic <dario.mirovic@scylladb.com>
2025-12-03 16:41:21 +0100

    test: cqlpy: test_protocol_exceptions.py: increase cpp exceptions threshold
    The initial problem:
    
    Some of the tests in test_protocol_exceptions.py started failing. The failure is
    on the condition that no more than `cpp_exception_threshold` happened.
    
    Test logic:
    
    These tests assert that specific code paths do not throw an exception anymore.
    Initial implementation ran a code path once, and asserted there were 0 exceptions.
    Sometimes an exception or several can occur, not directly related to the code paths
    the tests check, but those would fail the tests.
    
    The solution was to run the tests multiple times. If there is a regression, there
    would be at least as many exceptions thrown as there are test runs. If there is no
    regression, a few exceptions might happen, up to 10 per 100 test runs.
    I have arbitrarily chosen `run_count = 100` and `cpp_exception_threshold = 10` values.
    
    Note that the exceptions are counted per shard, not per code path.
    
    The new problem:
    
    The occassional exceptions thrown by some parts of the server now throw a bit more
    than before. Based on the logs linked on the issues, it is usually 12.
    
    There are possibly multiple ways to resolve the issue. I have considered logging
    exceptions and parsing them. I would have to filter exception logs only for wanted
    exceptions. However, if a new, different exception is introduced, it might not be
    counted.
    
    Another approach is to just increase the threshold a bit. The issue of throwing
    more exceptions than before in some other server modules should be addressed by
    a set of tests for that module, just like these tests check protocol exceptions,
    not caring who used protocol check code paths.
    
    For those reasons, the solution implemented here is to increase `cpp_exception_threshold`
    to `20`. It will not make the tests unreliable, because, as mentioned, if there is a
    regression, there would be at least `run_count` exceptions per `run_count` test runs
    (1 exception per single test run).
    
    Still, to make "background exceptions" occurence a bit more normalized, `run_count` too
    is doubled, from `100` to `200`. At the first glance this looks like nothing is changed,
    but actually doubling both run count and exception threshold here implies that the
    exception burst does not scale as much as run count, it is just that the "jitter" is
    bigger than the old threshold.
    
    Fixes #27247
    Fixes #27325
    
    (cherry picked from commit 807fc68dc5d4cba1b04a42f4c62a771b17d56f98)

--- test/cqlpy/test_protocol_exceptions.py
+++ test/cqlpy/test_protocol_exceptions.py
@@ -60,9 +60,9 @@ def try_connect(host, port, creds, protocol_version):
 
 # If there is a protocol version mismatch, the server should
 # raise a protocol error, which is counted in the metrics.
-def test_protocol_version_mismatch(scylla_only, request, host):
-    run_count = 100
-    cpp_exception_threshold = 10
+def test_protocol_version_mismatch(scylla_only, debug_exceptions_logging, request, host):
+    run_count = 200
+    cpp_exception_threshold = 20
 
     cpp_exception_metrics_before = get_cpp_exceptions_metrics(host)
     protocol_exception_metrics_before = get_protocol_error_metrics(host)
@@ -164,6 +164,44 @@ def _protocol_error_impl(host, *, trigger_bad_batch=False, trigger_unexpected_au
     finally:
         s.close()
 
+<<<<<<< HEAD
+||||||| parent of 807fc68dc5 (test: cqlpy: test_protocol_exceptions.py: increase cpp exceptions threshold)
+def _test_impl(host, flag):
+    run_count = 100
+    cpp_exception_threshold = 10
+
+    cpp_exception_metrics_before = get_cpp_exceptions_metrics(host)
+    protocol_exception_metrics_before = get_protocol_error_metrics(host)
+
+    for _ in range(run_count):
+        kwargs = {flag: True}
+        _protocol_error_impl(host, **kwargs)
+
+    protocol_exception_metrics_after = get_protocol_error_metrics(host)
+    assert protocol_exception_metrics_after > protocol_exception_metrics_before, f"Expected protocol errors to increase after running test with {flag}"
+
+    cpp_exception_metrics_after = get_cpp_exceptions_metrics(host)
+    assert cpp_exception_metrics_after - cpp_exception_metrics_before <= cpp_exception_threshold, f"Expected C++ protocol errors to not increase after running test with {flag}"
+
+=======
+def _test_impl(host, flag):
+    run_count = 200
+    cpp_exception_threshold = 20
+
+    cpp_exception_metrics_before = get_cpp_exceptions_metrics(host)
+    protocol_exception_metrics_before = get_protocol_error_metrics(host)
+
+    for _ in range(run_count):
+        kwargs = {flag: True}
+        _protocol_error_impl(host, **kwargs)
+
+    protocol_exception_metrics_after = get_protocol_error_metrics(host)
+    assert protocol_exception_metrics_after > protocol_exception_metrics_before, f"Expected protocol errors to increase after running test with {flag}"
+
+    cpp_exception_metrics_after = get_cpp_exceptions_metrics(host)
+    assert cpp_exception_metrics_after - cpp_exception_metrics_before <= cpp_exception_threshold, f"Expected C++ protocol errors to not increase after running test with {flag}"
+
+>>>>>>> 807fc68dc5 (test: cqlpy: test_protocol_exceptions.py: increase cpp exceptions threshold)
 @pytest.fixture
 def no_ssl(request):
     if request.config.getoption("--ssl"):
@@ -209,9 +247,9 @@ def test_unexpected_message_during_auth(scylla_only, no_ssl, host):
 # Test if the protocol exceptions do not decrease after running the test.
 # This is to ensure that the protocol exceptions are not cleared or reset
 # during the test execution.
-def test_no_protocol_exceptions(scylla_only, no_ssl, host):
-    run_count = 100
-    cpp_exception_threshold = 10
+def test_no_protocol_exceptions(scylla_only, no_ssl, debug_exceptions_logging, host):
+    run_count = 200
+    cpp_exception_threshold = 20
 
     cpp_exception_metrics_before = get_cpp_exceptions_metrics(host)
     protocol_exception_metrics_before = get_protocol_error_metrics(host)
