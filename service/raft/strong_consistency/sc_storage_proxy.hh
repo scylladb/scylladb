5d79b30d437d9ddefaac2ca6ddf98a95cf6c493e
Petr Gusev <petr.gusev@scylladb.com>
2025-12-11 12:56:30 +0100

    sc_storage_proxy: redirect to other shard
    Allow `sc_storage_proxy` methods to signal redirects using
    `sc_operation_result::redirect()`. Only same-node-other-shard redirects
    are supported for now; cross-node proxying will be added later.

--- service/raft/strong_consistency/sc_storage_proxy.hh
+++ service/raft/strong_consistency/sc_storage_proxy.hh
@@ -15,6 +15,41 @@ namespace service {
 
 class raft_group_registry;
 
+template <typename T = void>
+class sc_operation_result {
+    using stored_type = std::conditional_t<std::is_void_v<T>, std::monostate, T>;
+
+    std::variant<locator::tablet_replica, stored_type> _val;
+
+    sc_operation_result(std::variant<locator::tablet_replica, stored_type> val)
+        : _val(std::move(val))
+    {
+    }
+public:
+    static sc_operation_result<T> redirect(locator::tablet_replica target) {
+        return {target};
+    }
+
+    template <typename U = T>
+    static std::enable_if_t<!std::is_void_v<U>, sc_operation_result<T>> result(stored_type val) {
+        return {std::move(val)};
+    }
+
+    template <typename U = T>
+    static std::enable_if_t<std::is_void_v<U>, sc_operation_result<T>> result() {
+        return {std::monostate{}};
+    }
+
+    const locator::tablet_replica* get_if_redirect() const {
+        return std::get_if<locator::tablet_replica>(&_val);
+    }
+
+    template <typename U = T>
+    std::enable_if_t<!std::is_void_v<U>, U>&& extract_result() && {
+        return std::move(std::get<U>(_val));
+    }
+};
+
 class sc_storage_proxy: public peering_sharded_service<sc_storage_proxy> {
     raft_group_registry& _raft_groups;
     db::system_keyspace& _sys_ks;
@@ -22,7 +57,8 @@ public:
     sc_storage_proxy(raft_group_registry& raft_groups, db::system_keyspace& sys_ks);
 
     using mutatations_gen = noncopyable_function<utils::chunked_vector<mutation>(api::timestamp_type)>;
-    future<> mutate(const schema& schema, const dht::token& token, mutatations_gen&& mutatations_gen);
+    future<sc_operation_result<>> mutate(const schema& schema, const dht::token& token, 
+        mutatations_gen&& mutatations_gen);
 };
 
 }
