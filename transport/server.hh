ba1e6c0f1294d85ea66f4d4363994b4b57d49b9a
Alex <alex.dathskovsky@scylladb.com>
2025-12-11 13:30:43 +0200

    server: Refactor schedualing group update functionality. This change generalizes the scheduling-group update functionality and removes some copy-paste code, improving overall readability and maintainability. To achieve this, capturing lambdas were introduced. As a result, self-deducing this was added to those lambdas to avoid coroutine-related issues (“coroutine fiasco”).

--- transport/server.hh
+++ transport/server.hh
@@ -360,8 +360,9 @@ private:
 
         void write_response(foreign_ptr<std::unique_ptr<cql_server::response>>&& response, service_permit permit = empty_service_permit(), cql_compression compression = cql_compression::none);
         
-        void update_scheduling_group_v1();
-        void update_scheduling_group_v2();
+        void update_user_scheduling_group_v1(const std::optional<auth::authenticated_user>& usr);
+        void update_user_scheduling_group_v2(const std::optional<auth::authenticated_user>& usr);
+        void update_control_connection_scheduling_group();
         
         friend event_notifier;
     };
