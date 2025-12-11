ffae4a9698220e9a4428fd413f722d48c146b60c
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-12-11 11:04:45 +0100

    service/storage_service: update service levels cache after upgrade to v2
    Service levels cache is empty after upgrade to consistent topology
    if no mutations are commited to `system.service_levels_v2` or rolling
    restart is not done.
    
    To fix the bug, this commit adds service levels cache reloading after
    upgrading the SL data accessor to v2 in `storage_service::topology_state_load()`.
    
    Fixes SCYLLADB-90
    
    (cherry picked from commit be16e42cb0df05018dc3b51ab1380f778af57abe)

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -701,6 +701,7 @@ future<> storage_service::topology_state_load(state_change_hint hint) {
         co_await _sl_controller.invoke_on_all([this] (qos::service_level_controller& sl_controller) {
             sl_controller.upgrade_to_v2(_qp, _group0->client());
         });
+        co_await _sl_controller.local().update_cache(qos::update_both_cache_levels::yes, qos::query_context::group0);
     }
 
     // the view_builder is migrated to v2 in view_builder::migrate_to_v2.
