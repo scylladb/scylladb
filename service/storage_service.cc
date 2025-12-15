875b1ecacf7fe525bda0af776bacef03cc2f42f9
Michał Jadwiszczak <michal.jadwiszczak@scylladb.com>
2025-12-15 12:16:30 +0100

    service/storage_service: check if service levels were already upgraded before doing migration to raft
    There is no need to call `service_level_controller::upgrade_to_v2()`
    on every topology state load, we only need to do it once.
    
    (cherry picked from commit 53d0a2b5dc9a825b3097aa0c9e90f117d6ce7a2c)

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -718,9 +718,11 @@ future<> storage_service::topology_state_load(state_change_hint hint) {
         qp.auth_version = db::system_keyspace::auth_version_t::v2;
     });
 
-    co_await _sl_controller.invoke_on_all([this] (qos::service_level_controller& sl_controller) {
-        sl_controller.upgrade_to_v2(_qp, _group0->client());
-    });
+    if (!_sl_controller.local().is_v2()) {
+        co_await _sl_controller.invoke_on_all([this] (qos::service_level_controller& sl_controller) {
+            sl_controller.upgrade_to_v2(_qp, _group0->client());
+        });
+    }
 
     // the view_builder is migrated to v2 in view_builder::migrate_to_v2.
     // it writes a v2 version mutation as topology_change, then we get here
