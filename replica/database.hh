430059f64cc5f68e298989f8e0886c465d76ab80
Benny Halevy <bhalevy@scylladb.com>
2025-12-20 12:02:29 +0200

    replica: table, storage_group, compaction_group: add needs_flush
    Table needs flush if not all its memtable lists are empty.
    To be used in the next patch for a unit test.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
    (cherry picked from commit 5be6b80936d9b94a5dba256b330af3ee0a1c2e17)

--- replica/database.hh
+++ replica/database.hh
@@ -238,6 +238,10 @@ public:
         return may_flush() && !empty();
     }
 
+    bool needs_flush() const noexcept {
+        return !empty();
+    }
+
     bool empty() const noexcept {
         for (auto& m : _memtables) {
            if (!m->empty()) {
@@ -992,6 +996,7 @@ public:
     void start();
     future<> stop();
     future<> flush(std::optional<db::replay_position> = {});
+    bool needs_flush() const;
     future<> clear(); // discards memtable(s) without flushing them to disk.
     future<db::replay_position> discard_sstables(db_clock::time_point);
 
