989566e8a30e38f9d08daa3814f1158df92b7a89
Petr Gusev <petr.gusev@scylladb.com>
2025-12-23 23:48:54 +0100

    cql: add statement_helpers
    Introduce two helper methods that will be used for strongly consistent
    select_statement and modification_statement.
    
    redirect_statement() forwards the request to another shard or node.
    Currently, only shard forwarding is implemented; node-level proxying
    will be added in follow-up PRs.
    
    is_strongly_consistent() will be used in the prepare() method of raw
    statements to determine whether a strongly consistent statement should
    be created for the given CQL statement.

---
+++ cql3/statements/strong_consistency/statement_helpers.hh
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include "cql3/cql_statement.hh"
+#include "locator/tablets.hh"
+
+namespace cql3::statements::strong_consistency {
+
+future<::shared_ptr<cql_transport::messages::result_message>> redirect_statement(
+    query_processor& qp,
+    const query_options& options,
+    const locator::tablet_replica& target);
+
+bool is_strongly_consistent(data_dictionary::database db, std::string_view ks_name);
+
+}
