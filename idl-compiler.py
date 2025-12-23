26e978cf084fd0e4c3a2eb538198b95aab6ae7d4
Botond Dénes <bdenes@scylladb.com>
2025-12-23 14:49:34 +0200

    idl-compiler.py: add missing call to parent class constructor
    Replace manual init of parent fields.
    Found by CodeQL "Missing call to superclass `__init__` during object
    initialization".

--- idl-compiler.py
+++ idl-compiler.py
@@ -405,8 +405,8 @@ class RpcVerbParam(ASTBase):
     argument as an `foreign_ptr<lw_shared_ptr<>>`
     If the [[ref]] attribute is specified the send function signature will contain this type as const reference"""
     def __init__(self, type, name, attributes=Attributes()):
+        super().__init__(name)
         self.type = type
-        self.name = name
         self.attributes = attributes
 
     def __str__(self):
