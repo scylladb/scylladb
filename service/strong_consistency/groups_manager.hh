6f0040e007b7957106dfe05fb4db2f1a2bfcba8d
Petr Gusev <petr.gusev@scylladb.com>
2025-12-25 11:07:08 +0100

    strong_consistency: add groups_manager
    This class is reponsible for managing raft groups for
    strongly-consistent tablets.

---
+++ service/strong_consistency/groups_manager.hh
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include "locator/abstract_replication_strategy.hh"
+#include "message/messaging_service.hh"
+#include "service/raft/raft_group_registry.hh"
+#include "cql3/query_processor.hh"
+
+namespace service::strong_consistency {
+
+class raft_server;
+
+class groups_manager: public peering_sharded_service<groups_manager> {
+    class state_machine_impl;
+    class rpc_impl;
+
+    friend class raft_server;
+
+    struct raft_group_state {
+        bool has_tablet = false;
+        lw_shared_ptr<gate> gate = nullptr;
+        raft::server* server = nullptr;
+        shared_future<> server_op = make_ready_future<>();
+    };
+
+    netw::messaging_service& _ms;
+    raft_group_registry& _raft_gr;
+    cql3::query_processor& _qp;
+    replica::database& _db;
+    gms::feature_service& _features;
+    std::unordered_map<raft::group_id, raft_group_state> _raft_groups = {};
+    locator::token_metadata_ptr _pending_tm = nullptr;
+    bool _started = false;
+    gms::feature::listener_registration _feature_listener{};
+
+    future<> start_raft_group(locator::global_tablet_id tablet,
+        raft::group_id group_id,
+        locator::token_metadata_ptr tm);
+
+    void schedule_raft_group_deletion(raft::group_id group_id, raft_group_state& group_state);
+
+    void schedule_raft_groups_deletion(bool all);
+
+    future<> wait_for_groups_to_start();
+
+public:
+    groups_manager(netw::messaging_service& ms, raft_group_registry& raft_gr, 
+        cql3::query_processor& qp, replica::database& _db,
+        gms::feature_service& features);
+
+    // Called whenever a new token_metadata is published on this shard.
+    // Starts raft::server instances for all strongly consistent tablets now
+    // residing on this shard, and schedules removal of servers for tablets
+    // that have moved away.
+    //
+    // Note that the method is synchronous: it only initiates these operations
+    // and does not wait for their completion.
+    void update(locator::token_metadata_ptr new_tm);
+
+    // Used to submit write commands and perform read_barrier() before reads.
+    future<raft_server> acquire_server(raft::group_id group_id);
+
+    // Called during node boot. Waits for all raft::server instances corresponding
+    // to the latest group0 state to start.
+    future<> start();
+
+    // Called during node shutdown. Waits for all raft::server instances to stop.
+    future<> stop();
+};
+
+class raft_server {
+private:
+    groups_manager::raft_group_state& _state;
+    gate::holder _holder;
+
+public:
+    raft_server(groups_manager::raft_group_state& state, gate::holder holder);
+
+    raft::server& server() {
+        return *_state.server;
+    }
+};
+
+}
