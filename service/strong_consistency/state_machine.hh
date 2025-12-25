4eee5bc27380defc938e258b2feae537179b2c28
Petr Gusev <petr.gusev@scylladb.com>
2025-12-25 11:04:49 +0100

    strong_consistency: add state_machine and raft_command
    These commands will be used by strongly consistent tablets to submit
    mutations to Raft. A simple state_machine implementation is introduced
    to apply these commands.
    
    We apply commands in batches to reduce commitlog I/O overhead. The
    batched variant of database::apply has known atomicity issues. For
    example, it does not guarantee atomicity under memory pressure: some
    mutations may be published to the memtable while others are blocked in
    run_when_memory_available. We will address these issues later.

---
+++ service/strong_consistency/state_machine.hh
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include "service/raft/raft_state_machine.hh"
+#include "mutation/frozen_mutation.hh"
+#include "locator/tablets.hh"
+
+namespace service::strong_consistency {
+
+struct raft_command {
+    frozen_mutation mutation;
+};
+std::unique_ptr<raft_state_machine> make_state_machine(locator::global_tablet_id tablet,
+    raft::group_id gid,
+    replica::database& db);
+
+}
