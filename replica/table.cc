bhalevy/scylla

replica: table, storage_group, compaction_group: add needs_flush
Table needs flush if not all its memtable lists are empty.
To be used in the next patch for a unit test.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

replica/table.cc
@@ -3456,6 +3456,10 @@ bool compaction_group::can_flush() const {
     return _memtables->can_flush();
 }
 
+bool compaction_group::needs_flush() const {
+    return _memtables->needs_flush();
+}
+
 lw_shared_ptr<memtable_list>& compaction_group::memtables() noexcept {
     return _memtables;
 }
@@ -3488,6 +3492,14 @@ bool storage_group::can_flush() const {
 
 bool table::can_flush() const {
     return std::ranges::any_of(storage_groups() | std::views::values, std::mem_fn(&storage_group::can_flush));
+}
+
+bool storage_group::needs_flush() const {
+    return std::ranges::any_of(compaction_groups(), std::mem_fn(&compaction_group::needs_flush));
+}
+
+bool table::needs_flush() const {
+    return std::ranges::any_of(storage_groups() | std::views::values, std::mem_fn(&storage_group::needs_flush));
 }
 
 future<> compaction_group::clear_memtables() {

scylladb/scylladb

replica/table.cc
@@ -3598,6 +3598,10 @@ bool compaction_group::can_flush() const {
     return _memtables->can_flush();
 }
 
+bool compaction_group::needs_flush() const {
+    return _memtables->needs_flush();
+}
+
 lw_shared_ptr<memtable_list>& compaction_group::memtables() noexcept {
     return _memtables;
 }
@@ -3634,6 +3638,14 @@ bool storage_group::can_flush() const {
 
 bool table::can_flush() const {
     return std::ranges::any_of(storage_groups() | std::views::values, std::mem_fn(&storage_group::can_flush));
+}
+
+bool storage_group::needs_flush() const {
+    return std::ranges::any_of(compaction_groups_immediate(), std::mem_fn(&compaction_group::needs_flush));
+}
+
+bool table::needs_flush() const {
+    return std::ranges::any_of(storage_groups() | std::views::values, std::mem_fn(&storage_group::needs_flush));
 }
 
 future<> compaction_group::clear_memtables() {