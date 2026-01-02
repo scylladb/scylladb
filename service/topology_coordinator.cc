7a04dd2d22f4b21adad6bef918888c0a517771ab
Tomasz Grabiec <tgrabiec@scylladb.com>
2026-01-03 00:17:30 +0100

    topology_coordinator: Wake up the state machine after stats refresh
    Otherwise, coordinator may not react to changing stats after explicit
    calls to trigger_load_stats_refresh() done on node replace or table
    creation, if stats take longer to refresh than it takes the
    coordinator to go idle.
    
    The periodic refresh does wake up the topology coordinator, so the
    issue is not dramatic in production, but it's annoying in tests, which
    take longer because of that.
    
    Fixes #25163

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -3866,6 +3866,7 @@ future<> topology_coordinator::refresh_tablet_load_stats() {
     rtlogger.debug("raft topology: Refreshed table load stats for all DC(s).");
 
     _tablet_allocator.set_load_stats(make_lw_shared<const locator::load_stats>(std::move(stats)));
+    _topo_sm.event.broadcast(); // wake up load balancer.
 }
 
 future<> topology_coordinator::start_tablet_load_stats_refresher() {
@@ -3874,7 +3875,6 @@ future<> topology_coordinator::start_tablet_load_stats_refresher() {
         bool sleep = true;
         try {
             co_await _tablet_load_stats_refresh.trigger();
-            _topo_sm.event.broadcast(); // wake up load balancer.
         } catch (raft::request_aborted&) {
             rtlogger.debug("raft topology: Tablet load stats refresher aborted");
             sleep = false;
