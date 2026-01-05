7eaf2d6802c66c5ae72c2332f1571d7591f88b4d
Radosław Cybulski <radoslaw.cybulski@scylladb.com>
2026-01-05 19:14:49 +0100

    alternator: improve trace logging
    Alternator, when logging is set to trace, will emit `Request` logs
    with incoming body. This patch adds missing return value and an
    unique identifier per request, so requests and their responses
    could be easily matched.

--- alternator/server.cc
+++ alternator/server.cc
@@ -654,6 +654,9 @@ ungzip(chunked_content&& compressed_body, size_t length_limit) {
 }
 
 future<executor::request_return_type> server::handle_api_request(std::unique_ptr<request> req) {
+    thread_local unsigned int tl_request_identifier = 0;
+    unsigned int request_identifier, request_shard_id;
+
     _executor._stats.total_operations++;
     sstring target = req->get_header("X-Amz-Target");
     // target is DynamoDB API version followed by a dot '.' and operation type (e.g. CreateTable)
@@ -718,7 +721,9 @@ future<executor::request_return_type> server::handle_api_request(std::unique_ptr
         req->get_protocol_name() == "https");
 
     if (slogger.is_enabled(log_level::trace)) {
-        slogger.trace("Request: {} {} {}", op, truncated_content_view(content, _max_users_query_size_in_trace_output).as_view(), req->_headers);
+        request_identifier = ++tl_request_identifier;
+        request_shard_id = seastar::this_shard_id();
+        slogger.trace("Request: {} {}:{} {} {}", op, request_identifier, request_shard_id, truncated_content_view(content, _max_users_query_size_in_trace_output).as_view(), req->_headers);
     }
     auto callback_it = _callbacks.find(op);
     if (callback_it == _callbacks.end()) {
@@ -744,13 +749,42 @@ future<executor::request_return_type> server::handle_api_request(std::unique_ptr
     auto user = client_state.user();
     auto f = [this, content = std::move(content), &callback = callback_it->second,
             client_state = std::move(client_state), trace_state = std::move(trace_state),
-            units = std::move(units), req = std::move(req)] () mutable -> future<executor::request_return_type> {
+            units = std::move(units), req = std::move(req), op = std::move(op), request_identifier, request_shard_id] () mutable -> future<executor::request_return_type> {
                 rjson::value json_request = co_await _json_parser.parse(std::move(content));
                 if (!json_request.IsObject()) {
                     co_return api_error::validation("Request content must be an object");
                 }
-                co_return co_await callback(_executor, client_state, trace_state,
-                    make_service_permit(std::move(units)), std::move(json_request), std::move(req));
+                executor::request_return_type res;
+                try {
+                    res = co_await callback(_executor, client_state, trace_state,
+                        make_service_permit(std::move(units)), std::move(json_request), std::move(req));
+                }
+                catch(const std::exception& e) {
+                    if (slogger.is_enabled(log_level::trace)) {
+                        slogger.trace("Response exception: {} {}:{} {}", op, request_identifier, request_shard_id, e.what());
+                    }
+                    throw;
+                }
+                catch(...) {
+                    if (slogger.is_enabled(log_level::trace)) {
+                        slogger.trace("Response unknown exception: {} {}:{}", op, request_identifier, request_shard_id);
+                    }
+                    throw;
+                }
+                if (slogger.is_enabled(log_level::trace)) {
+                    std::visit(overloaded_functor {
+                        [&] (const std::string& str) {
+                            slogger.trace("Response: {} {}:{} {}", op, request_identifier, request_shard_id, str);
+                        },
+                        [&] (const executor::body_writer& body_writer) {
+                            slogger.trace("Response: {} {}:{} <body_writer>", op, request_identifier, request_shard_id);
+                        },
+                        [&] (const api_error& err) {
+                            slogger.trace("Response error: {} {}:{} {} {}", op, request_identifier, request_shard_id, err._type, err._msg);
+                        }
+                    }, res);
+                }
+                co_return std::move(res);
     };
     co_return co_await _sl_controller.with_user_service_level(user, std::ref(f));
 }
