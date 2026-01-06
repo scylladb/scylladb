be4755756130f09ae56e96ec698ccede88a19394
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2026-01-06 18:31:14 +0200

    address_provider: retry on failure
    If connecting to a provided address throws, reset the address list in
    address_provider and retry once (and only once) before giving up.

--- utils/http.cc
+++ utils/http.cc
@@ -117,6 +117,13 @@ utils::http::dns_connection_factory::dns_connection_factory(std::string uri, log
 {}
 
 future<connected_socket> utils::http::dns_connection_factory::make(abort_source*) {
+    try {
+        co_return co_await connect();
+    } catch (...) {
+        // On failure, reset the address provider and try again
+        _logger.debug("Connection failed, resetting address provider and retrying: {}", std::current_exception());
+    }
+    co_await _addr_provider.reset();
     co_return co_await connect();
 }
 
