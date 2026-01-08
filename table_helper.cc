71f537c82367be96d34f326011a34d21f75ba6f4
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2026-01-08 15:50:28 +0200

    table_helper: Pass initial_tablets to setup_keyspace()
    We have two system keyspaces that we need to move to tablets and they
    both use the `setup_keyspace` helper, which currently works with vnodes.
    
    Expose the `initial_tablets` option in the signature to allow selecting
    between vnodes and tablets.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- table_helper.cc
+++ table_helper.cc
@@ -142,7 +142,7 @@ future<> table_helper::insert(cql3::query_processor& qp, service::migration_mana
 }
 
 future<> table_helper::setup_keyspace(cql3::query_processor& qp, service::migration_manager& mm, std::string_view keyspace_name, sstring replication_strategy_name,
-                                      sstring replication_factor, service::query_state& qs, std::vector<table_helper*> tables) {
+                                      sstring replication_factor, service::query_state& qs, std::vector<table_helper*> tables, std::optional<unsigned int> initial_tablets) {
     if (this_shard_id() != 0) {
         co_return;
     }
@@ -176,7 +176,7 @@ future<> table_helper::setup_keyspace(cql3::query_processor& qp, service::migrat
             else {
                 opts["replication_factor"] = replication_factor;
             }
-            auto ksm = keyspace_metadata::new_keyspace(keyspace_name, replication_strategy_name, std::move(opts), std::nullopt, std::nullopt, true);
+            auto ksm = keyspace_metadata::new_keyspace(keyspace_name, replication_strategy_name, std::move(opts), initial_tablets, std::nullopt, true);
             try {
                 co_await mm.announce(service::prepare_new_keyspace_announcement(db.real_database(), ksm, ts),
                         std::move(group0_guard), seastar::format("table_helper: create {} keyspace", keyspace_name));
