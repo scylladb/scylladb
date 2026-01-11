9d7055d3f5d353b4f0c3fdf96497d3927f966093
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2026-01-11 12:44:43 +0200

    connection_factory: get rid of shared_future in dns_connection_factory
    Move state management from dns_connection_factory into state class
    itself to encapsulate its internal state and stop managing it from the
    `dns_connection_factory`
    
    (cherry picked from commit 66a33619da64fb2beced37713076577d289b2f46)

--- utils/http.cc
+++ utils/http.cc
@@ -23,37 +23,51 @@ future<shared_ptr<tls::certificate_credentials>> utils::http::system_trust_crede
     co_return system_trust_credentials;
 }
 
-utils::http::dns_connection_factory::state::state(shared_ptr<tls::certificate_credentials> cin) 
-    : creds(std::move(cin))
-{}
+future<> utils::http::dns_connection_factory::init_addresses() {
+    auto hent = co_await net::dns::get_host_by_name(_host, net::inet_address::family::INET);
+    _addr_list = std::move(hent.addr_list);
+    _logger.debug("Initialized addresses={}", _addr_list);
+}
 
-future<> utils::http::dns_connection_factory::initialize(lw_shared_ptr<state> state, std::string host, bool use_https, logging::logger& logger) {
-    co_await coroutine::all(
-        [state, host] () -> future<> {
-            auto hent = co_await net::dns::get_host_by_name(host, net::inet_address::family::INET);
-            state->addr_list = std::move(hent.addr_list);
-        },
-        [state, use_https] () -> future<> {
-            if (use_https && !state->creds) {
-                state->creds = co_await system_trust_credentials();
-            }
-            if (!use_https) {
-                state->creds = {};
-            }
+future<> utils::http::dns_connection_factory::init_credentials() {
+    if (_use_https && !_creds) {
+        _creds = co_await system_trust_credentials();
+    }
+    if (!_use_https) {
+        _creds = {};
+    }
+    _logger.debug("Initialized credentials, tls={}", _creds == nullptr ? "no" : "yes");
+}
+
+future<net::inet_address> utils::http::dns_connection_factory::get_address() {
+    if (!_addr_init) [[unlikely]] {
+        auto units = co_await get_units(_init_semaphore, 1);
+        if (!_addr_init) {
+            co_await init_addresses();
+            _addr_init = true;
         }
-    );
+    }
+    co_return _addr_list[_addr_pos++ % _addr_list.size()];
+}
 
-    state->initialized = true;
-    logger.debug("Initialized factory, addresses={} tls={}", state->addr_list, state->creds == nullptr ? "no" : "yes");
+future<shared_ptr<tls::certificate_credentials>> utils::http::dns_connection_factory::get_creds() {
+    if (!_creds_init) [[unlikely]] {
+        auto units = co_await get_units(_init_semaphore, 1);
+        if (!_creds_init) {
+            co_await init_credentials();
+            _creds_init = true;
+        }
+    }
+    co_return _creds;
 }
 
 future<connected_socket> utils::http::dns_connection_factory::connect() {
-    auto socket_addr = socket_address(_state->addr_list[_addr_pos++ % _state->addr_list.size()], _port);
-    if (_state->creds) {
-        _logger.debug("Making new HTTPS connection addr={} host={}", _state->addr_list, _host);
-        co_return co_await tls::connect(_state->creds, socket_addr, tls::tls_options{.server_name = _host});
+    auto socket_addr = socket_address(co_await get_address(), _port);
+    if (auto creds = co_await get_creds()) {
+        _logger.debug("Making new HTTPS connection addr={} host={}", socket_addr, _host);
+        co_return co_await tls::connect(creds, socket_addr, tls::tls_options{.server_name = _host});
     }
-    _logger.debug("Making new HTTP connection addr={} host={}", _state->addr_list, _host);
+    _logger.debug("Making new HTTP connection addr={} host={}", socket_addr, _host);
     co_return co_await seastar::connect(socket_addr, {}, transport::TCP);
 }
 
@@ -62,17 +76,11 @@ utils::http::dns_connection_factory::dns_connection_factory(dns_connection_facto
 utils::http::dns_connection_factory::dns_connection_factory(std::string host, int port, bool use_https, logging::logger& logger, shared_ptr<tls::certificate_credentials> certs)
     : _host(std::move(host))
     , _port(port)
-    , _use_https(use_https)
     , _logger(logger)
-    , _state(make_lw_shared<state>(std::move(certs)))
-    , _done(initialize(_state, _host, _use_https, _logger))
-{}
+    ,_creds(std::move(certs))
+    ,_use_https(use_https) {
+}
 
 future<connected_socket> utils::http::dns_connection_factory::make(abort_source*) {
-    if (!_state->initialized) {
-        _logger.debug("Waiting for factory to initialize");
-        co_await _done.get_future();
-    }
-
     co_return co_await connect();
 }
