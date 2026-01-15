d6226500f6f74b3a2afa0786670a67c0eed3b1db
Szymon Malewski <szymon.malewski@scylladb.com>
2026-01-15 12:05:38 +0100

    vector_search: Add more rescoring validation tests
    Adding tests for specific cases of rescoring processing:
    - wildcard selection - "SELECT * ..." is a case with slightly different path of rescoring processing. We want to confirm that it is handled correctly.
    - calculating similarity with other vectors in SELECT clause should not influence ANN ordering.
    - NULL handling - results that for any reason have NULL in a score should be filtered out.
    
    As rescoring is not implemented yet, the tests use boost::unit_test::expected_failures
    to indicate that the test reports errors.

--- test/vector_search/rescoring_test.cc
+++ test/vector_search/rescoring_test.cc
@@ -7,6 +7,8 @@
  */
 
 #include "types/types.hh"
+#include "types/vector.hh"
+#include "cql3/util.hh"
 #include "utils.hh"
 #include "configure.hh"
 #include "vector_search/vector_store_client.hh"
@@ -77,7 +79,33 @@ float get_similarity_col_value(const auto& row, int index = 1) {
     return value_cast<float>(float_type->deserialize(row.at(index).value()));
 }
 
+std::vector<float> get_embedding_col_value(const auto& row, int index = 1) {
+    auto bytes = row.at(index).value();
+    auto vec_type = vector_type_impl::get_instance(float_type, 2);
+    auto values = value_cast<vector_type_impl::native_type>(vec_type->deserialize(bytes));
+    return cql3::util::to_vector<float>(values);
+}
+
+auto to_embedding(const std::vector<float>& vec) {
+    auto vector_type = vector_type_impl::get_instance(float_type, vec.size());
+    std::vector<data_value> vector_data_values;
+    for (float f : vec) {
+        vector_data_values.push_back(data_value(f));
+    }
+    return vector_type->decompose(make_vector_value(vector_type, vector_data_values));
+}
+
 } // namespace
+
+// Enable BOOST_CHECK_EQUAL to print std::vector<float> values
+namespace boost::test_tools::tt_detail {
+template<>
+struct print_log_value<std::vector<float>> {
+    void operator()(std::ostream& os, const std::vector<float>& vec) const {
+        os << fmt::format("[{}]", fmt::join(vec, ", "));
+    }
+};
+}
 
 SEASTAR_TEST_CASE(index_option_quantization_valid_values) {
     std::vector<sstring> supported_quantizations = {"f32", "f16", "bf16", "i8", "b1", "F32", "F16", "BF16", "I8", "B1"};
@@ -317,6 +345,122 @@ SEASTAR_TEST_CASE(similarity_function_returns_correctly_rescored_results, *boost
                         BOOST_CHECK_EQUAL(get_id_col_value(rows.at(1)), 2);
                         BOOST_CHECK(is_similarity_eq(get_similarity_col_value(rows.at(1)), params.expected_similarity[1]));
                     }
+                },
+                make_config(format("http://server.node:{}", server->port())))
+                .finally(seastar::coroutine::lambda([&] -> future<> {
+                    co_await server->stop();
+                }));
+    }
+}
+
+// Rescoring is not implemented yet (see https://scylladb.atlassian.net/browse/SCYLLADB-83)
+// Test is expected to report errors until then.
+SEASTAR_TEST_CASE(wildcard_select_is_correctly_rescored, *boost::unit_test::expected_failures(12)) {
+    // Another case with slightly different path of processing is "SELECT * ...".
+
+    for (const auto& params : test_data) {
+        auto server = co_await make_vs_mock_server();
+        co_await do_with_cql_env(
+                [&](cql_test_env& env) -> future<> {
+                    configure(env.local_qp().vector_store_client()).with_dns({{"server.node", std::vector<std::string>{server->host()}}});
+                    env.local_qp().vector_store_client().start_background_tasks();
+                    co_await create_index_and_insert_data(env, params);
+
+                    // Mock Response: Return all keys but in REVERSE similarity order.
+                    server->next_ann_response({http::reply::status_type::ok, R"({
+                        "primary_keys": { "id": [4, 3, 2, 1] },
+                        "distances": [0, 0, 0, 0]
+                    })"});
+                    auto msg = co_await env.execute_cql("SELECT * FROM ks.cf ORDER BY embedding ANN OF [0.1, 0.1] LIMIT 2;");
+
+                    auto rms = dynamic_pointer_cast<cql_transport::messages::result_message::rows>(msg);
+                    BOOST_REQUIRE(rms);
+                    const auto& rows = rms->rs().result_set().rows();
+                    BOOST_REQUIRE(rows.size() >= 2);
+                    BOOST_CHECK_EQUAL(rows.size(), 2);
+                    BOOST_CHECK_EQUAL(rms->rs().result_set().get_metadata().column_count(), 2);
+                    BOOST_CHECK_EQUAL(get_id_col_value(rows.at(0)), test_ids[0]);
+                    BOOST_CHECK_EQUAL(get_embedding_col_value(rows.at(0)), params.vectors[0]);
+                    BOOST_CHECK_EQUAL(get_id_col_value(rows.at(1)), 2);
+                    BOOST_CHECK_EQUAL(get_embedding_col_value(rows.at(1)), params.vectors[1]);
+                },
+                make_config(format("http://server.node:{}", server->port())))
+                .finally(seastar::coroutine::lambda([&] -> future<> {
+                    co_await server->stop();
+                }));
+    }
+}
+
+// Rescoring is not implemented yet (see https://scylladb.atlassian.net/browse/SCYLLADB-83)
+// Test is expected to report errors until then.
+SEASTAR_TEST_CASE(select_similarity_function_other_than_ann_ordering, *boost::unit_test::expected_failures(4)) {
+    // Another tricky case with similarity column with argument different from ANN ordering vector.
+    // Especially if we use prepared statement and the difference is only seen at execution time.
+    const auto& params = test_data[0];
+    auto server = co_await make_vs_mock_server();
+    co_await do_with_cql_env(
+            [&](cql_test_env& env) -> future<> {
+                configure(env.local_qp().vector_store_client()).with_dns({{"server.node", std::vector<std::string>{server->host()}}});
+                env.local_qp().vector_store_client().start_background_tasks();
+                co_await create_index_and_insert_data(env, params);
+
+                // Mock Response: Return all keys but in REVERSE similarity order.
+                server->next_ann_response({http::reply::status_type::ok, R"({
+                    "primary_keys": { "id": [4, 3, 2, 1] },
+                    "distances": [0, 0, 0, 0]
+                })"});
+                auto prep = co_await env.prepare(fmt::format(
+                        "SELECT id, similarity_{}(embedding, ?) FROM ks.cf ORDER BY embedding ANN OF ? LIMIT 2;", params.function_name));
+                auto msg = co_await env.execute_prepared(prep, cql3::raw_value_vector_with_unset{
+                    cql3::raw_value::make_value(to_embedding(params.vectors[1])),
+                    cql3::raw_value::make_value(to_embedding({0.1f, 0.1f}))});
+
+                auto rms = dynamic_pointer_cast<cql_transport::messages::result_message::rows>(msg);
+                BOOST_REQUIRE(rms);
+                const auto& rows = rms->rs().result_set().rows();
+                BOOST_REQUIRE(rows.size() >= 2);
+                BOOST_CHECK_EQUAL(rows.size(), 2);
+                BOOST_CHECK_EQUAL(rms->rs().result_set().get_metadata().column_count(), 2);
+                BOOST_CHECK_EQUAL(get_id_col_value(rows.at(0)), 1);
+                BOOST_CHECK(is_similarity_eq(get_similarity_col_value(rows.at(0)), params.expected_similarity[1]));
+                BOOST_CHECK_EQUAL(get_id_col_value(rows.at(1)), 2);
+                BOOST_CHECK(is_similarity_eq(get_similarity_col_value(rows.at(1)), params.expected_similarity[0]));
+            },
+            make_config(format("http://server.node:{}", server->port())))
+            .finally(seastar::coroutine::lambda([&] -> future<> {
+                co_await server->stop();
+            }));
+}
+
+// Rescoring is not implemented yet (see https://scylladb.atlassian.net/browse/SCYLLADB-83)
+// Test is expected to report errors until then.
+SEASTAR_TEST_CASE(no_nulls_in_rescored_results, *boost::unit_test::expected_failures(6)) {
+
+    for (const auto& params : test_data) {
+        auto server = co_await make_vs_mock_server();
+        co_await do_with_cql_env(
+                [&](cql_test_env& env) -> future<> {
+                    configure(env.local_qp().vector_store_client()).with_dns({{"server.node", std::vector<std::string>{server->host()}}});
+                    env.local_qp().vector_store_client().start_background_tasks();
+                    co_await create_index_and_insert_data(env, params);
+                    co_await env.execute_cql(fmt::format("INSERT INTO ks.cf (id, embedding) VALUES (17, NULL)"));
+                    co_await env.execute_cql(fmt::format("INSERT INTO ks.cf (id, embedding) VALUES (16, [0.1, NaN])"));
+                    co_await env.execute_cql(fmt::format("INSERT INTO ks.cf (id, embedding) VALUES (15, [INFINITY, 0.1])"));
+
+                    // Mock Response: Return all keys but in REVERSE similarity order.
+                    server->next_ann_response({http::reply::status_type::ok, R"({
+                        "primary_keys": { "id": [55, 17, 16, 15, 2] },
+                        "distances": [0, 0, 0, 0, 0]
+                    })"});
+                    auto msg = co_await env.execute_cql("SELECT id FROM ks.cf ORDER BY embedding ANN OF [0.1, 0.1] LIMIT 3;");
+
+                    auto rms = dynamic_pointer_cast<cql_transport::messages::result_message::rows>(msg);
+                    BOOST_REQUIRE(rms);
+                    const auto& rows = rms->rs().result_set().rows();
+                    BOOST_REQUIRE(rows.size() >= 1);
+                    BOOST_CHECK_EQUAL(rows.size(), 1);
+                    BOOST_CHECK_EQUAL(rms->rs().result_set().get_metadata().column_count(), 1);
+                    BOOST_CHECK_EQUAL(get_id_col_value(rows.at(0)), 2);
                 },
                 make_config(format("http://server.node:{}", server->port())))
                 .finally(seastar::coroutine::lambda([&] -> future<> {
