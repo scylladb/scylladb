2ea3b981ee53f8b336dde53ad222898e61c42ed5
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2026-01-19 15:42:05 +0100

    db: add columns to system_schema.keyspaces
    Add two new columns to system_schema.keyspaces table: previous_replication
    and next_replication.
    
    While there is an ongoing RF change:
    - previous_replication keeps initial RF values - the ones we started the RF
      change with;
    - next_replication keeps the target RF values;
    - existing replication_v2 column keeps the current RF values - the ones
      that are determined by the number of tablet replicas.
    
    DESCRIBE KEYSPACE statement shows previous_replication until the request
    finishes.
    
    When there is no ongoing RF change for this keyspace, its
    previous_replication and next_replication are empty.
    
    In this commit no data is kept in the new columns.

--- cql3/statements/ks_prop_defs.cc
+++ cql3/statements/ks_prop_defs.cc
@@ -440,7 +440,7 @@ lw_shared_ptr<data_dictionary::keyspace_metadata> ks_prop_defs::as_ks_metadata_u
         sc = old->strategy_name();
         options = old_options;
     }
-    return data_dictionary::keyspace_metadata::new_keyspace(old->name(), *sc, options, initial_tablets, get_consistency_option(), get_boolean(KW_DURABLE_WRITES, true), get_storage_options());
+    return data_dictionary::keyspace_metadata::new_keyspace(old->name(), *sc, options, initial_tablets, get_consistency_option(), get_boolean(KW_DURABLE_WRITES, true), get_storage_options(), {}, old->previous_strategy_options_opt(), old->next_strategy_options_opt());
 }
 
 namespace {
