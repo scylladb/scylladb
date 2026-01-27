bd31ef1c5c2affd83eeba1ac92fd3a66f061927d
Botond Dénes <bdenes@scylladb.com>
2026-01-27 08:48:46 +0200

    streaming/stream_transfer_task: send_mutation_fragments(): coroutinize outer code
    The inner source_op and sink_op are left as continuations, to be
    converted in a next patch.
    
    Sprinkle some new lines around to make the code more readable.
    
    Indentation is left broken.

--- streaming/stream_transfer_task.cc
+++ streaming/stream_transfer_task.cc
@@ -108,16 +108,18 @@ struct send_info {
 };
 
 future<> send_mutation_fragments(lw_shared_ptr<send_info> si) {
- return si->reader.has_more_fragments().then([si] (bool there_is_more) {
-  if (!there_is_more) {
+  if (!co_await si->reader.has_more_fragments()) {
     // The reader contains no data
     sslog.info("[Stream #{}] Skip sending ks={}, cf={}, reader contains no data, with new rpc streaming",
         si->plan_id, si->cf->schema()->ks_name(), si->cf->schema()->cf_name());
-    return make_ready_future<>();
+    co_return;
   }
-  return si->estimate_partitions().then([si] (size_t estimated_partitions) {
+
+  const auto estimated_partitions = co_await si->estimate_partitions();
+
     sslog.info("[Stream #{}] Start sending ks={}, cf={}, estimated_partitions={}, with new rpc streaming", si->plan_id, si->cf->schema()->ks_name(), si->cf->schema()->cf_name(), estimated_partitions);
-    return si->ms.make_sink_and_source_for_stream_mutation_fragments(si->reader.schema()->version(), si->plan_id, si->cf_id, estimated_partitions, si->reason, si->topo_guard, si->id).then_unpack([si] (rpc::sink<frozen_mutation_fragment, stream_mutation_fragments_cmd> sink, rpc::source<int32_t> source) mutable {
+    auto [sink, source] = co_await si->ms.make_sink_and_source_for_stream_mutation_fragments(si->reader.schema()->version(), si->plan_id, si->cf_id, estimated_partitions, si->reason, si->topo_guard, si->id);
+
         auto got_error_from_peer = make_lw_shared<bool>(false);
         auto table_is_dropped = make_lw_shared<bool>(false);
 
@@ -143,7 +145,7 @@ future<> send_mutation_fragments(lw_shared_ptr<send_info> si) {
                     }
                 });
             });
-        }();
+        };
 
         auto sink_op = [sink, si, got_error_from_peer] () mutable -> future<> {
             mutation_fragment_stream_validator validator(*(si->reader.schema()));
@@ -181,9 +183,10 @@ future<> send_mutation_fragments(lw_shared_ptr<send_info> si) {
                     return sink.close();
                 });
             });
-        }();
+        };
 
-        return when_all_succeed(std::move(source_op), std::move(sink_op)).then_unpack([got_error_from_peer, table_is_dropped, si] {
+        co_await coroutine::all(std::move(source_op), std::move(sink_op));
+
             if (*got_error_from_peer) {
                 if (*table_is_dropped) {
                      sslog.info("[Stream #{}] Skipped streaming the dropped table {}.{}", si->plan_id, si->cf->schema()->ks_name(), si->cf->schema()->cf_name());
@@ -191,10 +194,6 @@ future<> send_mutation_fragments(lw_shared_ptr<send_info> si) {
                     throw std::runtime_error(format("Peer failed to process mutation_fragment peer={}, plan_id={}, cf_id={}", si->id, si->plan_id, si->cf_id));
                 }
             }
-        });
-    });
-  });
- });
 }
 
 future<> stream_transfer_task::execute() {
