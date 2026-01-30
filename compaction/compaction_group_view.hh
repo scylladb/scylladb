a915a7ea2886f8fdd04bc9b26324449dee63d009
Taras Veretilnyk <taras.veretilnyk@scylladb.com>
2026-01-30 03:22:45 +0100

    compaction: add compaction_group_view method to specify sstable version
    Add make_sstable() overload that accepts sstable_version_types parameter
    to compaction_group_view interface and all implementations.
    This will be useful in rewrite component mechanism, as we
    need to preserve sstable version when creating the new one for the replacement.

--- compaction/compaction_group_view.hh
+++ compaction/compaction_group_view.hh
@@ -46,6 +46,7 @@ public:
     virtual reader_permit make_compaction_reader_permit() const = 0;
     virtual sstables::sstables_manager& get_sstables_manager() noexcept = 0;
     virtual sstables::shared_sstable make_sstable(sstables::sstable_state) const = 0;
+    virtual sstables::shared_sstable make_sstable(sstables::sstable_state, sstables::sstable_version_types) const = 0;
     virtual sstables::sstable_writer_config configure_writer(sstring origin) const = 0;
     virtual api::timestamp_type min_memtable_timestamp() const = 0;
     virtual api::timestamp_type min_memtable_live_timestamp() const = 0;
