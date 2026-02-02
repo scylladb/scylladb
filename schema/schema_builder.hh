d4c6d44536c22fdef73b07fd6e9291ce255bf889
Wojciech Mitros <wojciech.mitros@scylladb.com>
2026-02-02 03:58:35 +0100

    schema: add with_sharder overload accepting static_sharder reference
    Add a schema_builder::with_sharder() overload that accepts a const
    reference to dht::static_sharder. This allows schemas to use custom
    sharder instances instead of only static sharder configurations.
    
    This is needed to support tables that use custom partitioning and
    sharding strategies, such as the incoming raft metadata tables for
    strongly consistent tables.

--- schema/schema_builder.hh
+++ schema/schema_builder.hh
@@ -246,6 +246,7 @@ public:
 
     schema_builder& with_partitioner(sstring name);
     schema_builder& with_sharder(unsigned shard_count, unsigned sharding_ignore_msb_bits);
+    schema_builder& with_sharder(const dht::static_sharder& sharder);
     schema_builder& set_in_memory(bool in_memory) {
         _raw._in_memory = in_memory;
         return *this;
