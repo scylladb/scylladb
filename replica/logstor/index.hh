436bb3235a3f5eea940c1adb5bb0151fe515dd35
Michael Litvak <michael.litvak@scylladb.com>
2026-02-02 08:29:23 +0100

    logstor: index: buckets
    divide the primary index to buckets, each bucket containing a btree. the
    bucket is determined by using bits from the key hash.

--- replica/logstor/index.hh
+++ replica/logstor/index.hh
@@ -18,13 +18,13 @@ struct index_key_less {
     }
 };
 
-class log_index {
+class log_index_bucket {
     using index_tree = bplus::tree<index_key, index_entry, index_key_less, 16>;
 
     index_tree _index;
 
 public:
-    log_index() : _index(index_key_less{}) {}
+    log_index_bucket() : _index(index_key_less{}) {}
 
     std::optional<index_entry> get(const index_key& key) const {
         auto it = _index.find(key);
@@ -55,6 +55,41 @@ public:
         }
         return false;
     }
+};
+
+class log_index {
+
+    static constexpr size_t NUM_BUCKETS = 1 << 20; // power of 2
+
+    std::array<log_index_bucket, NUM_BUCKETS> _buckets;
+
+    size_t bucket_index(const index_key& key) const noexcept {
+        uint32_t prefix;
+        std::memcpy(&prefix, key.digest.data(), sizeof(prefix));
+        return static_cast<size_t>(prefix & (NUM_BUCKETS - 1));
+    }
+
+    log_index_bucket& get_bucket(const index_key& key) noexcept {
+        return _buckets[bucket_index(key)];
+    }
+
+    const log_index_bucket& get_bucket(const index_key& key) const noexcept {
+        return _buckets[bucket_index(key)];
+    }
+
+public:
+    std::optional<index_entry> get(const index_key& key) const {
+        return get_bucket(key).get(key);
+    }
+
+    std::optional<index_entry> exchange(const index_key& key, index_entry new_entry) {
+        return get_bucket(key).exchange(key, std::move(new_entry));
+    }
+
+    bool update_record_location(const index_key& key, log_location old_location, log_location new_location) {
+        return get_bucket(key).update_record_location(key, old_location, new_location);
+    }
+
 };
 
 }
