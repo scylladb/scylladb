ae10b5a89775e61b2931e0930a973b1f11e8bdaf
Calle Wilund <calle@scylladb.com>
2026-02-03 13:16:14 +0100

    test::test_snapshot_with_tablets: Add small test for topo coordinated snapshot

--- test/pylib/rest_client.py
+++ test/pylib/rest_client.py
@@ -391,10 +391,19 @@ class ScyllaRESTAPIClient:
             params['scope'] = scope
         return await self.client.post_json(f"/storage_service/restore", host=node_ip, params=params, json=sstables)
 
-    async def take_snapshot(self, node_ip: str, ks: str, tag: str) -> None:
+    async def take_snapshot(self, node_ip: str, ks: str, tag: str, tables: list[str] = None) -> None:
         """Take keyspace snapshot"""
         params = { 'kn': ks, 'tag': tag }
+        if tables:
+            params['cf'] = ','.join(tables)
         await self.client.post(f"/storage_service/snapshots", host=node_ip, params=params)
+
+    async def take_cluster_snapshot(self, node_ip: str, ks: str, tag: str, tables: list[str] = None) -> None:
+        """Take keyspace snapshot"""
+        params = { 'keyspace': ks, 'tag': tag }
+        if tables:
+            params['table'] = ','.join(tables)
+        await self.client.post(f"/storage_service/tablets/snapshots", host=node_ip, params=params)
 
     async def cleanup_keyspace(self, node_ip: str, ks: str) -> None:
         """Cleanup keyspace"""
