642aa449370ed8fa905bb7615926a915b473491a
Calle Wilund <calle@scylladb.com>
2026-02-03 12:32:19 +0100

    topology_state_machine: Add snapshot_tables operation
    Note: placed after "noop" op, to not confuse ops in a mixed
    (upgrading) cluster

--- service/topology_coordinator.cc
+++ service/topology_coordinator.cc
@@ -1158,6 +1158,8 @@ class topology_coordinator : public endpoint_lifecycle_subscriber
             co_await update_topology_state(std::move(guard), std::move(updates), "no-op request completed");
         }
         break;
+        case global_topology_request::snapshot_tables: 
+        break;
         }
     }
 
@@ -3243,6 +3245,8 @@ class topology_coordinator : public endpoint_lifecycle_subscriber
                 break;
             case topology::transition_state::truncate_table:
                 co_await handle_truncate_table(std::move(guard));
+                break;
+            case topology::transition_state::snapshot_tables:
                 break;
         }
         co_return true;
