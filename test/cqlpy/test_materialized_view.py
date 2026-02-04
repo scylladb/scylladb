afcd7d3ea573ac2614ff86db70946a2bd1dc564d
Piotr Smaron <piotr.smaron@scylladb.com>
2026-02-04 14:04:44 +0100

    cql3: implementations of IS NULL/IS NOT NULL in statement restrictions
    This patch incorporates IS NULL and IS NOT NULL restrictions into the
    statement restrictions logic:
    - IS NULL/IS NOT NULL are treated as restrictions that require filtering
      (unless potentially optimized in future, currently they are filters).
    - Updated possible_lhs_values to return unbounded set for IS/IS NOT
      operators, as they don't restrict the range of values in a way that
      can be used for clustering key slicing (they are presence checks).
    - Fix a failing MVs testcase.

--- test/cqlpy/test_materialized_view.py
+++ test/cqlpy/test_materialized_view.py
@@ -312,14 +312,6 @@ def test_is_not_operator_must_be_null(cql, test_keyspace):
 # both in ordinary base-table SELECT and in materialized-view definition.
 def test_is_not_null_forbidden_in_filter(cql, test_keyspace, cassandra_bug):
     with new_test_table(cql, test_keyspace, 'p int primary key, xyz int') as table:
-        # Check that "IS NOT NULL" is not supported in a regular (base table)
-        # SELECT filter. Cassandra reports an InvalidRequest: "Unsupported
-        # restriction: xyz IS NOT NULL". In Scylla the message is different:
-        # "restriction '(xyz) IS NOT { null }' is only supported in materialized
-        # view creation".
-        #
-        with pytest.raises(InvalidRequest, match="xyz"):
-            cql.execute(f'SELECT * FROM {table} WHERE xyz IS NOT NULL ALLOW FILTERING')
         # Check that "xyz IS NOT NULL" is also not supported in a
         # materialized-view definition (where xyz is not a key column)
         # Reproduces #8517
