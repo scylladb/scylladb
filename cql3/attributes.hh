9279357d92e7ccc04dddb87a6edeb2be36e049c2
Szymon Malewski <szymon.malewski@scylladb.com>
2026-02-04 11:27:12 +0100

    select_statement: introduce OVERSAMPLING attribute for vector search
    This patch extends CQL grammar with `USING OVERSAMPLING <float>` in SELECT statements.
    It is allowed only in ORDER BY ANN statements.
    Its purpose is to overide per request corresponding index setting.
    This functionality is especially useful for testing and fine-tuning index, as it allows to check various settings without global index settings.
    The actual implementation of oversampling override comes in the next patch.
    
    Ref https://scylladb.atlassian.net/browse/SCYLLADB-184

--- cql3/attributes.hh
+++ cql3/attributes.hh
@@ -37,6 +37,8 @@ private:
     std::optional<cql3::expr::expression> _timeout;
     std::optional<sstring> _service_level;
     std::optional<cql3::expr::expression> _concurrency;
+    expr::unset_bind_variable_guard _oversampling_unset_guard;
+    std::optional<cql3::expr::expression> _oversampling;
 public:
     static std::unique_ptr<attributes> none();
 private:
@@ -44,7 +46,8 @@ private:
                std::optional<cql3::expr::expression>&& time_to_live,
                std::optional<cql3::expr::expression>&& timeout,
                std::optional<sstring> service_level,
-               std::optional<cql3::expr::expression>&& concurrency);
+               std::optional<cql3::expr::expression>&& concurrency,
+               std::optional<cql3::expr::expression>&& oversampling);
 public:
     bool is_timestamp_set() const;
 
@@ -56,6 +59,8 @@ public:
 
     bool is_concurrency_set() const;
 
+    bool is_oversampling_set() const;
+
     int64_t get_timestamp(int64_t now, const query_options& options);
 
     std::optional<int32_t> get_time_to_live(const query_options& options);
@@ -66,6 +71,8 @@ public:
 
     std::optional<int32_t> get_concurrency(const query_options& options) const;
 
+    std::optional<float> get_oversampling(const query_options& options) const;
+
     void fill_prepare_context(prepare_context& ctx);
 
     class raw final {
@@ -75,6 +82,7 @@ public:
         std::optional<cql3::expr::expression> timeout;
         std::optional<sstring> service_level;
         std::optional<cql3::expr::expression> concurrency;
+        std::optional<cql3::expr::expression> oversampling;
 
         std::unique_ptr<attributes> prepare(data_dictionary::database db, const sstring& ks_name, const sstring& cf_name) const;
     private:
@@ -85,6 +93,8 @@ public:
         lw_shared_ptr<column_specification> timeout_receiver(const sstring& ks_name, const sstring& cf_name) const;
 
         lw_shared_ptr<column_specification> concurrency_receiver(const sstring& ks_name, const sstring& cf_name) const;
+
+        lw_shared_ptr<column_specification> oversampling_receiver(const sstring& ks_name, const sstring& cf_name) const;
     };
 };
 
