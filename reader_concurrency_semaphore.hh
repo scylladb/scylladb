28939a091ebbe6e7689efa4d2f4e704052fefe33
Andrzej Jackowski <andrzej.jackowski@scylladb.com>
2026-02-19 15:53:34 +0100

    reader_concurrency_semaphore: allow using shared pool
    This commit introduces code that allows using
    the `shared_memory_pool` of `reader_concurrency_semaphore_group`
    if the `reader_concurrency_semaphore`'s dedicated memory is insufficient.
    The borrowed memory is returned to the shared pool as soon as possible;
    i.e., whenever memory is returned, it is returned to the shared pool
    first.
    
    The `shared_memory_pool` will be provided to semaphores in
    `reader_concurrency_semaphore_group` later in this patch series.
    
    Although `if` checks are added to the `reader_concurrency_semaphore`
    hot paths (`has_available_units`, `consume`, `signal`), based on
    `perf-simple-query` results, there is no visible degradation:
    
    Baseline:
    316852.88 tps ( 64.1 allocs/op,   0.0 logallocs/op,  14.1 tasks/op,   39725 insns/op,   15883 cycles/op,        0 errors)
    throughput:
    	mean=   317350.94 standard-deviation=7197.13
    	median= 317394.14 median-absolute-deviation=638.11
    	maximum=343387.08 minimum=296682.77
    instructions_per_op:
    	mean=   39697.26 standard-deviation=19.38
    	median= 39704.31 median-absolute-deviation=15.57
    	maximum=39732.59 minimum=39657.27
    cpu_cycles_per_op:
    	mean=   15886.74 standard-deviation=74.02
    	median= 15861.66 median-absolute-deviation=50.46
    	maximum=16118.46 minimum=15809.89
    
    With shared_memory_pool used in reader_concurrency_semaphore:
    319988.13 tps ( 64.1 allocs/op,   0.0 logallocs/op,  14.1 tasks/op,   39631 insns/op,   15703 cycles/op,        0 errors)
    throughput:
    	mean=   323245.28 standard-deviation=8266.03
    	median= 321067.80 median-absolute-deviation=2385.64
    	maximum=348343.97 minimum=311363.40
    instructions_per_op:
    	mean=   39634.52 standard-deviation=15.96
    	median= 39632.20 median-absolute-deviation=8.79
    	maximum=39671.80 minimum=39599.66
    cpu_cycles_per_op:
    	mean=   15671.11 standard-deviation=53.05
    	median= 15655.53 median-absolute-deviation=25.70
    	maximum=15914.82 minimum=15629.03
    
    Refs: SCYLLADB-455

--- reader_concurrency_semaphore.hh
+++ reader_concurrency_semaphore.hh
@@ -23,6 +23,7 @@ using namespace seastar;
 
 class mutation_reader;
 using mutation_reader_opt = optimized_optional<mutation_reader>;
+class shared_memory_pool;
 
 /// Specific semaphore for controlling reader concurrency
 ///
@@ -164,6 +165,8 @@ public:
 private:
     resources _initial_resources;
     resources _resources;
+    shared_memory_pool* _shared_pool = nullptr;
+    ssize_t _borrowed_from_shared = 0;
     utils::observer<int> _count_observer;
 
     struct wait_queue {
@@ -293,7 +296,8 @@ public:
             utils::updateable_value<uint32_t> kill_limit_multiplier,
             utils::updateable_value<uint32_t> cpu_concurrency,
             utils::updateable_value<float> preemptive_abort_factor,
-            register_metrics metrics);
+            register_metrics metrics,
+            shared_memory_pool* shared_pool = nullptr);
 
     reader_concurrency_semaphore(
             int count,
@@ -304,10 +308,11 @@ public:
             utils::updateable_value<uint32_t> kill_limit_multiplier,
             utils::updateable_value<uint32_t> cpu_concurrency,
             utils::updateable_value<float> preemptive_abort_factor,
-            register_metrics metrics)
+            register_metrics metrics,
+            shared_memory_pool* shared_pool = nullptr)
         : reader_concurrency_semaphore(utils::updateable_value(count), memory, std::move(name), max_queue_length,
                 std::move(serialize_limit_multiplier), std::move(kill_limit_multiplier), std::move(cpu_concurrency),
-                std::move(preemptive_abort_factor), metrics)
+                std::move(preemptive_abort_factor), metrics, shared_pool)
     { }
 
     /// Create a semaphore with practically unlimited count and memory.
@@ -328,9 +333,10 @@ public:
             utils::updateable_value<uint32_t> kill_limit_multipler = utils::updateable_value(std::numeric_limits<uint32_t>::max()),
             utils::updateable_value<uint32_t> cpu_concurrency = utils::updateable_value<uint32_t>(1),
             utils::updateable_value<float> preemptive_abort_factor = utils::updateable_value<float>(0.0f),
-            register_metrics metrics = register_metrics::no)
+            register_metrics metrics = register_metrics::no,
+            shared_memory_pool* shared_pool = nullptr)
         : reader_concurrency_semaphore(utils::updateable_value(count), memory, std::move(name), max_queue_length, std::move(serialize_limit_multipler),
-                std::move(kill_limit_multipler), std::move(cpu_concurrency), std::move(preemptive_abort_factor), metrics)
+                std::move(kill_limit_multipler), std::move(cpu_concurrency), std::move(preemptive_abort_factor), metrics, shared_pool)
     {}
 
     virtual ~reader_concurrency_semaphore();
