45dbc2e3cd7c3e8c77c17d983b1cc30da08d98c6
Calle Wilund <calle@scylladb.com>
2026-03-25 12:02:46 +0100

    system_distributed_keyspace: Break out snapshot entry structs to header
    To avoid cross-inclusion in dependents

---
+++ db/snapshot_types.hh
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2026-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.1
+ */
+
+#pragma once
+
+#include "schema/schema_fwd.hh"
+#include "dht/token.hh"
+#include "sstables/types.hh"
+
+namespace db {
+
+using is_downloaded = bool_class<class is_downloaded_tag>;
+
+struct snapshot_sstable_entry {
+    sstables::sstable_id sstable_id;
+    dht::token first_token;
+    dht::token last_token;
+    sstring toc_name;
+    sstring prefix;
+    is_downloaded downloaded{is_downloaded::no};
+};
+
+}
