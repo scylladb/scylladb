cd1dab636411e2272e8558c53736e4aafe750e12
Michael Litvak <michael.litvak@scylladb.com>
2026-04-16 18:50:14 +0200

    topology_coordinator: state machine for strongly consistent tablet migration
    extend the tablet migration state machine so it can be used for strongly
    consistent tables as well.
    
    we reuse some of the stages and give some of them a different name alias
    when used in the context of strongly consistent tables that is more
    appropriate.
    
    we add a new stage sc_rollback as the first rollback stage for strongly
    consistent tables before cleanup_target. it executes a barrier that will
    be used to ensure the pending replica is removed from the group before
    cleaning it.

--- service/tablet_allocator.cc
+++ service/tablet_allocator.cc
@@ -1031,6 +1031,8 @@ private:
                 return false;
             case tablet_transition_stage::cleanup:
                 return false;
+            case tablet_transition_stage::sc_rollback:
+                return false;
             case tablet_transition_stage::cleanup_target:
                 return false;
             case tablet_transition_stage::revert_migration:
