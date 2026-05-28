d7e1a3912a7d65113072bccb8d8d0faac61f609c
Michał Chojnowski <michal.chojnowski@scylladb.com>
2026-05-28 13:48:52 +0200

    sstables: add `mt` to sstable format lists
    This enables the format in various "for all sstable formats" tests.
    
    (cherry picked from commit 1b9771a58b351516619085b254bda5bdd9bf88d2)

--- sstables/version.hh
+++ sstables/version.hh
@@ -17,20 +17,22 @@ namespace sstables {
 enum class sstable_version_types { ka, la, mc, md, me, ms, mt };
 enum class sstable_format_types { big };
 
-constexpr std::array<sstable_version_types, 6> all_sstable_versions = {
+constexpr std::array<sstable_version_types, 7> all_sstable_versions = {
     sstable_version_types::ka,
     sstable_version_types::la,
     sstable_version_types::mc,
     sstable_version_types::md,
     sstable_version_types::me,
     sstable_version_types::ms,
+    sstable_version_types::mt,
 };
 
-constexpr std::array<sstable_version_types, 4> writable_sstable_versions = {
+constexpr std::array<sstable_version_types, 5> writable_sstable_versions = {
     sstable_version_types::mc,
     sstable_version_types::md,
     sstable_version_types::me,
     sstable_version_types::ms,
+    sstable_version_types::mt,
 };
 
 constexpr sstable_version_types oldest_writable_sstable_format = sstable_version_types::mc;
