596de3e2568aed16e1ad4a1c5bd0e28724ca4043
Michael Litvak <michael.litvak@scylladb.com>
2026-06-04 10:21:27 +0200

    logstor: add compaction trigger threshold to db config
    add the db config parameter logstor_compaction_trigger_threshold_percent
    that determines when logstor triggers compaction based on the number of
    free segments.

--- replica/database.cc
+++ replica/database.cc
@@ -966,6 +966,7 @@ database::init_logstor() {
             .file_size = _cfg.logstor_file_size_in_mb() * 1024ull * 1024ull,
             .disk_size = _cfg.logstor_disk_size_in_mb() * 1024ull * 1024ull,
             .format_on_startup = _cfg.logstor_format_on_startup(),
+            .trigger_compaction_threshold = _cfg.logstor_compaction_trigger_threshold,
             .compaction_sg = _dbcfg.compaction_scheduling_group,
             .compaction_static_shares = _cfg.compaction_static_shares,
             .separator_sg = _dbcfg.memtable_scheduling_group,
