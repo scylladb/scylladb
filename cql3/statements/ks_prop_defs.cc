10877b89baa2ba813f62e2ece1e7ffdd47a7684c
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2026-06-10 13:58:35 +0200

    cql3: extend extended_map_type to support nested map values
    Add map_type as a third variant alternative in extended_map_type,
    allowing property map values to be nested maps (map<sstring, sstring>)
    in addition to strings and lists.
    
    This enables CQL property syntax like:
      WITH option = {'key': 'value', 'nested': {'a': 'b'}}
    
    The grammar's convert_extended_property_map now detects map-style
    collection_constructor values and parses them as nested maps.
    
    The nested map isn't used yet, it will be utilized in the following
    changes.
    
    Add test to ensure that replication param does not support map type.

--- cql3/statements/ks_prop_defs.cc
+++ cql3/statements/ks_prop_defs.cc
@@ -22,6 +22,8 @@
 #include "db/config.hh"
 #include "db/cluster_config_registry.hh"
 #include "cql3/statements/cluster_config_props.hh"
+#include "utils/overloaded_functor.hh"
+#include "utils/on_internal_error.hh"
 #include <random>
 
 namespace cql3 {
@@ -352,10 +354,15 @@ void ks_prop_defs::validate(const gms::feature_service& feat) {
 
 locator::replication_strategy_config_options ks_prop_defs::get_replication_options() const {
     auto replication_options = get_extended_map(KW_REPLICATION);
-    if (replication_options) {
-        return replication_options.value();
+    if (!replication_options) {
+        return {};
     }
-    return {};
+    if (std::any_of(replication_options->begin(), replication_options->end(), [] (const auto& opt) {
+        return std::holds_alternative<map_type>(opt.second);
+    })) {
+        throw exceptions::configuration_exception("Cannot specify replication options as a map");
+    }
+    return *replication_options;
 }
 
 data_dictionary::storage_options ks_prop_defs::get_storage_options() const {
