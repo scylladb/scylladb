scylladb/scylladb

commit 853edcbf75ce25a1e86141dbf4faed6322fac855

tracing: add_query(): change query param to utils::chunked_string
Having to unconditionally linearize the chunked query string when
passing it to tracing undoes the work put into reducing large
alloctions on the query path. The add_query() is evaluated eagerly on
every query, even if tracing is disabled. Defer the linearization to
build_parameres_map(), which is only called if tracing is enabled.

transport/server.cc
@@ -1603,7 +1603,7 @@ future<std::unique_ptr<cql_server::response>> cql_server::connection::process_pr
     auto query = std::move(query_result).assume_value();
     auto dialect = get_dialect();
 
-    tracing::add_query(trace_state, query.linearize());
+    tracing::add_query(trace_state, query);
     tracing::begin(trace_state, "Preparing CQL3 query", client_state.get_client_address());
 
     return _server._query_processor.invoke_on_others([query, &client_state, dialect] (auto& qp) mutable {
@@ -1671,7 +1671,7 @@ process_execute_internal(service::client_state& client_state, sharded<cql3::quer
 
     if (init_trace) {
         tracing::set_page_size(trace_state, options.get_page_size());
-        tracing::add_query(trace_state, sstring(prepared->statement->raw_cql_statement.linearize()));
+        tracing::add_query(trace_state, prepared->statement->raw_cql_statement);
         tracing::add_prepared_statement(trace_state, prepared);
         tracing::set_common_query_parameters(trace_state, options.get_consistency(),
             options.get_serial_consistency(), options.get_specific_options().timestamp);
@@ -1777,7 +1777,7 @@ process_batch_internal(service::client_state& client_state, sharded<cql3::query_
                 needs_authorization = pending_authorization_entries.emplace(std::move(cache_key), ps->checked_weak_from_this()).second;
             }
             if (init_trace) {
-                tracing::add_query(trace_state, sstring(ps->statement->raw_cql_statement.linearize()));
+                tracing::add_query(trace_state, ps->statement->raw_cql_statement);
             }
             break;
         }

alternator/server.cc
@@ -9,6 +9,7 @@
 #include "alternator/server.hh"
 #include "audit/audit.hh"
 #include "alternator/executor_util.hh"
+#include "utils/chunked_string.hh"
 #include "gms/application_state.hh"
 #include "utils/log.hh"
 #include <fmt/ranges.h>
@@ -543,7 +544,7 @@ static tracing::trace_state_ptr maybe_trace_query(service::client_state& client_
     if (tracing_instance.trace_next_query() || tracing_instance.slow_query_tracing_enabled()) {
         trace_state = create_tracing_session(tracing_instance);
         tracing::add_session_param(trace_state, "alternator_op", op);
-        tracing::add_query(trace_state, truncated_content_view(query, max_users_query_size_in_trace_output).take_as_sstring());
+        tracing::add_query(trace_state, utils::chunked_string(truncated_content_view(query, max_users_query_size_in_trace_output).as_view()));
         tracing::begin(trace_state, seastar::format("Alternator {}", op), client_state.get_client_address());
         if (!username.empty()) {
             tracing::set_username(trace_state, auth::authenticated_user(username));

tracing/trace_state.cc
@@ -11,6 +11,7 @@
 #include "cql3/statements/prepared_statement.hh"
 #include "tracing/trace_state.hh"
 #include "mutation/timestamp.hh"
+#include "utils/chunked_string.hh"
 
 #include "cql3/values.hh"
 #include "cql3/query_options.hh"
@@ -29,7 +30,7 @@ struct trace_state::params_values {
 
     std::optional<host_id_vector_replica_set> batchlog_endpoints;
     std::optional<api::timestamp_type> user_timestamp;
-    std::vector<sstring> queries;
+    std::vector<utils::chunked_string> queries;
     std::optional<db::consistency_level> cl;
     std::optional<db::consistency_level> serial_cl;
     std::optional<int32_t> page_size;
@@ -75,12 +76,8 @@ void trace_state::set_response_size(size_t s) noexcept {
     _records->session_rec.response_size = s;
 }
 
-void trace_state::add_query(sstring &&val) {
+void trace_state::add_query(utils::chunked_string val) {
     _params_ptr->queries.emplace_back(std::move(val));
-}
-
-void trace_state::add_query(std::string_view val) {
-    _params_ptr->queries.emplace_back(sstring{ val });
 }
 
 void trace_state::add_session_param(std::string_view key, std::string_view val) {
@@ -145,11 +142,11 @@ void trace_state::build_parameters_map() {
     auto& queries = vals.queries;
     if (!queries.empty()) {
         if (queries.size() == 1) {
-            params_map.emplace("query", queries[0]);
+            params_map.emplace("query", queries[0].linearize());
         } else {
             // BATCH
             for (size_t i = 0; i < queries.size(); ++i) {
-                params_map.emplace(format("query[{:d}]", i), queries[i]);
+                params_map.emplace(format("query[{:d}]", i), queries[i].linearize());
             }
         }
     }

tracing/trace_state.hh
@@ -14,6 +14,7 @@
 #include <seastar/core/weak_ptr.hh>
 #include <seastar/core/checked_ptr.hh>
 #include "tracing/tracing.hh"
+#include "utils/chunked_string.hh"
 #include "gms/inet_address.hh"
 #include "auth/authenticated_user.hh"
 #include "db/consistency_level_type.hh"
@@ -362,17 +363,7 @@ private:
      *
      * @param val the query string
      */
-    void add_query(sstring &&val);
-
-    /**
-     * Store a query string.
-     *
-     * This value will eventually be stored in a params<string, string> map of a tracing session
-     * with a 'query' key.
-     *
-     * @param val the query string
-     */
-    void add_query(std::string_view val);
+     void add_query(utils::chunked_string val);
 
     /**
      * Store a custom session parameter.
@@ -493,8 +484,7 @@ private:
     friend void set_request_size(const trace_state_ptr& p, size_t s) noexcept;
     friend void set_response_size(const trace_state_ptr& p, size_t s) noexcept;
     friend void set_batchlog_endpoints(const trace_state_ptr& p, const host_id_vector_replica_set& val);
-    friend void add_query(const trace_state_ptr& p, sstring &&val);
-    friend void add_query(const trace_state_ptr& p, std::string_view val);
+    friend void add_query(const trace_state_ptr& p, utils::chunked_string val);
     friend void add_session_param(const trace_state_ptr& p, std::string_view key, std::string_view val);
     friend void set_common_query_parameters(const trace_state_ptr& p, db::consistency_level consistency,
         const std::optional<db::consistency_level>& serial_consistency, api::timestamp_type timestamp);
@@ -620,15 +610,9 @@ inline void set_batchlog_endpoints(const trace_state_ptr& p, const host_id_vecto
     }
 }
 
-inline void add_query(const trace_state_ptr& p, sstring &&val) {
+inline void add_query(const trace_state_ptr& p, utils::chunked_string val) {
     if (p) {
         p->add_query(std::move(val));
-    }
-}
-
-inline void add_query(const trace_state_ptr& p, std::string_view val) {
-    if (p) {
-        p->add_query(val);
     }
 }
 

commit ab34ded0e986b80948969a5d6c53ff265f43f736

transport: skip tracing init work when tracing is disabled
init_trace is (bool)!bounced, so it is true for nearly all requests. The
tracing helpers it guards are no-ops when no tracing session is active
(trace_state == nullptr), but their arguments are still evaluated at the
call site first. tracing::add_query takes utils::chunked_string by value,
so the query string is deep-copied on every QUERY/EXECUTE/BATCH even when
tracing is off, including raw_cql_statement on every prepared EXECUTE.

This copy is a regression from commit 853edcbf75ce25a1e86141dbf4faed6322fac855
("tracing: add_query(): change query param to utils::chunked_string"), which
changed add_query from std::string_view to utils::chunked_string by value.

Check trace_state at each call site so the argument evaluation and the
query-string copy are skipped when tracing is off, keeping init_trace intact
for its bounce semantics.

Benchmark (prepared statements, tracing off, release, single shard):

  ./build/release/scylla perf-cql-raw --workload <read|write> \
      --use-prepared true --duration 10 --memory 2G --smp 1 \
      --overprovisioned --partitions 1000

               before      after
  read   insns/op  ~66980     ~66740   (-240)
         allocs/op   96.6       95.4   (-1)
  write  insns/op  ~58243     ~58051   (-192)
         allocs/op   64.3       63.3   (-1)

Summary: 1 fewer allocation and ~200 fewer instructions per query.

Fixes: SCYLLADB-2612

transport/server.cc
@@ -1571,7 +1571,7 @@ process_query_internal(service::client_state& client_state, sharded<cql3::query_
     }
     auto skip_metadata = options.skip_metadata();
 
-    if (init_trace) {
+    if (init_trace && trace_state) {
         tracing::set_page_size(trace_state, options.get_page_size());
         tracing::add_query(trace_state, query.assume_value());
         tracing::set_common_query_parameters(trace_state, options.get_consistency(),
@@ -1669,7 +1669,7 @@ process_execute_internal(service::client_state& client_state, sharded<cql3::quer
     }
     auto skip_metadata = options.skip_metadata();
 
-    if (init_trace) {
+    if (init_trace && trace_state) {
         tracing::set_page_size(trace_state, options.get_page_size());
         tracing::add_query(trace_state, prepared->statement->raw_cql_statement);
         tracing::add_prepared_statement(trace_state, prepared);
@@ -1692,7 +1692,7 @@ process_execute_internal(service::client_state& client_state, sharded<cql3::quer
 
     options.prepare(prepared->bound_names);
 
-    if (init_trace) {
+    if (init_trace && trace_state) {
         tracing::add_prepared_query_options(trace_state, options);
     }
 
@@ -1730,7 +1730,7 @@ process_batch_internal(service::client_state& client_state, sharded<cql3::query_
     modifications.reserve(n.assume_value());
     values.reserve(n.assume_value());
 
-    if (init_trace) {
+    if (init_trace && trace_state) {
         tracing::begin(trace_state, "Execute batch of CQL3 queries", client_state.get_client_address());
     }
 
@@ -1752,7 +1752,7 @@ process_batch_internal(service::client_state& client_state, sharded<cql3::query_
             }
             stmt_ptr = qp.local().get_statement(query.assume_value(), client_state, dialect);
             ps = stmt_ptr->checked_weak_from_this();
-            if (init_trace) {
+            if (init_trace && trace_state) {
                 tracing::add_query(trace_state, query.assume_value());
             }
             break;
@@ -1776,7 +1776,7 @@ process_batch_internal(service::client_state& client_state, sharded<cql3::query_
                 // authorize a particular prepared statement only once
                 needs_authorization = pending_authorization_entries.emplace(std::move(cache_key), ps->checked_weak_from_this()).second;
             }
-            if (init_trace) {
+            if (init_trace && trace_state) {
                 tracing::add_query(trace_state, ps->statement->raw_cql_statement);
             }
             break;
@@ -1791,7 +1791,7 @@ process_batch_internal(service::client_state& client_state, sharded<cql3::query_
             return make_exception_future<cql_server::process_fn_return_type>(exceptions::invalid_request_exception("Invalid statement in batch: only UPDATE, INSERT and DELETE statements are allowed."));
         }
         ::shared_ptr<cql3::statements::modification_statement> modif_statement_ptr = static_pointer_cast<cql3::statements::modification_statement>(ps->statement);
-        if (init_trace) {
+        if (init_trace && trace_state) {
             tracing::add_table_name(trace_state, modif_statement_ptr->keyspace(), modif_statement_ptr->column_family());
             tracing::add_prepared_statement(trace_state, ps);
         }
@@ -1827,7 +1827,7 @@ process_batch_internal(service::client_state& client_state, sharded<cql3::query_
         options.set_cached_pk_function_calls(std::move(cached_pk_fn_calls));
     }
 
-    if (init_trace) {
+    if (init_trace && trace_state) {
         tracing::add_prepared_query_options(trace_state, options);
         tracing::set_common_query_parameters(trace_state, options.get_consistency(),
             options.get_serial_consistency(), options.get_specific_options().timestamp);