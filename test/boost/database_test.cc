4010b292429375f8da3fbdd43fbcbd972f291ca3
Botond Dénes <bdenes@scylladb.com>
2026-06-18 11:21:51 +0300

    replica: thread abort_source* to reader_concurrency_semaphore
    From replica::database, all the way to reader_concurrency_semaphore
    permit methods. Interface only changes and updated callers, cancellation
    support not implemented yet.
    The only user who passes non-null abort-source is
    replica::database::query[_mutations]().

--- test/boost/database_test.cc
+++ test/boost/database_test.cc
@@ -2427,6 +2427,7 @@ SEASTAR_TEST_CASE(replica_read_timeout_no_exception) {
                     "dummy",
                     1,
                     db::no_timeout,
+                    {},
                     {}));
             }).get();
 
