bhalevy/scylla

sstables: use update_sstable_id for compaction rewrite options
Replace the existing component_rewrite::update_sstable_id type with
sstables::update_sstable_id.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

test/boost/sstable_compaction_test.cc
@@ -7542,7 +7542,7 @@ SEASTAR_FIXTURE_TEST_CASE(failure_when_adding_new_sstable_test_gcs, gcs_fixture,
                                    test_env_config{.storage = make_test_object_storage_options("GS")});
 }
 
-static future<> test_perform_component_rewrite_single_sstable(compaction::compaction_type_options::component_rewrite::update_sstable_id update_id) {
+static future<> do_test_perform_component_rewrite_single_sstable(sstables::update_sstable_id update_id) {
     return test_env::do_with_async([update_id] (test_env& env) {
         simple_schema ss;
         auto s = ss.schema();
@@ -7589,10 +7589,13 @@ static future<> test_perform_component_rewrite_single_sstable(compaction::compac
         auto new_sst = it->second;
         BOOST_REQUIRE(new_sst->get_sstable_level() == new_level);
         BOOST_REQUIRE(new_sst->generation() != original_sst->generation());
-        if (update_id) {
-            BOOST_REQUIRE(new_sst->sstable_identifier() != original_sst->sstable_identifier());
-        } else {
-            BOOST_REQUIRE(new_sst->sstable_identifier() == original_sst->sstable_identifier());
+        switch (update_id) {
+        case sstables::update_sstable_id::yes:
+            BOOST_REQUIRE_NE(new_sst->sstable_identifier(), original_sst->sstable_identifier());
+            break;
+        case sstables::update_sstable_id::no:
+            BOOST_REQUIRE_EQUAL(new_sst->sstable_identifier(), original_sst->sstable_identifier());
+            break;
         }
 
         all_sstables = table->get_sstables();
@@ -7603,11 +7606,11 @@ static future<> test_perform_component_rewrite_single_sstable(compaction::compac
 }
 
 SEASTAR_TEST_CASE(test_perform_component_rewrite_single_sstable_with_backup) {
-    return test_perform_component_rewrite_single_sstable(compaction::compaction_type_options::component_rewrite::update_sstable_id::yes);
+    return do_test_perform_component_rewrite_single_sstable(sstables::update_sstable_id::yes);
 }
 
 SEASTAR_TEST_CASE(test_perform_component_rewrite_single_sstable_without_backup) {
-    return test_perform_component_rewrite_single_sstable(compaction::compaction_type_options::component_rewrite::update_sstable_id::no);
+    return do_test_perform_component_rewrite_single_sstable(sstables::update_sstable_id::no);
 }
 
 SEASTAR_TEST_CASE(test_perform_component_rewrite_multiple_sstables) {

scylladb/scylladb

test/boost/sstable_compaction_test.cc
@@ -7542,7 +7542,7 @@ SEASTAR_FIXTURE_TEST_CASE(failure_when_adding_new_sstable_test_gcs, gcs_fixture,
                                    test_env_config{.storage = make_test_object_storage_options("GS")});
 }
 
-static future<> test_perform_component_rewrite_single_sstable(compaction::compaction_type_options::component_rewrite::update_sstable_id update_id) {
+static future<> test_perform_component_rewrite_single_sstable(sstables::update_sstable_id update_id) {
     return test_env::do_with_async([update_id] (test_env& env) {
         simple_schema ss;
         auto s = ss.schema();
@@ -7603,11 +7603,11 @@ static future<> test_perform_component_rewrite_single_sstable(compaction::compac
 }
 
 SEASTAR_TEST_CASE(test_perform_component_rewrite_single_sstable_with_backup) {
-    return test_perform_component_rewrite_single_sstable(compaction::compaction_type_options::component_rewrite::update_sstable_id::yes);
+    return test_perform_component_rewrite_single_sstable(sstables::update_sstable_id::yes);
 }
 
 SEASTAR_TEST_CASE(test_perform_component_rewrite_single_sstable_without_backup) {
-    return test_perform_component_rewrite_single_sstable(compaction::compaction_type_options::component_rewrite::update_sstable_id::no);
+    return test_perform_component_rewrite_single_sstable(sstables::update_sstable_id::no);
 }
 
 SEASTAR_TEST_CASE(test_perform_component_rewrite_multiple_sstables) {