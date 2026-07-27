2dead7e109b9a55911509c8e723603343081a652
Benny Halevy <bhalevy@scylladb.com>
2026-07-27 12:57:47 +0300

    sstables: introduce emplace_sstable_identifier
    To be used in a following patch to ensure a new sstable_id
    when link_with_excluded_components is called with update_sstable_id::yes

--- sstables/sstables.hh
+++ sstables/sstables.hh
@@ -719,6 +719,7 @@ private:
                                std::optional<scylla_metadata::large_data_stats> ld_stats,
                                std::optional<scylla_metadata::ext_timestamp_stats> ts_stats,
                                std::optional<scylla_metadata::large_data_records> ld_records = std::nullopt);
+    sstable_id emplace_sstable_identifier() noexcept;
 
     future<> read_filter(sstable_open_config cfg = {});
 
