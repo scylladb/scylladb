77e58fa3b00133a5e2ff253a575e7684d648ba5c
Botond Dénes <bdenes@scylladb.com>
2026-07-31 21:18:47 +0300

    keys: drop dead prefixable_full_compound class hierachy
    Probably meant as a distinct type for full clustering keys. We ended up
    using the same type for prefixes and full clustering keys and this class
    hierarchy is completely dead code.

--- keys/keys.hh
+++ keys/keys.hh
@@ -25,15 +25,13 @@
 // We distinguish partition keys and clustering keys. API-wise they are almost
 // the same, but they're separate type hierarchies.
 //
-// Clustering keys are further divided into prefixed and non-prefixed (full).
-// Non-prefixed keys always have full component set, as defined by schema.
-// Prefixed ones can have any number of trailing components missing. They may
-// differ in underlying representation.
+// Clustering keys can have any number of trailing components missing (prefix).
+// A full key has all components as defined by the schema.
 //
 // The main classes are:
 //
 //   partition_key           - full partition key
-//   clustering_key          - full clustering key
+//   clustering_key          - full clustering key (alias for clustering_key_prefix)
 //   clustering_key_prefix   - clustering key prefix
 //
 // These classes wrap only the minimum information required to store the key
@@ -386,53 +384,6 @@ public:
     }
 };
 
-template <typename TopLevel, typename PrefixTopLevel>
-class prefix_view_on_full_compound {
-public:
-    using iterator = typename compound_type<allow_prefixes::no>::iterator;
-private:
-    bytes_view _b;
-    unsigned _prefix_len;
-    iterator _begin;
-    iterator _end;
-public:
-    prefix_view_on_full_compound(const schema& s, bytes_view b, unsigned prefix_len)
-        : _b(b)
-        , _prefix_len(prefix_len)
-        , _begin(TopLevel::get_compound_type(s)->begin(_b))
-        , _end(_begin)
-    {
-        std::advance(_end, prefix_len);
-    }
-
-    iterator begin() const { return _begin; }
-    iterator end() const { return _end; }
-
-    struct less_compare_with_prefix {
-        typename PrefixTopLevel::compound prefix_type;
-
-        less_compare_with_prefix(const schema& s)
-            : prefix_type(PrefixTopLevel::get_compound_type(s))
-        { }
-
-        bool operator()(const prefix_view_on_full_compound& k1, const PrefixTopLevel& k2) const {
-            return lexicographical_tri_compare(
-                prefix_type->types().begin(), prefix_type->types().end(),
-                k1.begin(), k1.end(),
-                prefix_type->begin(k2), prefix_type->end(k2),
-                tri_compare) < 0;
-        }
-
-        bool operator()(const PrefixTopLevel& k1, const prefix_view_on_full_compound& k2) const {
-            return lexicographical_tri_compare(
-                prefix_type->types().begin(), prefix_type->types().end(),
-                prefix_type->begin(k1), prefix_type->end(k1),
-                k2.begin(), k2.end(),
-                tri_compare) < 0;
-        }
-    };
-};
-
 template <typename TopLevel>
 class prefix_view_on_prefix_compound {
 public:
@@ -478,82 +429,6 @@ public:
                 tri_compare) < 0;
         }
     };
-};
-
-template <typename TopLevel, typename TopLevelView, typename PrefixTopLevel>
-class prefixable_full_compound : public compound_wrapper<TopLevel, TopLevelView> {
-    using base = compound_wrapper<TopLevel, TopLevelView>;
-protected:
-    prefixable_full_compound(bytes&& b) : base(std::move(b)) {}
-public:
-    using prefix_view_type = prefix_view_on_full_compound<TopLevel, PrefixTopLevel>;
-
-    bool is_prefixed_by(const schema& s, const PrefixTopLevel& prefix) const {
-        const auto& t = base::get_compound_type(s);
-        const auto& prefix_type = PrefixTopLevel::get_compound_type(s);
-        return ::is_prefixed_by(t->types().begin(),
-            t->begin(*this), t->end(*this),
-            prefix_type->begin(prefix), prefix_type->end(prefix),
-            ::equal);
-    }
-
-    struct less_compare_with_prefix {
-        typename PrefixTopLevel::compound prefix_type;
-        typename TopLevel::compound full_type;
-
-        less_compare_with_prefix(const schema& s)
-            : prefix_type(PrefixTopLevel::get_compound_type(s))
-            , full_type(TopLevel::get_compound_type(s))
-        { }
-
-        bool operator()(const TopLevel& k1, const PrefixTopLevel& k2) const {
-            return lexicographical_tri_compare(
-                prefix_type->types().begin(), prefix_type->types().end(),
-                full_type->begin(k1), full_type->end(k1),
-                prefix_type->begin(k2), prefix_type->end(k2),
-                tri_compare) < 0;
-        }
-
-        bool operator()(const PrefixTopLevel& k1, const TopLevel& k2) const {
-            return lexicographical_tri_compare(
-                prefix_type->types().begin(), prefix_type->types().end(),
-                prefix_type->begin(k1), prefix_type->end(k1),
-                full_type->begin(k2), full_type->end(k2),
-                tri_compare) < 0;
-        }
-    };
-
-    // In prefix equality two sequences are equal if any of them is a prefix
-    // of the other. Otherwise lexicographical ordering is applied.
-    // Note: full compounds sorted according to lexicographical ordering are also
-    // sorted according to prefix equality ordering.
-    struct prefix_equality_less_compare {
-        typename PrefixTopLevel::compound prefix_type;
-        typename TopLevel::compound full_type;
-
-        prefix_equality_less_compare(const schema& s)
-            : prefix_type(PrefixTopLevel::get_compound_type(s))
-            , full_type(TopLevel::get_compound_type(s))
-        { }
-
-        bool operator()(const TopLevel& k1, const PrefixTopLevel& k2) const {
-            return prefix_equality_tri_compare(prefix_type->types().begin(),
-                full_type->begin(k1), full_type->end(k1),
-                prefix_type->begin(k2), prefix_type->end(k2),
-                tri_compare) < 0;
-        }
-
-        bool operator()(const PrefixTopLevel& k1, const TopLevel& k2) const {
-            return prefix_equality_tri_compare(prefix_type->types().begin(),
-                prefix_type->begin(k1), prefix_type->end(k1),
-                full_type->begin(k2), full_type->end(k2),
-                tri_compare) < 0;
-        }
-    };
-
-    prefix_view_type prefix_view(const schema& s, unsigned prefix_len) const {
-        return { s, this->representation(), prefix_len };
-    }
 };
 
 template <typename TopLevel, typename FullTopLevel>
