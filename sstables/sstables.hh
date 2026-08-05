c9e3483716c754c47881d71248d401148370e51c
Jakub Czyszczoń <jakub.czyszczon@scylladb.com>
2026-08-05 10:37:01 +0200

    sstables: add scrub time to Scylla metadata
    Add the `ScrubTime` scylla metadata type.
    It will hold the timestamp when the sstable was last validated.

--- sstables/sstables.hh
+++ sstables/sstables.hh
@@ -1121,6 +1121,8 @@ public:
 
     std::optional<uint32_t> get_component_digest(component_type c) const;
 
+    std::optional<db_clock::time_point> get_scrub_time() const;
+
     // Gets ratio of droppable tombstone. A tombstone is considered droppable here
     // for cells and tombstones expired before the time point "GC before", which
     // is the point before which expiring data can be purged.
