274852b7dcec4988d4089d9fe2de8fedd747e3f7
Jakub Czyszczoń <jakub.czyszczon@scylladb.com>
2026-08-05 16:10:52 +0200

    test/boost: add a test for auto scrub without Scylla component
    Test whether automatic scrub rewrites ssstables without the scylla
    component into ones with.

--- test/boost/sstable_automatic_scrub_test.cc
+++ test/boost/sstable_automatic_scrub_test.cc
@@ -234,5 +234,34 @@ SEASTAR_THREAD_TEST_CASE(sstable_auto_scrub_scrub_time_updated_with_scylla) {
     });
 }
 
+SEASTAR_THREAD_TEST_CASE(sstable_auto_scrub_scrub_time_updated_without_scylla) {
+    automatic_scrub_test_framework test(tests::random_schema_specification::compress_sstable::yes);
+
+    auto& test_env = test.env();
+    constexpr auto sst_count = 5;
+
+    test.run(sst_count, [&test_env] (table_for_tests& table, compaction::compaction_group_view& ts, std::vector<sstables::shared_sstable> sstables) {
+        auto& cm = test_env.test_compaction_manager();
+
+        for (sstables::shared_sstable& sst : sstables) {
+            sst->set_scrub_time(db_clock::from_time_t(0));
+            sstables::test(sst).rewrite_toc_without_component(component_type::Scylla);
+        }
+
+        cm.set_scrub_period(std::chrono::seconds(3600));
+
+        auto timestamp_before = db_clock::now();
+        cm.trigger_auto_scrub_timer();
+
+        wait_on_enter("automatic_scrub_compaction_done", sst_count).get();
+
+        BOOST_REQUIRE_EQUAL(table->get_sstables()->size(), sstables.size());
+        for (auto& sst : *table->get_sstables()) {
+            BOOST_REQUIRE(sst->has_scylla_component());
+            BOOST_REQUIRE(sst->get_scrub_time() > timestamp_before);
+        }
+    });
+}
+
 } // namespace
 
