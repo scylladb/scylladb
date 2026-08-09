b74eecc64f686bb315fa8f414e47b2a79711df73
Michael Litvak <michael.litvak@scylladb.com>
2026-08-09 15:33:14 +0200

    logstor: return a structured outcome from primary_index::insert
    Replace the previous std::pair result in insert() with a clearer result
    struct.

--- replica/logstor/index.hh
+++ replica/logstor/index.hh
@@ -153,6 +153,21 @@ public:
         std::optional<mutation> cached_mutation;
     };
 
+    enum class insert_result {
+        // The index now holds the new entry, either under a new key or over an older entry.
+        inserted,
+        // The index already holds a newer entry for this key; nothing changed.
+        superseded,
+    };
+
+    struct insert_outcome {
+        insert_result result;
+        // The entry the index held for this key before the call, if it held one.
+        std::optional<index_entry> previous_entry;
+
+        bool inserted() const noexcept { return result == insert_result::inserted; }
+    };
+
 private:
     using iterator = typename partitions_type::iterator;
     using const_iterator = typename partitions_type::const_iterator;
@@ -497,7 +512,7 @@ public:
     using entry_cmp_fn = std::function<std::strong_ordering(const index_entry&, const index_entry&)>;
 
     template <typename EntryCmp = default_entry_cmp>
-    std::pair<bool, std::optional<index_entry>> insert(const primary_index_key& key, index_entry new_entry, const EntryCmp& cmp = {}) {
+    insert_outcome insert(const primary_index_key& key, index_entry new_entry, const EntryCmp& cmp = {}) {
         partitions_type::bound_hint hint;
         auto i = _partitions.lower_bound(key, primary_index_key_cmp(*_schema), hint);
         if (hint.match) {
@@ -510,16 +525,16 @@ public:
                 i->_e = std::move(new_entry);
                 _space_accounting.on_free_record(old_entry.location);
                 _space_accounting.on_add_record(i->_e.location);
-                return {true, std::make_optional(old_entry)};
+                return {insert_result::inserted, std::make_optional(old_entry)};
             } else {
-                return {false, std::make_optional(i->_e)};
+                return {insert_result::superseded, std::make_optional(i->_e)};
             }
-        } else {
-            auto it = _partitions.emplace_before(i, key.token().raw(), hint, key, std::move(new_entry));
-            _space_accounting.on_add_record(it->_e.location);
-            on_entry_added(*it);
-            return {true, std::nullopt};
         }
+
+        auto it = _partitions.emplace_before(i, key.token().raw(), hint, key, std::move(new_entry));
+        _space_accounting.on_add_record(it->_e.location);
+        on_entry_added(*it);
+        return {insert_result::inserted, std::nullopt};
     }
 
     bool erase(const primary_index_key& key, log_location loc) {
