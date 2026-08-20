309157a1a67e94c5a90dccef701076341e492fc7
Nadav Har'El <nyh@scylladb.com>
2026-08-20 11:38:51 +0300

    test/alternator: tests for new DynamoDB Vector Search API
    Alternator has a vector search API, which we developed before DynamoDB
    has a vector search feature. We have an extensive test suite for our
    feature in test/alternator/test_vector.py.
    
    In August 2026, DynamoDB announced a new DynamoDB API. It has many
    similarities to our own API, but also many differences. After we
    replaced our old API by the new one, in this patch I am replacing
    our test suite in test/alternator/test_vector.py by an extensive test
    suite for the new DynamoDB API. The new test suite is based on the
    old one with the necessary API changes, but also includes many new
    tests for DynamoDB features that did not have parallels in the old tests.
    The new test suite is very large - 208 tests and 8,000 lines of code -
    and aims to be exhaustive.
    
    Also in this patch we updated some vector-search-related tests in
    other files - test_metrics.py and test_cql_rbac.py, and added a vector
    search also in test_audit.py. As a cleanup related to these changes we
    moved a couple of RBAC functions - new_role and new_dynamod, already
    used in four different test files - from test_cql_rbac.py to util.py
    (without this move, we ran into nasty import loops).
    
    All new tests pass on DynamoDB - or are skipped if testing an
    Alternator-only extension. Almost all tests pass on Alternator, with
    a few xfail'ing on Alternator with comments explaining why.
    Most of these tests were written before we implemented the new API
    in Alternator, and served as acceptance tests for the new API's
    implementation.
    
    In addition to testing compatibility with DynamoDB's vector search API,
    the new test suite also tests the following Alternator-only extensions
    to the API:
    
       1. The "FLOAT32VECTOR" type, allowing Alternator to store vectors
          as efficient arrays of 32-bit floats instead of JSON strings or
          high-precision numbers.
       2. The "BaseRead" parameter to SearchVectors
       3. The "FilterExpression" parameter to SearchVectors
       4. Scylla-specific metrics
       5. Scylla-specific configuration (e.g., tablets vs. vnodes)

--- test/alternator/test_service_levels.py
+++ test/alternator/test_service_levels.py
@@ -6,8 +6,7 @@ import pytest
 from contextlib import contextmanager
 import time
 
-from .util import random_string, unique_table_name, metrics, get_metrics, check_increases_metric
-from .test_cql_rbac import new_role, new_dynamodb
+from .util import random_string, unique_table_name, metrics, get_metrics, check_increases_metric, new_role, new_dynamodb
 
 # new_service_level() is a context manager for temporarily creating a new
 # service level with a unique name and attaching it to the given role.
