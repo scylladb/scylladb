e99324c267850708388acead5db1e3a16c8e4247
Petr Gusev <petr.gusev@scylladb.com>
2026-09-05 09:47:32 +0200

    raft: replace _applied_index_changed with wait_for_apply()
    start() waited for the applier fiber to reach the persisted
    commit index on a condition variable the applier fiber broadcast
    whenever the applied index advanced. wait_for_apply() provides
    the same wait through _awaited_indexes, resolved by
    signal_applied() at the same points, so the condition variable
    duplicated it. It was also not broken by abort(): a start()
    still waiting when abort() ran would never have returned, while
    an _awaited_indexes waiter fails with stopped_error.

--- raft/server.cc
+++ raft/server.cc
@@ -236,9 +236,6 @@ private:
     // Becomes true during start(), becomes false on abort() or a background error
     bool _is_alive = false;
 
-    // Signaled when apply index is changed
-    condition_variable _applied_index_changed;
-
     // Signaled when _snapshot_desc_idx is changed
     condition_variable _snapshot_desc_idx_changed;
 
@@ -535,9 +532,7 @@ future<> server_impl::start() {
 
     // Wait for all committed entries to be applied before returning
     // to make sure that the user's state machine is up-to-date.
-    while (_applied_idx < commit_idx) {
-        co_await _applied_index_changed.wait();
-    }
+    co_await wait_for_apply(commit_idx, nullptr);
 
     co_return;
 }
@@ -1564,7 +1559,6 @@ future<> server_impl::applier_fiber() {
             resolve_subsumed(_awaited_applies, batch);
         }
         _applied_idx = snp.idx;
-        _applied_index_changed.broadcast();
         signal_applied();
         _stats.sm_load_snapshot++;
     };
@@ -1665,7 +1659,6 @@ future<> server_impl::applier_fiber() {
             co_await override_snapshot_thresholds();
 
             _applied_idx = last_idx;
-            _applied_index_changed.broadcast();
             signal_applied();
             notify_waiters(_awaited_applies, batch_ids);
 
