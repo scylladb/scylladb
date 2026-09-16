1049ff17f5aff564c83a826819dfa5353fa5a998
Taras Veretilnyk <taras.veretilnyk@scylladb.com>
2026-09-16 08:47:06 +0200

    config: add tablet_streaming_min_batch_size_in_bytes
    Amount of migration mass which has to be streaming to or from a shard
    before the tablet count concurrency limits are enforced for it. Not used
    yet.

--- db/config.cc
+++ db/config.cc
@@ -1765,6 +1765,9 @@ db::config::config(std::shared_ptr<db::extensions> exts)
          "Maximum number of tablets which may be leaving a shard at the same time. Effecting only on topology coordinator. Set to the same value on all nodes.")
     , tablet_streaming_write_concurrency_per_shard(this, "tablet_streaming_write_concurrency_per_shard", liveness::LiveUpdate, value_status::Used, 2,
          "Maximum number of tablets which may be pending on a shard at the same time. Effecting only on topology coordinator. Set to the same value on all nodes.")
+    , tablet_streaming_min_batch_size_in_bytes(this, "tablet_streaming_min_batch_size_in_bytes", liveness::LiveUpdate, value_status::Used, 1073741824,
+         "Amount of tablet data (in bytes) which has to be streaming to or from a shard before the per-shard tablet count " \
+         "concurrency limits are enforced for it, or 0 to always enforce them.")
     , service_levels_interval(this, "service_levels_interval_ms", liveness::LiveUpdate, value_status::Used, 10000, "Controls how often service levels module polls configuration table")
 
     , audit(this, "audit", value_status::Used, "table",
--- db/config.hh
+++ db/config.hh
@@ -607,6 +607,7 @@ public:
     named_value<uint64_t> target_tablet_size_in_bytes;
     named_value<unsigned> tablet_streaming_read_concurrency_per_shard;
     named_value<unsigned> tablet_streaming_write_concurrency_per_shard;
+    named_value<uint64_t> tablet_streaming_min_batch_size_in_bytes;
 
     named_value<uint32_t> service_levels_interval;
 
