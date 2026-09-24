ac31c2d36312a0bd89a8af305f5490b3b352856d
Nadav Har'El <nyh@scylladb.com>
2026-09-24 09:38:22 +0300

    mv: start to move C++ materialized-views tests to Python
    This patch starts to work on issue #16134 - translating the materialized
    views CQL functional tests, originally written in C++, to Python.
    
    This patch only introduces a new test file,
    test/cqlpy/test_materialized_view_old.py. The following patches will
    start to move the tests, one by one.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

---
+++ test/cqlpy/test_materialized_view_old.py
@@ -0,0 +1,9 @@
+# Copyright 2016-present ScyllaDB
+#
+# SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.1
+
+# These file contains the original functional tests written for materialized
+# views, originally written in C++ but translated (see issue #16134) to Python.
+# The tests originally in view_schema_test.cc, view_complex_test.cc,
+# view_schema_ckey_test.cc and view_schema_pkey_test.cc were moved here.
+
